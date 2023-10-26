608ff21cbc8568cd92846b80830be5811bc41970
wonkyum-kim <dodo12120@naver.com>
2023-10-26 16:18:46 +0900

    feat: validateRacingCarNameQueryInput 작성 - 이름이 5글자가 넘어가는지 확인 - 이름이 유일한지 확인

--- src/RacingCar.service.js
+++ src/RacingCar.service.js
@@ -5,7 +5,9 @@ export default class RacingCarService {
     const RACING_CAR_NAME_QUERY_INPUT = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
     );
-    this.validateRacingCarNameQueryInput(RACING_CAR_NAME_QUERY_INPUT);
+    this.validateRacingCarNameQueryInput(
+      RACING_CAR_NAME_QUERY_INPUT.split(',')
+    );
     const RACING_CAR_TRY_QUERY_INPUT = await Console.readLineAsync(
       '시도할 횟수는 몇 회인가요?'
     );
@@ -13,7 +15,18 @@ export default class RacingCarService {
     this.play();
   }
 
-  validateRacingCarNameQueryInput(cars) {}
+  validateRacingCarNameQueryInput(cars) {
+    cars.forEach((car) => {
+      if (car.length >= 5) {
+        throw new Error('[ERROR] 이름은 5자 이하여야 합니다.');
+      }
+    });
+    cars.forEach((car, index) => {
+      if (cars.indexOf(car) !== index) {
+        throw new Error('[ERROR] 이름은 유일해야 합니다.');
+      }
+    });
+  }
 
   validateRacingCarTryQueryInput(tries) {}
 
