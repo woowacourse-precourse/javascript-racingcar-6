219cd0ff19a989f4041b90f3d80913cd20489494
dyddks <dyddks0115@naver.com>
2023-10-26 23:39:01 +0900

    feat: 함수 병합 완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import CreateCar from './CreateCar.js';
-
+import InputNumber from './InputNumber.js';
+import Racing from './Racing.js';
+import Winner from './Winner.js';
 class App {
   async play() {
     const car = await CreateCar();
+    const n = await InputNumber();
+    const racing = Racing(car, n);
+    Winner(car, racing);
   }
 }
 
