f03eb635023da71745854932daaf258983920ff4
Steadystudy <sgmin206@naver.com>
2023-10-27 03:09:40 +0900

    Feat: 콘솔 Message 구현

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const MESSAGE = Object.freeze({
+  ENTER_CAR_NAMES: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+  ENTER_TRY: '시도할 횟수는 몇 회인가요?\n',
+  OUTCOME: '실행 결과',
+  tellResult: ({ name, step }) => `${name} : ${'-'.repeat(step)}\n`,
+  announceWinners: (winners) => `최종 우승자 : ${winners.join(', ')}`,
+});
