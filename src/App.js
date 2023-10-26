08363477e14b928c7c3bf60f9386c0e62419499c
함지현 <gkawlgus01@naver.com>
2023-10-27 05:36:53 +0900

    fix: 자동차 이름 뒤 마지막에 쉼표(,)가 있으면 에러처리

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { Console, Random } from '@woowacourse/mission-utils'
 class App {
 	async play() {
 		const carNameInput = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+		if (carNameInput.trim().endsWith(',')) {
+			throw new Error('\x1b[31m[ERROR] 쉼표로 끝나면 안됩니다.\x1b[37m');
+		}
 		const carNames = carNameInput.split(',').map(carNameInput => carNameInput.trim());
 
 		const cars = carNames.map(carName => ({
 			name: carName,
 			messages: [],
 			maxDash: 0,
-		  }));
+		}));
 
 		for (const carName of carNames) {
 			if (carName.length >= 6) {
