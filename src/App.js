bc01a9ac0c0013c2f9d79de0e3620fff45f816fe
김미르 <kimmireu@gimmileuui-iMac.local>
2023-10-27 01:59:21 +0900

    chore : 함수 분리

--- src/App.js
+++ src/App.js
@@ -3,45 +3,27 @@ import { Text } from "./common/text";
 class App {
   async play() {
     const names = await MissionUtils.Console.readLineAsync(Text.INPUT_NAMES);
-    if (!isValidNames(names)) throw new Error(Text.ERROR);
     const number = await MissionUtils.Console.readLineAsync(Text.INPUT_NUMBER);
-    if (!isValidNumber(number)) throw new Error(Text.ERROR);
+    if (!isValidNames(names) || !isValidNumber(number))
+      throw new Error(Text.ERROR);
+
     const result = initResult(names);
-    MissionUtils.Console.print(Text.RESULT);
-    for (let i = 0; i < number; i++) {
-      for (let j = 0; j < result.length; j++) {
-        const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
-        if (randomNumber >= 4) result[j][1] += "-";
-      }
-      for (let j = 0; j < result.length; j++) {
-        MissionUtils.Console.print(`${result[j][0]} : ${result[j][1]}`);
-      }
-    }
-    const points = [];
-    const winners = [];
-    for (let j = 0; j < result.length; j++) {
-      points.push(result[j][1].length);
-    }
-    const max_point = Math.max(...points);
-    for (let j = 0; j < result.length; j++) {
-      if (result[j][1].length == max_point) winners.push(result[j][0]);
-    }
-    MissionUtils.Console.print(`${Text.WINNER} : ${winners.join(", ")}`);
+    doRace(number, result);
+
+    const maxPoint = getMaxPoint(result);
+    const winners = getWinners(result, maxPoint);
+    printResult(winners);
   }
 }
 
 const isValidNames = (names) => {
-  names = names.split(",");
-  for (let name of names) {
+  for (let name of names.split(",")) {
     if (name.length > 5) return false;
   }
   return true;
 };
 
-const isValidNumber = (number) => {
-  if (isNaN(number)) return false;
-  return true;
-};
+const isValidNumber = (number) => !isNaN(number);
 
 const initResult = (names) => {
   const result = [];
@@ -50,5 +32,37 @@ const initResult = (names) => {
   }
   return result;
 };
+
+const getMaxPoint = (result) => {
+  const points = [];
+  for (let j = 0; j < result.length; j++) {
+    points.push(result[j][1].length);
+  }
+  return Math.max(...points);
+};
+
+const getWinners = (result, maxPoint) => {
+  const winners = [];
+  for (let j = 0; j < result.length; j++) {
+    if (result[j][1].length === maxPoint) winners.push(result[j][0]);
+  }
+  return winners;
+};
+
+const doRace = (number, result) => {
+  MissionUtils.Console.print(Text.RESULT);
+  for (let i = 0; i < number; i++) {
+    for (let j = 0; j < result.length; j++) {
+      const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (randomNumber >= 4) result[j][1] += "-";
+    }
+    for (let j = 0; j < result.length; j++) {
+      MissionUtils.Console.print(`${result[j][0]} : ${result[j][1]}`);
+    }
+  }
+};
+
+const printResult = (winners) =>
+  MissionUtils.Console.print(`${Text.WINNER} : ${winners.join(", ")}`);
 
 export default App;
--- src/common/text.js
+++ src/common/text.js
@@ -1,7 +1,6 @@
 export const Text = {
-  INPUT_NAMES:
-    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
-  INPUT_NUMBER: "시도할 횟수는 몇 회인가요?\n",
+  INPUT_NAMES: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
+  INPUT_NUMBER: "시도할 횟수는 몇 회인가요?",
   RESULT: "실행 결과",
   ERROR: "[ERROR]",
   WINNER: "최종 우승자",
