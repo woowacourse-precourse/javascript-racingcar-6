b98de43c153e61079ff586c35bf8d6a29ecaf0cf
JunSeok <ppu1234@naver.com>
2023-10-26 20:49:14 +0900

    feat: 자동차 이름 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Console } from '@woowacourse/mission-utils';
+import { INFO_MESSAGE } from './message.js';
+
 class App {
-  async play() {}
+  constructor() {
+    this.carName = new Map();
+  }
+  async play() {
+    await this.getCarName();
+  }
+  async getCarName() {
+    const nameArr = (await Console.readLineAsync(INFO_MESSAGE.GET_CAR_NAME)).split(',');
+    nameArr.forEach((name) => this.carName.set(name, ''));
+  }
 }
 
 export default App;
