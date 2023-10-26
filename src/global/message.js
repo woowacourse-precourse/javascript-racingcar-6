579bd39790e147c3523051d7bcc47ec327d87f2a
TMInstaller <ajvldmlqjqclr@naver.com>
2023-10-26 17:01:57 +0900

    시스템메세지, 에러메세지 객체 생성

---
+++ src/global/message.js
@@ -0,0 +1,14 @@
+export const systemMessage = {
+  START: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
+  TRY_COUNT: "시도할 회수는 몇회인가요?",
+  RESULT: "실행 결과",
+  WINNER: "최종 우승자: ",
+};
+
+export const errorMessage = {
+  INVALID_CAR_NAME: "자동차 이름은 5자 이하만 가능합니다.",
+  INVALID_TRY_COUNT: "시도 횟수는 1이상의 숫자만 가능합니다.",
+  INVALID_NUMBER: "숫자만 입력 가능합니다.",
+  INVALID_CAR_NAME_DUPLICATE: "중복된 자동차 이름은 사용할 수 없습니다.",
+  INVALID_CAR_NAME_EMPTY: "자동차 이름은 필수입니다.",
+};
