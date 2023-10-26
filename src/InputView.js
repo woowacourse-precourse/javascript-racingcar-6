f8570143453881e52864602160664c21c8a1cc2d
JUNU <bokjunwoo@gmail.com>
2023-10-26 21:50:44 +0900

    feat: 사용자로부터 자동차 이름 입력 및 구분(',') 구현

---
+++ src/InputView.js
@@ -0,0 +1,18 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { GAME_MESSAGES } from './constants.js';
+
+const InputView = {
+  async inputGetCarNames() {
+    try {
+      const userInput = await MissionUtils.Console.readLineAsync(
+        GAME_MESSAGES.START
+      );
+      const carNames = userInput.split(',');
+      return carNames;
+    } catch (error) {
+      throw error;
+    }
+  },
+};
+
+export default InputView;
