34fd7a29bf22654e87c5fd4f02588ec8ee0eb989
wonkyum-kim <dodo12120@naver.com>
2023-10-26 17:07:27 +0900

    feat: play 작성 - 시도할 횟수만큼 게임을 진행하고 각 자동차에 대한 결과를 출력합니다.

--- src/RacingCar.service.js
+++ src/RacingCar.service.js
@@ -3,16 +3,19 @@ import { Random, Console } from '@woowacourse/mission-utils';
 export default class RacingCarService {
   async racingCarQuery() {
     const RACING_CAR_NAME_QUERY_INPUT = await Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
     );
     this.validateRacingCarNameQueryInput(
       RACING_CAR_NAME_QUERY_INPUT.split(',')
     );
     const RACING_CAR_TRY_QUERY_INPUT = await Console.readLineAsync(
-      '시도할 횟수는 몇 회인가요?'
+      '시도할 횟수는 몇 회인가요?\n'
     );
     this.validateRacingCarTryQueryInput(RACING_CAR_TRY_QUERY_INPUT);
-    this.play();
+    this.play(
+      RACING_CAR_NAME_QUERY_INPUT.split(','),
+      RACING_CAR_TRY_QUERY_INPUT
+    );
   }
 
   validateRacingCarNameQueryInput(cars) {
@@ -37,7 +40,21 @@ export default class RacingCarService {
     }
   }
 
-  play() {}
+  play(cars, tries) {
+    Console.print('\n실행 결과');
+    const result = {};
+    cars.forEach((car) => {
+      result[car] = '';
+    });
+    for (let i = 0; i < tries; ++i) {
+      cars.forEach((car) => {
+        result[car] += Random.pickNumberInRange(1, 9) >= 4 ? '_' : '';
+        Console.print(`${car} : ${result[car]}`);
+      });
+      Console.print('');
+    }
+    this.printResult(result);
+  }
 
-  printResult() {}
+  printResult(result) {}
 }
