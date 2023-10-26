f7b1b774c785fe49502bbf9a0296466cca7f8de7
leejaejae <leesengjie@naver.com>
2023-10-26 16:42:48 +0900

    feat(입력): 자동차 이름 입력 기능 구현
    - 사용자는 쉼표로 구분된 여러 자동차 이름을 입력할 수 있습니다.
    - 각 자동차 이름은 5글자 이하로 제한됩니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Random, Console } from '@woowacourse/mission-utils';
+
+
 class App {
-  async play() {}
+  async play() {
+    try{
+      const input = await Console.readLineAsync("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분됩니다.) ");
+      const carNames = input.split(',').map(name => name.trim())
+      
+    } catch (error){
+      Console.print(error.message);
+      throw error;
+    }
+  }
 }
 
 export default App;
