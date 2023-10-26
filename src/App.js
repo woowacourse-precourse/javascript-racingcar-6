f63f2231180eb40352cfff0ded459a07a50b293a
Jiwon Lee <giwon.yi339@gmail.com>
2023-10-26 16:32:34 +0900

    feat : 횟수 입력, 0부터 9까지

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    // this.numberOfTries();
+  }
+
+  async carParticipants() {
+    //자동자 이름은 5자 이하, 쉼표로 구분
+  }
+
+  async numberOfTries() {
+    // 0부터 9까지 입력
+    const inputTries = await Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요?"
+    );
+
+    if (inputTries < 0 || inputTries > 9) {
+      throw new Error("[ERROR]");
+    }
+    // 4이상부터 전진
+  }
+
+  async displayOutcome() {
+    // 우승사 1명 이상 가능, 쉼표로 구분
+  }
 }
 
 export default App;
+const app = new App();
+app.play();
