d98d83e243617dd46441a374bfcd82a6469028e8
hyunjun9788 <namhyunjun9788@gmail.com>
2023-10-26 19:41:30 +0900

    Feat: 경주할 자동차 입력값 받기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 class App {
-  constructor() {}
+  constructor() {
+    // const gameResult = {};
+  }
   async play() {
     Console.print(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     );
+    const carNameList = await this.inputCarName();
+    splitCarNames(carNameList);
+  }
+  async inputCarName() {
+    const carNames = await Console.readLineAsync("");
+    return carNames;
   }
 }
 
