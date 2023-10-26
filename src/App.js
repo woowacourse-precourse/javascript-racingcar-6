632f9e5160c45499799e3f5f3adaf27935884a1f
신준혁 <oridori2705@naver.com>
2023-10-26 23:45:03 +0900

    자동차 경주 전진 혹은 정지 기능 추가
    상태를 저장해서 해당 상태를 바꿔주면서 반복문을 수행함

--- src/App.js
+++ src/App.js
@@ -1,35 +1,67 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
-function App() {
+function App(initialState = {}) {
   this.play = async () => {
     const raceCarNameArr = await inputRaceCarName();
+    this.setState(raceCarNameArr);
+    console.log(this.state);
     const raceCount = await inputRaceCount();
+    raceStart(this.state, raceCount);
+  };
 
-    raceStart(raceCarNameArr, raceCount);
+  this.state = initialState;
+
+  this.setState = (nextState) => {
+    this.state = nextState;
   };
 
   const inputRaceCarName = async () => {
-    const raceCarNameArr = await Console.readLineAsync(
+    const tempRaceCarNameArr = await Console.readLineAsync(
       "경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)\n"
     );
-    return raceCarNameArr.split(",");
+    const raceCarNameArr = tempRaceCarNameArr.split(",");
+    return raceCarNameArr.map((car) => {
+      return { carName: car, goCount: "" };
+    });
   };
+
   const inputRaceCount = async () => {
     const raceCount = await Console.readLineAsync(
       "시도할 횟수는 몇 회인가요?\n"
     );
-    return raceCount;
+    return parseInt(raceCount);
   };
+
   const raceStart = (raceCarNameArr, raceCount) => {
     Console.print("실행결과");
-    let count = 1;
-    while (count > raceCount) {
-      raceCarGoOrStop(raceCarNameArr, raceCount);
+    let count = 0;
+    while (count <= raceCount) {
+      raceCarGoOrStop();
+      Console.print("\n");
       count++;
     }
   };
-  const raceCarGoOrStop = (raceCarNameArr, raceCount) => {
-    //...
+
+  const raceCarGoOrStop = () => {
+    this.state.forEach((car) => {
+      const { carName, goCount } = car;
+      checkGoCount(carName, goCount);
+
+      Console.print(`${carName} : ${goCount} `);
+    });
+  };
+
+  const checkGoCount = (carName, goCount) => {
+    const checkRandomCount = Random.pickNumberInRange(0, 9);
+    if (checkRandomCount >= 4) {
+      goCount += "-";
+      this.setState(findCarName(carName, goCount));
+    }
+  };
+  const findCarName = (carName, goCount) => {
+    return this.state.map((item) =>
+      item.carName === carName ? { ...item, goCount: goCount } : item
+    );
   };
 }
 
