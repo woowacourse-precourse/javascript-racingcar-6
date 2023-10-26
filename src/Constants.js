3e54a08b2e8528e4de9145fbbf2d2464ad062222
kimdw524 <kimdw524@naver.com>
2023-10-26 23:39:15 +0900

    feat(Constants): 게임 진행 메세지 상수 추가

---
+++ src/Constants.js
@@ -0,0 +1,8 @@
+export const MESSAGE = {
+  INPUT_CAR_NAMES: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+  INPUT_TRY_COUNT: '시도할 횟수는 몇 회인가요?\n',
+  GAME_RESULT: '실행 결과',
+  FINAL_WINNER: '최종 우승자',
+}
+
+export default { MESSAGE }
