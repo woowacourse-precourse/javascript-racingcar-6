8163212610b5f4dfe5fc1359b739a85717ff6b7c
today-is-first <diligent0644@gmail.com>
2023-10-26 16:12:38 +0900

    chore: 입력, 에러, 프린트 메시지 상수화

---
+++ src/Message.js
@@ -0,0 +1,19 @@
+const INPUT_MESSAGES = {
+  INPUT_CAR_NAME:
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+  INPUT_TRY_NUM: '시도할 횟수는 몇 회인가요?',
+};
+
+const PRINT_MESSAGES = {
+  PROGRESS_RESULT: '실행 결과',
+  WINNER_LIST: `최종 우승자 : ${우승자}`,
+  PROGRESS_PROCESS: `${이름} : ${진행상황}`,
+  FORWARD: '-'
+};
+
+const ERROR_MESSAGES = {
+  ERROR_NAME_LEN: '이름은 5글자 이하만 가능합니다.',
+  ERROR_NAME_SPACE: '공백 없이 입력해야 합니다.',
+  ERROR_NAME_INPUT: '입력 값이 없습니다. 이름을 입력해주세요.',
+  ERROR_NUM_INPUT: '입력 값이 숫자가 아닙니다.',
+};
