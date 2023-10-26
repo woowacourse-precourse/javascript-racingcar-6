550b0f40bbac166cfa16ab19a127b7a71b910338
조예진 <joyejin@joyejins-MacBook-Air.local>
2023-10-26 19:43:37 +0900

    feat: 사용자가 입력한 자동차 이동 횟수 가져오기

--- src/App.js
+++ src/App.js
@@ -14,11 +14,20 @@ class App {
     // Console.print(INPUT);
     const CAR_NAMES = INPUT.split(',');
 
+    // 예외처리
     if (CAR_NAMES.length < 2 || CAR_NAMES.some(carName => carName.length > 5)) {
       throw new Error('[ERROR]');
     }
 
     this.carNames = CAR_NAMES;
+  }
+
+  //사용자가 입력한 자동차 이동 횟수 가져오기
+  async userInputNumMoves() {
+    const INPUT = await Console.readLineAsync();
+    const NUM_MOVES = parent(INPUT);
+
+    return NUM_MOVES;
   }
 }
 
