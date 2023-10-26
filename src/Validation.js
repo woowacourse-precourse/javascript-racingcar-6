3eb1270b656f52fe61612031ca45785765b0b198
kimdw524 <kimdw524@naver.com>
2023-10-27 01:02:40 +0900

    feat(Validation): 자동차 이름 유효성 검사 추가

---
+++ src/Validation.js
@@ -0,0 +1,15 @@
+import { CAR, MESSAGE } from './Constants';
+
+const Validation = {
+  /**
+   * 자동차 이름의 유효성 검사
+   * @param {string} name 자동차 이름
+  */
+  validateCarName: (name) => {
+    if (name.length < CAR.NAME_MIN_LENGTH || name.length > CAR.NAME_MAX_LENGTH) {
+      throw new Error(MESSAGE.INVALID_CAR_NAME);
+    }
+  },
+}
+
+export default Validation;
