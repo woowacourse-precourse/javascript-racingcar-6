11e33f51a4a4c4b967c7747276b906c402b99311
TMInstaller <ajvldmlqjqclr@naver.com>
2023-10-27 07:14:31 +0900

    에러메세지 세분화

--- src/global/message.js
+++ src/global/message.js
@@ -5,11 +5,15 @@ export const systemMessage = {
   WINNER: "최종 우승자: ",
 };
 
-export const errorMessage = {
+export const errorCarMessage = {
+  INVALID_CAR_NAME_EMPTY: "[ERROR] 자동차 이름은 필수입니다.",
   INVALID_CAR_NAME_LENGTH: "[ERROR] 자동차 이름은 5자 이하만 가능합니다.",
-  INVALID_TRY_COUNT: "[ERROR] 시도 횟수는 1이상의 숫자만 가능합니다.",
-  INVALID_NUMBER: "[ERROR] 숫자만 입력 가능합니다.",
   INVALID_CAR_NAME_DUPLICATE:
     "[ERROR] 중복된 자동차 이름은 사용할 수 없습니다.",
-  INVALID_CAR_NAME_EMPTY: "[ERROR] 자동차 이름은 필수입니다.",
+};
+
+export const errorTryCountMessage = {
+  INVALID_TRY_COUNT_ISNUMBER: "[ERROR] 시도 횟수는 숫자만 입력 가능합니다.",
+  INVALID_TRY_COUNT_MIN: "[ERROR] 시도 횟수는 1 이상의 숫자만 가능합니다.",
+  INVALID_TRY_COUNT_TYPE: "[ERROR] 시도 횟수는 정수 형태의 숫자만 가능합니다.",
 };
