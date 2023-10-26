46f242fd9ccf89b9e88ec46705ff688f7ec6f94e
songhyunseop <gustjq99455352@gmail.com>
2023-10-26 23:00:46 +0900

    Feat: 자동차 이동여부 판단 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
 import App from "./App.js";
 
 class GamePlay {
@@ -8,10 +8,30 @@ class GamePlay {
     return INPUT;
   }
 
+  isCarMoved(cars) {
+    let carList = cars.split(",");
+    const CAR_POINT = {};
+    const CAR_MOVEPOINT = [];
+
+    carList.map((el, idx) => {
+      CAR_POINT[el] = 0;
+      const number = MissionUtils.Random.pickNumberInRange(0, 9);
+      CAR_MOVEPOINT.push(number);
+
+      if (number >= 4) CAR_POINT[el] += 1;
+    });
+
+    console.log(CAR_POINT);
+    console.log(carList);
+    console.log(CAR_MOVEPOINT);
+  }
+
   async getCarNames() {
     const CARS = await this.getUserInput(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     );
+
+    this.isCarMoved(CARS);
   }
 
   async getTryTimes() {
