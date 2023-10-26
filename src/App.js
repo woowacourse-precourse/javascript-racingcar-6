35357152d844db528a5c0758b1816ef40afdd367
leeyulgok <dbfrhr20@gmail.com>
2023-10-26 16:51:59 +0900

    feat: 게임 설정 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import Car from "./Car.js";
+
 class App {
   async play() {
-    console.log("init");
+    const names = await this.getCarNames();
+    const times = await this.getPlayTimes();
+  }
+
+  async getCarNames() {
+    try {
+      const names = MissionUtils.Console.readLineAsync(
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+      );
+      return names;
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  async getPlayTimes() {
+    try {
+      const times =
+        MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+
+      return times;
+    } catch (error) {
+      console.log(error.message);
+    }
   }
 }
 
