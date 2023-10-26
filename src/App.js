5d8af7ae2895a0776b17072cb0e7ed7c7760bb95
myeongin <mungin0926@naver.com>
2023-10-26 16:10:16 +0900

    [feat] 자동차 이름 입력 및 공백 제거

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Random, Console } from "@woowacourse/mission-utils";
 class App {
-  async play() {}
+  async play() {
+    Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    this.carNames = this.getCarNamesInput();
+  }
+
+  async getCarNamesInput() {
+    const carNames = await Console.readLineAsync("");
+    const splitCarNames = carNames.split(",");
+    const carNamesRemoveWhitespace = splitCarNames.map((name) => name.trim());
+    return carNamesRemoveWhitespace;
+  }
 }
 
 export default App;
