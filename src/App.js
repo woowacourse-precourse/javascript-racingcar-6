e31391760f3f0c530532bf11bfc8e3cb3f4f01a4
최진욱 <jinwook.fe@gmail.com>
2023-10-27 01:02:21 +0900

    [feat] 경주할 자동차 이름 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const getMemberData = async () => {
+      const memberInput = await MissionUtils.Console.readLineAsync(
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
+      );
+      const members = memberInput.split(",");
+
+      return members;
+    };
+
+    const memberData = await getMemberData();
+  }
 }
 
 export default App;
