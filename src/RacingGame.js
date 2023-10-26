bab9c68c45b13e338e41ab31d401758b4ca4ba9a
JUNU <bokjunwoo@gmail.com>
2023-10-27 02:03:47 +0900

    feat : 경주 게임의 우승자를 결정하고 출력에 필요한 구조를 정의

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -1,5 +1,8 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
 import InputView from './InputView.js';
+import OutputView from './OutputView.js';
 import RacingCar from './RacingCar.js';
+import { GAME_MESSAGES } from './constants.js';
 
 class RacingGame {
   constructor() {
@@ -19,9 +22,14 @@ class RacingGame {
   }
 
   play() {
+    MissionUtils.Console.print('');
+    MissionUtils.Console.print(GAME_MESSAGES.EXECUTION_RESULT);
     for (let i = 0; i < this.tryCount; i++) {
       this.cars.forEach((car) => car.move());
+      OutputView.printRoundResults(this.cars);
     }
+
+    OutputView.printFinalResult(this.cars);
   }
 }
 
