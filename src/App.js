85403fbc03d3fb305f3edc783a641506fd7c29d1
whai2125 <whai@whai2125ui-MacBookAir.local>
2023-10-26 19:18:23 +0900

    feat: add carname creater and name length validater

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    await carCreater();
+  }
 }
 
 export default App;
+
+async function carCreater() {
+  const CAR_NAME = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
+  const CAR_NAME_SPLIT = CAR_NAME.split(',');
+  validationNameLength(CAR_NAME_SPLIT);
+}
+
+function validationNameLength(names) {
+  for (let i = 0; i < names.length; i++) {
+    if (names[i].length > 5) {
+      throw new Error("[ERROR]")
+    }
+  }
+}
