e12ef905ada2135de68a822b3e94806927652d1c
Hyuk <poiu03290@gmail.com>
2023-10-26 23:16:16 +0900

    feat: add winner select Func

--- src/game/result.js
+++ src/game/result.js
@@ -1,8 +1,21 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 import gameRounds from './gameRounds.js';
 
 export default function result(carArr, count) {
     let strArr = [...Array(carArr.length).fill('')];
     for(let round = 0; round < count; round++) {
         gameRounds(carArr, strArr);
+        MissionUtils.Console.print('\n');
     }
+    let res = -1;
+    let winner = null;
+    for(const [index, score] of strArr.entries()) {
+        if(score.length > res) {
+            res = score.length;
+            winner = carArr[index];
+        }
+    }
+
+    console.log(winner);
 }
