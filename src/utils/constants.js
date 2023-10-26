799cf240632ae69d979855713694b1662ac3814e
thewronghand <penfreak77@gmail.com>
2023-10-27 07:02:32 +0900

    feat: 상수 파일 생성

---
+++ src/utils/constants.js
@@ -0,0 +1,26 @@
+const SYMBOLS = {
+  comma: ',',
+  commaWithSpace: ', ',
+  dash: '-',
+  divider: ' : ',
+};
+
+const GAME_NUMBERS = {
+  rangeMin: 0,
+  rangeMax: 9,
+  movementThreshold: 4,
+  carNameMaxLength: 5,
+};
+
+const GAME_MESSAGES = {
+  carNameQuery:
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+  movementCountQuery: '시도할 횟수는 몇 회인가요?',
+  resultHeader: '실행 결과',
+  winnerAnnouncementHeader: '최종 우승자 : ',
+};
+
+const ERROR_MESSAGES = {
+  invalidCarNameLength: `자동차 이름은 ${GAME_NUMBERS.carNameMaxLength} 이하만 가능합니다.`,
+  invalidMovementCountNumber: '숫자가 잘못된 형식입니다.',
+};
