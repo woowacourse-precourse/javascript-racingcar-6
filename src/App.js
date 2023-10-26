4a831c2e97051bb7bd39954f1aad5fa093f8f87a
thewronghand <penfreak77@gmail.com>
2023-10-27 08:03:17 +0900

    feat: App의 play 메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import RacingGameController from './controllers/RacingGameController.js';
+import RacingGame from './models/RacingGame.js';
+
 class App {
-  async play() {}
+  async play() {
+    const racingGame = new RacingGame();
+    const racingGameController = new RacingGameController(racingGame);
+    await racingGameController.initiate();
+  }
 }
 
 export default App;
