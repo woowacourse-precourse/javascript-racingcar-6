1c1392837c62bac7f3eead1ad566ea86734fd55f
kimdw524 <kimdw524@naver.com>
2023-10-27 00:41:35 +0900

    feat(Car): 전진 시도하는 메소드 추가

--- src/Car.js
+++ src/Car.js
@@ -1,5 +1,9 @@
+import { Random } from '@woowacourse/mission-utils';
+import { CAR } from './Constants.js';
+
 class Car {
   #name
+  #distance
 
   /**
    * @constructor 
@@ -7,6 +11,7 @@ class Car {
    */
   constructor(name) {
     this.#name = name;
+    this.#distance = 0;
   }
 
   /**
@@ -15,4 +20,13 @@ class Car {
   getName() {
     return this.#name;
   }
+
+  // 전진을 시도하는 메소드
+  goForward() {
+    if (Random.pickNumberInRange(...CAR.FORWARD_RANGE) >= CAR.FORWARD_CONDITION) {
+      this.#distance += 1;
+    }
+  }
 }
+
+export default Car;
