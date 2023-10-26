cade8e054fcd82173ee30fef6855eaef435f311e
Steadystudy <sgmin206@naver.com>
2023-10-27 04:07:30 +0900

    Feat: 경주 게임 App 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,51 @@
+import Assistant from './Assistant.js';
+import RancingCar from './RacingCar.js';
+
 class App {
-  async play() {}
+  participants;
+  assistant;
+  numberOfTry;
+
+  constructor() {
+    this.participants = [];
+    this.assistant = new Assistant();
+  }
+
+  async play() {
+    await this.decideParticipants();
+    await this.decideNumberOfTry();
+    await this.process();
+    this.announceResult();
+  }
+
+  async decideParticipants() {
+    const carList = await this.assistant.getCarList();
+    this.assistant.validateCarName(carList);
+    carList.split(',').forEach((name) => {
+      const car = new RancingCar(name);
+      this.participants.push(car);
+    });
+  }
+
+  async decideNumberOfTry() {
+    const number = await this.assistant.getNumberOfTry();
+    this.assistant.validateNumberOfTry(number);
+    this.numberOfTry = number;
+  }
+
+  async process() {
+    this.assistant.announceProcess();
+    for (let count = 0; count < this.numberOfTry; count++) {
+      await Promise.all(this.participants.map((car) => car.race())).then((value) =>
+        this.assistant.markSteps(value),
+      );
+    }
+  }
+
+  announceResult() {
+    const winners = this.assistant.selectWinners([...this.participants]);
+    this.assistant.announceWinners(winners);
+  }
 }
 
 export default App;
