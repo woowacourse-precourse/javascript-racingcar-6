9c82b246746395a39f668fa2af5ea76c2f407dfa
hyunjun9788 <namhyunjun9788@gmail.com>
2023-10-26 23:42:49 +0900

    Feat: 랜덤 숫자 생성 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 class App {
   constructor() {
-    // const gameResult = {};
+    this.name = [];
+    this.count = [];
+    this.randomNum = [];
+    this.gameResult = {};
   }
   async play() {
     Console.print(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     );
     const carNameList = await this.inputCarName();
-    convertStrToArr(carNameList);
+    this.name = convertStrToArr(carNameList);
+    console.log(this.name);
     Console.print("시도할 횟수는 몇 회인가요?");
     const attemptCount = await this.inputAttemptCount();
+    this.isValidAttemptCount(attemptCount);
     Console.print("");
     Console.print("실행 결과");
-    this.isValidAttemptCount(attemptCount);
+    this.createRandomNumber();
+
+    console.log(this.randomNum);
   }
+
   async inputCarName() {
     const carNames = await Console.readLineAsync("");
     return carNames;
@@ -27,6 +35,13 @@ class App {
     if (isNaN(count)) {
       throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
     }
+  }
+  createRandomNumber() {
+    for (let i = 0; i < this.name.length; i++) {
+      const randomNum = Random.pickNumberInRange(0, 9);
+      this.randomNum.push(randomNum);
+    }
+    return this.randomNum;
   }
 }
 const convertStrToArr = (nameList) => {
