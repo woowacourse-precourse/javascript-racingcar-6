5cb97cd62b192febbda48c9dbdafcbe9ea194436
dyddks <dyddks0115@naver.com>
2023-10-26 23:45:41 +0900

    fix: 예외처리추가

--- src/InputNumber.js
+++ src/InputNumber.js
@@ -4,6 +4,12 @@ async function InputNumber() {
   Console.print('시도할 횟수는 몇 회인가요?');
   const n = Number(await Console.readLineAsync(''));
 
+  if (n <= 0) {
+    throw new Error('[ERROR]: 잘못된 입력입니다.');
+  }
+  if (Number.isInteger(n)) {
+    throw new Error('[ERROR]: 잘못된 입력입니다.');
+  }
   return n;
 }
 
