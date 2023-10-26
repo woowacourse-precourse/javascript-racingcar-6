b54a99d01af25714c328ef2b501907541160adbe
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-10-26 22:26:31 +0900

    refactor: carList에서 carNames로 변경

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import checkCarNames from "./checkCarNames";
 import inputCarNames from "./inputCarNames";
 
 class App {
   async play() {
-    const carList = inputCarNames();
+    const carNames = inputCarNames();
 
-    carList.forEach((car) => {
+    carNames.forEach((car) => {
       if (car.length > 5) {
         throw new Error("[ERROR] : 자동차 이름은 5자 이하만 입력 가능합니다.");
       }
--- src/inputCarNames.js
+++ src/inputCarNames.js
@@ -4,12 +4,12 @@ export default function inputCarNames() {
   const carInput = MissionUtils.Console.readLineAsync(
     "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
   );
-  let carList = [];
+  let carNames = [];
   if (typeof carInput === "string") {
-    carList = carInput.split(",");
+    carNames = carInput.split(",");
   } else {
     throw new Error("[ERROR] : 문자만 입력해 주세요.");
   }
 
-  return carList;
+  return carNames;
 }
