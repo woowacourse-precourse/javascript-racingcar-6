251be583d5dd697b453997203e22ad9f06cb17ec
TMInstaller <ajvldmlqjqclr@naver.com>
2023-10-27 06:43:53 +0900

    set ERROR message conventions

--- src/global/message.js
+++ src/global/message.js
@@ -6,9 +6,10 @@ export const systemMessage = {
 };
 
 export const errorMessage = {
-  INVALID_CAR_NAME: "자동차 이름은 5자 이하만 가능합니다.",
-  INVALID_TRY_COUNT: "시도 횟수는 1이상의 숫자만 가능합니다.",
-  INVALID_NUMBER: "숫자만 입력 가능합니다.",
-  INVALID_CAR_NAME_DUPLICATE: "중복된 자동차 이름은 사용할 수 없습니다.",
-  INVALID_CAR_NAME_EMPTY: "자동차 이름은 필수입니다.",
+  INVALID_CAR_NAME_LENGTH: "[ERROR] 자동차 이름은 5자 이하만 가능합니다.",
+  INVALID_TRY_COUNT: "[ERROR] 시도 횟수는 1이상의 숫자만 가능합니다.",
+  INVALID_NUMBER: "[ERROR] 숫자만 입력 가능합니다.",
+  INVALID_CAR_NAME_DUPLICATE:
+    "[ERROR] 중복된 자동차 이름은 사용할 수 없습니다.",
+  INVALID_CAR_NAME_EMPTY: "[ERROR] 자동차 이름은 필수입니다.",
 };
