3b0167ab013585ab7d254890fc012b7074e06cdc
Elisha0510 <sjeong35@knu.ac.kr>
2023-10-27 02:57:42 +0900

    feat: 자동차에 대한 객체를 생성하고 배열에 넣는 함수 추가

--- src/App.js
+++ src/App.js
@@ -21,15 +21,21 @@ class App {
     if(repeatNumber.includes(' ')) throw new Error('[ERROR] 공백은 넣지 말아주세요.');
   }
 
+  makeCarObject(carName){
+    const carObject = {
+      name: carName,
+      result: '',
+    }
+    this.raceResultArray.push(carObject);
+  }
+
   async play() {
     const userCarNames = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
     const userCarNamesArray = this.getUserCarNamesArray(userCarNames);
     this.checkUserCarNames(userCarNamesArray);
     const repeatNumber = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
     this.checkRepeatNumber(repeatNumber);
-    userCarNamesArray.forEach((element) => {
-      this.makeCarObject(element);
-    });
+    userCarNamesArray.forEach((element) => { this.makeCarObject(element); });
   }
 }
 
