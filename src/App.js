1a953735e395e468a32b815ae53be7b9ebb7ccdd
Elisha0510 <sjeong35@knu.ac.kr>
2023-10-27 02:39:32 +0900

    feat: 자동차 이름 배열로 반환하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ class App {
   constructor() {
     this.countRepeat = 0;
   }
-  checkUserCarNames(userCarNames) {
-    const userCarNamesArray = userCarNames.split(',');
+
+  getUserCarNamesArray(userCarNames) {
+    return userCarNames.split(',');
+  }
+
+  checkUserCarNames(userCarNamesArray) {
     userCarNamesArray.forEach((element) => {
       if(element.length > 5) throw new Error('[ERROR] 자동차 이름은 5자 이하로 작성해주세요.');
     });
@@ -18,7 +22,8 @@ class App {
 
   async play() {
     const userCarNames = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
-    this.checkUserCarNames(userCarNames);
+    const userCarNamesArray = this.getUserCarNamesArray(userCarNames);
+    this.checkUserCarNames(userCarNamesArray);
     const repeatNumber = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
     this.checkRepeatNumber(repeatNumber);
   }
