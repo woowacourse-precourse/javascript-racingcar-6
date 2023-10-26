52021b7053fa21fbc625cd285b03a026c761efc3
ssoferret <owosso1212@gmail.com>
2023-10-26 22:34:56 +0900

    경주차 생성자 함수 작성.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
   async play() {}
+
+  Car(name) {
+    this.name = name;
+    this.randomNumber;
+    this.pickRandomNumber = function(){
+      this.randomNumber = Random.pickNumberInRange(0, 9);
+    }
+    this.moveNumber;
+    this.addMoveNumber = function(){
+      if(this.randomNumber >= 4) this.moveNumber++;
+    }
+  }
 }
 
 export default App;
