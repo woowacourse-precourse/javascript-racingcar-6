f7164f246623b69906fb77d63e6644fb583e0e4a
solssak <gswoong1012@gmail.com>
2023-10-27 01:55:42 +0900

    feat: 기능 1 - 경주 참여 자동차 이름 받기 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async readUserRaceCarName() {
+    try {
+      const raceCarName = await MissionUtils.Console.readLineAsync(
+        `${"경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"}\n`,
+      );
+      const validNames = raceCarName.split(",").map((name) => name.trim());
+      for (const name of validNames) {
+        if (name.length > 5) {
+          throw new Error();
+        }
+      }
+    } catch (e) {
+      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    }
+  }
+
+  async play() {
+    await this.readUserRaceCarName();
+  }
 }
 
 export default App;
