e72fa859aef6302b2feb96a95423a4eab54735b7
정민규 <jmg081005@naver.com>
2023-10-26 16:27:08 +0900

    feat) 입력기능
    문자열을 입력받는 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async getInput(message) {
+    try {
+      const input = await MissionUtils.Console.readLineAsync(message);
+      return input;
+    } catch (error) {
+      throw "[ERROR] 입력을 받는 중 실패하였습니다.";
+    }
+  }
+
+  async play() {
+    const carStrList = await this.getInput(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+    const carList = carStrList.split(",");
+  }
 }
 
 export default App;
