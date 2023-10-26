45bb7dd1d08b318f7bcf52ca8583a68649987a8e
Elisha0510 <sjeong35@knu.ac.kr>
2023-10-27 02:08:16 +0900

    feat: 자동차 이름 예외처리 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
+  checkUserCarNames(userCarNames) {
+    const userCarNamesArray = userCarNames.split(',');
+    userCarNamesArray.forEach((element) => {
+      if(element.length > 5) throw new Error('[ERROR] 자동차 이름은 5자 이하로 작성해주세요.');
+    });
+  }
+
   async play() {
     const userCarNames = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
+    this.checkUserCarNames(userCarNames);
   }
 }
 
