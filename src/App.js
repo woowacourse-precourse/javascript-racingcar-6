2dab73df08a13d05fbf4815dd5a802a9aba6f513
ktyoung <ktyoung0507@gmail.com>
2023-10-26 15:47:08 +0900

    feat(user-input): 사용자 입력 기능 구현
    자동차 이름과 시도 횟수를 입력하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const carNamesInput = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) \n"
+    );
+    const roundsInputs = await Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요? \n"
+    );
+    const carNames = carNamesInput.split(",");
+    const round = Number(roundsInputs);
+  }
 }
 
 export default App;
