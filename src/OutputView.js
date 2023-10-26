5fd02b5d92de96c3f8e41ede243b0f1fee824bfc
JUNU <bokjunwoo@gmail.com>
2023-10-27 02:02:50 +0900

    feat : 자동차의 위치를 비교하여 우승자 결정 및 출력 기능 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,5 +1,5 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
-import { CAR_MOVEMENT_RESULT } from './constants';
+import { CAR_MOVEMENT_RESULT, GAME_MESSAGES } from './constants.js';
 
 const OutputView = {
   printRoundResults(cars) {
@@ -13,6 +13,15 @@ const OutputView = {
 
     MissionUtils.Console.print(roundResults.join('\n'));
     MissionUtils.Console.print('');
+  },
+
+  printFinalResult(cars) {
+    const maxPosition = Math.max(...cars.map((car) => car.position));
+    const winners = cars
+      .filter((car) => car.position === maxPosition)
+      .map((car) => car.name);
+
+    MissionUtils.Console.print(GAME_MESSAGES.FINAL_WINNER + winners.join(', '));
   },
 };
 
