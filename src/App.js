a67e78dac1f08f6c04661dbb93b3137348eda5be
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-10-26 22:28:37 +0900

    feat: checkCarNames.js 추가
    자동차 이름이 5자 이하인지 판단하는 함수

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import inputCarNames from "./inputCarNames";
 class App {
   async play() {
     const carNames = inputCarNames();
-
-    carNames.forEach((car) => {
-      if (car.length > 5) {
-        throw new Error("[ERROR] : 자동차 이름은 5자 이하만 입력 가능합니다.");
-      }
-    });
+    checkCarNames(carNames);
   }
 }
 
