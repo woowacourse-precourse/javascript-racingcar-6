0a6216f7e3ee5dcb59029a5169bfe12cd9d6cd20
leejaejae <leesengjie@naver.com>
2023-10-26 17:07:30 +0900

    feat(입력): 이동 횟수 입력 기능 구현
    - 사용자는 자동차들이 경주할 총 횟수를 입력할 수 있습니다.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import { Random, Console } from '@woowacourse/mission-utils';
 class App {
   async play() {
     try{
-      const input = await Console.readLineAsync("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분됩니다.) ");
+      const input = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) ");
       const carNames = input.split(',').map(name => name.trim())
+
+      const rounds = parseInt(await Console.readLineAsync("시도할 횟수는 몇 회인가요? "));
       
     } catch (error){
       Console.print(error.message);
