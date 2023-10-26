1a6b490b675ab2a8dd433b658224347e7d26f7cc
yui880 <kimyuna521@gmail.com>
2023-10-26 19:47:04 +0900

    feat(RacingCars): 자동차 이름에 맞게 Car 인스턴스 만들고, 배열에 저장하는 기능 구현

--- src/RacingCars.js
+++ src/RacingCars.js
@@ -1,7 +1,11 @@
+import Car from './Car.js';
+
 class RacingCars {
   #cars;
 
-  constructor(carNameList) {}
+  constructor(carNameList) {
+    this.#cars = carNameList.map((carName) => new Car(carName));
+  }
 }
 
 export default RacingCars;
