24501089e156af6c4377ecdd67ed5fa6d726cad7
juseong <jjs7280@gmail.com>
2023-10-26 22:34:02 +0900

    feat: 자동차 이름 입력 - 입력값 배열로 변환

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App {
     const userInput = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
     );
-    return userInput;
+    return userInput.split(',');
   }
 
   async play() {
