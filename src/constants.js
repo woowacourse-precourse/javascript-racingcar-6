1a21b490c0b34164e737c8d6b8a6be6303dbd957
JUNU <bokjunwoo@gmail.com>
2023-10-27 01:52:25 +0900

    add : 게임메시지, CAR_MOVEMENT_RESULT 상수 추가

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,8 @@
 export const GAME_MESSAGES = {
   START: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
   GET_TRIES: '시도할 횟수는 몇 회인가요?\n',
+  EXECUTION_RESULT: '실행결과',
+  FINAL_WINNER: '최종 우승자: ',
 };
 
 export const NUMBER_RANGE = {
@@ -9,3 +11,5 @@ export const NUMBER_RANGE = {
 };
 
 export const RANDOM_THRESHOLD = 4;
+
+export const CAR_MOVEMENT_RESULT = '-';
