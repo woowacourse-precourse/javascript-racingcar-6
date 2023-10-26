52f8b97490df04aaf1f777a425e4332c0d0e48db
ooherin <555ohr@naver.com>
2023-10-27 04:44:33 +0900

    feat: 이름 유효성 검사 구현

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { Console } from "@woowacourse/mission-utils";
 import selectWinner from "./utils/selectWinner.js";
 import moveWithRandomNum from "./utils/moveWithRandomNum.js";
+import { IsInputRightForm, IsNameLengthUnderFive } from "./utils/validation.js";
 
 class App {
   async play() {
     const carsInput =
       await Console.readLineAsync("경주할 자동차 이름을 입력하세요.");
+    IsInputRightForm(carsInput);
     const cars = carsInput.split(",");
     const carsWithMoveNum = {};
-    cars.forEach((player) => {
-      carsWithMoveNum[player] = 0;
+    cars.forEach((car) => {
+      IsNameLengthUnderFive(car);
+      carsWithMoveNum[car] = 0;
     });
     const lastCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
     let count = 0;
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,1 +1,9 @@
-const NameLengthUnderFive = () => {};
+export const IsNameLengthUnderFive = (name) => {
+  if (name.length > 5) throw new Error("[ERROR]이름이 올바른 형식이 아닙니다.");
+};
+//이름 인풋이 , 와 string으로만 이루어졌는지 검사
+export const IsInputRightForm = (inputs) => {
+  const regex = /^,+$/g;
+  if (!regex.test(inputs))
+    throw new Error("[ERROR]이름이 올바른 형식이 아닙니다.");
+};
