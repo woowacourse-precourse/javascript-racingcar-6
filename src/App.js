14e9811ae6d9074d2cd2f9f0f9cfbfabcb25c4c8
조예진 <joyejin@joyejins-MacBook-Air.local>
2023-10-26 19:28:24 +0900

    feat: 사용자가 입력한 자동차 이름 가져오기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
 class App {
+  //자동차 이름 초기화
+  constructor() {
+    this.carNames = [];
+  }
+
   async play() {}
+
+  //사용자가 입력한 자동차 이름 가져오기
+  async userInputCarNames() {
+    const INPUT = await Console.readLineAsync();
+    // Console.print(INPUT);
+    const CAR_NAMES = INPUT.split(',');
+
+    this.carNames = CAR_NAMES;
+  }
 }
 
 export default App;
