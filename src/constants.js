28f23d95dbdb72339b3f708fbe362304fcbc0459
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-10-26 21:59:24 +0900

    docs: create constants.js for long message

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+export const MESSAGE = {
+  enterCarName:
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
+  enterNumberOfRacing: "시도할 횟수는 몇 회인가요?",
+};
+
+export const ERROR_MESSAGE = {
+  enterRightCarName: "[ERROR] 자동차 이름을 5자 이하로 입력해주세요. ",
+  enterNaturalNumber: "[ERROR] 자연수를 입력해주세요.",
+};
