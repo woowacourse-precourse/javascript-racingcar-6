ed9dbb5f81dd5bf181504f5981ab5e205748ff91
Goongam <dlwjddn341@naver.com>
2023-10-26 16:49:06 +0900

    Feat: 이동에 성공했는지 여부 확인 함수

--- src/util/random.js
+++ src/util/random.js
@@ -1,5 +1,12 @@
-import { NUMBER_RANGE } from "../constants/constant";
+// import { NUMBER_RANGE } from "../constants/constant";
+import { Random } from "@woowacourse/mission-utils";
+import { NUMBER_RANGE } from "../constants/constant.js";
 
 export async function getRandomNumber() {
   return Random.pickNumberInRange(NUMBER_RANGE.MIN, NUMBER_RANGE.MAX);
+}
+
+export async function isMove() {
+  const RANDOM = await getRandomNumber();
+  return RANDOM >= 4 ? true : false;
 }
