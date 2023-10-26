b55f48b81e7ef521284b1b3f1c88b811c7225491
함지현 <gkawlgus01@naver.com>
2023-10-26 22:20:12 +0900

    feat: 자동차 이름 입력받기 완료
    쉼표기준으로 split 완료
    이름 앞뒤 공백 제거
    이름 6글자 이상 Error throw 후 종료 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console, Random } from '@woowacourse/mission-utils'
+
 class App {
-  async play() {}
+	async play() {
+		const carNameInput = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+
+		const carNames = carNameInput.split(',').map(carNameInput => carNameInput.trim());
+
+		for (const carName of carNames) {
+			if (carName.length >= 6) {
+				throw new Error('\x1b[31m[ERROR] 5글자 이하의 이름만 입력 가능합니다.\x1b[37m');
+			}
+			console.log(`${carName} :`);
+		}
+	}
 }
 
 export default App;
