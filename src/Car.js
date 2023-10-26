96f0fe2f5855144267b88917cb839773c447c9d4
youjeong Jeong <wjddbwjd1205@naver.com>
2023-10-26 22:39:28 +0900

    feat: Car 클래스에 자동자의 동작을 정하는 함수 작성

--- src/Car.js
+++ src/Car.js
@@ -1,6 +1,14 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class Car {
     constructor(carName) {
         this.carName = carName;
+    }
+
+    decideCarAction() {
+        let condition = MissionUtils.Random.pickNumberInRange(0, 9);
+        if(condition >= 4) return true;
+        else return false;
     }
 }
 
