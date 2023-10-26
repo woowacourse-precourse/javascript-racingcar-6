2e520eb8072510fc9543c918edaa88cc633520cd
songhyunseop <gustjq99455352@gmail.com>
2023-10-26 21:55:39 +0900

    Feat: 게임 시도 횟수 입력

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ class GamePlay {
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     );
   }
+
+  async getTryTimes() {
+    const TIMES = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+  }
 }
 
 export default GamePlay;
