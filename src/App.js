8698ef3f54c1d80888fdcca7798463ab80bb1aa9
gouz7514 <gouz7514@gmail.com>
2023-10-26 20:28:17 +0900

    Add(feature) : Car model을 활용해 기능 2. 입력한 n대만큼 자동차를 생성한다 를 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import { Console } from "@woowacourse/mission-utils";
+
 import InputController from "./Controller/InputController.js";
+import Car from "./Model/Car.js";
 
 class App {
   constructor() {
@@ -6,7 +9,13 @@ class App {
   }
 
   async play() {
+    // 1. 사용자의 입력을 받는다
     await this.InputController.init();
+
+    // 2. 입력된 n대만큼 자동차를 생성한다
+    const { carNames, tryCount } = this.InputController;
+    const car = new Car(carNames, tryCount);
+    Console.print(car);
   }
 }
 
