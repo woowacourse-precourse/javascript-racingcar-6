1a8ea135344fc77e6a2efed10c1e2b61d6224817
soyunim <togksk123@naver.com>
2023-10-26 22:35:48 +0900

    fix: 게임 출력 메세지 수정

--- src/message.js
+++ src/message.js
@@ -1,7 +1,7 @@
 const GAME_MSG = Object.freeze({
   GET_CARS_NAME:
     "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
-  GET_TYE_NUMBER: "시도할 횟수는 몇 회인가요?\n",
+  GET_TRY_TIMES: "시도할 횟수는 몇 회인가요?\n",
   EXECUTION_RESULT: "실행 결과",
   GAME_WINNER: "최종 우승자 : ",
 });
