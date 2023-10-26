246809efee1f09f9fc18afc85906f8f7c2c723d1
신준혁 <oridori2705@naver.com>
2023-10-26 22:52:18 +0900

    경주할 자동차 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-class App {
-  async play() {}
+import { Console, Random } from "@woowacourse/mission-utils";
+
+function App() {
+  this.play = async () => {
+    const raceCarNameArr = inputRaceCarName();
+  };
+
+  const inputRaceCarName = async () => {
+    const raceCarNameArr = await Console.readLineAsync(
+      "경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)"
+    );
+    return raceCarNameArr.split(",");
+  };
 }
 
 export default App;
