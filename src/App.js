5be7ceb69ba9cc44fbfd022a63647c18ed4703e5
ooherin <555ohr@naver.com>
2023-10-27 04:01:04 +0900

    feat: 이동값 입력받고 결과 프린트하기

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    const playersInput =
+    const carsInput =
       await Console.readLineAsync("경주할 자동차 이름을 입력하세요.");
-    const players = playersInput.split(",");
-    const playersWithMoveNum = {};
-    players.forEach((player) => {
-      playersWithMoveNum[player] = 0;
+    const cars = carsInput.split(",");
+    const carsWithMoveNum = {};
+    cars.forEach((player) => {
+      carsWithMoveNum[player] = 0;
     });
+    const lastCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+    let count = 0;
+    console.log(cars);
+    while (count <= lastCount) {
+      cars.forEach((car) => {
+        const randomNum = Random.pickNumberInRange(0, 9);
+        randomNum >= 4 ? carsWithMoveNum[car]++ : null;
+        const printMoveToLine = "-".repeat(carsWithMoveNum[car]);
+        Console.print(`${car} : ${printMoveToLine}`);
+      });
+      Console.print("");
+      count++;
+    }
   }
 }
 
-// const app = new App();
-// app.play();
+const app = new App();
+app.play();
 
 export default App;
