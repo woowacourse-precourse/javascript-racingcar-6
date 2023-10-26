0eaffc9dc7d5029ec1add17080fbf670f39bc7d0
ooherin <555ohr@naver.com>
2023-10-27 04:19:06 +0900

    feat: 우승자 판단 후 출력하기

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App {
     const lastCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
     let count = 0;
     console.log(cars);
-    while (count <= lastCount) {
+    while (count < lastCount) {
       cars.forEach((car) => {
         const randomNum = Random.pickNumberInRange(0, 9);
         randomNum >= 4 ? carsWithMoveNum[car]++ : null;
@@ -22,6 +22,19 @@ class App {
       Console.print("");
       count++;
     }
+    let winner = [];
+    cars.forEach((car) => {
+      if (!winner.length) {
+        winner.push(car);
+      } else if (carsWithMoveNum[winner] === carsWithMoveNum[car]) {
+        winner.push(car);
+      } else if (carsWithMoveNum[winner] < carsWithMoveNum[car]) {
+        winner.length = 0;
+        winner.push(car);
+      } else {
+      }
+    });
+    Console.print(`최종 우승자 : ${winner.join(", ")}`);
   }
 }
 
