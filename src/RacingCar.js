68b493494dbbc12d5805dee3358b7ea90e660c6a
JUNU <bokjunwoo@gmail.com>
2023-10-26 22:14:39 +0900

    feat : move 메소드를 통해 자동차의 position 업데이트 기능 구현

--- src/RacingCar.js
+++ src/RacingCar.js
@@ -1,7 +1,16 @@
+import { generateRandomNumbers } from './utils.js';
+
 class RacingCar {
   constructor(name) {
     this.name = name;
     this.position = 0;
+  }
+
+  move() {
+    const random = generateRandomNumbers();
+    if (random >= 4) {
+      this.position++;
+    }
   }
 }
 
