7a10982b4e28bd2ea6b7f9b373f159dbc26f2f96
juseong <jjs7280@gmail.com>
2023-10-26 22:24:00 +0900

    feat: 자동차 이름 입력 - 사용자에게 자동차 이름의 입력 요청

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async getUserInput() {
+    const userInput = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+    );
+    return userInput;
+  }
+
+  async play() {
+    this.getUserInput();
+  }
 }
 
 export default App;
