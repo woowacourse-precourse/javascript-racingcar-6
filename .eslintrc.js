cd8a5c0fc5755946ec4cb820daf59373cee6d5cf
Kled <1017yu@daum.net>
2023-10-26 22:55:00 +0900

    style: eslint & prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2022,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  rules: {},
+};
