50e010bd7ca7602ce051ad227ec045405c3a1797
ooherin <555ohr@naver.com>
2023-10-27 03:56:07 +0900

    feat: 자동차 이름 입력받은 후 형식 변환

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const playersInput =
+      await Console.readLineAsync("경주할 자동차 이름을 입력하세요.");
+    const players = playersInput.split(",");
+    const playersWithMoveNum = {};
+    players.forEach((player) => {
+      playersWithMoveNum[player] = 0;
+    });
+  }
 }
+
+// const app = new App();
+// app.play();
 
 export default App;
