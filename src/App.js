aea5dc010a9044647599d90fe2669b3bad03cc77
TMInstaller <ajvldmlqjqclr@naver.com>
2023-10-27 04:41:31 +0900

    make racing method

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+import { systemMessage } from "./global/message";
+import { distanceNumber } from "./global/number";
+
 class App {
-  async play() {}
+  racing(cars, tryCount) {
+    for (let i = 0; i < tryCount; i++) {
+      for (let j = 0; j < cars.length; j++) {
+        let distance = Random.pickNumberInRange(
+          distanceNumber.MIN_DISTANCE_LENGTH,
+          distanceNumber.MAX_DISTANCE_LENGTH
+        );
+        if (distance >= 4) {
+          cars[j] += "-";
+        }
+        Console.print(`${cars[j]} : ${cars[j]}`);
+      }
+    }
+  }
+
+  async play() {
+    try {
+      // 여기서 시작 메세지 출력
+      let cars = await Console.readLineAsync(systemMessage.START);
+      cars = cars.split(",");
+      let tryCount = await Console.readLineAsync(systemMessage.TRY_COUNT);
+      tryCount = Number(tryCount);
+      this.racing(cars, tryCount);
+    } catch (error) {
+      throw error;
+    }
+  }
 }
 
 export default App;
