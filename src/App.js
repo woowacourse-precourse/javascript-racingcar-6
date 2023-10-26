a22c9f3dfd696bcacf47d5be4a401699352bd352
신준혁 <oridori2705@naver.com>
2023-10-26 22:57:29 +0900

    자동차 경주 시작 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,19 +2,34 @@ import { Console, Random } from "@woowacourse/mission-utils";
 
 function App() {
   this.play = async () => {
-    const raceCarNameArr = inputRaceCarName();
-    const raceCount = inputRaceCount();
+    const raceCarNameArr = await inputRaceCarName();
+    const raceCount = await inputRaceCount();
+
+    raceStart(raceCarNameArr, raceCount);
   };
 
   const inputRaceCarName = async () => {
     const raceCarNameArr = await Console.readLineAsync(
-      "경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)"
+      "경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)\n"
     );
     return raceCarNameArr.split(",");
   };
   const inputRaceCount = async () => {
-    const raceCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+    const raceCount = await Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요?\n"
+    );
     return raceCount;
+  };
+  const raceStart = (raceCarNameArr, raceCount) => {
+    Console.print("실행결과");
+    let count = 1;
+    while (count > raceCount) {
+      raceCarGoOrStop(raceCarNameArr, raceCount);
+      count++;
+    }
+  };
+  const raceCarGoOrStop = (raceCarNameArr, raceCount) => {
+    //...
   };
 }
 
