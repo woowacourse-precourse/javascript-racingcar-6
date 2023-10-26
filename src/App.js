3bbd3b64e953f41b53ebfb2114b78d49fd8ee33c
dongbin420 <dongbin420@gmail.com>
2023-10-26 22:00:57 +0900

    feat: setup initial structure for game

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import RacingGame from './components/RacingGame.js';
+
 class App {
-  async play() {}
+  async play() {
+    const RACING_GAME = new RacingGame();
+    RACING_GAME.start();
+  }
 }
 
 export default App;
---
+++ src/components/RacingGame.js
@@ -0,0 +1,5 @@
+class RacingGame {
+  start() {}
+}
+
+export default RacingGame;
