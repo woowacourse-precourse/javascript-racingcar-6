9d0395fdd15a6248ad2352d9e5632abd933bbe79
yui880 <kimyuna521@gmail.com>
2023-10-26 18:16:16 +0900

    refactor(RacingCarGame): 자동차 입력받는 기능 메서드로 분리
    startGame에는 게임 진행과 관련된 기능만 두고, 나머지 기능은 다른 메서드를 호출해서 사용하는 방식으로 만드는 것이 좋을 것 같아서 따로 분리해주었다.

--- src/RacingCarGame.js
+++ src/RacingCarGame.js
@@ -1,11 +1,18 @@
 import { Console } from '@woowacourse/mission-utils';
 import { MESSAGE } from './Constant.js';
+import Validator from './Validator.js';
 
 class RacingCarGame {
   async startGame() {
-    const carNames = await Console.readLineAsync(MESSAGE.enterCarNames);
-    const carNameList = carNames.split(',');
+    const carNameList = await this.#getCarNamesInput();
     const numOfAttempts = await Console.readLineAsync(MESSAGE.enterNumbersOfAttempts);
+  }
+
+  async #getCarNamesInput() {
+    const carNames = await Console.readLineAsync(MESSAGE.enterCarNames);
+    const splitCarNames = carNames.split(',');
+
+    return splitCarNames;
   }
 }
 
