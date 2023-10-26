fdf13aca60d6589b6a17446177fcf0fba484c58d
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-10-26 22:28:15 +0900

    feat: create car from name user entered

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Console } from "@woowacourse/mission-utils";
+
+import { MESSAGE } from "./constants.js";
+import Car from "./Car.js";
+
 class App {
-  async play() {}
+  async play() {
+    const name = await Console.readLineAsync(MESSAGE.enterCarName);
+    const cars = this.createCarArr(name);
+  }
+
+  createCarArr(name) {
+    let cars = [];
+
+    const splitedName = this.splitName(name);
+    splitedName.forEach((name) => cars.push(new Car(name)));
+
+    return cars;
+  }
+
+  splitName(name) {
+    return name.split(",");
+  }
 }
 
 export default App;
