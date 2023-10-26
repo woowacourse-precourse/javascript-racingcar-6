165c847cc8ea7a9b7f47f5ae35e7525bd4dcd227
Taeyooooon <eric568@naver.com>
2023-10-26 17:24:27 +0900

    feat: 경주할 자동차 이름 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  constructor() {
+    this.joinCars = [];
+  }
+
+  async play() {
+    await this.getJoinCarsArray();
+  }
+
+  async getJoinCarsArray() {
+    const cars = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    this.joinCars = cars.split(',');
+  }
 }
 
 export default App;
