93e65c37e423291f7f1d15d30f10261b76cb9b5c
Elisha0510 <sjeong35@knu.ac.kr>
2023-10-27 02:14:11 +0900

    fix: 시도할 횟수 입력 기능 메세지 형식 수정

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App {
   async play() {
     const userCarNames = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
     this.checkUserCarNames(userCarNames);
-    const repeatNumber = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?');
+    const repeatNumber = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
   }
 }
 
