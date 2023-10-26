2cad6279bc12501fb15c88e9c6808b8afd7a202f
yui880 <kimyuna521@gmail.com>
2023-10-26 18:21:06 +0900

    feat(Constant): 게임 내 사용될 숫자들을 저장할 Constant 상수 선언

--- src/Constant.js
+++ src/Constant.js
@@ -1,3 +1,7 @@
+export const Constant = {
+  maxNameLength: 5,
+};
+
 export const MESSAGE = {
   enterCarNames: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
   enterNumbersOfAttempts: '시도할 횟수는 몇 회인가요?\n',
