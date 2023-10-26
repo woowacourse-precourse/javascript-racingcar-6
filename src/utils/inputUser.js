f6c0fe0f558946ad1abea3831175de99f4da08fa
jomoo02 <jomootgrn88@naver.com>
2023-10-27 00:17:48 +0900

    feat: inputUser.js validate code
    inputCarNames, inputAttemptNumber에 validate 코드 추가

--- src/utils/inputUser.js
+++ src/utils/inputUser.js
@@ -1,17 +1,20 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
+import { validateInputAttemptNumber, validateInputCarNames } from '../validations/validateInputUser.js';
 
-const input = async () => {
-    return await MissionUtils.Console.readLineAsync();
+const input = async (printString = '') => {
+    return await MissionUtils.Console.readLineAsync(printString);
 };
 
 const inputCarNames = async () => {
     const carNames = await input();
+    validateInputCarNames(carNames);
 
     return carNames.split(',');
 };
 
 const inputAttemptNumber = async () => {
     const attemptNumber = await input();
+    validateInputAttemptNumber(attemptNumber);
 
     return Number(attemptNumber);
 }
