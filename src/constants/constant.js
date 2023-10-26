eee824aa3b667d7f048ead35d70ed38e58244263
Goongam <dlwjddn341@naver.com>
2023-10-26 16:37:13 +0900

    Feat: 숫자 범위, 입출력 메시지 상수 선언

---
+++ src/constants/constant.js
@@ -0,0 +1,14 @@
+export const NUMBER_RANGE = {
+  MIN: 0,
+  MAX: 9,
+};
+
+export const INPUT_MESSAGE = {
+  INPUT_NAME: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
+  TRY_COUNT: "시도할 횟수는 몇 회인가요?",
+};
+
+export const OUTPUT_MESSAGE = {
+  RESULT: "실행 결과",
+  FINAL_WINNER: "최종 우승자 : ",
+};
