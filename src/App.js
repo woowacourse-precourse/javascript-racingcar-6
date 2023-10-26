4460c0b1dc6c3db0922ec6b4ccd29422e8be9e27
JsPark <junseongpark.dev@gmail.com>
2023-10-26 16:06:06 +0900

    feat: add input carNames and validator for carNames

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { Random, Console } from '@woowacourse/mission-utils';
+import { isValidCarNames } from './utils.js';
+
 class App {
-  async play() {}
+  constructor() {
+    this.carNamesAndDistanceMap = new Map();
+  }
+
+  async setCarNames() {
+    const carNamesString = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
+    );
+
+    if (isValidCarNames(carNamesString)) {
+      const carNamesArray = carNamesString.split(',');
+
+      for (const carName of carNamesArray) {
+        this.carNamesAndDistanceMap.set(carName, 0);
+      }
+    }
+  }
+
+  async play() {
+    this.setCarNames();
+  }
 }
 
 export default App;
---
+++ src/utils.js
@@ -0,0 +1,22 @@
+function isEveryCarNamesUnique(carNamesArray) {
+  const carNamesSet = new Set(carNamesArray);
+
+  return carNamesSet.size === carNamesArray.length;
+}
+
+function isValidCarNames(carNamesString) {
+  const carNamesArray = carNamesString.split(',');
+
+  for (const carName of carNamesArray) {
+    if (carName.length > 5)
+      throw new Error('[ERROR] 자동차 이름은 5자 이하로 설정되어야 합니다.');
+  }
+
+  if (!isEveryCarNamesUnique(carNamesArray)) {
+    throw new Error('[ERROR] 중복된 자동차 이름이 존재합니다.');
+  }
+
+  return true;
+}
+
+export { isValidCarNames };
