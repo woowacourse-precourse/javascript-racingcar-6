8b23674ffbb3a393df07d25e392e685954a9b314
hellosehyun <sehyun030526@gmail.com>
2023-10-27 03:39:19 +0900

    feat: getPlayer와 getNumber 함수 추가
    인원들을 입력받아 배열로 리턴하는 getPlayer,
    횟수를 입력받아 숫자로 리턴하는 getNumber 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+    async play() {
+        Console.print(await this.getPlayer());
+    }
+
+    async getPlayer() {
+        const PLAYERS = await Console.readLineAsync(
+            "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+        );
+
+        return PLAYERS.split(",");
+    }
+
+    async getNumber() {
+        const NUMBER = await Console.readLineAsync(
+            "시도할 횟수는 몇 회인가요?"
+        );
+        return Number(NUMBER);
+    }
 }
 
 export default App;
