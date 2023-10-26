acd621f0ceeb91b93290b2e4d8a91425aa52e49c
UBiiiii <willowman55@gmail.com>
2023-10-26 15:53:56 +0900

    [feat] 구현 함수 선언

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 class App {
-  async play() {}
+  async play() {
+
+  }
+
+  async getRacer() {
+
+  }
+
+  doRace(racers, times) {
+
+  }
+
+  printCurrentRace(racers, length) {
+
+  }
+
+  EndRace(racers, length) {
+
+  }
 }
 
 export default App;
