67dc05ba5afc0adb070c508f82969cb3044facbc
Kled <1017yu@daum.net>
2023-10-26 21:26:59 +0900

    test: 자동차 이름 유효성 테스트 코드 추가

---
+++ __tests__/CarNameTest.js
@@ -0,0 +1,44 @@
+import InputValidator from '../src/models/InputValidator.js';
+import { ERROR_MESSAGES } from '../src/constants/index.js';
+
+describe('자동차 이름 유효성 테스트', () => {
+  it('자동차 이름이 5자 초과', () => {
+    const carNames = ['Tesla', 'Hyundai'];
+
+    expect(() => {
+      InputValidator.validateCarNames(carNames);
+    }).toThrow(ERROR_MESSAGES.tooLongCarName);
+  });
+
+  it('자동차 이름을 의도적으로 입력하지 않음', () => {
+    const carNames = ['Audi', ''];
+
+    expect(() => {
+      InputValidator.validateCarNames(carNames);
+    }).toThrow(ERROR_MESSAGES.emptyCarName);
+  });
+
+  it('글자 사이의 의도된 공백', () => {
+    const carNames = ['be    nz'];
+
+    expect(() => {
+      InputValidator.validateCarNames(carNames);
+    }).toThrow(ERROR_MESSAGES.tooLongCarName);
+  });
+
+  it('단일의 유효한 자동차 이름 입력', () => {
+    const carNames = ['레인지로버'];
+
+    expect(() => {
+      InputValidator.validateCarNames(carNames);
+    }).not.toThrow();
+  });
+
+  it('다수의 유효한 자동차 이름 입력', () => {
+    const carNames = ['GV70', 'G80', 'G90'];
+
+    expect(() => {
+      InputValidator.validateCarNames(carNames);
+    }).not.toThrow();
+  });
+});
