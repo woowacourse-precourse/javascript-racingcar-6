483aa596bd49b24baa98cefc7fc4955206b50e60
dongbin420 <dongbin420@gmail.com>
2023-10-26 22:02:16 +0900

    feat: get user input for game

--- src/components/RacingGame.js
+++ src/components/RacingGame.js
@@ -1,5 +1,19 @@
+import { Console } from '@woowacourse/mission-utils';
+import { GET_INPUT_MESSAGE } from '../utils/constans';
+
 class RacingGame {
-  start() {}
+  start() {
+    this.getUserInput();
+  }
+
+  async getUserInput() {
+    const USER_INPUT_CARS = await Console.readLineAsync(
+      GET_INPUT_MESSAGE.carName
+    );
+    const USER_INPUT_COUNT = await console.readLineAsync(
+      GET_INPUT_MESSAGE.tryCount
+    );
+  }
 }
 
 export default RacingGame;
---
+++ src/utils/constans.js
@@ -0,0 +1,5 @@
+export const GET_INPUT_MESSAGE = Object.freeze({
+  carName:
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분하며, 각 이름은 5자 이하여야 합니다.',
+  tryCount: '시도할 횟수는 몇 회인가요?',
+});
