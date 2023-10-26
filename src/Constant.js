018632cd8ecb00536a339cd4b5079d49f14c373a
yui880 <kimyuna521@gmail.com>
2023-10-26 17:41:08 +0900

    refactor(Constant): 각 메세지에 줄바꿈 추가

--- src/Constant.js
+++ src/Constant.js
@@ -1,6 +1,6 @@
 export const MESSAGE = {
-  enterCarNames: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
-  enterNumbersOfMove: '시도할 횟수는 몇 회인가요?',
-  executeResult: '실행 결과',
+  enterCarNames: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+  enterNumbersOfMove: '시도할 횟수는 몇 회인가요?\n',
+  executeResult: '실행 결과\n',
   finalWinner: '최종 우승자 :',
 };
