0eecd24f71f8400695f54a61a27a25897334432e
Hong Boem Sun <qjatjs123123@naver.com>
2023-10-26 20:44:11 +0900

    feat(racingcar) 0~9까지 렌덤 넘버 기능 구현

--- src/Controller/RacingCarGameController.js
+++ src/Controller/RacingCarGameController.js
@@ -2,6 +2,10 @@ import printMessage from "../View/OutputView.js";
 import { inputRacingCarName, inputGameCount } from "../View/InputView.js";
 import { GAME_MESSAGE } from "../Util/Message.js";
 
+function racingCarMove(racingCarList, gameCount){
+
+}
+
 async function racingcarGameStart(){
   const racingCarList = await inputRacingCarName(GAME_MESSAGE.INPUT_CARNAME);
   const gameCount = await inputGameCount(GAME_MESSAGE.INPUT_GAMECOUNT);
---
+++ src/Model/RacingCarGame.js
@@ -0,0 +1,5 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+function generateRandomNumber(){
+  console.log(MissionUtils.Random.pickNumberInRange(0, 9));
+}
