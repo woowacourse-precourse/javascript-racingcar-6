d7cda126fb185766d07a7d96c99f4ad1bdad04b9
JUNU <bokjunwoo@gmail.com>
2023-10-27 01:36:30 +0900

    fix : generateRandomNumber 함수에서 반환문을 추가하여 버그 수정

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
 export const generateRandomNumber = (min, max) => {
-  MissionUtils.Random.pickNumberInRange(min, max);
+  return MissionUtils.Random.pickNumberInRange(min, max);
 };
