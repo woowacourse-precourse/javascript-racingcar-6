f61de80134d1cf0abbd8e60ea71242f9797f7bed
Elisha0510 <sjeong35@knu.ac.kr>
2023-10-27 01:56:21 +0900

    feat: 자동차 이름 입력받는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const userCarNames = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
+  }
 }
 
 export default App;
