c50b57404f656c4d038cf0f1450060c328fd82e5
Kled <1017yu@daum.net>
2023-10-26 22:50:41 +0900

    feat: 쉼표를 기준으로 나눈 자동차 이름 입력에 대한 검증 조건 추가

--- src/models/InputValidator.js
+++ src/models/InputValidator.js
@@ -1,10 +1,16 @@
-import { ERROR_MESSAGES } from '../constants/index.js';
+import { ERROR_MESSAGES, rSpecialChar } from '../constants/index.js';
 
 class InputValidator {
   static validateCarNames(carNames) {
+    const nameSet = new Set();
+
     carNames.forEach(el => {
-      if (el.length > 5) throw new Error(ERROR_MESSAGES.tooLongCarName);
-      if (!el.length) throw new Error(ERROR_MESSAGES.emptyCarName);
+      if (el.length > 5) throw new Error(ERROR_MESSAGES.tooLongName);
+      if (!el.length) throw new Error(ERROR_MESSAGES.emptyName);
+      if (rSpecialChar.test(el)) throw new Error(ERROR_MESSAGES.noSpecialChar);
+      if (nameSet.has(el)) throw new Error(ERROR_MESSAGES.noDuplicateName);
+
+      nameSet.add(el);
     });
   }
 }
