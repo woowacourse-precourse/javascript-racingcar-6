e43372ff052f40a8a685b89009947fa9ddaa7d57
UBiiiii <willowman55@gmail.com>
2023-10-26 17:11:39 +0900

    [fix] test 간 발생한 오류 수정

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App {
   async play() {
     const racers = await this.getRacer();
     const times = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
-    if(isNaN(times))  throw new Error("[Error] 시도 횟수는 숫자 형식만 가능합니다.")
+    if(isNaN(times))  throw new Error("[ERROR] 시도 횟수는 숫자 형식만 가능합니다.")
 
     Console.print("\n실행결과");
     const finalLen = this.doRace(racers, times);
@@ -13,7 +13,7 @@ class App {
 
   async getRacer() {
     const racers = (await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n")).split(",");
-    if(racers.filter(el=>el.length>5).length>0)  throw new Error("[Error] 자동차 이름은 5자 이하로만 입력할 수 있습니다.");
+    if(racers.filter(el=>el.length>5).length>0)  throw new Error("[ERROR] 자동차 이름은 5자 이하로만 입력할 수 있습니다.");
     return racers;
   }
 
@@ -30,13 +30,13 @@ class App {
   getLength(length) {
     return length.map(len=>{
       const num = Random.pickNumberInRange(0,9);
-      return num>4 ? len+1 : len;
+      return num>=4 ? len+1 : len;
     })
   }
 
   printCurrentRace(racers, length) {
     racers.forEach((racer,idx)=>{
-      const lenPrint = "_".repeat(length[idx]);
+      const lenPrint = "-".repeat(length[idx]);
       Console.print(racer+" : "+lenPrint);
     })
   }
