d30e68aacd04f7d1b9d64662ec9f8df6d0b2e7f8
haesa <xdhaesa@gmail.com>
2023-10-27 04:27:06 +0900

    [Test]: 자동차 이름 가져오기 테스트 추가

--- __tests__/CarTest.js
+++ __tests__/CarTest.js
@@ -2,10 +2,14 @@ import Car from '../src/Car';
 
 let car;
 beforeEach(() => {
-  car = new Car();
+  car = new Car('haesa');
 });
 
 describe('자동차 테스트', () => {
+  test('자동차 이름 가져오기', () => {
+    expect(car.getName()).toBe('haesa');
+  });
+
   test('임의의 숫자가 4이상일 때 자동차 전진', () => {
     car.race(0);
     expect(car.getForwardCount()).toBe(0);
