23b3748b4cf0c1fb70577b8ce3dda6b446f75729
jomoo02 <jomootgrn88@naver.com>
2023-10-26 19:30:15 +0900

    feat: class Car
    Car 클래스 추가
    - carName, forwardDistance 프로퍼티를 가짐
    - getCarName(): carName 리턴
    - getForwardDistance(): forwardDistance 리턴
    - moveForward(): forwardDistance += 1

---
+++ src/Car.js
@@ -0,0 +1,23 @@
+class Car {
+    #carName;
+    #forwardDistance;
+    
+    constructor(carName) {
+        this.#carName = carName;
+        this.#forwardDistance = 0;
+    }
+
+    getCarName() {
+        return this.#carName;
+    }
+
+    getForwardDistance() {
+        return this.#forwardDistance;
+    }
+
+    moveForward() {
+        this.#forwardDistance += 1;
+    }
+}
+
+export default Car;
