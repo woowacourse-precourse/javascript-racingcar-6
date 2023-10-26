0ab4d4d00737f8d66f9502b2aa4d565dacb05a40
Elisha0510 <sjeong35@knu.ac.kr>
2023-10-27 02:48:40 +0900

    feat: 한 자동차에 대한 객체를 만드는 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   constructor() {
     this.countRepeat = 0;
+    this.raceResultArray = [];
   }
 
   getUserCarNamesArray(userCarNames) {
@@ -26,6 +27,9 @@ class App {
     this.checkUserCarNames(userCarNamesArray);
     const repeatNumber = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
     this.checkRepeatNumber(repeatNumber);
+    userCarNamesArray.forEach((element) => {
+      this.makeCarObject(element);
+    });
   }
 }
 
