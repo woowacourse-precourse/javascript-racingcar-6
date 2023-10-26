20fa6e99ea9c97f07a9d48fac24e7299044e71d8
함지현 <gkawlgus01@naver.com>
2023-10-27 04:01:36 +0900

    feat: 자동차 전진 구현 완료
    경주 횟수 입력 구현 완료
    자동차마다 0~9 사이의 랜덤값 구현 완료
    4 이상일 경우 '각 자동차 이름 : -' 출력 완료

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import { Console, Random } from '@woowacourse/mission-utils'
 class App {
 	async play() {
 		const carNameInput = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
-
 		const carNames = carNameInput.split(',').map(carNameInput => carNameInput.trim());
+
+		const cars = carNames.map(carName => ({
+			name: carName,
+			messages: [],
+		  }));
 
 		for (const carName of carNames) {
 			if (carName.length >= 6) {
@@ -17,6 +21,24 @@ class App {
 		}
 		if (raceTry <= 0) {
 			throw new Error('\x1b[31m[ERROR] 1이상의 숫자만 입력 가능합니다.\x1b[37m');
+		}
+
+		Console.print('\n실행 결과');
+
+		let newRaceTry = raceTry;
+		while (newRaceTry > 0) {
+			for (let i = 0; i < cars.length; i++) {
+				const raceCarNumber = Random.pickNumberInRange(0, 9);
+				if (raceCarNumber >= 4) {
+					cars[i].messages.push('-');
+				}
+				if (raceCarNumber < 4) {
+					cars[i].messages.push(' ');
+				}
+				Console.print(`${cars[i].name} : ${cars[i].messages.join('')} ${raceCarNumber}`);
+			}
+			Console.print('');
+			newRaceTry--;
 		}
 	}
 }
