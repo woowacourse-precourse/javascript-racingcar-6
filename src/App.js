ca11d6639e6b363015c6afa8e60d9a61b939803e
yebin <yebind3308@gmail.com>
2023-10-26 18:00:30 +0900

    feat: 자동차 이름 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() { }
+  
+  async carNameInput() {
+    MissionUtils.Console.print('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
+    const carUserInput = await MissionUtils.Console.readLineAsync("");
+    const carName = carUserInput.split(",")
+
+    if (!carName || carName.length === 0 || carName.length > 5) {
+      throw new Error('[ERROR] 이름이 잘못된 형식입니다.');
+    }
+  }
 }
+
+
 
 export default App;
