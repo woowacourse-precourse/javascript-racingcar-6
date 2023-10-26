97f5d2b35ff8b2969267f6f73869018ae18f2d6c
soyunim <togksk123@naver.com>
2023-10-27 00:28:42 +0900

    feat: 자동차 이름, 게임 시도 횟수 입력받기

--- src/racingCar.js
+++ src/racingCar.js
@@ -1,16 +1,30 @@
 import { Console } from "@woowacourse/mission-utils";
-import { GAME_MSG } from "./message";
+import { ERROR_MSG, GAME_MSG } from "./message";
 
-export class RacingCar {
-  startGame() {
-    this.getCarsName();
-  }
+export const startGame = () => {
+  getCarsName();
+};
 
-  getCarsName() {
-    const carNames = Console.readLineAsync(GAME_MSG.GET_CARS_NAME);
+const getCarsName = async () => {
+  const cars = await Console.readLineAsync(GAME_MSG.GET_CARS_NAME);
+  Console.print(cars);
+  const carNames = cars.split(",");
+  Console.print(carNames);
+  for (let i = 0; i < carNames.length; i++) {
+    if (carNames[i].length > 5) {
+      throw new Error(ERROR_MSG.CAR_NAME_OVER_LENGTH);
+    }
   }
+  getTryTimes();
+};
 
-  endGame() {
-    return;
+const getTryTimes = async () => {
+  const tryTimes = Number(await Console.readLineAsync(GAME_MSG.GET_TRY_NUMBER));
+  if (isNaN(tryTimes)) {
+    throw new Error(ERROR_MSG.TRY_TIMES_NUMBER);
   }
-}
+};
+
+const endGame = () => {
+  return;
+};
