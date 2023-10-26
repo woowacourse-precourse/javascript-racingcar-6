0611701f95f14a6ab01d01cbeeb36eac5e83fa7a
Taeyooooon <eric568@naver.com>
2023-10-26 17:29:06 +0900

    feat: 시도할 횟수 입력받기

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
   constructor() {
     this.joinCars = [];
+    this.repeatNumber = '';
   }
 
   async play() {
     await this.getJoinCarsArray();
+    await this.getRepeatNumber();
   }
 
   async getJoinCarsArray() {
     const cars = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
     this.joinCars = cars.split(',');
+    return;
+  }
+
+  async getRepeatNumber() {
+    this.repeatNumber = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    return;
   }
 }
 
