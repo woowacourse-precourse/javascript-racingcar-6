10b3dfce45b4ba00d5c7c0b6fdc8a4067f9b5cab
thewronghand <penfreak77@gmail.com>
2023-10-27 08:00:28 +0900

    fix: 상수 변수 export 추가, query 메세지 줄바꿈 추가

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -14,13 +14,15 @@ const GAME_NUMBERS = {
 
 const GAME_MESSAGES = {
   carNameQuery:
-    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
-  movementCountQuery: '시도할 횟수는 몇 회인가요?',
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+  roundsNumberQuery: '시도할 횟수는 몇 회인가요?\n',
   resultHeader: '실행 결과',
   winnerAnnouncementHeader: '최종 우승자 : ',
 };
 
 const ERROR_MESSAGES = {
-  invalidCarNameLength: `자동차 이름은 ${GAME_NUMBERS.carNameMaxLength} 이하만 가능합니다.`,
-  invalidMovementCountNumber: '숫자가 잘못된 형식입니다.',
+  invalidCarNameLength: `[ERROR]자동차 이름은 ${GAME_NUMBERS.carNameMaxLength} 이하만 가능합니다.`,
+  invalidMovementCountNumber: '[ERROR]숫자가 잘못된 형식입니다.',
 };
+
+export { SYMBOLS, GAME_NUMBERS, GAME_MESSAGES, ERROR_MESSAGES };
