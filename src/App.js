8833e9f4132e9b9f3eed574d9450aef60574fa96
Kled <1017yu@daum.net>
2023-10-26 22:53:20 +0900

    feat: 입력받은 자동차 이름과 점수를`scoreBoard` 변수에 저장

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import Race from './models/Race.js';
+import InputView from './views/InputView.js';
+
 class App {
-  async play() {}
+  /**
+   * 레이스에 참가하는 자동차 이름과 점수를 나타내는 스코어보드
+   * @types { [key: string]: number }
+   */
+  #scoreBoard;
+
+  async play() {
+    const carNames = await InputView.getCarNames();
+    this.#scoreBoard = Race.getScoreBoard(carNames);
+  }
 }
 
 export default App;
