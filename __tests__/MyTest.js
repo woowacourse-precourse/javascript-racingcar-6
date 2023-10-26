90fc54e5312ffdad880e91fb1476a1d25ce96b9b
jaeml00 <jaeml0630@gmail.com>
2023-10-27 00:11:56 +0900

    feat: 배열의 요소가 Car 클래스인지 확인

--- __tests__/MyTest.js
+++ __tests__/MyTest.js
@@ -1,6 +1,6 @@
 import App from '../src/App.js';
 import { MissionUtils } from '@woowacourse/mission-utils';
-import CarRacingGame from '../src/CarRacingGame.js';
+import CarRacingGame from '../src/CarRacingGame';
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -22,12 +22,28 @@ describe('자동차 경주 게임', () => {
     expect(carNames).toEqual('pobi,woni');
   });
 
-  test('문자 길이 5이상', async () => {
+  test('문자 길이 5이상 혹은 0', async () => {
     const inputs = ['pobi,woni,,'];
     mockQuestions(inputs);
 
     // given
     const app = new App();
     await expect(app.play()).rejects.toThrow("[ERROR]");
+  });
+
+  test('배열에 저장되어 있는 값 확인', async () => {
+    const inputs = 'pobi,woni';
+    const expectedNames = inputs.split(',').map(name => name.trim());
+    mockQuestions([inputs]);
+
+    // given
+    const game = new CarRacingGame();
+    await game.gameStart();
+    const array = [...game.getCarNameArray()];
+
+    array.forEach((element, index) => {
+      expect(element.getName()).toEqual(expectedNames[index]);
+    });
+    
   });
 });
