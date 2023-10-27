12d60e0efa1067d09911630310397f4b53cafa0b
brgndy <brgndy96@gmail.com>
2023-10-27 21:01:28 +0900

    feat(play): App클래스에서 입력값 주입

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import { GAME_MESSAGES } from './constants/gameMessages.js';
 import { inputView } from './ViewController/InputView.js';
 import RacingGame from './models/RacingGame.js';
 import AllNamesOfCars from './models/AllNamesOfCars.js';
+import CountOfAttemp from './models/CountOfAttemp.js';
 
 class App {
   /**
-   * @type { RacingGame }
+   * @type { RacingGame } 게임 클래스
    */
 
   #game;
@@ -16,16 +17,26 @@ class App {
 
   #cars;
 
+  /**
+   * @type { number } 시도 횟수를 담을 필드
+   */
+
+  #attemps;
+
   async play() {
     this.#game = new RacingGame();
     await this.inputCarNames();
+    await this.inputCountOfAttemp();
   }
 
   async inputCarNames() {
     const valueOfCarNames = await inputView.readLine(GAME_MESSAGES.input_car_names);
     this.#cars = AllNamesOfCars.fromInputString(valueOfCarNames);
+  }
 
-    console.log(this.#cars.getAllCars());
+  async inputCountOfAttemp() {
+    const valueOfAttemps = await inputView.readLine(GAME_MESSAGES.input_count_of_attemp);
+    this.#attemps = CountOfAttemp.fromInputString(valueOfAttemps);
   }
 }
 
