f5e4ab9faa062d878903d4efd386396598a1c49d
jbilee <julishahn@gmail.com>
2023-10-27 17:52:21 +0900

    refactor: JS 컨벤션에 맞춰 코드 리펙토링 작업
    - `++` 연산자를 `+= 1`로 대체
    - 긴 코드에 라인 브레이킹 적용
    - 변하지 않는 변수를 `const`로 수정

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
-import Car from "./Car.js";
+import { MissionUtils } from '@woowacourse/mission-utils';
+import Car from './Car.js';
 
 class App {
   async play() {
     const carNames = await this.getCarNames();
     const playRounds = await this.getNumberOfRounds();
-
     const players = this.createPlayers(carNames);
 
     MissionUtils.Console.print('\n실행 결과');
 
     this.playGame(players, playRounds);
-
     const winner = this.getWinner(players);
 
     MissionUtils.Console.print(`최종 우승자 : ${winner}`);
   }
 
   async getCarNames() {
-    const input = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    const input = await MissionUtils.Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+    );
     const carNames = this.validateCarNames(input);
     return carNames;
   }
@@ -34,7 +34,8 @@ class App {
 
     const carNames = input.split(',');
 
-    if (carNames.includes('')) throw new Error('[ERROR] 자동차는 이름이 있어야 합니다.');
+    if (carNames.includes(''))
+      throw new Error('[ERROR] 자동차는 이름이 있어야 합니다.');
     carNames.forEach(this.checkNameLength);
 
     return carNames;
@@ -42,21 +43,24 @@ class App {
 
   validateRoundCount(input) {
     if (!Number(input)) throw new Error('[ERROR] 숫자가 아닙니다.');
-    if (!Number.isInteger(Number(input))) throw new Error('[ERROR] 정수가 아닙니다.');
+    if (!Number.isInteger(Number(input)))
+      throw new Error('[ERROR] 정수가 아닙니다.');
     return Number(input);
   }
 
   checkNameFormat(string) {
-    if (!string.includes(',')) throw new Error('[ERROR] 이름 형식이 맞지 않습니다.');
+    if (!string.includes(','))
+      throw new Error('[ERROR] 이름 형식이 맞지 않습니다.');
   }
 
   checkNameLength(name) {
-    if (name.length > 5) throw new Error('[ERROR] 자동차 이름은 5자 이하여야 합니다.');
+    if (name.length > 5)
+      throw new Error('[ERROR] 자동차 이름은 5자 이하여야 합니다.');
   }
 
   createPlayers(carNames) {
-    let players = [];
-    for (let i = 0; i < carNames.length; i++) {
+    const players = [];
+    for (let i = 0; i < carNames.length; i += 1) {
       const newPlayer = new Car(carNames[i]);
       players.push(newPlayer);
     }
@@ -64,7 +68,7 @@ class App {
   }
 
   playGame(players, rounds) {
-    for (let i = 0; i < rounds; i++) {
+    for (let i = 0; i < rounds; i += 1) {
       this.getPlayerMoves(players);
       this.printRoundResults(players);
     }
@@ -80,7 +84,7 @@ class App {
   }
 
   getPlayerMoves(players) {
-    for (let i = 0; i < players.length; i++) {
+    for (let i = 0; i < players.length; i += 1) {
       const randomNumber = this.getRandomNumber();
       const canMove = this.canMove(randomNumber);
 
@@ -89,7 +93,7 @@ class App {
   }
 
   printRoundResults(players) {
-    for (let i = 0; i < players.length; i++) {
+    for (let i = 0; i < players.length; i += 1) {
       const playerProgress = players[i].printMoves();
       const text = `${players[i].name} : ${playerProgress}`;
       MissionUtils.Console.print(text);
@@ -98,14 +102,14 @@ class App {
   }
 
   getWinner(players) {
-    const highestScore = players.reduce((max, cur, i) => {
+    const highestScore = players.reduce((max, cur) => {
       if (cur.moves > max) return cur.moves;
       return max;
     }, 0);
 
-    let winners = [];
+    const winners = [];
 
-    for (let i = 0; i < players.length; i++) {
+    for (let i = 0; i < players.length; i += 1) {
       if (players[i].moves === highestScore) winners.push(players[i].name);
     }
 
--- src/Car.js
+++ src/Car.js
@@ -1,18 +1,16 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
-
 class Car {
-    constructor(name) {
-        this.name = name;
-        this.moves = 0;
-    }
+  constructor(name) {
+    this.name = name;
+    this.moves = 0;
+  }
 
-    printMoves() {
-        let totalMoves = '';
-        for (let i = 0; i < this.moves; i++) {
-            totalMoves += '-';
-        }
-        return totalMoves;
+  printMoves() {
+    let totalMoves = '';
+    for (let i = 0; i < this.moves; i += 1) {
+      totalMoves += '-';
     }
+    return totalMoves;
+  }
 }
 
 export default Car;
