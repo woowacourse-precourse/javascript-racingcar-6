7056e29c5a6db10812e4433a6dc63639f4ebd92d
이진택 <109197023+LEEJINTAEK@users.noreply.github.com>
2023-10-27 18:49:17 +0900

    feat: 객체지향 프로그래밍으로 기능 구현 완료

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 import { nameError, numberError } from './gameError.js';
 
 class Car {
-  constructor(name) {
-    this.name = name;
+  constructor(carName) {
+    this.name = carName;
     this.position = 0;
   }
 
@@ -14,8 +14,54 @@ class Car {
     }
   }
 
-  carGetDisplay() {
-    return `${this.name} : ${'-'.repeat(this.position)}`;
+  getDisplay() {
+    MissionUtils.Console.print(`${this.name} : ${'-'.repeat(this.position)}`);
+  }
+}
+
+class RaceModel {
+  constructor() {
+    this.cars = [];
+    this.winners = [];
+  }
+
+  addCar(carName) {
+    const car = new Car(carName);
+    this.cars.push(car);
+  }
+
+  play(numAttempt) {
+    for (let attempt = 1; attempt <= numAttempt; attempt++) {
+      this.cars.forEach((car) => {
+        car.move();
+        car.getDisplay();
+      });
+      MissionUtils.Console.print('\n');
+    }
+  }
+
+  determineWinner() {
+    const maxPosition = Math.max(...this.cars.map((car) => car.position));
+    this.winners = this.cars.filter((car) => car.position === maxPosition).map((car) => car.name);
+  }
+}
+
+class RaceController {
+  constructor(model) {
+    this.model = model;
+  }
+
+  addCar(name) {
+    this.model.addCar(name);
+  }
+
+  play(numAttempt) {
+    this.model.play(numAttempt);
+    this.model.determineWinner();
+  }
+
+  getWinner() {
+    return this.model.winners.join('');
   }
 }
 
@@ -25,15 +71,31 @@ class App {
   }
 }
 
+const gamePlay = (carNames, gameAttempt) => {
+  MissionUtils.Console.print('\n실행 결과');
+
+  const raceModel = new RaceModel();
+  const raceController = new RaceController(raceModel);
+
+  carNames.forEach((carName) => raceController.addCar(carName));
+  raceController.play(gameAttempt);
+
+  MissionUtils.Console.print(`\n최종 우승자 : ${raceController.getWinner}`);
+};
+
 const gameStart = async () => {
   try {
-    const carName = await MissionUtils.Console.readLineAsync(
+    const nameInput = await MissionUtils.Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
     ); //배열
-    nameError(carName.split(','));
+    const carNames = nameInput.split(',');
+    nameError(carNames); //배열
 
     const gameAttempt = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?');
     numberError(gameAttempt); //문자열
+
+    //에러 통과 후 play
+    gamePlay(carNames, gameAttempt);
   } catch (error) {
     throw error;
   }
