9a23a78ae0dcffd6a6402ac110afda5b5f84dde8
j8won <joowoni99@gmail.com>
2023-10-27 20:11:03 +0900

    feat(Car): Create Car class with private fields and getters

---
+++ src/Car.js
@@ -0,0 +1,19 @@
+class Car {
+  #name;
+  #position;
+
+  constructor(name) {
+    this.#name = name;
+    this.#position = 0;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get position() {
+    return this.#position;
+  }
+}
+
+export default Car;
