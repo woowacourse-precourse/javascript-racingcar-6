6d31064cb19761dafbce8e869abbc7346a54a01f
Elisha0510 <sjeong35@knu.ac.kr>
2023-10-27 21:19:56 +0900

    refactor: 변수, 함수명 단어 2개 정도로 수정

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ class App {
     this.raceResultArray = [];
   }
 
-  getUserCarNamesArray(userCarNames) {
-    return userCarNames.split(",");
+  getCarNamesArray(carNames) {
+    return carNames.split(",");
   }
 
-  checkUserCarNames(userCarNamesArray) {
-    userCarNamesArray.forEach((element) => {
+  checkCarNames(carNamesArray) {
+    carNamesArray.forEach((element) => {
       if (element.length > 5)
         throw new Error("[ERROR] 자동차 이름은 5자 이하로 작성해주세요.");
     });
@@ -23,7 +23,7 @@ class App {
       throw new Error("[ERROR] 공백은 넣지 말아주세요.");
   }
 
-  makeCarObject(carName) {
+  makeObject(carName) {
     const carObject = {
       name: carName,
       result: "",
@@ -31,33 +31,33 @@ class App {
     this.raceResultArray.push(carObject);
   }
 
-  makeCarGoForwardOrStop(raceResultArray) {
+  makeRandomNumber(raceResultArray) {
     raceResultArray.forEach((element) => {
       const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
-      element = this.changeCarObjectByRandomNumber(element, randomNumber);
+      element = this.goForward(element, randomNumber);
     });
     return raceResultArray;
   }
 
-  changeCarObjectByRandomNumber(element, randomNumber) {
+  goForward(element, randomNumber) {
     if (randomNumber >= 4) {
       element.result = element.result.concat("-");
     }
   }
 
-  printCarRaceResult(raceResultArray) {
+  printResult(raceResultArray) {
     raceResultArray.forEach((e) => {
       MissionUtils.Console.print(`${e.name} : ${e.result}\n`);
     });
     MissionUtils.Console.print("\n");
   }
 
-  findNumberThatWentForward(raceResultArray) {
-    const forwardNumberArray = [];
+  findlengthArray(raceResultArray) {
+    const lengthArray = [];
     raceResultArray.forEach((element) => {
-      forwardNumberArray.push(element.result.length);
+      lengthArray.push(element.result.length);
     });
-    return forwardNumberArray;
+    return lengthArray;
   }
 
   pushWinnerIndexToArray(element, index, winnerIndexMaxNumber, winnerIndex) {
@@ -80,11 +80,11 @@ class App {
     return winnerIndex;
   }
 
-  findCarRaceWinnerIndexMax(forwardNumberArray) {
-    return Math.max(...forwardNumberArray);
+  findCarRaceWinnerIndexMax(lengthArray) {
+    return Math.max(...lengthArray);
   }
 
-  pushWinnerNameToArray(element, index, winnerIndexArray, winnerNameArray) {
+  pushWinnerName(element, index, winnerIndexArray, winnerNameArray) {
     if (winnerIndexArray.includes(index)) {
       winnerNameArray.push(element.name);
     }
@@ -94,7 +94,7 @@ class App {
   makeCarRaceWinnerArray(raceResultArray, winnerIndexArray) {
     let winnerNameArray = [];
     raceResultArray.forEach((element, index) => {
-      winnerNameArray = this.pushWinnerNameToArray(
+      winnerNameArray = this.pushWinnerName(
         element,
         index,
         winnerIndexArray,
@@ -104,11 +104,10 @@ class App {
     return winnerNameArray;
   }
 
-  checkCarRaceWinner(raceResultArray) {
+  checkWinner(raceResultArray) {
     /* 한 자동차당 전진한 수를 구하는 함수 */
-    const forwardNumberArray = this.findNumberThatWentForward(raceResultArray);
-    const winnerIndexMaxNumber =
-      this.findCarRaceWinnerIndexMax(forwardNumberArray);
+    const lengthArray = this.findlength(raceResultArray);
+    const winnerIndexMaxNumber = this.findCarRaceWinnerIndexMax(lengthArray);
     const winnerIndexArray = this.makeCarRaceWinnerIndexArray(
       raceResultArray,
       winnerIndexMaxNumber
@@ -120,33 +119,31 @@ class App {
     return carRaceWinnerArray;
   }
 
-  printCarRaceWinner(carRaceWinnerNameArray) {
-    const winnerNameString = carRaceWinnerNameArray.join(", ");
+  printWinner(winnerNameArray) {
+    const winnerNameString = winnerNameArray.join(", ");
     MissionUtils.Console.print(`최종 우승자 : ${winnerNameString}`);
   }
 
   async play() {
-    const userCarNames = await MissionUtils.Console.readLineAsync(
+    const carNames = await MissionUtils.Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     );
-    const userCarNamesArray = this.getUserCarNamesArray(userCarNames);
-    this.checkUserCarNames(userCarNamesArray);
+    const carNamesArray = this.getCarNamesArray(carNames);
+    this.checkCarNames(carNamesArray);
     const repeatNumber = await MissionUtils.Console.readLineAsync(
       "시도할 횟수는 몇 회인가요?\n"
     );
     this.checkRepeatNumber(repeatNumber);
-    userCarNamesArray.forEach((element) => {
-      this.makeCarObject(element);
+    carNamesArray.forEach((element) => {
+      this.makeObject(element);
     });
     while (this.countRepeat < repeatNumber) {
-      this.raceResultArray = this.makeCarGoForwardOrStop(this.raceResultArray);
-      this.printCarRaceResult(this.raceResultArray);
+      this.raceResultArray = this.makeRandomNumber(this.raceResultArray);
+      this.printResult(this.raceResultArray);
       this.countRepeat++;
     }
-    const carRaceWinnerNameArray = this.checkCarRaceWinner(
-      this.raceResultArray
-    );
-    this.printCarRaceWinner(carRaceWinnerNameArray);
+    const winnerNameArray = this.checkWinner(this.raceResultArray);
+    this.printWinner(winnerNameArray);
   }
 }
 
