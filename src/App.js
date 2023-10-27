89aeab4a0bcd797ff965705af18c00d5a6381a7a
hzzzi <hji2004@naver.com>
2023-10-27 22:41:52 +0900

    사용자 자동차 이름 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+// 예시: console.log(MissionUtils.Random.pickNumberInList([1, 2, 3]));
+
 class App {
   async play() {}
+
+  async getCarName() {
+    const carNames = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+    const carNameList = carNames.split(",");
+  }
 }
 
 export default App;
