4f92200ce8dcc4bba5eb65f52337709ff78022f2
6puritans9 <6puritans9@gmail.com>
2023-10-27 14:48:59 +0900

    feat(IO): Added basic IO features

---
+++ src/IO/Input.js
@@ -0,0 +1,10 @@
+import { Console } from "@woowacourse/mission-utils";
+
+export class Input {
+  async get() {
+    const input = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): "
+    );
+    return input;
+  }
+}
---
+++ src/IO/Output.js
@@ -0,0 +1,7 @@
+import { Console } from "@woowacourse/mission-utils";
+
+export class Output {
+  print(input) {
+    Console.print(input);
+  }
+}
