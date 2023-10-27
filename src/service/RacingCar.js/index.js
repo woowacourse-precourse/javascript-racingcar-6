3abcffd83093dd3142a199255fa6afa6c7ebfed0
jang_yoonsu <shagrat8546@gmail.com>
2023-10-28 03:20:31 +0900

    feat : promptTotalRounds 구현

--- src/service/RacingCar.js/index.js
+++ src/service/RacingCar.js/index.js
@@ -24,15 +24,38 @@ export class RacingCarGame {
     this.#setCars(carNames);
   }
 
+  async promptTotalRounds() {
+    const totalRoundsInput = await readLine(MESSAGES.PLACEHOLDER.ROUND);
+
+    this.#validateTotalRounds(totalRoundsInput);
+
+    this.#totalRounds = +totalRoundsInput;
+  }
+
   #setCars(carNames) {
     this.#cars = new Cars(carNames);
   }
 
   #validateCarNames(carName) {
     if (carName === "") throw new CustomError(MESSAGES.ERROR.CAR.NAME.EMPTY);
+
     if (carName.length > CAR_NAME_LENGTH_LIMIT)
       throw new CustomError(
         MESSAGES.ERROR.CAR.NAME.LENGTH(CAR_NAME_LENGTH_LIMIT)
       );
+  }
+
+  #validateTotalRounds(totalRounds) {
+    if (totalRounds === "")
+      throw new CustomError(MESSAGES.ERROR.TOTAL_ROUNDS.EMPTY);
+
+    if (isNaN(totalRounds))
+      throw new CustomError(MESSAGES.ERROR.TOTAL_ROUNDS.TYPE);
+
+    if (totalRounds <= 0)
+      throw new CustomError(MESSAGES.ERROR.TOTAL_ROUNDS.LENGTH);
+
+    if (!Number.isInteger(totalRounds))
+      throw new CustomError(MESSAGES.ERROR.TOTAL_ROUNDS.NOT_INTEGER);
   }
 }
