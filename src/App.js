b912afbbb9817fd5dfe0e179134d4a17ffaee1bf
yukyunglim <yukyung0826@naver.com>
2023-10-27 16:02:56 +0900

    feat : addCar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  // 자동차 이름 입력
+  constructor() {
+    MissionUtils.Console.print(
+      "경주 할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+    this.CAR_NAMES = [];
+  }
+
+  addCar(names) {
+    const carNames = names.split(",");
+    for (const carName of carNames) {
+      if (carName.trim().length > 5) {
+        throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+      }
+      this.CAR_NAMES.push({ name: carName, position: 0 });
+    }
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
