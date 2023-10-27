8fd227abea644ddb51dbf48c8b2b2ec6f2f0e555
최기환 <rlghks3004@gmail.com>
2023-10-27 16:04:01 +0900

    test(StringTest.js): 입력  문자열 검증 로직 테스트 추가
    입력 검증 로직 테스트 추가
     - 차 입력 유효성 검증
     - 횟수 유효성 검증

--- __tests__/StringTest.js
+++ __tests__/StringTest.js
@@ -1,30 +1,40 @@
-describe("문자열 테스트", () => {
-  test("split 메서드로 주어진 값을 구분", () => {
-    const input = "1,2";
-    const result = input.split(",");
+import { checkIsValidNumber, isUserInputValid, splitCarsInput } from '../src/utils.js';
 
-    expect(result).toContain("2", "1");
-    expect(result).toContainEqual("1", "2");
+describe('차 입력 문자열 유효성 검증 함수 테스트', () => {
+  test('입력에 공백 있으면 에러', () => {
+    const input = 'ab, abc, abcd';
+    expect(() => isUserInputValid(input)).toThrow('[ERROR]');
   });
 
-  test("split 메서드로 구분자가 포함되지 않은 경우 값을 그대로 반환", () => {
-    const input = "1";
-    const result = input.split(",");
-
-    expect(result).toContain("1");
+  test('이름에 숫자가 있으면 에러', () => {
+    const input = 'abc,ases1,abc';
+    expect(() => isUserInputValid(input)).toThrow('[ERROR]');
   });
 
-  test("substring 메서드로 특정 구간 값을 반환", () => {
-    const input = "(1,2)";
-    const result = input.substring(1, 4);
-
-    expect(result).toEqual("1,2");
+  test('이름이 5글자가 넘어가면 에러', () => {
+    const input = 'abcdef,abc,abcd';
+    expect(() => isUserInputValid(input)).toThrow('[ERROR]');
   });
 
-  test("at 메서드로 특정 위치의 문자 찾기", () => {
-    const input = "abc";
-    const result = input.at(0)
+  test('같은 이름이 존재한다면 에러', () => {
+    const input = 'abc,abc,abcd';
+    expect(() => splitCarsInput(input)).toThrow('[ERROR]');
+  });
 
-    expect(result).toEqual("a");
+  test('차가 10대 이상이라면 에러', () => {
+    const input = 'a,b,c,d,e,f,g,h,i,j,k,l,n,m';
+    expect(() => splitCarsInput(input)).toThrow('[ERROR]');
+  });
+});
+
+describe('횟수 입력 유효성 검증 함수 테스트', () => {
+  test('수가 50 이상이면 에러', () => {
+    const input = '51';
+    expect(() => checkIsValidNumber(input)).toThrow('[ERROR]');
+  });
+
+  test('숫자가 아니라면 에러', () => {
+    const input = '51a';
+    expect(() => checkIsValidNumber(input)).toThrow('[ERROR]');
   });
 });
