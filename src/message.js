6fe4831c7c6963cb20720fbeea19e5ac1840c9f5
hyun812 <sh980812@naver.com>
2023-10-28 01:02:14 +0900

    [refactor] 출력 메시지 수정

--- src/message.js
+++ src/message.js
@@ -7,5 +7,5 @@ export const GAME = {
   INPUT_CAR_NAME: `경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n`,
   INPUT_REPEAT_COUNT: `시도할 횟수는 몇 회 인가요?\n`,
   RESULT: "최종 우승자 : ",
-  START: "실행 결과\n",
+  START: "\n실행 결과\n",
 };
