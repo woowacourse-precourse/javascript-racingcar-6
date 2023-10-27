34585fa5f905bcecce48f98d58d290beea969b9c
leeyulgok <dbfrhr20@gmail.com>
2023-10-27 15:51:16 +0900

    refactor(car): 자동차 클래스 리팩토링(자동차에 관한 로직만 갖도록 함)

--- src/Car.js
+++ src/Car.js
@@ -1,24 +1,24 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
-
 class Car {
   constructor(name) {
-    if(name.length > 5) throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
-    this.name = name;
-    this.position = "";
+    this._name = name;
+    this._position = "";
   }
 
-  move() {
-    const value = MissionUtils.Random.pickNumberInRange(0, 9);
-    if(value > 4) this.position += "-";
+  move(value) {
+    if (value >= 4) this._position += "-";
+  }
+
+  printPosition() {
+    return `${this._name} : ${this._position}`;
   }
 
   getPosition() {
-    return MissionUtils.Console.print(`${this.name}: ${this.position}`);
+    return this._position;
   }
 
   getName() {
-    return this.name;
+    return this._name;
   }
-};
+}
 
 export default Car;
