9e446c93c5ca24d7b39bfdde9942f3e4ff1a3b6d
thumbthing <ilscc0902@gmail.com>
2023-10-28 02:10:28 +0900

    test: error의 test 케이스 추가

---
+++ __tests__/ErrorTest.js
@@ -0,0 +1,20 @@
+import errorhandler from '../src/util/error/errorhandler';
+
+describe('에러 처리 test', () => {
+  test('존재하는 에러코드에 대한 메세지 출력', () => {
+    // given
+    const code = 'noRacerName';
+    const output = '이름이 없는 자동차가 존재합니다.';
+
+    // when
+    expect(errorhandler(code)).rejects.toThrow(output);
+  });
+
+  test('존재하지 않는 에러코드에 대한 메세지 출력', () => {
+    //given
+    const code = 'testErrorNotDeclared';
+    const output = '예상치 못한 에러가 발생했습니다.';
+
+    expect(errorhandler(code)).rejects.toThrow(output);
+  });
+});
