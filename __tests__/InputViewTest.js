6499497449c537dcf91150804fc23b492c80c79c
0jenn0 <jenn0.6n@gmail.com>
2023-10-28 07:11:44 +0900

    Test: InputView class의 기능 테스트 코드 추가
    1. InputView가 비동기로 Console.readLineSync를 이용해 자동차 이름을 잘 입력받고 ,로 구분해 string 배열을 return하는지
    2. InputView가 비동기로 Console.readLineSync를 이용해 시도 횟수 입력을 잘 입력받고 그 값을 그대로 return하는지
    위 2가지 경우에 대한 테스트 코드를 추가하였습니다.

---
+++ __tests__/InputViewTest.js
@@ -0,0 +1,41 @@
+import InputView from "../src/Views/InputView.js";
+import { Console } from "@woowacourse/mission-utils";
+
+const mockQuestions = (inputs) => {
+  Console.readLineAsync = jest.fn();
+
+  Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  });
+};
+
+describe("InputView 기능 테스트", () => {
+  test("promptCarNames 메서드 - 자동차 이름 입력", async () => {
+    // given
+    const inputValues = ["daniel,hani"];
+
+    mockQuestions(inputValues);
+
+    // when
+    const inputView = new InputView();
+    const carNames = await inputView.promptCarNames();
+
+    // then
+    expect(carNames).toEqual(["daniel", "hani"]);
+  });
+
+  test("promptRaceRound 메서드 - 시도 횟수 입력", async () => {
+    // given
+    const inputValues = ["5"];
+
+    mockQuestions(inputValues);
+
+    // when
+    const inputView = new InputView();
+    const raceRound = await inputView.promptRaceRound();
+
+    // then
+    expect(raceRound).toEqual("5");
+  });
+});
