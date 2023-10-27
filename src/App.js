04087f1ba3810b27b58f5e2948df2aac3be89b8b
yukyunglim <yukyung0826@naver.com>
2023-10-27 16:09:45 +0900

    feat : inputNum

--- src/App.js
+++ src/App.js
@@ -46,6 +46,22 @@ class App {
       car.position++;
     }
   }
+
+  // 시도할 횟수
+  async inputNumber() {
+    const input = await MissionUtils.Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요?"
+    );
+    return parseInt(input);
+  }
+
+  // 자동차 이름 입력
+  async inputCarNames() {
+    const input = await MissionUtils.Console.readLineAsync(
+      "경주 할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+    return input;
+  }
 }
 
 const app = new App();
