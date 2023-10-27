13b857920b2eef60a0be179493b4ebb7ed453205
rkdrjsrks <rkdrjs71@naver.com>
2023-10-27 13:28:17 +0900

    App.js 비동기 함수명 작성 및 TODO 리스트 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 class App {
-  async play() {}
+  async play() {
+    //TODO : 자동차 객체 생성
+  }
+  async getUserInputString() {
+    //TODO : 한 줄을 입력 받고 쉼표로 구분하여 세개의 문자열을 만듦
+  }
+  async getUserInputInt(){
+    //TODO : 시도할 횟수를 입력 받음
+  }
+  async printResult(){
+    //TODO : 실행 결과를 출력
+  }
+  async randomInt(){
+    //TODO : 랜덤한 값을 반환
+  }
+  async goForward(){
+    //TODO : 자동차 전진
+  }
+  async printWinner(){
+    //TODO : 최송 우승자 안내 문구를 출력
+  }
 }
 
 export default App;
