cf4913193b8d1c08f75b1b17b9b24a1e043b0072
장호빈 <devhobin@gmail.com>
2023-10-27 16:59:20 +0900

    feat(App): App의 속성과 메서드들의 기본 탬플릿 작성
    - App이 가져야할 속성을 작성하였다
    - status 를 변경하는 transition 메서드를 작성하였다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
 class App {
-  async play() {}
+  #status = "idle";
+
+  #cars = [];
+
+  #round = null;
+
+  async play() {
+    this.#transition("setting");
+  }
+
+  async #transition(status) {
+    this.#status = status;
+    await this.#transitionEffect();
+  }
+
+  async #transitionEffect() {
+    switch (this.#status) {
+      case "idle": {
+        break;
+      }
+      case "setting": {
+        break;
+      }
+      case "start": {
+        break;
+      }
+      case "finish": {
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  }
 }
 
 export default App;
