31805392baf8f1c64c6eb60418ccb21cbd6c5677
Rki0 <pky00823@gmail.com>
2023-10-27 17:08:40 +0900

    refactor: 자동차명 입력시 문자열 양 끝 쉼표 문자 처리 테스트 문구 개선
    양 끝에 대해 판단해야하는데 맨 앞에 대해서만 테스트를 한다고 작성했기 때문에 혼동을 피하기 위해 변경하였다.
    또한, false를 반환하는 경우에 대해서도 테스트를 추가한다.

--- __tests__/ValidateTest.js
+++ __tests__/ValidateTest.js
@@ -1,7 +1,15 @@
 import Validate from "../src/Validate";
 import { ERROR } from "../src/constant/message";
 
-const inputs = [",pobi,woni", "pobi,woni,", ",pobi,woni,"];
+const inputsWithComma = [",pobi,woni", "pobi,woni,", ",pobi,woni,"];
+const inputsWithoutComma = [
+  "pobi,woni",
+  " pobi,woni",
+  "pobi,woni ",
+  " pobi,woni ",
+  "pobi ,woni",
+  "pobi, woni",
+];
 
 let validate;
 
@@ -10,14 +18,20 @@ beforeAll(() => {
 });
 
 describe("자동차명 입력에 대한 Validate 클래스 테스트", () => {
-  test("자동차 입력시 문자열 맨 앞에 쉼표(,)를 입력하면 안된다.", () => {
-    inputs.forEach((input) => {
+  test("자동차 입력시 문자열 양 끝에 한 쪽이라도 쉼표(,)를 입력하면 true 반환.", () => {
+    inputsWithComma.forEach((input) => {
       expect(validate.eachSideContainComma(input)).toBeTruthy();
     });
   });
 
-  test("자동차 입력시 문자열 맨 앞에 쉼표(,)를 입력하면 에러가 발생한다.", () => {
-    inputs.forEach((input) => {
+  test("자동차 입력시 문자열 양 끝에 쉼표(,)가 없으면 false 반환", () => {
+    inputsWithoutComma.forEach((input) => {
+      expect(validate.eachSideContainComma(input)).toBeFalsy();
+    });
+  });
+
+  test("자동차 입력시 문자열 양 끝에 한 쪽이라도 쉼표(,)를 입력하면 에러가 발생한다.", () => {
+    inputsWithComma.forEach((input) => {
       expect(() => validate.checkEnteredCarsIsValid(input)).toThrow(
         new Error(ERROR.CARS_START_AND_END_WITH_COMMA)
       );
