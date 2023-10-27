5c35197e74eb635a869560abfbffe2439bffec2b
jang_yoonsu <shagrat8546@gmail.com>
2023-10-28 03:11:40 +0900

    feat : promptCarNames 구현

--- src/service/RacingCar.js/index.js
+++ src/service/RacingCar.js/index.js
@@ -1,5 +1,7 @@
 import { MESSAGES } from "../../constants/messages";
+import { Cars } from "../../domain";
 import { CustomError } from "../../exceptions";
+import { readLine } from "../../utils";
 
 CAR_NAME_LENGTH_LIMIT = 5;
 
@@ -8,6 +10,23 @@ export class RacingCarGame {
   #totalRounds;
   #currentRound = 0;
   #winners;
+
+  async promptCarNames() {
+    const carNamesInput = await readLine(
+      MESSAGES.PLACEHOLDER.CAR.NAME(CAR_NAME_LENGTH_LIMIT)
+    );
+    const carNames = carNamesInput.split(",");
+
+    carNames.forEach((carName) => {
+      this.#validateCarNames(carName);
+    });
+
+    this.#setCars(carNames);
+  }
+
+  #setCars(carNames) {
+    this.#cars = new Cars(carNames);
+  }
 
   #validateCarNames(carName) {
     if (carName === "") throw new CustomError(MESSAGES.ERROR.CAR.NAME.EMPTY);
