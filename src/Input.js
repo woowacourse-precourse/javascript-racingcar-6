da2a4cb8725e7ec79ae7b85fdb2b7af4a30cafb0
Joseph Park <rkfnahs12e@gmail.com>
2023-10-27 21:38:53 +0900

    feat(src/Input.js) : getPlayCount() 함수 구현 src/Input.js에 새 함수를 구현 : - 시도할 횟수를 입력받고 반환하는 함수 구현

--- src/Input.js
+++ src/Input.js
@@ -1,13 +1,17 @@
 import { Console } from '@woowacourse/mission-utils';
 import { validateCarNameLength } from './Validation.js';
 // 5자 이하,
-async function getCarNamesList() {
+export async function getCarNamesList() {
 	const carNames = await Console.readLineAsync(
-		'경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분'
+		'경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n'
 	);
 	const carNamesList = carNames.split(',');
 	carNamesList.map((name) => {
 		validateCarNameLength(name);
 	});
 	return carNamesList;
+}
+export async function getPlayCount() {
+	const playCount = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+	return Number(playCount);
 }
