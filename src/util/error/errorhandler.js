3261df6b61dd429ae5dec1c4d8b08c278547ec89
thumbthing <ilscc0902@gmail.com>
2023-10-28 08:43:48 +0900

    refactor: export 방식 변경

--- src/util/error/errorhandler.js
+++ src/util/error/errorhandler.js
@@ -1,7 +1,7 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import errorList from './errorCode.js';
 
-function gameErrorhandler(code) {
+export function gameErrorhandler(code) {
   const occuredError = errorList.get(code);
 
   const errorMessage = occuredError !== undefined ? occuredError : '예상치 못한 에러가 발생했습니다.';
@@ -9,12 +9,9 @@ function gameErrorhandler(code) {
   const result = new Error(errorMessage);
 
   MissionUtils.Console.print(`[ERROR] ${errorMessage}`);
-
   return Promise.reject(result);
 }
 
-function defaultErrorHandler(error) {
+export function defaultErrorHandler(error) {
   return Promise.reject(error);
 }
-
-export { gameErrorhandler, defaultErrorHandler };
