e0c13ef20e4a1d519369780824dde5400d6b31bd
kimdaye77 <kimdaye77@naver.com>
2023-10-27 17:03:27 +0900

    feat: 경주 시도 횟수 입력 구현

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ class App {
     const userInput = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
+    //TODO: 예외사항
     const cars = userInput.split(",");
     cars.forEach((car) => {
       car = new Car(car);
       car.printPosition();
     });
+    const tryCount = await Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요?\n"
+    );
+    //TODO: 예외사항
   }
 }
 
