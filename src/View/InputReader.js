cec4e4374fc9aea6702738ce1ba3d5c422b8ba80
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-10-28 01:52:17 +0900

    refactor : user의 응답을 못받았을경우 예외처리
    fetch 함수를 사용할때 response.ok 를 확인하는것과 같이 느껴지도록 mocking
    
    isOk() : 응답이 없을때 false를 반환
    alert() : 미션에서 콘솔창만으로 view를 표시하기때문에 브라우저에서 alert 하는것처럼 만듬

--- src/View/InputReader.js
+++ src/View/InputReader.js
@@ -4,10 +4,24 @@ export default class InputReader {
   constructor() {}
 
   async #onRead(message) {
-    const response = await Console.readLineAsync(message);
-    if (response === null || response === undefined)
-      throw new Error('유저 응답 실패');
+    try {
+      const response = await Console.readLineAsync(message);
+      if (!isOk(response)) {
+        throw new Error(
+          `[ERROR] 예기치 못한 입력이 들어왔습니다 input :${response}`
+        );
+      }
 
-    return response;
+      return response;
+    } catch (error) {
+      alert(`\n${error.message} 라는 이유로 게임을 종료합니다 T_T`);
+      throw error;
+    }
   }
 }
+
+const isOk = (response) => response !== null || response !== undefined;
+
+const alert = (text) => {
+  console.log(text);
+};
