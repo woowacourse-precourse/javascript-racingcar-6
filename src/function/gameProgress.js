41d1100e4a948bce092823f661602cf92373fe94
limdumb <dlaruddls@nate.com>
2023-10-27 21:12:48 +0900

    feat: 게임진행 함수 생성

---
+++ src/function/gameProgress.js
@@ -0,0 +1,9 @@
+export const gaemProgress = (carsNames, carsNumber) => {
+  carsNames.forEach((el, index) => {
+    const carsName = Object.keys(el).join("");
+
+    if (carsNumber[index] > 4) {
+      el[carsName] += "-";
+    }
+  });
+};
