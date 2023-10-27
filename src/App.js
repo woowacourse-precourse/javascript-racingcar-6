95017b94cc84de85d679f2db93f2d6ec289a7399
이황제 <happy_kaiser@hotmail.com>
2023-10-28 01:02:10 +0900

    유저 이름 유효성 검사

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import * as MissionUtils from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    Console.print(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+  }
+
+  async setNumbers() {
+    const NUMBERS = [];
+  }
 }
+
+const checkCarNamesValid = (userInput) => {
+  const input = userInput.split(",");
+
+  const inputOverName = input.find((carName) => carName > 5);
+
+  if (inputOverName) {
+    throw new Error(`[ERROR] 입력이 올바른 형식이 아닙니다.`);
+  }
+  return input;
+};
 
 export default App;
---
+++ src/contants/message.js
@@ -0,0 +1,7 @@
+export const MESSAGE = {
+  start: `경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)`,
+  winner: `최종 우승자 `,
+  setTry: `시도할 횟수 `,
+  times: `시도할 횟수는 몇 회인가요?`,
+  error: `[ERROR] 입력이 올바른 형식이 아닙니다.`,
+};
