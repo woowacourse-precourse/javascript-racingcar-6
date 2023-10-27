c2df79699ccaa89562acb4624b226a5e84475ab3
[NDCC] Park jee-hoon <jh.m02h.park@misumi.co.kr>
2023-10-27 17:43:46 +0900

    feat: 자동차이름 개수에 맞는 랜덤값 추출 함수 생성

--- src/controller/RacingcarController.js
+++ src/controller/RacingcarController.js
@@ -1,15 +1,25 @@
 import { Console } from "@woowacourse/mission-utils";
+import Racing from '../model/Racing.js';
+import { Comm, MESSAGE } from '../model/Comm.js';
 import UserInput from '../view/UserInput.js';
 
 class RacingcarController{
+
+    #playCount
+
     constructor(){
-        
+        this.#playCount = 1;
     }
 
     async racingCarStart(){
         const RACING_CAR_NAME = await this.userInput.inputRacingcarName();
         const RACING_OUTPUT_NAME = this.comm.carNameOutPutUpdate(RACING_CAR_NAME);
         const RACING_COUNT = await this.userInput.inputRacingCount();
+        Console.print(MESSAGE.RACING_PLAY);
+
+        while(this.#playCount <= RACING_COUNT){
+            const RACING_CAR_FORWARD_NUMBER = this.racing.randomForwardCount(RACING_OUTPUT_NAME.length);
+        }
     }
 }
 
--- src/model/Racing.js
+++ src/model/Racing.js
@@ -1,6 +1,17 @@
+import { Random } from "@woowacourse/mission-utils";
+
 class Racing{
     constructor(){
 
     }
+
+    randomForwardCount = (num) => {
+        let forwardNumber = [];
+        for(let i = 0; i < num; i++){
+            forwardNumber.push(Random.pickNumberInRange(0, 9))
+        }
+        return forwardNumber;
+    }
+
 }
 export default Racing
