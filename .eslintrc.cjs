7309ae129949fa6111c2f1dbdab12c49d5a5b542
ryan-dia <cjfdnjs158@naver.com>
2023-10-27 21:57:06 +0900

    chore: 프리코스 요구사항을 위해 eslint 규칙 추가
    - indent 2까지만 허용

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,10 @@ module.exports = {
     ecmaVersion: 'latest',
   },
   rules: {
+    // 프리코스 프로그래밍 요구사항
+    // https://github.com/solo5star/javascript-lotto#-%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D-%EC%9A%94%EA%B5%AC-%EC%82%AC%ED%95%AD
+    'max-depth': ['error', 2],
+
     // ESM 사용으로 인한 파일 확장자 표기 의무화
     'import/extensions': ['error', 'always', { ignorePackages: true }],
 
