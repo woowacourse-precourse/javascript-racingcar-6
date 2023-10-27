8f2dfdd9d20af47945f06bb67bb8f686acc7abdf
Early Riser <tkfydtls464@gmail.com>
2023-10-27 19:18:36 +0900

    make function that increase car distance if random value is >= 4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
+function playGame(cnt) {
+  const DISTANCE = new Array(Number(cnt)).fill(0);
+  DISTANCE.forEach((_, i) => {
+    DISTANCE[i] = MissionUtils.Random.pickNumberInRange(0, 9);
+  });
+  return DISTANCE;
+}
+
 class App {
   async play() {
     const CAR_LIST_INPUT = await MissionUtils.Console.readLineAsync('');
@@ -11,6 +19,16 @@ class App {
     });
 
     const LOOP_COUNT = await MissionUtils.Console.readLineAsync('');
+
+    const DISTANCE_CAR_MOVE = new Array(Number(LOOP_COUNT)).fill(0);
+    for (let i = 0; i < LOOP_COUNT; i++) {
+      const DISTANCES = playGame(LOOP_COUNT);
+      DISTANCES.forEach((distance, index) => {
+        if (distance >= 4) {
+          DISTANCE_CAR_MOVE[index] += distance;
+        }
+      });
+    }
   }
 }
 
