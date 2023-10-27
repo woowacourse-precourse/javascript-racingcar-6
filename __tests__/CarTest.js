52b44647580d0bc9eec14e9fab515fac0c52136e
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-10-28 04:31:20 +0900

    test : getPorsitionResult() 테스트
    test-01 : 3번 움직였을때 '---' 반환 테스트

---
+++ __tests__/CarTest.js
@@ -0,0 +1,23 @@
+import Car from '../src/Car.js';
+
+describe('class Car test', () => {
+  let car;
+  beforeEach(() => {
+    const userName = '이두리';
+    car = new Car(userName);
+  });
+
+  describe('method test : getPositionResult() ', () => {
+    test('3번 움직였을때 --- 반환 테스트', () => {
+      const isMovable = true;
+
+      car.increasePosition(isMovable);
+      car.increasePosition(isMovable);
+      car.increasePosition(isMovable);
+
+      const result = car.getPositionResult();
+
+      expect(result).toBe('---');
+    });
+  });
+});
