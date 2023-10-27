55884f3831d5dc044660d801249b6bc406fa54ae
leedbswo107 <ljyu1@naver.com>
2023-10-28 00:28:51 +0900

    feat: 메시지 관리 업데이트

---
+++ src/Message.js
@@ -0,0 +1,12 @@
+const message = {
+    error : {
+        NAME_CREATION_ERROR : "[ERROR] 자동차의 이름은 5글자 이하여야 합니다.",
+    },
+    game : {
+        START_MESSAGE : "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
+        ATTEMPTS_NUMBER_QUESTION : "시도할 횟수는 몇 회인가요?"
+    }
+
+}
+
+export default message;
