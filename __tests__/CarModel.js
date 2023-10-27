0f36dfb8324bac63ea75c579856bf2969e6f3d40
PARK-CHANUK <doosan1224@naver.com>
2023-10-27 17:10:35 +0900

    test(CarModel): 전진여부 업데이트 기능 테스트 코드 작성 - 자동차가 전진했거나 정지했으면 상태를 업데이트

--- __tests__/CarModel.js
+++ __tests__/CarModel.js
@@ -4,11 +4,11 @@ describe('CarModel Test', () => {
   let carModel;
   beforeEach(() => {
     carModel = new CarModel();
+    const carNameList = 'pobi,ukgi';
+    carModel.makeCar(carNameList);
   });
 
   test('자동차 이름목록을 전달하면 자동차를 만든다.', () => {
-    const carNameList = 'pobi,ukgi';
-    carModel.makeCar(carNameList);
     expect(carModel.getCar()).toEqual([
       {
         name: 'pobi',
@@ -18,6 +18,14 @@ describe('CarModel Test', () => {
         name: 'ukgi',
         isMoves: [],
       },
+    ]);
+  });
+
+  test('자동차가 움직이거나 정지했으면 상태를 업데이트한다.', () => {
+    carModel.updateMove('pobi', true);
+    expect(carModel.getCar()).toEqual([
+      { name: 'pobi', isMoves: [true] },
+      { name: 'ukgi', isMoves: [] },
     ]);
   });
 });
