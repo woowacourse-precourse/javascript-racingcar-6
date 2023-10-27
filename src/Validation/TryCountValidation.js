49d7ac907b65edeb162b95f8e7d8026fa64b8d21
soosoo22 <lllllh00@naver.com>
2023-10-27 23:39:06 +0900

    feat: 시도 횟수 유효성 검사 기능 추가
    1. 빈 값을 입력한 경우
    2. 0보다 큰 수가 아닌 경우
    
    1과 2 중 하나라도 충족할 경우 에러 발생

---
+++ src/Validation/TryCountValidation.js
@@ -0,0 +1,7 @@
+import { ERROR } from '../constants.js';
+
+const validation = (count) => {
+  // 빈 값이면 에러
+  if (!count) throw new Error(ERROR.EMPTY_COUNT);
+  if (count <= 0) throw new Error(ERROR.MUST_ENTER_A_NUMBER_OVER_ZERO);
+};
