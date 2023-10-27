2150f32a3cee8962f04e845c184953073a23f1b6
seoyeon-jung <jungluchia1204@gmail.com>
2023-10-27 16:15:27 +0900

    feat: 자동차 입력받기
    - 자동차 이름을 입력하는 기능을 추가했습니다

--- src/App.js
+++ src/App.js
@@ -18,7 +18,21 @@ class Car {
 }
 
 class App {
-  async play() {}
+  constructor() {
+    this.cars = [];
+  }
+
+  async play() {
+    this.getCars();
+  }
+
+  getCars() {
+    Console.print(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+    const input = Console.readLineAsync();
+    this.cars = input.split(",").map((name) => new Car(name));
+  }
 }
 
 export default App;
