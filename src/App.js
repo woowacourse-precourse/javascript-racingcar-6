1126e8b76c57e65569a825fe950f65b8567808e8
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2023-10-27 21:02:36 +0900

    [feat] : Add 자동차 이름 구분

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ class App {
     // 1-a
     this.strCarName = await MissionUtils.Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    
     );
+    // 1-b
+    this.arrCarName = this.strCarName.split(",");
   }
 }
 
