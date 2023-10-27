d1e568c520862c0c0a37cd1357c575b8abe4ce96
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2023-10-27 18:08:18 +0900

    feat: 각 자동차가 전진한만큼 '-' 출력

--- src/RacingCar.js
+++ src/RacingCar.js
@@ -7,7 +7,7 @@ class RacingCar {
 
   async start() {
     await this.getCarsNames();
-    await this.getCountTimes();
+    await this.repeatForward();
   }
 
   async getCarsNames() {
@@ -24,7 +24,16 @@ class RacingCar {
     if(!checkValidNumber(input)){
       throw new Error('[ERROR] 올바른 숫자를 입력해주세요.');
     }
-    return input;
+    return Number(input);
+  }
+
+  async repeatForward(){
+    const input = await this.getCountTimes();
+    console.log(input);
+    for(let i = 0; i < input; i++){
+      await this.createNumber();
+      await this.printAllCars();
+    }
   }
 
   async createNumber() {
@@ -34,6 +43,28 @@ class RacingCar {
         car.moveForward();
       }
     })
+  }
+
+  async printAllCars() {
+    this.#carList.forEach(async (car) => {
+      const distance = car.getCarDistance();
+      const name = car.getCarName();
+      const slash = await this.getDistanceToSlash(distance);
+      this.carInfo(name,slash);
+    })
+    Console.print('\n');
+  }
+
+  carInfo(name, slash) {
+    Console.print(`${name} : ${slash}`);
+  }
+
+  async getDistanceToSlash(num) {
+    let slash = '';
+    while(slash.length < num){
+      slash += '-';
+    }
+    return slash;
   }
 }
 
