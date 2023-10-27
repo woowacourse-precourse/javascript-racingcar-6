57973dcd702f60a5f9ce7e6d8c3d185278898597
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2023-10-28 00:34:54 +0900

    refactor: 유효성 검사 방식 변경

--- src/Validation.js
+++ src/Validation.js
@@ -1,15 +1,22 @@
 const isLessThanFive = (list) => {
-  return !list.some((carName) => carName.length > 5);
+  if (list.some((carName) => carName.length > 5)) {
+    throw new Error('[ERROR] 이름은 5글자를 넘어선 안됩니다.');
+  }
 }
 
 const isDuplicated = (list) => {
-  return new Set(list).size === list.length;
+  if (new Set(list).size !== list.length) {
+    throw new Error('[ERROR] 중복된 이름이 있습니다.');
+  }
 }
 
 export const checkValidCarsName = (list) => {
-  return isDuplicated(list) && isLessThanFive(list);
+  isDuplicated(list);
+  isLessThanFive(list);
 }
 
 export const checkValidNumber = (num) => {
-  return !isNaN(num) && num > '0';
-} 
+  if (isNaN(num) || num <= '0') {
+    throw new Error('[ERROR] 유효하지 않은 숫자 입니다.');
+  }
+}
