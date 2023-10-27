3383d0f00b567e919c3873c95f99329987bb5c40
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-10-27 18:23:47 +0900

    :sparkles: feat: 자동차 인스턴스 생성과 라운드결과값 반환 기능추가

--- src/domain/CarRacingGame.js
+++ src/domain/CarRacingGame.js
@@ -1,17 +1,33 @@
+import Car from './Car.js';
+
 class CarRacingGame {
   #cars;
   #round;
 
+  // carNames 배열로 받아야됨. ex) ['Car1', 'Car2', 'Car3']
+  // Todo: 각 이름에 맞는 Car인스턴스 생성
   constructor(carNames, round) {
-    this.#cars = carNames;
+    this.#cars = carNames.map((carName) => new Car(carName));
     this.#round = round;
   }
 
   race() {}
 
-  getRoundResult() {}
+  // Todo: 해당 라운드의 Car인스턴스의 각 이름과 진행값을 return시킴
+  getRoundResult() {
+    return this.#cars.map((car) => {
+      const name = car.getName();
+      const progress = car.getProgress();
+
+      return console.log({ name, progress });
+    });
+  }
 
   getWinners() {}
 }
+
+// console.log 테스트를 위한 코드
+const racingGame = new CarRacingGame(['Car1', 'Car2', 'Car3'], 5);
+racingGame.getRoundResult();
 
 export default CarRacingGame;
