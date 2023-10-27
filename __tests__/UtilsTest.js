2f2af8a18890f1f38f4636f46480bdea1be87237
Rki0 <pky00823@gmail.com>
2023-10-27 23:55:54 +0900

    test: Utils 클래스 자동차명 배열을 Car 클래스 인스턴스로 반환해주는 로직 테스트

--- __tests__/UtilsTest.js
+++ __tests__/UtilsTest.js
@@ -1,4 +1,5 @@
 import Utils from "../src/Utils";
+import Car from "../src/Car";
 
 describe("Utils 클래스 테스트", () => {
   test("쉼표(,)로 구분된 자동차명을 입력받으면 공백을 제거하고 자동차명을 원소로 갖는 배열로 만든다.", () => {
@@ -15,5 +16,15 @@ describe("Utils 클래스 테스트", () => {
 
       expect(cars).toEqual(output);
     });
+  });
+
+  test("자동차명 배열을 입력받아 각각의 인스턴스를 생성하여 배열을 반환한다.", () => {
+    const input = ["qwer", "asdf", "zxcv"];
+
+    const output = [new Car("qwer"), new Car("asdf"), new Car("zxcv")];
+
+    const instanceArray = Utils.getEachCarInstanceArray(input);
+
+    expect(instanceArray).toEqual(output);
   });
 });
