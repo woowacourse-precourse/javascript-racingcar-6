b331c2547e4c2ba9b132b4feccb7998ba2977d6d
dabinchiii <dabinchi388@naver.com>
2023-10-28 05:23:58 +0900

    feat(Game): 자동차 이름을 입력받는 기능 추가
    - README.md에 자동차 이름의 입력에 관련된 제약 사항 추가
    - 자동차 이름 입력을 위한 안내 메시지를 출력함
    - 입력이 유효하지 않을 시 에러 throw하도록 함
    - 자동차 이름은 쉼표로 구분되고, 앞이나 뒤에 붙은 공백은 제거됨
    - 각 자동차 이름은 1자 이상 5자 이하이고 중복될 수 없음
    - 자동차는 한 대 이상 입력되어야 함

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import Game from "./Game.js";
 class App {
   async play() {
+    const game = new Game();
     try {
-      const game = new Game();
       await game.start();
     } catch (err) {
       throw new Error("[ERROR] " + err.message);
--- src/Game.js
+++ src/Game.js
@@ -1,3 +1,29 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class Game {
-  start = async () => {};
+  start = async () => {
+    const carNameList = await this.getCarNameList();
+    console.log(carNameList);
+  };
+
+  getCarNameList = async () => {
+    const input = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    const list = input.split(",").map((name) => name.trim());
+    if (input.length == 0 || !this.isCarNameListValid(list)) {
+      throw new Error("잘못된 형식입니다.");
+    }
+    return list;
+  };
+
+  isCarNameListValid = (list) => {
+    if (list.length == 0) return false;
+    if (new Set(list).size != list.length) return false;
+    return list.every((name) => {
+      return name.length > 0 && name.length <= 5;
+    });
+  };
 }
+
+export default Game;
