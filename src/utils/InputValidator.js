ff4148e23f44a272b1d13956d5256aae8c3dc47d
thewronghand <penfreak77@gmail.com>
2023-10-28 00:09:10 +0900

    refactor: InputValidator 내 메서드 분리

--- src/utils/InputValidator.js
+++ src/utils/InputValidator.js
@@ -3,15 +3,28 @@ import { ERROR_MESSAGES, GAME_NUMBERS } from './constants.js';
 
 class InputValidator {
   validateCarNames(names) {
+    this.#validateNameLength(names);
+    this.#validateBlankNames(names);
+    this.#validateUniqueNames(names);
+  }
+
+  #validateNameLength(names) {
     for (let name of names) {
       if (name.length > GAME_NUMBERS.carNameMaxLength) {
         throw new CustomError(ERROR_MESSAGES.carName.invalidLength);
       }
+    }
+  }
+
+  #validateBlankNames(names) {
+    for (let name of names) {
       if (name.trim().length === 0) {
         throw new CustomError(ERROR_MESSAGES.carName.blank);
       }
     }
+  }
 
+  #validateUniqueNames(names) {
     const uniqueNames = [...new Set(names)];
     if (uniqueNames.length !== names.length) {
       throw new CustomError(ERROR_MESSAGES.carName.duplicate);
@@ -19,10 +32,18 @@ class InputValidator {
   }
 
   validateRoundsNumber(roundsNumber) {
-    if (isNaN(roundsNumber)) {
+    this.#validateIsNumber(roundsNumber);
+    this.#validateNonNegativeValue(roundsNumber);
+  }
+
+  #validateIsNumber(value) {
+    if (isNaN(value)) {
       throw new CustomError(ERROR_MESSAGES.roundsNumber.notNumber);
     }
-    if (roundsNumber < 0) {
+  }
+
+  #validateNonNegativeValue(value) {
+    if (value < 0) {
       throw new CustomError(ERROR_MESSAGES.roundsNumber.negativeValue);
     }
   }
