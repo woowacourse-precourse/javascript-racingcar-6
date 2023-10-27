163c336e52f2ae81dcc3e52804396b67e417e4c1
brgndy <brgndy96@gmail.com>
2023-10-27 20:32:13 +0900

    feat(play): RacingGame 클래스 및 Car 클래스 바인딩

--- src/models/RacingGame.js
+++ src/models/RacingGame.js
@@ -1,12 +1,23 @@
+import Car from './Car.js';
 import AppError from '../error/AppError.js';
 import { ERROR_MESSAGES } from '../constants/errorMessages.js';
 
 class RacingGame {
+  /**
+   * @type { string[] }
+   */
+
   #cars;
+
+  /**
+   *
+   * @param { string[] } cars 입력 받은 모든 자동차 배열
+   */
 
   constructor(cars) {
     this.#cars = cars;
     this.validate();
+    this.initializeCars();
   }
 
   validate() {
@@ -14,7 +25,7 @@ class RacingGame {
   }
 
   validateOfDuplication() {
-    if (this.#cars.length !== new Set(this.#cars).size) {
+    if (this.#cars.length !== new Set([...this.#cars]).size) {
       throw new AppError(ERROR_MESSAGES.have_duplication);
     }
   }
@@ -22,6 +33,21 @@ class RacingGame {
   getCars() {
     return this.#cars;
   }
+
+  initializeCars() {
+    const carArray = this.#cars.map((carName) => {
+      const car = new Car(carName);
+      return car.getCar();
+    });
+
+    this.#cars = carArray;
+  }
+
+  /**
+   *
+   * @param { string } inputString
+   * @returns { RacingGame }
+   */
 
   static fromInputString(inputString) {
     const cars = inputString.split(',');
