e25b5f752cb8e91a60d1b32739b7ff99149dfd16
lurgi <wahoo9040@naver.com>
2023-10-27 15:38:39 +0900

    test(UnitTest): readCarsInput 통과 테스트
    Closes TestList 1.1-

--- __tests__/UnitTest.js
+++ __tests__/UnitTest.js
@@ -11,31 +11,39 @@ const mockQuestions = (inputs) => {
 };
 
 describe("유닛 테스트", () => {
-  test("readCarsInput 공백 에러", () => {
+  test("readCarsInput 공백 에러", async () => {
     const answer = [" car,car34,car1"];
     mockQuestions(answer);
 
     const carRace = new CarRace();
-    expect(carRace.readCarsInput()).rejects.toThrow(
+    await expect(carRace.readCarsInput()).rejects.toThrow(
       "[ERROR] 자동차 이름의 앞 뒤에는 공백이 있어선 안됩니다."
     );
   });
-  test("readCarsInput 길이 에러 ", () => {
+  test("readCarsInput 길이 에러 ", async () => {
     const answer = ["carcar,car34,car1"];
     mockQuestions(answer);
 
     const carRace = new CarRace();
-    expect(carRace.readCarsInput()).rejects.toThrow(
+    await expect(carRace.readCarsInput()).rejects.toThrow(
       "[ERROR] 자동차 이름의 길이는 5를 넘어선 안됩니다."
     );
   });
-  test("readCarsInput 중복 에러", () => {
+  test("readCarsInput 중복 에러", async () => {
     const answer = ["carca,carca,car1"];
     mockQuestions(answer);
 
     const carRace = new CarRace();
-    expect(carRace.readCarsInput()).rejects.toThrow(
+    await expect(carRace.readCarsInput()).rejects.toThrow(
       "[ERROR] 자동차 이름에 중복이 있습니다."
     );
+  });
+  test("readCarsInput 통과 테스트", async () => {
+    const answer = ["car1,car2,lurgi,tisi"];
+    mockQuestions(answer);
+
+    const carRace = new CarRace();
+    const cars = await carRace.readCarsInput();
+    expect(cars).toEqual(["car1", "car2", "lurgi", "tisi"]);
   });
 });
