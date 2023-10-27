8eecb3e79c14cfd50b9715419d132c8dedfd1458
deokisys <sjb378@me.com>
2023-10-27 12:25:35 +0900

    [feat] 자동차이름, 레이싱횟수 입력폼

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { MissionUtils,Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  constructor(){
+    this.cars = new Map();
+    this.try = 0;
+  }
+
+  async play() {
+
+    const carInput = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
+    this.carInit(carInput)
+
+    const tryInput = await Console.readLineAsync('시도할 횟수는 몇 회인가요?');
+    this.try = Number(tryInput)
+
+  }
+
+  carInit(input){
+    //,로 구분
+    let tmps = input.split(",");
+
+    //cars에 입력
+    tmps.forEach(ele => {
+      this.cars.set(ele,0);
+    });
+
+  }
 }
 
 export default App;
