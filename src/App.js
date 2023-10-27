b3116db2d05f69a018762e6e223484d60e1e06f1
Early Riser <tkfydtls464@gmail.com>
2023-10-27 22:22:26 +0900

    style: fix variable and constants to comply with the naming convention

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
 function playGame(cnt) {
-  const DISTANCE = new Array(Number(cnt)).fill(0);
-  DISTANCE.forEach((_, i) => {
-    DISTANCE[i] = MissionUtils.Random.pickNumberInRange(0, 9);
+  const randomNumList = new Array(Number(cnt)).fill(0);
+  randomNumList.forEach((_, i) => {
+    randomNumList[i] = MissionUtils.Random.pickNumberInRange(0, 9);
   });
-  return DISTANCE;
+  return randomNumList;
 }
 
 class App {
   async play() {
-    const CAR_LIST_INPUT = await MissionUtils.Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
-    );
-    const CAR_LIST = CAR_LIST_INPUT.split(',');
+    const CAR_LIST = (await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n')).split(',');
     CAR_LIST.forEach((CAR_NAME) => {
       if (CAR_NAME.length > 5) {
         throw new Error('[ERROR] 입력이 5자 이하가 아닙니다');
@@ -22,20 +19,21 @@ class App {
 
     const LOOP_COUNT = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
     MissionUtils.Console.print('실행 결과');
-    const DISTANCE_CAR_MOVE = new Array(CAR_LIST.length).fill(0);
+
+    const distanceCarMove = new Array(CAR_LIST.length).fill(0);
     for (let i = 0; i < LOOP_COUNT; i++) {
-      const DISTANCES = playGame(CAR_LIST.length);
+      const RANDOMNUMS = playGame(CAR_LIST.length);
       let print = '';
-      DISTANCES.forEach((distance, index) => {
-        if (distance >= 4) {
-          DISTANCE_CAR_MOVE[index] += distance;
+      RANDOMNUMS.forEach((randomNum, index) => {
+        if (randomNum >= 4) {
+          distanceCarMove[index] += randomNum;
         }
-        print += `${CAR_LIST[index]} : ${'-'.repeat(distance)}\n`;
+        print += `${CAR_LIST[index]} : ${'-'.repeat(randomNum)}\n`;
       });
       MissionUtils.Console.print(print);
     }
-    const WIN_DISTANCE = Math.max(...DISTANCE_CAR_MOVE);
-    const MEMBERS = CAR_LIST.filter((car, index) => DISTANCE_CAR_MOVE[index] >= WIN_DISTANCE);
+    const WIN_DISTANCE = Math.max(...distanceCarMove);
+    const MEMBERS = CAR_LIST.filter((car, index) => distanceCarMove[index] >= WIN_DISTANCE);
     MissionUtils.Console.print(`최종 우승자 : ${MEMBERS.join(', ')}`);
   }
 }
