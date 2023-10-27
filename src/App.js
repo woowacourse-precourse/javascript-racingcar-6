20572f14dbe4c907606af9fb2f051763a0587065
anr4876 <dustjdanr@naver.com>
2023-10-27 19:16:01 +0900

    feat: 전진 기능 구현 0에서 9사이에서 무작위 값을 생성, 무작위 값이 4이상일 경우 전진, map자료형을 사용하여 각각 자동차의 현재 전진 기록, 최종적으로 우승한 자동차를 출력하기 위해 가장 많이 이동한 거리 기록

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
+const MESSAGES = {
+  CAR: '경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)로 구분)',
+  NUM: '시도할 횟수는 몇 회인가요?',
+  RESULT: '실행 결과'
+};
 
-
-// 출력할 문구들
-const MESSAGE = {
-  CAR: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
-  NUM: '시도할 횟수는 몇 회인가요?\n',
-  RESULT: '실행결과'
+async function getUserData(message) {
+  const userData = await Console.readLineAsync(message);
+  return userData;
 }
 
-// 사용자 입력 추출
-async function getUserData(message){
-  const USER_DATA = await Console.readLineAsync(message);
-  return USER_DATA;
+function carToMap(carNames, carMap) {
+  carNames.split(',').forEach(name => {
+    handling(name.length);
+    carMap.set(name, "");
+  });
+}
+
+function goAndStop() {
+  return Random.pickNumberInRange(0, 9) >= 4;
+}
+
+function currentMatchResults(carMap) {
+  let highestScore = 0;
+  carMap.forEach((value, key) => {
+    if (goAndStop()) carMap.set(key, value + "-");
+    if (highestScore < carMap.get(key).length) highestScore = carMap.get(key).length;
+    Console.print(`${key} : ${carMap.get(key)}`);
+  });
+  Console.print('');
+  return highestScore;
 }
 
 class App {
   async play() {
-    const CAR_NAMES = await getUserData(MESSAGE.CAR);
-    const USER_NUMBER = await getUserData(MESSAGE.NUM);
+    const CAR_NAMES = await getUserData(MESSAGES.CAR);
+    const carMap = new Map();
+    let highestScore = 0;
 
-    
+    carToMap(CAR_NAMES, carMap);
+
+    const USER_NUMBER = await getUserData(MESSAGES.NUM);
+    Console.print('');
+
+    Console.print(MESSAGES.RESULT);
+    for (let i = 0; i < USER_NUMBER; i++) {
+      highestScore = currentMatchResults(carMap);
+    }
   }
 }
 
