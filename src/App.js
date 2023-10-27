184f1934ef76774eb3f6e258096d97261a8acae7
eunsung <esk114477@gmail.com>
2023-10-28 00:23:45 +0900

    remove: class 파일 삭제

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import { Console } from "@woowacourse/mission-utils";
-import CarNameInput from "./CarNameInput.js";
-import CarMoveCount from "./CarMoveCount.js";
 
 class App {
-  constructor() {
-    this.carNames = new CarNameInput();
-    this.moveCount = new CarMoveCount();
-  }
+  #carNames = [];
+  #moveCount;
+
   async play() {
-    const cars = await this.carNames.getCarNames();
-    const moveCount = await this.moveCount.getMoveCount();
+    
   }
 }
 
