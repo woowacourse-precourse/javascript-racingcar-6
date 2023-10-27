6a462e72b017ea1e22a3b8588b77df50ae1a0122
Seo San <dannysir@naver.com>
2023-10-28 04:14:08 +0900

    feat : 실행 결과 makeAndFind변수를 이용하여 나온 결과에 따라 배열에 추가, 해당 배열을 이용하여 문자열 생성, while문을 이용하여 실행 횟수만큼 실행

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import {MissionUtils} from "@woowacourse/mission-utils";
+import {makeAndFilter} from "./Random.js";
 
 class App {
-  async play() {
-    const NAMES = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
-    const ROUND = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
-    MissionUtils.Console.print("실행 결과");
-  }
+    async play() {
+        const NAMES = (await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"));
+        const NAME_ARR = NAMES.split(',');
+        const FORWARD_ARR = [...NAME_ARR].fill(0);
+        const ROUND = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+        MissionUtils.Console.print("\n실행 결과");
+        let cnt = 1;
+        while (cnt <= ROUND) {
+            let outString = ``;
+            let FORWARD_MARK = "-"
+            makeAndFilter(FORWARD_ARR.length).forEach((VALUE, INDEX) => {
+                FORWARD_ARR[INDEX] += VALUE;
+            });
+            for (let i = 0; i < NAME_ARR.length; i++) {
+                outString += `${NAME_ARR[i]} : ${FORWARD_MARK.repeat(FORWARD_ARR[i])}\n`;
+            }
+            MissionUtils.Console.print(outString);
+            cnt++;
+        }
+    }
 
 }
 
