f43f89982e0f013d00d9b6fb79404dae038c3cbd
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-10-28 02:21:36 +0900

    feat : 시도한 횟수 요청 메세지 상수화
    CAR_NAME : class InputReader 의 carNames()
    TRY_ROUND : class InputReader 의 tryRound()

--- src/constants/gameMessage.js
+++ src/constants/gameMessage.js
@@ -1,4 +1,6 @@
 export const GAME_MESSAGE = {
-  REQUEST_CAR_NAMES:
-    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+  REQUEST: {
+    CAR_NAME: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+    TRY_ROUND: '시도할 횟수는 몇 회인가요?',
+  },
 };
