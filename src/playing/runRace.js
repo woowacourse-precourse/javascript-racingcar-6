d322e513298c44672833c319558580d4f1480022
kwoncha <rnjsckgus@gmail.com>
2023-10-27 15:13:30 +0900

    feat: 레이싱 진행 및 출력 함수 구현

--- src/playing/runRace.js
+++ src/playing/runRace.js
@@ -1,6 +1,30 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
 
+const runRace = (totalRaces, participants) => {
+  const raceResults = {};
+  participants.forEach((participant) => {
+    raceResults[participant] = '';
+  })
+
+  for (let race = 1; race < totalRaces + 1; race++) {
+    participants.forEach((participant) => {
+      if (determineWin()) {
+        raceResults[participant] += '-';
+      }
+      MissionUtils.Console.print(`${participant} : ${raceResults[participant]}`);
+    })
+
+    MissionUtils.Console.print('\n');
+  }
+
+  const maxScore = findMaxScore(raceResults);
+  const winnerNamesArray = findWinnerName(raceResults, maxScore);
+
+  MissionUtils.Console.print(`최종 우승자 : ${winnerNamesArray.join(', ')}`);
+}
+
+
 const determineWin = () => {
   const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
 
