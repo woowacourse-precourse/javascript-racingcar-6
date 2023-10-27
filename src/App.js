fb6f2d1579c97df2c04de720d181127702893c14
rkdrjsrks <rkdrjs71@naver.com>
2023-10-27 17:58:19 +0900

    printWinner() 구현 및 예외 상황 추가

--- src/App.js
+++ src/App.js
@@ -22,31 +22,38 @@ class App {
             await this.printResult(cars);
             MissionUtils.Console.print("");
         }
+        await this.printWinner(cars);
     }
 
     async getUserInputString() {
         //TODO : 한 줄을 입력 받고 쉼표로 구분하여 세개의 문자열을 만듦
         let userInput = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
-        // if () {
-        //     throw new Error('[ERROR]');
-        // }
+
         const userInputSplit = userInput.split(',');
+
+        for (const name of userInputSplit) {
+            if (name.length > 5) {
+                throw new Error('[ERROR] 이름은 5자 이하만 가능합니다.');
+            }
+        }
         return userInputSplit;
     }
 
     async getUserInputInt() {
         //TODO : 시도할 횟수를 입력 받음
         let userInput = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
-        // if () {
-        //     throw new Error('[ERROR]');
-        // }
-        return parseInt(userInput);
+        const inputAsInt = parseInt(userInput);
+
+        if (isNaN(inputAsInt)) {
+            throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
+        }
+        return inputAsInt;
     }
 
     async printResult(cars) {
         //TODO : 실행 결과를 출력
         for (let i = 0; i < cars.length; i++) {
-            MissionUtils.Console.print(cars[i].name+" : "+'-'.repeat(cars[i].distance));
+            MissionUtils.Console.print(cars[i].name + " : " + '-'.repeat(cars[i].distance));
         }
     }
 
@@ -54,20 +61,29 @@ class App {
         //TODO : 랜덤한 값을 반환
         return MissionUtils.Random.pickNumberInRange(1, 9);
     }
-    async isValueAboveFour(random){
+
+    async isValueAboveFour(random) {
         //TODO : 무작위 값이 4이상일 경우인지를 판단
         return random >= 4;
     }
+
     async goForward(cars) {
         //TODO : 자동차 전진
         for (let i = 0; i < cars.length; i++) {
-            if(await this.isValueAboveFour(await this.randomInt()))cars[i].distance++;
+            if (await this.isValueAboveFour(await this.randomInt())) cars[i].distance++;
         }
     }
 
     async printWinner(cars) {
         //TODO : 최송 우승자 안내 문구를 출력
+        const maxDistance = Math.max(...cars.map(car => car.distance));
+        const winners = cars.filter(car => car.distance === maxDistance);
 
+        if (winners.length === 1) {
+            MissionUtils.Console.print(`최종 우승자 : ${winners[0].name}`);
+        } else {
+            MissionUtils.Console.print(`최종 우승자 : ${winners.map(winner => winner.name).join(', ')}`);
+        }
     }
 }
 
