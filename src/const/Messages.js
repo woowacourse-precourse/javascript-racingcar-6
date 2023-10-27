273940f3dfab28195b3b300de6b64ae0ec70c653
hyj5076 <hyj5076@hanmail.net>
2023-10-27 18:10:19 +0900

    docs(Messages): 상수 메시지 작성

---
+++ src/const/Messages.js
@@ -0,0 +1,6 @@
+export const QUESTION_CAR_NAME = '경주할 자동차 이름을 입력하세요.';
+export const QUESTION_GOAL_COUNT = '시도할 횟수는 몇 회인가요?';
+export const GAME_RESULT = '실행 결과';
+export const WINNER = '최종 우승자';
+
+export const ERROR_MESSAGE = '[ERROR] 숫자가 잘못된 형식입니다.';
