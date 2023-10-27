f0200f8ae28899cb4f9d40e016789adea16bf498
6puritans9 <6puritans9@gmail.com>
2023-10-27 16:31:00 +0900

    chore(Constants): Added a couple of strings for the case of exceeds and duplicates

--- src/Misc/Constatns.js
+++ src/Misc/Constatns.js
@@ -1,6 +1,8 @@
 export default class Constants {
   askNames = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): ";
 
+  exceeded = "이름은 5글자 이하로 입력해주세요";
+
   duplicates = "중복되지 않는 이름으로 입력해주세요.";
 
   tries = "시도할 횟수는 몇 회인가요? ";
