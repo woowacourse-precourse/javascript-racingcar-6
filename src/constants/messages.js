b9b46ef0163d8270f3da8e00f8932d2eae1a577a
ReturnReason <reasonz@daum.net>
2023-10-28 03:22:08 +0900

    chore(messages): MESSAGE라는 이름으로 import 할 수 있도록 export시 default 키워드 제거

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,4 +1,4 @@
-export default MESSAGE = Object.freeze({
+export const MESSAGE = Object.freeze({
   START: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
   TRY_COUNT: '시도할 횟수는 몇 회인가요?\n',
   RESULT: '실행 결과\n',
