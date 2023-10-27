b049d9dc265b1ef125ace14088d6c367492ddb4f
soyunim <togksk123@naver.com>
2023-10-27 21:08:11 +0900

    feat: 0부터 9까지 랜덤 숫자 입력 후 실행결과 가져오기

--- src/racingCar.js
+++ src/racingCar.js
@@ -1,28 +1,63 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 import { ERROR_MSG, GAME_MSG } from "./message";
 
-export const startGame = () => {
-  getCarsName();
+export const startGame = async () => {
+  const carNames = await getCarsName();
+  const tryTimes = await getTryTimes();
+  printExecutionResult(carNames, tryTimes);
 };
 
 const getCarsName = async () => {
   const cars = await Console.readLineAsync(GAME_MSG.GET_CARS_NAME);
-  Console.print(cars);
   const carNames = cars.split(",");
-  Console.print(carNames);
   for (let i = 0; i < carNames.length; i++) {
     if (carNames[i].length > 5) {
       throw new Error(ERROR_MSG.CAR_NAME_OVER_LENGTH);
     }
   }
-  getTryTimes();
+  for (let i = 0; i < carNames.length; i++) {
+    carNames[i] = carNames[i] + " : ";
+  }
+  return carNames;
 };
 
 const getTryTimes = async () => {
-  const tryTimes = Number(await Console.readLineAsync(GAME_MSG.GET_TRY_NUMBER));
-  if (isNaN(tryTimes)) {
-    throw new Error(ERROR_MSG.TRY_TIMES_NUMBER);
+  try {
+    const tryTimes = await Console.readLineAsync(GAME_MSG.GET_TRY_NUMBER);
+    if (isNaN(tryTimes)) {
+      throw new Error(ERROR_MSG.TRY_TIMES_NUMBER);
+    }
+    return tryTimes;
+  } catch (error) {
+    return error;
   }
+};
+
+const printExecutionResult = (carNames, tryTimes) => {
+  Console.print(GAME_MSG.EXECUTION_RESULT);
+  while (tryTimes > 0) {
+    getExecution(carNames);
+    Console.print(tryTimes);
+    Console.print(carNames);
+    tryTimes--;
+  }
+};
+
+const getExecution = (carNames) => {
+  for (let i = 0; i < carNames.length; i++) {
+    if (getRandomNumber == true) {
+      carNames[i] = carNames[i] + "-";
+    }
+  }
+};
+
+const getRandomNumber = () => {
+  const randomNumber = Random.pickNumberInRange(0, 9);
+  if (randomNumber > 3) {
+    console.log("random:" + randomNumber);
+    return true;
+  }
+  return false;
 };
 
 const endGame = () => {
