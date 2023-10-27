e7d7239e988ca773b37e010876b9d75bd104e160
coldok <2coldok@gmail.com>
2023-10-27 17:06:13 +0900

    feat : 게임 메세지, 에러 메세지 상수처리

---
+++ src/constants/Messeage.js
@@ -0,0 +1,12 @@
+export const GAME_MESSEAGE = Object.freeze({
+  inputName : '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+  inputAttempt : '시도할 횟수는 몆 회인가요?',
+  executeResult : '실행 결과',
+  winner : '최종 우승자',
+})
+
+export const ERROR_MESSEAGE = Object.freeze({
+  nameLength : '[ERROR] 자동차 이름은 최대 5글자까지 가능합니다.',
+  nameSingle : '[ERROR] 게임을 시작하기 위해선 2대 이상의 자동차가 필요합니다.',
+  commaMisuse : '[ERROR] 쉼표(,)의 사용이 잘못되었습니다.',
+})
