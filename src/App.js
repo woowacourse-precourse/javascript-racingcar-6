888507d43b9f13d35fe059a77df51d10336b2d44
unknown <dkvz@naver.com>
2023-10-27 12:12:12 +0900

    시도횟수 에러 수정. 전진 조건 메서드 추가

--- src/App.js
+++ src/App.js
@@ -12,23 +12,45 @@ async getNameOfCar(){
 }
 async attemptNumber(arrayCarName){
   const tryNumber = await MissionUtils.Console.readLineAsync(MESSAGE.NUMBER);
-  this.gameReady(arrayCarName,tryNumber);
+  this.errorTry(tryNumber);
+  this.gameReady(arrayCarName);
 }
 
-// 각 자동차 이름 객체 만들고 시도 횟수 에러 점검
-gameReady(arrayCarName,tryNumber){
-  if(isNaN(parseInt(tryNumber))===false){throw new Error(ERROR.NUMBER);}
-  if(parseFloat(tryNumber) !== parseInt(tryNumber)) {throw new Error(ERROR.INTEGER);}  
+// 각 자동차 이름 객체 만들기
+gameReady(arrayCarName){
+  const count =[];
   const objectCarName = new Object()
   arrayCarName.forEach((a,i) => {
     objectCarName[a] = 0;
+    count.push(0);
   });
+  this.gameStart(objectCarName, arrayCarName, count);  
 }
 
+// 매 게임이 마다 객체에 숫자 랜덤으로 넣고 5이상이면 count에 1 추가 
+// 나중에 count가 1인 것들은 print에 -추가
+gameStart(objectCarName, arrayCarName, count){
+  for(let i = 0; i<arrayCarName.length; i++){
+    objectCarName[i] = this.randomNumber;
+   if(Object.values(objectCarName)[i]>=5){
+      count[i]++
+    } 
+  }
+  this.gamePrint(count)
+}
 
 randomNumber(){
   const processNumber = MissionUtils.Random.pickNumberInRange(0,9)
   return processNumber;
+}
+
+errorTry(tryNumber){
+  if(isNaN(parseInt(tryNumber))===false){throw new Error(ERROR.NUMBER);}
+
+}
+
+gamePrint(){
+
 }
 
 }
