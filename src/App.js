d0e9fad007619fb62a39596249ad1e5978dc1364
minki3 <86992006+minki3@users.noreply.github.com>
2023-10-27 17:38:53 +0900

    Feat: 무작위의 숫자를 자동차에 부여, 그에 따른 결과 출력 로직 완성

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ class App {
     const userAttempts = await this.attempts("시도할 횟수는 몇 회인가요?");
     console.log(player);
     console.log(userAttempts);
+    const raceStart = this.race(player, userAttempts);
+    console.log(raceStart);
   }
 
   async playerRegistration(answer) {
@@ -16,7 +18,6 @@ class App {
     let player = [];
     for (let i = 0; i < playerArray.length; i++) {
       if (playerArray[i].length <= 5) {
-        console.log(playerArray[i].length);
         player.push(playerArray[i]);
       } else {
         throw new Error("[ERROR] 입력은 최대 5글자까지");
@@ -27,6 +28,32 @@ class App {
   async attempts(answer) {
     const userAttempts = await MissionUtils.Console.readLineAsync(answer);
     return userAttempts;
+  }
+  race(player, userAttempts) {
+    let results = {};
+    MissionUtils.Console.print("실행 결과");
+    for (let i = 1; i <= userAttempts; i++) {
+      // console.log(`실행횟수 ${i}번`);
+      this.race2(player, results);
+    }
+    return results;
+  }
+
+  race2(player, results) {
+    for (let j = 0; j < player.length; j++) {
+      let result = "";
+      if (MissionUtils.Random.pickNumberInRange(0, 9) >= 4) {
+        result += "-";
+      }
+      if (!results[player[j]]) {
+        results[player[j]] = result;
+      } else {
+        results[player[j]] += result;
+      }
+      MissionUtils.Console.print(`${player[j]} : ${results[player[j]]} `);
+    }
+
+    return results;
   }
 }
 
