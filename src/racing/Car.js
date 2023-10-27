a596aba32b0e89b2ab984b41a18a294523d2c9e8
jonique98 <tnals9899@naver.com>
2023-10-27 23:45:46 +0900

    fix: 출력 메세지가 undefined로 반환되던 오류 수정 초기 변수를 문자열로 지정해 주지 않아서 undefined가 반환되던 오류 수정 변수를 먼저 문자열로 초기화 후 반환

--- src/racing/Car.js
+++ src/racing/Car.js
@@ -1,6 +1,10 @@
 import { Random } from '@woowacourse/mission-utils';
 
 export default class Car {
+
+  carDistance = '';
+  carName = '';
+  
   constructor(carName) {
     this.carName = carName;
   }
