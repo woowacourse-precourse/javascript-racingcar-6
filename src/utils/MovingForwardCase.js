a3c2da50803d8178ccc81dbf24539c82c700ceed
hyj5076 <hyj5076@hanmail.net>
2023-10-27 22:10:58 +0900

    feat: 전진 조건 충족 시 대시 기호 출력 기능 추가

--- src/utils/MovingForwardCase.js
+++ src/utils/MovingForwardCase.js
@@ -2,5 +2,8 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 export const movingForwardCase = () => {
   const number = MissionUtils.Random.pickNumberInRange(0, 9);
-  return number >= 4; //4 이상 true 반환
+
+  if (number >= 4) {
+    Console.print('-');
+  }
 };
