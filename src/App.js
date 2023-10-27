300a0ab83cf90f1784c437d0bac622311ae73dc6
djLee77 <dkdnl232@gmail.com>
2023-10-27 17:46:51 +0900

    feat(App): 출력 기능 구현
    기능 목록 - 1.2 부분의 출력 기능 구현
    - `play` 메서드 내부에서 경기 상태 `raceState`와 우승자 'winners` 출력 로직 TODO 추가.
    - `displayRaceStateOfRound` 메서드를 통해 각 자동차의 이동 거리를 출력.
    - `displayWinners` 메서드를 통해 최종 우승자 출력.

--- src/App.js
+++ src/App.js
@@ -4,16 +4,38 @@ class App {
   async play() {
     const arrCarNames = await this.getCarNames();
     const totalRound = await this.getTotalRound();
+
+    /*
+      Todo :
+      raceState - [{'이름1' : '이동 거리'}, {'이름2' : '이동 거리'}] 형태의 배열
+      displayRaceStateOfRound(raceState);
+
+      winners - 우승자 명단이 들어있는 배열
+      displayWinners(winners);
+    */
   }
 
-  async getCarNames(){
-    const input = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+  async getCarNames() {
+    const input = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
     return input.split(",");
   }
 
-  async getTotalRound(){
+  async getTotalRound() {
     const input = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
     return Number(input);
+  }
+
+  displayRaceStateOfRound(raceState) {
+    raceState.forEach((car) => {
+      Console.print(`${car.name} : ${car.distance}`);
+    });
+    Console.print("");
+  }
+
+  displayWinners(winners) {
+    Console.print(`최종 우승자 : ${winners.join(", ")}`);
   }
 }
 
