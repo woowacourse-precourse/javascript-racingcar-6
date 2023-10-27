bb0850bfcaa68098dd1c0ea668cce4e904a32c1f
kimdw524 <kimdw524@naver.com>
2023-10-27 18:01:36 +0900

    feat(App): 경주할 자동차 이름 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { Console } from '@woowacourse/mission-utils';
+import { MESSAGE } from './Constants.js';
+import Validation from './Validation.js';
+import CarRacingGame from './CarRacingGame.js';
+import Car from './Car.js';
+
 class App {
-  async play() {}
+  /** @constructor */
+  constructor() {
+    /** @type {CarRacingGame} */
+    this.carRacingGame = new CarRacingGame();
+  }
+
+  async play() {
+    await this.readCarNames();
+  }
+
+  // 사용자로부터 경주할 자동차 이름들을 입력받음
+  async readCarNames() {
+    const answer = await Console.readLineAsync(MESSAGE.INPUT_CAR_NAMES);
+    const cars = answer.split(',');
+
+    cars.forEach(car => {
+      Validation.validateCarName(car)
+    });
+
+    Validation.validateDuplicateName(cars);
+
+    this.carRacingGame.setCars(cars.map(car => new Car(car)));
+  }
 }
 
 export default App;
