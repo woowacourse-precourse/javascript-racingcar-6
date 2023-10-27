dac28915ecb928ecd1487c69610346911148da58
soosoo22 <lllllh00@naver.com>
2023-10-27 17:49:49 +0900

    feat: "추출한 랜덤 값이 4이상일 경우 전진" 기능 추가
    추출한 랜덤 값이 4이상일 경우 advance 변수에 + 1

--- src/RacingCarGame.js
+++ src/RacingCarGame.js
@@ -1,8 +1,17 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
-import { GAME, ERROR } from './constants.js';
+// import { GAME, ERROR } from './constants.js';
 
 class RacingCarGame {
+  constructor(name) {
+    this.name = name;
+    this.advance = 0;
+  }
+
   makeRandomNumber() {
     return MissionUtils.Random.pickNumberInRange(0, 9);
+  }
+
+  advanceIfOverFour() {
+    if (this.makeRandomNumber() >= 4) this.advance += 1;
   }
 }
