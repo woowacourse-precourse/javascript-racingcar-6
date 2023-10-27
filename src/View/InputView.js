f5ec63701ea0cfd9a3d5e8ee03ef04db3be4062b
gouz7514 <gouz7514@gmail.com>
2023-10-27 15:52:51 +0900

    Update : 출력 메시지, 에러 메시지를 constant로 분리

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -1,18 +1,15 @@
-/**
- * @class InputView
- * @description 사용자의 입력을 받고, model에 전달한다
- */
-
 import { Console } from "@woowacourse/mission-utils";
+
+import { MESSAGE, ERROR } from "../constatnts/index.js";
 
 class InputView {
   // 1-1. 경주할 자동차의 이름을 입력받는다.
   async getCarNames() {
-    const result = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+    const result = await Console.readLineAsync(MESSAGE.GET_CAR_NAMES);
     const carNames = result.trim() === "" ? [] : result.split(",");
 
     if (!InputView.isValidCarNames(carNames)) {
-      throw new Error("[ERROR] 유효하지 않은 자동차 이름입니다.");
+      throw new Error(ERROR.ERROR_INVALID_CAR_NAMES);
     }
 
     return carNames;
@@ -20,10 +17,10 @@ class InputView {
 
   // 1-2. 시도할 횟수를 입력받는다.
   async getTryCount() {
-    const tryCount = await Console.readLineAsync("시도할 회수는 몇 회인가요?\n");
+    const tryCount = await Console.readLineAsync(MESSAGE.GET_TRY_COUNT);
 
     if (!InputView.isValidTryCount(tryCount)) {
-      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+      throw new Error(ERROR.ERROR_INVALID_TRY_COUNT);
     }
 
     return Number(tryCount);
