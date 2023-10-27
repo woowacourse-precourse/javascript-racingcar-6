5d510df7589cd30349389bac6e2143177521f0c8
working-zima <working.zima@gmail.com>
2023-10-27 21:51:06 +0900

    feat(App.js): 입력 기능 구현
    getCarNamesInput 메서드는 자동차 이름을 받음
    getMoveCountInput 메서드는 시도 횟수를 받음

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  static getCarNamesInput() {
+    return Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+    );
+  }
+
+  static getMoveCountInput() {
+    return Console.readLineAsync('시도할 횟수는 몇 회인가요?');
+  }
+
+  async startGame() {
+    const carNames = await App.getCarNamesInput();
+    const moveCount = await App.getMoveCountInput();
+  }
+
+  async play() {
+    this.startGame();
+  }
 }
 
 export default App;
