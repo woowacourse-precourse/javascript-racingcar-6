2ef687d22b78c741a31b8f16c9c22540667712ad
hojkim77 <rlaghwnd4534@gmail.com>
2023-10-27 18:12:30 +0900

    [refactor] 입력과 그 입력검증에 대한 코드 개선

--- src/App.js
+++ src/App.js
@@ -3,17 +3,15 @@ import InputValidator from './InputValidator.js';
 class App {
   async play() {
     const carNames = await this.#inputCarNames();
-    console.log(carNames);
     const tryNum = await this.#inputTryNum();
-    console.log(tryNum);
   }
 
   async #inputCarNames() {
     const carNamesInput = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
     );
-    InputValidator.carNameValidator(carNamesInput);
-    return carNamesInput;
+    const validatedCarNames = InputValidator.carNameValidator(carNamesInput);
+    return validatedCarNames;
   }
   async #inputTryNum() {
     const tryNumInput = await Console.readLineAsync(
