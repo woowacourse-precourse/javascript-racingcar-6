d359d1f83b2c1e35a98f2ac653f6041c16d6d495
noodle <noodle>
2023-10-27 17:44:39 +0900

    feat: App.js 차 이름, 횟수 입력값 유효성 검사 로직 추가

--- src/App.js
+++ src/App.js
@@ -4,11 +4,29 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
   async play() {
     // 1. (입력) 경주할 차 이름 입력
+    // 1.1 (입력) 경주할 차 이름 입력
     const carNames = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
-    const carNameList = carNames.split(',');
+
+    /**
+     * 1.2 validate
+     *  1) 5자 이하의 영어 대,소문자
+     *  2) ,(comma)로 구분짓는다.
+     *    ,,콤마가 연속으로 들어가거나 이로하여금 빈 문자열이 출력되면 안 됨.
+     */
+    const onlyEngLimit5Regex = /^[a-zA-Z]{1,5}$/; // 영어 대소문자, 5글자까지
+    const carNameList = carNames.split(',').map((car) => {
+      if (!onlyEngLimit5Regex.test(car)) {
+        throw new Error('[ERROR] 유효한 입력값이 아닙니다.');
+      }
+      return car;
+    });
 
     // 2. (입력) 시도할 횟수
     const tryCount = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    const onlyNumber = /^[1-9]\d*$/;
+    if (!onlyNumber.test(tryCount)) {
+      throw new Error('[ERROR] 유효한 숫자를 입력해주세요.');
+    }
 
     // 3. 자동차 객체 만들기 -> 인스턴스 배열 생성
     const racingCarItem = carNameList.map((carName) => {
