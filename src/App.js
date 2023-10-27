f1383b584f24b894d1927ed2f427d18c037090c7
고세종 <fulfilled7418@gmail.com>
2023-10-27 20:26:27 +0900

    feat: 구현 자동차 이름 입력 함수 getCarNames()

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Random, Console } from '@woowacourse/mission-utils';
+
+const MESSAGE = {
+  ENTER_CAR_NAME:
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+};
+
 class App {
   async play() {}
 }
+
+const getCarNames = async () => {
+  const carNames = await Console.readLineAsync(
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+  );
+  checkCarNameIsValid(carNames);
+};
+
+getCarNames();
+
+const getTryNumber = () => {};
+
+const checkCarNameIsValid = (userInput) => {};
+
+const checkTryNumberIsValid = (userInput) => {};
 
 export default App;
