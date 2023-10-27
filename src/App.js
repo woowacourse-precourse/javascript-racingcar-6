265beb1bd1a5bc3cd9ab42963b947ea59a61cad9
김경주 <popo505@naver.com>
2023-10-27 16:45:42 +0900

    feat: 자동차 이름 입력받고 쉼표 기분으로 구분

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { Console, MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
-  start(){
+  start() {
     Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분")
   }
-  async play() {}
+
+  async getCarNames() {
+    const names = await Console.readLineAsync();
+    const carNamesArray = names.split(',').map(name => name.trim)
+    return carNamesArray;
+  }
+  async play() { }
 }
 
 export default App;
