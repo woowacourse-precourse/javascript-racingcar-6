092814b065b15afe10a47fc862980763f8beff8c
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2023-10-27 21:00:08 +0900

    [feat] : Add 사용자 자동차 이름 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  constructor() {
+    this.strCarName = "";
+    this.arrCarName = [];
+  }
+  async play() {
+    // 1-a
+    this.strCarName = await MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+  }
 }
 
 export default App;
