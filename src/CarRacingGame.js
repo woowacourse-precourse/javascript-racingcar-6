258857152d8940ca1ae29dd4c6775e7bde481d28
kimdw524 <kimdw524@naver.com>
2023-10-27 21:24:32 +0900

    feat(CarRacingGame): 전진 후 결과를 콜백하는 메소드 구현

--- src/CarRacingGame.js
+++ src/CarRacingGame.js
@@ -13,6 +13,26 @@ class CarRacingGame {
   setCars(cars) {
     this.#cars = cars;
   }
+
+  /**
+   * @callback Distances
+   * @param {{ name: string, distance: number }[]} distances
+   * @returns {void}
+   */
+
+  /**
+   * Car[] 를 전진 시도한 후 이동 거리를 콜백하는 과정을 n 번 반복하는 메소드
+   * @param {number} count 시도 횟수 
+   * @param {Distances} callback 매 시도 후 결과 반환
+   * @returns {void}
+   */
+  goForward(count, callback) {
+    for (let i = 0; i < count; i++) {
+      callback(this.#cars.map(car => {
+        return { name: car.getName(), distance: car.goForward() };
+      }));
+    }
+  }
 }
 
 export default CarRacingGame;
