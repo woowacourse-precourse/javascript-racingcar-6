671466e1698ec8d1013951602026dd617e53ad92
Y.W KIM <kyw624@gmail.com>
2023-10-27 17:35:08 +0900

    Test: moveOrStop 메서드 테스트 코드 추가

--- __tests__/CarTest.js
+++ __tests__/CarTest.js
@@ -1,6 +1,13 @@
 import { Car } from '../src/App';
 import { Console, Random } from '@woowacourse/mission-utils';
 
+const mockRandoms = (numbers) => {
+  Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, Random.pickNumberInRange);
+};
+
 describe('자동차 경주 자체 테스트', () => {
   test('객체 생성 테스트', () => {
     const inputs = ['hCar', 'kCar', 'sCar'];
@@ -11,6 +18,20 @@ describe('자동차 경주 자체 테스트', () => {
       expect(car).toBeInstanceOf(Car);
       expect(car.name).toBe(input);
       expect(car.progress).toBe('');
+    });
+  });
+
+  test('전진-정지 테스트', async () => {
+    const car = new Car('hCar');
+    const outputs = ['', '-', '--'];
+    const randoms = [3, 4, 6];
+
+    mockRandoms([...randoms]);
+
+    outputs.forEach((output) => {
+      car.moveOrStop();
+
+      expect(car.progress).toBe(output);
     });
   });
 });
