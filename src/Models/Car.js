99e5e513b07f04a7f59b80897b77ab71558ced61
0jenn0 <jenn0.6n@gmail.com>
2023-10-28 05:35:09 +0900

    Fix: Car의 랜덤값이 4 이상 일 경우 전진,3 이하일 경우 멈춤 기능 구현
    랜덤으로 구한 값(randomDistance)이 4 이상일 경우에만 Car의 totalDistance에randomDistance값을 더해주는 방법으로 구현했습니다.

--- src/Models/Car.js
+++ src/Models/Car.js
@@ -2,10 +2,17 @@ import Name from "./Name.js";
 import { Random } from "@woowacourse/mission-utils";
 
 export default class Car {
+  #MIN_VALUE = 4;
   constructor(name) {
+    this.totalDistance;
     this.name = new Name(name).name;
   }
   #getRandomDistance() {
     return Random.pickNumberInRange(0, 9);
+  }
+
+  race() {
+    const randomDistance = this.#getRandomDistance();
+    if (randomDistance >= this.#MIN_VALUE) this.totalDistance += randomDistance;
   }
 }
