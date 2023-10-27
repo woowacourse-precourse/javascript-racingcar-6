51c72b12f2837b001e37f0e358022aec791f3851
hongst222 <tmdxordusdo@gmail.com>
2023-10-28 05:17:30 +0900

    feat: Validate 클래스 작성
    1. validateCarName() : 2,3,4를 이용하여 자동차 이름 유효성 검사
    2. removeBlankSplit() : 문자열 공백 제거, 배열로 반환
    3. checkDuplication() : 자동차 이름이 중복되는지 검사
    4. checkNameLength() : 자동차 이름 길이 검사
    5. validateTryMoveCount() : 전진 시도 회수 검사
    
    Validate 클래스의 5개의 메소드를 작성완료.

---
+++ src/Validate.js
@@ -0,0 +1,40 @@
+
+import { ERRORS, RULES } from "./constants/index.js";
+import { throwError } from "./utils/index.js";
+
+class Validate {
+    
+    validateCarName(input) {
+        const names = this.removeBlankSplit(input);
+        this.checkDuplication(names);
+        this.checkNameLength(names);
+    }
+
+    removeBlankSplit(input){
+        const inputs = input.replace(/\s/g, '');
+        const names= inputs.split(',');
+        return names;
+    }
+
+    checkDuplication(array){
+        const set = new Set(array);
+        const condition = set.size != array.length
+        throwError(condition,ERRORS.DUPLICATION);
+    }
+
+    checkNameLength(array){
+        array.forEach(value => {
+            throwError(value.length > RULES.MAX_STRING, ERRORS.NAME);
+        })
+    }
+    
+
+    validateTryMoveCount(count) {
+        const countTrim = count.trim();
+        const condition = !/^[1-9]\d*$/.test(countTrim);
+        throwError(condition, ERRORS.TRY);
+    }
+}
+
+export default Validate;
+
