545a3b4458028d17d3b1552556b090c35ceb6020
brgndy <brgndy96@gmail.com>
2023-10-27 20:17:26 +0900

    feat(play): 자동차 입력 테스트

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { GAME_MESSAGES } from './constants/gameMessages.js';
+import { inputView } from './ViewController/InputView.js';
+
 class App {
-  async play() {}
+  async play() {
+    await this.inputCarNames();
+  }
+
+  async inputCarNames() {
+    const valueOfCarNames = await inputView.readLine(GAME_MESSAGES.input_car_names);
+    console.log('valueOfCarNames : ', valueOfCarNames);
+  }
 }
 
 export default App;
