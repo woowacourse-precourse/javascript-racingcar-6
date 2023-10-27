525df01f274253ef3336e181509576940ff8e4c3
Early Riser <tkfydtls464@gmail.com>
2023-10-27 17:16:19 +0900

    feat: add function to collect car names from user input and save to list.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
   async play() {
-    const members = ['east', 'west', 'south']
-    members.map((member) => member).join(',') // "east,west,south"
+    const CAR_LIST_INPUT = await MissionUtils.Console.readLineAsync('');
+    const CAR_LIST = CAR_LIST_INPUT.split(',');
+    CAR_LIST.forEach((CAR_NAME) => {
+      if (CAR_NAME.length > 5) {
+        throw new Error('[ERROR] 입력이 5자 이하가 아닙니다');
+      }
+    });
   }
 }
 
-export default App
+export default App;
