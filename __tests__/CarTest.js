30a39ed1ea249d0d1ee27778713cd561ac2b34fe
leeyulgok <dbfrhr20@gmail.com>
2023-10-27 16:11:09 +0900

    feat: 자동차 객체 생성 및 기능 테스트 코드 작성

---
+++ __tests__/CarTest.js
@@ -0,0 +1,30 @@
+import Car from "../src/Car";
+
+describe("자동차 객체 ", () => {
+  let car;
+
+  beforeEach(() => {
+    car = new Car('testCar');
+  })
+
+  test('자동차 클래스 이름과 위치 가져오기', () => {
+    expect(car.getName()).toBe('testCar');
+    expect(car.getPosition()).toBe('');
+  });
+  
+  test('자동차 클래스 전진-멈춤 함수', () => {
+    car.move(4);
+    expect(car.getPosition()).toBe('-');
+
+    car.move(3);
+    expect(car.getPosition()).toBe('-');
+  });
+
+  test('자동차 클래스 현재 위치 함수', () => {
+    expect(car.printPosition()).toBe('testCar : ');
+
+    car.move(4);
+    expect(car.printPosition()).toBe('testCar : -');
+  });
+
+});
