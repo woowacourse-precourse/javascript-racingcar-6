e9c2e081b09bb0aa9468e5b42fcd5105c7d97e0f
jaeochoii <jaeochoiii0716@inha.edu>
2023-10-27 18:19:46 +0900

    feat: 입력값 유효성 검사 함수 try...catch로 생성

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,17 +1,28 @@
-import { PrintMessage } from "../static/Static.js";
+import InputValidator from "../utils/InputValidator.js";
+import { PrintMessage, ErrorMessage } from "../static/Static.js";
 import { Console } from "@woowacourse/mission-utils";
 
 const InputView = {
   async readCarsName(callback) {
-    await Console.readLineAsync(PrintMessage.INPUT_NAMES).then((input) => {
-      callback(input);
-    });
+    try {
+      await Console.readLineAsync(PrintMessage.INPUT_NAMES).then((input) => {
+        InputValidator.validateCarName(input);
+        callback(input);
+      });
+    } catch (error) {
+      Console.print(error);
+    }
   },
 
   async readTryTimes(callback) {
-    await Console.readLineAsync(PrintMessage.INPUT_TIMES).then((input) => {
-      callback(input);
-    });
+    try {
+      await Console.readLineAsync(PrintMessage.INPUT_TIMES).then((input) => {
+        InputValidator.validateTimesNumber(input);
+        callback(input);
+      });
+    } catch (error) {
+      Console.print(error);
+    }
   },
 };
 
