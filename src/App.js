79f76c342b1e3381e9d2ae3abec10dc3fdc04fd6
Y.W KIM <kyw624@gmail.com>
2023-10-27 15:50:35 +0900

    Feat: 사용자의 입력을 검증하는 메서드 validateInput 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import { Console, Random } from '@woowacourse/mission-utils';
 
 class App {
-  async play() {}
+  async play() {
+    const carNames = this.validateInput(
+      await Console.readLineAsync(
+        '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+      )
+    );
 
-  validateInput(input) {}
+    const totalRounds = this.validateInput(
+      await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n')
+    );
+  }
+
+  validateInput(input) {
+    if (input === '') {
+      throw new Error('[ERROR] 입력값이 없습니다.');
+    }
+
+    if (!isNaN(Number(input))) {
+      return Number(input);
+    }
+
+    if (input.indexOf(' ') !== -1) {
+      throw new Error('[ERROR] 입력값에 공백이 포함되었습니다.');
+    }
+
+    const cars = input.split(',').filter((v) => v);
+
+    if (cars.length < 2) {
+      throw new Error('[ERROR] 자동차는 최소 2대여야합니다.');
+    }
+
+    const isExceed = cars.some((car) => car.length > 5);
+
+    if (isExceed) {
+      throw new Error('[ERROR] 각 자동차명의 길이는 최대 5자입니다.');
+    }
+
+    return cars;
+  }
 
   getWinners(cars) {}
 }
