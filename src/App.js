90fb8ccac153a553d25668d85ccf1657ba31fa71
Moon Dahyun <dahyun723@ajou.ac.kr>
2023-10-27 20:13:01 +0900

    Fix: 에러처리

--- src/App.js
+++ src/App.js
@@ -5,28 +5,36 @@ class App {
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     );
     const cars_arr = cars.split(",");
+    for (let i = 0; i < cars_arr.length; i++) {
+      if (cars_arr[i].trim().length > 5) {
+        throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+      }
+    }
+
     let cars_win_count_num = new Array(cars_arr.length).fill(0);
     let cars_win_count_line = new Array(cars_arr.length).fill("");
     const trial_count = await MissionUtils.Console.readLineAsync(
       "시도할 횟수는 몇 회인가요?"
     );
-
-    for (let i = 0; i < cars_arr.length; i++) {
-      const random = MissionUtils.Random.pickNumberInRange(0, 9);
-      let win_line = "";
-      if (random >= 4) {
-        cars_win_count_num[i]++;
-        for (let j = 0; j < cars_win_count_num[i]; j++) {
-          win_line += "-";
-        }
-      }
-      MissionUtils.Console.print("실행결과₩n");
+    MissionUtils.Console.print("실행결과");
+    for (let k = 0; k < trial_count.length; k++) {
       for (let i = 0; i < cars_arr.length; i++) {
-        MissionUtils.Console.print(
-          `${cars_arr[i]} : ${cars_win_count_line[i]}`
-        );
+        const random = MissionUtils.Random.pickNumberInRange(0, 9);
+        let win_line = "";
+        if (random >= 4) {
+          cars_win_count_num[i]++;
+          for (let j = 0; j < cars_win_count_num[i]; j++) {
+            win_line += "-";
+          }
+        }
+
+        for (let i = 0; i < cars_arr.length; i++) {
+          MissionUtils.Console.print(
+            `${cars_arr[i]} : ${cars_win_count_line[i]}`
+          );
+        }
+        cars_win_count_line[i] = win_line;
       }
-      cars_win_count_line[i] = win_line;
     }
     console.log(cars, "주어진");
     console.log(cars_arr, "자동차");
