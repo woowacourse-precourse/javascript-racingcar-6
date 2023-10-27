baf8a7b917678c4bf56f865ff691f862dabc61ff
hongst222 <tmdxordusdo@gmail.com>
2023-10-28 06:58:22 +0900

    feat: Car 클래스 생성
    해당 클래스의 매개변수, 메소드를 작성하였다.
    
    name: 경주 자동차 이름
    distance: 이동한 거리를 '-'를 통해 표현한다.
    
    isMove(), move() : 자동차 이동 관련 메소드이다.
    
    getDistance() : 이동 거리를 구한다.
    
    toStringResult() : 현재 까지 이동한 거리를 문자열로 반환한다.

---
+++ src/Game/Car.js
@@ -0,0 +1,36 @@
+import { pickNumberInRange } from "../utils/index.js";
+import { RULES } from '../constants/index.js';
+class Car {
+    constructor(name) {
+        this.name = name;
+        this.distance = "";
+    }
+    isMove() {
+        const number = pickNumberInRange(RULES.MIN_MOVE,RULES.MAX_MOVE);
+        console.log(`(${this.name}) number: ${number}\n`);
+        if (number < RULES.STANDARD) {
+            console.log('(${this.name}) 이동 실패\n');
+            return false;
+        } else if (number >= RULES.STANDARD) {
+            console.log('(${this.name}) 이동 성공\n');
+            return true;
+        }
+    }
+
+    move() {
+        const isMove = this.isMove();
+        if (isMove) {
+            this.distance += "-";
+        }
+    }
+
+    getDistance() {
+        return this.distance.length;
+    }
+
+    toStringResult() {
+        return `${this.name} : ${this.distance}`;
+    }
+}
+
+export default Car;
