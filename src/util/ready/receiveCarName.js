03de4ecea16ab3134655c14b096886bfea133b47
thumbthing <ilscc0902@gmail.com>
2023-10-28 05:08:58 +0900

    feat: 차량명 입력 기능 구현(consoleInput, valid 검증)

--- src/util/ready/receiveCarName.js
+++ src/util/ready/receiveCarName.js
@@ -1,12 +1,16 @@
-import { Console } from '@woowacourse/mission-utils';
 import { checkCarValid } from './checkInputValid.js';
+import { consoleInput } from '../libraryFeatures/consoleHandler.js';
+import { defaultErrorHandler } from '../error/errorhandler.js';
 
 async function receiveCarName() {
-  const input = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
-  const carList = checkCarValid(input);
-  return carList;
+  try {
+    const input = await consoleInput('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    const carList = await checkCarValid(input);
+    return carList;
+  } catch (error) {
+    defaultErrorHandler(error);
+    return null;
+  }
 }
 
 export default receiveCarName;
-
-receiveCarName();
