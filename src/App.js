856a4abcaae861621825c9b29137775fac5e4ce6
skdltn210 <rhdmstn202@gmail.com>
2023-10-27 17:45:12 +0900

    feat: 자동차 이름 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-class App {
-  async play() {}
-}
+import { Console } from "@woowacourse/mission-utils";
+import { Random } from "@woowacourse/mission-utils";
 
+async function inputCar() {
+  let carNames = await Console.readLineAsync(
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+  );
+  return carNames.split(",").map((value) => [value, 0]);
+}
+class App {
+  async play() {
+    let car = await inputCar();
+  }
+}
 export default App;
