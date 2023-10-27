ac655b4dadd01c1a319e50929afa1226bdede5c2
mieum12 <qpdlqltb1215@gmail.com>
2023-10-27 23:03:15 +0900

    fix: 상수 변경

--- src/view/inputView.js
+++ src/view/inputView.js
@@ -1,2 +1,10 @@
-class InputView {}
+import { Random, Console } from "@woowacourse/mission-utils";
+import { MESSAGE } from "../constants/constants";
+class InputView {
+  getCarName() {
+    let carName = Console.readLineAsync(MESSAGE.START_CAR_NAME);
+  }
+
+  getNumberOfAttempt() {}
+}
 export default InputView;
