530befbb44b292af04f0e8441648f4a0436ab6de
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-10-28 04:20:52 +0900

    feat : 자신의 위치에 맞게 (-) 을 이용해 그림을 그리는 기능 추가
    getPositionResult() : this.position을 이용해 position 숫자만큼 (-) 을 반환

--- src/Car.js
+++ src/Car.js
@@ -7,4 +7,13 @@ export default class Car {
   increasePosition(isMovable) {
     if (isMovable) this.position += 1;
   }
+
+  getPositionResult() {
+    const positionString = Array.from(
+      { length: this.position },
+      () => '-'
+    ).join('');
+
+    return positionString;
+  }
 }
