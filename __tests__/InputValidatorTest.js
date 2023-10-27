99ddccf596c16254213d60ef85fb02822d044a5d
thewronghand <penfreak77@gmail.com>
2023-10-28 00:31:33 +0900

    chore: 테스트 문구 추가, 시도 횟수 테스트 추가

--- __tests__/InputValidatorTest.js
+++ __tests__/InputValidatorTest.js
@@ -1,14 +1,14 @@
 import CustomError from '../src/errors/CustomError';
 import InputValidator from '../src/utils/InputValidator';
 
-describe('InputValidator', () => {
+describe('InputValidator 테스트', () => {
   let validator;
 
   beforeEach(() => {
     validator = new InputValidator();
   });
 
-  describe('validateCarNames', () => {
+  describe('validateCarNames 테스트', () => {
     it('최대 길이 초과 시 반응 테스트', () => {
       expect(() => {
         validator.validateCarNames(['verylongname']);
@@ -24,6 +24,20 @@ describe('InputValidator', () => {
     it('중복 이름 입력 시 반응 테스트', () => {
       expect(() => {
         validator.validateCarNames(['car', 'car']);
+      }).toThrow(CustomError);
+    });
+  });
+
+  describe('validateRoundNumbers 테스트', () => {
+    it('숫자가 아닌 값 입력 시 반응 테스트', () => {
+      expect(() => {
+        validator.validateRoundsNumber(parseInt('asdf', 10));
+      }).toThrow(CustomError);
+    });
+
+    it('음수 입력 시 반응 테스트', () => {
+      expect(() => {
+        validator.validateRoundsNumber(-15);
       }).toThrow(CustomError);
     });
   });
