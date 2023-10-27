bbdc9b3659c4e5d0cd84211eae4d4a1acdaed291
rbgksqkr <rbgks1937@naver.com>
2023-10-28 00:34:15 +0900

    feat: 자동차가 4이상의 무작위값을 가질경우 전진

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import { Random, Console } from "@woowacourse/mission-utils";
+
+class Car {
+  name = "";
+  score = 0;
+
+  constructor(name, score) {
+    this.name = name;
+    this.score = score;
+  }
+
+  printMyScore() {
+    const number = Random.pickNumberInRange(0, 9);
+    if (number >= 4) {
+      this.score += 1;
+    }
+    const result = "-".repeat(this.score);
+    Console.print(`${this.name}: ${result}`);
+  }
+}
+
 class App {
   async play() {
-    // const number = Random.pickNumberInRange(0, 9);
     const carList = await this.getCarList();
     const tryNumber = await this.getTryNumber();
-    console.log(carList, tryNumber);
+
+    Console.print("\n실행 결과");
+    Array.from({ length: tryNumber }).map(() => {
+      carList.map((car) => {
+        car.printMyScore();
+      });
+      Console.print("");
+    });
   }
 
   async getCarList() {
     Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     const carInput = await Console.readLineAsync("");
     const carInputArray = carInput.split(",");
-    return carInputArray;
+    const carObjectArray = carInputArray.map((carName) => new Car(carName, 0));
+    return carObjectArray;
   }
 
   async getTryNumber() {
