2acc4b4badf18a28c8cf60c8b938ec502799e558
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-10-27 16:34:04 +0900

    feat: 자동차 이름 유효성 검사 기능 완료

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import { Console } from "@woowacourse/mission-utils";
-import { START } from "./constants/constants.js";
+import MESSAGE from "./constants/constants.js";
 
 class App {
+  constructor() {
+    this.cars = [];
+  }
+
   async play() {
-    const answer = await Console.readLineAsync(START);
+    const answer = await Console.readLineAsync(MESSAGE.start);
+    this.cars = answer.split(",");
+  }
+
+  CheckCarsName() {
+    if (this.cars.length > 10 || this.cars.length < 2) return false;
+
+    const hasNotSpace = (car) => !car.includes(" ");
+    const isNotSpace = (car) => car.length !== 0;
+    const checkFive = (car) => car.length >= 5;
+
+    const isValidate = this.cars.every(
+      (car) => hasNotSpace(car) && isNotSpace(car) && checkFive(car)
+    );
+
+    return isValidate;
   }
 }
 
-const app = new App();
-
-app.play();
 export default App;
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,1 +1,7 @@
-export const START = '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분'
+const MESSAGE = {
+  start:
+    "경주할 자동차 이름을 입력하세요.이름은 쉼표(,) 기준으로 구분하며 공백을 포함하면 안되고 5글자 이상 최대 10개 까지 등록가능합니다.",
+  roundsToPlay: "시도할 횟수는 몇 회인가요?",
+};
+
+export default MESSAGE;
