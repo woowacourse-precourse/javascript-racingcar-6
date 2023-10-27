fd725ec73b5393d3417325335011afcb8cdc3a25
leejaejae <leesengjie@naver.com>
2023-10-27 15:01:49 +0900

    자동차 전진 여부 결정: * 0에서 9까지의 무작위 숫자를 생성하고, 4 이상일 경우 자동차는 전진합니다. * 전진한 자동차는 '-'로 표시되고, 멈춘 자동차는 '-' 없이 표시됩니다.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import { Random, Console } from '@woowacourse/mission-utils';
 
 // 경주 자동차 클래스
+class Car{
+  constructor(name) {
+    this.name = name;
+    this.position = 0;  // 현재 위치
+  }
+
+  move(){
+    const randomNumber = Random.pickNumberInRage(0, 9);
+    if(randomNumber >= 4){  // 4 이상일 경우 자동차 전진
+      this.position++;
+    }
+  }
+
+  getPositionString(){
+    return '-'.repeat(this.position);  // 현재위치 수 만큼 "-" 반환
+  }
+}
 
 class App {
   async play() {
@@ -17,8 +34,14 @@ class App {
   }
 
   // 경기 시작
+  startRace (){
+
+  }
 
   // 우승자
+  raceWinner(){
+
+  }
 }
 
 export default App;
