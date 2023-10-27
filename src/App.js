fb63c69f76588b7ca169c3ebb840cb5005f163f3
stupidJoon <junsang.yu3@gmail.com>
2023-10-28 01:38:34 +0900

    refactor: change variable naming

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { validate, inputCarRegex } from './validate.js';
 class App {
   async play() {
     Console.print('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
-    const input = (await Console.readLineAsync('')).trim();
-    if (!validate(input, inputCarRegex)) {
+    const carInput = (await Console.readLineAsync('')).trim();
+    if (!validate(carInput, inputCarRegex)) {
       throw new Error('[ERROR] 입력이 잘못된 형식입니다.');
     }
     const cars = input.split(',');
