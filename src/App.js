695cb9a7d03413dcfdf3bf54df4c16fa8477a4f0
Hong sung soo <sow9707@daum.net>
2023-10-27 17:28:41 +0900

    feat: 자동차 이름 유효성 검사 함수 구현 및 사용자 입력 받는 함수에 추가

--- src/App.js
+++ src/App.js
@@ -5,10 +5,29 @@ const Random = MissionUtils.Random;
 
 class App {
   constructor() {}
+
   async carNameInput() {
-    const userInput = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-    return userInput.split(","); 
+    const userInput = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+    try {
+      this.carNameValidation(userInput);
+    } catch (error) {
+      Console.print(error.message);
+      throw error;
+    }
+    return userInput.split(",");
   }
+
+  carNameValidation(userInput) {
+    const carNames = userInput.split(",");
+    carNames.forEach((carName) => {
+      if (carName.length < 5) {
+        throw new Error("[ERROR] 자동차 이름이 5글자 아래입니다.");
+      }
+    });
+  }
+
   async play() {}
 }
 
