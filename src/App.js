d6944c6d526174c8318f8ba2e5c0d1a1dc28471d
ReturnReason <reasonz@daum.net>
2023-10-28 03:14:42 +0900

    feat(App): play 메서드가 실행되면 M, V, C 인스턴스를 생성 후 컨트롤러의 init 메서드를 실행하는 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import GameController from './controller/GameController.js';
+import GameView from './view/gameView.js';
+import Cars from './model/Cars.js';
+
 class App {
-  async play() {}
+  async play() {
+    try {
+      const model = new Cars();
+      const view = new GameView();
+      const controller = new GameController({ model, view });
+      controller.init();
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
 }
 
 export default App;
