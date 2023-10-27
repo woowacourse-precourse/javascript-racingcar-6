f526b70dead2c40b6f71eaa5c83e067b29679cb0
jaeochoii <jaeochoiii0716@inha.edu>
2023-10-28 00:42:31 +0900

    fix: 랜덤 값의 반환값이 배열이 아닌 상수로 수정

--- src/utils/RandomNumberGenerator.js
+++ src/utils/RandomNumberGenerator.js
@@ -2,16 +2,12 @@ import { Random } from "@woowacourse/mission-utils";
 import { StaticNumber } from "../static/Static.js";
 
 const RandomNumberGenerator = {
-  generate(size) {
-    const numbers = [];
-    while (numbers.length < size) {
-      const number = Random.pickNumberInRange(
-        StaticNumber.RANDOM_MIN_NUMBER,
-        StaticNumber.RANDOM_MAX_NUMBER
-      );
-      numbers.push(number);
-    }
-    return numbers;
+  generate() {
+    const number = Random.pickNumberInRange(
+      StaticNumber.RANDOM_MIN_NUMBER,
+      StaticNumber.RANDOM_MAX_NUMBER
+    );
+    return number;
   },
 };
 export default RandomNumberGenerator;
