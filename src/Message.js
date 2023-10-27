79bc22bb7bcc25f242fd78e79b2f5702495d5867
SU_VIN <pyotnqls25@naver.com>
2023-10-27 22:43:58 +0900

    Feat: 출력문 상수화

---
+++ src/Message.js
@@ -0,0 +1,14 @@
+const Message = {
+  INPUT_CAR_NAME:
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+  INPUT_NUMBER_OF_ATTEMPTS: '시도할 횟수는 몇 회인가요',
+  EXECUTION_RESULT: '실행 결과',
+  WINNER: '최종 우승자 : ',
+
+  ERROR: {
+    NOT_SPLIT_COMMA: '[ERROR] 쉼표(,)로 구분되어있지 않습니다.',
+    NOT_FIVE_FIGURES: '[ERROR] 5글자가 아닙니다.',
+    NOT_NUMBER: '[ERROR] 숫자형식이 아닙니다.',
+  },
+};
+export { Message };
