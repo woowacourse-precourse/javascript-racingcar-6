74500adaea9519aec388ac2128b8168239d3d069
leeyulgok <dbfrhr20@gmail.com>
2023-10-27 15:58:23 +0900

    refactor: App 코드 리팩토링
    불필요한 에러 정리, 반복하는 함수 정리, Javascript API 활용 등

--- src/App.js
+++ src/App.js
@@ -4,105 +4,75 @@ import Car from "./Car.js";
 class App {
   async play() {
     try {
-      const names = await this.getCarNames();
-      const pureNames = this.isValidName(names);
-      const cars = this.createCars(pureNames);
-
-      const times = await this.getPlayTimes();
-      const pureTimes = this.isValidNumber(times);
-
-      this.printPlayTimes(cars, pureTimes);
-      this.getWinner(cars);
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  async getCarNames() {
-    try {
-      const names = await MissionUtils.Console.readLineAsync(
+      const names = await this.getInput(
         "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
       );
-      return names;
+      const validNames = this.validateName(names);
+      const cars = validNames.map((name) => new Car(name));
+
+      const timesInput = await this.getInput("시도할 횟수는 몇 회인가요?\n");
+      const times = this.validateNumber(timesInput);
+
+      this.playGame(cars, times);
+      this.displayWinner(cars);
     } catch (error) {
       throw error;
     }
   }
 
-  async getPlayTimes() {
-    try {
-      const times = await MissionUtils.Console.readLineAsync(
-        "시도할 횟수는 몇 회인가요?\n"
-      );
-      return times;
-    } catch (error) {
-      throw error;
-    }
+  async getInput(promptMsg) {
+    return await MissionUtils.Console.readLineAsync(promptMsg);
   }
 
-  printPlayTimes(cars, times) {
+  playGame(cars, times) {
     MissionUtils.Console.print("\n실행결과");
-    while (times > 0) {
-      for (const car of cars) {
-        car.move();
-      }
-
-      for (const car of cars) {
-        car.getPosition();
-      }
+    while (times-- > 0) {
+      this.moveCars(cars);
+      this.printCarsPosition(cars);
       MissionUtils.Console.print("");
-      times -= 1;
     }
   }
 
-  getWinner(cars) {
-    let playResult = [];
-
+  moveCars(cars) {
     for (const car of cars) {
-      playResult.push(car.position.length);
+      const value = MissionUtils.Random.pickNumberInRange(0, 9);
+      car.move(value);
     }
-    let winner = cars
-      .filter((car) => {
-        if (car.position.length === Math.max(...playResult))
-          return car.getName();
-      })
-      .map((car) => car.name)
+  }
+
+  printCarsPosition(cars) {
+    for (const car of cars) {
+      MissionUtils.Console.print(car.printPosition());
+    }
+  }
+
+  displayWinner(cars) {
+    const maxPosition = Math.max(
+      ...cars.map((car) => car.getPosition().length)
+    );
+    const winners = cars
+      .filter((car) => car.getPosition().length === maxPosition)
+      .map((car) => car.getName())
       .join(", ");
 
-    MissionUtils.Console.print(`최종 우승자 : ${winner}`);
+    MissionUtils.Console.print(`최종 우승자 : ${winners}`);
   }
 
-  createCars(names) {
-    const cars = [];
-    for (const name of names) {
-      cars.push(new Car(name));
+  validateName(names) {
+    const parsedNames = names.split(",");
+    for (const name of parsedNames) {
+      if (name.length > 5) {
+        throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+      }
     }
-    return cars;
+    return parsedNames;
   }
 
-  isValidName(names) {
-    try {
-      const newNames = names.split(",").map((name) => {
-        if (name.length > 5) {
-          throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
-        } else {
-          return name;
-        }
-      });
-      return newNames;
-    } catch (error) {
-      throw error;
+  validateNumber(times) {
+    if (!/^\d+$/.test(times)) {
+      throw new Error("[ERROR] 잘못된 값을 입력하였습니다.");
     }
-  }
-
-  isValidNumber(times) {
-    try {
-      if (!/^\d+$/.test(times))
-        throw new Error("[ERROR] 잘못된 값을 입력하였습니다.");
-      return parseInt(times, 10);
-    } catch (error) {
-      throw error;
-    }
+    return parseInt(times, 10);
   }
 }
 
