1a9d53e13c0a2de43a0649c94f2b91537aa9d78f
JUNU <bokjunwoo@gmail.com>
2023-10-28 03:05:54 +0900

    feat : 자동차 이름 문자열에서 공백 제거 기능 추가

--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,15 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 export const generateRandomNumber = (min, max) => {
   return MissionUtils.Random.pickNumberInRange(min, max);
 };
+
+export const removeSpaces = (carNames) => {
+  const carNameArray = carNames.split(',');
+
+  for (let i = 0; i < carNameArray.length; i++) {
+    const trimmedName = carNameArray[i].trim();
+
+    carNameArray[i] = trimmedName;
+  }
+
+  return carNameArray;
+};
