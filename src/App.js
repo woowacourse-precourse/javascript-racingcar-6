c739c3dddef08f1a405dc7c28b98906380c54dd5
whai2125 <whai@whai2125ui-MacBookAir.local>
2023-10-27 12:53:25 +0900

    feat: add try counter and try number validater

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ class App {
 export default App;
 
 async function carCreater() {
-  const CAR_NAME = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
+  MissionUtils.Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+  const CAR_NAME = await MissionUtils.Console.readLineAsync('');
   const CAR_NAME_SPLIT = CAR_NAME.split(',');
   validationNameLength(CAR_NAME_SPLIT);
+  return tryCounter();
 }
 
 function validationNameLength(names) {
@@ -19,5 +21,20 @@ function validationNameLength(names) {
     if (names[i].length > 5) {
       throw new Error("[ERROR]")
     }
+  }
+}
+
+async function tryCounter() {
+  MissionUtils.Console.print("시도할 횟수는 몇 회인가요?");
+  const TRY_NUMBER = await MissionUtils.Console.readLineAsync('');
+  const VALIDATER_TRY_NUMBER = validationTryNumber(TRY_NUMBER);
+  //console.log(VALIDATER_TRY_NUMBER)
+}
+
+function validationTryNumber(number) {
+  if (/^[+]?\d+$/.test(number)) {
+    return Number(number);
+  } else {
+    throw new Error("[ERROR]");
   }
 }
