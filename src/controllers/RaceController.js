4a77293ac5f75b34a4395bda1de74d042a007344
pjc0179 <pjc0179@gmail.com>
2023-10-27 14:47:27 +0900

    feat(controller):RaceController.js 로직 구현
    - 사용자에게 값을 입력받아 몇회 반복할껀지 정하는 setMaxRound 함수 구현
    - start 메서드에 대부분의 로직 구현
    - 사용자로부터 자동차 이름 및 라운드 수 입력 받음
    - 각 라운드마다 자동차를 움직이고 경기 결과 저장
    - 최종 우승자를 결정하여 출력

--- src/controllers/RaceController.js
+++ src/controllers/RaceController.js
@@ -1,6 +1,8 @@
 import CONFIG from '../constants/config.js';
 import RACE_CONSOLE_VIEW from '../views/raceView.js';
 import Car from '../models/Car.js';
+import createRandomNumber from '../utils/createRandomNumber.js';
+import MESSAGES from '../constants/messages.js';
 
 class RaceController {
   #maxRound = 0;
@@ -10,10 +12,38 @@ class RaceController {
   async start() {
     const USER_INPUT_CAR_NAME = await RACE_CONSOLE_VIEW.getUserInputCarName();
     const CAR_LIST = this.createCarList(USER_INPUT_CAR_NAME);
+
+    await this.setMaxRound();
+    const raceResults = [];
+    while (this.#maxRound !== this.#currentRound) {
+      const roundResults = [];
+      CAR_LIST.forEach((car) => {
+        createRandomNumber() >= CONFIG.MINIMUM_CAN_MOVE_FOWARD && car.move();
+        // prettier-ignore
+        roundResults.push(`${car.name} : ${MESSAGES.ONE_STEP.repeat(car.getMoveCount())}`);
+      });
+      raceResults.push(roundResults);
+      this.#currentRound += 1;
+    }
+    RACE_CONSOLE_VIEW.raceResult(raceResults);
+    RACE_CONSOLE_VIEW.winner(this.findWinner(CAR_LIST));
+  }
+
+  findWinner(carList) {
+    const MAX_MOVE_COUNT = Math.max(
+      ...carList.map((car) => car.getMoveCount())
+    );
+    return carList
+      .filter((car) => car.getMoveCount() === MAX_MOVE_COUNT)
+      .map((car) => car.name);
   }
 
   createCarList(userInputCarName) {
     return userInputCarName.split(',').map((carName) => new Car(carName));
+  }
+
+  async setMaxRound() {
+    this.#maxRound = +(await RACE_CONSOLE_VIEW.getUserInputMaxRound());
   }
 }
 
