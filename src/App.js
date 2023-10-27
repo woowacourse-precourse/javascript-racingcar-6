f8507946a0503ca102c0d4230422e3c65708f87b
hyunjun9788 <namhyunjun9788@gmail.com>
2023-10-27 19:03:18 +0900

    Feat: 자동차 위치 업데이트 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 class App {
   constructor() {
-    this.name = [];
-    this.count = [];
-    this.randomNum = [];
-    this.gameResult = {};
+    this.carNameArr = [];
+    this.dashSymbol = [];
+    this.randomNumArr = [];
+    this.carPosition = {};
+    this.attemptCount = 0;
+    this.dash = "-";
   }
   async play() {
     Console.print(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     );
     const carNameList = await this.inputCarName();
-    this.name = convertStrToArr(carNameList);
-    console.log(this.name);
+    this.carNameArr = convertStrToArr(carNameList);
     Console.print("시도할 횟수는 몇 회인가요?");
-    const attemptCount = await this.inputAttemptCount();
-    this.isValidAttemptCount(attemptCount);
+    this.attemptCount = await this.inputAttemptCount();
+    this.isValidAttemptCount(this.attemptCount);
     Console.print("");
     Console.print("실행 결과");
-    this.createRandomNumber();
-
-    console.log(this.randomNum);
   }
 
   async inputCarName() {
@@ -36,14 +34,28 @@ class App {
       throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
     }
   }
+
   createRandomNumber() {
-    for (let i = 0; i < this.name.length; i++) {
+    this.randomNumArr = [];
+    for (let i = 0; i < this.carNameArr.length; i++) {
       const randomNum = Random.pickNumberInRange(0, 9);
-      this.randomNum.push(randomNum);
+      this.randomNumArr.push(randomNum);
     }
-    return this.randomNum;
+  }
+  updateCarPosition() {
+    if (!this.dashSymbol) {
+      this.dashSymbol = this.carNameArr.map(() => "");
+    }
+    for (let i = 0; i < this.carNameArr.length; i++) {
+      if (!this.dashSymbol[i]) {
+        this.dashSymbol[i] = "";
+      }
+      const dashes = "-".repeat(this.randomNumArr[i] >= 4 ? 1 : 0);
+      this.dashSymbol[i] += dashes;
+    }
   }
 }
+
 const convertStrToArr = (nameList) => {
   const splitName = nameList.split(",");
   return splitName;
