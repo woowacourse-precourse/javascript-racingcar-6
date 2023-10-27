9a9e04de8b53da76b967145666c817223c62af78
lcy <8410294@naver.com>
2023-10-27 21:45:05 +0900

    Fix: input description

--- src/InOutPut.js
+++ src/InOutPut.js
@@ -13,6 +13,7 @@ export const verificationNames = (carNames) => {
 };
 
 export const inputCarName = async () => {
+  Console.print('경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)');
   const carNames = await Console.readLineAsync().split(',');
 
   verificationNames(carNames);
