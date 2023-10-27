b50552a423505cf09945897ef33e36f5a09b4bf5
coldok <2coldok@gmail.com>
2023-10-27 22:05:31 +0900

    feat : 사용자 입력이 올바른지 판단후 에러메세지 출력

--- src/constants/Messeage.js
+++ src/constants/Messeage.js
@@ -9,4 +9,5 @@ export const ERROR_MESSEAGE = Object.freeze({
   nameLength : '[ERROR] 자동차 이름은 최대 5글자까지 가능합니다.',
   nameSingle : '[ERROR] 게임을 시작하기 위해선 2대 이상의 자동차가 필요합니다.',
   commaMisuse : '[ERROR] 쉼표(,)의 사용이 잘못되었습니다.',
+  attempt : '[ERROR] 올바른 숫자 형식이 아닙니다',
 })
--- src/errors/Errors.js
+++ src/errors/Errors.js
@@ -2,11 +2,18 @@ import { ERROR_MESSEAGE } from "../constants/Messeage.js";
 import SingleName from "./SingleName.js";
 
 class Errors extends SingleName {
-  show(players) {
+  name(players) {
     this.showCommaError(players);
     this.showOverNameError(players);
     this.showSingleNameError(players);
   }
+
+  attempt(number) {
+    if(isNaN(number)) {
+      throw new Error(ERROR_MESSEAGE.attempt);
+    }
+  }
 }
 
 export default Errors
+
