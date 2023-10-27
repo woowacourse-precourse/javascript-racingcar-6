94ab3407c2ae02e296661c5cf326d2e3bae62b7b
8leamnk <silkviewrk@gmail.com>
2023-10-27 14:34:39 +0900

    feat: private 변수들과 이에 대한 getter, setter 함수를 추가함

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 class App {
+  #cars = [];
+  #numberOfTimes = 0;
+
+  get cars() {
+    return this.#cars;
+  }
+
+  set cars(info) {
+    this.#cars = info;
+  }
+
+  get numberOfTimes() {
+    return this.#numberOfTimes;
+  }
+
+  set numberOfTimes(number) {
+    this.#numberOfTimes = number;
+  }
+
   async play() {}
 }
+
+const app = new App();
+app.play();
 
 export default App;
