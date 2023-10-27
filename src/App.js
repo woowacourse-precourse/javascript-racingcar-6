d055a56c36fbd7823fd226bc66ea8ba214a49316
hellosehyun <sehyun030526@gmail.com>
2023-10-27 10:55:44 +0900

    chore: getPlayer 리턴 타입 수정
    getPlayer의 리턴 타입을 배열에서 객체로 수정

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@ import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
     async play() {
-        Console.print(await this.getPlayer());
+        const PLAYERS = await this.getPlayer();
+        const NUMBER = await this.getNumber();
+
+        this.result(PLAYERS);
+        console.log(PLAYERS);
     }
 
+    // 플레이어 객체 반환 함수
     async getPlayer() {
-        const PLAYERS = await Console.readLineAsync(
+        let players = await Console.readLineAsync(
             "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
         );
+        players = players.split(","); // ['pobi', 'woni', 'jun']
 
-        return PLAYERS.split(",");
+        const TMP_OBJ = players.reduce((accumulator, value) => {
+            return { ...accumulator, [value]: "" };
+        }, {});
+
+        return TMP_OBJ;
     }
 
+    // 반복 횟수 반환 함수
     async getNumber() {
         const NUMBER = await Console.readLineAsync(
             "시도할 횟수는 몇 회인가요?"
