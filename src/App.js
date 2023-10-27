b61047918c21abd5062f8137c3add9a767ad78f5
yunsoeirm1116 <hhyjs2r@gmail.com>
2023-10-27 11:31:54 +0900

    [feat/fix] 몇번 이동할지 입력받는 함수 생성, 프린트 출력

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ class App {
 
   async play() {
     await this.setCarList();
-    this.moveCount = await Console.readLineAsync('시도할 횟수는 몇 회인가요?');
   }
 
   async setCarList() {
     const inputResult = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
     this.validateCarNameList(inputResult);
+    await this.printGameLimit();
   }
 
   validateCarNameList(inputResult) {
@@ -18,7 +18,6 @@ class App {
     this.carList.forEach(car => this.validateCarName(car));
 
     const isDuplicate = this.isDuplicateCarName();
-
     if (isDuplicate) {
       throw new Error('[ERROR] car 이름은 중복이 불가합니다.');
     }
@@ -37,6 +36,11 @@ class App {
     if (carName.length > 5) {
       throw new Error('[ERROR] car 이름은 5자 이하만 가능합니다.');
     }
+  }
+
+  async printGameLimit() {
+    this.moveCount = await Console.readLineAsync('시도할 횟수는 몇 회인가요?');
+    Console.print(`${this.moveCount}회`);
   }
 }
 
