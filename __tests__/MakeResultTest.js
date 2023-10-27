2789bf5dcda1ede917c0c1f179399637b47b60c2
PARK-CHANUK <doosan1224@naver.com>
2023-10-27 22:21:13 +0900

    test(MakeResultTest): 회차마다 결과를 저장하는 테스트 코드 작성 - 회차마다 레이싱 결과를 만듬

---
+++ __tests__/MakeResultTest.js
@@ -0,0 +1,21 @@
+import ResultModel from '../src/Model/ResultModel';
+
+describe('ResultModel Test', () => {
+  let resultModel;
+  beforeEach(() => {
+    resultModel = new ResultModel();
+  });
+  test('회차마다 레이싱 결과를 만든다.', () => {
+    const cars = [
+      { name: 'pobi', moveCounts: 1 },
+      { name: 'ukgi', moveCounts: 0 },
+    ];
+    resultModel.addAttempsResult(cars);
+    expect(resultModel.getResult()).toEqual([
+      [
+        ['pobi', 1],
+        ['ukgi', 0],
+      ],
+    ]);
+  });
+});
