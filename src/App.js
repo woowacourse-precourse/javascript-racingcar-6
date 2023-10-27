91ff55d8e51851de550cbadb5561c8fc62064603
Jiwon kang <kk3012002@gmail.com>
2023-10-27 15:18:44 +0900

    메시지 클래스 분리

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import { Message } from "./message.js";
 
 class App {
-  async play() {}
+  async play() {
+    const carArray = await getCarInput();
+    validateCarArray(carArray);
+    MissionUtils.Console.print(carArray);
+  }
 }
 
 async function getCarInput() {
@@ -10,24 +13,19 @@ async function getCarInput() {
   const input = await MissionUtils.Console.readLineAsync(
     `${message.carInputMessage}`
   );
-  const carArray = input.split(",");
-  return carArray;
-}
-
-async function inputToArray(input) {
-  const carArray = input.split(",");
+  const carArray = input.split(`,`);
   return carArray;
 }
 
 async function validateCarArray(carArray) {
   const message = new Message();
-  if (carArray.length < 2) {
+  if (carArray.length < MIN_CAR_COUNT) {
     throw new Error(`${message.carInputErrorMessage4}`);
   }
-  if (carArray.length > 10) {
+  if (carArray.length > MAX_CAR_COUNT) {
     throw new Error(`${message.carInputErrorMessage5}`);
   }
-  if (carArray.some((car) => car.length > 10)) {
+  if (carArray.some((car) => car.length > MAX_NAME_LENGTH)) {
     throw new Error(`${message.carInputErrorMessage}`);
   }
   if (carArray.some((car) => car.trim().length === 0)) {
@@ -37,5 +35,8 @@ async function validateCarArray(carArray) {
     throw new Error(`${message.carInputErrorMessage3}`);
   }
 }
+
+const app = new App();
+app.play();
 
 export default App;
---
+++ src/constants/constants.js
@@ -0,0 +1,12 @@
+// 파일명: constants.js
+// 설명: 상수 모음
+
+const CAR = {
+  MAX_NAME_LENGTH: 10,
+  MAX_CAR_COUNT: 10,
+  MIN_CAR_COUNT: 2,
+};
+
+const RACE = {
+  MAX_TRY_COUNT: 100,
+};
