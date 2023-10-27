a48f00317834075ff7d1f67a05dbd8720148d06b
hellosehyun <sehyun030526@gmail.com>
2023-10-27 11:03:58 +0900

    feat: raceController 추가, chore: inputPlayers, inputNumber로 함수명 변경
    players객체를 받아 1회 경기를 진행하는 함수 추가
    getPlayer => inputPlayer , getNumber => inputNumbe로 변경

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
     async play() {
-        const PLAYERS = await this.getPlayer();
-        const NUMBER = await this.getNumber();
+        let players = await this.inputPlayers();
+        const NUMBER = await this.inputNumber();
 
-        this.result(PLAYERS);
-        console.log(PLAYERS);
+        for (let i = 0; i < NUMBER; i++) {
+            this.race(players);
+        }
     }
 
     // 플레이어 객체 반환 함수
-    async getPlayer() {
+    async inputPlayers() {
         let players = await Console.readLineAsync(
             "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
         );
@@ -24,11 +25,19 @@ class App {
     }
 
     // 반복 횟수 반환 함수
-    async getNumber() {
+    async inputNumber() {
         const NUMBER = await Console.readLineAsync(
             "시도할 횟수는 몇 회인가요?"
         );
         return Number(NUMBER);
+    }
+
+    // 경기 진행 함수
+    raceController(players) {
+        Object.keys(players).forEach((value) => {
+            if (4 <= Random.pickNumberInRange(0, 9)) players[value] += "-";
+        });
+        return;
     }
 }
 
