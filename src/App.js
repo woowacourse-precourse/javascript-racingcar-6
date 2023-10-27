f6eb97e02026fc1056696a1512091a6199e8d8f7
KimKyungYun <60084235+KimKyungYun@users.noreply.github.com>
2023-10-27 15:13:24 +0900

    feat: 입력내용 쉼표기준 분리기능, 경주시작 기능 제작

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 class App {
 	// 게임 시작
-	async start() {}
+	async start() {
+		const inputCars = await Console.readLineAsync(MESSAGE.start);
+		this.getCars(inputCars);
+	}
+
 	// 자동차 이름 받고 분리하기
-	getCars(input) {}
+	getCars(input) {
+		const cars = input.split(',').map((element) => ({
+			name: element,
+			count: '',
+		}));
+		this.racing(cars);
+	}
+	
 	// 입력받은 시도횟수만큼 전진/정지
-	async racing(cars) {}
+	async racing(cars) {
+		const tryCount = await Console.readLineAsync(MESSAGE.race);
+		console.log('시도횟수: ', tryCount);
+		Console.print('실행 결과');
+		for (let i = 0; i < tryCount; i++) {
+			const carsNumber = getCarsNumber(cars.length);
+			this.addReps(cars, carsNumber);
+			this.printCars(cars);
+		}
+		this.winner(cars);
+	}
 	//전진/정지 판단
 	addReps(cars, carsNumber) {}
 	//단계별 진행상황 출력
