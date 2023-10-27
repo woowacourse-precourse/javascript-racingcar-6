f0650717bf82c2202b3cc350b5fa87217667f673
thewronghand <penfreak77@gmail.com>
2023-10-27 13:53:37 +0900

    refactor: 유효성 검사 내용 추가, 새 에러 메세지와 CustomError 적용

--- src/utils/InputValidator.js
+++ src/utils/InputValidator.js
@@ -1,18 +1,29 @@
-import { ERROR_MESSAGES, GAME_NUMBERS } from './constants';
+import CustomError from '../errors/CustomError.js';
+import { ERROR_MESSAGES, GAME_NUMBERS } from './constants.js';
 
 class InputValidator {
-  validateCarName(name) {
-    if (name.length > GAME_NUMBERS.carNameMaxLength) {
-      throw new Error(ERROR_MESSAGES.invalidCarNameLength);
+  validateCarNames(names) {
+    for (let name of names) {
+      if (name.length > GAME_NUMBERS.carNameMaxLength) {
+        throw new CustomError(ERROR_MESSAGES.carName.invalidLength);
+      }
+      if (name.trim().length === 0) {
+        throw new CustomError(ERROR_MESSAGES.carName.blank);
+      }
+    }
+
+    const uniqueNames = [...new Set(names)];
+    if (uniqueNames.length !== names.length) {
+      throw new CustomError(ERROR_MESSAGES.carName.duplicate);
     }
   }
 
   validateRoundsNumber(roundsNumber) {
     if (isNaN(roundsNumber)) {
-      throw new Error(ERROR_MESSAGES.invalidMovementCountNumber);
+      throw new CustomError(ERROR_MESSAGES.roundsNumber.notNumber);
     }
     if (roundsNumber < 0) {
-      throw new Error(ERROR_MESSAGES.invalidMovementCountNumber);
+      throw new CustomError(ERROR_MESSAGES.roundsNumber.negativeValue);
     }
   }
 }
