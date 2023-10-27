1bd6b32c86094db577f6af3a621eca0f7c2ce816
anr4876 <dustjdanr@naver.com>
2023-10-27 19:08:37 +0900

    feat: 사용자 입력 기능 구현 예외 처리를 제외한 기능, 중복으로 사용하는 코드를 하나의 함수로 구현,

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+
+
+// 출력할 문구들
+const MESSAGE = {
+  CAR: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+  NUM: '시도할 횟수는 몇 회인가요?\n',
+  RESULT: '실행결과'
+}
+
+// 사용자 입력 추출
+async function getUserData(message){
+  const USER_DATA = await Console.readLineAsync(message);
+  return USER_DATA;
+}
+
 class App {
-  async play() {}
+  async play() {
+    const CAR_NAMES = await getUserData(MESSAGE.CAR);
+    const USER_NUMBER = await getUserData(MESSAGE.NUM);
+
+    
+  }
 }
 
 export default App;
