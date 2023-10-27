4d233462eb3cddb4605f0dd146a77cfdbe1c3134
leedbswo107 <ljyu1@naver.com>
2023-10-27 16:13:15 +0900

    feat: StringTest 요구사항 만족

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    await Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    const cars = await Console.readLineAsync('');
+    const users = cars.split(',');
+    await Console.print(cars);
+    await Console.print(users);
+    await Console.print("시도할 횟수는 몇 회인가요?");
+    const numberOfAttempts = await Console.readLineAsync('');
+    await Console.print(`시도 횟수 : ${numberOfAttempts}`); 
+  }
 }
+
+// const status = await Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
 export default App;
