2e74270de7c3fdd3083dcf51daa419dbe041fbe0
ss0526100 <ss0526100@gmail.com>
2023-10-27 20:02:36 +0900

    feat(ConvertInputTo): input car name list

---
+++ src/modules/ConverInputTo.js
@@ -0,0 +1,10 @@
+import { Console } from '@woowacourse/mission-utils';
+
+const CAR_LIST_INPUT_MESSAGE =
+  '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)';
+
+class ConvertInputTo {
+  static async carList() {
+    const inputString = await Console.readLineAsync(query);
+  }
+}
