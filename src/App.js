cbfb086d7ee058f8d8b5d67f2a134899b76a78f3
Seungmani <dkelcncjs@naver.com>
2023-10-27 15:03:01 +0900

    [feat] 참가자 명단 확인 기능

--- src/App.js
+++ src/App.js
@@ -10,9 +10,30 @@ const readLineAsync = async (message) => {
   return await Console.readLineAsync(message);
 }
 
+
+/**
+ * 게임 참가자 명단을 받는 기능
+ * 입력 : x
+ * return : 참가자 명단은 배열로 반환
+ * 주의 : 참가자 이름이 5글자 초과면 ERROR
+ */
+const getParticipant = async () =>{
+  const participants = await readLineAsync('경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)');
+  const participantsArr = participants.split(',')
+
+  // *** 리팩토링 하면서 함수로 뺴기 ***
+  for(let name of participantsArr){
+    if(name.length > 5){
+      throw new Error('[ERROR] 참가자 이름이 5글자를 초과합니다.'); // 추후에 ERROR문구 상수로 빼기
+    }
+  }
+
+  return participantsArr;
+}
+
 class App {
   async play() {
-    print()
+    const participants = await getParticipant()
   }
 }
 
