3c402efa341314d553d899ab8d69b0647e431c95
zelkovaria <hyein396@gmail.com>
2023-10-27 18:00:09 +0900

    feat: 5글자 초과 이름 입력시 예외 처리

--- src/car.js
+++ src/car.js
@@ -4,5 +4,13 @@ export async function carGenerate() {
   const userInputCars = await MissionUtils.Console.readLineAsync("입력: ");
   const carsList = userInputCars.split(",");
 
-  return carsList;
+  const carNameIsValid = carsList.every(
+    (nameLength) => nameLength.length <= 5 && nameLength.length > 0
+  );
+  if (carNameIsValid) {
+    return carsList;
+  }
+  if (!carNameIsValid) {
+    throw new Error("[ERROR] 5글자 이내의 이름을 입력하세요.");
+  }
 }
