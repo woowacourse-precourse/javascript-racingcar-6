636ef276040fe1fc9725a8b6b60f263ef469103e
KimKyungYun <60084235+KimKyungYun@users.noreply.github.com>
2023-10-27 15:07:00 +0900

    feat: 함수 구조 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 class App {
-  async play() {}
+	// 게임 시작
+	async start() {}
+	// 자동차 이름 받고 분리하기
+	getCars(input) {}
+	// 입력받은 시도횟수만큼 전진/정지
+	async racing(cars) {}
+	//전진/정지 판단
+	addReps(cars, carsNumber) {}
+	//단계별 진행상황 출력
+	printCars(cars) {}
+	// 우승자 출력
+	winner(cars) {}
+
+	async play() {
+		this.start();
+	}
 }
 
 export default App;
---
+++ src/utils/Random.js
@@ -0,0 +1,6 @@
+// 자동차의 갯수만큼 랜덤값 받아오기
+const getCarsNumber = (carCount) => {
+
+};
+
+export default getCarsNumber;
