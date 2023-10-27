d58dd133816eb1446409bffb293d85d515a788c3
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-10-27 19:48:03 +0900

    :white_check_mark: test: 랜덤값 테스트코드 수정

--- __tests__/RacingGameTest.js
+++ __tests__/RacingGameTest.js
@@ -2,19 +2,20 @@ import CarRacingGame from '../src/domain/CarRacingGame';
 
 describe('CarRacingGame', () => {
   test('라운드마다 객체배열 형태의 진행결과값을 반환하는지 테스트', () => {
+    const randomNumberGenerator = jest.fn();
+    randomNumberGenerator.mockReturnValueOnce(1).mockReturnValueOnce(4);
+
     const carNames = ['a', 'b'];
     const round = 1;
-    const randomNumber = [1, 4];
     const carRacingGame = new CarRacingGame(carNames, round);
 
-    carRacingGame.race(randomNumber);
+    carRacingGame.race(randomNumberGenerator);
 
-    const result = carRacingGame.getRoundResult();
     const expected = [
       { name: 'a', progress: 0 },
       { name: 'b', progress: 1 },
     ];
 
-    expect(result).toEqual(expected);
+    expect(carRacingGame.getRoundResult()).toEqual(expected);
   });
 });
