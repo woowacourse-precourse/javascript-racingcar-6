10307d4fa24a112d534643295c0c8f873493be55
JiSeong Kim <12180543@inha.edu>
2023-10-28 02:40:24 +0900

    style(constants): change keys name to snake case in errorMessage
    메시지의 요소들은 상수이기에 snake case로 변환

--- src/constants/errorMessage.js
+++ src/constants/errorMessage.js
@@ -1,9 +1,9 @@
 const errorMessage = Object.freeze({
-	'emptyString' : '[ERROR] 자동차 이름을 입력해주세요',
-	'carNameNotSet' : '[ERROR] 모든 자동차에 이름을 지정해주세요',
-	'exceededLength' : '[ERROR] 5자 이하의 자동차 이름을 지정해주세요',
-	'duplicatedName' : '[ERROR] 중복되지 않은 이름을 지정해주세요',
-	'wrongRoundsCount' : '[ERROR] 자연수를 입력해주세요',
+	EMPTY_STRING: '[ERROR] 자동차 이름을 입력해주세요',
+	CAR_NAME_NOT_SET: '[ERROR] 모든 자동차에 이름을 지정해주세요',
+	EXCEEDED_LENGTH: '[ERROR] 5자 이하의 자동차 이름을 지정해주세요',
+	DUPLICATED_NAME: '[ERROR] 중복되지 않은 이름을 지정해주세요',
+	WRONG_ROUNDS_COUNT: '[ERROR] 자연수를 입력해주세요',
 });
 
 export default errorMessage;
