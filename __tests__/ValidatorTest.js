20aeae4be4aacabbaeb5474c7cc771f084eb80df
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-10-28 06:46:43 +0900

    :white_check_mark: test: 입력값 유효성 검사 테스트 추가 입력받은 이름이 2개 이상인지 테스트함, 입력받은 각 이름이 1글자 이상 5글자 이하인지 테스트함, 입력받은 시도할 횟수가 1이상인 숫자인지 테스트함

---
+++ __tests__/ValidatorTest.js
@@ -0,0 +1,19 @@
+import InputValidator from '../src/domain/InputValidator';
+
+describe('InputValidator', () => {
+  const inValidCountCars = [0, 1];
+  const invalidCarNames = ['아메리칸드림', ''];
+  const invalidRounds = ['0', '-1', '', '10a', 'a10', 'abc', '!'];
+
+  test.each(inValidCountCars)('입력한 자동차의 이름이 2개 이상인지 테스트한다.', (count) => {
+    expect(InputValidator.hasValidCountCars(count)).toBe(false);
+  });
+
+  test.each(invalidCarNames)('자동차 이름이 1글자 이상 5글자 이하인지 테스트한다.', (carNames) => {
+    expect(InputValidator.hasValidCarName(carNames)).toBe(false);
+  });
+
+  test.each(invalidRounds)('시도할 횟수가 1이상인 숫자인지 테스트한다.', (rounds) => {
+    expect(InputValidator.hasValidCarName(rounds)).toBe(false);
+  });
+});
