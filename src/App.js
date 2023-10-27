1ef0ff03c25d981a8eac54bdb003de7f766157e4
MinGu Jeong <mingu830@gmail.com>
2023-10-27 22:55:33 +0900

    feat: 자동차 이름 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    getCarName();
+  }
 }
 
 export default App;
+
+async function getCarName() {
+  const carName = await Console.readLineAsync(
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+  );
+  const carNames = carName.split(',').map((name) => name.trim());
+  const carObject = carNames.map((name) => ({ name, position: 0 }));
+  return carObject;
+}
