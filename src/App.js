11e77e802e1f8c4b182a31c8c0c18a1aab5fe26e
djLee77 <dkdnl232@gmail.com>
2023-10-27 20:16:54 +0900

    fix(App, Race): .js 확장자를 import 경로에 추가
    1. App.js에서 Race를 import 할 때 .js 확장자를 명시함.
    2. Race.js에서 Car를 import 할 때 .js 확장자를 명시함.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Console } from "@woowacourse/mission-utils";
-import Race from "./Race";
+import Race from "./Race.js";
 
 class App {
   async play() {
--- src/Race.js
+++ src/Race.js
@@ -1,5 +1,5 @@
 import { Random } from "@woowacourse/mission-utils";
-import Car from "./Car";
+import Car from "./Car.js";
 
 class Race {
   constructor(arrCarName) {
