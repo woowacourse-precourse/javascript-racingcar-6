0f4ab9d625538cdd8569ebf87b26ad2f33415914
Collection50 <fkaus65@naver.com>
2023-10-27 20:09:48 +0900

    feat(message): 게임에 사용되는 문자열을 상수화하여 저장

---
+++ src/RacingCarGame/constants/index.js
@@ -0,0 +1,1 @@
+export { default as message } from './message.js';
---
+++ src/RacingCarGame/constants/message.js
@@ -0,0 +1,13 @@
+export const SYMBOLS = {
+  nameDivider: ',',
+  winnerDivider: ', ',
+  colon: ' : ',
+};
+
+export const MESSAGE = {
+  askCarNames:
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+  askLapCount: '시도할 횟수는 몇 회인가요?\n',
+  result: '실행 결과\n',
+  winner: '최종 우승자 ',
+};
