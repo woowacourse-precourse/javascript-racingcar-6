eff4f30e81e21491e038b5ada654cae2e5e4ff7b
Rki0 <pky00823@gmail.com>
2023-10-27 09:24:11 +0900

    refactor: 경주 초기 설정 로직 모듈화
    경주의 초기 설정 입력하는 로직들을 하나로 묶어 메서드로 분리한다.
    이에 따라 this를 활용하기 위해 생성자를 추가하였다.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import Print from "./Print.js";
 import Car from "./Car.js";
 
 class App {
-  async play() {
-    const cars = await Print.getCars();
-    const tryCount = await Print.getTryCount();
+  constructor() {
+    this.carInstanceArray = [];
+    this.tryCount = 0;
+  }
 
-    let carInstanceArray;
-    carInstanceArray = cars.map((carName) => new Car(carName));
+  async play() {
+    await this.setInitialState();
+  }
+
+  async setInitialState() {
+    const cars = await Print.getCars();
+    this.tryCount = await Print.getTryCount();
+    this.carInstanceArray = cars.map((carName) => new Car(carName));
   }
 }
 
