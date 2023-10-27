a138e511e8c4f30cb147fc14f1cc66ae9d15dea2
ss0526100 <ss0526100@gmail.com>
2023-10-27 21:01:31 +0900

    feat(ErrorCheck): check game count input

--- src/modules/ConvertInputTo.js
+++ src/modules/ConvertInputTo.js
@@ -3,16 +3,18 @@ import ErrorCheck from './ErrorCheck.js';
 
 const CAR_LIST_INPUT_MESSAGE =
   '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)';
+const GAME_COUNT_INPUT_MESSAGE = '시도할 횟수는 몇 회인가요?';
 
 class ConvertInputTo {
   static async carList() {
-    const inputString = await Console.readLineAsync();
+    const inputString = await Console.readLineAsync(CAR_LIST_INPUT_MESSAGE);
     ErrorCheck.carListString(inputString);
     return inputString.split(',');
   }
 
   static async gameCount() {
-    const inputString = await Console.readLineAsync();
+    const inputString = await Console.readLineAsync(GAME_COUNT_INPUT_MESSAGE);
+    ErrorCheck.gameCountString(inputString);
   }
 }
 
--- src/modules/ErrorCheck.js
+++ src/modules/ErrorCheck.js
@@ -1,9 +1,15 @@
-const EXCEEDED_NAME_LENGTH_ERROR = '[ERROR] 이름 길이 제한을 초과했습니다.';
+const EXCEEDED_NAME_LENGTH_ERROR_MSG = '[ERROR] 이름 길이 제한을 초과했습니다.';
+const WRONG_FORMAT_NUMBER_ERROR_MSG = '[ERROR] 숫자가 잘못된 형식입니다.';
 class ErrorCheck {
   static carListString(string) {
     string.split(',').forEach(name => {
-      if (name.length > 5) throw new Error(EXCEEDED_NAME_LENGTH_ERROR);
+      if (name.length > 5) throw new Error(EXCEEDED_NAME_LENGTH_ERROR_MSG);
     });
+  }
+
+  static gameCountString(string) {
+    const numberFromString = Number(string);
+    if (isNaN(numberFromString)) throw new Error(WRONG_FORMAT_NUMBER_ERROR_MSG);
   }
 }
 
