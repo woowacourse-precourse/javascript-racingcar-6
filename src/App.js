1eca545c0664502f441e5ee7ff2b08e978593c2e
Seungmani <dkelcncjs@naver.com>
2023-10-27 14:40:39 +0900

    [feat] 입,출력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const { Console, Random } = MissionUtils;
+
+const print = (message) => {
+  Console.print(message);
+}
+
+const readLineAsync = async (message) => {
+  return await Console.readLineAsync(message);
+}
+
 class App {
   async play() {
+    print()
   }
 }
 
