8fd51d5c408235356c7a81050971dbbbe5cc69da
unknown <dkvz@naver.com>
2023-10-27 11:25:02 +0900

    자동차 이름 객체 만들고 시도 횟수 에러 점검

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import { MESSAGE } from "./constant/message";
+import { ERROR } from "./constant/error";
 
 class App {
   async play(){}
 
 async getNameOfCar(){
-  const carName = await MissionUtils.Console.readLineAsync(MESSAGE.name);
-  this.attemptNumber(carName);
+  const carName = await MissionUtils.Console.readLineAsync(MESSAGE.NAME);
+  const arrayCarName = carName.spilt(",")
+  this.attemptNumber(arrayCarName);
 }
-async attemptNumber(carName){
-  const tryNumber = await MissionUtils.Console.readLineAsync(MESSAGE.number);
+async attemptNumber(arrayCarName){
+  const tryNumber = await MissionUtils.Console.readLineAsync(MESSAGE.NUMBER);
+  this.gameReady(arrayCarName,tryNumber);
+}
 
+// 각 자동차 이름 객체 만들고 시도 횟수 에러 점검
+gameReady(arrayCarName,tryNumber){
+  if(isNaN(parseInt(tryNumber))===false){throw new Error(ERROR.NUMBER);}
+  if(parseFloat(tryNumber) !== parseInt(tryNumber)) {throw new Error(ERROR.INTEGER);}  
+  const objectCarName = new Object()
+  arrayCarName.forEach((a,i) => {
+    objectCarName[a] = 0;
+  });
 }
 
 
-
-
-
+randomNumber(){
+  const processNumber = MissionUtils.Random.pickNumberInRange(0,9)
+  return processNumber;
+}
 
 }
 
--- src/constant/error.js
+++ src/constant/error.js
@@ -1,3 +1,5 @@
 export const ERROR ={
-    five:'[ERROR] 이름은 5자 이내 입니다'
+    FIVE:'[ERROR] 이름은 5자 이내 입니다',
+    INTEGER:'[ERROR] 소수를 입력하시면 안됩니다.',
+    NUMBER:'[ERROR] 숫자를 입력하셔야 합니다'
 }
