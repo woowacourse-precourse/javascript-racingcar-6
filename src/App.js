1802d302cfed061d5714b69b58967040a6d8c57f
Hong sung soo <sow9707@daum.net>
2023-10-27 17:07:56 +0900

    feat: 사용자에게 자동차 이름을 입력받고 배열로 반환하는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const Console = MissionUtils.Console;
+const Random = MissionUtils.Random;
+
 class App {
+  constructor() {}
+  async carNameInput() {
+    const userInput = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    return userInput.split(","); 
+  }
   async play() {}
 }
 
