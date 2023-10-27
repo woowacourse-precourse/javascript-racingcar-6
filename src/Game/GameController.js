7d05077b3f37ac8b6f6f0dbc78678e290f8de43d
hongst222 <tmdxordusdo@gmail.com>
2023-10-28 07:08:44 +0900

    feat: game 폴더를 생성하여 GameController.js 파일위치를 변경, 메소드 추가
    파일 관리를 용이하게 하기 위해 폴더를 새로 생성하고, 파일 위치를 변경하였다.
    
    GameController.js에 아래와 같은 메소드를 추가하였다.
    
    inputTryMoveCount(): 전진 시도 횟수를 입력받는다.

---
+++ src/Game/GameController.js
@@ -0,0 +1,27 @@
+import { MESSAGES } from "../constants/index.js";
+import { print, readLineAsync } from "../utils/index.js";
+import Validate from "./Validate.js";
+import Racing from "./Racing.js";
+class GameController {
+  
+    constructor() {
+        this.validate = new Validate();
+        this.racing = new Racing();
+
+    }
+  
+    async inputCars() {
+        const input = await readLineAsync(MESSAGES.QUEST_CAR_NAME);
+        this.validate.validateCarName(input);
+        // Racing 클래스의 car 멤버변수에 배열을 집어넣을 예정
+        this.racing.registCar(input);
+    }
+    async inputTryMoveCount() {
+        const input = await readLineAsync(MESSAGES.QUEST_TRY_COUNT);
+        this.validate.validateTryMoveCount(input);
+        return input;
+    }
+    
+}
+
+export default GameController;
