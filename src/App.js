fc372470329c1efa4c3a5084b32993fb88a8b793
hyun812 <sh980812@naver.com>
2023-10-28 00:41:34 +0900

    [feat] 출력 및 에러 메세지 설정

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async play() {
     try {
-      const carName = await Console.readLineAsync(`경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n`);
-      const count = await Console.readLineAsync(`시도할 횟수는 몇 회 인가요?\n`);
+      const carName = await Console.readLineAsync();
+      const count = await Console.readLineAsync(
+        `시도할 횟수는 몇 회 인가요?\n`
+      );
 
       const carArr = carName.split(",");
-      
     } catch (error) {
       throw new Error("[ERROR] 잘못된 형식입니다.");
     }
-
   }
 }
 
