4748f999692c3e55882037bf800f68fb3baca38d
ceh20002@naver.com <ceh20002@naver.com>
2023-10-28 00:49:48 +0900

    자동차 이름 받아오는 기능 구현 및 사용자가 원하는 게임 전진 도전 횟수 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-class App {
-  async play() {
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { get } from 'mongoose';
 
+class App {
+
+  constructor() {
   }
+
+  async play() {
+    await this.getUserCarListInput();
+    await this.getUserWantMoveCount();
+  }
+
+  // 사용자가 부여한 자동차 이름들을 받아오는 코드
+  async getUserCarListInput(){
+    const userCarListInput = await MissionUtils.Console.readLineAsync("차 이름을 입력해주세여");
+    const carList = userCarListInput.split(',');
+    console.log(carList);
+    return carList;
+  }
+
+  // 사용자가 원하는 게임 전진 도전 횟수
+  async getUserWantMoveCount(){
+    const inputCount = Number(await MissionUtils.Console.readLineAsync("몇번"));
+    console.log(inputCount);
+    return inputCount;
+  }
+
+
+
+
 }
 
 export default App;
