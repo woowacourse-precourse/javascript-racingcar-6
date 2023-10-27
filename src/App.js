47d745c50c70e10bf90b1af8a3c77fb9b75b7a63
JunSeok <ppu1234@naver.com>
2023-10-27 12:27:58 +0900

    refactor: 데이터 입력 함수, 게임 플레이 함수 모듈화

--- src/App.js
+++ src/App.js
@@ -1,40 +1,15 @@
-import { checkErrorInputName, checkErrorPlayNumber } from './validation.js';
+import { getCarName, getPlayNumber } from './gameData.js';
 
-import { Console } from '@woowacourse/mission-utils';
-import { INFO_MESSAGE } from './message.js';
-import checkRandomNumber from './randomNumber.js';
+import playGame from './playGame.js';
 import printWinner from './printWinner.js';
 
 class App {
-  constructor() {
-    this.carName = new Map();
-    this.playNumber = 0;
-  }
   async play() {
-    await this.getCarName();
-    await this.getPlayNumber();
-    Console.print(INFO_MESSAGE.result);
-    while (this.playNumber--) {
-      for (const [name, moveLog] of this.carName) {
-        let flag = await checkRandomNumber();
-        if (flag) {
-          this.carName.set(name, moveLog + '-');
-        }
-        Console.print(`${name} : ${this.carName.get(name)}`);
-      }
-      Console.print(' ');
-    }
-    printWinner(this.carName);
-  }
-  async getCarName() {
-    const inputNameArr = (await Console.readLineAsync(INFO_MESSAGE.carName)).split(',').map((name) => name.trim());
-    checkErrorInputName(inputNameArr);
-    inputNameArr.forEach((name) => this.carName.set(name, ''));
-  }
-  async getPlayNumber() {
-    const inputNumber = Number(await Console.readLineAsync(INFO_MESSAGE.playNumber));
-    checkErrorPlayNumber(inputNumber);
-    this.playNumber = inputNumber;
+    let carName = new Map();
+    await getCarName(carName);
+    let playNumber = await getPlayNumber();
+    playGame(playNumber, carName);
+    printWinner(carName);
   }
 }
 
