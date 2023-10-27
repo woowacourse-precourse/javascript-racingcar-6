3d354c6e0d045cae9bedfb408701cd8192a4eaea
0jenn0 <jenn0.6n@gmail.com>
2023-10-28 07:02:23 +0900

    Refactor: InputView 메시지 상수를 사용하여 메시지 텍스트 개선
    InputView class에서 사용되는 메시지 텍스트를 상수로 정의하여 불변성 유지 및 유지보수성을 향상시켰습니다.

--- src/Views/InputView.js
+++ src/Views/InputView.js
@@ -1,15 +1,23 @@
 import { Console } from "@woowacourse/mission-utils";
 
+const INPUTVEIW_MESSAGE = Object.freeze({
+  promptCarNames_message:
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
+  promptRaceRound_message: "시도할 횟수는 몇 회인가요?",
+});
+
 export default class InputView {
   async promptCarNames() {
     const input = await Console.readLineAsync(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+      INPUTVEIW_MESSAGE.promptCarNames_message
     );
     return input.split(",");
   }
 
   async promptRaceRound() {
-    const input = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+    const input = await Console.readLineAsync(
+      INPUTVEIW_MESSAGE.promptRaceRound_message
+    );
     return input;
   }
 }
