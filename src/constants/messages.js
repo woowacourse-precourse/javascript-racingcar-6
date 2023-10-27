f371fba4a0576cb6d07b31895dc954c53270d8cc
d-charlie-kim <d.charlie.kim@gmail.com>
2023-10-27 15:17:00 +0900

    Feat: 메세지 상수 정리

---
+++ src/constants/messages.js
@@ -0,0 +1,16 @@
+export const ERROR_MSG = {
+  INVALID_CAR_NAME_LENGTH: "[ERROR] 자동차 이름은 5자 이하만 가능합니다.",
+  INVALID_MOVE_COUNT: "[ERROR] 시도할 횟수가 잘못된 형식입니다.",
+  DUPLICATED_CAR_NAME: "[ERROR] 자동차 이름은 중복될 수 없습니다.",
+  INVALID_CAR_NAME: "[ERROR] 자동차 이름이 잘못된 형식입니다.",
+};
+
+export const INPUT_MSG = {
+  CAR_NAME: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
+  MOVE_COUNT: "시도할 횟수는 몇 회인가요?\n",
+};
+
+export const OUTPUT_MSG = {
+  RESULT: "실행 결과\n",
+  WINNER: "최종 우승자 : ",
+};
