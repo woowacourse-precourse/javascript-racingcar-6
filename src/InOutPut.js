81537cb42777c343cec22543a362aff515396125
lcy <8410294@naver.com>
2023-10-27 21:32:50 +0900

    Feat: input Car Names
    - car name input function

--- src/InOutPut.js
+++ src/InOutPut.js
@@ -1,11 +1,21 @@
+import { Console } from '@woowacourse/mission-utils';
+
 export const verificationNames = (carNames) => {
   if (carNames.length === 0) {
     throw new Error('[ERROR] 자동차 이름을 입력해주세요');
   }
 
-  for (let i = 0; i < carNames.length; i++) {
+  for (let i = 0; i < carNames.length; i += 1) {
     if (carNames[i].length > 5) {
       throw new Error('[ERROR] 자동차 이름은 5자 이하만 가능합니다.');
     }
   }
+};
+
+export const inputCarName = async () => {
+  const carNames = await Console.readLineAsync().split(',');
+
+  verificationNames(carNames);
+
+  return carNames;
 };
