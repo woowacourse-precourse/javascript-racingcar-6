df04dc8ec536d81787ef2060d73d1c6b76240759
김경주 <popo505@naver.com>
2023-10-27 16:17:17 +0900

    feat: 게임 시작

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
+  start(){
+    Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분")
+  }
   async play() {}
 }
 
