8a0cb56809aeb5a27be43df81e757721957462f5
ooherin <555ohr@naver.com>
2023-10-27 12:00:20 +0900

    feat: 숫지입력 유효성 검사 구현

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import { Console } from "@woowacourse/mission-utils";
 import selectWinner from "./utils/selectWinner.js";
 import moveWithRandomNum from "./utils/moveWithRandomNum.js";
-import { IsInputRightForm, IsNameLengthUnderFive } from "./utils/validation.js";
+import {
+  isCountInputValid,
+  isNameLengthUnderFive,
+} from "./utils/validation.js";
 
 class App {
   async play() {
     const carsInput =
       await Console.readLineAsync("경주할 자동차 이름을 입력하세요.");
-    IsInputRightForm(carsInput);
     const cars = carsInput.split(",");
     const carsWithMoveNum = {};
     cars.forEach((car) => {
-      IsNameLengthUnderFive(car);
+      isNameLengthUnderFive(car);
       carsWithMoveNum[car] = 0;
     });
-    const lastCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
-    let count = 0;
-    while (count < lastCount) {
+    const countInput =
+      await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+    isCountInputValid(countInput);
+    const lastCount = countInput;
+    let currentCount = 0;
+    while (currentCount < lastCount) {
       moveWithRandomNum(cars, carsWithMoveNum);
       Console.print("");
-      count++;
+      currentCount++;
     }
     const winner = selectWinner(cars, carsWithMoveNum);
     Console.print(`최종 우승자 : ${winner.join(", ")}`);
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,9 +1,7 @@
-export const IsNameLengthUnderFive = (name) => {
+export const isNameLengthUnderFive = (name) => {
   if (name.length > 5) throw new Error("[ERROR]이름이 올바른 형식이 아닙니다.");
 };
-//이름 인풋이 , 와 string으로만 이루어졌는지 검사
-export const IsInputRightForm = (inputs) => {
-  const regex = /^,+$/g;
-  if (!regex.test(inputs))
-    throw new Error("[ERROR]이름이 올바른 형식이 아닙니다.");
+
+export const isCountInputValid = (input) => {
+  if (isNaN(input) || !input) throw new Error("[ERROR]유효한 숫자가 아닙니다.");
 };
