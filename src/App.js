f279dcc0ec51db6003c67c1675877dfa7c12db39
ceh20002@naver.com <ceh20002@naver.com>
2023-10-28 02:44:25 +0900

    사용자 공백 체크 기능

--- src/App.js
+++ src/App.js
@@ -24,8 +24,10 @@ class App {
     console.log("this.checkInvalidCarName(carList);", this.checkInvalidCarName(carList))
   }
 
-  // 사용자가 입력한 차들의 이름이 입력 기준을 만족하는지 체크하는 함수 집합
+  // 사용자가 입력한 차들의 이름이 입력 기준을 만족하는지 체크하는 기능
+  // 입력값에 띄어쓰기가 있다면 오류를 반환하려 했지만 람보르기니 우라칸 같은 차도 존재하기에 제외
   checkInvalidCarName(inputCarList){
+    this.checkSpace(inputCarList);
     this.ckeckLength(inputCarList);
     this.checkLastName(inputCarList);
     return inputCarList;
@@ -43,13 +45,27 @@ class App {
 
   // 정규 표현식을 사용한 글자 체크 마지막 자리를 제외한 문자열에 숫자가 포함되어 있다면 오류 ex sm5 k6 
   checkLastName(inputCarList){
-    const regex = /\d/;
+    const forCheckNumber = /\d/;
     inputCarList.forEach(car => {
-      if (regex.test(car.slice(0,car.length - 1))) {
+      if (forCheckNumber.test(car.slice(0,car.length - 1))) {
         throw new Error(ERROR.INVALID_CAR_NAME);
       }
     });
     return inputCarList;
+  }
+
+  // 차 이름 중간에는 공백이 존재할 수 있다고 생각함 하지만 첫자리와 마지막 자리는 그렇지 않기에 오류처리 or 공백을 없애줄까 고민중
+  checkSpace(inputCarList){
+    const forCheckSpace = /\s/;
+    inputCarList.forEach(car => {
+      console.log(`${car}끝자리 체크`, car, car.slice(car.length - 1, car.length), "끝자리");
+      if (forCheckSpace.test(car.slice(0,1))){
+        throw new ERROR("입력하신 차종들중 첫자리에 공백이 존재하는 차가 있습니다.")
+      }
+      if (forCheckSpace.test(car.slice(car.length-1, car.length))) {
+        throw new ERROR("입력하신 차종들중 끝자리에 공백이 존재하는 차가 있습니다.")
+      }
+    });
   }
 
   
