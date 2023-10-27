7b195cb21407a67975468c99d5f1bee78a1d0ee5
최기환 <rlghks3004@gmail.com>
2023-10-27 10:19:01 +0900

    refactor(App.js): 자동차 이름 입력 메서드 분리 및 단순화
    자동차 이름 유효성 검증 메서드 리팩토링
     - 메서드 로직 분리

--- src/App.js
+++ src/App.js
@@ -2,26 +2,28 @@ import { Console } from '@woowacourse/mission-utils';
 import { isUserInputValid, splitCarsInput } from './utils.js';
 
 class App {
-  /**
-   *  - [ ] 자동차 이름 입력 유효성 검증 기능
-   *   - 자동차 입력은 쉽표 **,** 로 구분된다. v
-   *   - 자동차 이름은 5자 이하로만 가능하다. v
-   *   - 숫자는 포함할 수 없다. v
-   *   - 같은 이름을 가진 차가 2개 이상 존재할 수 없다. v
-   *   - 공백을 포함할 수 없다. v
-   *   - 자동차 갯수는 10대 이하로 제한한다. v
-   *  */
+  cars = [];
 
   async play() {
-    const carsInput = await this.getCarName();
-    isUserInputValid(carsInput);
-    splitCarsInput(carsInput);
+    await this.getCarName();
   }
 
   async getCarName() {
-    return await Console.readLineAsync(
+    const input = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
     );
+    this.checkIsValidCars(input);
+    return input;
+  }
+
+  checkIsValidCars(carsInput) {
+    isUserInputValid(carsInput);
+    this.initializeCars(carsInput);
+  }
+
+  initializeCars(carsInput) {
+    const cars = splitCarsInput(carsInput);
+    this.cars = [...cars];
   }
 }
 
