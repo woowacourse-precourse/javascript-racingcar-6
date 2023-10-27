7a505cc38c09daa6e301ba8f1744ad5b0e0591f5
seoyeon-jung <jungluchia1204@gmail.com>
2023-10-27 16:14:47 +0900

    feat: 자동차 전진 기능 구현
    - 4 이상이면 전진하는 기능을 추가했습니다

--- src/App.js
+++ src/App.js
@@ -1,3 +1,22 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const { Random, Console } = MissionUtils;
+
+class Car {
+  constructor(name) {
+    this.name = name;
+    this.position = 0;
+  }
+
+  move() {
+    if (Random.pickNumberInRange(0, 9) >= 4) this.position++;
+  }
+
+  toString() {
+    return `${this.name} : ${"-".repeat(this.position)}`;
+  }
+}
+
 class App {
   async play() {}
 }
