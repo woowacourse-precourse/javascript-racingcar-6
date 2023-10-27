145d8a70265b5e011b6ad35c80d0ef0ec9b1e1ed
고세종 <fulfilled7418@gmail.com>
2023-10-27 20:44:39 +0900

    feat: 구현 자동차 이름 유효성 검사 함수 checkCarNamesAreValid

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Random, Console } from '@woowacourse/mission-utils';
 const MESSAGE = {
   ENTER_CAR_NAME:
     '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+  ERROR: '[ERROR] 숫자가 올바른 형식이 아닙니다.',
 };
 
 class App {
@@ -13,14 +14,20 @@ const getCarNames = async () => {
   const carNames = await Console.readLineAsync(
     '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
   );
-  checkCarNameIsValid(carNames);
+  checkCarNamesAreValid(carNames);
 };
-
-getCarNames();
 
 const getTryNumber = () => {};
 
-const checkCarNameIsValid = (userInput) => {};
+const checkCarNamesAreValid = (userInput) => {
+  const input = userInput.split(',');
+
+  const inputHaveOverName = input.find((carName) => carName.length > 5);
+
+  if (inputHaveOverName) {
+    throw new Error('[ERROR] 숫자가 올바른 형식이 아닙니다.');
+  }
+};
 
 const checkTryNumberIsValid = (userInput) => {};
 
