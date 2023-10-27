d172023be5b35b488b4c3cc85909e916161ac0a4
TMInstaller <ajvldmlqjqclr@naver.com>
2023-10-27 12:16:03 +0900

    공통 에러 규칙 분리

---
+++ src/global/convention.js
@@ -0,0 +1,1 @@
+export const ERROR_CONVENTION = "[ERROR]";
--- src/global/message.js
+++ src/global/message.js
@@ -1,3 +1,5 @@
+import { ERROR_CONVENTION } from "./convention";
+
 export const systemMessage = {
   START: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
   TRY_COUNT: "시도할 회수는 몇회인가요?",
@@ -6,15 +8,14 @@ export const systemMessage = {
 };
 
 export const errorCarMessage = {
-  INVALID_CAR_NAME_EMPTY: "[ERROR] 자동차 이름은 필수입니다.",
-  INVALID_CAR_NAME_LENGTH: "[ERROR] 자동차 이름은 5자 이하만 가능합니다.",
-  INVALID_CAR_NAME_DUPLICATE:
-    "[ERROR] 중복된 자동차 이름은 사용할 수 없습니다.",
-  INVALID_CAR_NAME_SPACED: "[ERROR] 자동차 이름 사이에 공백이 올 수 없습니다.",
+  INVALID_CAR_NAME_EMPTY: `${ERROR_CONVENTION} 자동차 이름은 필수입니다.`,
+  INVALID_CAR_NAME_LENGTH: `${ERROR_CONVENTION} 자동차 이름은 5자 이하만 가능합니다.`,
+  INVALID_CAR_NAME_DUPLICATE: `${ERROR_CONVENTION} 자동차 이름은 5자 이하만 가능합니다.`,
+  INVALID_CAR_NAME_SPACED: `${ERROR_CONVENTION} 자동차 이름 사이에 공백이 올 수 없습니다.`,
 };
 
 export const errorTryCountMessage = {
-  INVALID_TRY_COUNT_ISNUMBER: "[ERROR] 시도 횟수는 숫자만 입력 가능합니다.",
-  INVALID_TRY_COUNT_MIN: "[ERROR] 시도 횟수는 1 이상의 숫자만 가능합니다.",
-  INVALID_TRY_COUNT_TYPE: "[ERROR] 시도 횟수는 정수 형태의 숫자만 가능합니다.",
+  INVALID_TRY_COUNT_ISNUMBER: `${ERROR_CONVENTION} 시도 횟수는 숫자만 입력 가능합니다.`,
+  INVALID_TRY_COUNT_MIN: `${ERROR_CONVENTION} 시도 횟수는 1 이상의 숫자만 가능합니다.`,
+  INVALID_TRY_COUNT_TYPE: `${ERROR_CONVENTION} 시도 횟수는 정수 형태의 숫자만 가능합니다.`,
 };
