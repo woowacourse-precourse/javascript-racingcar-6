b5dd0b404718894f7f7a3e2bc3f984975a4a9c8a
0jenn0 <jenn0.6n@gmail.com>
2023-10-28 02:49:25 +0900

    Feat: 이름 길이 제한 추가
    처음에 공백제거를 해준 후 이름길이가 최대 5까지만 가능하도록 길이 제한을 추가했습니다

---
+++ src/Models/Name.js
@@ -0,0 +1,12 @@
+class Name {
+  constructor(string) {
+    const trimmedString = string.trim();
+    this.#validationSize(trimmedString);
+    this.name = trimmedString;
+  }
+
+  #validationSize(string) {
+    if (string.length > 5)
+      throw new Error("[ERROR] 이름은 최대 5자까지만 가능합나디.");
+  }
+}
