da571c988c450e46177c8a10ad3ca6f1e86a500d
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-10-28 00:29:32 +0900

    feat: 우승자 출력 기능 완료

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ class App {
   constructor() {
     this.cars = [];
     this.playTimes = 0;
+    this.winner = [];
+    this.randomNumber = 0;
+    this.racingCars = {};
   }
 
   async play() {
@@ -14,10 +17,9 @@ class App {
       const playTimes = await Console.readLineAsync(MESSAGE.roundsToPlay);
       this.playTimes = playTimes;
       if (this.CheckPlayTimes()) {
-        this.StartRacing();
-      } else {
-        throw new Error(MESSAGE.notValidPlaytimes);
+        return this.StartRacing();
       }
+      throw new Error(MESSAGE.notValidPlaytimes);
     } else {
       throw new Error(MESSAGE.notValidCarsName);
     }
@@ -31,7 +33,7 @@ class App {
     const checkFive = (car) => car.length <= 5;
 
     const isValidate = this.cars.every(
-      (car) => hasNotSpace(car) && isNotSpace(car) && checkFive(car)
+      (car) => hasNotSpace(car) && isNotSpace(car) && checkFive(car),
     );
 
     return isValidate;
@@ -49,29 +51,47 @@ class App {
 
   StartRacing() {
     let count = 1;
-    const cars = this.cars.reduce((acc, cur) => {
+    this.racingCars = this.cars.reduce((acc, cur) => {
       acc[cur] = "";
       return acc;
     }, {});
-
+    Console.print(MESSAGE.result);
     while (count <= this.playTimes) {
       this.cars.map((car) => {
         if (this.CanMoveForward()) {
-          cars[car] += "-";
+          this.racingCars[car] += "-";
         }
-        return Console.print(`${car} : ${cars[car]}`);
+        return Console.print(`${car} : ${this.racingCars[car]}`);
       });
+      Console.print("\n");
       count += 1;
     }
-    return cars;
+    this.WhoIsWinner();
   }
 
   CanMoveForward() {
-    const randomNumber = Random.pickNumberInRange(0, 9);
-    if (randomNumber >= 4) {
+    this.randomNumber = Random.pickNumberInRange(0, 9);
+    if (this.randomNumber >= 4) {
       return true;
     }
     return false;
+  }
+
+  WhoIsWinner() {
+    const scores = Object.values(this.racingCars);
+    const cars = Object.keys(this.racingCars);
+    const scoreNumbers = scores.map((score) => score.length);
+    let maxNumber = 0;
+    let winner = "";
+    scoreNumbers.forEach((scoreNumber, index) => {
+      if (scoreNumber > maxNumber) {
+        winner = cars[index];
+        maxNumber = scoreNumber;
+      } else if (scoreNumber === maxNumber) {
+        winner += `,${cars[index]}`;
+      }
+    });
+    Console.print(`${MESSAGE.winner} ${winner}`);
   }
 }
 
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,9 +1,11 @@
 const MESSAGE = {
   start:
-    "경주할 자동차 이름을 입력하세요.이름은 쉼표(,) 기준으로 구분하며 공백을 포함하면 안되고 5글자 이하 최대 10개 까지 등록가능합니다.",
-  roundsToPlay: "시도할 횟수는 몇 회인가요? 10회 이하로 입력해주세요.",
+    "경주할 자동차 이름을 입력하세요.이름은 쉼표(,) 기준으로 구분하며 공백을 포함하면 안되고 5글자 이하 최대 10개 까지 등록가능합니다.\n",
+  roundsToPlay: "시도할 횟수는 몇 회인가요? 10회 이하로 입력해주세요.\n",
   notValidCarsName: "[ERROR]: 이름이 잘못된 형식입니다.",
   notValidPlaytimes: "[ERROR]: 숫자가 잘못된 형식입니다.",
+  result: "\n실행결과",
+  winner: "최종 우승자 : ",
 };
 
 export default MESSAGE;
