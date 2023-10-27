71e345cf52a2e8c900820421e1072fa733e49abc
hanyoung <phanyoung@naver.com>
2023-10-28 02:22:38 +0900

    feat: 컨트롤러에 자동차 이름 입력부터 생성까지의 로직 연결

--- src/controller/Race.js
+++ src/controller/Race.js
@@ -1,12 +1,16 @@
 import InputView from "../view/InputView.js";
+import Car from "../model/Car.js";
+import Cars from "../model/Cars.js";
+import CarNamesParser from "../parser/CarNamesParser.js";
 
 class Race {
-  #inputView;
-
   constructor() {}
 
   async init() {
     const input = await InputView.readCarNames();
+    const carNames = CarNamesParser.parse(input);
+    const cars = carNames.map((name) => new Car(name));
+    new Cars(cars);
   }
 }
 
