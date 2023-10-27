f663230709b91731e787c208f05d941cbcfbdc40
Eunhee <82435813+joywhy@users.noreply.github.com>
2023-10-27 15:16:12 +0900

    feat: 경주 자동차리스트 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Random, Console } from '@woowacourse/mission-utils';
 class App {
-  async play() {}
-}
+  #racingcar;
 
+  constructor() {}
+  async play() {
+    //사용자에게 입력받는다.
+    const carList = await inputCars();
+    //실행결과를 보여준다.
+    console.log(carList);
+  }
+}
+const MESSAGE = Object.freeze({
+  inputCarList: `경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n`,
+  error: '[ERROR]',
+});
+
+async function inputCars() {
+  try {
+    const carList = await Console.readLineAsync(MESSAGE.inputCarList);
+    //에러처리
+    return carList;
+  } catch (errorCase) {
+    throw new Error(`${MESSAGE.error} ${errorCase}`);
+  }
+}
 export default App;
