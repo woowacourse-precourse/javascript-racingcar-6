9c95ac7655acc40054849db0b7dbfc3947bca989
djLee77 <dkdnl232@gmail.com>
2023-10-27 17:36:20 +0900

    feat(App): 입력 기능 구현
    기능 목록 - 1.1 부분의 입력 기능 구현
    
    App 클래스에서 사용자 입력으로 자동차 이름과 총 라운드를 받아오는
    `getCarNames`와 `getTotalRound` 메서드 구현.
    
    -`getCarNames`에서는 쉼표를 기준으로 자동차 이름들을 분리.
    -`getTotalRound`에서는 입력받은 총 라운드 수를 정수 타입으로 변환.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
   async play() {
-    
+    const arrCarNames = await this.getCarNames();
+    const totalRound = await this.getTotalRound();
+  }
+
+  async getCarNames(){
+    const input = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    return input.split(",");
+  }
+
+  async getTotalRound(){
+    const input = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+    return Number(input);
   }
 }
 
