ab9f309cb1975309a72d1a845d05518d0aedda32
Joseph Park <rkfnahs12e@gmail.com>
2023-10-27 22:11:55 +0900

    feat(src/Validation.js) : validateForMove 함수 구현, validateIsNumZeroToNine 함수명 변경 src/Validation 파일에 validateForMove 함수 구현 : - 차의 전진 유무를 판단하는 값이 4 이상인지 판단하는 함수
    src/Validation 파일의 validateIsNumZeroToNine 함수명 변경 :
    - validateRandomNum으로 변경 후 랜덤값의 타입이 숫자가 아닐 시 에러 출력 기능 추가

--- docs/README.md
+++ docs/README.md
@@ -5,8 +5,8 @@
 - [ ] 자동차 경주를 한다. - Racing#startRacing()
   - [ ] 차를 전진 시킨다. - Racing#carMove()
     - [x] 0~9 사이의 무작위 값을 얻어야 한다. - Number#createRandomNum()
-    - [x] 0~9 사이의 값인지 검증 해야 한다 - Validation#validateIsNumZeroToNine()
-    - [ ] 무작위 값이 4 이상인지 알 수 있다. - Validation#validateForMove()
+    - [x] 0~9 사이의 값인지 검증 해야 한다 - Validation#validateRandomNum()
+    - [x] 무작위 값이 4 이상인지 알 수 있다. - Validation#validateForMove()
   - [ ] 한명 이상의 우승자를 알 수 있다. - Racing#getWinner()
 - [ ] 사용자가 잘못된 값을 입력시 검증 해야 한다.
   - [x] 자동차 이름 입력을 검증한다. - Validation#validateCarNameLength()
--- src/Validation.js
+++ src/Validation.js
@@ -1,5 +1,3 @@
-function validateForMove() {}
-
 export function validateCarNameLength(name) {
 	if (name.length > 5) {
 		throw new Error('[ERROR] 특정 자동차의 이름이 5자를 초과하였습니다.');
@@ -8,10 +6,19 @@ export function validateCarNameLength(name) {
 
 function validatePlayCount() {}
 
-export function validateIsNumZeroToNine(num) {
+export function validateRandomNum(num) {
+	if (typeof num !== 'number') {
+		throw new Error(
+			'[ERROR] 차의 전진 유무를 판단하는 무작위 값이 숫자가 아닙니다.'
+		);
+	}
 	if (num < 0 || num > 9) {
 		throw new Error(
 			'[ERROR] 차의 전진 유무를 판단하는 무작위 값이 0보다 작거나 9보다 큽니다.'
 		);
 	}
+}
+
+export function validateForMove(num) {
+	return num >= 4;
 }
