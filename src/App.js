64cd13372db1749536c976b9e214f2dfa7ce36fe
rbgksqkr <rbgks1937@naver.com>
2023-10-28 01:54:35 +0900

    feat: 이름 5글자 초과 시 예외 발생시켜 애플리케이션 종료

--- src/App.js
+++ src/App.js
@@ -33,7 +33,6 @@ class App {
       Console.print("");
     });
 
-    const winner = [];
     carList.map((data) => (maxScore = Math.max(maxScore, data.score)));
     const result = carList.filter((data) => maxScore === data.score);
     const lastWinner = result.map((data) => data.name).join(", ");
@@ -44,6 +43,9 @@ class App {
     Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     const carInput = await Console.readLineAsync("");
     const carInputArray = carInput.split(",");
+    carInputArray.map((data) => {
+      if (data.length > 5) throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    });
     const carObjectArray = carInputArray.map((carName) => new Car(carName, 0));
     return carObjectArray;
   }
