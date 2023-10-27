85870e43dd0693de28bd50f213418d9696a6fba8
ryan-dia <cjfdnjs158@naver.com>
2023-10-27 23:43:51 +0900

    feat(inputView): 구분자 유효성 확인 기능 InputView에 추가

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,10 +1,13 @@
 import { Console } from '@woowacourse/mission-utils';
+import Validator from '../utils/validator';
 
 const InputView = {
   async readRacingCarNames() {
     const carNames = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
     );
+
+    Validator.checkRacingCarNames(carNames);
     return carNames;
   },
 };
