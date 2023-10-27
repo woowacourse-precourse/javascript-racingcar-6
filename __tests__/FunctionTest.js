a30ffe51d6c82cd13ddfed843b1cee33f98baa9c
Oh Dong Joon <ohdoju0905@gmail.com>
2023-10-28 01:47:33 +0900

    기능 1 : 사용자로부터 입력을 받아서 예외처리 후 결괏값 반환

--- __tests__/FunctionTest.js
+++ __tests__/FunctionTest.js
@@ -4,4 +4,27 @@ import App from '../src/App.js';
 describe('함수 기능 유닛 테스트', () => {
 	const app = new App();
 	const CARS_NAME = ['car1', 'car2', 'car3'];
+
+	describe('getInput', () => {
+		test('car 이름을 제대로 입력한 경우', async () => {
+			Console.readLineAsync = jest.fn().mockResolvedValueOnce('a,b,ccccc').mockResolvedValueOnce('5');
+			await expect(app.getInput()).resolves.toEqual([{ a: 0, b: 0, ccccc: 0 }, 5]);
+		});
+		test('car 이름이 5자가 넘어간 경우', async () => {
+			Console.readLineAsync = jest.fn().mockResolvedValueOnce('a,b,gggggg').mockResolvedValueOnce('5');
+			await expect(app.getInput()).rejects.toThrow('[ERROR]');
+		});
+		test('car 이름을 입력하지 않은 경우', async () => {
+			Console.readLineAsync = jest.fn().mockResolvedValueOnce('a,,c').mockResolvedValueOnce('5');
+			await expect(app.getInput()).rejects.toThrow('[ERROR]');
+		});
+		test('레이싱 횟수로 숫자를 입력하지 않은 경우', async () => {
+			Console.readLineAsync = jest.fn().mockResolvedValueOnce('a,b,c').mockResolvedValueOnce('a');
+			await expect(app.getInput()).rejects.toThrow('[ERROR]');
+		});
+		test('레이싱 횟수로 양수를 입력하지 않은 경우', async () => {
+			Console.readLineAsync = jest.fn().mockResolvedValueOnce('a,b,c').mockResolvedValueOnce('-1');
+			await expect(app.getInput()).rejects.toThrow('[ERROR]');
+		});
+	});
 });
--- src/App.js
+++ src/App.js
@@ -5,6 +5,22 @@ class App {
 		this.ERROR = '[ERROR]';
 	}
 
+	async getInput() {
+		Console.print('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
+		const cars = (await Console.readLineAsync('')).split(',');
+		if (cars.some((e) => e.length === 0 || e.length > 5)) {
+			throw new Error(`${this.ERROR} 자동차 이름을 입력하지 않았거나 5자를 초과하여 입력하셨습니다!`);
+		}
+
+		Console.print('시도할 횟수는 몇 회인가요?');
+		const limit = +(await Console.readLineAsync(''));
+		if (isNaN(limit) || limit <= 0) {
+			throw new Error(`${this.ERROR} 자연수를 입력하지 않으셨습니다.`);
+		}
+
+		return [Object.fromEntries(cars.map((e) => [e, 0])), limit];
+	}
+
 	async play() {}
 }
 
