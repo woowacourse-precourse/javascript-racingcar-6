20e6cfb315c895b32009a89ccb51a65becdc3779
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-10-27 13:38:13 +0900

    feat: 게임 시작 기능 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console } from "@woowacourse/mission-utils";
+import { START } from "./constants/constants.js";
+
 class App {
-  async play() {}
+  async play() {
+    const answer = await Console.readLineAsync(START);
+  }
 }
 
+const app = new App();
+
+app.play();
 export default App;
