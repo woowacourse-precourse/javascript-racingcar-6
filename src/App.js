4eac7885b233c40b7011c5b30f3554abcf63bc9a
장호빈 <devhobin@gmail.com>
2023-10-27 18:14:16 +0900

    feat(App): setting status에서 사용자 인풋받는 로직 구현
    자동차 경주를 위한 자동차의 이름, 시도 횟수 등의 사용자 인풋을 받는 로직을 구현하였다

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const print = (message) => MissionUtils.Console.print(message);
+const question = async (message) => MissionUtils.Console.readLineAsync(message);
+
 class App {
   #status = "idle";
 
@@ -20,6 +25,11 @@ class App {
         break;
       }
       case "setting": {
+        const namesInput = await question("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+        const roundInput = await question("시도할 횟수는 몇 회인가요?\n");
+
+        print(namesInput);
+        print(roundInput);
         break;
       }
       case "start": {
