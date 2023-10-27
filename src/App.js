4d5a0b40e9994a0e1a49dd0160acd577da68e6fc
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-10-28 00:07:06 +0900

    feat: 사용자 입력 중 자동차 입력 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Console } from "@woowacourse/mission-utils";
 class App {
-  async play() {}
+  constructor() {
+    this.racingCars;
+  }
+
+  async getRacingCars() {
+    let inputValue = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    this.racingCars = inputValue.split(",");
+  }
+
+  initPlay() {}
+
+  async play() {
+    this.initPlay();
+    this.getRacingCars();
+  }
 }
 
 export default App;
