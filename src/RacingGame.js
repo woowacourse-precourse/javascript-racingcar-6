6079c9d47e15fd6697f991b51e8fb745ee0dfbed
Jeongeun Lee <leeju1009@gmail.com>
2023-10-28 00:57:42 +0900

    feat: 등록한 자동차가 경주하는 기능 추가

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -1,7 +1,13 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class RacingGame {
   async getRaceTimes() {
     const raceTimes = await Console.readLineAsync('시도할 횟수는 몇 회인가요?');
     return Number(raceTimes);
+  }
+
+  async raceCars(carsArray) {
+    carsArray.forEach((car) => car.move());
   }
 }
 
