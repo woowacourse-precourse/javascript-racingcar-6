bda0938a999b8e5319f19bb3443730ad5c91bbb6
rustandbone <dlehdgh14@naver.com>
2023-10-27 19:55:36 +0900

    [기능구현] 자동차 이름 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Random, Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    try {
+      const RacingCarNames = await Console.readLineAsync(
+        '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+      );
+      const RacingCarNamesArray = RacingCarNames.split(',');
+    } catch (error) {
+      throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
+    }
+  }
 }
 
 export default App;
