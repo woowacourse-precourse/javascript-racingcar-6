650b5f3fb857912771ddc82ce5c68264c63e8ae3
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2023-10-27 21:16:21 +0900

    [refactor] : 질문 문자열을 상수로 변경

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import { ATTEMPTS_NUMBER, CAR_NAME } from "./constants/questions";
 
 class App {
   constructor() {
@@ -10,26 +11,25 @@ class App {
   }
   async play() {
     // 1-a
-    this.strCarName = await MissionUtils.Console.readLineAsync(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
-    
-    );
+    this.strCarName = await MissionUtils.Console.readLineAsync(CAR_NAME);
     // 1-b
     this.arrCarName = this.strCarName.split(",");
     // 1-c
     this.arrCarName.map((name) => {
       if (name.trim().length > 5 || name.trim().length < 1) {
-        throw new Error("[ERROR] : 자동차 이름은 5글자 이내로 입력해야 합니다!");
+        throw new Error(
+          "[ERROR] : 자동차 이름은 5글자 이내로 입력해야 합니다!"
+        );
       }
     });
 
     // 1-d
-    this.attempts = await MissionUtils.Console.readLineAsync(
-      "시도할 횟수는 몇 회인가요?"
-    );
+    this.attempts = await MissionUtils.Console.readLineAsync(ATTEMPTS_NUMBER);
     // 1-e
     if (this.attempts < 0 || isNaN(this.attempts) == true) {
-      throw new Error("[ERROR] : 시도 횟수가 잘못 입력되었습니다. 정수를 입력해주세요.");
+      throw new Error(
+        "[ERROR] : 시도 횟수가 잘못 입력되었습니다. 정수를 입력해주세요."
+      );
     }
 
     // 2-a
---
+++ src/constants/questions.js
@@ -0,0 +1,5 @@
+const CAR_NAME =
+  "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+const ATTEMPTS_NUMBER = "시도할 횟수는 몇 회인가요?";
+
+export {CAR_NAME, ATTEMPTS_NUMBER}
