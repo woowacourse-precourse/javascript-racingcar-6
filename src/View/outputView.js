d8152cf77a77dc6127685e06c15b1d78c7235d3f
shyeon0602 <july8560@naver.com>
2023-10-27 22:08:25 +0900

    feat: views 함수 생성

--- src/View/outputView.js
+++ src/View/outputView.js
@@ -1,7 +1,22 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import { ERROR, MESSAGE } from "../Utils/Message";
 
 const outputView = {
+    gameStrat(){
+        MissionUtils.Console.print(MESSAGE.START);
+    },
 
+    printError(){
+        MissionUtils.Console.print(ERROR);
+    },
+
+    printResultTitle(){
+        MissionUtils.Console.print(MESSAGE.RESULT_TITLE);
+    },
+
+    printResult(winner){
+        MissionUtils.Console.print(`${MESSAGE.RESULT}${winner.join(", ")}`);
+    }
 }
 
 module.exports = outputView;
