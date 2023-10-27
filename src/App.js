17f84de1134ab45d66a027c348e7552d6073082b
Jwirang <haha71119@gamil.com>
2023-10-27 20:24:32 +0900

    feat: 자동차 이름 입력받는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Console, MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    const names = await this.getCarNames();
+  }
+
+  async getCarNames() {
+    const carNamesInput = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    const carNames = carNamesInput
+      .split(',')
+      .map(item => item.trim());
+
+    if (carNames.some(name => name.length > 5)) {
+      throw new Error('이름은 5글자 이하로만 입력할 수 있습니다.');
+    }
+
+    return carNames;
+  } 
 }
 
 export default App;
