cd971fe890236965a3a7811fabad50fb4550d012
hyj5076 <hyj5076@hanmail.net>
2023-10-27 22:21:14 +0900

    refactor: 상수 선언 및 변수 적용

--- src/const/Messages.js
+++ src/const/Messages.js
@@ -3,4 +3,6 @@ export const QUESTION_GOAL_COUNT = '시도할 횟수는 몇 회인가요?';
 export const GAME_RESULT = '실행 결과';
 export const WINNER = '최종 우승자';
 
+export const FORWARD_MATCHING_POINT = 4;
+export const FORWARD_DASH = '-';
 export const ERROR_MESSAGE = '[ERROR] 숫자가 잘못된 형식입니다.';
--- src/utils/MovingForwardCase.js
+++ src/utils/MovingForwardCase.js
@@ -1,9 +1,10 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
+import { FORWARD_MATCHING_POINT, FORWARD_DASH } from '../const/Messages';
 
 export const movingForwardCase = () => {
   const number = MissionUtils.Random.pickNumberInRange(0, 9);
 
-  if (number >= 4) {
-    Console.print('-');
+  if (number >= FORWARD_MATCHING_POINT) {
+    Console.print(FORWARD_DASH);
   }
 };
