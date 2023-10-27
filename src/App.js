0ffaecad802b71202aa65e31900894354bce7af7
sejineeee <sejinee0918@gmail.com>
2023-10-28 00:07:53 +0900

    feature: 시도할 횟수 입력받는 함수 추가

--- src/App.js
+++ src/App.js
@@ -23,6 +23,12 @@ class App {
 
     return racingCars;
   }
+
+  async getTryCount() {
+    const inputTryCount = await Console.readLineAsync(
+      '시도할 횟수는 몇 회인가요?'
+    );
+  }
 }
 
 export default App;
