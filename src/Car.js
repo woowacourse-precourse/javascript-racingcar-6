b78e2bfe0864354e911f8ca7509539e8d1d61918
장호빈 <devhobin@gmail.com>
2023-10-27 20:39:42 +0900

    feat(Car): Car 클래스 구현
    - isBreakdown 유틸함수를 이용하여 일정 확률로 차량이 고장나지 않은 경우 mileage 프로퍼티 값을 1 증가시킨다.
    - mileage 프로퍼티 getter를 추가했다.

--- src/Car.js
+++ src/Car.js
@@ -1,11 +1,26 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const isBreakdown = () => MissionUtils.Random.pickNumberInRange(0, 9) <= 3;
+
 export default class Car {
   #name;
+
+  #mileage = 0;
 
   constructor(name) {
     this.#name = name;
   }
 
+  move() {
+    if (isBreakdown()) return;
+    this.#mileage += 1;
+  }
+
   getName() {
     return this.#name;
+  }
+
+  getMileage() {
+    return this.#mileage;
   }
 }
