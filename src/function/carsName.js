0d4ff5d7a4613fadd337673d9c82c6758e1b214d
limdumb <dlaruddls@nate.com>
2023-10-27 19:54:31 +0900

    feat: 자동차 이름 입력함수 구현

---
+++ src/function/carsName.js
@@ -0,0 +1,19 @@
+import { Console } from "@woowacourse/mission-utils";
+
+export const carsName = async () => {
+  const userInputValue = await Console.readLineAsync(
+    "경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)"
+  );
+
+  const result = userInputValue.split(",");
+
+  result.forEach((el) => {
+    if (el.length > 5) {
+      throw new Error(
+        "[ERROR] 자동차 이름의 길이는 5자 이하입니다. 게임을 다시 시작해주세요"
+      );
+    }
+  });
+
+  return result;
+};
