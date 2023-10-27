6ddf4e9c2fe653c603e8251f8fc2286fb836ec33
rbgksqkr <rbgks1937@naver.com>
2023-10-28 00:07:17 +0900

    Feat: 경주할 자동차 이름과 시도할 횟수 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Random, Console } from "@woowacourse/mission-utils";
 class App {
-  async play() {}
+  async play() {
+    // const number = Random.pickNumberInRange(0, 9);
+    const carList = await this.getCarList();
+    const tryNumber = await this.getTryNumber();
+    console.log(carList, tryNumber);
+  }
+
+  async getCarList() {
+    Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    const carInput = await Console.readLineAsync("");
+    const carInputArray = carInput.split(",");
+    return carInputArray;
+  }
+
+  async getTryNumber() {
+    Console.print("시도할 횟수는 몇 회인가요?");
+    const tryNumber = await Console.readLineAsync("");
+    return tryNumber;
+  }
 }
 
 export default App;
