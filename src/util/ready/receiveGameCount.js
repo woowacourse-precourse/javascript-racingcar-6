467fb278d80a4e4e6373293b924876dc4f374c16
thumbthing <ilscc0902@gmail.com>
2023-10-28 05:10:02 +0900

    feat: race 진행 수 입력 기능 구현 (consoleInput, valid 검증)

---
+++ src/util/ready/receiveGameCount.js
@@ -0,0 +1,20 @@
+import { defaultErrorHandler } from '../error/errorhandler.js';
+import { consoleInput } from '../libraryFeatures/consoleHandler.js';
+import { checkRaceCountValid } from './checkInputValid.js';
+
+async function receiveGameCount() {
+  try {
+    const count = await consoleInput('시도할 횟수는 몇 회인가요? \n');
+    const isValid = count && (await checkRaceCountValid(count));
+
+    const validCount = isValid && count;
+    return validCount;
+  } catch (error) {
+    defaultErrorHandler(error);
+    return null;
+  }
+}
+
+export default receiveGameCount;
+
+receiveGameCount();
