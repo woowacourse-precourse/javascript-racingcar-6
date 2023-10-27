2b7fee5300876eacbe98d9d4c1ac2562cabdf577
gunyongbok <qhrrjsdyd123@gmail.com>
2023-10-27 13:24:05 +0900

    feat: 자동차 이름 입력값 검증 구현 자동차 이름은 5자 이하의 영소문자로만 이루어져야 한다.

--- src/CarRacing.js
+++ src/CarRacing.js
@@ -1,8 +1,11 @@
 import { Console } from "@woowacourse/mission-utils";
 import Messages from "./common/message.js";
+import validateCarNameInput from "./utils/validateCarNameInput.js";
 
 class CarRacing {
-  constructor() {}
+  constructor() {
+    this.cars = [];
+  }
 
   startGame() {
     this.getCarName();
@@ -12,6 +15,13 @@ class CarRacing {
     let carName = await Console.readLineAsync(
       Messages.ENTER_RACING_CAR_NAME_MESSAGE
     );
+
+    this.cars = carName.split(",");
+    this.cars.map((car) => {
+      if (!validateCarNameInput(car)) {
+        throw new Error(Messages.RACING_CAR_INPUT_ERROR_MESSAGE);
+      }
+    });
   }
 }
 
