04987d4f5fe4049be450acb417f8cf4c29498bb4
Seungmani <dkelcncjs@naver.com>
2023-10-27 20:14:31 +0900

    [feat] 게임 횟수 설정 기능

--- src/App.js
+++ src/App.js
@@ -11,29 +11,44 @@ const readLineAsync = async (message) => {
 }
 
 
+
+/**
+ * 게임 진행 횟수를 입력받는 기능
+ * return : 숫자로 반환
+ * 주의 : 0 이하거나 숫자가 아니면 ERROR
+ */
+const getAttempt = async () =>{
+  const attempt = await readLineAsync('시도할 횟수는 몇 회인가요?\n');
+  
+  // *** 리팩토링 하면서 함수로 빼기 ***
+  if(!(+attempt)) throw new Error('[ERROR] 횟수는 숫자여야 합니다.');
+  if((+attempt) <=0 ) throw new Error('[ERROR] 횟수는 0회 보다 커야합니다.');
+
+  return +attempt;
+}
+
 /**
  * 게임 참가자 명단을 받는 기능
- * 입력 : x
  * return : 참가자 명단은 배열로 반환
  * 주의 : 참가자 이름이 5글자 초과면 ERROR
  */
 const getParticipant = async () =>{
-  const participants = await readLineAsync('경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)');
-  const participantsArr = participants.split(',')
+  const participants = await readLineAsync('경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)\n');
+  const participantsArr = participants.split(',');
 
   // *** 리팩토링 하면서 함수로 뺴기 ***
   for(let name of participantsArr){
-    if(name.length > 5){
-      throw new Error('[ERROR] 참가자 이름이 5글자를 초과합니다.'); // 추후에 ERROR문구 상수로 빼기
-    }
+    if(name.length > 5) throw new Error('[ERROR] 참가자 이름이 5글자를 초과합니다.'); // ERROR문구 상수로 빼기
   }
 
   return participantsArr;
 }
 
 class App {
+
   async play() {
     const participants = await getParticipant()
+    const attempt = await getAttempt();
   }
 }
 
