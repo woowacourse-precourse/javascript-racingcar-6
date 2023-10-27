3de1ebef8bc3a7fbe4d8044708515e6d1c4f8a3a
lord-potato <lordpotato1227@gmail.com>
2023-10-27 21:29:18 +0900

    feat : 우승자 판정 기능 추가

--- src/Function.js
+++ src/Function.js
@@ -11,3 +11,14 @@ function isCarForward(number) {
     return false;
   }
 }
+
+function whoIsWinner(score, name) {
+  let LARGEST_SCORE = Math.max.apply(null, score);
+  let WINNER_NAME = [];
+  for (let i = 0; i < score.length; i++) {
+    if (score[i] === LARGEST_SCORE) {
+      WINNER_NAME.push(name[i]);
+    }
+  }
+  return WINNER_NAME;
+}
