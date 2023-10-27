a2ee9470420e9c97295c0c132ba11b5207f3ab9e
gouz7514 <gouz7514@gmail.com>
2023-10-27 14:20:37 +0900

    Fix : 최종 우승자 출력시 ... 연산자 사용

--- src/View/GameView.js
+++ src/View/GameView.js
@@ -51,7 +51,7 @@ class GameView {
 
   // 3-4. 최종 우승자를 결정하고 출력한다
   printFinalResult(cars) {
-    const maxPosition = Math.max(Array.from(cars, (car) => car.position));
+    const maxPosition = Math.max(...cars.map((car) => car.position));
     const winners = cars.filter((car) => car.position === maxPosition);
 
     const winnerNames = winners.map((winner) => winner.name).join(", ");
