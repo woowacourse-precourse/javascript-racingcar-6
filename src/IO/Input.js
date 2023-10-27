79473259b2df112a41280f11e2e1865dea978f4b
6puritans9 <6puritans9@gmail.com>
2023-10-27 15:23:02 +0900

    refactor(IO): divided query strings from Input into Constants

--- src/IO/Input.js
+++ src/IO/Input.js
@@ -1,10 +1,8 @@
 import { Console } from "@woowacourse/mission-utils";
 
 export class Input {
-  async get() {
-    const input = await Console.readLineAsync(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): "
-    );
+  async get(query) {
+    const input = await Console.readLineAsync(query);
     return input;
   }
 }
---
+++ src/Misc/Constatns.js
@@ -0,0 +1,9 @@
+export default class Constants {
+  askNames = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): ";
+
+  duplicates = "중복되지 않는 이름으로 입력해주세요.";
+
+  tries = "시도할 횟수는 몇 회인가요? ";
+
+  winners = "최종 우승자: ";
+}
