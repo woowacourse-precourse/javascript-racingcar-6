62d83de2fc067e22d37330524bffe63282a47978
lord-potato <lordpotato1227@gmail.com>
2023-10-27 23:18:15 +0900

    feat : 자동차 1회 주행 구현 및 회차별 결과 출력 기능 추가

--- src/Function.js
+++ src/Function.js
@@ -1,5 +1,26 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
+export async function getCarName() {
+  try {
+    let NAME_CAR = MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+    return NAME_CAR;
+  } catch (e) {
+    MissionUtils.Console.print("[ERROR]");
+  }
+}
+
+export async function getIteration() {
+  try {
+    let ITERATION_RACE =
+      MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+    return ITERATION_RACE;
+  } catch (e) {
+    MissionUtils.Console.print("[ERROR]");
+  }
+}
+
 function generateRandomNumber() {
   return MissionUtils.Random.pickNumberInRange(0, 9);
 }
@@ -12,13 +33,28 @@ function isCarForward(number) {
   }
 }
 
-function whoIsWinner(score, name) {
-  let LARGEST_SCORE = Math.max.apply(null, score);
-  let WINNER_NAME = [];
+export function startOneIteration(score) {
   for (let i = 0; i < score.length; i++) {
-    if (score[i] === LARGEST_SCORE) {
-      WINNER_NAME.push(name[i]);
+    let RAND_NUM = generateRandomNumber();
+    if (isCarForward(RAND_NUM)) {
+      score[i]++;
     }
   }
-  return WINNER_NAME;
+}
+
+export function printCurrentIteration(score, name) {
+  for (let i = 0; i < score.length; i++) {
+    MissionUtils.Console.print(`${name[i]} : ${"-".repeat(score[i])}`);
+  }
+}
+
+export function whoIsWinner(score, name) {
+  let LARGEST_SCORE = Math.max.apply(null, score);
+  let NAME_WINNER = [];
+  for (let i = 0; i < score.length; i++) {
+    if (score[i] === LARGEST_SCORE) {
+      NAME_WINNER.push(name[i]);
+    }
+  }
+  return NAME_WINNER;
 }
