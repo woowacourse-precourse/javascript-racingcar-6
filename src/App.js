ed5b6fda2c596b06c827c2e11b3966477cc60c4b
ohgus <anakin1108@naver.com>
2023-10-27 21:10:10 +0900

    feat: 유저 입력값 받아오는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+import {
+  QUESTION_MESSAGES,
+  PURPOSE_OF_QUESTION_MESSAGES,
+} from "./constants/questionMessage.js";
+
 class App {
   async play() {}
+
+  async getUserInput(purpose) {
+    if (purpose === PURPOSE_OF_QUESTION_MESSAGES.SET_CARS_NAME) {
+      const cars = await Console.readLineAsync(
+        QUESTION_MESSAGES.WHAT_IS_CARS_NAME
+      );
+
+      return cars.split(",");
+    }
+
+    if (purpose === PURPOSE_OF_QUESTION_MESSAGES.SET_LAPS) {
+      const lap = await Console.readLineAsync(QUESTION_MESSAGES.HOW_MANY);
+
+      return Number(lap);
+    }
+  }
 }
 
 export default App;
+
+const app = new App();
+app.play();
