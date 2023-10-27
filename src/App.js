5cbb40dc942f51db47df668039e467d3469e052c
eunsung <esk114477@gmail.com>
2023-10-28 01:43:26 +0900

    feat: 자동차 이름 입력받아 배열에 저장

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import { Console } from "@woowacourse/mission-utils";
+import Car from "./Car.js";
 
 class App {
   #carNames = [];
   #moveCount;
 
   async play() {
-    
+    await this.setCarNames();
+  }
+
+  async setCarNames() {
+    const inputArray = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    inputArray.split(",").forEach((name) => {
+      const car = new Car(name);
+      this.#carNames.push(car);
+    });
   }
 }
 
