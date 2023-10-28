f5c50eb1095fe5ca6a235857ead7e53b8914195f
ReturnReason <reasonz@daum.net>
2023-10-28 16:39:52 +0900

    feat(messages): 에러 상수 메시지 추가

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -4,3 +4,13 @@ export const MESSAGE = Object.freeze({
   RESULT: '실행 결과\n',
   WINNER: '최종 우승자 : ',
 });
+
+const ERROR_PREFIX = '[ERROR]';
+export const ERROR_MESSAGE = Object.freeze({
+  DUPLICATION: `${ERROR_PREFIX} 중복된 자동차의 이름은 입력할 수 없습니다.\n`,
+  NUMBER_OF_CHARACTERS: `${ERROR_PREFIX} 각 자동차의 이름은 최소 글자 1글자 이상, 최대 5글자까지 입력할 수 있습니다.\n`,
+  INVALID_NAME: `${ERROR_PREFIX} 자동차의 이름은 영문으로만 입력할 수 있습니다.\n`,
+  INVALID_DIVISION: `${ERROR_PREFIX} 쉼표(,) 외의 특수문자는 입력할 수 없습니다.\n`,
+  PLAYER_COUNT: `${ERROR_MESSAGE} 경주할 자동차는 최소 2대 이상, 최대 10대까지 입력해야 합니다.\n`,
+  INVALID_MOVE_COUNT: `${ERROR_PREFIX} 이동할 수 있는 횟수는 최소 1회 ~ 최대 10회로 설정해야 합니다.\n`,
+});
