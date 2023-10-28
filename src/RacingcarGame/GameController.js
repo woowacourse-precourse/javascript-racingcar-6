58a0379cc8c5c61264a23a0515d5d3c9314c8fff
ysk9926 <74546856+ysk9926@users.noreply.github.com>
2023-10-28 21:27:16 +0900

    feat: 실행 횟수 유효 범위 검사 함수 구현
    사용자로부터 입력 받은 실행 횟수에 대한 유효 범위를 테스트하고 범위 외의 경우에는 에러를 반환하는 함수를 구현하였습니다.

--- src/RacingcarGame/GameController.js
+++ src/RacingcarGame/GameController.js
@@ -1,8 +1,8 @@
-import { GameText } from "../message";
-import GameModel from "./GameModel";
-import GameUtil from "./GameUtil";
-import GameView from "./GameView";
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { GameText } from '../message';
+import GameModel from './GameModel';
+import GameUtil from './GameUtil';
+import GameView from './GameView';
+import { MissionUtils } from '@woowacourse/mission-utils';
 
 class GameController {
   constructor() {
@@ -15,14 +15,14 @@ class GameController {
     while (true) {
       // 게임에 필요한 변수 값을 사용자로 부터 받아옴
       const CAR_NAMES = await this.getCarnameInput();
-      MissionUtils.Console.print(CAR_NAMES.join(","));
+      MissionUtils.Console.print(CAR_NAMES.join(','));
       const ATTEMPTS = await this.getRacingcarAttempts();
       MissionUtils.Console.print(ATTEMPTS);
 
       // 사용자의 입력값을 기반으로 게임 모델을 생성 후 결과 출력
       this.model = new GameModel(CAR_NAMES, ATTEMPTS);
       this.model.registerController(this);
-      MissionUtils.Console.print("실행결과");
+      MissionUtils.Console.print('실행결과');
       this.model.run();
       const winners = this.model.getWinner();
       this.view.printWinners(winners);
@@ -38,15 +38,16 @@ class GameController {
   async getCarnameInput() {
     this.view.printGetMessage(GameText.GET_CAR_NAME);
     const getUserInput = await MissionUtils.Console.readLineAsync();
-    const cars = getUserInput.split(",");
-    await cars.forEach((car) => this.util.carNameVaildator(car));
+    const cars = getUserInput.split(',');
+    await cars.forEach(car => this.util.carNameVaildator(car));
 
-    return getUserInput.split(",");
+    return getUserInput.split(',');
   }
 
   async getRacingcarAttempts() {
     this.view.printGetMessage(GameText.GET_GAME_ATTEMPTS);
     const getAttempts = await MissionUtils.Console.readLineAsync();
+    await this.util.attemptsValidator(getAttempts);
 
     return getAttempts;
   }
@@ -59,10 +60,10 @@ class GameController {
     this.view.printGetMessage(GameText.GET_GAME_RESTART);
     const getRestart = await MissionUtils.Console.readLineAsync();
     MissionUtils.Console.print(
-      `입력 값 : ${getRestart} returnValur = ${getRestart === "1"}`
+      `입력 값 : ${getRestart} returnValur = ${getRestart === '1'}`,
     );
 
-    return getRestart === "1";
+    return getRestart === '1';
   }
 }
 
--- src/RacingcarGame/GameUtil.js
+++ src/RacingcarGame/GameUtil.js
@@ -1,9 +1,17 @@
-import { ErrorMessage } from "../message";
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { ErrorMessage } from '../message';
 
 class GameUtil {
   carNameVaildator(car) {
     if (car.length > 5) {
       throw new Error(ErrorMessage.CAR_NAME_LENGTH);
+    }
+  }
+
+  attemptsValidator(attempts) {
+    if (attempts <= 0) {
+      MissionUtils.Console.print(ErrorMessage.ATTEMPTS_VALID_NUM);
+      throw new Error(ErrorMessage.ATTEMPTS_VALID_NUM);
     }
   }
 }
