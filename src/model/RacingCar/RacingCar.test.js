de12e3a4a701c95a930630b3b5bd8a266989e9d0
노태균 <taegyunn123@gmail.com>
2023-10-28 17:17:39 +0900

    test: RacingCar 객체 test code 추가

---
+++ src/model/RacingCar/RacingCar.test.js
@@ -0,0 +1,30 @@
+import RacingCar from "./RacingCar";
+
+const name = "my car";
+
+describe("RacingCar 객체 테스트", () => {
+  let racingCar;
+
+  beforeEach(() => {
+    racingCar = new RacingCar(name);
+  });
+
+  test("field가 private으로 선언되어 있는지 확인", () => {
+    expect(racingCar.name).toBe(undefined);
+    expect(racingCar.count).toBe(undefined);
+  });
+
+  test("move & getCount 메서드 검증", () => {
+    const prevCount = racingCar.getCount();
+    racingCar.move();
+    const nextCount = racingCar.getCount();
+    expect(nextCount).toBe(prevCount + 1);
+  });
+
+  test("getLog 메서드 검증", () => {
+    racingCar.move();
+    const log = racingCar.getLog();
+    const count = racingCar.getCount();
+    expect(log).toBe(`${name} : ${"-".repeat(count)}`);
+  });
+});
