4daf4ccea1e1d006d9f3170e532e3fb060c24920
soyoung125 <sypark041754@gmail.com>
2023-10-28 21:24:39 +0900

    feat: n대의 자동차 이름을 입력받는다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+
+  async inputCarNames() {
+    const names = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n")
+    MissionUtils.Console.print(names);
+  }
+
+  async play() {
+    await this.inputCarNames();
+  }
 }
 
 export default App;
