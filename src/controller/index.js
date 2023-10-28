76033f86ca6deae923018eea536a0b1bbdc05872
ryan-dia <cjfdnjs158@naver.com>
2023-10-28 19:56:03 +0900

    feat(controller): carNames을 저장하는 기능 추가

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,9 +1,17 @@
+import RacingCarModel from '../model/index.js';
 import InputView from '../view/InputView.js';
 
 class RacingCarController {
+  #model;
+
+  constructor() {
+    this.#model = new RacingCarModel();
+  }
+
   async startGame() {
     const carNames = await InputView.readRacingCarNames();
     const racingCount = await InputView.readRacingCount();
+    this.#model.saveCarNames(carNames);
   }
 }
 
