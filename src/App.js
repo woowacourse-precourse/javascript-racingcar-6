d3c74922d98f6d93fbacd6d358b30e38095bcd2f
sryung <sryung1225@gmail.com>
2023-10-28 16:24:54 +0900

    refactor: 출력문에 사용할 메세지를 상수로 지정 후 분리

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
+import { MESSAGE, ERROR } from './constants/index.js';
 
 class App {
   async play() {
     const CAR = [];
     let candidate = await MissionUtils.Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n',
+      MESSAGE.carNameForStart,
     );
     candidate.split(',').forEach(name => {
       if (name.length > 5) {
-        throw new Error('자동차 이름은 5자 이하여야 합니다.');
+        throw new Error(ERROR.carNameInputLong);
       }
       CAR.push([name, '']);
     });
