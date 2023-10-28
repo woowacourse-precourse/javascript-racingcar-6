c799f0de2aa3b05d734e31dc9b94baf29279bb07
SUJIN YOON <97013028+sujinnn@users.noreply.github.com>
2023-10-28 15:25:56 +0900

    Update App.js
    Feat : 경주할 자동차 이름 입력 받기,
    이름 6자 이상일 경우 에러

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import { Console } from "@woowacourse/mission-utils";
+let names;
+let err=0;
+
 class App {
-  async play() {}
+  async play() {
+    this.car_name();
+  }
+
+  //자동차 이름 입력 받기
+  async car_name(){
+    try{
+      const str= await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+      names=str.split(',');
+      this.car_name_length();
+      if(err==1){
+        throw "ERROR";
+      }
+    }
+    catch(error){
+      Console.print(error);
+    }
+  }
+
+  //자동차 이름 6자 이상일 경우 에러
+  async car_name_length(){
+    for(let i=0; i<names.length; i++){
+      if(names[i].length>=6){
+        err=1;
+      }
+    }
+  }
 }
 
 export default App;
