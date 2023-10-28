29c231eef8c502518b5e865b12e4796513389ff2
sehyun <e6d1fe@gmail.com>
2023-10-28 20:14:53 +0900

    feat: 시도 횟수 입력 받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from '@woowacourse/mission-utils';
+import { message } from './Constants.js';
+
 class App {
   async play() {}
+
+  async getRacecarNameInput() {
+    const racecarNameInput = await Console.readLineAsync(message.ASK_RACECAR_NAMES);
+  }
+
+  async getAttemptInput() {
+    const attemptInput = await Console.readLineAsync(message.ASK_ATTEMPT_NUMBER);
+  }
 }
 
 export default App;
