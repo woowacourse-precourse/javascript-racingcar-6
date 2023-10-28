176aa922955fd7ff8060c840e4ffb9f38b298b61
Junseo <monstazo911@gmail.com>
2023-10-28 18:31:49 +0900

    Feat: 자동차 이름 입력 메소드 추가
    - 게임 시작 전 자동차들의 이름을 입력받는 메소드
    - 정상 입력에 대한 테스트 코드 작성 ( 잘못된 입력은 추후 구현 예정 )
    - 자동차들을 입력받기 때문에 메소드 명은 carInput
    - 입력이 비동기로 진행되기 때문에 async 함수
    - 배열로 반환하여 각 자동차 인스턴스를 생성할 것이기 때문에, 문자열을 쉼표 기준으로 배열로 변환 후 반환

---
+++ __tests__/carInputTest.js
@@ -0,0 +1,70 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import App from "../src/App";
+
+const mockQuestions = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  });
+};
+
+describe("자동차 이름 입력 메소드 테스트", () => {
+  test("쉼표로 구분된 자동차 이름 입력 테스트", async () => {
+    const inputs = ["pobi,woni,jun"];
+
+    mockQuestions(inputs);
+
+    const result = await MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+
+    expect(result).toEqual("pobi,woni,jun");
+  });
+
+  test("배열 반환 테스트", async () => {
+    const inputs = ["pobi,woni,jun"];
+    const outputArray = ["pobi", "woni", "jun"];
+
+    mockQuestions(inputs);
+
+    const app = new App();
+    const result = await app.carInput();
+
+    expect(result).toStrictEqual(outputArray);
+  });
+
+  test("자동차 한 대 테스트", async () => {
+    const inputs = ["pobi"];
+    const outputArray = ["pobi"];
+
+    mockQuestions(inputs);
+
+    const app = new App();
+    const result = await app.carInput();
+
+    expect(result).toStrictEqual(outputArray);
+  });
+
+  test("자동차 8대 테스트", async () => {
+    const inputs = ["pobi,woni,jun,one,two,three,four,five"];
+    const outputArray = [
+      "pobi",
+      "woni",
+      "jun",
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+    ];
+
+    mockQuestions(inputs);
+
+    const app = new App();
+    const result = await app.carInput();
+
+    expect(result).toStrictEqual(outputArray);
+  });
+});
--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
+  async carInput() {
+    const carNameInput = await MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+    const carNameArray = carNameInput.split(",");
+    return carNameArray;
+  }
+
   async play() {}
 }
 
