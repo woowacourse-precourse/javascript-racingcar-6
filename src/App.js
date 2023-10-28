ceba7ad52f0e5cce9a871aef171acdb6f0df72e4
Goongam <dlwjddn341@naver.com>
2023-10-28 19:04:47 +0900

    Feat: 게임 차 초기화

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { INPUT_MESSAGE } from "./constants/constant.js";
+import { RacingGame } from "./game/index.js";
+import {getRandomNumber} from "./util/Random.js";
+import { input } from "./util/input.js";
+import { makeCarsArray } from "./util/message.js";
+import { print } from "./util/output.js";
+
 class App {
-  async play() {}
+
+  async play() {
+    // TODO: 대문자?
+    const racing = new RacingGame();
+
+    await racing.start();
+  }
 }
 
 export default App;
