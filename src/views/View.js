66acc1cb0af940c79c376c8a4a268982cb81b8f5
Ryong-E <think8867@gmail.com>
2023-10-28 17:11:32 +0900

    feature: 차량 이름 받는 기능 개발
    입력값에 빈 공백을 지우는 기능
    입력값에 대한 예외 처리

--- src/views/View.js
+++ src/views/View.js
@@ -1,8 +1,20 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
+import Validator from '../validators/Validator.js';
 
 class View {
   static print(message) {
     MissionUtils.Console.print(message);
+  }
+
+  static async askNameAnswer(message) {
+    let answer = await MissionUtils.Console.readLineAsync(message);
+    answer = this.removeSpace(answer);
+    Validator.nameValidate(answer);
+    return answer;
+  }
+
+  static removeSpace(string) {
+    return string.replaceAll(' ', '');
   }
 }
 export default View;
