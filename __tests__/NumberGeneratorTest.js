0d237765140ebb48635485c61867969c02c21529
zxxng <jeayeong123@naver.com>
2023-10-28 16:20:18 +0900

    feat: 0에서 9사이에서 무작위 값을 구하는 기능 추가

---
+++ __tests__/NumberGeneratorTest.js
@@ -0,0 +1,25 @@
+import NumberGenerator from '../src/lib/classes/NumberGenerator';
+
+// createRandomNumbers 통과
+test('createRandomNumbers / 랜덤 숫자 생성 test-1', () => {
+  const numbers = NumberGenerator.createRandomNumbers();
+  console.log(numbers);
+  // 반환된 값이 배열인지 확인
+  expect(Array.isArray(numbers)).toBe(true);
+  // 배열의 길이가 1인지 확인
+  expect(numbers.length).toBe(1);
+});
+
+test('createRandomNumbers / 랜덤 숫자 생성 test-2', () => {
+  const numbers = NumberGenerator.createRandomNumbers();
+  console.log(numbers);
+  expect(Array.isArray(numbers)).toBe(true);
+  expect(numbers.length).toBe(1);
+});
+
+test('createRandomNumbers / 랜덤 숫자 생성 test-3', () => {
+  const numbers = NumberGenerator.createRandomNumbers();
+  console.log(numbers);
+  expect(Array.isArray(numbers)).toBe(true);
+  expect(numbers.length).toBe(1);
+});
