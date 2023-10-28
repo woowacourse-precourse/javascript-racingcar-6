2e19013983855da1a1421b828ef99233c7a4b49e
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-10-29 02:40:26 +0900

    :recycle: refactor: 뷰에서 해당 메세지 상수를 호출하는 방식으로 수정 컨트롤러에서 주입시키기보단, 메세지가 자주 변경되지 않고 언어에 관계없이 일정한 경우이기 때문에 뷰에 책임을 둠

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,16 +1,15 @@
 import { Console } from '@woowacourse/mission-utils';
+import { MESSAGE } from '../constants/messages';
 
 const InputView = {
   async getCarNames() {
-    const carNames = await Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
-    );
+    const carNames = await Console.readLineAsync(MESSAGE.carsNameInputGuide);
 
     return carNames.split(',').map((name) => name.trim());
   },
 
   async getRound() {
-    return await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    return await Console.readLineAsync(MESSAGE.roundInputGuide);
   },
 };
 
