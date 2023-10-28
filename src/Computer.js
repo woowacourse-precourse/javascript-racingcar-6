dd435b68e58daa42d4296350e1987fe408e91c84
Boeun <boeun0904@naver.com>
2023-10-29 01:24:13 +0900

    refactor(User): move raw command line to User code

--- src/Computer.js
+++ src/Computer.js
@@ -9,9 +9,7 @@ export default class Computer {
   playGame(user) {}
 
   async getCarNameInputFromUser(user) {
-    const userInput = await user.getCarNameInput(
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
-    );
+    const userInput = await user.getCarNameInput();
     const carNameArr = this.getCarNameArrayFromString(userInput);
     this.racingCars = carNameArr.map((carName) => new RacingCar(carName));
   }
--- src/User.js
+++ src/User.js
@@ -1,10 +1,12 @@
 import { Console } from '@woowacourse/mission-utils';
 
 export default class User {
-  async getCarNameInput(query) {
-    return await Console.readLineAsync(query);
+  async getCarNameInput() {
+    return await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+    );
   }
-  async getTrialNumber(query) {
-    return Number(await Console.readLineAsync(query));
+  async getTrialNumber() {
+    return Number(await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n'));
   }
 }
