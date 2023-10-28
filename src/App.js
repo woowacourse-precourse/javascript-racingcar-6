df016d709e4f3977fb950db105154c15fd57f269
songudeum <songudeum@gmail.com>
2023-10-29 01:33:06 +0900

    feat: 경주할 자동차 이름 배열로 만들어주는 기능 및 시도횟수 숫자로 만들어주는 기능 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { Random, Console } from "@woowacourse/mission-utils";
 class App {
-  async play() {}
+    async play() {
+        Console.print(
+            "경주할 자동차 이름을 입력하세요.(이름은 5자 이하로 쉼표(,)를 기준으로 구분해주세요.)"
+        );
+
+        const carNameArr = await this.createCarNameArr();
+        console.log(carNameArr);
+
+        Console.print("시도할 횟수는 몇 회인가요?");
+
+        const numberOfGames = await this.getNumberOfGames();
+        console.log(numberOfGames);
+    }
+    async createCarNameArr() {
+        const carNameArr = await Console.readLineAsync().then((value) =>
+            value.split(",")
+        );
+        return carNameArr;
+    }
+    async getNumberOfGames() {
+        const numberOfGames = await Console.readLineAsync().then(
+            (value) => value / 1 //문자형 숫자형으로 변환
+        );
+
+        return numberOfGames;
+    }
 }
 
 export default App;
