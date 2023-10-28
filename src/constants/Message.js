438a191d892d36fbfd6314a3ac6112c327102ae6
osohyun0224 <osohyun0224@naver.com>
2023-10-29 00:25:37 +0900

    feat(Message): 사용자의 잘못된 입력값으로 인한 에러 메세지 상수화하여 정의
    - 사용자가 잘못된 값을 입력하면, 에러를 발생시키고 애플리케이션을 종료하는 기능을 구현하기 위해서 에러 메세지를 정의하였습니다.

--- src/constants/Message.js
+++ src/constants/Message.js
@@ -4,3 +4,12 @@ export const GAME_MESSAGE = Object.freeze({
   printResult: "실행 결과",
   printWinner: "최종 우승자",
 });
+
+export const ERROR_MESSAGE = Object.freeze({
+  emptyInput: "[ERROR] 입력한 값이 존재하지 않습니다.",
+  hasWhiteSpace: "[ERROR] 입력한 값에 공백이 존재합니다.",
+  exceedNameLimit: "[ERROR] 자동차의 이름은 5글자 이하만 가능합니다.",
+  duplicateName: "[ERROR] 중복된 자동차의 이름이 존재합니다.",
+  notNumber: "[ERROR] 자동차 이동 횟수는 숫자만 가능합니다.",
+  notNaturalNumber: "[ERROR] 자동차 이동 횟수는 1이상의 자연수만 가능합니다.",
+});
