38be2d610cfbb5f4bc8efd15380d53c88ad939fa
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-10-28 20:12:20 +0900

    refactor : 게임에 필요한 입력 요청과 게임준비를 위한 입력값 할당 분리
    ready() : user에게 입력받은 carNames와 tryRound를 이용해 RacingGame이 사용할 수 있도록 형태변환

--- src/App.js
+++ src/App.js
@@ -13,7 +13,13 @@ export default class App {
   async play() {
     const carNames = await this.#inputReader.carNames();
     const tryRound = await this.#inputReader.tryRount();
+
+    await this.ready(carNames, tryRound);
+  }
+
+  async ready(carNames, tryRound) {
     const carArray = carNames.split(',').map((name) => new Car(name));
+
     this.#racingGame = new RacingGame(carArray, tryRound);
   }
 }
