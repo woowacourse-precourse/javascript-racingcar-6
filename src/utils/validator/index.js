9cc2be344bdf121c56fbf4bc1153be479be960de
ryan-dia <cjfdnjs158@naver.com>
2023-10-28 13:05:59 +0900

    fix(validator): 잘못된 변수 참조 수정

--- src/utils/validator/index.js
+++ src/utils/validator/index.js
@@ -4,7 +4,7 @@ import { isDelimiter, isNameLength } from './utils/index.js';
 const Validator = {
   checkRacingCarNames(carNames) {
     if (isDelimiter(carNames)) {
-      throw new Error(`[ERROR] 구분자는 ${SYSTEM_TEXT}만 가능합니다.`);
+      throw new Error(`[ERROR] 구분자는 ${SYSTEM_TEXT.delimiter}만 가능합니다.`);
     }
     if (isNameLength(carNames)) throw new Error('[ERROR] 이름의 길이가 유효하지 않습니다.');
   },
