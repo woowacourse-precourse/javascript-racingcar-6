5f728294184fa550b2a911f0618f6a7951b32710
Seo San <dannysir@naver.com>
2023-10-28 16:26:02 +0900

    refactor : 결과 출력 기능 모듈화 결과 출력 기능 모듈화 진행

--- src/App.js
+++ src/App.js
@@ -1,43 +1,11 @@
-import {MissionUtils} from "@woowacourse/mission-utils";
-import {makeAndFilter} from "./Random.js";
 import {Input_First, Input_Second} from "./InputScreen.js";
 import {GAME_RESULT} from "./OutScreen.js";
 
 class App {
     async play() {
-
         const NAME_ARRAY = await Input_First();
         const COIN = await Input_Second();
-        const FORWARD_ARR = [...NAME_ARRAY].fill(0);
-        const ON_GAME = (NAME) => {
-            let outString = ``;
-            let FORWARD_MARK = "-"
-            let max = -1;
-            let WINNER = [];
-            makeAndFilter(FORWARD_ARR.length).forEach((VALUE, INDEX) => {
-                FORWARD_ARR[INDEX] += VALUE;
-            });
-            for (let i = 0; i < NAME.length; i++) {
-                outString += `${NAME[i]} : ${FORWARD_MARK.repeat(FORWARD_ARR[i])}\n`;
-            }
-            MissionUtils.Console.print(outString);
-            FORWARD_ARR.forEach(v => {
-                if (v > max) {
-                    max = v;
-                }
-            });
-            FORWARD_ARR.forEach((VALUE, INDEX) => {
-                if (VALUE === max) {
-                    WINNER.push(NAME_ARRAY[INDEX]);
-                }
-            });
-            return WINNER;
-        };
-
-
         GAME_RESULT(NAME_ARRAY, COIN);
     }
-
 }
-
 export default App;
--- src/OutScreen.js
+++ src/OutScreen.js
@@ -1,7 +1,34 @@
 import {MissionUtils} from "@woowacourse/mission-utils";
+import {makeAndFilter} from "./Random.js";
 
+let FORWARD_ARR= [];
+const ON_GAME = (NAME) => {
+    let outString = ``;
+    let FORWARD_MARK = "-"
+    let max = -1;
+    let WINNER = [];
+    makeAndFilter(FORWARD_ARR.length).forEach((VALUE, INDEX) => {
+        FORWARD_ARR[INDEX] += VALUE;
+    });
+    for (let i = 0; i < NAME.length; i++) {
+        outString += `${NAME[i]} : ${FORWARD_MARK.repeat(FORWARD_ARR[i])}\n`;
+    }
+    MissionUtils.Console.print(outString);
+    FORWARD_ARR.forEach(v => {
+        if (v > max) {
+            max = v;
+        }
+    });
+    FORWARD_ARR.forEach((VALUE, INDEX) => {
+        if (VALUE === max) {
+            WINNER.push(NAME[INDEX]);
+        }
+    });
+    return WINNER;
+};
 export const GAME_RESULT = (NAME, NUM) => {
     const RESULT_OUT = "\n실행 결과"
+    FORWARD_ARR = [...NAME].fill(0);
     MissionUtils.Console.print(RESULT_OUT);
     let cnt = 1;
     let RESULT_ARRAY = [];
@@ -9,6 +36,6 @@ export const GAME_RESULT = (NAME, NUM) => {
         RESULT_ARRAY = ON_GAME(NAME);
         cnt++;
     }
-
-    MissionUtils.Console.print(`최종 우승자 : ${RESULT_ARRAY.join(',')}`);
+    const WINNER_OUT = `최종 우승자 : ${RESULT_ARRAY.join(',')}`
+    MissionUtils.Console.print(WINNER_OUT);
 };
