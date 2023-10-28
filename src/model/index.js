ef432971b41d4435870d0342c5d08b24897c6aae
ryan-dia <cjfdnjs158@naver.com>
2023-10-28 19:50:22 +0900

    feat(model): 자동차가 전진할 수 있는지 판별해주는 기능 추가
    canMove - 랜덤으로 나온 숫자가 4이상 나와야 전진이 가능하기에
    4이상 나오면 true를 반환

--- src/model/index.js
+++ src/model/index.js
@@ -1,5 +1,13 @@
+import { SYSTEM } from '../constants/System.js';
+import RandomNumberGenerator from './RandomNumberGenerator.js';
+
 class RacingCarModel {
-  canMove() {}
+  #carData;
+
+  static canMove() {
+    const randomNumber = RandomNumberGenerator.run();
+    return randomNumber >= SYSTEM.moveStartPoint;
+  }
 }
 
 export default RacingCarModel;
