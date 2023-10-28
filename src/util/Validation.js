12573bf85ca5240e5c21c30f5db345a01c855b0d
chiman <rlaclghks123@naver.com>
2023-10-28 14:46:18 +0900

    feat: 유효성 검사 : 자동차 이름 중복 방지

--- src/util/Validation.js
+++ src/util/Validation.js
@@ -3,6 +3,10 @@ const Validation = {
     if (!carNames.every((carName) => carName.length <= 5)) {
       throw new Error('[ERROR] 자동차 이름은 5자 이하여야 합니다.');
     }
+
+    if (new Set(carNames).size !== carNames.length) {
+      throw new Error('[ERROR] 중복되는 자동차 이름이 존재합니다.');
+    }
   },
 };
 
