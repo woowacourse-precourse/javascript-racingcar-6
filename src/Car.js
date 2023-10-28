a46347bab92fb006bb9bd63d15817740a2972318
HarenKei <seungmin1266@gmail.com>
2023-10-28 21:55:38 +0900

    feat: 경주 할 자동차를 다룰 클래스 Car 구현

---
+++ src/Car.js
@@ -0,0 +1,13 @@
+class Car {
+  constructor(carName) {
+    this.carName = carName;
+    this.forwardCount = 0;
+  }
+
+  forwardCar(carName) {
+    this.carName = carName
+
+  }
+}
+
+export default Car;
