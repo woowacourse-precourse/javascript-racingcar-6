2d00cfe2edb1770b8f4f95006f4fd752ec8012cf
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-10-28 09:39:27 +0900

    :sparkles: feat: 컨트롤러에서 자동차경주게임 실행 후 각 라운드에 해당하는 값을 반환받음 사용자에게 자동차이름과 진행횟수를 입력받고 자동차경주게임 인스턴스에 주입시킴, 자동차경주게임이 마지막 라운드가 될때까지 자동차이름과 전진횟수를 객체배열형태로 받음, 마지막 라운드시 반복문이 종료되고 우승자 배열을 받음

--- src/controller/CarRacingGameController.js
+++ src/controller/CarRacingGameController.js
@@ -1,7 +1,28 @@
-class CarRacingGameController {
-  constructor() {}
+import CarRacingGame from '../domain/CarRacingGame.js';
+import randomNumberGenerator from '../utils/RandomNumberGenerator.js';
+import InputView from '../views/InputView.js';
 
-  startGame() {}
+class CarRacingGameController {
+  #carRacingGame;
+
+  async startGame() {
+    const carNames = await InputView.getCarNames();
+    const round = await InputView.getRound();
+    this.#carRacingGame = new CarRacingGame(carNames, round);
+    this.currentRacing();
+  }
+
+  currentRacing() {
+    while (this.#carRacingGame.isPlaying()) {
+      this.#carRacingGame.race(randomNumberGenerator);
+
+      const currentResult = this.#carRacingGame.getRoundResult();
+      console.log(currentResult);
+    }
+
+    const winners = this.#carRacingGame.getWinners();
+    console.log(winners);
+  }
 }
 
 export default CarRacingGameController;
