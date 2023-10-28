efafc342d2134ca250281ccbc89d8f2ff065dea7
김영은 <jessicak0828@gmail.com>
2023-10-28 17:54:26 +0900

    feat: getValue.js 생성- 차 이름, 횟수, 랜덤한 숫자
    1. getUserName(): 유저가 입력한 차 이름 가져오기
    2. getTrialFrequency(): 유저가 입력한 횟수 가져오기
    3. getRandomNumber: 랜덤으로 돌린 숫자 가져오기

---
+++ src/getValue.js
@@ -0,0 +1,26 @@
+import { INPUT_REQUEST } from './constants';
+import {Random} from '@woowacourse/mission-utils'
+import {validateName,validateNumber} from './validateInput'
+
+async function getUserName() {
+	Console.print(INPUT_REQUEST.NAME);
+	const input = await Console.readLineAsync(INPUT_REQUEST.NAME);
+	const inputArr = input.split(',');
+	const validNameArr = validateName(inputArr);
+	Console.print(...validNameArr);
+	return validNameArr;
+}
+
+async function getTrialFrequency() {
+	Console.print(INPUT_REQUEST.FREQUENCY);
+	const input = await Console.readLineAsync(INPUT_REQUEST.FREQUENCY);
+	const validNumber = validateNumber(input);
+	Console.print(validNumber);
+	return +validNumber;
+}
+
+async function getRandomNumber() {
+	const randomNumber = await Random.pickNumberInRange(0, 9);
+	return +randomNumber;
+}
+export { getUserName, getTrialFrequency, getRandomNumber };
