01b6d391278667cbb142c04e3b94e6b4b514c8a1
dawn0314 <sdawn0314@gmail.com>
2023-10-28 15:03:23 +0900

    feat: Add user's input
    - 시도할 횟수 입력 받음
    - 경주 할 자동차 이름 입력 받음
    - 자동차 이름 split하여 배열로 저장

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { MissionUtils, Console } from "@woowacourse/mission-utils";
+import carRacing from "./racing.js";
+
 class App {
-  async play() {}
+  async play() {
+    try {
+      const carNames = await Console.readLineAsync(
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+      );
+
+      const count = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+      console.log(carNames);
+      const splittedCarNames = carNames.split(",");
+      console.log(splittedCarNames);
+      carRacing(splittedCarNames, count);
+    } catch (err) {}
+  }
 }
 
 export default App;
