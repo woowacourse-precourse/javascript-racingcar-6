65033f1b40acc2f8d6dce44d8006a7b2cdb15d8a
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-10-28 19:32:42 +0900

    feat : 자동차 이름을 받고 횟수를 입력받아 게임을 진행함
    const carNames : (,)를 기준으로 자동차의 이름들을 받아옴
    const tryRound : 라운드 진행 횟수를 받아옴
    carArray : (,)를 기준으로 각각의 자동차 이름을 분리해 RacingGame 시작

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-class App {
-  async play() {}
-}
+import RacingGame from './RacingGame';
+import InputReader from './View/InputReader';
 
-export default App;
+export default class App {
+  #inputReader;
+  #racingGame;
+
+  constructor() {
+    this.#inputReader = new InputReader();
+    this.#racingGame = null;
+  }
+
+  async play() {
+    const carNames = await this.#inputReader.carNames();
+    const tryRound = await this.#inputReader.tryRount();
+    const carArray = carNames.split(',').map((name) => new Car(name));
+    this.#racingGame = new RacingGame(carArray, tryRound);
+  }
+}
