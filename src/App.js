d9dda8542de63235d1b7d788ef5677cb79e7ab5b
yjh1008 <exesea12@gmail.com>
2023-10-28 18:36:15 +0900

    feat: 경주할 자동차의 이름을 입력받는 함수 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+const { Console, Random } = MissionUtils;
+import Cars from "./Cras";
 class App {
-  async play() {}
+
+  async play() {
+    const input = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    const cars = new Cars(input);
+    const repetitions = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+  }
 }
 
 export default App;
