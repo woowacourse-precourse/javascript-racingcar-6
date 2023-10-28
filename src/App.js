bf25d7461148dd732a4d320431ff918139788416
Hyunji Seok <alleyji126@gmail.com>
2023-10-28 22:33:12 +0900

    feat: convert array carNames to string and assign it to array cars

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App {
 
   async play() {
     const carNames = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) \n");
+    this.cars = carNames.split(",");
     this.raceTimes = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요? \n");
   }
 
@@ -28,7 +29,7 @@ class App {
 
   }
 
-  async handleErrors() {
+  async handleErrors(cars, raceTimes) {
 
   }
 }
