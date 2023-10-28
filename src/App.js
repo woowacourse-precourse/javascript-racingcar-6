04d33045fcbc7c5512b55e38694ffe3441ec47f0
innerstella <suji0509@naver.com>
2023-10-28 20:12:33 +0900

    feat : 2. 경주할 자동차 이름 받기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
-  async play() {
-    Console.print(
+  constructor() {
+    this.random = 0;
+    this.carList = [];
+  }
+
+  // 2. 경주할 자동차 이름 받기
+  async inputCarList() {
+    const input = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     );
+    const inputArr = input.split(",");
+    this.carList = [...inputArr];
+    this.isValidate();
+  }
+
+  isValidate() {
+    this.carList.forEach((car) => {
+      if (car.length > 5) {
+        throw new Error("[ERROR] 자동차 이름은 5글자 이하만 가능합니다.");
+      } else {
+        return true;
+      }
+    });
+  }
+
+  async play() {
+    this.inputCarList();
   }
 }
 
