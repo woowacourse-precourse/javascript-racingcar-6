df9e9af270bc60bd83dd5024bad15f95f64077ab
rbgksqkr <rbgks1937@naver.com>
2023-10-28 13:04:57 +0900

    feat: Car class 파일 분리

--- src/App.js
+++ src/App.js
@@ -1,23 +1,5 @@
 import { Random, Console } from "@woowacourse/mission-utils";
-
-class Car {
-  name = "";
-  score = 0;
-
-  constructor(name, score) {
-    this.name = name;
-    this.score = score;
-  }
-
-  printMyScore() {
-    const number = Random.pickNumberInRange(0, 9);
-    if (number >= 4) {
-      this.score += 1;
-    }
-    const result = "-".repeat(this.score);
-    Console.print(`${this.name} : ${result}`);
-  }
-}
+import Car from "./Car";
 
 class App {
   async play() {
---
+++ src/Car.js
@@ -0,0 +1,22 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+
+class Car {
+  name = "";
+  score = 0;
+
+  constructor(name, score) {
+    this.name = name;
+    this.score = score;
+  }
+
+  printMyScore() {
+    const number = Random.pickNumberInRange(0, 9);
+    if (number >= 4) {
+      this.score += 1;
+    }
+    const result = "-".repeat(this.score);
+    Console.print(`${this.name} : ${result}`);
+  }
+}
+
+export default Car;
