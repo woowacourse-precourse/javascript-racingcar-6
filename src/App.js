830361e31d1ef9a1365a61f5cfd1d494b171a347
leehyewon0531 <ifpffk@naver.com>
2023-10-28 18:39:02 +0900

    feat: 자동차 이름 길이 유효성 검사 함수 구현

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    const [carsName, gamesNum] = await this.receiveUserInput();
+    const carsName = await this.receiveUserInput('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
     const cars = this.seperateNames(carsName);
+    const gamesNum = await this.receiveUserInput('시도할 횟수는 몇 회인가요?\n');
   }
 
-  async receiveUserInput() {
-    const carsName = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
-    const gamesNum = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
-    return [carsName, gamesNum];
+  async receiveUserInput(guideMsg) {
+    const userInput = await Console.readLineAsync(guideMsg);
+    return userInput;
   }
 
   seperateNames(names) {
@@ -20,12 +20,20 @@ class App {
       this.isValidName(el);
     });
 
+    seperated.forEach(el => {
+      this.isValidLen(el);
+    });
+
     return seperated;
   }
 
   isValidName(name) {
     const regEx = /^[a-z]*$/;
     if(!name.match(regEx)) throw new Error('[ERROR] 자동차 이름은 영문 소문자만 가능합니다.');
+  }
+
+  isValidLen(name) {
+    if(name.length > 5) throw new Error('[ERROR] 자동차 이름의 길이는 5 이하입니다.');
   }
 }
 
