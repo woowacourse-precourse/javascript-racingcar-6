41ec7877a5eca5e9805d4a52791cc5dbf6b3efbf
rkdcodus <rcd7275@naver.com>
2023-10-28 22:03:39 +0900

    feat: 경주할 자동차 이름 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Random, Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  constructor() {
+    this.carList = [];
+  }
+
+  async createCar() {
+    const inputCar = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+    );
+    const inputCarList = inputCar.split(',');
+    for (let i = 0; i < inputCarList.length; i += 1) {
+      this.carList.push({
+        name: inputCarList[i],
+        distance: 0,
+      });
+    }
+  }
+
+  async play() {
+    this.createCar();
+  }
 }
 
 export default App;
