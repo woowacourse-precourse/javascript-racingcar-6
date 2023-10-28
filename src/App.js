e54b1785893295c67f8c791eac4cc86b36bed470
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-10-28 14:51:05 +0900

    refactor: 자동차명 입력 변수명 변경

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ class App {
   }
 
   async getRacingCars() {
-    let inputValue = await Console.readLineAsync(
+    let inputRacingCars = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
-    return inputValue.split(",");
+    return inputRacingCars.split(",");
   }
 
   async setRacingCars(racingCars) {
