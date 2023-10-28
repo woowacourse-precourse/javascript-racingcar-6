ae8c0ba5cafc12eab72069c389217702c33710bc
sang-il Yoon <yunsangil7749@gmail.com>
2023-10-28 16:38:39 +0900

    feat: 이동 횟수를 입력받는 기능 작성
    유저에게 경주의 이동횟수를 입력받는 기능의 함수 추가

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async play() {}
 
-  async participating_car(){
+  async participatingCar(){
   let cars = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
   cars = cars.split(',')
   
@@ -14,6 +14,16 @@ class App {
   });
 
   return cars
+ }
+
+ async numberOfMoves(){
+  let NumberOfmoves = await Console.readLineAsync('시도할 횟수는 몇 회인가요?')
+  // 시도횟수의 제한은 없으며 숫자만 입력 가능하다.
+  if(isNaN(NumberOfmoves)){
+    throw new Error();
+  }
+
+  return NumberOfmoves;
  }
   
 }
