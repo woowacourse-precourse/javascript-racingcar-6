97d3497c4e782965e7cdb205c4b0efe20c7006c5
BangDori <qudwns8865@naver.com>
2023-10-28 20:46:57 +0900

    refactor: 시도 횟수의 길이를 검증하는 함수 제거

--- src/util/attempt-validation.js
+++ src/util/attempt-validation.js
@@ -1,23 +1,8 @@
 const ERROR_MESSAGE = Object.freeze({
-  LENGTH: "[ERROR] 시도 횟수를 입력하지 않으셨습니다.",
   NOT_NUMBER: "[ERROR] 시도 횟수는 숫자만 입력 가능합니다.",
   FLOAT_ERROR: "[ERROR] 시도 횟수는 정수만 가능합니다.",
   POSITIVE: "[ERROR] 시도 횟수는 1이상의 정수만 가능합니다.",
 });
-
-/**
- * 시도 횟수의 길이를 검증하는 함수
- * @param {string} inputAttemptCount 시도 횟수
- * @throws 시도 횟수의 길이가 0이라면 에러를 던진다.
- * @returns
- */
-function isLengthError(inputAttemptCount) {
-  if (inputAttemptCount.length < 1) {
-    throw new Error(ERROR_MESSAGE.LENGTH);
-  }
-
-  return;
-}
 
 /**
  * 시도 횟수가 숫자인지 검증하는 함수
@@ -65,7 +50,6 @@ function isPositiveIntegerError(inputAttemptCount) {
  * @returns {integer} 정수 형태의 검증 횟수
  */
 function validateAttemptCount(inputAttemptCount) {
-  isLengthError(inputAttemptCount);
   isNumeric(inputAttemptCount);
   isFloatError(inputAttemptCount);
   isPositiveIntegerError(inputAttemptCount);
