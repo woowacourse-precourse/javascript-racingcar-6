98c0d92c67490c76a403d89e59de4f5a9717c051
PARK-CHANUK <doosan1224@naver.com>
2023-10-28 22:35:11 +0900

    feat(Output): 최종 우승자 출력기능 구현

--- src/View/Output.js
+++ src/View/Output.js
@@ -4,4 +4,8 @@ export default class Output {
   static printTotalResult(totalResult) {
     Console.print(totalResult);
   }
+
+  static printWinners(winners) {
+    Console.print(`최종 우승자 : ${winners}`);
+  }
 }
