8c2b13691ce19621af957724e0d5e5ae6b16be4f
ryan-dia <cjfdnjs158@naver.com>
2023-10-28 14:43:59 +0900

    feat(controller): 레이싱 횟수를 입력받는 기능 추가

--- src/controller/index.js
+++ src/controller/index.js
@@ -3,6 +3,7 @@ import InputView from '../view/InputView.js';
 class RacingCarController {
   async startGame() {
     const carNames = await InputView.readRacingCarNames();
+    const racingCount = await InputView.readRacingCount();
   }
 }
 
