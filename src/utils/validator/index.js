6268758c2ae6e16dcc3eca9b85ea0d9cf21fc3db
ryan-dia <cjfdnjs158@naver.com>
2023-10-28 13:18:24 +0900

    feat(validator): 언어 유효성 확인 기능 추가

--- src/utils/validator/index.js
+++ src/utils/validator/index.js
@@ -1,5 +1,5 @@
 import { SYSTEM_TEXT } from '../../constants/System.js';
-import { isDelimiter, isNameLength } from './utils/index.js';
+import { isDelimiter, isLanguageValid, isNameLength } from './utils/index.js';
 
 const Validator = {
   checkRacingCarNames(carNames) {
@@ -7,6 +7,7 @@ const Validator = {
       throw new Error(`[ERROR] 구분자는 ${SYSTEM_TEXT.delimiter}만 가능합니다.`);
     }
     if (!isNameLength(carNames)) throw new Error('[ERROR] 이름의 길이가 유효하지 않습니다.');
+    if (!isLanguageValid(carNames)) throw new Error('[ERROR] 유효하지 않은 언어가 사용되었습니다.');
   },
 };
 
