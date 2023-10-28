fe780c97102249e946d55bb2a25a55f615cfec52
Trophy198 <spsp8426@naver.com>
2023-10-28 16:35:49 +0900

    test : Car 클래스의 메서드 테스트케이스 추가

---
+++ __tests__/model/Car.test.js
@@ -0,0 +1,40 @@
+import Car from '../../src/model/Car';
+
+const alwaysMoveStrategy = {
+  shouldMove: () => true
+};
+
+const neverMoveStrategy = {
+  shouldMove: () => false
+};
+
+describe('Car 클래스', () => {
+  describe('constructor 메서드', () => {
+    test('Car 객체가 올바르게 초기화되어야 한다', () => {
+      const car = new Car('pobi', alwaysMoveStrategy);
+      expect(car.name).toBe('pobi');
+      expect(car.position).toBe(0);
+    });
+  });
+
+  describe('getRepresentation 메서드', () => {
+    test('현재 위치를 문자열로 반환해야 한다', () => {
+      const car = new Car('pobi', alwaysMoveStrategy);
+      expect(car.getRepresentation()).toBe('');
+    });
+  });
+
+  describe('move 메서드', () => {
+    test('shouldMove가 true일 때 위치가 증가해야 한다', () => {
+      const car = new Car('pobi', alwaysMoveStrategy);
+      car.move();
+      expect(car.position).toBe(1);
+    });
+
+    test('shouldMove가 false일 때 위치가 그대로 유지되어야 한다', () => {
+      const car = new Car('pobi', neverMoveStrategy);
+      car.move();
+      expect(car.position).toBe(0);
+    });
+  });
+});
