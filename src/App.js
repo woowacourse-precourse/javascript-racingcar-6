ed05700ad4da915d8a432df1c50783aa1812b1fa
DWAN-00 <kkw1700@gmail.com>
2023-10-28 18:35:26 +0900

    feat: add 입력 제어 in play()

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { INPUT_MESSAGE, OUTPUT_MESSAGE, ERROR_MESSAGE } from "./Message.js";
+import { validateCarNames, validateRound } from "./InputOutput.js";
+
 class App {
-  async play() {}
+  constructor() {
+    this.carNames = [];
+    this.winner = [];
+    this.round = 0;
+  }
+
+  async play() {
+    try {
+      await this.getCarNames();
+      await this.getRound();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async getCarNames() {
+    const input = await MissionUtils.Console.readLineAsync(INPUT_MESSAGE.CARNAME);
+    this.carNames = input.split(",");
+    validateCarNames(this.carNames);
+  }
+
+  async getRound() {
+    const input = await MissionUtils.Console.readLineAsync(INPUT_MESSAGE.TRYCOUNT);
+    this.round = String(input);
+    validateRound(this.round);
+  }
 }
 
 export default App;
