796ecf042621d01ca0017f881acd4e04e7a66cb9
yong seock <myongdol132@gmail.com>
2023-10-29 02:43:38 +0900

    Fix(App.js): 상수 추출
    컨벤션을 까먹고 작성하여 상수를 추출 하여 코드를 작성 하였습니다.
    - 자동차 이름과 라운드 입력 메시지 상수로 사용
    - 에러 메시지를 별도의 상수로 사용
    - 랜덤 숫자의 임계값을 위한 상수 작성
    - App클래스 메서드에서 추출된 상수를 사용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
+const CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+const ROUNDS_MESSAGE = "시도할 횟수는 몇 회인가요?";
+const CAR_NAME_ERROR = "[ERROR] 자동차 이름은 5자 이하만 가능하고, 쉼표로 구분해야 합니다.";
+const ROUNDS_ERROR = "[ERROR] 시도할 횟수는 양의 정수여야 합니다.";
+const RACE_THRESHOLD = 4;
+const RANDOM_MIN = 0;
+const RANDOM_MAX = 9;
+
 class App {
   async play() {
     const names = await this.getInputCarNames();
@@ -8,18 +16,18 @@ class App {
   };
 
   async getInputCarNames() {
-    const names = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    const names = await Console.readLineAsync(CAR_NAMES_MESSAGE);
 
     if(!this.isValidNames(names)) {
-      throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능하고, 쉼표로 구분해야 합니다.");
+      throw new Error(CAR_NAME_ERROR);
     }
     return names.split(",");
   };
 
   async getInputRounds() {
-    const rounds = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+    const rounds = await Console.readLineAsync(ROUNDS_MESSAGE);
     if (!this.isValidRounds(rounds)) {
-      throw new Error("[ERROR] 시도할 횟수는 양의 정수여야 합니다.");
+      throw new Error(ROUNDS_ERROR);
     }
     return Number(rounds);
   };
@@ -35,7 +43,7 @@ class App {
     return Number.isInteger(numberRounds) && numberRounds > 0;
   };
 
-  //레이스
+  //레이스 부분 
 
   race(names, rounds) {
     let results = this.initializeResults(names);
@@ -55,7 +63,7 @@ class App {
   runRound(results) {
     const updatedResults = { ...results };
     for (let name in results) {
-      if (Random.pickNumberInRange(0, 9) >= 4) {
+      if (Random.pickNumberInRange(RANDOM_MIN, RANDOM_MAX) >= RACE_THRESHOLD) {
         updatedResults[name] += "-";
       }
     }
