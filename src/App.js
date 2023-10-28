b598d001e43924e69ace72042ce4d82763be1f72
Majesty-jun <hkwns01@gmail.com>
2023-10-29 01:14:26 +0900

    FEAT: 차량 별 이동 횟수 중 최댓값을 통한 최종 우승자 반환 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { Console } from '@woowacourse/mission-utils';
 import getCarNames from './input/getCarNames.js';
 import getRaceNumber from './input/getRaceNumber.js';
 import getMoveCount from './game/getMoveCount.js';
+import getResult from './game/getResult.js';
 
 class App {
   async play() {
     this.cars = await getCarNames();
     this.raceNumber = await getRaceNumber();
     this.moveCount = getMoveCount(this.cars, this.raceNumber);
+    this.result = getResult(this.cars, this.moveCount);
+    Console.print(`최종 우승자 : ${this.result}`);
   }
 }
 
