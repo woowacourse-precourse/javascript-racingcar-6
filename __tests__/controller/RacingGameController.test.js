e3e858c21f9fd8398532020367b432884a34db39
Trophy198 <spsp8426@naver.com>
2023-10-28 16:46:46 +0900

    test : RacingGameController 클래스의 start 메서드 테스트 케이스 추가

---
+++ __tests__/controller/RacingGameController.test.js
@@ -0,0 +1,49 @@
+import RacingGameController from '../../src/controller/RacingGameController';
+import InputView from '../../src/view/InputView';
+import OutputView from '../../src/view/OutputView';
+import InputValidator from '../../src/utils/InputValidator';
+import Car from '../../src/model/Car';
+import Race from '../../src/model/Race';
+import { processCarNames } from '../../src/utils/CarNameProcessor';
+
+jest.mock('../../src/view/InputView');
+jest.mock('../../src/view/OutputView');
+jest.mock('../../src/utils/InputValidator');
+jest.mock('../../src/model/Car');
+jest.mock('../../src/model/Race');
+jest.mock('../../src/utils/CarNameProcessor');
+
+describe('RacingGameController 클래스', () => {
+  let controller;
+
+  beforeEach(() => {
+    controller = new RacingGameController();
+  });
+
+  describe('start 메서드', () => {
+    test('게임이 올바르게 시작되고 종료되어야 한다', async () => {
+      InputView.printCarNames.mockResolvedValue('pobi,wono,jun');
+      processCarNames.mockReturnValue(['pobi', 'wono', 'jun']);
+      InputView.printNumberOfRounds.mockResolvedValue(5);
+      const mockRace = {
+        playRound: jest.fn(),
+        getRoundResults: jest.fn(),
+        getWinnersString: jest.fn()
+      };
+      Race.mockReturnValue(mockRace);
+
+      await controller.start();
+
+      expect(InputView.printCarNames).toHaveBeenCalled();
+      expect(InputView.printNumberOfRounds).toHaveBeenCalled();
+      expect(InputValidator.validateNumberOfCars).toHaveBeenCalled();
+      expect(InputValidator.validateCarName).toHaveBeenCalled();
+      expect(Car).toHaveBeenCalledTimes(3);
+      expect(Race).toHaveBeenCalledTimes(1);
+      expect(mockRace.playRound).toHaveBeenCalledTimes(5);
+      expect(OutputView.printRaceHeader).toHaveBeenCalled();
+      expect(OutputView.printRoundResult).toHaveBeenCalled();
+      expect(OutputView.printWinners).toHaveBeenCalled();
+    });
+  });
+});
