cb36e6e75f4abb2426d7d8d346e01ea7f76000d6
Seung Jun <pache31@naver.com>
2023-10-28 14:39:29 +0900

    [feat] 자동차 이름, 시도횟수 사용자 입력 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import {Console, Random} from '@woowacourse/mission-utils';
+import {GAME_MESSAGE} from './Message';
+
 class App {
-  async play() {}
+  async play() {  
+    const winner = this.carRacingGame();
+
+  }
+
+  async carRacingGame() {
+    this.carNames = await Console.readLineAsync(GAME_MESSAGE.INPUT_CAR_NAME);
+    const tryNumber = await Console.readLineAsync(GAME_MESSAGE.INPUT_TRY_NUMBER);
+
+
+  }
+
 }
 
 export default App;
---
+++ src/Message.js
@@ -0,0 +1,6 @@
+export const GAME_MESSAGE = Object.freeze({
+    INPUT_CAR_NAME: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
+    INPUT_TRY_NUMBER: "시도할 횟수는 몇 회인가요?\n",
+    EXECUTE_REULT: "실행결과\n",
+    GAME_WINNER: "최종 우승자 : "
+});
