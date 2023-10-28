ddfd9e1c8e0c4c6182a4faff417a56da5b6dad0a
ReturnReason <reasonz@daum.net>
2023-10-28 19:12:52 +0900

    feat(Players): 시도 횟수를 세팅하는 메서드 추가

--- src/model/Players.js
+++ src/model/Players.js
@@ -9,4 +9,12 @@ export default class Players {
   getPlayers() {
     return this.#players;
   }
+
+  setTryCount(tryCount) {
+    this.#tryCount = tryCount;
+  }
+
+  move() {
+    // 시도 횟수만큼 각각의 플레이어가 몇 번 움직일 지 무작위 값 구하기
+  }
 }
