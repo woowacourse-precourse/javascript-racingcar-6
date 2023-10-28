1e6a4ae56e15baad558401d6088e6430429cb902
Hyebin Lee <qnd1215@naver.com>
2023-10-28 12:59:13 +0900

    removeEmptyInpyt메서드 작성

--- src/App.js
+++ src/App.js
@@ -2,24 +2,33 @@ import { Console, MissionUtils } from "@woowacourse/mission-utils"; //이거 왜
 
 class App {
   async play() {
-    this.getCarName();
+    this.getCount();
 
   }
 
   async getCarName() {
-    const carsName = await MissionUtils.Console.readLineAsync(`경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)`);
-
+    const carsName = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
     let carNameArry = String(carsName).split(','); // 배열형태
     MissionUtils.Console.print(carNameArry);
     return carNameArry;
   }
 
-  async getCount() {ß
-    const HOW_MANY_MOVE = await MissionUtils.Console.readLineAsync(`시도할 횟수는 몇 회인가요?`);
+  async getCount() {
+    const HOW_MANY_MOVE = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
 
     return Number(HOW_MANY_MOVE); // 유효성 검사할 때 NaN면 throw ㄱㄱ
   }
 
+
+  validCheckAboutName() {
+
+  }
+
+  removeEmptyInput(input) { // input안에 getCarName() return 값 들어감.
+      for(let i = 0; i < input.length; i++) {
+        let removeEmpty = input[i].trim();
+      }
+  }//일단 이렇게 하긴 했는데 이걸 forEach로 하고 싶음!
 
 
 }
