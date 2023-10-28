ba89d95cfec8f3a3a80fc5f8ebeb78957a7617ae
swarvy <swarvy0826@naver.com>
2023-10-28 23:45:42 +0900

    feat : 공통적인 입력 유효성 검사를 위한 CommonValidator 생성

---
+++ src/validator/CommonValidator.js
@@ -0,0 +1,27 @@
+import { SYMBOLS } from '../constants/Symbols.js';
+import ValidationError from '../error/ValidationError.js';
+
+export default class CommonValidator {
+  #input;
+
+  constructor(input) {
+    this.#input = input;
+  }
+
+  static COMMON_VALIDATION_TYPES = Object.freeze({
+    emptyInput: Object.freeze({
+      message: '입력이 존재하지 않습니다, 다시 입력해주세요.',
+      isValid(input) {
+        return input !== SYMBOLS.empty;
+      },
+    }),
+  });
+
+  validate() {
+    Object.values(CommonValidator.COMMON_VALIDATION_TYPES).forEach(({ message, isValid }) => {
+      if (!isValid(this.#input)) {
+        throw new ValidationError(message);
+      }
+    });
+  }
+}
