a57c3d09e1e31e27146e9e294d93ee3739b10def
eunsung <esk114477@gmail.com>
2023-10-28 21:19:22 +0900

    feat: InputMoveCount validate 추가

--- src/InputMoveCount.js
+++ src/InputMoveCount.js
@@ -3,7 +3,19 @@ import { Console } from '@woowacourse/mission-utils';
 class InputMoveCount {
   async getMoveCount() {
     const input = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+
+    this.#validateMoveCount(input);
+
     return parseInt(input);
+  }
+
+  #validateMoveCount(input) {
+    if (isNaN(input)) {
+      throw new Error('숫자를 입력해주세요.');
+    }
+    if (input <= 0) {
+      throw new Error('1이상의 숫자를 입력해주세요.');
+    }
   }
 }
 
