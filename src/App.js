337d90489ad3a79b76ae6e888d0c5d5931443455
jong-jang <jjh0299@gmail.com>
2023-10-28 13:09:33 +0900

    feat: 에러메시지 추가

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import Car from './Car';
 
 class App {
 	async play() {}
-
+	// 에러메시지
+	error() {
+		throw new Error('[ERROR] 입력된 값이 숫자가 아닙니다.');
+	}
 	// 랜덤숫자
 	randomNumber() {
 		return Random.pickNumberInRange(0, 9);
