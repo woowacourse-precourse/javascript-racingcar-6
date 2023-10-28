03263d3e605eb8d39f06efe7638173318d9e8654
lord-potato <lordpotato1227@gmail.com>
2023-10-28 12:44:01 +0900

    feat : 비정상 입력값 검출 함수 추가 및 예외처리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import * as func from "./Function.js";
+import * as valid from "./Validation.js";
 
 class App {
   async play() {
     let NAME_CAR = await func.getCarName();
     NAME_CAR = NAME_CAR.split(",");
+    valid.validityCheckCarName(NAME_CAR);
     let SCORE_CAR = new Array(NAME_CAR.length).fill(0);
+
     let ITERATION_RACE = await func.getIteration();
+    valid.validityCheckIteration(ITERATION_RACE);
 
     MissionUtils.Console.print("\n실행 결과");
     for (let ROUND = 0; ROUND < ITERATION_RACE; ROUND++) {
