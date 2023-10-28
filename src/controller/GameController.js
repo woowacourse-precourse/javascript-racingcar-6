48d6ac8b4f5eb25dffb9666f0530b75ac63e6c89
ReturnReason <reasonz@daum.net>
2023-10-28 17:04:04 +0900

    feat(GameController): 사용자가 입력한 자동차의 수가 2대 이상, 10대 이하인지 체크하는 메서드 추가

--- src/controller/GameController.js
+++ src/controller/GameController.js
@@ -1,4 +1,5 @@
-import { MESSAGE } from '../constants/messages.js';
+import { GAME_RULL } from '../constants/gameRules.js';
+import { ERROR_MESSAGE, MESSAGE } from '../constants/messages.js';
 
 export default class GameController {
   constructor({ model, view }) {
@@ -6,7 +7,27 @@ export default class GameController {
     this.view = view;
   }
 
-  init() {
-    this.view.print(MESSAGE.START);
+  async init() {
+    const carNames = await this.view.getUserInput(MESSAGE.START);
+    this.checkValidation(carNames);
   }
+
+  checkValidation(userInput) {
+    this.#validateCarCount(userInput);
+
+    return true;
+  }
+
+  #validateCarCount(carNames) {
+    const cars = carNames.split(GAME_RULL.DIVISION).map((name) => name.trim());
+    const isWrongPlayerCount =
+      cars.length < GAME_RULL.PLAYER_MIN_NUMBER ||
+      cars.length > GAME_RULL.PLAYER_MAX_NUMBER;
+
+    if (isWrongPlayerCount) {
+      throw new Error(ERROR_MESSAGE.PLAYER_COUNT);
+    }
+  }
+
+  #validateCarName(carName) {}
 }
