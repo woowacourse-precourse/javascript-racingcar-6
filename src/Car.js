2383ce23e0654292488f7b7ef9279ba2d1b38deb
soyoung125 <sypark041754@gmail.com>
2023-10-29 00:16:35 +0900

    feat(Car): 자동차를 전진시키는 기능 추가

--- src/Car.js
+++ src/Car.js
@@ -1,7 +1,14 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class Car {
     constructor(name) {
         this.name = name;
         this.forwardCount = 0;
+    }
+
+    goForword() {
+        this.forwardCount += 1;
+        MissionUtils.Console.print(this.forwardCount);
     }
 }
 
