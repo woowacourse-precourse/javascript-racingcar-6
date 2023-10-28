de43d93e665fa12082e7ea894677d4d96d1df18d
bmj0053 <bmj05378@gmail.com>
2023-10-28 18:36:08 +0900

    feat: 사용자 입력값 받는 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
-}
+  async play() {
+    // 경주할 자동차 이름 입력
+    const cars = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
+    // 시도 횟수 입력
+    const counts = await Console.readLineAsync('시도할 횟수는 몇 회인가요?');
+    Console.print(cars)
+    Console.print(counts)
+  }
+};
+
+const app = new App();
+app.play();
 
 export default App;
