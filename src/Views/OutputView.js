c19ab603ef623107e7a100ce0c8041e4f1e690a3
0jenn0 <jenn0.6n@gmail.com>
2023-10-28 11:07:56 +0900

    Feat: 한 라운드 경기 결과 출력 추가
    경기에 참가한 모든 Car들의 status(name과 totalDistance가 들어있는 객체)들을 받아 각각의 status에 대해서 "이름 : ----"식으로 출력하도록 추가했습니다.

--- src/Views/OutputView.js
+++ src/Views/OutputView.js
@@ -5,7 +5,17 @@ const OUTPUTVIEW_MESSAGE = Object.freeze({
 });
 
 export default class OutputView {
+  static #ONE_STEP = "-";
+
   static printResultMessage() {
     Console.print(OUTPUTVIEW_MESSAGE.executionResult);
+  }
+
+  static printOneRound(carsStatus) {
+    const result = [];
+    carsStatus.forEach(({ name, totalDistance }) => {
+      result.push(`${name} : ${this.#ONE_STEP.repeat(totalDistance)}`);
+    });
+    Console.print(result.join("\n"));
   }
 }
