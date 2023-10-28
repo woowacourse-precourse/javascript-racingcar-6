90c6d7605c49676483edab92e9596e39552f6d95
ysk9926 <74546856+ysk9926@users.noreply.github.com>
2023-10-28 21:52:19 +0900

    chore: 재시작 기능 삭제
    요구 사항을 다시 확인하는 과정에서 재시작 기능이 없는 것을 확인하고 재시작 기능에 대한 내용을 삭제하였습니다.

--- src/RacingcarGame/GameController.js
+++ src/RacingcarGame/GameController.js
@@ -11,28 +11,20 @@ class GameController {
     this.util = new GameUtil();
   }
   async start() {
-    // 게임이 종료되기 전까지 반복
-    while (true) {
-      // 게임에 필요한 변수 값을 사용자로 부터 받아옴
-      const CAR_NAMES = await this.getCarnameInput();
-      MissionUtils.Console.print(CAR_NAMES.join(','));
-      const ATTEMPTS = await this.getRacingcarAttempts();
-      MissionUtils.Console.print(ATTEMPTS);
+    // 게임에 필요한 변수 값을 사용자로 부터 받아옴
+    const CAR_NAMES = await this.getCarnameInput();
+    MissionUtils.Console.print(CAR_NAMES.join(','));
+    const ATTEMPTS = await this.getRacingcarAttempts();
+    MissionUtils.Console.print(ATTEMPTS);
 
-      // 사용자의 입력값을 기반으로 게임 모델을 생성 후 결과 출력
-      this.model = new GameModel(CAR_NAMES, ATTEMPTS);
-      this.model.registerController(this);
-      MissionUtils.Console.print('실행결과');
-      this.model.run();
-      const winners = this.model.getWinner();
-      this.view.printWinners(winners);
+    // 사용자의 입력값을 기반으로 게임 모델을 생성 후 결과 출력
+    this.model = new GameModel(CAR_NAMES, ATTEMPTS);
+    this.model.registerController(this);
+    MissionUtils.Console.print('실행결과');
+    this.model.run();
 
-      // 게임 재시작 여부 확인
-      if (!(await this.getGameRestart())) {
-        this.view.printGameMessage(GameText.END_MESSAGE);
-        return;
-      }
-    }
+    const winners = this.model.getWinner();
+    this.view.printWinners(winners);
   }
 
   async getCarnameInput() {
@@ -54,16 +46,6 @@ class GameController {
 
   updateCarProgress(cars) {
     this.view.printCarProgress(cars);
-  }
-
-  async getGameRestart() {
-    this.view.printGetMessage(GameText.GET_GAME_RESTART);
-    const getRestart = await MissionUtils.Console.readLineAsync();
-    MissionUtils.Console.print(
-      `입력 값 : ${getRestart} returnValur = ${getRestart === '1'}`,
-    );
-
-    return getRestart === '1';
   }
 }
 
