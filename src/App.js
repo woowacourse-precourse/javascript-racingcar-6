cd3fc7084dce6bd24aaf450feaf138144ed3ca08
yheseul_kang <kangyheseul@naver.com>
2023-10-28 19:41:04 +0900

    Feat: 문구 출려과 기능 구현// 게임 시작 문구 출력, 자동차 이름 받기 문구 출력과 입력 받기 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-class App {
-  async play() {}
-}
+import { Console } from "@woowacourse/mission-utils";
+import { MESSAGE } from "./message";
 
-export default App;
+
+class App {
+  async play() {
+    Console.print(MESSAGE.GAME_START)
+    Console.readLineAsync(MESSAGE.RACING_CAR_NAME)
+  }
+}
+const app = new App(); 
+app.play();
+// export default App;
