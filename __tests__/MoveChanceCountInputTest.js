8c10b0b4625d3ccaf6bfeb2bfb6223aa06c14da1
JS Park <junseongpark.dev@gmail.com>
2023-10-28 12:50:49 +0900

    fix(`MoveChanceCountInputTest.js`): remove unused function declaration

--- __tests__/MoveChanceCountInputTest.js
+++ __tests__/MoveChanceCountInputTest.js
@@ -11,13 +11,6 @@ const mockQuestions = (inputs) => {
   });
 };
 
-const mockRandoms = (numbers) => {
-  MissionUtils.Random.pickNumberInRange = jest.fn();
-  numbers.reduce((acc, number) => {
-    return acc.mockReturnValueOnce(number);
-  }, MissionUtils.Random.pickNumberInRange);
-};
-
 const getLogSpy = () => {
   const logSpy = jest.spyOn(MissionUtils.Console, 'print');
   logSpy.mockClear();
