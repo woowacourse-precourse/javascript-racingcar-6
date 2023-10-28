9e4ceeb0b047e3942a3ff1c82b3865d513471a6b
donggu <zzzjdg@naver.com>
2023-10-28 14:19:09 +0900

    자동차 클래스,전진시킬지 말지 판단하는 drive 함수

--- src/App.js
+++ src/App.js
@@ -19,11 +19,38 @@ class App {
     const CARS = [];
     const USERINPUT = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     this.parsingInput(USERINPUT, CARS);
-    
+
     const ROUNDS = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?");
     this.checkRound(ROUNDS);
   }
 }
+
+
+class Car {
+  constructor(name) {
+    this.name = name;
+    this.location = 0;
+  }
+
+  drive() {
+    const RANDOM = MissionUtils.Random.pickNumberInRange(0, 9);
+
+    if (RANDOM >= 4) {
+      this.location += 1;
+    }
+
+    this.currentLocation();
+  }
+
+  currentLocation() {
+    let string = '';
+    for (let i = 0; i < this.location; i++) {
+      string += '-';
+    }
+    MissionUtils.Console.print(`${this.name} : ${string}`);
+  }
+}
+
 
 
 export default App;
