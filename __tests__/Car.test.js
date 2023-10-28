9453ec8a111e216f852e6c4315a2f6d337b0429c
uraflower <82873315+uraflower@users.noreply.github.com>
2023-10-28 17:32:04 +0900

    test: 랜덤 숫자에 따른 자동차 전진/정지 테스트를 파라미터화 테스트로 개선
    중복되는 부분을 묶어 test.each로 한 번에 테스트하도록 변경함

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -5,24 +5,14 @@ import generateRandomNumber from '../src/functions/generateRandomNumber.js';
 jest.mock('../src/functions/generateRandomNumber.js');
 
 describe(`랜덤 숫자가 ${MIN_MOVEMENT}이상이면 전진, 미만이면 정지한다`, () => {
-  test('랜덤 숫자가 0이면 정지한다', () => {
-    generateRandomNumber.mockReturnValue(0);
+  test.each([
+    [0, '정지', false],
+    [4, '전진', true],
+    [9, '전진', true],
+  ])('랜덤 숫자가 %d이면 %s한다', (randomNumber, description, expected) => {
+    generateRandomNumber.mockReturnValue(randomNumber);
     const car = new Car();
     const isMoved = car.move();
-    expect(isMoved).toBe(false);
-  });
-
-  test('랜덤 숫자가 4이면 전진한다', () => {
-    generateRandomNumber.mockReturnValue(4);
-    const car = new Car();
-    const isMoved = car.move();
-    expect(isMoved).toBe(true);
-  });
-
-  test('랜덤 숫자가 9이면 전진한다', () => {
-    generateRandomNumber.mockReturnValue(9);
-    const car = new Car();
-    const isMoved = car.move();
-    expect(isMoved).toBe(true);
+    expect(isMoved).toBe(expected);
   });
 });
