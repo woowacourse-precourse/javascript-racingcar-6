b31977254883eb654c1ab89bdf594aeb3fcb303c
전소희 <indian_potato@naver.com>
2023-10-28 21:25:20 +0900

    feat: 자동차 이름 배열로 변경
    유저가 입력한 자동차 이름을 ',' 기준으로 나누어 배열로 변경

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    MissionUtils.Console.readLineAsync(
+    const enterCarName = MissionUtils.Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) \n"
     );
+
+    const carName = enterCarName.split(",");
+
+    function verifyNameLength() {
+      if (carName.length > 5) {
+        throw new Error(
+          "[ERROR] 입력한 이름이 5자를 초과하였습니다. 게임을 다시 시작해주세요."
+        );
+      }
+    }
   }
 }
 
