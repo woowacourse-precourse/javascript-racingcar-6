29ff7aa07ceb30ea79b8249ff01a96b14d262cbd
songudeum <songudeum@gmail.com>
2023-10-29 01:32:22 +0900

    feat: 터미널 입력값 넣어줄 함수 구현 시도중

--- __tests__/FunctionTest.js
+++ __tests__/FunctionTest.js
@@ -1,33 +1,64 @@
-describe("기능 테스트", () => {
-    test("createCarNameArr함수 테스트", () => {
-        const input = `치이카와, 우사기, 하치와레`;
-        const result = input.split(",");
+import App from "../src/App.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
 
-        expect(createCarNameArr(input)).toContain(result);
+const mockQuestions = (inputs) => {
+    MissionUtils.Console.readLineAsync = jest.fn();
+
+    MissionUtils.Console.readLineAsync.mockImplementation(() => {
+        const input = inputs;
+        return Promise.resolve(input);
+    });
+};
+
+describe("기능 테스트", () => {
+    test("createCarNameArr함수 테스트", async () => {
+        const inputs = `치이카와, 우사기, 하치와레`;
+        const result = inputs.split(",");
+        // given
+        mockQuestions(inputs);
+
+        // when
+        const app = new App();
+        await expect(app.createCarNameArr()).toEqual(result);
     });
 
-    test("getNumberOfGames함수 테스트", () => {
-        const input = "1";
+    test("getNumberOfGames함수 테스트", async () => {
+        const inputs = "1";
         const result = 1;
+        // given
+        mockQuestions(inputs);
 
-        expect(getNumberOfGames(input)).toContain(result);
+        // when
+        const app = new App();
+
+        await expect(app.getNumberOfGames()).toContain(result);
     });
 
     test.each([``, `치이카와 우사기 하치와레`, `이름엄청길다,이름,이`])(
         "handleInvalidCarName함수 테스트",
-        (value) => {
-            expect(() => handleInvalidCarName.moving(value)).toThrow(
+        async (inputs) => {
+            // given
+            mockQuestions(inputs);
+
+            // when
+            const app = new App();
+            await expect(() => app.handleInvalidCarName.moving(value)).toThrow(
                 "[ERROR] 자동차 이름이 잘못된 형식입니다."
             );
         }
     );
 
-    test.each([``, `숫자`,`1,2`])(
+    test.each([``, `숫자`, `1,2`])(
         "handleInvalidNumberOfGames함수 테스트",
-        (value) => {
-            expect(() => handleInvalidNumberOfGames.moving(value)).toThrow(
-                "[ERROR] 시도 횟수가 잘못된 형식입니다."
-            );
+        async (inputs) => {
+            // given
+            mockQuestions(inputs);
+
+            // when
+            const app = new App();
+            await expect(() =>
+                app.handleInvalidNumberOfGames.moving(value)
+            ).toThrow("[ERROR] 시도 횟수가 잘못된 형식입니다.");
         }
     );
 });
