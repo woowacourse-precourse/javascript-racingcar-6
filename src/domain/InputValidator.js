5df5de657df08bcddf598e0557e387020b3a37c8
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-10-28 12:14:23 +0900

    :recycle: refactor: 유효성검사 모델에 throw 처리 로직 추가 각 에러상황에 맞는 메세지를 가짐

--- src/domain/InputValidator.js
+++ src/domain/InputValidator.js
@@ -4,20 +4,44 @@ const InputValidator = {
     return numberRegExp.test(input);
   },
 
-  hasValidCountCars(count) {
-    return count >= 2 && this.isNumeric(count);
+  isValidCountCars(count) {
+    return this.isNumeric(count) && count >= 2;
   },
 
-  hasValidCarName(name) {
+  isValidCarName(name) {
     return name.length >= 1 && name.length <= 5;
   },
 
-  hasValidUniqueName(names) {
+  isValidUniqueName(names) {
     return new Set(names).size === names.length;
   },
 
-  hasValidRoundNumber(round) {
-    return round >= 1 && this.isNumeric(round);
+  isValidRoundNumber(round) {
+    return this.isNumeric(round) && round >= 1;
+  },
+
+  hasValidCarNames(carNames) {
+    if (!this.isValidUniqueName(carNames)) {
+      throw new Error('[ERROR] 중복되는 이름이 존재합니다.');
+    }
+
+    if (carNames.some((name) => !this.isValidCarName(name))) {
+      throw new Error('[ERROR] 이름은 1글자 이상 5자 이하여야 합니다.');
+    }
+
+    if (!this.isValidCountCars(carNames.length)) {
+      throw new Error('[ERROR] 최소 2개 이상의 이름이 필요합니다.');
+    }
+
+    return carNames;
+  },
+
+  hasValidRound(round) {
+    if (!this.isValidRoundNumber(round)) {
+      throw new Error('[ERROR] 최소 1이상의 숫자여야 합니다.');
+    }
+
+    return round;
   },
 };
 
