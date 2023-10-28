a5f452baa723b12e00afdb88401e747317cabef7
ryan-dia <cjfdnjs158@naver.com>
2023-10-28 10:16:32 +0900

    feat(validator): 이름의 길이 유효성 체크 기능 추가

--- src/utils/validator/utils/index.js
+++ src/utils/validator/utils/index.js
@@ -1,11 +1,16 @@
-const ValidatorUtil = {
-  isDelimiter(input) {
-    const DELIMITER = ',';
-    const Reg = new RegExp(DELIMITER, 'g');
-    const result = input.match(Reg)?.length;
+import { SYSTEM } from '../../../constants/System.js';
 
-    return Boolean(result);
-  },
+export const isDelimiter = (input) => {
+  const DELIMITER = ',';
+  const Reg = new RegExp(DELIMITER, 'g');
+  const result = input.match(Reg)?.length;
+
+  return Boolean(result);
 };
 
-export default ValidatorUtil;
+export const isNameLength = (input) => {
+  const { name_length_max, name_length_min, delimiter } = SYSTEM;
+  return input.split(delimiter).every((name) => {
+    return name.length >= name_length_min && name.length <= name_length_max;
+  });
+};
