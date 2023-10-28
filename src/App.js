6d09063eee0702a9f4238e2dc5ceeffec9ebf0cf
anttiey <hanuuny@gmail.com>
2023-10-28 23:31:35 +0900

    feat: 자동차 이름 입력 기능 구현
    - 사용자에게 자동차 이름을 입력 받는 getRacingCarNameInput 함수 구현
    - 사용자 입력에 대한 유효성을 검사하는 checkRacingCarNameValidation 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { Random, Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  checkRacingCarNameValidation = (el) => {
+    return el.length < 6 ? true : false;
+  };
+
+  getRacingCarNameInput = async () => {
+    const input = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
+    );
+
+    const cars = input.split(',').map((el) => {
+      if (!this.checkRacingCarNameValidation(el)) {
+        throw new Error('[ERROR] 입력이 5자 이하가 아닙니다.');
+      }
+    });
+
+    return cars;
+  };
+
+  async play() {
+    try {
+      await this.getRacingCarNameInput();
+    } catch (err) {
+      Console.print(err.message);
+      throw err;
+    }
+  }
 }
 
 export default App;
