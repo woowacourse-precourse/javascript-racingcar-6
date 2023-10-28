fe71717442a373baa75d171b22176272acaf0060
PARK-CHANUK <doosan1224@naver.com>
2023-10-28 22:36:42 +0900

    feat(App): 게임시작 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import RacingGame from './Controller/RacingGame.js';
+import CarModel from './Model/CarModel.js';
+import ResultModel from './Model/ResultModel.js';
+
 class App {
-  async play() {}
+  async play() {
+    this.racingGame = new RacingGame(new CarModel(), new ResultModel());
+    await this.racingGame.start();
+  }
 }
 
 export default App;
