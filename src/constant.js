48a6461addab5eb416d86604bf87f65a6ae0407e
Ryong-E <think8867@gmail.com>
2023-10-28 16:23:46 +0900

    feature: 에러 메세지와 실행횟수에 관한 상수값 설정

--- src/constant.js
+++ src/constant.js
@@ -11,4 +11,16 @@ export const MESSAGE = Object.freeze({
   execution_result: '실행 결과',
 });
 
+export const ERROR_MESSAGE = Object.freeze({
+  wrong_answer: ',로 구분하여 2인 이상 이름을 입력해주세요',
+  invalid_number_of_time: '1회 이상 100회 이하의 횟수만 가능합니다',
+  only_number: '숫자만 입력이 가능합니다',
+  only_text: '문자만 입력이 가능합니다',
+});
+
+export const EXCUTION_NUMBER = Object.freeze({
+  min: 1,
+  max: 100,
+});
+
 export const DITANCE_SYMBOL = '-';
