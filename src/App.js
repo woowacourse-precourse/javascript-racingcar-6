fb949c7d6a899a6ce2dfe28c85897295ce891a2f
yu-heejin <jkgt0521@naver.com>
2023-10-28 14:42:39 +0900

    feat(App): 자동차 이름, 시도 횟수 받기 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    const carNames = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    const count = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+  }
 }
 
 export default App;
