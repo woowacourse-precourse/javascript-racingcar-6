2ea0040c4c9d7829cdfbba865cb7515b33362cbf
hongst222 <tmdxordusdo@gmail.com>
2023-10-28 10:24:36 +0900

    feat: 여러 테스트 케이스 추가
    Racing 클래스에 대한 테스트 케이스
    사용자 입력 (도전 횟수, 자동차 이름) 에러 처리 테스트
    출력 (공동 우승, 전진 실행 결과) 테스트

--- __tests__/RacingTest.js
+++ __tests__/RacingTest.js
@@ -1,31 +1,8 @@
-import App from "../src/App.js";
-import { MissionUtils } from "@woowacourse/mission-utils";
+
 import Car from "../src/Game/Car.js";
-const mockQuestions = (inputs) => {
-    MissionUtils.Console.readLineAsync = jest.fn();
 
-    MissionUtils.Console.readLineAsync.mockImplementation(() => {
-        const input = inputs.shift();
-        return Promise.resolve(input);
-    });
-};
 
-describe("이름 입력 테스트", () => {
-    test.each([
-        [["pobi,pobi"]],
-        [["pobi, pobi"]]
-    ])("이름 중복에 대한 예외 처리", async (inputs) => {
-        // given
-        mockQuestions(inputs);
 
-        // when
-        const app = new App();
-
-        // then
-        await expect(app.play()).rejects.toThrow("[ERROR]");
-    });
-    
-});
 
 describe("Racing 클래스 테스트", () => {
     
