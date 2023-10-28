5170abee0fe74967eeef81c8178cce5b38984631
kimdaye77 <kimdaye77@naver.com>
2023-10-28 14:09:27 +0900

    feat: 이름에 대한 예외 처리

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -11,17 +11,30 @@ class RacingGame {
     const carNamesInput = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
-    //TODO: 예외사항
     const carNames = carNamesInput.split(",");
-    carNames.forEach((car) => {
-      car = new Car(car);
-      this.cars.push(car);
+
+    const isValidCarName = (name) => {
+      return name.trim().length <= 5;
+    };
+
+    carNames.forEach((carName) => {
+      try {
+        if (!isValidCarName(carName)) {
+          throw new Error(
+            `[ERROR] 자동차 이름 "${carName}"은(는) 5자 이하여야 합니다.`
+          );
+        }
+        const car = new Car(carName);
+        this.cars.push(car);
+      } catch (error) {
+        throw error;
+      }
     });
+
     const tryCountInput = await Console.readLineAsync(
       "시도할 횟수는 몇 회인가요?\n"
     );
     this.tryCount = Number(tryCountInput);
-    //TODO: 예외사항
   }
 }
 
