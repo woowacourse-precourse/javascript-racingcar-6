9f0d2e30d970438de10f83dbc739e687df2d10f4
innerstella <suji0509@naver.com>
2023-10-28 19:18:20 +0900

    feat : 1. 게임 시작 1) 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    Console.print(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+  }
 }
 
 export default App;
+
+const game = new App();
+game.play();
