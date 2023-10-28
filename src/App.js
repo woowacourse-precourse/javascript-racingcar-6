d669f581decff349a9db2f0f9e9fdf91604a26d7
Hyunji Seok <alleyji126@gmail.com>
2023-10-28 22:04:43 +0900

    feat: enter car names and race times

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App {
   goTimes = {};
 
   async play() {
-    
+    const carNames = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) \n");
+    this.raceTimes = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요? \n");
   }
 
   async raceCars() {
