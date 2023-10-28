1b42b6b6b485683ed0b5f5b2042aad721f957657
rkdcodus <rcd7275@naver.com>
2023-10-28 22:28:48 +0900

    feat: 시도할 횟수 입력 받기

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { Random, Console } from '@woowacourse/mission-utils';
 class App {
   constructor() {
     this.carList = [];
+    this.times = 0;
   }
 
-  async createCar() {
+  async userInputCarAndTimes() {
     const inputCar = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
     );
@@ -16,10 +17,12 @@ class App {
         distance: 0,
       });
     }
+    const inputTimes = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    this.times = inputTimes;
   }
 
   async play() {
-    this.createCar();
+    this.userInputCarAndTimes();
   }
 }
 
