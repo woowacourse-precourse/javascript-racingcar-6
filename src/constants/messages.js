04ea6b8fc928a944638e37947c71e79651cc54a4
ReturnReason <reasonz@daum.net>
2023-10-29 02:49:30 +0900

    refactor(messages): 자동차의 이름이 잘못된 경우 메시지 조금 더 친절하게 알려주도록 수정

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -10,7 +10,7 @@ const ERROR_PREFIX = '[ERROR]';
 export const ERROR_MESSAGE = Object.freeze({
   DUPLICATION: `${ERROR_PREFIX} 중복된 자동차의 이름은 입력할 수 없습니다.\n`,
   NUMBER_OF_CHARACTERS: `${ERROR_PREFIX} 각 자동차의 이름은 최소 글자 1글자 이상, 최대 5글자까지 입력할 수 있습니다.\n`,
-  INVALID_NAME: `${ERROR_PREFIX} 자동차의 이름은 영문으로만 입력할 수 있습니다.\n`,
+  INVALID_NAME: `${ERROR_PREFIX} 잘못된 이름! 자동차의 이름은 영문으로만 입력할 수 있으며 쉼표(,)로 구분됩니다.\n`,
   PLAYER_COUNT: `${ERROR_PREFIX} 경주할 자동차는 최소 2대 이상, 최대 10대까지 입력해야 합니다.\n`,
   INVALID_MOVE_COUNT: `${ERROR_PREFIX} 이동할 수 있는 횟수는 최소 1회 ~ 최대 10회로 설정해야 합니다.\n`,
   NOT_NUMBER: `${ERROR_PREFIX} 숫자만 입력할 수 있습니다.`,
