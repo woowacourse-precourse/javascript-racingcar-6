53280d2128fe2b402b49554137ba8020648cc469
k-redstone <che0618@naver.com>
2023-10-28 20:47:53 +0900

    refactor: 유효성검사 함수에서 reduce를 forEach로 변경

--- src/racingGame/utils/validation.js
+++ src/racingGame/utils/validation.js
@@ -18,14 +18,15 @@ export const checkInputCarNameValidation = (inputs) => {
 
   if (inputs.length > GMAEVALIDATION.max_carlist_length)
     throw new Error(ERRORMSG.invalid_carlist_length);
-  inputs.reduce((acc, cur) => {
-    if (cur.length > GMAEVALIDATION.max_carname_length)
+
+  inputs.forEach((input) => {
+    if (input.length > GMAEVALIDATION.max_carname_length)
       throw new Error(ERRORMSG.invalid_carname_length);
-    if (cur.includes(" ")) throw new Error(ERRORMSG.invalid_contain_gap);
-    if (cur === "") throw new Error(ERRORMSG.invalid_contain_not);
-    if (GMAEVALIDATION.check_special_characters.test(cur))
+    if (input.includes(" ")) throw new Error(ERRORMSG.invalid_contain_gap);
+    if (input === "") throw new Error(ERRORMSG.invalid_contain_not);
+    if (GMAEVALIDATION.check_special_characters.test(input))
       throw new Error(ERRORMSG.invalid_special_character);
-    if (GMAEVALIDATION.check_korean_characters.test(cur))
+    if (GMAEVALIDATION.check_korean_characters.test(input))
       throw new Error(ERRORMSG.invalid_korean_character);
   }, 0);
 };
