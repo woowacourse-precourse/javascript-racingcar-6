bddbd5b41af695dbbf3d60b1122f0b9e9c2a39a2
seoyoung927 <seoyoung0000@naver.com>
2023-10-28 13:15:13 +0900

    feat: 자동차 이름 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  constructor() {}
+
+  /**
+   * 경주할 자동차 이름을 입력받아 쉼표 단위로 구분하여 반환
+   * @returns 자동차 이름을 담은 배열
+   */
+  async getInputCarNames() {
+    const input = await MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    const carNames = input.split(",");
+
+    //에러처리
+    carNames.forEach((carName) => {
+      if (carName.length > 5)
+        throw new Error("[ERROR] 자동차 이름은 5자이하여야 합니다.");
+    });
+
+    return carNames;
+  }
+
+  async play() {
+    const carNames = await this.getInputCarNames();
+  }
 }
 
 export default App;
