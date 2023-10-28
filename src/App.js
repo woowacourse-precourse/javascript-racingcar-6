0aa1035c5b310ca1f16887010d44673d51b45be6
osohyun0224 <osohyun0224@naver.com>
2023-10-29 00:04:52 +0900

    feat(App): 사용자로부터 자동차의 이름을 입력받는 기능 구현
    - 사용자에게 자동차의 이름을 입력받을 수 있도록 안내 메세지를 출력할 수 있도록 구현하였습니다.
    - 입력받은 자동차 이름을 carNames 변수에 저장할 수 있도록 구현하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Console } from "@woowacourse/mission-utils";
+import { GAME_MESSAGE } from "./constants/Message";
+
 class App {
-  async play() {}
+  async play() {
+    Console.print(GAME_MESSAGE.inputName);
+    const carNames = await Console.readLineAsync("");
+  }
 }
 
 export default App;
