4b73def4611628e5e6b3a85b637fcc86268cdaff
minSsan <msp1125@hanyang.ac.kr>
2023-10-28 21:25:12 +0900

    feat: 이동거리 출력 메소드(print_racing) 구현

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App {
     Console.print("시도할 횟수는 몇 회인가요?");
     const INPUT_TRY = await Console.readLineAsync("");
 
-    if (isNaN(INPUT_TRY) || parseInt(INPUT_TRY) <= 0) {
+    if (isNaN(INPUT_TRY) || parseInt(INPUT_TRY) < 0) {
       throw new Error(ERROR_MSG.WRONG_NUMBER);
     }
 
@@ -65,9 +65,13 @@ class App {
   race() {}
 
   /**
-   * 차수별 실행 결과를 출력하는 메소드
+   * 현재까지의 자동차 이동 거리를 출력하는 메소드
    */
-  print_racing() {}
+  print_racing() {
+    this.#RACE_CARS.forEach((car) =>
+      Console.print(`${car.name} : ${"-".repeat(car.distance)}`)
+    );
+  }
 
   /**
    * 최종 우승자를 판별하고 이를 출력하는 메소드
