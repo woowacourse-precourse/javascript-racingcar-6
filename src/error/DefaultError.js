d487930dd59bc423230d4389373ccb76085e7955
swarvy <swarvy0826@naver.com>
2023-10-29 00:04:15 +0900

    refactor : ERROR_PREFIX를 DefaultError 내부에서 관리하도록 수정

--- src/error/DefaultError.js
+++ src/error/DefaultError.js
@@ -1,7 +1,9 @@
 export default class DefaultError extends Error {
+  static ERROR_PREFIX = '[ERROR]';
+
   constructor(message) {
     super(message);
-    this.message = `[ERROR] ${message}`;
+    this.message = `${DefaultError.ERROR_PREFIX} ${message}`;
     this.name = this.constructor.name;
   }
 }
