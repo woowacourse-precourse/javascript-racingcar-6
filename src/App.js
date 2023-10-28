464febb18f707cdf93ce16e1796bd59143e9b8bf
Hyebin Lee <qnd1215@naver.com>
2023-10-28 11:57:19 +0900

    예외 사항 수정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { MissionUtils } from "@woowacourse/mission-utils"; //이거 왜 없다고 하냐..?
+import { Console, MissionUtils } from "@woowacourse/mission-utils"; //이거 왜 없다고 하냐..?
 
 class App {
   async play() {
@@ -7,14 +7,15 @@ class App {
   }
 
   async getCarName() {
-    const carsName = await Console.readLineAsync(`경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)`);
-    let carNameArry = String.carS.split(','); // 배열형태
-    
+    const carsName = await MissionUtils.Console.readLineAsync(`경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)`);
+
+    let carNameArry = String(carsName).split(','); // 배열형태
+    MissionUtils.Console.print(carNameArry);
     return carNameArry;
   }
 
-  async getCount() {
-    const HOW_MANY_MOVE = await Console.readLineAsync(`시도할 횟수는 몇 회인가요?`);
+  async getCount() {ß
+    const HOW_MANY_MOVE = await MissionUtils.Console.readLineAsync(`시도할 횟수는 몇 회인가요?`);
 
     return Number(HOW_MANY_MOVE); // 유효성 검사할 때 NaN면 throw ㄱㄱ
   }
@@ -24,5 +25,5 @@ class App {
 }
 
 //for test
-new App().playj();
+new App().play();
 export default App;
