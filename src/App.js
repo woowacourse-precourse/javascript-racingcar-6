ed8a9f54c0da56e35e1d83a5776326c840f2fe92
donggu <zzzjdg@naver.com>
2023-10-28 14:16:16 +0900

    파싱 함수 추가

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
 
+  parsingInput(input, array) {
+    const names = input.split(',');
+    for (let name = 0; name < names.length; name++) {
+      array.push(new Car(names[name]));
+    }
+  }
   
   async play() {
-   
+    const CARS = [];
+    const USERINPUT = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    this.parsingInput(USERINPUT, CARS);
+
   }
 }
 
