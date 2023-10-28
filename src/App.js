0a9a88abc4dc2c2d7ec60b8037d1181058b24067
Hyebin Lee <qnd1215@naver.com>
2023-10-28 23:48:36 +0900

    이름 길이가 1미만, 5초과인경우 에러

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import { Console, MissionUtils } from "@woowacourse/mission-utils"; //이거 왜
 
 class App {
   async play() {
-    this.getCount();
+    let cars = await this.getCarName();
+    this.validCheckAboutName(cars);
 
   }
 
   async getCarName() {
     const carsName = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
     let carNameArry = String(carsName).split(','); // 배열형태
-    MissionUtils.Console.print(carNameArry);
+    //MissionUtils.Console.print(carNameArry);
     return carNameArry;
   }
 
@@ -20,19 +21,38 @@ class App {
   }
 
 
-  validCheckAboutName() {
-
+  validCheckAboutName(carNameArry) {
+    if (!this.checkDupl(carNameArry)) {
+      throw new Error(`[ERROR] 중복된 이름입니다.`);
+    }
+    if(!this.checkNamelength(carNameArry)) {
+      throw new Error(`[ERROR] 유효하지 않은 이름 길이 입니다.`);
+    }
   }
 
-  removeEmptyInput(input) { // input안에 getCarName() return 값 들어감.
-      for(let i = 0; i < input.length; i++) {
-        let removeEmpty = input[i].trim();
-      }
-  }//일단 이렇게 하긴 했는데 이걸 forEach로 하고 싶음!
+  // removeEmptyInput(input) { // input안에 getCarName() return 값 들어감.
+  //     for(let i = 0; i < input.length; i++) {
+  //       let removeEmpty = input[i].trim();
+  //     }
+  // }//일단 이렇게 하긴 했는데 이걸 forEach로 하고 싶음!
+  
+  checkDupl(carNameArry) {
+    let carNameSet = new Set(carNameArry);
+    if (carNameArry.length !== carNameSet.size) {
+      return false;
+    }
+    return true;
+  }
 
-
+  checkNamelength(carNameArry) {
+    for(let i = 0; i < carNameArry.length; i++) {
+      Console.print(carNameArry[i].length);
+      if(carNameArry[i].length < 1 || carNameArry[i].length > 5) {
+        return false;
+      } 
+    }return true;
+  }
 }
-
 //for test
 new App().play();
 export default App;
