e08026e97d9a2164b506ebcaa3df3ff51b0b4a85
김영은 <jessicak0828@gmail.com>
2023-10-28 19:21:45 +0900

    chore: export 키워드 추가

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,14 @@
-const INPUT_REQUEST = {
+export const INPUT_REQUEST = {
 	NAME: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
 	FREQUENCY: '시도할 횟수는 몇 회인가요?',
 };
 
-const RESULT = {
+export const RESULT = {
 	GAME_PROGRESS: '실행 결과',
 	FINAL_WINNER: '최종 우승자 : ',
 };
 
-const ERROR = {
+export const ERROR = {
 	NAME: '[ERROR]',
 	NO_INPUT: '값을 입력해주세요.',
 	NO_ALONE: '두 명 이상을 입력해주세요.',
@@ -17,7 +17,7 @@ const ERROR = {
 	NO_ZERO:'1 이상을 입력해주세요.'
 };
 
-const REG_EXP = {
+export const REG_EXP = {
   SPECIAL_CHARACTER: "/[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/",
   STRING_CHARACTER: "/[a-zA-Z가-힣]/"
 }
