563a55df10d68c06fe77669535b0628515d22b30
uraflower <82873315+uraflower@users.noreply.github.com>
2023-10-29 02:34:03 +0900

    feat: 자동차 이름 입력받는 기능 추가

--- src/GameManager.js
+++ src/GameManager.js
@@ -1,6 +1,11 @@
 import { Console } from '@woowacourse/mission-utils';
+import { INPUT_CARS } from './constants/messages.js';
 
 class GameManager {
+  async recruitCars() {
+    const input = await Console.readLineAsync(INPUT_CARS);
+  }
+
   announceInterimResult() {
     const cars = [{ name: 'pobi', movement: '-' }];
     cars.forEach((car) => {
