3f6faa3e5af2debdb8359f7a7cc0ef8b21fb456b
yong seock <myongdol132@gmail.com>
2023-10-29 01:57:35 +0900

    Feat(자동차 이름 입력) 기능구현
    참가하는 자동차들의 이름을 입력받고 쉼표로 구분을 하고 차동차의  이름
    길이를 제한 합니다Feat(자동차 이름 입력) 기능구현
    
    참가하는 자동차들의 이름을 입력받고 쉼표로 구분을 하고 차동차의  이름
    길이를 제한 합니다..

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const names = await this.getInputCarNames();
+  }
+
+  async getInputCarNames() {
+    const names = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+    if(!this.isValidNames(names)) {
+      throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능하고, 쉼표로 구분해야 합니다.");
+    }
+    return names.split(",");
+  }
+
+  isValidNames(names) {
+    const nameArray = names.split(",");
+    
+    return nameArray.every(name => name.length > 0 && name.length <= 5);
+  }
 }
 
 
