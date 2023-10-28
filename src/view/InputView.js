630d0423f38aa28241eded7f9fbd381dbff19db1
swarvy <swarvy0826@naver.com>
2023-10-28 23:50:51 +0900

    feat : 입력데이터 유효성 검사 적용

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,16 +1,19 @@
 import { Console } from '@woowacourse/mission-utils';
 import { GRANDPRIX_START_NOTIFICATION } from '../constants/GrandPrixMessage.js';
+import { LapNumberValidator, RacingCarNameValidator } from '../validator/index.js';
 
 const InputView = Object.freeze({
   async readRacingCarNames() {
     const racingCarInput = await Console.readLineAsync(
       GRANDPRIX_START_NOTIFICATION.enterRacingCarName,
     );
+    RacingCarNameValidator.generate(racingCarInput).validateRacingCarName();
     return racingCarInput;
   },
 
   async readLapNumber() {
     const lapNumberInput = await Console.readLineAsync(GRANDPRIX_START_NOTIFICATION.enterLapNumber);
+    LapNumberValidator.generate(lapNumberInput).validateLapNumber();
     return lapNumberInput;
   },
 });
