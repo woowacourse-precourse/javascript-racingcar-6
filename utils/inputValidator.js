65e0e28e9ce33cc076f03385201dd034c58cc17b
okxooxoo <okxooxoo@naver.com>
2023-10-28 23:37:51 +0900

    ✨ feat: 시도할 횟수 입력받고 유효성 검사 기능 추가

--- utils/inputValidator.js
+++ utils/inputValidator.js
@@ -4,3 +4,10 @@ export const validateCarName = (carName) => {
   if (!regex.test(carName))
     throw new Error("[ERROR] 자동차 이름이 잘못된 형식입니다.");
 };
+
+export const validateTryNumber = (tryNumber) => {
+  const regex = new RegExp(/^[1-9]\d*$/); // 1 이상의 정수
+
+  if (!regex.test(tryNumber))
+    throw new Error("[ERROR] 시도할 횟수는 1 이상의 정수입니다.");
+};
--- view/inputView.js
+++ view/inputView.js
@@ -1,13 +1,21 @@
 import { Console } from '@woowacourse/mission-utils';
-import { splitByComma } from '../utils/splitByComma';
-import { validateCarName } from '../utils/inputValidator'
+import { splitByComma } from '../utils/splitByComma.js';
+import { validateCarName, validateTryNumber } from '../utils/inputValidator.js';
 
 class InputView {
+  
   async getCarName() {
     const carInput = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     validateCarName(carInput);
     const carNames = splitByComma(carInput);
     return carNames;
+  }
+
+  async getTryNumber() {
+    const tryInput = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+    validateTryNumber(tryInput);
+    const tryNumber = +tryInput;
+    return tryNumber;
   }
 }
 
