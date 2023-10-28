162a255920f487ddb05c65e356273eccfccd23f9
jiyun1006 <jiyun4251@gmail.com>
2023-10-28 17:35:16 +0900

    ✨ feat : 자동차 이름, 게임 실행 횟수 입력 메서드 및 유효성 검사 메서드 추가
    inputCarName, inputPlayNumber와 같은 입력 메서드와
    validateCarName, validatePlayNumber 와 같은 입력에 대한 유효성 검사 메서드 작성.
    
    ex) Fixes : #45 Related to : #34, #23

--- src/App.js
+++ src/App.js
@@ -1,5 +1,50 @@
+import { MissionUtils, Console } from "@woowacourse/mission-utils";
+
+
+// 상수
+const INPUT_CAR_NAME = '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n';
+const INPUT_TRY_NUMBER = '시도할 횟수는 몇 회인가요?\n';
+
+// 유효성 검사 정규표현식
+const regFirstForm = /^[가-힣a-zA-Z]{1}[가-힣a-zA-Z,]{1,}[가-힣a-zA-Z]{1}$/g;
+const regNumber = /^[0-9]$/g;
+
+
 class App {
-  async play() {}
+  async play() {
+    const carName = await this.inputCarName();
+    this.validateCarName(carName);
+    const playNumber = await this.inputPlayNumber();
+    this.validatePlayNumber(playNumber);
+   }
+  
+  // 차 이름 입력받는 메서드
+  async inputCarName() {
+    const carName = await Console.readLineAsync(`${INPUT_CAR_NAME}`);
+    return carName;
+  }
+
+  // inputCarName에 대한 유효성 검사
+  validateCarName(name) {
+    if (!regFirstForm.test(name)) throw new Error('[ERROR] 이름 규칙에 알맞지 않은 문자가 포함되어 있습니다.');
+
+    const nameSplitArr = name.split(",");
+    const nameSplitArrLen = nameSplitArr.length;
+    for (let idx = 0; idx < nameSplitArrLen; idx++){
+      if (!nameSplitArr[idx]) throw new Error('[ERROR] 이름이 빈 공간입니다.');
+    }
+  }
+
+  async inputPlayNumber() {
+    const playNumber = await Console.readLineAsync(`${INPUT_TRY_NUMBER}`);
+    return playNumber;
+  }
+
+  validatePlayNumber(number) {
+    if (isNaN(number)) throw new Error('[ERROR] 숫자만 입력가능합니다.');
+  }
 }
+
+
 
 export default App;
