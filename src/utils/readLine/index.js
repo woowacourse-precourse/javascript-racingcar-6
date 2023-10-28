2d1a9f0ace5df72f6ac8809220615f8346792ff1
jang_yoonsu <shagrat8546@gmail.com>
2023-10-28 13:40:55 +0900

    fix : readLine Async누락 수정

--- src/utils/readLine/index.js
+++ src/utils/readLine/index.js
@@ -1,5 +1,5 @@
 import { Console } from "@woowacourse/mission-utils";
 
 export async function readLine(question) {
-  return await Console.readLine(question);
+  return await Console.readLineAsync(question);
 }
