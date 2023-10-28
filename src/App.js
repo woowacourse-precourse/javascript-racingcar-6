e516c83f0fc6b185714e3289f23187a90ae4eec9
yu-heejin <jkgt0521@naver.com>
2023-10-28 15:24:01 +0900

    feat(App.js): 랜덤 숫자에 따른 게임 결과 생성 기능 추가

--- src/App.js
+++ src/App.js
@@ -7,12 +7,22 @@ class App {
 
     // 자동차 이름 분리하기
     const carNames = inputCarName.split(',');
+    const gameResults = [];
 
     for (let i = 0; i < count; i++) {
       // 랜덤 숫자 생성
       const randomNumbers = [];
       for (let i = 0; i < carNames.length; i++) {
         randomNumbers[i] = await MissionUtils.Random.pickNumberInRange(0, 9);
+      }
+
+      // 랜덤 숫자에 맞춰 - 문자열 생성
+      for (let i = 0; i < carNames.length; i++) {
+        const randomNumber = randomNumbers[i];
+        if (randomNumber > 4) {
+          const result = `${gameResults[i] ? gameResults[i] : ''}-`;
+          gameResults[i] = result;
+        }
       }
     }
   }
