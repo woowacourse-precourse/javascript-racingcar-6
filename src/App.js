bd8239e47846ca5f9132afd6c260c01dfe2b1b76
aaminha <dks4857@gmail.com>
2023-10-28 20:13:04 +0900

    feat(게임 시작): 자동차 이름 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
+import { Console } from '@woowacourse/mission-utils';
+import { MESSAGE } from './constants/message';
+
 class App {
+  constructor() {
+    this.carList = [];
+  }
+
+  async getCarName() {
+    const inputCarName = await Console.readLineAsync(MESSAGE.getCarName);
+    const carList = inputCarName.split(',');
+
+    // TODO : 자동차 이름 유효검사
+
+    this.carList = carList;
+  }
+
   async play() {}
 }
 
