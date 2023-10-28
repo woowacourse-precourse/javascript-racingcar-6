7c8f247eb6d58e592801af6e4d6f87c870afbf06
yunsoeirm1116 <hhyjs2r@gmail.com>
2023-10-28 23:50:35 +0900

    [refactor] car model 이름 변경 CarModel-> Car

--- src/models/Car.js
+++ src/models/Car.js
@@ -1,4 +1,4 @@
-class CarModel {
+class Car {
   constructor(carName) {
     this.carName = carName;
     this.position = '';
@@ -7,13 +7,13 @@ class CarModel {
   move() {
     const randomNumber = Math.floor(Math.random() * 10);
     if (randomNumber >= 4) {
-      this.position += '-';
+      this.setPosition('-');
     }
   }
 
   setPosition(position) {
-    this.position = position;
+    this.position += position;
   }
 }
 
-export default CarModel;
+export default Car;
