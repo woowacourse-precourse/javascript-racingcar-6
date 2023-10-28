fe6abbe1893714ca9e9564447f8b26f8b06769a4
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-10-28 12:17:15 +0900

    :white_check_mark: test: 입력값 검증 테스트에서 각 상황에 맞는 에러메세지를 띄우는지 테스트함

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -1,24 +1,29 @@
 import InputValidator from '../src/domain/InputValidator';
 
 describe('InputValidator', () => {
-  const inValidCountCars = [0, 1];
-  const invalidCarNames = ['아메리칸드림', ''];
-  const invalidRounds = ['0', '-1', '', '10a', 'a10', 'abc', '!'];
-  const invalidUniqueName = ['자동차', '슈퍼카', '자동차'];
+  const invalidInputs = {
+    carNames: ['아메리칸드림', ''],
+    rounds: ['0', '-1', '', '10a', 'a10', 'abc', '!'],
+  };
 
-  test.each(inValidCountCars)('입력한 자동차의 이름이 2개 이상인지 테스트한다.', (count) => {
-    expect(InputValidator.hasValidCountCars(count)).toBe(false);
-  });
-
-  test.each(invalidCarNames)('자동차 이름이 1글자 이상 5글자 이하인지 테스트한다.', (carName) => {
-    expect(InputValidator.hasValidCarName(carName)).toBe(false);
-  });
+  test.each(invalidInputs.carNames)(
+    '자동차 이름이 1글자 이상 5글자 이하인지 테스트한다.',
+    (carName) => {
+      expect(() => InputValidator.hasValidCarNames([carName])).toThrowError(
+        '[ERROR] 이름은 1글자 이상 5자 이하여야 합니다.'
+      );
+    }
+  );
 
   test('중복되는 이름이 있는지 테스트한다.', () => {
-    expect(InputValidator.hasValidUniqueName(invalidUniqueName)).toBe(false);
+    expect(() => InputValidator.hasValidCarNames(['자동차', '슈퍼카', '자동차'])).toThrowError(
+      '[ERROR] 중복되는 이름이 존재합니다.'
+    );
   });
 
-  test.each(invalidRounds)('시도할 횟수가 1이상인 숫자인지 테스트한다.', (round) => {
-    expect(InputValidator.hasValidRoundNumber(round)).toBe(false);
+  test.each(invalidInputs.rounds)('시도할 횟수가 1이상인 숫자인지 테스트한다.', (round) => {
+    expect(() => InputValidator.hasValidRound(round)).toThrowError(
+      '[ERROR] 최소 1이상의 숫자여야 합니다.'
+    );
   });
 });
