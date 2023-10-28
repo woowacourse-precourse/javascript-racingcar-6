f1493654663ef3fb4b49ba0f8f0a5705d08f2afc
swarvy <swarvy0826@naver.com>
2023-10-28 23:47:08 +0900

    refactor : ERROR_PREFIX 내용 삭제로 인한 DefaultError 내용 수정

--- src/error/DefaultError.js
+++ src/error/DefaultError.js
@@ -1,9 +1,7 @@
-import { ERROR_PREFIX } from '../constants/GrandPrixMessage.js';
-
 export default class DefaultError extends Error {
   constructor(message) {
     super(message);
-    this.message = `${ERROR_PREFIX} ${message}`;
+    this.message = `[ERROR] ${message}`;
     this.name = this.constructor.name;
   }
 }
