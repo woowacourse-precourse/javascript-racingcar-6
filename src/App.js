37b48d3e0178dc721bf5675b7e99f52c1dff8ab0
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-10-28 17:05:33 +0900

    refactor: 함수 App 클래스 메서드로 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import checkCarNames from "./checkCarNames";
-import inputCarNames from "./inputCarNames";
-import inputCount from "./inputCount";
+import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    const carNames = await inputCarNames();
-    checkCarNames(carNames);
-    const count = await inputCount();
+    const carNames = await this.inputCarNames();
+    this.checkCarNames(carNames);
+    const countCars = carNames.length;
+    const count = await this.inputCount();
+  }
+
+  async inputCarNames() {
+    const carInput = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+    let carNames = [];
+
+    if (typeof carInput === "string") {
+      carNames = carInput.split(",");
+    } else {
+      throw new Error("[ERROR] : 문자만 입력해 주세요.");
+    }
+
+    return carNames;
+  }
+
+  checkCarNames(carNames) {
+    carNames.forEach((car) => {
+      if (car.length > 5) {
+        throw new Error("[ERROR] : 자동차 이름은 5자 이하만 입력 가능합니다.");
+      }
+    });
+    return null;
+  }
+
+  async inputCount() {
+    let count = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+    count = Number(count);
+
+    if (Object.is(count, NaN)) {
+      throw new Error("[ERROR] : 숫자를 입력해주세요.");
+    }
+
+    return count;
   }
 }
 
--- src/checkCarNames.js
+++
@@ -1,8 +0,0 @@
-export default function checkCarNames(carNames) {
-  carNames.forEach((car) => {
-    if (car.length > 5) {
-      throw new Error("[ERROR] : 자동차 이름은 5자 이하만 입력 가능합니다.");
-    }
-  });
-  return null;
-}
