16ded0cd8577a070ef39374d6522e5faa9b64030
uraflower <82873315+uraflower@users.noreply.github.com>
2023-10-29 02:53:32 +0900

    test: 자동차 전진/정지 출력 테스트에 입출력 추가
    `mockQuestions`와 `mockRandoms`는 `ApplicationTest.js`에서 가져옴.

--- __tests__/GameManager.test.js
+++ __tests__/GameManager.test.js
@@ -1,5 +1,23 @@
-import { Console } from '@woowacourse/mission-utils';
+import { Console, Random } from '@woowacourse/mission-utils';
 import GameManager from '../src/GameManager.js';
+import { MAX_RANGE, MIN_RANGE } from '../src/constants/constants.js';
+
+const mockQuestions = (inputs) => {
+  Console.readLineAsync = jest.fn();
+
+  Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  });
+};
+
+const mockRandoms = (numbers) => {
+  Random.pickNumberInRange = jest.fn();
+  numbers.reduce(
+    (acc, number) => acc.mockReturnValueOnce(number),
+    Random.pickNumberInRange,
+  );
+};
 
 const getLogSpy = () => {
   const logSpy = jest.spyOn(Console, 'print');
@@ -7,11 +25,24 @@ const getLogSpy = () => {
   return logSpy;
 };
 
-test('자동차의 전진/정지 결과를 받아서, 형식에 맞게 출력한다', () => {
+test('자동차의 전진/정지 결과를 받아서, 형식에 맞게 출력한다', async () => {
   const logSpy = getLogSpy();
   const gameManager = new GameManager();
-  const expected = 'pobi : -';
+  const GO = MAX_RANGE;
+  const STOP = MIN_RANGE;
+  const inputs = ['pobi,woni,jun', '1'];
+  const randoms = [GO, STOP, GO];
+  const expected = ['pobi : -', 'woni : ', 'jun : -'];
+
+  mockQuestions(inputs);
+  mockRandoms([...randoms]);
+
+  await gameManager.recruitCars();
+  await gameManager.inputNumberOfAttempt();
   gameManager.announceInterimResult();
-  expect(logSpy).toHaveBeenCalledWith(expect.stringMatching(expected));
+
+  expected.forEach((output) => {
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+  });
 });
 
