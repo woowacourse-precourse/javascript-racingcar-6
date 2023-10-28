2dec9102b922d6a967c5844f0b8ea2b2e3bc7700
BangDori <qudwns8865@naver.com>
2023-10-28 15:06:18 +0900

    feat: 입력받은 시도 횟수가 숫자인지 검증하는 함수 추가

--- src/util/attempt-validation.js
+++ src/util/attempt-validation.js
@@ -1,5 +1,6 @@
 const ERROR_MESSAGE = Object.freeze({
   LENGTH: "[ERROR] 시도 횟수를 입력하지 않으셨습니다.",
+  NOT_NUMBER: "[ERROR] 시도 횟수는 숫자만 입력 가능합니다.",
 });
 
 /**
@@ -17,12 +18,27 @@ function isLengthError(inputAttemptCount) {
 }
 
 /**
+ * 시도 횟수가 숫자인지 검증하는 함수
+ * @param {string} inputAttemptCount 시도 횟수
+ * @throws 시도 횟수의 길이가 0이라면 에러를 던진다.
+ * @returns
+ */
+function isNumeric(inputAttemptCount) {
+  if (isNaN(Number(inputAttemptCount))) {
+    throw new Error(ERROR_MESSAGE.NOT_NUMBER);
+  }
+
+  return;
+}
+
+/**
  * 입력받은 시도 횟수를 검증하는 함수
  * @param {string} inputAttemptCount 입력받은 검증 횟수
  * @returns {integer} 정수 형태의 검증 횟수
  */
 function validateAttemptCount(inputAttemptCount) {
   isLengthError(inputAttemptCount);
+  isNumeric(inputAttemptCount);
 
   return parseInt(inputAttemptCount);
 }
