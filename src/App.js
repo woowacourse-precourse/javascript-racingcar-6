bbcd798ca9e9b0542de1c58d7c109df20194aabc
BangDori <qudwns8865@naver.com>
2023-10-28 14:14:54 +0900

    feat: 자동차의 이름을 입력받기 위한 안내 메시지를 출력하고 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console } from "@woowacourse/mission-utils";
+
+import INPUT_MESSAGE from "./constants/game.js";
+
 class App {
-  async play() {}
+  async play() {
+    const inputRacingCars = await Console.readLineAsync(
+      INPUT_MESSAGE.RACING_CARS
+    );
+  }
 }
 
 export default App;
