6f5527aae5cd763c05e8c9a6f9423464c76a53a2
Seo San <dannysir@naver.com>
2023-10-28 16:40:52 +0900

    feat : 중복된 이름 예외 처리 중복된 이름을 입력할 경우 Set 의 size를 이용해 원래 배열과 비교하여 예외 처리 진행

--- src/InputScreen.js
+++ src/InputScreen.js
@@ -4,6 +4,8 @@ export const Input_First = async () => {
     const FIRST_Q = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     const NAMES = (await MissionUtils.Console.readLineAsync(FIRST_Q));
     const NAME_ARR = NAMES.split(',');
+    const SET_NAME = new Set(NAME_ARR);
+    if (SET_NAME.size !== NAME_ARR.length) throw new Error("[ERROR] 중복된 이름을 입력")
     NAME_ARR.forEach(v => {
         if (v.length > 5) throw new Error("[ERROR] 이름 숫자는 5이하만 가능합니다.")
     })
@@ -12,5 +14,7 @@ export const Input_First = async () => {
 
 export const Input_Second = async () => {
     const SECOND_Q = "시도할 횟수는 몇 회인가요?\n"
-    return await MissionUtils.Console.readLineAsync(SECOND_Q);
+    const COUNT = await MissionUtils.Console.readLineAsync(SECOND_Q);
+    if (isNaN(COUNT)) throw new Error("[ERROR] 숫자만 입력해주세요");
+    return COUNT;
 };
