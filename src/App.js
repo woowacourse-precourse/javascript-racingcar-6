8cf5f058a84448cb1370c5b0ef093b7cfc1ff0c9
Hyunji Seok <alleyji126@gmail.com>
2023-10-28 22:34:49 +0900

    feat: call handleErrors method and raceCars method

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ class App {
     const carNames = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) \n");
     this.cars = carNames.split(",");
     this.raceTimes = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요? \n");
+  
+    this.handleErrors(this.cars, this.raceTimes);
+    this.raceCars();
   }
 
   async raceCars() {
