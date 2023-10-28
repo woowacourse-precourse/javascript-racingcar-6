b04f0d976cfd3c655ef0550e8910878267a74da5
Boeun <boeun0904@naver.com>
2023-10-29 00:23:15 +0900

    refactor(Computer.js): move input error check logic move input error check logic from User class to Computer class

--- src/Computer.js
+++ src/Computer.js
@@ -8,10 +8,19 @@ export default class Computer {
 
   playGame(user) {}
 
-  async getCarsNameArrayFromUser(user) {
-    const cars = await user.getUserInputCarsNameArray(
+  async getCarNameArrayFromUser(user) {
+    const userInput = await user.getCarNameInput(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
     );
-    this.racingCars = cars.map((car) => new RacingCar(car));
+    const carNameArr = this.getCarNameArrayFromUserInput(userInput);
+    this.racingCars = carNameArr.map((carName) => new RacingCar(carName));
+    console.log(this.racingCars);
+  }
+
+  getCarNameArrayFromUserInput(input) {
+    const carNameArr = input.replace(/ /g, '').split(',');
+    if (carNameArr.some((carName) => carName.length > 5 || carName === ''))
+      throw new Error('[ERROR] 잘못된 입력입니다.');
+    return carNameArr;
   }
 }
--- src/User.js
+++ src/User.js
@@ -1,13 +1,7 @@
 import { Console } from '@woowacourse/mission-utils';
 
 export default class User {
-  async getUserInputCarsNameArray(query) {
-    let input = await Console.readLineAsync(query);
-    input = input.replace(/ /g, '');
-    const cars = await input.split(',');
-    // console.log(cars);
-    if (cars.some((car) => car.length > 5 || car === ''))
-      throw new Error('[ERROR] 잘못된 입력입니다.');
-    return cars;
+  async getCarNameInput(query) {
+    return await Console.readLineAsync(query);
   }
 }
