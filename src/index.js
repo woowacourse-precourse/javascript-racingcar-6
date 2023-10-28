cf11b00941d60115b0de2b8a39e0fc3c18301fa2
Yoos99 <yoosh2520@naver.com>
2023-10-29 00:26:06 +0900

    Feat: "추가 사용자 입력 함수"
    사용자로부터 자동차 이름, 이동 횟수를 입력 받는다. validator.js를 import하여 에러를 체크한다.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 import App from "./App.js";
+import {isvalidCarNames, isvalidAttempt} from './validators.js';
 
-const app = new App();
-app.play();
+async function start() {
+    const app = new App();
+
+    
+    const carNamesInput = await MissionUtils.Console.readLineAsync("자동차 이름을 입력하세요 (쉼표(,)로 구분): ");
+    const carNames = carNamesInput.split(',');
+    console.log(carNames);
+    isvalidCarNames(carNames);
+
+    const attemptInput = await MissionUtils.Console.readLineAsync("몇 번의 이동을 할 것인가요?: ");
+    const attempt = parseInt(attemptInput, 10);
+    console.log(attempt);
+    isvalidAttempt(attempt);
+    // 게임 시작
+    app.play(carNames, attempt);
+}
+
+start();
