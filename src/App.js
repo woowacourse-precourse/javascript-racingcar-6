4b34b2792da2116b16a43f7d1b33fa5eb36dac91
이예지 <oyatpeach@naver.com>
2023-10-28 14:44:50 +0900

    Feat : 조건에 따른 전진, 정지 및 결과 출력
    Resolves : #3
    Related to : #2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
+  constructor() {
+    this.results = {}; // 각 자동차의 결과를 저장하는 객체
+  }
+
   async play() {
     this.gameStart();
   }
@@ -21,17 +25,52 @@ class App {
         throw new Error("숫자가 잘못된 형식입니다. 5자 이하만 가능합니다.");
       }
     });
-    this.inputTrialNum();
+    this.inputTrialNum(carArr);
   }
 
-  async inputTrialNum() {
+  async inputTrialNum(carArr) {
     Console.print("시도할 횟수는 몇 회인가요?");
-    const input = await Console.readLineAsync("");
+    const trialNum = await Console.readLineAsync("");
 
-    if (Number(isNaN(input))) {
+    if (Number(isNaN(trialNum))) {
       throw new Error("숫자가 아닙니다.");
+    }
+
+    this.racingCar(carArr, trialNum);
+  }
+
+  racingCar(carArr, trialNum) {
+    for (let i = 0; i < trialNum; i++) {
+      carArr.forEach((car) => {
+        this.calcResult(car);
+      });
+      Console.print("");
+      this.printResults();
+    }
+  }
+
+  printResults() {
+    for (const car in this.results) {
+      Console.print(`${car} : ${this.results[car]}`);
+    }
+  }
+
+  calcResult(car) {
+    const condition = this.racingCondition();
+
+    let carResult = this.results[car] || ""; // 자동차의 이전 결과 가져오기
+    if (condition === "전진") {
+      carResult += "-";
+    }
+    this.results[car] = carResult;
+  }
+
+  racingCondition() {
+    const random = Random.pickNumberInRange(0, 9);
+    if (random >= 4) {
+      return "전진";
     } else {
-      Console.print(input);
+      return "정지";
     }
   }
 }
