c3bf357be22cecc0049f411e81c19e86f5f13c02
delee3064 <delee3064@gmail.com>
2023-10-28 19:19:32 +0900

    feat : 자동차 이름 입력 및 검증

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const inputName = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    const carNames = inputName.split(",");
+    this.nameValidation(carNames);
+  }
+
+  nameValidation(carNames) {
+    for (let i = 0; i < carNames.length; i++) {
+      if (carNames[i].length > 5 || carNames[i].length < 1) {
+        throw new Error ("[ERROR] 1 ~ 5글자의 자동차 이름을 입력해주세요.")
+      }
+    }
+  }
 }
 
 export default App;
