bc44229216e41e7fe3285cd3729b4edef1a2c225
Trophy198 <spsp8426@naver.com>
2023-10-28 16:48:03 +0900

    feat : RacingGameController 클래스에서 전체적인 게임을 제어하는 메서드들을 추가

---
+++ src/controller/RacingGameController.js
@@ -0,0 +1,53 @@
+import InputView from '../view/InputView.js';
+import OutputView from '../view/OutputView.js';
+import Car from '../model/Car.js';
+import Race from '../model/Race.js';
+import InputValidator from '../utils/InputValidator.js';
+import { randomMovementStrategy } from '../model/movementStrategies.js';
+import { processCarNames } from '../utils/CarNameProcessor.js';
+
+export default class RacingGameController {
+  async start() {
+    const carNames = await this.#createCarNames();
+    const rounds = await this.#createRounds();
+    const race = this.#initRace(carNames, rounds);
+
+    this.#runRace(race, rounds);
+    this.#displayWinners(race);
+  }
+
+  async #createCarNames() {
+    const carNamesInput = await InputView.printCarNames();
+    const processedNames = processCarNames(carNamesInput);
+
+    InputValidator.validateNumberOfCars(processedNames.length);
+    processedNames.forEach((name) => InputValidator.validateCarName(name));
+
+    return processedNames;
+  }
+
+  async #createRounds() {
+    const rounds = await InputView.printNumberOfRounds();
+    InputValidator.validateNumberOfRounds(rounds);
+    return rounds;
+  }
+
+  #initRace(carNames, rounds) {
+    const cars = carNames.map((name) => new Car(name, randomMovementStrategy));
+    return new Race(cars, rounds);
+  }
+
+  #runRace(race, rounds) {
+    OutputView.printRaceHeader();
+    Array.from({ length: rounds }).forEach(() => {
+      race.playRound();
+      const roundResults = race.getRoundResults();
+      OutputView.printRoundResult(roundResults);
+    });
+  }
+
+  #displayWinners(race) {
+    const winnersString = race.getWinnersString();
+    OutputView.printWinners(winnersString);
+  }
+}
