eceee12c55a41b9a01154f24697f24aba7c65ae6
YeungJin <143728232+0calCOKE@users.noreply.github.com>
2023-10-28 19:46:56 +0900

    README 기능부문 재수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-class App {
-  async play() {}
-}
+import { MissionUtils } from "@woowacourse/mission-utils";
 
+class App {
+  constructor() {
+    this.players = [];
+    this.moveAttempts = 0;
+  }
+
+  async play() {}
+
+}
 export default App;
