d5c7fdb538c862e3925f6f18bcb8a895de9f0089
donggu <zzzjdg@naver.com>
2023-10-28 14:17:42 +0900

    입력 오류 체크

--- src/App.js
+++ src/App.js
@@ -8,12 +8,20 @@ class App {
       array.push(new Car(names[name]));
     }
   }
+
+  checkRound(input) {
+    if (/^\D+$/.test(input)) {
+      throw new Error("[ERROR]");
+    }
+  }
   
   async play() {
     const CARS = [];
     const USERINPUT = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     this.parsingInput(USERINPUT, CARS);
-
+    
+    const ROUNDS = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+    this.checkRound(ROUNDS);
   }
 }
 
