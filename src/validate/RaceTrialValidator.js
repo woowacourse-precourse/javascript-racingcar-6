b7c3419ea73a1979578a9d84d06539f2a515e4a4
osohyun0224 <osohyun0224@naver.com>
2023-10-29 01:36:53 +0900

    feat(RaceTrialValidator): 횟수 입력값에 공백 여부 유효성 검사 구현
    - RaceTrialValidator에 사용자가 시도할 횟수를 입력한 값에 공백이 있는지 유효성 검사 기능을 구현하였습니다.
    - 해당 유효성 검사에서 문제가 발생하면 에러를 던지도록 구현하였습니다.

--- src/validate/RaceTrialValidator.js
+++ src/validate/RaceTrialValidator.js
@@ -5,9 +5,15 @@ export default class RaceTrialValidator {
     if (this.isEmptyInput(trial)) {
       throw new Error(ERROR_MESSAGE.emptyInput);
     }
+    if (this.hasWhiteSpace(trial)) {
+      throw new Error(ERROR_MESSAGE.hasWhiteSpace);
+    }
   }
 
   static isEmptyInput(input) {
     return !input || input.trim().length === 0;
+  }
+  static hasWhiteSpace(trial) {
+    return trial.includes(' ');
   }
 }
