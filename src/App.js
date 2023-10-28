9e5ab5af7e00ecfb294e4bb3b8b9464a923e45fb
gon <hss3522@gmail.com>
2023-10-29 07:00:16 +0900

    feat: Add function to create car object from user input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
-  async play() {
-    Console.print("Hello World!");
+  async createCarObjectFromInput() {
+    const carObject = {};
+
+    Console.print(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+    const carInputString = await Console.readLineAsync("");
+    const carNames = carInputString.split(",");
+
+    carNames.forEach((name) => {
+      carObject[name] = 0;
+    });
+
+    return carObject;
+  }
+
+  play() {
+    this.createCarObjectFromInput();
   }
 }
 
