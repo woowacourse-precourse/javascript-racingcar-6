9cca5ab57107587c7b77a7472137090a4da2a49a
Doeunnkimm <doeunnkimm@gmail.com>
2023-10-28 18:50:19 +0900

    refactor: 입력 받고 레이싱 상황을 객체로 초기화하는 로직을 ready() 메서드로 분리

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import racingCarGame from './game';
 
 class App {
   async play() {
+  constructor() {
+    this.moveCount = 0;
+    this.racingStatus = [];
+  }
+
+  async ready() {
     const { carNameArray, moveCount } =
       await racingCarGame.promptToRacingObject();
-    const racingStatus = racingCarGame.makeCarsWithForwardCount(carNameArray);
+    this.moveCount = moveCount;
+    this.racingStatus = racingCarGame.makeCarsWithForwardCount(carNameArray);
+  }
+
+  async play() {
+    await this.ready();
   }
 }
 
