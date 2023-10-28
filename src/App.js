d6c6ad030699f6541835ac091ce5590f60bc50bc
jong-jang <jjh0299@gmail.com>
2023-10-28 13:09:14 +0900

    feat: 랜덤숫자 및 임포트 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+import { START_MESSAGE, LIMIT_NAME_LENGTH, COUNT_MESSAGE, RESULT_MESSAGE, GO_NUMBER } from './Const';
+import Car from './Car';
+
 class App {
-  async play() {}
+	async play() {}
+
+	// 랜덤숫자
+	randomNumber() {
+		return Random.pickNumberInRange(0, 9);
+	}
 }
 
 export default App;
