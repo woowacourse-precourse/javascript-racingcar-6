0f4d404f29ef1e3275c82ce5f060e5cabc3fb2af
전소희 <indian_potato@naver.com>
2023-10-28 23:53:42 +0900

    fix: 이름 글자수 검증
    이름 글자 수 검색 부분에서 배열의 요소가 아닌 배열을 검색해 에러가 상시로 뜨는 이슈가 있어 verifyNameLength 라는 변수에 이름 배열을 할당하고 이것은 includes로 검색해 true를 포함하고 있으면 ERROR가 발생하도록 변경

--- src/App.js
+++ src/App.js
@@ -2,29 +2,34 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    const enterCarName = MissionUtils.Console.readLineAsync(
+    const enterCarName = await MissionUtils.Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) \n"
     );
 
     const carName = enterCarName.split(",");
 
-    if (carName.length > 5) {
+    const verifyNameLength = carName.map((i) => i.length > 5);
+
+    if (verifyNameLength.includes(true) === true) {
       throw new Error(
         "[ERROR] 입력한 이름이 5자를 초과하였습니다. 게임을 다시 시작해주세요."
       );
     }
 
-    const numberOfRounds =
-      MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요? \n");
+    const numberOfRounds = await MissionUtils.Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요? \n"
+    );
 
     const verifyNumber = /^[0-9]+$/;
 
     if (verifyNumber.test(numberOfRounds) === false) {
-      throw new Error(
-        "[ERROR] 게임 횟수에는 숫자만 입력할 수 있습니다. 게임을 다시 시작해주세요"
+      MissionUtils.Console.print(
+        "게임 횟수에는 숫자만 입력할 수 있습니다. 다시 입력해주세요."
       );
     }
   }
+
+  forwardCar() {}
 }
 
 export default App;
