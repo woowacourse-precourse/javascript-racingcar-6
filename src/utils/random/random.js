211b22cfd5f5173ac581c526a46f2b34d0525dd6
노태균 <taegyunn123@gmail.com>
2023-10-28 14:25:03 +0900

    feat: getRandomNumber 함수 구현

--- src/utils/random/random.js
+++ src/utils/random/random.js
@@ -1,4 +1,20 @@
+import IncorrectFormatError from "../../error/IncorrectFormatError";
+import IncorrectParameterError from "../../error/IncorrectParameterError";
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 // start ~ end 사이의 무작위 값 리턴
-const getRandomNumber = (start, end) => {};
+const getRandomNumber = (start, end) => {
+  // parameter의 타입이 올바르지 않을 경우
+  if (typeof start !== "number" || typeof end !== "number") {
+    throw new IncorrectFormatError();
+  }
+
+  // parameter가 올바르지 않을 경우
+  if (start > end) {
+    throw new IncorrectParameterError();
+  }
+
+  return MissionUtils.Random.pickNumberInRange(start, end);
+};
 
 export { getRandomNumber };
--- src/utils/random/random.test.js
+++ src/utils/random/random.test.js
@@ -6,17 +6,21 @@ describe("getRandomNumber 유효성 체크", () => {
     const number = getRandomNumber(0, 0);
     expect(number).toBe(0);
   });
+
   test("parameter 값 : start < end => return start ~ end 사이 값", () => {
     const start = 0;
     const end = 10;
     const number = getRandomNumber(start, end);
-    expect(number).toBeGreaterThanOrEqual(start).toBeLessThanOrEqual(end);
+    expect(number).toBeGreaterThanOrEqual(start);
+    expect(number).toBeLessThanOrEqual(end);
   });
+
   test("parameter 값 : start > end => IncorrectParameterError", () => {
     const getRandomNumberError = () => getRandomNumber(10, 0);
     const errorMessage = ErrorMessage.incorrectParameterErrorMessage();
     expect(getRandomNumberError).toThrow(`[ERROR] ${errorMessage}`);
   });
+
   test("parameter 값 : 숫자가 아닐 경우 => IncorrectFormatError", () => {
     const getRandomNumberError = () => getRandomNumber("10", "0");
     const errorMessage = ErrorMessage.incorrectFormatErrorMessage();
