d301b6156a06d562976dbfa504e7bde5fa3578e1
Hyunji Seok <alleyji126@gmail.com>
2023-10-28 20:31:12 +0900

    feat: define class App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  cars = [];
+  raceTimes = 0;
+  raceResults = [];
+  goTimes = {};
+
+  async play() {
+    
+  }
+
+  async raceCars() {
+
+  }
+
+  goStop() {
+
+  }
+
+  showResults() {
+
+  }
+
+  showWinners() {
+
+
+  }
+
+  async handleErrors() {
+
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
