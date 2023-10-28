2d3e25d387c148f131d96de8c914b9bf51f97eb1
sbemjr1 <sbemjr1@naver.com>
2023-10-28 16:15:53 +0900

    feat:예외 사항 추가 및 완료한 기능 목록 수정

--- src/domain/Error.js
+++ src/domain/Error.js
@@ -1,15 +1,22 @@
 import { ERROR } from '../constants/constant.js';
 
 export const userInputCarNameLengthError = (carNamesArray) => {
+  const regExp = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/g;
   for (let i = 0; i < carNamesArray.length; i++) {
     if (carNamesArray[i].length > 5) {
       throw new Error(ERROR.ERROR_LENGTH);
+    }
+    if (regExp.test(carNamesArray[i])) {
+      throw new Error(ERROR.ERROR_TYPE);
     }
   }
 };
 
 export const userInputTryNumberError = (tryNumber) => {
   if (tryNumber <= 0) {
+    throw new Error(ERROR.ERROR_TYPE);
+  }
+  if (isNaN(tryNumber)) {
     throw new Error(ERROR.ERROR_TYPE);
   }
 };
