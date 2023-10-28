78bcf2719ff331ad20fb381ea6820855ef515bed
김영은 <jessicak0828@gmail.com>
2023-10-29 05:02:36 +0900

    style : 띄어쓰기 수정

--- src/getValue.js
+++ src/getValue.js
@@ -3,28 +3,27 @@ import { INPUT_REQUEST } from './constants';
 import { Random, Console } from '@woowacourse/mission-utils';
 import { validateName, validateNumber } from './validateInput';
 
-
-/**@type {function():Promise<string[]>} */
+/**	@type {function():Promise<string[]>} */
 export async function getUserName() {
 	Console.print(INPUT_REQUEST.NAME);
-	/**@type {string} */
+	/**	@type {string} */
 	const input = await Console.readLineAsync(INPUT_REQUEST.NAME);
-	/**@type {string[]} */
+	/**	@type {string[]} */
 	const inputArr = input.split(',');
-	/**@type {string[]} */
+	/**	@type {string[]} */
 	const validNameArr = validateName(inputArr);
 	return validNameArr;
 }
-/**@type {function():Promise<number>} */
+/**	@type {function():Promise<number>} */
 export async function getTrialFrequency() {
 	Console.print(INPUT_REQUEST.FREQUENCY);
-	/**@type {string} */
+	/**	@type {string} */
 	const input = await Console.readLineAsync(INPUT_REQUEST.FREQUENCY);
 
 	const validNumber = validateNumber(input);
 	return +validNumber;
 }
-/**@type {function():number} */
+/**	@type {function():number} */
 export function getRandomNumber() {
 	const randomNumber = Random.pickNumberInRange(0, 9);
 	return +randomNumber;
