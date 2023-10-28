4483ae99c3e58e0649bce34c87ef6ba09cc8760e
ryan-dia <cjfdnjs158@naver.com>
2023-10-28 10:25:50 +0900

    test(validator): 이름 길이 유효성 테스트 추가

--- __tests__/Vlidator.test.js
+++ __tests__/Vlidator.test.js
@@ -1,13 +1,23 @@
-import ValidatorUtil from '../src/utils/validator/utils';
+import { isDelimiter, isNameLength } from '../src/utils/validator/utils';
 
 describe('validator test', () => {
   describe('isDelimiter', () => {
     test('쉼표 구분자를 가지고 있을 때 true를 리턴한다.', () => {
-      expect(ValidatorUtil.isDelimiter('fobi,koko,noa')).toBe(true);
+      expect(isDelimiter('fobi,koko,noa')).toBe(true);
     });
 
     test(' 쉼표 구분자를 가지고 있지 않을 때 false를 리턴한다.', () => {
-      expect(ValidatorUtil.isDelimiter('fobi.koko.noa')).toBe(false);
+      expect(isDelimiter('fobi.koko.noa')).toBe(false);
+    });
+  });
+
+  describe('isNameLength', () => {
+    test('이름의 글자수가 조건에 맞을 때 true를 리턴한다.', () => {
+      expect(isNameLength('fobi,koko,noa')).toBe(true);
+    });
+
+    test('이름의 글자수가 조건에 맞을 때 false를 리턴한다.', () => {
+      expect(isNameLength('fobifobi', 'kakao', 'no')).toBe(false);
     });
   });
 });
