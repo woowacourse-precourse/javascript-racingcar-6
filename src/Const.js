5865361126e1f68e5c0782cf822034437aee5d70
jong-jang <jjh0299@gmail.com>
2023-10-28 11:32:42 +0900

    feat : 상수 등록

---
+++ src/Const.js
@@ -0,0 +1,7 @@
+// 상수
+export const START_MESSAGE = '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)';
+export const COUNT_MESSAGE = '시도할 횟수는 몇 회인가요?';
+export const RESULT_MESSAGE = '실행 결과';
+
+export const LIMIT_NAME_LENGTH = 5;
+export const GO_NUMBER = 4;
