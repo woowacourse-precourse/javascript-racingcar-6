ea79404ef2b7bed4dc8cbb95250d0d5d9cc1bdcd
swarvy <swarvy0826@naver.com>
2023-10-28 15:52:25 +0900

    feat, refactor : RacingCarGrid 에서 수상자의 정보를 반환하는 기능 구현 및 상수 분리 적용

---
+++ src/constants/GrandPrixOption.js
@@ -0,0 +1,5 @@
+export const GRANDPRIX_MAGICNUMBER = Object.freeze({
+  min: 0,
+  max: 9,
+  move: 4,
+});
--- src/model/RacingCarGrid.js
+++ src/model/RacingCarGrid.js
@@ -1,17 +1,19 @@
 import RacingCar from './RacingCar.js';
 import RandomNumberGenerator from './RandomNumberGenerator.js';
+import { SYMBOLS } from '../constants/Symbols.js';
+import { GRANDPRIX_MAGICNUMBER } from '../constants/GrandPrixOption.js';
 
 export default class RacingCarGrid {
   #racingCarList;
 
   constructor(names) {
-    this.#racingCarList = Array.from(names.split(','), (name) => new RacingCar(name));
+    this.#racingCarList = Array.from(names.split(SYMBOLS.comma), (name) => new RacingCar(name));
   }
 
   setRacingGrid() {
     const randomMovementResults = RandomNumberGenerator.generate(this.#racingCarList.length);
     randomMovementResults.forEach((value, idx) => {
-      if (value >= 4) {
+      if (value >= GRANDPRIX_MAGICNUMBER.move) {
         this.#racingCarList[idx].moveRacingCar();
       }
     });
@@ -19,5 +21,16 @@ export default class RacingCarGrid {
 
   getRacingGrid() {
     return Array.from(this.#racingCarList, (racingCar) => racingCar.getRacingCarStatus());
+  }
+
+  getPodium() {
+    const racingGrid = this.getRacingGrid();
+    const raceStatusList = racingGrid.map(({ status }) => status);
+    const max = Math.max(...Object.values(raceStatusList));
+
+    return racingGrid
+      .filter(({ status }) => status === max)
+      .map(({ name }) => name)
+      .join(`${SYMBOLS.comma}${SYMBOLS.space}`);
   }
 }
