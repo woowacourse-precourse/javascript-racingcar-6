1094a6dda013bdd8a0928190834895837646cda2
Collection50 <fkaus65@naver.com>
2023-10-28 16:12:54 +0900

    feat(App): App.js 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import RacingCarGame from './RacingCarGame/index.js';
+
 class App {
-  async play() {}
+  constructor() {
+    this.game = new RacingCarGame();
+  }
+
+  async play() {
+    await this.game.start();
+  }
 }
 
 export default App;
