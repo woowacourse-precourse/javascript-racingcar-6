cbd44a9a08f2830de24cdfe544e06051316ae077
jong-jang <jjh0299@gmail.com>
2023-10-28 16:03:45 +0900

    Rename :파일이름 변경

--- src/Constant.js
+++ src/Constant.js
@@ -2,6 +2,8 @@
 export const START_MESSAGE = '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)';
 export const COUNT_MESSAGE = '시도할 횟수는 몇 회인가요?';
 export const RESULT_MESSAGE = '실행 결과';
+export const NAME_ERROR_MESSAGE = '자동차 이름은 5자 이하만 가능합니다.';
+export const NOT_A_NUMBER = '숫자가 아닙니다.';
 
 export const LIMIT_NAME_LENGTH = 5;
 export const GO_NUMBER = 4;
