5384bdcbc5af00198d8d47654a2e48ad235ec701
정민승 <jeongminseung@jeongminseung-ui-MacBookAir.local>
2023-10-28 11:44:17 +0900

    startGame 메소드 생성: 게임 전체 진행

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 class App {
-  async play() {}
+  constructor() {
+    //사용자 입력 받기
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+  }
+  async play() {
+    await this.startGame();
+  }
+  async startGame() {
+    //차 이름 받기
+    const carName = this.getCarName();
+    //시도 횟수 받기
+    const tryNum = this.getTryNum();
+    //결과 받기
+    const result = this.result(carName, tryNum);
+    //최종우승자 받기
+    const winner = this.winner();
+    console.log(winner);
+  }
 }
 
 export default App;
