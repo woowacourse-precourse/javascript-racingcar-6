9535d752aa0314192e72d1cfb31be8040bd2ae81
Taeyooooon <eric568@naver.com>
2023-10-29 03:20:21 +0900

    feat: 경주 참가 입력값 검증

--- src/Input.js
+++ src/Input.js
@@ -10,6 +10,7 @@ class Input {
   static async getJoinList() {
     const joinString = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
     const joinList = joinString.split(',');
+    joinList.forEach((joinItem) => Validate.checkJoinItem(joinItem));
 
     return joinList.map((name) => new Participant(name));
   }
@@ -20,9 +21,7 @@ class Input {
    */
   static async getRepeatNumber() {
     const repeatNumber = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
-    if (!Validate.isPositiveInteger(repeatNumber)) {
-      throw new Error('[ERROR]');
-    }
+    Validate.checkRepeatNumber(repeatNumber);
 
     return repeatNumber;
   }
--- src/Validate.js
+++ src/Validate.js
@@ -1,11 +1,27 @@
 export default class Validate {
-  /**
-   * 시도할 횟수 입력값 검증 함수
-   * @param {string} number
-   * @returns {boolean}
-   */
+  static isEmpty(string) {
+    return string === '';
+  }
+
+  static isInvalidLength(string) {
+    return string.length <= 5;
+  }
+
   static isPositiveInteger(number) {
     const parsedNumber = Number(number);
+
     return Number.isInteger(parsedNumber) && parsedNumber > 0;
+  }
+
+  static checkJoinItem(joinItem) {
+    if (this.isInvalidLength(joinItem) && this.isEmpty(joinItem)) {
+      throw new Error('[ERROR]');
+    }
+  }
+
+  static checkRepeatNumber(repeatNumber) {
+    if (!this.isPositiveInteger(repeatNumber)) {
+      throw new Error('[ERROR]');
+    }
   }
 }
