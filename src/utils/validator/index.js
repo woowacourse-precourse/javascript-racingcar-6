e9f249d23c9135cc5313548ee0e8d2907a4c20f4
ryan-dia <cjfdnjs158@naver.com>
2023-10-28 10:24:21 +0900

    feat(validator): validator 객체에 이름 유효성 길이 체크 추가

--- src/utils/validator/index.js
+++ src/utils/validator/index.js
@@ -1,8 +1,12 @@
+import { SYSTEM_TEXT } from '../../constants/System.js';
+import { isDelimiter, isNameLength } from './utils/index.js';
+
 const Validator = {
   checkRacingCarNames(carNames) {
     if (isDelimiter(carNames)) {
-      throw new Error('[ERROR] 구분자는 쉼표만 가능합니다.');
+      throw new Error(`[ERROR] 구분자는 ${SYSTEM_TEXT}만 가능합니다.`);
     }
+    if (isNameLength(carNames)) throw new Error('[ERROR] 이름의 길이가 유효하지 않습니다.');
   },
 };
 
