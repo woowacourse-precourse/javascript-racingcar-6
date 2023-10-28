a74ddf320e48560c7a767ea2fa5253910f26982a
Dabin Jeong <allempti@gmail.com>
2023-10-29 05:07:52 +0900

    fix: static 메서드가 App.js에서 호출 안 되는 문제 해결

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import InputValue from './InputValue.js';
 
 class App {
   async play() {
-    const inputValue = new InputValue();
-    const carNameArr = await inputValue.carName();
-    const count = await inputValue.moveCount();
+    const carNameArr = await InputValue.carName();
+    const count = await InputValue.moveCount();
   }
 }
 
