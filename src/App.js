2a5b9b80c36bea6cdd84d39a67a8c3ec334fba17
yong seock <myongdol132@gmail.com>
2023-10-29 02:09:39 +0900

    Feat(App.js):시도한 횟수 입력 받기
    사용자가 시도하는 횟수를 입력받고 입력을 받을때 정수를 입력하지 않았다면
    에러메시지를 출력 합니다.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   async play() {
     const names = await this.getInputCarNames();
+    const rounds = await this.getInputRounds();
   }
 
   async getInputCarNames() {
@@ -12,12 +13,25 @@ class App {
       throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능하고, 쉼표로 구분해야 합니다.");
     }
     return names.split(",");
+  };
+
+  async getInputRounds() {
+    const rounds = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+    if (!this.isValidRounds(rounds)) {
+      throw new Error("[ERROR] 시도할 횟수는 양의 정수여야 합니다.");
+    }
+    return Number(rounds);
   }
 
   isValidNames(names) {
     const nameArray = names.split(",");
-    
+
     return nameArray.every(name => name.length > 0 && name.length <= 5);
+  }
+
+  isValidRounds(rounds) {
+    const numberRounds = Number(rounds);
+    return Number.isInteger(numberRounds) && numberRounds > 0;
   }
 }
 
