a3053d89227b0c2b86c74d4c907cdbd9fc86a714
jang_yoonsu <shagrat8546@gmail.com>
2023-10-28 23:34:06 +0900

    refactor : 레이싱룰 전략 추가

--- src/utils/shouldCarRun/index.js
+++
@@ -1,9 +0,0 @@
-import { pickNumberInRage } from "../pickNumberInRange";
-
-const MIN_VALUE = 0;
-const MAX_VALUE = 9;
-const THRESHOLD = 4;
-
-export function shouldCarRun() {
-  return pickNumberInRage(MIN_VALUE, MAX_VALUE) >= THRESHOLD;
-}
