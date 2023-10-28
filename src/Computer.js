0754bf5f82faf10042eef3636003cd1fd977eb9c
Boeun <boeun0904@naver.com>
2023-10-28 23:59:01 +0900

    feat(Computer.js): get cars name from user

--- src/Computer.js
+++ src/Computer.js
@@ -1,3 +1,17 @@
+import User from './User.js';
+import RacingCar from './RacingCar.js';
+
 export default class Computer {
+  constructor() {
+    this.racingCars = [];
+  }
+
   playGame(user) {}
+
+  async getCarsNameArrayFromUser(user) {
+    const cars = await user.getUserInputCarsNameArray(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+    );
+    this.racingCars = cars.map((car) => new RacingCar(car));
+  }
 }
---
+++ src/RacingCar.js
@@ -0,0 +1,6 @@
+export default class RacingCar {
+  constructor(name) {
+    this.name = name;
+    this.distance = 0;
+  }
+}
