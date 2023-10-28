72943ddbfbf0158dd794ef7505774ab9f8db48d3
soyoung125 <sypark041754@gmail.com>
2023-10-28 22:17:00 +0900

    feat: makeCarArray 메서드 추가
    - 자동차 이름을 쉼표로 구분한다.
    - 구분된 이름으로 자동차 객체를 생성한다.
    - 생성한 객체를 자동차 배열에 추가한다.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import Car from "./Car.js";
 
 class App {
+  
+  constructor() {
+    this.cars = [];
+  }
 
   async inputCarNames() {
-    const names = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n")
-    MissionUtils.Console.print(names);
+    return await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n")
+  }
+
+  makeCarArray(names) {
+    names.split(",").forEach((name) => {
+      this.cars.push(new Car(name));
+    })
+    
+    MissionUtils.Console.print(this.cars);
   }
 
   async play() {
-    await this.inputCarNames();
+    const names = await this.inputCarNames();
+
+    this.makeCarArray(names)
   }
 }
 
