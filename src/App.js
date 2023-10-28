fce38cf358fab4c8d56b060b13dabead03b87499
sang-il Yoon <yunsangil7749@gmail.com>
2023-10-28 16:15:27 +0900

    feat: 경주할 n대의 자동차 입력 기능 추가
    경주 할 n대의 자동차의 이름을 입력받는 기능을 하는 함수를 추가했습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
   async play() {}
+
+  async participating_car(){
+  let cars = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
+  cars = cars.split(',')
+  
+  cars.forEach(el => { // 유효성 검사.
+    if(el.legnth > 5){
+      throw new Error();
+    }
+  });
+
+  return cars
+ }
+  
 }
 
 export default App;
