adaacc3d52c5cfe1159d44637ffbb13bfff4d925
Boeun <boeun0904@naver.com>
2023-10-29 01:15:27 +0900

    chore(Computer): change method name more clear

--- src/Computer.js
+++ src/Computer.js
@@ -8,17 +8,16 @@ export default class Computer {
 
   playGame(user) {}
 
-  async getCarNameArrayFromUser(user) {
+  async getCarNameInputFromUser(user) {
     const userInput = await user.getCarNameInput(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
     );
-    const carNameArr = this.getCarNameArrayFromUserInput(userInput);
+    const carNameArr = this.getCarNameArrayFromString(userInput);
     this.racingCars = carNameArr.map((carName) => new RacingCar(carName));
-    console.log(this.racingCars);
   }
 
-  getCarNameArrayFromUserInput(input) {
-    const carNameArr = input.replace(/ /g, '').split(',');
+  getCarNameArrayFromString(str) {
+    const carNameArr = str.replace(/ /g, '').split(',');
     if (carNameArr.some((carName) => carName.length > 5 || carName === ''))
       throw new Error('[ERROR] 잘못된 입력입니다.');
     return carNameArr;
