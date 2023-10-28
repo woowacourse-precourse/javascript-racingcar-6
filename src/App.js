bbf347ba548e7642c89971d83bead6e965ea3b81
Majesty-jun <hkwns01@gmail.com>
2023-10-28 15:07:51 +0900

    FEAT: 자동차 이름 입력 받기 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Console } from '@woowacourse/mission-utils';
+import carNameInput from './input/carNameInput.js';
+
 class App {
-  async play() {}
+  async play() {
+    this.cars = await carNameInput();
+    Console.print(this.cars);
+  }
 }
 
 export default App;
---
+++ src/input/carNameInput.js
@@ -0,0 +1,13 @@
+import { Console } from '@woowacourse/mission-utils';
+
+async function carNameInput() {
+  try {
+    const USER_INPUT = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    const CARS = USER_INPUT.split(',');
+    return CARS;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+export default carNameInput;
