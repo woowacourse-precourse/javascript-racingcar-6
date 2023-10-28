b3754da31851294e38f7c9d3d8c73895f16e678c
hyzzzzy <shinhj4068@naver.com>
2023-10-28 22:59:08 +0900

    feat(constants): 출력 메시지 추가

--- src/constants.js
+++ src/constants.js
@@ -14,6 +14,7 @@ const SCORE = Object.freeze({
 
 const MESSAGE = Object.freeze({
   INPUT_NAME: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+  INPUT_NUMBER: '시도할 횟수는 몇 회인가요?',
   RESULT: '실행 결과',
   WINNER: '최종 우승자 : ',
 });
