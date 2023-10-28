a292b3405d5ca0b23e8c8aa7a8c0e38934dba2c6
xxziiko <xxziiko@gmail.com>
2023-10-28 14:16:05 +0900

    feat: 자동차의 이름을 입력받는 기능

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
+  constructor() {
+    this.ERROR = {
+      LENGTH : '[ERROR] 이름은 5자 이내로 작성해주세요.',
+      INCORRECT_FORMAT :'[ERROR] 이름을 쉼표(,)로 구분해주세요.'
+
+    }
+  }
+
   getRandomNumber() {
     return MissionUtils.Random.pickNumberInRange(0, 9);
   }
 
-  async play() {}
+  printErrorMessage(errorMessage) {
+    throw new Error(errorMessage);
+  }
+
+  async initializeCars() {
+    const pattern = /[.\/-]/;
+    const carNames = await this.getCarName();
+    const carArray = carNames.split(",");
+    const isCorrectName = carArray.every((value) => value.length < 5);
+
+    if(pattern.test(carNames)) this.printErrorMessage(this.ERROR.INCORRECT_FORMAT);
+    if(!isCorrectName) this.printErrorMessage(this.ERROR.LENGTH);
+  }
+
+  getCarName() {
+    const carNames = MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+
+    return carNames;
+  }
+
+  async play() {
+    this.initializeCars();
+  }
 }
 
 export default App;
