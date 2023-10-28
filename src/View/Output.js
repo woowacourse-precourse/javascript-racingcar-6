f52c1b6b653a4d4bddf55d5859839ba008a8c881
hojkim77 <rlaghwnd4534@gmail.com>
2023-10-28 16:34:22 +0900

    [feat] 우승자 출력 메서드 완성

--- src/View/Output.js
+++ src/View/Output.js
@@ -9,6 +9,16 @@ class Output {
   static printCarState(carName, carDistance) {
     Console.print(carName + ' : ' + '-'.repeat(carDistance));
   }
+
+  /**
+   *
+   * @param {[string]} winners
+   */
+  static printWinners(winners) {
+    const winnersString = winners.join(', ');
+
+    Console.print('최종 우승자 : ' + winnersString);
+  }
 }
 
 export default Output;
