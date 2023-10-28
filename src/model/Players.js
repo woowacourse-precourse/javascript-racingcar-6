1f25958b63da3a33352e4e53c63b15c9c5925abd
ReturnReason <reasonz@daum.net>
2023-10-28 22:25:13 +0900

    feat(Players): 모든 플레이어의 랜덤 값을 뽑아 4 이상인 경우 moveCount를 1 증가시키는 메서드 추가

--- src/model/Players.js
+++ src/model/Players.js
@@ -1,4 +1,5 @@
 import { Random } from '@woowacourse/mission-utils';
+import { GAME_RULL } from '../constants/gameRules.js';
 
 export default class Players {
   #tryCount = 0;
@@ -23,11 +24,12 @@ export default class Players {
     );
   }
 
-  // 모든 플레이어의 랜덤 넘버를 돌리게 한다.
-  // 4이상이 뜨는 유저는 move count를 1 증가 시키도록 한다.
-  // 그리고 무브 카운트 만큼 - 를 출력한다.
-
-  move() {
-    // 시도 횟수만큼 각각의 플레이어가 몇 번 움직일 지 무작위 값 구하기
+  setMoveCount() {
+    this.#players.forEach((player) => {
+      const pickedNumber = this.generateRandomNumber();
+      if (pickedNumber >= GAME_RULL.MOVE_CONDITION_COUNT) {
+        player.increaseMoveCount();
+      }
+    });
   }
 }
