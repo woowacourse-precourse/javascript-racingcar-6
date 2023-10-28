3c2aefd4d0cd1635f8a83bae466206ba84987888
ReturnReason <reasonz@daum.net>
2023-10-28 17:02:40 +0900

    feat(gameRules): 게임 룰과 관련된 상수 추가

--- src/constants/gameRules.js
+++ src/constants/gameRules.js
@@ -1,5 +1,8 @@
 export const GAME_RULL = Object.freeze({
   DIVISION: ',',
   PLAYER_MIN_NUMBER: 2,
+  PLAYER_MAX_NUMBER: 10,
   CAR_NAME_MAX_LENGTH: 5,
+  MOVE_MAX_COUNT: 10,
+  MOVE_MIN_COUNT: 1,
 });
