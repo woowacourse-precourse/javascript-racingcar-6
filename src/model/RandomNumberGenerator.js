decf8ce541a4921bc3795d21d4eaebe8ed206078
swarvy <swarvy0826@naver.com>
2023-10-28 15:55:20 +0900

    refactor : RandomNumberGenerator에 분리된 상수 적용

--- src/model/RandomNumberGenerator.js
+++ src/model/RandomNumberGenerator.js
@@ -1,7 +1,10 @@
 import { Random } from '@woowacourse/mission-utils';
+import { GRANDPRIX_MAGICNUMBER } from '../constants/GrandPrixOption.js';
 
 export default class RandomNumberGenerator {
   static generate(amount) {
-    return Array.from({ length: amount }, () => Random.pickNumberInRange(0, 9));
+    return Array.from({ length: amount }, () =>
+      Random.pickNumberInRange(GRANDPRIX_MAGICNUMBER.min, GRANDPRIX_MAGICNUMBER.max),
+    );
   }
 }
