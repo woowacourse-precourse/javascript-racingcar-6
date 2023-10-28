8fd59093bcb74d258fad669810a91ec7d2c219b7
jiyun1006 <jiyun4251@gmail.com>
2023-10-28 23:31:56 +0900

    ⚡️ feat : `moveCarGame` 게임의 진행을 맡는 메서드 기능 구현
    `moveCarGame`메서드를 통해서 게임판(하이폰 출력판)을 만들고
    점수를 기록할 수 있게 함.
    
    ex) Fixes : #45 Related to : #34, #23

--- src/App.js
+++ src/App.js
@@ -20,17 +20,31 @@ const regLanguage = /^[가-힣]{1,}$|^[a-z]{1,}$|^[A-Z]{1,}$/;
 
 class App {
   async play() {
+    // 자동차 이름 입력
     const carName = await this.inputCarName();
     this.validateCarName(carName);
-
-    const playNumber = await this.inputPlayNumber();
+    // 게임 진행 횟수 입력
+    let playNumber = await this.inputPlayNumber();
     this.validatePlayNumber(playNumber);
+    
     MissionUtils.Console.print('\n실행 결과');
 
-    // if (moveCar) {
-      
-    // }
-   }
+    // 자동차 이름을 배열로 만든 객체
+    const carNameArr = carName.split(",");
+
+    // 진행과정을 저장할 객체 만드는 메서드.
+    let gameBoard = this.createGameArr(carNameArr.length);
+
+    // 게임 시작
+    for (let idx = 0; idx < Number(playNumber); idx++){
+      gameBoard = this.moveCarGame(carNameArr, gameBoard);
+    }
+    
+    // 우승자 발표
+    this.resultGame(gameBoard, carNameArr);
+  }
+  
+
   
   // 차 이름 입력받는 메서드
   async inputCarName() {
@@ -42,7 +56,6 @@ class App {
   validateCarName(name) {
     if (!regFirstForm.test(name)) throw new Error(`${WRONG_NAME_ERROR}`);
 
-    let [korean, englishLow, englishUp] = [0,0,0];
     const nameSplitArr = name.split(",");
     const nameSplitArrLen = nameSplitArr.length;
     let beforeName = '';
@@ -66,11 +79,68 @@ class App {
     if (isNaN(number)) throw new Error('[ERROR] 숫자만 입력가능합니다.');
   }
 
-  moveCar() {
+
+  // 진행과정을 저장할 객체 만드는 메서드.
+  createGameArr(arrLen) {
+    const arr = [];
+    for (let idx = 0; idx < arrLen; idx++){
+      arr[idx] = 0;
+    }
+    return arr;
+  }
+
+
+  // 자동차 게임 진행 메서드
+  moveCarGame(carNameArr, gameBoard) {
+    const gameLen = gameBoard.length;
+    for (let idx = 0; idx < gameLen; idx++) {
+      if (this.isMove()) gameBoard[idx] += 1;
+      MissionUtils.Console.print(`${carNameArr[idx]}` + " : " + this.createGameBoard(gameBoard[idx]));      
+    }
+    MissionUtils.Console.print('\n');
+    return gameBoard;
+  }
+
+  // 랜덤 수를 입력받아 앞으로 가는지 아닌지 결정하는 메서드.
+  isMove() {
     const randomNum = MissionUtils.Random.pickNumberInRange(0, 9);
     return randomNum >= 4 ? true : false;
   }
+
+  // 게임 진행결과에따라 하이폰 개수 누적해가는 함수.
+  createGameBoard(num) {
+    let hyphen = '';
+    for (let idx = 0; idx < num; idx++){
+      hyphen += '-';
+    }
+    return hyphen;
+  }
+
+
+  // 우승자 발표 메서드
+  resultGame(gameResult, carNameArr) {
+    const max = Math.max(...gameResult); // 최대스코어 점수
+    let bestPlayerIdx = []; // 최대 스코어 가진 사람들 인덱스 배열
+    
+    // 최대스코어 가진 사람들 인덱스 찾는 메서드
+    bestPlayerIdx = gameResult.map((score, idx) => {
+      return score === max ? idx : null;
+    })
+    bestPlayerIdx = bestPlayerIdx.filter((el) => el !== null);
+
+    // 최대스코어 가진 사람들.
+    const bestPlayerName = bestPlayerIdx.map((nameIdx) => {
+      return carNameArr[nameIdx];
+    })
+
+    // 마지막 우승자 출력!
+    MissionUtils.Console.print('최종 우승자 : ' + `${bestPlayerName.join(', ')}`);
+  }
 }
+
+
+
+
 
 
 
