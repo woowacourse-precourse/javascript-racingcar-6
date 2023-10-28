4a7f2114062ef52ac8e6cd7c2763b85f375feabb
Doeunnkimm <doeunnkimm@gmail.com>
2023-10-28 17:52:41 +0900

    feat: 배열에서 중복 아이템을 검사하는 validation 메서드

--- src/game/game.error.js
+++ src/game/game.error.js
@@ -2,5 +2,9 @@ const validations = {
   validLength(item, min, max) {
     return item.length >= min && item.length <= max;
   },
+
+  validDuplication(array) {
+    return array.every((item, index) => array.lastIndexOf(item) === index);
+  },
 };
 export default validations;
