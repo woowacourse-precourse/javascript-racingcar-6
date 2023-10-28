416f844ed9aadcd3820d89b8d71f340f42becc55
noodle <noodle>
2023-10-29 02:14:03 +0900

    refactor: Car.js private 변수, getter, static method 추가

--- src/Car.js
+++ src/Car.js
@@ -2,17 +2,26 @@ import { CONSTANTS } from './constants/index.js';
 import { Random } from '@woowacourse/mission-utils';
 
 class Car {
+  #position = 0;
+
   constructor(name) {
     this.name = name;
-    this.position = 0;
   }
 
   move() {
-    const isMoving = Random.pickNumberInRange(0, 9) >= CONSTANTS.FORWARD_THRESHOLD;
+    const isMoving = Car.shouldMove();
 
     if (isMoving) {
-      this.position += 1;
+      this.#position += 1;
     }
+  }
+
+  getPosition() {
+    return this.#position;
+  }
+
+  static shouldMove() {
+    return Random.pickNumberInRange(0, 9) >= CONSTANTS.FORWARD_THRESHOLD;
   }
 }
 
