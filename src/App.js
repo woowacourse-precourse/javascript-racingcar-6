f34fa8d300af35b1660ad2db4f4fee0b13412e89
aaminha <dks4857@gmail.com>
2023-10-28 22:11:42 +0900

    feat(게임 시작): 게임 시작 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { getCarName, getPlayNum } from './functions/gameStart';
+
 class App {
   constructor() {
-    this.temp = 0;
+    this.carList = [];
+    this.playNum = 0;
+  }
+
+  start() {
+    this.carList = getCarName();
+    this.playNum = getPlayNum();
   }
 
   async play() {
