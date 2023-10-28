c988006e16bf7c2c3f839ced8e31bc4cc074e1f1
heonq <gkagjsrb@naver.com>
2023-10-28 20:11:52 +0900

    feat: 무작위 값이 4 이상일 경우 해당 자동차의 이동 거리를 더하는 기능 구현

--- src/Car.js
+++ src/Car.js
@@ -4,9 +4,11 @@ import CONSTANTS from '../utils/Constants';
 class Car {
   constructor(name) {
     this.name = name;
+    this.progressStatus = CONSTANTS.initialStatus;
   }
   progressDependingOnValue() {
     const value = Random.pickNumberInRange(CONSTANTS.minimumValue, CONSTANTS.maximumValue);
+    if (value >= CONSTANTS.progressValue) this.progressStatus += 1;
   }
 }
 
