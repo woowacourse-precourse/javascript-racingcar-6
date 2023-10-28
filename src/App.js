88ba31bd63d0be3baf90101502641b9304fc7e08
yong seock <myongdol132@gmail.com>
2023-10-29 02:34:39 +0900

    Feat(App.js): 자동차 레이스 조건 및 진행 구현
    - 자동차의 전진 조건을 Random.pickNumberInRange함수를 이용하여 결정
    합니다.
    - 경주 진행시 자동차별 전진 여부를 결정하고 결과를 업데이트 합니다.
    - 각 라운드 별 경주 결과를 출력 합니다.
    - 최종 우승자 출력 기능을 구현 합니다.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
     const names = await this.getInputCarNames();
     const rounds = await this.getInputRounds();
-  }
+    this.race(names, rounds);
+  };
 
   async getInputCarNames() {
     const names = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -21,18 +22,59 @@ class App {
       throw new Error("[ERROR] 시도할 횟수는 양의 정수여야 합니다.");
     }
     return Number(rounds);
-  }
+  };
 
   isValidNames(names) {
     const nameArray = names.split(",");
 
     return nameArray.every(name => name.length > 0 && name.length <= 5);
-  }
+  };
 
   isValidRounds(rounds) {
     const numberRounds = Number(rounds);
     return Number.isInteger(numberRounds) && numberRounds > 0;
-  }
+  };
+
+  //레이스
+
+  race(names, rounds) {
+    let results = this.initializeResults(names);
+    for (let i = 0; i < rounds; i++) {
+      results = this.runRound(results);
+      this.displayRoundResults(results);
+    }
+    this.displayWinners(results);
+  };
+  
+  initializeResults(names) {
+    let results = {};
+    names.forEach(name => results[name] = "");
+    return results;
+  };
+  
+  runRound(results) {
+    const updatedResults = { ...results };
+    for (let name in results) {
+      if (Random.pickNumberInRange(0, 9) >= 4) {
+        updatedResults[name] += "-";
+      }
+    }
+    return updatedResults;
+  };
+  
+  displayRoundResults(results) {
+    for (let name in results) {
+      Console.print(`${name} : ${results[name]}`);
+    }
+    Console.print("\n");
+  };
+  
+  displayWinners(results) {
+    const maxDistance = Math.max(...Object.values(results).map(result => result.length));
+    const winners = Object.keys(results).filter(name => results[name].length === maxDistance);
+    Console.print(`최종 우승자: ${winners.join(", ")}`);
+  };
+
 }
 
 
