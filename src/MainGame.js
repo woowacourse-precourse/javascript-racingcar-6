ddabf001eb8c79354e5a9d6f177074a5e5745a6b
SU_VIN <pyotnqls25@naver.com>
2023-10-28 19:26:44 +0900

    Feat: 경주할 자동차 이름 입력
    예외처리[1]-  이름이,로 구분이 되었는지, 즉 1대 이상인지

--- src/MainGame.js
+++ src/MainGame.js
@@ -4,6 +4,18 @@ import { Message } from './Message.js';
 class MainGame {
   start = () => {
     Console.print(Message.INPUT_CAR_NAME);
+    this.inputCarName();
+  };
+
+  static isSplitComma = (carNameList) => {
+    if (carNameList.split(',').length == 1) {
+      throw new Error(Message.ERROR.NOT_SPLIT_COMMA);
+    }
+  };
+
+  inputCarName = async () => {
+    const carNameList = await Console.readLineAsync(''); // 사용자 입력을 받기 위해 await 추가
+    MainGame.isSplitComma(carNameList);
   };
 }
 
