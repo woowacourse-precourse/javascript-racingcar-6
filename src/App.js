48f77a18b5e6039669774f15b753752e07a8ebd6
jypark38 <jury124@naver.com>
2023-10-28 16:13:25 +0900

    add : 주요 동작 기능 작성

--- src/App.js
+++ src/App.js
@@ -3,21 +3,17 @@ import { MIN_NAME_LENGTH, MAX_NAME_LENGTH } from "./lib/constants.js";
 import { INPUT_PROMPT1, INPUT_PROMPT2, INVALID_INPUT1, INVALID_INPUT2 } from "./lib/logs.js";
 
 class App {
-  constructor() {
-    this.cars;
-    this.carsNum;
-    this.carsProgress;
-    this.tryNum;
-  }
+  cars;
+  carsNum;
+  carsProgress;
+  tryNum;
 
   async play() {
     await this.inputCarsWithValidate();
     await this.inputTryWithValidate();
-    console.log(this.tryNum);
     this.init();
-    console.log(this.cars);
-    console.log(this.carsNum);
-    console.log(this.carsProgress);
+    this.run();
+    this.printWinners();
   }
 
   async inputCarsWithValidate() {
@@ -35,6 +31,7 @@ class App {
     const input = await Console.readLineAsync(INPUT_PROMPT2);
     this.validateTry(input);
     this.tryNum = parseInt(input, 10);
+    Console.print("");
   }
 
   validateTry(str) {
@@ -47,11 +44,28 @@ class App {
     this.carsProgress = Array.from({ length: CARS_NUM }, () => 0);
   }
 
-  // progress(curr) {
-  //   if (curr) return;
-  //   const random = Array.from({ length: CARS_NUM }, () => Random.pic);
-  //   this.progress(maxTry);
-  // }
+  // recursive
+  run(curr = 0) {
+    if (curr === 0) Console.print("실행 결과");
+    if (this.tryNum <= curr) return;
+    const randomArr = Array.from({ length: this.carsNum }, () => Random.pickNumberInRange(0, 9));
+    randomArr.forEach((e, i) => (e >= 4 ? this.carsProgress[i]++ : null));
+    this.carsProgress.forEach((_, i) => this.printEachProgress(i));
+    Console.print("");
+    this.run(curr + 1);
+  }
+
+  printEachProgress(idx) {
+    const PROGRESS = "-".repeat(this.carsProgress[idx]);
+    Console.print(`${this.cars[idx]} : ${PROGRESS}`);
+  }
+
+  printWinners() {
+    const MAX = Math.max(...this.carsProgress);
+    const WINNERS = [];
+    this.carsProgress.forEach((e, i) => (e === MAX ? WINNERS.push(this.cars[i]) : null));
+    Console.print(`최종 우승자 : ${WINNERS.join(", ")}`);
+  }
 }
 
 export default App;
