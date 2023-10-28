e17bbc3d8dbb79d0aa8ab3ea8edb580edc43fd5b
Seung Jun <pache31@naver.com>
2023-10-28 18:32:54 +0900

    [fix]: #1
    - 유효성 검사 반환 값 boolean으로 수정

--- src/Validation.js
+++ src/Validation.js
@@ -4,32 +4,33 @@ const MOVABLE = 4;
 const MAX_LENGTH = 5;
 const REG_NUMBER = /^[0-9]+/;
 
-class ValidationCheck {
-  /**
-   * 입력한 자동차 이름 유효성 검사
-   * @param {String} carNames
-   */
-  checkCarNames(carNames) {
-    const carArr = carNames.split(',');
+/**
+ * 입력한 자동차 이름 유효성 검사
+ * @param {string} carNames
+ * @returns {boolean}
+ */
+export function checkCarNames(carNames) {
+  const carNameArr = carNames.split(',');
 
-    carArr.forEach(carName => {
-      if (carName.length > MAX_LENGTH) {
-        throw new Error(ERROR_MESSAGE.LENGTH_ERROR);
-      }
-      if (carArr.indexOf(carName) != carArr.lastIndexOf(carName)) {
-        throw new Error(DUPLICATE_ERROR + carName);
-      }
-    });
-  }
-  /**
-   * 입력한 값이 숫자[0-9]인지 검사
-   * @param {String} number
-   */
-  checkTryNumber(number) {
-    if (!REG_NUMBER.test(ParseInt(number))) {
-      throw new Error(ERROR_MESSAGE.NOT_NUMBER_ERROR);
+  carNameArr.forEach(carName => {
+    if (carName.length > MAX_LENGTH) {
+      return false;
     }
-  }
+    if (carNameArr.indexOf(carName) != carNameArr.lastIndexOf(carName)) {
+      return false;
+    }
+  });
+  return true;
 }
 
-export default ValidationCheck;
+/**
+ * 입력한 값이 숫자[0-9]인지 검사
+ * @param {string} number
+ * @returns {boolean}
+ */
+export function checkTryNumber(number) {
+  if (!REG_NUMBER.test(parseInt(number))) {
+    return false;
+  }
+  return true;
+}
