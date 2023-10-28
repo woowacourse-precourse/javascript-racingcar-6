22fce78078b38d0bbf1830ebc2b211f0cec0ac5e
chiman <rlaclghks123@naver.com>
2023-10-28 20:02:46 +0900

    refactor: 클래스 내부 변수명 수정 carList -> carRegistry
    list는 좀 배열의 list의 느낌이 강해 수정

--- src/Refree.js
+++ src/Refree.js
@@ -2,14 +2,14 @@ import { Random } from '@woowacourse/mission-utils';
 import Car from './Car.js';
 
 class Refree {
-  #carList = new Map();
+  #carRegistry = new Map();
 
   registerCars(carNames) {
-    carNames.forEach((car) => this.#carList.set(car, new Car(car)));
+    carNames.forEach((car) => this.#carRegistry.set(car, new Car(car)));
   }
 
   moveCars() {
-    this.#carList.forEach((car) => {
+    this.#carRegistry.forEach((car) => {
       const randomNumber = Random.pickNumberInRange(0, 9);
       if (randomNumber >= 4) car.move(1);
       if (randomNumber < 4) car.stop();
@@ -18,7 +18,7 @@ class Refree {
 
   getRcordList() {
     const result = [];
-    this.#carList.forEach((carInstance) => result.push(carInstance.record()));
+    this.#carRegistry.forEach((carInstance) => result.push(carInstance.record()));
     return result;
   }
 
