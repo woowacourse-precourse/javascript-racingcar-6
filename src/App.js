b5fc496899540e92a9e149949a99f457b2d10713
nijuy <youjin6325@naver.com>
2023-10-28 16:50:22 +0900

    refactor: 경주할 자동차 이름을 검증하는 isValidCarList() 생성, 변수명 수정
    - 여러 대의 자동차는 cars -> carList로 수정

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import { Console } from '@woowacourse/mission-utils';
 
 class App {
-  async play() {
-    const inputCars = await Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
-    );
-
+  static isVaildCarList(input, carList) {
     const overMaxLengthReg = /[^,]{6,}/;
-    if (inputCars.match(overMaxLengthReg)) {
-      throw new Error('[ERROR] 자동차 이름은 최대 5자까지 가능합니다!');
-    }
 
-    const cars = inputCars.split(',');
-    if (cars.includes('')) {
+    if (input.match(overMaxLengthReg)) {
+      throw new Error('[ERROR] 자동차 이름은 최대 5자까지 가능합니다!');
+    } else if (carList.includes('')) {
       throw new Error('[ERROR] 자동차 이름은 최소 1자 이상 적어주세요!');
-    } else if (cars.length !== new Set(cars).size) {
+    } else if (carList.length !== new Set(carList).size) {
       throw new Error('[ERROR] 자동차 이름은 중복될 수 없습니다!');
-    } else if (cars.length === 1) {
+    } else if (carList.length === 1) {
       throw new Error(
         '[ERROR] 참여자가 한 명뿐이라 경주가 열리지 않았습니다..🤔',
       );
     }
+  }
+
+  async play() {
+    const inputCarList = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+    );
+    const carList = inputCarList.split(',');
+
+    App.isVaildCarList(inputCarList, carList);
   }
 }
 
