d73e61f75f986d91ab3300d540850f5743a7240b
yjh1008 <exesea12@gmail.com>
2023-10-28 18:37:25 +0900

    feat : 경기를 진행하는 함수 생성

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ const { Console, Random } = MissionUtils;
 import Cars from "./Cras";
 class App {
 
+  running_race(cars, repetitions) {
+    for(let i =0 ;i<repetitions;i++) {
+      const moves = Random.pickUniqueNumbersInRange(1, 10, cars.len); 
+    }
+  }
+
   async play() {
     const input = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
     const cars = new Cars(input);
     const repetitions = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+    this.running_race(cars, repetitions);
   }
 }
 
