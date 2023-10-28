1f88486fedafec4b6a28388fd5589ba7a1e75c03
JoSooha <dlrhdcjs@naver.com>
2023-10-28 20:55:15 +0900

    feat: 자동차 이름 입력 받고 예외 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const carInput = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+    const carArray = this.handleCarName(carInput);
+
+  }
+
+  handleCarName(input) {
+    try {
+      const arr = input.split(",");
+      arr.forEach(e=>{
+        if(e.length>6) throw new Error("[ERROR] 자동차 이름은 5자 이하여야 합니다.")
+      })
+      return arr;
+    } catch(error) {
+      console.error(error.message);
+      throw error;      
+    }
+  }
+
+  
+
+  
 }
 
 export default App;
