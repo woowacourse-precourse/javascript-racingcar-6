cfe8c2461c3728fde8703cc0f6ad7236bf599d2d
koblmom <nayun9191@gamil.com>
2023-10-28 18:20:14 +0900

    feat: 참가자별로 랜덤숫자 생성

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 const { Console, Random } = MissionUtils;
 
 class App {
+  constructor() {
+    this.participants = [];
+  }
+
   async inputName() {
     const input = await Console.readLineAsync(
-      "이름을 경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) : "
+      "이름을 경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) : \n"
     );
-    return this.getName(input);
+    const nameArray = this.getName(input);
+    this.participants = nameArray.map((name) => ({ name, position: 0 }));
   }
+
   getName(input) {
     const nameArray = input.split(",").map((name) => name.trim());
     this.validateInput(nameArray);
     return nameArray;
   }
+
   validateInput(nameArray) {
     nameArray.forEach((name) => {
       if (name.length > 5) {
@@ -20,8 +27,24 @@ class App {
       }
     });
   }
+
+  RandomNumber() {
+    return MissionUtils.Random.pickNumberInRange(1, 9);
+  }
+
+  async playRound() {
+    const numParticipants = this.participants.length;
+    for (let i = 0; i < numParticipants; i++) {
+      const randomNum = await this.RandomNumber();
+      if (randomNum >= 4) {
+        this.participants[i].position += 1;
+      }
+    }
+  }
+
   async play() {
     await this.inputName();
+    await this.playRound();
   }
 }
 const app = new App();
