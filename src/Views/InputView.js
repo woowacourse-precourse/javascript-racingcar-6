a24d5f226ea699cb57b2c3014c6b017240d887b1
heonq <gkagjsrb@naver.com>
2023-10-28 18:46:23 +0900

    refactor(InputView): 불필요한 모듈 삭제 - 유효성 검사 기능을 Controller에서 실시하면서 Validator 모듈 삭제

--- src/Views/InputView.js
+++ src/Views/InputView.js
@@ -1,6 +1,5 @@
 import {Console} from '@woowacourse/mission-utils';
 import MESSAGES from '../../utils/Messages';
-import Validator from '../../utils/Validator';
 
 const InputView = {
   async readCarNames() {
