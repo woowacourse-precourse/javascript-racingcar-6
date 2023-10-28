6a8271444190c3949f77da5271013673761d4b1f
ryan-dia <cjfdnjs158@naver.com>
2023-10-28 13:43:12 +0900

    chore: 불필요한 주석 삭제

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,7 +10,6 @@ module.exports = {
   },
   rules: {
     // 프리코스 프로그래밍 요구사항
-    // https://github.com/solo5star/javascript-lotto#-%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D-%EC%9A%94%EA%B5%AC-%EC%82%AC%ED%95%AD
     'max-depth': ['error', 2],
 
     // ESM 사용으로 인한 파일 확장자 표기 의무화
