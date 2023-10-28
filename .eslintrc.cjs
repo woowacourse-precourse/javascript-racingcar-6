f1e959c4b5a6a2fac271b4df31690f38fbd0e42a
kelvin3476 <winrain03226@gmail.com>
2023-10-29 04:47:04 +0900

    style(*): eslint & prettier 설정

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    // 들여쓰기 깊이 제한
+    'max-depth': ['error', 2],
+  },
+};
