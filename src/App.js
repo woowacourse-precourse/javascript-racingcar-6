b93c783d2d8c183b6548703b075b7a1ee927cac2
hyzzzzy <shinhj4068@naver.com>
2023-10-28 23:44:22 +0900

    feat(validation): 이름 입력값 유효성 검사 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { readLineAsync, isValidInputNames } from './utils';
+import { MESSAGE } from './constants';
+
+const { INPUT_NAME } = MESSAGE;
+
 class App {
-  async play() {}
+  async play() {
+    try {
+      const inputNames = await readLineAsync(INPUT_NAME);
+      isValidInputNames(inputNames);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
 }
 
 export default App;
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
 import { Console } from '@woowacourse/mission-utils';
+import { SETTING, ERROR_MESSAGE } from './constants';
 
 const printMessage = (message) => {
   return Console.print(message);
@@ -8,4 +9,29 @@ const readLineAsync = async (message) => {
   return Console.readLineAsync(message);
 }
 
-export { printMessage, readLineAsync };
+const isValidInputNames = (input) => {
+  input = input.trim();
+  const splittedInput = input.split(',');
+
+  const set = new Set(splittedInput);
+  const { MIN_NAME_NUMBER, MIN_NAME_LENGTH, MAX_NAME_LENGTH } = SETTING;
+  const { NOT_MULTIPLE_NAMES, NOT_NAME_LENGTH, NOT_UNIQUE } = ERROR_MESSAGE;
+  
+  if (splittedInput.length < MIN_NAME_NUMBER) {
+    throw new Error(`${NOT_MULTIPLE_NAMES}`);
+  } 
+
+  if (splittedInput
+      .filter(name => name.length < MIN_NAME_LENGTH || name.length > MAX_NAME_LENGTH)
+      .length > 0) {
+    throw new Error(`${NOT_NAME_LENGTH}`);
+  }
+
+  if (set.size !== splittedInput.length) {
+    throw new Error(`${NOT_UNIQUE}`);
+  }
+
+  return true;
+}
+
+export { printMessage, readLineAsync, isValidInputNames };
