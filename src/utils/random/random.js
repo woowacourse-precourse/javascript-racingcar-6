43beb7894984bfea775793a4890b9c0f183b0b27
노태균 <taegyunn123@gmail.com>
2023-10-28 14:18:28 +0900

    test: getRandomNumber 함수 test code 추가

---
+++ src/utils/random/random.js
@@ -0,0 +1,4 @@
+// start ~ end 사이의 무작위 값 리턴
+const getRandomNumber = (start, end) => {};
+
+export { getRandomNumber };
---
+++ src/utils/random/random.test.js
@@ -0,0 +1,25 @@
+import { ErrorMessage } from "../message/errorMessage";
+import { getRandomNumber } from "./random";
+
+describe("getRandomNumber 유효성 체크", () => {
+  test("parameter 값 : start = end => return start", () => {
+    const number = getRandomNumber(0, 0);
+    expect(number).toBe(0);
+  });
+  test("parameter 값 : start < end => return start ~ end 사이 값", () => {
+    const start = 0;
+    const end = 10;
+    const number = getRandomNumber(start, end);
+    expect(number).toBeGreaterThanOrEqual(start).toBeLessThanOrEqual(end);
+  });
+  test("parameter 값 : start > end => IncorrectParameterError", () => {
+    const getRandomNumberError = () => getRandomNumber(10, 0);
+    const errorMessage = ErrorMessage.incorrectParameterErrorMessage();
+    expect(getRandomNumberError).toThrow(`[ERROR] ${errorMessage}`);
+  });
+  test("parameter 값 : 숫자가 아닐 경우 => IncorrectFormatError", () => {
+    const getRandomNumberError = () => getRandomNumber("10", "0");
+    const errorMessage = ErrorMessage.incorrectFormatErrorMessage();
+    expect(getRandomNumberError).toThrow(`[ERROR] ${errorMessage}`);
+  });
+});
