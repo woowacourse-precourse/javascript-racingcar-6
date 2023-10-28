46c1b9cb1723b46060c857b1ca318ae026ed1202
noodle <noodle>
2023-10-29 02:10:52 +0900

    refactor: 상수 처리

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,24 @@
+/** 메시지 */
+export const INPUT_MESSAGES = Object.freeze({
+  CAR_NAMES: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+  ATTEMPTS: '시도할 횟수는 몇 회인가요?\n',
+});
+
+export const OUTPUT_MESSAGES = {
+  WINNER_PREFIX: '최종 우승자 : ',
+};
+
+export const ERROR_MESSAGES = Object.freeze({
+  PREFIX: '[ERROR]',
+  INVALID_CAR: '유효한 입력값이 아닙니다.',
+  INVALID_ATTEMPTS: '유효한 숫자를 입력해주세요.',
+});
+
 /** 전진 여부를 결정하는 최소값 */
 export const FORWARD_THRESHOLD = 4;
 
 export const CONSTANTS = {
   FORWARD_THRESHOLD,
+  OUTPUT_MESSAGES,
+  INPUT_MESSAGES,
 };
