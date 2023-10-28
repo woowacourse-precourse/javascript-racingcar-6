7433cc370729779a1b3d5b61c2fde9da1b7b493b
BangDori <qudwns8865@naver.com>
2023-10-28 15:47:41 +0900

    feat: 자동차 전진시 최대 전진 거리와 비교하고 갱신하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ class App {
 
     const carsObject = convertArrayToObject(racingCarNames);
 
+    let maxDistance = 0;
     for (let i = 0; i < inputAttemptCount; i++) {
       Object.entries(carsObject).forEach(([carName, carDistance]) => {
         const randomNumber = Random.pickNumberInRange(0, 9);
@@ -27,9 +28,13 @@ class App {
         if (randomNumber >= 4) {
           carsObject[carName] = carDistance + 1;
         }
+
+        maxDistance =
+          carsObject[carName] > maxDistance ? carsObject[carName] : maxDistance;
       });
 
       Console.print(carsObject);
+      Console.print(maxDistance);
     }
   }
 }
