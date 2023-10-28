f7b27f3fa46db3b859a486ae2907b16d930c03ac
Byunggwon Park <82306066+startartart@users.noreply.github.com>
2023-10-28 22:29:17 +0900

    [Feat] 자동차 이름 및 시도횟수를 입력할 수 있다.
    * feat: 자동차 이름 및 시도횟수 입력 구현
    
    * test: 자동차 입력 이름 테스트
    일반적인 상황, 정상적인 처리 확인
    
    * test: 자동차 이름 확인 함수 테스트
    공백 및 다섯글자 초과한 이름은 예외처리
    
    * fix: 테스트 코드 isNumber 함수 수정
    
    * feat: 입력조건 예외처리 함수 구현
    
    * docs: 사용자 입력 및 예외처리 요구사항 확인
    
    * style: Console.print 제거

--- src/App.js
+++ src/App.js
@@ -1,5 +1,49 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
+const GAME_INPUT_CAR_NAME_NUMBER = 1;
+const GAME_INPUT_TRY_NUMBER = 2;
+
 class App {
-  async play() {}
+  async play() {
+    const cars = await this.inputGameInfo(GAME_INPUT_CAR_NAME_NUMBER);
+    const tries = await this.inputGameInfo(GAME_INPUT_TRY_NUMBER);
+  }
+
+  validNameConvention(string) {
+    let count = 0;
+    for (let i = 0; i < string.length; i++) {
+      count++;
+      if (string[i] === ',') {
+        count = 0;
+        continue;
+      }
+      if (count > 5) return false;
+    }
+    return true;
+  }
+
+  isNumber(string) {
+    if (isNaN(string)) return false;
+    return true;
+  }
+
+  async inputGameInfo(number) {
+    let input;
+    switch (number) {
+      case GAME_INPUT_CAR_NAME_NUMBER:
+        input = await Console.readLineAsync(
+          '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+        );
+        if (!this.validNameConvention(input))
+          throw new Error('[ERROR] invalid name convention.');
+        return input.split(',');
+
+      case GAME_INPUT_TRY_NUMBER:
+        input = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+        if (!this.isNumber(input)) throw new Error('[ERROR] not a number.');
+        return Number(input);
+    }
+  }
 }
 
 export default App;
