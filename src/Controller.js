830afebff4769f5fcae88547133fac0d5ed5b41c
hellosehyun <sehyun030526@gmail.com>
2023-10-28 13:02:59 +0900

    chore: README.md 업데이트
    README.md 의 getNumber 관련 내용 업데이트

--- src/Controller.js
+++ src/Controller.js
@@ -5,10 +5,11 @@ class Controller {
     // 참가자 객체 반환 함수
     static async getPlayer() {
         let player = await Console.readLineAsync(Data.MESSAGE.GET_PLAYER);
+        // 공백 제거 및 배열로 분리
         player = player.split(",").map((value) => {
             return value.trim();
         });
-
+        // 배열의 요소를 가지고 객체화
         const PLAYER_OBJ = player.reduce((accumulator, value) => {
             return { ...accumulator, [value.trim()]: 0 };
         }, {});
