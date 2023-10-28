97e030da478ae97826e6d8d3a89836bc7d51106a
lbj23han <ljhan0215@gmail.com>
2023-10-28 21:06:29 +0900

    feat: 사용자 입력 사항

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    try {
+      const CAR_NAMES_INPUT = await Console.readLineAsync(
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+      );
+      const CAR_NAMES = CAR_NAMES_INPUT.split(",").map((name) => name.trim());
+
+      const ROUNDS_INPUT = await Console.readLineAsync(
+        "시도할 횟수는 몇 회인가요?\n"
+      );
+      const ROUNDS = parseInt(ROUNDS_INPUT, 10);
+
+      if (isNaN(ROUNDS) || ROUNDS <= 0) {
+        throw new Error("올바른 숫자를 입력해주세요.");
+      }
+
+      console.log(CAR_NAMES, ROUNDS);
+    } catch (error) {
+      Console.print(`[ERROR] ${error.message}`);
+    }
+  }
 }
 
 export default App;
