ff53567be356a70de6e10b6788d0ca0656ee7522
nocheol <qkdl60@gmail.com>
2023-10-28 18:30:05 +0900

    feat: 게임 진행 기능

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import {MissionUtils} from "@woowacourse/mission-utils";
 ### 기능 목록
 
 1. 자동차 수에 맞는 이름 입력✅
+
 - 이름은 ','로 구분, 5자 이하만, 유효성 검사
 
 2. 시도 횟수 입력✅
 
 - 숫자 입력 및 유효성 검사
 
-3.각 차량의 전진을 저장 기능✅
+3. 각 차량의 전진을 저장 기능✅
 
+4. 게임 진행✅
 
-4. 각 차량에 랜덤한 수 배정
-5. 배정된 수가 4이상이면 전진
-6. 우승자 발표
+- 각 차량에 랜덤한 수 배정
+- 배정된 수가 4이상이면 전진
 
-- 우승자는 한 명 이상일 수 있다. 여러명이면 ','로 구분
+5. 게임 진행도 출력
+6. 우승자 확인
+7. 우승자 발표
+
 
 */
 const {Console, Random} = MissionUtils;
@@ -24,17 +28,21 @@ const {Console, Random} = MissionUtils;
 class App {
   async play() {
     const progress = {};
-    const carNameArray = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)").then((names) =>
-      names.split(",")
-    );
-    carNameArray.forEach((name) => checkNameValidation(name));
-    initProgress(progress, carNameArray);
+    const cars = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)").then((names) => names.split(","));
+    cars.forEach((name) => checkNameValidation(name));
+    initProgress(progress, cars);
 
     const inputCount = await Console.readLineAsync("시도할 횟수:");
     checkInputCount(inputCount);
     const attemptCount = Number(inputCount);
 
-    for (let count = 1; count <= attemptCount; count++) {}
+    for (let count = 1; count <= attemptCount; count++) {
+      for (let car of cars) {
+        const randomNumber = Random.pickNumberInRange(0, 9);
+        if (randomNumber >= 4) progress[car]++;
+      }
+    }
+    console.log(progress);
   }
 }
 
@@ -45,6 +53,7 @@ const initProgress = (process, carNames) => {
     process[car] = 0;
   }
 };
+//TODO 중복 제거
 const checkNameValidation = (name) => {
   if (!name) throw Error("[ERROR]");
   if (name.length > 5) throw Error("[ERROR]");
