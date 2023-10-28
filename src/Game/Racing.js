44551b79e65c0cc73a2b32baf65c19822a7c0efd
hongst222 <tmdxordusdo@gmail.com>
2023-10-28 09:05:38 +0900

    feat: [Racing.js] getWinner() 추가
    getWinner() : 우승자를 구하는 메소드

--- src/Game/Racing.js
+++ src/Game/Racing.js
@@ -27,15 +27,22 @@ class Racing {
 
     getMaxDistance() {
         const cars = this.cars;
-        const distanceArr = cars.map(car=> car.distance.length);
+        const distanceArr = cars.map(car=> car.getDistance());
         
         const maxDistance = distanceArr.reduce((max,current) => {
             return current > max ? current : max
         },distanceArr[0]);
-        
+
         return maxDistance;
     }
-   
+
+    getWinner() {
+        const cars = this.cars;
+        const maxDistance = this.getMaxDistance();
+        const winCar = cars.filter(car => car.getDistance() === maxDistance);
+        return winCar.map(car => car.name).join(', ');
+    
+    }
 }
 
 export default Racing;
