279de702d043c66c06b414533404d0190304579e
simorimi <hori5020@gmail.com>
2023-10-29 15:56:45 +0900

    add input CAR_NAME

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const CAR_NAME = await MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+
+    const CAR_NAME_ARRAY = CAR_NAME.split(",");
+  }
 }
 
 export default App;
