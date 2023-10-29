31d3601883e7ca341528d5efafecd58c6f7c3f95
jonghyunRyu <willowise@naver.com>
2023-10-29 18:02:44 +0900

    시도 횟수 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -19,13 +19,16 @@ class App {
 
     return carMap;
   }
+  
+  async NumberOfAttempts() {
 
-  
-  
+    const attempts = await Console.readLineAsync('시도할 횟수는 몇 회인가요? \n');
+
+    return Number(attempts);
+  }
+
 }
-console.log('test');
 
 const app = new App();
-app.Usercar_input();
 
 export default App;
