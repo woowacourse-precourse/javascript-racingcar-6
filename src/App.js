4fd705f74082175f650ea6cbf7634986f27d95d7
ChoiSenn <quitechoisen@gmail.com>
2023-10-29 15:08:02 +0900

    feat: 자동차 이름 입력 및 예외 처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
+async function getInput(text) {  // 사용자로부터 입력받는 함수
+  try {
+    var userInput = await Console.readLineAsync(text);  // 입력 후 반환
+    return userInput;
+  } catch (error) { 
+    throw new Error("[ERROR] 잘못 된 형식의 입력입니다 !!");
+  }
+}
+
+function isCarNameValid(carName) {  // carName 배열을 받아 배열의 문자열들이 5자 이하인지 확인
+  for(var value of carName) {
+    Console.print(value.length);
+    if(value.length >= 5) {
+      throw new Error("[ERROR] 자동차의 이름이 5글자 이상입니다 !!");
+    }
+  }
+
+  return true;
+}
+
 class App { 
-  async play() {}
+  async play() {
+    
+    var carName = [];
+    var carNameStr = await getInput("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+    carName = carNameStr.split(',');
+
+    isCarNameValid(carName);
+  }
 }
 
 export default App;
