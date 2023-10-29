c8266c438ad46dedef970ad93e6fb0277e3d1a19
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-10-29 18:45:50 +0900

    feat(constants) : 게임에 필요한 메세지 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Console } from "@woowacourse/mission-utils";
+import { ERROR_MESSAGE, GAME_MESSAGE } from "./constants/index.js";
 
 class App {
   async play() {
-    Console.print("Hello world");
+    Console.print(GAME_MESSAGE.START);
+    Console.print(ERROR_MESSAGE.IS_COMMA);
   }
 }
 
