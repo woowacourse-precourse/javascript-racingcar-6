a68c2c7ab7a820ea092faa62ce5ba0c610309681
swarvy <swarvy0826@naver.com>
2023-10-29 13:01:43 +0900

    refactor : InputView.js에 JSDocs 추가

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -3,6 +3,11 @@ import { GRANDPRIX_START_NOTIFICATION } from '../constants/GrandPrixMessage.js';
 import { LapNumberValidator, RacingCarNameValidator } from '../validator/index.js';
 
 const InputView = Object.freeze({
+  /**
+   * @async
+   * @public
+   * @returns {Promise<string>}
+   */
   async readRacingCarNames() {
     const racingCarInput = await Console.readLineAsync(
       GRANDPRIX_START_NOTIFICATION.enterRacingCarName,
@@ -11,6 +16,11 @@ const InputView = Object.freeze({
     return racingCarInput;
   },
 
+  /**
+   * @async
+   * @public
+   * @returns {Promise<string>}
+   */
   async readLapNumber() {
     const lapNumberInput = await Console.readLineAsync(GRANDPRIX_START_NOTIFICATION.enterLapNumber);
     LapNumberValidator.generate(lapNumberInput).validateLapNumber();
