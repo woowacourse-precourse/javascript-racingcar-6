c42081b3e7a75fcb3d224e6e9b01d31a19f634c3
김태희 <>
2023-10-30 01:25:32 +0900

    feat: hasDuplicateName, isValidInput 기능 추가
    이름 입력값에 대한 중복 테스트 확인 기능 추가.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import { Console, Random } from "@woowacourse/mission-utils";
+import { Console } from "@woowacourse/mission-utils";
+
+const NAME_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 
 class App {
   /** 자동차 이름, 횟수 입력 
    * param: -
   */
   nameAndNumberInput = async () => {
-    Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    Console.print(NAME_PROMPT);
     let nameInput = await Console.readLineAsync();
     nameInput = nameInput.toString().split(",");
     Console.print("시도할 횟수는 몇 회인가요?");
@@ -13,6 +15,31 @@ class App {
 
     return [nameInput, numberInput];
   }
+
+  /** 이름 중복 테스트 */
+  hasDuplicateName = (input) => {
+    const result = input.split(",").map(name => name.trim());
+    const uniqueNames = new Set(result);
+
+    return result.length !== uniqueNames.size;
+  }
+
+  /** 입력값 테스트 */
+  isValidInput = (stringInput, ) => {
+    const input = stringInput;
+
+    describe("문자열 테스트", () => {
+      test("이름 입력값의 쉼표(,) 구분 및 중복값 확인", () => {
+
+        expect(this.hasDuplicateName(input)).toBe(True);
+      })
+    });
+
+    describe("숫자 테스트")
+
+  }
+
+
   async play() {
     let [nameList, count] = await this.nameAndNumberInput();
   }
