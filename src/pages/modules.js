45a56f5b8d626d5cbf06d79b811ef38d039ea29e
Dobbymin <gmin0701@knu.ac.kr>
2023-10-30 00:06:46 +0900

    [FEAT](modules): 파일 모듈화

---
+++ src/pages/modules.js
@@ -0,0 +1,6 @@
+import inputRacingCarName from '../components/inputRacingCarName.js';
+import inputTryNumber from '../components/inputTryNumber.js';
+import printGameWinner from '../components/printGameWinner.js';
+import randomNumber from '../components/randomNumber.js';
+
+export { inputRacingCarName, inputTryNumber, printGameWinner, randomNumber };
