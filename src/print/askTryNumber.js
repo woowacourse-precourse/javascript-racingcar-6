5ef10305d0b0de02b01f81b00f57306556990578
1eecan <busyx2modernsociety@gmail.com>
2023-10-29 23:29:29 +0900

    feat: askTryNumber.js 작성
    *  이동의 횟수를 묻는 문구를 출력하는 함수 추가
    * Message.js TRY_NUMBER -> ASK_TRY_NUMBER 로 수정

---
+++ src/print/askTryNumber.js
@@ -0,0 +1,8 @@
+import { Console } from "@woowacourse/mission-utils";
+import MESSAGES from "../constants/Messages.js";
+
+const askTryNumber = () => {
+  Console.print(MESSAGES.ASK_TRY_NUMBER);
+};
+
+export default askTryNumber;
