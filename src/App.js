5be34c7b9964f3215e5235446889f9846f6b83ee
lbj23han <ljhan0215@gmail.com>
2023-10-29 14:03:13 +0900

    refactor: App.js Promise 대신 직접 에러를 던지도록 수정

--- src/App.js
+++ src/App.js
@@ -3,27 +3,23 @@ import Race from "./Race.js";
 
 class App {
   async play() {
-    try {
-      const CAR_NAMES_INPUT = await Console.readLineAsync(
-        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) : \n"
-      );
-      const CAR_NAMES = CAR_NAMES_INPUT.split(",").map((name) => name.trim());
+    const CAR_NAMES_INPUT = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) : \n"
+    );
+    const CAR_NAMES = CAR_NAMES_INPUT.split(",").map((name) => name.trim());
 
-      const ROUNDS_INPUT = await Console.readLineAsync(
-        "시도할 횟수는 몇 회인가요?\n"
-      );
-      const ROUNDS = parseInt(ROUNDS_INPUT, 10);
+    const ROUNDS_INPUT = await Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요?\n"
+    );
+    const ROUNDS = parseInt(ROUNDS_INPUT, 10);
 
-      if (isNaN(ROUNDS) || ROUNDS <= 0) {
-        throw new Error("올바른 숫자를 입력해주세요.");
-      }
-
-      console.log(CAR_NAMES, ROUNDS);
-      const RACE = new Race(CAR_NAMES, ROUNDS);
-      RACE.run();
-    } catch (error) {
-      Console.print(`[ERROR] ${error.message}`);
+    if (isNaN(ROUNDS) || ROUNDS <= 0) {
+      throw new Error("[ERROR] 올바른 숫자를 입력해주세요.");
     }
+
+    console.log(CAR_NAMES, ROUNDS);
+    const RACE = new Race(CAR_NAMES, ROUNDS);
+    RACE.run();
   }
 }
 
