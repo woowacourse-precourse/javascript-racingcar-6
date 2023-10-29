a3a840d739fec8adbde70fae7cba3ef48554b987
at8in <at8in@naver.com>
2023-10-30 05:23:50 +0900

    refactor(views): InputView 객체로 변경

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,14 +1,14 @@
 import { Console } from '@woowacourse/mission-utils';
 import { INPUT_MESSAGE } from '../constants/views.js';
 
-class InputView {
+const InputView = {
   inputCarNames() {
     return Console.readLineAsync(INPUT_MESSAGE.carNames);
-  }
+  },
 
   inputMoveCount() {
     return Console.readLineAsync(INPUT_MESSAGE.moveCount);
-  }
-}
+  },
+};
 
 export default InputView;
