620115d50bca73562c61be96ea20143b57e755a7
bmj0053 <bmj05378@gmail.com>
2023-10-29 21:14:56 +0900

    refactor: 불필요한 주석 제거 및 변수명 변경

--- src/App.js
+++ src/App.js
@@ -2,18 +2,13 @@ import { Console, MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
   async play() {
-    // 경주할 자동차 이름 입력
     const cars = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
-    // 시도 횟수 입력
     const counts = Number(await Console.readLineAsync('시도할 횟수는 몇 회인가요?'));
     
-    // 형식 확인
     if (typeof(cars) !== 'string' || typeof(counts) !== 'number') {
       throw new Error('[ERROR] 잘못된 형식입니다.')
     };
     
-    
-    // 길이 확인
     const carList = cars.split(',').map(String);
     carList.map(car => {
       if (car.length > 5) {
@@ -21,42 +16,40 @@ class App {
       }
     });
     
-    // 반복
     let count = 0;
-    let carsForward = [];
-    carList.map(car => carsForward.push(0));
+    let forwardCounts = [];
+    carList.map(car => forwardCounts.push(0));
 
     Console.print('');
     Console.print('실행 결과');
 
     while (count !== counts) {
       count++;
-      // 리스트 순회하며 랜덤 숫자 선택
+    
       carList.map((car, idx) => {
-        const random = MissionUtils.Random.pickNumberInRange(0, 9);
-        if (random >= 4) {
-          carsForward[idx]++
+        const randomNum = MissionUtils.Random.pickNumberInRange(0, 9);
+        if (randomNum >= 4) {
+          forwardCounts[idx]++
         };
-        Console.print(car + ' : ' + '-'.repeat(carsForward[idx]))
+        Console.print(car + ' : ' + '-'.repeat(forwardCounts[idx]));
       })
-      Console.print('')
+      Console.print('');
     }
     
-    // 최댓값
-    let mx = 0;
+    let maxForward = 0;
     for (let i = 0; i < carList.length; i++) {
-      if (mx < carsForward[i]) {
-        mx = carsForward[i]
+      if (maxForward < forwardCounts[i]) {
+        maxForward = forwardCounts[i];
       };
-    }
-    const winners = []; // 우승자 배열
+    };
 
-    for (let i = 0; i < carsForward.length; i++) {
-      const element = carsForward[i];
-      if (element === mx) {
+    const winners = [];
+    for (let i = 0; i < forwardCounts.length; i++) {
+      if (forwardCounts[i] === maxForward) {
         winners.push(carList[i]);
-      }
-    }
+      };
+    };
+    
     Console.print('최종 우승자 : ' + winners);
   }
 };
