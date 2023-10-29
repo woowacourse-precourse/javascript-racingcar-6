f77f252d120b5dadfe6144f073efc0298c5afe67
chysis <chysiss@naver.com>
2023-10-29 20:12:09 +0900

    feat: Messages 객체 추가 구현

--- src/Messages.js
+++ src/Messages.js
@@ -3,6 +3,7 @@ const Messages = {
     '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
   INPUT_TRY_NUMBER: '시도할 횟수는 몇 회인가요?\n',
   PRINT_RACING_RESULT: '실행 결과',
+  PRINT_WINNERS: '최종 우승자 : ',
   ERROR_DEFAULT: '[ERROR] 에러가 발생했습니다.\n',
   ERROR_CAR_NAME_LENGTH: '[ERROR] 자동차 이름은 5자 이하로 입력해주세요.\n',
   ERROR_TRY_NUMBER: '[ERROR] 1 이상의 숫자를 입력해주세요.\n',
