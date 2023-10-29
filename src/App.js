b421c93b3bc2b60907e7866373715868f20e0fb8
Jaehyeok <milkboy2564@naver.com>
2023-10-29 22:48:04 +0900

    feat: 기능별 메서드 정의

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
+  constructor() {}
+
   async play() {}
+
+  receiveCarNameInput() {}
+
+  receiveTryCountInput() {}
+
+  generateRandomNumber() {}
+
+  accumulateDistanceEachCar() {}
+
+  printDistanceResult() {}
+
+  calculateDistance() {}
+
+  printWinnerResult() {}
+
+  validateUserInput() {}
 }
 
 export default App;
