30a01c187c3faf1f6e6dd6bbf82f8d21810640b6
kwoncha <rnjsckgus@gmail.com>
2023-10-29 23:01:14 +0900

    refactor: 불필요한 공백 제거, 가독성 개선

--- src/playing/checkList.js
+++ src/playing/checkList.js
@@ -4,17 +4,11 @@ const validateNames = (humanInputName) => {
   const trimmedNameArray = nameArray.map((name) => {
     const trimmedName = name.trim();
 
-    if (trimmedName.length === 0) {
-      throw new Error('[ERROR] 공백을 제외하고 이름을 입력해주세요.');
-    }
-
-    if (trimmedName.length > 5) {
-      throw new Error('[ERROR] 5자리 이하 이름만 입력해 주세요');
-    }
+    if (trimmedName.length === 0) throw new Error('[ERROR] 공백을 제외하고 이름을 입력해주세요.');
+    if (trimmedName.length > 5) throw new Error('[ERROR] 5자리 이하 이름만 입력해 주세요');
 
     return trimmedName;
   })
-
 
   return trimmedNameArray;
 }
