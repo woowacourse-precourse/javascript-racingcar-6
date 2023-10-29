f72bd717586fc03fb0d4c7da689499115de20e33
김진호 <rlawlsgh1227@gmail.com>
2023-10-29 17:54:40 +0900

    test: 0에서 9사이 랜덤 수 생성 검증

--- __tests__/PlayGameTest.js
+++ __tests__/PlayGameTest.js
@@ -1,5 +1,5 @@
-import RacingGame from "../src/RacingGame";
-import GameUtlis from "../src/utils/GameUtils";
+import RacingGame from "../src/RacingGame.js";
+import GameUtlis from "../src/utils/GameUtils.js";
 
 describe('게임 진행에 관한 테스트입니다', () => {
   test('시도 횟수만큼 게임이 반복됩니다.', () => {
@@ -14,5 +14,16 @@ describe('게임 진행에 관한 테스트입니다', () => {
 
     // then
     expect(racingGame.tryOneAttempt).toBeCalledTimes(input);
+  });
+
+  test('0부터 9사이의 무작위의 값이 나온다.', () => {
+    // given
+    const answers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    // when
+    const randomNumber = GameUtlis.generateRandomNumberFromZeroToNine();
+
+    // then
+    expect(answers).toContain(randomNumber);
   })
 })
