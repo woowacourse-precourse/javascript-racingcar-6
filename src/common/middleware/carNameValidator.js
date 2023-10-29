124c0ad21719d5764689cc6e005b3c05b8d3f713
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-10-30 04:00:42 +0900

    feat: 사용자가 입력한 자동차 이름의 유효성 검사 기능 추가

---
+++ src/common/middleware/carNameValidator.js
@@ -0,0 +1,18 @@
+const isUniqueCarName = (input) => {
+  const names = input.split(',').map(name => name.trim());
+  return names.length === new Set(names).size;
+};
+
+// TODO: 파라미터로 input.min & input.max 받고
+const isValidInputCarName = (input) => {
+  const carNames = input.split(',').map(name => name.trim().toUpperCase());
+
+  return (
+    carNames.length > 1 &&
+    carNames.every(name => name.length <= 5) &&
+    !carNames.includes('') &&
+    isUniqueCarName(carNames.join(','))
+  );
+};
+
+export default isValidInputCarName;
