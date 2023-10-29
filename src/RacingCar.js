38c1efad9c11d62c8f726ba3116afea44655350e
Boeun <boeun0904@naver.com>
2023-10-29 17:50:17 +0900

    feat(RacingCar): get random number

--- src/RacingCar.js
+++ src/RacingCar.js
@@ -1,6 +1,14 @@
+import { Random } from '@woowacourse/mission-utils';
+
 export default class RacingCar {
   constructor(name) {
     this.name = name;
     this.distance = 0;
+  }
+
+  moveForward() {}
+
+  getRandomNumber(start, end) {
+    return Random.pickNumberInRange(start, end);
   }
 }
