46393a1f23d8fd3ece02f5e91f7dabe0535357d2
kwoncha <rnjsckgus@gmail.com>
2023-10-29 23:35:33 +0900

    refactor: 필요 없는 출력 삭제

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ const { startGame } = require('./playing/startGame');
 
 class App {
   async play() {
-    MissionUtils.Console.print('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
     await startGame();
   }
 }
--- src/playing/startGame.js
+++ src/playing/startGame.js
@@ -4,11 +4,10 @@ const { runRace } = require('./runRace');
 
 
 const startGame = async () => {
-  const humanInputName = await MissionUtils.Console.readLineAsync();
+  const humanInputName = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
   const validatedNames = validateNames(humanInputName);
 
-  MissionUtils.Console.print('시도할 횟수는 몇 회인가요 ?\n');
-  const racingTrackInput = await MissionUtils.Console.readLineAsync();
+  const racingTrackInput = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요 ?\n');
   const racingTrack = validateParseNumber(racingTrackInput);
 
   runRace(racingTrack, validatedNames);
