c5ca5037b3e2925dbb23e980d67fcf3bc05bbdeb
leelsj2007 <leelsj2007@gmail.com>
2023-10-29 20:33:42 +0900

    feat : (입력기능) 자동차 이름이 5글자를 초과했을 경우 예외 처리 기능

--- src/components/logic/input_validation.js
+++ src/components/logic/input_validation.js
@@ -1,7 +1,12 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 const carNameValidation = (carName) =>{
-    
+    for (let i = 0; i < carName.length; i++) {
+        if (carName[i].length > 6) {
+            MissionUtils.Console.print('[ERROR] 자동차 이름이 5를 초과했습니다.');
+            throw new Error("[ERROR]");
+        }
+    }
 };
 
 export default carNameValidation;
