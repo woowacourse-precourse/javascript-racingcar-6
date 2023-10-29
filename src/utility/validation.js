06ae63a56af1bc5dedf5adeffcb7a82aa66387aa
ImxYJL <allensain14@gmail.com>
2023-10-29 22:53:46 +0900

    fix: 0~9 범위의 수인지 체크하는 함수를 4~9 범위로 변경

--- src/utility/validation.js
+++ src/utility/validation.js
@@ -1,14 +1,13 @@
 import { ERROR_MESSAGE } from '../constant/message';
 
-export const isWithinZeroToNine = (number) => number >= 0 && number <= 9;
+export const isWithinFourToNine = (number) => number >= 4 && number <= 9;
 
 export const isDecimalChar = (character) => /^[0-9]$/.test(character);
 
 export const isValidateNumber = (string) => {
   Array.from(string).forEach((digit) => {
-    if (!isDecimalChar(digit)) {
+    if (!isDecimalChar(digit))
       throw new Error(ERROR_MESSAGE.wrongGameCountInput);
-    }
   });
 };
 
