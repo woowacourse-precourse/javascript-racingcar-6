0248eb2714f29000ccdf26ce4a8f7cf3c15a30b7
swarvy <swarvy0826@naver.com>
2023-10-29 12:35:16 +0900

    refactor : RacingCar.js에 JSDocs 추가

--- src/model/RacingCar.js
+++ src/model/RacingCar.js
@@ -3,14 +3,25 @@ export default class RacingCar {
 
   #status = 0;
 
+  /**
+   * @constructor
+   * @param {string} name 자동차의 이름
+   */
   constructor(name) {
     this.#name = name;
   }
 
+  /**
+   * @public
+   */
   moveRacingCar() {
     this.#status += 1;
   }
 
+  /**
+   * @public
+   * @returns {{name: string, status: number}} 레이싱카의 현재 상태
+   */
   getRacingCarStatus() {
     return { name: this.#name, status: this.#status };
   }
