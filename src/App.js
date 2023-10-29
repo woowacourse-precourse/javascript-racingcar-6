80c323e339bac548c760fe3d4835273da8e56af7
kwani6684 <jack0910@naver.com>
2023-10-29 13:55:26 +0900

    feat: 입력받은 이름을 쉼표로 구분하여 각각 객체로 저장
    carArray를 생성해 car객체를 배열로 저장

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class Car {
   constructor(name) {
     this.name = name;
     this.distance = '';
   }
 }
+class StartGame{
+  async getCarName(){
+    let input = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
+    let nameList = input.split(',');
+    let carArray=[];
+    nameList.forEach((carName)=>{
+      let car = new Car(carName);
+      carArray.push(car);
+    })
+  }
+}
 
 class App {
   async play() {}
+  
 }
 
 export default App;
