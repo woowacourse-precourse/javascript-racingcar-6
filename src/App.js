cc32b41c8b812058bdafe79e10444ed7076e9559
jeongminsang <zjzj@hanmail.net>
2023-10-30 01:20:59 +0900

    feat: 예외처리 추가

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    const carNames = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
-    const repeatNumber = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+    const carNames = await MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    if (carNames.split(",").some((name) => name.trim().length > 5)) {
+      throw new Error("[ERROR] 자동차 이름은 최대 5자만 가능합니다.");
+    }
+    const repeatNumber = await MissionUtils.Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요?\n"
+    );
+    if (Number.isNaN(Number(repeatNumber))) {
+      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    }
     MissionUtils.Console.print("실행 결과");
-    const carNameArray = carNames.split(',');
-    const carObjects = carNameArray.map(name => ({ [name.trim()]: 0 }));
+    const carNameArray = carNames.split(",");
+    const carObjects = carNameArray.map((name) => ({ [name.trim()]: 0 }));
 
-    for( let i = 0 ; i < repeatNumber ; i++){
+    for (let i = 0; i < repeatNumber; i++) {
       for (const carObject of carObjects) {
         for (const carName in carObject) {
-          this.generateRandomNumber(carObject)
+          this.generateRandomNumber(carObject);
           const ResultNumber = this.repeatDash(carObject, carName);
           MissionUtils.Console.print(carName + " : " + ResultNumber);
         }
@@ -33,23 +43,23 @@ class App {
         }
       }
     }
-    MissionUtils.Console.print("최종 우승자 : " + winCarArr)
+    MissionUtils.Console.print("최종 우승자 : " + winCarArr);
   }
 
-  generateRandomNumber(carObject) {        
+  generateRandomNumber(carObject) {
     const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
-    if(randomNumber > 3){
+    if (randomNumber > 3) {
       for (const carName in carObject) {
         carObject[carName]++;
       }
     }
-    return carObject
+    return carObject;
   }
 
   repeatDash(carObject, carName) {
-    let ResultNumber = '';
-    for( let i = 0 ; i < carObject[carName] ; i++ ){
-      ResultNumber = ResultNumber.concat('-');
+    let ResultNumber = "";
+    for (let i = 0; i < carObject[carName]; i++) {
+      ResultNumber = ResultNumber.concat("-");
     }
     return ResultNumber;
   }
