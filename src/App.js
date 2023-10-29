224213632e1e7e415847a11959df03b579c71040
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-10-30 03:54:17 +0900

    feat: 게임을 시작하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import InputView from './view/InputView.js';
+
 class App {
-  async play() {}
+  constructor() {
+    this.game = new InputView();
+  }
+
+  async play() {
+    await this.game.startGame();
+  }
 }
 
 export default App;
