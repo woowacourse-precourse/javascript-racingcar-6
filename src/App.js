2bf7959d4d44817b158d7f808d4a19752982694a
Seoan Yoon <earthdyoon@gmail.com>
2023-10-30 02:20:34 +0900

    feat: 입력받은 이름으로 Car 인스턴스 배열 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Screen from './Screen';
+import Car from './Car';
+
 class App {
-  async play() {}
+  async play() {
+    const names = Screen.inputNames();
+    const cars = names.map((name) => new Car(name));
+  }
 }
 
 export default App;
