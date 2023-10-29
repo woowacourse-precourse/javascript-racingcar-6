88a7cc687853752a5fa0b23ae4ab0f1d57770a32
JungHun98 <j7papa@naver.com>
2023-10-29 23:56:02 +0900

    feat: 시도 횟수 유효성 검사 함수 구현

---
+++ src/modules/isValidAttempNumber.js
@@ -0,0 +1,7 @@
+const isValidAttempNumber = (string) => {
+  const numberExpression = /^[0-9]+$/;
+  const isNumber = numberExpression.test(string);
+  return isNumber;
+};
+
+export default isValidAttempNumber;
