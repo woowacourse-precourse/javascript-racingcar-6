a8ea3028952aff4e2fe170ee460d226d20979abd
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-10-29 16:24:07 +0900

    feat : 각 이름이 5자 초과일때 예외처리, 자동차 경주 round 실행 기능
    racingStart() : racingGame이 시도할 round를 가지고있기에 isFinish 함수를 통해 게임이 끝났는지 안끝났는지 받아옴.
    이후 1round씩 진행하고 매 라운드마다 결과 출력

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import RacingGame from './RacingGame';
-import InputReader from './View/InputReader';
+import Car from './Car.js';
+import RacingGame from './RacingGame.js';
+import InputReader from './View/InputReader.js';
+import OutputView from './View/OutputView.js';
+import { Console } from '@woowacourse/mission-utils';
 
 export default class App {
   #inputReader;
+  #outputView;
   #racingGame;
 
   constructor() {
     this.#inputReader = new InputReader();
+    this.#outputView = new OutputView();
     this.#racingGame = null;
   }
 
@@ -18,8 +23,21 @@ export default class App {
   }
 
   async ready(carNames, tryRound) {
+    if (carNames.split(',').some((carName) => carName.length > 5)) {
+      throw new Error('[ERROR] 이름은 5자 이하로 적어주세요 !');
+    }
     const carArray = carNames.split(',').map((name) => new Car(name));
 
     this.#racingGame = new RacingGame(carArray, tryRound);
+
+    await this.racingStart();
+  }
+
+  async racingStart() {
+    while (!this.#racingGame.isFinish()) {
+      this.#racingGame.roundStart();
+      const roundResult = this.#racingGame.getRoundResult();
+      this.#outputView.printRoundResult(roundResult);
+    }
   }
 }
