54adc9230cd2817eab1bb505b57d112eda008746
Boeun <boeun0904@naver.com>
2023-10-29 17:58:02 +0900

    feat(RacingCar): increase car distance increase car distance if random no is 4 or more

--- src/RacingCar.js
+++ src/RacingCar.js
@@ -1,14 +1,24 @@
 import { Random } from '@woowacourse/mission-utils';
 
 export default class RacingCar {
+  static RAND_START = 0;
+  static RAND_END = 9;
+
   constructor(name) {
     this.name = name;
     this.distance = 0;
   }
 
-  moveForward() {}
+  moveForward() {
+    const randomNumber = RacingCar.getRandomNumber(
+      RacingCar.RAND_START,
+      RacingCar.RAND_END
+    );
 
-  getRandomNumber(start, end) {
+    if (randomNumber >= 4) this.distance += 1;
+  }
+
+  static getRandomNumber(start, end) {
     return Random.pickNumberInRange(start, end);
   }
 }
