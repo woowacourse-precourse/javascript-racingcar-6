f5f80ed189c57ed905ceff8373cba8789c1df0de
Geeehyun <wkdwl578@gmail.com>
2023-10-29 20:45:15 +0900

    Refactor: user 입력 부분 코드 정리

--- src/App.js
+++ src/App.js
@@ -2,22 +2,32 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 
 class App {
-  userInput;
+  carName;
+  numOfGame
   
   //User 입력부분
   async getUserInput() {
+    this.getCarName(()=>this.getNumber());
+  }
+
+  async getCarName(callback) {
     const inputValue = await MissionUtils.Console.readLineAsync('경주 할 자동차 이름(이름은 쉼표(,)기준으로 구분)');
-    this.userInput = inputValue.split(',');
-    return this.checkUserInput(this.userInput);
+    this.carName = inputValue.split(',');
+    if(this.carName.some((value)=>value == '')) throw new Error('[ERROR] 입력값이 없습니다.');
+    if(this.carName.some((value)=>value.length>5)) throw new Error('[ERROE] 자동차 이름은 5자 이하로만 입력해주세요.');
+    MissionUtils.Console.print(`자동차 이름은 : ${this.carName}`);
+    callback();
   }
 
-  checkUserInput(userInput) {
-    if(userInput.some((value)=>value == '')) throw new Error('[ERROR] 입력값이 없습니다.');
-    if(userInput.some((value)=>value.length>5)) throw new Error('[ERROE] 자동차 이름은 5자 이하로만 입력해주세요.');
-    MissionUtils.Console.print(this.userInput);
+  async getNumber() {
+    const inputValue = await MissionUtils.Console.readLineAsync('시도할 횟수');
+    if(isNaN(inputValue)) throw new Error('[ERROR] 숫자만 입력해주세요.')
+    if(inputValue == '' || inputValue == 0) throw new Error('[ERROR] 시도할 횟수는 1회 이상이어야합니다.');
+    this.numOfGame = inputValue;
+    MissionUtils.Console.print(`시도할 회수는 : ${this.numOfGame}회`);
   }
 
-
+  
 
   async play() {
     this.getUserInput();
