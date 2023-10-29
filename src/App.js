83b1be9a97d5657e5aa64140e7eeaed8dde3a5a6
starcradle101 <dreamerdev12@gmail.com>
2023-10-29 13:39:54 +0900

    :bulb: Chore: 기능 구현에 필요한 메서드 주석 추가
    미션 구현에 필요한 메서드 목록 주석 추가(추후 삭제 예정)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 class App {
   async play() {}
+
+  async getPlayerCarsInput() {
+    const ANSWER = await Console.readLineAsync();
+  }
 }
 
 export default App;
+
+// 필요한 함수들
+// getPlayerCarsInput
+// isPlayerCarNameValidated
+// getPlayerTryNumberInput
+// isPlayerTryNumberValidated
+// startRace
+// shouldCarMoveForward
+// addCarMoveProgressBar
+// checkWinners
+// printRaceStatus
+// printWinners
