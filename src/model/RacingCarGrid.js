6b9c9969d3b40a16fe38f3c4448f66cfe2cc5c1b
swarvy <swarvy0826@naver.com>
2023-10-29 12:45:59 +0900

    refactor : RacingCarGrid.js에 JSDocs 추가

--- src/model/RacingCarGrid.js
+++ src/model/RacingCarGrid.js
@@ -4,12 +4,23 @@ import { SYMBOLS } from '../constants/Symbols.js';
 import { GRANDPRIX_MAGICNUMBER } from '../constants/GrandPrixOption.js';
 
 export default class RacingCarGrid {
+  /**
+   * @private
+   * @type {RacingCar[]}
+   */
   #racingCarList;
 
+  /**
+   * @constructor
+   * @param names InputView로부터 입력받은 레이싱카 이름
+   */
   constructor(names) {
     this.#racingCarList = Array.from(names.split(SYMBOLS.comma), (name) => new RacingCar(name));
   }
 
+  /**
+   * @public
+   */
   setRacingGrid() {
     const randomMovementResults = RandomNumberGenerator.generate(this.#racingCarList.length);
     randomMovementResults.forEach((value, idx) => {
@@ -19,10 +30,18 @@ export default class RacingCarGrid {
     });
   }
 
+  /**
+   * @public
+   * @returns {{name: string, status: number}[]} 레이싱카들의 현재 상태 반환
+   */
   getRacingGrid() {
     return Array.from(this.#racingCarList, (racingCar) => racingCar.getRacingCarStatus());
   }
 
+  /**
+   * @public
+   * @returns {string} 가장 멀리 간 레이싱카를 반환(여러개 일 시 쉼표로 구분하여 반환)
+   */
   getPodium() {
     const racingGrid = this.getRacingGrid();
     const raceStatusList = racingGrid.map(({ status }) => status);
