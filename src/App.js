406a6766887a7ae43e1820a30d5298a918bed53c
bmj0053 <bmj05378@gmail.com>
2023-10-29 20:59:58 +0900

    feat: 반복문, 랜덤 숫자 선택, 최댓값 찾는 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Console } from '@woowacourse/mission-utils';
+import { Console, MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
   async play() {
@@ -23,12 +23,45 @@ class App {
     
     // 반복
     let count = 0;
+    let carsForward = [];
+    carList.map(car => carsForward.push(0));
+    // Console.print(carsForward)
+    Console.print('실행 결과');
+
     while (count !== counts) {
       count++;
       Console.print(count)
+      // 리스트 순회하며 랜덤 숫자 선택
+      carList.map((car, idx) => {
+        const random = MissionUtils.Random.pickNumberInRange(0, 9);
+        if (random >= 4) {
+          // Console.print(car + ' 전진!');
+          carsForward[idx]++
+          // Console.print(carsForward + ' 전진!');
+        };
+        Console.print(car + ' : ' + '-')
+      })
     }
-    Console.print("종료")
+    
+    // 최댓값
+    let mx = 0;
+    for (let i = 0; i < carList.length; i++) {
+      if (mx < carsForward[i]) {
+        mx = carsForward[i]
+      };
+    }
+    const winners = []; // 우승자 배열
 
+    for (let i = 0; i < carsForward.length; i++) {
+      const element = carsForward[i];
+      // Console.print('여기는 왔니?'+mx);
+      if (element === mx) {
+        // Console.print(carList[i]+'우승');
+        winners.push(carList[i]);
+        // Console.print('winners : '+winners)
+      }
+    }
+    Console.print('최종 우승자 : ' + winners);
   }
 };
 
