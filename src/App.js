0c168104f6d7d551fb15429cc62efc4107f0607d
dalsu0222 <dalsu0222@sju.ac.kr>
2023-10-30 02:54:40 +0900

    feat: Add main logic to initiate race

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 // ** Car 클래스: 각 자동차의 상태 관리 **
 class Car {
-  //Car 클래스의 생성자 함수
+  // Car 클래스의 생성자 함수
   constructor(name) {
     this.name = name; // 자동차의 이름
     this.position = 0; // 자동차의 현재 위치 (전진한 횟수)
@@ -37,7 +37,7 @@ class Race {
     }
   }
 
-  //모든 자동차의 이름과 위치 출력
+  // 모든 자동차의 이름과 위치 출력
   async printRaceResult() {
     for (const car of this.cars) {
       await car.printPosition();
@@ -45,11 +45,65 @@ class Race {
     await MissionUtils.Console.print('\n');
   }
 
-  //경주가 끝난 후 최종 우승자 결정
+  // 경주가 끝난 후 최종 우승자 결정
   findWinners() {
     const maxPosition = Math.max(...this.cars.map(car => car.position));
     return this.cars.filter(car => car.position === maxPosition).map(car => car.name);
   }
 }
+
+
+// ** App 클래스: 애플리케이션의 메인 로직 담당 **
+class App {
+  // 사용자로부터 입력을 받고 경주를 진행
+  async start() {
+    try {
+      const cars = await this.getCars();
+      const tries = await this.getTries();
+      const race = new Race(cars, tries); // 경주 생성
+      await race.run(); // 경주 시작
+      const winners = race.findWinners(); // 최종 우승자 결정
+      await MissionUtils.Console.print("최종 우승자 : " + winners.join(", ") + "\n");
+    } catch (error) {
+      await MissionUtils.Console.print(error.message + "\n");
+      throw error;    // ★ 에러메세지를 다시 던져줘야 테스트에서 예외를 캐치할 수 있음.
+    }
+  }
+
+  // 사용자로부터 자동차 이름 입력받기
+  async getCars() {
+    const input = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+    const names = input.split(",").map(name => name.trim());
+
+    if (names.some(name => name.length > 5)) {
+      throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+    }
+
+    return names.map(name => new Car(name));
+  }
+
+  // 사용자로부터 시도할 횟수 입력받기
+  async getTries() {
+    const input = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+    const tries = parseInt(input, 10);
+
+    // 나중에 추가할 테스트 케이스
+    /*
+    if (isNaN(tries) || tries <= 0) {
+      throw new Error("[ERROR] 시도 횟수는 양의 정수여야 합니다.");
+    }
+    */
+
+    return tries;
+  }
+}
+
+// ** 애플리케이션의 시작점: App 인스턴스를 생성하고 게임을 시작시키기 **
+async function main() {
+  const app = new App();
+  await app.start();
+}
+
+main();
 
 export default App;
