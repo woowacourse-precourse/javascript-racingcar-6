fc2bbeb7e4e95c0c51c026b7083e3640d3406345
thumbthing <ilscc0902@gmail.com>
2023-10-29 17:01:58 +0900

    feat: 에러코드 추가, 중복 console 출력 삭제, appErrorHandler 에서 최종 error 메시지 출력

--- src/util/error/errorCode.js
+++ src/util/error/errorCode.js
@@ -1,6 +1,7 @@
 const errorData = [
   ['noRacerName', '이름이 없는 자동차가 존재합니다.'],
   ['tooLongRacerName', '자동차의 이름이 너무 깁니다.'],
+  ['hasDuplicateName', '중복된 자동차명이 존재합니다.'],
   ['inputIsZero', '0은 입력할 수 없습니다.'],
   ['inputIsNaN', '숫자가 잘못된 형식입니다.'],
 ];
--- src/util/error/errorhandler.js
+++ src/util/error/errorhandler.js
@@ -1,16 +1,20 @@
 import { consolePrint } from '../libraryFeatures/consoleHandler.js';
 import errorList from './errorCode.js';
 
-export function gameErrorhandler(code) {
+export function occuredErrorhandler(code) {
   const occuredError = errorList.get(code);
 
   const errorMessage = occuredError !== undefined ? occuredError : '예상치 못한 에러가 발생했습니다.';
 
-  consolePrint(`[ERROR] ${errorMessage}`);
   return Promise.reject(errorMessage);
 }
 
 export function defaultErrorHandler(error) {
-  consolePrint(`[ERROR] ${error}`);
   return Promise.reject(error);
+}
+
+export function appErrorHandler(error) {
+  const errorMessage = new Error(`[ERROR] ${error}`);
+  consolePrint(errorMessage);
+  return Promise.reject(errorMessage);
 }
