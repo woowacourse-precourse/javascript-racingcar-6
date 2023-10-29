7002ad84eb34a6dc220a648b6bc51eab6d512f74
Hyuk <poiu03290@gmail.com>
2023-10-29 17:48:26 +0900

    fix: result print

--- src/game/roundPrint.js
+++ src/game/roundPrint.js
@@ -1,10 +1,18 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
-import advanceCondition from '../data/advanceCondition.js';
-
 export default function roundPrint(carArr, scoreArr, car) {
-    if(advanceCondition()) {
+    const number = MissionUtils.Random.pickNumberInRange(0, 9);
+    if(number && advanceCondition(number)) {
         scoreArr[car] += '-';
     }
+
     MissionUtils.Console.print(`${carArr[car]} : ${scoreArr[car]}`);
+}
+
+export async function advanceCondition(number) {
+    if(number < 5) {
+        return false;
+    }
+
+    return true;
 }
