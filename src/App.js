2da25e6a46b16272e79f9438e9d57353acdb6676
gwahyun <60453182+gwahyun@users.noreply.github.com>
2023-10-29 20:04:45 +0900

    Feat: 실행결과를 출력하는 기능 구현
    textResult에 '-' 과정 printTextResult 함수로 분리

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import { Console, Random } from '@woowacourse/mission-utils';
+import { Console } from '@woowacourse/mission-utils';
 import validate from './Validate.js';
 import race from './Race.js';
 
 class App {
   async play() {
-    Console.print('');
     const inputCarName = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요 (이름은 쉼표(,)기준으로 구분)\n',
     );
@@ -19,6 +18,7 @@ class App {
     race.playRace(cars, inputPlayCount);
 
     Console.print(cars);
+    Console.print(`최종 우승자 : ${race.winner(cars)}`);
   }
 }
 
--- src/Race.js
+++ src/Race.js
@@ -1,4 +1,4 @@
-import { Random } from '@woowacourse/mission-utils';
+import { Console, Random } from '@woowacourse/mission-utils';
 
 const createRaceCars = userInput => {
   const cars = [];
@@ -14,15 +14,41 @@ const playRace = (cars, inputPlayCount) => {
     cars.forEach(car => {
       const random = Random.pickNumberInRange(0, 9);
       car.result.push(random);
-      random > 3 && (car.textResult += '-');
+      printTextResult(random, car);
     });
     count++;
   }
 };
 
+const printTextResult = (random, car) => {
+  if (random > 3) car.textResult += '-';
+  Console.print(`${car.name} : ${car.textResult}`);
+};
+
+const winner = cars => {
+  let maxLength = 0;
+  let winner = [];
+
+  cars.forEach(car => {
+    if (car.textResult.length >= maxLength) {
+      maxLength = car.textResult.length;
+    }
+  });
+
+  cars.forEach(car => {
+    if (car.textResult.length === maxLength) {
+      winner.push(car.name);
+    }
+  });
+
+  return winner.join(',');
+};
+
 const race = {
   createRaceCars,
   playRace,
+  printTextResult,
+  winner,
 };
 
 export default race;
