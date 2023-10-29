91cf37e781bed1dccdf1e3b7397bd54f0dca1888
kimmoonju-102 <kimmun3549@naver.com>
2023-10-29 17:14:59 +0900

    feat : 상수 관리 constants 파일 생성

---
+++ src/Racingcar/constants/index.js
@@ -0,0 +1,17 @@
+const GAME_MESSAGE = {
+  carName: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
+  lapCount: "시도할 횟수는 몇 회인가요?",
+  result: "실행 결과",
+  winner: "최종 우승자",
+};
+
+const ERROR = {
+  exception: "[ERROR]",
+  nameLength: "이름은 5자 이하만 가능합니다.",
+  lapNumber: "숫자가 잘못된 형식이거나 0보다 작습니다.",
+};
+
+module.exports = {
+  GAME_MESSAGE,
+  ERROR,
+};
