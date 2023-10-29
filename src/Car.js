7d4cacb26aa3a625b034a3a593cd0fd730c55d51
ChanYong Park <krap5075@naver.com>
2023-10-30 04:24:27 +0900

    refactor: Car 클래스 생성자 수정(멤버변수추가)

---
+++ src/Car.js
@@ -0,0 +1,10 @@
+import { NUM_RANGE } from "./constants/Number";
+class Car {
+  constructor(name) {
+    this.name = name;
+    this.move_times = 0;
+    this.dash = "-";
+  }
+}
+
+export default Car;
