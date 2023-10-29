9cf0bed05f3219b13b39f91de7177cc3eaa08a5f
uraflower <82873315+uraflower@users.noreply.github.com>
2023-10-30 03:39:16 +0900

    test: 랜덤 숫자 생성 테스트 dsecribe로 그룹화

--- __tests__/RaceManager.test.js
+++ __tests__/RaceManager.test.js
@@ -1,8 +1,10 @@
 import { Random } from '@woowacourse/mission-utils';
 import RaceManager from '../src/RaceManager';
 
-test('랜덤 숫자 생성 시 Random.pickNumberInRange를 호출한다', () => {
-  const randomSpy = jest.spyOn(Random, 'pickNumberInRange');
-  RaceManager.generateRandomNumber();
-  expect(randomSpy).toHaveBeenCalled();
+describe('랜덤 숫자 생성', () => {
+  test('랜덤 숫자 생성 시 Random.pickNumberInRange를 호출한다', () => {
+    const randomSpy = jest.spyOn(Random, 'pickNumberInRange');
+    RaceManager.generateRandomNumber();
+    expect(randomSpy).toHaveBeenCalled();
+  });
 });
