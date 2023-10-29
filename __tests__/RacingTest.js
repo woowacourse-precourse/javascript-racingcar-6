df7c5f316034bae3b568d8cbec389359e3f00aa5
gwahyun <60453182+gwahyun@users.noreply.github.com>
2023-10-29 19:15:37 +0900

    Test: 실행결과 계산 테스트 작성

---
+++ __tests__/RacingTest.js
@@ -0,0 +1,37 @@
+import race from '../src/Race';
+import { Random } from '@woowacourse/mission-utils';
+describe('레이스 게임 테스트', () => {
+  test('입력값에 따른 car 객체 생성', () => {
+    const userInput = 'a,b';
+    const equalObject = [
+      { name: 'a', result: [], textResult: '' },
+      { name: 'b', result: [], textResult: '' },
+    ];
+    expect(race.createRaceCars(userInput)).toEqual(equalObject);
+  });
+
+  test('무작위 값에 따른 게임 결과 계산', () => {
+    Random.pickNumberInRange = jest.fn();
+
+    Random.pickNumberInRange.mockReturnValueOnce(1);
+    Random.pickNumberInRange.mockReturnValueOnce(6);
+    Random.pickNumberInRange.mockReturnValueOnce(4);
+    Random.pickNumberInRange.mockReturnValueOnce(2);
+    Random.pickNumberInRange.mockReturnValueOnce(8);
+    Random.pickNumberInRange.mockReturnValueOnce(7);
+
+    const cars = [
+      { name: 'a', result: [], textResult: '' },
+      { name: 'b', result: [], textResult: '' },
+    ];
+    const inputPlayCount = 3;
+
+    race.playRace(cars, inputPlayCount);
+
+    expect(cars[0].result).toEqual([1, 4, 8]);
+    expect(cars[1].result).toEqual([6, 2, 7]);
+
+    expect(cars[0].textResult).toBe('--');
+    expect(cars[1].textResult).toBe('--');
+  });
+});
