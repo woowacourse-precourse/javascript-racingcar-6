70717b6f3dc32320932a198bdf74e9307766296b
inhwa <dlsghk0711@naver.com>
2023-10-29 23:00:05 +0900

    자동차 이름 & 경주 횟수 입력 (예외 처리 제외)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Console } from "@woowacourse/mission-utils";
+import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
-  async play() {}
-}
+  
+  Check_Name(){} //이름 예외 처리
 
+  Check_Match_Num(MATCH_NUM){}//숫자 예외 처리
+
+  async play() {
+    
+    
+    const USER_INPUT = await Console.readLineAsync("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)\n");
+    const USER_NAME = USER_INPUT.split(',');
+    this.Check_Name(USER_NAME);
+
+    const MATCH_NUM= await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+    this.Check_Match_Num(MATCH_NUM);
+    
+  }
+}
 export default App;
+
