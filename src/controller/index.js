a3c2f56f5c931f1b35aba993736f4f8bf276324a
ryan-dia <cjfdnjs158@naver.com>
2023-10-29 10:45:02 +0900

    feat(controller): 레이싱 실행 결과 출력 추가

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,5 +1,6 @@
 import RacingCarModel from '../model/index.js';
 import InputView from '../view/InputView.js';
+import OutputView from '../view/OutputView.js';
 
 class RacingCarController {
   #model;
@@ -12,8 +13,10 @@ class RacingCarController {
     const carNames = await InputView.readRacingCarNames();
     const racingCount = await InputView.readRacingCount();
     this.#model.saveCarNames(carNames);
+    OutputView.print('실행 결과');
     for (let i = 0; i < racingCount; i += 1) {
       this.#model.racing();
+      OutputView.printProgress(this.#model.getCarData());
     }
   }
 }
