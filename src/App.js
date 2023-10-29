1bf474d41a3d16a721a57f9a3abebc394a3ccc81
chysis <chysiss@naver.com>
2023-10-29 22:11:39 +0900

    fix: 테스트 실행 후 App.js 코드 수정

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import RacingGame from './RacingGame.js';
 class App {
   async play() {
     const game = new RacingGame();
-    game.startGame();
+    await game.startGame();
   }
 }
-
-const app = new App();
-app.play();
 
 export default App;
