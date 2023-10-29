87a1925283c6f4627d8f61ac441cde3771a3c995
Jeongeun Lee <leeju1009@gmail.com>
2023-10-29 12:00:24 +0900

    경주 시도 횟수의 유효성을 검사하는 기능 추가
    사용자에게 입력받은 경주 시도 횟수의 유효성을 검사하는 함수 생성

--- src/Validation.js
+++ src/Validation.js
@@ -6,4 +6,15 @@ class Validation {
   isValidCarName(carName) {
     return carName.length <= this.carNameLength;
   }
+
+  isValidRaceCount(raceCount) {
+    const raceCountNumber = Number(raceCount);
+    if (!Number.isInteger(raceCountNumber) || raceCountNumber <= 0) {
+      throw new Error(
+        '[ERROR] 숫자가 잘못된 형식입니다. 1 이상의 정수를 입력해주세요.'
+      );
+    }
+
+    return true;
+  }
 }
