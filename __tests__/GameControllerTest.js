2454dbf31a43659436595cc3b26d0178300b16a3
ReturnReason <reasonz@daum.net>
2023-10-29 19:58:46 +0900

    test(GameControllerTest): splitPlayerNames 메서드 테스트 추가

--- __tests__/GameControllerTest.js
+++ __tests__/GameControllerTest.js
@@ -12,12 +12,19 @@ describe('✨ [GameController] 클래스 메서드 테스트 ', () => {
     controller = new GameController({ model: mockModel, view: mockView });
   });
 
-  test('[getPlayerNames] 사용자로부터 경주할 자동차의 이름을 입력받는다. ', async () => {
+  test('[getPlayers] 사용자로부터 경주할 자동차의 이름을 입력받는다. ', async () => {
     const mockNames = 'Reason,LeeYu';
 
     mockView.getUserInputAsync.mockResolvedValueOnce(mockNames);
     await controller.getPlayers();
 
     expect(mockView.getUserInputAsync).toHaveBeenCalledWith(MESSAGE.START);
+  });
+
+  test('[splitPlayerNames] 사용자의 이름을 인자로 넣으면 쉼표(,)로 구분한 값이 반환된다.', () => {
+    const mockNames = 'Reason,LeeYu';
+    const expectedResult = ['Reason', 'LeeYu'];
+
+    expect(controller.splitPlayerNames(mockNames)).toEqual(expectedResult);
   });
 });
