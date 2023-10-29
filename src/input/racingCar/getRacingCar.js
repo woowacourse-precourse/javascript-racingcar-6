92518c85125a920be272dcfefe8d67cdd4a43ca9
1eecan <busyx2modernsociety@gmail.com>
2023-10-30 03:15:13 +0900

    feat:  getRacingCar.js작성
    * 쉼표로 구분되는 문자열을 받아 경주에 필요한 객체로 이뤄진 배열로 바꿔주는 함수 작성

--- src/input/racingCar/getRacingCar.js
+++ src/input/racingCar/getRacingCar.js
@@ -1,8 +1,13 @@
-import { Console } from "@woowacourse/mission-utils";
+import validateRacingCar from "./validateRacingCar.js";
 
 const racingCar = (input) => {
   const input_string = input;
   const input_array = input_string.split(",");
+  console.log(input_array);
+  console.log(validateRacingCar(input_array));
+  if (!validateRacingCar(input_array)) {
+    throw new Error("[ERROR] 문자가 잘못된 형식입니다.");
+  }
   const input_object_array = input_array.map((item) => {
     return {
       car: item,
