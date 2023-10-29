4b3e43d61b560e6e1d1c8af0abfd4153d755a206
uraflower <82873315+uraflower@users.noreply.github.com>
2023-10-30 03:39:54 +0900

    test: 시도 횟수 입력 기능 테스트 추가

--- __tests__/RaceManager.test.js
+++ __tests__/RaceManager.test.js
@@ -1,5 +1,15 @@
-import { Random } from '@woowacourse/mission-utils';
+import { Console, Random } from '@woowacourse/mission-utils';
 import RaceManager from '../src/RaceManager';
+
+describe('시도 횟수 입력', () => {
+  test('시도 횟수 입력 시 입력한 숫자를 반환한다', async () => {
+    const input = '5';
+    const expected = 5;
+    Console.readLineAsync = jest.fn().mockResolvedValue(input);
+    const output = await RaceManager.getNumberOfAttempts();
+    expect(output).toBe(expected);
+  });
+});
 
 describe('랜덤 숫자 생성', () => {
   test('랜덤 숫자 생성 시 Random.pickNumberInRange를 호출한다', () => {
