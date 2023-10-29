89d89027af894bf588f8f716ec6542d546a95208
김예빈 <kimyebin@gim-yebin-ui-MacBookPro.local>
2023-10-30 04:38:40 +0900

    docs: 기능 목록 README 수정

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ class App {
     const user = new User();
 
     try {
-      await user.inputPlayersName();
+      const players = await user.inputPlayersName();
     } catch (error) {
       console.error(error.message);
       throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
     }  
-
-    user.inputMoveNum();
   }
 }
 
