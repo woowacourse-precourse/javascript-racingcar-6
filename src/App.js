bb31e354803e0237046c66aa67830c5cbbc757c3
delee3064 <delee3064@gmail.com>
2023-10-29 15:22:24 +0900

    feat : 게임 진행 후 결과 출력

--- src/App.js
+++ src/App.js
@@ -7,9 +7,30 @@ class App {
     this.nameValidation(carNames);
     const cnt = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?");
     MissionUtils.Console.print("실행 결과")
+    this.printResult(carNames, cnt)
 
   }
 
+  printResult(carNames, cnt) {
+    var carDistance = Array.from({length : carNames.length}, ()=> 0)
+    for (let i = 0; i < cnt; i++) {
+      const gameResult = this.race(carNames, carDistance)
+      for (let i = 0; i < carNames.length; i++) {
+        MissionUtils.Console.print(carNames[i] + " : " + "-".repeat(gameResult[i]))
+      }
+      MissionUtils.Console.print("")
+    }
+  }
+
+  race(carNames, carDistance) {    
+    for (let i = 0; i < carNames.length; i++) {
+      let num = MissionUtils.Random.pickNumberInRange(0, 9);
+      if (num > 3) {
+        carDistance[i] += 1;
+      }
+    }
+    return carDistance    
+  }
 
   nameValidation(carNames) {
     for (let i = 0; i < carNames.length; i++) {
