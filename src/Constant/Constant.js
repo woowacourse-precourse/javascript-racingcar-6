a120ccd7c96ab7000219698e15c41c89d4b0443f
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-10-29 22:30:11 +0900

    refactor: 상수 수정

--- src/Constant/Constant.js
+++ src/Constant/Constant.js
@@ -2,7 +2,7 @@ const RACING_CAR_GAME = Object.freeze({
   GET_CAR: '경주 할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
   GET_NUMBER: '시도 할 횟수는 몇 회인가요?\n',
   RESULT: '실행 결과',
-  PRINT_WINNER: '최종 우승자 : ',
+  PRINT_WINNER: '최종 우승자',
   ERROR: {
     CAR_SPECIES: '[ERROR] 자동차 이름은 2가지 이상 작성하여야 합니다.',
     CAR_NAME_LENGTH: '[ERROR] 자동차의 이름은 5자 이하로 작성하여야 합니다.',
