eb52711485d4cdb5bcd854a11b7d5cf41790e5d6
1eecan <busyx2modernsociety@gmail.com>
2023-10-29 23:26:08 +0900

    feat: gameStart.js 작성
    * 게임 시작 시 나오는 문구를 출력하는 함수 추가

---
+++ src/print/gameStart.js
@@ -0,0 +1,8 @@
+import { Console } from "@woowacourse/mission-utils";
+import MESSAGES from "../constants/Messages.js";
+
+const gameStart = () => {
+  Console.print(MESSAGES.GAME_START);
+};
+
+export default gameStart;
