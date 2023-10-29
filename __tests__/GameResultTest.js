a879a2e6183dea1645d7ae53595e2dd70a0aaa2c
김진호 <rlawlsgh1227@gmail.com>
2023-10-29 21:44:03 +0900

    test: 단독 우승자 출력기능 검증

--- __tests__/GameResultTest.js
+++ __tests__/GameResultTest.js
@@ -1,5 +1,13 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 import RacingCar from "../src/domain/RacingCar";
 import GameUtils from "../src/utils/GameUtils";
+import MESSAGE from "../src/constants/Message";
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
 
 describe('게임 결과에 대한 테스트입니다.', () => {
   test('포비가 2칸, 크롱이 3칸 움직이면 우승자는 크롱으로 선정됩니다.', () => {
@@ -46,4 +54,28 @@ describe('게임 결과에 대한 테스트입니다.', () => {
     // then
     expect(winner).toEqual(expect.arrayContaining(answers));
   });
+
+  test('포비가 3칸 크롱이 2칸 움직였을 때 우승자로 포비가 출력됩니다.', () => {
+    // given
+
+    const pobiCar = new RacingCar('pobi');
+    const crongCar = new RacingCar('crong');
+
+    const answer = `${MESSAGE.FINAL_WINNERS} : pobi`;
+    const logSpy = getLogSpy();
+    
+    pobiCar.moveForward();
+    pobiCar.moveForward();
+    pobiCar.moveForward();
+
+    crongCar.moveForward();
+    crongCar.moveForward();
+
+    // when
+    const winner = GameUtils.selectWinners([pobiCar, crongCar]);
+    GameUtils.printFinalWinner(winner);
+
+    // then
+    expect(logSpy).toHaveBeenCalledWith(expect.stringMatching(answer));
+  })
 })
