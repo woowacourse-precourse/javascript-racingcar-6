ef507b83b37ace09f78978985119a4922f9a6d3a
kimsuyeon0916 <kimsuyeon_09162naver.com>
2023-10-29 21:16:29 +0900

    feat: 사용자에게 자동차 이름 입력 받기

--- src/utils/User.js
+++ src/utils/User.js
@@ -1,4 +1,14 @@
-class User{
+import { Console } from "@woowacourse/mission-utils";
+import { IN_GAME_MESSAGE } from "./Constants.js";
 
-}
+const User = {
+  async getUserInput() {
+    const userInput = (
+      await Console.readLineAsync(IN_GAME_MESSAGE.getCarName)
+    ).split(",");
+    return userInput;
+  },
+
+};
+
 export default User;
