ef8fba7d2564ce72426641f08579ddafd0b8f94a
coding-frog117 <syeshe2@gmail.com>
2023-10-29 09:24:28 +0900

    Feat : 올바른 시도 횟수를 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Random, Console } from "@woowacourse/mission-utils";
 class App {
   constructor() {
     this.racingCarMembers = [];
+    this.roundNumber = 0;
   }
 
   async play() {
@@ -34,6 +35,20 @@ class App {
         throw new Error("[ERROR] 이름은 총 10개까지 입력 가능합니다.");
       } else if (new Set(this.racingCarMembers).size !== this.racingCarMembers.length) {
         throw new Error("[ERROR] 중복된 이름은 입력할 수 없습니다.");
+      }
+    } catch (error) {
+      Console.print(error.message);
+    }
+
+    this.roundNumber = Number(await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n"));
+
+    try {
+      if (this.roundNumber == "") {
+        throw new Error("[ERROR] 시도 횟수를 입력해주세요.");
+      } else if (!Number.isInteger(this.roundNumber)) {
+        throw new Error("[ERROR] 시도 횟수는 자연수여야 합니다.");
+      } else if (this.roundNumber < 1 || this.roundNumber > 20) {
+        throw new Error("[ERROR] 시도 횟수는 1 이상 20 미만이어야 합니다.");
       }
     } catch (error) {
       Console.print(error.message);
