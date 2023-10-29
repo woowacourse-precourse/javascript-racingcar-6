dcc37d1c17bf7dfec1ab3b83217c8c23c8570f42
김용민 <dydals3440@gmail.com>
2023-10-29 19:52:57 +0900

    refactor: Print 클래스로 코드 이동

--- src/App.js
+++ src/App.js
@@ -5,12 +5,6 @@ import { COMMAS, ERROR } from './constants/constants';
 class App {
   async play() {
     const participateCars = await Print.getCarsName();
-
-    const cars = participateCars.split(COMMAS.SYMBOL);
-
-    if (!Validate.isCarNameLengthValid(cars)) {
-      throw new Error(ERROR.CAR_NAME_LENGTH);
-    }
   }
 }
 
--- src/Print.js
+++ src/Print.js
@@ -5,7 +5,13 @@ class Print {
   static async getCarsName() {
     const participateCars = await Console.readLineAsync(TEXT.INITIAL);
 
-    return participateCars;
+    const cars = participateCars.split(COMMAS.SYMBOL);
+
+    if (!Validate.isCarNameLengthValid(cars)) {
+      throw new Error(ERROR.CAR_NAME_LENGTH);
+    }
+
+    return cars;
   }
 }
 
