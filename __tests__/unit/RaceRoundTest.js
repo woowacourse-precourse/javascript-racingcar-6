fae078aabf0bac73c561b379a1acb6ca885b0ea6
sunub <bsc5672@gmail.com>
2023-10-29 19:40:10 +0900

    feat : 실패하는 Round 진행 시 기능을 수하는 RaceRound 모듈을 대상으로하는 실패하는 테스 코드 작성

--- __tests__/unit/RaceRoundTest.js
+++ __tests__/unit/RaceRoundTest.js
@@ -1,44 +1,18 @@
-import { MissionUtils, Random } from '@woowacourse/mission-utils';
-import Cars from '../../src/Cars';
+import { MissionUtils } from '@woowacourse/mission-utils';
 
-const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
-  logSpy.mockClear();
-  return logSpy;
-};
-
-const getRandomSpy = () => {
-  const randomSpy = jest.spyOn(MissionUtils.Random, 'pickNumberInRange');
-  randomSpy.mockClear();
-  return randomSpy;
-};
-
-describe('입력 받은 자동차 경기 라운드가', () => {
-  afterEach(() => {
-    jest.clearAllMocks();
+describe('경기가 진행되는 라운드는', () => {
+  test('0~9 사이의 랜덤한 숫자를 생성하는가?', () => {
+    RaceRound.createRandomNum();
   });
 
-  test('0이상의 정수인가?', () => {
-    const raceRound = new RaceRound();
-    expect(raceRound.count).toBeGreaterThan(1);
-  });
-});
-
-describe('매 라운드마다', () => {
-  afterEach(() => {
-    jest.clearAllMocks();
+  test('자동차의 이름 수 만큼 라운드가 진행되는가?', () => {
+    RaceRound.giveRandomNumToCarName();
   });
 
-  test('모든 자동차들에게 고유한 랜덤한 숫자가 생성되는가?', () => {
+  test('모든 자동차에게 랜덤한 숫자를 제공하는가?', () => {
+    const inputNamesCount = 3;
+    const randomSpy = jest.spyOn(MissionUtils.Random, 'pickNumberInRange');
+
+    expect(randomSpy).toBeCalledTimes(inputNamesCount);
   });
-
-  test('숫자가 4미만일 경우 차가 움직이지 않는가?', () => {
-  });
-
-  test('숫자가 4미만일 경우 차가 움직이면 예외 처리가 일어나는가?', () => {
-  });
-
-  test('숫자가 4이상일 경우 차가 앞으로 움직이는가?', () => {});
-
-  test('숫자가 4이상일 경우 차가 움직이지 않으면 예외 처리가 일어나는가?', () => {});
 });
