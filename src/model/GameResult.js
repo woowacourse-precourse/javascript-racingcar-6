2745646c416b99c43335f3796377cfc25a17efc4
Nahyun <from.owol@gmail.com>
2023-10-29 17:51:50 +0900

    refactor: GameResult 메서드 분리

--- src/model/GameResult.js
+++ src/model/GameResult.js
@@ -1,18 +1,22 @@
 class GameResult {
   getForwardResult(carPosition, prevLog) {
     carPosition.forEach((value, carName) => {
-      switch (true) {
-        case value > 0:
-          carPosition.set(carName, value - 1);
-          let log = prevLog.get(carName) || 0;
-          log += 1;
-          prevLog.set(carName, log);
-          break;
-        default:
-          prevLog.set(carName, prevLog.get(carName) || 0);
-      }
+      this.updateCarLog(carPosition, prevLog, carName, value);
     });
     return prevLog;
+  }
+
+  updateCarLog(carPosition, prevLog, carName, value) {
+    switch (true) {
+      case value > 0:
+        carPosition.set(carName, value - 1);
+        let log = prevLog.get(carName) || 0;
+        log += 1;
+        prevLog.set(carName, log);
+        break;
+      default:
+        prevLog.set(carName, prevLog.get(carName) || 0);
+    }
   }
 
   getWinner(carLog) {
