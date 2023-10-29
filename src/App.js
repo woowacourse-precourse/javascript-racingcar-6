cb9a52f56b995ef8286f9067a079230bcce2cae7
jonghyunRyu <willowise@naver.com>
2023-10-29 16:57:15 +0900

    자동차 입력 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+
+  }
+
+  async Usercar_input() {
+
+    const User_input = await Console.readLineAsync('경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분) \n');
+    
+    const Car = User_input.split(',');
+
+    const carMap = new Map();
+
+    for (let i in Car) {
+      carMap.set(Car[i], "");
+    }
+
+    return carMap;
+  }
+  
 }
+
+const app = new App();
+app.Usercar_input();
 
 export default App;
