2959ea00c65809b5a0289144c8c1af2721099203
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2023-10-29 14:56:17 +0900

    refactor: view 분리에 따른 valid 함수 코드 변경

--- src/Validation.js
+++ src/Validation.js
@@ -1,18 +1,26 @@
-const isLessThanFive = (list) => {
+const checkLessThanFive = (list) => {
   if (list.some((carName) => carName.length > 5)) {
     throw new Error('[ERROR] 이름은 5글자를 넘어선 안됩니다.');
   }
 }
 
-const isDuplicated = (list) => {
+const checkDuplicated = (list) => {
   if (new Set(list).size !== list.length) {
     throw new Error('[ERROR] 중복된 이름이 있습니다.');
   }
 }
 
+const checkHasSpace = (list) => {
+  let str = list.join('');
+  if (str !== str.replace(/(\s*)/g, "") || str === '') {
+    throw new Error('[ERROR] 띄어쓰기 미포함입니다.');
+  }
+}
+
 export const checkValidCarsName = (list) => {
-  isDuplicated(list);
-  isLessThanFive(list);
+  checkDuplicated(list);
+  checkLessThanFive(list);
+  checkHasSpace(list);
 }
 
 export const checkValidNumber = (num) => {
