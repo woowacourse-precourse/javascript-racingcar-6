728759ca194b2d363bcc2e3ad03e40bc742d3558
Jeongeun Lee <leeju1009@gmail.com>
2023-10-29 15:52:17 +0900

    feat: 경주 시도 횟수의 에러를 핸들링하는 기능 추가
    사용자가 입력한 경주 시도 횟수가 자연수가 아닐 경우, 에러를 던지는 기능 추가

--- src/ErrorHandler.js
+++ src/ErrorHandler.js
@@ -4,6 +4,12 @@ class ErrorHandler {
       '[ERROR] 자동차 이름은 5자 이하로 입력해주세요. (쉼표(,)로 구분)'
     );
   }
+
+  throwInvalidraceTimesError() {
+    throw new Error(
+      '[ERROR] 숫자가 잘못된 형식입니다. 1 이상의 정수를 입력해주세요.'
+    );
+  }
 }
 
 export default ErrorHandler;
