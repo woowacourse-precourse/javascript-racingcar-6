6882fda2c6b71d295f0489b82d2695949c047498
Baek <woonn97@gmail.com>
2023-10-29 22:50:11 +0900

    feat: 입력/출력 구현
    * 자동차 이름은 쉼표(,)를 기준으로 구분하도록 구현
    * 입력 받은 이름을 this.cars에 저장
    * 경주 횟수 입력하도록 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  constructor() {
+    this.cars = [];
+  }
+  async play() {
+    const nameInput = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    this.cars = nameInput.split(",").map((name) => ({ name, position: 0 }));
+    const tryCount = await Console.readLineAsync("시도할 회수는 몇회인가요?");
+    Console.print(this.cars);
+  }
 }
 
 export default App;
