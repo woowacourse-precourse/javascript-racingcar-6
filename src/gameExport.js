2addf642f4bb24de550dd4721dd3d435af4f83a6
woo7i <or2447221@gmail.com>
2023-10-29 19:58:38 +0900

    fix(gameExport.js) : gameExport 함수 뎁스 넘어가는 범위 수정.

--- src/gameExport.js
+++ src/gameExport.js
@@ -10,13 +10,23 @@ function condition(key) {
   return (carNames[key] += randomNum() > 3 ? '-' : '');
 }
 
+function getUpdatedCarStatus(key) {
+  return `${key} : ${condition(key)}\n`;
+}
+
+function createRoundResult(carNamesKey) {
+  let result = '';
+  for (let j = 0; j < carNamesKey.length; j++) {
+    const key = carNamesKey[j];
+    result += getUpdatedCarStatus(key);
+  }
+  return result;
+}
+
 async function gameExport() {
   let roundResult = '';
   for (let i = 0; i < gameCountNum; i++) {
-    for (let j = 0; j < carNamesKey.length; j++) {
-      const key = carNamesKey[j];
-      roundResult += `${key} : ${condition(key)}\n`;
-    }
+    roundResult += createRoundResult(carNamesKey);
     if (i < gameCountNum - 1) {
       roundResult += `\n`;
     }
