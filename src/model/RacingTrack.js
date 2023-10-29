4c72745fdd9de3ebf8f5648a4307c246d08e9d6f
pakxe <pigkill40@naver.com>
2023-10-29 23:34:23 +0900

    feat(RacingTrack): 트랙위의 자동차를 이동시키고 현재 위치를 추적하는 클래스 작성

---
+++ src/model/RacingTrack.js
@@ -0,0 +1,54 @@
+import GAME from '../constants/Game.js';
+import randomNumberGenerator from '../utils/randomNumberGenerator.js';
+import driverNamesValidation from '../utils/validation/driverNamesValidation.js';
+import tryCountValidation from '../utils/validation/tryCountValidation.js';
+
+class RacingTrack {
+	#drivers; // {name1: currentLocation, name2: ...}
+	#tryCount;
+
+	constructor() {
+		this.#drivers = {};
+	}
+
+	initDrivers(drivers) {
+		// TODO: 유효성 검사 필요
+		const driverArr = drivers.split(GAME.inputNameDelimiter);
+
+		driverNamesValidation(driverArr);
+
+		driverArr.forEach((name) => (this.#drivers[name] = 0));
+	}
+
+	initTryCount(tryCountStr) {
+		// TODO: 유효성 검사 필요
+		tryCountValidation(tryCountStr);
+
+		this.#tryCount = parseInt(tryCountStr);
+	}
+
+	getTryCount() {
+		return this.#tryCount;
+	}
+
+	#move() {
+		// 커런트 자체를 증가시켜야하나?
+		Object.entries(this.#drivers).forEach(([name, _]) => {
+			const moveProbability = randomNumberGenerator();
+
+			if (moveProbability >= GAME.moveThreshold) this.#drivers[name]++;
+		});
+	}
+
+	allDriversSingleMoveLocation() {
+		this.#move();
+
+		return this.#drivers;
+	}
+
+	getFinalLocations() {
+		return this.#drivers;
+	}
+}
+
+export default RacingTrack;
