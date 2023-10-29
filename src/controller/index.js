d76c66ed1497d8680ea833610cd5238c9de2f6ab
ryan-dia <cjfdnjs158@naver.com>
2023-10-29 09:45:02 +0900

    feat(controller): 레이싱 기능 추가

--- src/controller/index.js
+++ src/controller/index.js
@@ -12,6 +12,9 @@ class RacingCarController {
     const carNames = await InputView.readRacingCarNames();
     const racingCount = await InputView.readRacingCount();
     this.#model.saveCarNames(carNames);
+    for (let i = 0; i < racingCount; i += 1) {
+      this.#model.racing();
+    }
   }
 }
 
