63e389393fbb221441c04d4b9644ecc8fdfa1b5f
heonq <gkagjsrb@naver.com>
2023-10-29 14:59:29 +0900

    test(validator): 시도 횟수에 대한 유효성 검사 테스트 코드 작성 - 1 이상의 양의 정수가 아닌 값에 대한 유효성 검사 테스트 - 1 이상의 양의 정수인 값에 대한 유효성 검사 테스트

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -15,3 +15,20 @@ describe('자동차 이름에 대한 유효성 검사 테스트', () => {
     });
   });
 });
+
+describe('시도할 횟수에 대한 유효성 검사 테스트', () => {
+  test('1 이상의 양의 정수가 아닌 값에 대한 유효성 검사', () => {
+    const input = [-1, 0, 1.1, '한번'];
+    input.forEach((trialCount) => {
+      expect(() => Validator.validateTrialCount(trialCount)).toThrow(MESSAGES.invalidTrialCount);
+    });
+  });
+  test('1 이상의 양의 정수인 값에 대한 유효성 검사', () => {
+    const input = [1, 2, 3, 100];
+    input.forEach((trialCount) => {
+      expect(() => Validator.validateTrialCount(trialCount)).not.toThrow(
+        MESSAGES.invalidTrialCount,
+      );
+    });
+  });
+});
