d3627a48ad02aff69fbcb34b7a64ebbea0274516
sunub <bsc5672@gmail.com>
2023-10-29 14:30:20 +0900

    feat : Cars의 단위 테스트 코드를 통과하는 Cars 모듈 작성

--- src/Cars.js
+++ src/Cars.js
@@ -1,5 +1,33 @@
-class Cars{
+class Cars {
+  #INFO;
 
+  constructor(names) {
+    this.#INFO = Cars.initialize(names);
+  }
+
+  static initialize(names) {
+    return new Map(names.map((name) => {
+      const distance = 0;
+      return [
+        name, {
+          distance,
+        },
+      ];
+    }));
+  }
+
+  get getNames() {
+    return [...this.#INFO.keys()];
+  }
+
+  getDistanceByName(name) {
+    return this.#INFO.get(name);
+  }
+
+  movingForwardByName(name) {
+    const { distance } = this.#INFO.get(name);
+    this.#INFO.set(name, { distance: distance + 1 });
+  }
 }
 
 export default Cars;
