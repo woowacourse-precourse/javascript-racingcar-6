34e0b19502a31507e59e22b581690b6ab7f645dc
starcradle101 <dreamerdev12@gmail.com>
2023-10-29 14:38:25 +0900

    :sparkles: Feat: getPlayerCarsInput 구현
    사용자의 입력을 받는 getPlayerCarsInput 메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+import { PLAYER_INPUT, ERROR_MESSAGES, RACE } from './Logs.js';
+
 class App {
   async play() {}
 
   async getPlayerCarsInput() {
-    const ANSWER = await Console.readLineAsync();
+    const ANSWER = await Console.readLineAsync(PLAYER_INPUT.CARS_NAME_PROMPT);
+
+    // 유효성 검사
+    return ANSWER.split(',');
   }
 }
 
