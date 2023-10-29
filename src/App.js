d39558e435632411bdb74f81e52ffb01155e7b61
coddingyun <igy2840@gmail.com>
2023-10-29 12:40:05 +0900

    refactoring: 입력값 받는 함수 명 수정
    기존의 getInputValue 라는 함수명은 자동차 이름, 시도할 횟수 입력값을 다 받는 의미를 내포하고 있어서
    자동차 이름만 받기 때문에 getCarNameInput으로 함수명 수정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import * as MissionUtils from "@woowacourse/mission-utils";
 class App {
   async play() {
-    const getInputValue = async() => {
+    const getCarNameInput = async() => {
       const carName = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n')
       return carName.split(',')
     };
-    const carNameArr = await getInputValue();
+    const carNameArr = await getCarNameInput();
   }
 }
 
