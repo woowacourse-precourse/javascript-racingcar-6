b92a44f2bc9585367c287bb51d019a1a0794cb95
gon <hss3522@gmail.com>
2023-10-29 09:35:59 +0900

    feat: Add function to display race winners

--- src/App.js
+++ src/App.js
@@ -26,18 +26,25 @@ class App {
 
   runRacing(carObject, racingCount) {
     const newObject = { ...carObject };
-    const names = Object.keys(carObject);
+    const carNames = Object.keys(newObject);
 
     for (let count = 0; count < racingCount; count += 1) {
-      names.forEach((name) => {
+      carNames.forEach((car) => {
         const number = Random.pickNumberInRange(0, 9);
-        newObject[name] += number >= 4 ? 1 : 0;
+        newObject[car] += number >= 4 ? 1 : 0;
 
-        Console.print(`${name} : ${"-".repeat(newObject[name])}`);
+        Console.print(`${car} : ${"-".repeat(newObject[car])}`);
       });
 
       Console.print(" ");
     }
+
+    const maxDistance = Math.max(...Object.values(newObject));
+    const winners = Object.keys(newObject).filter(
+      (car) => newObject[car] === maxDistance
+    );
+
+    Console.print(`최종 우승자 : ${winners.join(", ")}`);
   }
 
   async play() {
