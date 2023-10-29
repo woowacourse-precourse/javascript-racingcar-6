1c6b3ae0185536c7f4d32c512b96550c99f9e08a
JiSeong Kim <12180543@inha.edu>
2023-10-29 18:06:22 +0900

    feat(constants): implement printMessage object
    - INPUT_NAMES(자동차 이름 입력 받기 전 메시지)
    - INPUT_COUNT(횟수 입력 받기 전 메시지)
    - RACING_RESULT(레이싱 결과 출력 하기 전의 메시지)

---
+++ src/constants/printMessage.js
@@ -0,0 +1,7 @@
+const printMessage = Object.freeze({
+	INPUT_NAMES : '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+	INPUT_COUNT : '시도할 횟수는 몇 회인가요?\n',
+	RACING_RESULT : '실행 결과'
+});
+
+export default printMessage;
