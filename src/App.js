9f5bc9d9a80649b98ad81545cbe9beaf315eb01c
김태희 <>
2023-10-29 23:05:51 +0900

    feat: nameAndNumberInput 기능 추가
    자동차의 이름과 횟수를 입력받는 함수를 추가.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  /** 자동차 이름, 횟수 입력 
+   * param: -
+  */
+  nameAndNumberInput = async () => {
+    Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    let nameInput = await Console.readLineAsync();
+    nameInput = nameInput.toString().split(",");
+    Console.print("시도할 횟수는 몇 회인가요?");
+    let numberInput = await Console.readLineAsync();
+
+    return [nameInput, numberInput];
+  }
+  async play() {
+    let [nameList, count] = await this.nameAndNumberInput();
+  }
 }
 
 export default App;
