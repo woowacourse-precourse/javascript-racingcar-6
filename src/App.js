cbe7ab80ace6a8fd77cd7ef39250e72eda525ae2
Myomyoring <narcissuscat@naver.com>
2023-10-30 01:56:41 +0900

    [feat] 입력값 예외 처리 메소드

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import { Console } from '@woowacourse/mission-utils';
 
 class App {
-	async play() {
-		const input = await Console.readLineAsync('경주 할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
-	}
+ validation(cars) {
+  const carLength = cars.length < 2;
+  const nameLength = cars.some((car) => car.length < 0 || car.length > 5);
+  const gap = cars.some((car) => /^[가-힣a-zA-Z0-9]+$/.test(car));
+  const deduplicateObj = new Set(cars);
+  const duplicate = deduplicateObj.size !== cars.length;
+  if (carLength || nameLength || gap || duplicate) {
+   throw new Error('[ERROR]');
+  }
+ }
+ async play() {
+  const input = await Console.readLineAsync('경주 할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+  //,쉼표 기준으로 자르기
+  const cars = input.split(',');
+  this.validation(cars);
+ }
 }
-
 export default App;
