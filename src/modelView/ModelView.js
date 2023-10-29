0f319db055fbfc7e958cd3f764fb4d60dc6e8a70
solssak <gswoong1012@gmail.com>
2023-10-30 02:22:56 +0900

    refactor: 자동차 이동 로직, 우승자 계산 로직 modelview영역으로 분리

---
+++ src/modelView/ModelView.js
@@ -0,0 +1,47 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const ModelView = {
+  currentMovements: {},
+
+  playGame(validNames) {
+    const raceResults = {};
+
+    for (const name of validNames) {
+      if (!this.currentMovements[name]) {
+        this.currentMovements[name] = "";
+      }
+      const movement = this.calcMoveCount();
+      this.currentMovements[name] += movement;
+      raceResults[name] = this.currentMovements[name];
+    }
+
+    MissionUtils.Console.print(" ");
+
+    return raceResults;
+  },
+
+  calcMoveCount() {
+    const movementCount = MissionUtils.Random.pickNumberInRange(0, 9);
+    return movementCount >= 4 ? "-" : "";
+  },
+
+  calculateWinners(names) {
+    const winners = [];
+    let maxDashes = 0;
+
+    for (const name of names) {
+      const dashes = (this.currentMovements[name].match(/-/g) || []).length;
+      if (dashes > maxDashes) {
+        maxDashes = dashes;
+        winners.length = 0;
+        winners.push(name);
+      } else if (dashes === maxDashes) {
+        winners.push(name);
+      }
+    }
+
+    return winners;
+  },
+};
+
+export default ModelView;
