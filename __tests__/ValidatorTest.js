1c8c8689c0e4cb842523d56f9b25a52ec88afc2c
heonq <gkagjsrb@naver.com>
2023-10-29 15:56:34 +0900

    style(test): 테스트 코드 변수명 수정 - 가독성을 위해 기존의 변수명 input을 각 테스트 대상의 이름으로 변경

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -3,29 +3,29 @@ import MESSAGES from '../utils/Messages';
 
 describe('자동차 이름에 대한 유효성 검사 테스트', () => {
   test('5글자 초과의 이름에 대한 유효성 검사', () => {
-    const input = [['carcar'], ['hyundai'], ['chevrolet']];
-    input.forEach((cars) => {
-      expect(() => Validator.validateCarNames(cars)).toThrow(MESSAGES.invalidNameLength);
+    const carNamesArray = [['carcar'], ['hyundai'], ['chevrolet']];
+    carNamesArray.forEach((carName) => {
+      expect(() => Validator.validateCarNames(carName)).toThrow(MESSAGES.invalidNameLength);
     });
   });
   test('5글자 이하의 이름에 대한 유효성 검사', () => {
-    const input = [['carca'], ['benz'], ['bmw'], ['ca'], ['c']];
-    input.forEach((cars) => {
-      expect(() => Validator.validateCarNames(cars).not.toThrow(MESSAGES.invalidNameLength));
+    const carNamesArray = [['carca'], ['benz'], ['bmw'], ['ca'], ['c']];
+    carNamesArray.forEach((carName) => {
+      expect(() => Validator.validateCarNames(carName).not.toThrow(MESSAGES.invalidNameLength));
     });
   });
 });
 
 describe('시도할 횟수에 대한 유효성 검사 테스트', () => {
   test('1 이상의 양의 정수가 아닌 값에 대한 유효성 검사', () => {
-    const input = [-1, 0, 1.1, '한번'];
-    input.forEach((trialCount) => {
+    const trialCounts = [-1, 0, 1.1, '한번'];
+    trialCounts.forEach((trialCount) => {
       expect(() => Validator.validateTrialCount(trialCount)).toThrow(MESSAGES.invalidTrialCount);
     });
   });
   test('1 이상의 양의 정수인 값에 대한 유효성 검사', () => {
-    const input = [1, 2, 3, 100];
-    input.forEach((trialCount) => {
+    const trialCounts = [1, 2, 3, 100];
+    trialCounts.forEach((trialCount) => {
       expect(() => Validator.validateTrialCount(trialCount)).not.toThrow(
         MESSAGES.invalidTrialCount,
       );
