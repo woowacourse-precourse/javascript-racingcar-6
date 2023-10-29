f2b52e4bfe6d8e417e05d1a1af773ffcfec06f72
JoSooha <dlrhdcjs@naver.com>
2023-10-29 22:59:41 +0900

    feat: 횟수 입력 받고 예외 처리

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ class App {
   async play() {
     const carInput = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
     const carArray = this.handleCarName(carInput);
+    const tryInput = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+    // const tryNumber = 
 
   }
 
@@ -14,6 +16,18 @@ class App {
         if(e.length>6) throw new Error("[ERROR] 자동차 이름은 5자 이하여야 합니다.")
       })
       return arr;
+    } catch(error) {
+      console.error(error.message);
+      throw error;      
+    }
+  }
+
+  handleTryNumber(input) {
+    try {
+      console.log(typeof(input)) //
+      if(input >= 48 && input <= 57) {
+        return Number(input)
+      } else throw new Error("[ERROR] 횟수는 숫자여야 합니다. ")
     } catch(error) {
       console.error(error.message);
       throw error;      
