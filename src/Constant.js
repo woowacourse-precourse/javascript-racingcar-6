53e06a92f3b27864ae63026d67b7e63ccbaecfa2
shgusgh12 <https://github.com/shgusgh12>
2023-10-30 03:47:21 +0900

    feat: 자동차 경주 진행 메세지 추가

---
+++ src/Constant.js
@@ -0,0 +1,9 @@
+const MESSAGE = {
+  INPUT_CAR_NAME:
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+  INPUT_RACE_NUMBER: '시도할 횟수는 몇 회인가요?',
+  WINNER: '최종 우승자',
+  RESULT: '실행 결과',
+};
+
+export default MESSAGE;
