8fea8ac23c81cf6c5305c067ef82d346ff200d7c
jjinheeWorld <wlsgml450@naver.com>
2023-10-30 01:34:23 +0900

    ✨ Feat: 자동차 이름 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    this.printGameStart();
+    const carNameList = await this.inputCarNames();
+  }
+
+  printGameStart() {
+    Console.print(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
+    );
+  }
+
+  async inputCarNames() {
+    const carNames = await Console.readLineAsync('');
+    const carNameList = carNames.split(',');
+    const trimmedCarNames = carNameList.map((carName) => carName.trim());
+
+    return trimmedCarNames;
+  }
 }
 
 export default App;
