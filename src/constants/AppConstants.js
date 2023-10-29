f9d616857e87fd72f3076f557c540f3ec26b4fe4
jinsil <jinsil378900@gmail.com>
2023-10-30 00:17:42 +0900

    Feat: 1차 constants 작성

---
+++ src/constants/AppConstants.js
@@ -0,0 +1,8 @@
+const AppConstants = Object.freeze({
+  MINIMUM: 0,
+  MAXIMUM: 9,
+  FORWARD_CONDITION: 4,
+  FORWARD: "-",
+});
+
+export default AppConstants;
---
+++ src/constants/Messages.js
@@ -0,0 +1,9 @@
+const Messages = Object.freeze({
+  INPUT_NAME: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
+  INPUT_NUMBER: "시도할 횟수는 몇 회인가요?\n",
+  OUTPUT: "\n실행결과",
+  WINNER: "최종 우승자 : ",
+
+  ERROR: {},
+});
+export default Messages;
