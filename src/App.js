8ce1bcbb9f73263ef9012dd7795e65d85c126f6c
yjh1008 <exesea12@gmail.com>
2023-10-29 11:25:51 +0900

    feat : 각 차수별 실행 결과를 출력하는 함수 추가

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ const { Console, Random } = MissionUtils;
 import Cars from "./Cras";
 class App {
 
+  printCarMoves(cars) {
+    let runningResult = "";
+    Object.keys(cars.obj).forEach((item) => {
+      runningResult += `${item} : `+"-".repeat(cars.obj[item])+"\n";
+    });
+    Console.print(runningResult);
+  }
+
   running_race(cars, repetitions) {
     for(let i =0 ;i<repetitions;i++) {
       cars.move_cars(this.getMoveArr(cars));
       this.printCarMoves(cars);
     }
   }
-
-  
   async play() {
     const input = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     const cars = new Cars(input);
