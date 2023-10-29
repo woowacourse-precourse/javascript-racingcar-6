fddbe43cf3ad8ce300bd5d3149f020641c68efca
Jeongeun Lee <leeju1009@gmail.com>
2023-10-29 21:29:24 +0900

    feat: 경주 시도 횟수 유효성 검사 함수를 사용자 입력값에 적용
    경주를 시도할 횟수에 대한 유효성 검사 함수를 사용자 입력값에 적용함

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -1,4 +1,6 @@
 import { Console } from '@woowacourse/mission-utils';
+import Validation from './Validation.js';
+import ErrorHandler from './ErrorHandler.js';
 
 class RacingGame {
   statusMark = '-';
@@ -7,7 +9,14 @@ class RacingGame {
     const racingCount = await Console.readLineAsync(
       '시도할 횟수는 몇 회인가요?'
     );
-    return Number(racingCount);
+    const racingCountNumber = Number(racingCount);
+    const validation = new Validation();
+    const errorHandler = new ErrorHandler();
+    if (!validation.isValidRacingCount(racingCountNumber)) {
+      errorHandler.throwInvalidRacingCountError();
+    }
+
+    return racingCountNumber;
   }
 
   playRacing(carsArray) {
