1c70469cb79a1bdce9f4682415fab16023525a73
Hyunji Seok <alleyji126@gmail.com>
2023-10-29 09:10:00 +0900

    feat: update object goTimes in play method

--- src/App.js
+++ src/App.js
@@ -9,18 +9,15 @@ class App {
   async play() {
     const carNames = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) \n");
     this.cars = carNames.split(",");
+    this.cars.forEach(car => { this.goTimes[car] = 0; });
     this.raceTimes = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요? \n");
-  
-    // 배열 길이 검사를 handleErrors 메소드 호출 이전에 수행
-    // if (this.cars.length === 0) {
-    //   throw new Error("[ERROR] 자동차 이름을 입력하지 않았습니다.");
-    // }
 
     this.handleErrors(this.cars, this.raceTimes);
     this.raceCars();
   }
 
   async raceCars() {
+
 
   }
 
