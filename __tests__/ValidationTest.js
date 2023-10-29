1e9f849c8217d4bca85b87235c4fbf7320883ae8
sbemjr1 <sbemjr1@naver.com>
2023-10-29 15:57:55 +0900

    test:테스트 코드 일괄 수정 및 오류 아닐 시 처리 코드 추가

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -6,27 +6,17 @@ import {
   userInputCarNameOverlapError,
 } from '../src/domain/Error.js';
 
-// Jest 테스트 스위트 시작
 describe('userInputTryNumberError 함수 테스트', () => {
-  // 0 이하인 경우 오류가 throw되어야 함
   test('0 이하인 경우 오류 throw', () => {
     expect(() => userInputTryNumberError(-1)).toThrowError(ERROR.ERROR_TYPE);
     expect(() => userInputTryNumberError(0)).toThrowError(ERROR.ERROR_TYPE);
   });
 
-  // 0 이상인 경우 오류가 throw되지 않아야 함
   test('0 이상인 경우 오류 throw 안 됨', () => {
     expect(() => userInputTryNumberError(1)).not.toThrow();
     expect(() => userInputTryNumberError(100)).not.toThrow();
   });
 
-  // 숫자인 경우 오류가 throw되지 않아야 함
-  test('정수형 숫자인 경우 오류 throw 안 됨', () => {
-    expect(() => userInputTryNumberError(123)).not.toThrow();
-    expect(() => userInputTryNumberError(1)).not.toThrow();
-  });
-
-  // 숫자가 아닌 경우 오류가 throw되어야 함
   test('숫자가 아닌 경우 또는 정수형이 아닐 경우 오류 throw', () => {
     expect(() => userInputTryNumberError(45.67)).toThrowError(ERROR.ERROR_TYPE);
     expect(() => userInputTryNumberError('문자열')).toThrowError(ERROR.ERROR_TYPE);
@@ -34,12 +24,22 @@ describe('userInputTryNumberError 함수 테스트', () => {
     expect(() => userInputTryNumberError(undefined)).toThrowError(ERROR.ERROR_TYPE);
     expect(() => userInputTryNumberError(NaN)).toThrowError(ERROR.ERROR_TYPE);
   });
+
+  test('정수형 숫자인 경우 오류 throw 안 됨', () => {
+    expect(() => userInputTryNumberError(123)).not.toThrow();
+    expect(() => userInputTryNumberError(1)).not.toThrow();
+  });
 });
 
 describe('getCarName 함수 테스트', () => {
   test('공백이 포함될 경우 오류 throw', () => {
     expect(() => userInputCarNameFormError('a b c')).toThrowError(ERROR.ERROR_SPACE);
     expect(() => userInputCarNameFormError('de f')).toThrowError(ERROR.ERROR_SPACE);
+  });
+
+  test('공백이 포함되지 않을 경우 오류 throw 안 됨', () => {
+    expect(() => userInputCarNameFormError('abc,def')).not.toThrowError();
+    expect(() => userInputCarNameFormError('def,abc')).not.toThrowError();
   });
 
   test('이름 길이가 5를 초과할 때 오류 throw', () => {
@@ -51,9 +51,19 @@ describe('getCarName 함수 테스트', () => {
     );
   });
 
+  test('이름 길이가 5를 초과하지 않을 때 오류 throw 안 됨', () => {
+    expect(() => userInputCarNameLengthError(['abcd', 'aaaa', 'jun'])).not.toThrowError();
+    expect(() => userInputCarNameLengthError(['abcde', 'bbbbb', 'jun'])).not.toThrowError();
+  });
+
   test('이름 개수가 2개 미만일 때 오류 throw', () => {
     expect(() => userInputCarNameLengthError(['abcd'])).toThrowError(ERROR.ERROR_SHORT_LENGTH);
     expect(() => userInputCarNameLengthError([])).toThrowError(ERROR.ERROR_SHORT_LENGTH);
+  });
+
+  test('이름 개수가 2개 이상일 때 오류 throw 안 됨', () => {
+    expect(() => userInputCarNameLengthError(['abcd', 'aaaaa', 'b'])).not.toThrowError();
+    expect(() => userInputCarNameLengthError(['aaaa', 'bbbb', 'ccc'])).not.toThrowError();
   });
 
   test('이름에 특수문자가 포함되면 오류 throw', () => {
@@ -63,6 +73,11 @@ describe('getCarName 함수 테스트', () => {
     expect(() => userInputCarNameLengthError(['./$#', '$@#!'])).toThrowError(ERROR.ERROR_TYPE);
   });
 
+  test('이름에 특수문자가 포함되지 않으면 오류 throw 안 됨', () => {
+    expect(() => userInputCarNameLengthError(['ac', 'abcd', 'qwe'])).not.toThrowError();
+    expect(() => userInputCarNameLengthError(['aaa', '김나박이'])).not.toThrowError();
+  });
+
   test('이름이 중복될 때 오류 throw', () => {
     expect(() => userInputCarNameOverlapError(['abcd', 'abcd', 'aaaaa'])).toThrowError(
       ERROR.ERROR_OVERLAP,
@@ -70,5 +85,10 @@ describe('getCarName 함수 테스트', () => {
     expect(() => userInputCarNameOverlapError(['aaaaa', 'abcd', 'abcd'])).toThrowError(
       ERROR.ERROR_OVERLAP,
     );
+  });
+
+  test('이름이 중복되지 않을 때 오류 throw', () => {
+    expect(() => userInputCarNameOverlapError(['abcde', 'abcd', 'aaaaa'])).not.toThrowError();
+    expect(() => userInputCarNameOverlapError(['bbb', 'abcd', 'abcde'])).not.toThrowError();
   });
 });
