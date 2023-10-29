908a059a3075bc551d35c9a070e8f0f36b8dc774
osohyun0224 <osohyun0224@naver.com>
2023-10-29 20:35:17 +0900

    fix(Message): 출력 요구사항에 맞게 printWinner 메세지 수정
    - 문제에 제시된 출력 요구사항에 맞도록 printWinner 메세지를 수정하였습니다.

--- src/constants/Message.js
+++ src/constants/Message.js
@@ -2,7 +2,7 @@ export const GAME_MESSAGE = Object.freeze({
   inputName: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
   inputNumber: "시도할 횟수는 몇 회인가요?",
   printResult: "실행 결과",
-  printWinner: "최종 우승자",
+  printWinner: "최종 우승자 : ",
 });
 
 export const ERROR_MESSAGE = Object.freeze({
