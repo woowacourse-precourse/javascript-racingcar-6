a564ef37ba875356288b39f81d5d8f690a79a9c5
0jenn0 <jenn0.6n@gmail.com>
2023-10-30 08:20:34 +0900

    Refactor: Name 클래스의 이름 길이 제한 상수화
    이름 최대 길이값인 5를 NAME_SETTINGS 안으로 넣어 상수화하였음

--- src/Models/Name.js
+++ src/Models/Name.js
@@ -1,3 +1,7 @@
+const NAME_SETTINGS = Object.freeze({
+  max_name_length: 5,
+});
+
 export default class Name {
   constructor(string) {
     const trimmedString = string.trim();
@@ -6,7 +10,7 @@ export default class Name {
   }
 
   #validationSize(string) {
-    if (string.length <= 0 || string.length > 5)
+    if (string.length <= 0 || string.length > NAME_SETTINGS.max_name_length)
       throw new Error("[ERROR] 이름은 공백이 아니고 5자 이하여야합니다.");
   }
 }
