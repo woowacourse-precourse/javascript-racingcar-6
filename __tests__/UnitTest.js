4534cf5b1cf4a908c6e603d35b2e47614f075135
stegano9744 <stegano9744@gmail.com>
2023-10-29 11:49:47 +0900

    ✅ test: 자동차 이동 횟수 검증 테스트 추가

--- __tests__/UnitTest.js
+++ __tests__/UnitTest.js
@@ -7,4 +7,11 @@ describe('기능 테스트', () => {
       expect(car.length).toBeGreaterThan(5);
     });
   });
+
+  test('자동차 이동 횟수 검증 테스트', () => {
+    const inputs = ['pobi,woni', 'ab1'];
+    const count = Number(inputs[1]);
+
+    expect(Number.isNaN(count)).toBe(true);
+  });
 });
