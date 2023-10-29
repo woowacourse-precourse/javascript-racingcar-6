e669649f626c208429c54d193b373dc4ab429d50
kimsuyeon0916 <kimsuyeon_09162naver.com>
2023-10-29 18:39:14 +0900

    feat: 입력 받은 시도 횟수만큼 게임 반복

--- src/utils/CarRacing.js
+++ src/utils/CarRacing.js
@@ -2,10 +2,13 @@ import { Console } from "@woowacourse/mission-utils";
 import { IN_GAME_MESSAGE } from "./Constants.js";
 
 const CarRacing = {
-  async playCarRacing(){
+  async playCarRacing() {
     const carNameList = await Console.readLineAsync(IN_GAME_MESSAGE.getCarName);
     const tryCount = await Console.readLineAsync(IN_GAME_MESSAGE.getTryCount);
 
-  }
-}
+    for (let count = 0; count < tryCount; count+=1) {
+      
+    }
+  },
+};
 export default CarRacing;
