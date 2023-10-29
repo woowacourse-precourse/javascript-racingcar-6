51d63502aee6d84453452f43966c056bfbfc5ecb
at8in <at8in@naver.com>
2023-10-30 06:37:43 +0900

    feat(controller): 자동차 전진, 멈춤 기능 추가

---
+++ src/constants/controllers.js
@@ -0,0 +1,5 @@
+/**
+ * controllers 폴더 내에 사용되는 상수 정의
+ */
+
+export const STANDARD_NUMBER = 4;
--- src/controllers/RacingCarController.js
+++ src/controllers/RacingCarController.js
@@ -1,17 +1,31 @@
+import { STANDARD_NUMBER } from '../constants/controllers.js';
 import Car from '../models/Car.js';
 import InputValidator from '../utils/InputValidator.js';
+import NumberGenerator from '../utils/NumberGenerator.js';
 import InputView from '../views/InputView.js';
 
 class RacingCarController {
   #cars;
 
+  #moveCount;
+
   constructor() {
     this.#cars = [];
+    this.#moveCount = 0;
   }
 
   async startGame() {
     const names = await InputView.inputCarNames();
     this.makeCars(names.split(','));
+
+    const moveCount = await InputView.inputMoveCount();
+    if (InputValidator.checkMoveCount(moveCount)) {
+      this.#moveCount = moveCount;
+    }
+
+    for (let count = 0; count < this.#moveCount; count += 1) {
+      this.moveCars();
+    }
   }
 
   makeCars(names) {
@@ -19,6 +33,17 @@ class RacingCarController {
       if (InputValidator.checkCarName(name)) {
         const car = new Car(name);
         this.#cars.push(car);
+      }
+    });
+  }
+
+  moveCars() {
+    const numberGenerator = new NumberGenerator();
+
+    this.#cars.forEach(car => {
+      const randomNumber = numberGenerator.createRandomNumbers();
+      if (randomNumber >= STANDARD_NUMBER) {
+        car.moveForward();
       }
     });
   }
