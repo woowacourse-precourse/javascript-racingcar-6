c68965b98fd59c76d8370afb85a024e66924c9ee
donggyun <donggyunkim12@gmail.com>
2023-10-29 13:32:50 +0900

    refactor(Game): carListArr private 변수 사용

--- src/Game.js
+++ src/Game.js
@@ -3,23 +3,21 @@ import Car from './Car.js';
 import CarList from './CarList.js';
 
 class Game {
+  #carListArr = [];
+
   async init() {
     const carListInput = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
     );
-    const carListArr = carListInput.split(',');
-
-    return carListArr;
+    this.#carListArr = carListInput.split(',');
   }
 
-  async createCars(carListArr) {
+  async createCars() {
     const carList = new CarList();
 
-    carListArr.forEach((carName) => {
+    this.#carListArr.forEach((carName) => {
       carList.add(new Car(carName));
     });
-
-    return carList.cars;
   }
 }
 
