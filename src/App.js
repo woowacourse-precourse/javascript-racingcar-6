55e0cf34cd2c9dd7ea26582a64ed30aa7cd6f923
sang-il Yoon <yunsangil7749@gmail.com>
2023-10-29 14:26:37 +0900

    feat: 전체기능 동작코드 추가
    App 클래스의 paly 메소드가 동작 가능하도록 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
-  async play() {}
+  async play() {
+    try {
+       const CARS = await this.participatingCar()
+       const NUMBEROFCARS = await this.numberOfMoves()
+       this.startRacing(CARS, NUMBEROFCARS) 
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
 
   async participatingCar(){
   let cars = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
@@ -9,7 +17,7 @@ class App {
   
   cars.forEach(el => { // 유효성 검사.
     if(el.legnth > 5){
-      throw new Error();
+      throw new Error('[ERROR] 올바른 형식으로 입력해주세요');
     }
   });
 
@@ -20,7 +28,7 @@ class App {
   let NumberOfmoves = await Console.readLineAsync('시도할 횟수는 몇 회인가요?')
   // 시도횟수의 제한은 없으며 숫자만 입력 가능하다.
   if(isNaN(NumberOfmoves)){
-    throw new Error();
+    throw new Error('[ERROR] 올바른 형식으로 입력해주세요');
   }
 
   return NumberOfmoves;
