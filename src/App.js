51ad641596e25ac1282eb600a0e6157583fdf40f
suyeon <moa0823@naver.com>
2023-10-30 01:37:01 +0900

    feat(App) : 자동차 이름 입력받기
    -inputCarName() 메서드를 구현하여 사용자에게 입력받는 기능 추가
    
    5~9번째 줄 : 예시를 참고하여 문구 출력 후 줄바꿈한 뒤에 입력을 받기위해 이와 같이 코드 작성함

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import * as MissionUtils from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async inputCarName() {
+    await MissionUtils.Console.print(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+
+    return await MissionUtils.Console.readLineAsync("");
+  }
+
+  async play() {
+    const CAR_NAME_LIST = await this.inputCarName();
+    // MissionUtils.Console.print(CAR_NAME_LIST);
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
