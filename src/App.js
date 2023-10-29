6eb0cb85afb9ea32e5d8f2737f5508091e49d778
조민기 <58856846+Nail0606@users.noreply.github.com>
2023-10-29 16:56:45 +0900

    Fix : 횟수에 실수 타입 입력 불가능하게 수정

--- src/App.js
+++ src/App.js
@@ -2,66 +2,82 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 const ERROR_MESSAGES = {
   CAR_NAME_OVER: "[ERROR] 차량 이름은 5자 이하로 입력해 주세요",
-  CAR_NAME_ZERO: "[ERROR] 숫자만 입력해 주세요",
+  CAR_NAME_ZERO: "[ERROR] 정수만 입력해 주세요",
   INPUT_ATTEMPT_ZERO: "[ERROR] 횟수는 0을 입력할 수 없습니다.",
   INPUT_ATTEMPT_NOT_INTEGER: "[ERROR] 숫자만 입력해 주세요.",
 };
 
 class App {
   async play() {
-    const CARS = await MissionUtils.Console.readLineAsync(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
-    );
-    const ATTEMPS_COUNT = await MissionUtils.Console.readLineAsync(
-      "시도할 횟수는 몇 회인가요?\n"
-    );
-
-    const CAR_ARRAY = CARS.split(",");
-    CAR_ARRAY.forEach((car) => {
-      checkCarName(car);
-    });
-    checkAttempsCount(ATTEMPS_COUNT);
-
+    const CAR_ARRAY = await inputCarNames();
+    checkCarName(CAR_ARRAY);
+    const ATTEMPT_COUNT = +(await inputAttempt());
+    checkAttemptCount(ATTEMPT_COUNT);
+    const CAR_OBJECT_ARRAY = makeCarObject(CAR_ARRAY);
     MissionUtils.Console.print("실행 결과");
-    //각 변수를 object화 해서 값을 1씩 증가시키는 방법이 있음.
-    const OBJECT = CAR_ARRAY.map((car) => ({ name: car, value: 0 }));
 
-    for (let i = 0; i < ATTEMPS_COUNT; i++) {
-      OBJECT.forEach((car, index) => {
+    for (let i = 0; i < ATTEMPT_COUNT; i++) {
+      CAR_OBJECT_ARRAY.forEach((car, index) => {
         const RANDOMNUMBER = MissionUtils.Random.pickNumberInRange(0, 9);
         if (RANDOMNUMBER >= 4) {
-          OBJECT[index].value++;
+          CAR_OBJECT_ARRAY[index].value++;
         }
-        let value = "-".repeat(OBJECT[index].value);
-        MissionUtils.Console.print(`${OBJECT[index].name} : ${value}`);
+        let value = "-".repeat(CAR_OBJECT_ARRAY[index].value);
+        MissionUtils.Console.print(
+          `${CAR_OBJECT_ARRAY[index].name} : ${value}`
+        );
       });
       MissionUtils.Console.print("");
     }
 
-    const maxValue = OBJECT.reduce(
+    const maxValue = CAR_OBJECT_ARRAY.reduce(
       (max, current) => Math.max(max, current.value),
       -Infinity
     );
-    const maxObjects = OBJECT.filter((obj) => obj.value === maxValue);
+    const maxObjects = CAR_OBJECT_ARRAY.filter((obj) => obj.value === maxValue);
     const names = maxObjects.map((obj) => obj.name).join(", ");
     MissionUtils.Console.print(`최종 우승자 : ${names}`);
   }
 }
+function raceStart(car_object_array, attempt_count) {}
 
-function checkCarName(car) {
-  if (car.length > 5) {
-    throw new Error(ERROR_MESSAGES.CAR_NAME_OVER);
-  }
-  if (car.length === 0) {
-    throw new Error(ERROR_MESSAGES.CAR_NAME_ZERO);
-  }
+function makeCarObject(CAR_ARRAY) {
+  //각 변수를 object화 해서 값을 1씩 증가시키는 방법이 있음.
+  const CAR_OBJECT_ARRAY = CAR_ARRAY.map((car) => ({ name: car, value: 0 }));
+  return CAR_OBJECT_ARRAY;
 }
 
-function checkAttempsCount(attemps) {
-  if (isNaN(attemps)) {
+async function inputCarNames() {
+  const CARS = await MissionUtils.Console.readLineAsync(
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+  );
+  const CAR_ARRAY = CARS.split(",");
+  return CAR_ARRAY;
+}
+
+function inputAttempt() {
+  const ATTEMPT_COUNT =
+    MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+  return ATTEMPT_COUNT;
+}
+
+function checkCarName(CAR_ARRAY) {
+  CAR_ARRAY.forEach((car) => {
+    if (car.length > 5) {
+      throw new Error(ERROR_MESSAGES.CAR_NAME_OVER);
+    }
+    if (car.length === 0) {
+      throw new Error(ERROR_MESSAGES.CAR_NAME_ZERO);
+    }
+  });
+  return CAR_ARRAY;
+}
+
+function checkAttemptCount(attempt) {
+  if (isNaN(attempt) || !Number.isInteger(attempt)) {
     throw new Error(ERROR_MESSAGES.INPUT_ATTEMPT_NOT_INTEGER);
   }
-  if (+attemps === 0) {
+  if (attempt === 0) {
     throw new Error(ERROR_MESSAGES.INPUT_ATTEMPT_ZERO);
   }
 }
