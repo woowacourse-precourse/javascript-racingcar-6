79b603d7318c5eb132c53ad757429b2fb3671298
HarenKei <seungmin1266@gmail.com>
2023-10-29 23:43:40 +0900

    feat: 경주 할 횟수를 입력 받는 getInputGameCount 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,58 +1,64 @@
-import { Console, Random } from '@woowacourse/mission-utils';
-import Car from './Car.js';
+  import { Console, Random } from '@woowacourse/mission-utils';
+  import Car from './Car.js';
 
-class App {
-  constructor() {
-    this.CARS_ARRAY = [];
-    this.TEMP_CARS_ARRAY = [];
-  }
-
-  #checkArrayOfCars() {
-
-
-    if (new Set(this.TEMP_CARS_ARRAY).size !== this.TEMP_CARS_ARRAY.length) {
-      Console.print('중복 발생');
-      throw new Error('[Error]: 자동차 이름이 중복됩니다.');
+  class App {
+    constructor() {
+      this.CARS_ARRAY = [];
+      this.TEMP_CARS_ARRAY = [];
+      this.GAME_COUNT = 0;
     }
 
-    this.TEMP_CARS_ARRAY.forEach((element) => {
-      if (element.includes(' ') || element.length === 0 || element.length > 5) {
-        Console.print('올바르지 않은 입력');
-        throw new Error('[Error]: 자동차 이름이 중복되거나 올바르지 않습니다.');
+    #checkArrayOfCars() {
+      if (new Set(this.TEMP_CARS_ARRAY).size !== this.TEMP_CARS_ARRAY.length) {
+        Console.print('중복 발생');
+        throw new Error('[Error]: 자동차 이름이 중복됩니다.');
       }
-    });
 
-    return true;
-  }
+      this.TEMP_CARS_ARRAY.forEach((element) => {
+        if (element.includes(' ') || element.length === 0 || element.length > 5) {
+          Console.print('올바르지 않은 입력');
+          throw new Error('[Error]: 자동차 이름이 중복되거나 올바르지 않습니다.');
+        }
+      });
 
-  #makeArrayOfCars(carInput) {
-    this.TMP_CARS = carInput.split(',');
+      return true;
+    }
 
-    this.TMP_CARS.forEach((element) => {
-      this.CARS_ARRAY.push(new Car(element, 0));
-    });
+    #makeArrayOfCars(carInput) {
+      this.TMP_CARS = carInput.split(',');
 
-    try {
-      if (this.#checkArrayOfCars(this.TMP_CARS)) {
-        Console.print('올바른 입력');
-        Console.print(this.CARS_ARRAY);
+      this.TMP_CARS.forEach((element) => {
+        this.CARS_ARRAY.push(new Car(element, 0));
+      });
+
+      try {
+        if (this.#checkArrayOfCars(this.TMP_CARS)) {
+          Console.print('올바른 입력');
+          Console.print(this.CARS_ARRAY);
+        }
+      } catch (e) {
+        Console.print(e);
       }
-    } catch (e) {
-      Console.print(e);
+    }
+
+    async #getInputCars() {
+      const CAR_INPUT = await Console.readLineAsync(
+        '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+      );
+
+      this.#makeArrayOfCars(CAR_INPUT);
+    }
+
+    async #getInputGameCount() {
+      this.GAME_COUNT = await Console.readLineAsync(
+        '시도할 횟수는 몇 회인가요?\n'
+      );
+    }
+
+    async play() {
+      await this.#getInputCars();
+      await this.#getInputGameCount();
     }
   }
 
-  async #getInputCars() {
-    const CAR_INPUT = await Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
-    );
-
-    this.#makeArrayOfCars(CAR_INPUT);
-  }
-
-  async play() {
-    await this.#getInputCars();
-  }
-}
-
-export default App;
+  export default App;
