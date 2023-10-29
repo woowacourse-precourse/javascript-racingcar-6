5d9c5c222eb52e82db09692bcec01f9695c50306
coldok <2coldok@gmail.com>
2023-10-29 11:03:29 +0900

    refactor : 자동차 이름과 시도횟수 질응답 기능을 게임커스터마이즈 파일로 분리

--- src/App.js
+++ src/App.js
@@ -4,14 +4,27 @@ import { Console } from "@woowacourse/mission-utils";
 import { randomArrayGenerator } from "./util/RandomArrayGenerator.js";
 import Error from "./error/Error.js";
 
-class App {
+import { getPlayersName, getAttemptNumber } from "./controller/GameCustomize.js";
 
+class App {
+  
+  async play() {
+    const a = await getPlayersName();
+    const b = await getAttemptNumber();
+
+    Console.print('');
+    const j = new RacingController(a,b);
+    j.startRacing(randomArrayGenerator(a.length))
+  }
+
+  
+  /*
   constructor() {
     
     this.error = new Error();
   }
-  
 
+  
   async play() {
     const playersArray = await Console.readLineAsync(`${GAME_MESSEAGE.inputName}\n`);
     this.error.name(playersArray);
@@ -28,6 +41,8 @@ class App {
      
     
   }
+  */
+  
 
 
 }
---
+++ src/controller/GameCustomize.js
@@ -0,0 +1,24 @@
+import { Console } from "@woowacourse/mission-utils";
+import { GAME_MESSEAGE } from "../constants/Messeage.js";
+import Error from "../error/Error.js";
+
+export const getPlayersName = async () => {
+  const playersArray = await Console.readLineAsync(`${GAME_MESSEAGE.inputName}\n`);
+
+  const check = new Error();
+  check.name(playersArray);
+
+  return playersArray.split(',');
+}
+
+export const getAttemptNumber = async () => {
+  const attemptNumber = await Console.readLineAsync(`${GAME_MESSEAGE.inputAttempt}\n`);
+
+  const check = new Error();
+  check.attempt(attemptNumber);
+
+  return Number(attemptNumber);
+}
+
+
+
