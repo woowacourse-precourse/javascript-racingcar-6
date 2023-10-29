de25e8c09b117c9ad3a59e26665700b6e56b28f9
함지현 <gkawlgus01@naver.com>
2023-10-29 23:03:41 +0900

    fix: 자동차 이름 입력 수정
    쉽표(,)앞이 공백일 때 에러 출력

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
 import { Console, Random } from '@woowacourse/mission-utils'
 
+class Car {
+	constructor(name) {
+		this.name = name;
+		this.message = [];
+		this.maxDash = 0;
+	}
+
+	updateDash() {
+		const raceCarNumber = Random.pickNumberInRange(0, 9);
+		if (raceCarNumber >= 4) {
+			this.message.push('-');
+		}
+		const dashCount = this.message.filter((message) => message === '-').length;
+		if (dashCount > this.maxDash) {
+			this.maxDash = dashCount;
+		}
+	}
+}
+
 class App {
 	async play() {
 		const carNameInput = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+		if (!carNameInput.trim()) {
+			throw new Error('\x1b[31m[ERROR] 자동차 이름을 입력해야 합니다.\x1b[37m');
+		}
 		if (carNameInput.trim().endsWith(',')) {
 			throw new Error('\x1b[31m[ERROR] 쉼표로 끝나면 안됩니다.\x1b[37m');
 		}
 		const carNames = carNameInput.split(',').map((carNameInput) => carNameInput.trim());
 
-		const cars = carNames.map(carName => ({
-			name: carName,
-			messages: [],
-			maxDash: 0,
-		}));
-
 		carNames.forEach((carName) => {
 			if (carNames < 2) {
 				throw new Error('\x1b[31m[ERROR] 2명 이상의 이름을 입력해야합니다.\x1b[37m');
+			}
+			if (!carName) {
+				throw new Error('\x1b[31m[ERROR] 빈 자동차 이름은 입력할 수 없습니다.\x1b[37m');
 			}
 			if (carName.length >= 6) {
 				throw new Error('\x1b[31m[ERROR] 5글자 이하의 이름만 입력 가능합니다.\x1b[37m');
 			}
 		});
-		const raceTry = parseInt(await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n'));
+		const cars = carNames.map((carName) => new Car(carName));
+
+		let raceTry = parseInt(await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n'));
+
 		if (isNaN(raceTry)) {
 			throw new Error('\x1b[31m[ERROR] 숫자만 입력 가능합니다.\x1b[37m');
 		}
@@ -32,22 +54,16 @@ class App {
 
 		Console.print('\n실행 결과');
 
-		let newRaceTry = raceTry;
-		while (newRaceTry > 0) {
-			for (let i = 0; i < cars.length; i++) {
-				const raceCarNumber = Random.pickNumberInRange(0, 9);
-				if (raceCarNumber >= 4) {
-					cars[i].messages.push('-');
-				}
-				const dashCount = cars[i].messages.filter((message) => message === '-').length;
-				if (dashCount > cars[i].maxDash) {
-					cars[i].maxDash = dashCount;
-				}
-				Console.print(`${cars[i].name} : ${cars[i].messages.join('')}`);
-			}
+		while (raceTry > 0) {
+			cars.forEach((car) => car.updateDash());
+			cars.forEach((car) => {
+				Console.print(`${car.name} : ${car.message.join('')}`);
+			});
+			
 			Console.print('');
-			newRaceTry--;
+			raceTry--;
 		}
+
 		const maxDashCount = Math.max(...cars.map(car => car.maxDash));
 		const winner = cars.filter(car => car.maxDash === maxDashCount);
 		const winnerName = winner.map(winner => winner.name);
