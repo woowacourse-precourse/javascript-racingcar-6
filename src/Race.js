73ac185fd81d6898912f49250b15b0bf1176372d
noodle <noodle>
2023-10-30 01:19:53 +0900

    refactor: Race.js 불필요한 주석 삭제, 변수명 수정

--- src/Race.js
+++ src/Race.js
@@ -26,14 +26,13 @@ class Race {
   printRaceStatus() {
     for (const car of this.cars) {
       const name = car.name;
-      const position = car.getPosition();
-      const dash = '-'.repeat(position);
+      const currentPosition = car.getPosition();
+      const dash = '-'.repeat(currentPosition);
 
       PrinterView.print(`${name} : ${dash}`);
     }
   }
 
-  // TODO: 맥스값 로직 수정 필요
   findWinner() {
     const maxPosition = Math.max(...this.cars.map((car) => car.getPosition()));
     return this.cars.filter((car) => car.getPosition() === maxPosition).map((winner) => winner.name);
