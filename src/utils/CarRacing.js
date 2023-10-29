0bab2c639541652f58fa66ec0d13ecab1716dd76
kimsuyeon0916 <kimsuyeon_09162naver.com>
2023-10-29 19:27:18 +0900

    feat: 자동차 별, 무작위 전진 값 출력

--- src/utils/CarRacing.js
+++ src/utils/CarRacing.js
@@ -2,13 +2,21 @@ import { Console } from "@woowacourse/mission-utils";
 import { IN_GAME_MESSAGE } from "./Constants.js";
 
 const CarRacing = {
+
   async playCarRacing() {
-    const carNameList = await Console.readLineAsync(IN_GAME_MESSAGE.getCarName);
+    const carNameList = await Console.readLineAsync(IN_GAME_MESSAGE.getCarName).split(',');
     const tryCount = await Console.readLineAsync(IN_GAME_MESSAGE.getTryCount);
 
     for (let count = 0; count < tryCount; count+=1) {
-      
+      this.showTryResult(carNameList);
     }
   },
+
+  showTryResult(carNameList, carMovesList){
+    carNameList.forEach((carName, index) => {
+      Console.print(`${carName} : ${carMovesList[index]}`);
+    })
+  }
+
 };
 export default CarRacing;
--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,7 +1,7 @@
 const IN_GAME_MESSAGE = {
   getCarName: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
-  getTryCount: '시도할 횟수는 몇 회인가요?',
-  tryResult: '실행 결과',
+  getTryCount: '시도할 횟수는 몇 회인가요?\n',
+  tryResult: '\n실행 결과\n',
   finalWinner: '최종 우승자 : ',
 }
 
