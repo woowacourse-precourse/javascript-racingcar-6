991feb42daacae26e33b121aea46d5d34b4b8320
thumbthing <ilscc0902@gmail.com>
2023-10-29 14:29:27 +0900

    test: 에러발생 test 케이스 추가

--- __tests__/receiveCarNameTest.js
+++ __tests__/receiveCarNameTest.js
@@ -1,17 +1,16 @@
-import { defaultErrorHandler, gameErrorhandler } from '../src/util/error/errorhandler';
+import { defaultErrorHandler } from '../src/util/error/errorhandler';
 import { consolePrint } from '../src/util/libraryFeatures/consoleHandler';
-import { checkCarValid } from '../src/util/ready/checkInputValid';
+import { checkCarNameValid } from '../src/util/ready/checkInputValid';
 
 const mockReciveCarName = async (inputValue, text) => {
   try {
     await consolePrint(text);
     const input = inputValue;
-    const carList = await checkCarValid(input);
-    console.log(carList);
+    const carList = await checkCarNameValid(input);
     return carList;
   } catch (error) {
-    defaultErrorHandler(error);
-    throw error;
+    const promise = defaultErrorHandler(error);
+    return promise;
   }
 };
 
@@ -24,8 +23,17 @@ describe('자동차 경주 게임 입력 테스트', () => {
 
     // when
     const carNames = await mockReciveCarName(input, consoleText);
-
     // then
     expect(carNames).toEqual(result);
+  });
+
+  test('valid 검사를 통과 못하는 입력값일 경우 error code에 해당하는 Error를 발생', async () => {
+    // given
+    const consoleText = '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n';
+    const input = 'aaaaaaaa,qqqqqqqq,zzzzzzzzzz';
+    const result = '자동차의 이름이 너무 깁니다';
+
+    // then
+    await expect(() => mockReciveCarName(input, consoleText).rejects.toThrow(result));
   });
 });
