5d8342e366abf85f444d54f4a38b9381439a0f29
swarvy <swarvy0826@naver.com>
2023-10-29 13:01:53 +0900

    refactor : OutputView.js에 JSDocs 추가

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -3,10 +3,19 @@ import { GRANDPRIX_RESULT_NOTIFICATION } from '../constants/GrandPrixMessage.js'
 import { SYMBOLS } from '../constants/Symbols.js';
 
 const OutputView = Object.freeze({
+  /**
+   * @public
+   * @returns void
+   */
   printLapResult() {
-    return Console.print(GRANDPRIX_RESULT_NOTIFICATION.printLapResult);
+    Console.print(GRANDPRIX_RESULT_NOTIFICATION.printLapResult);
   },
 
+  /**
+   * @public
+   * @param {{name: string, status: number}} racingGrid
+   * @returns {void}
+   */
   printRacingGrid(racingGrid) {
     racingGrid.forEach(({ name, status }) =>
       Console.print(GRANDPRIX_RESULT_NOTIFICATION.printRacingGrid(name, status)),
@@ -14,8 +23,13 @@ const OutputView = Object.freeze({
     Console.print(SYMBOLS.empty);
   },
 
+  /**
+   * @public
+   * @param {string} winner
+   * @returns {void}
+   */
   printRaceWinner(winner) {
-    return Console.print(GRANDPRIX_RESULT_NOTIFICATION.printRaceWinner(winner));
+    Console.print(GRANDPRIX_RESULT_NOTIFICATION.printRaceWinner(winner));
   },
 });
 
