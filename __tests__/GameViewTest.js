0cf45ceb431e3ff1e91995e2026b3a84960bb5c1
ReturnReason <reasonz@daum.net>
2023-10-29 17:39:38 +0900

    test(GameViewTest): printResult 메서드 테스트 추가

--- __tests__/GameViewTest.js
+++ __tests__/GameViewTest.js
@@ -1,19 +1,30 @@
-import GameView from '../src/model/GameView.js';
+import GameView from '../src/view/GameView.js';
 import { MESSAGE } from '../src/constants/messages.js';
+import { Console } from '@woowacourse/mission-utils';
 
-const getLogSpy = () => {
-  const logSpy = jest.spyOn(GameView, 'print');
+const createSpy = () => {
+  const logSpy = jest.spyOn(Console, 'print');
   logSpy.mockClear();
   return logSpy;
 };
 
 describe('✨ [GameView] 클래스 메서드 테스트', () => {
-  const logSpy = getLogSpy();
+  const logSpy = createSpy();
+  const view = new GameView();
 
-  test('print 메서드는 인자로 받은 메시지를 출력한다.', () => {
+  test('[print] 인자로 받은 메시지를 출력한다.', () => {
     const message = MESSAGE.START;
 
-    GameView.print(message);
+    view.print(message);
     expect(logSpy).toHaveBeenCalledWith(message);
+  });
+
+  test('[printResult] name과 count키 객체를 인자로 받아 count만큼 - 개수를 출력한다.', () => {
+    const name = 'reason';
+    const count = 4;
+    const expectedString = `${name} : ${'-'.repeat(count)}`;
+
+    view.printResult({ name, count });
+    expect(logSpy).toHaveBeenCalledWith(expectedString);
   });
 });
