539744beb0aaa32379239113ae5b74b5c94db8b8
Lee-Sunho <sunho9889@naver.com>
2023-10-29 23:38:02 +0900

    feat: GameController 클래스 생성 및 경주 실행 결과 출력 메소드 구현

--- src/GameController.js
+++ src/GameController.js
@@ -2,6 +2,7 @@ import { RandomNumberGenerator } from "./utils/RandomNumberGenerator.js";
 import { User } from "./User.js";
 import { Console } from "@woowacourse/mission-utils";
 import { Car } from "./Car.js";
+import * as m from "./constants/message.js";
 
 export class GameController {
   constructor() {
@@ -14,5 +15,30 @@ export class GameController {
     const tryNumber = await this.user.inputTryNumber();
 
     names.forEach((name) => this.carList.push(new Car(name)));
+    Console.print(m.TRY_RESULT_MESSAGE);
+    for (let i = 0; i < tryNumber; i++) {
+      this.startRace();
+      this.printRaceResult();
+    }
+  }
+
+  startRace() {
+    this.carList.forEach((car) => {
+      const randomNumber = RandomNumberGenerator.generateRandomNumber();
+      if (randomNumber >= 4) {
+        car.move();
+      }
+    });
+  }
+
+  printRaceResult() {
+    this.carList.forEach((car) => {
+      const distance = [];
+      for (let i = 0; i < car.distance; i++) {
+        distance.push("-");
+      }
+      Console.print(`${car.name} : ${distance.join("")}`);
+    });
+    Console.print("");
   }
 }
