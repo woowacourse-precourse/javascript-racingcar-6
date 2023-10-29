72afc261e4dd48c4ab84d3c1a1f1b45f9c3fa37c
jinsil <jinsil378900@gmail.com>
2023-10-30 00:15:30 +0900

    Feat: 경주할 자동차 이름과 횟수 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { Console } from "@woowacourse/mission-utils";
+import RacingCar from "./components/RacingCar.js";
+import Messages from "./constants/Messages.js";
+
 class App {
-  async play() {}
+  async play() {
+    const inputName = await Console.readLineAsync(Messages.INPUT_NAME);
+    const inputNameArray = inputName.split(",");
+    if (!inputName.length) {
+      throw new Error("[ERROR] 자동차 이름을 입력해주세요");
+    }
+    for (let i = 0; i < inputNameArray.length; i++) {
+      if (inputNameArray[i].includes(" ")) {
+        throw new Error("[ERROR] 띄어쓰기 안 됩니다");
+      }
+      if (inputNameArray[i].length > 5) {
+        throw new Error("[ERROR] 이름은 5자 이하로만 써주세요");
+      }
+    }
+    const inputNumber = Number(await Console.readLineAsync(Messages.INPUT_NUMBER));
+    if (!Number.isInteger(inputNumber) || inputNumber < 1) {
+      throw new Error("[ERROR] 1이상 정수로 적어주세요");
+    }
+    const racingCar = new RacingCar(inputNameArray, inputNumber);
+    racingCar.output();
+  }
 }
 
 export default App;
