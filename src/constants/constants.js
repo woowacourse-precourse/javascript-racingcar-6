59d779298039993b68bf3a12914208b296518765
PARK-CHANUK <doosan1224@naver.com>
2023-10-29 16:22:10 +0900

    feat(constants): 상수 추가

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,10 +1,11 @@
 const ZERO = 0;
 const ONE = 1;
 const NEW_LINE = '\n';
+const SPACE = ' ';
 
 const STICK = Object.freeze({
   newSticks: '',
   oneStick: '-',
 });
 
-export { ZERO, ONE, NEW_LINE, STICK };
+export { ZERO, ONE, NEW_LINE, SPACE, STICK };
