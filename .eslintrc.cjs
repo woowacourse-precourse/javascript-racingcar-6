645cb6fd284e09876ff742184542b5378911f24e
stegano9744 <stegano9744@gmail.com>
2023-10-29 11:43:12 +0900

    🔧 config: eslint, prettier, airbnb 코딩 컨벤션 적용

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier', 'jest'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    // 들여쓰기 깊이 제한
+    'max-depth': ['error', 2],
+  },
+};
