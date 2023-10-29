b25d1d3a10a05ba993bcd494a38c017c6926704b
hong <ahi200@naver.com>
2023-10-29 16:43:54 +0900

    feat: 사용자 입력 유효성 확인 메서드 추가

--- src/Validation.js
+++ src/Validation.js
@@ -1,5 +1,19 @@
 const CAR_NAME_MAX_LENGTH = 5;
 class Validation {
+  isProperInput(userInput) {
+    const carList = userInput.split(',');
+    if (carList.length === 0) {
+      return false;
+    }
+    if (!carList.every(this.hasProperCarName)) {
+      return false;
+    }
+    if (!carList.every(this.hasProperCarNameLength)) {
+      return false;
+    }
+    return true;
+  }
+
   hasProperCarName(car) {
     const regex = /^[a-zA-Z]+$/;
     return regex.test(car);
