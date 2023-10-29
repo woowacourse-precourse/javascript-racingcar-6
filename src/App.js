831977d0fcbb381045782a2770aa7aa7a2a12f00
김용민 <dydals3440@gmail.com>
2023-10-29 19:48:47 +0900

    feat: 입력받은 자동차 이름을 쉼표로 구분합니다.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Print from './Print';
 import Validate from './Validate';
-import { ERROR } from './constants/constants';
+import { COMMAS, ERROR } from './constants/constants';
 
 class App {
   async play() {
     const participateCars = await Print.getCarsName();
+
+    const cars = participateCars.split(COMMAS.SYMBOL);
 
     if (!Validate.isCarNameLengthValid(cars)) {
       throw new Error(ERROR.CAR_NAME_LENGTH);
