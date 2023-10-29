6acc9aa49c8044833c87d4d1b9303ccbc2f4062b
coddingyun <igy2840@gmail.com>
2023-10-29 12:37:58 +0900

    feat: 경주할 자동차 이름 입력값 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import * as MissionUtils from "@woowacourse/mission-utils";
 class App {
-  async play() {}
+  async play() {
+    const getInputValue = async() => {
+      const carName = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n')
+      return carName.split(',')
+    };
+    const carNameArr = await getInputValue();
+  }
 }
 
 export default App;
