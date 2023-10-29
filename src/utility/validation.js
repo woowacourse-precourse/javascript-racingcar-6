7bb405d772bb40b2a970f4644c5749b4ff1b4730
ImxYJL <allensain14@gmail.com>
2023-10-29 20:24:03 +0900

    feat: 10진수 숫자인지를 판단하는 함수 작성

--- src/utility/validation.js
+++ src/utility/validation.js
@@ -1,3 +1,5 @@
 import { ERROR_MESSAGE } from '../constant/message';
 
 export const isWithinZeroToNine = (number) => number >= 0 && number <= 9;
+
+export const isDecimalChar = (character) => /^[0-9]$/.test(character);
