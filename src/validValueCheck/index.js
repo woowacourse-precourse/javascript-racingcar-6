8b6712b808c79e7122f31aedc9d2096befd9a728
Jihyun_Kim <jihyun3313@daum.net>
2023-10-29 22:48:14 +0900

    fix: 자동차이름입력 중복체크 수정

--- src/validValueCheck/index.js
+++ src/validValueCheck/index.js
@@ -1,6 +1,8 @@
 export const carNameCheck = async (carsArray) => {
-  carsArray.forEach((e) => {
-    if (e.length > 5 || carsArray.includes(e)) {
+  console.log("carsArray", carsArray);
+  carsArray.forEach((e, index) => {
+    console.log("e", e);
+    if (e.length > 5 || carsArray.lastIndexOf(e)!==index) {
       throw new Error("[ERROR] 숫자가 잘못된 형식입니다.")
     }
   })
