e8db3fd5b823593c57b87d39d2f297b8ee9d7ea9
ss0526100 <ss0526100@gmail.com>
2023-10-29 17:51:00 +0900

    chore(Print): constantize blank

--- src/modules/Print.js
+++ src/modules/Print.js
@@ -2,13 +2,14 @@ import { Console } from '@woowacourse/mission-utils';
 import Get from './Get.js';
 const RESULT_FIRST_MESSAGE = '\n실행 결과';
 const WINNER_FROM_FIRST_MESSAGE = '최종 우승자 : ';
+const BLANK = '';
 
 class Print {
   static racingResultFrom(racingInfo) {
     Console.print(RESULT_FIRST_MESSAGE);
     for (let gameCount = 0; gameCount < racingInfo.numberOfGame; gameCount++) {
       Print.racingPerGame(racingInfo, gameCount);
-      Console.print('');
+      Console.print(BLANK);
     }
   }
   static racingPerGame(racingInfo, gameCount) {
