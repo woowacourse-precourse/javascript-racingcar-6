e0945a9b7c6bf18f8fb5e625912720de5c8e0ea7
working-zima <working.zima@gmail.com>
2023-10-29 20:52:41 +0900

    feat(simulateCarRace): 게임 진행기능 구현
    calculateRaceProgress 메서드로 레이싱 실행 결과를 반환
    calculateRaceProgress 에서 반환된 결과를 printRaceResults 메서드로 출력

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Console } from '@woowacourse/mission-utils';
+import { Console, Random } from '@woowacourse/mission-utils';
 import Validator from './utils/Validator.js';
 
 class App {
@@ -12,19 +12,54 @@ class App {
     return Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
   }
 
-  static splitAndValidate(carNames) {
-    Validator.validateUndefinedOrNullOrSpacesOrLengthZero(carNames);
-    carNames.split(',').forEach(name => {
+  static ValidateCarNames(arrayedCarNames) {
+    Validator.validateUndefinedOrNullOrSpacesOrLengthZero(arrayedCarNames[0]);
+    arrayedCarNames.forEach(name => {
       Validator.isMaxLengthFive(name);
     });
     return true;
   }
 
+  static isMinNumFour() {
+    return Random.pickNumberInRange(0, 9) >= 4;
+  }
+
+  static calculateRaceProgress(raceProgressGraph) {
+    return raceProgressGraph.map((_, idx) => {
+      if (App.isMinNumFour()) return `${raceProgressGraph[idx]}-`;
+      return raceProgressGraph[idx];
+    });
+  }
+
+  static printRaceResults(arrayedCarNames, raceProgressGraph) {
+    let RaceResults = '';
+    arrayedCarNames.forEach((car, idx) => {
+      RaceResults = `${RaceResults}${car} : ${raceProgressGraph[idx]}\n`;
+    });
+    Console.print(RaceResults);
+  }
+
+  static simulateCarRace(arrayedCarNames, moveCount) {
+    let raceProgressGraph = Array.from(
+      { length: arrayedCarNames.length },
+      () => '',
+    );
+    let copiedMoveCount = moveCount;
+    Console.print('\n실행 결과');
+    while (copiedMoveCount) {
+      raceProgressGraph = App.calculateRaceProgress(raceProgressGraph);
+      App.printRaceResults(arrayedCarNames, raceProgressGraph);
+      copiedMoveCount -= 1;
+    }
+  }
+
   static async startGame() {
     const carNames = await App.getCarNamesInput();
-    App.splitAndValidate(carNames);
+    const arrayedCarNames = carNames.split(',');
+    App.ValidateCarNames(arrayedCarNames);
     const moveCount = await App.getMoveCountInput();
     Validator.validateMoveCount(moveCount);
+    this.simulateCarRace(arrayedCarNames, moveCount);
   }
 
   async play() {
