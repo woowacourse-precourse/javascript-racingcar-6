a392951700fefae640c82f51f983efbd062a05d3
ImxYJL <allensain14@gmail.com>
2023-10-29 21:17:00 +0900

    feat: 배열에서 key의 최댓값을 찾는 함수 작성

--- src/utility/array.js
+++ src/utility/array.js
@@ -1,2 +1,8 @@
 export const filterObjectsByValue = (array, key, value) =>
   array.filter((object) => object[key] === value);
+
+export const findMaxByKey = (array, key) =>
+  array.reduce(
+    (max, object) => (object[key] > max ? object[key] : max),
+    array[0][key],
+  );
