e3665e55a666b4262d6c2d1057ddb745e62dcfa3
김민승 <albert050509@naver.com>
2023-10-29 10:24:01 +0900

    test(racingcar) : test Game Start Validation Part

--- __tests__/GamStartTest.js
+++ __tests__/GamStartTest.js
@@ -1,10 +1,11 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import App from "../src/App";
 
-const mockQuestions = (input) => {
+const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
 
   MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
     return Promise.resolve(input);
   });
 };
@@ -12,9 +13,22 @@ const mockQuestions = (input) => {
 describe("어플리케이션 입력 테스트", () => {
   test("자동차 이름 입력에서 이름의 길이가 5자리를 넘는지 확인", async () => {
     //given
-    const input = "hellllo";
+    const inputs = ["hellllo", "1"];
 
-    mockQuestions(input);
+    mockQuestions(inputs);
+
+    //when
+    const app = new App();
+
+    //then
+    await expect(app.play()).rejects.toThrow("[ERROR]");
+  });
+
+  test("자동차 이름 입력에서 이름에 중복이 있는지 없는지 확인", async () => {
+    //given
+    const inputs = ["kim,kim", "1"];
+
+    mockQuestions(inputs);
 
     //when
     const app = new App();
@@ -25,9 +39,22 @@ describe("어플리케이션 입력 테스트", () => {
 
   test("사용자 숫자 입력에서 숫자 형식을 지키는 지 확인", async () => {
     //given
-    const input = "hello world!";
+    const inputs = ["pobi,woni", "helloworld"];
 
-    mockQuestions(input);
+    mockQuestions(inputs);
+
+    //when
+    const app = new App();
+
+    //then
+    await expect(app.play()).rejects.toThrow("[ERROR]");
+  });
+
+  test("사용자 숫자 입력에서 0이상의 숫자가 들어오는지 확인", async () => {
+    //given
+    const inputs = ["pobi,woni", "-1"];
+
+    mockQuestions(inputs);
 
     //when
     const app = new App();
