16f094b74297e613db23c6258ca9d0fc8f2486a0
shyeon0602 <july8560@naver.com>
2023-10-29 16:14:30 +0900

    feat: validate 코드 작성

--- src/Controller/Validate/CarName.js
+++ src/Controller/Validate/CarName.js
@@ -1,17 +1,64 @@
+const outputView = require("../../View/outputView");
+import { ERROR } from "../../Utils/Message";
+
 class CarName {
-    #input;
+  #input;
 
-    constructor(input){
-        this.#input = input.split(',');
+  constructor(input) {
+    this.#input = input.split(",");
+  }
+
+  // 에러메세지 전달
+  validate() {
+    if (this.check()) {
+      outputView.printError(ERROR.CAR_NAME);
+      return false;
     }
+    return true;
+  }
 
-    validate(){
-        if()
-    }
+  // 유효성 검사
+  check() {
+    return this.isAnotherStr() || this.isDuplicate() || this.isNameLength();
+  }
 
-    check(){
+  // 자동차이름 5글자 이하
+  isNameLength() {
+    let check = false;
+    this.#input.forEach((carname) => {
+      if (carname.length > 5 || carname.length < 1) {
+        check = true;
+      }
+    });
+    return check;
+  }
 
-    }
+  // 문자 유효성검사
+  isAnotherStr() {
+    const regex = /^[가-힣|a-z|A-Z|]/;
+    let check = false;
+    this.#input.forEach((carname) => {
+      carname.split("").forEach((value) => {
+        if (!regex.test(value)) {
+          check = true;
+        }
+      });
+    });
+    return check;
+  }
 
-    
+  // 중복 검사
+  isDuplicate() {
+    let check = false;
+    this.#input.forEach((carname) => {
+      carname.split("").forEach((carnameArr) => {
+        if (new Set(carnameArr).size != 3) {
+          check = true;
+        }
+      });
+    });
+    return check;
+  }
 }
+
+module.exprots = CarName;
