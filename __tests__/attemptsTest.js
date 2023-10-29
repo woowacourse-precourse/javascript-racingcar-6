cd3146f11b51b96a005df592b395401e35d132a1
minki3 <86992006+minki3@users.noreply.github.com>
2023-10-30 00:29:07 +0900

    test:시도 횟수 입력 테스트코드 작성

--- __tests__/attemptsTest.js
+++ __tests__/attemptsTest.js
@@ -1,1 +1,9 @@
-test("시도 횟수 입력 테스트", () => {});
+import attempts from "../src/attempts";
+
+test("시도 횟수 입력 테스트", () => {
+  const input = 3;
+
+  expect(() => {
+    attempts(input).toBe(3);
+  });
+});
