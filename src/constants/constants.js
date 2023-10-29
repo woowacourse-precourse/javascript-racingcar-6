a7adfb4dc90ca26d665adacdff406aacca7f1fe0
uraflower <82873315+uraflower@users.noreply.github.com>
2023-10-29 23:15:15 +0900

    comment: 상수가 사용되는 부분에 대한 주석 추가

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,10 @@
 export const MIN_NAME_LENGTH = 1;
 export const MAX_NAME_LENGTH = 5;
 
+// random number generation
 export const MIN_RANGE = 0;
 export const MAX_RANGE = 9;
+
+// car movement
 export const MIN_MOVEMENT = 4;
 export const MOVEMENT_FORWARD = '-';
