ffc6c0dc7e31e0625a4d4de2991a07d7589593b4
yunsoeirm1116 <hhyjs2r@gmail.com>
2023-10-30 01:40:14 +0900

    [refactor] private 변수명 변경, 쓸모없는 util 함수 삭제

--- src/models/Car.js
+++ src/models/Car.js
@@ -1,7 +1,7 @@
 import { Random } from '@woowacourse/mission-utils';
 
 class Car {
-  #MINIMUM_MOVE_VALUE = 4;
+  _MINIMUM_MOVE_VALUE = 4;
   _position;
   /**
    * @param {string} carName - 차량의 이름을 나타내는 문자열
@@ -13,7 +13,7 @@ class Car {
 
   move() {
     const randomNumber = Random.pickNumberInRange(0, 9);
-    if (randomNumber >= this.#MINIMUM_MOVE_VALUE) {
+    if (randomNumber >= this._MINIMUM_MOVE_VALUE) {
       this._setPosition('-');
     }
   }
--- src/utils/calculateLongestDistance.js
+++
@@ -1,4 +0,0 @@
-export function calculateLongestDistance(carModles) {
-  const maxPosition = Math.max(...carModles.map(car => car.position.length));
-  return maxPosition;
-}
