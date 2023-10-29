2ff73de4aac2387a7470aab7ad6d4b8cff4bbfcc
leehanjun00 <yeehj20@naver.com>
2023-10-30 00:32:04 +0900

    style: 변수명 표기법 변경

--- src/CarName.js
+++ src/CarName.js
@@ -1,15 +1,15 @@
 import { Console } from '@woowacourse/mission-utils';
 import Car from './Car.js';
-import Play from './play.js';
+import Play from './Play.js';
 
 class CarName {
   checkNumberDigits(carNames) {
     const carNamesInArray = carNames.split(',');
 
     const carList = carNamesInArray.map((carName) => {
-      const MORE_THAN_5_CHARACTERS = carName.length > 5;
+      const moreThan5Characters = carName.length > 5;
 
-      if (MORE_THAN_5_CHARACTERS) {
+      if (moreThan5Characters) {
         throw new Error('[ERROR] 자동차의 이름은 5자 이하만 가능합니다.');
       }
       // 같은이름 검사기능 추가하기
@@ -17,22 +17,22 @@ class CarName {
     });
 
     const play = new Play();
-    play.횟수입력(carList);
+    play.enterNumberOfTimes(carList);
   }
 
   checkComma(carNames) {
-    const HAS_COMMA = carNames.includes(',');
-    const HAS_EMTY = carNames.includes(' ');
+    const hasComma = carNames.includes(',');
+    const hasEmty = carNames.includes(' ');
 
-    if (HAS_COMMA) {
+    if (hasComma) {
       this.checkNumberDigits(carNames);
     }
 
-    if (!HAS_COMMA) {
+    if (!hasComma) {
       throw new Error('[ERROR] 쉼표(,)를 사용해 이름을 구분 하세요.');
     }
 
-    if (HAS_EMTY) {
+    if (hasEmty) {
       throw new Error('[ERROR] 빈공간이 없이 작성해주세요.');
     }
   }
