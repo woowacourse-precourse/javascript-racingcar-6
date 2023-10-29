16906bece5be126e955c783cb0357b666dc73150
DESKTOP-K188DV0\tlstj <tlstjsdud566@naver.com>
2023-10-29 13:31:46 +0900

    [feat] 자동차 경주에서 사용될 상수 선언

---
+++ src/Constant.js
@@ -0,0 +1,8 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+export const { Console, Random } = MissionUtils;
+
+export const READ_RACING_CAR_NAMES =
+  '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)';
+export const READ_NUMBER_OF_ATTEMPTS = '시도할 횟수는 몇 회인가요?';
+export const RACING_RESULT = '실행 결과';
+export const RACING_FINAL_WINNER = '최종 우승자 : ';
