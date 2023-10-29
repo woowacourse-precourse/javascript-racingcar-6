5d575d3fcf1de86dd287497624a5e95178bdfbd1
k-redstone <che0618@naver.com>
2023-10-29 22:20:09 +0900

    fix: 게임결과 출력 테스트 코드 수정

--- __tests__/gameResultTest.js
+++ __tests__/gameResultTest.js
@@ -1,21 +1,6 @@
 import App from "../src/App.js";
+import { getWinList, printResult } from "../src/racingGame/utils/gameFn.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
-
-const mockQuestions = (inputs) => {
-  MissionUtils.Console.readLineAsync = jest.fn();
-
-  MissionUtils.Console.readLineAsync.mockImplementation(() => {
-    const input = inputs.shift();
-    return Promise.resolve(input);
-  });
-};
-
-const mockRandoms = (numbers) => {
-  MissionUtils.Random.pickNumberInRange = jest.fn();
-  numbers.reduce((acc, number) => {
-    return acc.mockReturnValueOnce(number);
-  }, MissionUtils.Random.pickNumberInRange);
-};
 
 const getLogSpy = () => {
   const logSpy = jest.spyOn(MissionUtils.Console, "print");
@@ -23,22 +8,54 @@ const getLogSpy = () => {
   return logSpy;
 };
 
-describe("결과출력", () => {
+describe("결과 출력 (단독우승)", () => {
   test("결과 출력", async () => {
     // given
-    const inputs = ["hong,woni,che12", "3"];
-    const randoms = [4, 3, 2, 5, 5, 6, 1, 2, 9];
+    const inputs = [
+      {
+        name: "hong",
+        moveCount: 5,
+      },
+      {
+        name: "woni",
+        moveCount: 2,
+      },
+      {
+        name: "che12",
+        moveCount: 5,
+      },
+    ];
+    const maxMove = 5;
     const result = "최종우승자 : hong, che12";
     const logSpy = getLogSpy();
 
-    mockQuestions(inputs);
-    mockRandoms([...randoms]);
+    printResult(getWinList(inputs, maxMove));
 
-    // when
-    const app = new App();
-    await app.play();
+    expect(logSpy).toHaveBeenCalledWith(expect.stringMatching(result));
+  });
 
-    // then
-    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(result));
+  test("결과 출력 (단독우승)", async () => {
+    // given
+    const inputs = [
+      {
+        name: "hong",
+        moveCount: 3,
+      },
+      {
+        name: "woni",
+        moveCount: 2,
+      },
+      {
+        name: "che12",
+        moveCount: 4,
+      },
+    ];
+    const maxMove = 4;
+    const result = "최종우승자 : che12";
+    const logSpy = getLogSpy();
+
+    printResult(getWinList(inputs, maxMove));
+
+    expect(logSpy).toHaveBeenCalledWith(expect.stringMatching(result));
   });
 });
