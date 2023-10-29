2894268647d783b64d3b512638b73b0ea53b9cb0
yheseul_kang <kangyheseul@naver.com>
2023-10-29 14:49:33 +0900

    Feat: 예외 발생 기능 추가 // player가 경주할 자동차 이름을 입력할 때 잘못된 값을 입력한 경우 throw문을 사용해 [ERROR]로 시작하는 메시지를 가지는 예외를 발생시킨 후, 애플리케이션은 종료되는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import { Console } from "@woowacourse/mission-utils";
 import { MESSAGE } from "./message.js";
 
-
 class App {
   async play() {
     Console.print(MESSAGE.GAME_START)
-    Console.readLineAsync(MESSAGE.RACING_CAR_NAME)
+
+    const player = await Console.readLineAsync(MESSAGE.RACING_CAR_NAME);
+    
+    const player_array = player.split(',');
+    player_array.forEach(function (value, index) {
+    if (player_array[index].length > 5) throw new Error(MESSAGE.ERROR);
+    });
+
+    const REGEX = /[`~!@#$%^&*()_|+\-=?;:'".<>\{\}\[\]\\\/ ]/gim;
+    if (player.includes(',') !== true &&
+    REGEX.test(player) !== true &&
+    player_array.length <= 1) {throw new Error(MESSAGE.ERROR)};  
   }
 }
 const app = new App(); 
