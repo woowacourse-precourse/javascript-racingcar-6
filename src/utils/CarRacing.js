24e3fa249e648924ebb5536d1fd311ec569f1741
kimsuyeon0916 <kimsuyeon_09162naver.com>
2023-10-29 18:35:09 +0900

    feat: 사용자에게 이동 시도 횟수 입력 받기

--- src/utils/CarRacing.js
+++ src/utils/CarRacing.js
@@ -4,7 +4,8 @@ import { IN_GAME_MESSAGE } from "./Constants.js";
 const CarRacing = {
   async playCarRacing(){
     const carNameList = await Console.readLineAsync(IN_GAME_MESSAGE.getCarName);
-    
+    const tryCount = await Console.readLineAsync(IN_GAME_MESSAGE.getTryCount);
+
   }
 }
 export default CarRacing;
