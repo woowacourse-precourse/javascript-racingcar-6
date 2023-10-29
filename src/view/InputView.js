b43cbae87be15e0b40236ed6f5e94796ce367709
yu-retel <dbdudals100@naver.com>
2023-10-30 05:51:52 +0900

    style: inputView(메서드명 명시적으로 변경), input.js삭제

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -6,7 +6,7 @@ const InputView = {
     return name;
   },
 
-  async getAttemptsCount () {
+  async getAttemptsCount() {
     const count = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
     return count;
   }
