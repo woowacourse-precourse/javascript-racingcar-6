6ec2e016cafeb53a58bd89c832aa2372905f28b5
김예빈 <kimyebin@gim-yebin-ui-MacBookPro.local>
2023-10-30 04:59:45 +0900

    feat: 무작위 값 구하기, 조건별 전진하기 이동 횟수만큼 반복하기 구현

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 import User from "./User.js";
-
+import Judge from "./Judge.js";
 
 class App {
   async play() {
     const user = new User();
+    const judge = new Judge();
 
+    let players;
     try {
       players = await user.inputPlayersName();
     } catch (error) {
       console.error(error.message);
       throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
     }  
+
+    const gameMoveNum = await user.inputMoveNum();
+
+    const gameResult = players.map((name) => ({
+      name,
+      moveNum: 0
+    }));
+
+    let gameStep = 0;
+    while (gameStep < gameMoveNum) {
+      gameResult.forEach((player) => {
+        const randomNum = Random.pickNumberInRange(0, 9);
+        if (judge.checkMoveCondition(randomNum)) {
+          player.moveNum++;
+        }
+      })
+      gameStep++;
+    }
   }
 }
 
