51b0fee0cce97ae143640f8e2be702c4953ee4f7
chiman <rlaclghks123@naver.com>
2023-10-30 00:40:22 +0900

    refactor: 자동차 욕설 유효성 검사를 Car 클래스로 이동
    자동차 클래스에서 name을 사용하기 때문에 유효성 검사를 Car에서 하는게 맞다고 판단하여 수정

--- src/Car.js
+++ src/Car.js
@@ -1,10 +1,22 @@
+import { KOREAN_BAD_WORD } from './db/badWordDB.js';
+
 class Car {
   #name;
   #position;
 
   constructor(name) {
-    this.#name = name;
+    this.#name = this.#checkBadWord(name);
     this.#position = 0;
+  }
+
+  #checkBadWord(carName) {
+    const isBadWord = KOREAN_BAD_WORD.some((badWord) => carName.includes(badWord));
+
+    if (isBadWord) {
+      throw new Error(`[ERROR] 자동차 이름에 ${carName}을 사용할 수 없습니다.`);
+    }
+
+    return carName;
   }
 
   move(distance) {
--- src/Refree.js
+++ src/Refree.js
@@ -1,25 +1,11 @@
 import { Random } from '@woowacourse/mission-utils';
-import { KOREAN_BAD_WORD } from './db/badWordDB.js';
 import Car from './Car.js';
 
 class Refree {
   #carRegistry = new Map();
 
   registerCars(carNames) {
-    this.#checkBadWord(carNames);
     carNames.forEach((car) => this.#carRegistry.set(car, new Car(car)));
-  }
-
-  #checkBadWord(carNames) {
-    const result = [];
-
-    carNames.forEach((carName) => {
-      if (KOREAN_BAD_WORD.some((badWord) => carName.includes(badWord))) result.push(carName);
-    });
-
-    if (result.length) {
-      throw new Error(`[ERROR] 자동차 이름에 ${result.join(',')}을 사용할 수 없습니다.`);
-    }
   }
 
   moveCars() {
