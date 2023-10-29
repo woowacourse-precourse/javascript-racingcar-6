299251e5be770b255182d7f16d021c32e6f94e84
thumbthing <ilscc0902@gmail.com>
2023-10-30 02:16:20 +0900

    feat: rootService에 gameStart, gameInProgress 적용, rootService를 App에 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import rootService from './service/rootService.js';
+import { appErrorHandler } from './util/error/errorhandler.js';
+
 class App {
-  async play() {}
+  async play() {
+    try {
+      const game = await rootService();
+      return game;
+    } catch (error) {
+      const ERROR = appErrorHandler(error);
+      return ERROR;
+    }
+  }
 }
 
 export default App;
--- src/service/rootService.js
+++ src/service/rootService.js
@@ -1,10 +1,14 @@
 import { defaultErrorHandler } from '../util/error/errorhandler.js';
+import { consolePrint } from '../util/libraryFeatures/MissionUtilsHandler.js';
+import gameInProgress from './gameInProgress.js';
 import gameStart from './gameStart.js';
 
 export default async function rootService() {
   try {
     const gameData = await gameStart();
-    return gameData;
+    const gameResult = await gameInProgress(gameData);
+    consolePrint(gameResult);
+    return gameResult;
   } catch (error) {
     const promise = defaultErrorHandler(error);
     return promise;
