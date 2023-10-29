a02d8ebddbc9bca884a6490a8cf81ff5e930a2bf
ryan-dia <cjfdnjs158@naver.com>
2023-10-29 13:25:12 +0900

    feat(controller): 최종 우승자 가져오기

--- src/controller/index.js
+++ src/controller/index.js
@@ -18,6 +18,7 @@ class RacingCarController {
       this.#model.racing();
       OutputView.printProgress(this.#model.getCarData());
     }
+    const finalWinner = this.#model.getFinalWinner();
   }
 }
 
