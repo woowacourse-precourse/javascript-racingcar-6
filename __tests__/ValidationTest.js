3801d3a1b6c375a6212b76201672967ad879ef01
chorong <chorong4641@naver.com>
2023-10-29 17:23:07 +0900

    test(validation): 경주할 횟수 테스트 코드 작성

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,4 +1,4 @@
-import { checkCarName } from '../src/Validation.js';
+import { checkCarName, checkRaceCount } from '../src/Validation.js';
 
 describe("자동차 이름 테스트", () => {
     test("구분자가 포함되지 않은 경우 에러 반환", () => {
@@ -11,5 +11,13 @@ describe("자동차 이름 테스트", () => {
         const input = "test,errortest";
 
         expect(() => checkCarName(input)).toThrow("[ERROR]");
+    });
+});
+
+describe("경주할 횟수 테스트", () => {
+    test("숫자가 아닐 경우 에러 반환", () => {
+        const input = "test";
+
+        expect(() => checkRaceCount(input)).toThrow("[ERROR]");
     });
 });
