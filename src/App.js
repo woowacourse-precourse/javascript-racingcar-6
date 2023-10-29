5e7b5d22fde223d50ef6966cce75549d36971683
Y.W KIM <kyw624@gmail.com>
2023-10-29 16:45:33 +0900

    Refactor: App 클래스의 play() 메서드 기능 분리

--- src/App.js
+++ src/App.js
@@ -2,41 +2,31 @@ import { Console, Random } from '@woowacourse/mission-utils';
 
 class App {
   async play() {
-    const carNames = this.validateInput(
-      await Console.readLineAsync(
-        '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
-      )
-    );
-
-    const totalRounds = this.validateInput(
-      await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n')
-    );
-
+    const carNames = await this.getUserInput('name');
+    const totalRounds = await this.getUserInput('round');
     const cars = carNames.map((carName) => new Car(carName));
 
-    Console.print('\n실행 결과');
-
-    for (let i = 0; i < totalRounds; i++) {
-      cars.forEach((car) => {
-        car.moveOrStop();
-        Console.print(`${car.name} : ${car.progress}`);
-      });
-
-      Console.print('');
-    }
-
-    const winners = this.getWinners(cars);
-
-    Console.print(`최종 우승자 : ${winners.join(', ')}`);
+    this.startRace(cars, totalRounds);
+    this.printWinners(cars);
   }
 
-  validateInput(input) {
-    if (input === '') {
-      throw new Error('[ERROR] 입력값이 없습니다.');
+  async getUserInput(flag) {
+    const message =
+      flag === 'name'
+        ? '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+        : '시도할 횟수는 몇 회인가요?\n';
+    const input = await Console.readLineAsync(message);
+
+    if (flag === 'name') {
+      return this.validateCarNames(input);
     }
 
-    if (!isNaN(Number(input))) {
-      return Number(input);
+    return this.validateTotalRounds(input);
+  }
+
+  validateCarNames(input) {
+    if (input === '') {
+      throw new Error('[ERROR] 입력값이 없습니다.');
     }
 
     if (input.indexOf(' ') !== -1) {
@@ -60,6 +50,35 @@ class App {
     }
 
     return cars;
+  }
+
+  validateTotalRounds(input) {
+    const result = Number(input);
+
+    if (isNaN(result)) {
+      throw new Error('[ERROR] 숫자를 입력해주세요.');
+    }
+
+    return result;
+  }
+
+  startRace(cars, totalRounds) {
+    Console.print('\n실행 결과');
+
+    for (let i = 0; i < totalRounds; i++) {
+      cars.forEach((car) => {
+        car.moveOrStop();
+        Console.print(`${car.name} : ${car.progress}`);
+      });
+
+      Console.print('');
+    }
+  }
+
+  printWinners(cars) {
+    const winners = this.getWinners(cars);
+
+    Console.print(`최종 우승자 : ${winners.join(', ')}`);
   }
 
   getWinners(cars) {
