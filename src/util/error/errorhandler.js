a8e8866b239a9d03fd51e0212d2d0a4bbac8c94a
thumbthing <ilscc0902@gmail.com>
2023-10-29 14:26:58 +0900

    feat: new Error 생성 삭제, return 값에 message로 직접 선언

--- src/util/error/errorhandler.js
+++ src/util/error/errorhandler.js
@@ -1,4 +1,4 @@
-import { MissionUtils } from '@woowacourse/mission-utils';
+import { consolePrint } from '../libraryFeatures/consoleHandler.js';
 import errorList from './errorCode.js';
 
 export function gameErrorhandler(code) {
@@ -6,12 +6,11 @@ export function gameErrorhandler(code) {
 
   const errorMessage = occuredError !== undefined ? occuredError : '예상치 못한 에러가 발생했습니다.';
 
-  const result = new Error(errorMessage);
-
-  MissionUtils.Console.print(`[ERROR] ${errorMessage}`);
-  return Promise.reject(result);
+  consolePrint(`[ERROR] ${errorMessage}`);
+  return Promise.reject(errorMessage);
 }
 
 export function defaultErrorHandler(error) {
+  consolePrint(`[ERROR] ${error}`);
   return Promise.reject(error);
 }
