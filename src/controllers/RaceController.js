68888938a70fdeb8a1f2424942e69809222e88ca
pjc0179 <pjc0179@gmail.com>
2023-10-29 14:59:09 +0900

    refactor(controllers)RaceController.js 자동차 생성부분과 유저입력조건 리펙토링 - CAR_NAME_LIST 변수 생성 - createCarList 메서드의 기능을 분리 더이상 유저가 입력한 값을 , 로 구분하여 나눠 리스트로 저장하던 부분을 CAR_NAME_LIST 선언문에서 해결하게 분리 후 오로직 전달받은 자동차 리스트 이름을 통해 Car 인스턴스를 생성하여 리스트에 담아 반환하는 기능만 유지하게끔 리펙토링 - checkCarNameUserInput 메서드가 유저가 값을 입력하고 인스턴스가 생성 된 후 동작하던 부분을 유저가 입력한 값을 먼저 검사 후 문제가 없을시 인스턴스를 생성하게끔 변경 - 위에 따라 유저의 입력값을 받고 인스턴스가 생성 된 후 검사를 진행하던 메서드들 checkSameCarName checkCarNameLength checkCarNameVoid 의 동작방식이 인스턴스가 아닌 문자열 자체로만 검사하게끔 변경

--- src/controllers/RaceController.js
+++ src/controllers/RaceController.js
@@ -12,68 +12,71 @@ class RaceController {
 
   async start() {
     const USER_INPUT_CAR_NAME = await RACE_CONSOLE_VIEW.getUserInputCarName();
-    const CAR_LIST = this.createCarList(USER_INPUT_CAR_NAME);
-    this.checkCarNameUserInput(CAR_LIST);
-
+    const CAR_NAME_LIST = USER_INPUT_CAR_NAME.split(',');
+    RaceController.checkCarNameUserInput(CAR_NAME_LIST);
+    const CAR_LIST = RaceController.createCarList(CAR_NAME_LIST);
     await this.setMaxRound();
+
     const raceResults = [];
     while (this.#maxRound !== this.#currentRound) {
       const roundResults = [];
+
       CAR_LIST.forEach((car) => {
         createRandomNumber() >= CONFIG.MINIMUM_CAN_MOVE_FOWARD && car.move();
         // prettier-ignore
         roundResults.push(`${car.name} : ${MESSAGES.ONE_STEP.repeat(car.getMoveCount())}`);
       });
+
       raceResults.push(roundResults);
       this.#currentRound += 1;
     }
+
     RACE_CONSOLE_VIEW.raceResult(raceResults);
-    RACE_CONSOLE_VIEW.winner(this.findWinner(CAR_LIST));
+    RACE_CONSOLE_VIEW.winner(RaceController.findWinner(CAR_LIST));
   }
 
-  findWinner(carList) {
-    const MAX_MOVE_COUNT = Math.max(
-      ...carList.map((car) => car.getMoveCount())
-    );
-    return carList
-      .filter((car) => car.getMoveCount() === MAX_MOVE_COUNT)
-      .map((car) => car.name);
-  }
-
-  createCarList(userInputCarName) {
-    return userInputCarName.split(',').map((carName) => new Car(carName));
+  static createCarList(carNameList) {
+    return carNameList.map((carName) => new Car(carName));
   }
 
   async setMaxRound() {
     this.#maxRound = +(await RACE_CONSOLE_VIEW.getUserInputMaxRound());
   }
 
-  checkSameCarName(carList) {
+  static findWinner(carList) {
+    // prettier-ignore
+    const MAX_MOVE_COUNT = Math.max(...carList.map((car) => car.getMoveCount()));
+    return carList
+      .filter((car) => car.getMoveCount() === MAX_MOVE_COUNT)
+      .map((car) => car.name);
+  }
+
+  static checkSameCarName(carNameList) {
     return (
-      Array.from(new Set(carList.map((car) => car.name))).length !==
-      carList.length
+      Array.from(new Set(carNameList.map((car) => car))).length !==
+      carNameList.length
     );
   }
 
-  checkCarNameLength(carList) {
+  static checkCarNameLength(carNameList) {
     return (
-      carList.filter((car) => car.name.length > CONFIG.MAX_CAR_NAME_LENGTH)
+      carNameList.filter((car) => car.length > CONFIG.MAX_CAR_NAME_LENGTH)
         .length > 0
     );
   }
 
-  checkCarNameVoid(carList) {
-    return carList.filter((car) => car.name === '').length > 0;
+  static checkCarNameVoid(carNameList) {
+    return carNameList.filter((car) => car === '').length > 0;
   }
 
-  checkCarNameUserInput(carList) {
-    if (this.checkSameCarName(carList)) {
+  static checkCarNameUserInput(carList) {
+    if (RaceController.checkSameCarName(carList)) {
       throwError('중복된 자동차 이름이 있습니다');
     }
-    if (this.checkCarNameLength(carList)) {
+    if (RaceController.checkCarNameLength(carList)) {
       throwError(`자동차의 이름이 ${CONFIG.MAX_CAR_NAME_LENGTH} 보다 깁니다`);
     }
-    if (this.checkCarNameVoid(carList)) {
+    if (RaceController.checkCarNameVoid(carList)) {
       throwError(`이름이 존재하지 않는 차가 있습니다`);
     }
   }
