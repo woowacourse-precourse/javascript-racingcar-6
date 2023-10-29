812953e4f8b0974d059e157a30545af57575f044
Goonco <s07019@naver.com>
2023-10-30 01:10:11 +0900

    :sparkles: 하나의 자동차를 관리하는 class Car 생성
    * name - 자동차의 이름 field
    * position - 자동차의 위치 field
    * constructor - 자동차의 이름을 받아 저장하고, position은 0으로 초기화

---
+++ src/Car.js
@@ -0,0 +1,17 @@
+class Car {
+  #position;
+
+  #name;
+
+  constructor(name) {
+    this.#position = 0;
+    this.#name = name;
+  }
+
+  // 구현 전 기능
+  // tryMove() {}
+
+  // printPosition() {}
+}
+
+module.exports = Car;
