443b0b46a01190691013fe3ec6211ea61a96be8d
thumbthing <ilscc0902@gmail.com>
2023-10-29 15:40:01 +0900

    feat: game 시작시 동작할 기능 (input 담당 함수 호출 후 gameData 반환)

---
+++ src/service/gameStart.js
@@ -0,0 +1,24 @@
+import { defaultErrorHandler } from '../util/error/errorhandler.js';
+import receiveCarName from '../util/ready/receiveCarName.js';
+import receiveGameCount from '../util/ready/receiveGameCount.js';
+import parseArrayToMap from '../util/ready/parseArrayToMap.js';
+
+export default async function gameStart() {
+  try {
+    const carList = await receiveCarName();
+    const gameCount = await receiveGameCount();
+    const carData = parseArrayToMap(carList);
+
+    const gameData = {
+      carData,
+      gameCount,
+    };
+
+    return gameData;
+  } catch (error) {
+    const promise = defaultErrorHandler(error);
+    return promise;
+  }
+}
+
+gameStart();
