bb4fa077365533ac97887eb56eb55a2c1ee9b073
chorong <chorong4641@naver.com>
2023-10-29 22:12:58 +0900

    feat(race): 주어진 횟수 동안 n대의 자동차 전진 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 import { readCarName, readRaceCount } from './RaceInput.js';
 
+const START_RANGE = 0;
+const END_RANGE = 9;
+
 class App {
+  constructor() {
+    this.forwardCarData = {};
+  }
+
   async play() {
-    await readCarName();
-    await readRaceCount();
+    const carName = await readCarName();
+    const carNames = carName.split(',');
+    const raceCount = await readRaceCount();
+
+    this.startRace(carNames, raceCount);
+  }
+
+  startRace(carNames, raceCount) {
+    this.forwardCarData = carNames.reduce((acc, name) => {
+      return { ...acc, [name]: 0 };
+    }, {});
+
+    for (let index = 0; index < raceCount; index += 1) {
+      this.randomForward(carNames);
+    }
+   return this.forwardCarData;
+  }
+
+  randomForward(carNames) {
+    carNames.forEach((name) => {
+      const randomNumber = MissionUtils.Random.pickNumberInRange(START_RANGE, END_RANGE);
+      if (randomNumber >= 4) {
+        this.forwardCarData[name] += 1;
+      }
+    });
   }
 };
 
