e551b28b7ffb4ea0c1a38d1b9d7d22737b37951e
Hayeon-Lee <papepopepe@naver.com>
2023-10-29 21:40:46 +0900

    Feat: 작성한 컴포넌트를 이용하여 App.js의 play() 함수를 작성합니다.
    과제에서 제시한대로 자동차 경주를 진행합니다.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import { getUserInput } from './components/getUserInput.js';
+import { startRacing } from './components/startRacing.js';
 
 class App {
   async play() {
     const userInput = await getUserInput();
-    console.log(userInput.carNameDict);
-    console.log(userInput.trialCount);
+    await startRacing(userInput.carNameDict, userInput.trialCount);
   }
 }
-
-const app = new App();
-app.play();
 
 export default App;
