4ef6033d7b7d2abfc9a667b52e9b15073590f32e
leehanjun00 <yeehj20@naver.com>
2023-10-30 08:07:35 +0900

    refactor(play): 공백 추가 및 불필요한 함수선언 삭제

--- src/play.js
+++ src/play.js
@@ -2,10 +2,10 @@ import { Console, Random } from '@woowacourse/mission-utils';
 import Result from './Result.js';
 
 class Play {
-  constructor() {}
   racing(carList) {
     carList.forEach((car) => {
       const number = Random.pickNumberInRange(0, 9);
+
       if (number >= 4) {
         car.go();
       }
@@ -19,12 +19,15 @@ class Play {
   raceStart(numberOfTimes, carList) {
     const inputNumber = !Number.isNaN(numberOfTimes);
     const inputString = isNaN(numberOfTimes);
+
     Console.print('\n실행결과');
+
     if (inputNumber) {
       for (let i = 1; i <= numberOfTimes; i++) {
         this.racing(carList);
       }
     }
+
     if (inputString) {
       throw new Error('[ERROR] 숫자를 입력하세요.');
     }
