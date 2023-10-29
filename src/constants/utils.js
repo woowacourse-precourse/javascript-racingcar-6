9908621e37cfb9cc12a4c6889dd93a50e82b9f31
at8in <at8in@naver.com>
2023-10-30 05:22:51 +0900

    refactor(utils): 객체로 변경, 잘못된 값 에러 발생 class로 관리할 특징이 없어 객체로 변경, 잘못된 값이 입력될 경우 커스텀 에러 메시지 발생하게 수정

--- src/constants/utils.js
+++ src/constants/utils.js
@@ -6,3 +6,11 @@ export const RANDOM_NUMBER = Object.freeze({
   from: 0,
   to: 9,
 });
+
+export const ERROR_MESSAGE = Object.freeze({
+  print(message) {
+    return `[ERROR] ${message}`;
+  },
+  invalidName: '이름은 5자 이하의 영문, 한글로 이루어져야 합니다.',
+  invalidCount: '시도 횟수는 0이상의 수만 입력할 수 있습니다.',
+});
