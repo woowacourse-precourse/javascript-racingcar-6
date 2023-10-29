ea168d37da8cdefd71a324f135fb81d7781a0c35
Nahyun <from.owol@gmail.com>
2023-10-29 19:12:23 +0900

    test: 우승자 출력 테스트 케이스 추가

--- __tests__/WinnerTest.js
+++ __tests__/WinnerTest.js
@@ -4,13 +4,19 @@ describe('Winner', () => {
   test('단독 또는 공동 우승자 출력', () => {
     const gameResult = new GameResult();
 
-    const carLog = new Map();
-    carLog.set('a', 2);
-    carLog.set('b', 1);
-    carLog.set('c', 2);
+    const soloCase = new Map();
+    soloCase.set('a', 2);
+    soloCase.set('b', 1);
 
-    const winners = gameResult.getWinner(carLog);
+    const shareCase = new Map();
+    shareCase.set('a', 2);
+    shareCase.set('b', 1);
+    shareCase.set('c', 2);
 
+    const winner = gameResult.getWinner(soloCase);
+    const winners = gameResult.getWinner(shareCase);
+
+    expect(winner).toEqual('a');
     expect(winners).toEqual('a, c');
   });
 });
