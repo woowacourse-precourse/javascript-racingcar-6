818ba3d4ab5a8344a677705923508a9ad3a88195
Seoan Yoon <earthdyoon@gmail.com>
2023-10-30 02:13:19 +0900

    feat: Screen 클래스 선언 및 자동차 이름 입력 구현

---
+++ src/Screen.js
@@ -0,0 +1,20 @@
+import { Console } from '@woowacourse/mission-utils';
+
+class Screen {
+  static MESSAGES = {
+    RACE_NAME_INPUT_MESSAGE:
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표 (,) 기준으로 구분)',
+    INVALID_NAME_ERROR_MESSAGE:
+      '[ERROR] 자동차 이름은 5자를 초과할 수 없습니다.',
+  };
+
+  static async inputNames() {
+    Console.print(this.MESSAGES.RACE_NAME_INPUT_MESSAGE);
+    const nameInput = await Console.readLineAsync();
+    const names = nameInput.split(',').map((name) => name.trim());
+
+    return names;
+  }
+}
+
+export default Screen;
