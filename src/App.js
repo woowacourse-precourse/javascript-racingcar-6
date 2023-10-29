1f0aec447fcf4d22b5c422206f9a663e1ec0fe06
김다인 <dbslzhs1218@naver.com>
2023-10-30 05:10:07 +0900

    feat(racingcar): 사용자가 자동차에 이름 부여 기능 구현
    사용자가 입력한 이름이 잘못된 값일 경우 예외 발생 후 에러처리,
    맞게 입력했을 경우 입력한 이름 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import { Console } from '@woowacourse/mission-utils';
 import strings from './constants.js';
+import CreateCarName from './CreateCarName.js';
 
 class App {
   async play() {
     const inputName = await Console.readLineAsync(strings.ASK_NAME);
-    Console.print(inputName);
-
-    return this.null;
+    const namingCar = new CreateCarName();
+    namingCar.carName(inputName);
   }
 }
-
 export default App;
--- src/CreateCarName.js
+++ src/CreateCarName.js
@@ -1,10 +1,33 @@
+import { Console } from '@woowacourse/mission-utils';
 class CreateCarName {
-  carName() {
-    return this.null;
+  constructor() {
+    this.carNameArr = [];
   }
 
-  wrongName() {
-    return this.null;
+  carName(inputName) {
+    const inputNameArr = inputName.split(',');
+
+    inputNameArr.forEach((inputNameElement) => {
+      this.wrongName(inputNameElement);
+    });
+
+    Console.print(this.carNameArr.join(','));
+  }
+
+  wrongName(inputNameElement) {
+    if (inputNameElement === '') {
+      throw new Error('[ERROR] 값이 입력되지 않았습니다.');
+    }
+
+    if (inputNameElement.length >= 5) {
+      throw new Error('[ERROR] 이름은 5자 이하로 입력해주세요.');
+    }
+
+    if (/[^a-z]/i.test(inputNameElement)) {
+      throw new Error('[ERROR] 이름을 제대로 입력해주세요.');
+    }
+
+    return this.carNameArr.push(inputNameElement);
   }
 }
 export default CreateCarName;
