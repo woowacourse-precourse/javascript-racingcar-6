82767e812063399c156db100d7b3f1945c8870f0
ReturnReason <reasonz@daum.net>
2023-10-29 18:06:07 +0900

    test(GameViewTest): printWinner 메서드 테스트 코드 추가

--- __tests__/GameViewTest.js
+++ __tests__/GameViewTest.js
@@ -22,9 +22,48 @@ describe('✨ [GameView] 클래스 메서드 테스트', () => {
   test('[printResult] name과 count키 객체를 인자로 받아 count만큼 - 개수를 출력한다.', () => {
     const name = 'reason';
     const count = 4;
-    const expectedString = `${name} : ${'-'.repeat(count)}`;
+    const expectedResult = `${name} : ${'-'.repeat(count)}`;
 
     view.printResult({ name, count });
-    expect(logSpy).toHaveBeenCalledWith(expectedString);
+    expect(logSpy).toHaveBeenCalledWith(expectedResult);
+  });
+
+  test('[printWinner] 인자로 받은 winners 배열이 비어있는 경우 없음을 출력한다.', () => {
+    const expectedResult = `${MESSAGE.WINNER}${MESSAGE.NONE}`;
+
+    view.printWinner([]);
+    expect(logSpy).toHaveBeenCalledWith(expectedResult);
+  });
+
+  test('[printWinner] 인자로 받은 winners 배열에 있는 요소의 name을 우승자로 출력한다.', () => {
+    const winnerList = [
+      {
+        name: 'reason',
+        count: 4,
+      },
+    ];
+    const winnerNames = winnerList.map((winner) => winner.name).join(', ');
+    const expectedResult = `${MESSAGE.WINNER}${winnerNames}`;
+
+    view.printWinner(winnerList);
+    expect(logSpy).toHaveBeenCalledWith(expectedResult);
+  });
+
+  test('[printWinner] 인자로 받은 winners가 여러 명인 경우 우승자를 ,로 구분하여 출력한다.', () => {
+    const winnerList = [
+      {
+        name: 'reason',
+        count: 4,
+      },
+      {
+        name: 'leeyu',
+        count: 4,
+      },
+    ];
+    const winnerNames = winnerList.map((winner) => winner.name).join(', ');
+    const expectedResult = `${MESSAGE.WINNER}${winnerNames}`;
+
+    view.printWinner(winnerList);
+    expect(logSpy).toHaveBeenCalledWith(expectedResult);
   });
 });
