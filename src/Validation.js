c46003d0afbdc18d01742273538a67bb88516f07
Jeongeun Lee <leeju1009@gmail.com>
2023-10-29 19:04:38 +0900

    refactor: 경주 횟수의 유효성을 검사하는 함수의 가독성을 개선
    경주 시도 횟수의 유효성을 검사하는 isValidRaceCount 가독성 개선

--- src/Validation.js
+++ src/Validation.js
@@ -8,13 +8,16 @@ class Validation {
   }
 
   isValidRaceCount(raceCount) {
-    const raceCountNumber = Number(raceCount);
-    if (!Number.isInteger(raceCountNumber) || raceCountNumber <= 0) {
-      throw new Error(
-        '[ERROR] 숫자가 잘못된 형식입니다. 1 이상의 정수를 입력해주세요.'
-      );
+    if (!Number.isInteger(raceCount)) {
+      return false;
+    }
+
+    if (raceCount < 1) {
+      return false;
     }
 
     return true;
   }
 }
+
+export default Validation;
