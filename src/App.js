ca2748d51dcdcc79e6d838f4a798d796c4a07815
f0ever0 <thddndud0830@gmail.com>
2023-10-29 15:12:12 +0900

    feat : 자동차 이름 및 시도 횟수 입력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+const InputError = require("./lib/utils/error.js");
+const { GAME_MESSAGE } = require("./lib/constants/message.js");
+
 class App {
-  async play() {}
+  constructor() {
+    this.Error = new InputError();
+  }
+
+  async play() {
+    this.playGame();
+  }
+
+  async playGame() {
+    const carNameInput = await Console.readLineAsync(
+      GAME_MESSAGE.CARNAME_INPUT
+    );
+    const carNameInputList = [...carNameInput.split(",")];
+    this.Error.validateCarNameInput(carNameInputList);
+
+    const tryCount = await Console.readLineAsync(GAME_MESSAGE.COUNT_INPUT);
+    this.Error.validateCountInput(tryCount);
+  }
 }
 
 export default App;
