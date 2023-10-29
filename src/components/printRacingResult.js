4b668733c75ee08291175ebed3dcdbf78fedec09
Hayeon-Lee <papepopepe@naver.com>
2023-10-29 20:54:34 +0900

    Feat: 자동차 경주의 결과를 출력합니다.

---
+++ src/components/printRacingResult.js
@@ -0,0 +1,13 @@
+import { Console } from "@woowacourse/mission-utils";
+
+const printRacingResult = (carNameDict) => {
+  for (let carName in carNameDict) {
+    let resultStr = carName + " : ";
+    for (let i = 0; i < carNameDict[carName]; i++) {
+      resultStr += "-";
+    }
+    Console.print(resultStr);
+  }
+};
+
+export { printRacingResult };
