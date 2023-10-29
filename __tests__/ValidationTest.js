33984fac2589bcf4e4fe6409bfe0d31169b844b3
8leamnk <silkviewrk@gmail.com>
2023-10-29 21:03:24 +0900

    test: 자동차 이름 입력 메시지 점검 기능에 대한 테스트 코드를 수정함
    수정 내용:
    - test.each로 각각 테스트 한 것을 test로 수정하여 그 안에서 forEach로 테스트가 가능하도록 수정함

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -3,19 +3,21 @@ import Validation from '../src/modules/Validation.js';
 describe('자동차 이름 입력', () => {
   const LIMITED_LENGTH = 5;
 
-  test.each([['람보르기니,롤스로이스,스피라'], ['Fiat,BMW,Jeep,Ford,Volvo']])(
-    '이름이 담긴 Map을 성공적으로 반환',
-    (input) => {
-      // given
-      const result = new Map(input.split(',').map((car) => [car, 0]));
+  test('자동차 이름 입력값을 Map으로 반환', () => {
+    // given
+    const inputs = ['람보르기니,롤스로이스,스피라', 'Fiat,BMW,Jeep,Ford,Volvo'];
+    const outputs = inputs.map(
+      (input) => new Map(input.split(',').map((car) => [car, 0])),
+    );
 
-      // when
-      const { getNamesOfCar } = Validation;
+    // when
+    const { getNamesOfCar } = Validation;
 
-      // then
-      expect(getNamesOfCar(input, LIMITED_LENGTH)).toEqual(result);
-    },
-  );
+    // then
+    outputs.forEach((output, idx) => {
+      expect(getNamesOfCar(inputs[idx], LIMITED_LENGTH)).toEqual(output);
+    });
+  });
 
   test.each([['KIA,Honda,BMW!'], ['Honda,Jaguar'], ['Honda,Honda']])(
     '이름에 대한 예외 처리',
