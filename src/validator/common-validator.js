c905bfed4ef88b69517ee59dfefe4ddbd7bf3d98
BangDori <qudwns8865@naver.com>
2023-10-29 17:24:55 +0900

    refactor: 띄어쓰기를 확인하기 위한 정규표현식 외부로 분리

--- src/validator/common-validator.js
+++ src/validator/common-validator.js
@@ -1,12 +1,13 @@
 import { SPACE_ERROR_MESSAGE } from "../constants/error-message.js";
 
+const spacePattern = /\s/g;
+
 /**
  * 입력받은 문자열에 공백이 존재하는지 검증하기 위한 함수
  * @param {string} inputString 문자열
  * @throw 문자열에 공백이 존재한다면 에러를 던진다.
  */
 function checkSpace(inputString) {
-  const spacePattern = /\s/g;
   if (inputString.match(spacePattern)) {
     throw new Error(SPACE_ERROR_MESSAGE);
   }
