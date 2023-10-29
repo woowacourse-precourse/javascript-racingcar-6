c1d9b82f6c929e218a37d6b0b2842b614a144a4e
kelvin3476 <winrain03226@gmail.com>
2023-10-30 03:03:03 +0900

    feat(App): 기능4 각 차수별 실행 결과 출력 메서드 구현 완료

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -1,3 +1,4 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
 import Car from './Car';
 
 class RacingGame {
@@ -7,6 +8,15 @@ class RacingGame {
   }
 
   // TODO: Mission 4: 각 차수별 실행 결과 출력 메서드입니다.
+  playGame() {
+    for (let attempt = 0; attempt < this.totalAttempts; attempt += 1) {
+      MissionUtils.Console.print(`시도 ${attempt + 1}:`);
+      this.cars.forEach(car => {
+        car.move();
+        MissionUtils.Console.print(`${car.name} : ${'-'.repeat(car.position)}`);
+      });
+    }
+  }
 
   // TODO: Mission 6: 우승자 결정 메서드입니다.
 }
