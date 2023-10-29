047b6854366a9299424e73455bce4b2df33c6101
thumbthing <ilscc0902@gmail.com>
2023-10-29 17:08:12 +0900

    test: error test 로직 수정

--- __tests__/ErrorTest.js
+++ __tests__/ErrorTest.js
@@ -1,4 +1,4 @@
-import { gameErrorhandler } from '../src/util/error/errorhandler';
+import { occuredErrorhandler } from '../src/util/error/errorhandler';
 
 describe('에러 처리 test', () => {
   test('존재하는 에러코드에 대한 메세지 출력', () => {
@@ -7,7 +7,7 @@ describe('에러 처리 test', () => {
     const output = '이름이 없는 자동차가 존재합니다.';
 
     // when
-    expect(gameErrorhandler(code)).rejects.toThrow(output);
+    expect(() => occuredErrorhandler(code).rejects.toThrow(output));
   });
 
   test('존재하지 않는 에러코드에 대한 메세지 출력', () => {
@@ -16,6 +16,6 @@ describe('에러 처리 test', () => {
     const output = '예상치 못한 에러가 발생했습니다.';
 
     // when
-    expect(gameErrorhandler(code)).rejects.toThrow(output);
+    expect(() => occuredErrorhandler(code).rejects.toThrow(output));
   });
 });
