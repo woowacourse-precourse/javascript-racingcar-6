5dc18548c80c71c086063bb8263396ca658fb19b
1eecan <busyx2modernsociety@gmail.com>
2023-10-30 00:04:22 +0900

    feat: racingCar.js 작성
    * 참가하는 자동차 문자열을 자동차의 이름과 현재 달리는 상태를 나타내는 객체들로 구성된 배열로 만드는 함수 추가

---
+++ src/input/racingCar/racingCar.js
@@ -0,0 +1,13 @@
+import { Console } from "@woowacourse/mission-utils";
+
+const racingCar = (input) => {
+  const input_string = input;
+  const input_array = input_string.split(",");
+  const input_object_array = input_array.map((item) => {
+    return {
+      car: item,
+      currentState: 0,
+    };
+  });
+  return input_object_array;
+};
