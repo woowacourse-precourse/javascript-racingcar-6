5e60c59b6e7ca3767ae3fa53ef8a97c2553020aa
anttiey <hanuuny@gmail.com>
2023-10-29 21:37:49 +0900

    fix: 자동차 이름이 배열에 할당되지 않는 오류 수정
    - getRacingCarNameInput 내의 map 함수가 return 값을 반환해 자동차 이름이 배열에 할당되도록 수정

--- src/App.js
+++ src/App.js
@@ -10,13 +10,14 @@ class App {
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
     );
 
-    const cars = input.split(',').map((el) => {
+    const carNames = input.split(',').map((el) => {
       if (!this.checkRacingCarNameValidation(el)) {
         throw new Error('[ERROR] 입력이 5자 이하가 아닙니다.');
       }
+      return el;
     });
 
-    return cars;
+    return carNames;
   };
 
   checkRacingCountValidation = (el) => {
@@ -35,7 +36,7 @@ class App {
 
   async play() {
     try {
-      await this.getRacingCarNameInput();
+      const carNames = await this.getRacingCarNameInput();
       await this.getRacingCountInput();
     } catch (err) {
       Console.print(err.message);
