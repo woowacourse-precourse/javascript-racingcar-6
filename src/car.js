9e9528c3ce6cb42669576f868c1750e5221f1891
zelkovaria <hyein396@gmail.com>
2023-10-29 16:51:52 +0900

    fix: carGenerate()의 변수명 수정

--- src/car.js
+++ src/car.js
@@ -4,13 +4,13 @@ export async function carGenerate() {
   const userInputCars = await MissionUtils.Console.readLineAsync("");
   const carsList = userInputCars.split(",");
 
-  const carNameIsValid = carsList.every(
+  const carLengthIsValid = carsList.every(
     (nameLength) => nameLength.length <= 5 && nameLength.length > 0
   );
-  if (carNameIsValid) {
+  if (carLengthIsValid) {
     return carsList;
   }
-  if (!carNameIsValid) {
+  if (!carLengthIsValid) {
     throw new Error("[ERROR] 5글자 이내의 이름을 입력하세요.");
   }
 }
