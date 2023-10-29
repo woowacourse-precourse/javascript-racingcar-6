19fc41a66087926b91b30d5a3c377c62c70d2802
chaeryeon823 <chaechae823@naver.com>
2023-10-29 22:33:18 +0900

    Feat: 무작위 수 함수 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-class App {
-  async play() {}
-}
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { randomNumber } from "./randomNumber.js";
 
+class App {
+  async play() {
+    MissionUtils.Console.print(randomNumber());
+  }
+}
+const app = new App();
+app.play();
 export default App;
