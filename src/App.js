d3d72f6fdd36afc1ea2d0180f21db38d40440a9c
kelvin3476 <winrain03226@gmail.com>
2023-10-30 02:40:23 +0900

    feat(App): 구현해야하는 기능 단위별 주석 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 class App {
-  async play() {}
+  async play() {
+    // TODO: Mission 1: 자동차 생성 및 이름 부여 메서드입니다.
+    
+    // TODO: Mission 2: 이동 시도 횟수 입력 메서드입니다.
+    
+    // TODO: Mission 3: 자동차 이동 로직 메서드입니다.
+    
+    // TODO: Mission 4: 각 차수별 실행 결과 출력 메서드입니다.
+    
+    // TODO: Mission 5: 오류 처리 메서드입니다.
+    
+    // TODO: Mission 6: 우승자 결정 메서드입니다.
+  }
 }
 
 export default App;
