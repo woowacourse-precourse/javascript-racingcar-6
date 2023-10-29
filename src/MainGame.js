82aa5da58e12bf8de7dfc322334d7f6cc1c05b13
SU_VIN <pyotnqls25@naver.com>
2023-10-29 18:42:12 +0900

    Feat: 경주할 자동차 이름 입력[2]
    예외처리[1]- 이름이 다섯글자 이하인지

--- src/MainGame.js
+++ src/MainGame.js
@@ -2,20 +2,32 @@ import { Console } from '@woowacourse/mission-utils';
 import { Message } from './Message.js';
 
 class MainGame {
-  start = () => {
+  start = async () => {
     Console.print(Message.INPUT_CAR_NAME);
-    this.inputCarName();
+    await this.inputCarName();
   };
 
   static isSplitComma = (carNameList) => {
     if (carNameList.split(',').length == 1) {
       throw new Error(Message.ERROR.NOT_SPLIT_COMMA);
     }
+    return;
+  };
+
+  static isFiveLength = (carNameList) => {
+    let carName = carNameList.split(',');
+
+    for (let i = 0; i < carName.length; i++) {
+      if (carName[i].length < 1 || carName[i].length > 5) {
+        throw new Error(Message.ERROR.NOT_FIVE_FIGURES);
+      }
+    }
   };
 
   inputCarName = async () => {
     const carNameList = await Console.readLineAsync(''); // 사용자 입력을 받기 위해 await 추가
     MainGame.isSplitComma(carNameList);
+    MainGame.isFiveLength(carNameList);
   };
 }
 
