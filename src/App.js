7798db26db55a9a41d2b4d4911f5f10343c55c6a
Kidprogramer1039 <bnpo5231@naver.com>
2023-10-29 20:46:04 +0900

    feat : 자동차별 전진 횟수 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import { MissionUtils, Console } from "@woowacourse/mission-utils";
 class App {
   async play() {
     /** 레이싱 게임 함수 */
-    const game = function racingGame(nameList, runCount) {
-
+    const game = function racingGame(arr, count) {
+      // 배열을 오브젝트로 변경
+      let nameObj = {};
+      arr.forEach((element) => {
+        nameObj[element] = 0;
+      });
+      for (let i = 0; i < count; i++) {
+        for (let j in arr) {
+          let randomNum = MissionUtils.Random.pickNumberInRange(1, 9);
+          if (randonNum >= 4) {
+            nameObj[j] += 1;
+          }
+        }
+        
+      }
     }
     
     /** 무응답, 형식 오류 검증 함수 */
-    function validCheck(nameList,runCount) {
+    const error = function validCheck(nameList,runCount) {
       let nullCheck = nameList ?? false;
       nullCheck = runCount ?? false;
       for (let i = 0; i < nameArr.length; i++) {
@@ -20,10 +33,16 @@ class App {
           throw new Error(`[ERROR] 형식이 올바르지 않습니다.`);
       }
     }
+
     const nameList = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
     const runCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
     const nameArr = nameList.split(",");
     validCheck(nameArr, runCount);
+
+    // 배열을 오브젝트로 변경
+    
+
+    racingGame(nameObj, runCount);
     
   }
 }
