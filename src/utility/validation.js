7d61b78fbc15e5b96a85a7e678d2dec07b8741c7
ImxYJL <allensain14@gmail.com>
2023-10-29 20:25:31 +0900

    feat: 문자열 전체가 유효한 10진수 숫자인지 판단하는 함수 작성

--- src/utility/validation.js
+++ src/utility/validation.js
@@ -3,3 +3,11 @@ import { ERROR_MESSAGE } from '../constant/message';
 export const isWithinZeroToNine = (number) => number >= 0 && number <= 9;
 
 export const isDecimalChar = (character) => /^[0-9]$/.test(character);
+
+export const isValidateNumber = (string) => {
+  Array.from(string).forEach((digit) => {
+    if (!isDecimalChar(digit)) {
+      throw new Error(ERROR_MESSAGE.wrongGameCountInput);
+    }
+  });
+};
