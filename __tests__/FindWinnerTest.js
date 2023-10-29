330d2185a25f83f2e048baed05e0395012d256fb
coldok <2coldok@gmail.com>
2023-10-29 19:51:37 +0900

    test : 자동차 이름을 잘못 입력한 다양한 경우에 대한 에러 테스트 구현

--- __tests__/FindWinnerTest.js
+++ __tests__/FindWinnerTest.js
@@ -1,9 +1,9 @@
 import { findWinner } from "../src/controller/FindeWinner.js";
 
-test('점수 배열을 통해 우승자 추출하기 테승트', () => {
+test('점수 배열을 통해 우승자 추출하기 테스트', () => {
   const playersArray = ['카멘','카마인','카단','카제로스','루페온','프로키온'];
   const scoreArray = [4,7,1,3,7,7];
   const result = ['카마인','루페온','프로키온'];
+
   expect(findWinner(playersArray, scoreArray)).toEqual(result);
-  
 })
--- __tests__/GameCustomizeTest.js
+++ __tests__/GameCustomizeTest.js
@@ -1,35 +1,80 @@
 import { getPlayersName, getAttemptNumber } from "../src/controller/GameCustomize.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
 
+//값을 넣을때 [] 한겹 빼주기
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
 
   MissionUtils.Console.readLineAsync.mockImplementation(() => {
-    /*
-    const input = inputs.shift();
-    return Promise.resolve(input);*/
+    
     return Promise.resolve(inputs);
   });
 };
+//값을 넣을때 [] 필요
+const mockQuestions2 = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
 
-describe('유저가 입력한 정보를 비동기 적으로 잘 가져오는지 테스트', () => {
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    
+    const input = inputs.shift();
+    return Promise.resolve(input);
+    
+  });
+};
 
-
-  test('플레이어 이름 테스트', async () => {
+describe('비동기 유저 입력값 유효성 및 오류 잡아내는지 테스트', () => {
+  test('올바른 자동차 이름을 넣었을때', async () => {
     const userInput = "pobi,crong,woong";
     const result = ["pobi","crong","woong"];
+    const userWrongInput = "pobi,crong,,";
 
     mockQuestions(userInput);
     
-    await expect(getPlayersName()).resolves.toEqual(result);  
+    await expect(getPlayersName()).resolves.toEqual(result);
   })
 
-  test('게임 시도 횟수 테스트', async () => {
-    const userInput = "5";
-    const result = 5;
+  //이건 한단어만 테스트할 수 있음
+  test('플레이어 이름 오류났을때', async () => {
+    const userInput = "pobi,";
+    
+    mockQuestions(userInput);
+
+    await expect(getPlayersName()).rejects.toThrow('[ERROR]');
+  })
+
+  test('올바른 게임 시도 횟수 넣었을때', async () => {
+    const userInput = '1004';
+    const result = 1004;
 
     mockQuestions(userInput);
 
     await expect(getAttemptNumber()).resolves.toEqual(result);
   })
+
+  //mockQuestions2 사용
+  test.each([
+    [["pobi,"]],
+    [["pobi,crong,,"]],
+    [["pobi,cronnnng"]],
+    [["pobi,,crong"]]
+  ])('자동차 이름 규칙에 위반되는 것들', async (inputs) => {
+
+    mockQuestions2(inputs);
+
+    await expect(getPlayersName()).rejects.toThrow("[ERROR]");
+  })
+  //mockQuestions 사용
+  test.each([
+    ["pobi"],
+    ["chanwoong,pobi"],
+    ["pobi,"],
+    [",pobi"],
+    ["pobi,,crong"],
+    [",pobi,chanwoong,,crong,"]
+  ])('자동차 이름 규칙에 위반되는 것들', async (inputs) => {
+
+    mockQuestions(inputs);
+    await expect(getPlayersName()).rejects.toThrow("[ERROR]");
+  })
+
 })
