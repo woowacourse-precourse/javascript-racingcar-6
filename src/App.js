6ec127a3172f305cc18538372ea19dca99e05978
KYHH <kyhh93@naver.com>
2023-10-29 13:22:24 +0900

    feat : 0-9 사이의 무작위 값을 비교하여 전진 혹은 멈춤을 출력하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import { Console } from '@woowacourse/mission-utils';
-import CreateCarMoveCount from './RandomNumber.js';
+import MoveCar from './MoveCar.js';
 
 class App {
   async play() {
-    const number = new CreateCarMoveCount();
-    const numberResult = number.createRandomNumber();
-    Console.print(numberResult);
+    const moveCar = new MoveCar();
+    moveCar.carMoveCompare();
   }
 }
 
