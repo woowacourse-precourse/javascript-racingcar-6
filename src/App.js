1ff09967d5d3588726b8ad62e4f6bdbda260a83f
1eecan <busyx2modernsociety@gmail.com>
2023-10-30 03:20:52 +0900

    feat:  App class의 play 메소드 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { Console } from "@woowacourse/mission-utils";
+
+import gameStart from "./print/gameStart.js";
+import getRacingCar from "./input/racingCar/getRacingCar.js";
+import askTryNumber from "./print/askTryNumber.js";
+import getTryNumber from "./input/tryNumber/getTryNumber.js";
+import currentRacingStateArray from "./print/currentRacingStateArray.js";
+import sortArray from "./utils/sortArray.js";
+import findWinner from "./utils/findWinner.js";
+import announceWinner from "./print/announceWinner.js";
+
 class App {
-  async play() {}
+  async play() {
+    gameStart();
+
+    const car_name_string = await Console.readLineAsync("");
+    let car_object_array = getRacingCar(car_name_string);
+
+    askTryNumber();
+    let try_number = await getTryNumber();
+
+    Console.print("");
+    while (try_number-- > 0) {
+      car_object_array = currentRacingStateArray(car_object_array);
+      Console.print("");
+    }
+    announceWinner(findWinner(sortArray(car_object_array)));
+  }
 }
 
 export default App;
