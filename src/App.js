143c3d48450bea07e295f1544ed82df898d2aae8
yhyem <yoonhyemin0120@gmail.com>
2023-10-30 02:41:30 +0900

    feat: input car name

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  inputData = "";
+
+  async start() {
+    return Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+  }
+
+  async play() {
+    this.inputData = await this.start();
+  }
 }
 
 export default App;
