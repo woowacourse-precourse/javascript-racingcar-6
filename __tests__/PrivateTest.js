f250927c0b4abb2dbc8027186e664e5e5e49a8ee
김예빈 <kimyebin@gim-yebin-ui-MacBookPro.local>
2023-10-30 04:10:31 +0900

    test: 이동 횟수 입력 받기 함수 테스트

--- __tests__/PrivateTest.js
+++ __tests__/PrivateTest.js
@@ -14,8 +14,8 @@ const mockQuestions = (inputs) => {
 describe('사용자 입력 테스트', () => {
   test.each([
     [['kim,park']]
-  ])('split 메서드로 주어진 값을 구분', async (inputs) => {
-    mockQuestions(inputs);
+  ])('split 메서드로 주어진 값을 구분', async (input) => {
+    mockQuestions(input);
 
     const user = new User();
    
@@ -24,8 +24,8 @@ describe('사용자 입력 테스트', () => {
 
   test.each([
     [['kim']]
-  ])('split 메서드로 구분자가 포함되지 않은 경우 값을 그대로 반환', async (inputs) => {
-    mockQuestions(inputs);
+  ])('split 메서드로 구분자가 포함되지 않은 경우 값을 그대로 반환', async (input) => {
+    mockQuestions(input);
 
     const user = new User();
 
@@ -43,5 +43,15 @@ describe('사용자 입력 테스트', () => {
 
     await expect(user.inputPlayersName()).rejects.toThrow("[ERROR]");
   });
+
+  test.each([
+    [['5']]
+  ])('이동 횟수에 대한 예외 처리', async (input) => {
+    mockQuestions(input);
+
+    const user = new User();
+
+    await expect(user.inputMoveNum()).resolves.toEqual(5);
+  })
   
 })
