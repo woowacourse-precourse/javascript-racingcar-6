35c2d3b96ac17b370219ea5714b32807de4db2eb
정민승 <jeongminseung@jeongminseung-ui-MacBookAir.local>
2023-10-29 21:35:07 +0900

    결과 출력 생성

--- src/App.js
+++ src/App.js
@@ -19,6 +19,8 @@ class App {
     const carName = await this.getCarName();
     //시도 횟수 받기
     const tryNum = await this.getTryNum();
+    //결과, 최종 우승자 받기
+    const result = this.result(carName, tryNum);
   }
   //차 이름 받기 (유저 입력 받는 것 앞에는 비동기가 있어야 하나?)
   async getCarName() {
@@ -55,8 +57,32 @@ class App {
       });
     });
   }
+  //결과 받기
+  result(carName, tryNum) {
+    console.log("실행 결과");
+    //전진한 횟수 저장하는 변수
+    var goCount = [];
+    for (let i = 0; i < carName.length; i++) {
+      goCount.push(0);
+    }
+    // 총 tryNum만큼 실행
+    for (let k = 0; k < tryNum; k++) {
+      for (let i = 0; i < carName.length; i++) {
+        const randomNum = MissionUtils.Random.pickNumberInRange(0, 9); //랜덤한 숫자 불러오기
+        if (randomNum >= 4) {
+          goCount[i] += 1;
+        }
+        console.log(carName[i], ":", "-".repeat(goCount[i]));
+      }
+    }
+    //가장 긴 자동차 이름 return하기
+    const maxLong = Math.max.apply(null, goCount);
+    console.log(`최종 우승자 : ${carName[maxLong]}`);
+  }
 }
 const app = new App();
 app.play();
 
 export default App;
+
+//왜 게임 재실행 하려고 하면 안 되는 거지?
--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,1 +1,1 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+//왜 게임 재실행 하려고 하면 안 되는 거지?
