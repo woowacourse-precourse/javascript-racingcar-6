148a3bc56e774f5252ad884655d5bfd6540013e2
gwahyun <60453182+gwahyun@users.noreply.github.com>
2023-10-29 20:12:25 +0900

    Test: 우승자 출력 테스트 작성

--- __tests__/RacingTest.js
+++ __tests__/RacingTest.js
@@ -34,4 +34,24 @@ describe('레이스 게임 테스트', () => {
     expect(cars[0].textResult).toBe('--');
     expect(cars[1].textResult).toBe('--');
   });
+
+  test('우승자가 여러명일 경우 출력', () => {
+    const cars = [
+      { name: 'a', result: [4, 5, 8], textResult: '---' },
+      { name: 'b', result: [3, 0, 4], textResult: '-' },
+      { name: 'c', result: [4, 9, 5], textResult: '---' },
+    ];
+
+    expect(race.winner(cars)).toEqual('a,c');
+  });
+
+  test('우승자가 한 명일 경우 출력', () => {
+    const cars = [
+      { name: 'a', result: [4, 5, 8], textResult: '---' },
+      { name: 'b', result: [3, 0, 4], textResult: '-' },
+      { name: 'c', result: [4, 9, 3], textResult: '--' },
+    ];
+
+    expect(race.winner(cars)).toEqual('a');
+  });
 });
