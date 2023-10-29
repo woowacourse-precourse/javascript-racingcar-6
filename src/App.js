30ab8a7f2b7521c32cde8ba1a6e203af553983a7
hyj5076 <hyj5076@hanmail.net>
2023-10-29 17:52:16 +0900

    refactor(App): PlayGame 파일 코드 이동 및 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,53 @@
+import CarNameManager from "./utils/UserInputCarName";
+import { movingForwardCase } from "./utils/MovingForwardCase";
+import { FORWARD_DASH, WINNER, GAME_RESULT } from "./const/Messages";
+
 class App {
-  async play() {}
+  constructor() {
+    this.carManager = new CarNameManager();
+    this.carPositions = {};
+  }
+
+  async play() {
+    this.setupGame();
+  }
+
+  async setupGame() {
+    await this.carManager.inputCarName();
+    this.carManager.getCarName().forEach((carName) => {
+      this.carPositions[carName] = "";
+    });
+  }
+
+  playRound() {
+    Object.keys(this.carPositions).forEach((carName) => {
+      if (movingForwardCase()) {
+        this.carPositions[carName] += FORWARD_DASH;
+        Console.print(FORWARD_DASH);
+      }
+    });
+  }
+
+  printRoundResult() {
+    Object.entries(this.carPositions).forEach(([carName, position]) => {
+      Console.print(`${carName} : ${position}`);
+    });
+    Console.print();
+  }
+
+  findWinner() {
+    const maxPosition = Math.max(...Object.values(this.carPositions).map((pos) => pos.length));
+    return Object.entries(this.carPositions)
+      .filter(([_, position]) => position.length === maxPosition)
+      .map(([carName, _]) => carName);
+  }
+
+  printGameResult(carGame) {
+    Console.print(GAME_RESULT);
+    carGame.printRoundResult();
+    const winners = carGame.findWinner();
+    Console.print(`${WINNER} : ${winners.join(", ")}`);
+  }
 }
 
 export default App;
