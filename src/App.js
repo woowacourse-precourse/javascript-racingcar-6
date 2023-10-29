a21e19eaf7a63161c263254b5ea2a5c07663c1e2
hzzzi <hji2004@naver.com>
2023-10-29 17:02:58 +0900

    자동차 이름 입력 확인 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,38 +1,75 @@
-import { Console, MissionUtils } from "@woowacourse/mission-utils";
+import { Console, MissionUtils } from '@woowacourse/mission-utils';
 
 class Car {
-  constructor(name, position) {
+  constructor(name) {
     this.name = name;
-    this.position = position;
+    this.position = 0;
   }
-  position() {
+
+  makeRandomNumber() {
+    return MissionUtils.Random.pickNumberInRange(0, 9);
+  }
+
+  goForward() {
+    if (this.makeRandomNumber() >= 4) {
+      this.position++;
+    }
+  }
+
+  displayPosition() {
     return `${Car.name}: ${Car.position}`;
   }
 }
 
-function applyCarName(totalCarNum, carNameArr) {
-  const carArr = [];
-  for (let i = 0; i < totalCarNum; i++) {
-    let car = new Car(carNameArr[i], 0);
-    carArr.push(car);
-  }
+function makeCarArray(carNameArr) {
+  let carArr = [];
+  carNameArr.forEach((element) => {
+    carArr.push(new Car(element));
+  });
   return carArr;
 }
 
+function isCarNameOverFiveLetters(carNameArr) {
+  for (const name of carNameArr) {
+    if (name.length > 5) {
+      throw new Error('[ERROR] 자동차 이름은 5자 이하여야 합니다.');
+    }
+  }
+}
+
+function isCarNameEmpty(carNameArr) {
+  if (carNameArr.length === 0) {
+    throw new Error('[ERROR] 자동차 이름을 입력해주세요.');
+  }
+}
+
+function isCarNameSame(carNameArr) {
+  const carNameSet = new Set(carNameArr);
+  if (carNameSet.size !== carNameArr.length) {
+    throw new Error('[ERROR] 자동차 이름은 중복되지 않아야 합니다.');
+  }
+}
+
 class App {
-  async play() {}
+  async play() {
+    let carNameArr = await this.getCarName();
+    isCarNameEmpty(carNameArr);
+    isCarNameOverFiveLetters(carNameArr);
+    isCarNameSame(carNameArr);
+
+    let carArr = makeCarArray(carNameArr);
+  }
 
   async getCarName() {
     const carNames = await Console.readLineAsync(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
     );
-    const carNameArr = carNames.split(",");
-    const totalCarNum = carNameArr.length;
-    return [carNameArr, totalCarNum];
+    const carNameArr = carNames.split(',');
+    return carNameArr;
   }
 
   async getMoveCount() {
-    const moveCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+    const moveCount = await Console.readLineAsync('시도할 횟수는 몇 회인가요?');
     return moveCount;
   }
 }
