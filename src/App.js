4a0d7b0057747b0ac9b8a04131f2e50c79b4d7ce
seok-pyo <hongseokpyou@gmail.com>
2023-10-29 13:03:57 +0900

    feat: inputLapLength 메서드 작성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console } from '@woowacourse/mission-utils';
 
 class App {
+  constructor() {
+    this.entry = [];
+    this.lapLength = 0;
+  }
+
   async play() {}
 
   async inputEntry() {
@@ -9,10 +14,16 @@ class App {
     this.entry = tmpEntry;
   }
 
+  async inputLapLength() {
+    const inputNum = await Console.readLineAsync(this.message('ASK_LAP'));
+    this.lapLength = inputNum;
+  }
+
   message(option) {
     const MESSAGE = {
-      INPUT_NAME: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
-      ASK_LAB: '시도할 횟수는 몇 회인가요?\n',
+      INPUT_NAME:
+        '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+      ASK_LAP: '시도할 횟수는 몇 회인가요?\n',
     };
     return MESSAGE[option];
   }
