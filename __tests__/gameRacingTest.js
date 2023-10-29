16ec5f840a2ce123cc12f8a094772facde695475
k-redstone <che0618@naver.com>
2023-10-29 21:56:49 +0900

    feat: 게임 진행 및 결과 테스트 코드 작성

---
+++ __tests__/gameRacingTest.js
@@ -0,0 +1,46 @@
+import App from "../src/App.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const mockQuestions = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  });
+};
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe("자동차 게임 진행", () => {
+  test("전진-정지", async () => {
+    // given
+    const inputs = ["hong,woni,che12", "3"];
+    const outputs = ["hong : --", "woni : -", "che12 : --"];
+    const randoms = [4, 3, 2, 5, 5, 6, 1, 2, 9];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([...randoms]);
+
+    // when
+    const app = new App();
+    await app.play();
+
+    // then
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+});
