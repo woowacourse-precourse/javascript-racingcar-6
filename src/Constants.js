6db2d3ab525560b6753642057fb177890f914bb0
KYHH <kyhh93@naver.com>
2023-10-29 14:34:20 +0900

    feat : 출력 메시지를 상수로 관리

---
+++ src/Constants.js
@@ -0,0 +1,10 @@
+const ERROR_MESSAGE = "[ERROR] 입력이 올바르지 않습니다. 게임을 종료합니다.";
+
+const PLAY_GAME = {
+  input: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+  tryCount: '시도할 횟수는 몇 회인가요?',
+  result: '\n실행 결과',
+  winner: '최종 우승자 : ',
+};
+
+export { PLAY_GAME, ERROR_MESSAGE };
