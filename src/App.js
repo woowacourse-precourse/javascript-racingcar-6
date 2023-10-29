a57c21a51d8c14a227dbd922cc7fe7028e9d6316
songhee Kim <soonghee2@gmail.com>
2023-10-30 01:10:56 +0900

    [feat] 랜덤 전진함수 go_randomly 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 class App {
-  async play() {}
+  async play() {
+    function go_randomly(scores){
+      let random_number;
+      for (let i=0;i<scores.length;i++){
+        random_number= MissionUtils.Random.pickNumberInRange(0, 9);
+        // random_number=Math.floor(Math.random() * 10);
+        if (random_number>3){ scores[i]=scores[i]+1; }
+      }
+      return scores;
+    }
+  }
 }
 
 export default App;
