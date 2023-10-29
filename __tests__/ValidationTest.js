6f1fdee661cb5836ea082ac010bac21054f72e85
sbemjr1 <sbemjr1@naver.com>
2023-10-29 14:18:18 +0900

    test:테스트 코드 추가

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,8 +1,12 @@
 import { ERROR } from '../src/constants/constant.js';
-import { userInputTryNumberError } from '../src/domain/Error.js';
+import {
+  userInputTryNumberError,
+  userInputCarNameFormError,
+  userInputCarNameLengthError,
+} from '../src/domain/Error.js';
 
 // Jest 테스트 스위트 시작
-describe('checkIfNegativeOrZero 함수 테스트', () => {
+describe('userInputTryNumberError 함수 테스트', () => {
   // 0 이하인 경우 오류가 throw되어야 함
   test('0 이하인 경우 오류 throw', () => {
     expect(() => userInputTryNumberError(-1)).toThrowError(ERROR.ERROR_TYPE);
@@ -14,10 +18,7 @@ describe('checkIfNegativeOrZero 함수 테스트', () => {
     expect(() => userInputTryNumberError(1)).not.toThrow();
     expect(() => userInputTryNumberError(100)).not.toThrow();
   });
-});
 
-// Jest 테스트 스위트 시작
-describe('checkIfNumber 함수 테스트', () => {
   // 숫자인 경우 오류가 throw되지 않아야 함
   test('정수형 숫자인 경우 오류 throw 안 됨', () => {
     expect(() => userInputTryNumberError(123)).not.toThrow();
@@ -32,4 +33,16 @@ describe('checkIfNumber 함수 테스트', () => {
     expect(() => userInputTryNumberError(undefined)).toThrowError(ERROR.ERROR_TYPE);
     expect(() => userInputTryNumberError(NaN)).toThrowError(ERROR.ERROR_TYPE);
   });
+});
+
+describe('userInputCarName 함수 테스트', () => {
+  test('공백이 포함될 경우 오류 throw', () => {
+    expect(() => userInputCarNameFormError('a b c')).toThrowError(ERROR.ERROR_SPACE);
+    expect(() => userInputCarNameFormError('de f')).toThrowError(ERROR.ERROR_SPACE);
+  });
+
+  // test('길이가 5를 초과할 때 오류 throw', () => {
+  //   expect(() => userInputCarNameLengthError('abcdef')).toThrowError(ERROR.ERROR_LENGTH);
+  //   expect(() => userInputCarNameLengthError('안녕하시렵니까')).toThrowError(ERROR.ERROR_LENGTH);
+  // });
 });
