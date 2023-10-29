bdb3ea5b111574cf26d7c936cccb9e3ef55b6bf7
Jeongeun Lee <leeju1009@gmail.com>
2023-10-29 19:22:50 +0900

    refactor: 에러 메세지를 상수로 분리
    에러 메세지 문자열을 상수로 분리

--- src/ErrorHandler.js
+++ src/ErrorHandler.js
@@ -1,14 +1,17 @@
 class ErrorHandler {
+  ERROR_MESSAGE = {
+    invalidCarName:
+      '[ERROR] 자동차 이름은 5자 이하로 입력해주세요. (쉼표(,)로 구분)',
+    invalidRaceTimes:
+      '[ERROR] 숫자가 잘못된 형식입니다. 1 이상의 정수를 입력해주세요.',
+  };
+
   throwInvalidCarNameError() {
-    throw new Error(
-      '[ERROR] 자동차 이름은 5자 이하로 입력해주세요. (쉼표(,)로 구분)'
-    );
+    throw new Error(ERROR_MESSAGE.invalidCarName);
   }
 
   throwInvalidraceTimesError() {
-    throw new Error(
-      '[ERROR] 숫자가 잘못된 형식입니다. 1 이상의 정수를 입력해주세요.'
-    );
+    throw new Error(ERROR_MESSAGE.invalidRaceTimes);
   }
 }
 
