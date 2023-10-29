3ca96bd26bdae1935e92a54d4302507de541b2ac
Jaewon <wellbird@naver.com>
2023-10-29 19:51:21 +0900

    feat : Car 클래스 내부 move 메서드 구현

--- src/Car.js
+++ src/Car.js
@@ -1,6 +1,20 @@
+import { Random } from "@woowacourse/mission-utils";
+import { AfterMoveMessage } from "./Messages/Message";
+
 class Car {
   constructor(name) {
     this.name = name;
+    this.countMove = 0;
+    this.showMove = "";
+  }
+
+  move() {
+    const RANDOM_NUMBER = Random.pickNumberInRange(0, 9);
+    if (RANDOM_NUMBER >= 4) {
+      this.countMove += 1;
+      this.showMove += "-";
+    }
+    AfterMoveMessage(this.name, this.showMove);
   }
 }
 
