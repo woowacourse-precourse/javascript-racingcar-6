c3fd21668063d43144e7cbd9ff53d5d2e6373003
BangDori <qudwns8865@naver.com>
2023-10-29 17:23:50 +0900

    style: OutputView의 함수들 주석 추가

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -3,18 +3,33 @@ import { Console } from "@woowacourse/mission-utils";
 import { OUTPUT_MESSAGE } from "../constants/io-message.js";
 
 const OutputView = {
+  /**
+   * Racing의 시작과 함께 실행 결과 안내 메시지를 출력하는 함수
+   */
   printRacingStart: () => {
     Console.print(OUTPUT_MESSAGE.ROUND_RESULT);
   },
 
+  /**
+   * 현재 Racing 중인 자동차의 전진 거리를 출력하는 함수
+   * @param {string} carName 자동차의 이름
+   * @param {number} carDistance 자동차의 전진 거리
+   */
   printCurrentRacingCar: (carName, carDistance) => {
     Console.print(`${carName} : ${"-".repeat(carDistance)}`);
   },
 
+  /**
+   * 줄띄움 함수
+   */
   printSpacing: () => {
     Console.print("");
   },
 
+  /**
+   * 우승자들을 출력하는 함수
+   * @param {string[]} winners 우승자들의 이름이 담긴 배열
+   */
   printRacingFinalWinners: (winners) => {
     Console.print(`${OUTPUT_MESSAGE.FINAL_WINNERS} : ${winners}`);
   },
