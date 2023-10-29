469c62698ff4a7707292727f0b6e7edede99c046
starcradle101 <dreamerdev12@gmail.com>
2023-10-29 15:25:16 +0900

    :fire: Refactor: Log.js 모듈 수정
    오류 처리와 관련된 코드는 Errors.js 모듈에서 관리하므로 Logs.js 내부의 불필요한 코드 삭제

--- src/Logs.js
+++ src/Logs.js
@@ -4,14 +4,6 @@ export const PLAYER_INPUT = {
   TRY_NUMBER_PROMPT: '시도할 횟수는 몇 회인가요?',
 };
 
-export const ERROR_MESSAGES = {
-  INVALID_CARS_NAME_LENGTH:
-    '[ERROR] 자동차의 이름은 5글자 이하로 입력해 주세요',
-  INVALID_CARS_NAME_STRING: '[ERROR] 자동차의 이름에는 영어만 사용할 수 있어요',
-  INVALID_CARS_NAME_DUPLICATE: '[ERROR] 자동차의 이름은 중복될 수 없어요',
-  INVALID_TRY_NUMBER_NEGATIVE: '[ERROR] 시도 횟수는 0보다 큰 정수입니다',
-};
-
 export const RACE = {
   PROGRESS_BAR: '-',
   START: '실행 결과',
