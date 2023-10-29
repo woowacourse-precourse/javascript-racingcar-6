9c5f1a2f925bc2b5be9a9f437b526ece9ddd6837
soyunim <togksk123@naver.com>
2023-10-29 20:11:35 +0900

    feat: 최종 우승자 출력 및 게임종료 기능

--- src/racingCar.js
+++ src/racingCar.js
@@ -1,12 +1,19 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 import { ERROR_MSG, GAME_MSG } from "./message";
 
+const CAR_NAME = [];
+const CAR_NAME_LENGTH = [];
+
+// 게임 시작
 export const startGame = async () => {
   const carNames = await getCarsName();
   const tryTimes = await getTryTimes();
   printExecutionResult(carNames, tryTimes);
+  printFinalWinner(carNames);
+  endGame();
 };
 
+// 자동차 이름 입력
 const getCarsName = async () => {
   const cars = await Console.readLineAsync(GAME_MSG.GET_CARS_NAME);
   const carNames = cars.split(",");
@@ -16,11 +23,14 @@ const getCarsName = async () => {
     }
   }
   for (let i = 0; i < carNames.length; i++) {
+    CAR_NAME.push(carNames[i]);
+    CAR_NAME_LENGTH[i] = carNames[i].length;
     carNames[i] = carNames[i] + " : ";
   }
   return carNames;
 };
 
+// 시도 횟수 입력
 const getTryTimes = async () => {
   try {
     const tryTimes = await Console.readLineAsync(GAME_MSG.GET_TRY_NUMBER);
@@ -33,31 +43,52 @@ const getTryTimes = async () => {
   }
 };
 
+// 실행 결과 출력
 const printExecutionResult = (carNames, tryTimes) => {
   Console.print(GAME_MSG.EXECUTION_RESULT);
   while (tryTimes > 0) {
     getExecution(carNames);
-    Console.print(tryTimes);
-    Console.print(carNames);
+    for (let i = 0; i < carNames.length; i++) {
+      Console.print(carNames[i]);
+    }
+    Console.print("\n");
     tryTimes--;
   }
 };
-
+// 전진-정지에 대한 결과 추가
 const getExecution = (carNames) => {
   for (let i = 0; i < carNames.length; i++) {
-    if (getRandomNumber == true) {
+    if (getRandomNumber()) {
       carNames[i] = carNames[i] + "-";
     }
   }
 };
-
+// 전진-정지를 위한 랜덤 숫자 받기
 const getRandomNumber = () => {
-  const randomNumber = Random.pickNumberInRange(0, 9);
+  const randomNumber = Random.pickNumberInRange(1, 9);
   if (randomNumber > 3) {
-    console.log("random:" + randomNumber);
     return true;
   }
   return false;
+};
+
+const printFinalWinner = (carNames) => {
+  const resultLength = [];
+  carNames.forEach((car, idx) => {
+    resultLength.push(car.length - CAR_NAME_LENGTH[idx]);
+  });
+
+  let max = 0;
+  const maxCars = [];
+  resultLength.forEach((len) => {
+    max < len ? (max = len) : "";
+  });
+  resultLength.forEach((len, idx) => {
+    max == len ? maxCars.push(CAR_NAME[idx]) : "";
+  });
+
+  let gameResult = maxCars.join(", ");
+  Console.print(`최종 우승자 : ${gameResult}`);
 };
 
 const endGame = () => {
