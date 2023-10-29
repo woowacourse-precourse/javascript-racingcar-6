62f1719a5112843b0975f4e01040bf5add0a338c
Yoonyesol <maniac970430@naver.com>
2023-10-29 16:52:35 +0900

    feat(input): 사용자에게 자동차 이름 입력받기 기능 구현
    Add:
    -사용자에게 입력을 받아 ','로 이름을 구별한 뒤 리턴해 주는 inputHandler 함수 구현
    -App.js에서 inputHandler 함수를 호출하고 결과를 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Console } from "@woowacourse/mission-utils";
+import { inputHandler } from "./utils/inputHandler.js";
+
 class App {
-  async play() {}
+  async play() {
+    const carNameList = await inputHandler();
+    Console.print(carNameList);
+  }
 }
 
 export default App;
---
+++ src/utils/inputHandler.js
@@ -0,0 +1,8 @@
+import { Console } from "@woowacourse/mission-utils";
+
+export async function inputHandler() {
+  const carName = await Console.readLineAsync(
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+  );
+  return carName.split(",");
+}
