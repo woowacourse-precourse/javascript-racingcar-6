992f6ef4d496c41075f413560311a981a852c0e6
Jaewon <wellbird@naver.com>
2023-10-29 19:57:26 +0900

    feat : 자동차 이름을 입력받는 getCar메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,35 @@
+import { Console } from "@woowacourse/mission-utils";
+import { GetCarNameMessage } from "./Messages/Message";
+import {
+  CAR_NAME_DUPLICATE_ERROR,
+  CAR_NAME_LENGTH_ERROR,
+} from "./Messages/Error";
+
 class App {
+  constructor() {
+    this.cars = [];
+    this.carNames = [];
+    this.loopNumber = 0;
+  }
+
+  async getCar() {
+    GetCarNameMessage();
+    const inputCarName = await Console.readLineAsync().then((res) =>
+      res.split(",")
+    );
+    inputCarName.forEach((name) => {
+      if (name.length === 0 || name.length > 5) {
+        CAR_NAME_LENGTH_ERROR();
+      }
+      if (this.carNames.includes(name)) {
+        CAR_NAME_DUPLICATE_ERROR();
+      }
+      const newCar = new Car(name);
+      this.carNames.push(name);
+      this.cars.push(newCar);
+    });
+  }
+
   async play() {}
 }
 
