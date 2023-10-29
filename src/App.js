9cc940b3886b2612b1fe411799741e0db0487fc8
doongeon <ndmb2012@gamil.com>
2023-10-29 16:11:33 +0900

    사용자에게 입력받는 함수 구현완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const getUserInput = async(text) => {
+  let userInput = '';
+  await MissionUtils.Console.readLineAsync(text)
+    .then((input) => {userInput = input})
+    .catch((err) => {throw err});
+
+  return userInput;
+}
+
 class App {
-  async play() {}
+  async play() {
+    try {
+      const userInput = await getUserInput('Enter your Input : ');
+
+      MissionUtils.Console.print(userInput);
+    } catch(err) {
+
+    }
+  }
 }
 
 export default App;
