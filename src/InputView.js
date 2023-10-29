b10d8704f5c2d856c76b94c689697393b66f6557
DESKTOP-K188DV0\tlstj <tlstjsdud566@naver.com>
2023-10-29 15:52:01 +0900

    [feat] 자동차 각각의 이름 길이가 0인지  검증으로 수정

--- src/InputView.js
+++ src/InputView.js
@@ -3,10 +3,14 @@ const { Console, READ_RACING_CAR_NAMES } = require('./Constant');
 const InputView = {
   async readCarNames() {
     const answer = await Console.readLineAsync(READ_RACING_CAR_NAMES);
+    const answerArray = answer.split(',');
 
-    if (answer.length === 0) {
-      throw new Error('[ERROR] 2대 이상의 자동차 이름을 입력해 주세요.');
-    }
+    answerArray.map(item => {
+      if (item.length === 0) {
+        throw new Error('[ERROR] 2대 이상의 자동차 이름을 입력해 주세요.');
+      }
+    });
+
     return answer;
   },
 };
