75cbbb20097820be0c2942400d366aa4633dbdef
seok-pyo <hongseokpyou@gmail.com>
2023-10-29 16:12:03 +0900

    docs: 기능 목록 수정

--- src/App.js
+++ src/App.js
@@ -34,16 +34,6 @@ class App {
     this.lapLength = inputNum;
   }
 
-  message(option) {
-    const MESSAGE = {
-      INPUT_NAME: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
-      ASK_LAP: '시도할 횟수는 몇 회인가요?\n',
-      RESULT: '\n실행 결과',
-      NO_WINNER: '최종 우승자 : 없음',
-    };
-    return MESSAGE[option];
-  }
-
   printWinner(finalRecord) {
     finalRecord.forEach((record) => {
       const countLength = record.lastIndexOf('-') - record.indexOf('-') + 1;
@@ -53,6 +43,16 @@ class App {
 
     if (this.winner.length === 0) Console.print(this.message('NO_WINNER'));
     else Console.print(`최종 우승자 : ${this.winner.join(', ')}`);
+  }
+
+  message(option) {
+    const MESSAGE = {
+      INPUT_NAME: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+      ASK_LAP: '시도할 횟수는 몇 회인가요?\n',
+      RESULT: '\n실행 결과',
+      NO_WINNER: '최종 우승자 : 없음',
+    };
+    return MESSAGE[option];
   }
 }
 
