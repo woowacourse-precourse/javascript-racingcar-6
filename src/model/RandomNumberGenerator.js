d0ae544fdfbe4a03cff733b7a7cd0daf411e50e3
swarvy <swarvy0826@naver.com>
2023-10-29 12:46:18 +0900

    refactor : RandomNumberGenerator.js에 JSDocs 추가

--- src/model/RandomNumberGenerator.js
+++ src/model/RandomNumberGenerator.js
@@ -2,6 +2,11 @@ import { Random } from '@woowacourse/mission-utils';
 import { GRANDPRIX_MAGICNUMBER } from '../constants/GrandPrixOption.js';
 
 export default class RandomNumberGenerator {
+  /**
+   * @static
+   * @param {number} amount
+   * @returns {number[]} (0 ~ 9)까지 랜덤으로 생성된 숫자들을 반환
+   */
   static generate(amount) {
     return Array.from({ length: amount }, () =>
       Random.pickNumberInRange(GRANDPRIX_MAGICNUMBER.min, GRANDPRIX_MAGICNUMBER.max),
