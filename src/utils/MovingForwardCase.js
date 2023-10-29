a03bf4e4beaa395e5ffcabf71c70f232b9b16a0d
hyj5076 <hyj5076@hanmail.net>
2023-10-29 17:32:58 +0900

    refactor: 대시 출력 로직 제거 및 코드 정리

--- src/utils/MovingForwardCase.js
+++ src/utils/MovingForwardCase.js
@@ -1,10 +1,7 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import { FORWARD_MATCHING_POINT, FORWARD_DASH } from "../const/Messages";
+import { FORWARD_MATCHING_POINT } from "../const/Messages";
 
 export const movingForwardCase = () => {
   const number = MissionUtils.Random.pickNumberInRange(0, 9);
-
-  if (number >= FORWARD_MATCHING_POINT) {
-    Console.print(FORWARD_DASH);
-  }
+  return number >= FORWARD_MATCHING_POINT;
 };
