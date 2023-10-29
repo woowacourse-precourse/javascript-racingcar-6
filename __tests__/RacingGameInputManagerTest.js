cde63f0f1d933752c952b8270feae38d9a2d9782
zxxng <jeayeong123@naver.com>
2023-10-29 23:46:26 +0900

    test: getRacingInfo 함수 테스트 코드 추가

--- __tests__/RacingGameInputManagerTest.js
+++ __tests__/RacingGameInputManagerTest.js
@@ -1,55 +1,57 @@
 import { Console } from '@woowacourse/mission-utils';
 import RacingGameInputManager from '../src/racingcar/lib/classes/RacingGameInputManager';
 
-// 사용자 입력을 가로채기 위한 모의 함수를 만드는 함수
-const mockQuestions = (inputs) => {
+let inputManager;
+
+// beforeEach: 각 테스트 실행 전 공통적으로 호출되는 함수
+beforeEach(() => {
+  inputManager = new RacingGameInputManager();
   Console.readLineAsync = jest.fn();
-
-  Console.readLineAsync.mockImplementation(() => {
-    const input = inputs.shift();
-    return Promise.resolve(input);
-  });
-};
-
-const inputManager = new RacingGameInputManager();
+});
 
 describe('자동차 경주 게임', () => {
-  const validNameList = [
-    [['pobi,woni']],
-    [['pobi,zxxng,jena']],
-    [['pobi,woni,jun,zxxng']],
-  ];
-  const invalidNameList = [
-    [['pobi,javaji']],
-    [['pobi,eastjun']],
-    [['zxxng']],
-    [['123,233,233']],
-  ];
+  const validNameList = ['pobi,woni', 'pobi,woni,jun,zxxng'];
+  const invalidNameList = ['pobi,eastjun', 'zxxng', '123,233,233'];
 
-  test.each(validNameList)('유효한 이름 목록', async (inputs) => {
-    // 주어진 입력을 모의화
-    mockQuestions(inputs);
+  test.each(validNameList)('유효한 이름 목록', async (input) => {
+    Console.readLineAsync.mockResolvedValueOnce(input);
     await expect(inputManager.getRacingCars()).resolves.not.toThrow('[ERROR]');
   });
 
-  test.each(invalidNameList)('유효하지 않은 이름 목록', async (inputs) => {
-    mockQuestions(inputs);
+  test.each(invalidNameList)('유효하지 않은 이름 목록', async (input) => {
+    Console.readLineAsync.mockResolvedValueOnce(input);
     await expect(inputManager.getRacingCars()).rejects.toThrow('[ERROR]');
   });
 
-  const validNumberList = ['1', '2', '3', '4', '5'];
+  const validNumberList = ['1', '3'];
   const invalidNumberList = ['0', '-2', '2번', 'two'];
 
-  validNumberList.forEach((input) => {
-    test(`유효한 숫자 목록 - ${input}`, async () => {
-      mockQuestions([input]);
-      await expect(inputManager.getPlayCount()).resolves.not.toThrow('[ERROR]');
-    });
+  test.each(validNumberList)('유효한 숫자 목록', async (input) => {
+    Console.readLineAsync.mockResolvedValueOnce(input);
+    await expect(inputManager.getPlayCount()).resolves.not.toThrow('[ERROR]');
   });
-  invalidNumberList.forEach((input) => {
-    test(`유효하지 않은 숫자 목록 - ${input}`, async () => {
-      mockQuestions([input]);
-      await expect(inputManager.getPlayCount()).rejects.toThrow('[ERROR]');
-    });
+
+  test.each(invalidNumberList)('유효하지 않은 숫자 목록', async (input) => {
+    Console.readLineAsync.mockResolvedValueOnce(input);
+    await expect(inputManager.getPlayCount()).rejects.toThrow('[ERROR]');
   });
+
+  const validInput = [
+    ['pobi,woni', '5'],
+    ['pobi,jun,jena', '3'],
+  ];
+
+  test.each(validInput)(
+    'getRacingInfo() 함수 테스트',
+    async (carNames, playCount) => {
+      Console.readLineAsync
+        .mockResolvedValueOnce(carNames)
+        .mockResolvedValueOnce(playCount);
+      const racingInfo = await inputManager.getRacingInfo();
+      expect(racingInfo).toMatchObject({
+        racingCars: inputManager.racingCars,
+        playCount: inputManager.playCount,
+      });
+    },
+  );
 });
