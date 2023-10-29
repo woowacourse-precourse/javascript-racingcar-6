5741a7ea73f248428b1bf2d29e7a66e0549fd2e0
kimsuyeon0916 <kimsuyeon_09162naver.com>
2023-10-29 17:10:08 +0900

    feat: 상수 모듈화

---
+++ src/utils/Constants.js
@@ -0,0 +1,10 @@
+const IN_GAME_MESSAGE = {
+  getCarName: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+  getTryCount: '시도할 횟수는 몇 회인가요?',
+  tryResult: '실행 결과',
+  finalWinner: '최종 우승자 : ',
+}
+
+const Constants = [ IN_GAME_MESSAGE, ]
+
+export default Constants;
