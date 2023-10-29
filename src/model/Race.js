9422f7d6435d7576a95f41a8d9184882e6a28452
osohyun0224 <osohyun0224@naver.com>
2023-10-29 20:49:22 +0900

    feat(Race): 자동차 경주 최종 우승자를 구하는 기능 구현
    - getWinners()를 생성해 모든 자동차들 중에서 가장 멀리 이동한 자동차들의 이름을 반환하도록 구현하였습니다.
    - 최대 위치를 가진 자동차의 이름을 배열로 반환해줘서 여러 우승자가 있을 경우도 구현하였습니다.

--- src/model/Race.js
+++ src/model/Race.js
@@ -33,4 +33,11 @@ export default class Race {
       });
     }
   }
+  
+  getWinners() {
+    const maxPosition = Math.max(...this.#cars.map(car => car.getPosition()));
+    return this.#cars
+      .filter(car => car.getPosition() === maxPosition)
+      .map(car => car.getName());
+  }
 }
