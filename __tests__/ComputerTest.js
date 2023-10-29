14b5f86b626d53838d1773dd90cefe28c066fed6
Boeun <boeun0904@naver.com>
2023-10-29 22:11:37 +0900

    test(ComputerTest): fix method to static

--- __tests__/ComputerTest.js
+++ __tests__/ComputerTest.js
@@ -4,23 +4,20 @@ describe('컴퓨터 예외 처리 테스트', () => {
   test.each([',car', 'car,', ',car,'])(
     '문장의 맨 앞이나 뒤에 ,를 입력할 경우 예외 처리',
     (inputs) => {
-      const computer = new Computer();
-      expect(() => computer.getCarNameArrayFromString(inputs)).toThrow(
-        '[ERROR]'
+      expect(() => Computer.getCarNameArrayFromString(inputs)).toThrow(
+        '[ERROR]',
       );
-    }
+    },
   );
 
   test('이름이 5자 초과인 차 이름을 입력하는 경우 예외 처리', () => {
-    const computer = new Computer();
-    expect(() => computer.getCarNameArrayFromString('longcar, car')).toThrow(
-      '[ERROR]'
+    expect(() => Computer.getCarNameArrayFromString('longcar, car')).toThrow(
+      '[ERROR]',
     );
   });
 
   test('차 이름을 올바르게 입력한 경우 배열 반환', () => {
-    const computer = new Computer();
-    expect(computer.getCarNameArrayFromString('carA, carB, carC')).toEqual([
+    expect(Computer.getCarNameArrayFromString('carA, carB, carC')).toEqual([
       'carA',
       'carB',
       'carC',
@@ -28,12 +25,10 @@ describe('컴퓨터 예외 처리 테스트', () => {
   });
 
   test('숫자 이외의 문자를 입력한 경우 예외 처리', () => {
-    const computer = new Computer();
-    expect(() => computer.getTrialNumberFromString('abc')).toThrow('[ERROR]');
+    expect(() => Computer.getTrialNumberFromString('abc')).toThrow('[ERROR]');
   });
 
   test('숫자를 올바르게 입력한 경우 숫자 반환', () => {
-    const computer = new Computer();
-    expect(computer.getTrialNumberFromString('10')).toEqual(10);
+    expect(Computer.getTrialNumberFromString('10')).toEqual(10);
   });
 });
