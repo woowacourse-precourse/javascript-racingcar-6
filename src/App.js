cb87ea7a69b2cc51077b0ccd7be6525f1fb68af5
KYHH <kyhh93@naver.com>
2023-10-29 12:21:18 +0900

    feat : 자동차를 움직이기 위한 0-9 사이의 무작위 값 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from '@woowacourse/mission-utils';
+import CreateCarMoveCount from './RandomNumber.js';
+
 class App {
-  async play() {}
+  async play() {
+    const number = new CreateCarMoveCount();
+    const numberResult = number.createRandomNumber();
+    Console.print(numberResult);
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
