efc12e3a71390da0e27d4976bc1607c5df158d90
solssak <gswoong1012@gmail.com>
2023-10-30 02:48:15 +0900

    refactor: 자동차 이동 로직, 우승자 계산 로직 modelview영역으로 분리 후 실행 로직에 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import InputView from "./view/InputView.js";
+import { readUserRaceCarName, readAttemptsCount } from "./view/InputView.js";
+import { printResultMsg, printRaceResult, printWinners } from "./view/OutputView.js";
+import { playGame, calculateWinners } from "./modelview/Modelview.js";
 
 class App {
   async play() {
-    await InputView.readUserRaceCarName();
-    await InputView.readAttemptsCount();
+    const names = await readUserRaceCarName();
+
+    const tryCount = await readAttemptsCount();
+
+    printResultMsg();
+
+    for (let i = 0; i < tryCount; i++) {
+      const raceResults = playGame(names);
+      for (const name in raceResults) {
+        printRaceResult(name, raceResults[name]);
+      }
+    }
+
+    const winners = calculateWinners(names);
+
+    printWinners(winners);
   }
 }
 
