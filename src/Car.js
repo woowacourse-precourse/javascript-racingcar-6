a4231424d99d09f18e129a9aaf0f80b638dcd47f
Junseo <monstazo911@gmail.com>
2023-10-29 18:29:26 +0900

    Feat: 자동차 무작위 수 뽑는 메소드 추가
    - 각 자동차에서 무작위 수를 뽑는 메소드 randomNumGenerate 를 추가했다.
    - woowacourse의 모듈을 사용하여 0에서 9사이의 랜덤한 숫자를 생성한다.
    - 그 후 해당 숫자를 반환한다.

--- src/Car.js
+++ src/Car.js
@@ -1,8 +1,15 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class Car {
   name = "";
 
   constructor(carName) {
     this.name = carName;
+  }
+
+  static randomNumGenerate() {
+    const randomNum = MissionUtils.Random.pickNumberInRange(0, 9);
+    return randomNum;
   }
 }
 
