1c87b19e7eda5195e63d3a7d30bf8418ff2a1d72
dyddks <dyddks0115@naver.com>
2023-10-29 22:58:42 +0900

    style: 코드컨벤션 수정

--- __tests__/RacingTest.js
+++ __tests__/RacingTest.js
@@ -9,11 +9,11 @@ const mockRandoms = (numbers) => {
 };
 
 describe("Racing 함수 테스트", () => {
-  test(" n만큼 반복하여 RandomPick 반환값을 배열에 저장한뒤 반환", () => {
+  test("n만큼 반복하여 RandomPick 반환값을 배열에 저장한뒤 반환", () => {
     const car = ['용안', '이삭', '광호'];
     const n = 2;
     const inputs = ['6', '2', '5', '2', '6', '7'];
-    
+
     mockRandoms(inputs);
     expect(Racing(car, n)).toEqual(['-', '-', '--']);
   });
