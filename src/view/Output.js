d30fccf2b3dfd71e052bbb56e8d9a14130f10bef
ChanYong Park <krap5075@naver.com>
2023-10-30 04:21:52 +0900

    feat: 1번 이동마다 결과 출력하는 기능 구현

--- src/view/Output.js
+++ src/view/Output.js
@@ -3,10 +3,20 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 const Console = MissionUtils.Console;
 
 class Output {
-  // 매개변수로 Car 객체배열과, 이동횟수를 받는다.
-  printResults(Cars, try_number) {}
+  constructor() {
+    Console.print("실행 결과\n");
+  }
+  // 매개변수로 Car 객체배열을 받는다.
+  printResult(Cars) {
+    // 1번 실행할 때 마다 출력해야한다.
+    // 각 객체의 이동횟수 만큼 - 출력해준다.
+    Cars.forEach((car) => {
+      Console.print(`${car.name} : ${car.dashes.repeat(car.move_times)}`);
+    });
+    Console.print("\n");
+  }
 
-  // 각 객체들의 멤버변수로 이동한 횟수를 가진다.
+  // 객체 멤버변수로 이동한 횟수를 가진다는 가정
   printWinners(Cars) {
     const max_times = Math.max(...Cars.map((car) => car.move_times));
     //todo 숫자형인지 문자인지 비교가 되는지 체크해봐야한다
