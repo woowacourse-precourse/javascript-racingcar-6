139e1fb60086fbb1567e5b1b6237b8751b8263d1
coddingyun <igy2840@gmail.com>
2023-10-29 12:44:39 +0900

    feat: 자동차 이름 5자 초과시 에러 출력 checkValidCarName 함수 생성 - 파라미터로 carNameArr 받음 - carNameArr를 for문으로 모두 확인하며 5자가 넘을 경우 에러 발생

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ class App {
       const carName = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n')
       return carName.split(',')
     };
+    const checkValidCarName = async(carNameArr) => {
+      for (let i=0; i<carNameArr.length; i++) {
+        if (carNameArr[i].length > 5) {
+          throw new Error("[ERROR] 자동차 이름이 잘못된 형식입니다.")
+        }
+      }
+    }
     const carNameArr = await getCarNameInput();
+    checkValidCarName(carNameArr)
   }
 }
 
