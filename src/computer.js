97f186ac13825ce54327f8aa1098de29c234eb91
yheseul_kang <kangyheseul@naver.com>
2023-10-29 22:02:02 +0900

    Feat: 컴퓨터의 무작위 값 생성 기능을 구현 // 추출된 Random 값에 따른 전진 기능 구현

--- src/computer.js
+++ src/computer.js
@@ -1,3 +1,16 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
-MissionUtils.Random.pickNumberInRange(0, 9);
+export  const number = function () {
+  const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
+  return randomNumber;
+}
+export const realNumber = number()
+console.log(realNumber);
+console.log(typeof(realNumber));
+
+export  const try_racing = function (number) {
+  let bar;
+  if (number > 4) bar = '-';
+  return bar;
+}
+console.log(try_racing(realNumber))
