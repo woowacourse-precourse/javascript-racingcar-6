d057d61e4e4b8be1a50ec3a4141d89141d1cd7a9
okxooxoo <okxooxoo@naver.com>
2023-10-29 20:50:14 +0900

    📝 docs: 구현 시 참고할 기타 사항 작성

--- utils/inputValidator.js
+++ utils/inputValidator.js
@@ -1,5 +1,5 @@
 export const validateCarName = (carName) => {
-  const regex = new RegExp(/^(\w{1,5},)+\w{1,5}$/);
+  const regex = new RegExp(/^(\w{1,5},\s?)+\w{1,5}$/);
 
   if (!regex.test(carName))
     throw new Error("[ERROR] 자동차 이름이 잘못된 형식입니다.");
