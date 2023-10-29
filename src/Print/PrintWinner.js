3282a6953c9bef547ee0df97cdecb901dd9034b7
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-10-29 22:14:16 +0900

    refactor: 반복문 변경

--- src/Print/PrintWinner.js
+++ src/Print/PrintWinner.js
@@ -4,17 +4,17 @@ import RACING_CAR_GAME from '../Constant/Constant.js';
 const PrintWinner = (racingCar) => {
   const RACING_CAR_NUMBER = [];
 
-  [...racingCar].forEach((item) => {
-    RACING_CAR_NUMBER.push(item[1].length);
-  });
+  for (const CAR of racingCar) {
+    RACING_CAR_NUMBER.push(CAR[1].length);
+  }
 
   const WINNER_NUMBER = Math.max(...RACING_CAR_NUMBER);
 
   const WINNER = [];
 
-  [...racingCar].forEach((item) => {
-    if (item[1].length === WINNER_NUMBER) WINNER.push(item[0]);
-  });
+  for (const CAR of racingCar) {
+    if (CAR[1].length === WINNER_NUMBER) WINNER.push(CAR[0]);
+  }
 
   MissionUtils.Console.print(
     `${RACING_CAR_GAME.PRINT_WINNER}${WINNER.join(', ')}`
