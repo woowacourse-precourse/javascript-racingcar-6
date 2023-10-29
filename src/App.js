5b3116428e6b79e2abd6998a3a0562ad704feff8
김예빈 <kimyebin@gim-yebin-ui-MacBookPro.local>
2023-10-30 02:19:28 +0900

    feat: 자동차 이름 입력받기 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    const user = new User();
+    user.inputPlayersName();
+  }
+}
+
+class User {
+  async inputPlayersName() {
+    const inputPlayersName = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    const players = inputPlayersName.split(',');
+
+    return players;
+  }
 }
 
 export default App;
