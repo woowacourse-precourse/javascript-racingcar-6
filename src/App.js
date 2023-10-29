ce16e84bd5b63c2f709ed65effb94bc075e7edbd
uchaen <u_chaen@naver.com>
2023-10-29 16:12:08 +0900

    랜덤값 생성 후 전진 함수 goForward 구현

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async readCar() {
-    let inputCar = await MissionUtils.Console.readLineAsync(
+    const inputCar = await MissionUtils.Console.readLineAsync(
       `경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)`
     );
 
     // 입력값 검증
-    let temp = inputCar.split(",");
+    const temp = inputCar.split(",");
     for (let i = 0; i < temp.length; i++) {
       if (temp[i].length <= 0) {
         throw new Error("[ERROR] 이름이 없는 자동차가 있습니다.");
@@ -19,26 +19,40 @@ class App {
 
     return temp;
   }
-  async readCount(){
-    let inputCount = await MissionUtils.Console.readLineAsync(
+  async readCount() {
+    const inputCount = await MissionUtils.Console.readLineAsync(
       `시도할 횟수는 몇 회인가요?`
     );
 
+    // 입력값 검증
     if (isNaN(inputCount)) {
       throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
     }
-    if (inputCount <= 0){
+    if (inputCount <= 0) {
       throw new Error("[ERROR] 1 이상인 숫자를 입력해야 합니다.");
     }
-    
-    return inputCount
+
+    return inputCount;
   }
+  async goForward(forwardList) {
+    for (let carIdx = 0; carIdx < forwardList.length; carIdx++) {
+      const number = MissionUtils.Random.pickNumberInRange(0, 9);
+      if (number >= 4) forwardList[carIdx]++;
+    }
+  }
+
   async play() {
-    // const number = MissionUtils.Random.pickNumberInRange(1, 9);
-    let carList = await this.readCar();
-    console.log(carList);
-    let count = await this.readCount();
-    console.log(count);
+    const carList = await this.readCar();
+    // console.log(carList);
+    const count = await this.readCount();
+    // console.log(count);
+    let forwardList = new Array(carList.length).fill(0);
+
+    MissionUtils.Console.print("\n실행 결과");
+    for (let i = 0; i < count; i++) {
+      this.goForward(forwardList);
+      console.log(forwardList);
+    }
   }
 }
 
