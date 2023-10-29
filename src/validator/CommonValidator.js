f7aa3cd428bce7edf1e0c2e9793989513e34c5f6
swarvy <swarvy0826@naver.com>
2023-10-29 13:02:08 +0900

    refactor : CommonValidator.js에 JSDocs 추가

--- src/validator/CommonValidator.js
+++ src/validator/CommonValidator.js
@@ -2,12 +2,25 @@ import { SYMBOLS } from '../constants/Symbols.js';
 import ValidationError from '../error/ValidationError.js';
 
 export default class CommonValidator {
+  /**
+   * @private
+   * @type {string}
+   */
   #input;
 
+  /**
+   * @constructor
+   * @param {string} input
+   */
   constructor(input) {
     this.#input = input.trim();
   }
 
+  /**
+   * @static
+   * @public
+   * @constant
+   */
   static COMMON_VALIDATION_TYPES = Object.freeze({
     emptyInput: Object.freeze({
       message: '입력이 존재하지 않습니다, 다시 입력해주세요.',
@@ -17,6 +30,11 @@ export default class CommonValidator {
     }),
   });
 
+  /**
+   * @public
+   * @throws {ValidationError}
+   * @returns {void}
+   */
   validate() {
     Object.values(CommonValidator.COMMON_VALIDATION_TYPES).forEach(({ message, isValid }) => {
       if (!isValid(this.#input)) {
