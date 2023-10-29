8e41ce11955dd391b4709c091c4f459fd17124e4
PARK-CHANUK <doosan1224@naver.com>
2023-10-29 18:10:20 +0900

    test(validator): 테스트 케이스 추가 - 자동차 이름이 중복되면 에러를 던집니다.

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -11,6 +11,13 @@ describe('Validator Test', () => {
     },
   );
 
+  test('자동차 이름이 중복되면 에러를 던집니다.', () => {
+    const cars = 'pobi,pobi,ukgi';
+    expect(() => {
+      isValidateCarName(cars);
+    }).toThrow('[ERROR]');
+  });
+
   test.each(['', ' ', '1f'])(
     '시도횟수가 숫자가 아니면 에러를 던집니다.',
     (input) => {
