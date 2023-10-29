bfdaec97668d535ef2cd21eaf943e07cdc7c0a6e
Jules <dali1999@naver.com>
2023-10-30 00:10:59 +0900

    feat: App.js 에 게임 실행 메소드 선언

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import GameController from "./controllers/GameController.js";
+import Car from "./models/Car.js";
+import Attempt from "./models/Attempt.js";
+
 class App {
-  async play() {}
+  constructor() {
+    this.controller = new GameController();
+    this.car = new Car();
+    this.attempt = new Attempt();
+  }
+  async play() {
+    this.car.getCarModelsArr(await this.car.getCarModels());
+    await this.attempt.getRaceAttempt();
+
+    let carModels = this.car.carModels;
+    let carModelsArr = this.car.carModelsArr;
+    let attempt = this.attempt.attemptCount;
+
+    this.controller.repeatRace(carModels, attempt);
+    this.controller.printWinner(carModels, carModelsArr);
+  }
 }
+const app = new App();
+app.play();
 
 export default App;
