d62595ab50e27b50fba4e6536b6822252d744cbd
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-10-29 22:20:08 +0900

    refactor: 반복문 변경

--- src/Print/PrintResults.js
+++ src/Print/PrintResults.js
@@ -8,15 +8,15 @@ const PrintResults = (user, gameNumber) => {
   }
 
   for (let index = 0; index < gameNumber; index += 1) {
-    user.forEach((racingCar) => {
+    for (const CAR of user) {
       const RANDOM_NUMBER = MissionUtils.Random.pickNumberInRange(0, 9);
 
       if (RANDOM_NUMBER >= 4) {
-        RACING_CAR.set(racingCar, RACING_CAR.get(racingCar) + '-');
+        RACING_CAR.set(CAR, RACING_CAR.get(CAR) + '-');
       }
 
-      MissionUtils.Console.print(`${racingCar} : ${RACING_CAR.get(racingCar)}`);
-    });
+      MissionUtils.Console.print(`${CAR} : ${RACING_CAR.get(CAR)}`);
+    }
 
     MissionUtils.Console.print('\n');
   }
