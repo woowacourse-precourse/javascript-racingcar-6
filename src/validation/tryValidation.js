e0f1742e166a1c5e3368e422bc3f33fb3442235e
Goongam <dlwjddn341@naver.com>
2023-10-29 21:18:51 +0900

    Fix: 시도횟수 에러메시지 수정

--- src/validation/tryValidation.js
+++ src/validation/tryValidation.js
@@ -1,7 +1,7 @@
 import { ERROR_MESSAGE } from "../constants/constant.js";
 
 export function vaildTryCount(count){
-    if(isNaN(count)) throw new Error(`${ERROR_MESSAGE.NOT_NUMBER}`);
-    if(count <= 0) throw new Error(`${ERROR_MESSAGE.DOWN1_COUNT}`);
+    if(isNaN(count)) throw new Error(`[ERROR] ${ERROR_MESSAGE.NOT_NUMBER}`);
+    if(count <= 0) throw new Error(`[ERROR] ${ERROR_MESSAGE.DOWN1_COUNT}`);
     return count;
 }
