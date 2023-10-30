b6ac335cd2405ff6a3a180318434a767e3958b92
yook-jongho <dbrwhdgh1203@naver.com>
2023-10-31 01:32:39 +0900

    1. 유저 입력 처리(자동차 및 반복횟수)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  constructor() {
+    this.carList = [];
+    this.tryNum = 0;
+  }
+
+  async play() {
+    this.carList = await this.carInput();
+    this.tryNum = await this.numInput();
+  }
+
+  async carInput() {
+    const userInputCar = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+    );
+    return userInputCar;
+  }
+
+  async numInput() {
+    const userInputNum =
+      await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    return userInputNum;
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
