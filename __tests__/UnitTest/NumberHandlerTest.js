19915090ab0ef5291c1317cf7f92fe2dbd2dfb44
lurgi <wahoo9040@naver.com>
2023-10-30 17:33:43 +0900

    refactor(NumberHandlerTest): 메서드 분리

--- __tests__/UnitTest/NumberHandlerTest.js
+++ __tests__/UnitTest/NumberHandlerTest.js
@@ -1,43 +1,61 @@
 import { mockQuestions } from "../../src/utils/testUtils";
 import numberHandler from "../../src/utils/numberHandler";
 
+const ERROR_MESSAGES = {
+  ONLY_NUMBER: "[ERROR] 시도 횟수는 숫자 값만 입력해주세요.",
+  NOT_BLANK: "[ERROR] 시도 횟수를 입력하지 않으셨습니다.",
+};
+
+async function numberHandlerTestFn({
+  answer,
+  message,
+  type = "error",
+  valueExpected,
+}) {
+  mockQuestions(answer);
+  if (type === "error")
+    await expect(numberHandler.readTryNumber()).rejects.toThrow(message);
+  if (type === "pass") {
+    const OUTPUT = await numberHandler.readTryNumber();
+    expect(OUTPUT).toEqual(valueExpected);
+  }
+}
+
 describe("NumberHandler 테스트", () => {
   test("readTryNumber 에러1", async () => {
-    const answer = ["1e"];
-    mockQuestions(answer);
-
-    await expect(numberHandler.readTryNumber()).rejects.toThrow(
-      "[ERROR] 시도 횟수는 숫자 값만 입력해주세요."
-    );
+    await numberHandlerTestFn({
+      answer: ["1e"],
+      message: ERROR_MESSAGES.ONLY_NUMBER,
+    });
   });
+
   test("readTryNumber 에러2", async () => {
-    const answer = ["1234q"];
-    mockQuestions(answer);
-
-    await expect(numberHandler.readTryNumber()).rejects.toThrow(
-      "[ERROR] 시도 횟수는 숫자 값만 입력해주세요."
-    );
+    await numberHandlerTestFn({
+      answer: ["1234q"],
+      message: ERROR_MESSAGES.ONLY_NUMBER,
+    });
   });
+
   test("readTryNumber 에러3", async () => {
-    const answer = [""];
-    mockQuestions(answer);
-
-    await expect(numberHandler.readTryNumber()).rejects.toThrow(
-      "[ERROR] 시도 횟수를 입력하지 않으셨습니다."
-    );
+    await numberHandlerTestFn({
+      answer: [""],
+      message: ERROR_MESSAGES.NOT_BLANK,
+    });
   });
+
   test("readTryNumber 통과", async () => {
-    const answer = ["5"];
-    mockQuestions(answer);
-
-    const output = await numberHandler.readTryNumber();
-    expect(output).toEqual(5);
+    await numberHandlerTestFn({
+      answer: ["5"],
+      type: "pass",
+      valueExpected: 5,
+    });
   });
-  test("readTryNumber 통과2", async () => {
-    const answer = ["99"];
-    mockQuestions(answer);
 
-    const output = await numberHandler.readTryNumber();
-    expect(output).toEqual(99);
+  test("readTryNumber 통과2", async () => {
+    await numberHandlerTestFn({
+      answer: ["99"],
+      type: "pass",
+      valueExpected: 99,
+    });
   });
 });
