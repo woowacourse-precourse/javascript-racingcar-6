e7acf2a3b522e92931e62149e19fb205ced80cbe
lurgi <wahoo9040@naver.com>
2023-10-30 16:54:43 +0900

    refactor(CarHandlerTest): 함수 분리

--- __tests__/UnitTest/CarHandlerTest.js
+++ __tests__/UnitTest/CarHandlerTest.js
@@ -1,31 +1,33 @@
 import { mockQuestions } from "../../src/utils/testUtils";
 import carHandler from "../../src/utils/carHandler";
 
+const ERROR_MESSAGES = {
+  NOT_SPACE: "[ERROR] 자동차 이름의 앞 뒤에는 공백이 있어선 안됩니다.",
+  NOT_OVER_5: "[ERROR] 자동차 이름의 길이는 5를 넘어선 안됩니다.",
+  NOT_DUPLICATION: "[ERROR] 자동차 이름에 중복이 있습니다.",
+};
+
+async function errorCheck(answer, message) {
+  mockQuestions(answer);
+  await expect(carHandler.readCarsInput()).rejects.toThrow(message);
+}
+
 describe("carHandler 테스트", () => {
   test("readCarsInput 공백 에러", async () => {
     const answer = [" car,car34,car1"];
-    mockQuestions(answer);
-
-    await expect(carHandler.readCarsInput()).rejects.toThrow(
-      "[ERROR] 자동차 이름의 앞 뒤에는 공백이 있어선 안됩니다."
-    );
+    await errorCheck(answer, ERROR_MESSAGES.NOT_SPACE);
   });
+
   test("readCarsInput 길이 에러 ", async () => {
     const answer = ["carcar,car34,car1"];
-    mockQuestions(answer);
-
-    await expect(carHandler.readCarsInput()).rejects.toThrow(
-      "[ERROR] 자동차 이름의 길이는 5를 넘어선 안됩니다."
-    );
+    await errorCheck(answer, ERROR_MESSAGES.NOT_OVER_5);
   });
+
   test("readCarsInput 중복 에러", async () => {
     const answer = ["carca,carca,car1"];
-    mockQuestions(answer);
-
-    await expect(carHandler.readCarsInput()).rejects.toThrow(
-      "[ERROR] 자동차 이름에 중복이 있습니다."
-    );
+    await errorCheck(answer, ERROR_MESSAGES.NOT_DUPLICATION);
   });
+
   test("readCarsInput 통과 테스트", async () => {
     const answer = ["car1,car2,lurgi,tisi"];
     mockQuestions(answer);
