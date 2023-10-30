1bbfacb3a1dbec20c13985bce3ac17c0e470eb24
JongInWon <jonginwon1203@gmail.com>
2023-10-30 22:30:27 +0900

    Feat: 함수형 filter 추가
    이터러블을 필터링 하는 함수
    if문 대신 사용하기 위해 추가

--- src/carRacing/utility/utilityFunctions.js
+++ src/carRacing/utility/utilityFunctions.js
@@ -1,3 +1,14 @@
+// if문 대신 사용
+export const filter = (func, iterable) => {
+  let result = [];
+
+  for (const value of iterable) {
+    if (func(value)) ret.push(value);
+  }
+
+  return result;
+};
+
 // 이터러블 값을 다른 값으로 축약해나가는 함수
 export const reduce = (func, accumulate, iterable) => {
   if (!iterable) {
