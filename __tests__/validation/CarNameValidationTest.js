7a77b64fa2ebc3d0e05e4117ab0ab31920c81991
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-10-30 16:09:07 +0900

    feat:Input 테스트, 자동차 이름 유효성 검증 테스트 추가
    * InputTest 추가
       - Input 함수 호출 했을 시, Console.readLineAsync 메서드가 호출되는 지 테스트
    * CarNameValidationTest 추가

---
+++ __tests__/validation/CarNameValidationTest.js
@@ -0,0 +1,17 @@
+import { ERROR_PREFIX } from "../../src/constants";
+import { CarNameValidation } from "../../src/features/car/Validation"
+
+const limitExceededInput = "abcdef";
+
+describe("자동차 이름 입력 유효성 검증",()=>{
+    test("유효성 클래스 인스턴스 유무", ()=>{
+        const validation = new CarNameValidation("");
+        expect(validation).toBeTruthy();
+    })
+
+    test("문자열 길이 5 이하 검증", ()=> {
+        const validation = new CarNameValidation(limitExceededInput);
+        const result = ()=> validation.validate();
+        expect(result).toThrow(ERROR_PREFIX);
+    })
+})
