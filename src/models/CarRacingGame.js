16a05701a4cac4043a1cacfaa29856d8a08538e3
yuziwoo <yuziwoo@naver.com>
2023-10-31 00:46:06 +0900

    docs: 불필요한 주석 수정

--- src/models/CarRacingGame.js
+++ src/models/CarRacingGame.js
@@ -2,7 +2,7 @@ export default class CarRacingGames {
   constructor() {};
 
   /**
-   * 유저에게 입력받은 자동차 이름을 객체를 담은 배열의 형태로 만드는 메서드
+   * 유저에게 입력받은 자동차 이름들을 객체를 담은 배열의 형태로 만드는 메서드
    * @params {string}
    * @return [{
    *  names: {string},
--- src/utils/console/readLineAsync.js
+++ src/utils/console/readLineAsync.js
@@ -1,10 +1,5 @@
 import { Console } from "@woowacourse/mission-utils";
 
-/**
- * 메시지를 출력하고, 사용자의 값을 입력받는다.
- * @params {string} 출력할 메시지
- * @returns {string} 사용자가 입력한 값
- */
 export const readLineAsync = async (message) => {
   const input = await Console.readLineAsync(message);
   return input;
