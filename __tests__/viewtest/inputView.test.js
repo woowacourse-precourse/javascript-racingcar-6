19d0bcce94cf2f254957bcbba61d65d3091e9a01
최나기 <choenagi@choenagiui-MacBookAir.local>
2023-10-30 19:25:54 +0900

    feat(inputView): inputCarName 함수 구현
    사용자로부터 표준입력을 받고 리턴하는 함수

--- __tests__/viewtest/inputView.test.js
+++ __tests__/viewtest/inputView.test.js
@@ -1,14 +1,33 @@
 import InputView from '../../src/view/InputView';
-import Console from '@woowacourse/mission-utils';
+import { MissionUtils } from "@woowacourse/mission-utils";
 
-Console.readLineAsync = jest.fn();
+const mockQuestions = (inputs) => {
+	MissionUtils.Console.readLineAsync = jest.fn();
+  
+	MissionUtils.Console.readLineAsync.mockImplementation(() => {
+	  const input = inputs.shift();
+	  return Promise.resolve(input);
+	});
+  };
 
 describe("inputView inputCarName", () => {
 	test("함수가 존재하는가? ", () => {
 		expect(typeof InputView.inputCarName).toBe("function");
 	})
-	test("Console.readLineAsync", () => {
+	test("Console.readLineAsync는 호출되었는가? ", () => {
+		MissionUtils.Console.readLineAsync = jest.fn();
 		InputView.inputCarName();
-		expect(Console.readLineAsync).toBecalled();
+		expect(MissionUtils.Console.readLineAsync).toHaveBeenCalled();
 	})
-})
+	const testCases = [
+		{ input: "pobi,woni", expected: "pobi,woni" },
+		{ input: "alice,bob", expected: "alice,bob" },
+	];
+	testCases.forEach (({input, expected}) => {
+		test(`inputView inputCarName은 어떤 값를 리턴하는가? '${input}'`, async () => {
+			const inputs = [input];
+			mockQuestions(inputs);
+			expect(await InputView.inputCarName()).toStrictEqual(expected);
+		});
+	});
+});
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,8 +1,10 @@
-import Console from '@woowacourse/mission-utils';
+import { Console } from '@woowacourse/mission-utils';
 
 const InputView =  {
 	async inputCarName(){
+		let carNames = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
+		return (carNames);
 	}
 }
 
