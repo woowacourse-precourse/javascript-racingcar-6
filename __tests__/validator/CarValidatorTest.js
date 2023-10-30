134089398276a3722a45324cf59195001d852e2a
BangDori <qudwns8865@naver.com>
2023-10-30 14:43:33 +0900

    test: 자동차 이름을 검증하는 함수들에 대한 테스트 코드 추가

---
+++ __tests__/validator/CarValidatorTest.js
@@ -0,0 +1,41 @@
+import {
+  CAR_ERROR_MESSAGE,
+  ERROR_PREFIX,
+} from "../../src/constants/error-message";
+import validateRacingCarNames from "../../src/validator/car-validator";
+
+describe("자동차 이름에 문제가 없는 경우", () => {
+  test("자동차 이름에 문제가 없는 경우 에러를 반환하지 않는다.", () => {
+    const racingCarNames = ["pobi", "woony", "jun"];
+
+    expect(() => validateRacingCarNames(racingCarNames)).not.toThrow();
+  });
+});
+
+describe("자동차 이름의 길이가 1보다 작거나 5보다 큰 경우", () => {
+  test("이름의 길이가 1보다 작은 자동차가 있다면 에러를 반환한다.", () => {
+    const racingCarNames = ["pobi", "woony", ""];
+
+    expect(() => validateRacingCarNames(racingCarNames)).toThrow(
+      ERROR_PREFIX + CAR_ERROR_MESSAGE.LENGTH
+    );
+  });
+
+  test("이름의 길이가 5보다 큰 자동차가 있다면 에러를 반환한다.", () => {
+    const racingCarNames = ["pobi", "woony", "abcdef"];
+
+    expect(() => validateRacingCarNames(racingCarNames)).toThrow(
+      ERROR_PREFIX + CAR_ERROR_MESSAGE.LENGTH
+    );
+  });
+});
+
+describe("중복된 자동차 이름이 존재하는 경우", () => {
+  test("중복된 자동차 이름이 존재한다면 에러를 반환한다.", () => {
+    const racingCarNames = ["pobi", "woony", "pobi"];
+
+    expect(() => validateRacingCarNames(racingCarNames)).toThrow(
+      ERROR_PREFIX + CAR_ERROR_MESSAGE.DUPLICATION
+    );
+  });
+});
