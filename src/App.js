569da583ffd6807a4354a52f8be56cef89144b12
aaapark <parksc1534.dev@gmail.com>
2023-10-30 18:45:32 +0900

    자동차 이릅 입력 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import User from './UserInput.js'
+
 class App {
-  async play() {}
+  async play() {
+    MissionUtils.Console.print('자동차 경주 게임을 시작합니다.');
+    const user = new User
+    user.userInputCarName()
+  }
 }
 
 export default App;
