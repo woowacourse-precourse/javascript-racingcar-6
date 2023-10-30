59fd0c1bbffa350817c339239d21e8d6b673bbb9
fecapark <fecapark@gmail.com>
2023-10-30 18:58:05 +0900

    test: 에러 검증을 위한 테스트 코드 세분화

--- __tests__/ValidityTest.js
+++ __tests__/ValidityTest.js
@@ -1,50 +1,70 @@
+import { InvalidPlayerNameError, TryAmountError } from "../src/utils/error";
 import { assertNameValid, assertTryAmountValid } from "../src/utils/validity";
 
 describe("검증 함수 테스트", () => {
   test("이름 검증", () => {
-    const pass = ["aA", "123", "a", "a1", "abcde", " a2z1V "];
-    const fail = [
-      "ab cd",
-      "_a_",
-      "a!",
-      "1!",
-      "!@#",
-      "-_-",
-      " a a ",
-      "123456",
-      "",
-    ];
+    const inputs = ["aA", "123", "a", "a1", "abcde", " a2z1V "];
 
-    pass.forEach((name) => {
+    inputs.forEach((name) => {
       assertNameValid(name);
     });
+  });
 
-    fail.forEach((name) => {
-      expect(assertNameValid(name)).rejects.toThrow("[ERROR]");
+  test("이름 검증 - 5자 초과의 길이", () => {
+    const inputs = ["123456", "1212321321asasdsad"];
+
+    inputs.forEach((name) => {
+      expect(assertNameValid(name)).rejects.toThrowError(
+        new InvalidPlayerNameError(InvalidPlayerNameError.TYPE_LENGTH)
+      );
+    });
+  });
+
+  test("이름 검증 - 문자 사이에 공백 포함", () => {
+    const inputs = ["ab cd", " a  a"];
+
+    inputs.forEach((name) => {
+      expect(assertNameValid(name)).rejects.toThrowError(
+        new InvalidPlayerNameError(InvalidPlayerNameError.TYPE_SPACE_BETWEEN)
+      );
+    });
+  });
+
+  test("이름 검증 - 특수문자 포함", () => {
+    const inputs = ["_a_", "a!", "1!", "!@#", "-_-"];
+
+    inputs.forEach((name) => {
+      expect(assertNameValid(name)).rejects.toThrowError(
+        new InvalidPlayerNameError(InvalidPlayerNameError.TYPE_CONTAINS_SPECIAL)
+      );
     });
   });
 
   test("횟수 검증", () => {
-    const pass = [1, 3, 5, 100000];
-    const fail = [
-      -1,
-      -1.1,
-      -0.123456,
-      0,
-      1.1,
-      0.123456,
-      Infinity,
-      -Infinity,
-      NaN,
-      -NaN,
-    ];
+    const inputs = [1, 3, 5, 100000];
 
-    pass.forEach((amount) => {
+    inputs.forEach((amount) => {
       assertTryAmountValid(amount);
     });
+  });
 
-    fail.forEach((amount) => {
-      expect(assertNameValid(amount)).rejects.toThrow("[ERROR]");
+  test("횟수 검증 - 실수 입력", () => {
+    const inputs = [-1.1, -0.123456, 1.1, 0.123456, Infinity, -Infinity, NaN];
+
+    inputs.forEach((amount) => {
+      expect(assertNameValid(amount)).rejects.toThrowError(
+        new TryAmountError(TryAmountError.TYPE_NOT_INTEGER)
+      );
+    });
+  });
+
+  test("횟수 검증 - 1 미만 입력", () => {
+    const inputs = [0, -1];
+
+    inputs.forEach((amount) => {
+      expect(assertNameValid(amount)).rejects.toThrowError(
+        new TryAmountError(TryAmountError.TYPE_NOT_INTEGER)
+      );
     });
   });
 });
