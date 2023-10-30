d6305b6ac3f854299bbeb174a3c3cccb69330a7d
Goonco <s07019@naver.com>
2023-10-30 22:38:47 +0900

    :art: 게임 내 출력되는 메세지 & 에러 메세지 상수화

---
+++ src/Constants.js
@@ -0,0 +1,11 @@
+export const InGameMessages = {
+  PRINT_INPUT_CARS:
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+  PRINT_INPUT_ATTEMPT: '시도할 횟수는 몇 회인가요?\n',
+  PRINT_RESULT: '실행 결과\n',
+  PRINT_WINNER: '최종 우승자 : ',
+};
+
+export const ErrorMessages = {
+  PRINT_TOO_LONG_NAME: '[ERROR] 입력된 이름이 5자 이상입니다.',
+};
