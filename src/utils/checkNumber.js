9de306b0d44683165a0e65f36a569f99de60df08
pjc0179 <pjc0179@gmail.com>
2023-10-30 21:17:03 +0900

    feat(utils):checkNumber.js 메서드 추가
    -입력된 값 두개에 대하여 좌항의 값이 우항의 값보다 큰지를 판단하는 checkGreaterThan 메서드 구현

--- src/utils/checkNumber.js
+++ src/utils/checkNumber.js
@@ -2,5 +2,8 @@ const checkNumber = {
   checkNumberType(value) {
     return !Number.isNaN(parseInt(value, 10));
   },
+  checkGreaterThan(number1, number2) {
+    return number1 > number2;
+  },
 };
 export default checkNumber;
