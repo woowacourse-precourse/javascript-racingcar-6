d70394355e607f5ec67eb59ef91b7b310b3b44dd
yeokyoung song <spolice0324@gmail.com>
2023-10-31 00:13:07 +0900

    feat : 레이스 프로세스를 관리하는 RaceControllter 구현
    - 인풋으로 자동차 이름 목록을 받는 insertCarNames 추가
    - 인풋으로 시도 횟수를 받아 경기를 시도횟수만큼 진행하여 출력하는 insertTryCount 추가

--- src/controller/RaceController.js
+++ src/controller/RaceController.js
@@ -1,4 +1,6 @@
+import { Console } from '@woowacourse/mission-utils';
 import CarRace from '../model/CarRace';
+import InputView from '../view/InputView';
 
 class RaceController {
   constructor() {
@@ -9,7 +11,34 @@ class RaceController {
     this.insertCarNames();
   }
 
-  insertCarNames() {}
+  insertCarNames() {
+    InputView.inputCarNames(carNames => {
+      try {
+        this.carRace.makeCarList(carNames);
+        this.insertTryCount();
+      } catch (error) {
+        Console.print(error);
+        this.insertCarNames();
+      }
+    });
+  }
+
+  insertTryCount() {
+    InputView.inputTryCount(tryCount => {
+      try {
+        this.carRace.setTryCount(tryCount);
+        const result = this.carRace
+          .makeCarList()
+          .getRace()
+          .getWinners()
+          .getResult();
+        Console.print(result);
+      } catch (error) {
+        Console.print(error);
+        this.insertTryCount();
+      }
+    });
+  }
 }
 
 export default RaceController;
