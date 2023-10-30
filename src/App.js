e7dad47708a79ec6065210f3542420cc115da98a
23YUJIN <asdbnm066@daum.net>
2023-10-31 06:12:02 +0900

    feat: 실행하고 실행 결과 출력하기 구현

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Console } from "@woowacourse/mission-utils";
 import Game from "./Game.js";
 
 class App {
@@ -5,6 +6,23 @@ class App {
     const game = new Game();
     await game.inputCarName();
     await game.inputAttemptNumber();
+    // 시도할 횟수만큼 시도
+    Console.print("실행 결과");
+    for (let i = 0; i < game.attemptNumber; i++) {
+      for (const car of game.carList) {
+        car.calculatePosition();
+        this.printResultMessage(car);
+      }
+      Console.print(""); // 개행해줌
+    }
+  }
+
+  printResultMessage(car) {
+    let resultMessage = car.name + " : ";
+    for (let i = 0; i < car.position; i++) {
+      resultMessage += "-";
+    }
+    Console.print(resultMessage);
   }
 }
 
