3fbfc00ccb6a3fd848b5b2d8aa25b8b9920c58e4
Han <mayo3610@naver.com>
2023-10-30 16:32:26 +0900

    feat #1 : 사용자에게 자동차 이름 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console } from "@woowacourse/mission-utils";
 class App {
-  async play() {}
+  constructor() {
+    this.name = "";
+  }
+  async readUserRacingCars() {
+    await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    ).then((input) => (this.name = input));
+  }
+  async play() {
+    await this.readUserRacingCars();
+    console.log(this.name);
+  }
 }
+const app = new App();
+app.play();
 
 export default App;
