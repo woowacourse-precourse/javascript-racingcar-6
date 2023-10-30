bde78d651e965f361593eefb7caaf0cc3fa8a35c
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-10-31 03:51:21 +0900

    ✨ feat : checkNumber 함수를 추가한다.

--- src/util/check.js
+++ src/util/check.js
@@ -4,10 +4,16 @@ import RACING from "../constants/message"
  * 
  * @param {*} nameArray 5글자 이하인 이름들의 배열을 받아옵니다. 
  */
-const checkName = async (nameArray) => {
+export const checkName = async (nameArray) => {
     nameArray.forEach(name => {
         if (name.length > 5 || name.trim() === 0) throw new Error(RACING.NAMING_ERROR)
     })
 }
 
-export default checkName
+/**
+ * 
+ * @param {*} string 전달받은 값이 숫자인지 확인합니다.
+ */
+export const checkNumber = async (number) => {
+    if (isNaN(number)) throw new Error(RACING.NUMBER_ERROR)
+}
