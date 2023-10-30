c249c61f8d14a30fdb8cfa7d3aebad5ceab0da25
kimjinwoo <rlawlsdn177@naver.com>
2023-10-30 16:08:43 +0900

    feat($play): 사용자에게 자동차 이름 입력받기 구현
    Define.js에 입력시 출력될 안내 메세지를 정의한 후 자동차 이름 입력받기 구현

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import { Console } from '@woowacourse/mission-utils';
 import { CAR_NAME_INPUT_MESSAGE } from './Define';
 class App {
     async play() {
-        const CAR_NAMES_INPUT = await this.getCarNames();
-    }
-
-    async getCarNames() {
-        const CAR_NAME_INPUT = await Console.readLineAsync(CAR_NAME_INPUT_MESSAGE);
-        return CAR_NAME_INPUT;
+        const CAR_NAMES_INPUT = await Console.readLineAsync(CAR_NAME_INPUT_MESSAGE);
     }
 }
 
---
+++ src/Define.js
@@ -0,0 +1,1 @@
+export const CAR_NAME_INPUT_MESSAGE = '경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분): ';
