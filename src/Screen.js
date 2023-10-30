8e40d99c5998734b4cb4721fc817def41676afc3
Seoan Yoon <earthdyoon@gmail.com>
2023-10-30 22:16:06 +0900

    feat: 시도할 횟수 입력에 대한 예외 처리 구현

--- src/Screen.js
+++ src/Screen.js
@@ -7,6 +7,7 @@ class Screen {
     RACE_COUNT_INPUT_MESSAGE: '시도할 횟수는 몇 회인가요?',
     INVALID_NAME_ERROR_MESSAGE:
       '[ERROR] 자동차 이름은 5자를 초과할 수 없습니다.',
+    INVALID_COUNT_ERROR_MESSAGE: '[ERROR] 숫자가 잘못된 형식입니다.',
     DUPLICATED_NAME_ERROR_MESSAGE: '[ERROR] 중복된 이름을 입력할 수 없습니다.',
   };
 
@@ -30,7 +31,11 @@ class Screen {
 
   static async inputCount() {
     Console.print(this.MESSAGES.RACE_COUNT_INPUT_MESSAGE);
-    const count = await Console.readLineAsync();
+    const count = Number(await Console.readLineAsync());
+
+    if (!Number.isInteger(count) || count <= 0) {
+      throw new Error(this.MESSAGES.INVALID_COUNT_ERROR_MESSAGE);
+    }
 
     return count;
   }
