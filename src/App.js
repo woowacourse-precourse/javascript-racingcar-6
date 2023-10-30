ac839bd2aa00e1667925d9c3f0ac1bf00e138a3f
Lim-shweety <smile70200@naver.com>
2023-10-30 21:16:16 +0900

    :sparkles: feat: 시도할 횟수 입력받기 시도할 횟수 입력받고 입력에 대한 예외처리 자동차 이름 예외처리

--- src/App.js
+++ src/App.js
@@ -4,10 +4,26 @@ class App {
 
     async play() {
 
-        const car_name_input = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
-        const CARS = car_name_input.split(",");
+        const CAR_NAME_INPUT = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+        const CARS = CAR_NAME_INPUT.split(",");
         const CAR_CNT = CARS.length;
+
+        //자동차 이름이 5글자 이하인지 확인
+        for (let i = 0; i < CAR_CNT; i++) {
+            if (CARS[i].length > 5) {
+                throw("[ERROR] 자동차 이름이 5글자 이상입니다.");
+            }
+        }
+
         let cnt_cars_go = [...new Array(CAR_CNT)].map((_, i) => 0);
+        let try_cnt_input = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+
+        //숫자 형식 확인
+        if (Number.isInteger(Number(try_cnt_input)) === false) {
+            throw("[ERROR] 숫자가 잘못된 형식입니다.");
+        }
+        const TRY_CNT = Number(try_cnt_input);
+
 
     }
 }
