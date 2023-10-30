ef1fcb2af7fb2a40041d5ef2177e24e9e7fc0a15
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-31 01:11:49 +0900

    feat: [Controller] 입력 받은 자동차 이름 목록이 유효한지 검증 구현

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,8 +1,11 @@
+import Validation from '../utils/Validation.js';
 import InputView from '../view/InputView.js';
 
 export default class Controller {
   async run() {
     const carNames = await InputView.carName();
     const carNameList = carNames.split(',');
+
+    Validation.carNameInput(carNameList);
   }
 }
