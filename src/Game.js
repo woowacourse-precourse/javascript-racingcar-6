565d88350c333a1cf9096ed0db382cf2c0d0b406
donggyun <donggyunkim12@gmail.com>
2023-10-30 16:27:32 +0900

    add(Game): 입력 유효성 검사를 위한 return 추가

--- src/Game.js
+++ src/Game.js
@@ -13,6 +13,8 @@ class Game {
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
     );
     this.#carListArr = carListInput.split(',');
+
+    return this.#carListArr;
   }
 
   async createCars() {
@@ -23,6 +25,7 @@ class Game {
 
   async moveQuestion() {
     this.#tryMove = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    return this.#tryMove;
   }
 
   async process() {
