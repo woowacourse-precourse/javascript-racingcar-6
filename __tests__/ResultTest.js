0f7d36e0dc447dbaa7faf094d4f6772b76016359
Nahyun <from.owol@gmail.com>
2023-10-30 16:21:56 +0900

    test: 기존 테스트 코드 수정

--- __tests__/ResultTest.js
+++ __tests__/ResultTest.js
@@ -2,13 +2,12 @@ import GameResult from '../src/model/GameResult.js';
 
 describe('GameResult', () => {
   test('1회차 자동차 전진 거리 Map 반환', () => {
-    const prevLog = new Map();
     const carPosition = new Map();
 
     carPosition.set('a', 3);
     carPosition.set('b', 0);
 
-    const carLog = new GameResult().getForwardResult(carPosition, prevLog);
+    const carLog = new GameResult().getForwardResult(carPosition);
 
     // 'a' 자동차의 전진 거리 = 로그 1 증가
     expect(carLog.get('a')).toBe(1);
