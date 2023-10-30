1de87c13b484bce8dbb98e5969151012e71708cf
kimjinwoo <rlawlsdn177@naver.com>
2023-10-30 16:17:43 +0900

    feat($play): CREATE_CARS 메서드를 통한 자동차 객체 할당
    자동차를 생성해서 play 메서드 내 할당

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Console } from '@woowacourse/mission-utils';
 import { CAR_NAME_INPUT_MESSAGE } from './Define';
-import { VALIDATE_CAR_NAME } from './Race';
+import {
+    VALIDATE_CAR_NAME,
+    CREATE_CARS,
+} from './Race';
 
 class App {
     async play() {
         const CAR_NAMES_INPUT = await this.getCarNames();
+        const CARS = CREATE_CARS(CAR_NAMES_INPUT);
     }
 
     async getCarNames() {
