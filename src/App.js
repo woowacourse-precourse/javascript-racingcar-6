d85a51fc3a107f779a3022d2e26f1b9d760ae9a5
dev-junseo <80705329+dev-junseo@users.noreply.github.com>
2023-10-30 23:43:45 +0900

    자동차 이름 input 초안

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+const { Random, Console } = require('@woowacourse/mission-utils');
+
 class App {
-  async play() {}
+  async play() {
+
+    async function getCarName() {
+      let carNameArray = [];
+      let carNameString = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+      carNameArray = this.isValidName(carNameString);
+    }
+
+    function isValidName(userInput) {
+      if (userInput.includes(" ")) {
+        throw new Error("[ERROR] 띄워쓰기를 하지 마세요.");
+      } else if (userInput.length > 6) {
+        throw new Error("[ERROR] 5글자 이하로 적어주세요.");
+      } 
+
+      return userInput.split(",");
+    }
+
+    console.log(getCarName());
+  }
 }
 
 export default App;
