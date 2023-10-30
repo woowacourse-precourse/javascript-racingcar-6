6afce4b609299e5b16caac293250a50dec714636
abi-hong <flffjakfwm20@gmail.com>
2023-10-30 13:29:53 +0900

    feat(Validator): null 검증 구현

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -1,23 +1,21 @@
 const Validator = {
-  // 자동차 이름은 쉼표(,)를 기준으로 구분하며 
-
-  // 이름은 5자 이하만 가능하다.
-  checkNameLength(nameList) {
-    nameList.map((name) => {
+  checkNull(input) {
+    if (!input) throw new Error('[ERROR] 값을 입력해주세요.');
+  },
+  checkNameLength(input) {
+    input.map((name) => {
       if (name.length > 6) {
         throw new Error('[ERROR] 각 입력값은 5자 이하만 가능합니다.');
       }
     })
   },
-  checkCount(count) {
-    count.split('').forEach((char) => {
+  checkCount(input) {
+    input.split('').forEach((char) => {
       if (!'0123456789'.includes(char)) {
         throw new Error('[ERROR] 입력값이 정수가 아닙니다.');
       }
     });
-  }
-
-
+  },
 }
 
 export { Validator };
