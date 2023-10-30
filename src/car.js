65659cb605ef89df8e2a7c64b4a7bd25148d272b
meunrud <113085963+meunrud@users.noreply.github.com>
2023-10-30 17:13:55 +0900

    feat : Car클래스 생성 및 랜덤 숫자 생성 기능 구현 Car : 자동차의 이름과 위치를 저장함. generateRandomNumbers() : 0-9 사이 랜덤 숫자 생성

---
+++ src/car.js
@@ -0,0 +1,12 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+class Car {
+  constructor(name) {
+    this.name = name;
+    this.position = 0;
+  }
+
+  generateRandomNumbers() {
+    return MissionUtils.Random.pickNumberInRange(0, 9);
+  }
+}
