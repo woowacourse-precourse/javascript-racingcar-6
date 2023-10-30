11dd6f3338dcc57f0ecd2e58bb842c27c29d997c
JaeJunday <109643814+JaeJunday@users.noreply.github.com>
2023-10-30 23:19:16 +0900

    feat: addCars() 함수
    - 자동차이름 입력을 받아서 cars배열을 {name, score} 객체의 배열 형태로 저장하는 함수

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import { MissionUtils, Console, Random } from "@woowacourse/mission-utils";
 class App {
   constructor() {
     this.cars = [];
-    this.score = [];
     this.gameCount = 0;
+  }
+
+  addCars(carNames) {
+    this.cars = carNames.split(",");
+    this.cars = this.cars.map((name) => ({ name, score: 0 }));
   }
 
   checkNumber(input) {
@@ -19,6 +23,11 @@ class App {
 
   async play() {
     try {
+      const inputCarNames = await Console.readLineAsync(
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+      );
+      this.addCars(inputCarNames);
+
       const inputNumber = await Console.readLineAsync(
         "시도할 횟수는 몇 회인가요?\n"
       );
