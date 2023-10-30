39a4256e8e7312c4f4b3b8e0703e05d2b80b7069
kimsuyeon0916 <kimsuyeon_09162naver.com>
2023-10-30 20:06:07 +0900

    fix: printWinner 출력 함수 수정

--- src/utils/CarRacing.js
+++ src/utils/CarRacing.js
@@ -1,4 +1,5 @@
 import { Console } from "@woowacourse/mission-utils";
+import { IN_GAME_MESSAGE } from "./Constants.js";
 import Car from "./Car.js";
 import User from "./User.js";
 
@@ -6,22 +7,21 @@ const CarRacing = {
   async playCarRacing() {
     const carNameList = await User.getCarNames();
     const tryCount = await User.getTryCount();
-
-    const carMoveForwardList = Car.getMoveForwards(carNameList);
+    let carMoveForwardList = Car.getMoveForwards(carNameList);
 
     for (let count = 0; count < tryCount; count += 1) {
       const carMovesList = Car.createCarMoves(carNameList.length);
-
-      this.showTryResult(carNameList, carMovesList);
-      Car.countMoveForward(carMoveForwardList, carMovesList);
-      const winners = this.decideWinner(carNameList,carMoveForwardList);
-      this.printWinner(winners);
+      carMoveForwardList = Car.countMoveForward(carMoveForwardList, carMovesList);
+      this.showMoveResult(carNameList, carMoveForwardList);
     }
+
+    const winners = this.decideWinner(carNameList, carMoveForwardList);
+    this.printWinner(winners);
   },
 
-  showTryResult(carNames, carMoves) {
-    Console.print('\n');
-    const moveSign = carMoves.map((moves) => '-'.repeat(moves));
+  showMoveResult(carNames, carMoves) {
+    Console.print("\n");
+    const moveSign = carMoves.map((moves) => "-".repeat(moves));
     carNames.forEach((name, index) => {
       Console.print(`${name} : ${moveSign[index]}`);
     });
@@ -42,7 +42,7 @@ const CarRacing = {
 
   printWinner(winners) {
     const finalWinners = winners.map((winner) => winner).join(",");
-    return finalWinners;
+    Console.print(`${IN_GAME_MESSAGE.finalWinner} ${finalWinners}`);
   },
 };
 
--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -2,7 +2,7 @@ const IN_GAME_MESSAGE = {
   getCarName: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
   getTryCount: '시도할 횟수는 몇 회인가요?\n',
   tryResult: '\n실행 결과\n',
-  finalWinner: '최종 우승자 : ',
+  finalWinner: '최종 우승자 :',
 }
 
 const FORWARD_CRITERIA = 4;
