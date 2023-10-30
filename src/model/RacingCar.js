c5f1a402efad1bd897f08573551aceeb61c0424e
kang <kangju2000@naver.com>
2023-10-30 21:48:59 +0900

    :white_check_mark: RacingCar 테스트 코드 추가

---
+++ src/model/RacingCar.js
@@ -0,0 +1,14 @@
+class RacingCar {
+  constructor(name) {
+    this.name = name
+    this.distance = 0
+  }
+
+  move() {}
+
+  getDistance() {
+    return this.distance
+  }
+}
+
+export default RacingCar
