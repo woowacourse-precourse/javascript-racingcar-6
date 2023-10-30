85292650ad5728be0cd4f6e88de5d3807e667c50
00kang <kdb070970@gmail.com>
2023-10-30 23:29:24 +0900

    feat: 자동차 이름 입력 유효성 검사 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,17 +2,35 @@ import { Console } from '@woowacourse/mission-utils';
 
 class App {
   async play() {
-    this.getCarName();
+    try {
+      const carArr = await this.getCarName();
+      Console.print(carArr);
+    } catch (error) {
+      console.error(error.message);
+    }
   }
 
   async getCarName() {
-    const userInput = await Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) : ',
-    );
-    const carArr = userInput.trim().split(',');
+    try {
+      const userInput = await Console.readLineAsync(
+        '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) : ',
+      );
+      const carArr = userInput.split(',').map(carName => carName.trim());
+      this.validateGetCarName(carArr);
+      return carArr;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
 
-    for (const carName of carArr) {
-      Console.print(carName);
+  validateGetCarName(carArr) {
+    if (carArr.length === 0 || carArr.some(carName => carName.length === 0)) {
+      throw new Error(
+        '[ERROR] 자동차 이름이 입력되지 않았거나 빈 공백이 포함되어 있습니다.',
+      );
+    }
+    if (carArr.some(carName => carName.length > 5)) {
+      throw new Error('[ERROR] 자동차 이름이 5글자를 초과했습니다.');
     }
   }
 }
