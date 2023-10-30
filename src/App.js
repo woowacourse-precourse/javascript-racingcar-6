540ee3e1b1839df81edfa846e28e92bae52849b1
양진성 <anfakt0606@naver.com>
2023-10-30 16:44:55 +0900

    feat : Add a function for user to input car names

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { Console , Random } from '@woowacourse/mission-utils';
+import { USER_INPUT , MESSAGE, ERROR_MESSAGE} from './constants.js';
+
 class App {
-  async play() {}
+  constructor(){
+    this.carName = [];
+  }
+
+  async play() {
+    await this.getCarName();
+  }
+
+  async getCarName() {
+    const input = await Console.readLineAsync(USER_INPUT.CAR_NAME);
+    
+    this.validateCarName(input);
+    this.carName = input.split(',');
+  }
+
+  validateCarName(input){
+    input = input.split(',');
+
+    input.forEach(carName => {
+      this.checkCarName(carName);
+    })
+  }
+
+  checkCarName(carName){
+    if(carName.length > 5) throw new Error(ERROR_MESSAGE.INPUT_CAR_NAME_ERROR);
+  }
 }
 
 export default App;
