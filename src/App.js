9e483f3e7209a9552d726da3f779228efe9c8d99
KimRosa <108081283+KimRosa@users.noreply.github.com>
2023-10-30 23:48:32 +0900

    Feat: checkNameOfLength
    레이서의 이름을 입력받는 콘솔을 작성한다.
    ","을 기준으로 구분하여 조건에 맞게 이름을 구분하는 checkNameOfLength을 작성한다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  constructor() {
+    this.gameStatus = true;
+  }
+  async play() {
+    while (this.gameStatus) {
+      // 1. 이름 입력하세요 콘솔
+      const nameOfCars = await MissionUtils.Console.readLineAsync(
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+      );
+      let cars = this.checkNameOfCarsLength(nameOfCars);
+    }
+  }
+  // + 이름 5자 이하. 쉼표로 구분.
+  checkNameOfCarsLength(nameOfCars) {
+    const cars = nameOfCars.split(",");
+    for (let i = 0; i < cars.length; i++) {
+      const carName = cars[i].trim();
+      if (carName.length > 5) {
+        throw Error("[ERROR] 이름은 다섯자 이하만 가능합니다.");
+      }
+    }
+    return cars;
+  }
 }
 
 export default App;
