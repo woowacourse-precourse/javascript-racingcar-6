4f6eb77efffc5712fa2f255173fc0db4987a083f
unknown <pih9134@naver.com>
2023-10-31 02:52:38 +0900

    feat: 자동차 이름 입력 및 입력 조건

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    const cars = await this.returnCarNames();
+    Console.print(cars);
+  }
+
+  async returnCarNames() {
+    const carNames = await Console.readLineAsync('자동차 이름을 입력하세요. (이름은 쉼표(,)로 구분)\n');
+
+    return this.validName(carNames);
+  }
+
+  validName(carNames) {
+    const names = carNames.split(',').map((name) => name.trim());
+    this.isWrongName(names);
+    this.isDuplicateName(names);
+
+    return names;
+  }
+
+  isWrongName(names) {
+    if (names.some((name) => !(name.length > 0 && name.length <= 5))) {
+      throw new Error('[ERROR] 잘못된 입력입니다.');
+    }
+  }
+
+  isDuplicateName(names) {
+    if (new Set(names).size !== names.length) {
+      throw new Error('[ERROR] 중복된 이름입니다.');
+    }
+  }
 }
 
 export default App;
