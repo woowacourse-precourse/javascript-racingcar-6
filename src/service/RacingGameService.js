b64f3710822a5b94fc83ed5a3551244349114f09
손진영 <sm099@naver.com>
2023-10-30 18:40:16 +0900

    style(RacingGameService): lastRacingStatus 변수를 추가한다.
    racingResult.at(-1)을 명확하게 만들기 위해 lastRacingStatus라는 새로운 변수를 추가하여 할당한다.

--- src/service/RacingGameService.js
+++ src/service/RacingGameService.js
@@ -3,8 +3,8 @@ import { RacingGame, RacingWinnerCalculator } from '../models/index.js';
 const RacingGameService = {
   calculateRacingCarGameResult(carNames, moveCount) {
     const racingResult = RacingGame.of(carNames, moveCount).play();
-    // TODO: racingResult.at(-1) 네이밍 고민해보기
-    const racingWinners = RacingWinnerCalculator.from(racingResult.at(-1)).calculateRacingWinners();
+    const lastRacingStatus = racingResult.at(-1);
+    const racingWinners = RacingWinnerCalculator.from(lastRacingStatus).calculateRacingWinners();
     return { racingResult, racingWinners };
   },
 };
