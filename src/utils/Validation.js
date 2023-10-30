23ef5ac7e91b78a35b7090d514bcbd41805ee4a6
kimsuyeon0916 <kimsuyeon_09162naver.com>
2023-10-30 21:43:28 +0900

    feat: 아무 입력도 안하고 엔터 예외 처리

--- src/utils/Validation.js
+++ src/utils/Validation.js
@@ -1,4 +1,13 @@
-class Validation{
+import { CHARACTER_LIMIT, NAME_INPUT_ERROR } from "./Constants.js";
+
+const Validation = {
+
+  isNull(nameList){
+    if(nameList.length === 1 && nameList[0] === ''){
+      throw new Error(NAME_INPUT_ERROR.null);
+    }
+  },
+
 
 }
 export default Validation;
