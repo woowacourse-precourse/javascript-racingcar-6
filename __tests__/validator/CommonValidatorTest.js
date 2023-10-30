3614c67b956b1c05de9157e13e60d9cecbd82097
BangDori <qudwns8865@naver.com>
2023-10-30 14:42:07 +0900

    test: 공백 체크 함수에 대한 테스트 코드 추가

---
+++ __tests__/validator/CommonValidatorTest.js
@@ -0,0 +1,25 @@
+import {
+  ERROR_PREFIX,
+  SPACE_ERROR_MESSAGE,
+} from "../../src/constants/error-message";
+import checkSpace from "../../src/validator/common-validator";
+
+describe("공통으로 사용되는 공백 체크 검증 함수를 확인", () => {
+  test("공백이 포함되지 않은 문자열이 입력되는 경우 에러를 반환하지 않는다.", () => {
+    const inputStrings = ["pobi", "jun", "kor"];
+
+    inputStrings.forEach((inputString) =>
+      expect(() => checkSpace(inputString)).not.toThrow()
+    );
+  });
+
+  test("공백이 포함된 문자열이 입력되는 경우 에러를 반환한다.", () => {
+    const inputStrings = [" ", " pobi", "pobi ", "po bi", " pobi ", " po bi "];
+
+    inputStrings.forEach((inputString) =>
+      expect(() => checkSpace(inputString)).toThrow(
+        ERROR_PREFIX + SPACE_ERROR_MESSAGE
+      )
+    );
+  });
+});
