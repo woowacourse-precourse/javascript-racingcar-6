cfed381ab5b573420505a3f9bb5cb70637a3b6d5
aaapark <parksc1534.dev@gmail.com>
2023-10-30 19:04:26 +0900

    자동차 이름 입력값 받는 객체 만들기

--- src/UserInput.js
+++ src/UserInput.js
@@ -1,16 +1,20 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import Save from './ValueSave.js'
 
 export default class User {
     async userInputCarName() {
         const inputCarName = await MissionUtils.Console.readLineAsync(
             '경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분) : \n'
-        )
-        this.getCarNames(inputCarName)
+        );
+        this.getCarNames(inputCarName);
     }
 
     getCarNames(userInput) {
-        this.cars = []
-        let names = userInput.split(',')
-        console.log(names)
+        this.cars = [];
+        let names = userInput.split(',');
+        names.map((element) => {
+            this.cars.push(new Save(element))
+        });
+        console.log(this.cars)
     }
 }
---
+++ src/ValueSave.js
@@ -0,0 +1,5 @@
+export default class Save {
+    constructor(name) {
+        this.name = name
+    }
+}
