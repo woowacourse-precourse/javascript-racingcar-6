79936c2e15d7dd26f113275b760b0f3b47047158
seok-pyo <hongseokpyou@gmail.com>
2023-10-30 17:06:12 +0900

    fix: printResult 로직 수정

--- src/game/printResult.js
+++ src/game/printResult.js
@@ -6,8 +6,9 @@ const printResult = (finalRecord, lapLength) => {
   // 우승자 판단 로직
   finalRecord.forEach((record) => {
     const countLength = record.lastIndexOf('-') - record.indexOf('-') + 1;
-    const carName = record.split(' ');
-    if (countLength === lapLength) winner.push(carName[0]);
+    // const carName = record.split(' ');
+    const carSplitIndex = record.indexOf(':') - 1;
+    if (countLength === lapLength) winner.push(record.substring(0, carSplitIndex));
   });
   if (winner.length === 0) computer.tell(MESSAGE.NO_WINNER);
   else computer.tell(`${MESSAGE.FINAL_WINNER + winner.join(', ')}`);
