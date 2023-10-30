e4346b4dc08faaf18639c02aad2ba228ba5ae438
Doeunnkimm <doeunnkimm@gmail.com>
2023-10-31 01:24:06 +0900

    refactor: 자동차 이름 입력 유효성 검사 테스트 - describe로 테스트 블록화

--- __tests__/validationTest.js
+++ __tests__/validationTest.js
@@ -2,24 +2,26 @@ import racingGameValidationMethods from '../src/game/game.error';
 
 const { validDuplication, validLength } = racingGameValidationMethods;
 
-test('validLength 메서드 : min, max 범위에 포함되는 길이의 문자열인지 검사', () => {
-  const MIN_LENGTH = 1;
-  const MAX_LENGTH = 5;
-  const passInput = ['hello', 'a', 'abc'];
-  const nonPassInput = ['', 'abcdef'];
+describe('자동차 이름 입력 유효성 검사', () => {
+  test('validLength 메서드 : min, max 범위에 포함되는 길이의 문자열인지 검사', () => {
+    const MIN_LENGTH = 1;
+    const MAX_LENGTH = 5;
+    const passInput = ['hello', 'a', 'abc'];
+    const nonPassInput = ['', 'abcdef'];
 
-  passInput.forEach((text) =>
-    expect(validLength(text, MIN_LENGTH, MAX_LENGTH)).toBe(true),
-  );
-  nonPassInput.forEach((text) =>
-    expect(validLength(text, MIN_LENGTH, MAX_LENGTH)).toBe(false),
-  );
-});
+    passInput.forEach((text) =>
+      expect(validLength(text, MIN_LENGTH, MAX_LENGTH)).toBe(true),
+    );
+    nonPassInput.forEach((text) =>
+      expect(validLength(text, MIN_LENGTH, MAX_LENGTH)).toBe(false),
+    );
+  });
 
-test('validDuplication : 배열 안에 중복된 아이템이 존재하는지 검사', () => {
-  const passInput = ['a', 'b', 'c'];
-  const nonPassInput = ['a', 'a', 'b'];
+  test('validDuplication : 배열 안에 중복된 아이템이 존재하는지 검사', () => {
+    const passInput = ['a', 'b', 'c'];
+    const nonPassInput = ['a', 'a', 'b'];
 
-  expect(validDuplication(passInput)).toBe(true);
-  expect(validDuplication(nonPassInput)).toBe(false);
+    expect(validDuplication(passInput)).toBe(true);
+    expect(validDuplication(nonPassInput)).toBe(false);
+  });
 });
