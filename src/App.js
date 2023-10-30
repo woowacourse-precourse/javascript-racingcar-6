6cf6fb11f491e81fed7fde9485980a5f1bfb4e50
f0ever0 <thddndud0830@gmail.com>
2023-10-30 11:11:42 +0900

    feat : 전진 라운드 실행

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 const InputError = require("./lib/utils/error.js");
-const { GAME_MESSAGE } = require("./lib/constants/message.js");
+const { GAME_MESSAGE, WORD } = require("./lib/constants/message.js");
 
 class App {
   constructor() {
@@ -10,12 +10,14 @@ class App {
   async play() {
     const carList = await this.getCarList();
     const count = await this.getTryCount();
+
+    this.moveForwardCar(carList, count);
   }
 
   async getCarList() {
-    const carInput = await Console.readLineAsync(GAME_MESSAGE.CARNAME_INPUT);
+    const carInput = await Console.readLineAsync(GAME_MESSAGE.CAR_INPUT);
     const carList = [...carInput.split(",")];
-    this.Error.validateCarNameInput(carList);
+    this.Error.validateCarInput(carList);
 
     return carList;
   }
@@ -25,6 +27,25 @@ class App {
     this.Error.validateCountInput(tryCount);
 
     return tryCount;
+  }
+
+  moveForwardCar(carList, count) {
+    Console.print(GAME_MESSAGE.RESULT);
+
+    let score = {};
+    carList.forEach((car) => (score[car] = 0));
+    for (let i = 0; i < count; i++) {
+      this.round(carList, score);
+    }
+  }
+
+  round(carList, score) {
+    carList.forEach((car) => {
+      const randomValue = Random.pickNumberInRange(0, 9);
+
+      if (randomValue >= 4) score[car] += 1;
+      Console.print(`${car} : ${WORD.PROGRESS.repeat(score[car])}`);
+    });
   }
 }
 
