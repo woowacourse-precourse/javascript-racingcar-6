c8a5ff4d0aa13485a71cc32c26965f234fab97ed
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-10-31 03:04:40 +0900

    ✨ feat : 출력문을 상수로 정의한다.

---
+++ src/constants/message.js
@@ -0,0 +1,10 @@
+const RACING = {
+    NAMING_GUIDE: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
+    NAMING_ERROR: "[ERROR] 이름이 잘못된 형식입니다.",
+    NUMBER_ERROR: "[ERROR] 숫자가 잘못된 형식입니다.",
+    QUESTION_COUNT: "시도할 횟수는 몇 회인가요?",
+    GAME_RESULT: "실행 결과",
+    END_GAME: (winnerName) => `최종 우승자 : ${winnerName}`
+}
+
+export default RACING
