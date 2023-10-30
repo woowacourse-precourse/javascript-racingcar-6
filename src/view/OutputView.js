b25afc0033086038702ce59d0943667d985966f0
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-31 01:28:31 +0900

    feat: [OutputView] 실행 결과 메시지를 출력하는 메서드 구현

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,7 +1,12 @@
 import { Console } from '@woowacourse/mission-utils';
+import Message from '../constant/Message.js';
 
 export default class OutputView {
   static printNewLine() {
     Console.print('');
+  }
+
+  static printTryResult() {
+    Console.print(Message.TRY_RESULT);
   }
 }
