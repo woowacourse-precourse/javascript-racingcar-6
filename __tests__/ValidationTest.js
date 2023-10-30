71445302c3d8ae25bc89745da1f44c98078264d9
soyoung125 <sypark041754@gmail.com>
2023-10-30 19:55:39 +0900

    feat(ValidationTest): isNumberAtLeast4 테스트 케이스 추가

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,4 +1,4 @@
-import { isLongerThan5, isNumber } from "../src/utils/validation"
+import { isLongerThan5, isNumber, isNumberAtLeast4 } from "../src/utils/validation"
 
 describe("validation.js 테스트", () => {
     test("숫자 형식 여부 검사 - 숫자 입력", () => {
@@ -24,5 +24,17 @@ describe("validation.js 테스트", () => {
         const result = isLongerThan5(input);
 
         expect(result).toBeTruthy();
+    });
+    test("랜덤 숫자가 4 이상인지 검사 - 4 이상인 경우", () => {
+        const random = 7;
+        const result = isNumberAtLeast4(random);
+
+        expect(result).toBeTruthy();
+    });
+    test("랜덤 숫자가 4 이상인지 검사 - 4 미만인 경우", () => {
+        const random = 2;
+        const result = isNumberAtLeast4(random);
+
+        expect(result).toBeFalsy();
     });
 })
