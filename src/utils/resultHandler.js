4e64a88d127fad909f840b62018a1241242b7d88
lurgi <wahoo9040@naver.com>
2023-10-30 10:16:42 +0900

    refactor(resultHandler): 메서드 분리

--- src/utils/resultHandler.js
+++ src/utils/resultHandler.js
@@ -4,31 +4,33 @@ function getRandomeNumberInRange(min, max) {
   return MissionUtils.Random.pickNumberInRange(min, max);
 }
 
+function getResultStringOneLine(cars) {
+  let string = "";
+  cars.forEach((carClass) => {
+    const RANDOM = getRandomeNumberInRange(0, 9);
+    if (RANDOM >= 4) carClass.setDistancePlusOne();
+    string += `${carClass.getName()} : ${carClass.getDistanceString()}\n`;
+  });
+  return string;
+}
+
 const resultHandler = {
   getResultString: ({ tryNumber, cars }) => {
     let stringToPrint = "";
-
     for (let tryOrder = 0; tryOrder < tryNumber; tryOrder += 1) {
-      let string = "";
-      cars.forEach((carClass) => {
-        const RANDOM = getRandomeNumberInRange(0, 9);
-        if (RANDOM >= 4) carClass.setDistancePlusOne();
-        string += `${carClass.getName()} : ${carClass.getDistanceString()}\n`;
-      });
-      stringToPrint += string;
+      const STRING_ONE_LINE = getResultStringOneLine(cars);
+      stringToPrint += STRING_ONE_LINE;
       stringToPrint += "\n";
     }
-
     return stringToPrint;
   },
+
   getWinner: (cars) => {
+    const winner = [];
     const SORTED_CARS = [...cars].sort(
       (car1, car2) => car2.getDistanceString().length - car1.getDistanceString()
     );
-
     const WIN_DISTANCE = SORTED_CARS[0].getDistanceString();
-
-    const winner = [];
     SORTED_CARS.forEach((car) => {
       if (car.getDistanceString() === WIN_DISTANCE) winner.push(car.getName());
     });
