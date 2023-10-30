0c3e362589a51ad73b0f5a3ed7fee99ffe5d462f
Lim-shweety <smile70200@naver.com>
2023-10-30 20:51:37 +0900

    :sparkles: feat: 자동차 이름 입력받기 자동차 이름 저장

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import {Console, MissionUtils} from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+
+    async play() {
+
+        const car_name_input = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+        const CARS = car_name_input.split(",");
+        const CAR_CNT = CARS.length;
+        let cnt_cars_go = [...new Array(CAR_CNT)].map((_, i) => 0);
+
+    }
 }
 
 export default App;
