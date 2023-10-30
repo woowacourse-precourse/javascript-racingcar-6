4fba3feda9cd7bdb929868a3c4c815394bf26d01
Seoan Yoon <earthdyoon@gmail.com>
2023-10-30 22:23:14 +0900

    feat: 무작위 값 생성 및 전진 여부 판단 기능 구현

--- src/Car.js
+++ src/Car.js
@@ -1,6 +1,17 @@
+import { Random } from '@woowacourse/mission-utils';
+
 class Car {
   constructor(name) {
     this.name = name;
+    this.moveCount = 0;
+  }
+
+  moveFoward() {
+    const number = Random.pickNumberInRange(0, 9);
+
+    if (number >= 4) {
+      this.moveCount += 1;
+    }
   }
 }
 
