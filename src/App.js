24d3aa642ff00da7ac91d5707176b1d76b6fe7d9
Youmi Kim <flordelavidaa@naver.com>
2023-10-30 23:48:06 +0900

    Feat: 입력값(이름) 검증
    - 이름에 공백이 있으면 에러
    - 이름이 5글자 초과이면 에러

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App {
     const carNames = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
     );
-    const carNameArray = carNames.split(',');
+    const carNameArray = makeNameArray(carNames);
     const carObjectArray = carNameArray.map((name) => new RacingCar(name));
 
     const chanceInput =
@@ -24,6 +24,18 @@ class App {
     const winners = findWinner(carObjectArray);
     printWinner(winners);
   }
+}
+
+function makeNameArray(names) {
+  const splittedNames = names.split(',');
+
+  for (const name of splittedNames) {
+    if (name.trim().length !== name.length)
+      throw new Error('[ERROR] 이름에 공백이 있습니다.');
+    if (name.length > 5)
+      throw new Error('[ERROR] 이름은 5 글자를 초과할 수 없습니다.');
+  }
+  return splittedNames;
 }
 
 function printResult(carArray) {
