dabfd4ab9e539142eada89fabcf6c34c93731f01
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-10-30 21:08:31 +0900

    test: getPositionResult() 메서드가 잘못된 값 반환시 실패 확인

--- __tests__/domains/CarTest.js
+++ __tests__/domains/CarTest.js
@@ -1,4 +1,4 @@
-import Car from '../src/Car.js';
+import Car from '../../src/Car.js';
 
 describe('class Car test', () => {
   let car;
@@ -19,5 +19,21 @@ describe('class Car test', () => {
 
       expect(result).toEqual({ name: '이두리', position: '---' });
     });
+
+    test.failing(
+      '2번 움직였는데 position이 변경되지 않았을때 fail 테스트',
+      () => {
+        const isMovable = true;
+        const stop = false;
+
+        car.increasePosition(stop);
+        car.increasePosition(isMovable);
+        car.increasePosition(isMovable);
+
+        const result = car.getPositionResult();
+
+        expect(result).toEqual({ name: '이두리', position: '' });
+      }
+    );
   });
 });
