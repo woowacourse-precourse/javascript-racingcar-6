1351cdf2cc6c26eccb3731b4d3af468c496be1bf
teawon <xodnjsvkvk9@gmail.com>
2023-10-31 00:32:03 +0900

    feat : InputView클래스 생성 및 입력 로직 분리

--- src/controller/CarRaceController.js
+++ src/controller/CarRaceController.js
@@ -1,6 +1,7 @@
 import { Console } from "@woowacourse/mission-utils";
 import Car from "../models/Car.js";
 import Race from "../models/Race.js";
+import InputView from "../views/InputView.js";
 
 // TODO Validtaion처리 및 Input별도로 분리할 것
 class CarRaceController {
@@ -8,17 +9,14 @@ class CarRaceController {
     this.cars = [];
     this.race = null;
     this.totalRound = 0;
+    this.InputView = new InputView();
   }
 
   async setup() {
-    const carInput = await Console.readLineAsync(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
-    );
+    const carInput = await this.InputView.getCarNamesUserInput();
     this.cars = carInput.split(",").map((carName) => new Car(carName));
 
-    const totalRoundInput = await Console.readLineAsync(
-      "시도할 횟수는 몇 회인가요?\n"
-    );
+    const totalRoundInput = await this.InputView.getTotalRoundUserInput();
     this.totalRound = totalRoundInput;
   }
 
---
+++ src/views/InputView.js
@@ -0,0 +1,33 @@
+import { Console } from "@woowacourse/mission-utils";
+
+class InputView {
+  async getCarNamesUserInput() {
+    const userInput = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    this.validateIsNull(userInput);
+    return userInput;
+  }
+
+  async getTotalRoundUserInput() {
+    const userInput = await Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요?\n"
+    );
+    this.validateIsNumber(userInput);
+    return userInput;
+  }
+
+  validateIsNumber(input) {
+    if (Number.isNaN(input)) {
+      throw new Error("[ERROR] 정수를 입력해주세요");
+    }
+  }
+
+  validateIsNull(input) {
+    if (input === "") {
+      throw new Error("[ERROR] 1개 이상의 자동차 이름을 입력해주세요");
+    }
+  }
+}
+
+export default InputView;
