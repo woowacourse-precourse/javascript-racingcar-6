18271906b4095f3d6468261f39267587ab220f1c
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-31 01:12:58 +0900

    feat: [Controller] 입력 받은 자동차 이름 목록을 통해 Car 객체 생성 구현

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,11 +1,23 @@
 import Validation from '../utils/Validation.js';
 import InputView from '../view/InputView.js';
+import Car from '../model/Car.js';
 
 export default class Controller {
+  constructor() {
+    this.carList = [];
+  }
+
+  createCar(carNameList) {
+    carNameList.forEach((carName) => {
+      this.carList.push(new Car(carName));
+    });
+  }
+
   async run() {
     const carNames = await InputView.carName();
     const carNameList = carNames.split(',');
 
     Validation.carNameInput(carNameList);
+    this.createCar(carNameList);
   }
 }
