31b602ad428ec0797971f78013f58c1ebdad25cf
JungHun98 <j7papa@naver.com>
2023-10-30 22:32:56 +0900

    feat: App Class startRace 메소드 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Car from './car/Car';
+import print from './utils/print';
 
 class App {
   constructor() {
     this.cars = [];
+    this.winners = [];
   }
 
   createCar(nameString) {
@@ -12,6 +14,23 @@ class App {
       const newCar = new Car(name);
       this.cars.push(newCar);
     });
+  }
+
+  startRace(attemp) {
+    print();
+    print('실행 결과');
+
+    while (this.winners.length === 0) {
+      this.cars.forEach((car) => {
+        car.move();
+      });
+
+      this.printMoveResult();
+
+      this.winners = this.cars.filter((car) => {
+        return car.getPosition() === Number(attemp);
+      });
+    }
   }
 
   async play() {}
