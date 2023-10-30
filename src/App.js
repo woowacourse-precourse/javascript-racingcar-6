f438d48b16fc0c4944fa55575f8057842c4039df
Dobbymin <gmin0701@knu.ac.kr>
2023-10-30 17:24:28 +0900

    [FEAT](App): 기능구현 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import { GAME } from './pages/texts.js';
+import { CAR } from './pages/texts.js';
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { inputRacingCarName, inputTryNumber, isStart, printWinner, progressRacingCar } from './pages/modules.js';
 
 class App {
-    async play() {}
+    async play() {
+        const CARS = await inputRacingCarName();
+        let TRY = await inputTryNumber();
+
+        MissionUtils.Console.print(CAR.RESULT);
+
+        while (TRY--) {
+            CARS.forEach((car) => {
+                if (isStart()) car.go++;
+            });
+            progressRacingCar(CARS);
+        }
+        const winner = printWinner(CARS);
+        MissionUtils.Console.print(CAR.WINNER + winner);
+    }
 }
 
 export default App;
