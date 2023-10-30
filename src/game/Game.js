038f24d88efe1b989e9cc52129487667177b0ec4
lee juyoung <juyung0903@naver.com>
2023-10-30 16:36:54 +0900

    feat: 사용자에게 시도할 횟수를 입력받고 입력받은 횟수만큼 반복문 실행

--- src/game/Game.js
+++ src/game/Game.js
@@ -3,11 +3,23 @@ class Game {
   constructor() {}
 
   // 자동차 이름을 초기 전진횟수와 함께 CARS에 저장
-  storeCarNames(CAR_NAMES_INPUT) {
+  storeCars(CAR_NAMES_INPUT) {
     const CAR_NAMES = CAR_NAMES_INPUT.split(",");
     CAR_NAMES.forEach((eachCarName) => {
       CARS.push([eachCarName,0]);
     });
+  }
+
+  //시도할 횟수만큼 레이스 시작
+  runRaceGamesWithAttempts(ATTEMPT_COUNT){
+    for(let i=0; i<ATTEMPT_COUNT; i++) {
+      this.eachRaceGame();
+    } 
+  }
+  
+  //각각의 레이스 게임을 실행하는 메소드
+  eachRaceGame(){
+    //TODO: 레이스게임 구현
   }
 }
 export default Game;
--- src/game/GameUi.js
+++ src/game/GameUi.js
@@ -6,12 +6,20 @@ class GameUi {
     this.game = new Game();
   }
 
-  //사용자가 차이름을 입력
+  // 사용자가 차이름을 입력
   async askCarName() {
     const CAR_NAMES_INPUT = await MissionUtils.Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
-    this.game.storeCarNames(CAR_NAMES_INPUT);
+    this.game.storeCars(CAR_NAMES_INPUT);
+  }
+
+  // 사용자가 시도할 횟수를 입력
+  async askAttemptCount() {
+    const ATTEMPT_COUNT = await MissionUtils.Console.readLineAsync(
+      '시도할 횟수는 몇 회인가요?'
+    );
+    this.game.runRaceGamesWithAttempts(ATTEMPT_COUNT);
   }
 }
 export default GameUi;
