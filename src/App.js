6eca7ac702f01269dbd38651dab79d1f9f630787
stegano9744 <stegano9744@gmail.com>
2023-10-30 18:53:44 +0900

    🔗 link: 무작위 숫자 생성, 자동차 전진 기능 연결

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Console } from '@woowacourse/mission-utils';
 import { PRINT_MESSAGE, READ_MESSAGE } from './constants/constants';
 import { validateIsNumber, validateLength } from './utils/validate';
 import { createCarData } from './utils/createCarData';
+import { createRandomNumber } from './utils/createRandomNumber';
+import { goStopCar } from './utils/goStopCar';
 class App {
   async play() {
     const carName = await Console.readLineAsync(READ_MESSAGE.start);
@@ -13,6 +15,12 @@ class App {
     Console.print(PRINT_MESSAGE.result);
 
     const carData = createCarData(carName);
+
+    for (let i = 0, moveCount = Number(moveCount); i < moveCount; i++) {
+      carData = createRandomNumber(carData);
+
+      carData = goStopCar(carData);
+    }
   }
 }
 
--- src/utils/goStopCar.js
+++ src/utils/goStopCar.js
@@ -1,7 +1,7 @@
-export const goStopCar = (car) => {
-  car.forEach((v) => {
-    if (v.number >= 4) v.result = v.result + '-';
+export const goStopCar = (carData) => {
+  carData.forEach((data) => {
+    if (data.number >= 4) data.result = data.result + '-';
   });
 
-  return car;
+  return carData;
 };
