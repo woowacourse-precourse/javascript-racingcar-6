89bd55712e8fb974ffc6263227db353d180fbc1c
yonainthefish <yonaisgood@gmail.com>
2023-10-31 03:39:32 +0900

    feat: 경주 시작시 경주할 자동차 이름 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import startGame from "./controller/GameController.js"
+
 class App {
-  async play() {}
+  // eslint-disable-next-line class-methods-use-this
+  async play() {
+    startGame();
+  }
 }
+
+const app = new App();
+app.play()
 
 export default App;
---
+++ src/controller/GameController.js
@@ -0,0 +1,7 @@
+import getCarNames from '../view/inputView.js';
+
+const startGame = () =>{
+   getCarNames();
+}
+
+export default startGame;
