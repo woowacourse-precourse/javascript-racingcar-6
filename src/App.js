8a842d405b873d68b090ac13240ead08f3fbf0e7
jjo-mi <whdal2636@naver.com>
2023-10-30 21:27:17 +0900

    [feat] 기능2 자동차 생성 입력값에 대한 유효성 검사

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
 import { Random, Console } from "@woowacourse/mission-utils";
+import { MESSAGE, ERROR } from "./common/text.js";
 
 class App {
   async play() {
-    let CAR_NAME = [];
-    Console.readLine(
-      "경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)\n",
-      (answer) => {
-        // console.log(`닉네임: ${answer}`);
-        CAR_NAME = answer.split(",");
-        console.log("CAR_NAME: ", CAR_NAME);
-      }
-    );
+    const CAR_NAME = await makeCar();
+    console.log("다음거");
+
+    // 3. 몇번 이동하는지 입력받기
   }
 }
+// 1. n개의 자동차 생성
+const makeCar = async () => {
+  try {
+    let name = await Console.readLineAsync(`${MESSAGE.GAME_START}`);
+    let nameArr = name.trim().split(",");
+
+    let validName = await validationName(nameArr);
+    if (validName) {
+      return true;
+    } else if (validName === "length") {
+      throw new Error(`${ERROR.NAME_LENGTH}`);
+    } else {
+      throw new Error(`${ERROR.NAME_COMMA}`);
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+// 2. 자동차 생성 입력값에 대한 유효성 검사
+const validationName = async (nameArr) => {
+  console.log("ghkrdls", nameArr.length);
+  // const errors = [];
+  if (nameArr.length === 1) {
+    throw new Error(`${ERROR.NAME}`);
+  } else {
+    nameArr.forEach((el, index) => {
+      if (el.length >= 5 || el.length <= 1) {
+        throw new Error(
+          `${ERROR.NAME_LENGTH} ${index + 1}번째요소에 대한 에러`
+        );
+      }
+    });
+  }
+  return true;
+};
 
 export default App;
 
--- src/common/text.js
+++ src/common/text.js
@@ -1,3 +1,8 @@
-export const massege = {
-  GAME_START: "경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)",
+export const MESSAGE = {
+  GAME_START: "경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)\n",
+};
+
+export const ERROR = {
+  NAME_: "[ERROR] 이름은 쉼표로 구분해야합니다. 게임이 종료됩니다.",
+  NAME_LENGTH: "[ERROR] 이름의 글자 수는 5자 이하입니다. 게임이 종료됩니다.",
 };
