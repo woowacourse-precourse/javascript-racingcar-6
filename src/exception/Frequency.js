1c868bfde8770c3601eaf09be382387fb7189552
Dev-steadyBoy <85480086+Dev-steadyBoy@users.noreply.github.com>
2023-10-30 09:18:08 +0900

    feat: Frequency객체 구현
    Lap이 0이나 음수이거나, 21바퀴 이상이면 에러가 발생하는 기능 추가
    영어, 한글, 특수기호를 입력하면 에러가 발생하는 기능 추가

---
+++ src/exception/Frequency.js
@@ -0,0 +1,23 @@
+import BaseExceptionHandler from "./Errorcase.js";
+
+class Frequency extends BaseExceptionHandler {
+  constructor(laps) {
+    super();
+    this.laps = laps;
+  }
+
+  handleException() {
+    const { laps } = this;
+    if (0 >= laps || laps >= 20) {
+      throw new Error(
+        "[ERROR] 저희 F1 월드 챔피언쉽은 최소 1바퀴, 최대 20바퀴만 돌 수 있습니다.",
+      );
+    }
+
+    if (!Number.isInteger(Number(laps))) {
+      throw new Error("[ERROR] 정수만 작성할 수 있습니다.");
+    }
+  }
+}
+
+export default Frequency;
