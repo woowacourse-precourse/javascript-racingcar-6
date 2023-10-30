46a97449af7fa97105e150c3a359fdcffd7a4222
donggyun <donggyunkim12@gmail.com>
2023-10-30 16:20:02 +0900

    feat(validateFn): 이동할 횟수 입력 검사 구현

--- src/validateFn.js
+++ src/validateFn.js
@@ -7,3 +7,11 @@ export const validateCarName = (carListArr) => {
   if (carListArr.length !== new Set(carListArr).size)
     throw new Error('[ERROR] 자동차 이름은 중복 되지않게 입력해주세요');
 };
+
+export const validateMoveNum = (moveNum) => {
+  console.log(parseInt(moveNum));
+  if (parseInt(moveNum).toString() !== moveNum)
+    throw new Error('[ERROR] 숫자만 입력해주세요.');
+  else if (parseInt(moveNum) < 1)
+    throw new Error('[ERROR] 1이상의 숫자를 입력해주세요.');
+};
