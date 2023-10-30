5e21e40ab6981830401af581d4165684cdf7084c
Seung Jun <pache31@naver.com>
2023-10-30 23:00:04 +0900

    [test] 예외처리 테스트 파일 분리

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -68,24 +68,8 @@ describe('자동차 경주 게임', () => {
     });
   });
 
-  test.each([
-    [['pobi,javaji']],
-    [['pobi,eastjun']],
-    [['pobi,jun,pobi']],
-    [['']],
-  ])('이름에 대한 예외 처리', async inputs => {
-    // given
-    mockQuestions(inputs);
-
-    // when
-    const app = new App();
-
-    // then
-    await expect(app.play()).rejects.toThrow('[ERROR]');
-  });
-
-  test.each([[['pobi,java', '']], [['pobi,east', 'number']]])(
-    '시도 횟수에 대한 예외 처리',
+  test.each([[['pobi,javaji']], [['pobi,eastjun']]])(
+    '이름에 대한 예외 처리',
     async inputs => {
       // given
       mockQuestions(inputs);
