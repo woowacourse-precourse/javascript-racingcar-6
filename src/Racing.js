c172914f2c2675e0b0153735482eb28c71a3df0d
Sungwk <73461032+Sungwk@users.noreply.github.com>
2023-10-30 13:03:47 +0900

    :sparkles: 사용자에게 자동차들이 몇 번 이동할 것인지 입력받는 기능 추가
    사용자에게 자동차들이 몇 번 이동할 것인지 입력받는 함수 구현

--- src/Racing.js
+++ src/Racing.js
@@ -4,11 +4,17 @@ class Racing {
   async start() {
     this.cars = await this.getCarsName();
     this.validateCarName();
+    this.moveCount = await this.getMoveCount();
   }
 
   async getCarsName() {
     const cars = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
     return cars.split(',');
+  }
+
+  async getMoveCount() {
+    const count = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    return count;
   }
 
   validateCarName() {
@@ -17,5 +23,4 @@ class Racing {
     });
   }
 }
-
 export default Racing;
