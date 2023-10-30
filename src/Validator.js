d7d847c1dafa9a25054b5b80474c4ab4ceacf6b5
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-10-31 00:27:19 +0900

    feat: 자동차 이름이 6자 이상일 경우 에러가 발생하는 기능 구현

--- src/Validator.js
+++ src/Validator.js
@@ -1,15 +1,22 @@
 import ErrorMessage from './ErrorMessage.js';
 
 class Validator {
+  static #MIN_LENGTH = 1;
   static #MAX_LENGTH = 5;
 
   static validateCarNames(array) {
     if (this.#isEmpty(array[0])) throw new Error(ErrorMessage.EMPTY);
-    throw new Error(ErrorMessage.MAX_LENGTH);
+    if (!array.every((string) => this.#isLength(string))) {
+      throw new Error(ErrorMessage.MAX_LENGTH);
+    }
   }
 
   static #isEmpty(value) {
     return value === '';
+  }
+
+  static #isLength(string, min = this.#MIN_LENGTH, max = this.#MAX_LENGTH) {
+    return min <= string.length && string.length <= max;
   }
 }
 
