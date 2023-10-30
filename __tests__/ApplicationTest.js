84d905f9502d84a551169a1734abae3e5b34a6de
leehyewon0531 <ifpffk@naver.com>
2023-10-30 22:54:49 +0900

    test: 이름 유효성에 대한 테스트 코드 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -51,7 +51,23 @@ describe("자동차 경주 게임", () => {
     [["pobi,eastjun"]],
     [[",pobi"]],
     [["pobi,"]],
-  ])("이름에 대한 예외 처리", async (inputs) => {
+  ])("이름 길이에 대한 예외 처리", async (inputs) => {
+    // given
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.play()).rejects.toThrow("[ERROR]");
+  });
+
+  test.each([
+    [["pobi,pobi"]],
+    [["a,1,c"]],
+    [["a,$,c"]],
+    [["a,j&b,c"]],
+  ])("이름 유효성에 대한 예외 처리", async (inputs) => {
     // given
     mockQuestions(inputs);
 
