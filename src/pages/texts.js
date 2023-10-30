6fde0518c8e18d8d8f988066abbc1ddcae2f604a
Dobbymin <gmin0701@knu.ac.kr>
2023-10-30 17:21:10 +0900

    [CHORE](texts): 결과에 줄바꿈 추가

--- src/pages/texts.js
+++ src/pages/texts.js
@@ -9,7 +9,7 @@ export const ERROR = {
 export const CAR = {
     INPUT: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
     COUNT: '시도할 횟수는 몇 회인가요?\n',
-    RESULT: '실행 결과',
+    RESULT: '\n실행 결과',
     WINNER: '최종 우승자 : ',
     PROCESS_BAR: '-',
 };
