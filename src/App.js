c048069a481172f2c3d53af374d0efdebb8a7e17
BK <3031011@naver.com>
2023-10-30 10:15:24 +0900

    feat(racing-game): 레이싱 게임 전체적 흐름 App.js에 작성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import { Console } from "@woowacourse/mission-utils";
-import gameQnA from "./classes/gameQnA";
-import Validator from "./classes/validator";
+import GameQnA from './classes/gameQnA';
+import Validator from './classes/validator';
+import RacingGame from './classes/racingGame';
+import GameManager from './classes/gameManager';
 
 class App {
   async play() {
-    // const carNameAnswer = await gameQnA.carsName();
-    // Validator.carsNameInput(carNameAnswer);
+    const carNameInput = await GameQnA.carsName();
+    Validator.carsNameInput(carNameInput);
+    const tryTimeInput = await GameQnA.tryTime();
+    Validator.tryTime(tryTimeInput);
+
+    const racingGame = new RacingGame(carNameInput);
+    const game = racingGame.play(tryTimeInput);
+    let racingStatus = game.next();
+    /**
+     * 제너레이터 객체가 done이 true가 되었을 시 value는 undefined를 반환함
+     * 이를 해결하기 위해 lastRacingStatus를 변수를 통해 값을 할당
+     * lastRacingStatus 변수에 할당되어 있는 마지막 상태를 통해 우승자 결과 값 계산
+     */
+    let lastRacingStaus;
+
+    while (!racingStatus.done) {
+      GameManager.printStatus(racingStatus.value);
+      lastRacingStaus = racingStatus.value;
+      racingStatus = game.next();
+    }
+
+    GameManager.printWinner(lastRacingStaus);
   }
 }
 
