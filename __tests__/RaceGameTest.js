00ad5f6da771b4971a5a78d6d1e68aa8a51f651d
today-is-first <diligent0644@gmail.com>
2023-10-31 00:57:04 +0900

    test: checkTryNumType Test

--- __tests__/RaceGameTest.js
+++ __tests__/RaceGameTest.js
@@ -74,41 +74,69 @@ describe('유틸 기능 테스트', () => {
     });
   });
   describe('입력 값 검증 테스트', () => {
-    describe('checkCarNameEmpty 테스트', () => {
-      test('빈 문자열인 경우 에러 발생', () => {
-        const carName = '';
-        expect(() => checkCarNameEmpty(carName)).toThrowError(
-          ERROR_MESSAGES.errorNameEmpty,
-        );
+    describe('이름 입력 값 테스트', () => {
+      describe('checkCarNameEmpty 테스트', () => {
+        test('빈 문자열인 경우 에러 발생', () => {
+          const carName = '';
+          expect(() => checkCarNameEmpty(carName)).toThrowError(
+            ERROR_MESSAGES.errorNameEmpty,
+          );
+        });
+        test('빈 문자열이 아니면 에러 발생하지 않음', () => {
+          const carName = 'chan';
+          expect(() => checkCarNameEmpty(carName)).not.toThrow();
+        });
       });
-      test('빈 문자열이 아니면 에러 발생하지 않음', () => {
-        const carName = 'chan';
-        expect(() => checkCarNameEmpty(carName)).not.toThrow();
-      });
-    });
 
-    describe('checkCarNameSpace 테스트', () => {
-      test('공백이 있으면 에러 반환', () => {
-        const carName = 'chan, bo';
-        expect(() => checkCarNameSpace(carName)).toThrowError(
-          ERROR_MESSAGES.errorNameSpace,
-        );
+      describe('checkCarNameSpace 테스트', () => {
+        test('공백이 있으면 에러 반환', () => {
+          const carName = 'chan, bo';
+          expect(() => checkCarNameSpace(carName)).toThrowError(
+            ERROR_MESSAGES.errorNameSpace,
+          );
+        });
+        test('공백이 없으면 에러 발생하지 않음', () => {
+          const carName = 'chan,bo';
+          expect(() => checkCarNameSpace(carName)).not.toThrow();
+        });
       });
-      test('공백이 없으면 에러 발생하지 않음', () => {
-        const carName = 'chan,bo';
-        expect(() => checkCarNameSpace(carName)).not.toThrow();
+      describe('checkCarNameLen 테스트', () => {
+        test('길이가 5보다 길면 에러 발생', () => {
+          const carName = 'LeeChan';
+          expect(() => checkCarNameLen(carName)).toThrowError(
+            ERROR_MESSAGES.errorNameLen,
+          );
+        });
+        test('길이가 5이하면 에러 발생하지 않음 ', () => {
+          const carName = 'chan';
+          expect(() => checkCarNameLen(carName)).not.toThrow();
+        });
+      });
+      describe('checkCarNameType', () => {
+        test('한글, 영어가 아니면 에러 발생', () => {
+          const carName = '123a,bo22';
+          expect(() => checkCarNameType(carName)).toThrowError(
+            ERROR_MESSAGES.errorNameType,
+          );
+        });
+        test('한글, 영어면 에러 발생하지 않음', () => {
+          const carName = '이찬,Chan';
+          expect(() => checkCarNameType(carName)).not.toThrow();
+        });
       });
     });
-    describe('checkCarNameLen 테스트', () => {
-      test('길이가 5보다 길면 에러 발생', () => {
-        const carName = 'LeeChan';
-        expect(() => checkCarNameLen(carName)).toThrowError(
-          ERROR_MESSAGES.errorNameLen,
+  });
+  describe('시도 횟수 입력 값 테스트', () => {
+    describe('checkTryNumType 테스트', () => {
+      test('숫자가 아니면 에러 발생', () => {
+        const tryNum = 'a';
+        expect(() => checkTryNumType(tryNum)).toThrowError(
+          ERROR_MESSAGES.errorTryNumType,
         );
       });
-      test('길이가 5이하면 에러 발생하지 않음 ', () => {
-        const carName = 'chan';
-        expect(() => checkCarNameLen(carName)).not.toThrow();
+      test('숫자면 에러 발생하지 않음', () => {
+        const tryNum = '1';
+        expect(() => checkTryNumType(tryNum)).not.toThrow();
       });
     });
   });
