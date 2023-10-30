eecae358bdf6f950a876ab22fb115600a8491882
ChanYong Park <krap5075@naver.com>
2023-10-31 02:28:22 +0900

    comment: output class관련 필요없는 주석제거

--- src/view/Output.js
+++ src/view/Output.js
@@ -3,24 +3,16 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 const Console = MissionUtils.Console;
 
 class Output {
-  constructor() {
-    Console.print("실행 결과\n");
-  }
-  // 매개변수로 Car 객체배열을 받는다.
   printResult(Cars) {
-    // 1번 실행할 때 마다 출력해야한다.
-    // 각 객체의 이동횟수 만큼 - 출력해준다.
     Cars.forEach((car) => {
-      Console.print(`${car.name} : ${car.dashes.repeat(car.move_times)}`);
+      Console.print(`${car.name} : ${car.dash.repeat(car.move_times)}`);
     });
     Console.print("\n");
   }
 
-  // 객체 멤버변수로 이동한 횟수를 가진다는 가정
   printWinners(Cars) {
-    const max_times = Math.max(...Cars.map((car) => car.move_times));
-    //todo 숫자형인지 문자인지 비교가 되는지 체크해봐야한다
-    const winners = Cars.filter((car) => car.move_times === max_times)
+    const max_time = Math.max(...Cars.map((car) => car.move_times));
+    const winners = Cars.filter((car) => car.move_times === max_time)
       .map((car) => car.name)
       .join(",");
     Console.print(`최종 우승자 : ${winners}`);
