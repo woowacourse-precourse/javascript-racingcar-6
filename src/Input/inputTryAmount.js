4184919e29e3b70e8fdfae14f1e5ce6f1672c02d
Eunseo Na <nes020781@gmail.com>
2023-10-31 03:50:52 +0900

    fix: inputTryAmount 숫자를 받아올 수 있도록 수정

--- src/Input/inputTryAmount.js
+++ src/Input/inputTryAmount.js
@@ -1,12 +1,12 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import GameMessage from "../Message/gameMessage";
+import GameMessage from "../Message/gameMessage.js";
 
-function InputTryAmount(tryNumber) {
-  MissionUtils.Console.readLineAsync(tryNumber);
+function InputTryAmount() {
+  const inputNumber = MissionUtils.Console.readLineAsync(GameMessage.INPUT_TRY_AMOUNT);
 
-  const intNumber = parseInt(tryNumber, 10);
+  const intNumber = parseInt(inputNumber, 10);
 
-  if (tryNumber.length < 1 || intNumber <= 0) {
+  if (inputNumber.length < 1 || intNumber <= 0) {
     throw new Error(GameMessage.ERROR.TRY_NUMBER_INVALID);
   }
 
