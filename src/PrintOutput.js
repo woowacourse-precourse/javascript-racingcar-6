81fba66ff1308613e733e932cb6ebaa9833d5f12
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2023-10-30 23:08:12 +0800

    style: divide functions from PrintOutput

--- src/PrintOutput.js
+++ src/PrintOutput.js
@@ -1,23 +1,26 @@
 import { Console } from '@woowacourse/mission-utils';
 
-const PrintOutput = {
-    printResultTitle() {
-        Console.print('실행 결과\n');
-    },
-    //각 게임 실행 결과 출력 
-    printEachResult(names, racingCars) {
-        names.forEach((name) => {
-            if (racingCars.has(name)) racingCars.set(name, 1 + racingCars.get(name));
-            else racingCars.set(name, 1);
-            Console.print(`${name} : ${'-'.repeat(racingCars.get(name))}`);
-          })
-    },
-    //우승자 출력 
-    printWinner(maxRate, racingCars) {
-        let result = [];
-        racingCars.forEach((rate, name) => {
-            if (rate === maxRate) result.push(name);
-        })
-        Console.print(`최종 우승자 : ${result.join(', ')}`);
-    }
+
+function printResultTitle() {
+    Console.print('실행 결과\n');
 }
+
+//각 게임 실행 결과 출력 
+function printEachResult(names, racingCars) {
+    names.forEach((name) => {
+        if (racingCars.has(name)) racingCars.set(name, 1 + racingCars.get(name));
+        else racingCars.set(name, 1);
+        Console.print(`${name} : ${'-'.repeat(racingCars.get(name))}`);
+    })
+}
+
+//우승자 출력 
+function printWinner(maxRate, racingCars) {
+    let result = [];
+    racingCars.forEach((rate, name) => {
+        if (rate === maxRate) result.push(name);
+    })
+    Console.print(`최종 우승자 : ${result.join(', ')}`);
+}
+
+export { printResultTitle, printEachResult, printWinner };
