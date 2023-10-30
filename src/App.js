7042542e91ade1688a95200315ad21bcd4e77e55
KimRosa <108081283+KimRosa@users.noreply.github.com>
2023-10-30 23:52:26 +0900

    Feat: add tryNumber Console
    시도할 횟수를 입력받을 수 있는 콘솔을 작성한다

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ class App {
         "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
       );
       let cars = this.checkNameOfCarsLength(nameOfCars);
+
+      // 2. 시도할 횟수는 몇 회인가요 콘솔
+      const tryNumber = await MissionUtils.Console.readLineAsync(
+        "시도할 횟수는 몇 회인가요?"
+      );
     }
   }
   // + 이름 5자 이하. 쉼표로 구분.
