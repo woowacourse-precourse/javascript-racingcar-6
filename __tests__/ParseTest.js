8cf30abfc3f380b416709398019a9e3b4ea3acd6
fecapark <fecapark@gmail.com>
2023-10-30 21:35:28 +0900

    test: 형 변환 테스트 추가

---
+++ __tests__/ParseTest.js
@@ -0,0 +1,13 @@
+import { makeScoreboardByNames } from "../src/utils/parse.js";
+
+describe("형 변환 테스트", () => {
+  test("이름들로 빈 스코어보드 만들기 테스트", () => {
+    const inputs = [[], ["a", "b"], ["a", "", "b", "c"], ["", ""], ["a", "a"]];
+    const outputs = [{}, { a: 0, b: 0 }, { a: 0, b: 0, c: 0 }, {}, { a: 0 }];
+
+    inputs.forEach((input, index) => {
+      const scoreboard = makeScoreboardByNames(input);
+      expect(scoreboard).toEqual(outputs[index]);
+    });
+  });
+});
