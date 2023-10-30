00b3182d1dfbf347d78f5df7aeacc728c7538a6b
leehyeyeong <elto789@naver.com>
2023-10-30 22:46:58 +0900

    feat: 자동차 이름과, 시도할 횟수 입력 받기 - 자동차의 이름은 split을 이용하여 , 를 기준으로 저장

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    start();
+
+  }
 }
 
+async function start() {
+  Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+  const inputCarName = await Console.readLineAsync("");
+  const carName = inputCarName.split(',');
+  Console.print("시도할 횟수는 몇 회인가요?");
+  const inputNumber = await Console.readLineAsync("");
+}
+
+
 export default App;
+
+const app = new App();
+app.play();
