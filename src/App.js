734839afe54110786fd2b389923e2047deacc721
kimyonghui <kyhui1115@gmail.com>
2023-10-30 20:29:03 +0900

    feat: 경주 자동차 등록 기능 및 관련변수 생성, 관련 validate 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    Console.print(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
+    );
+
+    const CAR = await Console.readLineAsync('');
+    const CAR_LIST = CAR.split(',');
+    this.validateCarNameLength(CAR_LIST);
+
+    const SCORES = [];
+    const RACE = CAR_LIST.map((car) => {
+      SCORES.push(0);
+      return `${car} : `;
+    });
+
+
+  validateCarNameLength(carList) {
+    carList.forEach((car) => {
+      if (car.length > 5) {
+        throw new Error('[ERROR] 자동차 이름은 5자 이하로 입력해 주세요.');
+      }
+    });
+  }
+
 }
 
 export default App;
