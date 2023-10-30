bb89c28d4c2a8a05a608f9405fe1fc2b56b9dd48
devkyoung2 <devkyoung2@gmail.com>
2023-10-30 23:36:21 +0900

    feat : 경주 할 자동차 이름 입력 기능 구현
    자동차의 이름이 올바른 형식으로 입력되었는지 확인

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
-class App {
-  async play() {}
+import { Console } from "@woowacourse/mission-utils";
+
+export default class App {
+  constructor() {
+    this.player = "";
+    this.race = "";
+  }
+
+  async play() {
+    await this.getCarsName();
+  }
+
+  async getCarsName() {
+    const cars = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    const validCars = isValidCars(cars);
+    this.player = validCars;
+  }
 }
 
-export default App;
+function isValidCars(cars) {
+  const carList = cars.split(",");
+
+  const underFiveDigitCars = [];
+
+  for (const car of carList) {
+    const trimmedCarName = car.trim();
+
+    if (trimmedCarName.length > 5)
+      throw new Error(
+        "[ERROR] 자동차 이름은 쉼표(,)를 기준으로 구분하며 이름은 5자 이하만 가능합니다."
+      );
+
+    underFiveDigitCars.push(trimmedCarName);
+  }
+
+  return underFiveDigitCars;
+}
