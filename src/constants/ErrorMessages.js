699bb2b5ef2c1a051a432948c1204b33c8f9173f
Lee sang Yeop <mma7710@naver.com>
2023-10-31 03:28:00 +0900

    Feat: 에러 메세지를 상수로 정의

---
+++ src/constants/ErrorMessages.js
@@ -0,0 +1,11 @@
+import CONDITIONS from './Conditions.js';
+
+const ERROR_MESSAGES = Object.freeze({
+	car_name_exceeds_maximum_digits: `[ERROR] 자동차 글자수가 ${CONDITIONS.car_name_max_digits}를 초과했습니다.`,
+	use_escape_sequence: '[ERROR] 단어 \\ 은(는) 사용할 수 없습니다.',
+	value_is_not_a_number: '[ERROR] 입력받은 값이 숫자가 아닙니다.',
+	value_is_negative_number: '[ERROR] 입력받은 값이 0보다 작습니다.',
+	value_is_exceeds_maximum_length: `[ERROR] ${CONDITIONS.value_maximum_length}보다 작은 값을 입력해주세요.`,
+});
+
+export default ERROR_MESSAGES;
