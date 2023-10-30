d1fc47ea355805d3e5fd3909202b259e085c587e
seok-pyo <hongseokpyou@gmail.com>
2023-10-30 18:18:02 +0900

    refactor: printResult 내 변수명 변경

--- src/game/printResult.js
+++ src/game/printResult.js
@@ -5,9 +5,11 @@ const printResult = (finalRecord, lapLength) => {
   const winner = [];
   // 우승자 판단 로직
   finalRecord.forEach((record) => {
-    const countLength = record.lastIndexOf('-') - record.indexOf('-') + 1;
+    const stepCount = record.lastIndexOf('-') - record.indexOf('-') + 1;
     const carSplitIndex = record.indexOf(':') - 1;
-    if (countLength === lapLength) winner.push(record.substring(0, carSplitIndex));
+    if (stepCount === lapLength) {
+      winner.push(record.substring(0, carSplitIndex));
+    }
   });
   if (winner.length === 0) computer.tell(MESSAGE.NO_WINNER);
   else computer.tell(`${MESSAGE.FINAL_WINNER + winner.join(', ')}`);
