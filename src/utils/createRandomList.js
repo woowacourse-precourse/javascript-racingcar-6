5ab23ed4d942a8a1647136279e619cf086e5319a
stegano9744 <stegano9744@gmail.com>
2023-10-30 16:54:49 +0900

    ✨ feat: 무작위 숫자 생성 기능 변경 (배열 → 숫자)

--- src/utils/createRandomList.js
+++ src/utils/createRandomList.js
@@ -1,15 +1,11 @@
 import { Random } from '@woowacourse/mission-utils';
 import { CONFIG } from '../constants/constants';
 
-export const createRandomList = (moveCount) => {
-  const randomList = [];
-  const count = Number(moveCount);
-  for (let i = 0; i < count; i++) {
-    let number = Random.pickNumberInRange(
-      CONFIG.range.minNumber,
-      CONFIG.range.maxNumber,
-    );
-    randomList.push(number);
-  }
-  return randomList;
+export const createRandomNumber = () => {
+  let number = Random.pickNumberInRange(
+    CONFIG.range.minNumber,
+    CONFIG.range.maxNumber,
+  );
+
+  return number;
 };
