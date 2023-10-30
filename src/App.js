73ccd9d0333da4dc8b8505bde06582599e5f4aed
rustandbone <dlehdgh14@naver.com>
2023-10-31 00:21:14 +0900

    [예외 처리] 이름 중복 체크, [기능 향상] 자동차 진행도 repeat 대신 -으로 변경

--- src/App.js
+++ src/App.js
@@ -11,10 +11,17 @@ class App {
     if (carNames.indexOf(',') === -1)
       throw new Error('[ERROR] 자동차 이름 입력이 잘못되었습니다.');
 
-    carNames.split(',').map((car) => {
+    const carNamesArray = carNames.split(',');
+
+    const checkNumberSet = new Set(carNamesArray);
+
+    if (carNamesArray.length !== checkNumberSet.size)
+      throw new Error('[ERROR] 자동차 이름 입력이 잘못되었습니다.');
+
+    carNamesArray.map((car) => {
       if (car.length > 5)
         throw new Error('[ERROR] 자동차 이름은 5자 이내로 작성해주세요.');
-      this.racingCars[car] = 1;
+      this.racingCars[car] = '-';
       if (car === '')
         throw new Error('[ERROR] 자동차 이름 입력이 잘못되었습니다.');
     });
@@ -35,32 +42,30 @@ class App {
       const racingCarNames = await Console.readLineAsync(
         '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
       );
-
       this.carNameValidate(racingCarNames);
 
       const racingTryNumbers = await Console.readLineAsync(
         '시도할 횟수는 몇 회인가요?\n'
       );
-
       this.tryNumberCheck(racingTryNumbers);
 
       Console.print('\n실행 결과');
       for (let i = 0; i < racingTryNumbers; i++) {
         Object.keys(this.racingCars).map((car) => {
           const number = Random.pickNumberInRange(1, 9);
-          if (number > 4) this.racingCars[car]++;
-          let meter = '-'.repeat(this.racingCars[car]);
-          Console.print(`${car} : ${meter}`);
+          if (number > 4) this.racingCars[car] += '-';
+          Console.print(`${car} : ${this.racingCars[car]}`);
         });
         Console.print(`\n`);
       }
 
       Object.values(this.racingCars).map((score) =>
-        this.winnerScore < score ? (this.winnerScore = score) : ''
+        this.winnerScore < score.length ? (this.winnerScore = score.length) : ''
       );
 
       for (const car in this.racingCars) {
-        if (this.racingCars[car] === this.winnerScore) this.winner.push(car);
+        if (this.racingCars[car].length === this.winnerScore)
+          this.winner.push(car);
       }
 
       Console.print(`최종 우승자 : ${this.winner}`);
