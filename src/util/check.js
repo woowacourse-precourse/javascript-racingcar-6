c5c49ecde47b4e9930aaa445636a9dd74e49ac34
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-10-31 03:12:27 +0900

    ✨ feat : 이름을 체크하는 checkName 함수를 추가합니다.

---
+++ src/util/check.js
@@ -0,0 +1,13 @@
+import RACING from "../constant/messages.js";
+
+/**
+ * 
+ * @param {*} nameArray 5글자 이하인 이름들의 배열을 받아옵니다. 
+ */
+const checkName = (nameArray) => {
+    nameArray.forEach(name => {
+        if (name.length > 5 || name.trim() === 0) throw new Error(RACING.NAMING_ERROR)
+    })
+}
+
+export default checkName
