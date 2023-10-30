5b090e043141e7fcd10387b95c6dfb62e2955ec8
stegano9744 <stegano9744@gmail.com>
2023-10-30 11:16:49 +0900

    ✅ test: 자동차 이동 횟수 예외 검사 수정

--- __tests__/UnitTest.js
+++ __tests__/UnitTest.js
@@ -1,4 +1,7 @@
-import { validateCarNameLength } from '../src/utils/validate';
+import {
+  validateCarNameLength,
+  validateTypeNumber,
+} from '../src/utils/validate';
 
 describe('예외 테스트', () => {
   test('자동차 이름 길이 예외 검사', () => {
@@ -7,9 +10,7 @@ describe('예외 테스트', () => {
   });
 
   test('자동차 이동 횟수 예외 검사', () => {
-    const inputs = ['pobi,woni', 'ab1'];
-    const count = Number(inputs[1]);
-
-    expect(Number.isNaN(count)).toBe(true);
+    const count = 'ab1';
+    validateTypeNumber(count);
   });
 });
