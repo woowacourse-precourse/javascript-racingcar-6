a1d51024f8a65acfe75e70e1060ea76156ee3dc6
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2023-10-30 19:48:08 +0900

    test: Car 객체 테스트 코드 추가

--- __tests__/CarNameTest.js
+++ __tests__/CarNameTest.js
@@ -1,10 +1,31 @@
-import Car from "../src/Car";
+import Car from '../src/Car';
+import { checkLessThanFive, checkHasSpace, checkDuplicated } from '../src/Validation.js';
 
-describe("올바른 car 객체 판단 테스트", () => {  
+describe('올바른 car 객체 판단 테스트', () => {
   const carList = ['아빠차', '엄마차', '내차'];
-  
-  test("자동차 이름 테스트", () => {
+
+  test('자동차 이름 테스트', () => {
     const testList = carList.map((car) => new Car(car, 0));
-    testList.forEach((car, index) => expect(car.getName()).toEqual(carList.at(index)));
+    testList.forEach((car, index) =>
+      expect(car.getName()).toEqual(carList.at(index)),
+    );
+  });
+
+  test('[detail] 5글자가 넘을 때 오류 테스트', () => {
+    const longNameList = ['우아한테크코스', '우아한형제들', '우리가어떤민족'];
+
+    expect(() => checkLessThanFive(longNameList)).toThrow();
+  });
+
+  test('[detail] 공백포함 테스트', () => {
+    const spaceNameList = ['우아한 테크코스', '우아한 형제들', '우리가 어떤민족'];
+
+    expect(() => checkHasSpace(spaceNameList)).toThrow();
+  });
+
+  test('[detail] 중복된 이름 테스트', () => {
+    const duplicatedNameList = ['Do', 'Do', 'Your', 'Best'];
+
+    expect(() => checkDuplicated(duplicatedNameList)).toThrow();
   });
 });
