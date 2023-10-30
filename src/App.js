1934f8202ca10cc05176a967c432d0931e885abd
donggyun <donggyunkim12@gmail.com>
2023-10-30 16:32:38 +0900

    feat(App): 게임시작 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import Game from './Game.js';
+import { validateCarName, validateMoveNum } from './validateFn.js';
+
 class App {
-  async play() {}
+  async play() {
+    const game = new Game();
+
+    const carListArr = await game.init();
+    validateCarName(carListArr);
+
+    await game.createCars();
+
+    const moveNum = await game.moveQuestion();
+    validateMoveNum(moveNum);
+
+    await game.process();
+    await game.printWinner();
+  }
 }
 
 export default App;
