b1266dff20bc5d3cdfb0b03280b335839a81b931
meunrud <113085963+meunrud@users.noreply.github.com>
2023-10-30 15:32:47 +0900

    feat : 게임 메세지 상수로 관리하는 클래스 생성

---
+++ src/constant/gameMessage.js
@@ -0,0 +1,8 @@
+export const GAME_MESSAGE = {
+  INPUT_CAR_NAME:
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+  INPUT_NUMBER_OF_TIMES: '시도할 횟수는 몇 회인가요?',
+  GAME_RESULT: '실행 결과',
+};
+
+export default GAME_MESSAGE;
