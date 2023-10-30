49146c02d5f3e645394a8137188aecc958e86b4a
JUN <gurwns232@naver.com>
2023-10-30 21:29:29 +0900

    feat: 레이싱 승자 찾기 기능 추가

--- src/model/RacingGame.js
+++ src/model/RacingGame.js
@@ -7,6 +7,7 @@ class RacingGame {
     this.racingCar = [];
   }
 
+  /** @returns {Array} */
   get getRacingCar() {
     return this.racingCar;
   }
@@ -21,6 +22,21 @@ class RacingGame {
       const randomNumber = generateRandomNumber();
       if (randomNumber >= RACING_CAR.MOVE_NUMBER) car.move();
     });
+  }
+
+  /** 레이싱 승자 찾기 함수 */
+  winner() {
+    /** @type {number} carPostion 중 가장 큰 숫자 */
+    const maxPostion = this.racingCar.sort(
+      (nxt, cur) => cur.carPosition - nxt.carPosition,
+    )[0].carPosition;
+
+    /** @type {string} 레이싱 승자 (,로 연결한 문자열) */
+    const win = this.racingCar
+      .filter((car) => car.carPosition == maxPostion)
+      .map((car) => car.carName)
+      .join(', ');
+    return win;
   }
 }
 
