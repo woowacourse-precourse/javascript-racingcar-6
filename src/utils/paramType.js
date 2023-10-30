d888d07af73704882f8a27cba6c5a0e3df0a3ede
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-10-30 15:56:11 +0900

    fix : 상속받은 sub class가 super class의 형 검사가 안되는 문제 해결
    super class를 상속받은 sub class가 super 클래스의 인스턴스로 형검사가 안되어 로직 변경
    
    primitive타입 검증할땐 typeof의 반환값 'number', 'boolean' 과 같이 검사하도록 변경
    
    reference 타입은 Array, Object, class, Map, Set으로 검사가능하도록 변경

--- src/utils/paramType.js
+++ src/utils/paramType.js
@@ -4,28 +4,58 @@
  * @link https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Functions/Default_parameters
  * @warning : null과 undefined는 생성자가 없기에 원시타입을 넣어주세요 !
  * @param {*} target : 검증하고자 하는 value
- * @typedef {'String' | 'Number' | 'undefined' | 'null'} PrimitiveConstructorType
- * @typedef {'Object' | 'Array' | 'Function'} ReferenceConstructorType
- * @param {PrimitiveConstructorType | undefined | null | ReferenceConstructorType | Class} type - target의 타입 또는 생성자 함수
- * @returns {undefined}
+ * @param {TypeofPrimitiveValueType | ReferenceType } type - target의 타입 또는 생성자 함수
+ * @typedef {'string' | 'number' | 'boolean' | 'undefined' | 'null'} PrimitiveConstructorType
+ * @typedef { Object | Array | Function | Map | Set | ClassValue } ReferenceConstructorType
+ * @returns {void}
  * @throws {Error}
  * @example
  * class WooTeCo {}
  * const foo (param, _0 = paramType(param, WooTeCo)) => {} // WooTeCo
  * foo('학생 왕돈까스 먹어') // Error (WooTeCo 형이 들어와야하는데 string 타입이 옴)
  */
+
 export const paramType = (target, type) => {
-  if (type === undefined || type === null) {
-    if (target !== type) {
+  if (isNull(target)) {
+    if (type === null) {
+      throw new Error('[ERROR] null check는 type에 "null" 로 입력해주세요');
+    }
+    if (type !== 'null') {
+      throw new Error(`[ERROR] target은 null인데 type이 ${type}이 아닙니다`);
+    }
+    return;
+  }
+
+  if (isPrimitive(type)) {
+    if (typeof target !== type) {
       throw new Error(
         `[ERROR] target : ${target}은 (${type}) 타입이 아닙니다 !`
       );
     }
+    return;
   }
 
-  if (target.constructor !== type) {
-    throw new Error(
-      `[ERROR] target : ${target}은 (${type.constructor.name}) 타입이 아닙니다 !`
-    );
+  if (isReference(type)) {
+    if (type === Object && target.constructor !== Object) {
+      throw new Error(
+        `[ERROR] ${target.constructor.name}으로 타입검사를 해주세요`
+      );
+    }
+
+    if (!(target instanceof type)) {
+      throw new Error(
+        `[ERROR] target : ${target.constructor.name}은 (${type}) 타입이 아닙니다 !`
+      );
+    }
   }
 };
+
+const isNull = (target) => {
+  return target === null;
+};
+
+const isPrimitive = (type) => {
+  return typeof type === 'string';
+};
+
+const isReference = (type) => true;
