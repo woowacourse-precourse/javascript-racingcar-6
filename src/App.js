321f4b3fdc79dbc3dc456734a60b1e37a314631d
JoSooha <dlrhdcjs@naver.com>
2023-10-30 21:11:28 +0900

    feat: 한 턴에 각 자동차의 랜덤 숫자에 따라 전진 여부 결정 + string 출력

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
+  constructor() {
+    this.carObj = {};
+  }
+  
   async play() {
     const carInput = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
-    const carArray = this.handleCarName(carInput);
+    this.handleCarName(carInput);
     const tryInput = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
-    // const tryNumber = 
+    const tryNumber = this.handleTryNumber(tryInput);
+    
+  }
 
+  oneTurn(carArray) {
+    for(let i=0; i<carArray.length; i++) {
+      const car = carArray[i];
+      oneTurnEachCar(eachCar); //전역변수에 각 자동차 전진여부에 따라 - 붙임    
+      MissionUtils.Console.print(`${car} : ${this.carObj[car]}`)
+    }
+  }
+
+  oneTurnEachCar(car) {
+    const carRandomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
+    if(carRandomNumber >= 4) {
+      this.carObj[car].concat("-"); 
+    }
   }
 
   handleCarName(input) {
     try {
       const arr = input.split(",");
       arr.forEach(e=>{
+        this.carObj[e] = ""; //객체 초기화
         if(e.length>6) throw new Error("[ERROR] 자동차 이름은 5자 이하여야 합니다.")
       })
       return arr;
@@ -25,7 +45,7 @@ class App {
   handleTryNumber(input) {
     try {
       console.log(typeof(input)) //
-      if(input >= 48 && input <= 57) {
+      if(typeof(input)==='number') {
         return Number(input)
       } else throw new Error("[ERROR] 횟수는 숫자여야 합니다. ")
     } catch(error) {
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import App from "./App.js";
 
 const app = new App();
-app.handleTryNumber(3);
+app.handleTryNumber("hh");
