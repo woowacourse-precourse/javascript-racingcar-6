78e326ce523f3c6111c09092964adfb85be9f3b7
이용성 <ukal5006@kakao.com>
2023-10-30 14:54:25 +0900

    refactor: extract car name related functions into CarNames.js

---
+++ src/CarNames.js
@@ -0,0 +1,41 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+async function getAndValidateCarNames() {
+  const carNames = await getCarNames();
+  const carNamesArray = convertNamesToArray(carNames);
+  validateCarNames(carNamesArray);
+  return carNamesArray;
+}
+
+async function getCarNames() {
+  const carNames = await MissionUtils.Console.readLineAsync(
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+  );
+  return carNames;
+}
+
+function convertNamesToArray(carNames) {
+  const carNamesArray = carNames.split(',');
+  return carNamesArray;
+}
+
+function validateCarNames(carNamesArray) {
+  const pattern = /^[a-z]{1,5}$/;
+  const uniqueCarNamesArray = [...new Set(carNamesArray)];
+
+  carNamesArray.forEach((carName) => {
+    if (!pattern.test(carName)) {
+      throw new Error('[ERROR] 이름의 형식이 잘못됐습니다.');
+    }
+  });
+
+  if (carNamesArray.length < 2) {
+    throw new Error('[ERROR] 이름은 두 개 이상 입력해야 합니다.');
+  }
+
+  if (carNamesArray.length !== uniqueCarNamesArray.length) {
+    throw new Error('[ERROR] 이름은 중복될 수 없습니다.');
+  }
+}
+
+export default getAndValidateCarNames;
