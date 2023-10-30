eca36d130b4af61615e99f2d611f9d900bd50f89
silver0108 <kk1170@naver.com>
2023-10-30 21:59:57 +0900

    feat: Car 클래스 생성

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import { inputCarName } from "./InputCarName.js";
 import { invaildCarName } from "./InvalidCarName.js";
+import Car from "./Car.js";
 class App {
   async play() {
     const carNames = await inputCarName();
 
     carNames.split(',').map(name => {
       if(invaildCarName(name)){
-        // const car = new Car(name);
+        const car = new Car(name);
+        // MissionUtils.Console.print(car.name)
       }
     })
   }
