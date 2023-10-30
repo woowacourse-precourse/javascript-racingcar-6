f3a31aabd62a77e97daf5561fd074966d8febfb4
sihyun10 <hyhy200170@gmail.com>
2023-10-30 18:21:31 +0900

    feat: 게임 완료 후 최종 우승자 출력하는 기능 구현 자동차 경주 게임을 완료한 후에 최종 우승자를 출력해주는 기능을 구현하였습니다. 우승자가 한명인 경우는 그 우승자만 출력해주며, 우승자가 여러명일 경우 쉼표(,)를 이용해 구분해 출력하도록 코드를 구현해주었습니다.

--- src/App.js
+++ src/App.js
@@ -23,6 +23,26 @@ class App {
       MissionUtils.Console.print(result);
       MissionUtils.Console.print(' ');
     });
+
+    const lastRaceResult = raceResults[raceResults.length - 1];
+    const lastCarResults = lastRaceResult.split('\n').map((line) => {
+      const [name, position] = line.split(' : ');
+      return { name, position };
+    });
+
+    const maxPositionCount = Math.max(
+      ...lastCarResults.map((car) => car.position.split('-').length - 1),
+    );
+    const winners = lastCarResults.filter(
+      (car) => car.position.split('-').length - 1 === maxPositionCount,
+    );
+    const winnerNames = winners.map((winner) => winner.name);
+
+    if (winners.length > 1) {
+      MissionUtils.Console.print('최종 우승자 : ' + winnerNames.join(', '));
+    } else {
+      MissionUtils.Console.print('최종 우승자 : ' + winnerNames[0]);
+    }
   }
 }
 
