baf5fb88d2fc2c6726c7033171d3bbfd961833d1
JUNU <bokjunwoo@gmail.com>
2023-10-30 20:32:44 +0900

    refactor: RacingGame 클래스 업데이트 (private 필드 추가)

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -5,31 +5,34 @@ import RacingCar from './RacingCar.js';
 import { GAME_MESSAGES } from './constants.js';
 
 class RacingGame {
+  #cars;
+  #tryCount;
+
   constructor() {
-    this.cars = [];
-    this.tryCount = 0;
+    this.#cars = [];
+    this.#tryCount = 0;
   }
 
   async setupGame() {
     const carNames = await InputView.inputGetCarNames();
     carNames.forEach((name) => this.addCar(name));
 
-    this.tryCount = await InputView.inputgetTryCount();
+    this.#tryCount = await InputView.inputgetTryCount();
   }
 
   addCar(name) {
-    this.cars.push(new RacingCar(name));
+    this.#cars.push(new RacingCar(name));
   }
 
   play() {
     MissionUtils.Console.print('');
     MissionUtils.Console.print(GAME_MESSAGES.EXECUTION_RESULT);
-    for (let i = 0; i < this.tryCount; i++) {
-      this.cars.forEach((car) => car.move());
-      OutputView.printRoundResults(this.cars);
+    for (let i = 0; i < this.#tryCount; i++) {
+      this.#cars.forEach((car) => car.move());
+      OutputView.printRoundResults(this.#cars);
     }
 
-    OutputView.printFinalResult(this.cars);
+    OutputView.printFinalResult(this.#cars);
   }
 }
 
