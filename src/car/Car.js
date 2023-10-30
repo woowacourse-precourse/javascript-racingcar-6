1af613beaabef3187ef56881161ef176914567bc
JungHun98 <j7papa@naver.com>
2023-10-30 12:18:59 +0900

    feat: Car Class 구현 - move: 위치 이동 - getName: 이름 반환 - getPosition: 현재 위치 반환

--- src/car/Car.js
+++ src/car/Car.js
@@ -1,7 +1,28 @@
+import getRandomNumber from '../utils/getRandomNumber';
+
+const MOVING_FORWARD = 4;
+
 class Car {
   constructor(name) {
     this.name = name;
     this.position = 0;
+  }
+
+  move() {
+    const randomNumber = getRandomNumber();
+    const isMoved = randomNumber >= MOVING_FORWARD;
+
+    if (isMoved) {
+      this.position += 1;
+    }
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getPosition() {
+    return this.position;
   }
 }
 
