a5fdf0cb1d3d185b88f4c24fcbabede9bb8f099b
hamo-o <yheel369@gmail.com>
2023-10-31 00:42:01 +0900

    refactor: 입력메시지 상수 분리

--- src/Input.js
+++ src/Input.js
@@ -1,10 +1,11 @@
 import { Console } from '@woowacourse/mission-utils';
 import ERROR from './constants/Error.js';
+import MESSAGE from './constants/Message.js';
 
 const Input = {
   async getCarNames() {
     const names = await Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+      MESSAGE.carName,
     );
 
     if (!names) {
@@ -15,7 +16,7 @@ const Input = {
   },
 
   async getRepeatTimes() {
-    const times = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    const times = await Console.readLineAsync(MESSAGE.attemptNum);
 
     const pattern = /^[1-9]d*$/;
     if (!pattern.test(times)) {
---
+++ src/constants/Message.js
@@ -0,0 +1,7 @@
+const MESSAGE = {
+  carName: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+  attemptNum: '시도할 횟수는 몇 회인가요?\n',
+};
+Object.freeze(MESSAGE);
+
+export default MESSAGE;
