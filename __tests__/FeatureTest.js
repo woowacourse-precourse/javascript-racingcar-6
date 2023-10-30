ecc393c26bd5fcb9ef7557f47185367ecc0fb43b
seok-pyo <hongseokpyou@gmail.com>
2023-10-30 14:13:48 +0900

    feat: 기능 테스트 작성, story1.자동차 입력

--- __tests__/FeatureTest.js
+++ __tests__/FeatureTest.js
@@ -1,18 +1,32 @@
 import App from '../src/App.js';
 import { MissionUtils } from '@woowacourse/mission-utils';
 
-const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, "print");
-  logSpy.mockClear();
-  return logSpy;
+const mockQuestions = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  });
 };
 
-// describe("기능 테스트", () => {
-//   test("Lap.printStage 메서드 테스트", () => {
-//     const array = "1,2";
-//     const result = input.split(",");
+describe("story1. 자동차 이름 입력", () => {
+  test("입력을 받아서 문자열을 반환한다.", async () => {
+    // given
+    const input = '산,바다,강,하늘';
+    const answer = '산,바다,강,하늘';
 
-//     expect(result).toContain("2", "1");
-//     expect(result).toContainEqual("1", "2");
-//   });
-// });
+    mockQuestions(input);
+
+    // when
+    expect(answer).toEqual('산,바다,강,하늘');
+  });
+
+  test("문자열을 받아서 배열로 반환한다.", () => {
+    const input = "산,바다,강,하늘";
+    const result = input.split(",");
+
+    expect(result).toContain("산", "바다", "강", "하늘");
+    expect(result).toContainEqual("산", "바다", "강", "하늘");
+  });
+});
