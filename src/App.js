fcba2328ea43f85a19d244d0b3f0c3adab1aa9e0
hwsg0912@naver.com <hwsg0912@naver.com>
2023-10-30 14:13:44 +0900

    feat: 사용자에게 입력 받은 자동차 이름 배열로 저장 구현

--- src/App.js
+++ src/App.js
@@ -4,7 +4,19 @@ import { Messages } from "./Constants/Messages.js";
 class App {
   async play() {
     MissionUtils.Console.print(Messages.INPUT_CARNAME);
+
+    const carNameInput = await MissionUtils.Console.readLineAsync('');
   }
+  
+  async carName(carNameInput) {
+    let carNameArr = carNameInput.split(',');
+
+    return carNameArr;
+  }
+
 }
+
+const app = new App();
+app.play();
 
 export default App;
