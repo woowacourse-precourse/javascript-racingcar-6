feb2342890e03d8324fb3b04eae23d020c74985f
devGEP <ju0032564@gmail.com>
2023-10-31 01:18:09 +0900

    feat: 사용자에게 시도할 횟수 입력받기 기능
    [Game.js] IOManager.js에서 getRaceCount를 호출하고 결과를 출력
    [IOManager.js] getRaceCount() 함수를 통해 입력 받고 리턴

--- src/game/Game.js
+++ src/game/Game.js
@@ -1,14 +1,18 @@
 import { Console } from '@woowacourse/mission-utils';
+import Race from './Race.js';
 import IOManager from '../utils/IOManager.js';
 
 class Game {
   constructor() {
     this.ioManager = new IOManager();
+    this.race = new Race();
   }
 
   async playGame() {
     const carsName = await this.ioManager.getCarsName();
-    Console.print('carsName: ', carsName);
+    const raceCount = await this.ioManager.getRaceCount();
+
+    Console.print('carsName: ' + carsName + ', raceCount: ' + raceCount);
   }
 }
 
--- src/utils/IOManager.js
+++ src/utils/IOManager.js
@@ -9,6 +9,10 @@ class IOManager {
     const cars = await this.getUserInput('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) ');
     return cars.split(',').map((name) => name.trim());
   }
+
+  async getRaceCount() {
+    return await this.getUserInput('시도할 횟수는 몇 회인가요? ');
+  }
 }
 
 export default IOManager;
