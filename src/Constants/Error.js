d2e9771b1ad26c5e9c4386aa287f741856aaf870
dooohun <ehgns0305@naver.com>
2023-10-30 22:49:25 +0900

    refactor: 에러 문구 상수로 수정

---
+++ src/Constants/Error.js
@@ -0,0 +1,3 @@
+export const CAR_NAME_LENGTH_ERROR = '[ERROR] 자동차 이름의 문자가 5글자를 초과했습니다.';
+export const INVALID_MOVE_COUNT = '[ERROR] 입력한 값이 음수이거나 0 입니다.';
+export const INVALID_INPUT_TYPE = '[ERROR] 입력한 값이 문자입니다.';
