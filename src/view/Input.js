de6c15c3493ef13856428febc512d36200be1a21
ChanYong Park <krap5075@naver.com>
2023-10-31 04:14:02 +0900

    refactor: Input클래스에 예외처리 메서드 호출 추가

--- src/view/Input.js
+++ src/view/Input.js
@@ -1,18 +1,26 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import Error from "../error/Error";
 
 const Console = MissionUtils.Console;
 
 class Input {
+  constructor() {
+    this.error = new Error();
+  }
   async inputCarNames() {
     const carInput = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
+    this.error.checkCarName(carInput);
+    // 예외처리 통과해야 return이 실행
     return carInput.split(",").map((name) => name.trim());
   }
   async inputTryNumber() {
     const numInput = await Console.readLineAsync(
       "시도할 횟수는 몇 회인가요?\n"
     );
+    this.error.checkTryNumber(numInput);
+    // 예외처리 통과해야 return이 실행
     return numInput;
   }
 }
