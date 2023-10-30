66ae6c485a661a0d3e770275dc4d88b9ad5ca12e
yesiamok3 <jaehyunok@cau.ac.kr>
2023-10-31 02:51:27 +0900

    feat: Car 클래스 추가
    name, position 속성을 가진 Car 클래스 추가

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+export class Car {
+  name;
+  position;
+  constructor(name) {
+    this.name = name;
+    this.position = 0;
+  }
+}
 class App {
   async play() {}
 }
