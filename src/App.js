c5f264e408c6067f1e7309d5165b44575e17063d
Baek <woonn97@gmail.com>
2023-10-30 22:01:19 +0900

    feat: 0에서 9사이의 무작위 값
    * MissionUtils.Random.pickNumberInRange(0, 9)로 무작위값 추출
    * 무작위 값이 4이상이면 position값 1이 증가

--- src/App.js
+++ src/App.js
@@ -8,7 +8,18 @@ class App {
     const nameInput = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     this.cars = nameInput.split(",").map((name) => ({ name, position: 0 }));
     const tryCount = await Console.readLineAsync("시도할 회수는 몇회인가요?");
-    Console.print(this.cars);
+    for (let i = 0; i < tryCount; i++) {
+      this.randomMove();
+      Console.print(this.cars);
+    }
+  }
+  randomMove() {
+    this.cars.forEach((car) => {
+      if (MissionUtils.Random.pickNumberInRange(0, 9) >= 4) {
+        // 랜덤 숫자가 4 이상일 경우 전진
+        car.position += 1;
+      }
+    });
   }
 }
 
