a84ff966d71777d41dd678fdd4254327f51894a5
coldok <2coldok@gmail.com>
2023-10-30 20:23:18 +0900

    feat : 컨버터 테스트 코드 추가 및 테스트 예시 추가

--- __tests__/GameCustomizeTest.js
+++ __tests__/GameCustomizeTest.js
@@ -2,7 +2,7 @@ import { getPlayersName, getAttemptNumber } from "../src/racing/GameCustomize.js
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 //값을 넣을때 [] 한겹 빼주기
-const mockQuestions = (inputs) => {
+const mockQuestions2 = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
 
   MissionUtils.Console.readLineAsync.mockImplementation(() => {
@@ -11,7 +11,7 @@ const mockQuestions = (inputs) => {
   });
 };
 //값을 넣을때 [] 필요
-const mockQuestions2 = (inputs) => {
+const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
 
   MissionUtils.Console.readLineAsync.mockImplementation(() => {
@@ -27,7 +27,7 @@ describe('비동기 유저 입력값 유효성 및 오류 잡아내는지 테스
     const userInput = "pobi,crong,woong";
     const result = ["pobi","crong","woong"];
     
-    mockQuestions(userInput);
+    mockQuestions2(userInput);
     
     await expect(getPlayersName()).resolves.toEqual(result);
   })
@@ -36,7 +36,7 @@ describe('비동기 유저 입력값 유효성 및 오류 잡아내는지 테스
   test('플레이어 이름 오류났을때', async () => {
     const userInput = "pobi,";
     
-    mockQuestions(userInput);
+    mockQuestions2(userInput);
 
     await expect(getPlayersName()).rejects.toThrow('[ERROR]');
   })
@@ -45,24 +45,24 @@ describe('비동기 유저 입력값 유효성 및 오류 잡아내는지 테스
     const userInput = '1004';
     const result = 1004;
 
-    mockQuestions(userInput);
+    mockQuestions2(userInput);
 
     await expect(getAttemptNumber()).resolves.toEqual(result);
   })
 
-  //mockQuestions2 사용
+  //mockQuestions 사용
   test.each([
     [["pobi,"]],
     [["pobi,crong,,"]],
     [["pobi,cronnnng"]],
     [["pobi,,crong"]]
-  ])('자동차 이름 규칙에 위반되는 것들', async (inputs) => {
+  ])('자동차 이름 예외 테스트', async (inputs) => {
 
-    mockQuestions2(inputs);
+    mockQuestions(inputs);
 
     await expect(getPlayersName()).rejects.toThrow("[ERROR]");
   })
-  //mockQuestions 사용
+  //mockQuestions2 사용
   test.each([
     ["pobi"],
     ["chanwoong,pobi"],
@@ -70,9 +70,9 @@ describe('비동기 유저 입력값 유효성 및 오류 잡아내는지 테스
     [",pobi"],
     ["pobi,,crong"],
     [",pobi,chanwoong,,crong,"]
-  ])('자동차 이름 규칙에 위반되는 것들', async (inputs) => {
+  ])('자동차 이름 예외 테스트', async (inputs) => {
 
-    mockQuestions(inputs);
+    mockQuestions2(inputs);
     await expect(getPlayersName()).rejects.toThrow("[ERROR]");
   })
 
--- __tests__/RacingControllerTest.js
+++ __tests__/RacingControllerTest.js
@@ -14,15 +14,15 @@ const getLogSpy = () => {
   return logSpy;
 };
 
-
-test('레이싱 게임 컨트롤러 테스트', () => {
+test('레이싱 게임 컨트롤러 2사이클 테스트', () => {
   
   const playersArray = ['이찬웅','한병학','박병학'];
-  const attemptNumber = 1;
-  const outputs = ['실행 결과', '이찬웅 : ','한병학 : ', '박병학 : -', '최종 우승자 : 박병학'];
+  const attemptNumber = 2;
+  const outputs = ['실행 결과','이찬웅 : ','한병학 : ','박병학 : -','이찬웅 : -','한병학 : -','박병학 : -','최종 우승자 : 이찬웅, 한병학, 박병학'];
+  const random = [1,2,4,7,8,0];
   const logSpy = getLogSpy();
-
-  mockRandoms([1,2,4]);
+  
+  mockRandoms([...random]);
 
   const racingGame = new RacingController(playersArray,attemptNumber);
   racingGame.startRacing();
@@ -31,3 +31,5 @@ test('레이싱 게임 컨트롤러 테스트', () => {
     expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
   });
 })
+
+
