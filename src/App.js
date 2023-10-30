1fa5a8519de7fbb7422b46c3f678f76bb5371a33
hectick <ch.yeondev@gmail.com>
2023-10-30 22:28:36 +0900

    feat: 자동차 이름이 5자 이하인지 검증하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
+
+  validateCarNames = (carNames) => {
+    for(var carName of carNames) {
+      if(carName.length > 5) {
+        throw new Error("[Error] 자동차 이름은 5자 이하만 가능합니다.")
+      }
+    }
+  }
+
   async play() {
     const input = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n")
     const carNames = input.split(",")
+    this.validateCarNames(carNames)
   }
 }
 
