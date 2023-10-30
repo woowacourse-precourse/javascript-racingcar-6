02e11d7308835cf522f2a5be8e464740ff96f512
hj7321 <oboki2001@naver.com>
2023-10-31 02:11:12 +0900

    test: Test_getCarNames 테스트 코드 수정

--- __tests__/Test_getCarNames.js
+++ __tests__/Test_getCarNames.js
@@ -1,6 +1,14 @@
 import App from "../src/App.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
 
+const mockQuestions = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  });
+};
+
 describe("getCarNames 함수", () => {
   beforeAll(() => {
     MissionUtils.Console.readLineAsync = jest.fn();
@@ -22,23 +30,28 @@ describe("getCarNames 함수", () => {
   test("이름을 2개 이상 입력하지 않은 경우(예외)", async () => {
     MissionUtils.Console.readLineAsync.mockResolvedValue("car1");
     const app = new App();
-    await expect(app.getCarNames()).rejects.toThrow("이름은 2개 이상 입력해야 합니다.");
+    try {
+      await app.getCarNames();
+    } catch (error) {
+      expect(error).toThrowError("이름은 2개 이상 입력해야 합니다.");
+    }
   });
 
-  test.each([
-    [["car1&car2&car3"]],
-    [["car1 car2 car3"]]
-  ])("이름을 ,(콤마)로 구분하지 않은 경우(예외)", async (input) => {
-    MissionUtils.Console.readLineAsync.mockResolvedValue(input);
+  test("이름을 ,(콤마)로 구분하지 않은 경우(예외)", async () => {
+    MissionUtils.Console.readLineAsync.mockResolvedValue("car1&car2&car3");
     const app = new App();
-    await expect(app.getCarNames()).rejects.toThrow("이름은 ,(콤마)로 구분합니다.");
+    try {
+      await app.getCarNames();
+    } catch (error) {
+      expect(error).toThrowError("이름은 ,(콤마)로 구분합니다.");
+    }
   });
 
   test.each([
     [["car1, carname2, car3"]],
     [["car1, car2, car3, carname4"]]
-  ])("이름을 5자 이하로 입력하지 않은 경우(예외)", async (input) => {
-    MissionUtils.Console.readLineAsync.mockResolvedValue(input);
+  ])("이름을 5자 이하로 입력하지 않은 경우(예외)", async (inputs) => {
+    mockQuestions(inputs);
     const app = new App();
     await expect(app.getCarNames()).rejects.toThrow("이름은 5자 이하만 가능합니다.");
   });
