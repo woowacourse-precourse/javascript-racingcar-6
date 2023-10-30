305534bb0121a354e1a54d4d6966261257361051
yebin <yebind3308@gmail.com>
2023-10-30 23:00:20 +0900

    fix: 2번째 이후 자동차 이름 예외처리 수정

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App {
     Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     const carUserInput = await Console.readLineAsync("");
     const carName = carUserInput.split(",");
-    if (carName.some(name => name.trim() === "" || carName.length <= 1 || carName.length > 5)) {
+    if (carName.some(name => name.trim() === "" || name.length <= 1 || name.length > 5)) {
       throw new Error("[ERROR] 이름이 잘못된 형식입니다.");
     }
     return carName; 
