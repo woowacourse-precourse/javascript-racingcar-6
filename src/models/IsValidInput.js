3c0106b6e4d179428c297262b7ece734c577be1a
Youjiiin <yujinjang24@gmail.com>
2023-10-30 11:06:17 +0900

    feat: 자동차 이름의 유효성 요소 추가 - 잘못된 쉼표의 사용

--- src/models/IsValidInput.js
+++ src/models/IsValidInput.js
@@ -2,30 +2,36 @@ import NAME_ERROR from "../constants/ErrorMsg";
 
 //입력받은 자동차의 이름 유효성 검사
 export const isValidInput = (name) => {
-    const nameArray = name.split(',');
+    //1. NULL값인 경우
+    if ( name === null ) {
+        throw new Error( NAME_ERROR.NULL_ERROR );
+    }
 
-    //1. ','로 구분되있지 않을 경우
+    //2. ','로 구분되있지 않을 경우
     if ( !name.inculdes(',') ) {
         throw new Error( NAME_ERROR.ONE_NAME_ERROR );
     }
+
+    const nameArray = name.split(',');
     
-    //2. 5글자 이상일 경우
     for ( let i = 0; i < nameArray.length; i++ ) {
+        //3. 5글자 이상일 경우
         if ( nameArray[i].length > 5 ) {
             throw new Error( NAME_ERROR.LENGTH_ERROR );
         }
+
+        //4. 잘못된 쉼표의 사용
+        if ( nameArray[i] === "" || nameArray[i] === " " ) {
+            throw new Error( NAME_ERROR.COMMA_ERROR );
+        }
     }
         
-    //3. 중복된 이름이 있을 경우
+    //5. 중복된 이름이 있을 경우
     let nameSet = new Set(nameArray);
     if ( nameArray.length !== nameSet.length ) {
         throw new Error( NAME_ERROR.SAME_NAME_ERROR );
     }
 
-    //4. NULL값인 경우
-    if ( name === null ) {
-        throw new Error( NAME_ERROR.NULL_ERROR );
-    }
 };
 
 //입력받은 시도횟수의 유효성 검사
