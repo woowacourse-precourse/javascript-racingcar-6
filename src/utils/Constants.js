f2ea4b8490312da4dc5116cae23c5a6983cc3953
kimsuyeon0916 <kimsuyeon_09162naver.com>
2023-10-30 20:56:13 +0900

    feat: ERROR_MESSAGE 상수화
    - null, empty, long 추가

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -8,7 +8,9 @@ const IN_GAME_MESSAGE = {
 const FORWARD_CRITERIA = 4;
 
 const ERROR_MESSAGE = {
-
+  null: '[ERROR] 아무것도 입력하지 않았습니다. 이름을 입력해 주세요.',
+  empty: '[ERROR] 이름의 길이가 0 입니다. 한 글자 이상의 이름을 입력해 주세요.', 
+  long: '[ERROR] 이름이 다섯 글자를 초과했습니다. 다섯 글자 이하의 이름을 입력해 주세요.',
 }
 
 export { IN_GAME_MESSAGE, FORWARD_CRITERIA, ERROR_MESSAGE};
