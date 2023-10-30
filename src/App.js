f0f23eb090820f1cf01827f3e982a27f1690b4d7
김경주 <popo505@naver.com>
2023-10-30 18:09:05 +0900

    fix: 자동차 입력받는 readLineAsync() 수정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import { Console, MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
-  start() {
-    Console.print(
+  async getCarNames() {
+    const names = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
     );
-  }
-
-  async getCarNames() {
-    const names = await Console.readLineAsync('');
     if (names === null) {
       throw new Error('[ERROR] 올바른 값을 입력하세요');
     }
