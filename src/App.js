94c916cfa5ac78baa7938f4867d866d6f73a74f8
greeneeya <suzythetravelerr@gmail.com>
2023-10-30 16:40:34 +0900

    feat: 이름이 5자 초과일 경우, 에러를 발생시킨다

--- src/App.js
+++ src/App.js
@@ -17,6 +17,11 @@ class App {
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
     );
     const cars = user.split(',');
+    cars.forEach((car) => {
+      car = car.toString();
+      if (car.length > 5)
+        throw new Error('[ERROR] 자동차 이름은 5자 이하만 가능합니다.');
+    });
     return cars;
   }
 }
