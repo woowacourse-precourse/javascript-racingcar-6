1888058888c1775e17565ba9e0d444c0ab3bcea0
DongWook Kang <rkdehddnr15123@gmail.com>
2023-10-30 11:06:35 +0900

    refactor: 에러 리팩토링

--- src/validInput.js
+++ src/validInput.js
@@ -1,5 +1,9 @@
 function validPlayerInput(players) {
-  const filterPlayers = players.filter((player, i) => player.length > 5 || players.indexOf(player) !== i);
+  const filterPlayers = players.filter((player, i) => player.length > 5 || players.indexOf(player) !== i || player.length === 0);
+
+  if (players.length < 2) {
+    throw new Error("[ERROR] 플레이어는 2명 이상이여야 합니다");
+  }
 
   if (filterPlayers.length !== 0) {
     throw new Error("[ERROR] 올바른 형식으로 이름을 입력하세요");
