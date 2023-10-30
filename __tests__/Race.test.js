2edf17511e3547caaf9a055d9d0005e3cddb5f1d
lbj23han <ljhan0215@gmail.com>
2023-10-30 20:25:28 +0900

    feat: 테스트코드 작성 2차(Race.test) 추가

--- __tests__/Race.test.js
+++ __tests__/Race.test.js
@@ -1,16 +1,46 @@
-// __tests__/Race.test.js
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
 import Race from "../src/Race";
 
-// MissionUtils.Random.pickNumberInRange 함수가 호출되는지 확인하는 테스트
-test("moveCars 메서드 내에서 MissionUtils.Random.pickNumberInRange 함수를 호출한다.", () => {
-  // given
-  const race = new Race(["car1", "car2"], 1);
-  MissionUtils.Random.pickNumberInRange = jest.fn().mockReturnValue(5);
+jest.mock("@woowacourse/mission-utils", () => {
+  return {
+    Console: {
+      print: jest.fn(),
+    },
+    MissionUtils: {
+      ...jest.requireActual("@woowacourse/mission-utils").MissionUtils,
+      Random: {
+        pickNumberInRange: jest.fn(),
+      },
+    },
+  };
+});
 
-  // when
-  race.moveCars();
+describe("Race 클래스 테스트", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-  // then
-  expect(MissionUtils.Random.pickNumberInRange).toHaveBeenCalledWith(0, 9);
+  test("moveCars 메서드 내에서 MissionUtils.Random.pickNumberInRange 함수를 호출한다.", () => {
+    // given
+    const race = new Race(["car1", "car2"], 1);
+    MissionUtils.Random.pickNumberInRange = jest.fn().mockReturnValueOnce(5); // 한 번 호출될 때만 5를 반환하도록 설정
+
+    // when
+    race.moveCars();
+
+    // then
+    expect(MissionUtils.Random.pickNumberInRange).toHaveBeenCalledWith(0, 9);
+  });
+
+  test("determineWinners 메서드가 올바르게 최종 우승자를 결정한다.", () => {
+    // given
+    const race = new Race(["car1", "car2", "car3"], 3);
+    race.POSITIONS = [3, 4, 2]; // 특정 라운드의 자동차 위치 설정
+
+    // when
+    const winners = race.determineWinners();
+
+    // then
+    expect(winners).toEqual(["car2"]); // car2가 최종 우승자여야 함
+  });
 });
