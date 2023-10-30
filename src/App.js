2b05a3a715e958689181d31841d2b3fb7243b9fa
youngchann <lych96@gmail.com>
2023-10-30 20:03:33 +0900

    feat: 자동차 이름 및 이동 횟수 입력 받기 기능 추가
    - 사용자에게 입력  받아서 상수로 저장

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 class App {
   constructor() {
     this.startMessage =
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)';
-    this.countMessage = '시도할 횟수는 몇 회인가요?';
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n';
+    this.countMessage = '시도할 횟수는 몇 회인가요?\n';
   }
 
   async play() {
-    await MissionUtils.Console.print(this.startMessage);
+    const userCar = await MissionUtils.Console.readLineAsync(this.startMessage);
+    const userCount = await MissionUtils.Console.readLineAsync(
+      this.countMessage,
+    );
   }
 }
 
