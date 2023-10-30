614415feaca6621112f770a4b4168397ef7c3950
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2023-10-31 00:51:46 +0800

    feat: edit UserInput Exceptions

--- src/Game.js
+++ src/Game.js
@@ -1,18 +1,21 @@
 import { Console } from '@woowacourse/mission-utils';
 import { enterCarNames, enterAttempts } from './UserInput.js';
 import { printResultTitle, printEachResult, printWinner } from './PrintOutput.js';
-import { RandomNumbers } from './RandomNumbers.js';
+import { RandomNumbers } from './RandomNumber.js';
 
 
 
 function Game() {
     let racingCars = new Map();
+    let carNames = enterCarNames();
+    let attempt = enterAttempts();
     printResultTitle(); 
-    for (let i = 0; i < enterAttempts(); i++) {
-        printEachResult(enterCarNames(), racingCars);
+    for (let i = 0; i < attempt; i++) {
+        printEachResult(carNames, racingCars);
         Console.print('\n');
     }
     const maxRate = Array.from(racingCars.values()).reduce((a, b) => Math.max(a, b));
+    console.log(racingCars);
     printWinner(maxRate, racingCars);
 }
 
--- src/UserInput.js
+++ src/UserInput.js
@@ -1,20 +1,20 @@
 import { Console } from '@woowacourse/mission-utils';
 // 사용자로부터 게임에 필요한 값을 입력받는 역할
 
-function enterCarNames() {
-    const carNames = Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+async function enterCarNames() {
+    const carNames = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
     if (carNames.split(',').length < 2) throw new Error('[ERROR] 자동차 이름이 잘못된 형식입니다.');
-    if (carNames.split(',').forEach((name) => name.length > 5)) throw new Error('[ERROR] 자동차 이름이 잘못된 형식입니다.');
+    if (carNames.split(',').some((name) => name.length > 5)) throw new Error('[ERROR] 자동차 이름이 잘못된 형식입니다.');
 
     return carNames.split(',');
 }
 
-function enterAttempts() {
-    const attempts = Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
-    if (attempts < 1) throw new Error('[ERROR] 시도할 횟수가 잘못된 형식입니다.');
-    if (isNaN(attempts)) throw new Error('[ERROR] 시도할 횟수가 잘못된 형식입니다.');
+async function enterAttempts() {
+    const attempt = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+    if (attempt < 1) throw new Error('[ERROR] 시도할 횟수가 잘못된 형식입니다.');
+    // if (isNaN(attempt)) throw new Error('[ERROR] 시도할 횟수가 잘못된 형식입니다.');
 
-    return Number(attempts);
+    return Number(attempt);
 }
 
 
