05fac880c1315d57dfb18102ea1ae8aa89d2cbe4
손진영 <sm099@naver.com>
2023-10-30 19:50:16 +0900

    test(RacingCar): 테스트 케이스 들의 전체적인 형태를 변경한다.
    1. initialInfo와 randomNumber를 별도로 두지 않고 input 객체 내 함께 관리되도록 변경한다.
    
    2. 명확한 네이밍을 위해 initialInfo를 racingCarInfo로 변경한다.

--- __tests__/models/RacingCar.test.js
+++ __tests__/models/RacingCar.test.js
@@ -3,31 +3,39 @@ import { RacingCar } from '../../src/models/index.js';
 describe('RacingCar 테스트', () => {
   test.each([
     {
-      initialInfo: { position: 0 },
-      randomNumber: RacingCar.MOVE_THRESHOLD,
-      expected: { position: 1 },
+      input: {
+        racingCarInfo: { carName: 'A', position: 0 },
+        randomNumber: RacingCar.MOVE_THRESHOLD,
+      },
+      expected: { carName: 'A', position: 1 },
     },
     {
-      initialInfo: { position: 0 },
-      randomNumber: RacingCar.MOVE_THRESHOLD - 1,
-      expected: { position: 0 },
+      input: {
+        racingCarInfo: { carName: 'B', position: 0 },
+        randomNumber: RacingCar.MOVE_THRESHOLD - 1,
+      },
+      expected: { carName: 'B', position: 0 },
     },
     {
-      initialInfo: { position: 0 },
-      randomNumber: RacingCar.MOVE_THRESHOLD + 1,
-      expected: { position: 1 },
+      input: {
+        racingCarInfo: { carName: 'C', position: 0 },
+        randomNumber: RacingCar.MOVE_THRESHOLD + 1,
+      },
+      expected: { carName: 'C', position: 1 },
     },
     {
-      initialInfo: { position: 5 },
-      randomNumber: RacingCar.MOVE_THRESHOLD,
-      expected: { position: 6 },
+      input: {
+        racingCarInfo: { carName: 'D', position: 5 },
+        randomNumber: RacingCar.MOVE_THRESHOLD,
+      },
+      expected: { carName: 'D', position: 6 },
     },
   ])(
-    '입력 받은 position이 $initialInfo.position이고 랜덤한 숫자가 $randomNumber일 때, 예상 position은 $expected.position 이다.',
-    ({ initialInfo, randomNumber, expected }) => {
+    '입력 받은 자동차의 position이 $input.racingCarInfo.position 이고 랜덤 숫자가 $input.randomNumber일 때, 예상 position은 $expected.position 이다.',
+    ({ input: { randomNumber, racingCarInfo }, expected }) => {
       // given
       const mockRandomGenerator = jest.fn(() => randomNumber);
-      const racingCar = new RacingCar(initialInfo, mockRandomGenerator);
+      const racingCar = new RacingCar(racingCarInfo, mockRandomGenerator);
 
       // when
       const updateRacingCarInfo = racingCar.move();
