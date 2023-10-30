129dfda442f2df66cc359eecca97e377910a8329
seoye0ng <bsy0313@gmail.com>
2023-10-31 00:43:38 +0900

    Feat: 입력받은 자동차 이름에 대해 유효성 검사하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -13,6 +13,17 @@ class App {
 
     return carNamesArr;
   }
+
+  // 자동차 이름 검증하기
+  validateCarName(carName) {
+    if (!carName) throw new Error('[ERROR] 입력값이 없습니다.');
+    if (!/^[A-Za-z]+$/.test(carName))
+      throw new Error(
+        '[ERROR] 자동차 이름은 공백없이 알파벳 문자만 포함해야 합니다.',
+      );
+    if (carName.length > 5)
+      throw new Error('[ERROR] 자동차 이름은 5자리 이하여야 합니다.');
+  }
 }
 
 export default App;
