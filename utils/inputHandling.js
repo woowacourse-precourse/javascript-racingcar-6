f10cf3aaf8b51b0cc2af81a86ef42ebc35fd2461
Hys-Lee <dltkdgus3275@gmail.com>
2023-10-31 00:11:44 +0900

    Feat: running trial number times using 'for'

--- utils/inputHandling.js
+++ utils/inputHandling.js
@@ -2,7 +2,6 @@ export const seperateCarNames = (carNameString) => {
   const result = carNameString.split(',');
   result.forEach((carName) => {
     if (carName.length > 5) {
-      console.log('wowowow');
       throw Error(
         '[ERROR] 자동차 이름은 5자 이하, 각 이름은 쉼표(,)로 구분해주세요.',
       );
