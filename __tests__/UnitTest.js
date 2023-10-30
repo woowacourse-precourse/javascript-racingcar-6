95eb04f3594f0ff7fa52f7a9c8dc43f8f1d1fc6d
stegano9744 <stegano9744@gmail.com>
2023-10-30 16:38:24 +0900

    ✅ test: 자동차 전진

--- __tests__/UnitTest.js
+++ __tests__/UnitTest.js
@@ -1,14 +1,17 @@
 import { createRandomList } from '../src/utils/createRandomList';
+import { goStopCar } from '../src/utils/goStopCar';
 import { validateLength, validateIsNumber } from '../src/utils/validate';
 
 describe('예외 테스트', () => {
   test('자동차 이름 길이 예외 검사', () => {
     const carName = 'Juru99,Juru100';
+
     validateLength(carName);
   });
 
   test('자동차 이동 횟수 예외 검사', () => {
     const moveCount = 'ab1';
+
     validateIsNumber(moveCount);
   });
 });
@@ -17,9 +20,48 @@ describe('유틸리티 함수', () => {
   test('무작위 숫자 생성', () => {
     const moveCount = '5';
     const randomList = createRandomList(moveCount);
+
     randomList.forEach((number) => {
       expect(number).toBeGreaterThanOrEqual(0);
       expect(number).toBeLessThan(10);
     });
+  });
+
+  test('자동차 전진', () => {
+    const car = [
+      {
+        name: 'pobi',
+        number: 5,
+        result: '',
+      },
+      {
+        name: 'woni',
+        number: 4,
+        result: '',
+      },
+      {
+        name: 'juru',
+        number: 1,
+        result: '',
+      },
+    ];
+
+    expect(goStopCar(car)).toEqual([
+      {
+        name: 'pobi',
+        number: 5,
+        result: '-',
+      },
+      {
+        name: 'woni',
+        number: 4,
+        result: '-',
+      },
+      {
+        name: 'juru',
+        number: 1,
+        result: '',
+      },
+    ]);
   });
 });
