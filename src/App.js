ca8ec0a8bf9a72125da2d51f88790806d604ec5b
kwani6684 <jack0910@naver.com>
2023-10-30 10:41:56 +0900

    feat: solve exception #2.1

--- src/App.js
+++ src/App.js
@@ -84,6 +84,10 @@ class App {
 
   async getGameCount() {
     let gameCount = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    // 1. 아무것도 입력하지 않았을 경우
+    if (gameCount === '') {
+      throw new Error('[ERROR] 입력값이 없습니다.');
+    }
     // 2. 숫자가 아닌 값을 입력한 경우
     if(Number.isNaN(Number(gameCount))){
       throw new Error('[ERROR] 숫자를 입력해야 합니다.')
