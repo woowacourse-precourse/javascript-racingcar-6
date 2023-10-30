30fe586536e13540b16f7be0b9a1abf228c001ef
sehyun <e6d1fe@gmail.com>
2023-10-30 16:31:22 +0900

    fix: printFinalResult 이름만 출력하도록 수정

--- src/App.js
+++ src/App.js
@@ -120,7 +120,12 @@ class App {
   }
 
   async printFinalResult(finalWinner) {
-    Console.print(`${message.FINAL_WINNER}${finalWinner.join(', ')}`);
+    const namesOnly = finalWinner.map((item) => {
+      const slicePoint = item.indexOf(' : ');
+      return item.slice(0, slicePoint);
+    });
+
+    Console.print(`${message.FINAL_WINNER}${namesOnly.join(', ')}`);
   }
 }
 
