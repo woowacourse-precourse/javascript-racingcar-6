92a4f920b620bb7a48231f716400e6cc53b16456
daeun-Kim <82816029+ddaeunbb@users.noreply.github.com>
2023-10-30 16:19:08 +0900

    [Feat]: 난수 생성함수 만들기 (#9)
    - `Random.pickNumberInRange`을 사용해 0부터 9까지의 숫자를 랜덤으로 뽑아주는 함수를 만들었습니다.
    - 함수는 utils에 만들어 저장하였습니다.

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,4 @@
 export const REGEXP = /^[a-z]+$/;
 export const CARNAME_LENGTH = 5;
+export const FIRSTNUM = 0;
+export const LASTNUM = 9;
--- src/utils/typeConvertor.js
+++ src/utils/typeConvertor.js
@@ -1,5 +1,3 @@
 export const strToStrArr = (str) => str.split(',');
-export const strToNum = (str) => +str;
 
-// eslint 에러 방지를 위한 임의의 코드 (추후삭제)
-export const a = {};
+export const strToNum = (str) => +str;
