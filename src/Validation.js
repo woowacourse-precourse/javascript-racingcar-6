0dd9f351f8eea2f39eb17441043c51ab4523015d
jeongin98 <eon7500@gmail.com>
2023-10-30 22:47:06 +0900

    feat(Validation): 자동차 이름이 문자열인지 검사하는 기능 구현

--- src/Validation.js
+++ src/Validation.js
@@ -5,5 +5,14 @@ const Validation = {
     const elements = userInput.split(',');
     if (!elements.every((element) => element.length <= 5)) throw new Error(ERROR.CAR_NAME_LENGTH);
   },
+
+  validateCarNameAlphabetic(userInput) {
+    const alphabeticRegex = /^[a-zA-Z]+$/;
+
+    const elements = userInput.split(',');
+    if (!elements.every((element) => alphabeticRegex.test(element))) {
+      throw new Error(ERROR.CAR_NAME_TYPE);
+    }
+  },
 };
 export default Validation;
