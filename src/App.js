115901d2742d18949e7cbf3efcb4df6731169cf0
Rilee-0320 <go13209@naver.com>
2023-10-30 23:42:26 +0900

    feat: 자동차 이름 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const carNameList = await this.inputCarNames();
+  }
+
+  async inputCarNames() {
+    const carNames = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    return carNames.split(",");
+  }
 }
 
 export default App;
