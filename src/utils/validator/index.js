0faf5b133544e8230404d77a75a800fecf3c9ce5
ryan-dia <cjfdnjs158@naver.com>
2023-10-30 23:53:20 +0900

    feat(validator): 자동차 이름의 길이 유혀성 확인 기능 추가

--- src/utils/validator/index.js
+++ src/utils/validator/index.js
@@ -1,9 +1,11 @@
 import SYSTEM from '../../constants/System.js';
 import isValidDelimiter from './utils/is-valid-delimiter/index.js';
+import isValidNameLength from './utils/is-valid-name-length/index.js';
 
 const Validators = {
   checkRacingVehicleName(input) {
     isValidDelimiter(input, SYSTEM.delimiter);
+    isValidNameLength(input);
   },
 };
 
