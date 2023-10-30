e38d91bd27899a7de3939bff57720a2c82b4de66
greeneeya <suzythetravelerr@gmail.com>
2023-10-30 16:19:57 +0900

    [refactor] decideWinner 함수 기능 분리
    compareScores로 함수를 분리하여, 하나의 함수당 하나의 기능만 구현하도록 수정함

--- src/Judge.js
+++ src/Judge.js
@@ -8,15 +8,20 @@ export class Judge {
   }
 
   decideWinner(totalScores) {
-    let max = 0;
-    Object.keys(totalScores).map((player) => {
-      const score = totalScores[player];
-      if (score > max) max = score;
-    });
+    const max = this.compareScores(totalScores);
     const winner = Object.keys(totalScores).filter((player) => {
       const score = totalScores[player];
       return score === max;
     });
     return this.printWinner(winner);
+  }
+
+  compareScores(totalScores) {
+    let max = 0;
+    Object.keys(totalScores).map((player) => {
+      const score = totalScores[player];
+      if (score > max) max = score;
+    });
+    return max;
   }
 }
