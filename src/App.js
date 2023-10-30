3e97dbb4fe3e300a25c17f0e34cc3218e4115efc
fecapark <fecapark@gmail.com>
2023-10-30 22:35:40 +0900

    feat: 이름으로 스코어보드 점수 올리는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+//@ts-check
+
+import { askNames, askTryAmount } from "./core/io";
+import { makeScoreboardByNames } from "./utils/parse";
+
 class App {
-  async play() {}
+  async play() {
+    this.names = await askNames();
+    this.tryAmount = await askTryAmount();
+    this.scoreboard = makeScoreboardByNames(this.names);
+  }
+
+  /**
+   *
+   * @param {string} name
+   */
+  giveScoreTo(name) {
+    if (!this.scoreboard) return;
+    if (!this.scoreboard.hasOwnProperty(name)) return;
+
+    this.scoreboard[name] += 1;
+  }
 }
 
 export default App;
