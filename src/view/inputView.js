4e017b4df8ca8239dd14a954b89c2e50828e3b30
mieum12 <qpdlqltb1215@gmail.com>
2023-10-30 20:49:57 +0900

    feat: input view 구현

--- src/view/inputView.js
+++ src/view/inputView.js
@@ -1,10 +1,21 @@
 import { Random, Console } from "@woowacourse/mission-utils";
 import { MESSAGE } from "../constants/constants";
 class InputView {
-  inputCarName() {
-    let carName = Console.readLineAsync(MESSAGE.START_CAR_NAME);
+  #inputValidator;
+  constructor(inputValidator) {
+    this.#inputValidator = inputValidator;
   }
-
-  inputNumberOfAttempt() {}
+  // 1. 자동차 이름 입력
+  async inputCarName() {
+    const input = await Console.readLineAsync(MESSAGE.START_CAR_NAME);
+    // 검증 보내기
+    this.#inputValidator.validateCarsInput(input);
+  }
+  // 2. 시도 횟수 입력
+  async inputNumberOfAttempt() {
+    const input = await Console.readLineAsync(MESSAGE.START_NUMBER_OF_ATTEMPT);
+    // 검증 보내기
+    this.#inputValidator.validateNumbersInput(input);
+  }
 }
 export default InputView;
