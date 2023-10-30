0f4fe524a619de898378aac69e8c29c8a94f64df
Eunseo Na <nes020781@gmail.com>
2023-10-31 03:47:59 +0900

    fix: gamemessage 수정

--- src/Message/gameMessage.js
+++ src/Message/gameMessage.js
@@ -1,4 +1,4 @@
-const GameMessage = object.freeze({
+const GameMessage = Object.freeze({
   INPUT_CAR_NAME: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
   INPUT_TRY_AMOUNT: "시도할 횟수는 몇 회인가요?\n",
   RACING_RESULT: "실행 결과\n",
