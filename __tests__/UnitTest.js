09a844e9f8bc2d588efc056434fb7525b5e1baa0
stegano9744 <stegano9744@gmail.com>
2023-10-30 17:22:34 +0900

    ✅ test: 실패 코드 변경

--- __tests__/UnitTest.js
+++ __tests__/UnitTest.js
@@ -5,13 +5,13 @@ import { validateLength, validateIsNumber } from '../src/utils/validate';
 
 describe('예외 테스트', () => {
   test('자동차 이름 길이 예외 검사', () => {
-    const carName = 'Juru99,Juru100';
+    const carName = 'Juru9,Jru';
 
     validateLength(carName);
   });
 
   test('자동차 이동 횟수 예외 검사', () => {
-    const moveCount = 'ab1';
+    const moveCount = '8';
 
     validateIsNumber(moveCount);
   });
