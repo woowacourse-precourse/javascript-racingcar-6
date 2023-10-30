092978a29f5be9028c9255fbf37fc7e09e7a9fed
silver0108 <kk1170@naver.com>
2023-10-30 23:38:37 +0900

    refactor: 줄바꿈 추가

--- src/InputCount.js
+++ src/InputCount.js
@@ -1,6 +1,6 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 export async function inputCount(){
-  const count = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?');
+  const count = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
   return count;
 }
