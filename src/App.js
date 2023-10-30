42d415c4e3c1999562b901043ac9c2dd877380e6
swJaNG12 <cbdjej38483@gmail.com>
2023-10-31 00:47:00 +0900

    feat(App): add method get  ForwardCount  from user
    Add getUserInputForwardCount method in App to get car attempts count

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ class App {
         throw new Error('[ERROR] 자동차 이름은 5자 이하만 가능합니다. ');
         return;
       }
+      const attemptForwardCount = await this.getUsetInputForwardCount();
+      Console.print(attemptForwardCount);
     } catch (error) {
       console.error(error);
     }
@@ -19,6 +21,9 @@ class App {
     return await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분'
     );
+  }
+  async getUsetInputForwardCount() {
+    return await Console.readLineAsync('시도할 횟수는 몇 회인가요?');
   }
 }
 
