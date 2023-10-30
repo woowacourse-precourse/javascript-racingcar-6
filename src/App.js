bb8bdc0e820d0857a6a62934e8d53d9d1bfc4cb7
Narachan97 <qkrtkf3112@naver.com>
2023-10-30 17:57:15 +0900

    feat: 자동차 0~9 무작위수 배정 4 이상 전진표시
    src/App.js 자동차 0~9 무작위수 배정 4 이상 전진표시
    
    App.js 새로운기능
    -컴퓨터가 각각 라운드 만큼 자동차에 0~9사이 무작위수 리턴
    -무작위수가 4 이상 일경우 진행도 증가
    -각 차량마다 진행도에 따라 '-' 전진표시 추가한다

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import { Random, Console } from "@woowacourse/mission-utils";
 class App {
   async play() {
-    const carNameInput = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    const carNameInput = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
     const carNameArray = carNameInput.split(",");
-    
+    const carProgress = new Array(carNameArray.length).fill(0);
     const playRound = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
-    
+
+    function playRacing() {
+      for (let round = 0; round < playRound; round++) {
+        for (let i = 0; i < carProgress.length; i++) {
+          const value = randomValue();
+          if (value >= 4) {
+            carProgress[i]++;
+          }
+        }
+        console.log(carProgress);
+      }
+      return carProgress;
+    }
+
+    playRacing();
+
+    function randomValue() {
+      return Math.floor(Math.random() * 10);
+    }
+    function printResults(names, progress) {
+      for (let i = 0; i < names.length; i++) {
+        let progressString = "-".repeat(progress[i]);
+        Console.print(`${names[i]} : ${progressString}`);
+        console.log(`${names[i]} : ${progressString}`);
+      }
+    }
   }
 }
 
