60d368d3b075dc395abcfe2cbb71b0e2b75b2ab0
Dobbymin <gmin0701@knu.ac.kr>
2023-10-30 17:06:59 +0900

    [REFACTOR](modules): printWinner 모듈 추가

--- src/pages/modules.js
+++ src/pages/modules.js
@@ -1,6 +1,7 @@
 import inputRacingCarName from '../components/inputRacingCarName.js';
 import inputTryNumber from '../components/inputTryNumber.js';
-import progressRacingCar from '../components/progressRacingCar.js';
 import isStart from '../components/isStart.js';
+import printWinner from '../components/printWinner.js';
+import progressRacingCar from '../components/progressRacingCar.js';
 
-export { inputRacingCarName, inputTryNumber, progressRacingCar, isStart };
+export { inputRacingCarName, inputTryNumber, isStart, printWinner, progressRacingCar };
