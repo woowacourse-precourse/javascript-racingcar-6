a615bec94935c803dbd8eb29ad8db2bbf23adde1
FirstBright <mipo8890@gmail.com>
2023-10-30 22:59:17 +0900

    :sparkles: feat: 레이싱게임 우승자 발표 구현

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 import Car from "./Car.js";
 class App {
   async play() {
+
+    function findCarsWithMaxForward(cars) {
+      if (cars.length === 0) {
+        return [];
+      }
+    
+      const maxForward = Math.max(...cars.map(car => car.forward));
+      const carsWithMaxForward = cars.filter(car => car.forward === maxForward);
+    
+      return carsWithMaxForward;
+    }
+    function roundInputCheck(round) {
+      return parseInt(round);
+    }
+
     let players = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
     let round = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
     const playerArray = players.split(",");
@@ -14,13 +29,18 @@ class App {
       const car = new Car(player);
       cars.push(car);
     }
+    round = roundInputCheck(round);
+
     await MissionUtils.Console.print("실행 결과")
     for (let i =0; i<round;i++) {
       for(let car of cars) {
         car.run();
+        car.printRunResult();        
       }
+      await MissionUtils.Console.print("");
     }
-
+    const members = this.findCarsWithMaxForward(cars)
+    await MissionUtils.Console.print("최종 우승자 : "+members.map((member)=>member).join(', ')); 
     
   }
 }
