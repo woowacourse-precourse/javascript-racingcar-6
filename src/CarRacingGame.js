7fde12a6c6c6fb4b56447198f54e0a8f97a132cd
youjeong Jeong <wjddbwjd1205@naver.com>
2023-10-31 00:37:15 +0900

    feat: 자동차 이름 입력 예외상황 추가, 예외사항 처리 함수 수정
    자동차 이름이 하나일 때 예외상황 발생, 예외사항 처리 함수에 사용하는 자바스크립트 메소드 some()으로 변경

--- src/CarRacingGame.js
+++ src/CarRacingGame.js
@@ -3,72 +3,77 @@ import Car from "./Car.js";
 
 class CarRacingGame {
 
-    constructor() {
-        this.cars = [];
-        this.attempts;
+  constructor() {
+    this.cars = [];
+    this.attempts = '';
+  }
+
+
+  async start() {
+    await this.getCarNames();
+    await this.getNumberOfAttempts();
+  }
+
+
+  async getCarNames() {
+    try {
+      const carNames = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+      if (carNames === '') {
+        throw new Error("[ERROR] 잘못된 자동차 이름 입력입니다.");
+      }
+
+      const carNamesArray = carNames.split(",");
+      if (this.validateCarNames(carNamesArray)) {
+        throw new Error("[ERROR] 잘못된 자동차 이름 입력입니다.");
+      }
+      if (carNamesArray.length < 2) {
+        throw new Error("[ERROR] 자동차가 1개 입니다");
+      }
+
+      for (let i = 0; i < carNamesArray.length; i++) {
+        this.cars.push(new Car(carNamesArray[i]));
+      }
+
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async start() {
-        await this.getCarNames();
-        await this.getNumberOfAttempts();
 
+  validateCarNames(carNamesArray) {
+    return carNamesArray.some(carName => {
+      return carName.length > 5 || carName.length === 0;
+    });
+  }
+
+
+  async getNumberOfAttempts() {
+    try {
+      const inputAttempts = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+
+      if (this.validateAttempts(inputAttempts)) {
+        throw new Error("[ERROR] 잘못된 시도 횟수 입력입니다.");
+      }
+
+      this.attempts = parseInt(inputAttempts, 10);
+
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async getCarNames() {
-        try {
-            const carNames = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
-            if (carNames === '') {
-                throw new Error("[ERROR] 잘못된 입력입니다.");
-            }
 
-            const carNamesArray = carNames.split(",");
-            if (this.validateCarNames(carNamesArray)) {
-                throw new Error("[ERROR] 잘못된 입력입니다.");
-            }
-
-            for(let i = 0; i < carNamesArray.length; i++) {
-                this.cars.push(new Car(carNamesArray[i]));
-            }
-
-            console.log(this.cars);
-
-        } catch (error) {
-            throw error;
-        }
+  validateAttempts(attempts) {
+    for (let i = 0; i < attempts.length; i++) {
+      let char = attempts.charAt(i)
+      let ascii = char.charCodeAt()
+      if ('1'.charCodeAt() <= ascii && ascii <= '9'.charCodeAt()) {
+        continue;
+      }
+      return true;
     }
-
-
-    validateCarNames(carNamesArray) {
-        carNamesArray.forEach(carName => {
-            if (carName.length > 5 || carName.length === 0) return true;
-            return false;
-        });
-    }
-
-    async getNumberOfAttempts() {
-        try {
-            this.attempts = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
-
-            if(this.validateAttempts(this.attempts)) {
-                throw new Error("[ERROR] 잘못된 입력입니다.");
-            }
-        } catch (error) {
-            throw error;
-        }
-    }
-
-
-    validateAttempts(attempts) {
-        for (let i = 0; i < attempts.length; i++) {
-            let char = attempts.charAt(i)
-            let ascii = char.charCodeAt()
-            if ('1'.charCodeAt() <= ascii && ascii <= '9'.charCodeAt()) {
-                continue;
-            }
-            return true;
-        }
-        return false;
-    }
+    return false;
+  }
 
 }
 
