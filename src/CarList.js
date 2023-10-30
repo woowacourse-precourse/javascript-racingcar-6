587ead06bd47eb3f09132d9d56ee2d65d32f7611
donggyun <donggyunkim12@gmail.com>
2023-10-30 13:57:09 +0900

    feat(CarList): 우승자 필터링 기능 구현

--- src/CarList.js
+++ src/CarList.js
@@ -14,6 +14,23 @@ class CarList {
   printCarCurrnetState() {
     this.cars.forEach((car) => car.printCarData());
   }
+
+  async setWinner() {
+    const carStatesArr = this.cars.map((car) => car.getCarState());
+    const currentStateLengthArr = this.cars.map(
+      (car) => car.getCarState().currentStateLength,
+    );
+
+    const maxLength = currentStateLengthArr.reduce((acc, curr) => {
+      return Math.max(acc, curr);
+    });
+
+    const winners = carStatesArr.filter((item) => {
+      return item.currentStateLength === maxLength;
+    });
+
+    return winners.map((obj) => obj.carName);
+  }
 }
 
 export default CarList;
