6daf70f2bad5515f4d6cf13bd3cde98f2294330d
kelvin3476 <winrain03226@gmail.com>
2023-10-31 02:19:09 +0900

    refactor(*): App.js 분리 후 입력값 모듈화 작업 완료

--- src/App.js
+++ src/App.js
@@ -1,37 +1,10 @@
-import { MissionUtils } from '@woowacourse/mission-utils';
 import RacingGame from './RacingGame';
 
 class App {
+  game = new RacingGame();
+
   async play() {
-    MissionUtils.Console.print('자동차 경주 게임을 시작합니다.');
-
-    // TODO: Mission 1: 자동차 생성 및 이름 부여 메서드입니다.
-    const carNames = await MissionUtils.Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요 (이름은 쉼표(,)로 구분):',
-    );
-
-    // TODO: Mission 2: 이동 시도 횟수 입력 메서드입니다.
-    const totalAttempts = parseInt(
-      await MissionUtils.Console.readLineAsync('시도할 횟수를 입력하세요:'),
-      10,
-    );
-
-    // TODO: Mission 5: 오류 처리 메서드입니다.
-    // 예외 처리: 입력 값이 부적절한 경우
-    if (!carNames || carNames.trim() === '') {
-      throw new Error('[ERROR] 자동차 이름은 필수 입력 항목입니다.');
-    }
-
-    // 예외 처리: 시도 횟수가 유효하지 않은 경우
-    if (Number.isNaN(totalAttempts) || totalAttempts <= 0) {
-      throw new Error('[ERROR] 시도 횟수는 양의 정수여야 합니다.');
-    }
-
-    const game = new RacingGame(carNames, totalAttempts);
-    game.playGame();
-
-    const winners = game.determineWinner();
-    MissionUtils.Console.print(`최종 우승자: ${winners.join(', ')}`);
+    await this.game.start();
   }
 }
 
---
+++ src/utils/Input.js
@@ -0,0 +1,27 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { MESSAGE } from './Constant';
+import Validation from './Validation';
+
+const Input = {
+  async startGame() {
+    // 게임 시작 메시지 출력
+    MissionUtils.Console.print(MESSAGE.start);
+
+    // TODO: Mission 1: 자동차 생성 및 이름 부여 메서드입니다.
+    const carNames = await MissionUtils.Console.readLineAsync(MESSAGE.car);
+    Validation.checkNull(carNames);
+
+    const carNameList = carNames.split(',');
+    Validation.checkNameLength(carNameList);
+    return carNameList;
+  },
+  async moveCount() {
+    // TODO: Mission 2: 이동 시도 횟수 입력 메서드입니다.
+    const totalAttempts = await MissionUtils.Console.readLineAsync(MESSAGE.count);
+    Validation.checkNull(totalAttempts);
+    Validation.checkCount(totalAttempts);
+    return parseInt(totalAttempts, 10);
+  },
+};
+
+export default Input;
