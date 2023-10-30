95c2e9adc7ddca69269e4e58e6b0f028699068ee
uraflower <82873315+uraflower@users.noreply.github.com>
2023-10-30 20:07:54 +0900

    test: 경주 결과 판정 테스트 추가

---
+++ __tests__/Referee.test.js
@@ -0,0 +1,21 @@
+import Car from '../src/Car';
+import Referee from '../src/Referee';
+
+jest.mock('../src/Car.js');
+Car.mockImplementation((name, movement) => ({
+  name,
+  movement,
+  move: jest.fn(),
+}));
+
+test('경주가 끝난 후, 가장 많이 전진한 자동차(들)을 반환한다.', () => {
+  const pobi = Car('pobi', '-');
+  const woni = Car('woni', '---');
+  const jun = Car('jun', '---');
+  const inputs = [pobi, woni, jun];
+  const expected = [woni, jun];
+
+  const outputs = Referee.checkWinners(inputs);
+
+  expect(outputs).toStrictEqual(expected);
+});
