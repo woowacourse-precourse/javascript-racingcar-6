9f55b765ae1af009e8762d6db2ac90bcd5b087be
hyzzzzy <shinhj4068@naver.com>
2023-10-30 14:43:30 +0900

    feat(CarRace): 자동차 경주 클래스 정의

---
+++ src/CarRace.js
@@ -0,0 +1,8 @@
+export default class CarRace {
+  constructor(carNames, count) {
+    /** @type Array<string> */
+    this.carNames = carNames;
+    /** @type number */
+    this.count = count;
+  }
+}
