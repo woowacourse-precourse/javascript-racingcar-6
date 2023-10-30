94836268ebb500c655f22450a2e61ecec738e141
pjc0179 <pjc0179@gmail.com>
2023-10-30 21:57:56 +0900

    refactor(utils):checkNumber.js 수정
    - 여러가지 에러사항(null = 0 등)있는 parseInt를 사용하여 Number.isNaN으로 검사하던 checkNumberType 메서드의 조건식을 정규식으로 변경

--- src/utils/checkNumber.js
+++ src/utils/checkNumber.js
@@ -1,9 +1,10 @@
 const checkNumber = {
   checkNumberType(value) {
-    return !Number.isNaN(parseInt(value, 10));
+    return /^[0-9]+$/.test(value);
   },
   checkGreaterThan(number1, number2) {
     return number1 > number2;
   },
 };
+
 export default checkNumber;
