94179f54f31ba1c63af05d29854dfc8519d7c6de
delee3064 <delee3064@gmail.com>
2023-10-30 16:55:59 +0900

    feat : 자동차 이름 예외처리

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import MSG from "./message.js"
-import nameValidation from "./validation.js"
+import carValidation from "./validation.js"
 import game from "./game.js"
 
 class App {
   async play() {
     const inputName = await MissionUtils.Console.readLineAsync(MSG.GAMESTART);
     const carNames = inputName.split(",");
-    nameValidation(carNames);
+    carValidation(carNames);
     const cnt = await MissionUtils.Console.readLineAsync(MSG.INPUTCOUNT);
     MissionUtils.Console.print(MSG.RESULT)
     game(carNames, cnt)
--- src/validation.js
+++ src/validation.js
@@ -1,9 +1,20 @@
-function nameValidation(carNames) {
-    for (let i = 0; i < carNames.length; i++) {
-        if (carNames[i].length > 5 || carNames[i].length < 1) {
-          throw new Error ("[ERROR] 1 ~ 5글자의 자동차 이름을 입력해주세요.")
-        }
-    }    
+function carValidation(carNames) {
+  const cars = new Set(carNames);
+  const space = /\s/g;
+  if (cars.size !== carNames.length) {
+    throw new Error ("[ERROR] 자동차의 이름을 중복되지 않게 입력해주세요.")
+  }
+  if (carNames.length < 2) {
+    throw new Error ("[ERROR] 최소한 두개 이상의 이름을 입력해주세요.")
+  }
+  for (let i = 0; i < carNames.length; i++) {
+    if (carNames[i].length > 5 || carNames[i].length < 1) {
+      throw new Error ("[ERROR] 1 ~ 5글자의 자동차 이름을 입력해주세요.")
+    }
+    if (carNames[i].match(space)) {
+      throw new Error ("[ERROR] 공백을 제외한 자동차의 이름을 입력해주세요.")
+    }
+  }
 }
 
-export default nameValidation;
+export default carValidation;
