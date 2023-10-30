0f8e4050cdce190ed77c7f3232f96f717a1ea6e7
ChanYong Park <krap5075@naver.com>
2023-10-31 02:35:34 +0900

    feat: 각 car의 랜덤숫자 뽑기 기능 추가

--- src/Car.js
+++ src/Car.js
@@ -3,7 +3,14 @@ class Car {
   constructor(name) {
     this.name = name;
     this.move_times = 0;
+    this.decision_number = 0;
     this.dash = "-";
+  }
+  pickRandomNumber() {
+    this.decision_number = Random.pickNumberInRange(
+      NUM_RANGE.car_start_range,
+      NUM_RANGE.car_end_range
+    );
   }
 }
 
