7e924f036a5b287497993ca721ae3bc58515564d
hanyoung <phanyoung@naver.com>
2023-10-30 21:25:05 +0900

    fix: 테스트용 인풋을 string 자료형으로 통일

--- __tests__/FeatureTest.js
+++ __tests__/FeatureTest.js
@@ -41,11 +41,11 @@ describe("세부 기능 테스트", () => {
     test("등록하는 자동차가 2대 미만일 경우에 대해 예외 처리를 하는가", async () => {
       const app = new App();
 
-      const correctInputs = ["자동차1, 자동차2", 2];
+      const correctInputs = ["자동차1, 자동차2", "2"];
       mockQuestions(correctInputs);
       await expect(app.play()).resolves.not.toThrowError();
 
-      const wrongInputs = ["자동차1", 2];
+      const wrongInputs = ["자동차1", "2"];
       mockQuestions(wrongInputs);
       await expect(app.play()).rejects.toThrow("[ERROR]");
     });
@@ -65,7 +65,7 @@ describe("세부 기능 테스트", () => {
     test("시도 횟수 입력 값이 1미만인 경우에 대해 예외 처리를 하는가", async () => {
       const app = new App();
 
-      const inputs = ["자동차1, 자동차2", 0];
+      const inputs = ["자동차1, 자동차2", "0"];
 
       mockQuestions(inputs);
 
@@ -75,7 +75,7 @@ describe("세부 기능 테스트", () => {
     test("시도 횟수 입력 값이 1000을 초과할 경우에 대해 예외 처리를 하는가", async () => {
       const app = new App();
 
-      const inputs = ["자동차1, 자동차2", 1001];
+      const inputs = ["자동차1, 자동차2", "1001"];
 
       mockQuestions(inputs);
 
@@ -83,7 +83,7 @@ describe("세부 기능 테스트", () => {
     });
   });
 
-  describe("자동차 이동 기능에 대한 테스트", () => {
+  describe("라운드 진행에 대한 테스트", () => {
     test("시도 횟수만큼 자동차가 이동하는가(라운드 결과 출력 함수가 실행되는가)", async () => {
       const printRoundResultSpy = getPrintRoundResultSpy();
       const TRIAL_COUNT = 5;
