28ac86e956b6f0c6265ee4f77a0c87b73864983f
hyeong1 <mi75625@gmail.com>
2023-10-31 01:47:27 +0900

    feat: 자동차 이름 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+import PRINTOUT from "../src/Printout.js";
+
 class App {
-  async play() {}
+  async play() {
+    // 자동차 이름 받기
+    const inputCars = await MissionUtils.Console.readLineAsync(
+      PRINTOUT.ASK_NAME
+    );
+
+    // 쉼표로 이름 분리
+    let enterCars = inputCars.split(",");
+    MissionUtils.Console.print(enterCars);
+  }
 }
 
 export default App;
