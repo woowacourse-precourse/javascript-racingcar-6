1bebd8c334feebf4492370ab9ee31afb2611eab6
yeokyoung song <spolice0324@gmail.com>
2023-10-31 00:11:05 +0900

    feat : 레이싱 한 경기를 다루는 CarRace 클래스 구현
    - 자동차 이름 인풋을 포매팅하여 Car 클래스들의 배열 형태로 반환하는 makeCarList 추가
    - 레이스 횟수에 따라 전진 여부의 결과 값을 추가하는 getRace 추가
    - 전진횟수 인풋을 포매팅하여 전진횟수를 카운팅하는 getRaceRound 추가
    - 최대 전진횟수를 필터링하여 최종 승자를 반환하는 getWinners 추가
    - 최종 결과 값 반환하는 getResult 추가

--- src/model/CarRace.js
+++ src/model/CarRace.js
@@ -2,14 +2,60 @@ import Car from './Car';
 
 class CarRace {
   constructor() {
+    this.tryCount = 0;
+    this.carNames = [];
     this.carList = [];
+    this.result = {
+      moveList: [],
+      winners: null,
+    };
   }
 
-  makeCarList(carArr) {
-    carArr.forEach(car => {
-      this.carList.push(new Car(car));
+  makeCarList(carNames) {
+    // 유효성 검사
+    const nameFormatting = names => names.replace(/(\s*)/g, '').split(',');
+    this.carNames = nameFormatting(carNames);
+    this.carNames.forEach(name => {
+      this.carList.push(new Car(name));
     });
+    return this.carList;
+  }
+
+  getRace() {
+    // 유효성 검사
+    this.tryCount = Number(this.tryCount);
+    for (let count = 0; count < this.tryCount; count += 1) {
+      const round = this.getRaceRound();
+      this.result.moveList.push(round);
+    }
+  }
+
+  getRaceRound() {
+    const forwardFormatting = (name, forward) => {
+      const forwardArray = Array.from({ length: forward }, () => '-');
+      return `${name}: ${forwardArray.join('')}`;
+    };
+
+    return this.carList.reduce((acc, car) => {
+      car.forward();
+      acc.push(forwardFormatting(car.getCarName(), car.getMoveCount()));
+      return acc;
+    }, []);
+  }
+
+  getWinners() {
+    const maxMoveCount = this.carList
+      .sort((a, b) => b.getMoveCount() - a.getMoveCount())[0]
+      .getMoveCount();
+    const winners = this.carList.filter(
+      car => car.getMoveCount() === maxMoveCount,
+    );
+    this.result.winners = winners.map(winner => winner.getCarName()).join(', ');
+    return this.result.winners;
+  }
+
+  getResult() {
+    return this.result;
   }
 }
-
 export default CarRace;
