aef79fbdadbddcf951b174719610c36c5246d447
hojkim77 <rlaghwnd4534@gmail.com>
2023-10-30 16:52:43 +0900

    [refactor] 주석 추가 및 코드컨벤션 적용

--- src/InputValidator.js
+++ src/InputValidator.js
@@ -4,23 +4,27 @@ class InputValidator {
    * @param {string} input
    * @returns {[string]}
    */
-  static async carNameValidator(input) {
+  static carNameValidator(input) {
     const inputToArr = input.split(',');
+
     inputToArr.forEach((carName) => {
       if (carName.length > 5)
         throw new Error(
           '[ERROR] 자동차의 이름은 쉼표로 구분하여 5글자 이하로 입력해주세요.'
         );
     });
+
     return inputToArr;
   }
+
   /**
    *
    * @param {string} input
    * @returns {number}
    */
-  static async tryNumValidator(input) {
+  static tryNumValidator(input) {
     if (isNaN(input)) throw new Error('[ERROR] 숫자를 입력해주세요.');
+
     return Number(input);
   }
 }
