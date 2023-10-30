5bd39fb1d9fd26eb22538f76a5ea048f371fd333
soyoung125 <sypark041754@gmail.com>
2023-10-30 17:55:30 +0900

    feat: 이름 길이를 검사하는 테스트 케이스 추가

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,4 +1,4 @@
-import { isNumber } from "../src/utils/validation"
+import { isBiggerThan5, isNumber } from "../src/utils/validation"
 
 describe("validation.js 테스트", () => {
     test("숫자 형식 여부 검사 - 숫자 입력", () => {
@@ -12,5 +12,17 @@ describe("validation.js 테스트", () => {
         const result = isNumber(input);
 
         expect(result).toBeFalsy();
+    });
+    test("이름의 길이 검사 - 5자 이하", () => {
+        const input = "bong";
+        const result = isBiggerThan5(input);
+
+        expect(result).toBeFalsy();
+    });
+    test("이름의 길이 검사 - 5자 초과", () => {
+        const input = "soyoung125";
+        const result = isBiggerThan5(input);
+
+        expect(result).toBeTruthy();
     });
 })
