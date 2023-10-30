06e114c91be1e648711cb70e963b99cc1120450b
yoovin <soo22839@gmail.com>
2023-10-30 23:00:48 +0900

    feat: 성공하는 케이스 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+    async play() {}
+
+    getUserInput() {
+        return ['yoo', 'vin'];
+    }
+
+    createCars(names) {
+        return {
+            yoo: 0,
+            vin: 0,
+        };
+    }
+
+    getTryCount() {
+        return 5;
+    }
+
+    playGame(tryCount, cars) {
+        return {
+            yoo: 1,
+            vin: 2,
+        };
+    }
+
+    printGameStatus(cars) {
+        MissionUtils.Console.print('yoo : -');
+    }
+
+    printWinner(cars) {
+        MissionUtils.Console.print('최종 우승자 : vin');
+    }
 }
 
 export default App;
