73c989d4188f50cbe237ee53fb9d1678a6b352bf
kang <kangju2000@naver.com>
2023-10-30 16:22:25 +0900

    :recycle: validator 객체 -> 클래스로 변경, `validNameLengthByNameList` 메서드 추가

--- src/validator/validator.js
+++ src/validator/validator.js
@@ -1,30 +1,42 @@
-const validator = {
+class Validator {
+  /**
+   * 이름 리스트의 길이가 유효한지 검사한다.
+   * @param {string[]} nameList 이름 리스트
+   * @param {number} maxLength 최대 길이
+   * @returns {boolean}
+   */
+  static validNameLengthByNameList(nameList, maxLength) {
+    return nameList.every((name) => this.validNameLength(name, maxLength))
+  }
+
   /**
    * 이름의 길이가 유효한지 검사한다.
    * @param {string} name 이름
    * @param {number} maxLength 최대 길이
    * @returns {boolean}
    */
-  validNameLength: (name, maxLength) => {
+  static validNameLength(name, maxLength) {
     return name.length <= maxLength && name.length > 0
-  },
+  }
+
   /**
    * 이름이 중복되는지 검사한다.
    * @param {string[]} nameList 이름 리스트
    * @returns {boolean}
    */
-  isDuplicateName: (nameList) => {
+  static isDuplicateNameByNameList(nameList) {
     return nameList.length !== new Set(nameList).size
-  },
+  }
+
   /**
    * 시도 횟수가 유효한지 검사한다.
    * @param {number} inputTryCount 입력된 시도 횟수
    * @param {number} minTryCount 최소 시도 횟수
    * @returns {boolean}
    */
-  validTryCount: (inputTryCount, minTryCount) => {
+  static validTryCount(inputTryCount, minTryCount) {
     return Number.isInteger(inputTryCount) && inputTryCount >= minTryCount
-  },
+  }
 }
 
-export default validator
+export default Validator
