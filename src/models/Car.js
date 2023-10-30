d1be5afc8fc3724fcd313cefc496a351ba8b3122
teawon <xodnjsvkvk9@gmail.com>
2023-10-31 00:19:52 +0900

    fix : Car 도메인의 빈 문자열에 대한 예외처리 추가

--- src/models/Car.js
+++ src/models/Car.js
@@ -11,6 +11,10 @@ class Car {
     if (name.length > 5) {
       throw new Error("[ERROR] 자동차의 이름은 5자 이하만 가능합니다.");
     }
+
+    if (name === "") {
+      throw new Error("[ERROR] 자동차의 이름은 필수입니다.");
+    }
   }
 
   move() {
