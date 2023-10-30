2018b77e1e9504106df027cec9822c3e54cad539
uraflower <82873315+uraflower@users.noreply.github.com>
2023-10-30 16:42:35 +0900

    feat: 실시간 결과 출력 메서드에서 car.move 호출 코드 제거
    ScoreBoard는 경주 결과를 출력하는 일만 함. 따라서 car.move()를 호출해선 안 됨.

--- src/ScoreBoard.js
+++ src/ScoreBoard.js
@@ -3,7 +3,6 @@ import { Console } from '@woowacourse/mission-utils';
 class ScoreBoard {
   static announceInterimResult(cars) {
     cars.forEach((car) => {
-      car.move();
       Console.print(`${car.name} : ${car.movement}`);
     });
   }
