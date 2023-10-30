381f4888b25ed1d9409467d7965e8a71896ae7bf
DongWook Kang <rkdehddnr15123@gmail.com>
2023-10-30 10:14:19 +0900

    refactor: 에러 출력 리팩토링

--- src/RacingCar.js
+++ src/RacingCar.js
@@ -3,13 +3,18 @@ import { validPlayerInput, validTryInput } from "./validInput.js";
 
 class RacingCar {
   async init() {
-    const playerInput = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
-    const tryInput = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
-    const players = playerInput.replace(/\s/g, "").split(",");
+    try {
+      const playerInput = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+      const tryInput = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+      const players = playerInput.replace(/\s/g, "").split(",");
 
-    validPlayerInput(players);
-    validTryInput(tryInput);
-    this.start(players, tryInput);
+      validPlayerInput(players);
+      validTryInput(tryInput);
+      this.start(players, tryInput);
+    } catch (err) {
+      Console.print(err.message);
+      throw err;
+    }
   }
 
   async start(players, tryInput) {
