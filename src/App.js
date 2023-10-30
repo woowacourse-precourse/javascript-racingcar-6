39e84cf50d31d3eaadc10a3becf8a5c77aabf835
Eunseo Na <nes020781@gmail.com>
2023-10-31 03:49:44 +0900

    feat: App.js 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import RacingGame from "./racingGame.js";
+
 class App {
-  async play() {}
+  async play() {
+    const racingGame = new RacingGame();
+    racingGame.start();
+  }
 }
 
 export default App;
