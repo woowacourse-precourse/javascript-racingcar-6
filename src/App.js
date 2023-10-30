6015f4d03b0de82dbd271d971b8c8182b35980e6
yebin <yebind3308@gmail.com>
2023-10-30 21:25:49 +0900

    refactor : MissionUtils. -> { Console, Random } 변경

--- src/App.js
+++ src/App.js
@@ -1,50 +1,39 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    try {
       const carNames = await this.carNameInput();
       const userNumber = await this.numberTimesInput();
       const numberTimes = parseInt(userNumber);
       const result = this.playGame(carNames, numberTimes);
       this.endGame(result);
-    } catch (error) {
-      throw new Error("[ERROR]");
-    }
   }
 
   async carNameInput() {
-    MissionUtils.Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-    const carUserInput = await MissionUtils.Console.readLineAsync("");
+    Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    const carUserInput = await Console.readLineAsync("");
     const carName = carUserInput.split(",");
-
     if (carName.some(name => name.trim() === "" || carName.length <= 1 || carName.length > 5)) {
       throw new Error("[ERROR] 이름이 잘못된 형식입니다.");
     }
-
-    // MissionUtils.Console.print(`${carName.join(',')}`);
     return carName; 
   }
 
   async numberTimesInput() {
-    MissionUtils.Console.print("시도할 횟수는 몇 회인가요?");
-    const numberUserInput = await MissionUtils.Console.readLineAsync("");
-
+    Console.print("시도할 횟수는 몇 회인가요?");
+    const numberUserInput = await Console.readLineAsync("");
     if (!/^\d+$/.test(numberUserInput)) {
       throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
     };
-
-    // MissionUtils.Console.print(`${numberUserInput}`);
     return numberUserInput; 
   }
 
   playGame(carNames, numberTimes) {
     const result = []; 
     let recordSave = Array(carNames.length).fill(0); 
-
     for (let i = 0; i < numberTimes; i++) { 
       const saveResult = carNames.map((carName, index) => { 
-        const randomValue = MissionUtils.Random.pickNumberInRange(0, 9); 
+        const randomValue = Random.pickNumberInRange(0, 9); 
         const action = randomValue >= 4 ? '-' : ''; 
         if (action === '-') { 
           recordSave[index]++; 
@@ -59,10 +48,9 @@ class App {
   endGame(result) {
     for (const saveResult of result) {
       for (const { carName, action } of saveResult) { 
-        MissionUtils.Console.print(`${carName} : ${action}`);  
+        Console.print(`${carName} : ${action}`);  
       }
     }
-
     let maxDistance = 0;
     const winners = [];
     const lastRound = result[result.length - 1];
@@ -75,7 +63,7 @@ class App {
         winners.push(carName);
       }
     }
-    MissionUtils.Console.print(`최종 우승자: ${winners.join(', ')}`); 
+    Console.print(`최종 우승자: ${winners.join(', ')}`); 
   }
 }
 
