7b10ad90680bca3ee68be32258bf1a3b1ebee12a
areumH <94692566+areumH@users.noreply.github.com>
2023-10-30 22:34:50 +0900

    feat: 우승자 안내 문구 출력하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { getCarName } from "./utils/getCarName.js";
+import { getRaceCount } from "./utils/getRaceCount.js";
+import { getRaceResult } from "./utils/getRaceResult.js";
+import { getWinner } from "./utils/getWinner.js";
+
+import { Car } from "./CarObject.js";
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const carNameArray = await getCarName();
+    let carObject = Car.createCars(carNameArray);
+
+    const raceCount = await getRaceCount();
+    Console.print("\n실행 결과");
+
+    for (let i = 0; i < raceCount; i++) {
+      carObject = Car.addRace(carObject);
+      getRaceResult(carObject);
+    }
+    
+    getWinner(carObject);
+  }
 }
 
 export default App;
