bdb233410e2750131a36260c7c6e9670b6a7848e
mieum12 <qpdlqltb1215@gmail.com>
2023-10-30 23:04:57 +0900

    feat: 전진 여부를 판단하는 moning generator 생성

---
+++ src/domian/movingGenerator.js
@@ -0,0 +1,18 @@
+import { Random } from ("@woowacourse/mission-utils");
+import { SYMBOL } from "../constants/constants";
+// 전진 여부 판단
+export class MovingGenerator {
+  constructor() {
+    this.progress = "";
+  }
+  canMove() {
+    const randomNumber = Random.pickNumberInRange(1, 9);
+    return randomNumber >= 4;
+  }
+  move() {
+    if (this.canMove) {
+      this.progress += SYMBOL.MOVE
+      return this.progress
+    }
+  }
+}
