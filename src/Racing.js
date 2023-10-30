97bf13fbfad7dac9c224ca235f59866133856dfa
Joseph Park <rkfnahs12e@gmail.com>
2023-10-30 13:47:57 +0900

    feat(src/Racing.js) :레이싱의 승자를 알 수 있도록 해주는  getWinner()함수 구현

--- src/Racing.js
+++ src/Racing.js
@@ -3,31 +3,62 @@ import { getCarNamesList, getPlayCount } from './Input.js';
 import { validateForMove } from './Validation.js';
 import { createRandomNum } from './Number.js';
 
+function isGoForward(name, carNameObj) {
+	const randomNum = createRandomNum();
+	if (validateForMove(randomNum)) {
+		carNameObj[name] += 1;
+	}
+	Console.print(`${name} :${'-'.repeat(carNameObj[name])}`);
+}
+
+async function carMove(carNames, carNameObj) {
+	carNames.map((name, index) => {
+		isGoForward(name, carNameObj);
+	});
+}
+
+function getWinner(carNameObj) {
+	let max = -Infinity;
+	let maxKeys = [];
+	let isDuplicate = false;
+
+	Object.entries(carNameObj).forEach(([key, value]) => {
+		if (value > max) {
+			max = value;
+			maxKeys = [key];
+			isDuplicate = false;
+		}
+	});
+
+	Object.entries(carNameObj).forEach(([key, value]) => {
+		if (value === max && value !== maxKeys[0]) {
+			maxKeys.push(key);
+			isDuplicate = true;
+		}
+	});
+
+	if (isDuplicate) {
+		console.log(`최종 우승자 : ${maxKeys.join(', ')}`);
+	} else {
+		console.log(`최종 우승자 : ${maxKeys[0]}`);
+	}
+}
+
 async function startRacing() {
 	const carNames = await getCarNamesList();
-	const countObject = {};
+	const carNameObj = {};
 	carNames.forEach((name) => {
-		countObject[name] = 0;
+		carNameObj[name] = 0;
 	});
 	const playCount = await getPlayCount();
 	let count = 0;
 	Console.print(`실행 결과`);
 	while (count < playCount) {
-		carMove(carNames, countObject);
+		carMove(carNames, carNameObj);
 		count += 1;
+		Console.print('');
 	}
-}
-function isGoForward(name, countObject) {
-	const randomNum = createRandomNum();
-	if (validateForMove(randomNum)) {
-		countObject[name] += 1;
-	}
-	Console.print(`${name} :${'-'.repeat(countObject[name])}`);
+	getWinner(carNameObj);
 }
 
-async function carMove(carNames, countObject) {
-	carNames.map((name, index) => {
-		isGoForward(name, countObject);
-	});
-}
 startRacing();
