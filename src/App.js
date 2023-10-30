87ef900dfee0015a82341ebc65a6648c2cdf4368
greeneeya <suzythetravelerr@gmail.com>
2023-10-30 17:15:48 +0900

    refactor: 숫자 및 메세지 상수 변수화

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { Console } from '@woowacourse/mission-utils';
 import { Dice } from './Dice.js';
 import { Judge } from './Judge.js';
 import { RacingGame } from './RacingGame.js';
@@ -11,28 +10,6 @@ class App {
     const cars = await this.getRaceCars();
     const round = await this.getRaceRounds();
     game.play(round, cars);
-  }
-
-  async getRaceCars() {
-    const user = await Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
-    );
-    const cars = user.split(',');
-    cars.forEach((car) => {
-      car = car.toString();
-      if (car.length > 5)
-        throw new Error('[ERROR] 자동차 이름은 5자 이하만 가능합니다.');
-    });
-    return cars;
-  }
-
-  async getRaceRounds() {
-    const user = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
-    const rounds = Number(user);
-    if (!Number.isInteger(rounds))
-      throw new Error('횟수는 정수만 입력 가능합니다.');
-    if (rounds < 1) throw new Error('1회 부터 플레이 가능합니다.');
-    return rounds;
   }
 }
 
---
+++ src/Messages.js
@@ -0,0 +1,11 @@
+export const ERROR_MESSAGE = Object.freeze({
+  CHAR_LENGTH_LIMIT: '[ERROR] 자동차 이름은 5자 이하만 가능합니다.',
+  INTEGER_ONLY: '[ERROR] 횟수는 정수만 입력 가능합니다.',
+  MORE_THAN_ONE_ROUND: '[ERROR] 1회 부터 플레이 가능합니다.',
+});
+
+export const CONSOLE_MESSAGE = Object.freeze({
+  NAME_OF_CARS:
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+  NUMBER_OF_ROUNDS: '시도할 횟수는 몇 회인가요?\n',
+});
