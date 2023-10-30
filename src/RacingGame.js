44897740f4a9502ac1e5504318fe808a234626fe
kelvin3476 <winrain03226@gmail.com>
2023-10-31 02:32:01 +0900

    refactor(*): 기능7 게임 시작 메서드 구현 완료 및 RacingGame.js 분리 후 출력값 모듈화 작업 완료

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -1,27 +1,44 @@
-import { MissionUtils } from '@woowacourse/mission-utils';
 import Car from './Car';
+import Input from './utils/Input';
+import Output from './utils/Output';
 
 class RacingGame {
-  constructor(carNames, totalAttempts) {
-    this.cars = carNames.split(',').map(name => new Car(name));
+  #cars;
+
+  constructor(totalAttempts) {
+    this.#cars = [];
     this.totalAttempts = totalAttempts;
+  }
+
+  // TODO: Mission 7: 게임 시작 메서드입니다.
+  async start() {
+    const cars = await Input.startGame();
+    this.#car(cars);
+    const count = await Input.moveCount();
+    for (let i = 0; i < count; i += 1) this.playGame();
+    const winners = this.determineWinner();
+    Output.winner(winners);
+  }
+
+  #car(cars) {
+    cars.forEach(car => this.#cars.push(new Car(car)));
   }
 
   // TODO: Mission 4: 각 차수별 실행 결과 출력 메서드입니다.
   playGame() {
     for (let attempt = 0; attempt < this.totalAttempts; attempt += 1) {
-      MissionUtils.Console.print(`시도 ${attempt + 1}:`);
-      this.cars.forEach(car => {
-        car.move();
-        MissionUtils.Console.print(`${car.name} : ${'-'.repeat(car.position)}`);
-      });
+      Output.roundAttempt(attempt);
     }
+    this.#cars.forEach(car => {
+      car.move();
+      Output.roundResult(car);
+    });
   }
 
   // TODO: Mission 6: 우승자 결정 메서드입니다.
   determineWinner() {
-    const maxPosition = Math.max(...this.cars.map(car => car.position));
-    const winners = this.cars
+    const maxPosition = Math.max(...this.#cars.map(car => car.position));
+    const winners = this.#cars
       .filter(car => car.position === maxPosition)
       .map(car => car.name);
     return winners;
