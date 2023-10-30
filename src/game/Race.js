351e4fd2884d0ccc5dd224937f4d77a47e931170
devGEP <ju0032564@gmail.com>
2023-10-31 02:26:03 +0900

    feat: 가장 많이 전진한 자동차 구하고, 최종 우승자 출력하는 기능
    [Race]
    - 가장 많이 전진한 자동차 계산
    - 최종 우승자 출력

--- src/game/Race.js
+++ src/game/Race.js
@@ -1,4 +1,4 @@
-import { Random } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 import Car from "./Car.js";
 
 class Race {
@@ -10,17 +10,28 @@ class Race {
   }
 
   playRace(carsName, raceCount) {
-    let playersPosition = new Array(carsName.length).fill(0);
+    let carsPosition = new Array(carsName.length).fill(0);
     
     for (let i=0 ; i<raceCount; i++) {
       for (let j=0; j<carsName.length; j++) {
         let randomNum = this.createRandomNum();
 
-        playersPosition[j] += this.car.moveOrStop(carsName[j], randomNum);
-        this.car.printCurrentPosition(carsName[j], playersPosition[j]);
+        carsPosition[j] += this.car.moveOrStop(carsName[j], randomNum);
+        this.car.printCurrentPosition(carsName[j], carsPosition[j]);
       }
       console.log();
     }
+
+    return this.calculateWinner(carsName, carsPosition);
+  }
+
+  calculateWinner(carsName, carsPosition) {
+    let winnerPosition = Math.max(...carsPosition);
+    return this.printWinner(carsName[carsPosition.indexOf(winnerPosition)]);
+  }
+
+  printWinner(winnerCarName) {
+    Console.print('최종 우승자: '+ winnerCarName);
   }
 }
 
