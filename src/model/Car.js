37ced53e0df91983fbc393ab92cf8e73fb382fdf
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-10-31 04:28:35 +0900

    feat: 게임 진행 시 필요한 자동차 정보를 담은 Model 추가

---
+++ src/model/Car.js
@@ -0,0 +1,24 @@
+class Car {
+
+  #name;
+
+  #distance = 0;
+
+	constructor(name) {
+		this.#name = name;
+	}
+
+  getName() {
+    return this.#name;
+  }
+
+  getDistance() {
+    return this.#distance;
+  }
+
+  moveForward() {
+    this.#distance += 1;
+  }
+}
+
+export default Car;
