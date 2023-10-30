3ee1fb60b7a9b98bd82545541f1eb09cc4dbbe37
delee3064 <delee3064@gmail.com>
2023-10-30 16:29:58 +0900

    refactor : 레이싱게임 함수 분리

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import MSG from "./message.js"
 import nameValidation from "./validation.js"
+import game from "./game.js"
 
 class App {
   async play() {
@@ -9,42 +10,9 @@ class App {
     nameValidation(carNames);
     const cnt = await MissionUtils.Console.readLineAsync(MSG.INPUTCOUNT);
     MissionUtils.Console.print(MSG.RESULT)
-    this.printResult(carNames, cnt)
-
+    game(carNames, cnt)
   }
-
-  printWinner(carNames, carDistance) {
-    const maxDistance = Math.max(...carDistance)
-    var winner = new Array()
-    for (let i = 0; i < carNames.length; i++) {
-      if (carDistance[i] === maxDistance) {
-        winner.push(carNames[i])
-      }
-    }
-    MissionUtils.Console.print(`${MSG.WINNER} ${winner.join(", ")}`)
-  }
-
-  printResult(carNames, cnt) {
-    var carDistance = Array.from({length : carNames.length}, ()=> 0)
-    for (let i = 0; i < cnt; i++) {
-      const gameResult = this.race(carNames, carDistance)
-      for (let i = 0; i < carNames.length; i++) {
-        MissionUtils.Console.print(carNames[i] + " : " + "-".repeat(gameResult[i]))
-      }
-      MissionUtils.Console.print("")
-    }
-    this.printWinner(carNames, carDistance)
-  }
-
-  race(carNames, carDistance) {    
-    for (let i = 0; i < carNames.length; i++) {
-      let num = MissionUtils.Random.pickNumberInRange(0, 9);
-      if (num > 3) {
-        carDistance[i] += 1;
-      }
-    }
-    return carDistance    
-  }  
+    
 }
 
 export default App;
