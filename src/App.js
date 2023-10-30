8ec91d7cb320ab4d5899e51b2d95c7279a6abb83
uraflower <82873315+uraflower@users.noreply.github.com>
2023-10-30 21:24:41 +0900

    feat: app.play 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { Console } from '@woowacourse/mission-utils';
+import RaceManager from './RaceManager.js';
+import ReceptionDesk from './ReceptionDesk.js';
+import Referee from './Referee.js';
+import ScoreBoard from './ScoreBoard.js';
+import { MOVEMENT_STATUS } from './constants/messagesConstants.js';
+
+function race(cars, attempts) {
+  Console.print(MOVEMENT_STATUS);
+  for (let i = 0; i < attempts; i += 1) {
+    cars.forEach((car) => {
+      const randomNumber = RaceManager.generateRandomNumber();
+      car.move(randomNumber);
+    });
+    ScoreBoard.announceInterimResult(cars);
+    Console.print('');
+  }
+}
+
 class App {
-  async play() {}
+  async play() {
+    const cars = await ReceptionDesk.registerRacingCars();
+    const attempts = await RaceManager.getNumberOfAttempts();
+    race(cars, attempts);
+    const winners = Referee.checkWinners(cars);
+    ScoreBoard.announceWinners(winners);
+  }
 }
 
 export default App;
--- src/constants/messagesConstants.js
+++ src/constants/messagesConstants.js
@@ -1,5 +1,6 @@
 export const INPUT_CARS =
   '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) : ';
+export const MOVEMENT_STATUS = '경주 진행 결과';
 
 // error messages
 export const ERROR_NAME_LENGTH = '[ERROR] 이름은 5자 이내여야 합니다.';
