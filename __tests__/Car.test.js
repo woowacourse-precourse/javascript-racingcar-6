7903af0c0191041ec1930cbad5f9bb77600cea1e
uraflower <82873315+uraflower@users.noreply.github.com>
2023-10-30 15:16:13 +0900

    test: 전진/정지 테스트가 입력-출력 값을 명시하도록 변경
    이 애플리케이션에서 중요한 건, '랜덤 숫자에 따라 전진/정지했다고 판단하도록 구현했는가?' 아니라, '랜덤 숫자에 따라 전진/정지했음을 사용자가 볼 수 있도록 출력했는가?'라고 생각함.
    그래서 테스트 이름에 입력-출력 값을 명시하고, 실제 테스트 내용도 사용자에게 보여지는 값을 기준으로 테스트하도록 변경함.

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,18 +1,17 @@
 import Car from '../src/Car.js';
 import { MIN_MOVEMENT } from '../src/constants/constants.js';
-import generateRandomNumber from '../src/functions/generateRandomNumber.js';
-
-jest.mock('../src/functions/generateRandomNumber.js');
 
 describe(`랜덤 숫자가 ${MIN_MOVEMENT}이상이면 전진, 미만이면 정지한다`, () => {
   test.each([
-    [0, '정지', false],
-    [4, '전진', true],
-    [9, '전진', true],
-  ])('랜덤 숫자가 %d이면 %s한다', (randomNumber, description, expected) => {
-    generateRandomNumber.mockReturnValue(randomNumber);
-    const car = new Car();
-    const isMoved = car.move();
-    expect(isMoved).toBe(expected);
-  });
+    [0, '정지', ''],
+    [4, '전진', '-'],
+    [9, '전진', '-'],
+  ])(
+    '랜덤 숫자가 %d이면 %s한다 (결과: "%s")',
+    (randomNumber, description, expected) => {
+      const car = new Car();
+      car.move(randomNumber);
+      expect(car.movement).toBe(expected);
+    },
+  );
 });
