1981832a17c01432d6f355ae3fd1dd029d2fb4b0
shinheylynn <heylub@gmail.com>
2023-10-31 00:21:22 +0900

    Feat: getCarNamesFromUser 메소드
    입출력 기능 추가:
    - 사용자로부터 자동차 이름 입력 값 받기
    - 사용자로부터 입력 받은 자동차 이름 ","로 구별 짓기
    - 사용자로부터 입력 받은 자동차 이름 출력하기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
-	async play() {}
+	constructor() {
+		this.cars = [];
+	}
+
+	async play() {
+		await this.getCarNamesFromUser();
+	}
+
+	async getCarNamesFromUser() {
+		const userInputCar = await MissionUtils.Console.readLineAsync();
+		await MissionUtils.Console.print(
+			"경주할 자동차 이름을 입력하세요 (5자 이하): "
+		);
+		this.cars = userInputCar.split(",");
+
+		MissionUtils.Console.print(String(this.cars));
+
+		return this.cars;
+	}
 }
 
 export default App;
