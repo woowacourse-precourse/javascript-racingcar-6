a61d527b97ba26b2b95b99ee1e6cdeb256093553
uraflower <82873315+uraflower@users.noreply.github.com>
2023-10-30 21:27:59 +0900

    refactor: 시도 횟수 입력 메시지를 상수로 선언

--- src/RaceManager.js
+++ src/RaceManager.js
@@ -1,10 +1,11 @@
 import { Console, Random } from '@woowacourse/mission-utils';
 import { validateAttempts } from './functions/validate.js';
 import { MAX_RANGE, MIN_RANGE } from './constants/randomNumberConstants.js';
+import { INPUT_ATTEMPTS } from './constants/messagesConstants.js';
 
 class RaceManager {
   static async getNumberOfAttempts() {
-    const input = await Console.readLineAsync('시도 횟수 : ');
+    const input = await Console.readLineAsync(INPUT_ATTEMPTS);
     validateAttempts(input);
     return Number(input);
   }
