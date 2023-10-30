209078ecae224b47fcb848e71d5e60c3faaf197d
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-10-30 23:40:59 +0900

    feat:문자열에서 배열로 변환하는 유틸 함수 추가

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,9 @@
-import { ERROR_PREFIX } from './constants';
+import { DIVIDER, ERROR_PREFIX } from './constants.js';
 
 export const errorMessage = (issue) => {
   return `${ERROR_PREFIX}${issue}`;
+};
+
+export const stringToArray = (input) => {
+  return input.split(DIVIDER);
 };
