d889e1afadbfa0d72c2d81cd9279e476e7481a18
hectick <ch.yeondev@gmail.com>
2023-10-30 21:51:23 +0900

    feat: 이름을 쉼표 기준으로 구분하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    const carNames = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n")
+    const input = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n")
+    const carNames = input.split(",")
   }
 }
 
