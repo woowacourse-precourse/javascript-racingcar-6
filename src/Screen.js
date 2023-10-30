d8bb74275eb4f9ecf2af3988b154026319ae009e
hong <ahi200@naver.com>
2023-10-30 21:14:53 +0900

    feat: 우승자 출력 메소드 구현

--- src/Screen.js
+++ src/Screen.js
@@ -2,6 +2,7 @@ import { Console } from '@woowacourse/mission-utils';
 
 const MSG_GAME_START =
   '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n';
+const MSG_TRY_TIME = '시도할 횟수는 몇 회인가요?\n';
 
 class Screen {
   async getUserInput(message) {
@@ -15,6 +16,10 @@ class Screen {
     });
   }
 
+  printWinner([...winnerList]) {
+    Console.print(`최종 우승자 : ${winnerList.join(', ')}`);
+  }
+
   makeDashString(moveCount) {
     let str = '';
     for (let i = 0; i < moveCount; i++) {
@@ -22,7 +27,5 @@ class Screen {
     }
     return str;
   }
-
-  printWinner(winnerList) {}
 }
 export default Screen;
