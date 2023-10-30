345afbc7c3d7be5b60c2188ba6e477a1c6337990
kang <kangju2000@naver.com>
2023-10-30 15:55:35 +0900

    :sparkles: validator 구현

--- src/validator/validator.js
+++ src/validator/validator.js
@@ -5,20 +5,26 @@ const validator = {
    * @param {number} maxLength 최대 길이
    * @returns {boolean}
    */
-  validNameLength: (name, maxLength) => {},
+  validNameLength: (name, maxLength) => {
+    return name.length <= maxLength && name.length > 0
+  },
   /**
    * 이름이 중복되는지 검사한다.
    * @param {string[]} nameList 이름 리스트
    * @returns {boolean}
    */
-  isDuplicateName: (nameList) => {},
+  isDuplicateName: (nameList) => {
+    return nameList.length !== new Set(nameList).size
+  },
   /**
    * 시도 횟수가 유효한지 검사한다.
    * @param {number} inputTryCount 입력된 시도 횟수
    * @param {number} minTryCount 최소 시도 횟수
    * @returns {boolean}
    */
-  validTryCount: (inputTryCount, minTryCount) => {},
+  validTryCount: (inputTryCount, minTryCount) => {
+    return Number.isInteger(inputTryCount) && inputTryCount >= minTryCount
+  },
 }
 
 export default validator
