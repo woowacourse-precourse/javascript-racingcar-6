3c9550e16b824f1a6122e251746946aa0fb5f988
jeeyng <petitesize22@gmail.com>
2023-10-30 16:56:01 +0900

    FEAT: 자동차 전진 여부를 결정하는 canMove() 메서드 구현

--- src/Car.js
+++ src/Car.js
@@ -1,9 +1,11 @@
+/* eslint-disable import/extensions */
 import { Random } from '@woowacourse/mission-utils';
 
 export default class Car {
   constructor() {
     this.RANDOM_NUMER = '';
     this.GET_RANDOM = '';
+    this.CAN_MOVE_FORWARD = 4;
     this.MOVE_COUNT = 0;
     this.POSITION = '';
   }
@@ -11,5 +13,15 @@ export default class Car {
   generateRandomNum() {
     this.RANDOM_NUMER = Random.pickNumberInRange(0, 9);
     return this.RANDOM_NUMER;
+  }
+
+  canMove(tryNum) {
+    for (let n = 0; n < tryNum; n += 1) {
+      this.GET_RANDOM = this.generateRandomNum();
+      if (this.GET_RANDOM > this.CAN_MOVE_FORWARD) {
+        this.MOVE_COUNT += 1;
+        this.POSITION += '-';
+      }
+    }
   }
 }
