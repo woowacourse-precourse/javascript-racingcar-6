a0c5675241482f19e167277cf51f770acd01b3e0
unknown <pih9134@naver.com>
2023-10-31 02:55:20 +0900

    feat: 시도횟수 및 입력조건

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
   async play() {
     const cars = await this.returnCarNames();
-    Console.print(cars);
+    const tryCount = await this.getTryCount();
+    Console.print(cars, tryCount);
   }
 
   async returnCarNames() {
     const carNames = await Console.readLineAsync('자동차 이름을 입력하세요. (이름은 쉼표(,)로 구분)\n');
 
     return this.validName(carNames);
+  }
+
+  async getTryCount() {
+    const tryCount = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    return this.isInvalidCount(tryCount);
+  }
+
+  isInvalidCount(tryCount) {
+    if (Number.isNaN(tryCount) || tryCount < 1) {
+      throw new Error('[ERROR] 시도 횟수는 0 이상의 숫자여야 합니다.');
+    }
+    return tryCount;
   }
 
   validName(carNames) {
