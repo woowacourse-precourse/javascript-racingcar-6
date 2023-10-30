07b0fc1f1b1c590bc9535174fedd794cb44427bd
Mungjin01 <beckaljm@naver.com>
2023-10-30 14:45:04 +0900

    refactor: 불필요한 코드들 제거

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import Car from './car.js';
 
 class App {
   async play() {
-    Car.createCarsFromInput((cars) => {
-      const carNames = cars.map(car => car.name).join(', ');
-      console.log(`입력받은 자동차 이름들 출력: ${carNames}`);
-    });
+    Car.createCarsFromInput((cars) => {});
   }
 }
 
--- src/car.js
+++ src/car.js
@@ -5,36 +5,36 @@ class Car {
     this.name = name;
   }
 
-  static createCarsFromInput(callback) {
-    Console.readLine('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)', (carNamesString) => {
-      console.log('입력받은 자동차 이름들:', carNamesString);
+  static async createCarsFromInput() {
+    const carNamesString = await Console.readLineAsync('경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)로 구분): \n');
 
-      const carNames = carNamesString.trim().split(',').map(name => name.trim());
+    console.log('입력받은 자동차 이름들:', carNamesString);
 
-      // 예외 처리: 자동차 이름이 6자 이상일 경우
-      if (carNames.some(name => name.length >= 6)) {
-        throw new Error('[ERROR] 자동차 이름은 5자 이하여야 합니다.');
-      }
+    const carNames = carNamesString.trim().split(',').map(name => name.trim());
 
-      // 예외 처리: 자동차가 2대 미만일 때
-      if (carNames.length < 2) {
-        throw new Error('[ERROR] 최소 2대의 자동차가 필요합니다.');
-      }
+    // 예외 처리: 자동차 이름이 6자 이상일 경우
+    if (carNames.some(name => name.length >= 6)) {
+      throw new Error('[ERROR] 자동차 이름은 5자 이하여야 합니다.');
+    }
 
-      // 예외 처리: 자동차 이름이 중복일 때
-      const uniqueCarNames = [...new Set(carNames)];
-      if (carNames.length !== uniqueCarNames.length) {
-        throw new Error('[ERROR] 중복된 자동차 이름이 있습니다.');
-      }
+    // 예외 처리: 자동차가 2대 미만일 때
+    if (carNames.length < 2) {
+      throw new Error('[ERROR] 최소 2대의 자동차가 필요합니다.');
+    }
 
-      // 예외 처리: 자동차 이름을 입력하지 않고 쉼표만 입력했을 때
-      if (carNames.length === 1 && carNames[0] === '') {
-        throw new Error('[ERROR] 자동차 이름을 입력해야 합니다.');
-      }
+    // 예외 처리: 자동차 이름이 중복일 때
+    const uniqueCarNames = [...new Set(carNames)];
+    if (carNames.length !== uniqueCarNames.length) {
+      throw new Error('[ERROR] 중복된 자동차 이름이 있습니다.');
+    }
 
-      const cars = carNames.map(name => new Car(name));
-      callback(cars);
-    });
+    // 예외 처리: 자동차 이름을 입력하지 않고 쉼표만 입력했을 때
+    if (carNames.length === 1 && carNames[0] === '') {
+      throw new Error('[ERROR] 자동차 이름을 입력해야 합니다.');
+    }
+
+    const cars = carNames.map(name => new Car(name));
+    return cars;
   }
 }
 
