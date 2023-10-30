2fdd2a97d291085449f8ed04c84ea20f4a77a95e
FirstBright <mipo8890@gmail.com>
2023-10-30 20:52:46 +0900

    :sparkles: feat: car class 전진, 이름 구현

--- src/Car.js
+++ src/Car.js
@@ -1,9 +1,17 @@
-class Car{    
-    constructor(name){
+import { MissionUtils } from '@woowacourse/mission-utils';
+class Car {    
+    constructor(name) {
         this.name = name;
         this.forward = 0;
+        if (this.name.length>5){
+            throw new Error('[ERROR] 5자리 이하의 이름만 가능합니다.');
+        }
     }
-    run(){        
+    run() { 
+        if (MissionUtils.Random.pickNumberInRange(0, 9)>=4){
+            forward++;
+        }       
         return forword;
     }
+
 }
