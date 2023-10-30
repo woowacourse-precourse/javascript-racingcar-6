07978099951d7681c117265b49d3b11659ceeddc
Goonco <s07019@naver.com>
2023-10-31 00:23:44 +0900

    :adhesive_bandage: 올바른 출력을 위한 메세지 상수 수정

--- src/Constants.js
+++ src/Constants.js
@@ -2,7 +2,7 @@ export const InGameMessages = {
   PRINT_INPUT_CARS:
     '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
   PRINT_INPUT_ATTEMPT: '시도할 횟수는 몇 회인가요?\n',
-  PRINT_RESULT: '실행 결과\n',
+  PRINT_RESULT: '\n실행 결과',
   PRINT_WINNER: '최종 우승자 : ',
 };
 
