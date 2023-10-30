0d0d4a8cd902e3182dd7987165bf44a51316f2f7
Dev-steadyBoy <85480086+Dev-steadyBoy@users.noreply.github.com>
2023-10-30 15:06:27 +0900

    refactor: RaceStatus객체 내 상수 모듈화 구현

--- src/exception/RaceStatus.js
+++ src/exception/RaceStatus.js
@@ -1,6 +1,9 @@
+// 상수 모듈
+import { NUMBER } from "../utils/Constants.js";
+
 class RaceStatus {
   static reportTrackIssue(lap) {
-    if (lap.length === 0) {
+    if (lap.length === NUMBER.ZERO) {
       throw new Error(
         "[ERROR] 트랙에 문제가 발생해 경기에 차질이 생겼습니다. 트랙을 확인해주세요.",
       );
