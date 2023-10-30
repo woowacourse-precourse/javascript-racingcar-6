2e6f5e171163ff9f40c07727011218fd789767ed
yohan11 <yipco77@naver.com>
2023-10-30 16:48:55 +0900

    feat: 자동차 입력 받아 배열로 저장하는 기능 구현

--- src/game/init.js
+++ src/game/init.js
@@ -1,17 +1,43 @@
 import {MissionUtils} from "@woowacourse/mission-utils";
-import {INPUT_CARS_STRING, INPUT_PLAY_COUNT_STRING} from "./constants.js";
+import {ERROR_HEAD, INPUT_CARS_STRING, INPUT_PLAY_COUNT_STRING, SPLIT_MARK} from "./constants.js";
+
+import {carListValidator, carNameValidator, carStringValidator, playCountValidator} from "./validator.js";
 
 class Init {
+
     async getUserInput() {
+        let carsList = new Array(String);
+
         MissionUtils.Console.print(INPUT_CARS_STRING);
         const carsString = await MissionUtils.Console.readLineAsync('');
-        // 입력받은 문자열 안에 문자, 숫자, 쉼표만이 들어있는지 validate
-        // 문자열 분리 함수 호출
+
+        if (carStringValidator(carsString)) {
+            carsList = this.makeCarsList(carsString);
+        } else {
+            throw new Error(ERROR_HEAD);
+        }
+
         MissionUtils.Console.print(INPUT_PLAY_COUNT_STRING);
         const playCount = await MissionUtils.Console.readLineAsync('');
-        // 숫자 validate
 
-        //Racing(carsList, playCount);
+        if (playCountValidator(playCount)) {
+            // Racing(carsList, playCount);
+        } else {
+            throw new Error(ERROR_HEAD);
+        }
+    }
+
+    makeCarsList(carsString) {
+        const carsList = carsString.split(SPLIT_MARK).map(carName => carName.trim());
+
+        const isCarNameValid = carsList.every(carName => carNameValidator(carName));
+        const isCarListValid = carListValidator(carsList)
+
+        if (!isCarNameValid || !isCarListValid) {
+            throw new Error(ERROR_HEAD);
+        }
+
+        return carsList;
     }
 }
 
