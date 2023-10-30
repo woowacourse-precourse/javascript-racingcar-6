63d9fd262c81a757d89b16529780f448baba9c09
Narachan97 <qkrtkf3112@naver.com>
2023-10-30 16:25:57 +0900

    feat: 사용자 라운드 입력
    src/App.js 사용자 라운드 입력 추가
    
    App 새로운 기능
    -사용자 로 부터 레이싱 라운드를 입력을 받는다

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ class App {
     const carNameInput = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     const carNameArray = carNameInput.split(",");
     
+    const playRound = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+    
   }
 }
 
