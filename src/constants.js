7ff4d39343aa2c188022eb7f6bb22077b8f7f4f7
soosoo22 <lllllh00@naver.com>
2023-10-30 23:06:09 +0900

    chore: constants.js 출력 문구 변경
    약간의 문구 변경

--- src/constants.js
+++ src/constants.js
@@ -17,6 +17,6 @@ export const ERROR = {
   INVALID_CAR_NAME: `${PREFIX}유효하지 않은 자동차 이름 형식입니다.`,
   DUPLICATE_CAR_NAME: `${PREFIX}중복되는 자동차 이름이 존재합니다.`,
   OVER_CAR_NAME_LENGTH: `${PREFIX}자동차 이름이 공백 포함 5자 이하가 아닙니다.`,
-  INVALID_COUNT: `${PREFIX}시도 횟수를 입력해 주세요.`,
+  INVALID_COUNT: `${PREFIX}시도할 횟수를 입력하세요.`,
   MUST_ENTER_A_NUMBER_OVER_ZERO: `${PREFIX}시도 횟수는 0보다 큰 수여야 합니다.`,
 };
