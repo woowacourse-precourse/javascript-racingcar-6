a48e2e0ca11461bace7c26c363cd6a5ec1bc5751
sunub <bsc5672@gmail.com>
2023-10-30 18:51:36 +0900

    feat : Round 진행 시 출력되기 기대되는 Message를 출력하는지 확인하는 실패하는 테스트 코드 작성

--- __tests__/unit/MessageTest.js
+++ __tests__/unit/MessageTest.js
@@ -1,5 +1,4 @@
 import { Console } from '@woowacourse/mission-utils';
-import Message from '../../src/Message';
 import RaceRound from '../../src/RaceRound';
 
 const getLogSpy = () => {
@@ -8,29 +7,44 @@ const getLogSpy = () => {
   return logSpy;
 };
 
-const mockRandom = (inputs) => {
+const mockRandom = (numbers) => {
   RaceRound.createRandomNum = jest.fn();
-  RaceRound.createRandomNum.mockImplementation(() => {
-    const input = inputs.shift();
-    return input;
-  });
+  numbers.reduce((acc, number) => acc.mockReturnValueOnce(number), RaceRound.createRandomNum);
 };
 
 describe('특정 상황에서 출력되어야 하는 문자에 대한 테스트', () => {
-  test('차가 전진할 때마다 "-" 문자가 출력 되는가?', () => {
-    const names = ['pobi', 'nori'];
-    const raceRound = new RaceRound(names);
-    const logSpy = getLogSpy();
+  const MOVING_FORWARD = 4;
+  const STOP = 0;
+  const names = ['pobi', 'nori'];
+  const randoms = [MOVING_FORWARD, STOP];
+  const raceRound = new RaceRound(names, 1);
 
-    Message.forMovingForward;
-    expect(logSpy).toContain('-');
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
 
-  test('게임 종료 시 최종 우승자를 출력하는가? (우승자가 1명일 경우)', () => {
-    Message.forGameResult;
+  test('차가 전진할 때마다 "-" 문자가 출력 되는가?', () => {
+    mockRandom([...randoms]);
+    const logSpy = getLogSpy();
+    const expectedMessage = '-';
+
+    mockRandom([...randoms]);
+
+    raceRound.proceedRound();
+    expect(logSpy).toHaveBeenCalledWith(
+      expect.stringContaining(expectedMessage),
+    );
   });
 
   test('게임 종료 시 최종 우승자를 출력하는가? (우승자가 여러명일 경우)', () => {
-    Message.forGameResult;
+    const expectedMessage = '최종 우승자 : pobi';
+    const logSpy = getLogSpy();
+
+    mockRandom([...randoms]);
+
+    raceRound.proceedRound();
+    expect(logSpy).toHaveBeenCalledWith(
+      expect.stringContaining(expectedMessage),
+    );
   });
 });
