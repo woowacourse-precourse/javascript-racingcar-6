b4924ca6d05aed8266fb2850ab2f540836812341
kelvin3476 <winrain03226@gmail.com>
2023-10-31 05:32:36 +0900

    refactor(*): 게임 시작 메시지 출력 삭제

--- src/utils/Input.js
+++ src/utils/Input.js
@@ -4,9 +4,6 @@ import Validation from './Validation';
 
 const Input = {
   async startGame() {
-    // 게임 시작 메시지 출력
-    MissionUtils.Console.print(MESSAGE.start);
-
     // TODO: Mission 1: 자동차 생성 및 이름 부여 메서드입니다.
     const carNames = await MissionUtils.Console.readLineAsync(MESSAGE.car);
     Validation.checkNull(carNames);
