0e26c513610cb51949d73b3aa8fd9b8fcc2c35bc
youngchann <lych96@gmail.com>
2023-10-30 16:26:10 +0900

    feat: 게임 시작 문구 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  constructor() {
+    this.startMessage =
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)';
+    this.countMessage = '시도할 횟수는 몇 회인가요?';
+  }
+
+  async play() {
+    await MissionUtils.Console.print(this.startMessage);
+  }
 }
 
 export default App;
