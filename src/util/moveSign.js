50e45d14213d12331821d677cda47497846abd11
ChoByungHyun <cbj973@gmail.com>
2023-10-30 21:45:55 +0900

    ✨ Feat: 자동차 움직임 여부 확인하는 함수 구현

--- src/util/moveSign.js
+++ src/util/moveSign.js
@@ -1,9 +1,5 @@
 import { Random } from "@woowacourse/mission-utils";
-import NUMBER from "../constant/number.js";
+import { NUMBER } from "../constant/number.js";
 export default function moveSign() {
-  if (Random.pickNumberInRange(NUMBER.MIN, NUMBER.MAX) >= NUMBER.MOVE_NUMBER) {
-    return true;
-  } else {
-    return false;
-  }
+  return Random.pickNumberInRange(NUMBER.MIN, NUMBER.MAX) >= NUMBER.MOVE_NUMBER;
 }
