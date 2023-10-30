79eef8e3f96792309cb5cf297a73868a8e2fb1d2
zelkovaria <hyein396@gmail.com>
2023-10-30 22:26:28 +0900

    test: split 메서드로 주어진 값을 구분

--- __tests__/racingGameTest.js
+++ __tests__/racingGameTest.js
@@ -1,8 +1,18 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 jest.mock("../src/car", () => ({
   carGenerate: jest.fn().mockResolvedValue(["car1", "car2"]),
 }));
 
+const mockReadLineAsync = jest.fn();
+jest.mock("@woowacourse/mission-utils", () => ({
+  Console: {
+    readLineAsync: mockReadLineAsync,
+  },
+}));
+
 const { generateCars } = require("../src/racingGame");
+const { carGenerate } = require("../src/car");
 
 describe("자동차 경주 게임", () => {
   test("자동차 생성시 이름, 거리 반환", async () => {
@@ -11,5 +21,15 @@ describe("자동차 경주 게임", () => {
       { carName: "car1", carDistance: "" },
       { carName: "car2", carDistance: "" },
     ]);
+  });
+
+  test("split 메서드로 주어진 값을 구분", async () => {
+    const input = "pobi,woni";
+    mockReadLineAsync.mockResolvedValue(input);
+    carGenerate.mockResolvedValueOnce(["pobi", "woni"]);
+
+    const result = await carGenerate();
+
+    expect(result).toContainEqual("woni", "pobi");
   });
 });
