a8eb9fb755f7f46a14b25626ef82b4ed846c9f35
yoovin <soo22839@gmail.com>
2023-10-30 23:20:56 +0900

    feat: 유저 이름 받아오는 함수 구현

--- src/App.js
+++ src/App.js
@@ -3,8 +3,27 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 class App {
     async play() {}
 
-    getUserInput() {
-        return ['yoo', 'vin'];
+    async getUserInput() {
+        const userinput = await MissionUtils.Console.readLineAsync(
+            '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+        );
+
+        console.log(userinput);
+
+        const users = userinput.split(',').map((name) => name.trim());
+        users.forEach((name) => {
+            if (name.length > 5) {
+                throw new Error('[ERROR] 이름이 너무 깁니다.');
+            }
+
+            if (name.length === 0) {
+                throw new Error('[ERROR] 이름이 없습니다.');
+            }
+        });
+
+        console.log(users);
+
+        return users;
     }
 
     createCars(names) {
