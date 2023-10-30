f5b198ff8abe3ed53ef5e0e6a93c6805a26d8653
MinGu Jeong <mingu830@gmail.com>
2023-10-30 09:49:47 +0900

    refactor: 상수 네이밍 컨벤션 수정

--- src/Validation.js
+++ src/Validation.js
@@ -2,21 +2,21 @@ import { ERROR } from './constants/Error.js';
 
 export function validName(name) {
   if (name === '') {
-    throw new Error(ERROR.nameBlank);
+    throw new Error(ERROR.NAME_NULL);
   }
   const carNames = name.split(',').map((name) => name.trim());
   for (const carName of carNames) {
     if (carName.length > 5) {
-      throw new Error('[ERROR] 이름은 5글자를 초과할 수 없습니다.');
+      throw new Error(ERROR.NAME_LENGTH);
     }
   }
 }
 
 export function validTryCount(tryCount) {
   if (tryCount === '') {
-    throw new Error(ERROR.tryCountBlank);
+    throw new Error(ERROR.TRY_COUNT_NULL);
   }
   if (isNaN(tryCount)) {
-    throw new Error(ERROR.tryCountNaN);
+    throw new Error(ERROR.TRY_COUNT_NAN);
   }
 }
--- src/constants/Error.js
+++ src/constants/Error.js
@@ -1,6 +1,6 @@
 export const ERROR = {
-  nameBlank: '[ERROR] 이름을 입력해주세요.',
-  nameLength: '[ERROR] 이름은 5자 이하만 가능합니다.',
-  tryCountBlank: '[ERROR] 시도 횟수를 입력해주세요.',
-  tryCountNaN: '[ERROR] 시도 횟수는 숫자여야 합니다.',
+  NAME_NULL: '[ERROR] 이름을 입력해주세요.',
+  NAME_LENGTH: '[ERROR] 이름은 5자 이하만 가능합니다.',
+  TRY_COUNT_NULL: '[ERROR] 시도 횟수를 입력해주세요.',
+  TRY_COUNT_NAN: '[ERROR] 시도 횟수는 숫자여야 합니다.',
 };
