c9ae0ff3d8f57e9c4196e9a17f004a73c9f7debd
jonique98 <tnals9899@naver.com>
2023-10-30 17:41:28 +0900

    refactor: class에서 함수형으로 변경 static 클래스 메소드 대신 함수형으로 변경해서 메모리 자원 최적화

--- src/view/Validation.js
+++ src/view/Validation.js
@@ -1,50 +1,43 @@
 import { ERROR } from '../constants/constants.js';
 
-export default class Validation {
-  static checkValidation(carNames) {
-    this.hasSpace(carNames);
-    this.isMoreThanFiveLetters(carNames);
-    this.hasSpecialChar(carNames);
-    this.hasEmptyName(carNames);
-    this.hasDuplicateName(carNames);
-  }
-
-  static hasSpace(carNames) {
+const Validation = {
+  hasSpace: (carNames) => {
     carNames.forEach((carName) => {
       if (carName.includes(' ')) {
         throw new Error(ERROR.space);
       }
     });
-  }
+  },
 
-  static isMoreThanFiveLetters(carNames) {
+  isMoreThanFiveLetters: (carNames) => {
     carNames.forEach((carName) => {
       if (carName.length > 5) {
         throw new Error(ERROR.moreThanFiveLetters);
       }
     });
-  }
+  },
 
-  static hasSpecialChar(carNames) {
+  hasSpecialChar: (carNames) => {
     carNames.forEach((carName) => {
       if (carName.match(/[^a-zA-Z0-9가-힣]/)) {
         throw new Error(ERROR.specialChar);
       }
     });
-  }
+  },
 
-  static hasEmptyName(carNames) {
+  hasEmptyName: (carNames) => {
     carNames.forEach((carName) => {
       if (carName === '') {
         throw new Error(ERROR.empty);
       }
     });
-  }
+  },
 
-  static hasDuplicateName(carNames) {
+  hasDuplicateName: (carNames) => {
     const set = new Set(carNames);
     if (set.size !== carNames.length) {
       throw new Error(ERROR.duplicate);
     }
-  }
-}
+  },
+};
+
