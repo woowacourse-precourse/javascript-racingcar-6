906887cb492a68842b9b70b01ef87fe40340c330
hj7321 <oboki2001@naver.com>
2023-10-30 22:50:32 +0900

    test: MyTestCode 테스트 코드 수정

--- __tests__/MyTestCode.js
+++ __tests__/MyTestCode.js
@@ -1,31 +1,35 @@
 import App from "../src/App.js";
-import { mockQuestions } from "./ApplicationTest.js"
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 describe("getCarNames 함수", () => {
-  test("올바르게 입력한 경우", () => {
-    const input = "car1, car2, car3";
-    const result = input.split(",").map(name => name.trim());
+  beforeAll(() => {
+    MissionUtils.Console.readLineAsync = jest.fn();
+  });
+
+  test("올바르게 입력한 경우", async () => {
+    MissionUtils.Console.readLineAsync.mockResolvedValue("car1, car2, car3");
+    const app = new App();
+    const result = await app.getCarNames();
     expect(result).toEqual(["car1", "car2", "car3"]);
   });
 
-  test.each([[""]])("이름을 입력하지 않은 경우(예외)", async (input) => {
-    mockQuestions(input);
+  test("이름을 입력하지 않은 경우(예외)", async () => {
+    MissionUtils.Console.readLineAsync.mockResolvedValue("");
     const app = new App();
     await expect(app.getCarNames()).rejects.toThrow("이름을 입력하지 않았습니다.");
   });
 
-  test.each([["car1"]])("이름을 2개 이상 입력하지 않은 경우(예외)", async (input) => {
-    mockQuestions(input);
+  test("이름을 2개 이상 입력하지 않은 경우(예외)", async () => {
+    MissionUtils.Console.readLineAsync.mockResolvedValue("car1");
     const app = new App();
     await expect(app.getCarNames()).rejects.toThrow("이름은 2개 이상 입력해야 합니다.");
   });
 
   test.each([
-    [["car1  car2  car3"]],
-    [["car1 & car2 & car3"]]
-  ])("이름을 ,(콤마)로 구분하지 않은 경우(예외)", async (inputs) => {
-    mockQuestions(inputs);
+    [["car1&car2&car3"]],
+    [["car1 car2 car3"]]
+  ])("이름을 ,(콤마)로 구분하지 않은 경우(예외)", async (input) => {
+    MissionUtils.Console.readLineAsync.mockResolvedValue(input);
     const app = new App();
     await expect(app.getCarNames()).rejects.toThrow("이름은 ,(콤마)로 구분합니다.");
   });
@@ -33,8 +37,8 @@ describe("getCarNames 함수", () => {
   test.each([
     [["car1, carname2, car3"]],
     [["car1, car2, car3, carname4"]]
-  ])("이름을 5자 이하로 입력하지 않은 경우(예외)", async (inputs) => {
-    mockQuestions(inputs);
+  ])("이름을 5자 이하로 입력하지 않은 경우(예외)", async (input) => {
+    MissionUtils.Console.readLineAsync.mockResolvedValue(input);
     const app = new App();
     await expect(app.getCarNames()).rejects.toThrow("이름은 5자 이하만 가능합니다.");
   });
