4921dee41d26ea6561ee5ff5ae69a9c84dc30259
dongbin420 <dongbin420@gmail.com>
2023-10-31 02:34:09 +0900

    refactor: change user input data to property and refactor validate function

--- src/components/RacingGame.js
+++ src/components/RacingGame.js
@@ -1,38 +1,64 @@
 import { Console } from '@woowacourse/mission-utils';
-import { GET_INPUT_MESSAGE, OUTPUT_MESSAGE } from '../utils/constants.js';
+import { INPUT_MESSAGE, OUTPUT_MESSAGE, OTHERS } from '../utils/constants.js';
 import GameCalculator from './GameCalculator.js';
-import UserInputValidator from './UserInputValidator.js';
 
 class RacingGame {
   constructor() {
-    this.userInputCars = null;
+    this.userInputCarNameList = null;
+    this.userInputCarCount = null;
     this.userInputTryCount = null;
   }
 
+  // CONTROLLER
   async start() {
     await this.getUserInput();
   }
 
+  // VIEW
   async getUserInput() {
-    this.userInputCars = await Console.readLineAsync(GET_INPUT_MESSAGE.carName);
-    this.userInputTryCount = await Console.readLineAsync(
-      GET_INPUT_MESSAGE.tryCount
-    );
-    const USER_INPUT_VALIDATOR = new UserInputValidator(
-      this.userInputCars,
-      this.userInputTryCount
-    );
-    USER_INPUT_VALIDATOR.validateCar();
-    USER_INPUT_VALIDATOR.validateNumber();
+    const USER_INPUT_CAR_NAME = await Console.readLineAsync(INPUT_MESSAGE.carName);
+    const USER_INPUT_TRY_COUNT = await Console.readLineAsync(INPUT_MESSAGE.tryCount);
+
+    this.userInputCarNameList = USER_INPUT_CAR_NAME.split(OTHERS.comma);
+    this.userInputCarCount = this.userInputCarNameList.length;
+    this.userInputTryCount = Number(USER_INPUT_TRY_COUNT);
+
+    this.validateUserInputCarName();
+    this.validateUserInputTryCount();
 
     this.showGameResult();
   }
 
-  showGameResult() {
-    const GAME_CALCULATOR = new GameCalculator(
-      this.userInputCars,
-      this.userInputTryCount
+  // MODEL
+  validateUserInputCarName() {
+    const IS_VALID = this.userInputCarNameList.every(
+      (carName) => carName.length <= 5 && carName.length > 0
     );
+
+    if (!IS_VALID) {
+      throw new Error(ERROR_MESSAGE.input);
+    }
+
+    return IS_VALID;
+  }
+
+  // MODEL
+  validateUserInputTryCount() {
+    // = 주위에서 줄바꿈 X -> 나중에 소괄호로 묶기
+    const IS_VALID =
+      typeof this.userInputTryCount === OTHERS.number &&
+      this.userInputTryCount >= 0 &&
+      Math.floor(this.userInputTryCount) === this.userInputTryCount;
+
+    if (!IS_VALID) {
+      throw new Error(ERROR_MESSAGE.input);
+    }
+
+    return IS_VALID;
+  }
+
+  showGameResult() {
+    const GAME_CALCULATOR = new GameCalculator(this.userInputCars, this.userInputTryCount);
     const raceResult = GAME_CALCULATOR.calculate();
     Console.print(OUTPUT_MESSAGE.result);
     Console.print(raceResult[0]);
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const GET_INPUT_MESSAGE = Object.freeze({
+export const INPUT_MESSAGE = Object.freeze({
   carName:
     '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분하며, 각 이름은 5자 이하여야 합니다.',
   tryCount: '시도할 횟수는 몇 회인가요?',
@@ -11,4 +11,15 @@ export const OUTPUT_MESSAGE = Object.freeze({
 
 export const ERROR_MESSAGE = Object.freeze({
   input: '[ERROR] 입력 값이 잘못된 형식입니다.',
+});
+
+export const OTHERS = Object.freeze({
+  comma: ',',
+  number: 'number',
+  baseNum: 3,
+  empty: '',
+  dash: '-',
+  colon: ':',
+  lineBreak: '\n',
+  commaWithSpace: ', ',
 });
