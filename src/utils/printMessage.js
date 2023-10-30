d185f7adc4eca9092c9afe73fd89678d93d49d58
MiHye <57440192+kangmihye@users.noreply.github.com>
2023-10-31 01:39:59 +0900

    refactor: 유틸 삭제
    - printMessage.js 삭제

--- src/utils/printMessage.js
+++
@@ -1,9 +0,0 @@
-import { Console } from "@woowacourse/mission-utils";
-
-export const printMessage = async (message) => {
-	const inputValue = await Console.readLineAsync(message);
-	return inputValue;
-	
-};
-
-
