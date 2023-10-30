b4970186974076b2026e2761e5effb5a3ad8d9d9
rustandbone <dlehdgh14@naver.com>
2023-10-31 00:02:15 +0900

    [기능 향상] 입력값 유효성 체크 메서드 분리

--- src/App.js
+++ src/App.js
@@ -7,34 +7,42 @@ class App {
     this.winner = [];
   }
 
+  carNameValidate(carNames) {
+    if (carNames.indexOf(',') === -1)
+      throw new Error('[ERROR] 자동차 이름 입력이 잘못되었습니다.');
+
+    carNames.split(',').map((car) => {
+      if (car.length > 5)
+        throw new Error('[ERROR] 자동차 이름은 5자 이내로 작성해주세요.');
+      this.racingCars[car] = 1;
+      if (car === '')
+        throw new Error('[ERROR] 자동차 이름 입력이 잘못되었습니다.');
+    });
+  }
+
+  tryNumberCheck(TryNumbers) {
+    if (
+      isNaN(+TryNumbers) ||
+      TryNumbers < 1 ||
+      TryNumbers === Infinity ||
+      TryNumbers === -Infinity
+    )
+      throw new Error('[ERROR] 숫자 입력이 잘못된 형식입니다.');
+  }
+
   async play() {
     try {
       const racingCarNames = await Console.readLineAsync(
         '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
       );
 
-      if (racingCarNames.indexOf(',') === -1)
-        throw new Error('[ERROR] 자동차 이름 입력이 잘못되었습니다.');
-
-      racingCarNames.split(',').map((car) => {
-        if (car.length > 5)
-          throw new Error('[ERROR] 자동차 이름은 5자 이내로 작성해주세요.');
-        this.racingCars[car] = 0;
-        if (car === '')
-          throw new Error('[ERROR] 자동차 이름 입력이 잘못되었습니다.');
-      });
+      this.carNameValidate(racingCarNames);
 
       const racingTryNumbers = await Console.readLineAsync(
         '시도할 횟수는 몇 회인가요?\n'
       );
 
-      if (
-        isNaN(+racingTryNumbers) ||
-        racingTryNumbers < 1 ||
-        racingTryNumbers === Infinity ||
-        racingTryNumbers === -Infinity
-      )
-        throw new Error('[ERROR] 숫자 입력이 잘못된 형식입니다.');
+      this.tryNumberCheck(racingTryNumbers);
 
       Console.print('\n실행 결과');
       for (let i = 0; i < racingTryNumbers; i++) {
