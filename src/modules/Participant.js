218560384607b3ac1a66b3448dd106ee21d39b0f
hs <tlsgustn93@naver.com>
2023-10-30 18:45:24 +0900

    참여자에 대한 유효성 검사 로직 개발

---
+++ src/modules/Participant.js
@@ -0,0 +1,48 @@
+class Participant {
+  constructor(input) {
+    this.input = input.split(",");
+  }
+
+  validate() {
+    if (this.errorCheck()) throw new Error("[ERROR]");
+    return true;
+  }
+
+  errorCheck() {
+    return (
+      this.validateUserCnt() ||
+      this.validateUserNameLength() ||
+      this.validateIsName() ||
+      this.isDuplicate()
+    );
+  }
+
+  validateUserCnt() {
+    return this.input.length === 0;
+  }
+
+  validateUserNameLength() {
+    let check = false;
+    this.input.forEach((user) => {
+      if (user.length > 5 || user.length < 1) check = true;
+    });
+    return check;
+  }
+
+  validateIsName() {
+    const regex = /^[가-힣|a-z|A-Z|]/;
+    let check = false;
+    this.input.forEach((userName) => {
+      userName.split("").forEach((user) => {
+        if (!regex.test(user)) check = true;
+      });
+    });
+    return check;
+  }
+
+  isDuplicate() {
+    return this.input.length !== [...new Set(this.input)].length;
+  }
+}
+
+export default Participant;
