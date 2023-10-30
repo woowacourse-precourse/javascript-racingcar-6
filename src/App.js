b59fe32646451f3cc22c216325fdfc838ac114fc
stegano9744 <stegano9744@gmail.com>
2023-10-30 18:00:27 +0900

    🔗 link: 입출력 기능 연결

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from '@woowacourse/mission-utils';
+import { PRINT_MESSAGE, READ_MESSAGE } from './constants/constants';
+import { validateIsNumber, validateLength } from './utils/validate';
 class App {
-  async play() {}
+  async play() {
+    const carName = await Console.readLineAsync(READ_MESSAGE.start);
+    validateLength(carName);
+
+    const moveCount = await Console.readLineAsync(READ_MESSAGE.move);
+    validateIsNumber(moveCount);
+
+    Console.print(PRINT_MESSAGE.result);
+  }
 }
 
 export default App;
