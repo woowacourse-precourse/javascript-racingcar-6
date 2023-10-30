978c32b14114d5c99dfa59ac6e7079f569fd33cd
okxooxoo <okxooxoo@naver.com>
2023-10-30 19:41:41 +0900

    ✨ feat: 자동차 이름 한글도 허용하도록 수정

--- src/utils/inputValidator.js
+++ src/utils/inputValidator.js
@@ -1,5 +1,5 @@
 export const validateCarNames = (carNames) => {
-	const regex = new RegExp(/^(\w{1,5},\s?)+\w{1,5}$/);
+	const regex = new RegExp(/^([\w가-힣]{1,5},\s?)+[\w가-힣]{1,5}$/);
 
 	if (!regex.test(carNames))
 		throw new Error("[ERROR] 자동차 이름이 잘못된 형식입니다.");
