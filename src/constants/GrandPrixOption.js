397f4acde5a91a11e2f03394bc7848fe6cffe581
swarvy <swarvy0826@naver.com>
2023-10-30 21:44:04 +0900

    refactor : GrandPrixOption의 상수들을 기능에 어울리는 네이밍으로 변경

--- src/constants/GrandPrixOption.js
+++ src/constants/GrandPrixOption.js
@@ -1,5 +1,10 @@
-export const GRANDPRIX_MAGICNUMBER = Object.freeze({
+export const GRANDPRIX_MOVE_OPTION = Object.freeze({
   min: 0,
   max: 9,
   move: 4,
+});
+
+export const GRANDPRIX_GAME_OPTION = Object.freeze({
+  maxInputLength: 5,
+  minInputLength: 1,
 });
