e0c9500e4f81ca5431bc098d48e039ba9e6f8704
MiHye <57440192+kangmihye@users.noreply.github.com>
2023-10-31 00:06:52 +0900

    feat: 함수 추가 및 수정
    - steps 변수 추가
    - getter 함수 추가
    - canMoveStep 함수 수정

--- src/Car.js
+++ src/Car.js
@@ -1,20 +1,39 @@
 import { CAR } from "./Constants.js";
-import { Random } from "@woowacourse/mission-utils";
+import { Random, Console } from "@woowacourse/mission-utils";
 
 class Car {
 	#name;
+	#steps;
 	constructor(name) {
 		if (name.length > CAR.nameLength)
 			throw "자동차 이름의 길이는 5자이하여야 합니다";
 		this.#name = name;
+		this.#steps = "";
 	}
 
 	get name() {
 		return this.#name;
 	}
 
-	get canMoveStep() {
+	get steps() {
+		return this.#steps;
+	}
+
+	get printCarStpes() {
+		return Console.print(`${this.#name} : ${this.#steps}`);
+	}
+
+	canMoveStep() {
 		const number = Random.pickNumberInRange(0, 9);
-		return number >= CAR.StepsNumber ? true : false;
+		// console.log("랜덤숫자: ", number); //여기 나중에 지움
+		number >= CAR.StepsNumber && (this.#steps += "-");
+		return this;
 	}
 }
+
+export default Car;
+// const car = new Car("차1");
+// for (let i = 0; i < 5; i++) {
+// 	car.canMoveStep();
+// }
+// car.printCarStpes;
