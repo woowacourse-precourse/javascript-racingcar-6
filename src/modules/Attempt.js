c77408ddbeca4a044a07d3ea40e2e0779b38f386
hs <tlsgustn93@naver.com>
2023-10-30 19:14:11 +0900

    feat: 몇 회 시도할 것인지에 대한 로직

--- src/modules/Attempt.js
+++ src/modules/Attempt.js
@@ -3,9 +3,7 @@ import { Console } from "@woowacourse/mission-utils";
 class Attempt {
   async userInputTry() {
     const input = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
-
     this.userInput = Number(input);
-
     if (!this.validate()) throw new Error("[ERROR]");
     return true;
   }
