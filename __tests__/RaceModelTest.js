0679a04ed84233448ff9aa8af259dfeecf62f14f
Jin Kwan Kim <kkole3897@gmail.com>
2023-10-31 02:43:51 +0900

    test(RaceModelTest): 다음 라운드 이동 테스트 케이스 추가
    - 총 라운드가 0일 경우 다음 라운드 이동 케이스 추가
    - 다음 라운드 이동 테스트 결과 오류 수정

--- __tests__/RaceModelTest.js
+++ __tests__/RaceModelTest.js
@@ -21,7 +21,23 @@ describe("Race Model 테스트", () => {
   test("다음 라운드 이동", () => {
     // given
     const totalRound = 3;
-    const results = [2, 3, 3, 3];
+    const results = [1, 2, 3, 3];
+
+    // when
+    const race = new Race();
+    race.setTotalRound(totalRound);
+
+    // then
+    results.forEach((result) => {
+      race.goNextRound();
+      expect(race.getCurrentRound()).toBe(result);
+    });
+  });
+
+  test("총 라운드를 0으로 설정했을 경우 다음 라운드 이동", () => {
+    // given
+    const totalRound = 0;
+    const results = [0, 0, 0, 0];
 
     // when
     const race = new Race();
