f0ad2b9b747153cbc1f9974aba160ccc874dbff6
DWAN-00 <kkw1700@gmail.com>
2023-10-30 15:06:33 +0900

    fix: 함수 목적에 따른 모듈 이름 변경

---
+++ src/Validate.js
@@ -0,0 +1,30 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { INPUT_MESSAGE, OUTPUT_MESSAGE, ERROR_MESSAGE } from "./Message.js";
+
+//자동차 이름을 입력받는 함수
+const isUnderTwo = (carNames) => {
+  return carNames.length < 2;
+};
+
+const isLogngName = (carNames) => {
+  return carNames.some((carName) => carName.length > 5);
+};
+
+const isDuplicate = (carNames) => {};
+
+export const validateCarNames = (carNames) => {
+  if (isUnderTwo(carNames)) {
+    throw new Error(ERROR_MESSAGE.DISABLE_RACE);
+  }
+  if (isLogngName(carNames)) {
+    throw new Error(ERROR_MESSAGE.LONG_CARNAME);
+  }
+  return carNames;
+};
+
+export const validateRound = (round) => {
+  if (round < 1) {
+    throw new Error(ERROR_MESSAGE.INVALID_ROUND);
+  }
+  return round;
+};
