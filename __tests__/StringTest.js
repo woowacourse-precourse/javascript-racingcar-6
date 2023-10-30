e953b72d2fc29fd30f53a3cd6e7159b95cea5fc9
CreateGreen <yg.jiseok@gmail.com>
2023-10-31 01:08:56 +0900

    feat : 자동차 이름 저장 기능 추가
    - 스트링 파라미터를 쉼표 기준으로 나누어 맵 객체에 저장
    - 클래스 생성시 선언한 맵 객체 변경을 위한 setter 추가

--- __tests__/StringTest.js
+++ __tests__/StringTest.js
@@ -1,30 +1,53 @@
-describe("문자열 테스트", () => {
-  test("split 메서드로 주어진 값을 구분", () => {
-    const input = "1,2";
-    const result = input.split(",");
+import App from '../src/App';
 
-    expect(result).toContain("2", "1");
-    expect(result).toContainEqual("1", "2");
+describe('문자열 테스트', () => {
+  test('split 메서드로 주어진 값을 구분', () => {
+    const input = '1,2';
+    const result = input.split(',');
+
+    expect(result).toContain('2', '1');
+    expect(result).toContainEqual('1', '2');
   });
 
-  test("split 메서드로 구분자가 포함되지 않은 경우 값을 그대로 반환", () => {
-    const input = "1";
-    const result = input.split(",");
+  test('split 메서드로 구분자가 포함되지 않은 경우 값을 그대로 반환', () => {
+    const input = '1';
+    const result = input.split(',');
 
-    expect(result).toContain("1");
+    expect(result).toContain('1');
   });
 
-  test("substring 메서드로 특정 구간 값을 반환", () => {
-    const input = "(1,2)";
+  test('substring 메서드로 특정 구간 값을 반환', () => {
+    const input = '(1,2)';
     const result = input.substring(1, 4);
 
-    expect(result).toEqual("1,2");
+    expect(result).toEqual('1,2');
   });
 
-  test("at 메서드로 특정 위치의 문자 찾기", () => {
-    const input = "abc";
-    const result = input.at(0)
+  test('at 메서드로 특정 위치의 문자 찾기', () => {
+    const input = 'abc';
+    const result = input.at(0);
 
-    expect(result).toEqual("a");
+    expect(result).toEqual('a');
+  });
+});
+
+describe('자동차 이름 입력', () => {
+  test('split 메서드로 이름 구분', () => {
+    const input = 'apple,ban,cake';
+    const result = input.split(',');
+
+    expect(result).toContainEqual('apple', 'ban', 'cake');
+    expect(result).toHaveLength(3);
+  });
+
+  test('자동차 이름을 받았을 때 저장', () => {
+    const input = ['apple', 'ban', 'cake'];
+    const object = new Map();
+    input.forEach((name) => object.set(name, 0));
+
+    input.forEach((name) => {
+      expect(object.has(name)).toBeTruthy();
+      expect(object.get(name)).toBe(0);
+    });
   });
 });
--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Console, Random } from '@woowacourse/mission-utils';
 class App {
-  async play() {}
+  async play() {
+    Console.print('Hello');
+  }
+
+  constructor() {
+    this.CAR_LISTMAP = new Map();
+  }
+
+  setCarList(name, distance) {
+    this.CAR_LISTMAP.set(name, distance);
+  }
+
+  inputCarName(carlist) {
+    const CAR_LIST = carlist.split(',');
+    for (let CAR_NAME of CAR_LIST) {
+      setCarList(CAR_NAME, 0);
+    }
+  }
 }
 
 export default App;
