625b22f5dbbab6182bad5fd59f8e556db3919381
seok-pyo <hongseokpyou@gmail.com>
2023-10-30 14:24:37 +0900

    feat: 기능 테스트 작성, story3.유효성 검증 로직

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -3,12 +3,12 @@ import validate from '../src/game/validation.js';
 describe('유효성 검사 테스트', () => {
   describe('이름 입력 테스트 확인', () => {
     test('입력으로 5자리 이상으로 들어오면 [ERROR]을 던진다', () => {
-      const nameArray = ['좋아합니다', '우테코,감사합니다.'];
+      const nameArray = ['TO', '우테코.. .. ..'];
       expect(() => validate.carName(nameArray)).toThrowError('[ERROR] 입력은 최대 5자입니다.');
     });
 
     test('입력으로 1자리 이하일 경우 [ERROR]을 던진다', () => {
-      const nameArray = ['TO', '우테코', ''];
+      const nameArray = ['좋아합니다', '이번엔 진심이라구요', ''];
       expect(() => validate.carName(nameArray)).toThrowError('[ERROR] 한자리 이상의 이름을 입력해주세요.');
     });
 
@@ -33,7 +33,11 @@ describe('유효성 검사 테스트', () => {
       expect(() => validate.lapLength('0')).toThrowError('[ERROR] 1-9 사이에 숫자를 입력해주세요.');
     });
 
-    test('입력으로 3자리 수가 들어오면 [ERROR]를 던진다.', () => {
+    test('입력으로 음수가 들어오면 [ERROR]를 던진다.', () => {
+      expect(() => validate.lapLength('-7')).toThrowError('[ERROR] 1-9 사이에 숫자를 입력해주세요.');
+    });
+
+    test('입력으로 3자리 수 이상이 들어오면 [ERROR]를 던진다.', () => {
       expect(() => validate.lapLength('111')).toThrowError('[ERROR] 최대 2자리 수까지 입력이 가능합니다.');
     });
 
