4fc6987b6b679c855f09b90c54c374c08fbf795e
pjc0179 <pjc0179@gmail.com>
2023-10-30 19:34:29 +0900

    refactor(utils):createRandomNumber.js 리펙토링
    - 랜덤으로 생성할 최소값과 최대값을 0 9 로 지정했던 부분을 constants/config.js 부분으로 이관하여 관리하게함

--- src/utils/createRandomNumber.js
+++ src/utils/createRandomNumber.js
@@ -1,5 +1,7 @@
 import { Random } from '@woowacourse/mission-utils';
+import CONFIG from '../constants/config';
 
-const createRandomNumber = () => Random.pickNumberInRange(0, 9);
+const createRandomNumber = () =>
+  Random.pickNumberInRange(CONFIG.MIN_RANDOM_COUNT, CONFIG.MAX_RANDOM_COUNT);
 
 export default createRandomNumber;
