7ac20dceb6740d98da00e72bef92c459bced8b1c
fecapark <fecapark@gmail.com>
2023-10-30 19:12:36 +0900

    feat: 이름 검증 함수 구현 완료

--- src/utils/validity.js
+++ src/utils/validity.js
@@ -1,13 +1,42 @@
 //@ts-check
 
+import { ALPHABET, NAME_LENGTH_LIMIT, NUMBERS } from "./const";
+import { InvalidPlayerNameError } from "./error";
+
 /**
  *
  * @param {string} name
  */
-export function assertNameValid(name) {}
+export function assertNameValid(name) {
+  name = name.trim();
+
+  if (name.length > NAME_LENGTH_LIMIT)
+    throw new InvalidPlayerNameError(InvalidPlayerNameError.TYPE_LENGTH);
+  if (name.includes(" "))
+    throw new InvalidPlayerNameError(InvalidPlayerNameError.TYPE_SPACE_BETWEEN);
+  if (includesSpecialIn(name))
+    throw new InvalidPlayerNameError(
+      InvalidPlayerNameError.TYPE_CONTAINS_SPECIAL
+    );
+}
 
 /**
  *
  * @param {number} amount
  */
 export function assertTryAmountValid(amount) {}
+
+/**
+ *
+ * @param {string} str
+ * @returns {boolean}
+ */
+function includesSpecialIn(str) {
+  for (const char of str) {
+    const isAlphabet = ALPHABET.includes(char);
+    const isNumber = NUMBERS.includes(char);
+    if (!isAlphabet && !isNumber) return true;
+  }
+
+  return false;
+}
