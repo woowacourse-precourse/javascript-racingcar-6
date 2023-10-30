ad8511693a51d1d8bafd7efea02ba2e3514d6b5d
swarvy <swarvy0826@naver.com>
2023-10-30 21:43:31 +0900

    refactor : RacingCarGrid에서 직접 RacingCar의 정보를 관리하도록 개선

--- src/model/RacingCarGrid.js
+++ src/model/RacingCarGrid.js
@@ -1,21 +1,20 @@
-import RacingCar from './RacingCar.js';
 import RandomNumberGenerator from './RandomNumberGenerator.js';
 import { SYMBOLS } from '../constants/Symbols.js';
-import { GRANDPRIX_MAGICNUMBER } from '../constants/GrandPrixOption.js';
+import { GRANDPRIX_MOVE_OPTION } from '../constants/GrandPrixOption.js';
 
 export default class RacingCarGrid {
   /**
    * @private
-   * @type {RacingCar[]}
+   * @type {{ name: string, status: number }[]}
    */
-  #racingCarList;
+  #racingCarList = [];
 
   /**
    * @constructor
-   * @param names InputView로부터 입력받은 레이싱카 이름
+   * @param {string} names InputView로부터 입력받은 레이싱카 이름
    */
   constructor(names) {
-    this.#racingCarList = Array.from(names.split(SYMBOLS.comma), (name) => new RacingCar(name));
+    names.split(SYMBOLS.comma).forEach((name) => this.#racingCarList.push({ name, status: 0 }));
   }
 
   /**
@@ -24,8 +23,8 @@ export default class RacingCarGrid {
   setRacingGrid() {
     const randomMovementResults = RandomNumberGenerator.generate(this.#racingCarList.length);
     randomMovementResults.forEach((value, idx) => {
-      if (value >= GRANDPRIX_MAGICNUMBER.move) {
-        this.#racingCarList[idx].moveRacingCar();
+      if (value >= GRANDPRIX_MOVE_OPTION.move) {
+        this.#racingCarList[idx].status += 1;
       }
     });
   }
@@ -35,7 +34,7 @@ export default class RacingCarGrid {
    * @returns {{name: string, status: number}[]} 레이싱카들의 현재 상태 반환
    */
   getRacingGrid() {
-    return Array.from(this.#racingCarList, (racingCar) => racingCar.getRacingCarStatus());
+    return this.#racingCarList;
   }
 
   /**
@@ -43,11 +42,10 @@ export default class RacingCarGrid {
    * @returns {string} 가장 멀리 간 레이싱카를 반환(여러개 일 시 쉼표로 구분하여 반환)
    */
   getPodium() {
-    const racingGrid = this.getRacingGrid();
-    const raceStatusList = racingGrid.map(({ status }) => status);
+    const raceStatusList = this.#racingCarList.map(({ status }) => status);
     const max = Math.max(...Object.values(raceStatusList));
 
-    return racingGrid
+    return this.#racingCarList
       .filter(({ status }) => status === max)
       .map(({ name }) => name)
       .join(`${SYMBOLS.comma}${SYMBOLS.space}`);
