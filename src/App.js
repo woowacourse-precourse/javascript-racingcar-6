a86146c156056b0d0ded5356c7c2eeaa49d4a74e
coddingyun <igy2840@gmail.com>
2023-10-30 11:28:07 +0900

    refactoring: play 내부의 함수를 play 외부로 이동

--- src/App.js
+++ src/App.js
@@ -4,73 +4,81 @@ import { randomNumGenerator } from "./utils/RandomNumGenerator";
 import { messagePrinter } from "./utils/messagePrinter";
 class App {
   async play() {
-    const getCarNameInput = async() => {
-      const carName = await messagePrinter.inputPrint(GAME_MESSAGE.get_car_name)
-      return carName.split(',')
-    };
-    const checkValidCarName = async(carNameArr) => {
-      for (let i=0; i<carNameArr.length; i++) {
-        if (carNameArr[i].length > 5 || carNameArr[i].length ===0) {
-          messagePrinter.errorPrint(ERROR_MESSAGE.more_than_five_letters)
-        }
-      }
-    }
-    const getTrialNumInput = async() => {
-      const trialNum = await messagePrinter.inputPrint(GAME_MESSAGE.get_trial_num)
-      return trialNum
-    }
-    const checkValidTrialNum = async(trialNum) => {
-      if (isNaN(trialNum)) {
-        messagePrinter.errorPrint(ERROR_MESSAGE.not_number)
-      }
-    }
-    const raceStart = async(carNameArr) => {
-      const carNum = carNameArr.length
-      let moveForwardArr = new Array(carNum).fill(0);
-      for (let i=0; i<carNum; i++) {
-        const randomNum = randomNumGenerator();
-        if (randomNum >= 4) {
-          moveForwardArr[i] = 1
-        }
-      }
-      return moveForwardArr
-    }
-    const printCurCarMove = async(carNameArr, curMoveForwardArr) => {
-      for (let i=0; i<carNameArr.length; i++){
-        const carName = carNameArr[i]
-        const curMoveForward = curMoveForwardArr[i]
-        await messagePrinter.outputPrint(GAME_MESSAGE.print_move_status(carName, curMoveForward))
-      }
-      await messagePrinter.outputPrint(GAME_MESSAGE.line_break)
-    }
-    const printWinners = async(carNameArr, curMoveForwardArr) => {
-      const maxMove = Math.max(...curMoveForwardArr);
-
-      let winners = [];
-
-      for (let i=0; i<carNameArr.length; i++) {
-        if (maxMove === curMoveForwardArr[i]) {
-          winners.push(carNameArr[i])
-        }
-      }
-
-      await messagePrinter.outputPrint(GAME_MESSAGE.print_winners(winners))
-    }
-    const carNameArr = await getCarNameInput();
-    await checkValidCarName(carNameArr)
-    const trialNum = await getTrialNumInput();
-    await checkValidTrialNum(trialNum)
+    
+    const carNameArr = await this.getCarNameInput();
+    await this.checkValidCarName(carNameArr);
+    const trialNum = await this.getTrialNumInput();
+    await this.checkValidTrialNum(trialNum);
 
     let curMoveForwardArr = new Array(carNameArr.length).fill(0);
-    let i = 0
+    let i = 0;
     while(i< trialNum) {
-      const moveForwardArr = await raceStart(carNameArr)
+      const moveForwardArr = await this.raceStart(carNameArr);
       curMoveForwardArr = curMoveForwardArr.map((value, index) => value + moveForwardArr[index]);
-      await printCurCarMove(carNameArr, curMoveForwardArr)
-      i += 1
+      await this.printCurCarMove(carNameArr, curMoveForwardArr);
+      i += 1;
     }
 
-    await printWinners(carNameArr, curMoveForwardArr)
+    await this.printWinners(carNameArr, curMoveForwardArr);
+  }
+
+  async getCarNameInput () {
+    const carName = await messagePrinter.inputPrint(GAME_MESSAGE.get_car_name);
+    return carName.split(',');
+  }
+
+  async checkValidCarName (carNameArr) {
+    for (let i=0; i<carNameArr.length; i++) {
+      if (carNameArr[i].length > 5 || carNameArr[i].length ===0) {
+        messagePrinter.errorPrint(ERROR_MESSAGE.more_than_five_letters);
+      }
+    }
+  }
+
+  async getTrialNumInput () {
+    const trialNum = await messagePrinter.inputPrint(GAME_MESSAGE.get_trial_num);
+    return trialNum;
+  }
+  
+  async checkValidTrialNum (trialNum) {
+    if (isNaN(trialNum)) {
+      messagePrinter.errorPrint(ERROR_MESSAGE.not_number);
+    }
+  }
+
+  async raceStart (carNameArr) {
+    const carNum = carNameArr.length
+    let moveForwardArr = new Array(carNum).fill(0);
+    for (let i=0; i<carNum; i++) {
+      const randomNum = randomNumGenerator();
+      if (randomNum >= 4) {
+        moveForwardArr[i] = 1;
+      }
+    }
+    return moveForwardArr
+  }
+
+  async printCurCarMove (carNameArr, curMoveForwardArr) {
+    for (let i=0; i<carNameArr.length; i++){
+      const carName = carNameArr[i];
+      const curMoveForward = curMoveForwardArr[i];
+      await messagePrinter.outputPrint(GAME_MESSAGE.print_move_status(carName, curMoveForward));
+    }
+    await messagePrinter.outputPrint(GAME_MESSAGE.line_break);
+  }
+
+  async printWinners (carNameArr, curMoveForwardArr) {
+    const maxMove = Math.max(...curMoveForwardArr);
+
+    let winners = [];
+
+    for (let i=0; i<carNameArr.length; i++) {
+      if (maxMove === curMoveForwardArr[i]) {
+        winners.push(carNameArr[i]);
+      }
+    }
+
+    await messagePrinter.outputPrint(GAME_MESSAGE.print_winners(winners));
   }
 }
 
