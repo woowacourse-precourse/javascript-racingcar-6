d56c5a62c553a9b2287d0c7e4c377ad34f2a1912
Ohreum <105328855+Ohreum@users.noreply.github.com>
2023-10-30 20:54:40 +0900

    feat: 자동차 이름 입력 메소드 getCarName() 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  constructor() {
+    this.carNameArr = [];
+  }
+
+  async inputCarName() {
+    const carName = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    this.carNameArr = carName.split(',');
+
+    const nameSet = new Set();
+
+    this.carNameArr.forEach(name => {
+      if (name.length >= 5) {
+        throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
+      }
+      if (nameSet.has(name)) {
+        throw new Error('[ERROR] 중복된 이름이 있습니다.');
+      }
+      nameSet.add(name);
+    });
+
+    return this.carNameArr;
+  }
+
+  async play() {
+    this.inputCarName();
+  }
 }
 
 export default App;
