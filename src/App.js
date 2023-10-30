e72ea6176299a6deda2493f2c21d253f91075b9a
leehyeyeong <elto789@naver.com>
2023-10-30 23:02:57 +0900

    feat: 경기 반복 횟수 구현 - 주어진 예시처럼 3대가 5번 시도한다면 총 15번 - 여기에 4 이상이면 '-'를 하나씩 늘려서 출력하는 코드를 추가 작성할 예정

--- src/App.js
+++ src/App.js
@@ -2,17 +2,37 @@ import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    start();
+    gameStart();
 
   }
 }
 
-async function start() {
+async function gameStart() {
   Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
   const inputCarName = await Console.readLineAsync("");
   const carName = inputCarName.split(',');
+  Console.print(carName.length);
   Console.print("시도할 횟수는 몇 회인가요?");
   const inputNumber = await Console.readLineAsync("");
+  gamePlay(inputNumber, carName);
+}
+
+function gamePlay(inputNumber, carName) {
+  Console.print("실행 결과");
+  let step;
+  for (step = 0; step < inputNumber; step++) {
+    randomNumber(carName)
+  }
+
+}
+
+function randomNumber(carName) {
+  let step;
+  for (step = 0; step < carName.length; step++) {
+    const pickNumber = Random.pickNumberInRange(0, 9);
+    Console.print(pickNumber);
+  }
+
 }
 
 
