07e8c17ac6b08e3cf96e055ee9398e5f660e325a
hs <tlsgustn93@naver.com>
2023-10-30 19:14:48 +0900

    feat: 경주할 이름을 입력받는 첫 시작

--- src/modules/GameMain.js
+++ src/modules/GameMain.js
@@ -7,7 +7,6 @@ class GameMain {
     const userInput = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
-
     this.participant = new Participant(userInput);
     this.Attempt = new Attempt();
 
