25fe71ff8e171fe2bb017509e8f58fee0043a92f
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-10-31 01:52:13 +0900

    test: 사용자에게 경주할 자동차의 이름을 입력받는다

--- __tests__/View/ViewTest.js
+++ __tests__/View/ViewTest.js
@@ -1,6 +1,7 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import View from '../../src/View/View';
 import ERROR from '../../src/constants/error';
+import InputView from '../../src/View/InputView';
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -12,18 +13,61 @@ const mockQuestions = (inputs) => {
 };
 
 describe('View 테스트', () => {
+  let view;
+
+  beforeEach(() => {
+    view = new View();
+  });
+
   describe('사용자로부터 입력값을 받는다.', () => {
-    test('비어있는 문자열에 대해 예외 처리를 진행한다.', async () => {
+    test('InputView는 비어있는 문자열에 대해 예외 처리를 진행한다.', async () => {
       // given
       const emptyString = '';
-      const validator = jest.fn();
-      const view = new View();
 
       mockQuestions([emptyString]);
 
       // then
-      await expect(view.readUserInput(emptyString, validator)).rejects.toThrow(
+      await expect(InputView.readLineAsync(emptyString)).rejects.toThrow(
         `[ERROR] ${ERROR.message.emptyString}`,
+      );
+    });
+  });
+
+  describe('사용자로부터 자동차 이름을 입력받는다.', () => {
+    test('비어있는 문자열에 대해 예외 처리를 진행한다.', async () => {
+      // given
+      const emptyString = '';
+
+      mockQuestions([emptyString]);
+
+      // then
+      await expect(view.readCarName()).rejects.toThrow(
+        `[ERROR] ${ERROR.message.emptyString}`,
+      );
+    });
+
+    test('자동차 이름을 입력받는다.', async () => {
+      // given
+      const validCarNames = 'pobi,crong,honux';
+
+      mockQuestions([validCarNames]);
+
+      // when
+      const result = await view.readCarName();
+
+      // then
+      expect(result).toEqual(validCarNames.split(','));
+    });
+
+    test('자동차 이름이 5자를 초과하는 경우 예외 처리를 진행한다.', async () => {
+      // given
+      const invalidCarNames = 'pobipobi,crongcrong,honuxhonux';
+
+      mockQuestions([invalidCarNames]);
+
+      // then
+      await expect(view.readCarName()).rejects.toThrow(
+        `[ERROR] ${ERROR.message.invalidCarNameLength}`,
       );
     });
   });
