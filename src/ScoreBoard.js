01ecf6e0beddda8649544c180b88be86572e896b
uraflower <82873315+uraflower@users.noreply.github.com>
2023-10-30 19:49:47 +0900

    feat: 최종 우승자 출력 기능 추가

--- src/ScoreBoard.js
+++ src/ScoreBoard.js
@@ -1,10 +1,19 @@
 import { Console } from '@woowacourse/mission-utils';
 
+function getNameOfWinners(winners) {
+  return winners.map((car) => car.name);
+}
+
 class ScoreBoard {
   static announceInterimResult(cars) {
     cars.forEach((car) => {
       Console.print(`${car.name} : ${car.movement}`);
     });
+  }
+
+  static announceWinners(winners) {
+    const winnersNameList = getNameOfWinners(winners);
+    Console.print(`최종 우승자 : ${winnersNameList.join(', ')}`);
   }
 }
 
