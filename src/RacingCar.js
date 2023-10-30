c8a643227eccab0fa25094ec28815dd5cb21ae95
JUNU <bokjunwoo@gmail.com>
2023-10-30 20:19:58 +0900

    refactor: private 필드 및 getPosition 메서드 추가
    RacingCar 클래스에서 name, position 속성을 private 필드로 변경 및 getName, getPosition 메서드 추가로 RacingCar 클래스의 내부 상태를 보호

--- src/RacingCar.js
+++ src/RacingCar.js
@@ -2,16 +2,27 @@ import { NUMBER_RANGE, RANDOM_THRESHOLD } from './constants.js';
 import { generateRandomNumber } from './utils.js';
 
 class RacingCar {
+  #name;
+  #position;
+
   constructor(name) {
-    this.name = name;
-    this.position = 0;
+    this.#name = name;
+    this.#position = 0;
   }
 
   move() {
     const random = generateRandomNumber(NUMBER_RANGE.MIN, NUMBER_RANGE.MAX);
     if (random >= RANDOM_THRESHOLD) {
-      this.position++;
+      this.#position++;
     }
+  }
+
+  getName() {
+    return this.#name;
+  }
+
+  getPosition() {
+    return this.#position;
   }
 }
 
