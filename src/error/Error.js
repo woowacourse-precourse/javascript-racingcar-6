3cce0c5e5ddd685c60f71d8146419c5dcc08116b
ChanYong Park <krap5075@naver.com>
2023-10-31 04:07:12 +0900

    feat: 자동차 이름 예외처리 기능 구현

---
+++ src/error/Error.js
@@ -0,0 +1,24 @@
+class Error {
+  checkCarName(input) {
+    this.checkCarNameOver(input);
+    this.checkCarNameWrong(input);
+  }
+  checkCarNameOver(input) {
+    const name_arr = input.split(",");
+    const exist_overlength = name_arr.some((name) => name.length > 5);
+    if (exist_overlength) {
+      throw new Error("[ERROR] 이름이 5글자 초과입니다");
+    }
+  }
+  checkCarNameWrong(input) {
+    const exist_wrong_name = filtered_name_arr.some((name) =>
+      /^[a-zA-Z]+$/.test(name)
+    );
+    if (exist_wrong_name) {
+      throw new Error("[ERROR] 이름이 잘못된 형식입니다.");
+    }
+  }
+  checkTryNumber(input) {}
+}
+
+export default Error;
