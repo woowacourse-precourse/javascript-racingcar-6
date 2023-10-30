eb49c84203dd427199016fdc32063c02f224ab31
JUNU <bokjunwoo@gmail.com>
2023-10-31 00:22:15 +0900

    feat: 유효성 검사 코드 상세한 업데이트
    테스트 코드를 작성 후 발생한 오류에 대한 업데이트(자동차 이름 및 시도 횟수 검사 조건 추가)

--- src/utils.js
+++ src/utils.js
@@ -22,6 +22,10 @@ export const validateCarNames = (carNameArray) => {
   }
 
   for (const name of carNameArray) {
+    if (name.length === 0) {
+      throw new Error('[ERROR] 자동차 이름은 빈 문자열일 수 없습니다.');
+    }
+
     if (name.length > 5) {
       throw new Error('[ERROR] 자동차 이름은 5자 이하이어야 합니다.');
     }
@@ -31,12 +35,24 @@ export const validateCarNames = (carNameArray) => {
 };
 
 export const validateNumberInput = (input) => {
-  if (input === '0') {
-    throw new Error('[ERROR] 시도할 횟수는 0 이상이여야 합니다.');
+  if (isNaN(input)) {
+    throw new Error('[ERROR] 시도할 횟수는 숫자여야 합니다.');
   }
 
-  if (isNaN(input)) {
-    throw new Error('[ERROR] 입력값은 숫자여야 합니다.');
+  if (parseInt(input) === 0) {
+    throw new Error('[ERROR] 시도 횟수는 0일 수 없습니다.');
+  }
+
+  if (parseInt(input) < 0) {
+    throw new Error('[ERROR] 시도 횟수는 음수일 수 없습니다.');
+  }
+
+  if (!Number.isInteger(Number(input))) {
+    throw new Error('[ERROR] 시도 횟수는 정수여야 합니다.');
+  }
+
+  if (input === '') {
+    throw new Error('[ERROR] 시도 횟수는 빈 문자열일 수 없습니다.');
   }
 
   return true;
