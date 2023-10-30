00254bf0abb675e514238a65eeb294d66153294e
최기환 <rlghks3004@gmail.com>
2023-10-30 10:55:45 +0900

    refactor(App.js): 프로퍼티 private 으로 변경 및 함수 이름 변경
    private 프로퍼티 선언 및 함수 이름 변경
     - check 로 시작하는 함수들 insert로 시작하도록 이름 변경
     - private 를 통해 프로퍼티 캡슐레이팅

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import {
   generateRandomNumber,
   isUserInputValid,
   splitCarsInput,
-} from './utils.js';
+} from './utils';
 
 class App {
-  cars = new Map();
-  tryNumber = 0;
-  winner = [];
-  topScore = 0;
+  #cars = new Map();
+  #tryNumber = 0;
+  #winner = [];
+  #topScore = 0;
 
   async play() {
     await this.getCarName();
     await this.getTryNumber();
     this.startRacing();
-    this.checkWinner();
+    this.insertWinner();
     this.printWinner();
   }
 
@@ -30,65 +30,65 @@ class App {
     isUserInputValid(carsInput);
     const cars = splitCarsInput(carsInput);
     cars.forEach((car) => {
-      this.cars.set(car, 0);
+      this.#cars.set(car, 0);
     });
   }
 
   async getTryNumber() {
     Console.print('시도할 횟수는 몇 회인가요?');
     const input = await Console.readLineAsync('');
-    this.initializeTryNumber(input);
+    this.initTryNumber(input);
   }
 
-  initializeTryNumber(numberInput) {
+  initTryNumber(numberInput) {
     checkIsValidNumber(numberInput);
-    this.tryNumber = Number(numberInput);
+    this.#tryNumber = Number(numberInput);
   }
 
   startRacing() {
     Console.print('');
     Console.print('실행 결과');
-    for (let i = 0; i < this.tryNumber; i++) {
+    for (let i = 0; i < this.#tryNumber; i += 1) {
       this.moveRandomDistance();
       Console.print('');
     }
   }
 
   moveRandomDistance() {
-    this.cars.forEach((distance, car) => {
+    this.#cars.forEach((distance, car) => {
       const randomNumber = generateRandomNumber();
-      this.cars.set(car, distance + randomNumber);
+      this.#cars.set(car, distance + randomNumber);
       this.printMoveResult(car);
     });
   }
 
   printMoveResult(car) {
-    const distance = this.cars.get(car);
+    const distance = this.#cars.get(car);
     const result = '-'.repeat(distance);
     Console.print(`${car} : ${result}`);
   }
 
-  checkWinner() {
-    this.checkTopScore();
+  insertWinner() {
+    this.initTopScore();
     this.insertWinner();
   }
 
-  checkTopScore() {
-    this.cars.forEach((value) => {
-      this.topScore = Math.max(this.topScore, value);
+  initTopScore() {
+    this.#cars.forEach((value) => {
+      this.#topScore = Math.max(this.#topScore, value);
     });
   }
 
   insertWinner() {
-    this.cars.forEach((value, key) => {
-      if (value === this.topScore) {
-        this.winner.push(key);
+    this.#cars.forEach((value, key) => {
+      if (value === this.#topScore) {
+        this.#winner.push(key);
       }
     });
   }
 
   printWinner() {
-    const winnerString = this.winner.join(', ');
+    const winnerString = this.#winner.join(', ');
     Console.print(`최종 우승자 : ${winnerString}`);
   }
 }
