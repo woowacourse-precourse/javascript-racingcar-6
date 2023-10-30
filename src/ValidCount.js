6290b33dbcc5778f3864e0bc68c56138a9e0a21a
silver0108 <kk1170@naver.com>
2023-10-30 22:57:10 +0900

    refactor: 시도 횟수 유효 판별 조건 변경

--- src/ValidCount.js
+++ src/ValidCount.js
@@ -1,6 +1,6 @@
 export function validCount(count){
   const numCount = Number(count);
-  if(!(/^\d+$/.test(numCount)) || numCount < 0){
+  if(!(/^\d+$/.test(numCount)) || numCount <= 0){
     throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
   }
   return true;
