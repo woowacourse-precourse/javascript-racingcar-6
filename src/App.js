ed75d118720626d0e907836a01f0dc53a1e22a55
Jiwon Lee <giwon.yi339@gmail.com>
2023-10-30 22:32:34 +0900

    feat : racing car 점수 출력

--- src/App.js
+++ src/App.js
@@ -2,31 +2,54 @@ import { Random, Console } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    this.gameStart();
-  }
-
-  async gameStart() {
     await this.displayResults();
   }
 
-  async inputCarNames() {
-    const carNamesInput = await Console.readLineAsync(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
-    );
-    carNamesInput
-      .split(",")
-      .map((carName) => carName.length > 5 || carName.length <= 0)
-      ? Console.print(carNamesInput)
-      : Console.print("[ERROR]");
-  }
+  // async inputCarNames() {
+  //   const carNamesInput = await Console.readLineAsync(
+  //     "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+  //   );
 
-  async inputRotations() {
-    await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
-  }
+  //   carNamesInput
+  //     .trim()
+  //     .split(",")
+  //     .map((carName) => {
+  //       if (carName.length > 5 || carName.length <= 0) {
+  //         throw new Error("[ERROR]");
+  //       }
+  //     });
+  // }
 
   async displayResults() {
-    const RACECAR = await this.inputCarNames();
-    const SCORES = await this.inputRotations();
+    // const RACECAR = await this.inputCarNames();
+    const carName = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+    const CARS = carName.trim().split(",");
+
+    CARS.map((el) => {
+      if (el.length > 5 || el.length <= 0) {
+        throw new Error("[ERROR]");
+      }
+    });
+    const ROTATION = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+
+    Console.print("실행 결과");
+    for (let i = 0; i < ROTATION; i++) {
+      CARS.map((car) => {
+        const score = Random.pickNumberInRange(0, 9);
+        //
+        if (score <= 4) {
+          // return Console.print(`${el} : ${result}`);
+          return Console.print(`${car} : -`);
+        } else if (score > 4) {
+          Console.print(`${car} : ${score}`);
+          // totalScore[el] = (totalScore[el] || 0) + result;
+        } else {
+          throw new Error("[ERROR]");
+        }
+      });
+    }
   }
 }
 
