4899e2773eef59689f26c7d8c8fbda5a24be0ceb
coolIlive <galilee155@naver.com>
2023-10-30 20:42:42 +0900

    style: 테스트 코드를 airbnb 스타일 가이드에 따라 수정

--- __tests__/CarMovingTest.js
+++ __tests__/CarMovingTest.js
@@ -1,9 +1,9 @@
-import CarMoving from "../src/model/CarMoving.js";
+import CarMoving from '../src/model/CarMoving.js';
 
-describe("자동차 랜덤 이동값 테스트", () => {
+describe('자동차 랜덤 이동값 테스트', () => {
   const carMoving = new CarMoving();
 
-  test("자동차 랜덤값이 4 미만일 경우 전진하지 않음", () => {
+  test('자동차 랜덤값이 4 미만일 경우 전진하지 않음', () => {
     const failRandomValue = [-1, 0, 1, 2, 3];
 
     failRandomValue.forEach((number) => {
@@ -11,7 +11,7 @@ describe("자동차 랜덤 이동값 테스트", () => {
     });
   });
 
-  test("자동차 랜덤값이 4 이상일 때만 전진", () => {
+  test('자동차 랜덤값이 4 이상일 때만 전진', () => {
     const succesRandomValue = [4, 5, 6, 7, 8, 9];
 
     succesRandomValue.forEach((number) => {
@@ -19,14 +19,14 @@ describe("자동차 랜덤 이동값 테스트", () => {
     });
   });
 
-  test("각 레이싱 별 우승자 계산(한 명일 경우)", () => {
+  test('각 레이싱 별 우승자 계산(한 명일 경우)', () => {
     const singleWinner = [0, 8, 9];
     const winnerIndex = [2];
 
     expect(() => carMoving.countWinnerIndex(singleWinner).toEqual(winnerIndex));
   });
 
-  test("각 레이싱 별 우승자 계산(여러명인 경우)", () => {
+  test('각 레이싱 별 우승자 계산(여러명인 경우)', () => {
     const doubleWinndr = [0, 5, 5];
     const winnerIndex = [1, 2];
 
--- __tests__/ValidateTest.js
+++ __tests__/ValidateTest.js
@@ -1,52 +1,52 @@
-import CarListValidate from "../src/validate/CarListCheck.js";
-import TryNumberValidate from "../src/validate/TryNumberCheck.js";
-import { ERROR } from "../src/util/constants.js";
+import CarListValidate from '../src/validate/CarListCheck.js';
+import TryNumberValidate from '../src/validate/TryNumberCheck.js';
+import { ERROR } from '../src/util/constants.js';
 
-describe("자동차 이름 입력 테스트", () => {
+describe('자동차 이름 입력 테스트', () => {
   const carValidate = new CarListValidate();
 
-  test("split 메서드로 주어진 값을 구분", () => {
-    const input = "1,2,abc";
-    const result = ["1", "2", "abc"];
+  test('split 메서드로 주어진 값을 구분', () => {
+    const input = '1,2,abc';
+    const result = ['1', '2', 'abc'];
 
     expect(() => carValidate.validate(input).toContainEqual(result));
   });
 
-  test("이름을 하나도 입력하지 않은 경우", () => {
-    const input = [""];
+  test('이름을 하나도 입력하지 않은 경우', () => {
+    const input = [''];
     const result = ERROR.noInputCarList;
 
     expect(() => carValidate.checkNoInput(input).toThrow(result));
   });
 
-  test("중복되는 이름을 입력한 경우", () => {
-    const input = ["1", "1", "2"];
+  test('중복되는 이름을 입력한 경우', () => {
+    const input = ['1', '1', '2'];
     const result = ERROR.carCount;
 
     expect(() => carValidate.checkSameName(input).toThrow(result));
   });
 
-  test("5글자를 초과하는 이름을 입력한 경우", () => {
-    const notErrorInput = ["12345", "1"];
-    const errorInput = ["123456", "1"];
+  test('5글자를 초과하는 이름을 입력한 경우', () => {
+    const notErrorInput = ['12345', '1'];
+    const errorInput = ['123456', '1'];
     const result = ERROR.carNameLength;
 
     expect(() => carValidate.checkNameLength(notErrorInput).not.toThrow());
     expect(() => carValidate.checkNameLength(errorInput).toThrow(result));
   });
 
-  test("영문, 한글, 특수문자 입력 가능", () => {
-    const notErrorInput = [" ", "!", "가나다", "a"];
+  test('영문, 한글, 특수문자 입력 가능', () => {
+    const notErrorInput = [' ', '!', '가나다', 'a'];
 
     expect(() => carValidate.validate(notErrorInput).not.toThrow());
   });
 });
 
-describe("레이싱 횟수 입력 테스트", () => {
+describe('레이싱 횟수 입력 테스트', () => {
   const racingNumberValidate = new TryNumberValidate();
 
-  test("0이하 입력 또는 입력값 없는 경우", () => {
-    const input = [-2, 0, ""];
+  test('0이하 입력 또는 입력값 없는 경우', () => {
+    const input = [-2, 0, ''];
     const result = ERROR.tryNumberZero;
 
     input.forEach((input) => {
@@ -54,8 +54,8 @@ describe("레이싱 횟수 입력 테스트", () => {
     });
   });
 
-  test("숫자가 아닌 형식을 입력한 경우", () => {
-    const input = ["a", "!", " ", "가"];
+  test('숫자가 아닌 형식을 입력한 경우', () => {
+    const input = ['a', '!', ' ', '가'];
     const result = ERROR.tryNumberZero;
 
     input.forEach((input) => {
