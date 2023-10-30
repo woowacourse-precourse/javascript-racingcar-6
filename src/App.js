7cac42bd5d0340e7c4c670fe4ccfbdb138aa8f4a
yesiamok3 <jaehyunok@cau.ac.kr>
2023-10-31 02:54:58 +0900

    feat: 자동차 이름 입력 기능
    자동차 이름을 , 단위로 구분하여 입력받음
    입력받은 이름을 Car 객체로 생성하여 Car 배열 리턴

--- src/App.js
+++ src/App.js
@@ -9,7 +9,21 @@ export class Car {
   }
 }
 class App {
-  async play() {}
+  async play() {
+    let cars = await setRaceCars();
+  }
+}
+
+async function setRaceCars(){
+  let carInput = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+  let carStrings = carInput.split(',');
+  let cars = [];
+  for (let i = 0; i < carStrings.length; i++){
+    let name = carStrings[i];
+    let car = new Car(name);
+    cars.push(car);
+  }
+  return cars
 }
 
 export default App;
