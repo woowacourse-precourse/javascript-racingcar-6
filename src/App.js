430647eab1dd72df43a5711f385e0e36b9006fbb
FirstBright <mipo8890@gmail.com>
2023-10-30 22:23:20 +0900

    :sparkles: feat: 게임 구현 사용자 입력만큼 시도 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import Car from "./Car.js";
 class App {
-  async play() {}
+  async play() {
+    let players = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+    let round = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+    const playerArray = players.split(",");
+    const cars = [];
+
+    for (let player of playerArray) {
+      if (player.length>5){
+        throw new Error('[ERROR] 5자리 이하의 이름만 가능합니다.');
+      }
+      const car = new Car(player);
+      cars.push(car);
+    }
+    await MissionUtils.Console.print("실행 결과")
+    for (let i =0; i<round;i++) {
+      for(let car of cars) {
+        car.run();
+      }
+    }
+
+    
+  }
 }
 
 export default App;
--- src/Car.js
+++ src/Car.js
@@ -3,15 +3,14 @@ class Car {
     constructor(name) {
         this.name = name;
         this.forward = 0;
-        if (this.name.length>5){
-            throw new Error('[ERROR] 5자리 이하의 이름만 가능합니다.');
-        }
+        
     }
     run() { 
         if (MissionUtils.Random.pickNumberInRange(0, 9)>=4){
-            forward++;
+            this.forward++;
         }       
-        return forword;
+        return this.forword;
     }
+    
 
 }
