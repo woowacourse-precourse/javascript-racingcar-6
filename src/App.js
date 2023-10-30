76d2fef63c8a2a1497ea3988fdedf3b86f48e328
00kang <kdb070970@gmail.com>
2023-10-31 02:24:29 +0900

    feat: 랜덤 값에 따른 경주 진행 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Console } from '@woowacourse/mission-utils';
+import { Console, Random } from '@woowacourse/mission-utils';
 import Input from './Input.js';
 
 class App {
@@ -6,12 +6,40 @@ class App {
     try {
       const input = new Input(); // Input 클래스의 인스턴스를 생성
       const carArr = await input.getCarName();
-      Console.print(carArr);
       const inputNumber = await input.getRepeatNumber();
-      Console.print(inputNumber);
+
+      const carDictionary = {};
+      carArr.forEach(name => {
+        carDictionary[name] = 0;
+      });
+
+      // Console.print('실행 결과');
+      for (let i = 0; i < inputNumber; i++) {
+        this.runRace(carArr, carDictionary);
+        Console.print(carDictionary);
+      }
     } catch (error) {
       console.error(error.message);
     }
+  }
+
+  generateRandomNumber() {
+    const startInclusive = 0;
+    const endInclusive = 9;
+    let randomNumber = Random.pickNumberInRange(startInclusive, endInclusive);
+
+    if (randomNumber >= 4) {
+      return true;
+    }
+    return false;
+  }
+
+  runRace(carArr, carDictionary) {
+    carArr.forEach(key => {
+      if (this.generateRandomNumber()) {
+        carDictionary[key] += 1;
+      }
+    });
   }
 }
 
