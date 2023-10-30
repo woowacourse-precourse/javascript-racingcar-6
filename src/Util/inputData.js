1e4de58ca5738b132d4d8a290ecf4c5f2e0246e6
JunSeok <ppu1234@naver.com>
2023-10-30 14:39:33 +0900

    style: 파일 이름 변경

--- src/Util/inputData.js
+++ src/Util/inputData.js
@@ -4,15 +4,15 @@ import { Console } from '@woowacourse/mission-utils';
 import { INFO_MESSAGE } from './message.js';
 
 const getPlayNumber = async () => {
-  const inputNumber = Number(await Console.readLineAsync(INFO_MESSAGE.playNumber));
+  const inputNumber = +(await Console.readLineAsync(INFO_MESSAGE.playNumber));
   checkErrorPlayNumber(inputNumber);
   return inputNumber;
 };
 
-const getCarName = async (carName) => {
+const getCarName = async () => {
   const inputNameArr = (await Console.readLineAsync(INFO_MESSAGE.carName)).split(',').map((name) => name.trim());
   checkErrorInputName(inputNameArr);
-  inputNameArr.forEach((name) => carName.set(name, ''));
+  return inputNameArr;
 };
 
 export { getPlayNumber, getCarName };
