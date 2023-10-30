02b774045f9557afc4e3057691c51ce86ab8d835
DWAN-00 <kkw1700@gmail.com>
2023-10-30 10:32:08 +0900

    feat: add 정지와 전진 여부를 결정하는 함수

--- src/Racing.js
+++ src/Racing.js
@@ -4,3 +4,23 @@ export const randomNumber = () => {
   const number = MissionUtils.Random.pickNumberInRange(0, 9);
   return number;
 };
+
+const overFour = (number) => {
+  return number >= 4;
+};
+
+const underFour = (number) => {
+  return number < 4;
+};
+
+export const checkMoving = () => {
+  let number = randomNumber();
+  let isMoving = false;
+  let isStop = false;
+  if (overFour(number)) {
+    return (isMoving = true);
+  }
+  if (underFour(number)) {
+    return (isStop = true);
+  }
+};
