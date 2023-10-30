3894f10e56d5efcdfa35c78d0d1a26b2c858dc47
jjo-mi <whdal2636@naver.com>
2023-10-30 17:03:01 +0900

    [feat] 기능 1 사용자에서 이름 받는 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    let CAR_NAME = [];
+    Console.readLine(
+      "경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)\n",
+      (answer) => {
+        // console.log(`닉네임: ${answer}`);
+        CAR_NAME = answer.split(",");
+        console.log("CAR_NAME: ", CAR_NAME);
+      }
+    );
+  }
 }
 
 export default App;
+
+const app = new App();
+app.play();
