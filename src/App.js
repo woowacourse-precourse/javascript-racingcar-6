0dd8ae47e218168c581232c5f05bde9f1e5eebb5
sojeong0302 <01duqrl@naver.com>
2023-10-30 15:40:51 +0900

    자동차 경주 과정 구현

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
-  //자동차 이름 입력받기
+  constructor() {
+    this.roundResults = [];
+    this.tryInput = 0;
+    this.carNames = [];
+  }
+
   async carName() {
     let carNameInput = await MissionUtils.Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
-    const carNames = carNameInput.split(",");
+    this.carNames = carNameInput.split(",");
   }
 
-  //시도할 횟수 입력받기
   async try() {
     let tryInput = await MissionUtils.Console.readLineAsync(
       "시도할 횟수는 몇 회인가요?\n"
     );
+    this.tryInput = parseInt(tryInput, 10);
+  }
+
+  async process() {
+    console.log("실행 결과");
+    const roundResults = new Array(this.carNames.length).fill("");
+
+    for (let i = 0; i < this.tryInput; i++) {
+      for (let j = 0; j < this.carNames.length; j++) {
+        const move = Math.floor(Math.random() * 10) >= 4 ? "-" : "";
+        roundResults[j] += move;
+      }
+
+      this.carNames.forEach((carName, j) => {
+        console.log(`${carName} : ${roundResults[j]}`);
+      });
+    }
   }
 
   async play() {
     await this.carName();
     await this.try();
+    await this.process();
   }
 }
 
