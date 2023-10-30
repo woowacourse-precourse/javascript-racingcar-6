0dce96b6d8ffcf60a21369aea73ed134cb15e587
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-10-30 22:31:01 +0900

    test: 자동차 이름에 대한 예외 처리

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,5 +1,5 @@
-import App from "../src/App.js";
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { MissionUtils } from '@woowacourse/mission-utils';
+import App from '../src/App.js';
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -18,18 +18,18 @@ const mockRandoms = (numbers) => {
 };
 
 const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
   logSpy.mockClear();
   return logSpy;
 };
 
-describe("자동차 경주 게임", () => {
-  test("전진-정지", async () => {
+describe('자동차 경주 게임', () => {
+  test('전진-정지', async () => {
     // given
     const MOVING_FORWARD = 4;
     const STOP = 3;
-    const inputs = ["pobi,woni", "1"];
-    const outputs = ["pobi : -"];
+    const inputs = ['pobi,woni', '1'];
+    const outputs = ['pobi : -'];
     const randoms = [MOVING_FORWARD, STOP];
     const logSpy = getLogSpy();
 
@@ -46,17 +46,63 @@ describe("자동차 경주 게임", () => {
     });
   });
 
+  test.each([[['pobi,javaji']], [['pobi,eastjun']]])(
+    '이름의 길이에 대한 예외 처리',
+    async (inputs) => {
+      // given
+      mockQuestions(inputs);
+
+      // when
+      const app = new App();
+
+      // then
+      await expect(app.play()).rejects.toThrow('[ERROR]');
+    },
+  );
+
+  test.each([[['']], [['sujin,']], [['suji,jun,']]])(
+    '이름이 공백인 경우 예외 처리',
+    async (inputs) => {
+      mockQuestions(inputs);
+
+      const app = new App();
+
+      await expect(app.play()).rejects.toThrow('[ERROR] 공백을 입력했습니다.');
+    },
+  );
+
+  test.each([[['pobi,pobi']], [['수진,수진']]])(
+    '중복된 이름에 대한 예외 처리',
+    async (inputs) => {
+      mockQuestions(inputs);
+
+      const app = new App();
+
+      await expect(app.play()).rejects.toThrow(
+        '[ERROR] 동일한 이름을 중복하여 입력했습니다.',
+      );
+    },
+  );
+
   test.each([
-    [["pobi,javaji"]],
-    [["pobi,eastjun"]]
-  ])("이름에 대한 예외 처리", async (inputs) => {
-    // given
-    mockQuestions(inputs);
+    [['jun ,jason']],
+    [['pobi,jun  ']],
+    [['i,💗,JS']],
+    [['pobi,jas😊']],
+    [['왼손,잡이!']],
+    [['hi~,nick']],
+    [['a(A),java']],
+    [['joe,star*']],
+  ])(
+    '공백, 특수문자, 이모티콘이 포함된 이름에 대한 예외 처리',
+    async (inputs) => {
+      mockQuestions(inputs);
 
-    // when
-    const app = new App();
+      const app = new App();
 
-    // then
-    await expect(app.play()).rejects.toThrow("[ERROR]");
-  });
+      await expect(app.play()).rejects.toThrow(
+        '[ERROR] 이름에 공백, 특수문자, 이모티콘을 포함하지 마세요.',
+      );
+    },
+  );
 });
