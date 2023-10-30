506e132f58231e3f8bb79cf9ff127c7237929d54
Kysiam <how2logout@gmail.com>
2023-10-30 22:44:09 +0900

    Docs: create readme file in docs directory

--- __tests__/StringTest.js
+++ __tests__/StringTest.js
@@ -1,30 +1,36 @@
-describe("문자열 테스트", () => {
-  test("split 메서드로 주어진 값을 구분", () => {
-    const input = "1,2";
-    const result = input.split(",");
+import App from '../src/App';
 
-    expect(result).toContain("2", "1");
-    expect(result).toContainEqual("1", "2");
+describe('문자열 테스트', () => {
+  test('split 메서드로 주어진 값을 구분', () => {
+    const input = '1,2';
+    const result = input.split(',');
+
+    expect(result).toContain('2', '1');
+    expect(result).toContainEqual('1', '2');
   });
 
-  test("split 메서드로 구분자가 포함되지 않은 경우 값을 그대로 반환", () => {
-    const input = "1";
-    const result = input.split(",");
+  test('split 메서드로 구분자가 포함되지 않은 경우 값을 그대로 반환', () => {
+    const input = '1';
+    const result = input.split(',');
 
-    expect(result).toContain("1");
+    expect(result).toContain('1');
   });
 
-  test("substring 메서드로 특정 구간 값을 반환", () => {
-    const input = "(1,2)";
+  test('substring 메서드로 특정 구간 값을 반환', () => {
+    const input = '(1,2)';
     const result = input.substring(1, 4);
 
-    expect(result).toEqual("1,2");
+    expect(result).toEqual('1,2');
+  });
+  describe('자바스크립트 테스트', () => {
+    test('should ', () => {
+      const app = new App();
+    });
   });
 
-  test("at 메서드로 특정 위치의 문자 찾기", () => {
-    const input = "abc";
-    const result = input.at(0)
-
-    expect(result).toEqual("a");
+  test('at 메서드로 특정 위치의 문자 찾기', () => {
+    const input = 'abc';
+    const result = input.at(0);
+    expect(result).toEqual('a');
   });
 });
