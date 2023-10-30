beff38953c3ea8ed8e7df21b469460a6e6176e48
JaeJunday <109643814+JaeJunday@users.noreply.github.com>
2023-10-30 22:51:49 +0900

    feat: 생성자 함수 추가
    - 생성자 함수에서 자동차 이름 배열, 게임횟수, 게임 점수 초기화

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import { MissionUtils, Console, Random } from "@woowacourse/mission-utils";
 
 class App {
+  constructor() {
+    this.cars = [];
+    this.score = [];
+    this.gameCount = 0;
+  }
+
   checkNumber(input) {
     const numericRegex = /^[0-9]+$/;
     if (numericRegex.test(input) === false)
@@ -12,8 +18,15 @@ class App {
   }
 
   async play() {
-    const number = Console.readLineAsync("시도할 횟수는 몇 회인가요?");
-    this.checkNumber(number);
+    try {
+      const inputNumber = await Console.readLineAsync(
+        "시도할 횟수는 몇 회인가요?\n"
+      );
+      this.checkNumber(inputNumber);
+      this.gameCount = inputNumber;
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
