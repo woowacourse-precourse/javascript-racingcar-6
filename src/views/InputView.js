7254df39f00a56f9ce287d48767bf7596c3f7e61
Kim Da Eun <midekuna@gmail.com>
2023-10-30 23:55:39 +0900

    ✨ feat: getCarName에 유효성 검사 로직 추가

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,10 +1,19 @@
 import { Console } from '@woowacourse/mission-utils';
 import { InputValidator } from '../models/InputValidator';
+import { throwError } from '../utils/throwError';
+import { MESSAGES } from '../constants/messages';
 
 class InputView {
-  async getCarName(msg) {
-    const carNameInput = await Console.readLineAsync(msg);
-    return carNameInput;
+  async getCarName() {
+    const carsNameInput = await Console.readLineAsync(MESSAGES.getCarName);
+    const carNames = carsNameInput.split(',');
+
+    carNames.forEach((carName) => {
+      if (!InputValidator.isValidCarName(carName))
+        throwError(`유효하지 않은 입력입니다: ${carName}`);
+    });
+
+    return carNames;
   }
 
   async getAttemptNum(msg) {
