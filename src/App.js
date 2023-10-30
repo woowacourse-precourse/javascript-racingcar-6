bf73a5821c4209bd7beebfcebfe5ad815119405c
00kang <kdb070970@gmail.com>
2023-10-30 22:56:29 +0900

    feat: 자동차 이름 입력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    this.getCarName();
+  }
+
+  async getCarName() {
+    const userInput = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) : ',
+    );
+    const carArr = userInput.trim().split(',');
+
+    for (const carName of carArr) {
+      Console.print(carName);
+    }
+  }
 }
 
 export default App;
