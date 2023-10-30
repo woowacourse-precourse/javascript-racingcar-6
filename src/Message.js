ba6ea81f6f929a5b76cb41821fa775fdad6253d2
leedbswo107 <ljyu1@naver.com>
2023-10-31 02:07:16 +0900

    feat: 메시지 추가

--- src/Message.js
+++ src/Message.js
@@ -1,13 +1,15 @@
 const message = {
     error : {
         NAME_CREATION_ERROR : "[ERROR] 자동차의 이름은 5글자 이하여야 합니다.",
+        NOT_A_NUMBER_ERROR : "[ERROR] 입력된 값이 숫자가 아닙니다.",
     },
     game : {
         START_MESSAGE : "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
         ATTEMPTS_NUMBER_QUESTION : "시도할 횟수는 몇 회인가요?",
-        RESULT : "실행 결과",
+        PROCESS_RESULT : "실행 결과",
+        RESULT_PRINT: "최종 우승자 : ",
+        CONTINUE_OR_STOP_QUESTION : "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+        END_MESSAGE : "게임을 종료합니다.",
     }
-
 }
-
 export default message;
