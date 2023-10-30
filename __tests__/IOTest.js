3ea738420ce3b157b637a5c047611a2a120cdd8e
fecapark <fecapark@gmail.com>
2023-10-30 20:24:41 +0900

    test: askNames에 대한 테스트 코드 작성

--- __tests__/IOTest.js
+++ __tests__/IOTest.js
@@ -1,1 +1,20 @@
-describe("입출력 테스트", () => {});
+import { askNames } from "../src/core/io.js";
+import { mockQuestions } from "./ApplicationTest.js";
+
+describe("입출력 테스트", () => {
+  test("askNames가 이름을 제대로 분할하는지 테스트", () => {
+    const inputs = ["ab, 12,abc", "abc,a,b", "  bc,   a,        d     "];
+    const outputs = [
+      ["ab", "12", "abc"],
+      ["abc", "a", "b"],
+      ["bc", "a", "d"],
+    ];
+
+    mockQuestions(inputs);
+
+    inputs.forEach(async (_, index) => {
+      const names = await askNames();
+      expect(names).toEqual(outputs[index]);
+    });
+  });
+});
