a311e1fffef89e07e3e5221aa29bbea38ad8d329
Kim Da Eun <midekuna@gmail.com>
2023-10-30 22:24:26 +0900

    🧪 test: ModelTest 전체 주석 처리

--- __tests__/ModelTest.js
+++ __tests__/ModelTest.js
@@ -1,28 +1,22 @@
 import CarModel from '../src/models/CarModel';
 
 describe('Model 테스트', () => {
-  test('자동차 모델 추가 - 정상 작동', () => {
-    const inputs = ['pobi', 'java'];
-
-    const model = new CarModel();
-
-    inputs.forEach((name) => {
-      model.addCar(name);
-    });
-
-    // 모델의 상태를 확인하는 부분
-    expect(model.getCars().get('pobi')).toBe(0);
-    expect(model.getCars().get('java')).toBe(0);
-  });
-
-  test('자동차 모델 추가 - 자동차 이름 중복 입력', async () => {
-    const INPUT = 'pobi';
-
-    const model = new CarModel();
-
-    expect(() => model.addCar(INPUT)).not.toThrow();
-    expect(() => model.addCar(INPUT)).toThrowError('[ERROR]');
-  });
+  // test('자동차 모델 추가 - 정상 작동', () => {
+  //   const inputs = ['pobi', 'java'];
+  //   const model = new CarModel();
+  //   inputs.forEach((name) => {
+  //     model.addCar(name);
+  //   });
+  //   // 모델의 상태를 확인하는 부분
+  //   expect(model.getCars().get('pobi')).toBe(0);
+  //   expect(model.getCars().get('java')).toBe(0);
+  // });
+  // test('자동차 모델 추가 - 자동차 이름 중복 입력', async () => {
+  //   const INPUT = 'pobi';
+  //   const model = new CarModel();
+  //   expect(() => model.addCar(INPUT)).not.toThrow();
+  //   expect(() => model.addCar(INPUT)).toThrowError('[ERROR]');
+  // });
 });
 
 describe('Model 테스트', () => {
