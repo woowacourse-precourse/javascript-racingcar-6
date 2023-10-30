335726a322aed42e02174352686d76f88855cb4a
j8won <joowoni99@gmail.com>
2023-10-30 18:12:50 +0900

    feat(RacingGame): Add the play method for moving car with the attempt count

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -1,12 +1,32 @@
 class RacingGame {
   #carArray;
+  #attemptCount;
+  #resultArray = [];
 
-  constructor(carArray) {
+  constructor(carArray, attemptCount) {
     this.#carArray = carArray;
+    this.#attemptCount = attemptCount;
+  }
+
+  play() {
+    for (let i = 0; i < this.#attemptCount; i++) {
+      this.moveCars();
+    }
   }
 
   moveCars() {
     this.#carArray.forEach((car) => car.moveRandomly());
+    this.#resultArray.push(this.#carNamePositionArray);
+  }
+
+  get #carNamePositionArray() {
+    return this.#carArray.map((car) => {
+      return { name: car.name, position: car.position };
+    });
+  }
+
+  get resultArray() {
+    return this.#resultArray;
   }
 }
 
