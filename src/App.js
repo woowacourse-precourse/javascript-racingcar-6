3ddba20f9518efae5a461d7fc1666361e1f323e5
starcradle101 <dreamerdev12@gmail.com>
2023-10-30 22:40:20 +0900

    :recycle: Refactor: App.js 리팩토링
    GameSetup 모듈을 사용해서 App.js의 코드 구조 개선

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Console } from '@woowacourse/mission-utils';
-import { PLAYER_INPUT, RACE } from './Logs';
-import Validation from './Validation';
+import { RACE } from './Logs';
 import RaceUtils from './RaceUtils';
+import GameSetup from './GameSetup';
 
 class App {
   constructor() {
@@ -12,37 +12,14 @@ class App {
   }
 
   async play() {
-    await this.getPlayerCarsInput();
-    this.createPlayerData(this.raceEntry);
-    await this.getPlayerTryNumberInput();
+    await this.initRaceSetting();
     this.startRace();
-    RaceUtils.announceWinners(this.winners);
   }
 
-  async getPlayerCarsInput() {
-    const CAR_NAMES_INPUT = await Console.readLineAsync(
-      PLAYER_INPUT.CARS_NAME_PROMPT,
-    );
-    const CAR_NAMES_INPUT_ARRAY = CAR_NAMES_INPUT.split(',');
-
-    if (Validation.isPlayerCarNameValidated(CAR_NAMES_INPUT_ARRAY)) {
-      this.raceEntry = [...CAR_NAMES_INPUT_ARRAY];
-    }
-  }
-
-  createPlayerData() {
-    this.raceEntry.forEach(playerName => {
-      this.playersData.push({ playerName, trackLocation: '' });
-    });
-  }
-
-  async getPlayerTryNumberInput() {
-    const TRY_NUMBER_INPUT = await Console.readLineAsync(
-      PLAYER_INPUT.TRY_NUMBER_PROMPT,
-    );
-    if (Validation.isPlayerTryNumberValidated(TRY_NUMBER_INPUT)) {
-      this.tryNumber = Number(TRY_NUMBER_INPUT);
-    }
+  async initRaceSetting() {
+    this.raceEntry = await GameSetup.getPlayerCarsInput();
+    this.playersData = GameSetup.createPlayerData(this.raceEntry);
+    this.tryNumber = await GameSetup.getPlayerTryNumberInput();
   }
 
   startRace() {
@@ -52,6 +29,7 @@ class App {
       RaceUtils.printRaceStatus(this.playersData);
     }
     this.winners = RaceUtils.findWinners(this.playersData);
+    RaceUtils.announceWinners(this.winners);
   }
 }
 
