5c47452896aed8ce2e4ce5afd64772ba9408ccbc
Youmi Kim <flordelavidaa@naver.com>
2023-10-30 23:09:34 +0900

    Feat: 랜덤값 생성하여 전진
    - 시도 횟수 입력받기
    - 게임 시도 횟수 동안 랜덤값 생성
    - 랜덤값에 따라 각 자동차는 전진 또는 정지

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import { Console } from '@woowacourse/mission-utils';
-import { RacingCar } from './RacingCar.js'
+import { RacingCar } from './RacingCar.js';
 
 class App {
   async play() {
-
-    const carNames = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    const carNames = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+    );
     const carNameArray = carNames.split(',');
 
     const carObjectArray = carNameArray.map((name) => new RacingCar(name));
 
+    const chanceInput =
+      await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    const gameChance = parseInt(chanceInput, 10);
+
+    Console.print('\n실행결과');
+    for (let i = 0; i < gameChance; i++) {
+      carObjectArray.forEach((car) => {
+        car.moveOrStay();
+      });
+    }
   }
 }
 
--- src/RacingCar.js
+++ src/RacingCar.js
@@ -3,13 +3,13 @@ import { Random } from '@woowacourse/mission-utils';
 export class RacingCar {
   constructor(name) {
     this.name = name;
-			this.moveCount = 0;
-    }
+    this.moveCount = 0;
+  }
 
-	moveOrStay() {
-			const randomNumber = Random.pickNumberInRange(0, 9);
-			if (randomNumber >= 4) {
-				this.moveCount += 1;
-			}
-	}
+  moveOrStay() {
+    const randomNumber = Random.pickNumberInRange(0, 9);
+    if (randomNumber >= 4) {
+      this.moveCount += 1;
+    }
+  }
 }
