90ef29c1b765509b1d9122a6e24b150e743b7171
abi-hong <flffjakfwm20@gmail.com>
2023-10-30 10:30:44 +0900

    feat(RacingCar): RacingCar 객체 생성

--- src/RacingCar.js
+++ src/RacingCar.js
@@ -1,4 +1,22 @@
-class RacingCar { 
+class RacingCar {
+  constructor(name, distance) {
+    this.name = name;
+    this.distance = distance;
+  }
+
+  get() {
+    return this._distance;
+  }
+  set(value) {
+    this._distance = value;
+  }
+
+  // 전진, 후진
+  goForward(number) {
+    if (number < 4 ) return false;
+    return true;
+  } 
+
 }
 
-export default RacingCar;
+export { RacingCar };
