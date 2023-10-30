bb8ea26a259a8defbe4995d03fcb552c104dfefb
DongjaJ <jdh981118@naver.com>
2023-10-30 22:48:57 +0900

    feat: 사용자에게 이름과 이동 횟수를 입력받는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  carNames = [];
+  tryNumber = 0;
+
+  async getCarName() {
+    const receivedCarNames = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    this.carNames = receivedCarNames.split(",");
+    Console.print(this.carNames);
+  }
+
+  async getTryNumber() {
+    const receivedTryNumber = await Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요?\n"
+    );
+    this.tryNumber = Number(receivedTryNumber);
+  }
+
+  async play() {
+    await this.getCarName();
+    await this.getTryNumber();
+  }
 }
 
 export default App;
