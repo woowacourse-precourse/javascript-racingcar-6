3b7eba255acbda505b936cf4ac578ca0700567a9
양진성 <anfakt0606@naver.com>
2023-10-30 12:30:04 +0900

    feat : Add constants for user_input , message, and error_message

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const USER_INPUT = {
+  CAR_NAME: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) \n',
+  ROUND: '시도할 횟수는 몇 회인가요? \n'
+}
+
+export const MESSAGE = {
+  RESULT: '실행 결과',
+  WINNER: '최종 우승자 : '
+}
+
+export const ERROR_MESSAGE = {
+  INPUT_CAR_NAME_ERROR : '[ERROR] 자동차 이름은 5글자 이하여야 합니다.',
+  INPUT_CAR_MOVE_ERROR : '[ERROR] 이동횟수는 숫자가 입력되어야 합니다.',
+}
