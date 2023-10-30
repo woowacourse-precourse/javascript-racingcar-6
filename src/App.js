3deb74d0d505ea900aeb572c469910eeb2b8b468
skiende74 <skiende74@gmail.com>
2023-10-31 03:29:19 +0900

    feat(GameProgress, App): Implement game proceeding feature
    Implemented the following methods in GameProgress class:
    - proceed: Handles the game logic for each round.
    - printRoundResult: Outputs the result of each round.
    - canMoveForward: Determines if a car can move forward.
    
    Updated the App class:
    - Modified play method to create a GameProgress instance and invoke the proceed method.
    
    BREAKING CHANGE:
    - play method in App class now utilizes GameProgress, which may affect existing functionality if any dependencies on the old implementation.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import GameSettingInput from "./GameSettingInput.js";
+import GameProgress from "./GameProgress.js";
 import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    const gameSettingInput = new GameSettingInput();
-    const gameSetting = await gameSettingInput.collect();
+    const gameSetting = await this.setting();
     Console.print(gameSetting);
+    const gameProgress = new GameProgress(gameSetting);
+
+    Console.print("\n실행 결과");
+    for (let i = 0; i < gameSetting.trialCount; i++) {
+      gameProgress.proceed();
+    }
+  }
+
+  async setting() {
+    const gameSettingInput = new GameSettingInput();
+    return await gameSettingInput.collect();
   }
 }
 
---
+++ src/GameProgress.js
@@ -0,0 +1,28 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
+export default class GameProgress {
+  constructor(gameSettings) {
+    this.settings = gameSettings;
+    this.advance = Object.fromEntries(
+      gameSettings.carNames.map((key) => [key, 0])
+    );
+  }
+
+  proceed() {
+    for (const carName in this.advance) {
+      if (this.canMoveForward()) this.advance[carName]++;
+    }
+    this.printRoundResult();
+  }
+
+  printRoundResult() {
+    for (const carName in this.advance) {
+      Console.print(`${carName} : ` + "-".repeat(this.advance[carName]));
+    }
+    Console.print("");
+  }
+
+  canMoveForward() {
+    return Random.pickNumberInRange(0, 9) >= 4;
+  }
+}
