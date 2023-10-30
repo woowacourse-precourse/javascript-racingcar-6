5ddfe45fe2347d689f9cf7bd1511f25f9a4ade78
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-10-30 18:19:08 +0900

    refactor : parameter 타입 검사 추가
    0번째 param : carNames의 타입은 string만 가능
    1번째 param : tryRound의 타입은 number만 가능

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import RacingGame from './RacingGame.js';
 import InputReader from './View/InputReader.js';
 import OutputView from './View/OutputView.js';
 import { Console } from '@woowacourse/mission-utils';
+import { paramType } from './utils/paramType.js';
 
 export default class App {
   #inputReader;
@@ -17,12 +18,17 @@ export default class App {
 
   async play() {
     const carNames = await this.#inputReader.carNames();
-    const tryRound = await this.#inputReader.tryRount();
+    const tryRound = Number(await this.#inputReader.tryRount());
 
     await this.ready(carNames, tryRound);
   }
 
-  async ready(carNames, tryRound) {
+  async ready(
+    carNames,
+    tryRound,
+    _0 = paramType(carNames, 'string'),
+    _1 = paramType(tryRound, 'number')
+  ) {
     if (carNames.split(',').some((carName) => carName.length > 5)) {
       throw new Error('[ERROR] 이름은 5자 이하로 적어주세요 !');
     }
