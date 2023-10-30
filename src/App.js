a8b26d8991ea458e5f9b8716da37a8c03b12e36c
sunub <bsc5672@gmail.com>
2023-10-31 00:50:52 +0900

    feat : ApplicationTest를 통과하게끔 play() 메소 작성
    테스트 코드를 모두 통과할 수 있게끔 작성한 모듈들을 활용하여 play() 호출시 제시된 요구조건에 맞는 순서대로 기능들이 실행되게끔 코드를 작성했다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Input from "./Input";
+import RaceRound from "./RaceRound";
 class App {
   async play() {
+    const carNames = await Input.enterCarNames();
+    const totalRound = await Input.enterTryingCount();
+
+    const raceRound = new RaceRound(carNames, totalRound);
+    raceRound.proceedRound();
   }
 }
 
