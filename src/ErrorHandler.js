549fb25af40fa3b5d194d515e72b64f7fad3609b
yu-retel <dbdudals100@naver.com>
2023-10-31 02:53:30 +0900

    feat: 입력 예외사항판별기능추가(중복된이름, 0이나 공백을 입력한 경우)

--- src/ErrorHandler.js
+++ src/ErrorHandler.js
@@ -1,16 +1,19 @@
 const ErrorHandler = {
   carNamesType(cars) {
-    if (cars.indexOf(',') === -1) {
-      throw new Error('쉼표(,)를 기준으로 구분해야 합니다.');
+    if (cars.indexOf(',') === -1 || cars.indexOf(',') === cars.length - 1) {
+      throw new Error('[ERROR] 쉼표(,)를 기준으로 구분해야 합니다.');
     }
 
     const namesTypeConversion = cars.split(',');
 
-    if (namesTypeConversion.filter((car) => car.lngeth === 0).length > 0) {
-      throw new Error('각 이름은 1자 이상이여야 합니다.');
+    if (namesTypeConversion.some((car) => car.trim() === "")) {
+      throw new Error('[ERROR] 각 이름은 공백을 제외한 1자 이상이여야 합니다.');
     }
-    if (namesTypeConversion.filter((car) => car.lngeth >= 5).length > 0) {
-      throw new Error('각 이름은 5자 이하여야 합니다.');
+    if (namesTypeConversion.some((car) => car.length >= 5)) {
+      throw new Error('[ERROR] 각 이름은 5자 이하여야 합니다.');
+    }
+    if (new Set(namesTypeConversion).size !== namesTypeConversion.length) {
+      throw new Error('[ERROR] 중복된 이름을 입력하셨습니다.');
     }
   },
 
@@ -18,7 +21,11 @@ const ErrorHandler = {
     const isNotNumber = /[^0-9]/;
 
     if (isNotNumber.test(count)) {
-      throw new Error('0~9의 숫자형태로 입력해야합니다.');
+      throw new Error('[ERROR] 0~9의 숫자형태로 입력해야 합니다.');
+    }
+
+    if (Number(count) === 0) {
+      throw new Error('[ERROR] 1이상의 숫자를 입력해야 합니다. ');
     }
   }
 }
