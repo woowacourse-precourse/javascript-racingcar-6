17cff34e3f14d22f053181eb9957d11ec8d28dcf
23YUJIN <asdbnm066@daum.net>
2023-10-31 05:39:07 +0900

    feat: 자동차 위치 계산하기 구현

--- src/Car.js
+++ src/Car.js
@@ -9,6 +9,13 @@ class Car {
   generateRandomValue() {
     return Random.pickNumberInRange(0, 9);
   }
+
+  calculatePosition() {
+    const randomValue = this.generateRandomValue();
+    if (randomValue >= 4) {
+      this.position++;
+    }
+  }
 }
 
 export default Car;
