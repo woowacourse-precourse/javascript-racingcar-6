3aa79f3814e9ced4159d4717ad86f892136a62f7
Yeojin Kim <yeojin58@naver.com>
2023-10-30 19:21:31 +0900

    게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+
 class App {
-  async play() {}
+  async play() {
+    MissionUtils.Console.print('자동차 경주 게임을 시작합니다!')
+  }
 }
 
 export default App;
