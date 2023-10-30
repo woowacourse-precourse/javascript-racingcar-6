45048deb7ce94b009423414d700990ad7ed557f4
kimjinwoo <rlawlsdn177@naver.com>
2023-10-30 16:24:31 +0900

    refactor($getRaceTimes): 경주 횟수 입력 구조 변경
    getRaceTimes 메서드를 생성해 유효값 검사 후 play 메서드에 반환

--- src/App.js
+++ src/App.js
@@ -12,13 +12,18 @@ class App {
     async play() {
         const CAR_NAMES_INPUT = await this.getCarNames();
         const CARS = CREATE_CARS(CAR_NAMES_INPUT);
-        const RACE_TIME_INPUT = await Console.readLineAsync(RACE_TIME_INPUT_MESSAGE);
+        const RACE_TIME_INPUT = await this.getRaceTimes();
     }
 
     async getCarNames() {
         const CAR_NAME_INPUT = await Console.readLineAsync(CAR_NAME_INPUT_MESSAGE);
         VALIDATE_CAR_NAME(CAR_NAME_INPUT);
         return CAR_NAME_INPUT.split(',').map(name => name.trim());
+    }
+
+    async getRaceTimes() {
+        const RACE_TIME_INPUT = await Console.readLineAsync(RACE_TIME_INPUT_MESSAGE);
+        return VALIDATE_RACE_TIME(RACE_TIME_INPUT);
     }
 }
 
