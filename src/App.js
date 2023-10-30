517f218b2e29a0ab9dabea83374d5c7799acac0b
youngchann <lych96@gmail.com>
2023-10-30 21:35:29 +0900

    feat: 사용자가 잘못된 값을 입력한 경우 throw문 적용
    - 자동차 이름이 5자 초과하는 경우
    - 자동차 이름이 0자로 입력된 경우
    - 시도할 횟수가 숫자가 아닌 경우
    - 시도할 숫자가 음수이거나 9보다 큰 경우

--- src/App.js
+++ src/App.js
@@ -12,6 +12,26 @@ class App {
     const userCount = await MissionUtils.Console.readLineAsync(
       this.countMessage,
     );
+    if (!App.isValidCar(userCar)) {
+      throw new Error('[ERROR] 자동차 이름을 잘못 입력했습니다.');
+    }
+    if (!App.isValidNumber(userCount)) {
+      throw new Error('[ERROR] 시도 횟수를 잘못 입력했습니다.');
+    }
+  }
+
+  static isValidCar(userCar) {
+    const carNames = userCar.split(',');
+    console.log(carNames);
+    if (carNames.every(name => name.length > 5)) return false;
+    if (carNames.some(name => name.trim().length === 0)) return false;
+    return true;
+  }
+
+  static isValidNumber(userCount) {
+    if (Number.isNaN(Number(userCount))) return false;
+    if (userCount > 9 || userCount < 0) return false;
+    return true;
   }
 }
 
