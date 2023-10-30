f1ddd0eaaddbfc80302ebe32924dc0375a85fb43
YeungJin <143728232+0calCOKE@users.noreply.github.com>
2023-10-30 21:28:36 +0900

    사용자 이름 입력 기능 리팩터링 - 입력 받기, 유효성 검사 분리

--- src/App.js
+++ src/App.js
@@ -5,11 +5,21 @@ class App {
     this.players = [];
   }
 
+  async getAndValidatePlayerName() {
+    const PLAYER_NAME_INPUT = await this.getPlayerName();
+    this.validatePlayerName(PLAYER_NAME_INPUT);
+  }
+
   async getPlayerName() {
-    const PLAYER_NAME = await MissionUtils.Console.readLineAsync(
+    return await MissionUtils.Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
     );
-    const PLAYER_NAME_SPLIT = PLAYER_NAME.split(',').map((name) => name.trim());
+  }
+
+  validatePlayerName(playerNameInput) {
+    const PLAYER_NAME_SPLIT = playerNameInput
+      .split(',')
+      .map((name) => name.trim());
 
     if (
       PLAYER_NAME_SPLIT.some((name) => name.length > 5 || name.length === 0)
@@ -56,7 +66,7 @@ class App {
   }
 
   async play() {
-    await this.getPlayerName();
+    await this.getAndValidatePlayerName();
     await this.getPlayerInputRacingRounds();
 
     MissionUtils.Console.print('\n실행결과');
