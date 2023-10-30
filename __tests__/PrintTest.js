e439e7c59c573c13e332695bc4f9fa915aaf4d6a
8leamnk <silkviewrk@gmail.com>
2023-10-30 22:30:49 +0900

    test: 실행 결과 출력 기능에 대한 테스트 코드를 수정함
    추가 내용:
    - '실행 결과' 문구가 출력되는지에 대한 테스트 코드를 추가함
    
    수정 내용:
    - 코드의 가독성을 개선함
      - Map을 나중에 set하는 방식에서 처음부터 데이터가 있는 상태로 변경함

--- __tests__/PrintTest.js
+++ __tests__/PrintTest.js
@@ -32,26 +32,30 @@ describe('실행 결과와 우승자 가려내기', () => {
       STOP,
       MOVING_FORWARD,
     ];
-    const inputs = new Map();
-    const result = new Map();
+    const EXECUTION_MESSAGE = '실행 결과';
+    const input = new Map([
+      ['Fiat', 0],
+      ['BMW', 0],
+      ['Volvo', 0],
+    ]);
+    const result = new Map([
+      ['Fiat', 2],
+      ['BMW', 1],
+      ['Volvo', 1],
+    ]);
     const outputs = ['Fiat : -', 'Fiat : --', 'Fiat : --'];
     const logSpy = getLogSpy();
-
-    inputs.set('Fiat', 0);
-    inputs.set('BMW', 0);
-    inputs.set('Volvo', 0);
-
-    result.set('Fiat', 2);
-    result.set('BMW', 1);
-    result.set('Volvo', 1);
 
     mockRandoms([...randoms]);
 
     // when
     const { executeProcess } = Print;
-    const updatedCars = executeProcess(inputs, NUMBER_OF_TIMES);
+    const updatedCars = executeProcess(input, NUMBER_OF_TIMES);
 
     // then
+    expect(logSpy).toHaveBeenCalledWith(
+      expect.stringContaining(EXECUTION_MESSAGE),
+    );
     outputs.forEach((output) => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
     });
