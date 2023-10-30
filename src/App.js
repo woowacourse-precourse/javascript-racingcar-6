d5c3f9f9442857cf509a01ddd6399326679eb7b1
Youmi Kim <flordelavidaa@naver.com>
2023-10-30 16:35:07 +0900

    Feat: 자동차 이름 입력받기
    - 입력 받은 이름으로 자동차 객체 배열 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from '@woowacourse/mission-utils';
+import { RacingCar } from './RacingCar.js'
+
 class App {
-  async play() {}
+  async play() {
+
+    const carNames = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    const carNameArray = carNames.split(',');
+
+    const carObjectArray = carNameArray.map((name) => new RacingCar(name));
+
+  }
 }
 
 export default App;
---
+++ src/RacingCar.js
@@ -0,0 +1,15 @@
+import { Random } from '@woowacourse/mission-utils';
+
+export class RacingCar {
+  constructor(name) {
+    this.name = name;
+			this.moveCount = 0;
+    }
+
+	moveOrStay() {
+			const randomNumber = Random.pickNumberInRange(0, 9);
+			if (randomNumber >= 4) {
+				this.moveCount += 1;
+			}
+	}
+}
