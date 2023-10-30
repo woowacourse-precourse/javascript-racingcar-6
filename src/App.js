42216ec6c2c8ddb74bb8dd5a44ce69c5954acf22
hectick <ch.yeondev@gmail.com>
2023-10-30 23:08:38 +0900

    feat: 자동차 경주 우승자를 출력하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -40,6 +40,39 @@ class App {
     }
     MissionUtils.Console.print("");
   }
+
+  findWinner = (carNames, locations) => {
+    let maxLocation = this.findMaxLocation(locations);
+    let winner = [];
+    let index = 0;
+    for(let carName of carNames) {
+      const location = locations.get(carName);
+      if(location == maxLocation) {
+        winner[index] = carName;
+        index++;
+      }
+    }
+    return winner;
+  }
+
+  findMaxLocation = (locations) => {
+    let max = -1;
+    for (let location of locations.values()) {
+      if (location > max) {
+        max = location;
+      }
+    }
+    return max;
+  }
+
+  showWinner = (winners) => {
+    let result = ""
+    for (let winner of winners) {
+      result += winner + ", ";
+    }
+    result = result.substring(0, result.length - 2);
+    MissionUtils.Console.print("최종 우승자 : " + result);
+  }
  
   async play() {
     const input = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
@@ -54,6 +87,8 @@ class App {
       this.showRoundResult(carNames, locations);
       tryCount--;
     }
+    const winner = this.findWinner(carNames, locations);
+    this.showWinner(winner);
   }
 }
 
