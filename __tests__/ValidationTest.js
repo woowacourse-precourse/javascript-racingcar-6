2a0f0399c88897364c52abebb33aa7e143a11d10
soyoung125 <sypark041754@gmail.com>
2023-10-30 21:00:15 +0900

    feat: 이름 예외 처리 테스트 추가

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,4 +1,5 @@
-import { isLongerThan5, isNumber, isNumberAtLeast4 } from "../src/utils/validation"
+import Car from "../src/Car";
+import { isLongerThan5, isNumber, isNumberAtLeast4, validateName } from "../src/utils/validation"
 
 describe("validation.js 테스트", () => {
     test("숫자 형식 여부 검사 - 숫자 입력", () => {
@@ -36,5 +37,23 @@ describe("validation.js 테스트", () => {
         const result = isNumberAtLeast4(random);
 
         expect(result).toBeFalsy();
+    });
+    test("랜덤 숫자가 4 이상인지 검사 - 4 미만인 경우", () => {
+        const random = 2;
+        const result = isNumberAtLeast4(random);
+
+        expect(result).toBeFalsy();
+    });
+    test("이름 예외 처리 - 글자수 초과", () => {
+        const name = "soyoung125";
+        const cars = [];
+
+        expect(() => validateName(name, cars)).toThrow("[ERROR]");
+    });
+    test("이름 예외 처리 - 중복된 이름", () => {
+        const name = "bong";
+        const cars = [new Car("bong")];
+
+        expect(() => validateName(name, cars)).toThrow("[ERROR] 중복된 이름이 존재합니다.");
     });
 })
