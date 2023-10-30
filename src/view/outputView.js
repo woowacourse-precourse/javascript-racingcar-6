1025dfed122681b2407d4724f7b2ab009e8ae0d9
mieum12 <qpdlqltb1215@gmail.com>
2023-10-31 03:00:10 +0900

    fix: output view의 메서드 디테일 추가

--- src/view/outputView.js
+++ src/view/outputView.js
@@ -1,8 +1,16 @@
 import { Console } from "@woowacourse/mission-utils";
+import { MESSAGE } from "../constants/constants";
 
 class OutputView {
-  printResult(result) {
+  // car의 moveCount를 받아서 '-'의 수로 표현
+  printTotalProgress() {}
+
+  printSingleRaceResult({ car, progress }) {
+    const result = `${car} : ${progress}`;
     Console.print(result);
+  }
+  printWinner() {
+    Console.print(MESSAGE.GAME_RESULT + `${winner}`);
   }
 }
 
