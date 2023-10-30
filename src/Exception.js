99ef51e3e7ede0fbefc5ec2f825ad6fd67c3f9eb
soominn <marisol03@naver.com>
2023-10-30 16:16:47 +0900

    :sparkles:feat: Exception 분리

---
+++ src/Exception.js
@@ -0,0 +1,7 @@
+export const EXEPTION = {
+    CAR_NAME_DUPLICATE_ERROR : "[ERROR] 자동차의 이름은 한 번씩만 사용할 수 있습니다.",
+    CAR_NAME_COUNT_ERROR : "[ERROR] 자동차의 이름을 2개 이상 입력해주세요.",
+    CAR_NAME_LENGTH_ERROR : "[ERROR] 자동차의 이름을 4자 이하로 정해주세요.",
+    INVALID_TYPE_TRY_ERROR : "[ERROR] 시도할 횟수를 숫자로 입력해주세요.",
+    TRY_COUNT_ERROR : "[ERROR] 시도할 횟수를 1회 이상 입력해주세요."
+}
