3db8ff0174f8869ccfe094c67eb55291ac831563
abi-hong <flffjakfwm20@gmail.com>
2023-10-30 15:35:06 +0900

    refactor(InputTest): 테스트 명세에 비지니스 행위 담도록

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -1,9 +1,7 @@
-import App from '../src/App.js';
-import { MissionUtils } from '@woowacourse/mission-utils';
 import { Validator } from '../src/utils/Validator.js';
 
 describe('입력 테스트', () => {
-  test('값을 입력하지 않았을 경우, 예외 처리', () => {
+  test('값을 입력하지 않았을 경우, 예외 처리한다.', () => {
     const inputs = ['', null, undefined];
 
     inputs.forEach((input) => {
@@ -11,11 +9,11 @@ describe('입력 테스트', () => {
     })
   });
 
-  test('각 자동차 이름이 6자 이상일 경우, 예외 처리', () => {
+  test('각 자동차 이름이 6자 이상일 경우, 예외 처리한다.', () => {
     expect(() => Validator.checkNameLength(['nameislong'])).toThrow('[ERROR] 각 입력값은 5자 이하만 가능합니다.');
   });
 
-  test('숫자가 아닌 입력인 경우, 예외 처리', () => {
+  test('숫자가 아닌 입력인 경우, 예외 처리한다.', () => {
     expect(() => Validator.checkCount('string')).toThrow('[ERROR] 입력값이 정수가 아닙니다.');
   });
 })
