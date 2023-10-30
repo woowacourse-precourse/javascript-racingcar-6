133dc3f723365586c49ae89f8c0ea3a601cc5a71
aaapark <parksc1534.dev@gmail.com>
2023-10-30 18:53:49 +0900

    자동차 이름 입력값을 쉼표(,)로 구분하기

--- src/UserInput.js
+++ src/UserInput.js
@@ -5,6 +5,12 @@ export default class User {
         const inputCarName = await MissionUtils.Console.readLineAsync(
             '경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분) : \n'
         )
-        console.log(inputCarName)
+        this.getCarNames(inputCarName)
+    }
+
+    getCarNames(userInput) {
+        this.cars = []
+        let names = userInput.split(',')
+        console.log(names)
     }
 }
