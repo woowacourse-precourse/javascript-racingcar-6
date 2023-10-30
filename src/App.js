b0bcbf8633aa39f19449424d3e57319dfa55e930
Jaehyeok <milkboy2564@naver.com>
2023-10-30 18:07:26 +0900

    feat: 사용자 입력 - 잘못된 입력 시 에러 메세지 출력 후 프로그램 종료

--- src/App.js
+++ src/App.js
@@ -10,13 +10,34 @@ class App {
   }
 
   async receiveCarNameInput() {
-    this.#carNameList = await Console.readLineAsync(
+    const carNameInput = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     );
+
+    const isInvalidCarName = carNameInput
+      .trim("")
+      .split(",")
+      .some((carName) => carName.length > 5);
+
+    if (isInvalidCarName) {
+      throw new Error(
+        "[ERROR] 자동차의 이름은 쉼표로 구분된 5자 이하 문자만 가능합니다. 프로그램을 종료합니다."
+      );
+    }
   }
 
   async receiveTryCountInput() {
     this.#tryCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+  }
+
+  validateUserInput() {
+    const isInvalidCount = isNaN(parseInt(this.#tryCount));
+
+    if (isInvalidCount) {
+      throw new Error(
+        "[ERROR] 입력값이 유효하지 않습니다. 프로그램을 종료합니다."
+      );
+    }
   }
 
   accumulateDistanceEachCar() {}
@@ -26,8 +47,6 @@ class App {
   calculateDistance() {}
 
   printWinnerResult() {}
-
-  validateUserInput() {}
 }
 
 export default App;
