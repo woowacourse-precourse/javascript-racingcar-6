740746c1616560e4257022a71a5e8482c7f10b8b
Jeongwook Park <hatchling3713@gmail.com>
2023-10-30 20:35:54 +0900

    feat(output): add features which output calculated results to Console

---
+++ src/output.js
@@ -0,0 +1,28 @@
+import { Console } from '@woowacourse/mission-utils';
+
+const outputCurrentProgress = results => {
+  results.forEach(result => {
+    const { name, distanceFromStart } = result;
+
+    let distance = '';
+
+    for (let i = 0; i < distanceFromStart; i++) {
+      distance++;
+    }
+
+    Console.print(`${name} : ${distance}`);
+  });
+
+  Console.print('');
+};
+
+const outputFinalProgress = results => {
+  const distances = results.map(result => result.distanceFromStart);
+  const maxValue = Math.max(...distances);
+
+  const winner = results
+    .filter(result => result.distanceFromStart === maxValue)
+    .map(result => result.name);
+
+  Console.print(`최종 우승자 : ${winner.join(', ')}`);
+};
