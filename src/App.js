ddfdb92e7f7bc5d413aa2499692ac6def881373d
Modern-HD <102591908+Modern-HD@users.noreply.github.com>
2023-10-30 20:25:25 +0900

    Feat: 경주할 자동차 이름을 입력 받는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+    async play() {}
+
+    /**
+     * @returns {Promise<string>}
+     */
+    async getInputNames() {
+        return await MissionUtils.Console.readLineAsync(
+            '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+        );
+    }
 }
 
 export default App;
