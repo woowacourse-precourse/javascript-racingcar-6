5b9fba8379bde286bda52caae3e436d426ecfd23
seoye0ng <bsy0313@gmail.com>
2023-10-31 00:40:02 +0900

    Feat: 경주할 자동차 이름을 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
   async play() {}
+
+  // 자동차 이름 입력받기
+  async getCarNames() {
+    const carNames = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+    );
+    const carNamesArr = carNames.split(',');
+    carNamesArr.forEach((carName) => this.validateCarName(carName));
+
+    return carNamesArr;
+  }
 }
 
 export default App;
