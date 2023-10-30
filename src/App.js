c65b11420ed76b35269f4709a076ab74b44d5589
JaeJunday <109643814+JaeJunday@users.noreply.github.com>
2023-10-30 22:44:12 +0900

    feat: 게임횟수 사용자 입력 검사하는 함수

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import { MissionUtils, Console, Random } from "@woowacourse/mission-utils";
 
 class App {
+  checkNumber(input) {
+    const numericRegex = /^[0-9]+$/;
+    if (numericRegex.test(input) === false)
+      throw new Error("[Error]: invalid number");
+  }
+
   rollDice() {
     return Random.pickNumberInRange(0, 9);
   }
 
-  async play() {}
+  async play() {
+    const number = Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+    this.checkNumber(number);
+  }
 }
 
 export default App;
