2cefd95d85536ee0b0b64d8cd78c8b0fd37de978
MiHye <57440192+kangmihye@users.noreply.github.com>
2023-10-31 01:26:27 +0900

    style: 주석 제거
    - car class에 주석제거

--- src/Car.js
+++ src/Car.js
@@ -4,6 +4,7 @@ import { Random, Console } from "@woowacourse/mission-utils";
 class Car {
 	#name;
 	#steps;
+
 	constructor(name) {
 		if (name.length > CAR.nameLength)
 			throw "자동차 이름의 길이는 5자이하여야 합니다";
@@ -25,15 +26,9 @@ class Car {
 
 	canMoveStep() {
 		const number = Random.pickNumberInRange(0, 9);
-		// console.log("랜덤숫자: ", number); //여기 나중에 지움
 		number >= CAR.StepsNumber && (this.#steps += "-");
 		return this;
 	}
 }
 
 export default Car;
-// const car = new Car("차1");
-// for (let i = 0; i < 5; i++) {
-// 	car.canMoveStep();
-// }
-// car.printCarStpes;
