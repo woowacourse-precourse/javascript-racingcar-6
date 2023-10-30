8a92b606a6cc542c3f94de9e2a93e5b45249b9a5
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-30 19:43:04 +0900

    feat: [Controller] run 메서드 구현

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,1 +1,8 @@
-export default class Controller {}
+import InputView from '../view/InputView.js';
+
+export default class Controller {
+  async run() {
+    const carNames = await InputView.carName();
+    const carNameList = carNames.split(',');
+  }
+}
