a25a7cc032e92f415e964aed25b7e007fd62174e
soosoo22 <lllllh00@naver.com>
2023-10-30 10:22:01 +0900

    chore: Input 클래스에 static 적용
    클래스에 인스턴스없이 바로 호출 가능하기 때문에 static 적용함

--- src/view/input.js
+++ src/view/input.js
@@ -3,7 +3,8 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 // 폴더 변경
 class Input {
   static async text(message) {
-    return await MissionUtils.Console.readLineAsync(message);
+    const carName = await MissionUtils.Console.readLineAsync(message);
+    return carName;
   }
 }
 export default Input;
