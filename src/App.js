01f672ac7b8e638e81b4db7d8a189fb27ea0f46e
Narachan97 <qkrtkf3112@naver.com>
2023-10-30 16:15:41 +0900

    feat: 입력받은 자동차 (,) 기준으로 나누기
    src/App.js 입력받은 자동차 (,) 기준으로 나누기
    
    App 새로운기능
    -사용자 로 부터 입력받은 이름을 (,) 쉼표 기준 으로 나눈다.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Random, Console } from "@woowacourse/mission-utils";
 class App {
   async play() {
     const carNameInput = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    const carNameArray = carNameInput.split(",");
+    
   }
 }
 
