50b22b925165713e05e40fb351bee77b1f9a232a
nyeonge <nnnyeonge@gmail.com>
2023-10-30 23:51:34 +0900

    Style: code convention

--- src/App.js
+++ src/App.js
@@ -7,19 +7,18 @@ class App {
     this.UserInput = new UserInput();
     this.PlayGame = new PlayGame();
   }
-  
   async play() {
     const CarNames = await this.UserInput.inputCarNames();
     const TryNumber = await this.UserInput.inputTryNum();
-    MissionUtils.Console.print('실행 결과');
-    let count = new Array(CarNames.length).fill(0);
-    for(let i=0;i<TryNumber;i++) {
-      const GetRandomNumber = await this.PlayGame.getRandomNumber(CarNames, count);
-      const DisplayResult = await this.PlayGame.displayResult(CarNames, GetRandomNumber);
-      count = GetRandomNumber;
+    MissionUtils.Console.print('\n 실행 결과');
+    let Count = new Array(CarNames.length).fill(0);
+    for (let i = 0; i < TryNumber; i++) {
+      const StepForward = await this.PlayGame.getStepForward(CarNames, Count);
+      await this.PlayGame.displayResult(CarNames, StepForward);
+      Count = StepForward;
       MissionUtils.Console.print('\n');
     }
-    const PickWinner = await this.PlayGame.pickWinner(CarNames, count);
+    const PickWinner = await this.PlayGame.pickWinner(CarNames, Count);
     MissionUtils.Console.print('최종 우승자 : ' + PickWinner);
   }
 }
