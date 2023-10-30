6ce2df51a3656f6217743206b85e6c39f9416aee
한지은 <hjn510450@gmail.com>
2023-10-30 15:38:10 +0900

    App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console } from '@woowacourse/mission-utils';
+import ProcessInput from './utils/ProcessInput';
+import Test from './utils/Test';
+import PrintWinner from './utils/PrintWinner';
+
 class App {
-  async play() {}
+  async play() {
+    const { carNameArr, testNum } = await ProcessInput(); 
+
+    Console.print('실행 결과');
+    const test = new Test(carNameArr);
+    for (let i = 0; i < testNum; i++) await test.testing(carNameArr);
+
+    await PrintWinner(carNameArr, test.moveCount);
+  }
 }
 
 export default App;
