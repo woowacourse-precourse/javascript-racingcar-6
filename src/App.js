b70c8fa2e1451d141e42328ff618b7f5671655fc
meunrud <113085963+meunrud@users.noreply.github.com>
2023-10-30 15:41:04 +0900

    refactor : 입력 받은 값을 검증하고 배열로 바꾸는 기능 따로 분리 toArray()

--- src/App.js
+++ src/App.js
@@ -12,11 +12,23 @@ class App {
     return input.includes(',');
   }
 
+  toArray(input) {
+    this.input = input;
+    if (!this.includeSemiColon(input)) {
+      throw new Error('[ERROR] 이름은 쉼표로 구분되어야 합니다.');
+    }
+    const names = input.split(',').map((name) => name.trim());
+    if (!this.isValidNameFormat(names)) {
+      throw new Error('[ERROR] 이름은 5자를 넘을 수 없습니다.');
+    }
+    return names;
+  }
+
   async printGameMessage() {
     const playerInput = await Console.readLineAsync(
       GAME_MESSAGE.INPUT_CAR_NAME,
     );
-    const playerCarName = playerInput.split(',').map((name) => name.trim());
+    const playerCarName = this.toArray(playerInput);
     return playerCarName;
   }
 }
