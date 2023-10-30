f0db8197c1f7b62ed934c70788ffe2fc1edd409b
Yeojin Kim <yeojin58@naver.com>
2023-10-30 19:45:36 +0900

    README 수정, 경주 자동차 이름 입력

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
+class Car {
+  constructor(carName) {
+    this.validateName(name);
+    this.name = name;
+    this.position = 0;
+  }
+
+  validateName(name) {
+    if (name.length > 5) {
+      throw new Error("[ERROR] 자동차 이름은 5자를 초과할 수 없습니다.")
+    }
+  }
+}
 
 class App {
   async play() {
-    MissionUtils.Console.print('자동차 경주 게임을 시작합니다!')
+    MissionUtils.Console.print('자동차 경주 게임을 시작합니다!');
+    try {
+      await this.inputCarNames();
+     } catch (error) {
+      console.error(error.message);
+     }
+  }
+
+  async inputCarNames() {
+    const names = await MissionUtils.Console.readLineAsync();
+    this.cars = names.split(',').map(name => new Car(name));
   }
 }
 
