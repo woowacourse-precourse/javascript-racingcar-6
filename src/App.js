2bd814577df9aa739891e37adc34a8eaf04ce900
Rilee-0320 <go13209@naver.com>
2023-10-30 23:44:16 +0900

    feat: 게임 라운드 수 입력받기

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Random, Console } from "@woowacourse/mission-utils";
 class App {
   async play() {
     const carNameList = await this.inputCarNames();
+    const roundNumber = await this.inputRoundNumber();
   }
 
   async inputCarNames() {
@@ -10,6 +11,10 @@ class App {
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
     return carNames.split(",");
+  }
+
+  async inputRoundNumber() {
+    return Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
   }
 }
 
