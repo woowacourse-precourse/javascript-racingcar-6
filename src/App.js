73ff4e94cbfa161c68d7794dd8896095b25e936b
hyzzzzy <shinhj4068@naver.com>
2023-10-30 15:49:18 +0900

    feat(CarRace): 자동차 경주 구현 1. 플레이 조건 함수 구현 2. 랜덤 함수 구현 3. 앞으로 가는 조건 함수 구현 4. 결과 판정 함수 구현 5. 결과 출력 함수 구현

--- src/App.js
+++ src/App.js
@@ -13,11 +13,15 @@ class App {
       const carNames = getValidInputNames(inputNames);
       const inputNumber = await readLineAsync(INPUT_NUMBER);
       const count = getValidInputNumber(inputNumber);
-
-      this.carRace = new CarRace(carNames, count);
       
+      this.carRace = new CarRace(carNames, count);
     } catch (error) {
       throw new Error(error);
+    }
+
+    while (this.carRace.isPlaying()) {
+      this.carRace.calculateResult();
+      this.carRace.getResultMessage();
     }
   }
 }
--- src/CarRace.js
+++ src/CarRace.js
@@ -1,10 +1,45 @@
-import Car from './Car.js';
+import Car from './Car';
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { SETTING, SCORE } from './constants';
+import { printMessage } from './utils';
 
 export default class CarRace {
   constructor(carNames, count) {
-    /** @type Array<string> */
+    /** @type Array<Car> */
     this.carNames = carNames.map((name) => new Car(name));
     /** @type number */
     this.count = count;
+  }
+
+  isPlaying() {
+    return this.count > 0;
+  }
+
+  getRandomNumber() {
+    const { MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER } = SETTING;
+    return MissionUtils.Random.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
+  }
+  
+  isforwardNumber(num) {
+    const { TARGET_FORWARD_NUMBER } = SETTING;
+    return num >= TARGET_FORWARD_NUMBER;
+  }
+
+  calculateResult() {
+    this.carNames.forEach((car) => {
+      const random = this.getRandomNumber();
+      if (this.isforwardNumber(random)) {
+        car.forward();
+      }
+    });
+    this.count -= 1;
+  }
+  
+  getResultMessage() {
+    const { FORWARD } = SCORE;
+    this.carNames.forEach((car) => {
+      printMessage(`${car.name} : ${FORWARD.repeat(car.position)}`);
+    });
+    printMessage('\n');
   }
 }
