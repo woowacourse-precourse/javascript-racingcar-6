78c678fd6fb6b2372bf49f7ee389636e8a88d327
Modern-HD <102591908+Modern-HD@users.noreply.github.com>
2023-10-30 20:27:18 +0900

    Feat: 이름에 유효성 검사를 하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -19,6 +19,18 @@ class App {
     getSplittedName(nameStr) {
         return nameStr.split(',');
     }
+
+    /**
+     * @param {string[]} nameArr
+     */
+    nameValidate(nameArr) {
+        if (!(nameArr instanceof Array && nameArr.length > 0)) {
+            throw new Error('[ERROR] 하나 이상의 이름을 입력해주세요.');
+        }
+        if (nameArr.find(str => !(typeof str === 'string' && str.length > 0 && str.length <= 5)) !== undefined) {
+            throw new Error('[ERROR] 1글자 이상, 5글자 이하의 이름만 사용 가능 합니다.');
+        }
+    }
 }
 
 export default App;
