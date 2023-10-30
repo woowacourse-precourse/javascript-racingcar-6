d79f45dff76a7d9656138ff2360f9cf1c5181573
yheseul_kang <kangyheseul@naver.com>
2023-10-30 21:03:19 +0900

    Refactor: 기능 모듈화 // 추출된 Random 값과 실행 횟수에 따른 전진 기능 모듈 파일로 생성

--- src/App.js
+++ src/App.js
@@ -1,43 +1,30 @@
 import { Console } from "@woowacourse/mission-utils";
 import { MESSAGE } from "./message.js";
-import { player, player_array, lengthError, regexError } from "./player.js"
-
+import StartCarName from "./RacingCar/StartCarName.js";
+import { carNameArray } from "./Validates/CarName.js";
 
 class App {
-  constructor() {
-    this.player = null;
-    this.player_array = null;
-  }
   async play() {
-    await Console.print(MESSAGE.GAME_START);
-
-    this.player = player;
-    this.player_array = player_array;
     
-    if (lengthError.some((element) => !element)) {
-    throw new Error(MESSAGE.ERROR);
-    }
+    StartCarName;
 
-    try {regexError();} 
-    catch (error) {
-    throw new Error(MESSAGE.ERROR);
-    }
+
 
 
     const try_number = await Console.readLineAsync(MESSAGE.TRY_NUMBER);
     console.log(typeof(try_number));
-    Console.print(' ');
+    Console.print(MESSAGE.TAB);
 
 Console.print(MESSAGE.ACTION_RESULT);
 
 for (let i = 0; i < try_number; i++) {
   var try_game = {};
-  player_array.forEach(function (value,index) {
-    const action_result = (try_game.name = player_array[index] + ' ' + ':' + ' ');
-    Console.print(action_result);
+  carNameArray.forEach(function (value,index) {
+    const action_result = (try_game.name = carNameArray[index] + ' ' + ':' + ' ');
+    Console.print(MESSAGE.ACTION_RESULT);
 }
 ) 
-Console.print(' ');
+Console.print(MESSAGE.TAB);
 }
 
 
