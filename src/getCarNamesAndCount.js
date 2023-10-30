b5623006934547d18e5da3f982694b73762dafbf
nkEllaaa <dhrtns515@gmail.com>
2023-10-30 12:55:52 +0900

    docs: 시도할 횟수 입력받는 함수 getRaceCount 구현

--- src/getCarNamesAndCount.js
+++ src/getCarNamesAndCount.js
@@ -1,5 +1,13 @@
 import { Console } from '@woowacourse/mission-utils'
 
+const getRaceCount = () => {
+  const raceCount = Console.readLineAsync('시도할 횟수는 몇 회인가요?');
+  const raceCountsToNumber = parseInt(raceCount);
+  //콘솔로그 지우기
+  console.log(raceCountsToNumber,'카운트')
+  return raceCountsToNumber;
+}
+
 const getCarNames = () => {
   const carNames = Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
   const carNamesToArray = carNames.split('');
