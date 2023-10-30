fc4c9e2bbaba8eda0d38079cd32719440441348c
lucaseunchae <lucaskim.dev@gmail.com>
2023-10-30 23:30:21 +0900

    test(input): add movement count input test

---
+++ __tests__/helpers/inputMovementCount.test.js
@@ -0,0 +1,20 @@
+import { InputError } from "../../src/errors";
+import inputMovementCount, {
+  ERROR_MESSAGES,
+} from "../../src/helpers/inputMovementCount";
+import { mockQuestions } from "../test-utils";
+
+describe("이동 횟수 입력 예외 테스트", () => {
+  test.each([[["0"]], [["1a3"]], [["1 2"]]])(
+    "%#. 1 이상의 숫자가 아닌 입력",
+    async (inputs) => {
+      // given
+      mockQuestions(inputs);
+
+      // when & then
+      await expect(inputMovementCount()).rejects.toThrow(
+        new InputError(ERROR_MESSAGES.NOT_POSITIVE_INTEGER)
+      );
+    }
+  );
+});
