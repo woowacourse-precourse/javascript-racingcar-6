aabbf968b3a0d75cc2b47edd1d00fba3c6a48f77
hanyoung <phanyoung@naver.com>
2023-10-30 19:48:34 +0900

    test: 등록하는 자동차가 2대 미만인 경우 예외 처리하는지 테스트

--- __tests__/FeatureTest.js
+++ __tests__/FeatureTest.js
@@ -11,20 +11,37 @@ const mockQuestions = (inputs) => {
   });
 };
 
-describe("자동차 이름 입력 기능에 대한 테스트", () => {
-  test("6자 이상의 이름에 대해 예외 처리를 하는가", async () => {
-    const app = new App();
+describe("세부 기능에 대한 테스트", () => {
+  describe("자동차 이름 입력 기능에 대한 테스트", () => {
+    test("6자 이상의 이름에 대해 예외 처리를 하는가", async () => {
+      const app = new App();
 
-    const inputs = ["자동차, 멋있는자동차"];
+      const inputs = ["자동차, 멋있는자동차"];
 
-    mockQuestions(inputs);
+      mockQuestions(inputs);
 
-    await expect(app.play()).rejects.toThrow("[ERROR]");
+      await expect(app.play()).rejects.toThrow("[ERROR]");
+    });
+
+    test("이름 입력 시 ','를 기준으로 분리하는가", async () => {
+      const input = "자동차1, 자동차2, 자동차3, 자동차4";
+
+      expect(CarNamesParser.parse(input)).toHaveLength(4);
+    });
   });
 
-  test("이름 입력 시 ','를 기준으로 분리하는가", async () => {
-    const input = "자동차1, 자동차2, 자동차3, 자동차4";
+  describe("자동차 등록 기능에 대한 테스트", () => {
+    test("등록하는 자동차가 2대 미만일 경우에 대해 예외 처리를 하는가", async () => {
+      const app = new App();
 
-    expect(CarNamesParser.parse(input)).toHaveLength(4);
+      // input이 정상일 경우 테스트 통과하는지 확인
+      const correctInputs = ["자동차1, 자동차2", 2];
+      mockQuestions(correctInputs);
+      await expect(app.play()).resolves.not.toThrowError();
+
+      const wrongInputs = ["자동차1", 2];
+      mockQuestions(wrongInputs);
+      await expect(app.play()).rejects.toThrow("[ERROR]");
+    });
   });
 });
