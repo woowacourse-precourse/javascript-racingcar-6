180c69e2aef396ef74e120abaf8191d2da236f7e
meunrud <113085963+meunrud@users.noreply.github.com>
2023-10-30 15:36:48 +0900

    feat : 입력받은 자동차 이름이 5자 이하인지 확인하는 기능 구현   isValidNameFormat()

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console, MissionUtils } from '@woowacourse/mission-utils';
+import GAME_MESSAGE from './constants/gameMessage.js';
+
 class App {
   async play() {}
+
+  isValidNameFormat(names) {
+    return names.every((name) => name.length <= 5);
+  }
 }
 
 export default App;
