29b4d82244f756eddf94af78ab80cc4cbba54359
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2023-10-30 17:11:31 +0800

    feat(PrintOutput): add  화면에 출력할 결과

---
+++ src/PrintOutput.js
@@ -0,0 +1,23 @@
+import { Console } from '@woowacourse/mission-utils';
+
+const PrintOutput = {
+    printResultTitle() {
+        Console.print('실행 결과\n');
+    },
+    //각 게임 실행 결과 출력 
+    printEachResult(names, racingCars) {
+        names.forEach((name) => {
+            if (racingCars.has(name)) racingCars.set(name, 1 + racingCars.get(name));
+            else racingCars.set(name, 1);
+            Console.print(`${name} : ${'-'.repeat(racingCars.get(name))}`);
+          })
+    },
+    //우승자 출력 
+    printWinner(maxRate, racingCars) {
+        let result = [];
+        racingCars.forEach((rate, name) => {
+            if (rate === maxRate) result.push(name);
+        })
+        Console.print(`최종 우승자 : ${result.join(', ')}`);
+    }
+}
