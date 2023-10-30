d5314013e3abbf417ad824da9e4efe2ac568c21b
chorong <chorong4641@naver.com>
2023-10-30 22:39:23 +0900

    test(racePrint): 전진하는 자동차 진행 상황 출력하는 테스트 코드 작성
     - 기능 추가에 따라 기존 테스트 코드 제거

--- __tests__/RaceTest.js
+++ __tests__/RaceTest.js
@@ -1,15 +1,22 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import App from '../src/App.js';
+import { printForwardCar } from "../src/PrintMessage.js";
 
 const app = new App();
 
 describe("자동차 경주 테스트", () => {
-    test("주어진 횟수 동안 n대의 자동차 전진", () => {
-        const carNames = ['car1', 'car2'];
-        const raceCount = 3;
-        MissionUtils.Random.pickNumberInRange = jest.fn();
-        MissionUtils.Random.pickNumberInRange.mockReturnValue(5);
-        expect(app.startRace(carNames, raceCount)).toEqual({ "car1": 3, "car2": 3 });
-        jest.clearAllMocks();
+    test("전진하는 자동차들의 정보 저장할 객체 초기화", () => {
+        const carNames = ['car1', 'car2', 'car3'];
+        expect(app.setForwardCarData(carNames)).toStrictEqual({ "car1": 0, "car2": 0, "car3": 0 });
     });
+
+    test("전진하는 자동차 이름과 전진한 만큼 대시(-) 출력", () => {
+        const carName = 'car1';
+        const forwardCount = 5;
+        MissionUtils.Console.print = jest.fn();
+        printForwardCar(carName, forwardCount);
+        expect(MissionUtils.Console.print).toHaveBeenCalledWith("car1 : -----");
+    });
+
+    jest.clearAllMocks();
 });
