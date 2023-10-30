ac994da3c132ff842668cee808f307be17fb9ded
Jeongeun Lee <leeju1009@gmail.com>
2023-10-30 17:57:54 +0900

    refactor: 자동차 이름을 입력받아 객체를 생성하는 메서드를 정적 메서드로 전환

--- src/SettingCars.js
+++ src/SettingCars.js
@@ -4,7 +4,7 @@ import ErrorHandler from './ErrorHandler.js';
 import Car from './Car.js';
 
 class SettingCars {
-  async registerCars() {
+  static async registerCars() {
     const carNameList = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
     );
