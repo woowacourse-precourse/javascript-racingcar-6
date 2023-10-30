26e8dc5a538c26c5fb71b39ac8101f5200c4e41a
23YUJIN <asdbnm066@daum.net>
2023-10-31 05:00:29 +0900

    feat: 무작위 값 구하기 구현

--- src/Car.js
+++ src/Car.js
@@ -1,7 +1,13 @@
+import { Random } from "@woowacourse/mission-utils";
+
 class Car {
   constructor(name) {
     this.name = name;
     this.position = 0;
+  }
+
+  generateRandomValue() {
+    return Random.pickNumberInRange(0, 9);
   }
 }
 
