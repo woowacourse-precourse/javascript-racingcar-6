2c12d37d5313030af949bb783d76eedb81b09754
Jeongwook Park <hatchling3713@gmail.com>
2023-10-30 20:22:00 +0900

    feat(process): add feature which repeats calculation for certain time

--- src/process.js
+++ src/process.js
@@ -1,6 +1,16 @@
 import { Random } from '@woowacourse/mission-utils';
 import CONSTANTS from './constants';
 
+const executeRace = (names, numberOfAttempts) => {
+  let results = names.map(name => {
+    return { name, distanceFromStart: 0 };
+  });
+
+  for (let i = 0; i < numberOfAttempts; i++) {
+    results = results.map(calculateNextResult);
+  }
+};
+
 const calculateNextResult = result => {
   const randomInt = Random.pickNumberInRange(0, 9);
 
