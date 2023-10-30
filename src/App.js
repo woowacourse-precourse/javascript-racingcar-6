48160dc6dc708ff62cded857734186f98333d8be
GimGayeon <kky1373@naver.com>
2023-10-30 16:35:12 +0900

    feat: 사용자 입력받기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const { Console } = MissionUtils;
 class App {
+  async inputCarNames() {
+    const carNames = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+    return carNames;
+  }
+  async inputTryCount() {
+    const tryCount = await Console.readLineAsync("시도할 회수는 몇회인가요?");
+    return tryCount;
+  }
+  input() {
+    this.inputCarNames();
+    this.inputTryCount();
+  }
   async play() {}
 }
 
