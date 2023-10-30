d680b1dbd4b47fdd49f2dd4a0f093a623e66fff8
sojeong0302 <01duqrl@naver.com>
2023-10-30 14:43:01 +0900

    자동차 이름 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    let carNameInput  = await MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+
+     // 쉼표(,)로 구분된 자동차 이름을 배열로 분리
+     const carNames = carNameInput.split(',');
+
+     // carNames 배열에 자동차 이름이 들어 있음
+     console.log(carNames);
+  }
 }
 
 export default App;
