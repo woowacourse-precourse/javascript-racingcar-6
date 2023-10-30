4b3b73484679035c402b1d9556b1da46e940f574
dongbin420 <dongbin420@gmail.com>
2023-10-31 03:37:20 +0900

    test: edit test code for new structure of application

--- __tests__/FunctionTest.js
+++ __tests__/FunctionTest.js
@@ -1,34 +1,30 @@
-import UserInputValidator from '../src/components/UserInputValidator.js';
-import { ERROR_MESSAGE } from '../src/utils/constants';
+import RacingGame from '../src/components/RacingGame';
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+const mockQuestions = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  });
+};
 
 describe('유효성 테스트', () => {
-  test('차 이름 5글자 이하 여부 확인', () => {
-    const wrong = 'red,darkred,lightyellow';
-    const right = 'red,blue,green';
-    const userInputValidatorWrong = new UserInputValidator(wrong, '');
-    const userInputValidatorRight = new UserInputValidator(right, '');
+  test.each([[['red,darkred,lightyellow']], [['red,blue,greeeen']]])(
+    '차 이름 5글자 이하 여부 확인',
+    async (inputs) => {
+      mockQuestions(inputs);
+      const racingGame = new RacingGame();
 
-    expect(userInputValidatorRight.validateCar()).toBeTruthy();
-
-    try {
-      userInputValidatorWrong.validateCar();
-    } catch (error) {
-      expect(error.message).toEqual(ERROR_MESSAGE.input);
+      await expect(racingGame.start()).rejects.toThrow('[ERROR]');
     }
-  });
+  );
 
-  test('경주 횟수 입력 숫자 양의 정수 여부 확인', () => {
-    const wrong = '-1';
-    const right = '2';
-    const userInputValidatorWrong = new UserInputValidator('', wrong);
-    const userInputValidatorRight = new UserInputValidator('', right);
+  test.each([[['-1']], [['-2']]])('경주 횟수 입력 숫자 양의 정수 여부 확인', async (inputs) => {
+    mockQuestions(inputs);
+    const racingGame = new RacingGame();
 
-    expect(userInputValidatorRight.validateNumber()).toBeTruthy();
-
-    try {
-      userInputValidatorWrong.validateNumber();
-    } catch (error) {
-      expect(error.message).toEqual(ERROR_MESSAGE.input);
-    }
+    await expect(racingGame.start()).rejects.toThrow('[ERROR]');
   });
 });
