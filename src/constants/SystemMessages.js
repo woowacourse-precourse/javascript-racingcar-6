0a769933d3c63cd00e02b287b60360ec106f3f14
Lee sang Yeop <mma7710@naver.com>
2023-10-31 02:24:39 +0900

    Feat: 시스템 출력에 사용될 단어를 상수로 처리

---
+++ src/constants/SystemMessages.js
@@ -0,0 +1,8 @@
+const SYSTEM_MESSAGES = Object.freeze({
+	input_racingcar_name: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+	number_of_attempts: '시도할 횟수는 몇 회인가요?\n',
+	execution_result: '실행결과\n',
+	final_winner: '최종 우승자 : ',
+});
+
+export default SYSTEM_MESSAGES;
