a6fbc4432fbde7aff86c446ea72f13ec50d0c21d
dongsuseo <poopoo0515@naver.com>
2023-10-30 17:34:22 +0900

    feat: 자동차 이름 입력 시 예외 처리 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import getPlayInfo from "./getPlayInfo";
 
+const player = {};
 class App {
   async play() {
     const [PARTICIPANTS, TRIALS] = await getPlayInfo();
+    PARTICIPANTS.map((participant) => {
+      if (participant.length != 0) {
+        player[participant] = "";
+      }
+    });
   }
 }
 
