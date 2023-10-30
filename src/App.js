bd5ce9b2802d07299240b41057d4f9d8ff6e547a
JungHun98 <j7papa@naver.com>
2023-10-30 22:30:59 +0900

    feat: App Class createCar 메소드 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,19 @@
+import Car from './car/Car';
+
 class App {
+  constructor() {
+    this.cars = [];
+  }
+
+  createCar(nameString) {
+    const nameArr = nameString.split(',');
+
+    nameArr.forEach((name) => {
+      const newCar = new Car(name);
+      this.cars.push(newCar);
+    });
+  }
+
   async play() {}
 }
 
