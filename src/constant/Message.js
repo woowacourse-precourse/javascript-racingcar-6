44d097964fa0c763902bc374a719c555a071e057
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-30 18:38:54 +0900

    feat: [Message] 클래스 구현

---
+++ src/constant/Message.js
@@ -0,0 +1,14 @@
+export default class Message {
+  static RACE_START =
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n';
+
+  static TRY = '시도할 횟수는 몇 회인가요?\n';
+
+  static RUN_RESULT = '실행 결과';
+
+  static CAR_NAME_INPUT_ERROR = '[ERROR] 자동차 이름은 5자 이하만 가능합니다.';
+
+  static TRY_COUNT_INPUT_ERROR = '[ERROR] 숫자가 잘못된 형식입니다.';
+
+  static FINAL_WINNER = '최종 우승자 : ';
+}
