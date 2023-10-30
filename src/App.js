6e83f69c22e4cfb9b710d3ee39ae54776b3798d3
kwani6684 <jack0910@naver.com>
2023-10-30 09:36:36 +0900

    feat: exception #1.1
    when user input nothing

--- src/App.js
+++ src/App.js
@@ -34,6 +34,10 @@ class Car {
 class App {
   async getCarName() {
     let input = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    // 1. 아무것도 입력하지 않았을 경우
+    if (input === '') {
+      throw new Error('[ERROR] 입력값이 없습니다.');
+    }
     let nameList = input.split(',');
     let carArray = [];
     nameList.forEach((carName) => {
@@ -80,6 +84,6 @@ class App {
     this.printWinner(carArray);
   }
 }
-// const app = new App();
-// await app.play();
+const app = new App();
+await app.play();
 export default App;
