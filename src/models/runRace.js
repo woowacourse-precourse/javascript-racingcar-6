290575de1044739543c4c83dd6ac12ebfda419df
Youjiiin <yujinjang24@gmail.com>
2023-10-31 03:10:51 +0900

    fix: 진행 상황 출력오류 addDashRunRace 함수 삭제 및 runRace로 '-'삽입 기능 병합

--- src/models/runRace.js
+++ src/models/runRace.js
@@ -3,13 +3,12 @@ import { racingOutput } from "../views/OutputView";
 
 //입력받은 count에 따라 반복
 export const runByCount = (cars, count) => {
-  const carObject = runRace(cars);
   racingOutput("실행 결과");
   for (let round = 0; round < count; round++) {
+    const carObject = runRace(cars);
     racingOutput(carObject);
   }
 };
-
 
 //랜덤수에 따른 전진 상태 결정
 export const runRace = (cars) => {
@@ -18,17 +17,8 @@ export const runRace = (cars) => {
 
   //받은 랜덤수가 3이하면 0, 4이상 9이하면 1을 추가
   carNames.forEach((carName, index) => {
-    cars[carName] += randomNums[index] >= 4 ? 1 : 0;
+    cars[carName] += randomNums[index] >= 4 ? '-' : '';
   });
 
-  return addDashRunRace(cars);
-};
-
-//cars객체를 받아 value값에 따른 전진상태 '-'로 변경
-const addDashRunRace = (cars) => {
-  const carObject = {};
-  Object.entries(cars).forEach(([carName, distance]) => {
-    carObject[carName] = '-'.repeat(distance);
-  });
-  return carObject;
+  return cars;
 };
