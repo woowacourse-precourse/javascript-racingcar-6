8bbb2a821c7c0b92985b0752dd2609da20e12896
8leamnk <silkviewrk@gmail.com>
2023-10-30 20:20:10 +0900

    test: 전진 혹은 정지 기능에 대한 테스트 코드를 수정함
    수정 내용:
    - mock함수와는 별개로 테스트가 동작하는 현상이 있어서 테스트 코드의 오류가 있음을 깨달음
    - test.each를 test 안에서 forEach로 테스트 하도록 변경함

--- __tests__/DecisionTest.js
+++ __tests__/DecisionTest.js
@@ -1,21 +1,26 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import Decision from '../src/modules/Decision.js';
 
-const mockRandoms = (number) => {
+const mockRandoms = (numbers) => {
   MissionUtils.Random.pickNumberInRange = jest.fn();
-  MissionUtils.Random.pickNumberInRange.mockReturnValueOnce(number);
+  numbers.reduce(
+    (acc, number) => acc.mockReturnValueOnce(number),
+    MissionUtils.Random.pickNumberInRange,
+  );
 };
 
-test.each([3, 4, 7])('전진할지 정지할지 결정', (input) => {
+test('전진할지 정지할지 결정', () => {
   // given
-  const MOVING_FORWARD = 4;
-  const output = input >= MOVING_FORWARD;
+  const randoms = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const outputs = randoms.map((random) => random >= 4);
 
-  mockRandoms(input);
+  mockRandoms([...randoms]);
 
   // when
   const { moveForward } = Decision;
 
   // then
-  expect(moveForward()).toBe(output);
+  outputs.forEach((output) => {
+    expect(moveForward()).toBe(output);
+  });
 });
