7abdb7eb9bfd15cf397969a73ce0c352cbaa83bf
kimjinwoo <rlawlsdn177@naver.com>
2023-10-30 16:06:59 +0900

    feat($getCarNames): 사용자에게 자동차 이름 입력받는 메서드 구현
    사용자에게

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from '@woowacourse/mission-utils';
+import { CAR_NAME_INPUT_MESSAGE } from './Define';
 class App {
-  async play() {}
+    async play() {
+        const CAR_NAMES_INPUT = await this.getCarNames();
+    }
+
+    async getCarNames() {
+        const CAR_NAME_INPUT = await Console.readLineAsync(CAR_NAME_INPUT_MESSAGE);
+        return CAR_NAME_INPUT;
+    }
 }
 
 export default App;
