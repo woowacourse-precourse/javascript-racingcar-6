4a52ab4d4e06a92d90f344ae7e26ac203a540c5c
Jules <dali1999@naver.com>
2023-10-31 03:03:53 +0900

    refactor(Attempt): import 순서 재정렬 및 radix 매개변수 추가

--- src/models/Attempt.js
+++ src/models/Attempt.js
@@ -1,13 +1,14 @@
-import { ErrorMessage, PromptMessage } from "../views/Messages.js";
 import { Console } from "@woowacourse/mission-utils";
+import { ErrorMessage, PromptMessage } from "../views/Messages.js";
 
 class Attempt {
   async getRaceAttempt() {
     const getRaceAttempt = await Console.readLineAsync(
       PromptMessage.ENTER_ATTEMPT
     );
-    this.attemptCount = parseInt(getRaceAttempt);
+    this.attemptCount = parseInt(getRaceAttempt, 10);
 
+    // eslint-disable-next-line no-restricted-globals
     if (isNaN(getRaceAttempt) !== false) {
       throw new Error(ErrorMessage.INVALID_INPUT);
     }
