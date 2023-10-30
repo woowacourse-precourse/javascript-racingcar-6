139e1b88a835e2206861fd51991d027c58b87e24
yebin <yebind3308@gmail.com>
2023-10-30 23:40:40 +0900

    refactor : 입력한 이름과 횟수 유형성 함수로 구분처리

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App {
     Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     const carUserInput = await Console.readLineAsync("");
     const carName = carUserInput.split(",");
-    if (carName.some(name => name.trim() === "" || name.length <= 1 || name.length > 5)) {
+    if (this.nameValidity(carName)) {
       throw new Error("[ERROR] 이름이 잘못된 형식입니다.");
     }
     return carName; 
@@ -22,10 +22,21 @@ class App {
   async numberTimesInput() {
     Console.print("시도할 횟수는 몇 회인가요?");
     const numberUserInput = await Console.readLineAsync("");
-    if (!/^\d+$/.test(numberUserInput)) {
+    if (this.numberValidity(numberUserInput)) {
       throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
     };
     return numberUserInput; 
+  }
+
+  nameValidity(carName) {
+    const spaceError = carName.some(name => name.trim() === "");
+    const countError = carName.some(name => name.length > 5 || name.length < 1);
+    return spaceError || countError;
+  }
+
+  numberValidity(input) {
+    const validNumberError = !/^[0-9]+$/.test(input)
+    return validNumberError;
   }
 
   playGame(carNames, numberTimes) {
