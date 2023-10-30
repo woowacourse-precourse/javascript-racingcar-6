c4f7499b9d0522fc30979a69fa6083b53184ca27
dev-junseo <80705329+dev-junseo@users.noreply.github.com>
2023-10-30 23:54:18 +0900

    오류 수정1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-const { Random, Console } = require('@woowacourse/mission-utils');
+const { Random, Console } = require("@woowacourse/mission-utils");
 
 class App {
   async play() {
-
     async function getCarName() {
       let carNameArray = [];
-      let carNameString = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+      let carNameString = await Console.readLineAsync(
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+      );
       carNameArray = this.isValidName(carNameString);
     }
 
@@ -14,12 +15,12 @@ class App {
         throw new Error("[ERROR] 띄워쓰기를 하지 마세요.");
       } else if (userInput.length > 6) {
         throw new Error("[ERROR] 5글자 이하로 적어주세요.");
-      } 
+      }
 
       return userInput.split(",");
     }
 
-    console.log(getCarName());
+    Console.print(getCarName());
   }
 }
 
