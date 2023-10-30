89ebcf430058cfa12b26db349e745ce93778b6f8
bl0ckp1n9 <blockp1n9@gmail.com>
2023-10-30 20:30:42 +0900

    feat: 시도할 횟수 입력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import {Console, Random} from "@woowacourse/mission-utils";
+
+const MESSAGE = {
+  INPUT_TRY_COUNT: "시도할 횟수는 몇 회인가요?",
+}
 class App {
-  async play() {}
+  async play() {
+    const tryCount = await Console.readLineAsync(MESSAGE.INPUT_TRY_COUNT);
+  }
 }
 
 export default App;
