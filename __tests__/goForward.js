57452ed156a593f684b07417eff31e8222434d1e
kwani6684 <jack0910@naver.com>
2023-10-30 16:13:07 +0900

    fix: give name testCar

--- __tests__/goForward.js
+++ __tests__/goForward.js
@@ -3,7 +3,7 @@ const Car = require('../src/Car');
 
 describe('정지 or 전진 함수 goForWard ', () => {
   test('0 ~ 3 일때 정지 ', () => {
-    const testCar = new Car();
+    const testCar = new Car('testCar');
     const mockRandoms = (number) => {
       MissionUtils.Random.pickNumberInRange = jest.fn(() => number);
     };
@@ -12,7 +12,7 @@ describe('정지 or 전진 함수 goForWard ', () => {
     expect(testCar.distance).toBe('');
   });
   test(' 4 ~ 9 일때 전진', () => {
-    const testCar = new Car();
+    const testCar = new Car('testCar');
     const mockRandoms = (number) => {
       MissionUtils.Random.pickNumberInRange = jest.fn(() => number);
     };
