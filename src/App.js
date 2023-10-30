6941d764aa8e6ba3744dc9ce74d193c8d0963645
jomoo02 <jomootgrn88@naver.com>
2023-10-30 21:11:39 +0900

    refactor: App.js function play 수정
    자동차들의 이름과 시도할 횟수를 입력받는다
    입력된 값들을 race함수에 전달한다

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { inputCarNames, inputAttemptNumber } from './utils/inputUser';
+import race from './race';
+
 class App {
   // eslint-disable-next-line class-methods-use-this
-  async play() {}
+  async play() {
+    const carNames = await inputCarNames();
+    const attemptNumber = await inputAttemptNumber();
+
+    race(carNames, attemptNumber);
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import App from './App';
 
 const app = new App();
-app.play();
+await app.play();
