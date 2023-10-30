733540ce999603f5d989cad34579cb2f887c1a26
lucaseunchae <lucaskim.dev@gmail.com>
2023-10-30 23:01:20 +0900

    test: add play racing round test

---
+++ __tests__/Car/playRound.test.js
@@ -0,0 +1,34 @@
+import Car from "../../src/Car";
+import { getLogSpy, mockRandoms } from "../test-utils";
+
+describe("자동차 경주 한 라운드 진행 테스트", () => {
+  test("전진(0~9중 4이상)", async () => {
+    // given
+    const car = new Car("lucas");
+    const output = "lucas : -";
+    const logSpy = getLogSpy();
+
+    mockRandoms([4]);
+
+    // when
+    car.playRound();
+
+    // then
+    expect(logSpy).toHaveBeenCalledWith(output);
+  });
+
+  test("중지(0~9중 4미만)", async () => {
+    // given
+    const car = new Car("lucas");
+    const output = "lucas : ";
+    const logSpy = getLogSpy();
+
+    mockRandoms([0]);
+
+    // when
+    car.playRound();
+
+    // then
+    expect(logSpy).toHaveBeenCalledWith(output);
+  });
+});
