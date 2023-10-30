85129da316a073f5f065a048c8f0eb79036cbf22
hyzzzzy <shinhj4068@naver.com>
2023-10-30 17:23:57 +0900

    refactor(Car): private 키워드 적용 및 getter 정의

--- src/Car.js
+++ src/Car.js
@@ -1,12 +1,23 @@
 export default class Car {
+  #name;
+  #position;
+
   constructor(name) {
     /** @type string */
-    this.name = name;
+    this.#name = name;
     /** @type number */
-    this.position = 0; 
+    this.#position = 0; 
+  }
+
+  get name() {
+    return this.#name.trim();
+  }
+
+  get position() {
+    return this.#position;
   }
 
   forward() {
-    this.position += 1;
+    this.#position += 1;
   }
 }
