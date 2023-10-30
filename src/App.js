9da2b884e7ddc6e56a3eeef56630bc5ce126f25c
Myomyoring <narcissuscat@naver.com>
2023-10-30 21:00:39 +0900

    [feat] 이동 횟수 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { Console } from '@woowacourse/mission-utils';
 
 class App {
- validation(cars) {
+ validation(input) {
+  const cars = input.split(',');
   const carLength = cars.length < 2;
   const nameLength = cars.some((car) => car.length < 0 || car.length > 5);
   const gap = cars.some((car) => /^[가-힣a-zA-Z0-9]+$/.test(car));
@@ -10,12 +11,14 @@ class App {
   if (carLength || nameLength || gap || duplicate) {
    throw new Error('[ERROR]');
   }
+  return cars;
  }
+
  async play() {
   const input = await Console.readLineAsync('경주 할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
-  //,쉼표 기준으로 자르기
-  const cars = input.split(',');
-  this.validation(cars);
+  const cars = this.validation(input);
+
+  const times = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
  }
 }
 export default App;
