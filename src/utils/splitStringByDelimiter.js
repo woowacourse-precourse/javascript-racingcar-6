b56af45d1a17598aee8a200b558d2f721c625408
kang <kangju2000@naver.com>
2023-10-30 17:24:47 +0900

    :sparkles: splitStringByDelimiter 유틸 함수 추가

---
+++ src/utils/splitStringByDelimiter.js
@@ -0,0 +1,9 @@
+/**
+ * 구분자를 기준으로 문자열을 나누는 함수
+ * @param {string} inputString
+ * @param {string} delimiter
+ * @returns {string[]}
+ */
+const splitStringByDelimiter = (inputString, delimiter) => inputString.split(delimiter)
+
+export default splitStringByDelimiter
