de067e02620a28bcc73ed3118b98311d02357a2d
Jiwon Lee <giwon.yi339@gmail.com>
2023-10-30 23:00:56 +0900

    feat : 최종 우승자 출력

--- src/App.js
+++ src/App.js
@@ -5,51 +5,48 @@ class App {
     await this.displayResults();
   }
 
-  // async inputCarNames() {
-  //   const carNamesInput = await Console.readLineAsync(
-  //     "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
-  //   );
-
-  //   carNamesInput
-  //     .trim()
-  //     .split(",")
-  //     .map((carName) => {
-  //       if (carName.length > 5 || carName.length <= 0) {
-  //         throw new Error("[ERROR]");
-  //       }
-  //     });
-  // }
-
   async displayResults() {
-    // const RACECAR = await this.inputCarNames();
     const carName = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     );
-    const CARS = carName.trim().split(",");
+    const cars = carName.trim().split(",");
+    let carScores = new Array(cars.length).fill(0);
 
-    CARS.map((el) => {
+    cars.map((el) => {
       if (el.length > 5 || el.length <= 0) {
         throw new Error("[ERROR]");
       }
     });
     const ROTATION = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
 
+    let HIGHSCORE = 0;
+
     Console.print("실행 결과");
     for (let i = 0; i < ROTATION; i++) {
-      CARS.map((car) => {
-        const score = Random.pickNumberInRange(0, 9);
+      cars.map((car, index) => {
+        const SCORE = Random.pickNumberInRange(0, 9);
         //
-        if (score <= 4) {
-          // return Console.print(`${el} : ${result}`);
+        if (SCORE <= 4) {
           return Console.print(`${car} : -`);
-        } else if (score > 4) {
-          Console.print(`${car} : ${score}`);
-          // totalScore[el] = (totalScore[el] || 0) + result;
+        } else if (SCORE > 4) {
+          Console.print(`${car} : ${SCORE}`);
+          carScores[index] += SCORE;
         } else {
           throw new Error("[ERROR]");
         }
       });
     }
+
+    const highestScore = Math.max(...carScores);
+    const highestScoringCars = cars.filter(
+      (_, carIndex) => carScores[carIndex] === highestScore
+    );
+    Console.print(
+      "최종 우승자 : " +
+        (highestScoringCars.length === 1
+          ? highestScoringCars
+          : highestScoringCars.join(", "))
+    );
   }
 }
 
