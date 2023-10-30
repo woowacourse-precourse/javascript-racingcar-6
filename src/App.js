241aa4b26763d523d91e386bffb0b854b19b8ca2
shinheylynn <heylub@gmail.com>
2023-10-31 00:36:23 +0900

    Feat: getCarNamesFromUser 메소드 기능 추가
    입출력 기능 추가:
    - 사용자로부터 입력 받은 자동차 이름의 길이가 5를 초과하지는 않는지 확인하기 (5초과 시 error로 예외처리)

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App {
 
 	async play() {
 		await this.getCarNamesFromUser();
+		await MissionUtils.Console.print("시도할 횟수는 몇 회인가요?");
 	}
 
 	async getCarNamesFromUser() {
@@ -15,6 +16,15 @@ class App {
 			"경주할 자동차 이름을 입력하세요 (5자 이하): "
 		);
 		this.cars = userInputCar.split(",");
+
+		this.cars.map((carName) => {
+			if (carName.length > 5) {
+				MissionUtils.Console.print(String(this.cars));
+				throw new Error(
+					"[ERROR] 자동차 이름이 잘못된 형식입니다. (5자 이하만 가능)"
+				);
+			}
+		});
 
 		MissionUtils.Console.print(String(this.cars));
 
