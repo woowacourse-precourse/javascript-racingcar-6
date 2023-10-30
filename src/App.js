0c9eb4ead9bf047eda0ae4f47d2f668ac8dfd0aa
OhJeongHwan <wjdghks0316@naver.com>
2023-10-30 12:46:57 +0900

    docs .prettierrc 문서 추가, refactor 전진할지 여부를 car에서 직접 판단하도록 변경

--- src/App.js
+++ src/App.js
@@ -7,10 +7,9 @@ class App {
     this.number = 0;
   }
   async inputCars() {
-    MissionUtils.Console.print(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
-    );
+    MissionUtils.Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     const input = await MissionUtils.Console.readLineAsync("");
+
     const splitInputs = input.split(",");
     splitInputs.map((car) => {
       if (car.length <= 5) this.cars.push(new Car(car, 0));
@@ -20,6 +19,7 @@ class App {
   async inputNumber() {
     MissionUtils.Console.print("시도할 횟수는 몇 회인가요?");
     const input = await MissionUtils.Console.readLineAsync("");
+
     if (/\d/.test(input) && input > 0) this.number = parseInt(input);
     else throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
   }
@@ -27,25 +27,23 @@ class App {
     MissionUtils.Console.print("실행 결과");
     for (let i = 0; i < this.number; i++) {
       this.cars.map((car) => {
-        if (MissionUtils.Random.pickNumberInRange(0, 9) >= 4) {
-          car.go();
-        }
-        MissionUtils.Console.print(
-          `${car.getName()} : ${"-".repeat(car.getDistance())}`
-        );
+        car.go();
+        MissionUtils.Console.print(`${car.getName()} : ${"-".repeat(car.getDistance())}`);
       });
       MissionUtils.Console.print("");
     }
   }
   checkWinner() {
     const winners = [];
-    let highScore = 0;
+    let highScore = 0; //가장 높은 점수 저장
+
     this.cars.map((car) => {
-      if (car.getDistance() > highScore) highScore = car.getDistance();
+      if (car.getDistance() > highScore) highScore = car.getDistance(); // 더 높은 값이 있는 경우 갱신
     });
     this.cars.map((car) => {
-      if (car.getDistance() === highScore) winners.push(car.getName());
+      if (car.getDistance() === highScore) winners.push(car.getName()); //최고점과 점수가 같은 경우 우승자이다.
     });
+
     MissionUtils.Console.print(`최종 우승자 : ${winners}`);
   }
   async play() {
--- src/Car.js
+++ src/Car.js
@@ -1,10 +1,14 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class Car {
   constructor(name, distance) {
     this.name = name;
     this.distance = distance;
   }
   go() {
-    this.distance++;
+    if (MissionUtils.Random.pickNumberInRange(0, 9) >= 4) {
+      this.distance++;
+    }
   }
   getName() {
     return this.name;
