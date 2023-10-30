84fd7ace5f733ddde1f023d5e64bcffbbd640bf4
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-10-30 23:40:19 +0900

    feat:시도 횟수 관련 상수 추가
    * 입력 문구, 에러 문구 추가

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-import { errorMessage } from './utils';
+import { errorMessage } from './utils.js';
 
 export const ERROR_PREFIX = '[ERROR]';
 
@@ -7,6 +7,14 @@ export const MESSAGE = Object.freeze({
     INPUT: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
     ERROR: {
       IS_OVER_FIVE: errorMessage('자동차 하나의 문자열 길이는 5이하이어야 합니다'),
+    },
+  },
+  TRY: {
+    INPUT: '시도할 횟수는 몇 회인가요?',
+    ERROR: {
+      IS_NOT_INPUT: errorMessage('입력 값이 없습니다. 값을 입력해주세요'),
+      IS_MINUS: errorMessage('0 이상의 수를 입력해주세요.'),
+      IS_NOT_NUMBER: errorMessage('숫자가 아닌 문자열을 입력했습니다. 0 이상의 수를 입력해주세요.'),
     },
   },
 });
