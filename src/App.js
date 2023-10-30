d22a8e7040c6abfcdf7f73b1aedd2fc08efba219
yongZin-MAC <>
2023-10-30 13:44:43 +0900

    [자동차 경주 게임] 이벤트 정리 및 라이브러리 가져오기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { MissionUtils, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const START_GAME = async() => {
+      // 게임 시작 이벤트
+    }
+
+    const RACING_CAR_GAME = async(OBJECT_CAR) => {
+      // 레이싱 게임 이벤트(각 자동차의 정보를 가져오기)
+    }
+
+    const UPDATE_CAR_PROGRESS = (OBJECT_CAR) => {
+      // 각 자동차의 이름과 진행도(각 자동차의 정보를 가져오기)
+    }
+
+    await START_GAME();
+  }
 }
 
 export default App;
