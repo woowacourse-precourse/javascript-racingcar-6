84fdd4b7086d0321eae0cbdabc68e7415a2755e2
Seungil Jung <jungsi1217@naver.com>
2023-10-30 18:55:28 +0900

    feat(text): Create Message test

---
+++ src/Message.js
@@ -0,0 +1,12 @@
+const MESSAGE = {
+    INPUT_CAR_NAME: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+    INPUT_NUMBER: '시도할 횟수는 몇 회인가요?',
+    WINNER: '최종 우승자 :'
+}
+
+const ERROR = {
+    INPUT_LENGTH: "[ERROR] 경주할 자동차 이름은 5자 이하 또는 공백이 아니여야 합니다.",
+    NUM_INT: "[ERROR] 정수를 입력해주세요",
+}
+
+export {MESSAGE, ERROR}
