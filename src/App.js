8757cfacb9ce575cd702d4d613ecffc775130c8f
songhee Kim <soonghee2@gmail.com>
2023-10-30 10:54:16 +0900

    [style] eslint 적용

--- src/App.js
+++ src/App.js
@@ -1,62 +1,62 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
-  async play() {
-    const MAX_CAR_NAME_LENGTH = 5;
+	async play() {
+		const MAX_CAR_NAME_LENGTH = 5;
 
-    MissionUtils.Console.print(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
-    );
-    const input = await MissionUtils.Console.readLineAsync();
-    const runners = input.split(",");
-    runners.map((runner) => {
-      if (runner.length > MAX_CAR_NAME_LENGTH) {
-        throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
-      }
-    });
+		MissionUtils.Console.print(
+			"경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+		);
+		const input = await MissionUtils.Console.readLineAsync();
+		const runners = input.split(",");
+		runners.map((runner) => {
+			if (runner.length > MAX_CAR_NAME_LENGTH) {
+				throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+			}
+		});
 
-    MissionUtils.Console.print("시도할 횟수는 몇 회인가요?");
-    const try_number = await MissionUtils.Console.readLineAsync();
-    if (isNaN(try_number)) {
-      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
-    }
+		MissionUtils.Console.print("시도할 횟수는 몇 회인가요?");
+		const try_number = await MissionUtils.Console.readLineAsync();
+		if (isNaN(try_number)) {
+			throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+		}
 
-    //경기 시작, 경기 상황 출력
-    let scores = Array(5).fill(0);
-    for (let round = 0; round < try_number; round++) {
-      let race_str = [];
-      scores = go_randomly(scores);
-      runners.map((runner, index) => {
-        race_str.push(`${runner} : ${"-".repeat(scores[index])}`);
-      });
-      MissionUtils.Console.print(race_str.join("\n"));
-    }
+		//경기 시작, 경기 상황 출력
+		let scores = Array(5).fill(0);
+		for (let round = 0; round < try_number; round++) {
+			let race_str = [];
+			scores = go_randomly(scores);
+			runners.map((runner, index) => {
+				race_str.push(`${runner} : ${"-".repeat(scores[index])}`);
+			});
+			MissionUtils.Console.print(race_str.join("\n"));
+		}
 
-    print_winners();
+		print_winners();
 
-    function go_randomly(scores) {
-      let random_number;
-      for (let i = 0; i < scores.length; i++) {
-        random_number = MissionUtils.Random.pickNumberInRange(0, 9);
-        // random_number=Math.floor(Math.random() * 10);
-        if (random_number > 3) {
-          scores[i] = scores[i] + 1;
-        }
-      }
-      return scores;
-    }
+		function go_randomly(scores) {
+			let random_number;
+			for (let i = 0; i < scores.length; i++) {
+				random_number = MissionUtils.Random.pickNumberInRange(0, 9);
+				// random_number=Math.floor(Math.random() * 10);
+				if (random_number > 3) {
+					scores[i] = scores[i] + 1;
+				}
+			}
+			return scores;
+		}
 
-    function print_winners() {
-      const max = Math.max(...scores);
-      let winners = [];
-      scores.map((score, index) => {
-        if (score >= max) {
-          winners.push(runners[index]);
-        }
-      });
-      MissionUtils.Console.print(`최종 우승자 : ${winners.join()}`);
-    }
-  }
+		function print_winners() {
+			const max = Math.max(...scores);
+			let winners = [];
+			scores.map((score, index) => {
+				if (score >= max) {
+					winners.push(runners[index]);
+				}
+			});
+			MissionUtils.Console.print(`최종 우승자 : ${winners.join()}`);
+		}
+	}
 }
 
 export default App;
