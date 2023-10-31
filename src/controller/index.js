1358f2a31169382a08ab54f6d1165928fd4a1bef
ryan-dia <cjfdnjs158@naver.com>
2023-11-01 00:41:43 +0900

    feat(controller): 메시지 출력 기능 추가

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,10 +1,12 @@
 import { SYSTEM } from '../constants/System.js';
 import InputView from '../view/InputView.js';
+import OutputView from '../view/OutputView.js';
 
 class RacingController {
   async startGame() {
     const racingVehicleName = await InputView.readRacingVehicleName(SYSTEM.car);
     const racingCount = await InputView.readRacingCount();
+    OutputView.print('\n실행 결과');
   }
 }
 
