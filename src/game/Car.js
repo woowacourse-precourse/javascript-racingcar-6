cf941e7edda7da5ea8c79989ba994702aada912f
unknown <pih9134@naver.com>
2023-11-01 05:38:09 +0900

    test: Car 클래스 생성

---
+++ src/game/Car.js
@@ -0,0 +1,25 @@
+import { Random } from '@woowacourse/mission-utils';
+import { MOVE_NUMBER } from '../utils/constants';
+
+class Car {
+  constructor(name) {
+    this.name = name;
+    this.position = 0;
+  }
+
+  move() {
+    if (Random.pickNumberInRange(0, 9) >= MOVE_NUMBER) {
+      this.position += 1;
+    }
+  }
+
+  getPosition() {
+    return this.position;
+  }
+
+  getName() {
+    return this.name;
+  }
+}
+
+export default Car;
