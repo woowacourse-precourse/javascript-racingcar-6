abff97333f78c04a560272f5128431529adf7f2b
MacBook Air <macbookair@MacBookui-MacBookAir.local>
2023-10-31 18:45:04 +0900

    Feat: Add findError, moveArr 자동차 이름이 5자 초과면 에러발생, 이동횟수 입력받기 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import { Console } from "@woowacourse/mission-utils";
 import * as MissionUtils from "@woowacourse/mission-utils";
 
+function findError(){
+  try{
+    throw new Error("[ERROR] 자동차 이름은 5자를 초과할 수 없습니다.");
+  }catch(e){
+    console.log(e.message);
+  }
+}
+
 class App {
   async play() {
     const userInput = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
     const carName = userInput.split(",");
     
-    console.log(carName);
+    for(let i =0;i<carName.length;i++){
+      if(carName[i].length>5){
+        findError();
+        return;
+      }
+    }
+
+    const userNum = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+    var move = new Array();
+
+    while(move.length < carName.length){
+      move.push(0);
+    } // 배열 초기화
   }
 }
 
