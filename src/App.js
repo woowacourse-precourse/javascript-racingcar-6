c1b7e6fbe8576e4b034dfa6655c60a842043cd7d
hyewon <hyewon.parkhw@gmail.com>
2023-10-31 20:11:51 +0900

    feat: 자동차 이동 기능 추가

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ class App {
     const carNames = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
     const moves = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n")
   }
+
+  moveCar(){
+    const result = MissionUtils.Random.pickNumberInRange(0, 9);
+    if (result>=4) return "-"
+    else return ""
+  }
 }
 
 export default App;
