9980869500622d8787414c73b8ee958e2cf9b0e3
pongpongie <arevolvera@gmail.com>
2023-11-01 04:40:51 +0900

    refactor(views): create RaceView.js
    deleted InputCarNameView, InputTrialNumberView and RoundResultView
    refactored into RaceView

--- src/views/RoundResultView.js
+++
@@ -1,8 +0,0 @@
-import { Console } from "@woowacourse/mission-utils";
-import { MESSAGES } from "../constants/Messages";
-
-const RoundResultView = () => {
-  Console.print(MESSAGES.RESULT);
-};
-
-export default RoundResultView;
