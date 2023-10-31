9f003ffdda8d1adf61037b58cbd4582fe0070212
Goonco <s07019@naver.com>
2023-10-31 18:45:09 +0900

    :white_check_mark: class Car - printPosition 테스트 생성 및 통과

--- __tests__/CarTest.js
+++ __tests__/CarTest.js
@@ -1,6 +1,20 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import Car from '../src/Car.js';
 
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce(
+    (acc, number) => acc.mockReturnValueOnce(number),
+    MissionUtils.Random.pickNumberInRange,
+  );
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
+
 describe('class Car test', () => {
   const car = new Car('Goonco');
 
@@ -10,19 +24,19 @@ describe('class Car test', () => {
     const randoms = [STOP, STOP, MOVING_FORWARD, STOP, MOVING_FORWARD];
     const postionResults = [0, 0, 1, 1, 2];
 
-    const mockRandoms = (numbers) => {
-      MissionUtils.Random.pickNumberInRange = jest.fn();
-      numbers.reduce(
-        (acc, number) => acc.mockReturnValueOnce(number),
-        MissionUtils.Random.pickNumberInRange,
-      );
-    };
-
-    mockRandoms([...randoms]);
+    mockRandoms(randoms);
 
     randoms.forEach((mockedRandom, idx) => {
-      car.tryMove(mockedRandom);
+      car.tryMove();
       expect(car.getPosition()).toBe(postionResults[idx]);
     });
+  });
+
+  test('[Method Test] printPosition', () => {
+    const printResult = 'Goonco : --';
+    const logSpy = getLogSpy();
+
+    car.printPosition();
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(printResult));
   });
 });
