92cea956462ed65e3f31c794e44a21c53acd1836
bini59 <myuni0234@gmail.com>
2023-10-31 19:48:40 +0900

    feat(input) : 시도횟수 입력 오류 처리 테스트 추가
    모든 경우에 대한 테스트 추가 후, 정상 입력에 대한 테스트 추가
    1. 문자열입력
    2. 공백 입력
    3. 0 입력
    4. 음수 입력
    5. 소수 입력
    6. 정상 입력

---
+++ __tests__/TryInputTest.js
@@ -0,0 +1,68 @@
+import App from "../src/App.js";
+import tryMessage from "../src/messages/Try.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const mockQuestions = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  });
+};
+
+describe("자동차 경주 게임 시도 횟수 입력 테스트", () => {
+  test("문자열 입력", async () => {
+    const input = ["pobi,java", "string"];
+    mockQuestions(input);
+
+    const app = new App();
+
+    await expect(app.play()).rejects.toThrow(tryMessage.ERROR_INPUT_TRY);
+  });
+
+  test("공백 입력", async () => {
+    const input = ["pobi,java", " "];
+    mockQuestions(input);
+
+    const app = new App();
+
+    await expect(app.play()).rejects.toThrow(tryMessage.ERROR_INPUT_TRY);
+  });
+
+  test("0 입력", async () => {
+    const input = ["pobi,java", "0"];
+    mockQuestions(input);
+
+    const app = new App();
+
+    await expect(app.play()).rejects.toThrow(tryMessage.ERROR_INPUT_TRY);
+  });
+
+  test("음수 입력", async () => {
+    const input = ["pobi,java", "-1"];
+    mockQuestions(input);
+
+    const app = new App();
+
+    await expect(app.play()).rejects.toThrow(tryMessage.ERROR_INPUT_TRY);
+  });
+
+  test("소수 입력", async () => {
+    const input = ["pobi,java", "1.5"];
+    mockQuestions(input);
+
+    const app = new App();
+
+    await expect(app.play()).rejects.toThrow(tryMessage.ERROR_INPUT_TRY);
+  })
+
+  test("정상 입력", async () => {
+    const input = ["pobi,java", "5"];
+    mockQuestions(input);
+
+    const app = new App();
+
+    await expect(app.play()).resolves.not.toThrow();
+  });
+});
