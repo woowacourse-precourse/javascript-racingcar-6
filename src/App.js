6c34fe9af31278fe9251762b97ca1ca45cbc4f26
serin-B <bangserin980302@gmail.com>
2023-11-01 03:10:25 +0900

    feat(App): 자동차 경주 게임을 시작하는 play 함수 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import RacingcarGame from "./RacingcarGame.js";
+
 class App {
-  async play() {}
+  async play() {
+    try {
+      const game = new RacingcarGame();
+      await game.start();
+    } catch (error) {
+      throw error;
+    }
+  }
 }
 
 export default App;
