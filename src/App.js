ba40d7e8e3ca2590c6580fd4264b9679a99de22a
jadewisemann <jadewisemann@gmail.com>
2023-10-31 16:46:53 +0900

    feat: getUserInput 함수 추가, 사용자의 입력을 받아오는 함수

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console } from "@woowacourse/mission-utils";
+import message from "./util/Message.js"
+
+
 class App {
-  async play() {}
+  async play() {
+    const userInput = await this.getUserInput();
+  }
+  
+  async getUserInput() {
+    const input = await Console.readLineAsync(message.starting)
+    return input
+  }
 }
+
+
+
 
 export default App;
---
+++ src/util/Message.js
@@ -0,0 +1,5 @@
+const message = {
+  starting: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+}
+
+export default message
