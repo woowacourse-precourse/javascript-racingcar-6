69cae6054a057635081e4b588bbf3f4331e42ac7
김미르 <kimmireu@gimmileuui-iMac.local>
2023-10-31 15:08:34 +0900

    test : 테스트 코드 수정

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -74,26 +74,4 @@ describe("자동차 경주 게임", () => {
     // then
     await expect(app.play()).rejects.toThrow("[ERROR]");
   });
-
-  test("최종 우승자 테스트", async () => {
-    // given
-    const MOVING_FORWARD = 4;
-    const STOP = 3;
-    const inputs = ["pobi,woni", "1"];
-    const outputs = ["pobi : -", "최종 우승자 : pobi"];
-    const randoms = [MOVING_FORWARD, STOP];
-    const logSpy = getLogSpy();
-
-    mockQuestions(inputs);
-    mockRandoms([...randoms]);
-
-    // when
-    const app = new App();
-    await app.play();
-
-    // then
-    outputs.forEach((output) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
-    });
-  });
 });
