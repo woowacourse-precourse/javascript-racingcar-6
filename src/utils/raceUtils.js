7e62dcf27902bcc5b2265af527c9ebb61b6f78f4
77JY <kwn03111@naver.com>
2023-11-01 00:52:40 +0900

    FEAT: formatRaceResult 및 outputRaceResults 메서드 추가

--- src/utils/raceUtils.js
+++ src/utils/raceUtils.js
@@ -1,19 +1,28 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
 import { GameSetting } from "../constants/gameConstants.js";
 
 export class RaceUtils {
   static generateRaceResults(carCount) {
-    let results = []
-    for (let i = 0; i < carCount; i++){
+    let results = [];
+    for (let i = 0; i < carCount; i++) {
       let number = MissionUtils.Random.pickNumberInRange(0, 9);
-      results.push(GameSetting.minValue < number)
+      results.push(GameSetting.raceSuccessLimit < number);
     }
-    return results
+    return results;
   }
 
   static addRaceStep(RaceResults, Cars) {
-    for (let i = 0; i < RaceResults.length; i++){
-      if (RaceResults[i]) Cars[i].trackStatus += "-"
+    for (let i = 0; i < RaceResults.length; i++) {
+      if (RaceResults[i]) Cars[i].trackStatus += "-";
     }
+  }
+  static formatRaceResult(Car) {
+    return `${Car.name} : ${Car.trackStatus}`;
+  }
+
+  static outputRaceResults(Cars) {
+    Cars.forEach(Car => {
+      Console.print(`${Car.name} : ${Car.trackStatus}\n`)
+    });
   }
 }
