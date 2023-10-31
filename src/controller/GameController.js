8eb3f23ac64842c88984b276611d4743140fadb5
mieum12 <qpdlqltb1215@gmail.com>
2023-10-31 13:52:08 +0900

    fix: game controller 로직 수정

--- src/controller/GameController.js
+++ src/controller/GameController.js
@@ -13,17 +13,17 @@ export class GameController {
 
   async start() {
     // 자동차 이름과 시도 횟수 받아오기
-    const cars = await this.#inputView.getCarNameFromInput();
+    const carsList = await this.#inputView.getCarNameFromInput();
     const attempt = await this.#inputView.getAttemptFromInput();
     // 결과 전달
-    const winners = this.race(cars, attempt);
+    const winners = this.race(carsList, attempt);
     this.#outputView.printWinner(winners);
   }
 
   // 시도 횟수만큼 반복
   async race() {
     for (let i = 0; i < this.#attempt; i++) {
-      const result = cars.move();
+      const result = carsList.moveGenerator();
       this.#outputView.printGameResult(result);
     }
   }
--- src/domian/Car.js
+++ src/domian/Car.js
@@ -9,14 +9,12 @@ class Car {
     this.progress = 0;
   }
 
-  // 이동 여부 판단 후 이동 결과 출력
+  // 이동 여부 판단 후 이동 결과 반환
   MoveGenerator() {
     const randomNumber = Random.pickNumberInRange(1, 9);
-
     if (randomNumber > 4) {
       this.progress++;
     }
-
     return progress;
   }
 }
