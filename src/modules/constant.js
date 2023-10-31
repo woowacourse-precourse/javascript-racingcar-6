abdc07ac9f3b8a2e9caa70874dd6dd2d372a8f33
hs <tlsgustn93@naver.com>
2023-10-31 12:19:49 +0900

    feat: 상수에 대한 모듈

---
+++ src/modules/constant.js
@@ -0,0 +1,14 @@
+const GAME_MESSAGE = {
+  GAME_START:
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
+  TRY_INPUT: "시도할 횟수는 몇 회인가요?\n",
+  GAME_RESULT: "\n실행 결과",
+  DASH: "-",
+  WINNER: "최종 우승자 : ",
+};
+
+const GAME_ERROR = {
+  ERROR_NAME: "[ERROR] 이름의 형식이 잘못되었습니다.",
+  ERROR_NUMBER: "[ERROR] 숫자가 잘못된 형식입니다.",
+};
+export { GAME_MESSAGE, GAME_ERROR };
