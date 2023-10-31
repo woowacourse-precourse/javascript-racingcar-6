9e26c4685cb6bae76a91c2071d27b8bec17c5ab3
이황제 <happy_kaiser@hotmail.com>
2023-10-31 15:03:55 +0900

    feat: 랜덤 숫자 생성, 입력값 4이상 판단기능

---
+++ src/domain/controller.js
@@ -0,0 +1,10 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+export const randomAndJudge = async (trynumber) => {
+  const RANDOM_NUMBER_ARRAY = [];
+  for (let i = 0; i < trynumber; i++) {
+    const NUMBER = await MissionUtils.Random.pickNumber.InRange(0, 9);
+    NUMBER >= 4 ? RANDOM_NUMBER_ARRAY.push(NUMER) : RANDOM_NUMBER_ARRAY.push(0);
+  }
+  return RANDOM_NUMBER_ARRAY;
+};
