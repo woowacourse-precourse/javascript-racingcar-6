b3ab91f1af782a007f536fbbf5ebc16a30465f57
bini59 <myuni0234@gmail.com>
2023-10-31 22:45:16 +0900

    feat : 모델 생성 후 자동차 추가
    - 자동차들을 관리할 수 있는 Model class 제작.
    - 게임 시작시 받았던 자동차들을 Model에 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { Console } from '@woowacourse/mission-utils';
 import { getCarName, getTryCount } from './View.js';
+import Model from './Model/Model.js';
 
 class App {
+  constructor() {
+    this.model = new Model();
+  }
+
   async play() {
-    await getCarName();
+    const cars = await getCarName();
+    this.model.addCar(cars);
     await getTryCount();
 
     return this;
---
+++ src/Model/Model.js
@@ -0,0 +1,29 @@
+import Car from './Car.js';
+
+class Model {
+  constructor() {
+    this.cars = [];
+  }
+
+  addCar(names) {
+    names.forEach((name) => {
+      this.cars.push(new Car(name));
+    });
+
+    return this;
+  }
+
+  goCars() {
+    this.cars.forEach((car) => {
+      car.move();
+    });
+
+    return this;
+  }
+
+  get cars() {
+    return this.cars;
+  }
+}
+
+export default Model;
