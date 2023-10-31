4ea2f249632ae78078191e301765ff9123da4e7b
ccconac <qwerpoiu91@naver.com>
2023-11-01 02:59:28 +0900

    :sparkles: feat: 게임 우승자 출력 구현

--- src/App.js
+++ src/App.js
@@ -90,10 +90,30 @@ class App {
     }
   };
 
+  getRaceWinner = () => {
+    let max = 0;
+    let winners = [];
+
+    this.forward.forEach((advance) => {
+      if (advance.length > max) {
+        max = advance.length;
+      }
+    });
+
+    this.forward.forEach((advance, key) => {
+      if (max === advance.length) {
+        winners.push(key);
+      }
+    });
+
+    Console.print(`\n최종 우승자: ${winners.join(', ')}`);
+  };
+
   async play() {
     await this.getCarName();
     await this.getAttempts();
     this.getRaceResult();
+    this.getRaceWinner();
   }
 }
 
