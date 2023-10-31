09f182bc662b17194550886da097e048e90dc4bd
yonainthefish <yonaisgood@gmail.com>
2023-10-31 13:22:11 +0900

    feat: 입력 받은 자동차 이름 객체에 저장

--- src/controller/GameController.js
+++ src/controller/GameController.js
@@ -1,9 +1,11 @@
 import { getCarNames, getRoundCount } from '../view/inputView.js';
 import { InputValid, RoundValid } from '../models/InputValid.js';
+import CarsList from '../models/CarList.js';
 
 const startGame = async () => {
   let inputName = await getCarNames();
   InputValid(inputName);
+  CarsList(inputName);
 
   let round = await getRoundCount();
   RoundValid(round);
