bceddf3be8d61f3924137afa3f4414e484c2bcd5
Hyurim <kyo2301@naver.com>
2023-11-01 01:02:21 +0900

    fix(App.js): App.js 내 play 메서드 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import CarNameInput from "./mvc/view/input/CarNameInput.js";
+import { Console } from "@woowacourse/mission-utils";
+import GAME_MESSAGE from "./mvc/constants/gameMessage.js";
+import NameValid from "./mvc/controller/valid/NameValid.js";
+import TryNumberInput from "./mvc/view/input/TryNumberInput.js";
+import resultOutput from "./mvc/view/output/ResultOutput.js";
 
 class App {
+  constructor() {
+    this.nameValid = new NameValid();
+    this.tryNumberInput = new TryNumberInput();
+  }
   async play() {
-    const carnameinput = new CarNameInput();
-    await carnameinput.carName();
+    let nameSplit;
+    try {
+      const userName = await Console.readLineAsync(`${GAME_MESSAGE.carName}`);
+      nameSplit = this.nameValid.nameIsValid(userName);
+    } catch (error) {
+      throw error;
+    }
+    const result = await this.tryNumberInput.tryNumber(nameSplit);
+    resultOutput.winnerPrint(result);
     return;
   }
 }
 
 export default App;
+
