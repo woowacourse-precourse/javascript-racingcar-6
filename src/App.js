dd29846fec3ff139e2aed8c091dec9f082989b6f
geunwoo <olo90632951@gmail.com>
2023-11-01 00:29:30 +0900

    feat: 시도 횟수만큼 전진할지 멈출지 판단하는 기능

--- src/App.js
+++ src/App.js
@@ -8,25 +8,41 @@ class App {
   async play() {
     await this.initializeCars();
     const rounds = await this.getRounds();
+    this.playRounds(rounds);
   }
   
   async initializeCars() {
     const carNamesInput = await Console.readLineAsync('경주할 자동차이름을입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
-    this.cars = this.parseCarNamesInput(carNamesInput);
+    const carNames = this.parseCarNamesInput(carNamesInput);
+    this.cars = this.createCars(carNames);
   }
 
   parseCarNamesInput(input) {
     return input.trim().split(',').map((name) => name.trim());
   }
 
+  createCars(names) {
+    return names.map((name) => ({ name: name, moveCount: 0 }));
+  }
+
   async getRounds() {
     return await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');  
+  }
+
+  playRounds(rounds) {
+    for (let i = 0; i < rounds; i++) {
+      this.cars.forEach((car) => {
+        if (this.isMovable()) {
+          car.moveCount += 1;
+        }
+      });
+    }
   }
 
   isMovable() {
     const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
     return randomNumber >= 4;
-  }
+  } 
 }
 
 export default App;
