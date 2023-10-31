aae46f44a2165de864fa4583045c0c044a871f99
손진영 <sm099@naver.com>
2023-10-31 19:11:38 +0900

    docs: model, service layer에 대해 jsDoc을 추가 한다.
    1. model layer인 RacingCar, RacingGame, RacingWinnerCalculator에 대해 부족한 타입 및 설명을 추가한다.
    
    2. service layer인 RacingGameService에 대해 부족한 타입 및 설명을 제공한다.
    
    3. jsDoc 모듈 내 필요한 custom type 들을 추가한다.

--- src/models/RacingGame.js
+++ src/models/RacingGame.js
@@ -2,11 +2,24 @@ import { RANDOM_NUMBER_RANGE } from '../constants/random.js';
 import { pickRandomNumberInRange } from '../utils/random.js';
 import RacingCar from './RacingCar.js';
 
+/**
+ * @class RacingGame
+ * @classdesc '자동차 경주 결과 생성'의 책임을 수행
+ */
 class RacingGame {
+  /**
+   * @type {string[]}
+   */
   #carNames;
 
+  /**
+   * @type {number}
+   */
   #moveCount;
 
+  /**
+   * @type {import('../utils/jsDoc.js').RacingStatus}
+   */
   #racingStatus;
 
   constructor(carNames, moveCount) {
@@ -15,14 +28,29 @@ class RacingGame {
     this.#racingStatus = this.#initializeRacingStatus();
   }
 
+  /**
+   * 매개변수가 2개 이상인 정적 팩토리 메서드
+   * @static
+   * @public
+   * @param {string[]} carNames - 자동차 이름들
+   * @param {number} moveCount - 이동 횟수
+   * @returns {RacingGame} RacingGame 인스턴스
+   */
   static of(carNames, moveCount) {
     return new RacingGame(carNames, moveCount);
   }
 
+  /**
+   * @private
+   * @returns {import('../utils/jsDoc.js').RacingStatus} 초기 레이싱 진행 상태(ex 1lap)
+   */
   #initializeRacingStatus() {
     return this.#carNames.map((carName) => ({ carName, position: 0 }));
   }
 
+  /**
+   * @private
+   */
   #updateRacingStatus() {
     const { minNumber, maxNumber } = RANDOM_NUMBER_RANGE;
     this.#racingStatus = this.#racingStatus.map((currentRacingCarInfo) =>
@@ -30,6 +58,10 @@ class RacingGame {
     );
   }
 
+  /**
+   * @public
+   * @returns {import('../utils/jsDoc.js').RacingResult} moveCount만큼 진행된 레이싱의 결과
+   */
   runRace() {
     return Array.from({ length: this.#moveCount }, () => {
       this.#updateRacingStatus();
--- src/utils/jsDoc.js
+++ src/utils/jsDoc.js
@@ -34,4 +34,24 @@
  * @property {MoveCountValidationType} checkNaturalNumber - 이동 횟수가 자연수인지 확인하기 위한 객체
  */
 
+/**
+ * @typedef {object} RacingCarInfo
+ * @property {string} carName - 자동차 이름
+ * @property {number} position - 자동차가 이동한 위치
+ */
+
+/**
+ * @typedef {RacingCarInfo[]} RacingStatus
+ */
+
+/**
+ * @typedef {RacingStatus[]} RacingResult
+ */
+
+/**
+ * @typedef {object} RacingGameResult
+ * @property {RacingResult} racingResult - 이동 횟수 만큼 진행한 자동차들의 레이싱 결과
+ * @property {string} racingWinners - 레이싱 게임의 우승자들의 이름
+ */
+
 export {};
