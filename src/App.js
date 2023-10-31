ac3549e221ddc3d421e4aea2787a530b85cba9f8
lucaseunchae <lucaskim.dev@gmail.com>
2023-11-01 00:31:40 +0900

    feat: implement racingcar game

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Car from "./Car.js";
+import { inputCarNames, inputMovementCount } from "./helpers/index.js";
+
 class App {
-  async play() {}
+  async play() {
+    const carNamesInput = await inputCarNames();
+    const movementCountInput = await inputMovementCount();
+
+    const cars = Car.initializeCars(carNamesInput.split(","));
+
+    Car.playGame(cars, movementCountInput);
+  }
 }
 
 export default App;
