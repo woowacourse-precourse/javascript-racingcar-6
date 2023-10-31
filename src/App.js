d5dbb101d810035267909b15140db8ca14113284
Parksangwoo <psu8430@gmail.com>
2023-10-31 17:47:17 +0900

    feat: 레이싱 결과 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import RacingCar from './Model/RacingCar.js';
 import IOManager from './Utils/IOManager.js';
 import Validator from './Utils/Validator.js';
+import Computer from './Utils/Computer.js';
+import { Console } from '@woowacourse/mission-utils';
+import { CONSTANTS } from './constants.js';
 
 class App {
 	constructor() {
@@ -8,14 +11,53 @@ class App {
 	}
 
 	enrollRacingCar = (racingCars) => {
-		const carArray = racingCars.split(',');
+		const carNameArray = racingCars.split(',');
 
-		carArray.forEach((name) => {
+		carNameArray.forEach((name) => {
 			const newCar = new RacingCar(name);
 
-			carArray.push(newCar);
+			this.carArray.push(newCar);
 		});
 	};
+
+	moveCar() {
+		this.carArray.forEach((car) => {
+			const randomNumber = Computer.getRandomNumber();
+
+			if (randomNumber >= CONSTANTS.FORWARD_STANDARD_VALUE) car.go();
+
+			car.showCarPosition();
+		});
+	}
+
+	printProcess(playNumber) {
+		for (let count = 0; count < playNumber; count++) {
+			this.moveCar();
+			Console.print('');
+		}
+	}
+
+	printWinner() {
+		let winnerArray = [];
+
+		this.carArray.forEach((car) => {
+			if (winnerArray.length === 0 || winnerArray[0].distance === car.distance) {
+				winnerArray.push(car);
+			} else if (winnerArray[0].distance < car.distance) {
+				winnerArray = [car];
+			}
+		});
+
+		const winnerText = winnerArray.map((car) => car.name).join(', ');
+
+		Console.print(`최종 우승자 : ${winnerText}`);
+	}
+
+	printResult(playNumber) {
+		Console.print('\n실행 결과');
+		this.printProcess(playNumber);
+		this.printWinner();
+	}
 
 	async play() {
 		const racingCars = await IOManager.getUserInput(
@@ -25,10 +67,14 @@ class App {
 
 		this.enrollRacingCar(racingCars);
 
-		const playNumber = await IOManager.getUserInput(
-			'시도할 횟수는 몇 회인가요?\n',
-			Validator.checkAllCarNameValidate,
+		const playNumber = parseInt(
+			await IOManager.getUserInput(
+				'시도할 횟수는 몇 회인가요?\n',
+				Validator.checkAllCarNameValidate,
+			),
 		);
+
+		this.printResult(playNumber);
 	}
 }
 
