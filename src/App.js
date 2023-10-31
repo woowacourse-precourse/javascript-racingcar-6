054ead578026fab86ab5a957f99ab10fb8c3861b
nari <naringst@gmail.com>
2023-10-31 12:47:57 +0900

    feat : 각 자동차 별 무작위 수 구하기

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App {
     const carNames = carNameUserInput.split(",");
 
     //5자 이하인지 확인하기
-    const isNameUpper5 = carNames.every((carName) => carName.length() > 5);
+    const isNameUpper5 = carNames.every((carName) => carName.length > 5);
 
     //5자 이상이면 예외 처리
 
@@ -18,7 +18,29 @@ class App {
     const tryNumber = await MissionUtils.Console.readLineAsync(
       "시도할 횟수는 몇 회인가요?"
     );
+
+    //초기 출력
+    const carScoreObj = {};
+    carNames.forEach((car) => {
+      carScoreObj[car] = "";
+    });
+
+    //각 자동차 별 무작위 수 구하기
+    for (let i = 0; i < tryNumber; i++) {
+      const movingCars = [];
+
+      carNames.forEach((car) => {
+        const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
+
+        if (randomNumber >= 4) {
+          movingCars.push(car);
+        }
+      });
+    }
   }
 }
+
+const app = new App();
+app.play();
 
 export default App;
