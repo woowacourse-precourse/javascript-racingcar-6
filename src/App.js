80a52dedc7d5a944deee3c20ed86ee23fca67efa
조경은 <kavvy99@naver.com>
2023-10-31 16:04:41 +0900

    feat: 쉼표(,)를 기준으로 한 자동차 이름의 배열 생성 및 이름의 길이에 대한 예외처리

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-class App {
-  async play() {
-    console.log("CddddLI 애플리케이션 시작");
+// class App {
+//   async play() {
+//     console.log("애플리케이션 시작");
 
     
-    console.log(MissionUtils.Random.pickNumberInRange(0, 9))
+//     console.log(MissionUtils.Random.pickNumberInRange(0, 9))
 
+//   }
+// }
+
+// export default App;
+
+// 자동차 이름 예외 처리
+const carNameInput = document.querySelector('#carName');
+
+function carNameCheckEvent() {
+  const carName = carNameInput.value.split(",");
+  console.log(carName);
+
+  // 유저가 입력한 자동차 이름이 다섯 자 이하가 아니라면?
+  for (i=0; i<carName.length; i++) {
+    if (carName[i].length > 5) {
+      alert("[ERROR] 자동자의 이름이 너무 깁니다.");
+      console.log("[ERROR] 자동자의 이름이 너무 깁니다.");
+    }
   }
-}
 
-export default App;
+}
