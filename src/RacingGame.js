e07f52484bcea24d553c7b61e6123e30b2cf6c1a
Sunghyun627 <kksscc4@gmail.com>
2023-10-31 22:36:49 +0900

    feat: 자동차 이름 리스트를 이용하여, 이름 하나 당 각각의 자동차 인스턴스를 생성하는 기능 구현

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -1,15 +1,26 @@
 import { Console } from '@woowacourse/mission-utils';
 import InputManager from './InputManager.js';
+import Car from './Car.js';
 
 class RacingGame {
+  constructor() {
+    this.cars = [];
+  }
   async playRacing() {
     const inputManager = new InputManager();
     const carNameList = await this.getCarNameList(inputManager);
-    Console.print(carNameList);
+    await this.genearteCarObjects(carNameList);
   }
 
   async getCarNameList(inputManager) {
     return inputManager.enterCarNameList();
+  }
+
+  async genearteCarObjects(carNameList) {
+    carNameList.forEach((carName) => {
+      const car = new Car(carName);
+      this.cars.push(car);
+    });
   }
 }
 
