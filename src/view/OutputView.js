b43026bfe5dace97e361a9d1fd9157d35d1be763
cho-hyeonjin <gusls2005@gmail.com>
2023-11-01 00:55:59 +0900

    feat: OutputView > 회차별 각 차량 전진 횟수 프린트 메서드 작성

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,2 +1,18 @@
-// printResult
-// printWinner
+import { Console } from '@woowacourse/mission-utils';
+import RaceController from '../controller/RaceController';
+
+export default class OutputView {
+  constructor() {
+    this.raceController = new RaceController();
+  }
+
+  printResult() {
+    // 회차별 각 차량 전진 횟수 프린트 함수
+    const { carList } = this.raceController;
+    carList.forEach(car => {
+      const score = `-`.repeat(car.score);
+      Console.print(`${car.name} : ${car.score}`, score);
+    });
+    Console.print('\n');
+  }
+}
