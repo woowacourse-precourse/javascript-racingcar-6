f8a768cbf08f5367c8266d9774a8e7003d16675e
Jeongeun Lee <leeju1009@gmail.com>
2023-10-31 16:53:41 +0900

    test: 게임 시도 횟수의 유효성을 검사하는 함수 테스트
    사용자가 입력한 게임 시도 횟수가 조건에 맞는지 확인하는 함수 테스트

--- __tests__/Validation.js
+++ __tests__/Validation.js
@@ -12,4 +12,27 @@ describe('사용자 입력값의 유효성을 검사하는 함수 테스트', ()
 
     expect(isValidOutput).toBe(expected);
   });
+
+  test.each([[5], [1], [100]])(
+    '게임 시도 횟수 유효성 검사 함수 테스트 (true)',
+    (inputs) => {
+      const isValidOutput = validation.isValidRacingCount(inputs);
+
+      expect(isValidOutput).toBe(true);
+    },
+  );
+
+  test.each([
+    [['숫자가 아님']],
+    [[-1]],
+    [[0]],
+    [NaN],
+    [undefined],
+    [null],
+    [true],
+  ])('게임 시도 횟수 유효성 검사 함수 테스트 (false)', (inputs) => {
+    const isValidOutput = validation.isValidRacingCount(inputs);
+
+    expect(isValidOutput).toBe(false);
+  });
 });
