f2645f2ffb5172997f0103c0d4f4433b9a4aa390
Kysiam <how2logout@gmail.com>
2023-10-31 15:16:21 +0900

    Modify: input roundInput variable in raceSet function

--- src/controllers/RaceController.js
+++ src/controllers/RaceController.js
@@ -2,17 +2,21 @@ import { Console } from '@woowacourse/mission-utils';
 import Car from '../models/Car.js';
 import Track from '../models/Track.js';
 class RaceController {
+  /** @type {Track} */
   #race;
+
+  /**  @type {number} */
   #round;
 
   async raceSet() {
-    const carNameList = await Console.readLineAsync(
+    const carNamesInput = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
     );
-    const carNames = carNameList.split(',');
+    const carNames = carNamesInput.split(',');
     const cars = carNames.map((carName) => new Car(carName));
     this.#race = new Track(cars);
-    this.#round = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    const roundInput = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    this.#round = Number(roundInput);
   }
 }
 
