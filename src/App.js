57f73d9b293302aad5ee178687ac9a0b02f3223d
hamo-o <yheel369@gmail.com>
2023-11-01 06:33:13 +0900

    refactor: Car 클래스 분리

--- src/App.js
+++ src/App.js
@@ -1,62 +1,33 @@
-import { Console, Random } from '@woowacourse/mission-utils';
+import { Console } from '@woowacourse/mission-utils';
 import Input from './Input.js';
-import ERROR from './constants/Error.js';
 import MESSAGE from './constants/Message.js';
+import Car from './Car.js';
 
 class App {
-  #cars;
-
   #times;
 
   #num;
 
   constructor() {
-    this.#cars = [];
     this.#times = 0;
     this.#num = 0;
   }
 
-  startGoOrStop() {
-    const updatedCars = this.#cars.map((car) => {
-      const newCar = { ...car };
-      if (Random.pickNumberInRange(0, 9) >= 4) {
-        newCar.count += 1;
-      }
-      Console.print(`${newCar.name} : ${'-'.repeat(newCar.count)}`);
-      return newCar;
-    });
-
-    this.#cars = updatedCars;
-  }
-
-  findMaxCount() {
-    const maxCount = Math.max(...this.#cars.map((car) => car.count));
-    const maxCountCars = this.#cars.filter((car) => car.count === maxCount);
-    const maxCountCarNames = maxCountCars.map((car) => car.name);
-    return maxCountCarNames;
-  }
-
   async play() {
-    const names = await Input.getCarNames();
-    names.split(',').forEach((name) => {
-      if (name.length > 5 || name.length < 1) {
-        throw new Error(ERROR.nameLength);
-      }
-      this.#cars.push({ name, count: 0 });
-    });
+    const cars = await Input.getCarNames();
+    const car = new Car(cars);
 
     this.#times = await Input.getRepeatTimes();
     Console.print('');
 
     Console.print(MESSAGE.result);
-
     while (this.#num < this.#times) {
-      this.startGoOrStop();
+      car.startGoOrStop();
       Console.print('');
       this.#num += 1;
     }
 
-    const max = this.findMaxCount();
+    const max = car.findMaxCount();
     Console.print(`${MESSAGE.winner}${max.join(', ')}`);
   }
 }
---
+++ src/Car.js
@@ -0,0 +1,39 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+import ERROR from './constants/Error.js';
+
+class Car {
+  #cars;
+
+  constructor(names) {
+    this.#cars = [];
+
+    names.split(',').forEach((name) => {
+      if (name.length > 5 || name.length < 1) {
+        throw new Error(ERROR.nameLength);
+      }
+      this.#cars.push({ name, count: 0 });
+    });
+  }
+
+  startGoOrStop() {
+    const updatedCars = this.#cars.map((car) => {
+      const newCar = { ...car };
+      if (Random.pickNumberInRange(0, 9) >= 4) {
+        newCar.count += 1;
+      }
+      Console.print(`${newCar.name} : ${'-'.repeat(newCar.count)}`);
+      return newCar;
+    });
+
+    this.#cars = updatedCars;
+  }
+
+  findMaxCount() {
+    const maxCount = Math.max(...this.#cars.map((car) => car.count));
+    const maxCountCars = this.#cars.filter((car) => car.count === maxCount);
+    const maxCountCarNames = maxCountCars.map((car) => car.name);
+    return maxCountCarNames;
+  }
+}
+
+export default Car;
