1a95b785a80d72feb8812920fa9568bf532ec1ee
unknown <pih9134@naver.com>
2023-11-01 02:03:57 +0900

    fix: 빌드 실패 확인용

--- src/App.js
+++ src/App.js
@@ -1,63 +1,10 @@
-import { Console, Random } from '@woowacourse/mission-utils';
+import CarRacingGame from './CarRacingGame.js';
 
 class App {
   async play() {
-    const cars = await this.returnCarNames();
-    const tryCount = await this.getTryCount();
-    Console.print('\n실행 결과');
-    this.runRace(cars, tryCount);
+    const carRacingGame = new CarRacingGame();
+    await carRacingGame.run();
   }
-
-  isInvalidName(name) {
-    return name.length > 5 || !name || /[!@#$%^&*(),.?":]/.test(name);
-  }
-
-  async returnCarNames() {
-    const carNames = await Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
-    );
-
-    const names = carNames.split(',').map((name) => name.trim());
-
-    if (names.some(this.isInvalidName)) {
-      throw new Error('[ERROR] 잘못된 입력입니다.');
-    }
-
-    return names;
-  }
-
-  async getTryCount() {
-    const tryCount = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
-    return parseInt(tryCount, 10);
-  }
-
-  runRace(cars, tryCount) {
-    const progressList = new Array(cars.length).fill('');
-
-    for (let i = 0; i < tryCount; i += 1) {
-      cars.forEach((car, carIndex) => {
-        progressList[carIndex] += this.generateRandomProgress();
-        Console.print(`${car} : ${progressList[carIndex]}`);
-      });
-      Console.print('');
-    }
-
-    this.printWinners(cars, progressList);
-  }
-
-  printWinners = (cars, progressList) =>
-    Console.print(`최종 우승자 : ${this.findWinners(cars, progressList).join(' ')}`);
-
-  getRandomMove = () => Random.pickNumberInRange(0, 9);
-
-  generateRandomProgress = () => (this.getRandomMove() >= 4 ? '-' : '');
-
-  findWinners = (cars, progressList) =>
-    cars.filter(
-      (car, carIndex) => progressList[carIndex].length === this.maxProgress(progressList),
-    );
-
-  maxProgress = (progressList) => Math.max(...progressList.map((progress) => progress.length));
 }
 
 export default App;
