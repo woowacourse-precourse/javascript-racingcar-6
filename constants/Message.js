d0377bc7e7094cb52f996923e3a4dab28a3fa87b
Blue-Kite <bluelemon112@gmail.com>
2023-10-31 20:52:36 +0900

    feat: 자동차이름 입력받고 자동차이름 예외처리

---
+++ constants/Message.js
@@ -0,0 +1,8 @@
+export const Message = {
+  INIT: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
+};
+
+export const ErrorMessage = {
+  InputNumber: "[ERROR] 숫자가 잘못된 형식입니다.",
+  InputCarName: "[ERROR] 자동차 이름은 5자 이하만 가능합니다.",
+};
--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { Message, ErrorMessage } from "../constants/Message";
 class App {
-  async play() {}
+  async play() {
+    const namesInput = await MissionUtils.Console.readLineAsync(Message.INIT);
+    const carsName = namesInput.split(",");
+    InputTest(carsName);
+  }
 }
+
+const InputTest = (carsName) => {
+  carsName.map((item) => {
+    if (item.length > 5 || item.length < 1) {
+      throw new Error(ErrorMessage.InputCarName);
+    }
+  });
+};
 
 export default App;
