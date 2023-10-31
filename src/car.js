9841697dc12ac933bad88112d987b97f7d744291
BellYun <95991290+BellYun@users.noreply.github.com>
2023-11-01 07:26:57 +0900

    fix : if문 조건 수정

--- src/car.js
+++ src/car.js
@@ -6,7 +6,7 @@ class Car{
         this.far = [];
         for( let i = 0; i < this.name.length; i++){
             this.far.push(0)
-            if(this.name[i].length < 6){
+            if(this.name[i].length > 5){
                 throw new Error('[ERROR] 이름은 5자 이하만 가능합니다')
             }
         }
