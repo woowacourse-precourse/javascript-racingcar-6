7f55e83037ca8069b7636f1fcba4a60158b3a68a
LJH <049494@ewhain.net>
2023-11-01 03:19:59 +0900

    ✨ feat : 자동차 이름 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  constructor() {
+    this.carNames = [];
+    this.trialNumbers = 0;
+  }
+
+  async getCarNames() {
+    Console.print(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+    const INPUT = await Console.readLineAsync("");
+    const IS_VALID = validateCarNames(INPUT);
+
+    if (IS_VALID) {
+      addCarNames(INPUT, this.carNames);
+      await this.getTrialNumbers();
+    }
+  }
+
+  async play() {
+    await this.getCarNames();
+  }
 }
 
 export default App;
