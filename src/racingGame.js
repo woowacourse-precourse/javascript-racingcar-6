84a89d47908eb7557cd5b32161d0723fb9e6653b
Eunseo Na <nes020781@gmail.com>
2023-10-31 19:38:40 +0900

    feat: 우승자 찾는 로직 분리

--- src/racingGame.js
+++ src/racingGame.js
@@ -3,6 +3,7 @@ import InputCarName from "./Input/inputCarName.js";
 import InputTryAmount from "./Input/inputTryAmount.js";
 import MakeRandomNum from "./RandomNum/makeRandomNum.js";
 import createCarList from "./Car/createCarList.js";
+import FindWinner from "./Process/findWinner.js";
 
 class RacingGame {
   constructor() {
@@ -23,6 +24,8 @@ class RacingGame {
 
   async racing({ gameTryCount, carList }) {
     MissionUtils.Console.print("실행 결과\n");
+    MissionUtils.Console.print(carList);
+    MissionUtils.Console.print(gameTryCount);
     for (let i = 0; i < gameTryCount; i++) {
       carList.forEach((carElement) => {
         let randomNumber = MakeRandomNum();
@@ -30,31 +33,32 @@ class RacingGame {
           carElement.plusCount();
         }
 
-        MissionUtils.Console.print(`${element.name} : ${"-".repeat(element.count)}`);
+        MissionUtils.Console.print(`${carElement.name} : ${"-".repeat(carElement.count)}`);
       });
     }
 
-    const winner = await this.printWinner(carList);
+    const winner = await FindWinner(carList);
 
     const result = [...winner].join(", ");
     await MissionUtils.Console.print(`최종 우승자 : ${result}`);
+    return false;
   }
 
-  printWinner({ carList }) {
-    let winner = [];
-    let winnerCount = 0;
+  // printWinner({ carList }) {
+  //   let winner = [];
+  //   let winnerCount = 0;
 
-    carList.forEach((winCarElement) => {
-      if (winnerCount < winCarElement.count) {
-        winnerCount = winCarElement.count;
-        winner = [winCarElement.name];
-      } else if (winnerCount === winCarElement.count) {
-        winner.push(winCarElement.name);
-      }
-    });
+  //   carList.forEach((winCarElement) => {
+  //     if (winnerCount < winCarElement.count) {
+  //       winnerCount = winCarElement.count;
+  //       winner = [winCarElement.name];
+  //     } else if (winnerCount === winCarElement.count) {
+  //       winner.push(winCarElement.name);
+  //     }
+  //   });
 
-    return winner;
-  }
+  //   return winner;
+  // }
 }
 
 export default RacingGame;
