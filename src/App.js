109cdc972cec5ebab903b73b229a8a1fd9855804
useon <yoosk4986@naver.com>
2023-11-01 00:11:23 +0900

    feat: 경주할 자동차 이름을 입력 받는 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    const nameInputValue = await this.userInput(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+    );
+  }
+
+  async userInput(message) {
+    const inputValue = await MissionUtils.Console.readLineAsync(message);
+    return inputValue;
+  }
 }
 
 export default App;
