e7935ff7da801e60ee1b35f55da09da5f4fd44c3
suwonthugger <ddww1029wwdd@gmail.com>
2023-10-31 11:02:06 +0900

    Feat: 이동 횟수 값을 생성하는 생성자 함수와 이동 횟수 값을 초기화 하는 함수 추가

--- src/App.js
+++ src/App.js
@@ -2,15 +2,33 @@ import { MESSAGE } from "./constants/messages.js";
 import { Console, Random } from "@woowacourse/mission-utils";
 
 export class App {
-  async play() {
-    this.getInputValue();
+  constructor() {
+    this.moveCount;
   }
 
-  startGame() {}
+  setMoveCount(carNames) {
+    this.moveCount = carNames.reduce((obj, name) => {
+      return {
+        ...obj,
+        [name]: "",
+      };
+    }, {});
+  }
+
+  async play() {
+    this.startGame();
+  }
+
+  async startGame() {
+    const { carNames, tryNumber } = await this.getInputValue();
+    this.setMoveCount(carNames);
+    MESSAGE.getGameScore(this.moveCount);
+  }
 
   async getInputValue() {
     Console.print(MESSAGE.START);
-    const carNames = await Console.readLineAsync("");
+    const carNames = (await Console.readLineAsync("")).split(",");
+
     Console.print(MESSAGE.INPUT);
     const tryNumber = await Console.readLineAsync("");
 
--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,7 +1,14 @@
+import { Console } from "@woowacourse/mission-utils";
+
 export const MESSAGE = Object.freeze({
   START: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
   INPUT: "시도할 횟수는 몇 회인가요?",
   RESULT: "실행 결과",
   END: "최종 우승자 : ",
-  getWinnersName: (...names) => names.join(","),
+  getGameScore: (moveCount) => {
+    for (let car in moveCount) {
+      Console.print(`${car} : ${moveCount[car]}`);
+    }
+  },
+  getWinnersName: (winnerNames) => names.join(","),
 });
