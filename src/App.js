4aff72146a13a680b0093538a19531ac4ed9580d
iamreum <102291571+iamreum@users.noreply.github.com>
2023-10-31 12:28:36 +0900

    기본 입출력 코드 작성
    - 자동차 경주에 필요한 기본 입출력 코드 작성 (이름, 횟수)
    
    - 횟수가 잘못될 시 발생할 에러 처리 구문 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  constructor() {
+    this.randomNumbers = this.generateRandomNumbers();
+  }
+
+  generateRandomNumbers() {
+    const NUMBERS = [];
+    const NUMBER = MissionUtils.Random.pickNumberInRange(0, 9);
+    // if ( number >= 4 이면 전진하는 함수 호출) {
+    //
+    // }
+  }
+
+  async play() {
+    const CARNAME_RESPONSE = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    const CARNAMES = CARNAME_RESPONSE.split(',').map(name => name.trim());
+    console.log(CARNAMES);
+
+    const PLAYTIME_RESPONSE = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+    const PLAYTIME = parseInt(PLAYTIME_RESPONSE);
+
+    if ( isNaN(PLAYTIME) || PLAYTIME < 0 ) {
+      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    }
+
+    MissionUtils.Console.print("실행 결과");
+    MissionUtils.Console.print(this.generateRandomNumbers());
+  }
 }
 
 export default App;
