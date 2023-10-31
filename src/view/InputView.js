89fb3ba4b7b9fcf4ff25db10178e56ef96876e4b
ryan-dia <cjfdnjs158@naver.com>
2023-11-01 00:28:03 +0900

    feat(inputView): 레이싱 카 이름 유효성 확인

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,9 +1,11 @@
 import { Console } from '@woowacourse/mission-utils';
 import { INPUT_MESSAGE_FUNCTION, OUTPUT_MESSAGE } from '../constants/Messages.js';
+import Validators from '../utils/validator/index.js';
 
 const InputView = {
   async readRacingVehicleName(vehicle) {
     const racingVehicleName = await Console.readLineAsync(INPUT_MESSAGE_FUNCTION.name(vehicle));
+    Validators.checkRacingVehicleName(racingVehicleName);
     return racingVehicleName;
   },
 
