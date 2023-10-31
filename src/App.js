35c8992a53ccf4b8d4c5354673cae4e170998ef6
Baek <woonn97@gmail.com>
2023-10-31 23:29:11 +0900

    feat: 우승자 찾기 구현
    * getWinners() 메서드로 우승자를 구하고 출력하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -8,12 +8,17 @@ class App {
     const nameInput = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
     this.cars = nameInput.split(",").map((name) => ({ name, position: 0 }));
     const tryCount = await Console.readLineAsync("시도할 회수는 몇회인가요?\n");
+    Console.print("\n실행 결과");
     for (let i = 0; i < tryCount; i++) {
       this.randomMove();
       this.printResult();
+      Console.print("");
     }
+    const winnerNames = this.getWinners().map((car) => car.name);
+    Console.print(`최종 우승자 : ${winnerNames.join(", ")}`);
   }
   printResult() {
+    // 게임 결과를 출력하는 메서드
     this.cars.forEach((car) => {
       Console.print(`${car.name}: ${"-".repeat(car.position)}`);
     });
@@ -25,6 +30,11 @@ class App {
         car.position += 1;
       }
     });
+  }
+  getWinners() {
+    // 최종 우승자 return하는 메서드
+    const maxPosition = Math.max(...this.cars.map((car) => car.position));
+    return this.cars.filter((car) => car.position === maxPosition);
   }
 }
 
