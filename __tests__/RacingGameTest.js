d64d36746de643ef641f87ed2c1270225daa27e7
Jeongeun Lee <leeju1009@gmail.com>
2023-10-31 23:19:12 +0900

    test: 유효하지 않는 게임 시도 횟수에 대한 예외 처리 테스트 추가

--- __tests__/RacingGameTest.js
+++ __tests__/RacingGameTest.js
@@ -1,7 +1,6 @@
 import { Console } from '@woowacourse/mission-utils';
 import RacingGame from '../src/RacingGame.js';
 import Validation from '../src/Validation.js';
-import ErrorHandler from '../src/ErrorHandler.js';
 
 const mockRacingCountInput = (racingCount) => {
   Console.readLineAsync = jest.fn();
@@ -13,7 +12,6 @@ const mockRacingCountInput = (racingCount) => {
 
 describe('레이싱 게임 플레이와 관련된 함수 테스트', () => {
   const racingGame = new RacingGame();
-  const validation = new Validation();
 
   test('게임 시도 횟수를 숫자로 변환하여 반환하는지 테스트', async () => {
     const racingCountInput = '5';
@@ -21,5 +19,14 @@ describe('레이싱 게임 플레이와 관련된 함수 테스트', () => {
     const racingCount = await racingGame.getRacingCount();
 
     expect(typeof racingCount).toBe('number');
+  });
+
+  test.each([
+    ['-1'],
+    ['숫자 아님']
+  ])('유효하지 않는 게임 시도 횟수에 대한 예외 처리 테스트', async (inputs) => {
+    mockRacingCountInput(inputs);
+
+    await expect(racingGame.getRacingCount()).rejects.toThrow('[ERROR]');
   });
 });
