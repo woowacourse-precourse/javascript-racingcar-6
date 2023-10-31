8d81bf811fd09978880a09290bb4d4f5c997447f
yeonkyoung <keep.ykk@gmail.com>
2023-11-01 02:33:23 +0900

    feat: 전진 조건 성립시 자동차 인스턴스의 moveCount 증가

--- src/utils/gameSupport.js
+++ src/utils/gameSupport.js
@@ -1,7 +1,14 @@
+import { moveCount } from "./move.js";
 import Car from "../Car.js";
 
 export const getCarsClassArray = (carsStringNameArray) => {
   return carsStringNameArray.map((carName) => new Car(carName));
+};
+
+const setCarsMoveCount = (cars) => {
+  for (const car of cars) {
+    if (moveCount()) car.setMoveCount();
+  }
 };
 
 export const getMatchResult = (cars, trialCountNumber) => {
