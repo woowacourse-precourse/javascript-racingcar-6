fb82e0e63c3ba5a58e8a96e51742054ebd9d00f5
Kim Da Eun <midekuna@gmail.com>
2023-11-01 01:35:03 +0900

    📝 docs: Controller 클래스 JSDoc 추가

--- src/controller/GameController.js
+++ src/controller/GameController.js
@@ -5,6 +5,9 @@ import InputView from '../views/InputView.js';
 import { MESSAGES } from '../constants/messages.js';
 import OutputView from '../views/OutputView.js';
 
+/**
+ * 게임을 진행하는 Controller
+ */
 class GameController {
   constructor() {
     this.inputView = new InputView();
@@ -12,11 +15,16 @@ class GameController {
     this.gameModel = null;
   }
 
+  /**
+   * 게임 시작
+   * 1. 게임 초기화
+   * 2. 시도 횟수만큼 라운드 진행
+   * 3. 결과 출력
+   */
   async startGame() {
     await this.initGame();
 
     Console.print(MESSAGES.startRound);
-
     while (!this.gameModel.isGameOver()) {
       this.gameModel.playRound();
 
@@ -28,6 +36,10 @@ class GameController {
     this.outputView.printWinner(winners);
   }
 
+  /**
+   * 게임 초기화 함수.
+   * GameModel 인스턴스를 생성하여 저장
+   */
   async initGame() {
     const cars = await this.inputCars();
     const attemptNum = await this.inputAttemptNum();
@@ -35,11 +47,19 @@ class GameController {
     this.gameModel = new GameModel(attemptNum, cars);
   }
 
+  /**
+   * 사용자에게 자동차 이름을 입력 받아 CarModel 배열을 생성하여 반환
+   * @returns {CarModel[]} CarModel 배열
+   */
   async inputCars() {
     const carNames = await this.inputView.getCarNames();
     return carNames.map((name) => new CarModel(name));
   }
 
+  /**
+   * 사용자에게 시도 횟수를 입력 받아 반환
+   * @returns {number} 시도 횟수(number)
+   */
   async inputAttemptNum() {
     const attemptNum = await this.inputView.getAttemptNum();
     return attemptNum;
