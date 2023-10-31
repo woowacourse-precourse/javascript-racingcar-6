cf54bdb0d644e7cad33b9a6ef619dc4fffc50be6
soominnnn <marisol03@naver.com>
2023-11-01 02:33:36 +0900

    🐛Fix: 테스트 후 예외 처리 추가 및 수정

--- src/Exception.js
+++ src/Exception.js
@@ -1,19 +1,23 @@
 class Exception {
   CAR_NAME_DUPLICATE_ERROR = "[ERROR] 자동차의 이름은 한 번씩만 사용할 수 있습니다.";
+  CAR_NAME_BLANK_ERROR = "[ERROR] 자동차 이름에 공백은 포함될 수 없습니다.";
   CAR_NAME_COUNT_ERROR = "[ERROR] 자동차의 이름을 2개 이상 입력해주세요.";
   CAR_NAME_LENGTH_ERROR = "[ERROR] 자동차의 이름을 4자 이하로 정해주세요.";
   INVALID_TYPE_TRY_ERROR = "[ERROR] 시도할 횟수를 숫자로 입력해주세요.";
   TRY_COUNT_ERROR = "[ERROR] 시도할 횟수를 1회 이상 입력해주세요.";
 
   carNameInputException(userInput) {
-    if(userInput == '' || userInput[userInput.length-1] == '') {
-      throw new Error(this.CAR_NAME_DUPLICATE_ERROR);
+    if(userInput.length <= 1) {
+      throw new Error(this.CAR_NAME_COUNT_ERROR);
+    }
+    else if(userInput == '' || userInput[userInput.length-1] == '') {
+      throw new Error(this.CAR_NAME_BLANK_ERROR);
     }
     else if(userInput.some(name => name.length >= 5)) {
       throw new Error(this.CAR_NAME_LENGTH_ERROR);
     }
     else if(userInput.length != [...new Set(userInput)].length) {
-      throw new Error(this.CAR_NAME_COUNT_ERROR);
+      throw new Error(this.CAR_NAME_DUPLICATE_ERROR);
     }
     else {
       return userInput;
