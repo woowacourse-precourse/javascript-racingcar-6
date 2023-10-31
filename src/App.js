0598d090aa39cabc57d157ceb2906efb248e6521
leehyeyeong <elto789@naver.com>
2023-10-31 10:38:49 +0900

    feat: 우승자 출력 - 가장 최고점과 같은 키를 저장 - 1이면 출력, 초과한다면 반복문을 돌면서 배열에 넣고 ','로 구분하여 출력

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ async function gameStart() {
   Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
   const inputCarName = await Console.readLineAsync("");
   const carName = inputCarName.split(',');
-  Console.print(carName.length);
   Console.print("시도할 횟수는 몇 회인가요?");
   const inputNumber = await Console.readLineAsync("");
   const carDict = {};
@@ -29,19 +28,41 @@ function gamePlay(inputNumber, carDict) {
   for (let i = 0; i < inputNumber; i++) {
     randomNumber(carDict)
   }
+  gameResult(carDict)
 
 }
 
 function randomNumber(carDict) {
   for (let i = 0; i < Object.keys(carDict).length; i++) {
     const pickNumber = Random.pickNumberInRange(0, 9);
-    //Console.print(pickNumber);
     if (pickNumber >= 4) {
       carDict[i]["score"] += 1
     }
     Console.print(carDict[i]["name"] + " : " + ("-".repeat(carDict[i]["score"])));
   }
   Console.print("")
+}
+
+function gameResult(carDict) {
+  let winScore = 0
+  for (let i = 0; i < Object.keys(carDict).length; i++) {
+    if (carDict[i]["score"] > winScore) {
+      winScore = carDict[i]["score"]
+    }
+  }
+  const winCarName = [];
+  const keysOfcarDict = Object.keys(carDict)
+  const key = keysOfcarDict.filter((key) => carDict[key]["score"] === winScore);
+  if (key.length === 1) {
+    Console.print("최종 우승자 : " + carDict[key]["name"])
+  }
+  else {
+    for (let i = 0; i < key.length; i++) {
+      winCarName.push(carDict[key[i]]["name"])
+    }
+    let strTemp = winCarName.join(', ')
+    Console.print("최종 우승자 : " + strTemp)
+  }
 }
 
 
