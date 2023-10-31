82196db29b20f74119c4659e780680fecc8b133e
pongpongie <arevolvera@gmail.com>
2023-10-31 23:32:46 +0900

    refactor(views): refactor InputCarNameView.js
    changing MESSAGES into ERROR_MESSAGES due to importing ERROR_MESSAGES

--- src/views/InputCarNameView.js
+++ src/views/InputCarNameView.js
@@ -1,11 +1,11 @@
 import { Console } from "@woowacourse/mission-utils";
-import MESSAGES from "../constants/Messages";
+import { MESSAGES, ERROR_MESSAGES } from "../constants/Messages";
 
 const InputCarNameView = () => {
   try {
     Console.readLineAsync(MESSAGES.INPUT_CARNAME);
-  } catch (err) {
-    err.Console.print("[ERROR]");
+  } catch {
+    Console.print(ERROR_MESSAGES.CARNAME_ERROR);
   }
 };
 
