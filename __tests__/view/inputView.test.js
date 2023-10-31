885641baad26e584ffc21df23f11ec4dcd314980
이태훈 <rhymint@gmail.com>
2023-10-31 14:33:04 +0900

    test : RacingCarValidator 테스트 코드 추가

--- __tests__/view/inputView.test.js
+++ __tests__/view/inputView.test.js
@@ -5,37 +5,24 @@ const inputView = new InputView();
 const mockInput = (input) => {
   MissionUtils.Console.readLineAsync = jest.fn();
   MissionUtils.Console.readLineAsync.mockImplementation(() => {
-    console.log(`${input} input`);
     return input;
-  })
-}
-const mockInputs = (inputs) => {
-  MissionUtils.Console.readLineAsync = jest.fn();
-  MissionUtils.Console.readLineAsync.mockImplementation(() => {
-    const input = inputs.shift();
-
-    return Promise.resolve(input);
   })
 }
 
 describe('inputView test', () => {
-  test.each('func readRacingCarNames', async () => {
+  test('func readRacingCarNames', async () => {
   const input = 'kim,park,cho';
-
   mockInput(input);
 
   const result = await inputView.readRacingCarNames();
-
   expect(result).toEqual(['kim', 'park', 'cho']);
   });
 
   test('func readRetryCount', async () => {
     const input = '3';
-  
     mockInput(input);
   
     const result = await inputView.readRetryCount();
-  
     expect(result).toEqual('3');
     });
 })
