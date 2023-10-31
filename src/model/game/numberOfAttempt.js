31d9b27e557bf233e0e1fbdfc11de75333a7055c
yunji <dbswl330@gmail.com>
2023-11-01 01:13:58 +0900

    feat: Game이 끝났는지 확인하는 함수 구현
    시도 횟수가 0 이 되면 게임이 끝난다

--- src/model/game/numberOfAttempt.js
+++ src/model/game/numberOfAttempt.js
@@ -12,5 +12,12 @@ class NumberOfAttempt {
       throw new Error("[ERROR] 0이상의 정수를 입력해 주세요. ");
     }
   }
+
+  isFinished() {
+    if (this.NumberOfAttempt === 0) {
+      return true;
+    }
+    return false;
+  }
 }
 export default NumberOfAttempt;
