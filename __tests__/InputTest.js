ba759be5e91bfe77941447801a48eafc7171fafe
won0104 <98508955+won0104@users.noreply.github.com>
2023-11-01 00:20:33 +0900

    test : -  - [ ]  사용자에게 경주할 자동차의 이름을 입력받음

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -1,5 +1,15 @@
 import validateUserInput from '../src/Utils/Validator';
 import { IS_EMPTY_ERROR, VALIDATE_CAR_NAME_ERROR } from '../src/Utils/Define';
+import InputView from '../src/view/InputView';
+import { Console } from '@woowacourse/mission-utils';
+
+const mockReadLineAsync = (inputs) => {
+  const inputsCopy = [...inputs]; // inputs 배열을 복사하여 사용
+  Console.readLineAsync = jest.fn().mockImplementation(() => {
+    const input = inputsCopy.shift();
+    return Promise.resolve(input);
+  });
+};
 
 describe('유저 입력 테스트', () => {
   test('차 이름이 공백일때', () => {
@@ -25,6 +35,15 @@ describe('유저 입력 테스트', () => {
       result = error.message;
     }
     expect(result).toContain(VALIDATE_CAR_NAME_ERROR);
+  });
+
+  test('inputView 작동 테스트', async () => {
+    const testInputs = ['Car1,Car2,Car3'];
+    mockReadLineAsync(testInputs);
+
+    const result = await InputView();
+
+    expect(result).toContain('Car1');
   });
 });
 
