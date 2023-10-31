103d9ba84a22858b5382e5c64fa6c712e4e3e86b
Eunseo Na <nes020781@gmail.com>
2023-11-01 02:27:31 +0900

    feat: 자동차가 이동한 결과 테스트 코드 구현

---
+++ __tests__/RaceTest.js
@@ -0,0 +1,52 @@
+import RacingGame from "../src/racingGame";
+import Car from "../src/Car/car";
+import CarRacing from "../src/Process/carRacing";
+import FindWinner from "../src/Process/findWinner";
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const mockQuestions = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  });
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
+describe("자동차 경주 출력 테스트", () => {
+  test("자동차가 이동한 결과 출력", () => {
+    //given
+    const inputList = [new Car("kim", 0), new Car("park", 0)];
+    const randomNum = [3, 4];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputList);
+    mockRandoms([...randomNum]);
+
+    //when
+    CarRacing(inputList);
+
+    //then
+    expect(logSpy).toHaveBeenCalledWith("kim : ");
+    expect(logSpy).toHaveBeenCalledWith("park : -");
+  });
+
+  test("우승자가 여러 명이면, 쉼표를 통해 출력", () => {
+    //given
+    //when
+    //then
+  });
+});
