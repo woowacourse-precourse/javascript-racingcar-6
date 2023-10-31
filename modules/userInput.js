4ec094a0613de224fa0b54cff0a741ef3b702edf
고세종 <fulfilled7418@gmail.com>
2023-11-01 01:54:47 +0900

    refactor: 리팩토링 유저의 입력을 받는 기능과 유효성 검사 분리

--- modules/userInput.js
+++ modules/userInput.js
@@ -8,13 +8,12 @@ const getCarNames = async () => {
   const carNames = await Console.readLineAsync(
     '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
   );
-  return checkCarNamesAreValid(carNames);
+  return carNames;
 };
 
 const getTryNumber = async () => {
   const tryNumber = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
   Console.print('\n실행 결과');
-  checkTryNumberIsValid(tryNumber);
 
   return tryNumber;
 };
