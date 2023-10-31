bb1d963e56abdf8daa5e850a8fddd151466a6ffd
iamreum <102291571+iamreum@users.noreply.github.com>
2023-10-31 21:41:14 +0900

    Indent depth 수정
    - 깊이 3이 넘어가는 부분을 2로 수정

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import RacingCar from "./RacingCar.js";
 class App {
   async play() {
     const CARNAME_RESPONSE = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
-    const CARNAMES = CARNAME_RESPONSE.split(',').map(name => {
-      if (name.length >= 5) {
-        throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
-      }
-      return name.trim()
-    });
+    const CARNAMES = CARNAME_RESPONSE.split(',').map(name => this.validateCarName(name.trim()));
 
     const PLAYTIME_RESPONSE = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
     MissionUtils.Console.print("");
@@ -21,6 +16,13 @@ class App {
 
     const racingCar = new RacingCar(CARNAMES, PLAYTIME);
     racingCar.runRace();
+  }
+
+  validateCarName(name) {
+    if (name.length >= 5) {
+      throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+    }
+    return name;
   }
 }
 
