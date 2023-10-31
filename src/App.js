3db5be3a36fbbea58d048eb6b3aa4c531169e1af
jadewisemann <jadewisemann@gmail.com>
2023-10-31 16:51:01 +0900

    feat: printMessage(message) 함수 추가, 파라미터를 받아서 콘솔에 출력하는 함수

--- src/App.js
+++ src/App.js
@@ -5,14 +5,19 @@ import message from "./util/Message.js"
 class App {
   async play() {
     const userInput = await this.getUserInput();
+    const msg =  'test'
+    this.printMessage(msg)
   }
   
   async getUserInput() {
     const input = await Console.readLineAsync(message.starting)
     return input
   }
-}
 
+  printMessage(message) {
+    Console.print(message)
+  }
+}
 
 
 
