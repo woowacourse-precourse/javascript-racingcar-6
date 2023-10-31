233d20b5c1152c342351a0410d01707ead29afa5
kwaksj329 <kwaksj329@gmail.com>
2023-10-31 15:10:49 +0900

    feat: 게임 시작 및 자동차, 시도 횟수 입력받는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const inputCar = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
+    const inputNumber = await Console.readLineAsync('시도할 횟수는 몇 회인가요?');
+    Console.print('실행 결과');
+
+    const racingCarList = inputCar.split(',');
+    const distanceList = Array.from({length: racingCarList.length}, () => 0);
+  }
 }
 
 export default App;
