7e0c4a106924ecd73eba31a43766472710acc18f
cobocho <rlaalsrb1111@naver.com>
2023-10-31 12:14:36 +0900

    feat: `Car` 예외 처리 구현

--- src/domain/Car.js
+++ src/domain/Car.js
@@ -1,3 +1,6 @@
+import ERROR_MESSAGE from '../constants/error.js';
+import ApplicationError from '../exceptions/ApplicationError.js';
+
 class Car {
   static DEAD_ZONE = 4;
 
@@ -10,8 +13,15 @@ class Car {
   }
 
   move(power) {
+    this.#validateMove(power);
     if (power >= Car.DEAD_ZONE) {
       this.#distance += Car.SPEED;
+    }
+  }
+
+  #validateMove(power) {
+    if (!Number.isInteger(power)) {
+      throw new ApplicationError(ERROR_MESSAGE.common.moveArgNotInteger);
     }
   }
 }
