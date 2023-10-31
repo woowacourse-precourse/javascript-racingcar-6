95e94b722b12f82b236994ef8c3fb7f967e1d200
hyunyeee <5004ohy@naver.com>
2023-10-31 23:43:50 +0900

    feat: 사용자로부터 자동차 이름 및 횟수를 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const CAR_NAME_MESSAGE =
+  "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
+const ATTEMP_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?\n";
+
 class App {
-  async play() {}
+  async play() {
+    this.#controller();
+  }
+
+  async #controller() {
+    const carName = await this.#readCarName();
+    const attempCount = await this.#readAttempCount();
+  }
+
+  async #readCarName() {
+    return MissionUtils.Console.readLineAsync(CAR_NAME_MESSAGE);
+  }
+
+  async #readAttempCount() {
+    return MissionUtils.Console.readLineAsync(ATTEMP_COUNT_MESSAGE);
+  }
 }
 
 export default App;
