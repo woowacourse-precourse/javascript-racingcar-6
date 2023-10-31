2454dfb2f9555b37ad84a358a612ca887539aed5
j8won <joowoni99@gmail.com>
2023-10-31 22:11:41 +0900

    test(OutputHandler): Test printing all the car's name and hyphens to represent car positions in each round

--- __tests__/OutputHandler.test.js
+++ __tests__/OutputHandler.test.js
@@ -11,7 +11,33 @@ const getLogSpy = () => {
 describe("메시지 출력", () => {
   test("게임을 시작할 때 '실행 결과' 출력하기", () => {
     const logSpy = getLogSpy();
-    OutputHandler.printInitialExecutionMessage();
-    expect(logSpy).toHaveBeenCalledWith(MESSAGE.INITIAL_RACE_EXECUTION);
+    const result = [];
+    OutputHandler.printGameExecution(result);
+    expect(logSpy).toHaveBeenCalledWith(MESSAGE.GAME_RESULT);
+  });
+
+  test.each([
+    [1, "-"],
+    [3, "---"],
+  ])("주어진 숫자만큼 하이픈 생성하기 - %d개", (number, expectedString) => {
+    expect(OutputHandler.generateHyphens(number)).toBe(expectedString);
+  });
+
+  test.each([
+    ["car", [{ name: "car", position: 1 }], ["car : -"]],
+    [
+      "car, bus",
+      [
+        { name: "car", position: 2 },
+        { name: "bus", position: 3 },
+      ],
+      ["car : --", "bus : ---"],
+    ],
+  ])("라운드 결과 출력 - %s", (testName, roundArray, resultArray) => {
+    const logSpy = getLogSpy();
+    OutputHandler.printRoundResult(roundArray);
+    [...resultArray, ""].forEach((result, index) =>
+      expect(logSpy).toHaveBeenNthCalledWith(index + 1, result)
+    );
   });
 });
