b4090a002674ec573058571b8d5a2df7a409e000
ryan-dia <cjfdnjs158@naver.com>
2023-10-31 23:42:00 +0900

    fix: input message  오타 수정 및 줄바꿈 추가

--- src/constants/Messages.js
+++ src/constants/Messages.js
@@ -1,6 +1,6 @@
 export const INPUT_MESSAGE_FUNCTION = Object.freeze({
   name(vehicle) {
-    return `경주할 ${vehicle} 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분`;
+    return `경주할 ${vehicle} 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n`;
   },
 });
 
