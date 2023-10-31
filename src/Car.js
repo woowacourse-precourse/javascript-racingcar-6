62dc50bb2d8465779cf6b01e3634822429ca0794
Sunghyun627 <kksscc4@gmail.com>
2023-11-01 00:53:37 +0900

    refactor: 전진여부를 반환하는 메서드의 이름을 변경

--- src/Car.js
+++ src/Car.js
@@ -4,14 +4,13 @@ class Car {
   constructor(name) {
     this.name = name;
     this.moveCount = 0;
-    this.randomNumber = -1;
   }
 
   async generateRandomNumber() {
     return await Random.pickNumberInRange(0, 9);
   }
 
-  async canMove() {
+  async getIsCanMove() {
     const randomNumber = await this.generateRandomNumber();
     return randomNumber >= MOVE_FORWARD_CRITERIA;
   }
