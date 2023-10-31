1bfedda9ff22b0576bbf2be782de0a4719e15271
songhyunseop <gustjq99455352@gmail.com>
2023-10-31 13:53:30 +0900

    Test: 시도횟수에 따른 자동차 이동거리 계산결과 함수 테스트케이스 작성

--- __tests__/UnitTest.js
+++ __tests__/UnitTest.js
@@ -1,5 +1,6 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import Controllers from "../src/Controllers/index.js";
+import Model from "../src/Models/index.js";
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -73,6 +74,54 @@ describe("게임 시도 횟수 입력", () => {
     // when
     const controllers = new Controllers();
     const result = controllers.getTryTimes();
+
+    //then
+    await expect(result).rejects.toThrow("[ERROR]");
+  });
+});
+
+describe("시도횟수 만큼 반복한 각 자동차의 이동 거리 계산", () => {
+  const model = new Model();
+  const controllers = new Controllers();
+
+  test("정상적으로 계산이 이루어졌을 때", async () => {
+    const cars = ["caro", "cari"];
+    const tryTime = 3;
+
+    mockRandoms([5, 3, 5, 2, 6, 2]);
+
+    model.removeWhitespace = jest.fn((carName) => carName);
+    model.setInitialCarMovePoint = jest.fn(() => {
+      return { caro: 0, cari: 0 };
+    });
+    controllers.repeatCarRacing = jest.fn(() => {
+      return { caro: 3, cari: 0 };
+    });
+
+    //when
+    const result = await controllers.carMoveCheck(cars, tryTime);
+
+    //then
+    expect(result).toEqual({ cars, moveResult: { caro: 3, cari: 0 } });
+  });
+
+  test("자동차 이름이 5글자 이상일 시 예외처리", async () => {
+    const cars = ["abcdef", "gghhf"];
+    const tryTime = 1;
+
+    //when
+    const result = controllers.carMoveCheck(cars, tryTime);
+
+    //then
+    await expect(result).rejects.toThrow("[ERROR]");
+  });
+
+  test("잘못된 형식의 이름을 입력 시 예외처리", async () => {
+    const cars = ["l#uc", "@ㅡ@"];
+    const tryTime = 1;
+
+    //when
+    const result = controllers.carMoveCheck(cars, tryTime);
 
     //then
     await expect(result).rejects.toThrow("[ERROR]");
