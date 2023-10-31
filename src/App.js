1cb16bd4f68d1888f46344fbec94a43c0cb7b842
gloxinia106 <gloxinia106@gmail.com>
2023-10-31 13:41:34 +0900

    [기능 추가] 자동차 이름 및 시도 횟수 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
+class Car {
+  constructor(name) {
+    this.name = name;
+    this.position = 0;
+  }
+}
+
 class App {
-  async play() {}
+  async play() {
+    const carNames = (
+      await Console.readLineAsync(
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+      )
+    ).split(",");
+    const times = Number(
+      await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n")
+    );
+    const cars = carNames.map((name) => new Car(name));
+  }
 }
 
 export default App;
