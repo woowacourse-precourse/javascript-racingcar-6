57aafe9730a57346b3d9c8483893a15647fab945
soosoo22 <lllllh00@naver.com>
2023-10-31 23:30:30 +0900

    chore: Input클래스 사용
    src/view/input.js 파일 내 Input 클래스 사용

--- test/GameInput.test.js
+++ test/GameInput.test.js
@@ -1,11 +1,10 @@
 import Input from '../src/view/input.js';
-import { RacingCarName } from '../src/racingcargame/index.js';
 
 describe('게임 시작 문구', () => {
   test('경주할 자동차 이름을 입력하세요 메시지가 출력되는지 확인', async () => {
     const expectedMessage = '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)';
     jest.spyOn(Input, 'text').mockResolvedValue(expectedMessage);
-    const result = await RacingCarName.input();
+    const result = await Input.text();
 
     expect(result).toBe(expectedMessage);
   });
@@ -13,7 +12,7 @@ describe('게임 시작 문구', () => {
   test('시도할 횟수는 몇 회인가요 메시지가 출력되는지 확인', async () => {
     const expectedMessage = '시도할 횟수는 몇 회인가요?';
     jest.spyOn(Input, 'text').mockResolvedValue(expectedMessage);
-    const result = await RacingCarName.input();
+    const result = await Input.text();
 
     expect(result).toBe(expectedMessage);
   });
