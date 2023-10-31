690163cfe0eac7ba1cb2b6ed8487c06ebf03a4a8
se0jinYoon <tjwls1999@gmail.com>
2023-11-01 02:59:41 +0900

    refactor(gameStartInput): seperate gameStart to each input function

--- src/App.js
+++ src/App.js
@@ -6,30 +6,37 @@ class App {
     this.GAME_ROUND = 0;
     this.GAME_ROUND_RESULT = {};
   }
+
   async play() {
-    await this.gameStart();
+    await this.getUserNameInput();
+    await this.getRoundNumInput();
     this.makePlayerInfo();
     this.playGame();
     this.printGameResult();
   }
-
-  async gameStart() {
+  // 이름 입력 받기
+  async getUserNameInput() {
     try {
       const GAME_PLAYER_INPUT_REGEX = /^([a-zA-Z가-힣]{1,5},)*[a-zA-Z가-힣]{1,5}$/i;
-      // 이름 입력받기
       const GAME_PLAYER_INPUT = await Console.readLineAsync(
-        '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분'
+        '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
       );
 
       if (GAME_PLAYER_INPUT_REGEX.test(GAME_PLAYER_INPUT)) {
         this.GAME_PLAYERS = GAME_PLAYER_INPUT.split(',');
       } else {
-        throw new Error('[ERROR] 경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분');
+        throw new Error('[ERROR] 경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
       }
+    } catch (error) {
+      throw error;
+    }
+  }
 
-      // 게임 횟수 입력받기
+  // 게임 횟수 입력 받기
+  async getRoundNumInput() {
+    try {
       this.GAME_ROUND = await Console.readLineAsync('시도할 횟수는 몇 회인가요?');
-      if (isNaN(this.GAME_ROUND)) {
+      if (this.GAME_ROUND <= 0 || isNaN(this.GAME_ROUND)) {
         throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
       }
     } catch (error) {
@@ -39,9 +46,9 @@ class App {
 
   // 게임 플레이어 초기세팅
   makePlayerInfo() {
-    this.GAME_PLAYERS.forEach(player => {
+    this.GAME_PLAYERS.forEach((player) => {
       this.GAME_ROUND_RESULT[player] = 0;
-    })
+    });
   }
 
   // 라운드 당 진행할 게임 로직
@@ -58,7 +65,7 @@ class App {
   // 라운드 출력 형식
   printRoundResult() {
     for (const [player, result] of Object.entries(this.GAME_ROUND_RESULT)) {
-      Console.print(`${player} : ${"-".repeat(result)}`);
+      Console.print(`${player} : ${'-'.repeat(result)}`);
     }
     Console.print('\n');
   }
