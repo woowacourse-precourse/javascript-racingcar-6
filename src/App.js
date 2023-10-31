900cfd23e97aa611652b07610cf35958ee7c9979
0UNG2-FIRST\MS <popduddms@gmail.com>
2023-10-31 13:00:59 +0900

    feat: play()를 작동시킨다 가정하고 들어갈 흐름 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  constructor() {}
+
+  async play() {
+    const userInputCarNames = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
+    );
+    this.validateCarNames(userInputCarNames);
+    const userInputRoundCount = await Console.readLineAsync('시도할 횟수는 몇 회인가요?');
+    this.validateRoundCount(userInputRoundCount);
+
+    this.racing(userInputCarNames, userInputRoundCount);
+    this.winner(result);
+  }
 }
 
 export default App;
