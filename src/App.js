fd9051eb89697365b7399ecb74016827c38289f5
Hyelim Choi <HyelimChoi01@gmail.com>
2023-10-31 13:48:40 +0900

    feat: playing game go or stop

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App {
     );
     // TODO: , 로 구분해서 저장
     const carNames = InputCarNames.split(",");
+    const cars = carNames.map((name) => ({ name, position: 0 }));
 
     // TODO: 유효처리 - 이름이 5자 이하인지
     this.checkCarNameLength(carNames);
@@ -18,9 +19,12 @@ class App {
     const tryCount = this.checkInputNumber(InputTryCount);
 
     // TODO: 입력받은 횟수만큼 모든 차에 전진 or 멈춤 게임 진행
-    // TODO: 전진할 경우 - 랜덤 숫자 생성 후 4이상일 경우 전진
-    // TODO: 멈출 경우 - 랜덤 숫자 생성 후 3이하일 경우 멈춤
-    // TODO: 한번 진행할 때마다 현재 상태 출력
+    Console.print("\n실행 결과");
+    for (let i = 0; i < tryCount; i++) {
+      this.GoOrStop(cars);
+      // TODO: 한번 진행할 때마다 현재 상태 출력
+    }
+
     // TODO: 게임 종료 후 우승자 출력
   }
 
@@ -39,6 +43,10 @@ class App {
     }
     return Number(InputTryCount);
   }
+
+  // TODO: 전진할 경우 - 랜덤 숫자 생성 후 4이상일 경우 전진
+  // TODO: 멈출 경우 - 랜덤 숫자 생성 후 3이하일 경우 멈춤
+  GoOrStop(cars) {}
 }
 
 export default App;
