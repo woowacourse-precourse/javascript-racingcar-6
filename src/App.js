586ac9efe348db954ac29b6ade4904225f93f5d9
chichoc <kny6152@naver.com>
2023-11-01 06:58:21 +0900

    feat: 자동차 이름 입력받는 함수 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Random, Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  constructor() {
+    this.carName = [];
+  }
+  async play() {
+    this.carName = await this.getCarName();
+  }
+
+  async getCarName() {
+    const input = await Console.readLineAsync(`경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n`);
+    return input.split(',');
+  }
 }
 
 export default App;
