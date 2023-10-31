23fc79a2e0a6796f762b4a22e78f64a87de4e0ad
cho-hyeonjin <gusls2005@gmail.com>
2023-11-01 00:54:44 +0900

    feat: RaceController > 게임 시작 메서드 작성

--- src/controller/RaceController.js
+++ src/controller/RaceController.js
@@ -1,4 +1,5 @@
 import InputView from '../view/InputView';
+import randomNumberGenerator from '../utils/randomNumberGenerator';
 
 export default class RaceController {
   constructor() {
@@ -14,5 +15,24 @@ export default class RaceController {
       this.carList.userInputCarList[index] = { carName: userInputCar, score: 0 };
     });
     return this.carList.userInputCarList;
+  }
+
+  startGame() {
+    let currentPlayNumber = 0;
+    while (this.playNumber > currentPlayNumber) {
+      this.countScore();
+      // Output.playResult(); 해줄 예정
+      currentPlayNumber += 1;
+    }
+    // Output.playWinner(); 해줄 예정
+  }
+
+  countScore() {
+    // 전진 카운팅 함수
+    this.carList.forEach((_, index) => {
+      if (randomNumberGenerator >= 4) {
+        this.carList[index].score += 1;
+      }
+    });
   }
 }
