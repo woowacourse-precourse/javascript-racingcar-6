6a309bdbdb8e34730e7f9a3519823d0371c2d60a
sihyonn <sihyeon982@naver.com>
2023-11-01 01:03:25 +0900

    test: 유효성 검사 테스트 3(시도 횟수가 숫자가 아닐 시)

--- __tests__/additionalTest/ValidationTest.js
+++ __tests__/additionalTest/ValidationTest.js
@@ -1,4 +1,7 @@
-import { validateCarNames } from '../../src/utils/Validation.js';
+import {
+  validateCarNames,
+  validateTryCount,
+} from '../../src/utils/Validation.js';
 import { VALIDATION } from '../../src/constants/MESSAGE.js';
 
 describe('자동차이름 확인', () => {
@@ -13,6 +16,15 @@ describe('자동차이름 확인', () => {
     const 잘못된이름 = ['자동차1']; // 개수 검증에 어긋나는 이름 추가
     expect(() => validateCarNames(잘못된이름)).toThrowError(
       VALIDATION.CAR_COUNT
+    );
+  });
+});
+
+describe('시도횟수 확인', () => {
+  test('시도 횟수가 숫자가 아닐 시 에러', () => {
+    const 잘못된횟수 = 'abc'; // 유효성 검사에 어긋나는 횟수
+    expect(() => validateTryCount(잘못된횟수)).toThrowError(
+      VALIDATION.TRY_COUNT
     );
   });
 });
