d1ad97eba9b2cd8bc72a088b57f4916467817896
mieum12 <qpdlqltb1215@gmail.com>
2023-10-31 18:09:27 +0900

    fix: 반복문을 제거하고 재귀함수로 대체

--- src/controller/GameController.js
+++ src/controller/GameController.js
@@ -1,30 +1,77 @@
-export class GameController {
-  #inputView;
-  #car;
-  #attempt;
-  #outputView;
+import { Cars } from "../domian/cars";
+import InputView from "../view/inputView";
+import OutputView from "../view/outputView";
 
-  constructor({ inputView, outputView, car, attempt }) {
+// @ts-check
+export class GameController {
+  /**
+   * @type {InputView}
+   */
+  #inputView;
+  /**
+   * @type {OutputView}
+   */
+  #outputView;
+  /**
+   *
+   * @param {InputView} inputView
+   * @param {OutputView} outputView
+   */
+  #race
+
+  constructor(inputView, outputView) {
     this.#inputView = inputView;
-    this.#car = car;
     this.#outputView = outputView;
   }
+  /**
+   * @return {Promise<void>}
+   */
 
   async start() {
     // 자동차 이름과 시도 횟수 받아오기
-    const cars = await this.#inputView.getCarsNameFromInput();
-    const attemptCount = await this.#inputView.getAttemptFromInput();
-    for (let i = 0; i < attemptCount; i++) {
-      this.race(cars);
+    const cars = await this.#inputView.inputCars();
+    await this._race(cars);
+  }
+  /**
+   *
+   * @param {Cars} cars
+   * @returns {Promise<void>}
+   */
+
+  // 시도 횟수를 받아와서 _race를 호출
+  async #race(cars) {
+    //처음 한번 실행
+    //attemptCount가 조건 변수인 것 -> 조건 인자를 가져오는 것을 한번 감싸주는 것이다
+    const attemptCount = await this.#inputView.inputAttemptCount();
+    await this.#_race(cars, attemptCount)
+  }
+  /**
+   *
+   * @param {Cars} cars
+   * @param {number} currentAttemptCount
+   * @returns {Promise<void>}
+   * @description - 한 라운드 :반복문은 재귀로 바꾸기, 본체 함수
+  */
+  async #_race(cars, currentAttemptCount) {
+    //재귀함수는 종료 조건을 꼭 써주기
+    if(currentAttemptCount ===0){ 
+      return
     }
-    // 결과 전달
-    const winners = this.race(carsList, attemptCount);
-    this.#outputView.printWinner(winners);
+   cars.moveAll(); // cars가 도메인 객체라 비즈니스 로직 실행
+    const carsDto = cars.makeCarsDto();
+    this.#outputView.printRoundResult(carsDto);
+    this.#_race(cars, currentAttemptCount -1)
+    
   }
 
-  // 시도 횟수만큼 반복
-  async race(cars) {
-    cars.moveAll(); // cars가 도메인 객체라 비즈니스 로직 실행
-    this.#outputView.printGameResult(result);
+  #printWinners(cars) {
+    //winnersDto만들기(winner,winners모두 만들기)
+    // WinnerDto {
+    //   winners: [
+    //     WinnweDto{name:'a'},
+    //     WinnweDto{name:'b'},
+    // ...
+    //   ]
+    // }
   }
 }
