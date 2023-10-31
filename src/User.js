94d196ef63f5cb96d59515451edb088531fa121a
mmeat512 <wndus0958@naver.com>
2023-10-31 17:00:45 +0900

    feat: Error 기능 구현

--- src/User.js
+++ src/User.js
@@ -1,16 +1,25 @@
 import { Console } from '@woowacourse/mission-utils';
-import MESSAGE from './constants/message.js';
+import { MESSAGE, ERROR } from './constants/message.js';
+import ValidationError from './error/ValidationError.js';
+import utils from './utils/utils.js';
+import CAR from './constants/car.js';
 
 class User {
   async getRacingCarList() {
-    const racingCarList = await Console.readLineAsync(MESSAGE.racingCar + '\n');
-    // TODO: 예외처리
-    return racingCarList.split(',');
+    const racingCarList = await Console.readLineAsync(`${MESSAGE.racingCar}\n`);
+    const isCarListValidate = utils.isCarListSeparate(racingCarList);
+    if (!isCarListValidate) {
+      throw new ValidationError(ERROR.carListValidate);
+    }
+    return racingCarList.split(CAR.separateMark);
   }
 
   async getRaceNumber() {
-    const raceNumber = await Console.readLineAsync(MESSAGE.raceNumber + '\n');
-    // TODO: 예외처리
+    const raceNumber = await Console.readLineAsync(`${MESSAGE.raceNumber}\n`);
+    const isValid = utils.isNumberExcludeZero(Number(raceNumber));
+    if (!isValid) {
+      throw new ValidationError(ERROR.raceNumberValidate);
+    }
     return raceNumber;
   }
 }
