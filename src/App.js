5b5190f085882b929ef1b0933da0e4ac706be53b
BellYun <95991290+BellYun@users.noreply.github.com>
2023-11-01 07:48:27 +0900

    style: Airbnb 코드 컨벤션 적용

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import Car from "./Car";
 
 class App {
   async play() {
-    let player = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-    const car = new Car(player);
-    let playTime = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?");
-    car.playgame(playTime)
-    car.pritnResult()
+    let playersName = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
+    const car = new Car(playersName);
+    let playTime = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?');
+    if(isNaN(playTime)){
+      throw new Error('[ERROR] 입력 값이 숫자가 아닙니다');
+    }
+    car.playRacing(playTime);
+    car.printResult();
     return
   }
 }
--- src/car.js
+++ src/car.js
@@ -1,50 +1,51 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class Car{
-    constructor(names){
-        this.name = names.split(',');
-        this.far = [];
-        for( let i = 0; i < this.name.length; i++){
-            this.far.push(0)
-            if(this.name[i].length > 5){
-                throw new Error('[ERROR] 이름은 5자 이하만 가능합니다')
-            }
-        }
+  constructor(names) {
+  this.name = names.split(',');
+  this.far = [];
+  for( let i = 0; i < this.name.length; i++){
+    this.far.push(0);
+      if(this.name[i].length > 5){
+        throw new Error('[ERROR] 이름은 5자 이하만 가능합니다');
+      }
     }
-    playgame(playTime){
-        MissionUtils.Console.print('실행 결과')
-        for(let i = 0; i<playTime; i++){
-            for(let i = 0; i<this.name.length;i++){
-                this.randomRacing(i)
-            }
-            MissionUtils.Console.print(' ')
+  }
+
+  playRacing(playTime) {
+    MissionUtils.Console.print('실행 결과');
+      for(let i = 0; i<playTime; i++){
+        for(let i = 0; i<this.name.length;i++){
+          this.Racing(i);
         }
+        MissionUtils.Console.print('');
+      }
     }
 
-    randomRacing(i){
-      let randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
-      if(randomNumber >= 4 ){
-        this.far[i] = this.far[i] + 1
-      }
-      let mark = ''
-      for(let i = 0; i<this.far[i];i++){
-        mark = mark+'-'
-      }
-      MissionUtils.Console.print(`${this.name[i]} : ${mark}`)
+  Racing(i) {
+    let randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
+    if(randomNumber >= 4 ){
+      this.far[i] = this.far[i] + 1;
     }
+    let mark = '';
+    for(let i = 0; i < this.far[i]; i++){
+      mark = mark + '-';
+    }
+    MissionUtils.Console.print(`${this.name[i]} : ${mark}`);
+  }
 
-    pritnResult(){
-        let winner = ''
-        let maxfar = 0
-        for(let i = 0; i<this.far.length; i++){
-            if(this.far[i] > maxfar){
-                winner = this.name[i]
-            }else if(this.far[i] == maxfar){
-                winner = winner +', '+this.name[i]
-            }
-        }
-        MissionUtils.Console.print(`최종우승자 : ${winner}`)
-    }
+  printResult() {
+  let winner = '';
+      let maxfar = 0;
+      for(let i = 0; i < this.far.length; i++){
+          if(this.far[i] > maxfar){
+              winner = this.name[i];
+          }else if(this.far[i] === maxfar){
+              winner = winner + ', ' + this.name[i];
+          }
+      }
+      MissionUtils.Console.print(`최종우승자 : ${winner}`);
+   }
   }
 
   export default Car
