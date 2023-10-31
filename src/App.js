5bb826b0a46b092393fa998b2cca4783b1d9e457
MINJEONG <lxxmnmn@gmail.com>
2023-10-31 14:10:17 +0900

    Feat: 1. 자동차 이름 입력 기능 & 2. 이동 횟수 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { DEFAULT, LOG, ERROR } from './util/constants.js';
+
 class App {
-  async play() {}
+  async play() {
+    const car = this.input(LOG.INPUT_CAR);
+    const round = this.input(LOG.INPUT_ROUND);
+  }
+
+  async input(log) {
+    const readLine = await MissionUtils.Console.readLineAsync(log);
+
+    switch (log) {
+      case LOG.INPUT_CAR:
+        const arr = readLine.trim().split(',');
+
+        arr.map((value) => {
+          if (value.length > DEFAULT.NAME_MAX_LENGTH)
+            throw new Error(ERROR.NOT_UNDER_LEN);
+        });
+        const set = new Set(arr);
+        if (set.size !== arr.length) throw new Error(ERROR.NOT_UNIQUE);
+
+        return arr;
+      case LOG.INPUT_ROUND:
+        if (isNaN(readLine)) throw new Error(ERROR.NOT_NUMBER);
+        return Number(readLine.trim());
+      default:
+        throw new Error(ERROR.NOT_CAR_OR_ROUND);
+    }
+  }
 }
 
 export default App;
