18204680f6ae6366e261dd39c4ba996aa9ee828e
한지은 <hjn510450@gmail.com>
2023-10-31 19:28:18 +0900

    자동차 이름, 시도 횟수 입력 및 에러 처리 함수 수정(게임 메시지, 에러 메시지 등 상수 처리)

--- src/utils/ProcessInput.js
+++ src/utils/ProcessInput.js
@@ -1,28 +1,32 @@
 import { Console } from '@woowacourse/mission-utils';
+import { GAME_MESSAGE, ERROR_MESSAGE } from '../constants/messages'
+
+const DELIMITER = ',';
+const CAR_NAME_MAX_LENGTH = 5;
 
 /**
  * @description 자동차 이름, 시도 횟수 입력 받기 및 에러 처리 함수
  * @returns 자동차 이름 배열, 시도 횟수
  */
-export default async function ProcessInput() {
-    try {
-        Console.print('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
-        const carName = await Console.readLineAsync();
-        const delimiter = ',';
-        const carNameArr = carName.split(delimiter);
-        if (carNameArr.length !== new Set(carNameArr).size) throw new Error('[ERROR] 자동차 이름이 중복되었습니다.');
-        if (carNameArr.length !== carNameArr.filter((car) => car.length < 5).length) throw new Error('[ERROR] 자동차 이름이 5글자 초과입니다.');
-        Console.print(carName);
+export default async function processInput() {
+  try {
+    Console.print(GAME_MESSAGE.carNameInput);
+    const carName = await Console.readLineAsync();
+    const carNameArr = carName.split(DELIMITER);
 
-        Console.print('시도할 횟수는 몇 회인가요?');
-        const testNum = Number(await Console.readLineAsync());
-        if (testNum === NaN || testNum !== parseInt(testNum) || testNum < 0) throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
-        Console.print(testNum);
-        Console.print('\n');
+    if (carNameArr.length !== new Set(carNameArr).size) throw new Error(ERROR_MESSAGE.duplicateCarName);
+    if (carNameArr.length !== carNameArr.filter((car) => car.length < CAR_NAME_MAX_LENGTH).length) throw new Error(ERROR_MESSAGE.overCarNameMaxLength);
+    Console.print(carName);
 
-        return { carNameArr, testNum }
-    } catch (error) {
-        Console.print(error.message);
-        throw error;
-    }
+    Console.print(GAME_MESSAGE.testNumInput);
+    const testNum = Number(await Console.readLineAsync());
+    if (testNum === NaN || testNum !== parseInt(testNum) || testNum < 0) throw new Error(ERROR_MESSAGE.numFormatError);
+    Console.print(testNum);
+    Console.print(GAME_MESSAGE.lineBreak);
+
+    return { carNameArr, testNum }
+  } catch (error) {
+    Console.print(error.message);
+    throw error;
+  }
 }
