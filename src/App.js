61589b1593fc3ca63ad48b2eabb0d0ad91e06542
criticbanggu@gmail.com <criticbanggu@gmail.com>
2023-11-01 00:26:39 +0900

    Fix :차량 이름, 시도 횟수 검증 class 작성

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
 import { Console, MissionUtils } from "@woowacourse/mission-utils";
+class Validate {
+  tryNumber(tryNumber) {
+    if (isNaN(tryNumber)) {
+      throw new Error("[ERROR] 시도할 횟수는 숫자로만 입력해주세요!")
+    }
+  }
+  carName(carNames) {
+    for (let i = 0; i < carNames.length; i++) {
+      if (carNames[i].length > 6) {
+        throw new Error("[ERROR] 차량 이름이 너무 길어요. 5글자 이하로 적어주세요")
+      }
+    }
+  }
+}
 class Controll {
   constructor() {
     this.carNames = [];
@@ -16,9 +30,6 @@ class Controll {
   }
   carNameValidate() {
     for (let i = 0; i < this.carNames.length; i++) {
-      if (this.carNames[i].length > 6) {
-        throw new Error("[ERROR] 차량 이름이 너무 길어요. 5글자 이하로 적어주세요")
-      }
     }
   }
   checkGoOrStop(carScoreIndex) {
@@ -59,8 +70,8 @@ class App {
       this.controll.carNames = await this.controll.input("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
       this.controll.carNames = this.controll.carNames.split(',');
       this.controll.tryNumber = Number(await this.controll.input("시도할 횟수는 몇 회인가요?\n"));
-      this.controll.carNameValidate();
-      this.controll.tryNumberValidate();
+      this.validate.carName(this.controll.carNames);
+      this.validate.tryNumber(this.controll.tryNumber);
       this.controll.initialCarScore();
       for (let i = 0; i < this.controll.tryNumber; i++) {
         this.controll.makeRandomNumber();
