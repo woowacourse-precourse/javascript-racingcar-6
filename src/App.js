92a2fc62ceb36b763cb571793abe974e099f66d7
gyucheolshin <nsfsgc2295@gmail.com>
2023-10-31 18:10:55 +0900

    feat : 시도횟수 입력

--- src/App.js
+++ src/App.js
@@ -24,6 +24,12 @@ class App {
     const cars = input.split(",");
     this.validateCar(cars);
   }
+
+  async attempts() {
+    const input = await MissionUtils.Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요?\n"
+    );
+  }
 }
 
 export default App;
