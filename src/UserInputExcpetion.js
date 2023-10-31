f5fc22025e373b0261044bed7e8e6a2bfb5dffa3
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2023-10-31 22:29:12 +0800

    feat: add exception of NameException

--- src/UserInputExcpetion.js
+++ src/UserInputExcpetion.js
@@ -4,6 +4,7 @@ async function NamesException() {
     const carNames = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
     if (carNames.split(',').length < 2) throw new Error('[ERROR] 자동차 이름이 잘못된 형식입니다.');
     if (carNames.split(',').some((name) => name.length > 5)) throw new Error('[ERROR] 자동차 이름이 잘못된 형식입니다.');
+    if (carNames.split(',').length !== new Set(carNames.split(',')).size) throw new Error('[ERROR] 자동차 이름이 잘못된 형식입니다.');
     return carNames.split(',');
 }
   
