bc571f4dfcb4801915577bd6db98ab6cbec13d65
CreateGreen <yg.jiseok@gmail.com>
2023-10-31 14:43:30 +0900

    feat : 자동차 이름을 입력받는 기능추가
    - 입력받은 스트링의 예외처리를 위해  함수 작성
    - 5자이하, 중복, 특수문자 예외처리를 통과하면 입력받은 문자열을 리턴

---
+++ __tests__/CarnameexceptionTest.js
@@ -0,0 +1,11 @@
+import App from '../src/App';
+
+describe('자동차 스트링 예외처리', () => {
+  test('이름 5자 이하 제한', () => {
+    const input = ['cake', 'apple', 'banana'];
+  });
+
+  test('이름 중복 테스트', () => {
+    const input = ['cake', 'caake', 'cake'];
+  });
+});
--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { Console, Random } from '@woowacourse/mission-utils';
+
 class App {
   async play() {
-    Console.print('Hello');
+    Console.print('자동차 경주를 시작합니다.');
+    const CAR_STRING = await this.handleInputName();
+    this.inputCarName(CAR_STRING);
   }
 
   constructor() {
@@ -15,8 +18,35 @@ class App {
   inputCarName(carlist) {
     const CAR_LIST = carlist.split(',');
     for (let CAR_NAME of CAR_LIST) {
-      setCarList(CAR_NAME, 0);
+      this.setCarList(CAR_NAME, 0);
     }
+  }
+
+  async handleInputName() {
+    const CAR_STRING = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) : ',
+    );
+    this.validateString(CAR_STRING);
+    return CAR_STRING;
+  }
+
+  validateString(carstring) {
+    const CAR_SPLIT = carstring.split(',');
+    const set = new Set();
+    for (let CAR of CAR_SPLIT) {
+      if (CAR.match(/\./)) {
+        throw new Error('[ERROR] : 자동차 이름은 쉼표로 구분하여야 합니다.');
+      }
+      if (CAR.length > 5) {
+        throw new Error('[ERROR] :  자동차 이름은 5자 이하여야 합니다.');
+      }
+      if (set.has(CAR)) {
+        throw new Error('[ERROR] : 중복된 이름은 사용할 수 없습니다.');
+      } else {
+        set.add(CAR);
+      }
+    }
+    return true;
   }
 }
 
