427b78c6ddf917f1949b927c5698501a9286fa6a
MacBook Air <macbookair@MacBookui-MacBookAir.local>
2023-10-31 19:07:15 +0900

    Feat: Add findMax, print final winner App.js에 이동횟수 배열을 돌면서 가장 큰 값을 찾는 findMax 함수 추가, 찾은 값을 최종 우승자로 출력

--- src/App.js
+++ src/App.js
@@ -28,6 +28,18 @@ function printResult(nameArray, moveArray){
   }
 }
 
+function findMax(nameArray, moveArray, maxIdx){
+  const resultArr=[];
+  resultArr.push(nameArray[maxIdx]);
+  for(let i =maxIdx+1; i<moveArray.length; i++){
+    if(moveArray[maxIdx]===moveArray[i]){
+      resultArr.push(nameArray[i]);
+    }
+  }
+  return resultArr;
+}
+
+
 class App {
   async play() {
     const userInput = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
@@ -47,11 +59,35 @@ class App {
       move.push(0);
     } // 배열 초기화
 
+    process.stdout.write("\n");
+    console.log("실행 결과");
     for(let i=0; i<userNum;i++){
       getRandom(carName.length, move);
       printResult(carName,move);
       process.stdout.write("\n");
     }
+
+    let max = move[0];
+    let maxIndex = 0;
+    for(let i = 0; i<move.length; i++){
+      if(move[i]>max){
+        max = move[i];
+        maxIndex = i;
+      }
+    }
+
+    const winner = findMax(carName, move, maxIndex);
+
+    process.stdout.write("최종 우승자 : ");
+    for(let i =0;i<winner.length;i++){
+      if(i === winner.length-1){
+        process.stdout.write(winner[i]+"\n");
+      }
+      else{
+        process.stdout.write(winner[i] + ", ");
+      }
+    }
+
   }
 }
 
