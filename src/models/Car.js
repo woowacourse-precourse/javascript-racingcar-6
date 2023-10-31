d7a4a16dc49bfc42c71e7c72723b3efc34d3caec
Kysiam <how2logout@gmail.com>
2023-10-31 15:25:30 +0900

    Feat: 자동차의 이름을 반환하는 함수

--- src/models/Car.js
+++ src/models/Car.js
@@ -1,9 +1,20 @@
 class Car {
+  /** @type {string} */
   #carName;
+
+  /** @type {number} */
   #position;
+
+  /**
+   * @param {string} carName
+   */
   constructor(carName) {
     this.#carName = carName;
     this.#position = 0;
+  }
+
+  getCarName() {
+    return this.#carName;
   }
 }
 
