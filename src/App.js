7b967d416e4b3d2717a4ffb3be29fafa6f1036e6
yesiamok3 <jaehyunok@cau.ac.kr>
2023-10-31 23:31:44 +0900

    refactor: space before blocks
    block이 나오기 전 공백 추가

--- src/App.js
+++ src/App.js
@@ -32,11 +32,11 @@ class App {
   }
 }
 
-async function setRaceCars(){
+async function setRaceCars() {
   let carInput = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
   let carStrings = carInput.split(',');
   let cars = [];
-  for (let i = 0; i < carStrings.length; i++){
+  for (let i = 0; i < carStrings.length; i++) {
     const name = carStrings[i];
     const car = new Car(name);
     cars.push(car);
