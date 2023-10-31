899254e871a75b0a35b33404dc7cd4da3e6403f2
Lim-shweety <smile70200@naver.com>
2023-10-31 20:19:25 +0900

    :art: refactor: 메소드 분리 indent 2개 넘어가면 메소드로 분리함 기능에 따라서 메소드로 분리함

--- src/App.js
+++ src/App.js
@@ -1,89 +1,105 @@
 import {Console, MissionUtils, Random} from "@woowacourse/mission-utils";
 
 class App {
-    check_car_name(CARS, CAR_CNT) {
-        for (let i = 0; i < CAR_CNT; i++) {
+    async play() {
+
+        const CARS = await this.getCarsName();
+        const TRY_CNT = await this.getTryCNT();
+
+        const CARS_GO_CNT = this.printCarGo(CARS, CARS.length, TRY_CNT);
+        this.printGameResult(CARS_GO_CNT, CARS);
+
+    }
+
+    async getCarsName() {
+        const CAR_NAME_INPUT = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+        const CARS = CAR_NAME_INPUT.split(",");
+        try {
+            this.checkCarName(CARS);
+        } catch (e) {
+            throw e;
+        }
+
+        return CARS;
+    }
+
+    checkCarName(CARS) {
+        for (let i = 0; i < CARS.length; i++) {
             if (CARS[i].length > 5) {
                 throw new Error("[ERROR] 자동차 이름이 5글자 이상입니다.");
             }
         }
     }
 
-    check_try_cnt(try_cnt_input) {
-        if (Number.isInteger(Number(try_cnt_input)) === false) {
+    async getTryCNT() {
+        let tryCntInput = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+        try {
+            this.checkTryCNT(tryCntInput);
+        } catch (e) {
+            throw e;
+        }
+        return Number(tryCntInput);
+    }
+
+    checkTryCNT(tryCntInput) {
+        if (Number.isInteger(Number(tryCntInput)) === false) {
             throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
         }
     }
 
-    car_go_from_random(CAR_CNT, cnt_cars_go) {
-        let random_value;
-        for (let j = 0; j < CAR_CNT; j++) {
-            random_value = Random.pickNumberInRange(0, 9)
-            if (random_value >= 4) {
-                cnt_cars_go[j] += 1;
-            }
-        }
-        return cnt_cars_go;
-    }
+    printCarGo(CARS, CAR_CNT, TRY_CNT) {
 
-    print_game_state(CARS,CAR_CNT,cnt_cars_go){
-        for (let j = 0; j < CAR_CNT; j++) {
-            Console.print(CARS[j] + ' : ' + '-'.repeat(cnt_cars_go[j]))
-        }
-    }
+        let carGoCNT = Array.from({length: CAR_CNT}, () => 0);
 
-
-    async play() {
-
-        const CAR_NAME_INPUT = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
-        const CARS = CAR_NAME_INPUT.split(",");
-        const CAR_CNT = CARS.length;
-        this.check_car_name(CARS, CAR_CNT)
-
-        let cnt_cars_go = Array.from({length: CAR_CNT}, () => 0);
-
-        let try_cnt_input = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
-
-        this.check_try_cnt(try_cnt_input);
-
-        const TRY_CNT = Number(try_cnt_input);
-
-        Console.print("실행 결과");
-
-
+        Console.print("\n실행 결과");
 
         for (let i = 0; i < TRY_CNT; i++) {
-
-            cnt_cars_go = this.car_go_from_random(CAR_CNT, cnt_cars_go)
-            this.print_game_state(CARS,CAR_CNT,cnt_cars_go)
-            Console.print("")
-
+            carGoCNT = this.carGoFromRandom(CAR_CNT, carGoCNT);
+            this.printEachGameState(CARS, CAR_CNT, carGoCNT);
+            Console.print("");
         }
 
-
-        const WINNER_VALUE = Math.max.apply(Math, cnt_cars_go);
-
-        let winner_index = 1;
-        let winner_print = "최종 우승자 : ";
-        let cnt = 0;
-        while (winner_index !== -1) {
-            cnt += 1;
-            winner_index = cnt_cars_go.indexOf(WINNER_VALUE);
-
-            if (winner_index !== -1) {
-                if (cnt > 1) {
-                    winner_print += ', ';
-                }
-                winner_print += CARS[winner_index]
-                cnt_cars_go[winner_index] = -1;
-
+        return carGoCNT;
+    }
+    carGoFromRandom(carCNT, carGoCNT) {
+        let randomValue;
+        for (let j = 0; j < carCNT; j++) {
+            randomValue = Random.pickNumberInRange(0, 9);
+            if (randomValue >= 4) {
+                carGoCNT[j] += 1;
             }
+        }
+        return carGoCNT;
+    }
+    printEachGameState(CARS, CAR_CNT, carGoCNT) {
+        for (let j = 0; j < CAR_CNT; j++) {
+            Console.print(CARS[j] + ' : ' + '-'.repeat(carGoCNT[j]));
+        }
+    }
 
+    printGameResult(carGoCNT, CARS) {
+        const WINNER_VALUE = Math.max.apply(Math, carGoCNT);
+
+        let winnerIndex ;
+        let winnerSentence = "최종 우승자 : ";
+        let winners = new Array();
+
+        winnerIndex = carGoCNT.indexOf(WINNER_VALUE);
+
+        while (winnerIndex !== -1) {
+            carGoCNT[winnerIndex] = -1
+            winners.push(winnerIndex);
+            winnerIndex = carGoCNT.indexOf(WINNER_VALUE);
         }
 
-        Console.print(winner_print);
+        if(winners.length===1){
+            winnerSentence = winnerSentence + CARS[winners[0]]
+        }
+        else if (winners.length > 1) {
+            winnerSentence = winnerSentence + winners.map((winner) => CARS[winner]).join(', ');
+        }
 
-
+        Console.print(winnerSentence);
     }
 
 
