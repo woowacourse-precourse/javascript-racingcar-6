d4be4d32b221b46d721abfd97a71a398d3346d30
Kidprogramer1039 <bnpo5231@naver.com>
2023-10-31 22:03:32 +0900

    feat : 자동차 게임 기능 함수 구현

--- src/App.js
+++ src/App.js
@@ -6,43 +6,47 @@ class App {
     const game = function racingGame(arr, count) {
       // 배열을 오브젝트로 변경
       let nameObj = {};
-      arr.forEach((element) => {
-        nameObj[element] = 0;
-      });
+      for (let i = 0; i < arr.length; i++) {
+        nameObj[arr[i]] = 0;
+      };
       for (let i = 0; i < count; i++) {
-        for (let j in arr) {
+        for (const j of arr) {
           let randomNum = MissionUtils.Random.pickNumberInRange(1, 9);
-          if (randonNum >= 4) {
+          if (randomNum >= 4) {
             nameObj[j] += 1;
           }
         }
-        
+        for (const k of arr) {
+          const dashes = '-'.repeat(nameObj[k]);
+          Console.print(`${k} : ${dashes}`);
+        }
       }
+      
     }
     
     /** 무응답, 형식 오류 검증 함수 */
-    const error = function validCheck(nameList,runCount) {
+    const error = function validCheck(nameArr, nameList,runCount) {
       let nullCheck = nameList ?? false;
       nullCheck = runCount ?? false;
       for (let i = 0; i < nameArr.length; i++) {
         if (nameArr[i].length >= 5) {
-          throw new Error(`[ERROR] 자동차 이름은 5글자 이내로 입력해주세요.`);
+          throw new Error('[ERROR] 자동차 이름은 5글자 이내로 입력해주세요.');
         }
       }
       if (nullCheck == false) {
-          throw new Error(`[ERROR] 형식이 올바르지 않습니다.`);
+          throw new Error('[ERROR] 형식이 올바르지 않습니다.');
       }
     }
 
-    const nameList = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
-    const runCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
-    const nameArr = nameList.split(",");
-    validCheck(nameArr, runCount);
+    const nameList = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    const runCount = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    const nameArr = nameList.split(',');
+    error(nameArr, nameList, runCount);
 
     // 배열을 오브젝트로 변경
     
 
-    racingGame(nameObj, runCount);
+    game(nameArr, runCount);
     
   }
 }
