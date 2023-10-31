c57cbeea3cc82281f6708ed08994973c8741878e
nari <naringst@gmail.com>
2023-10-31 12:18:06 +0900

    feat : 입력받은 자동차 이름 쉼표 기준 분리

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App {
     const carNameUserInput = await MissionUtils.Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     );
+    const carNames = carNameUserInput.split(",");
   }
 }
 
