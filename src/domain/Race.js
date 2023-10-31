c18970265d5306e3cf1a79a48031e0c86873907b
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-10-31 16:24:19 +0900

    refactor: tryToMoveForward 함수를 Race 클래스에서 RacingCar 클래스로 옮기기

--- src/domain/Race.js
+++ src/domain/Race.js
@@ -1,14 +1,9 @@
-import { Random } from '@woowacourse/mission-utils';
 import Console from '../Console.js';
 import RacingCar from './RacingCar.js';
 
 class Race {
   cars = [];
   attemptCount = 0;
-
-  #START_INCLUSIVE = 0;
-  #END_INCLUSIVE = 9;
-  #BASE_NUMBER = 4;
 
   async prepare() {
     const carNames = await Console.askCarNames();
@@ -20,18 +15,10 @@ class Race {
 
   race() {
     while (this.attemptCount > 0) {
-      this.cars.forEach((car) => this.tryToMoveForward(car));
+      this.cars.forEach((car) => car.tryToMoveForward());
       this.attemptCount -= 1;
     }
     console.log(this.cars);
-  }
-
-  tryToMoveForward(car) {
-    const randomNumber = Random.pickNumberInRange(
-      this.#START_INCLUSIVE,
-      this.#END_INCLUSIVE,
-    );
-    if (this.#BASE_NUMBER <= randomNumber) car.moveForward();
   }
 }
 
--- src/domain/RacingCar.js
+++ src/domain/RacingCar.js
@@ -1,7 +1,20 @@
+import { Random } from '@woowacourse/mission-utils';
+
 class RacingCar {
+  #START_INCLUSIVE = 0;
+  #END_INCLUSIVE = 9;
+  #STANDARD_NUMBER = 4;
   constructor(name) {
     this.name = name;
     this.moveCount = 0;
+  }
+
+  tryToMoveForward() {
+    const randomNumber = Random.pickNumberInRange(
+      this.#START_INCLUSIVE,
+      this.#END_INCLUSIVE,
+    );
+    if (this.#STANDARD_NUMBER <= randomNumber) this.moveForward();
   }
 
   moveForward() {
