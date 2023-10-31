8367624f73478a2d49b5a6e3894ad8129c429abf
shgusgh12 <https://github.com/shgusgh12>
2023-10-31 11:02:36 +0900

    feat: 자동차 이름 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+import MESSAGE from './Constant.js';
+import { checkCarNameDuplicate, checkCarNameLength } from './Validate.js';
+import Car from './Car.js';
 class App {
-  async play() {}
+  constructor() {
+    this.cars = [];
+  }
+  initCarList(carNameArray) {
+    carNameArray.forEach((carName) => {
+      this.cars.push(new Car(carName));
+    });
+  }
+
+  async getCarName() {
+    const inputCarName = await Console.readLineAsync(MESSAGE.INPUT_CAR_NAME);
+    const carNameArray = inputCarName.split(',');
+    if (
+      !checkCarNameDuplicate(carNameArray) ||
+      !checkCarNameLength(carNameArray)
+    )
+      throw new Error('[ERROR] 자동차 이름을 다시 확인해주세요');
+    this.initCarList(carNameArray);
+  }
+
+  async play() {
+    await this.getCarName();
+    console.log(this.cars);
+  }
 }
 
 export default App;
