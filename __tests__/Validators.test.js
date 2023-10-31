bf9034b24c3d3f05329acb44e4aa34145f58c573
ryan-dia <cjfdnjs158@naver.com>
2023-10-31 16:55:14 +0900

    test(validator): 잘못 설계한 유효성 확인  테스트 수정
    checkRacingVehicleName() 메서드는 return 값이 없다
    올바른 값을 입력하면 에러를 발생시키지 않고
    올바르지 않은 값을 입력하면 에러를 발생시킨다

--- __tests__/Validators.test.js
+++ __tests__/Validators.test.js
@@ -1,24 +1,28 @@
-import Validators from '../src/utils/validator';
+import Validators from '../src/utils/validator/index.js';
 
 describe('Validators test', () => {
   describe('checkRacingVehicleName', () => {
     test.each([
-      ['aa,bb,cc,dd', 'true'],
-      ['도레,미파,솔라,시도', 'true'],
-      ['도a,레b,미c', 'true'],
-      ['airbnb', 'where', 'tomato', 'true'],
-    ])(' %s을 입력하였을 때 return %s', (input, expected) => {
-      expect(Validators.checkRacingVehicleName(input)).toBe(expected);
+      ['aa,bb,cc,dd'],
+      ['도레,미파,솔라,시도'],
+      ['도a,레b,미c'],
+      ['airbnb', 'where', 'tomato'],
+    ])(' %s을 입력하였을 때 에러가 발생하지 않는다.', (input) => {
+      expect(() => {
+        Validators.checkRacingVehicleName(input);
+      }).not.toThrow();
     });
 
     test.each([
-      ['23,45,23', false],
-      ['도레.미파.솔라', false],
-      [',aa,bb,cc,', false],
-      ['aa/bb/cc/dd', false],
-      ['도레23,mi#,tomato', false],
-    ])(' %s을 입력하였을 때 return %s', (input, expected) => {
-      expect(Validators.checkRacingVehicleName(input)).toBe(expected);
+      ['23,45,23'],
+      ['도레.미파.솔라'],
+      [',aa,bb,cc,'],
+      ['aa/bb/cc/dd'],
+      ['도레23,mi#,tomato'],
+    ])(' %s을 입력하였을 때 return %s', (input) => {
+      expect(() => {
+        Validators.checkRacingVehicleName(input);
+      }).toThrow();
     });
   });
 });
