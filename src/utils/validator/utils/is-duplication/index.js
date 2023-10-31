0d5414e585aecaf4cf86de1d97c651c0772ecdf1
ryan-dia <cjfdnjs158@naver.com>
2023-10-31 16:40:22 +0900

    feat(validator): 자동차 이름 중복성 체크 기능 구현

---
+++ src/utils/validator/utils/is-duplication/index.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string[]} input
+ * @returns {boolean}
+ */
+export default function isDuplication(input) {
+  const inputSet = new Set([...input]);
+
+  return input.length !== inputSet.size;
+}
