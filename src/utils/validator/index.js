32a5c7c0d47bdb9f0b57baf70845e174b816e13d
ryan-dia <cjfdnjs158@naver.com>
2023-10-31 23:35:51 +0900

    feat(validator): 유효성 통과 못할 시 에러 발생하도록 설정

--- src/utils/validator/index.js
+++ src/utils/validator/index.js
@@ -1,5 +1,7 @@
+import { ERROR_MESSAGE, ERROR_MESSAGE_FUNCTION } from '../../constants/Messages.js';
 import { LANGUAGE_OPTION, SYSTEM } from '../../constants/System.js';
 import Converter from '../StringConvertor.js';
+import ValidationError from '../error/ValidationError.js';
 import {
   isDuplication,
   isValidDelimiter,
@@ -10,13 +12,20 @@ import {
 
 const Validators = {
   checkRacingVehicleName(input) {
-    const namesArray = Converter.splitStringToArrayByDelimiter(input, SYSTEM.delimiter);
+    const { delimiter, nameLengthMin, nameLengthMax, quantityMin, quantityMax } = SYSTEM;
+    const { delimiterError, nameLength, language, quantity } = ERROR_MESSAGE_FUNCTION;
+    const namesArray = Converter.splitStringToArrayByDelimiter(input, delimiter);
 
-    isValidDelimiter(input, SYSTEM.delimiter);
-    isValidNameLength(namesArray);
-    isValidLanguage(namesArray, LANGUAGE_OPTION);
-    isValidQuantity(namesArray);
-    isDuplication(namesArray);
+    if (!isValidDelimiter(input, delimiter)) {
+      throw new ValidationError(delimiterError(delimiter));
+    }
+    if (!isValidNameLength(namesArray)) {
+      throw new ValidationError(nameLength(nameLengthMin, nameLengthMax));
+    }
+    if (!isValidLanguage(namesArray, LANGUAGE_OPTION))
+      throw new ValidationError(language(LANGUAGE_OPTION));
+    if (!isValidQuantity(namesArray)) throw new ValidationError(quantity(quantityMin, quantityMax));
+    if (isDuplication(namesArray)) throw new ValidationError(ERROR_MESSAGE.duplication);
   },
 };
 
