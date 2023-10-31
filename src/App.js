9d980102243f697a23279d8908b5e44618162ca4
Klomachenko <leekyumin0901@naver.com>
2023-11-01 02:38:00 +0900

    feat(app): 사용자로부터 자동차 이름 입력 기능 구현
    Description: 사용자로부터 자동차 이름을 입력받는 기능을 구현하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 class App {
-  async play() {}
+  async play() {
+    const carNames = await this.getCarNameInput();
+  }
+
+  async getCarNameInput() {
+    const carNamesInput = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)로 구분, 5자 이하):");
+    return carNamesInput
+  }
 }
 
 export default App;
