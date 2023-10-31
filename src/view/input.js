e301d17d0d559cf00f24bfb43f2df307127c103c
soosoo22 <lllllh00@naver.com>
2023-11-01 00:29:20 +0900

    불필요한 주석 제거

--- src/view/input.js
+++ src/view/input.js
@@ -1,6 +1,5 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
-// 폴더 변경
 class Input {
   static async text(message) {
     const carName = await MissionUtils.Console.readLineAsync(message);
--- src/view/output.js
+++ src/view/output.js
@@ -1,6 +1,5 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
-// 폴더 변경
 class Output {
   static text(message) {
     return MissionUtils.Console.print(message);
