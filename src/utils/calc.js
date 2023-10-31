39daa2596bf6849c7ad023f606dc212ced667beb
meaculpa9450 <132068319+meaculpa9450@users.noreply.github.com>
2023-10-31 15:58:30 +0900

    feat: 유틸리티 기능 추가

--- src/utils/calc.js
+++ src/utils/calc.js
@@ -9,3 +9,13 @@ export const pickProgressRandomBool = () => {
   const randomValue = MU.Random.pickNumberInRange(MIN, MAX);
   return Boolean(GAME_RULE.MIN_NUM_TO_PROGRESS <= randomValue);
 };
+
+export const getWinnerArray = (cars) => {
+  const maxScore = getMaxProgress(cars);
+  return getMaxProgressCars(cars, maxScore);
+};
+
+const getMaxProgress = (cars) =>
+  Math.max(...Array.from(cars, (car) => car.progress));
+const getMaxProgressCars = (cars, maxScore) =>
+  cars.filter((car) => car.progress === maxScore).map((car) => car.name);
