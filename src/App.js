dfd26b6308549ef72cf2d0776ddfdaffb0bb0baa
kwaksj329 <kwaksj329@gmail.com>
2023-10-31 15:42:23 +0900

    feat: 입력받은 수만큼 게임 진행 기능 추가

--- src/App.js
+++ src/App.js
@@ -8,7 +8,28 @@ class App {
 
     const racingCarList = inputCar.split(',');
     const distanceList = Array.from({length: racingCarList.length}, () => 0);
+    for(let i = 0 ; i < inputNumber ; i++){
+      racingCarList.forEach((carName, index) => {
+        if(Random.pickNumberInRange(1, 9) >= 4){
+          distanceList[index]++;
+          Console.print(`${carName} : ${"-".repeat(distanceList[index])}`)
+        }
+      })
+    }
+    const maxDistance = Math.max(...distanceList);
+    const winnerCarList = racingCarList.reduce((acc, carName, index) => {
+      if(distanceList[index] === maxDistance){
+        acc.push(carName);
+      }
+      return acc
+    }, [])
+    if(winnerCarList.length === 1){
+      Console.print(`최종 우승자 : ${winnerCarList[0]}`)
+    }else{
+      Console.print(`최종 우승자 : ${winnerCarList.join(', ')}`)
+    }
   }
+
 }
 
 export default App;
