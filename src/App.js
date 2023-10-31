8f64efdae9a1db3d4306e52bf3244b4d27c04234
j8won <joowoni99@gmail.com>
2023-11-01 01:59:31 +0900

    feat(App): Create the play method for starting and executing the game

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import InputHandler from "./InputHandler.js";
+import Car from "./Car.js";
+import RacingGame from "./RacingGame.js";
+import OutputHandler from "./OutputHandler.js";
+
 class App {
-  async play() {}
+  #carNameArray;
+  #attemptCount;
+
+  async play() {
+    await this.#getUserInput();
+
+    const game = new RacingGame(
+      this.#carNameArray.map((name) => new Car(name)),
+      this.#attemptCount
+    );
+    game.play();
+
+    const allRoundResult = game.resultArray;
+    OutputHandler.printGameExecution(allRoundResult);
+
+    const winnerArray = game.getWinnerNameArray();
+    OutputHandler.printWinner(winnerArray);
+  }
+
+  async #getUserInput() {
+    this.#carNameArray = await InputHandler.getCarNameArray();
+    this.#attemptCount = await InputHandler.getMoveAttemptCount();
+  }
 }
 
 export default App;
