d46542e908df287cfd76702aa3f8828596133aa8
gillyongs <khs20010327@gmail.com>
2023-11-01 04:22:09 +0900

    feat: 입출력 자동 실행 코드 구현

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ class App {
       Console.print('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
       const carNamesInput = await Console.readLineAsync();
       const carNames = carNamesInput.split(',');
+
+      Console.print('시도할 횟수는 몇 회인가요?');
+      const tryCountInput = await Console.readLineAsync();
+      const tryCount = parseInt(tryCountInput);
+
+      if (isNaN(tryCount)) {
+        throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
+      }
+
     }
     catch (error) {
       Console.print(error.message);
