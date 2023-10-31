3aae4bcc07c4279e14ee0a6746c6f9fa37b79d11
yu-retel <dbdudals100@naver.com>
2023-11-01 04:10:01 +0900

    feat: APP (게임진행하는 역할)구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import InputView from "./view/InputView.js";
+import OutputView from './view/OutputView.js';
+
+import RacingManager from './RacingManager.js';
+import Data from './Data.js';
+import ErrorHandler from './ErrorHandler.js';
+
 class App {
-  async play() {}
+  constructor() {
+    this.data = new Data();
+  }
+
+  async play() {
+    const carNames = await InputView.getCarNames();
+    ErrorHandler.carNamesType(carNames);
+    const count = await InputView.getAttemptsCount();
+    ErrorHandler.countType(count);
+  
+    const refinedCarNames = this.data.carNamesTypeConversion(carNames);
+    const manager = new RacingManager(refinedCarNames, count, this.data);
+  
+    OutputView.racingProgress(refinedCarNames, manager.racingResult());
+    OutputView.printWinner(manager.determineWinner());
+  }
+  
 }
 
 export default App;
+
+let a = new App();
+a.play();
