abc5e7e4b96f1345f4474393c2e8b40bdb3d9933
cowhead9805 <cowhead9805@gmail.com>
2023-10-31 11:53:56 +0900

    자동차 이름 입력 기능_에러기능추가

---
+++ src/Computer.js
@@ -0,0 +1,4 @@
+import { Random } from '@woowacourse/mission-utils';
+
+const randomNumber = Random.pickNumberInRange(0, 9);
+const canGoNumber = randomNumber >= 4;
---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+export async function carNames() {
+//     const carNames = await getCarNames();
+
+//     if (carNames === null) {
+//         return;
+//     }
+
+//     console.Print(`입력한 자동차 이름: ${carNames.join(', ')}`);
+// }
