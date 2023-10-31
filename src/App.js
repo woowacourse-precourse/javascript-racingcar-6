eb1469c0c8a00682216bf7c2b332d0854029ff0f
youngchann <lych96@gmail.com>
2023-10-31 14:57:35 +0900

    feat: 각 차수별 실행 결과 표시 기능 추가
    - moveCar() : 자동차 이동 기능
    - printResult() : 결과 표시 기

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ class App {
     this.startMessage =
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n';
     this.countMessage = '시도할 횟수는 몇 회인가요?\n';
+    this.resultMessage = '실행 결과';
+    this.carData = {};
   }
 
   async play() {
@@ -12,17 +14,23 @@ class App {
     const userCount = await MissionUtils.Console.readLineAsync(
       this.countMessage,
     );
-    if (!App.isValidCar(userCar)) {
+    const carNames = userCar.split(',');
+    if (!App.isValidCar(carNames)) {
       throw new Error('[ERROR] 자동차 이름을 잘못 입력했습니다.');
     }
     if (!App.isValidNumber(userCount)) {
       throw new Error('[ERROR] 시도 횟수를 잘못 입력했습니다.');
     }
+    carNames.forEach(name => {
+      this.carData[name] = '';
+    });
+    for (let i = 0; i < userCount; i++) {
+      this.moveCar();
+      this.printResult();
+    }
   }
 
-  static isValidCar(userCar) {
-    const carNames = userCar.split(',');
-    console.log(carNames);
+  static isValidCar(carNames) {
     if (carNames.every(name => name.length > 5)) return false;
     if (carNames.some(name => name.trim().length === 0)) return false;
     return true;
@@ -30,8 +38,24 @@ class App {
 
   static isValidNumber(userCount) {
     if (Number.isNaN(Number(userCount))) return false;
-    if (userCount > 9 || userCount < 0) return false;
+    if (userCount <= 0) return false;
     return true;
+  }
+
+  moveCar() {
+    Object.keys(this.carData).forEach(name => {
+      const randNumber = MissionUtils.Random.pickNumberInRange(0, 9);
+      if (randNumber >= 4) {
+        this.carData[name] += '-';
+      }
+    });
+  }
+
+  printResult() {
+    MissionUtils.Console.print(this.resultMessage);
+    Object.keys(this.carData).forEach(name => {
+      MissionUtils.Console.print(`${name} : ${this.carData[name]}`);
+    });
   }
 }
 
--- src/components/CarName.js
+++
@@ -1,9 +0,0 @@
-import { MissionUtils } from '@woowacourse/mission-utils';
-
-const CarName = async startMessage => {
-  const userInput = await MissionUtils.Console.readLineAsync(startMessage);
-  const list = [];
-  userInput.map(car => list.push(car));
-};
-
-export default CarName;
