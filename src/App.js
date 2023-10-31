01b6b4af22e370493c3a4fe5d52d38a14cefde1d
hyunjun9788 <namhyunjun9788@gmail.com>
2023-10-31 17:53:17 +0900

    Fix: 공백 수정

--- src/App.js
+++ src/App.js
@@ -55,17 +55,18 @@ class App {
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     );
   }
+
   async inputCarName() {
     const carNames = await Console.readLineAsync("");
 
     return carNames.split(",");
   }
+
   isValidCarNames() {
     for (let i = 0; i < this.carNameList.length; i++) {
       const isValidCarName = !(
         this.carNameList[i].length > 5 || this.carNameList[i].length === 0
       );
-
       if (!isValidCarName) {
         return false;
       }
