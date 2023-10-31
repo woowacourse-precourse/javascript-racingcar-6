c6b0fbd7a5205422ccb8e334e455d1ae8a10ff41
장호빈 <devhobin@gmail.com>
2023-10-31 16:33:48 +0900

    test(ApplicationTest): 최종 우승자 출력 테스트 추가
    - 최종 우승자를 출력 테스트케이스를 추가하였다

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -115,4 +115,26 @@ describe("자동차 경주 게임", () => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
     });
   });
+
+  test("최종 우승자 출력 테스트", async () => {
+    // given
+    const MOVING_FORWARD = 4;
+    const STOP = 3;
+    const inputs = ["pobi,woni", "1"];
+    const outputs = ["최종 우승자 : pobi"];
+    const randoms = [MOVING_FORWARD, STOP];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([...randoms]);
+
+    // when
+    const app = new App();
+    await app.play();
+
+    // then
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
 });
