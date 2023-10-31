bab4e5f8b677421d79ec28527c8702402437f8b8
steve-shim <steveshim496@gmail.com>
2023-10-31 11:34:07 +0900

    Make Car class

--- src/App.js
+++ src/App.js
@@ -1,3 +1,30 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+class Car {
+  constructor(name) {
+    this.name = name;
+    this.distance = 0;
+  }
+  static reportDistance(...cars) {
+    let carDistances = cars.reduce((acc, car) => {
+      if (acc[car.name]) {
+        acc[car.name] += car.distance;
+      } else {
+        acc[car.name] = car.distance;
+      }
+      return acc;
+    }, {});
+
+    for (let key in carDistances) {
+      MissionUtils.Console.print(`${key} : ` + "-".repeat(carDistances[key]));
+    }
+    return carDistances;
+  }
+  static addDistance(car) {
+    car.distance += 1;
+  }
+}
+
 class App {
   async play() {}
 }
