d78948542b4d3c30b68364406265a8e413f528e2
jinjaehwan <jjh099@icloud.com>
2023-10-31 23:09:49 +0900

    feat(ConsoleView) : printRaceResult(경기결과를 받아와 출력해주는 함수) 생성

--- src/controllers/RaceController.js
+++ src/controllers/RaceController.js
@@ -1,18 +1,25 @@
-import { Random } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
+import ConsoleView from "../views/ConsoleView.js";
 class RaceController {
   static RESULT_NOTIFICATION = "실행 결과";
+  constructor() {
+    this.consoleView = new ConsoleView();
+  }
 
   createNumber = (carList) => {
     for (const car in carList) {
       const moveNum = Random.pickNumberInRange(0, 9);
-      carList[car] = moveNum;
+      if (moveNum >= 4) {
+        carList[car] += 1;
+      }
     }
-    return carList;
+    this.consoleView.printRaceResult(carList);
   };
 
   moveCar = (inputChance, carList) => {
+    Console.print(RaceController.RESULT_NOTIFICATION);
     for (let i = 0; i < inputChance; i++) {
-      const number = this.createNumber(carList);
+      this.createNumber(carList);
     }
   };
 }
--- src/views/ConsoleView.js
+++ src/views/ConsoleView.js
@@ -1,2 +1,12 @@
-class ConsoleView {}
+import { Console } from "@woowacourse/mission-utils";
+class ConsoleView {
+  static MOVE_LINE = "-";
+
+  printRaceResult(carList) {
+    for (const car in carList) {
+      Console.print(`${car} : ${ConsoleView.MOVE_LINE.repeat(carList[car])}`);
+    }
+    Console.print("");
+  }
+}
 export default ConsoleView;
