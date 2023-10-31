14f8ca196833a69a5a0e7c5509232cf3602e0c38
byungyong <quddyddlrk@naver.com>
2023-10-31 21:35:33 +0900

    feat:get the name of the car user want to race

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    Console.print(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+    const USER_INPUT = await Console.readLineAsync("");
+    const CARS = USER_INPUT.split(",");
+
+    if (CARS.length !== 3) {
+      throw new Error("[Error] 경주할 자동차는 총 3대입니다.");
+    }
+
+    for (let car of CARS) {
+      if (car.length > 5) {
+        throw new Error("[Error] 각 자동차의 이름은 5글자 이하여야 합니다.");
+      }
+    }
+  }
 }
 
 export default App;
