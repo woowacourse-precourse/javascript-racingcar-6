e357c349ad6055384442afd767e7b0ad1fcdbe2b
gillyongs <khs20010327@gmail.com>
2023-11-01 05:49:52 +0900

    feat: car 클래스 정의 및 map으로 생성

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import { Console } from "@woowacourse/mission-utils";
+import { Car } from "./car.js";
 
 class App {
   async play() {
-    const carNamesInput = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
+    const carNamesInput = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
     const carNames = carNamesInput.split(',');
     this.checkCarNames(carNames);
-    
-    const tryCountInput = await Console.readLineAsync('시도할 횟수는 몇 회인가요?');
+    //Console.print(carNames);
+
+    const tryCountInput = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
     const tryCount = parseInt(tryCountInput);
+    if (isNaN(tryCount)) {
+      throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
+    }
+    //Console.print(tryCountInput);
+
+    const cars = carNames.map((name) => new Car(name));
+    //Console.print(cars);
+ 
+    
   }
 
   checkCarNames = (carNames) => {
@@ -15,6 +26,8 @@ class App {
       if(carName.length > 5){ throw new Error('[ERROR] 자동차 이름은 5자 이하여야 합니다.')}
     }
   }
+
+ 
 }
 
 export default App;
