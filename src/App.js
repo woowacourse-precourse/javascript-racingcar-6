d14a3cd7e74866aa97612cb595db48cb5c94f2c6
dongminlee <gmgmgun@gmail.com>
2023-10-31 18:19:10 +0900

    feat: 자동차 이름 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+const INPUT_CAR_NAMES_MSG =
+  '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)';
+
 class App {
-  async play() {}
+  async getCarNameArr() {
+    try {
+      const carNames = await MissionUtils.Console.readLineAsync(
+        INPUT_CAR_NAMES_MSG
+      );
+      const carNameArr = carNames.split(',');
+      return carNameArr;
+    } catch (error) {
+      MissionUtils.Console.print('입력 받는 도중 에러가 발생했습니다.', error);
+      return [];
+    }
+  }
+
+  async play() {
+    const carNameArr = await this.getCarNameArr();
+  }
 }
 
 export default App;
