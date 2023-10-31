d6e965c40ba4bf61a4833c9a589130e692dc819c
kinajin <kinajin22@gmail.com>
2023-11-01 05:04:11 +0900

    FIX:bug fixed

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import { getCarName, getRoundNum } from "./Computer.js";
-import Car from "./Car.js";
+import { getCarName, getRoundNum, compete } from "./Computer.js";
 
 class App {
   async play() {
     let carNames = await getCarName();
     let round = await getRoundNum();
 
-    MissionUtils.Console.print("\n실행 결과");
-
-    //종민이에게 물어보기
-    // for (let car of carNames) {
-    //   car = new Car(car);
-    //   // console.log(car);
-    //   // console.log(car.resultString);
-    // }
-
-    let cars = carNames.map((name) => new Car(name));
-
-    for (let i = 0; i < round; i++) {
-      for (let car of cars) {
-        // car = new Car(car);
-        car.resultString = car.runOrNot(car.resultString);
-        MissionUtils.Console.print(`${car.name} : ${car.resultString}`);
-        // console.log(car);
-      }
-      MissionUtils.Console.print(`\n`);
-    }
-
-    MissionUtils.Console.print("최종 우승자 : pobi, jun");
+    compete(carNames, round);
   }
 }
 
