bc2557cf798de340f7dc726eb9e6ce91cbd5a45e
mieum12 <qpdlqltb1215@gmail.com>
2023-10-31 18:12:32 +0900

    fix: game controller라는 인스턴스로 만들어서 가져오기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+// @ts-check
 import { GameController } from "./controller/GameController";
+import { InputConverter } from "./view/inputConverter";
+import { InputValidator } from "./view/inputValidator";
+import InputView from "./view/inputView";
+import OutputView from "./view/outputView";
 class App {
+  /**
+   * @returns {Promise<void>}
+   */
   async play() {
-    await GameController.start();
+    // 클래스는 하나, 인스턴스는 만들기 나름
+    const gameController = new GameController(
+      new InputView(new InputValidator(), new InputConverter()),
+      new OutputView()
+    );
+    await gameController.start();
+    //static이면(static start~) 클래스명.메소드() 가능한데, GameController라는 인스턴스가 없음, static끼리만 사용 가능, static은 클래스명으로만 불러올 수 있음
   }
 }
 
