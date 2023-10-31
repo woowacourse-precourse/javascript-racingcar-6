b6417f01d4ea9dea4754fd56cd2fc6c599f7ae02
Jeongminchang <min413903@naver.com>
2023-10-31 20:57:31 +0900

    1-1 : 입력을 받는 부분을 구성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const nameInput = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+    const tryNum = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+    const names = nameInput.split(',')
+  }
 }
 
 export default App;
