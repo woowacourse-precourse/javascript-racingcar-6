aced56b2bd15abe84d75525f045b3b5ed2819d38
Myomyoring <narcissuscat@naver.com>
2023-11-01 00:03:50 +0900

    [feat] 최종 우승자 출력 및 디폴트 테스트 완료

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -32,22 +32,36 @@ export class RacingGame {
 		return Random.pickNumberInRange(RULE.MIN_LIMIT, RULE.MAX_LIMIT);
 	}
 
+	roundResult(readyCars) {
+		for (let car of readyCars) {
+			const random = this.getRandomNumber();
+			RULE.RUN_LIMIT <= random ? car.run() : '';
+			car.state();
+		}
+		Console.print('');
+	}
+
 	racingStart(tryNumber, readyCars) {
 		Console.print(MESSAGE.STARTING_TITLE);
 		for (let round = 0; round < tryNumber; round++) {
-			for (let car of readyCars) {
-				const random = this.getRandomNumber();
-				RULE.RUN_LIMIT <= random ? car.run() : '';
-				car.state();
-			}
-			Console.print('');
+			this.roundResult(readyCars);
 		}
+	}
+
+	finalResult(readyCars) {
+		const maxScore = Math.max(...readyCars.map((car) => car.score));
+		const win = readyCars.filter((car) => maxScore === car.score);
+
+		const winners = win.map((a) => a.car);
+		Console.print(`최종 우승자 : ${winners.toString()}`);
 	}
 
 	async start() {
 		const cars = await this.getCars();
 		const readyCars = this.setRacing(cars);
 		const tryNumber = await this.getTryNumber();
+
 		this.racingStart(tryNumber, readyCars);
+		this.finalResult(readyCars);
 	}
 }
