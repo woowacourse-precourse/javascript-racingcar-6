79a503b79de9e34963f7c493677c127bc12bc624
SWBAE0501 <tkddnjsgla@naver.com>
2023-10-31 17:43:53 +0900

    FEAT: class Car added, DOC:README.md modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import Car from './car.js';
+import race from './race.js';
+import victory from './victory.js';
+
+
 class App {
-  async play() {}
+  async play() {
+    const carNamesInput = await MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): "
+    );
+    const carNames = carNamesInput.split(',').map(name => name.trim());
+    const cars = carNames.map(name => new Car(name));
+
+    this.race();
+    this.victory();
+  }
 }
 
 export default App;
