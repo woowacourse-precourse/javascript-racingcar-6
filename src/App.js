63a85b87d56a90fb300772f7990aa1f76a8c805d
deeyo <130573068+jeehyojeon@users.noreply.github.com>
2023-10-31 18:21:52 +0900

    feat: input carNames

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-class App {
-  async play() {}
-}
+import { Random, Console } from "@woowacourse/mission-utils";
 
-export default App;
+class App {
+  constructor() {
+    this.roundCount = null;
+  }
+
+  async play() {
+    const input = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    const carNames = input.split(",");
+  }
+}
