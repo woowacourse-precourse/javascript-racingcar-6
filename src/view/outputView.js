c90884c9b75cacfba75323cf5e565de64c5771a5
mieum12 <qpdlqltb1215@gmail.com>
2023-10-31 17:59:08 +0900

    feat: 처음 라운드 실행 시에만 메세지가 뜨는 코드를 함수로 만들어 구현

--- src/view/outputView.js
+++ src/view/outputView.js
@@ -1,14 +1,41 @@
+// @ts-check
 import { Console } from "@woowacourse/mission-utils";
-import { MESSAGE } from "../constants/constants";
+import { SYMBOL, MESSAGE } from "../constants/constants";
+import { CarsDto } from "../domian/dto/carsDto";
 
 class OutputView {
-  printGameResult(car, progress) {
-    const result = `${car} : ${SYMBOL.MOVE.repeat(progress)}`;
-    Console.print(result);
+  /**
+   * @type {boolean}
+   */
+  #isFirstCall = true;
+
+  /**
+   *
+   * @param {CarsDto} CarsDto
+   * @description - 각 라운드의 결과를 출력
+   */
+  printRoundResult(CarsDto) {
+    this.#printStartMessageWhenFirstCall();
+
+    CarsDto.cars.forEach(({ name, distance }) => {
+      Console.print(`${name} : ${"-".repeat(distance)}`);
+    });
+    Console.print("");
   }
+
   printWinner() {
-    const winners = cars.join(",");
-    Console.print(MESSAGE.GAME_RESULT + `${winners}`);
+    // const winners = cars.join(",");
+    // Console.print(MESSAGE.GAME_RESULT + `${winners}`);
+  }
+  /**
+   * @returns {void}
+   */
+
+  #printStartMessageWhenFirstCall() {
+    if (this.#isFirstCall) {
+      Console.print("\n실행결과");
+      this.#isFirstCall = false;
+    }
   }
 }
 
