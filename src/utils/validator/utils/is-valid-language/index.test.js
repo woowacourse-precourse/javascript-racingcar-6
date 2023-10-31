56908ebc700d744e31cf43b851d2a0a0405cc980
ryan-dia <cjfdnjs158@naver.com>
2023-10-31 12:38:03 +0900

    test(validator): 의존성 주입으로 인한 Converter 유틸 추가

--- src/utils/validator/utils/is-valid-language/index.test.js
+++ src/utils/validator/utils/is-valid-language/index.test.js
@@ -1,3 +1,5 @@
+import { LANGUAGE_OPTION, SYSTEM } from '../../../../constants/System.js';
+import Converter from '../../../StringConvertor.js';
 import isValidLanguage from './index.js';
 
 describe('isValidLanguage', () => {
@@ -6,7 +8,8 @@ describe('isValidLanguage', () => {
     ['사랑해요,한국,러뷰스', true],
     ['하입boy,new진스,coco샤넬', true],
   ])('%s 값을 입력시 return %s', (input, expected) => {
-    expect(isValidLanguage(input)).toBe(expected);
+    const names = Converter.splitStringToArrayByDelimiter(input, SYSTEM.delimiter);
+    expect(isValidLanguage(names, LANGUAGE_OPTION)).toBe(expected);
   });
 
   test.each([
@@ -14,6 +17,7 @@ describe('isValidLanguage', () => {
     ['_#_$,~!@$', false],
     ['오호_, ,도#', false],
   ])('%s 값을 입력시 return %s', (input, expected) => {
-    expect(isValidLanguage(input)).toBe(expected);
+    const names = Converter.splitStringToArrayByDelimiter(input, SYSTEM.delimiter);
+    expect(isValidLanguage(names, LANGUAGE_OPTION)).toBe(expected);
   });
 });
