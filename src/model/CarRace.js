d17343ff06cde4c4b56e685b40af261b3919c1d8
yeokyoung song <spolice0324@gmail.com>
2023-10-31 20:46:35 +0900

    refactor : CarRace 클래스 역할 및 로직 수정
    - 자동차 이름과 전진데이터의 반환 포맷팅 없이 반환할 수있도록 로직 개편
    - 유효성 검증 inputView으로 이동
    -getRaceRound 에서 난수를 생성하고, 해당 난수가 전진 조건인지 판별

--- src/model/CarRace.js
+++ src/model/CarRace.js
@@ -1,62 +1,20 @@
-import { validCarName, validtryCount } from '../utils/validateInput';
+import { FORWARD } from '../utils/constants';
+import getRandomNumber from '../utils/getRandomNumber';
 import Car from './Car';
 
 class CarRace {
-  constructor() {
-    this.tryCount = 0;
-    this.carNames = [];
-    this.carList = [];
-    this.result = {
-      moveList: [],
-      winners: null,
-    };
-  }
-
-  makeCarList(carNames) {
-    validCarName(carNames);
-    const nameFormatting = names => names.replace(/(\s*)/g, '').split(',');
-    this.carNames = nameFormatting(carNames);
-    this.carNames.forEach(name => {
-      this.carList.push(new Car(name));
-    });
-    return this.carList;
-  }
-
-  getRace() {
-    validtryCount(this.tryCount);
-    this.tryCount = Number(this.tryCount);
-    for (let count = 0; count < this.tryCount; count += 1) {
-      const round = this.getRaceRound();
-      this.result.moveList.push(round);
-    }
+  constructor(names) {
+    this.cars = names.map(name => new Car(name));
   }
 
   getRaceRound() {
-    const forwardFormatting = (name, forward) => {
-      const forwardArray = Array.from({ length: forward }, () => '-');
-      return `${name}: ${forwardArray.join('')}`;
-    };
-
-    return this.carList.reduce((acc, car) => {
-      car.forward();
-      acc.push(forwardFormatting(car.getCarName(), car.getMoveCount()));
-      return acc;
-    }, []);
-  }
-
-  getWinners() {
-    const maxMoveCount = this.carList
-      .sort((a, b) => b.getMoveCount() - a.getMoveCount())[0]
-      .getMoveCount();
-    const winners = this.carList.filter(
-      car => car.getMoveCount() === maxMoveCount,
-    );
-    this.result.winners = winners.map(winner => winner.getCarName()).join(', ');
-    return this.result.winners;
-  }
-
-  getResult() {
-    return this.result;
+    this.cars.forEach(car => {
+      if (getRandomNumber() >= FORWARD.CONDITION) {
+        car.forward();
+      }
+    });
+    return this.cars;
   }
 }
+
 export default CarRace;
