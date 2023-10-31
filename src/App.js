e231946e1d4dbf7cf3f5ec3ff293c9ce6c21b245
suwonthugger <ddww1029wwdd@gmail.com>
2023-10-31 09:20:47 +0900

    Feat: 입력 값을 받아와서 저장하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
-class App {
-  async play() {}
+import { MESSAGE } from "./constants/messages.js";
+import { Console, Random } from "@woowacourse/mission-utils";
+
+export class App {
+  async play() {
+    this.getInputValue();
+  }
+
+  startGame(params) {}
+
+  async getInputValue() {
+    Console.print(MESSAGE.START);
+    const carNames = await Console.readLineAsync("");
+    Console.print(MESSAGE.INPUT);
+    const tryNumber = await Console.readLineAsync("");
+
+    return { carNames, tryNumber };
+  }
+
+  gameStart(tryNumber) {
+    Console.print("실행 결과");
+    for (let i = 0; i < tryNumber; i++) {
+      this.resultResult;
+    }
+  }
+
+  resultOutput(...carNames) {
+    carNames.forEach((name) => Console.print(`${name} : `));
+  }
 }
 
 export default App;
