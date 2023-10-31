7e803ea7a4864d5e58efdc873da894f923bc2b91
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-10-31 22:51:18 +0900

    :recycle: refactor : checkName을 수정한다.

--- src/util/check.js
+++ src/util/check.js
@@ -9,7 +9,7 @@ import RACING from "../constants/message"
 export const checkName = async (nameArray) => {
     nameArray.forEach(name => {
         if (name.length > 5 || name.trim() === 0) throw new Error(RACING.NAMING_ERROR)
-        if (nameArray.filter(value => name === value).length > 2) throw new Error(RACING.NAMING_ERROR)
+        if (nameArray.filter(value => name === value).length >= 2) throw new Error(RACING.NAMING_ERROR)
     })
 }
 
