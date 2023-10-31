26779fc5b074390577e63080fa1971c4b499b77a
한지은 <hjn510450@gmail.com>
2023-10-31 19:31:26 +0900

    게임 클래스 수정(게임 메시지 등 상수 처리, 이동 횟수 배열 프로퍼티 접근 지정)

--- src/utils/Test.js
+++ src/utils/Test.js
@@ -1,19 +1,29 @@
 import { Console, Random } from '@woowacourse/mission-utils';
+import { GAME_MESSAGE } from '../constants/messages';
+
+const DECISION_NUM = 4;
+const FORWARD_MARKER = '-';
 
 class Test {
-    constructor(carNameArr) {
-        this.moveCount = Array(carNameArr.length).fill(0);
-    }
+  _moveCounts;
 
-    async testing(carNameArr) {
-        for (let i = 0; i < carNameArr.length; i++) {
-            if (Random.pickNumberInRange(0, 9) >= 4) this.moveCount[i] += 1;
-        }
-        for (let i = 0; i < carNameArr.length; i++) {
-            Console.print(`${carNameArr[i]} : ${'-'.repeat(this.moveCount[i])}`);
-        }
-        Console.print('\n');
+  constructor(carNameArr) {
+    this._moveCounts = Array(carNameArr.length).fill(0);
+  }
+
+  get moveCounts() {
+    return this._moveCounts;
+  }
+
+  async testing(carNameArr) {
+    for (let i = 0; i < carNameArr.length; i++) {
+      if (Random.pickNumberInRange(0, 9) >= DECISION_NUM) this._moveCounts[i] += 1;
     }
+    for (let i = 0; i < carNameArr.length; i++) {
+      Console.print(`${carNameArr[i]} : ${FORWARD_MARKER.repeat(this._moveCounts[i])}`);
+    }
+    Console.print(GAME_MESSAGE.lineBreak);
+  }
 
 }
 
