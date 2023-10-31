ab047185facb296bd18acb684cef600cfefe8a65
ryan-dia <cjfdnjs158@naver.com>
2023-10-31 17:01:43 +0900

    test(validator): 기존함수가 의존성 주입으로 변경되면서 코드 수정

--- src/utils/validator/utils/is-valid-name-length/index.test.js
+++ src/utils/validator/utils/is-valid-name-length/index.test.js
@@ -3,7 +3,10 @@ import isValidNameLength from './index.js';
 describe('isValidNameLength', () => {
   test('알맞은 이름의 길이(2~6자)일때 true 리턴', () => {
     // given
-    const inputs = ['도레,레미파,솔라시도레미', 'aa,bbb,ccc,dddd,eeee'];
+    const inputs = [
+      ['도레', '레미파', '솔라시도레미'],
+      ['aa', 'bbb', 'ccc', 'dddd', 'eeee'],
+    ];
 
     // when
     // then
@@ -14,7 +17,11 @@ describe('isValidNameLength', () => {
 
   test('알맞은 이름의 길이가 아닐 때 false 리턴', () => {
     // given
-    const inputs = ['도,레,미', 'a,b,c,dd,eeee', '도레미파솔라시,도,시시'];
+    const inputs = [
+      ['도', '레', '미'],
+      ['a', 'b', 'c', 'dd', 'eeee'],
+      ['도레미파솔라시', '도', '시시'],
+    ];
 
     // when
     // then
