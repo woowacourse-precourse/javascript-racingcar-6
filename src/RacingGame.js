16cc956df89eca67f8110406f4769dcde5cafaac
rabbit-22 <chlwlal20@gmail.com>
2023-11-01 01:51:30 +0900

    feat: 0에서 9 사이의 랜덤값 출력 기능

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -1,9 +1,14 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 import Car from "./Car";
 
 class RacingGame {
   constructor(carNames, raceRounds) {
     this.cars = carNames.map((name) => new Car(name));
     this.rounds = raceRounds;
+  }
+
+  generateRandomNumber() {
+    return MissionUtils.Random.pickNumberInRange(0, 9);
   }
 }
 
