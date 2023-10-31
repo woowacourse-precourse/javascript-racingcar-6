c8e7341611f264f7fe9ae795fe8e9026113aad11
OAO <ashgrayblue0@gmail.com>
2023-11-01 03:49:34 +0900

    feat(App.js): integrate whole program
    - implement 'play' method
    - finish final test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Computer from "./module/Computer.js";
+
 class App {
-  async play() {}
+  async play() {
+    const computer = new Computer();
+
+    await computer.start();
+    computer.finish();
+  }
 }
 
 export default App;
