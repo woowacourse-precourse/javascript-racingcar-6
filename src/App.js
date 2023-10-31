1e3f13a61f9eea4ebe620acb4a196ceb0d8d4d8f
BaeDongWoo <ehddn5410@naver.com>
2023-10-31 19:24:23 +0900

    feat:경기 결과에 따라 우승자 출력

--- src/App.js
+++ src/App.js
@@ -29,39 +29,41 @@ class App {
       throw new Error('플레이어는 1명 이상입력해주세요');
     }
     players.map((player) => this.validatePlayer(player));
-    console.log('올바른 입력입니다.');
+    // console.log('올바른 입력입니다.');
     return players;
   }
   validatePlayer(player) {
     console.log(player);
     if (player.trim() === '') {
-      this.Exceptionoccurred();
+      this.ExceptionOccurred();
     }
     if (player.length > 5) {
-      this.Exceptionoccurred();
+      this.ExceptionOccurred();
     }
   }
   validateGames(input) {
     if (isNaN(input)) {
-      this.Exceptionoccurred();
+      this.ExceptionOccurred();
     }
   }
-  Exceptionoccurred() {
+  ExceptionOccurred() {
     throw new Error('[ERROR] 잘못된 입력입니다.');
   }
   startGame(players, games) {
     let playersMove = new Array(players.length);
     playersMove.fill(0);
+    Console.print('실행 결과');
     for (let i = 0; i < Number(games); i++) {
       playersMove = this.moveOrWait(players, playersMove);
       this.printRound(players, playersMove);
+      Console.print(' ');
     }
+    const winner = this.printWinner(players, playersMove);
+    Console.print('최종우승자 : ' + winner.join(', '));
   }
   moveOrWait(players, playersMove) {
     for (let i = 0; i < players.length; i++) {
-      if (this.createRandomNumber() >= 4) {
-        playersMove[i]++;
-      }
+      if (this.createRandomNumber() >= 4) playersMove[i]++;
     }
     return playersMove;
   }
@@ -70,10 +72,9 @@ class App {
     return randomNumber;
   }
   printRound(players, playersMove) {
-    console.log('실행 경과');
     for (let i = 0; i < players.length; i++) {
       const moveBar = this.createBar(playersMove[i]);
-      console.log(players[i] + ' : ' + moveBar);
+      Console.print(players[i] + ' : ' + moveBar);
     }
   }
   createBar(playersMove) {
@@ -82,6 +83,15 @@ class App {
       bar += '-';
     }
     return bar;
+  }
+  printWinner(players, playersMove) {
+    const maxMove = Math.max(...playersMove);
+    console.log(maxMove);
+    let winner = [];
+    for (let i = 0; i < players.length; i++) {
+      if (playersMove[i] === maxMove) winner.push(players[i]);
+    }
+    return winner;
   }
 }
 const app = new App();
