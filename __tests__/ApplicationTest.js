320a5730894cf8022e5a0af8f383015942a97ece
f0ever0 <thddndud0830@gmail.com>
2023-10-31 13:30:09 +0900

    test : 실행 횟수 입력 테스트

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -59,4 +59,13 @@ describe("자동차 경주 게임", () => {
       await expect(app.play()).rejects.toThrow("[ERROR]");
     }
   );
+
+  test.each([[["tony,julia"], "a"]])(
+    "실행 횟수롤 숫자가 아닌 값 입력시 오류",
+    async (inputs) => {
+      mockQuestions(inputs);
+      const app = new App();
+      await expect(app.play()).rejects.toThrow("[ERROR]");
+    }
+  );
 });
