05a1f79ff13763d49d67edab2ef065c07a67a95c
Han <mayo3610@naver.com>
2023-10-31 15:37:13 +0900

    refactor #1 inputView 모듈화

---
+++ src/view/InputView.js
@@ -0,0 +1,18 @@
+import { Console } from "@woowacourse/mission-utils";
+
+const InputView = {
+  async readUserRacingCars(callback) {
+    await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    ).then((input) => {
+      callback(input);
+    });
+  },
+  async readUserRacingTryNumber(callback) {
+    await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n").then(
+      (input) => {
+        callback(input);
+      }
+    );
+  },
+};
