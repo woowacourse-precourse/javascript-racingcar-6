2f25092f56e8d47a38ab82e1d3d81d446a1dc866
bl0ckp1n9 <blockp1n9@gmail.com>
2023-11-01 00:43:35 +0900

    refactor: arrow function 제거,this 키워드 사용,자동차 이름 validation 함수 분리

--- src/IO.js
+++ src/IO.js
@@ -7,42 +7,43 @@ export const input = {
       '경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)\n',
     ERROR: '[ERROR] 자동차 이름은 5자 이하만 가능합니다.',
   },
-  carNames: async () => {
-    const carNames = (await Console.readLineAsync(input.MESSAGE.CAR_NAMES))
+  async carNames() {
+    const carNames = (await Console.readLineAsync(this.MESSAGE.CAR_NAMES))
       .split(',')
       .map(carName => {
-        if (carName.length > 5) {
-          throw new Error(input.MESSAGE.ERROR);
-        }
+        this.validateCarName(carName);
         return carName;
       });
     return carNames;
   },
-  tryCount: async () => {
-    const tryCount = await Console.readLineAsync(input.MESSAGE.TRY_COUNT);
+  async tryCount() {
+    const tryCount = await Console.readLineAsync(this.MESSAGE.TRY_COUNT);
     return tryCount;
+  },
+  validateCarName(carName) {
+    if (carName.length > 5) {
+      throw new Error(this.MESSAGE.ERROR);
+    }
   },
 };
 
 export const output = {
   MESSAGE: {
-    ERROR: '[ERROR] 자동차 이름은 5자 이하만 가능합니다.',
     EXECUTION_RESULT: '\n실행 결과',
     WINNER: winners => `${winners.join(', ')}가 최종 우승했습니다.\n`,
     RACING_STATUS: racer => `${racer.carName} : ${'-'.repeat(racer.move)}`,
   },
-  statesEveryIteration: racers => {
+  statesEveryIteration(racers) {
     const result = racers
-      .map(racer => output.MESSAGE.RACING_STATUS(racer))
+      .map(racer => this.MESSAGE.RACING_STATUS(racer))
       .join('\n')
       .concat('\n');
     Console.print(result);
   },
-  winners: racers => {
-    const winners = racers
-      .sort((a, b) => b.move - a.move)
-      .filter(racer => racer.move === racers[0].move)
-      .map(racer => racer.carName);
-    Console.print(output.MESSAGE.WINNER(winners));
+  winners(racers) {
+    const maxMove = Math.max(...racers.map(racer => racer.move));
+    const winningRacers = racers.filter(racer => racer.move === maxMove);
+    const winnerNames = winningRacers.map(racer => racer.carName);
+    Console.print(this.MESSAGE.WINNER(winnerNames));
   },
 };
