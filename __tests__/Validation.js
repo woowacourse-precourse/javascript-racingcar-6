abc6251ab00ddcd82d3d493993fd655b980ce48b
Jeongeun Lee <leeju1009@gmail.com>
2023-10-31 18:06:31 +0900

    test: 자동차 이름의 최소 글자수를 고려하여 테스트 케이스 추가

--- __tests__/Validation.js
+++ __tests__/Validation.js
@@ -7,6 +7,8 @@ describe('사용자 입력값의 유효성을 검사하는 함수 테스트', ()
     ['제네시스', true],
     ['벤츠', true],
     ['5글자 이상', false],
+    ['', false],
+    [' ', false],
   ])('자동차 이름 유효성 검사 함수 테스트', (inputs, expected) => {
     const isValidOutput = validation.isValidCarName(inputs);
 
