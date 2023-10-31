63251439df8e3313a1a164bfb33bdf65b6d9a963
areumH <94692566+areumH@users.noreply.github.com>
2023-10-31 22:21:04 +0900

    refactor: 사용자 입력 메세지 파일 정리

--- src/utils/getCarName.js
+++ src/utils/getCarName.js
@@ -1,10 +1,10 @@
 import { Console } from "@woowacourse/mission-utils";
+import { inputMessage } from "../messages/inputMessage.js";
 import { checkEmptyValue, checkCarNameLength } from "../validators/carNameValidator.js";
 
 export const getCarName = async () => {
-  Console.print(
-    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
-  );
+  Console.print(inputMessage.CAR_MESSAGE);
+  
   const carName = await Console.readLineAsync("");
   const carNameArray = carName.split(",");
 
--- src/utils/getRaceCount.js
+++ src/utils/getRaceCount.js
@@ -1,8 +1,9 @@
 import { Console } from "@woowacourse/mission-utils";
+import { inputMessage } from "../messages/inputMessage.js";
 import { checkRaceCount } from "../validators/raceValidator.js";
 
 export const getRaceCount = async () => {
-  Console.print("시도할 횟수는 몇 회인가요?");
+  Console.print(inputMessage.RACE_MESSAGE);
 
   const input = await Console.readLineAsync("");
   const raceCount = parseInt(input);
