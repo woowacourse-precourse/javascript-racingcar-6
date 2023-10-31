52988514f1ab60103006442874b977b0acca27b7
jayming66 <mjw8941@gmail.com>
2023-10-31 21:07:16 +0900

    feat : 사용자의 자동차 이름 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const carName = await MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+
+    const carArray = carName.split(",");
+    MissionUtils.Console.print(carArray);
+  }
 }
 
 export default App;
