f629f360fb682db5bff146e12d30e979aad0c4e5
juseong <jjs7280@gmail.com>
2023-10-31 19:41:33 +0900

    refactor: 입력 및 에러 메시지 constant.js로 모듈화

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import { Console, Random } from '@woowacourse/mission-utils';
 import Car from './Car.js';
-
-const ERROR_MESSAGE = '[ERROR]';
-const ERROR_MESSAGES = {
-  OVER_MAX_NAME: `${ERROR_MESSAGE} 자동차 이름이 5글자를 초과했습니다.`,
-  NO_NAME: `${ERROR_MESSAGE} 이름이 입력되지 않았습니다.`,
-  INVALID_NUMBER: `${ERROR_MESSAGE} 숫자가 잘못된 형식입니다.`,
-  NO_INTEGER: `${ERROR_MESSAGE} 1 이상의 정수를 입력해주세요.`,
-};
+import { ERROR_MESSAGES, MESSAGES } from './constant.js';
 
 class App {
   async getUserInput() {
-    const userInput = await Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
-    );
+    const userInput = await Console.readLineAsync(MESSAGES.INPUT_CAR_NAMES);
     return userInput.split(',');
   }
 
@@ -28,9 +19,7 @@ class App {
   }
 
   async getCounts() {
-    return parseInt(
-      await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n')
-    );
+    return parseInt(await Console.readLineAsync(MESSAGES.INPUT_RACING_COUNT));
   }
 
   checkCountValid(number) {
---
+++ src/constant.js
@@ -0,0 +1,14 @@
+export const ERROR_MESSAGE = '[ERROR]';
+
+export const ERROR_MESSAGES = {
+  OVER_MAX_NAME: `${ERROR_MESSAGE} 자동차 이름이 5글자를 초과했습니다.`,
+  NO_NAME: `${ERROR_MESSAGE} 이름이 입력되지 않았습니다.`,
+  INVALID_NUMBER: `${ERROR_MESSAGE} 숫자가 잘못된 형식입니다.`,
+  NO_INTEGER: `${ERROR_MESSAGE} 1 이상의 정수를 입력해주세요.`,
+};
+
+export const MESSAGES = {
+  INPUT_CAR_NAMES:
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+  INPUT_RACING_COUNT: '시도할 횟수는 몇 회인가요?\n',
+};
