54f3877d2947883d05c240480dcd4c76c9525dde
Eunsoo Ko <rhdmstn202@gmail.com>
2023-10-31 21:18:57 +0900

    test: 함수별 테스트 작성

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -2,6 +2,9 @@ import App from "../src/App.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
 import Validation from "../src/Validation/Validation.js";
 import CONSTANTS from "../src/Constants/Constants.js";
+import CarRacingModel from "../src/Model/CarRacingModel.js";
+import CarRacingView from "../src/View/CarRacingView.js";
+import CarRacingController from "../src/Controller/CarRacingController.js";
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -68,10 +71,83 @@ describe("자동차 경주 게임", () => {
     [() => {}],
     [undefined],
     [["this", "is", "array"]],
-  ])("유효하지 않은 숫자에 대한 예외 처리", (inputs) => {
+  ])("유효하지 않은 숫자에 대한 예외 처리", async (inputs) => {
     const validation = new Validation();
-    expect(() => validation.isValidTrialNumber(inputs)).toThrow(
+    await expect(() => validation.isValidTrialNumber(inputs)).toThrow(
       CONSTANTS.ERROR.ISNUMBER
     );
+  });
+
+  test("moveOrStay 함수", () => {
+    const randoms = [8, 2];
+    mockRandoms(randoms);
+
+    const carRacingController = new CarRacingController();
+    carRacingController.carRacingModel.setCarData("car1,car2");
+
+    carRacingController.carRacingModel.moveOrStay(
+      carRacingController.carRacingModel.getCarData()
+    );
+
+    expect(carRacingController.carRacingModel.getCarData()).toEqual([
+      ["car1", 0],
+      ["car2", 1],
+    ]);
+  });
+
+  test("printResult 함수", () => {
+    const carData = [
+      ["car1", 3],
+      ["car2", 5],
+    ];
+    const expectedOutput = ["car1 : ---", "car2 : -----"];
+    const logSpy = getLogSpy();
+
+    const carRacingView = new CarRacingView();
+    carRacingView.printResult(carData);
+
+    expectedOutput.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("printWinner 함수", () => {
+    const carData = [
+      ["car1", 3],
+      ["car2", 5],
+      ["car3", 5],
+    ];
+    const expectedOutput = "car2, car3";
+    const logSpy = getLogSpy();
+
+    const carRacingView = new CarRacingView();
+    carRacingView.printWinner(carData);
+
+    expect(logSpy).toHaveBeenCalledWith(
+      expect.stringContaining(expectedOutput)
+    );
+  });
+
+  test("inputCarName 함수", async () => {
+    const inputs = ["car1,car2"];
+    mockQuestions(inputs);
+
+    const carRacingController = new CarRacingController();
+    await carRacingController.inputCarName();
+
+    expect(carRacingController.carRacingModel.getCarData()).toEqual([
+      ["car1", 0],
+      ["car2", 0],
+    ]);
+  });
+
+  test("inputTrialNumber 함수", async () => {
+    const inputs = [5];
+    mockQuestions(inputs);
+
+    const carRacingController = new CarRacingController();
+    await carRacingController.inputTrialNumber();
+
+    expect(carRacingController.carRacingModel.getTrialNumber()).toBe(5);
   });
 });
