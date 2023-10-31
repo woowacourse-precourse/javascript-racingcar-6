9c657c798bb29e331957efa2dbacd92a6522f98e
Hyunji Seok <alleyji126@gmail.com>
2023-10-31 21:38:04 +0900

    test: change names of tests

--- __tests__/ErrorTest.js
+++ __tests__/ErrorTest.js
@@ -2,7 +2,7 @@ import App from "../src/App.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 describe('에러 반환 테스트', () => {
-  test('자동차 이름이 두 대 미만인 경우 에러를 반환한다.', () => {
+  test('자동차 이름이 두 대 미만인 경우 에러 반환', () => {
     const app = new App();
     const cars = ['Alley'];
     const raceTimes = '5';
@@ -10,7 +10,7 @@ describe('에러 반환 테스트', () => {
     expect(() => app.handleErrors(cars, raceTimes)).toThrow('[ERROR]');
   });
 
-  test('중복된 자동차 이름을 입력한 경우 에러를 반환한다.', () => {
+  test('중복된 자동차 이름을 입력한 경우 에러 반환', () => {
     const app = new App();
     const cars = ['Alley', 'Tami', 'Alley'];
     const raceTimes = '5';
@@ -18,7 +18,7 @@ describe('에러 반환 테스트', () => {
     expect(() => app.handleErrors(cars, raceTimes)).toThrow('[ERROR]');
   });
 
-  test('유효하지 않은 자동차 이름을 입력한 경우 에러를 반환한다.', () => {
+  test('유효하지 않은 자동차 이름을 입력한 경우 에러 반환', () => {
     const app = new App();
     const cars = ['Alley', 'Tami', '123!'];
     const raceTimes = '5';
@@ -26,7 +26,7 @@ describe('에러 반환 테스트', () => {
     expect(() => app.handleErrors(cars, raceTimes)).toThrow('[ERROR]');
   });
 
-  test('자동차 이름이 다섯 글자를 초과한 경우 에러를 반환한다.', () => {
+  test('자동차 이름이 다섯 글자를 초과한 경우 에러 반환', () => {
     const app = new App();
     const cars = ['Alley', 'Tami', 'Yunaaaaaaa'];
     const raceTimes = '5';
@@ -34,7 +34,7 @@ describe('에러 반환 테스트', () => {
     expect(() => app.handleErrors(cars, raceTimes)).toThrow('[ERROR]');
   });
 
-  test('유효하지 않은 자동차 경주 횟수를 입력한 경우 에러를 반환한다.', () => {
+  test('유효하지 않은 자동차 경주 횟수를 입력한 경우 에러 반환', () => {
     const app = new App();
     const cars = ['Alley', 'Tami', 'Yuna'];
     const raceTimes = 'abc';
@@ -42,7 +42,7 @@ describe('에러 반환 테스트', () => {
     expect(() => app.handleErrors(cars, raceTimes)).toThrow('[ERROR]');
   });
 
-  test('자동차 경주 횟수로 음수를 입력한 경우 에러를 반환한다.', () => {
+  test('자동차 경주 횟수로 음수를 입력한 경우 에러 반환', () => {
     const app = new App();
     const cars = ['Alley', 'Tami', 'Yuna'];
     const raceTimes = '-5';
--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -2,7 +2,7 @@ import App from "../src/App.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 describe('사용자 입력 테스트', () => {
-  test('사용자로부터 자동차 이름과 자동차 경주 횟수를 입력받아 변수에 저장한다', async () => {
+  test('사용자로부터 자동차 이름과 자동차 경주 횟수를 입력받아 변수에 저장', async () => {
     const userCarInput = 'Alley,Tami,Yuna';
     const userRaceTimesInput = '1';
     
