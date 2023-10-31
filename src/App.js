efa76d12579ab603179cf60540b6e5f94c00d45d
hwangheetae <tejaia@naver.com>
2023-10-31 23:14:07 +0900

    style: Airbnb JavaScript 스타일 가이드 적용

--- src/App.js
+++ src/App.js
@@ -1,99 +1,91 @@
-import { Console, Random } from "@woowacourse/mission-utils";
-
-const INPUT_NUMBER_ERR_MSG = "[ERROR] 숫자가 잘못된 형식입니다."
-const PLAYER_NUMBER_ERR_MSG = "[ERROR] 최소 2명 이상 입력하세요"
-const PLAYER_NAME_ASK_MSG="경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
-const ATTEMPT_NUM_ASK_MSG = "시도할 횟수는 몇 회인가요?\n"
-const FINAL_WINNER_MSG= "최종 우승자"
-const RESULT_MSG="실행 결과"
-
+import { Console, Random } from '@woowacourse/mission-utils';
+import {
+  INPUT_NUMBER_ERR_MSG,
+  PLAYER_NUMBER_ERR_MSG,
+  PLAYER_NAME_ASK_MSG,
+  ATTEMPT_NUM_ASK_MSG,
+  FINAL_WINNER_MSG,
+  RESULT_MSG,
+  FORWARD_CONDITIONS,
+  MIN_PLAYER_NUMBER
+} from './constant.js';
 
 class App {
-
-  // 에러처리
-  numberCheck(input){
-    if (isNaN(input)){
+  numberCheck(input) {
+    if (isNaN(input)) {
       throw new Error(INPUT_NUMBER_ERR_MSG);
     }
   }
 
-  playerNumebrCheck(array){
-    if (array.length <2){
+  playerNumberCheck(array) {
+    if (array.length < MIN_PLAYER_NUMBER) {
       throw new Error(PLAYER_NUMBER_ERR_MSG);
     }
   }
-  
-  //사용자 입력, 분리
-  async playerInfoInput(){
+
+  async playerInfoInput() {
     const inputName = await Console.readLineAsync(PLAYER_NAME_ASK_MSG);
-    const playerList= inputName.split(",").map(str => str.trim());
-    this.playerNumebrCheck(playerList);
+    const playerList = inputName.split(',').map(str => str.trim());
+    this.playerNumberCheck(playerList);
 
     return playerList;
   }
 
-  //사용자 객체,키리스트 생성
-  playerSetting(playerList){
-    const playerObject= {};
-    for(let i =0; i<playerList.length;i++){
-      const playerName=playerList[i];
-      playerObject[playerName] =0;
-    }
-    const objectKeyList=Object.keys(playerObject);
+  // 사용자 객체, 키 리스트 생성
+  playerSetting(playerList) {
+    const playerObject = {};
+    playerList.forEach(playerName => {
+      playerObject[playerName] = 0;
+    });
+    const objectKeyList = Object.keys(playerObject);
 
-    return {playerObject,objectKeyList};
+    return { playerObject, objectKeyList };
   }
 
-  //게임 시작 및 출력
-  gamePlay(playerObject,tryNum,objectKeyList) {
-
-    for(let i =0; i<tryNum;i++){
-      for (let item of objectKeyList){
-        const goForward=Random.pickNumberInRange(0,9);
-        if(goForward>=4){
+  gamePlay(playerObject, tryNum, objectKeyList) {
+    for (let i = 0; i < tryNum; i++) {
+      objectKeyList.forEach(item => {
+        const goForward = Random.pickNumberInRange(0, 9);
+        if (goForward >= FORWARD_CONDITIONS) {
           playerObject[item]++;
         }
-        Console.print(`${item} : `+"-".repeat(playerObject[item]));
-      }
-      Console.print("");
+        Console.print(`${item} : ${'-'.repeat(playerObject[item])}`);
+      });
+      Console.print('');
     }
   }
-  
-  //우승자 찾기
-  findWinner(playerObject,playersKeyList){
-    let max=0;
-    const winnerList= [];
 
-    for (let item of playersKeyList){
-      if (playerObject[item]>max){
-        max=playerObject[item];
+  findWinner(playerObject, playersKeyList) {
+    let max = 0;
+    const winnerList = [];
+
+    playersKeyList.forEach(item => {
+      if (playerObject[item] > max) {
+        max = playerObject[item];
       }
-    }
+    });
 
-    for (let item of playersKeyList){
-      if(playerObject[item]===max){
+    playersKeyList.forEach(item => {
+      if (playerObject[item] === max) {
         winnerList.push(item);
       }
-    }    
+    });
 
-    //출력
-    const output=winnerList.join(", ");
+    const output = winnerList.join(', ');
     Console.print(`${FINAL_WINNER_MSG} : ${output}`);
   }
 
-
   async play() {
-
-
     const playerList = await this.playerInfoInput();
-    const {playerObject:players, objectKeyList:playersKeyList} = this.playerSetting(playerList);
-    //시도횟수 입력
-    const numberAttempts = Number(await Console.readLineAsync(ATTEMPT_NUM_ASK_MSG));
-    this.numberCheck(numberAttempts);
+    const { playerObject: players, objectKeyList: playersKeyList } = this.playerSetting(playerList);
 
-    Console.print("\n"+RESULT_MSG);
+    // 시도 횟수 입력
+    const numberOfAttempts = Number(await Console.readLineAsync(ATTEMPT_NUM_ASK_MSG));
+    this.numberCheck(numberOfAttempts);
 
-    this.gamePlay(players, numberAttempts,playersKeyList);
+    Console.print(`\n${RESULT_MSG}`);
+
+    this.gamePlay(players, numberOfAttempts, playersKeyList);
     this.findWinner(players, playersKeyList);
   }
 }
---
+++ src/constant.js
@@ -0,0 +1,9 @@
+export const INPUT_NUMBER_ERR_MSG = '[ERROR] 숫자가 잘못된 형식입니다.';
+export const PLAYER_NUMBER_ERR_MSG = '[ERROR] 최소 2명 이상 입력하세요';
+export const PLAYER_NAME_ASK_MSG = '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n';
+export const ATTEMPT_NUM_ASK_MSG = '시도할 횟수는 몇 회인가요?\n';
+export const FINAL_WINNER_MSG = '최종 우승자';
+export const RESULT_MSG = '실행 결과';
+
+export const MIN_PLAYER_NUMBER = 2;
+export const FORWARD_CONDITIONS = 4;
