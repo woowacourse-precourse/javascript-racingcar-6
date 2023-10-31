374a2fb35c68f9882c1a01455efb12e7b8fcc648
baegyeong <beth0929@naver.com>
2023-11-01 00:21:37 +0900

    feat(carArr): throw를 통한 에러 반환으로 변경

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App {
     car = car.map((carName) => carName.replace(/\s/g, ""));
 
     if (car.some((carName) => carName.length > 6)) {
-      console.error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+      throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
     }
 
     this._carArr = car;
