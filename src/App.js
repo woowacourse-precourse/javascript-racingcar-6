9d32db4d958ba617f119863f70e3fc8204f7bc13
j-nary <dlwls0921@naver.com>
2023-11-01 03:06:40 +0900

    feat: 사용자 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    userInput();
+  }
+
+  async userInput() {
+    try {
+      const input = await MissionUtils.Console.readLineAsync(
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+      );
+      const carName = input.split(',');
+      if (!this.areElementsFiveCharactersOrLess(carName)) {
+        throw new Error("[ERROR] 5자가 넘는 자동차 이름이 존재합니다.")
+      }
+      if (!this.areCarNamesUnique(carName)) {
+        throw new Error("[ERROR] 중복된 자동차 이름이 존재합니다.")
+      }
+      const carLength = carName.length;
+    } catch(error) {
+      MissionUtils.Console.print(error.message);
+      return;
+    }
+  }
+
+  areElementsFiveCharactersOrLess(carName) {
+    return carName.every(e => e.length <= 5);
+  }
+
+  areCarNamesUnique(carName) {
+    const uniqueElements = new Set(carName);
+    return uniqueElements.size === carName.length;
+  }
 }
 
 export default App;
