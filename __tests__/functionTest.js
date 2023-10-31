cceec59fddc6d5fefc6e6e59106c4afbb56237e6
육종호 <dbrwhdgh1203@naver.com>
2023-10-31 12:41:34 +0900

    입력 에러 처리 함수 테스트 작성

--- __tests__/functionTest.js
+++ __tests__/functionTest.js
@@ -32,3 +32,42 @@ describe('유저 입력', () => {
     expect(input).toEqual(result);
   });
 });
+
+describe('입력 에러 처리', () => {
+    test.each(['pobi,javaji', 'pobi,,eastjun'])('checkCarInput', (inputs) => {
+      // given
+      const invalidInput = inputs;
+      // when
+      const app = new App();
+      // then
+      expect(() => {
+        app.checkCarInput(invalidInput);
+      }).toThrowError('[ERROR] 입력이 잘못된 형식입니다.');
+    });
+  
+    test.each([
+      ['pobi', 'pobi'],
+      ['test', 'test'],
+    ])('checkDupliCar', (input1, input2) => {
+      // given
+      const invalidInput = [input1, input2];
+      // when
+      const app = new App();
+      // then
+      expect(() => {
+        app.checkDupliCar(invalidInput);
+      }).toThrowError('[ERROR] 중복된 이름이 있습니다.');
+    });
+  
+    test.each(['a', 'ab', 'abc'])('checkNumInput', (inputs) => {
+      // given
+      const invalidInput = inputs;
+      // when
+      const app = new App();
+      app.tryNum = invalidInput;
+      // then
+      expect(() => {
+        app.checkNumInput();
+      }).toThrowError('[ERROR] 숫자가 잘못된 형식입니다.');
+    });
+  });
