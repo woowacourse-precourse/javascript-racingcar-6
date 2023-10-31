832ff5f6a161e61095a20432a2ae6393929afdf2
unknown <pih9134@naver.com>
2023-11-01 05:26:34 +0900

    feat: 상수화

--- utils/Validation.js
+++ utils/Validation.js
@@ -1,6 +1,8 @@
+import ERROR_MESSAGE, { MAX_NAME_LENGTH, MIN_NAME_LENGTH } from './constants.js';
+
 class Validation {
   static isWrongCarName(name) {
-    return !(name.length > 0 && name.length <= 5);
+    return !(name.length > MIN_NAME_LENGTH && name.length <= MAX_NAME_LENGTH);
   }
 
   static isDuplicateCarName(names) {
@@ -10,17 +12,17 @@ class Validation {
   static validateCarNames(carNames) {
     const names = carNames.split(',').map((name) => name.trim());
     if (names.some(this.isWrongCarName)) {
-      throw new Error('[ERROR]');
+      throw new Error(ERROR_MESSAGE.wrongInput);
     }
     if (this.isDuplicateCarName(names)) {
-      throw new Error('[ERROR]');
+      throw new Error(ERROR_MESSAGE.repeatCarName);
     }
     return names;
   }
 
   static validateTryCount(tryCount) {
     if (!Number.isInteger(Number(tryCount)) || tryCount < 1) {
-      throw new Error('[ERROR]');
+      throw new Error(ERROR_MESSAGE.wrongTryCount);
     }
     return tryCount;
   }
---
+++ utils/constants.js
@@ -0,0 +1,11 @@
+export const MAX_NAME_LENGTH = 5;
+export const MIN_NAME_LENGTH = 0;
+export const MOVE_NUMBER = 4;
+
+const ERROR_MESSAGE = {
+  wrongInput: '[ERROR] 잘못된 입력입니다.',
+  repeatCarName: '[ERROR] 중복된 이름입니다.',
+  wrongTryCount: '[ERROR] 시도 횟수는 양의 정수여야 합니다.',
+};
+
+export default ERROR_MESSAGE;
