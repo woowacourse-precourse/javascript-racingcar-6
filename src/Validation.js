5265b764e84e8272e4e098811e97be0a37a2fd05
Sunghyun627 <kksscc4@gmail.com>
2023-10-31 23:10:45 +0900

    refactor: carNameList의 input을 validation하는 함수들의 함수명 변경

--- src/Validation.js
+++ src/Validation.js
@@ -1,15 +1,16 @@
 import { ERROR_MESSAGES } from './constants.js';
 
 export const isValidCarNames = (carNames) => {
-  if (!isNotEmptyInput(carNames)) throw new Error(ERROR_MESSAGES.EMPTY_INPUT);
-  if (!carNames.every(isValidLength)) throw new Error(ERROR_MESSAGES.NOT_VALID_CARNAME_LENGTH);
+  if (!isNotEmptyCarNames(carNames)) throw new Error(ERROR_MESSAGES.EMPTY_INPUT);
+  if (!carNames.every(isValidCarNameLength))
+    throw new Error(ERROR_MESSAGES.NOT_VALID_CARNAME_LENGTH);
   return true;
 };
 
-const isNotEmptyInput = (input) => {
+const isNotEmptyCarNames = (input) => {
   return input.length !== 1 || input[0] !== '';
 };
 
-const isValidLength = (carName) => {
+const isValidCarNameLength = (carName) => {
   return carName.length !== 0 && carName.length <= 5;
 };
