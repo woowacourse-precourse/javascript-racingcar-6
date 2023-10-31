fc45e483d8a80d22a4e0acb43a87d0b37c113905
kimmoonju-102 <kimmun3549@naver.com>
2023-11-01 00:48:40 +0900

    refactor : Validations 정의
    - Validations 모듈 가져와서 호출

--- src/Racingcar/views/InputView.js
+++ src/Racingcar/views/InputView.js
@@ -1,11 +1,12 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import Validations from "./../validations/index";
 
 class Input {
   static async getCarNames() {
     MissionUtils.Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     const carNamesInput = await MissionUtils.Console.readLineAsync();
     const carNames = carNamesInput.split(",");
-    this.validateCarNames(carNames);
+    Validations.validateCarNames(carNames);
     return carNames;
   }
 
@@ -13,7 +14,7 @@ class Input {
     MissionUtils.Console.print("시도할 횟수는 몇 회인가요?");
     const numAttemptsInput = await MissionUtils.Console.readLineAsync();
     const numAttempts = parseInt(numAttemptsInput);
-    this.validateNumAttempts(numAttempts);
+    Validations.validateNumAttempts(numAttempts);
     return numAttempts;
   }
 }
