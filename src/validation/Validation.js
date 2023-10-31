1b5ddd3f1fa4ccc4ae0b22e2eb9f36550d0732a1
lee juyoung <juyung0903@naver.com>
2023-10-31 16:38:53 +0900

    refactor: 에러메시지 및 콘솔메시지 상수화

--- src/validation/Validation.js
+++ src/validation/Validation.js
@@ -1,9 +1,11 @@
+import { ERROR_MESSAGE } from "../Constants.js";
+
 class Validation {
 
   // 사용자가 입력한 차 이름 입력값 검증
   static validateCarNameInput(input) {
-    this.isNoComma(input);
     this.isemptyInput(input);
+    this.isNoComma(input);
   }
 
   // 각 차이름 검증
@@ -24,42 +26,42 @@ class Validation {
   // 입력값에 ','가 있는지 검증
   static isNoComma(input) {
     if (!input.includes(",")) {
-      throw new Error("[ERROR] ,가 없거나 하나의 차만 입력하셨습니다.");
+      throw new Error(ERROR_MESSAGE.NO_COMMA);
     }
   }
 
   // 입력을 안하진 않았는지 검증
   static isemptyInput(input) {
     if (!input) {
-      throw new Error("[ERROR] 값을 입력하지 않으셨습니다.");
+      throw new Error(ERROR_MESSAGE.NO_INPUT);
     }
   }
 
   // 쉼표 뒤에 아무것도 입력하지 않았을 때 에러 발생
   static isNoInputafterComma(carName) {
     if (carName === "") {
-      throw new Error("[ERROR] 쉼표 뒤에 아무 것도 입력하지 않으셨습니다.");
+      throw new Error(ERROR_MESSAGE.NO_INPUT_AFTER_COMMA);
     }
   }
 
   // 5자를 초과 했을 때 에러 발생
   static isexceedsFiveCharacters(carName) {
     if (carName.length > 5) {
-      throw new Error("[ERROR] 5자를 초과해서 입력할 수 없습니다.");
+      throw new Error(ERROR_MESSAGE.EXCEED_FIVE_CHARACTORS);
     }
   }
 
   // 중복된 차 이름이 있다면 에러 발생
   static validateDuplicateCarName(carNames) {
      if(new Set(carNames).size !== Number(carNames.length)) {
-        throw new Error('[ERROR] 차 이름이 중복되었습니다.');
+        throw new Error(ERROR_MESSAGE.DUPLICATE_CAR_NAME);
      }
   }
 
   // 숫자를 입력하지 않았을 때
   static isNotNumber(input) {
     if (isNaN(input)) {
-      throw new Error('[ERROR] 숫자를 입력하지 않으셨습니다.');
+      throw new Error(ERROR_MESSAGE.NO_NUMBER);
     }
   }
 }
--- src/view/GameUi.js
+++ src/view/GameUi.js
@@ -1,13 +1,14 @@
-import { MissionUtils } from '@woowacourse/mission-utils';
-import { CARS } from '../Constants.js';
-import Validation from '../validation/Validation.js';
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { CARS, INPUT_MESSAGE, OUTPUT_MESSAGE } from "../Constants.js";
+import Validation from "../validation/Validation.js";
+
 class GameUi {
   constructor() {}
 
   // 사용자가 차이름을 입력
   async askCarName() {
     const CAR_NAMES_INPUT = await MissionUtils.Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+      INPUT_MESSAGE.INPUT_CAR_NAMES
     );
     Validation.validateCarNameInput(CAR_NAMES_INPUT);
     return CAR_NAMES_INPUT;
@@ -16,7 +17,7 @@ class GameUi {
   // 사용자가 시도할 횟수를 입력
   async askAttemptCount() {
     const ATTEMPT_COUNT_INPUT = await MissionUtils.Console.readLineAsync(
-      '시도할 횟수는 몇 회인가요?\n'
+      INPUT_MESSAGE.INPUT_ATTEMPT_COUNT
     );
     Validation.validateAttemptCountInput(ATTEMPT_COUNT_INPUT);
     return ATTEMPT_COUNT_INPUT;
@@ -24,23 +25,25 @@ class GameUi {
 
   // 레이스 게임 실행 결과 출력 안내 메시지 출력
   printResultMessage() {
-    MissionUtils.Console.print('\n실행 결과');
+    MissionUtils.Console.print(OUTPUT_MESSAGE.RESULT_GUIDE_MESSAGE);
   }
 
   // 각 레이스 게임당 자동차들의 전진현황 출력
   printeachRaceGame() {
     for (let carIndex in CARS) {
-      const CAR_NAME = `${CARS[carIndex].carName}`
+      const CAR_NAME = `${CARS[carIndex].carName}`;
       const CAR_FORWARD_NUMBER = Number(CARS[carIndex].forwardNumber);
-      const FORWARDED = `${'-'.repeat(CAR_FORWARD_NUMBER)}`;
-      MissionUtils.Console.print(`${CAR_NAME} : ${FORWARDED}`);
+
+      MissionUtils.Console.print(
+        OUTPUT_MESSAGE.EACHRACE(CAR_NAME, CAR_FORWARD_NUMBER)
+      );
     }
-    MissionUtils.Console.print('');
+    MissionUtils.Console.print("");
   }
 
   // 최종 우승자 출력
   printWinner(winnerCarNames) {
-    MissionUtils.Console.print(`최종 우승자: ${winnerCarNames}`)
+    MissionUtils.Console.print(OUTPUT_MESSAGE.WINNER(winnerCarNames));
   }
 }
 export default GameUi;
