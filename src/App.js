33feb8fa0c370a301f108b6643f04d05a6fd4d85
steve-shim <steveshim496@gmail.com>
2023-10-31 11:37:40 +0900

    Add validationInput to check user input

--- src/App.js
+++ src/App.js
@@ -26,7 +26,23 @@ class Car {
 }
 
 class App {
-  async play() {}
+  validationInput(carList) {
+    //MissionUtils.Console.print(carList);
+    if (!carList.every((car) => car.length < 5)) {
+      throw new Error("[ERROR] 자동차 이름은 4자 이하만 가능 합니다");
+    }
+    if (carList.length !== [...new Set(carList)].length) {
+      throw new Error("[ERROR] 중복된 자동차 이름이 존재합니다");
+    }
+  }
+
+  async play() {
+    let carList = await MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    carList = carList.split(",").map((car) => car.trim());
+    this.validationInput(carList);
+  }
 }
 
 export default App;
