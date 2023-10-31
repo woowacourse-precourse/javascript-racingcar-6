c0472314bb4ac7f124cb74acf36cad238737cbcd
sihyonn <sihyeon982@naver.com>
2023-10-31 17:17:25 +0900

    feat: 전진하는 기능 추가

--- src/game/CarGame.js
+++ src/game/CarGame.js
@@ -1,7 +1,16 @@
+import generateRandomNum from './RandomNum.js';
+
 // 자동차 위치 초기화 객체
 const initializeCarsPosition = (carNames) => {
   return carNames.reduce((carsPosition, name) => {
     carsPosition[name] = 0;
     return carsPosition;
   }, {});
+};
+
+// 무작위 수 조건에 따라 전진
+const moveCar = (carNames, carsPosition, randomNum) => {
+  carNames.map((name) => {
+    if (randomNum >= 4) carsPosition[name] += 1;
+  });
 };
