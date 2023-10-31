480ac3bc4ffd45da35f91f8d963ce42baa59812d
jjo-mi <whdal2636@naver.com>
2023-10-31 19:34:32 +0900

    [fix] error 후 종료 안되는 오류 수정

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
 import { Random, Console } from "@woowacourse/mission-utils";
 import { MESSAGE, ERROR } from "./common/text.js";
 
+const MAX_LENGTH = 5;
+const MIN_LENGTH = 1;
+
 class App {
   async play() {
-    const CAR_NAME = await makeCar();
-    const MOVE = await inputMoveCount();
-    console.log("다음거", MOVE);
+    const carArr = await makeCar();
+    console.log("차", carArr);
+
+    const move = await inputMoveCount();
+    console.log("다음거", move);
 
     // 3. 몇번 이동하는지 입력받기
   }
 }
 // 1. n개의 자동차 생성
 const makeCar = async () => {
+  let nameArr = [];
   try {
     let name = await Console.readLineAsync(`${MESSAGE.GAME_START}`);
-    let nameArr = name.trim().split(",");
-
-    let validName = await validationName(nameArr);
-    if (validName) {
-      return true;
-    } else if (validName === "length") {
-      throw new Error(`${ERROR.NAME_LENGTH}`);
-    } else {
-      throw new Error(`${ERROR.NAME_COMMA}`);
-    }
+    nameArr = name.trim().split(",");
   } catch (error) {
-    console.error(error.message);
+    return console.error(error.message);
+  }
+  let validName = await validationName(nameArr);
+
+  if (validName) {
+    return nameArr;
+  } else if (validName === "length") {
+    throw new Error(`${ERROR.NAME_LENGTH}`);
+  } else {
+    throw new Error(`${ERROR.NAME_COMMA}`);
   }
 };
 
@@ -37,7 +43,7 @@ const validationName = async (nameArr) => {
     throw new Error(`${ERROR.NAME_COMMA}`);
   } else {
     nameArr.forEach((el, index) => {
-      if (el.length >= 5 || el.length < 1) {
+      if (el.length >= `${MAX_LENGTH}` || el.length < `${MIN_LENGTH}`) {
         throw new Error(
           `${ERROR.NAME_LENGTH} ${index + 1}번째요소에 대한 에러`
         );
@@ -65,7 +71,7 @@ const inputMoveCount = async () => {
 };
 // 4. 이동 입력값 유효성 검토
 const validationCount = async (count) => {
-  console.log("ㅇㅇ", count);
+  console.log("이동", count);
 
   if (isNaN(count)) {
     throw new Error(`${ERROR.COUNT}`);
@@ -73,6 +79,18 @@ const validationCount = async (count) => {
   }
   return true;
 };
+
+// // 이동할 번호 생성
+// const moveNum = () =>
+
+// // 5. 횟수에 맞춰서 랜덤값 생성
+// const repeatPrint = (move) => {
+//   let cnt = 0;
+//   while (move >= cnt) {
+
+//   }
+
+// }
 
 export default App;
 
