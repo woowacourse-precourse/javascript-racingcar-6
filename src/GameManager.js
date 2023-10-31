f42020ef2f173f47c7804d357b8cde2c87d4d05d
Nam-Sumin <52562061+cerezo00@users.noreply.github.com>
2023-10-31 12:37:51 +0900

    comment: describe the functions in GameManager

--- src/GameManager.js
+++ src/GameManager.js
@@ -7,20 +7,30 @@ class GameManager {
   /** @type {Board} */
   #board = null;
 
+  /**
+   * 프로그램을 실행한다.
+   */
   async play() {
     await this.#startGame();
     this.#playGame();
     this.#finishGame();
   }
 
+  /**
+   * 사용자로부터 자동차 이름과 시도 횟수를 입력받는다.
+   */
   async #startGame() {
     this.#board = new Board();
     Console.print(Strings.INPUT_CAR_NAMES);
     await this.#board.setCars()
     Console.print(Strings.INPUT_NUM_TURNS);
     this.#board.setNumTurns()
+    Console.print(Strings.NULL);
   }
 
+  /**
+   * 시도 횟수만큼 자동차 이동을 반복 시행하고, 매 회 진행상황을 출력한다.
+   */
   #playGame() {
     const numTurns = this.#board.getNumTurns()
     Console.print(Strings.GAME_RESULT);
@@ -30,6 +40,9 @@ class GameManager {
     }
   }
 
+  /**
+   * 최종 결과를 출력한다.
+   */
   #finishGame() {
     this.#board.pickOutWinner();
     this.#board.printFinalResult();
