69331196cb563a13e6f4d669527c6153f06595e0
kwonja <kownja87@gmail.com>
2023-10-31 22:53:27 +0900

    [feat] : 자동차 이름은 쉼표(,)을 기준으로 구분하며 이름은 5자 이하만 가능하다.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
 
-  async racingCarNameMessage(){
+  async racingCarNameInput(){
      const carNameInput = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+     if(!this.checkCarNameValidation(carNameInput))
+     {
+      throw new Error("[ERROR] 이름은 5자 이하만 가능합니다.");
+     }
+  }
+  checkCarNameValidation(carNameInput)
+  {
+    const carNames = carNameInput.split(',');
+    return carNames.every( (carName)=> carName.length < 6)
   }
   async play() {
-    this.racingCarNameMessage();
+    this.racingCarNameInput();
   }
 }
 
