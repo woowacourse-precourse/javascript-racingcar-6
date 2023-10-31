6978ec0a60a705009d67c5c846cba261d406eb11
Klomachenko <leekyumin0901@naver.com>
2023-11-01 02:58:43 +0900

    feat(app, validTeat): 자동차 이름 유효성 검사 기능 구현
    Description: 입력받은 자동차 이름의 유효성을 검사하고, 에러를 출력하는
    기능을 추가하였습니다. 자동차 이름 입력에 대한 유효성 검사 테스트를
    추가하였습니다.

--- __tests__/ValidTest.js
+++ __tests__/ValidTest.js
@@ -2,11 +2,19 @@ import App from "../src/App.js";
 
 describe('App 테스트', () => {
 	let app;
-	app = new App();
+
+	beforeEach(() => {
+		app = new App();
+	});
 
 	test('입력 받은 자동차 이름을 배열 변환 테스트', () => {
 		const input = 'abc,def';
 		const result = app.validateCarNames(input.split(','));
 		expect(result).toBeTruthy();
+	});
+
+	test('자동차 입력 유효성 검사 테스트', () => {
+		const input = 'abcdefg,hijklmnop';
+		expect(() => app.validateCarNames(input.split(',')));
 	});
 });
--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 class App {
   async play() {
     const carNames = await this.getCarNameInput();
+
+    if (!this.validateCarNames(carNames)) return;
   }
 
   async getCarNameInput() {
     const carNamesInput = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)로 구분, 5자 이하):");
     const carNames = carNamesInput.split(",");
     return carNames;
+  }
+
+  validateCarNames(carNames) {
+    if (carNames.some(name => name.length > 5)) {
+      MissionUtils.Console.readLineAsync("[ERROR] 자동차 이름은 5자 이하여야 합니다.");
+    }
+    return true;
   }
 }
 
