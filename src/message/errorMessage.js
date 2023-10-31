54b0c609fe09fde815eb0a37aefe65e4c739f46a
이진택 <109197023+LEEJINTAEK@users.noreply.github.com>
2023-10-31 20:03:08 +0900

    feat: 예외처리 양수가 아닐 때 추가

--- src/message/errorMessage.js
+++ src/message/errorMessage.js
@@ -2,4 +2,5 @@ export const INPUT_ERROR = {
   lengthError: '[ERROR] 이름의 길이가 형식과 다릅니다.',
   spaceError: '[ERROR] 입력에 공백이 있습니다.',
   numError: '[ERROR] 숫자 외에 입력이 있습니다.',
+  numValueError: '[ERROR] 양수가 아닙니다.',
 };
