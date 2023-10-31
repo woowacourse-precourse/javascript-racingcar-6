301c4f72cc14f62a5beef5a989b3a2cfdd8f0f02
BaeDongWoo <ehddn5410@naver.com>
2023-10-31 17:50:03 +0900

    feat:올바르지 않은 입력일 경우 예외처리

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import { Random, Console } from '@woowacourse/mission-utils';
 class App {
   async play() {
-    const input = await this.inputPlayer();
-    console.log(input);
+    try {
+      const input = await this.inputPlayers();
+      this.validateInputPlayers(input);
+    } catch (e) {
+      return Promise.reject(e);
+    }
   }
-  inputPlayer() {
+  inputPlayers() {
     const input = Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
     );
     return input;
+  }
+  validateInputPlayers(input) {
+    const players = input.split(',');
+    if (players.length < 2) {
+      throw new Error('플레이어는 1명 이상입력해주세요');
+    }
+    players.map((player) => this.validatePlayer(player));
+    console.log('올바른 입력입니다.');
+  }
+  validatePlayer(player) {
+    console.log(player);
+    if (player.trim() === '') {
+      throw new Error('잘못된 입력입니다.');
+    }
+    if (player.length > 5) {
+      throw new Error('잘못된 입력입니다.');
+    }
   }
 }
 const app = new App();
