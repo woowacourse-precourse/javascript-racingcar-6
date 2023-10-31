93fc3494a590cb862b7394fcf3c8553623dc4a65
Taek Ki Min <127868094+TaekkiMin@users.noreply.github.com>
2023-10-31 17:06:03 +0900

    feat : 자동차 이름 입력 받는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,60 @@
-class App {
-  async play() {}
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+class Car {
+  constructor() {
+    this.name = name;
+    this.distance = 0;
+  }
+
+  move(randomGoNum) {
+    if (randomGoNum >= 4) {
+      this.distance++;
+    }
+  }
+
+  getDistance() {
+    return "-".repeat(this.distance);
+  }
 }
 
-export default App;
+class App {
+  constructor() {
+    this.cars = [];
+    this.numberOfAttempts = 0;
+  }
+
+  validateCarNames(carNames) {
+    for (let i = 0; i < carNames.length; i++) {
+      if (carNames[i].length === 0 || carNames[i].length > 5) {
+        throw new Error(
+          "[ERROR] 자동차 이름은 1자 이상 5자 이하이어야 합니다."
+        );
+      }
+    }
+  }
+
+  async play() {
+    const input = await MissionUtils.Console.readLineAsync(
+      "경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분"
+    );
+    const carNames = input.split(",");
+    this.validateCarNames(carNames);
+    this.cars = carNames.map((name) => new Car(name));
+
+    this.numberOfAttempts = Number(
+      await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요")
+    );
+    if (isNaN(this.numberOfAttempts) || this.numberOfAttempts <= 0) {
+      throw new Error("[ERROR] 올바른 횟수를 입력하세요.");
+    }
+  }
+
+  getCarNames() {
+    // 자동차 이름 입력 받기
+    const input = MissionUtils.Console.readLineAsync(
+      "경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분"
+    );
+    this.validateCarNames(carNames);
+    this.carNames = input.split(",");
+  }
+}
