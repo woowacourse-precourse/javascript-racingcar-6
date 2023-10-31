f8214409f0d78495f8625c6d033cd9385a5de764
hyj5076 <hyj5076@hanmail.net>
2023-10-31 20:47:48 +0900

    refactor: 게임 실행 결과 상수 변경 및 출력 코드 추가

--- src/utils/RoundManager.js
+++ src/utils/RoundManager.js
@@ -1,6 +1,6 @@
 import { Console } from "@woowacourse/mission-utils";
 import { movingForwardCase } from "./MovingForwardCase";
-import { FORWARD_DASH } from "../const/Messages";
+import { FORWARD_DASH, PLAY_LOG } from "../const/Messages";
 
 class RoundManager {
   constructor(carPositions) {
@@ -8,6 +8,8 @@ class RoundManager {
   }
 
   playRound() {
+    Console.print(PLAY_LOG);
+
     Object.keys(this.carPositions).forEach((carName) => {
       if (movingForwardCase()) {
         this.carPositions[carName] += FORWARD_DASH;
