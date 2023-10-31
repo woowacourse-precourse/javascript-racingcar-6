6d96024f04cb6bcde868a47abe62be9270570c5c
chichoc <kny6152@naver.com>
2023-11-01 07:06:15 +0900

    feat: 시도할 횟수 입력받는 함수 생성

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import { Random, Console } from '@woowacourse/mission-utils';
 class App {
   constructor() {
     this.carName = [];
+    this.attempts = 0;
   }
   async play() {
     this.carName = await this.getCarName();
+    this.attempts = await this.getAttempts();
   }
 
   async getCarName() {
     const input = await Console.readLineAsync(`경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n`);
     return input.split(',');
   }
+
+  async getAttempts() {
+    const input = await Console.readLineAsync(`시도할 횟수는 몇 회인가요?\n`);
+    return +input;
+  }
+
 }
 
 export default App;
