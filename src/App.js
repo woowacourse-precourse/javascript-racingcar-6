a0e7a9e7f10d8b20ea2523e4370f3c277a1cc0f9
kmkkkp <kmkkkp@ajou.ac.kr>
2023-10-31 22:01:40 +0900

    feat: carName Parsing and Validate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { parse } from "@babel/core";
 import {Random, Console} from "@woowacourse/mission-utils";
 
 class App {
   async play() {
     const carName = await getCarInput();
     console.log(carName);
+    let carNames = parsing(carName);
+    console.log(carNames[0]);
     const tryNum = await getTryNumInput();
     console.log(tryNum);
+
   }
 }
 
@@ -24,9 +28,24 @@ async function getTryNumInput(){
   return tryNumInput;
 }
 
-function ParsingAndValidate(){
+function parsing(carNames){
+  const strings = carNames.split(',');
+  const parsedStrings =  [];
 
+  for(let i=0; i<strings.length; i++){
+    const str = strings[i].trim();
+    validate(str);
+    parsedStrings.push(str);
+  }
+  return parsedStrings;
 }
+
+function validate(carName){
+  if (carName === '' || !/^[a-zA-Z]+$/.test(carName)){
+    throw Error("[Error] ")
+  }
+}
+
 
 function getRandom(){
 
