e9441c8acfed6ea893281f145df1b10734394ce8
yuziwoo <yuziwoo@naver.com>
2023-11-01 08:33:52 +0900

    test: 레이싱 게임 모델 테스트 추가
    test: 전진 여부를 결정짓고, 전진한 값을 추가하는 함수 테스트 추가
    test: 배열에서 최종 우승자를 추려 반환하는 함수  테스트 추가

---
+++ __tests__/models/CarRacingGameTest.js
@@ -0,0 +1,47 @@
+import CarRacingGames from "../../src/models/carRacingGame"
+
+describe("CarRacingGame 모델 기능 테스트", () => {
+  test("입력 받은 자동차 이름 문자열을 배열의 형태로 반환", () => {
+    const carListString = "pobi, jiwoo";
+    const result = CarRacingGames.setupCarList(carListString);
+
+    expect(result[0].name).toBe("pobi");
+    expect(result[1].name).toBe("jiwoo");
+  });
+
+  test("전진 여부를 결정짓고, 값을 추가한다.", () => {
+    const carListArr = [
+      {
+        name: "pobi",
+        numberOfMovesForward: 0,
+      },
+      {
+        name: "jiwoo",
+        numberOfMovesForward: 0,
+      }
+    ];
+    while(true) {
+      CarRacingGames.decideWheterToMoveForward(carListArr);
+      
+      if (carListArr.findIndex((car) => car.numberOfMovesForward > 0) >= 0) {
+        break;
+      }
+    }
+  });
+
+  test("배열에서 최종 우승자를 추려 반환한다.", () => {
+    const carListArr = [
+      {
+        name: "pobi",
+        numberOfMovesForward: 0,
+      },
+      {
+        name: "jiwoo",
+        numberOfMovesForward: 2,
+      }
+    ];
+    const result = CarRacingGames.getFinalWinner(carListArr);
+
+    expect(result[0].name).toBe("jiwoo");
+  });
+})
