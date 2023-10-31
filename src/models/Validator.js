04c3316894a40f8ebe4cd6cd2174c32e79b53a0e
yuziwoo <yuziwoo@naver.com>
2023-10-31 10:07:40 +0900

    style: 불필요한 세미 콜론 제거

--- src/models/Validator.js
+++ src/models/Validator.js
@@ -19,23 +19,23 @@ export default class Validator {
       // 자동차 이름의 길이가 최소 길이보다 작은 경우 에러를 발생시킨다.
       if (carNames.length < CAR_NAMES.MIN_LENGTH) {
         throw new Error(ERROR_MESSAGE.INPUT_CAR_NAMES_TOO_SHORT);
-      };
+      }
 
       // 자동차 이름의 길이가 최대 길이보다 긴 경우 에러를 발생시킨다.
       if (carNames.length > CAR_NAMES.MAX_LENGTH) {
         throw new Error(ERROR_MESSAGE.INPUT_CAR_NAMES_TOO_LONG);
-      };
+      }
     });
 
     // 자동차 이름이 중복된 경우 에러를 발생시킨다.
     if (carListArr.length !== new Set(carListArr).size) {
       throw new Error(ERROR_MESSAGE.INPUT_CAR_NAME_IS_DUPLICATED);
-    };
+    }
 
     // 자동차의 대수를 최소 대수 미만으로 입력한 경우
     if (carListArr.length < CARS.MIN_NUMBER_OF_CARS) {
       throw new Error(ERROR_MESSAGE.INPUT_CARS_LESS_THAN_TWO);
-    };
+    }
 
     return true;
   };
