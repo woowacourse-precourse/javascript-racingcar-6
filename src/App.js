3bba78c4be0a76720d3188254e9977a7bc10df6b
westofsky <westofsky159@gmail.com>
2023-10-31 21:45:30 +0900

    refactor: validation 함수 분리

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Car } from './models/CarModel.js';
 import { MESSAGES } from './constants/messages.js';
 import { ERRORS } from './constants/errors.js';
 import { isNumber } from './utils/validationUtils.js';
+import { InputValidator } from './validation/inputValidator.js';
 class App {
   constructor(){
     this.carList = [];
@@ -22,7 +23,7 @@ class App {
     
     // 유효성 검사
     carNames.forEach ((carName) => {
-      if(!this.isValidCarName(carName))
+      if(!InputValidator.isValidCarName(carName))
         throw new Error(ERRORS.INVALID_INPUT);
       else
         this.carList.push(new Car(carName));
@@ -31,7 +32,7 @@ class App {
   async getPlayCount(){
     // 1-2. 몇 번 이동을 진행할지 입력받기
     const playCountInput = await Console.readLineAsync(MESSAGES.GET_PLAY_COUNT);
-    if(!this.isValidPlayCountInput(playCountInput)){
+    if(!InputValidator.isValidPlayCountInput(playCountInput)){
       throw new Error(ERRORS.INVALID_INPUT);
     }
     this.gameCount = playCountInput;
@@ -67,28 +68,6 @@ class App {
   }
   getWinner(winnerLength){
     return this.carList.filter(car => car.getMovingLength() === winnerLength).map(car => car.getName());
-  }
-  isValidCarName(carName){
-    // 이름에 없는 경우 ex) ,,tom,elice
-    if (carName === '') return false;
-    
-    // 이름이 6글자 이상인 경우
-    if (carName.length >= 6) return false;
-    
-    // 이름에 공백이 있는 경우
-    if (carName.includes(' ')) return false;
-    return true;
-  }
-  isValidPlayCountInput(playCount){ 
-    // 입력에 공백이 있는 경우
-    if (playCount === '') return false;
-    // 숫자가 아닌 경우
-    if (!isNumber(playCount)) return false;
-    
-    // 1이상의 수가 아닌 경우
-    if (Number(playCount) < 1) return false;
-    
-    return true;
   }
 }
 
--- src/utils/validationUtils.js
+++ src/utils/validationUtils.js
@@ -2,3 +2,6 @@ export const isNumber = (str) => {
   const number = Number(str);
   return Number.isInteger(number);
 }
+export const isIncludeSpace = (str) => {
+  return str.includes(' ');
+}
