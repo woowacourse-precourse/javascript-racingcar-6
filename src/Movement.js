cde50d89bcaf39c812ca6afbb43944d4e55cd7b0
byunjiin <jibyun5808@naver.com>
2023-10-31 19:00:10 +0900

    feat: 기능3. 랜덤값을 받아 자동차 이동

--- src/Movement.js
+++ src/Movement.js
@@ -1,4 +1,14 @@
-//기능3.
-import { Console } from "@woowacourse/mission-utils";
+//기능3. 랜덤값을 받아 자동차 이동
+import { Console } from '@woowacourse/mission-utils';
+import { random } from './Random.js';
+import { repeat } from './Repeat.js';
 
-Random.pickNumberInRange();
+export function movement(count, name, move) {
+  let temp = move;
+  for (let i = 0; i < count; i++) {
+    let output = random(name);
+    temp = repeat(name, move, output);
+    Console.print(``); // 공백 나옴
+  }
+  return temp;
+}
