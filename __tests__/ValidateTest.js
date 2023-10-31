928644321b2cdc7061572184c42958c9c145e0f7
JUNU <bokjunwoo@gmail.com>
2023-10-31 21:01:01 +0900

    test : splitAndTrim 검증 및 중복 자동차 이름에 대한 검증 테스트 추가

--- __tests__/ValidateTest.js
+++ __tests__/ValidateTest.js
@@ -1,6 +1,17 @@
-import { validateCarNames, validateNumberInput } from '../src/utils';
+import {
+  splitAndTrim,
+  validateCarNames,
+  validateNumberInput,
+} from '../src/utils';
 
 describe('유효성 검사 테스트', () => {
+  describe('splitAndTrim', () => {
+    it('빈 값 입력에 대한 예외처리', () => {
+      const validInput = '';
+      expect(() => splitAndTrim(validInput)).toThrow('[ERROR]');
+    });
+  });
+
   describe('validateCarNames', () => {
     it('유효한 자동차 이름에 대해 true를 반환', () => {
       const validInput = ['Car1', 'Car2', 'Car3'];
@@ -21,6 +32,11 @@ describe('유효성 검사 테스트', () => {
       const validInput = ['Car1'];
       expect(() => validateCarNames(validInput)).toThrow('[ERROR]');
     });
+
+    it('중복된 자동차 이름이 들어온 경우에 대한 예외처리', () => {
+      const validInput = ['Car1', 'Car1'];
+      expect(() => validateCarNames(validInput)).toThrow('[ERROR]');
+    });
   });
 
   describe('validateNumberInput', () => {
@@ -31,11 +47,6 @@ describe('유효성 검사 테스트', () => {
 
     it('숫자가 아닌 입력에 대한 예외처리', () => {
       const validInput = 'abc';
-      expect(() => validateNumberInput(validInput)).toThrow('[ERROR]');
-    });
-
-    it('빈 문자열 입력에 대한 예외처리', () => {
-      const validInput = '';
       expect(() => validateNumberInput(validInput)).toThrow('[ERROR]');
     });
 
