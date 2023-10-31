c1a2cab759c5d6de15cbaca9ba3edc3faf450389
SWBAE0501 <tkddnjsgla@naver.com>
2023-11-01 00:42:59 +0900

    CHORE: improving readability

--- src/App.js
+++ src/App.js
@@ -11,20 +11,25 @@ class App {
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
     const carNames = carNamesInput.split(',').map(name => name.trim());
+
     if ( carNames.some(name => name === null || name === undefined || name.length > 5)) {
       throw new Error("[ERROR] 이름이 잘못된 형식입니다. 게임 종료");
     }
+
     const cars = carNames.map(name => new Car(name));
     cars.forEach(car => {
       car.position = '';
     });
     const raceCount = await MissionUtils.Console.readLineAsync(
       '시도할 횟수는 몇 회인가요?\n');
+
     if ( isNaN(raceCount) || raceCount === 0 ) {
       throw new Error("[ERROR] 숫자가 잘못된 형식입니다. 게임 종료");
     }
+
     MissionUtils.Console.print("\n실행 결과");
     const racingGame = new RacingGame();
+
     for (let i = 0; i < raceCount; i++){
       racingGame.race(cars);
       cars.forEach(car => {
@@ -32,6 +37,7 @@ class App {
       });
       MissionUtils.Console.print('\n');
     }
+
     const victory = new Victory();
     victory.compare(cars);
   }    
--- src/race.js
+++ src/race.js
@@ -1,19 +1,22 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
-class RacingGame { // 레이싱 게임 결과를 리턴하는 클래스
+class RacingGame {
     async race(cars) {
+
         for (const car of cars) {
             car.position = this.getRaceCount(car.position);
         }
-        // return cars;
+
     }
 
     getRaceCount(position) {   
         const num = MissionUtils.Random.pickNumberInRange(1, 9);
+
         if (num >= 4) {
             position += '-';
         }
         return position;
+        
     }
 }
 
