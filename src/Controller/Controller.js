1f9903996eec8b114112e508c885ae6e574c9fbf
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-10-31 17:16:19 +0900

    feat($Controller) : ✨ 무작위 값에 따른 전진 기능 추가

--- src/Controller/Controller.js
+++ src/Controller/Controller.js
@@ -1,14 +1,26 @@
 import User from '../Model/User'
+import Race from '../Model/Race'
 
 export default class Controller {
 	constructor() {
 		this.carsObj = {};
 		this.attempts = '';
 		this.user = new User();
+		this.race = new Race();
 	}
 
 	async readyGame() {
 		this.carsObj = await this.user.getCarNames();
 		this.attempts = await this.user.getNumberOfAttempts();
+	}
+
+	startGame() {
+		let isPlaying = Number(this.attempts);
+
+		while (isPlaying > 0) {
+			this.race.rollDiceAndGoForward(this.carsObj);
+
+			isPlaying -= 1;
+		}
 	}
 }
--- src/Model/Race.js
+++ src/Model/Race.js
@@ -5,4 +5,17 @@ export default class Race {
 	getRandomNumber() {
 		return MissionUtils.Random.pickNumberInRange(0, 9);
 	}
+
+  rollDiceAndGoForward(carsObj) {
+		const NAMES = Object.keys(carsObj);
+
+		for (let i = 0; i < NAMES.length; i += 1) {
+			const RANDOM_NUMBER = this.getRandomNumber();
+			if (RANDOM_NUMBER >= MOVING_FORWARD) {
+				carsObj[NAMES[i]] += 1;
+			}
+		}
+
+		return carsObj
+	}
 }
