cf1348542f918f3d339c1fe548f2d93c42b3a023
jayming66 <mjw8941@gmail.com>
2023-11-01 03:08:48 +0900

    refactor : cars를 dictionary가 아닌 Class 객체로 만든다.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import Car from "./Car.js";
 
 class App {
   async play() {
-    const carName = await MissionUtils.Console.readLineAsync(
+    const carNames = await MissionUtils.Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
 
-    const carArray = carName.split(",");
-    const carAndDistance = {};
+    let carArray = carNames.split(",");
 
-    carArray.forEach((car) => {
-      carAndDistance[car] = 0;
-    });
+    carArray = carArray.map((carName) => new Car(carName));
   }
 }
 
