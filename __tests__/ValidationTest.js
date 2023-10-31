7f207ad93b50d015634905597a9834cb5858297a
anttiey <hanuuny@gmail.com>
2023-10-31 17:11:07 +0900

    test: 유효성 테스트 케이스 이름 및 결과 수정
    - 더욱 명확한 설명을 제공하도록 테스트 케이스 이름 수정
    - 더욱 정확한 결과를 나타내도록 테스트 결과 수정

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,5 +1,6 @@
 import App from '../src/App.js';
 import Car from '../src/Car.js';
+import { ERRORS } from '../src/Constants.js';
 import { MissionUtils } from '@woowacourse/mission-utils';
 
 const mockQuestions = (inputs) => {
@@ -12,7 +13,7 @@ const mockQuestions = (inputs) => {
 };
 
 describe('자동차 이름 입력 유효성 테스트', () => {
-  test('자동차 이름이 정상적으로 입력된 경우', async () => {
+  test('자동차 이름이 정상적으로 입력된 경우 Car 객체 배열 반환', async () => {
     const input = ['aria,evan'];
 
     const app = new App();
@@ -25,7 +26,7 @@ describe('자동차 이름 입력 유효성 테스트', () => {
     );
   });
 
-  test('자동차 이름이 하나만 입력된 경우', async () => {
+  test('자동차 이름이 하나만 입력된 경우 Car 객체 배열 반환', async () => {
     const input = ['aria'];
 
     const app = new App();
@@ -36,42 +37,42 @@ describe('자동차 이름 입력 유효성 테스트', () => {
     expect(JSON.stringify(result)).toEqual(JSON.stringify([new Car('aria')]));
   });
 
-  test('자동차 이름이 입력되지 않은 경우', async () => {
+  test('자동차 이름이 입력되지 않은 경우 오류 발생', async () => {
     const input = [];
 
     const app = new App();
     mockQuestions(input);
 
     await expect(async () => await app.getRacingCarNames()).rejects.toThrow(
-      '[ERROR]'
+      ERRORS.null
     );
   });
 
-  test('자동차 이름이 중복되는 경우', async () => {
+  test('자동차 이름이 중복되는 경우 오류 발생', async () => {
     const input = ['aria,aria'];
 
     const app = new App();
     mockQuestions(input);
 
     await expect(async () => await app.getRacingCarNames()).rejects.toThrow(
-      '[ERROR]'
+      ERRORS.carName.duplicate
     );
   });
 
-  test('자동차 이름이 다섯 글자보다 긴 경우', async () => {
+  test('자동차 이름이 다섯 글자보다 긴 경우 오류 발생', async () => {
     const input = ['ariana,evan'];
 
     const app = new App();
     mockQuestions(input);
 
     await expect(async () => await app.getRacingCarNames()).rejects.toThrow(
-      '[ERROR]'
+      ERRORS.carName.length
     );
   });
 });
 
 describe('자동차 경주 횟수 입력 유효성 테스트', () => {
-  test('자동차 경주 횟수가 정상적으로 입력된 경우', async () => {
+  test('자동차 경주 횟수가 정상적으로 입력된 경우 입력된 값 반환', async () => {
     const input = ['3'];
 
     const app = new App();
@@ -82,58 +83,58 @@ describe('자동차 경주 횟수 입력 유효성 테스트', () => {
     expect(result).toEqual('3');
   });
 
-  test('자동차 경주 횟수가 입력되지 않은 경우', async () => {
+  test('자동차 경주 횟수가 입력되지 않은 경우 오류 발생', async () => {
     const input = [''];
 
     const app = new App();
     mockQuestions(input);
 
     await expect(async () => await app.getRacingGameCount()).rejects.toThrow(
-      '[ERROR]'
+      ERRORS.null
     );
   });
 
-  test('자동차 경주 횟수가 숫자가 아닌 경우', async () => {
+  test('자동차 경주 횟수가 숫자가 아닌 경우 오류 발생', async () => {
     const input = ['str'];
 
     const app = new App();
     mockQuestions(input);
 
     await expect(async () => await app.getRacingGameCount()).rejects.toThrow(
-      '[ERROR]'
+      ERRORS.gameCount.type
     );
   });
 
-  test('자동차 경주 횟수가 소수인 경우', async () => {
+  test('자동차 경주 횟수가 소수인 경우 오류 발생', async () => {
     const input = ['1.8'];
 
     const app = new App();
     mockQuestions(input);
 
     await expect(async () => await app.getRacingGameCount()).rejects.toThrow(
-      '[ERROR]'
+      ERRORS.gameCount.range
     );
   });
 
-  test('자동차 경주 횟수가 0인 경우', async () => {
+  test('자동차 경주 횟수가 0인 경우 오류 발생', async () => {
     const input = ['0'];
 
     const app = new App();
     mockQuestions(input);
 
     await expect(async () => await app.getRacingGameCount()).rejects.toThrow(
-      '[ERROR]'
+      ERRORS.gameCount.range
     );
   });
 
-  test('자동차 경주 횟수가 음수인 경우', async () => {
+  test('자동차 경주 횟수가 음수인 경우 오류 발생', async () => {
     const input = ['-1'];
 
     const app = new App();
     mockQuestions(input);
 
     await expect(async () => await app.getRacingGameCount()).rejects.toThrow(
-      '[ERROR]'
+      ERRORS.gameCount.range
     );
   });
 });
