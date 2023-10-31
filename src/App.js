035c72319d9ad81a38d71de2e516b583d60ee0eb
KangYunJi <dbswl701@ajou.ac.kr>
2023-10-31 23:11:14 +0900

    feat: 자동차 이름 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    // 자동차 이름 입력
+    const carStr = await MissionUtils.Console.readLineAsync('닉네임을 입력해주세요.');
+    const carArr = carStr.split(',');
+
+  }
 }
 
 export default App;
