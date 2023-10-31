64fbda9cebab84048cab081437c079098532e59b
ssoferret <owosso1212@gmail.com>
2023-10-31 22:37:12 +0900

    test : 경주차 이름 예외 처리 테스트 작성.

--- __tests__/VerificationTest.js
+++ __tests__/VerificationTest.js
@@ -4,24 +4,20 @@
 import { Console } from "@woowacourse/mission-utils";
 import App from "../src/App.js";
 
-const mockQuestions = (inputs) => {
-  Console.readLineAsync = jest.fn();
-  Console.readLineAsync.mockImplementation(() => {
-    return Promise.resolve(inputs);
-  });
-};
-
-const getLogSpy = () => {
-  const logSpy = jest.spyOn(Console, "print");
-  logSpy.mockClear();
-  return logSpy;
-};
-
 describe("함수별 기능 테스트", () => {
-  test("경주차 생성자 만드는 함수(makeCars(cars))에 대한 테스트", () => {
+  test("경주차 생성자 만드는 함수에 대한 테스트", () => {
     const input = ["abc","def","ghi"];
     const app = new App();
     const result = `${app.makeCars(input)}`;
     expect(result).toContain("object Object"); 
+  });
+
+  test("경주차 이름 예외 처리 테스트", () => {
+    const inputs = ["", "abc,defghij","abc,,defg","abc","abc,de,de,fg"];
+    const app = new App();
+    for (let i = 0; i < inputs.length; i += 1) {
+      mockQuestions(inputs[i]);
+      expect(app.inputCarNames()).rejects.toThrow("[ERROR]");
+    }
   });
 });
