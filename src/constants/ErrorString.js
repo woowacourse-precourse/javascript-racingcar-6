c5d3b2e114d9047bd9cfc69a7853964711ba3085
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-10-31 14:06:26 +0900

    ✨ Feat: 입력한 자동차 이름이 5자 이하가 아닐 경우 예외 처리 추가

---
+++ src/constants/ErrorString.js
@@ -0,0 +1,5 @@
+const ErrorString = {
+  ERROR_CARS_LENGTH: '[ERROR] 자동차 이름은 5자 이하로 입력해주세요.',
+};
+
+export const { ERROR_CARS_LENGTH } = ErrorString;
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,9 +1,12 @@
 import { Console } from '@woowacourse/mission-utils';
 import { INPUT_CARS_NAME, INPUT_COUNT } from '../constants/InputString.js';
+import { ERROR_CARS_LENGTH } from '../constants/ErrorString.js';
 
 const InputView = {
   readCarsInput: async () => {
     const inputCars = await Console.readLineAsync(INPUT_CARS_NAME);
+
+    validateCarsInput(inputCars);
 
     return inputCars;
   },
@@ -13,6 +16,14 @@ const InputView = {
 
     return inputCount;
   },
+
+  validateCarsInput: inputCars => {
+    const inputCarsArray = inputCars.split(',').map(inputCar => inputCar.trim());
+
+    inputCarsArray.map(inputCar => {
+      if (inputCar.length > 5) throw new Error(ERROR_CARS_LENGTH);
+    });
+  },
 };
 
-export const { readCarsInput, readCountInput } = InputView;
+export const { readCarsInput, readCountInput, validateCarsInput } = InputView;
