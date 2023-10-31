e858c4f4f9c6ce2a19cce954cf26f644b54fbbd5
geunwoo <olo90632951@gmail.com>
2023-11-01 00:14:11 +0900

    feat: 자동차 이름을 입력 받는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-class App {
-  async play() {}
+import { Console } from '@woowacourse/mission-utils';
+
+class App {    
+  async play() {
+    const carNamesInput = await Console.readLineAsync('경주할 자동차이름을입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');   
+  }
 }
 
 export default App;
