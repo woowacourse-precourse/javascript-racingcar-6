462d29b2496c5c9cb413b8cae193658e15afc21d
cho-hyeonjin <gusls2005@gmail.com>
2023-11-01 00:52:55 +0900

    feat: RaceController> 회차별 출력용 객체 생성 메서드 작성

--- src/controller/RaceController.js
+++ src/controller/RaceController.js
@@ -1,1 +1,18 @@
-export default class RaceController {}
+import InputView from '../view/InputView';
+
+export default class RaceController {
+  constructor() {
+    this.inputView = new InputView();
+    this.carList = [];
+    // this.playNumber = 0;
+  }
+
+  // 회차별 출력에 쓸 양식 만들기 { pobi : ----- }
+  async makeCarObj() {
+    const { userInputCarList } = this.inputView;
+    userInputCarList.forEach((userInputCar, index) => {
+      this.carList.userInputCarList[index] = { carName: userInputCar, score: 0 };
+    });
+    return this.carList.userInputCarList;
+  }
+}
