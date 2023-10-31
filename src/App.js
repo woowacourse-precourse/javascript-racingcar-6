b3844a99ea7336c8397f943c87b617c765f36cf6
조경은 <kavvy99@naver.com>
2023-10-31 15:31:10 +0900

    docs: 기능의 흐름 정리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 class App {
-  async play() {}
+  async play() {
+    console.log("CddddLI 애플리케이션 시작");
+
+    
+    console.log(MissionUtils.Random.pickNumberInRange(0, 9))
+
+  }
 }
 
 export default App;
