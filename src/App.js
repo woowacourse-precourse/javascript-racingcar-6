56b9d588f3c6096c281e89a222a3275cee215bee
rokaf340 <rokaf340@gmail.com>
2023-10-31 23:42:06 +0900

    Refactor: Change 함수 분리

--- src/App.js
+++ src/App.js
@@ -1,21 +1,61 @@
-/* eslint-disable guard-for-in */
-/* eslint-disable no-restricted-syntax */
-/* eslint-disable no-plusplus */
+/* eslint-disable max-classes-per-file */
 import { Random, Console } from '@woowacourse/mission-utils';
+
+export class Car {
+  constructor(name, value = 0) {
+    this.name = name;
+    this.value = value;
+  }
+}
+
+export function goStop(carObj) {
+  carObj.forEach((e) => {
+    const randomNum = Random.pickNumberInRange(0, 9);
+    if (randomNum >= 4) {
+      e.value += 1;
+    }
+  });
+}
+
+export function carPrint(carObj) {
+  carObj.forEach((e) => Console.print(`${e.name} : ${'-'.repeat(e.value)}`));
+  Console.print('');
+}
+export function winnerValue(carObj) {
+  const winnerCount = carObj.sort((a, b) => a.value < b.value);
+  const winnerCountArr = winnerCount.map((a) => +a.value);
+  const winNumber = Math.max(...winnerCountArr);
+
+  return winNumber;
+}
+
+export function winner(carObj) {
+  const winNumber = winnerValue(carObj);
+
+  const winName = [];
+  carObj.forEach((element) => {
+    if (element.value === winNumber) {
+      winName.push(element.name);
+    }
+  });
+  Console.print(`최종 우승자: ${winName}`);
+}
+
+export function carPlay(numInput, carObj) {
+  for (let index = 0; index < numInput; index += 1) {
+    goStop(carObj);
+    carPrint(carObj);
+  }
+}
 
 class App {
   async play() {
-    function Car(name, value = 0) {
-      this.name = name;
-      this.value = value;
-    }
-
     const carInput = await Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n ',
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): ',
     );
 
     const numInput = await Console.readLineAsync(
-      '시도할 횟수는 몇 회인가요?\n',
+      '시도할 횟수는 몇 회인가요?: ',
     );
 
     const carArr = carInput.split(',').map((e) => {
@@ -26,54 +66,8 @@ class App {
     });
     const carObj = carArr.map((e) => new Car(e));
 
-    // 전진할지 멈출지 정하는 함수
-    function goStop() {
-      for (const obj of carObj) {
-        const value = Random.pickNumberInRange(0, 9);
-        if (value >= 4) {
-          obj.value += 1;
-        }
-      }
-    }
-
-    // 승자 정하는 함수
-    function winner(winnerCountArr) {
-      const winNubmer = Math.max(...winnerCountArr);
-      const winName = [];
-      for (const obj of carObj) {
-        if (obj.value === winNubmer) {
-          winName.push(obj.name);
-        }
-      }
-      Console.print(`최종 우승자: ${winName}`);
-    }
-
-    // 승리 갯수 정렬 함수
-    function winnerValue() {
-      const winnerCount = carObj.sort((a, b) => a.value < b.value);
-      const winnerCountArr = winnerCount.map((a) => +a.value);
-      // console.log(typeof winnerCountArr[1]);
-      winner(winnerCountArr);
-    }
-
-    // 진행상황 출력함수
-    function carPrint() {
-      for (const obj of carObj) {
-        Console.print(`${obj.name} : ${'-'.repeat(obj.value)}`);
-      }
-      Console.print('');
-    }
-
-    // 게임횟수 실행
-    function carValue() {
-      for (let index = 0; index < numInput; index++) {
-        goStop();
-        carPrint();
-      }
-    }
-
-    carValue();
-    winnerValue();
+    carPlay(numInput, carObj);
+    winner(carObj);
   }
 }
 
