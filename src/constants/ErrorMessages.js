227c16ced40c16de5029795abc90765b5ceca6de
Lee sang Yeop <mma7710@naver.com>
2023-10-31 16:11:11 +0900

    Feat: 자동차 이름이 없는 경우 에러발생 메세지 상수 선언에 추가

--- src/constants/ErrorMessages.js
+++ src/constants/ErrorMessages.js
@@ -2,6 +2,7 @@ import CONDITIONS from './Conditions.js';
 
 const ERROR_MESSAGES = Object.freeze({
 	car_name_exceeds_maximum_digits: `[ERROR] 자동차 글자수가 ${CONDITIONS.car_name_max_digits}를 초과했습니다.`,
+	car_has_no_name: '[ERROR] 자동차의 이름이 있어야 합니다.',
 	duplicate_car_name: '[ERROR] 동일한 자동차 이름이 존재합니다.',
 	exceed_maximum_car_number: `[ERROR] 자동차 최대 개수 ${CONDITIONS.car_maximum_number}대를 넘었습니다.`,
 	use_escape_sequence: '[ERROR] 단어 \\ 은(는) 사용할 수 없습니다.',
