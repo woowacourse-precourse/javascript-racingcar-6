4ee64caa668990b0dd2bc62197e662c6cb5393f5
rlarlduf20 <kky8274@naver.com>
2023-10-31 17:52:31 +0900

    Fix: 자동차 배열 split 사용해서 담기-가독성 향상 및 반복문 감소

--- src/utils.js
+++ src/utils.js
@@ -4,21 +4,10 @@ export const getCarArrFromInputValue = async () => {
   let inputCarNames = await MissionUtils.Console.readLineAsync(
     '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
   );
-  let carArr = [];
-  while (1) {
-    let commaIndex = inputCarNames.indexOf(',');
+  let carArr = inputCarNames.split(',').map((item) => {
+    return { name: item, moveCnt: 0 };
+  });
 
-    if (commaIndex === -1) {
-      carArr.push({ name: inputCarNames.substr(0), moveCnt: 0 });
-      break;
-    }
-    carArr.push({
-      name: inputCarNames.substr(0, commaIndex),
-      moveCnt: 0,
-    });
-
-    inputCarNames = inputCarNames.substr(commaIndex + 1);
-  }
   carArr.forEach((carInfo) => {
     if (carInfo.name.length > 5)
       throw new Error('[ERROR] 자동차 이름은 다섯 글자 이하로 입력해주세요.');
