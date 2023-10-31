d81e4489881e38ae55673807dd624a191feaee4c
ryan-dia <cjfdnjs158@naver.com>
2023-10-31 18:30:27 +0900

    feat: 미션 필수조건인 ERROR prefix 상수화

--- src/constants/Messages.js
+++ src/constants/Messages.js
@@ -3,3 +3,7 @@ export const INPUT_MESSAGE_FUNCTION = Object.freeze({
     return `경주할 ${vehicle} 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분`;
   },
 });
+
+export const ERROR_MESSAGE = Object.freeze({
+  prefix: '[ERROR]',
+});
