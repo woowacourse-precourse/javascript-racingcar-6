290793693a7c79663f5c8c921d348514114335bd
yunji <dbswl330@gmail.com>
2023-11-01 00:15:23 +0900

    feat: 시도횟수 입력받는 함수 구현

--- src/gameView/input.js
+++ src/gameView/input.js
@@ -3,9 +3,14 @@ import { Console } from '@woowacourse/mission-utils';
 class Input {
   async getRacingcarsNames() {
     const racingcarNames = await Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): ',
     );
     return racingcarNames;
+  }
+
+  async getNumberOfAttempt() {
+    const NumberOfAttemps = await Console.readLineAsync('시도 횟수: ');
+    return NumberOfAttemps;
   }
 }
 export default Input;
