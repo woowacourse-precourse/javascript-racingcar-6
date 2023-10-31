aac045772583fe1dad9554d886c7e54e00be39c7
DESKTOP-97R11IF\HWANG2 <tejaia@naver.com>
2023-10-31 16:05:20 +0900

    refactor: 사용자 입력 및 세팅 메소드 분리

--- src/App.js
+++ src/App.js
@@ -13,6 +13,27 @@ class App {
     }
   }
   
+  //사용자 입력, 분리
+  async playerInfoInput(){
+    const playerNameInput = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    const playerList= playerNameInput.split(",").map(str => str.trim());
+    this.playerNumebrCheck(playerList);
+
+    return playerList;
+  }
+
+  //사용자 객체,키리스트 생성
+  playerSetting(playerList){
+    const players= {};
+    for(let i =0; i<playerList.length;i++){
+      const playerName=playerList[i];
+      players[playerName] =0;
+    }
+    const playersKeyList=Object.keys(players);
+
+    return {players,playersKeyList};
+  }
+
   //게임 시작 및 출력
   gamePlay(playerObject,tryNum,keyList,) {
 
@@ -29,26 +50,16 @@ class App {
   }
 
   async play() {
-    //사용자 입력, 분리
-    
-    const playerNameInput = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-    const playerList= playerNameInput.split(",").map(String);
-    this.playerNumebrCheck(playerList);
-    const players= {};
 
-    //사용자 객체생성
-    for(let i =0; i<playerList.length;i++){
-      const playerName=playerList[i];
-      players[playerName] =0;
-    }
 
-    //사용자 객체 키 리스트
-    const playersKeyList=Object.keys(players);
+    const playerList = await this.playerInfoInput();
+    const {players, playersKeyList}= this.playerSetting(playerList);
+
     //시도횟수 입력
     const numberAttempts = Number(await Console.readLineAsync("시도할 횟수는 몇 회인가요?"));
     this.numberCheck(numberAttempts);
+   
     Console.print("실행 결과");
-      
     this.gamePlay(players, numberAttempts,playersKeyList);
 
     //최대값찾기
