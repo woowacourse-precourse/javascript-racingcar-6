3889d46cd9d18fb2cabfb0175dcc75d161bf7ae2
mingyeong:D <63152086+Kim-Mingyeong@users.noreply.github.com>
2023-10-31 23:37:46 +0900

    feat: 경주할 자동차 이름 받는 기능 완료

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@ import { Random, Console } from "@woowacourse/mission-utils";
 import { GAME, ERROR } from "./message";
 
 class App {
+  validateCarName = (carNamesArray) => {
+    // for (let i = 0; i < carNamesArray.length; i++) {
+    //   if (carNamesArray[i].length > 5) throw new Error(ERROR.CAR_NAME);
+    // }
+    for (let i = 0; i < carNamesArray.length; i++) {
+      const carNames = carNamesArray[i].trim(); // 공백 제거
+      if (carNames.length > 5) {
+        throw new Error("ERROR.CAR_NAME");
+      }
+    }
+  };
+
+  getCarName = async () => {
+    const getCarName = await Console.readLineAsync(GAME.GET_CAR_NAME);
+    //const carNamesArray = [...new Set(getCarName.split(",").map(String))];
+    const carNamesArray = getCarName.split(",");
+    this.validateCarName(carNamesArray);
+
+    return carNamesArray;
+  };
   async play() {}
 }
 
