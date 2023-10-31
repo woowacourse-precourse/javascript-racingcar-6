013fc569a43ac87e02dd13e1a9240c0e42d9de2b
Collection50 <fkaus65@naver.com>
2023-10-31 21:25:08 +0900

    refactor(Validator): 시도 횟수를 검증하는 isSafeIneger 메서드가 음수에서 에러를 던지도록 리팩토링

--- src/RacingCarGame/Validator/index.js
+++ src/RacingCarGame/Validator/index.js
@@ -3,13 +3,13 @@ import { NUMBER, SYMBOLS, ERROR } from '../constants/index.js';
 
 class Validator {
   static isValidCarNameLength({ length }) {
-    if (length > NUMBER.maxNameLength || !length) {
+    if (length > NUMBER.maxNameLength) {
       throw new ValidationError(ERROR.invalidNameLength);
     }
   }
 
   static isSafeInteger(number) {
-    if (!Number.isSafeInteger(Number(number)) || !number) {
+    if (!Number.isSafeInteger(Number(number)) || number <= 0) {
       throw new ValidationError(ERROR.invalidLapCount);
     }
   }
