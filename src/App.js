42a63ac1ff69231d6d283b99b85f459a42d68bd9
meaculpa9450 <132068319+meaculpa9450@users.noreply.github.com>
2023-10-31 16:00:20 +0900

    feat: 앱 play 메서드에 게임 진행 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+// class
+import { RacingGame } from "./Class/RacingGame";
+// constants
+import { ERROR_MESSAGE } from "./Constant";
+// utils
+import { getWinnerArray } from "./utils/calc";
+import { HandleInput } from "./utils/HandleInput";
+import { HandleOutput } from "./utils/HandleOutput";
+
 class App {
-  async play() {}
+  async play() {
+    try {
+      const carNames = await HandleInput.carNames();
+      const gameRounds = await HandleInput.gameRounds();
+      const racingGame = new RacingGame(carNames);
+
+      HandleOutput.initial();
+      for (let i = 0; i < gameRounds; i++) {
+        racingGame.playSingleRound();
+        HandleOutput.roundResult(racingGame.cars);
+      }
+      const winners = getWinnerArray(racingGame.cars);
+      HandleOutput.finalResult(winners);
+    } catch (err) {
+      if (Object.values(ERROR_MESSAGE).includes(err.message)) throw err;
+      throw new Error(ERROR_MESSAGE.UNKNOWN_ERROR);
+    }
+  }
 }
 
 export default App;
