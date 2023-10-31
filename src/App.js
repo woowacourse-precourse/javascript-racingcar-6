68dcbd050d5202098fca4d4db3c92070640c3ec3
chaeyeon <321codus@naver.com>
2023-10-31 12:50:10 +0900

    feat: 자동차 이름 받기
    - 경주할 자동차 이름을 String으로 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    //1. 경주할 자동차 이름 받기
+    const USER_INPUT_STRING = await MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+    //자동차 쉼표로 분리해서 저장
+
+  }
 }
 
 export default App;
