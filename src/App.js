dfd00683b01c4871e8a708483d49cb22ce9189e0
ccconac <qwerpoiu91@naver.com>
2023-11-01 01:33:47 +0900

    :sparkles: feat: 자동차 이름 검증 및 에러 처리 구현

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
 import { Console } from '@woowacourse/mission-utils';
 
 class App {
+  constructor() {
+    this.cars = [];
+  }
+
   getCarName = async () => {
     const input = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
     );
-    const cars = input.split(',').map((v) => v.trim());
-    return cars;
+    this.cars = input.split(',').map((v) => v.trim());
+    this.isValid();
+
+    return this.cars;
   };
 
   getAttempts = async () => {
     const input = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
     return input;
+  };
+
+  isValidLength = () => {
+    const LENGTH_REGEX = /^.{1,5}$/;
+    return this.cars.every((name) => LENGTH_REGEX.test(name));
+  };
+
+  isValidString = () => {
+    const NAME_REGEX = /^[A-Za-z]*$/;
+    return this.cars.every((name) => NAME_REGEX.test(name));
+  };
+
+  isValidDuplication = () => {
+    const DUPLICATION = new Set(this.cars);
+    return DUPLICATION.size === this.cars.length;
+  };
+
+  isValid = () => {
+    if (!this.isValidLength())
+      throw new Error('[ERROR] 1글자에서 5글자 사이의 이름만 입력 가능합니다.');
+
+    if (!this.isValidString()) throw new Error('[ERROR] 영문으로 이루어진 이름만 입력 가능합니다.');
+    if (!this.isValidDuplication()) throw new Error('[ERROR] 중복되는 이름은 입력할 수 없습니다.');
   };
 
   async play() {
