59265ac85410b8f02e6ee2a1fb8799438ee4e353
Motoong <sonada903@gmail.com>
2023-10-31 15:03:38 +0900

    feat(moveCar): 경주 실행 구현/차량전진
    사용자 입력 라운드 수 만큼 반복
    
    각 차량이 랜덤한 수(0-9)를 부여받고 4이상일때 전진

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Console } from '@woowacourse/mission-utils';
 import userInput from './userInput.js';
+import moveCar from './moveCar.js';
 
 
 class App {
@@ -11,6 +12,14 @@ class App {
     //입력 이름이 5글자 초과 시 예외처리
     if (nameList.some((name) => name.length > 5)) {
       throw new Error('[ERROR]');
+    }
+
+    const carCount = nameList.map(() => 0);
+
+    Console.print('실행 결과');
+
+    for (let i = 0; i < round; i += 1) {
+      moveCar(carCount);
     }
 
   }
