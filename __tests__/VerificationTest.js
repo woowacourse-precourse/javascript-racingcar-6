2630aae11337357a5e8bcfc06252af686cc80625
ssoferret <owosso1212@gmail.com>
2023-10-31 22:41:10 +0900

    test : 각 경주차마다 랜덤수 부여 함수에 대한 테스트 작성.

--- __tests__/VerificationTest.js
+++ __tests__/VerificationTest.js
@@ -4,6 +4,13 @@
 import { Console } from "@woowacourse/mission-utils";
 import App from "../src/App.js";
 
+const mockQuestions = (inputs) => {
+  Console.readLineAsync = jest.fn();
+  Console.readLineAsync.mockImplementation(() => {
+    return Promise.resolve(inputs);
+  });
+};
+
 describe("함수별 기능 테스트", () => {
   test("경주차 생성자 만드는 함수에 대한 테스트", () => {
     const input = ["abc","def","ghi"];
@@ -20,4 +27,20 @@ describe("함수별 기능 테스트", () => {
       expect(app.inputCarNames()).rejects.toThrow("[ERROR]");
     }
   });
-});
+
+  test("시도 횟수 입력 예외 처리 테스트", async() => {
+    const input = "a";
+    const app = new App();
+    mockQuestions(input);
+    await expect(app.inputTryTimes()).rejects.toThrow("[ERROR]");
+  });
+  
+  test("각 경주차마다 랜덤수 부여하는 함수에 대한 테스트", () => {
+    const app = new App();
+    const car = app.makeCars(["a"]);
+    for (let i = 0; i < 100; i += 1) {
+      app.getEachCarsRandomNumber(car);
+      expect(car[0].randomNumber >= 0 && car[0].randomNumber < 10).toBeTruthy();
+    } 
+  });
+})
