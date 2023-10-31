362d458b2ffae128aa75e6d84d93608dd7b2d0b7
Sunghyun627 <kksscc4@gmail.com>
2023-10-31 21:52:07 +0900

    refactor: 사용자로부터 차이름을 입력받는 함수의 함수명 변경

--- src/InputManager.js
+++ src/InputManager.js
@@ -1,8 +1,9 @@
+import { Console } from '@woowacourse/mission-utils';
 import { GUIDE_MESSAGES } from './constants.js';
 import { isValidCarNames } from './Validation.js';
 
 class InputManager {
-  async getCarNames() {
+  async enterCarNames() {
     const input = (await Console.readLineAsync(GUIDE_MESSAGES.ENTER_CARNAMES)).split(',');
     return isValidCarNames(input) && input;
   }
