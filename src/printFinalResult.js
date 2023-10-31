4fab57f6fb01ddf39e1fe1da97d66864ccecddae
chaeyeon <321codus@naver.com>
2023-10-31 15:46:54 +0900

    fix: 결과값 2차 배열 조회 오류
    - 1차 배열 조회 -> 2차 배열값 조회로 수정

--- src/printFinalResult.js
+++ src/printFinalResult.js
@@ -2,19 +2,19 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 export default function printFinalResult(map) {
   const MAP_TO_ARRAY = Array.from(map);
-  MAP_TO_ARRAY.sort((a, b) => a[1] - b[1]);
-  if (MAP_TO_ARRAY[0] !== MAP_TO_ARRAY[1]) {
+  MAP_TO_ARRAY.sort((a, b) => b[1] - a[1]);
+  if (MAP_TO_ARRAY[0][1] !== MAP_TO_ARRAY[1][1]) {
     //단독 우승자
-    MissionUtils.Console.print(`최종 우승자 : ${map.get(MAP_TO_ARRAY[0])}`);
+    MissionUtils.Console.print(`최종 우승자 : ${MAP_TO_ARRAY[0][0]}`);
   } else {
     //공동 우승자
     let i = 0;
     MissionUtils.Console.print(`최종 우승자 : `);
 
-    while (MAP_TO_ARRAY[i] !== MAP_TO_ARRAY[i + 1]) {
-      MissionUtils.Console.print(MAP_TO_ARRAY[i] + ", ");
+    while (MAP_TO_ARRAY[i][1] !== MAP_TO_ARRAY[i + 1][1]) {
+      MissionUtils.Console.print(MAP_TO_ARRAY[i][0] + ", ");
       i++;
     }
-    MissionUtils.Console.print(MAP_TO_ARRAY[i]);
+    MissionUtils.Console.print(MAP_TO_ARRAY[i][0]);
   }
 }
