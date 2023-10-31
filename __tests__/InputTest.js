724fc400da418b323462f7dc30725d9249edaed3
hojkim77 <rlaghwnd4534@gmail.com>
2023-10-31 18:13:47 +0900

    [test] 자동차 이름과 시도횟수 입력에 대한 test 추가

---
+++ __tests__/InputTest.js
@@ -0,0 +1,147 @@
+import App from '../src/App.js';
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+describe('입력 Test', () => {
+  const mockQuestions = (inputs) => {
+    MissionUtils.Console.readLineAsync = jest.fn();
+
+    MissionUtils.Console.readLineAsync.mockImplementation(() => {
+      const input = inputs.shift();
+      return Promise.resolve(input);
+    });
+  };
+
+  test('정상적인 자동차 이름 입력', async () => {
+    // given
+    const inputs = ['호중,hoj,77,KHJ'];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    expect(await app.inputCarNames()).toEqual(['호중', 'hoj', '77', 'KHJ']);
+  });
+
+  test.each([[['khj,ghwndrla']], [['hojkim77']]])(
+    '5자리 초과한 이름 입력',
+    async (inputs) => {
+      // given
+      mockQuestions(inputs);
+
+      // when
+      const app = new App();
+
+      // then
+      await expect(app.play()).rejects.toThrow(
+        '[ERROR] 자동차의 이름은 쉼표로 구분하여 5글자 이하로 입력해주세요.'
+      );
+    }
+  );
+
+  test.each([[['hoj77,hoj77']], [['KHJ,KHJ']]])(
+    '중복된 이름 입력',
+    async (inputs) => {
+      // given
+      mockQuestions(inputs);
+
+      // when
+      const app = new App();
+
+      // then
+      await expect(app.play()).rejects.toThrow(
+        '[ERROR] 중복된 자동차 이름을 입력했습니다.'
+      );
+    }
+  );
+
+  test.each([[['hoj, hoj']], [['hoj,hoj ']]])(
+    '시작 또는 끝 공백인 이름 입력',
+    async (inputs) => {
+      // given
+      mockQuestions(inputs);
+
+      // when
+      const app = new App();
+
+      // then
+      await expect(app.play()).rejects.toThrow(
+        '[ERROR] 자동차 이름 시작과 끝의 공백은 오해를 일으킬 수 있습니다.'
+      );
+    }
+  );
+
+  test.each([[['']], [[',']], [['hoj,,hoj']], [[',hoj,']]])(
+    '빈 이름 입력',
+    async (inputs) => {
+      // given
+      mockQuestions(inputs);
+
+      // when
+      const app = new App();
+
+      // then
+      await expect(app.play()).rejects.toThrow(
+        '[ERROR] 이름이 없는 자동차가 있습니다.'
+      );
+    }
+  );
+
+  test.each([[['3']], [['03']], [['+3']], [['+03']]])(
+    '정상적인 시도 횟수 입력',
+    async (inputs) => {
+      // given
+      mockQuestions(inputs);
+
+      // when
+      const app = new App();
+
+      // then
+      expect(await app.inputTryNum()).toBe(3);
+    }
+  );
+
+  test.each([[['hoj77', '']], [['hoj77', ' ']], [['hoj77', '  ']]])(
+    '공백 입력',
+    async (inputs) => {
+      // given
+      mockQuestions(inputs);
+
+      // when
+      const app = new App();
+
+      // then
+      await expect(app.play()).rejects.toThrow(
+        '[ERROR] 아무 입력도 하지 않았습니다.'
+      );
+    }
+  );
+
+  test.each([[['hoj77', 'hojkim77']], [['hoj77', 'a1']], [['hoj77', '1a']]])(
+    '숫자 외의 입력',
+    async (inputs) => {
+      // given
+      mockQuestions(inputs);
+
+      // when
+      const app = new App();
+
+      // then
+      await expect(app.play()).rejects.toThrow('[ERROR] 숫자를 입력해주세요.');
+    }
+  );
+
+  test.each([[['hoj77', '-3']], [['hoj77', '-3']]])(
+    '음수 입력',
+    async (inputs) => {
+      // given
+      mockQuestions(inputs);
+
+      // when
+      const app = new App();
+
+      // then
+      await expect(app.play()).rejects.toThrow('[ERROR] 양수를 입력해주세요.');
+    }
+  );
+});
