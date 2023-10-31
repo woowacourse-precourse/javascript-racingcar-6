5ff1af41b68e0dc26eb03e752aadec46b7c878b3
minSsan <msp1125@hanyang.ac.kr>
2023-10-31 21:58:44 +0900

    refactor: MVC 구조 - Controller set value 메소드(set_winners, set_try, set_race_cars) 구현

--- src/Controller/RaceGameController.js
+++ src/Controller/RaceGameController.js
@@ -33,7 +33,12 @@ export class RaceGameController {
    * 올바른 try 값이면 해당 값을 model에 갱신한다.
    * @param {string} input_try
    */
-  #set_try(input_try) {}
+  #set_try(input_try) {
+    if (!this.#is_valid_try(input_try)) {
+      throw new Error(ERROR_MSG.WRONG_NUMBER);
+    }
+    this.#model.set_try(parseInt(input_try));
+  }
 
   /**
    * cars 문자열에 대해 검증하는 메소드
@@ -56,7 +61,21 @@ export class RaceGameController {
    * 올바른 cars 문자열이면 해당 값을 model에 갱신한다.
    * @param {string} cars
    */
-  #set_race_cars(cars) {}
+  #set_race_cars(cars) {
+    let car_names = cars.split(",");
+    car_names = car_names.map((value) => value.trim());
+
+    if (!this.#is_valid_car_name(car_names)) {
+      throw new Error(ERROR_MSG.NAME_LEN_EXCEED);
+    }
+
+    const RACE_CARS = car_names.map((car_name) => ({
+      name: car_name,
+      distance: 0,
+    }));
+
+    this.#model.set_race_cars(RACE_CARS);
+  }
 
   /**
    * 이동 거리를 랜덤하게 리턴하는 메소드
@@ -70,10 +89,22 @@ export class RaceGameController {
   #race() {}
 
   /**
-   * 최종 우승자를 계산하고, 최종 우승자 명단 배열을 리턴하는 함수
+   * 최종 우승자를 계산하고, 최종 우승자 명단 배열을 model에 저장하는 메소드
    * @param {Object[]} race_cars
    */
-  #set_winners(race_cars) {}
+  #set_winners(race_cars) {
+    // ? 1. 최대 거리 구하기
+    const MAX_DIST = race_cars.reduce(
+      (max, cur) => (cur.distance > max ? cur.distance : max),
+      0
+    );
+
+    // ? 2. 우승자 판별 - 최대 거리를 갖는 경주차
+    const RESULT = race_cars.filter((car) => car.distance === MAX_DIST);
+
+    const WINNERS_NAME = RESULT.map((winner) => winner.name);
+    this.#model.set_winners(WINNERS_NAME);
+  }
 
   /**
    * 최종 우승자 문자열을 리턴하는 함수
