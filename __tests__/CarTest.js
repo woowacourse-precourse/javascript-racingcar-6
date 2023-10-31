c8d31160d4a0970266c0eade9189fe5bac27982a
meaculpa9450 <132068319+meaculpa9450@users.noreply.github.com>
2023-10-31 22:45:02 +0900

    test: 테스트 중 불필요한 랜덤 모킹 제거

--- __tests__/CarTest.js
+++ __tests__/CarTest.js
@@ -1,11 +1,9 @@
 import { Car } from '../src/Class/Car';
-import { mockRandoms } from './ApplicationTest';
-import { MissionUtils } from '@woowacourse/mission-utils';
 
 describe('자동차 테스트', () => {
     test('move 메서드를 실행하면 자동차의 progress는 1 증가해야 한다', async () => {
         const car = new Car('foo');
-        const randoms = [
+        const mockedProgressArray = [
             [4, 7, 3, 2, 9, 1, 3, 5, 8, 4],
             [4, 5, 2, 3, 4, 4, 1, 2, 3, 2],
             [4, 5, 6, 7, 8, 9, 8, 0, 1, 3],
@@ -15,12 +13,11 @@ describe('자동차 테스트', () => {
             [5, 5, 4, 7, 1, 2, 2, 1, 5, 5],
             [8, 3, 8, 9, 8, 5, 3, 5, 8, 1],
         ].reduce((acc, cur) => acc.concat(cur));
-        mockRandoms(randoms);
 
-        randoms.forEach((random) => {
-            car.progress = MissionUtils.Random.pickNumberInRange(0, 9);
+        mockedProgressArray.forEach((givenNumber) => {
+            car.progress = givenNumber;
             car.move();
-            expect(car.progress).toEqual(random + 1);
+            expect(car.progress).toEqual(givenNumber + 1);
         });
     });
 });
