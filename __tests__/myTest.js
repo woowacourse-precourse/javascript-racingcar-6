4846eb5e89480047bc0789ee84bc5ff2edaa9801
Hys-Lee <dltkdgus3275@gmail.com>
2023-10-31 23:57:24 +0900

    Style(myTest): remove comments, fix test name for trial number
    remove it because it doesn't need comments because of test name.

--- __tests__/myTest.js
+++ __tests__/myTest.js
@@ -23,7 +23,6 @@ const getLogSpy = () => {
 const MOVING_FORWARD = 4;
 const STOP = 3;
 describe('입력 테스트', () => {
-  // Car Name
   test('자동차 이름 5자 이하, 구분자는 ","만 - 통과', async () => {
     const randoms = [MOVING_FORWARD, STOP];
     const inputs = ['it/is,two', '1'];
@@ -49,8 +48,7 @@ describe('입력 테스트', () => {
     await expect(app.play()).rejects.toThrow('[ERROR]');
   });
 
-  // Trial Number
-  test('도할 횟수 입력 0이상 정수 - 통과', async () => {
+  test('시도할 횟수 입력 0이상 정수 - 통과', async () => {
     const randoms = [MOVING_FORWARD, STOP, MOVING_FORWARD];
     const inputs = ['int', '3'];
     const outputs = ['최종 우승자 : int'];
