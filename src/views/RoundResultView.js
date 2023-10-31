bb0ab171ef5c96e51a1bc61ebb702e2d9c21a66a
pongpongie <arevolvera@gmail.com>
2023-11-01 03:02:46 +0900

    feat(views): create RoundResultView.js
    created RoundResultView and function to print out the MESSAGES.RESULT

---
+++ src/views/RoundResultView.js
@@ -0,0 +1,8 @@
+import { Console } from "@woowacourse/mission-utils";
+import { MESSAGES } from "../constants/Messages";
+
+const RoundResultView = () => {
+  Console.print(MESSAGES.RESULT);
+};
+
+export default RoundResultView;
