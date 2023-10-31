327e53352a10e66d9d8f4e69f71cf7117a0f6491
yuziwoo <yuziwoo@naver.com>
2023-10-31 15:30:22 +0900

    refactor(controllers/CarRacingGameController): 객체 추상화를 위해 메서드를 private로 변경
    - docs: 한 눈에 이해가지 않는 변수에 주석 추가

--- src/constants/carRacing.js
+++ src/constants/carRacing.js
@@ -8,10 +8,17 @@ export const CARS = Object.freeze({
 });
 
 export const RACING = Object.freeze({
+  // 시도할 횟수 범위
   MIN_NUMBER_OF_ATTEMPTS: 1,
   MAX_NUMBER_OF_ATTEMPTS: 1000,
+
+  // 랜덤으로 생성할 숫자의 범위
   MIN_NUMBER_OF_RANDOM_RANGE: 0,
   NAX_NUMBER_OF_RANDOM_RANGE: 9,
+
+  // 전진하기 위한 숫자의 최소값
   MIN_NUMBER_TO_MOVE_FORWARD: 4,
+
+  // 게임 실행 결과를 출력할 때 사용할 1회 전진 표시
   ONE_STEP_FORWARD_PROGRESS: "-",
 });
