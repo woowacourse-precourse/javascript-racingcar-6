3618eec11b9326cc5d2514071d3dda894b94335c
ryan-dia <cjfdnjs158@naver.com>
2023-10-31 23:48:22 +0900

    feat(inputView): 레이싱 횟수 입력받는 기능 구현

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,10 +1,15 @@
 import { Console } from '@woowacourse/mission-utils';
-import { INPUT_MESSAGE_FUNCTION } from '../constants/Messages.js';
+import { INPUT_MESSAGE_FUNCTION, OUTPUT_MESSAGE } from '../constants/Messages.js';
 
 const InputView = {
   async readRacingVehicleName(vehicle) {
     const racingVehicleName = await Console.readLineAsync(INPUT_MESSAGE_FUNCTION.name(vehicle));
     return racingVehicleName;
+  },
+
+  async readRacingCount() {
+    const racingCount = await Console.readLineAsync(OUTPUT_MESSAGE.count);
+    return racingCount;
   },
 };
 
