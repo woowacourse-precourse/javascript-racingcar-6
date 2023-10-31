360a921dea2dc3f4f5b1772fb2fda10e6aa39682
77JY <kwn03111@naver.com>
2023-11-01 01:16:02 +0900

    feat: DataFetcher 클래스 추가
    기능을 작은 단위로 나눠 하나의 매서드로 묶어줌
    자동차의 객체를 만들어 리턴하는 getCars
    라운드를 만들어 리턴하는 getRounds

---
+++ src/controllers/gameDataFetcher.js
@@ -0,0 +1,20 @@
+import { GameOutputMsg } from "../constants/gameConstants";
+import { Validator } from "../validators/validator";
+import { Converter } from "../utils/converter.js";
+import { Prompt } from "../prompt/prompt.js";
+
+export class DataFetcher {
+  static async getCars() {
+    const USER_INPUT = await Prompt.getString(GameOutputMsg.gameStartMessage); // 사용자로부터 자동차 이름 입력 받기
+    const CAR_NAME_LIST = USER_INPUT.split(","); // 입력값을 , 기준으로 분리
+    Validator.validateString(CAR_NAME_LIST); // 이름 유효성 검사
+    const CARS = Converter.createCarsFromNames(CAR_NAME_LIST); // 이름을 기반으로 자동차 객체 생성
+    return CARS;
+  }
+
+  static async getRounds() {
+    const RACE_ROUND = await Prompt.getNumber(GameOutputMsg.roundsInputMessage); // 레이스 라운드 숫자 입력 받기
+    Validator.validateNumber(RACE_ROUND); // 라운드 수 유효성 검사
+    return RACE_ROUND;
+  }
+}
