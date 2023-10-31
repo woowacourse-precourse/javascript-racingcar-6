d78e915f465af1a6d8774f35a01e3384e1ad5b9a
yeong-hwan <jghff700@naver.com>
2023-11-01 05:16:28 +0900

    feat: getCarNames 함수 구현

---
+++ input/getCarNames.js
@@ -0,0 +1,36 @@
+import { Console } from "@woowacourse/mission-utils";
+import validateCarNames from "../validator/validateCarNames.js";
+import RacingCar from "../classes/RacingCar.js";
+
+function makeCarsByNames(carNames) {
+  const racingCars = [];
+
+  for (const carName of carNames) {
+    const racingCar = new RacingCar(carName);
+    racingCars.push(racingCar);
+  }
+  
+  return racingCars;
+}
+
+/**
+ * 사용자 입력을 받아 쉼표를 기준으로 차량을 구분하고, 차량 목록을 반환한다.
+ * 스페이스 등의 공백이 있을 경우 제거한다.
+ * 
+ * @param {string} carNames
+ * @returns {list<RacingCar>}
+ */
+
+export default async function getCarNames() {
+  const userInput = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+  const carNamesWithoutSpace = String(userInput).replace(/(\s*)/g, '');
+  const carNames = carNamesWithoutSpace.split(",");
+
+  validateCarNames(carNames);
+
+  const racingCars = makeCarsByNames(carNames);
+
+  return racingCars;
+}
+
+// console.log(await getCarNames());
