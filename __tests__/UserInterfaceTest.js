fb10be0d4048b85937d7aebb3431e9918fbf5633
rabbit-22 <chlwlal20@gmail.com>
2023-11-01 00:26:38 +0900

    test: 이동횟수 입력 기능 테스트코드 작성

--- __tests__/UserInterfaceTest.js
+++ __tests__/UserInterfaceTest.js
@@ -27,4 +27,28 @@ describe("UserInterface 클래스 테스트", () => {
       }).toThrow("[ERROR] 자동차 이름은 공백일 수 없습니다.");
     });
   });
+
+  describe("validateRounds 함수", () => {
+    test("시도 횟수가 양의 정수가 아닐 시 에러 반환", () => {
+      expect(() => {
+        UserInterface.validateRounds("-1");
+      }).toThrow("[ERROR] 시도 횟수는 양의 정수만 가능합니다.");
+
+      expect(() => {
+        UserInterface.validateRounds("0");
+      }).toThrow("[ERROR] 시도 횟수는 양의 정수만 가능합니다.");
+
+      expect(() => {
+        UserInterface.validateRounds("3.5");
+      }).toThrow("[ERROR] 시도 횟수는 양의 정수만 가능합니다.");
+
+      expect(() => {
+        UserInterface.validateRounds("text");
+      }).toThrow("[ERROR] 시도 횟수는 양의 정수만 가능합니다.");
+    });
+
+    test("양의 정수를 입력했을 때 정수 반환", () => {
+      expect(UserInterface.validateRounds("5")).toBe(5);
+    });
+  });
 });
