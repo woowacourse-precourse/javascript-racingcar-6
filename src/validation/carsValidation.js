1e45ece290243b976a66fb1882f10dbfa44a4477
Goongam <dlwjddn341@naver.com>
2023-10-31 22:08:40 +0900

    Feat: 시도횟수 정수 예외 처리

--- src/validation/carsValidation.js
+++ src/validation/carsValidation.js
@@ -9,7 +9,7 @@ export const validCars = (carsArray) => {
 
     else if (car?.length > 5) throw new Error(`[ERROR] ${ERROR_MESSAGE.MORE5_NAME}`);
 
-    // else if (car.trim() !== car) throw new Error(`[ERROR] ${ERROR_MESSAGE.TRIM_EMPTY_NAME}`);
+    else if (car.trim() !== car) throw new Error(`[ERROR] ${ERROR_MESSAGE.TRIM_EMPTY_NAME}`);
   });
 
 }
