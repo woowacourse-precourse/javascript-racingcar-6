016cdcc8fa8916555d3334274b6d57aefc795702
rabbit-22 <chlwlal20@gmail.com>
2023-11-01 01:52:10 +0900

    test: 랜덤값 출력 테스트코드 작성

--- __tests__/RacingGameTest.js
+++ __tests__/RacingGameTest.js
@@ -1,5 +1,13 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 import Car from "../src/Car";
 import RacingGame from "../src/RacingGame";
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
 
 describe("RacingGame 클래스", () => {
   test("자동차 이름 및 라운드 수 받아서 인스턴스 생성", () => {
@@ -8,5 +16,16 @@ describe("RacingGame 클래스", () => {
     expect(racingGame.cars[0].name).toBe("car1");
     expect(racingGame.cars[1].name).toBe("car2");
     expect(racingGame.rounds).toBe(5);
+  });
+
+  describe("generateRandomNumber 함수", () => {
+    test("0에서 9 사이 랜덤값 반환", () => {
+      const racingGame = new RacingGame(["car1", "car2"], 5);
+
+      mockRandoms([1, 2]);
+
+      expect(racingGame.generateRandomNumber()).toBe(1);
+      expect(racingGame.generateRandomNumber()).toBe(2);
+    });
   });
 });
