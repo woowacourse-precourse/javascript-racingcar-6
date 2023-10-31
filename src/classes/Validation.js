9c27eb3e7ae3edfd9b27613dd41ef7f51d5a98b2
hsgh085 <hsyu0917@gmail.com>
2023-11-01 05:10:44 +0900

    feat: 사용자 입력값 유효성 검사

--- src/classes/Validation.js
+++ src/classes/Validation.js
@@ -1,4 +1,5 @@
 import Constraints from '../constants/Constraints';
+import ErrorMessage from '../constants/ErrorMessage';
 
 export default class Validation {
   #cars;
@@ -10,24 +11,36 @@ export default class Validation {
     this.#tryNumber = tryNumber;
   }
 
-  isCheckDuplicate() {
+  isValid() {
+    if (this.#isCheckDuplicate()) {
+      throw new Error(ErrorMessage.DUPLICATE);
+    }
+    if (!this.#isCheckLength()) {
+      throw new Error(ErrorMessage.LENGTH);
+    }
+    if (!this.#isCheckNumber()) {
+      throw new Error(ErrorMessage.NUMBER);
+    }
+    if (!this.#isCheckNatural()) {
+      throw new Error(ErrorMessage.NATURAL);
+    }
+    return true;
+  }
+
+  #isCheckDuplicate() {
     const set = new Set(this.#cars);
     return set.size !== this.#cars.length;
   }
 
-  isCheckLength() {
-    return this.#cars.every(
-      (car) =>
-        car.length >= Constraints.MIN_LENGTH &&
-        car.length <= Constraints.MAX_LENGTH,
-    );
+  #isCheckLength() {
+    return this.#cars.every((car) => car.length <= Constraints.MAX_LENGTH);
   }
 
-  isCheckNumber() {
+  #isCheckNumber() {
     return !Number.isNaN(this.#tryNumber);
   }
 
-  isCheckNatural() {
+  #isCheckNatural() {
     const number = parseFloat(this.#tryNumber);
     return Number.isInteger(number) && number > 0;
   }
--- src/constants/Constraints.js
+++ src/constants/Constraints.js
@@ -1,5 +1,4 @@
 const Constraints = {
-  MIN_LENGTH: 1,
   MAX_LENGTH: 5,
 };
 export default Constraints;
