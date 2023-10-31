1d7c22d07deb0340c9d0b6436958092c93701ece
SWBAE0501 <tkddnjsgla@naver.com>
2023-10-31 22:09:18 +0900

    FIX: race.js file fixed, modified fomula

--- src/race.js
+++ src/race.js
@@ -10,7 +10,7 @@ class RacingGame { // 레이싱 게임 결과를 리턴하는 클래스
 
     getRaceCount(position) {   
         const num = MissionUtils.Random.pickNumberInRange(1, 9);
-        if (num <= 4) {
+        if (num >= 4) {
             position += '-';
         }
         return position;
