a0073bf8e9d33da370a21bd46cbd3fb0b426ab73
cobocho <rlaalsrb1111@naver.com>
2023-10-31 12:13:36 +0900

    test: `Car` 예외 테스트 작성

---
+++ __tests__/__units__/Car/Car.exception.test.js
@@ -0,0 +1,23 @@
+import ERROR_MESSAGE from '../../../src/constants/error.js';
+import { Car } from '../../../src/domain/index.js';
+
+describe('Car 예외 테스트', () => {
+  it.each([
+    { power: 1.1 },
+    { power: '1' },
+    { power: {} },
+    { power: [] },
+    { power: true },
+    { power: null },
+    { power: undefined },
+  ])('`move(power)`의 인자가 정수가 아닐 경우 에러를 발생시킨다. (power: $power)', ({ power }) => {
+    // given
+    const car = new Car();
+
+    expect(() => {
+      // when
+      car.move(power);
+      // then
+    }).toThrow(ERROR_MESSAGE.common.notInteger);
+  });
+});
---
+++ src/constants/error.js
@@ -0,0 +1,7 @@
+const ERROR_MESSAGE = Object.freeze({
+  car: Object.freeze({
+    moveArgNotInteger: '정수를 입력해주세요!',
+  }),
+});
+
+export default ERROR_MESSAGE;
