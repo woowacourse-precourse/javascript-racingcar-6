85e8a2bed8819ea99998ec00946ec3bf5005d7e2
forest7592 <forest7592@hanmail.net>
2023-11-01 01:05:09 +0900

    feat: random logic
    Add moveForward function to move cars randomly

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
   async play() {
+    const moveForward = (result) => {
+      if (MissionUtils.Random.pickNumberInRange(0, 1) === 1) {
+        result.push('-');
+      }
+    };
     const string = await MissionUtils.Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
     );
@@ -13,8 +18,6 @@ class App {
     const number = await MissionUtils.Console.readLineAsync(
       '시도할 횟수는 몇 회인가요?\n',
     );
-
-    MissionUtils.Console.print('\n실행 결과');
   }
 }
 
