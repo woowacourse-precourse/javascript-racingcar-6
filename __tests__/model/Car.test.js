c2ac9f5d6e3a8730fbf282bd51877ad44a1e047f
Trophy198 <spsp8426@naver.com>
2023-10-31 17:24:41 +0900

    test : Car.js의 변경사항을 반영하기 위해 Car.test.js 테스트 업데이트

--- __tests__/model/Car.test.js
+++ __tests__/model/Car.test.js
@@ -1,26 +1,29 @@
 import Car from '../../src/model/Car';
 
 const alwaysMoveStrategy = {
-  shouldMove: () => true
+  shouldMove: () => true,
 };
 
 const neverMoveStrategy = {
-  shouldMove: () => false
+  shouldMove: () => false,
 };
 
 describe('Car 클래스', () => {
   describe('constructor 메서드', () => {
     test('Car 객체가 올바르게 초기화되어야 한다', () => {
       const car = new Car('pobi', alwaysMoveStrategy);
-      expect(car.name).toBe('pobi');
-      expect(car.position).toBe(0);
+      const carInfo = car.getCarInformation();
+      expect(carInfo.name).toBe('pobi');
+      expect(carInfo.position).toBe(0);
     });
   });
 
-  describe('getCurrentPositionRepresentation 메서드', () => {
-    test('현재 위치를 문자열로 반환해야 한다', () => {
+  describe('getCarInformation 메서드', () => {
+    test('현재 이름과 위치를 객체로 반환해야 한다', () => {
       const car = new Car('pobi', alwaysMoveStrategy);
-      expect(car.getCurrentPositionRepresentation()).toBe('');
+      const carInfo = car.getCarInformation();
+      expect(carInfo.name).toBe('pobi');
+      expect(carInfo.position).toBe(0);
     });
   });
 
@@ -28,13 +31,15 @@ describe('Car 클래스', () => {
     test('shouldMove가 true일 때 위치가 증가해야 한다', () => {
       const car = new Car('pobi', alwaysMoveStrategy);
       car.move();
-      expect(car.position).toBe(1);
+      const carInfo = car.getCarInformation();
+      expect(carInfo.position).toBe(1);
     });
 
     test('shouldMove가 false일 때 위치가 그대로 유지되어야 한다', () => {
       const car = new Car('pobi', neverMoveStrategy);
       car.move();
-      expect(car.position).toBe(0);
+      const carInfo = car.getCarInformation();
+      expect(carInfo.position).toBe(0);
     });
   });
 });
