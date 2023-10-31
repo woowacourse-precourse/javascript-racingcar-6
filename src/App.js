1a162d0dee0a28a51eb612bde929abad365d2c6c
Jeongminchang <min413903@naver.com>
2023-10-31 21:14:42 +0900

    1-2 : 자동차 이름 길이 Error 탐지

--- src/App.js
+++ src/App.js
@@ -5,7 +5,18 @@ class App {
     const nameInput = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
     const tryNum = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
     const names = nameInput.split(',')
+
+    let result = []
+    for(let i = 0; i < names.length; i++){
+      if(names[i].length > 5){
+        throw new Error("[ERROR] 이름의 길이가 5를 초과했습니다 (길이 > 5)");
+      } else if(names[i].length == 0){
+        throw new Error("[ERROR] 비어있는 이름을 작성하였습니다. (길이 = 0)");
+      }
+      result.push('');
+    }
   }
+  
 }
 
 export default App;
