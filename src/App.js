155906cdadca357da38003d3b204a237f8a3aa78
gilgor2 <misby0327@gmail.com>
2023-10-31 20:06:28 +0900

    feat(App): sketch main logic of app
    app 메인 로직의 뼈대를 잡았습니다. 변수, 함수명을 다시 리팩토링 해 의미를 분명히 할 필요가 있습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { Random, Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  constructor() {
+    this.carDataList = []; // [{name:string, distance:number},]
+  }
+
+  async play() {
+    const carNameStr = getValidCarNameStr();
+    const initialCarDataList = generateCarDataListFromStr(carNameStr);
+    this.updateCarDataList(initialCarDataList); // 정확하게 업데이트되는지 테스트 필요
+
+    const totalRoundNumber = getValidTotalRoundNumber();
+
+    for (let round = 0; round < totalRoundNumber; round++) {
+      const roundResult = moveCarRandomly(this.carDataList);
+      announceRoundResult(roundResult);
+      this.updateCarDataList(roundResult);
+    }
+
+    announceWinner(this.carDataList);
+  }
+
+  updateCarDataList(newCarDataList) {
+    this.carDataList = newCarDataList;
+  }
 }
 
 export default App;
