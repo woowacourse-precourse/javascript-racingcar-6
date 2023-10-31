3d5de3b1f749d1723f8a93059c21b7b6b90e4ac1
JunSeok <ppu1234@naver.com>
2023-10-31 23:07:05 +0900

    test: 무작위 값 체크 함수 테스트 코드 작성

--- __tests__/RandomNumberTest.js
+++ __tests__/RandomNumberTest.js
@@ -1,5 +1,6 @@
 import checkRandomNumber from '../src/Util/randomNumber';
 
-test('무작위 값 체크', () => {
-  expect(typeof checkRandomNumber()).toBe('boolean');
+test('무작위 값 체크 함수의 리턴 값 타입은 boolean', () => {
+  const result = checkRandomNumber();
+  expect(typeof result).toBe('boolean');
 });
