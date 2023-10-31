08fda4b61270d17eb181fd54f8c4b52dc2e9bd6c
HyunseokLEE <unducklee@gmail.com>
2023-11-01 05:26:44 +0900

    feat: 앞으로 갈 수 있는 조건 생성

--- src/Controller/CarRaceGameController.js
+++ src/Controller/CarRaceGameController.js
@@ -1,5 +1,6 @@
 import Input from "../Input/Input";
 import CompareRaceCarResult from "../Compare/CompareRaceCarResult";
+import CarForwardRandomNumberGenerator from "../utils/CarForwardRandomNumberGenerator";
 
 class CarRaceGameController {
   #raceCars;
@@ -19,6 +20,14 @@ class CarRaceGameController {
   async setRaceCars(racecars) {
     this.#raceCars = new CompareRaceCarResult(racecars);
     await this.InputRaceCarNumberOfAttempts();
+  }
+
+  setMoveForwardCondition() {
+    this.#raceCars.map((raceCar) => {
+      const randomNumber =
+        CarForwardRandomNumberGenerator.RandomNumberGenerator();
+      if (randomNumber >= 4) raceCar.moveForward();
+    });
   }
 }
 
