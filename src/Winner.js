05d86e021c761353e070f09a5737569b3f68d264
byunjiin <jibyun5808@naver.com>
2023-10-31 19:01:57 +0900

    feat: 기능6. 우승자 정하기

--- src/Winner.js
+++ src/Winner.js
@@ -1,1 +1,10 @@
-//기능5.
+// 기능6. 우승자 정하기
+export function winner(result_menu, name) {
+  const ranking = [];
+  for (let i = 0; i < result_menu.length; i++) {
+    if (result_menu[i] === Math.max(...result_menu)) {
+      ranking.push(name[i]);
+    }
+  }
+  return ranking;
+}
