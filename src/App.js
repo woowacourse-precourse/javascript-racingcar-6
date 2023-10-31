4b092788943c352ea55130402ff877d67e0f6a1e
useon <yoosk4986@naver.com>
2023-11-01 00:39:37 +0900

    feat: 경주할 자동차 이름 입력값에 대해 검증하는 기능

--- src/App.js
+++ src/App.js
@@ -5,11 +5,30 @@ class App {
     const nameInputValue = await this.userInput(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
     );
+    this.nameValidation(nameInputValue);
   }
 
   async userInput(message) {
     const inputValue = await MissionUtils.Console.readLineAsync(message);
     return inputValue;
+  }
+
+  nameValidation(inputValue) {
+    if (inputValue === '') throw new Error('[ERROR]');
+
+    if (!inputValue.includes(',') && inputValue.length > 5)
+      throw new Error('[ERROR]');
+
+    if (inputValue.includes(',')) {
+      const nameArr = inputValue.split(',');
+      const uniqueNameSet = new Set(nameArr);
+      if (nameArr.length !== uniqueNameSet.size) throw new Error('[ERROR]');
+
+      nameArr.forEach((element) => {
+        if (element.length < 1 || element.length > 5)
+          throw new Error('[ERROR]');
+      });
+    }
   }
 }
 
