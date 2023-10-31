f6a28cb5c8cb542fb6ef973b39744d1894ba64d3
rabbit-22 <chlwlal20@gmail.com>
2023-11-01 02:10:43 +0900

    feat: 우승자 체크 및 반환 기능

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -25,6 +25,17 @@ class RacingGame {
       });
     }
   }
+
+  getWinners() {
+    const maxDistance = Math.max(...this.cars.map((car) => car.distance));
+    if (maxDistance === 0) {
+      return [];
+    }
+
+    const winners = this.cars.filter((car) => car.distance === maxDistance);
+    const winnerNames = winners.map((car) => car.name);
+    return winnerNames;
+  }
 }
 
 export default RacingGame;
