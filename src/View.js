479d8a3a47e34cf1b39070ffdefc1153e5f07382
kkanghh <attsky2@gmail.com>
2023-10-31 22:03:13 +0900

    feat: 예외 케이스 추가

--- src/View.js
+++ src/View.js
@@ -1,5 +1,9 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-import { checkNameValid, checkRoundValid } from "./inputValidCheck.js";
+import {
+  checkNameDuplication,
+  checkNameValid,
+  checkRoundValid,
+} from "./inputValidCheck.js";
 
 class View {
   async getCarName() {
@@ -7,6 +11,7 @@ class View {
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
     const NAME_ARRAY = NAME_STRING.split(",");
+    checkNameDuplication(NAME_ARRAY);
     NAME_ARRAY.forEach((element) => {
       checkNameValid(element);
     });
