ed280041fe0600fd35ead6f18e9bd971a544a3e4
lottt105 <hyobin0601@naver.com>
2023-10-31 15:04:09 +0900

    Feat: 시도 횟수 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 class App {
   async play() {
     const CARS = await this.getNameInput();
+    let count = await this.getCountInput();
   }
 
   async getNameInput() {
@@ -10,10 +11,21 @@ class App {
     return names;
   }
 
+  async getCountInput() {
+    let count = await Console.readLineAsync(MESSAGES.writeCount);
+    count = parseInt(count);
+    this.checkCountInput(count);
+    return count;
+  }
+
   checkNameInput(names) {
     names.forEach((name) => {
       if(name.length > 5) throw new Error(MESSAGES.nameLengthError);
     });
+  }
+
+  checkCountInput(count) {
+    if(isNaN(count)) throw new Error(MESSAGES.numberError);
   }
 }
 
