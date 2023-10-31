8976842b6be5199ae910c389c99cc5b82111a08d
Narachan97 <qkrtkf3112@naver.com>
2023-11-01 02:50:03 +0900

    test: printResut 테스트 코드 추가
    __tests__/FunctionTest.js printResut 테스트 코드 추가
    
    FunctionTest.js 추가점
    -printReulst에 대한 테스트 코드 추가

--- __tests__/FunctionTest.js
+++ __tests__/FunctionTest.js
@@ -1,4 +1,4 @@
-import { playRacing,finalWinner,printWinner } from "../src/App";
+import { playRacing,finalWinner,printWinner,printResults,tryForward } from "../src/App";
 describe(" 함수 테스트", () => {
   test("playRacing  배열 길이  테스트", () => {
     const playRound = 5;
@@ -12,7 +12,7 @@ describe(" 함수 테스트", () => {
     expect(result.length).toBe(carProgress.length); // 반환된 배열의 길이는 carProgress와 같아야 합니다
 
     result.forEach((roundProgress) => {
-      expect(roundProgress).toEqual(expect.any(Number)); // 각 라운드 진행도는 숫자여야 합니다
+      expect(roundProgress).toEqual(expect.any(Number)); 
     });
   });
 
@@ -26,21 +26,31 @@ describe(" 함수 테스트", () => {
     expect(() => playRacing(playRound, carProgress, carNameArray)).toThrow();
   });
 
-  test("우승자를 찾아서 반환하는지 테스트", () => {
+  test("finalWinner 우승자 반환 테스트", () => {
     const names = ["car1", "car2", "car3"];
     const progress = [3, 5, 3]; // "car2"가 우승
     const result = finalWinner(names, progress);
     expect(result).toEqual([{ name: "car2", progress: 5 }]);
   });
 
-  test("우승자 정보를 출력하는지 테스트", () => {
+  test("printWinner 우승자 출력 테스트", () => {
     const winners = [
       { name: "car1", progress: 4 },
       { name: "car2", progress: 4 },
     ];
     const consoleSpy = jest.spyOn(console, "log").mockImplementation();
     printWinner(winners);
-    expect(consoleSpy).toHaveBeenCalledWith("최종 우승자:car1, car2");
+    expect(consoleSpy).toHaveBeenCalledWith("최종 우승자 : car1, car2");
+    consoleSpy.mockRestore();
+  });
+  test(`printResult "-" 가 정상적으로 출력 되는지 테스트`, ()=>{
+    const consoleSpy = jest.spyOn(console, "log");
+    const names = ["car1","car2"];
+    const progress = [3,5];
+    printResults(names,progress);
+    expect(consoleSpy).toHaveBeenCalledWith("car1 : ---");
+    expect(consoleSpy).not.toHaveBeenCalledWith("car2 : ---");
+  
     consoleSpy.mockRestore();
   });
 });
