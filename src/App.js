46f60cd21073409ac0ad2697bdfb5cb998fa61d6
yeonkyoung <keep.ykk@gmail.com>
2023-11-01 02:42:22 +0900

    feat: 자동차 이름을 갖는 자동차 인스턴스 배열 반환

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { userInputCarsName, userInputTrialCount } from "./userInterface.js";
+import {
+  getCarsClassArray,
+} from "./utils/gameSupport.js";
 class App {
   async play() {
     const carsStringNameArray = await userInputCarsName();
+    const cars = getCarsClassArray(carsStringNameArray);
   }
 }
 
