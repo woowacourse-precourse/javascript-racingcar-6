9fa175b9d7565e80ada0db1b0d801db32881ba2a
rheefine <rheefine@gmail.com>
2023-11-01 02:41:21 +0900

    refactor: 불필요한 constructor 제거

--- src/Model/Winners.js
+++ src/Model/Winners.js
@@ -3,10 +3,6 @@ import assessWinner from '../utils/assessWinner.js';
 export default class Winner {
   #winners;
 
-  constructor() {
-    this.#winners = [];
-  }
-
   setWinners(cars) {
     this.#winners = assessWinner(cars);
   }
