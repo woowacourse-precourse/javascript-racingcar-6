659ae4c4a9280b96b18f681f0c91ff38a679b9f7
hyunyeee <5004ohy@naver.com>
2023-11-01 02:08:33 +0900

    feat: 각 자동차에 해당하는 값을 할당하고 실행결과를 출력한다.

--- src/App.js
+++ src/App.js
@@ -4,38 +4,52 @@ const CAR_NAME_MESSAGE =
   "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
 const ATTEMP_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?\n";
 
+class Car {
+  constructor(name) {
+    this.name = name;
+    this.distance = 0;
+  }
+
+  move() {
+    const randomValue = MissionUtils.Random.pickNumberInRange(0, 9);
+    if (randomValue > 3) {
+      this.distance++;
+    }
+  }
+
+  displayDistance() {
+    return this.name + " : " + "-".repeat(this.distance);
+  }
+}
+
 class App {
   async play() {
     this.#controller();
   }
 
   async #controller() {
-    const carName = await this.#readCarName();
+    const carNames = await this.#readCarNames();
+    const cars = carNames.map((car) => new Car(car.trim()));
     const attempCount = await this.#readAttempCount();
-    this.#getDistance(carName, attempCount);
+    await this.#race(cars, attempCount);
   }
 
-  async #readCarName() {
-    return MissionUtils.Console.readLineAsync(CAR_NAME_MESSAGE);
+  async #readCarNames() {
+    const cars = await MissionUtils.Console.readLineAsync(CAR_NAME_MESSAGE);
+    return cars.split(",");
   }
 
   async #readAttempCount() {
     return MissionUtils.Console.readLineAsync(ATTEMP_COUNT_MESSAGE);
   }
 
-  #getDistance(carName, attempCount) {
-    MissionUtils.Console.print("실행 결과");
-    let distance = 0;
-    for (let i = 0; i < attempCount; i++) {
-      distance += this.#checkGo();
-      MissionUtils.Console.print(carName + " : " + "-".repeat(distance));
+  #race(cars, attemptCount) {
+    MissionUtils.Console.print("\n실행 결과");
+    for (let i = 0; i < attemptCount; i++) {
+      cars.forEach((car) => car.move());
+      cars.forEach((car) => MissionUtils.Console.print(car.displayDistance()));
       MissionUtils.Console.print(" ");
     }
-  }
-
-  #checkGo() {
-    const randomValue = MissionUtils.Random.pickNumberInRange(0, 9);
-    return randomValue > 3;
   }
 }
 
