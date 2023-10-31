7d0592ed916ff373b4d1c8a299beca7a1c5af6a0
geunwoo <olo90632951@gmail.com>
2023-11-01 00:19:58 +0900

    refactor: 입력받는 부분 함수로 분리

--- src/App.js
+++ src/App.js
@@ -6,8 +6,16 @@ class App {
   }
 
   async play() {
-    const carNamesInput = await Console.readLineAsync('경주할 자동차이름을입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');   
-    this.cars = carNamesInput.trim().split(',').map((name) => name.trim());
+    await this.initializeCars();
+  }
+  
+  async initializeCars() {
+    const carNamesInput = await Console.readLineAsync('경주할 자동차이름을입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    this.cars = this.parseCarNamesInput(carNamesInput);
+  }
+
+  parseCarNamesInput(input) {
+    return input.trim().split(',').map((name) => name.trim());
   }
 }
 
