7baba84b73c19def487675fb8867ef5deb066d44
0jenn0 <jenn0.6n@gmail.com>
2023-10-31 12:49:36 +0900

    Feat: RacingExecutor에 '실행결화' 메시지 프린트 추가

--- src/Controllers/RacingExecutor.js
+++ src/Controllers/RacingExecutor.js
@@ -2,6 +2,8 @@ import { OutputView } from "../Views/index.js";
 
 export default class RacingExecutor {
   static runRaces(cars, raceround) {
+    OutputView.printResultMessage();
+
     Array.from({ length: raceround }, () => {
       cars.forEach((car) => car.race());
       OutputView.printOneRound(cars);
