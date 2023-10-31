37b60c4eaff08c1ee44c9f88d412c39207da0088
jadewisemann <jadewisemann@gmail.com>
2023-10-31 21:22:58 +0900

    REFACTORING: change function check all array's elements are validate 이전 커밋의 checkIsStringBelow5 함수의 동작을 수정하였음. 이전의 경우 함수 순회는 App 내부에서 실행되도록 순회한 어레이의 원소를 체크하느는 함수를 만들었음. 어레이 순회를 담당하는 함수를 따로 만들어서 더 추상화시키고 가독성의 증진을 꾀함

--- src/App.js
+++ src/App.js
@@ -7,33 +7,31 @@ class App {
     // const userInput = await this.getUserInput();
     // this.printMessage(msg)
     // const array = this.splitStringToArrayByComma(string)
-
   }
-  
-  async getUserInput() {
-    const input = await Console.readLineAsync(message.starting)
-    return input
-  }
-
-  printMessage(message) {
-    Console.print(message)
-  }
-
-  splitStringToArrayByComma(string) {
-    const array = string.split(',')
-    return array
-  }
-
-  checkIsStringBelow5(string) {
-    if (string.length > 5) {
-      throw console.error(message.InputValidationError);
-    } else {
-      string
-    }
-  }
-
 }
 
+async function getUserInput() {
+  const input = await Console.readLineAsync(message.starting)
+  return input
+}
 
+function printMessage(message) {
+  Console.print(message)
+}
+
+function splitStringToArrayByComma(string) {
+  const array = string.split(',')
+  return array
+}
+
+function checkIsAllElementsFitCondition(array) {
+  array.map(element => checkStringLengthBelow5(string))
+}
+
+function checkStringLengthBelow5(string) {
+  if (string.length < 5) {
+    throw console.error(message.inputValidationError);
+  }
+}
 
 export default App;
