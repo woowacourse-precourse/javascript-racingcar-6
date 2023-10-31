9ee6c48427f847d1010df1a943d9789c3a7b6fdc
eternalclash <suchasucha@naver.com>
2023-11-01 08:31:47 +0900

    Refactor: 게임종료 리팩토링

--- src/classes/EndGame.js
+++ src/classes/EndGame.js
@@ -1,12 +1,25 @@
 import { Console } from "@woowacourse/mission-utils";
 export default class EndGame {
     result(carList) {
-        let winnerList = [];
-        carList.sort((a, b) => b[1] - a[1]);
-        carList.forEach((car) => {
-          if (car.count != carList[0].count) return winnerList;
-          winnerList.push(car.name);
-        });
-        Console.print("최종 우승자 : " + winnerList.join(", "));
+        this.decendCarList(carList)
+        const winnerList = this.makeWinnerList(carList)
+        Console.print("최종 우승자 : " + winnerList);
       }
+
+    decendCarList (carList) {
+        carList.sort((a, b) => b[1] - a[1]);
+    }
+
+    makeWinnerList (carList) {
+
+        const result = []
+
+        carList.forEach((car) => {
+            if (car.count != carList[0].count) return result;
+            result.push(car.name);
+          });
+        
+        return result.join(", ")
+        
+    }
 }
