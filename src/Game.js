04e75b9edb8ec0e26ca5bd86185272477ec80b3e
jungmyungjin <myung2667@gmail.com>
2023-11-01 01:29:01 +0900

    feat: 인터페이스 매서드 추가

--- src/Game.js
+++ src/Game.js
@@ -7,5 +7,31 @@ class Game {
   #moveCount = 0;
   #maxDistance = 0;
   constructor() {}
+
+  async #initializeCars(arrayCar) {
+    if (!arrayCar || !Array.isArray(arrayCar))
+      throw new Error(PRINT.ERROR.INPUT);
+
+    for (let car of arrayCar) {
+      this.#cars.set(car, 0);
+    }
+  }
+
+  async #interface() {
+    // set #cars
+    const arrayCars = await parseValidCarNames(
+      await Console.readLineAsync(PRINT.MESSAGE.INPUT_CARS),
+      ","
+    );
+    await this.#initializeCars(arrayCars);
+
+    // set #moveCount
+    const inputCount = await parseValidNumber(
+      await Console.readLineAsync(PRINT.MESSAGE.INPUT_NUMBER)
+    );
+    if (!inputCount) throw new Error(PRINT.ERROR.INPUT);
+
+    this.#moveCount = inputCount;
+  }
 }
 export default Game;
