754cd2d0517c717d574c20892c87e7adaa0bfbfd
song-eun <dmswo546@naver.com>
2023-10-31 19:37:15 +0900

    feat: 자동차 이름 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+import InputView from './view/inputView';
+// import { InputView } from './view/InputView.js';
+function checkValidation(input) {
+  input.forEach((element) => {
+    if (element.length < 1 || element.length > 5) {
+      throw new Error('[ERROR] 자동차 이름이 1자 이상 5자 이하여야 합니다.');
+    }
+  });
+
+  const set = new Set(input);
+  if (input.length !== set.size()) {
+    throw new Error('[ERROR] 자동차 이름이 중복되었습니다.');
+  }
+}
+
 class App {
-  async play() {}
+  constructor() {
+    this.cars = [];
+  }
+
+  async play() {
+    await this.getCarName();
+  }
+
+  async getCarName() {
+    const playerInput = await InputView.readCarName();
+    checkValidation(playerInput.split(','));
+  }
 }
 
 export default App;
