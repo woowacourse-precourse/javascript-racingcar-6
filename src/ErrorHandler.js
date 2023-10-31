3d0bd6881614f35ed407538819d1d95ce232b7e3
Jeongeun Lee <leeju1009@gmail.com>
2023-10-31 17:32:51 +0900

    refactor: 자동차 이름 글자수 기준이 변경되면서 error문구 수정
    자동차 이름에 최소 글자수 기준이 생기면서 error 문구도 수정함

--- src/ErrorHandler.js
+++ src/ErrorHandler.js
@@ -1,7 +1,7 @@
 class ErrorHandler {
   ERROR_MESSAGE = {
     invalidCarName:
-      '[ERROR] 자동차 이름은 5자 이하로 입력해주세요. (쉼표(,)로 구분)',
+      '[ERROR] 자동차 이름은 1자 이상 5자 이하로 입력해주세요. (쉼표(,)로 구분)',
     invalidRacingCount:
       '[ERROR] 숫자가 잘못된 형식입니다. 1 이상의 정수를 입력해주세요.',
   };
