297925bacb15ad822c6e8902a3e7e80be26e8e8d
gillyongs <khs20010327@gmail.com>
2023-11-01 02:08:20 +0900

    feat: 자동차 이름을 받아 split 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    try{
+      Console.print('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
+      const carNamesInput = await Console.readLineAsync();
+      const carNames = carNamesInput.split(',');
+    }
+    catch (error) {
+      Console.print(error.message);
+    }
+
+  }
 }
 
 export default App;
