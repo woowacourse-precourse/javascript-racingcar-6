9d9690872b8d730febee026b298dc31dea6759d0
Jeongminchang <min413903@naver.com>
2023-10-31 22:05:39 +0900

    code : 함수로 정의하여 관리

--- src/App.js
+++ src/App.js
@@ -7,28 +7,10 @@ class App {
     const names = nameInput.split(',')
 
     let result = []
-    for(let i = 0; i < names.length; i++){
-      if(names[i].length > 5){
-        throw new Error("[ERROR] 이름의 길이가 5를 초과했습니다 (길이 > 5)");
-      } else if(names[i].length == 0){
-        throw new Error("[ERROR] 비어있는 이름을 작성하였습니다. (길이 = 0)");
-      }
-      result.push('');
-    }
+    this.len_Check(result, names)
 
     MissionUtils.Console.print('\n실행 결과')
-    for(let i = 0; i < tryNum; i++){
-      for(let n = 0; n < names.length; n++){
-        let randNum = MissionUtils.Random.pickNumberInRange(0, 9);
-        if(randNum >= 4){
-          result[n] += '-';
-        }
-      }
-      for(let n = 0; n < names.length; n++){
-        MissionUtils.Console.print(names[n] + ' : ' + result[n]);
-      }
-      MissionUtils.Console.print('');
-    }
+    this.run_car(tryNum, names, result)
 
     let maxNum = -1;
     let winners = [];
@@ -41,6 +23,36 @@ class App {
       }
     }
 
+    this.show_winners(winners)
+  }
+  
+  len_Check(result, names){
+    for(let i = 0; i < names.length; i++){
+      if(names[i].length > 5){
+        throw new Error("[ERROR] 이름의 길이가 5를 초과했습니다 (길이 > 5)");
+      } else if(names[i].length == 0){
+        throw new Error("[ERROR] 비어있는 이름을 작성하였습니다. (길이 = 0)");
+      }
+      result.push('');
+    }
+  }
+
+  run_car(tryNum, names, result){
+    for(let i = 0; i < tryNum; i++){
+      for(let n = 0; n < names.length; n++){
+        let randNum = MissionUtils.Random.pickNumberInRange(0, 9);
+        if(randNum >= 4){
+          result[n] += '-';
+        }
+      }
+      for(let n = 0; n < names.length; n++){
+        MissionUtils.Console.print(names[n] + ' : ' + result[n]);
+      }
+      MissionUtils.Console.print('');
+    }
+  }
+
+  show_winners(winners){
     let dap = '최종 우승자 : '
     for (let i = 0; i < winners.length; i++){
       if (i == winners.length - 1) {
@@ -51,7 +63,6 @@ class App {
     }
     MissionUtils.Console.print(dap);
   }
-  
 }
 
 export default App;
