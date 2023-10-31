4fa898a6d17e9163a65a680382f8b38a0e236a1b
uraflower <82873315+uraflower@users.noreply.github.com>
2023-10-31 18:19:31 +0900

    feat: 경기 무효 안내 문구 출력 기능 추가

--- src/ScoreBoard.js
+++ src/ScoreBoard.js
@@ -1,4 +1,5 @@
 import { Console } from '@woowacourse/mission-utils';
+import { RACE_INVALIDATED } from './constants/messagesConstants.js';
 
 function getNameOfWinners(winners) {
   return winners.map((car) => car.name);
@@ -14,6 +15,10 @@ class ScoreBoard {
   static announceWinners(winners) {
     const winnersNameList = getNameOfWinners(winners);
     Console.print(`최종 우승자 : ${winnersNameList.join(', ')}`);
+  }
+
+  static announceRaceInvalidated() {
+    Console.print(RACE_INVALIDATED);
   }
 }
 
--- src/constants/messagesConstants.js
+++ src/constants/messagesConstants.js
@@ -2,6 +2,8 @@ export const INPUT_ATTEMPTS = '시도할 횟수를 입력하세요. : ';
 export const INPUT_CARS =
   '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) : ';
 export const RACE_STATUS = '경주 진행 결과';
+export const RACE_INVALIDATED =
+  '경주에 성공한 자동차가 없어, 경기를 무효 처리합니다.';
 
 // error messages
 export const ERROR_NAME_LENGTH = '[ERROR] 이름은 5자 이내여야 합니다.';
