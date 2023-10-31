b579a8947f633a189a6230194ee61335c37725f7
sihyun10 <hyhy200170@gmail.com>
2023-11-01 03:42:46 +0900

    test: 마지막 레이스 결과를 기반으로 우승자 구하는 기능 테스트 Winner.js파일의 calculateWinners()를 테스트 작성하였습니다. Winner.js파일은 마지막 레이스 결과를 기반으로 우승자를 구하는 기능이 담긴 파일입니다. 따라서 여러가지 경우를 테스트해보았습니다. '우승자가 여러명일 경우', '우승자가 한명일 경우', '빈값으로 똑같을 경우' 이렇게 3가지 경우를 테스트해보았습니다.

--- cargame/Winner.js
+++ cargame/Winner.js
@@ -1,14 +1,14 @@
 class Winner {
   calculateWinners(lastRaceResult) {
-    const lastCarResults = lastRaceResult.split('\n').map((line) => {
+    const lastRaceResults = lastRaceResult.split('\n').map((line) => {
       const [name, position] = line.split(' : ');
       return { name, position };
     });
 
     const maxPositionCount = Math.max(
-      ...lastCarResults.map((car) => car.position.split('-').length - 1),
+      ...lastRaceResults.map((car) => car.position.split('-').length - 1),
     );
-    const winners = lastCarResults.filter(
+    const winners = lastRaceResults.filter(
       (car) => car.position.split('-').length - 1 === maxPositionCount,
     );
     const winnerNames = winners.map((winner) => winner.name);
