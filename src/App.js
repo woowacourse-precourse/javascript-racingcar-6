d7b0bf1228a13929ef76ec6748e65f1e2df405ea
baegyeong <beth0929@naver.com>
2023-10-31 23:47:42 +0900

    feat: 자동차 이름을 입력받는 메서드 구현
    - 자동차 배열을 생성자로 선언
    - setter로 입력에서 공백은 제거
    - setter로 자동차 이름이 6자 이상일 경우 에러처리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { MissionUtils, Console } from "@woowacourse/mission-utils";
+import { GAME_INFO } from "./constant.js";
+
 class App {
-  async play() {}
+  constructor() {
+    this._carArr = [];
+  }
+
+  async play() {
+    this.getInputCar();
+  }
+
+  async getInputCar() {
+    const carName = await Console.readLineAsync(GAME_INFO.INPUT_CAR);
+    this.carArr = carName.split(",");
+  }
+
+  set carArr(car) {
+    car = car.map((carName) => carName.replace(/\s/g, ""));
+
+    if (car.some((carName) => carName.length > 6)) {
+      console.error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+    }
+
+    this._carArr = car;
+  }
 }
 
 export default App;
