ce79be02a3a6c3796ba843862fb9acdda04593c4
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-10-31 16:24:22 +0900

    feat : 입력받은 시도 횟수보다 round를 더 진행했을때 error throw 기능
    roundStart() : this.#tryRound를 이용해 게임이 끝남을 알리는 isFinish() 메서드를 이용해 게임 종료가 true인데 메서드를 호출하면 throw

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -26,6 +26,11 @@ export default class RacingGame {
   }
 
   roundStart() {
+    if (this.isFinish()) {
+      throw new Error(
+        '[ERROR] 입력받은 round 횟수만큼 게임을 진행했습니다. 결과를 확인해주세요'
+      );
+    }
     this.#carList.forEach((car) => {
       const carPickNumber = this.createRandomNumber();
       const isMovable = this._isMovalbe(carPickNumber);
@@ -43,6 +48,7 @@ export default class RacingGame {
 
   getWinners() {
     if (this.#tryRound !== 0) {
+      // this.#tryRound 가 0이 아니면 error를 던지고 0이면 테스트 결과 반환하는 테스트 만들어보기
       throw new Error(
         `[ERROR] 게임 round가 전부 실행되지 않았습니다. 잔여 라운드 : ${
           this.#tryRound
