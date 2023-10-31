e863170eb7fb1b909ffa0fea02f18889c41f077d
0jenn0 <jenn0.6n@gmail.com>
2023-10-31 12:20:57 +0900

    Refactor: SRP에 따라 분리한 class들을 RacingCarGame에 반영함
    RacingCarGame에서 start 메서드로 게임이 시작될 때 마다 cars와 raceround를 초기화하므로
    멤버 변수로 관리하던것을 start 메서드 안에 넣음.

--- src/Controllers/RacingCarGame.js
+++ src/Controllers/RacingCarGame.js
@@ -1,67 +1,11 @@
-import { InputView, OutputView } from "../Views/index.js";
-import { Car, RaceRound } from "../Models/index.js";
+import { RacingInitializer, RacingExecutor, RacingResult } from "./index.js";
 
 export default class RacingCarGame {
-  constructor() {
-    this.cars = [];
-    this.raceround;
-  }
-
   async start() {
-    await this.#setupCars();
-    await this.#setupRaceRound();
-    this.#runRaces();
-    const winners = this.#getWinner();
-    OutputView.printWinner(winners);
-  }
+    const cars = await RacingInitializer.initializeCars();
+    const raceround = await RacingInitializer.initializeRaceRound();
 
-  async #setupCars() {
-    const carNamesInput = await InputView.promptCarNames();
-    Array.from(carNamesInput, (carName) => {
-      this.cars.push(new Car(carName));
-    });
-  }
-
-  async #setupRaceRound() {
-    const raceround = await InputView.promptRaceRound();
-    this.raceround = new RaceRound(raceround).raceRound;
-  }
-
-  #runRaces() {
-    Array.from({ length: this.raceround }, () => {
-      this.#executeRace();
-      this.#displayRaceResults();
-    });
-  }
-
-  #executeRace() {
-    Array.from(this.cars, (car) => {
-      car.race();
-    });
-  }
-
-  #displayRaceResults() {
-    const carsStatus = this.#getCarsStatus();
-    OutputView.printOneRound(carsStatus);
-  }
-
-  #getCarsStatus() {
-    const carsStatus = [];
-    Array.from(this.cars, (car) => {
-      const carStus = car.getStatus();
-      carsStatus.push(carStus);
-    });
-    return carsStatus;
-  }
-
-  #getWinner() {
-    const carsStatus = this.#getCarsStatus();
-    const maxDistance = Math.max(...carsStatus.map((car) => car.totalDistance));
-
-    const winners = carsStatus
-      .filter((car) => car.totalDistance === maxDistance)
-      .map((car) => car.name);
-
-    return winners.join(", ");
+    RacingExecutor.runRaces(cars, raceround);
+    RacingResult.announceWinner(cars);
   }
 }
