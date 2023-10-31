3f403c12293c076fbd616bd4ada03d0c9d74550f
Yoonyesol <maniac970430@naver.com>
2023-10-31 15:45:29 +0900

    test(ValidateTryNumberTest): 시도 횟수에 대한 예외 처리 테스트 케이스 수정
    MODIFY
    -Console.readLineAsync로 mock를 이용해 사용자 입력을 받은 뒤, 함수를 비동기적으로 호출하고 반환값을 검사하도록 테스트케이스를 수정

--- __tests__/ValidateTryNumberTest.js
+++ __tests__/ValidateTryNumberTest.js
@@ -1,23 +1,24 @@
 import { inputTryNumberHandler } from "../src/utils/inputHandler.js";
+import { Console } from "@woowacourse/mission-utils";
 
 describe("시도 횟수 입력 함수 테스트", () => {
-  test("올바른 숫자 입력", () => {
-    const input = "5";
-    expect(async () => inputTryNumberHandler(input)).not.toThrow();
+  test("올바른 숫자 입력", async () => {
+    Console.readLineAsync = jest.fn().mockResolvedValue("5");
+    await expect(inputTryNumberHandler()).resolves.toBe("5");
   });
 
-  test("음수 값 입력", () => {
-    const input = "-3";
-    expect(async () => inputTryNumberHandler(input)).rejects.toThrow("[ERROR]");
+  test("음수 입력", async () => {
+    Console.readLineAsync = jest.fn().mockResolvedValue("-3");
+    await expect(inputTryNumberHandler()).rejects.toThrow("[ERROR]");
   });
 
-  test("숫자가 아닌 값을 입력", () => {
-    const input = "abc";
-    expect(async () => inputTryNumberHandler(input)).rejects.toThrow("[ERROR]");
+  test("숫자가 아닌 값을 입력", async () => {
+    Console.readLineAsync = jest.fn().mockResolvedValue("abc");
+    await expect(inputTryNumberHandler()).rejects.toThrow("[ERROR]");
   });
 
-  test("빈 문자열 입력", () => {
-    const input = "";
-    expect(async () => inputTryNumberHandler(input)).rejects.toThrow("[ERROR]");
+  test("빈 문자열 입력", async () => {
+    Console.readLineAsync = jest.fn().mockResolvedValue("");
+    await expect(inputTryNumberHandler()).rejects.toThrow("[ERROR]");
   });
 });
