7f1af1767999a14aedc781ff41a84fce5df77bb5
조경은 <kavvy99@naver.com>
2023-10-31 16:50:17 +0900

    feat: 시도할 횟수에 대한 입력창 생성 및 입력값이 숫자가 아닐 경우에 대한 예외처리

--- src/App.js
+++ src/App.js
@@ -17,12 +17,42 @@ function carNameCheckEvent() {
   const carName = carNameInput.value.split(",");
   console.log(carName);
 
+  let hasError = false;
   // 유저가 입력한 자동차 이름이 다섯 자 이하가 아니라면?
   for (i=0; i<carName.length; i++) {
     if (carName[i].length > 5) {
+      hasError = true;
       alert("[ERROR] 자동자의 이름이 너무 깁니다.");
       console.log("[ERROR] 자동자의 이름이 너무 깁니다.");
+      break;
+    }
+  } 
+
+  // 자동차 이름 관련한 에러가 없으면
+  if (!hasError) {
+    const body = document.querySelector('body');
+    const tryNumQuestion = document.createElement('h3');
+    tryNumQuestion.innerHTML = "시도할 횟수는 몇 회인가요?";
+
+    const tryNumber = document.createElement('input');
+    tryNumber.type='text';
+    const tryNumberCheckBtn = document.createElement('button');
+    tryNumberCheckBtn.innerHTML = "확인";
+    tryNumberCheckBtn.onclick = tryNumberCheckEvent;
+
+    body.appendChild(tryNumQuestion);
+    body.appendChild(tryNumber);
+    body.appendChild(tryNumberCheckBtn);
+
+    // tryNumberCheckEvent 시작
+    function tryNumberCheckEvent() {
+      console.log(isNaN(tryNumber.value));
+      if (isNaN(tryNumber.value) === true) {
+        hasError = true;
+        alert("[ERROR] 숫자가 잘못된 형식입니다.");
+        console.log("[ERROR] 숫자가 잘못된 형식입니다.");
+      }
     }
   }
-
 }
+
