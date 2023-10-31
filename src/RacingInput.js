3d171699946c485224431f63425fad8586118ad6
yeeun426 <thsudkcla7@naver.com>
2023-10-31 15:40:39 +0900

    feat: 자동차 이름 형식 예외 처리

--- src/RacingInput.js
+++ src/RacingInput.js
@@ -5,10 +5,21 @@ class RacingInput {
     const racingCars = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     );
-    const racingArr = racingCars.split(",");
-    const scoreArr = new Array(racingArr.length).fill(0);
 
-    return { racingArr, scoreArr };
+    const racingArr = racingCars.split(",");
+
+    if (this.errortext(racingArr)) {
+      const scoreArr = new Array(racingArr.length).fill(0);
+      return { racingArr, scoreArr };
+    }
+  }
+
+  errortext(racingArr) {
+    for (let i = 0; i < racingArr.length; i++) {
+      if (racingArr[i].length > 5)
+        throw new Error("[ERROR] 잘못된 형식의 자동차 이름입니다.");
+    }
+    return true;
   }
 
   async attemptNumInput() {
