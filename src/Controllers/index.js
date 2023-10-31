2888ac7d0cc40d95ee04d4f3908dc51d566f7d9f
0jenn0 <jenn0.6n@gmail.com>
2023-10-31 12:34:20 +0900

    Refactor: Controllers 폴더에 index.js 추가
    index.js 파일을 추가하여 Controllers import를 단순화함

---
+++ src/Controllers/index.js
@@ -0,0 +1,4 @@
+export { default as RacingCarGame } from "./RacingCarGame.js";
+export { default as RacingInitializer } from "./RacingInitializer.js";
+export { default as RacingExecutor } from "./RacingExecutor.js";
+export { default as RacingResult } from "./RacingResult.js";
