2c4dc9de3139fb2e3e7f663cf9638e6e1eaa2568
OAO <ashgrayblue0@gmail.com>
2023-10-31 22:44:02 +0900

    test(CarTest.js): test to 'goingCount' attribute's getter and setter

--- __tests__/CarTest.js
+++ __tests__/CarTest.js
@@ -2,12 +2,22 @@ import Car from '../src/module/Car.js';
 
 describe('Car 클래스 테스트', () => {
   test('name의 getter, setter 테스트', () => {
-    const car1 = new Car();
+    const car = new Car();
 
-    car1.name = 'aaaaa';
-    expect(car1.name).toEqual('aaaaa');
+    car.name = 'aaaaa';
+    expect(car.name).toEqual('aaaaa');
     expect(() => {
-      car1.name = 'bbbbbb';
+      car.name = 'bbbbbb';
+    }).toThrow('[ERROR]');
+  });
+
+  test('goingCount의 getter, setter 테스트', () => {
+    const car = new Car();
+
+    car.goingCount = true;
+    expect(car.goingCount).toEqual(1);
+    expect(() => {
+      car.goingCount = 1;
     }).toThrow('[ERROR]');
   });
 });
