4b1d1f57959f1c821b855aa76ae6e43a1eb0b3cb
ryan-dia <cjfdnjs158@naver.com>
2023-10-31 23:34:36 +0900

    feat: 유효성 검사 에러 메시지 추가

--- src/constants/Messages.js
+++ src/constants/Messages.js
@@ -6,4 +6,21 @@ export const INPUT_MESSAGE_FUNCTION = Object.freeze({
 
 export const ERROR_MESSAGE = Object.freeze({
   prefix: '[ERROR]',
+  duplication: '중복되는 이름은 사용할 수 없습니다.',
+});
+
+export const ERROR_MESSAGE_FUNCTION = Object.freeze({
+  delimiterError(delimiter) {
+    return `올바른 구분자가 아닙니다. ${delimiter} 구분자를 사용해 주세요`;
+  },
+  nameLength(min, max) {
+    return `이름의 길이는 ${min}자 ~ ${max}자사이 여야 합니다.`;
+  },
+  language(languageOption) {
+    const languages = Object.keys(languageOption);
+    return `다음의 언어만 사용할 수 있습니다. ${languages.join('')}`;
+  },
+  quantity(min, max) {
+    return `자동차 대수는 ${min}대부터 ${max}까지 가능합니다.`;
+  },
 });
