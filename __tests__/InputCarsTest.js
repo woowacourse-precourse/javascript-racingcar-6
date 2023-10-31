6b0fb004652f680efacb7a296f78e372fa3b277d
OhJeongHwan <wjdghks0316@naver.com>
2023-10-31 10:49:09 +0900

    test -inputCars()를 테스트하는 inpuCarsTest 작성

---
+++ __tests__/InputCarsTest.js
@@ -0,0 +1,29 @@
+import App from "../src/App";
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const mockQuestions = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  });
+};
+
+describe("자동차 입력 테스트", () => {
+  test("이름이 5자리가 넘어가는 경우", async () => {
+    mockQuestions(["pobi,gabozago"]);
+    const app = new App();
+
+    await expect(app.inputCars()).rejects.toThrow("[ERROR]");
+  });
+});
+
+describe("자동차 입력 테스트", () => {
+  test("이름의 길이가 0인 경우", async () => {
+    mockQuestions(["pobi,gabozago,"]);
+    const app = new App();
+
+    await expect(app.inputCars()).rejects.toThrow("[ERROR]");
+  });
+});
