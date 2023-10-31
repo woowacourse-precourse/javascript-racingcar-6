2903b53585d97e006cc20c16894a47cddf8d7d65
mieum12 <qpdlqltb1215@gmail.com>
2023-10-31 15:42:21 +0900

    fix: carsDto 수정

--- src/domian/dto/carsDto.js
+++ src/domian/dto/carsDto.js
@@ -1,6 +1,13 @@
 export class CarsDto {
+  #cars;
   constructor(carDtoList) {
-    this.cars = carDtoList;
+    // 배열은 요소가 변경 될 가능성이 있어서 불병성 유지를 위해 Object.freeze() 사용
+    this.#cars = Object.freeze(carDtoList);
+  }
+
+  // 불변, 읽기 전용으로 만들기
+  get cars() {
+    return this.#cars;
   }
 }
 
