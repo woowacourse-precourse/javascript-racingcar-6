27c786198af9341d6a6f31978ef0f018d6816b10
nari <naringst@gmail.com>
2023-10-31 12:24:43 +0900

    feat : 자동차 이름 입력 5자 이하인지 확인하기

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ class App {
     const carNameUserInput = await MissionUtils.Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     );
+    //쉼표 기준 분리
     const carNames = carNameUserInput.split(",");
+
+    //5자 이하인지 확인하기
+    const isNameUpper5 = carNames.every((carName) => carName.length() > 5);
+
+    //5자 이상이면 예외 처리
   }
 }
 
