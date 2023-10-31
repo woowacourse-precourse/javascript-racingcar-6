2b75bc7087c4016fe39d173bdeeb5ed99c3656e8
useon <yoosk4986@naver.com>
2023-11-01 01:29:54 +0900

    feat: 입력한 시도 횟수만큼 레이싱을 진행하고, 데이터를 시각화 하여, 출력하는 기능

--- src/App.js
+++ src/App.js
@@ -11,6 +11,14 @@ class App {
       '시도할 횟수는 몇 회인가요?\n',
     );
     this.roundValidation(roundInputValue);
+    const roundNumber = Number(roundInputValue);
+    this.print('\n실행 결과');
+    let roundCount = 1;
+    while (roundCount <= roundNumber) {
+      const roundResult = this.racing(carDistanceMap);
+      this.print(roundResult);
+      roundCount += 1;
+    }
   }
 
   async userInput(message) {
@@ -54,6 +62,21 @@ class App {
     if (isNaN(stringToNumber)) throw new Error('[ERROR]');
 
     if (stringToNumber < 1) throw new Error('[ERROR]');
+  }
+
+  racing(carDistanceMap) {
+    let roundResult = '';
+    carDistanceMap.forEach((value, key) => {
+      const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
+      if (randomNumber >= 4)
+        carDistanceMap.set(key, carDistanceMap.get(key) + '-');
+      roundResult += `${key} : ${value}\n`;
+    });
+    return roundResult;
+  }
+
+  print(message) {
+    MissionUtils.Console.print(message);
   }
 }
 
