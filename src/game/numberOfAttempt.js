e3007c53955385b67f176ae1ff84957c2f2a05d6
yunji <dbswl330@gmail.com>
2023-11-01 00:48:57 +0900

    feat: 시도횟수 validation 함수 구현
    입력받은 시도횟수가 0 이상의 정수인지 검사

---
+++ src/game/numberOfAttempt.js
@@ -0,0 +1,16 @@
+class NumberOfAttempt {
+  constructor(inputNumber) {
+    this.NumberOfAttempt = inputNumber;
+    this.validateNumber();
+  }
+
+  validateNumber() {
+    if (!Number.isInteger(Number(this.NumberOfAttempt))) {
+      throw new Error("[ERROR] 정수를 입력해 주세요.");
+    }
+    if (this.NumberOfAttempt <= 0) {
+      throw new Error("[ERROR] 0이상의 정수를 입력해 주세요. ");
+    }
+  }
+}
+export default NumberOfAttempt;
