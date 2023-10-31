b2b931a6ac6cb814adb13e46aebf87de75939124
hanyoung <phanyoung@naver.com>
2023-10-31 19:43:08 +0900

    test: Cars 생성 시의 유효성 검증 로직에 대한 테스트 추가

--- __tests__/FunctionTest.js
+++ __tests__/FunctionTest.js
@@ -11,4 +11,14 @@ describe("함수 기능 테스트", () => {
     expect(car.getName()).toBe(NAME);
     expect(car.getName()).not.toBe(WRONG_NAME);
   });
+
+  test("Cars 생성 시 배열에 Car가 아닌 값을 넣었을 경우에 대한 예외 처리를 했는가", () => {
+    const WRONG_INPUT1 = ["가짜 차"];
+    const WRONG_INPUT2 = [{ name: "가짜 차" }];
+    const CORRECT_INPUT = [new Car("진짜 차")];
+
+    expect(() => new Cars(WRONG_INPUT1)).toThrow("[ERROR]");
+    expect(() => new Cars(WRONG_INPUT2)).toThrow("[ERROR]");
+    expect(() => new Cars(CORRECT_INPUT)).not.toThrow("[ERROR]");
+  });
 });
