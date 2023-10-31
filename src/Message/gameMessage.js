7888a115a98be7a6475692eccae6d16a7140df03
Eunseo Na <nes020781@gmail.com>
2023-10-31 19:39:05 +0900

    fix: 메시지 통일성 있게 수정

--- src/Message/gameMessage.js
+++ src/Message/gameMessage.js
@@ -5,8 +5,8 @@ const GameMessage = Object.freeze({
   RACING_WINNER: "최종 우승자 : ",
 
   ERROR: {
-    CAR_NOT_INPUTED: "[ERROR] 차의 이름이 입력되지 않았습니다.",
-    CAR_NAME_INVALID: "[ERROR] 차의 이름은 5자가 넘지 않아야 합니다.",
+    CAR_NOT_INPUTED: "[ERROR] 자동차의 이름이 입력되지 않았습니다.",
+    CAR_NAME_INVALID: "[ERROR] 자동차의 이름은 5자가 넘지 않아야 합니다.",
     CAR_NAME_DUPLICATED: "[ERROR] 차의 이름은 중복되지 않아야 합니다.",
     TRY_NUMBER_INVALID: "[ERROR] 시도할 횟수를 정확하게 입력해 주세요.",
   },
