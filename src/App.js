ec137c47c78b1aceecfa8ca32db5da686da03d14
swJaNG12 <cbdjej38483@gmail.com>
2023-11-01 08:35:27 +0900

    feat(App) : add convertCarNamesToObject method
    this method converts a car names array
    into an object format

--- src/App.js
+++ src/App.js
@@ -12,23 +12,18 @@ class App {
 
       const carNameArray = carNamesString.split(',');
       const attemptForwardCount = await this.getUsetInputForwardCount();
-      Console.print(attemptForwardCount);
-      Console.print(carNameArray);
 
-      let carStatus = carNameArray.map((name) => {
-        return { name, status: '' };
-      });
-      Console.print(carStatus);
+      let carStatusArray = this.convertCarNamesToObject(carNameArray);
 
       for (let i = 0; i < parseInt(attemptForwardCount); i++) {
-        carStatus = carStatus.map((car) => {
+        carStatusArray = carStatusArray.map((car) => {
           return {
             name: car.name,
             status: this.attepmtMove(car.status),
           };
         });
-        Console.print(carStatus);
-        carStatus.forEach((car) => {
+        Console.print(carStatusArray);
+        carStatusArray.forEach((car) => {
           Console.print(`${car.name} : ${car.status}`);
         });
         Console.print('\n');
@@ -45,6 +40,11 @@ class App {
   }
   async getUsetInputForwardCount() {
     return await Console.readLineAsync('시도할 횟수는 몇 회인가요?');
+  }
+  convertCarNamesToObject(array) {
+    return array.map((name) => {
+      return { name, status: '' };
+    });
   }
   attepmtMove(status) {
     const randomNumber = Random.pickNumberInRange(0, 9);
