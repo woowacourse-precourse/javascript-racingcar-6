75c7ae12806fee88117ebfdcfb9d6656d4636789
geunwoo <olo90632951@gmail.com>
2023-11-01 00:15:59 +0900

    feat: 자동차 이름을 쉼표로 구분하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import { Console } from '@woowacourse/mission-utils';
 
 class App {    
+  constructor() {
+    this.cars = [];
+  }
+
   async play() {
     const carNamesInput = await Console.readLineAsync('경주할 자동차이름을입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');   
+    this.cars = carNamesInput.trim().split(',').map((name) => name.trim());
   }
 }
 
