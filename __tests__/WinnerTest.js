73a3cf5f9527ca7b3ebf780e9ad52dd3a38969ae
이용성 <ukal5006@kakao.com>
2023-10-31 22:10:41 +0900

    test: fix log spy function

--- __tests__/WinnerTest.js
+++ __tests__/WinnerTest.js
@@ -1,20 +1,22 @@
 import findAndPrintWinner from '../src/Winner';
 import { MissionUtils } from '@woowacourse/mission-utils';
 
-const getLogSpy = () => {};
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
 
 describe('Winner.js 테스트', () => {
   test('작동 테스트 - 우승자 한명', () => {
     const inputCarNamesArray = ['sk', 'lg', 'kia'];
     const inputraceResults = ['---', '--', '-'];
-
     const output = '최종 우승자 : sk';
-
-    const spy = jest.spyOn(MissionUtils.Console, 'print');
+    const logSpy = getLogSpy();
 
     findAndPrintWinner(inputCarNamesArray, inputraceResults);
 
-    expect(spy).toHaveBeenCalledWith(output);
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
   });
   test('작동 테스트 - 우승자 두명', () => {
     const inputCarNamesArray = ['sk', 'lg', 'kia'];
@@ -26,6 +28,6 @@ describe('Winner.js 테스트', () => {
 
     findAndPrintWinner(inputCarNamesArray, inputraceResults);
 
-    expect(spy).toHaveBeenCalledWith(output);
+    expect(spy).toHaveBeenCalledWith(expect.stringContaining(output));
   });
 });
