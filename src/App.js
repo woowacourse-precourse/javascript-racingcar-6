bded6123e714be7b733d10001047884623575331
dongminlee <gmgmgun@gmail.com>
2023-10-31 20:41:16 +0900

    feat: 최종 우승자 출력

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ const INPUT_CAR_NAMES_MSG =
   '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)';
 const INPUT_ATTEMPT_TIMES_MSG = '시도할 횟수는 몇 회인가요?';
 const INPUT_ERROR_MSG = '[ERROR] 입력 받는 도중 에러가 발생했습니다.';
+const OUTPUT_WINNERS_MSG = '최종 우승자 : ';
 
 class App {
   async getCarNameArr() {
@@ -43,16 +44,29 @@ class App {
       return shift;
     }
 
+    let maxShift = -1;
+    let winner = [];
+
     for (let i = 0; i < carArr.length; i++) {
       const shift = await getShift(attemptTimes);
+      if (shift > maxShift) {
+        maxShift = shift;
+        winner = [carArr[i]];
+      } else if (shift === maxShift) winner.push(carArr[i]);
+
       MissionUtils.Console.print(`${carArr[i]} : ${'-'.repeat(shift)}`);
     }
+
+    return winner;
   }
 
   async play() {
     const carNameArr = await this.getCarNameArr();
     const attemptTimes = await this.getAttemptTimes();
-    await this.printShifts(carNameArr, attemptTimes);
+    const winners = await this.printShifts(carNameArr, attemptTimes);
+    MissionUtils.Console.print(
+      `${OUTPUT_WINNERS_MSG}` + `${winners.join(', ')}`
+    );
   }
 }
 
