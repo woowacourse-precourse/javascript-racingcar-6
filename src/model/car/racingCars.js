6bebd467d736aa6b4f4003ea3cf76810aa68bd29
yunji <dbswl330@gmail.com>
2023-11-01 00:26:57 +0900

    feat: 입력받은 이름을 , 를 기준으로  파싱하는 함수 구현
    입력받은 이름 스트링을 쉼표를 기준으로 나누어 이름 리스트로 반환한다.

--- src/model/car/racingCars.js
+++ src/model/car/racingCars.js
@@ -4,4 +4,9 @@ class RacingCars {
       throw new Error("[ERROR] 이름을 입력해 주세요.");
     }
   }
+
+  parseCarNames(inputNamesString) {
+    const carNamesList = inputNamesString.split(",").map((name) => name.trim());
+    return carNamesList;
+  }
 }
