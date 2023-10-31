467a4823dfa5a5405c54e4440e7c32d62a08a4e1
kwonja <kownja87@gmail.com>
2023-11-01 01:16:06 +0900

    [fix] : 자동차 객체 추가 및 초기화 구현

--- src/App.js
+++ src/App.js
@@ -5,25 +5,33 @@ class App {
   #TRY_COUNT;
   async racingCarNameInput(){
      await MissionUtils.Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-     const carNameInput = await MissionUtils.Console.readLineAsync("");
-     if(!this.checkCarNameValidation(carNameInput))
+      const carNameInput = await MissionUtils.Console.readLineAsync("");
+      const carNames = carNameInput.split(',');
+     if(!this.checkCarNameValidation(carNames))
      {
       throw new Error("[ERROR] 이름은 5자 이하만 가능합니다.");
      }
+     this.initializeCarList(carNames)
   }
-  checkCarNameValidation(carNameInput)
+  initializeCarList(racingCarNames){
+    racingCarNames.forEach( (carName)=>{
+      this.#RACING_CAR_LIST.push({
+        carName : carName,
+        state : ''
+      })
+    })
+  }
+  checkCarNameValidation(carNames)
   {
-    const carNames = carNameInput.split(',');
     return carNames.every( (carName)=> carName.length < 6)
   }
   async racingTryInput(){
     await MissionUtils.Console.print("시도할 횟수는 몇 회인가요?");
-    const carTryInput = await MissionUtils.Console.readLineAsync("");
-    if(this.checkCarTryValidation(carTryInput))
+    this.#TRY_COUNT = await MissionUtils.Console.readLineAsync("");
+    if(this.checkCarTryValidation(this.#TRY_COUNT))
     {
       throw new Error("[ERROR] 횟수는 숫자만 입력 가능합니다.");
     }
-    this.#TRY_COUNT=carTryInput;
   }
   checkCarTryValidation(carTryInput){
     return isNaN(parseInt(carTryInput));
@@ -32,16 +40,22 @@ class App {
     await MissionUtils.Console.print("실행결과");
     for(let i=0;i<this.#TRY_COUNT;i++)
     {
-      this.moveCar();
+
     }
+  }
+  raceStart(){
+    this.#RACING_CAR_LIST.forEach(element => {
+
+    });
   }
   moveCar(){
 
   }
+
   async play() {
     await this.racingCarNameInput();
     await this.racingTryInput();
-    this.doRace();
+    // this.raceStart();
   }
 }
 
