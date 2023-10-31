ef535cf9197410d99e26f6fb443124f492f42697
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-11-01 01:38:43 +0900

    feat: n 대의 자동차 전진, 위치 증가, 위치 구하기 기능 추가
    * Cars 클래스 추가
       - 자동차 경주 게임에서 각각의 자동차 인 Car 클래스 인스턴스를 배열 요소로 가지는 #list 속성
       - move 메서드로 n 대의 자동차 전진
       - getPositions 메서드로 n 대의 자동차 위치 구하기

---
+++ src/Cars.js
@@ -0,0 +1,36 @@
+import { Car } from './Car.js';
+import { RandomGenerator } from './RandomGenerator.js';
+
+export class Cars {
+  #list = [];
+
+  constructor(carNames) {
+    this.initialize(carNames);
+  }
+
+  getList() {
+    return this.#list;
+  }
+
+  getPositions() {
+    return this.#list.map((car) => car.getPosition());
+  }
+
+  initialize(carNames) {
+    carNames.forEach((name) => {
+      const randomGenerator = this.makeNewRandomGenerator();
+      const car = new Car(name, randomGenerator);
+      this.#list.push(car);
+    });
+  }
+
+  makeNewRandomGenerator() {
+    return new RandomGenerator(RandomGenerator.RANGE.MIN, RandomGenerator.RANGE.MAX);
+  }
+
+  move() {
+    this.#list.forEach((car) => {
+      car.moveOrStop();
+    });
+  }
+}
