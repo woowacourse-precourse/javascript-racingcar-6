b73b58ab744246652ed036c497742456bac7cc51
jayming66 <mjw8941@gmail.com>
2023-11-01 03:14:45 +0900

    feat : 유저의 자동차 이동 횟수를 입력

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ class App {
     let carArray = carNames.split(",");
 
     carArray = carArray.map((carName) => new Car(carName));
+
+    const roundNum = await MissionUtils.Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요?\n"
+    );
   }
 }
 
