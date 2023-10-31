6cd6e45e62ad9b1b2c00b8d6c6dd15deac28a927
hyewon <hyewon.parkhw@gmail.com>
2023-10-31 20:24:30 +0900

    feat: 자동차 움직임 반복 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async play() {
     const carNames = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
-    const moves = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n")
+    const moves = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+    let moveResult = Object.fromEntries(carNames.split(",").map(name=>[name,""]))
+
+    for (let i=0; i<moves; i++){
+      Object.keys(moveResult).map((key) => moveResult[key]+=this.moveCar())
+    }
   }
 
   moveCar(){
