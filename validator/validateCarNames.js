4e26d494ed5751abe515d53e70dcb952642aa733
yeong-hwan <jghff700@naver.com>
2023-11-01 05:13:55 +0900

    refactor: validateCarName 함수의 input parameter 자료형 변경
    {Promise<string>}을 강제 형변환 하지 않고, input 자체를 {string}으로 받도록 변경

--- validator/validateCarNames.js
+++ validator/validateCarNames.js
@@ -24,22 +24,19 @@ function checkDuplication(carNames) {
 
 /**
  * 입력 받은 문자열을 검증하여, 문제가 있을 경우 에러를 발생시킨다.
- * 1. 스페이스 등의 공백이 있을 경우 제거하여 판단한다.
- * 2. 이름이 1글자 이상, 5글자 이하인지 구분한다.
- * 3. 아무것도 입력되지 않았거나, 이름이 공백인지 구분한다.
+ * 1. 중복된 이름이 있는지 확인한다.
+ * 2. 이름이 1글자 이상, 5글자 이하인지 확인한다.
+ * 3. 아무것도 입력되지 않았거나, 이름이 공백인지 확인한다.
  * 
- * @param {Promise<string>} carNamesInput
+ * @param {list} carNames
  * @throws 사용자가 잘못된 값을 입력했을 경우 throw Error
  */
-export default function validateCarNames(carNamesInput) {
-  const carNamesWithoutSpace = String(carNamesInput).replace(/(\s*)/g, '');
-  const carNames = carNamesWithoutSpace.split(",");
-
+export default function validateCarNames(carNames) {
   checkDuplication(carNames);
 
   for (const carName of carNames) {
-    checkBlankInput(carName);
     checkOverLength(carName);
+    checkBlankInput(carName);
   }
 
 
