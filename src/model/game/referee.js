7d05454fbe7b53060b460607d7c71ffd6a3ee30b
yunji <dbswl330@gmail.com>
2023-11-01 01:41:26 +0900

    feat:Add Referee class for comparing movemen of cars

---
+++ src/model/game/referee.js
@@ -0,0 +1,14 @@
+class Referee {
+  #winners = [];
+
+  compareCars(racingCars) {
+    const cars = Object.values(racingCars);
+    const maxMoveCount = Math.max(...cars.map((car) => car.getMoveCount()));
+    this.#winners = cars.filter((car) => car.getMoveCount() === maxMoveCount);
+
+    const winnerNames = this.#winners.map((car) => car.getName());
+
+    return winnerNames;
+  }
+}
+export default Referee;
