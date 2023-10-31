a69d48d3fc9ebb087ab1e8d4e66f2a493298602c
shgusgh12 <https://github.com/shgusgh12>
2023-10-31 17:40:33 +0900

    feat: 자동차 이동하는 메서드, 전진한 거리 출력하는 메서드 구현

--- src/Car.js
+++ src/Car.js
@@ -3,6 +3,14 @@ class Car {
     this.name = name;
     this.move = 0;
   }
+
+  advance() {
+    this.move += 1;
+  }
+
+  printMove() {
+    return '-'.repeat(this.move);
+  }
 }
 
 export default Car;
