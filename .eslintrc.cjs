1e1506d22661e4fa1ea4e338363f17f13dea2d3c
Jinhyo Park <vkstk0131@yonsei.ac.kr>
2023-11-01 07:33:37 +0900

    style: ESLint, Prettier 적용
    생성된 파일:
    - .eslintrc.cjs (airbnb style)
    - .prettierrc.cjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+		'import/prefer-default-export': 'off',
+		'import/extensions': ['off'],
+	},
+};
