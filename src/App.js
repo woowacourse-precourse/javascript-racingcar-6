c8ba40ee983ab4909451a595734cf4d8feb9f0df
Mijin Sim <202110106@bssm.hs.kr>
2023-10-31 17:49:08 +0900

    delete(App.js) : 불필요한 부분 제거

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import { Console } from "@woowacourse/mission-utils";
 import RacingcarGame from "./RacingcarGame.js";
 
 class App {
   async play() {
     const racingcar = new RacingcarGame();
-    const startGame = await racingcar.startGame();
-    // if (inputValue) Console.print(inputValue);
+    await racingcar.startGame();
   }
 }
 
