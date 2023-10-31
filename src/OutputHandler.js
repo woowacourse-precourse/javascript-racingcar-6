53946e02811bb33501fe33c24f97226d2997fccd
j8won <joowoni99@gmail.com>
2023-10-31 22:08:59 +0900

    feat(OutputHandler): Add the printGameExecution method for priniting car positions in each round

--- src/OutputHandler.js
+++ src/OutputHandler.js
@@ -2,8 +2,20 @@ import { Console } from "@woowacourse/mission-utils";
 import MESSAGE from "./constant/MESSAGE.js";
 
 class OutputHandler {
-  static printInitialExecutionMessage() {
-    Console.print(MESSAGE.INITIAL_RACE_EXECUTION);
+  static printGameExecution(roundArray) {
+    Console.print(MESSAGE.GAME_RESULT);
+    roundArray.forEach((round) => this.printRoundResult(round));
+  }
+
+  static printRoundResult(carArray) {
+    carArray.forEach((car) =>
+      Console.print(`${car.name} : ${this.generateHyphens(car.position)}`)
+    );
+    Console.print("");
+  }
+
+  static generateHyphens(number) {
+    return "-".repeat(number);
   }
 }
 
