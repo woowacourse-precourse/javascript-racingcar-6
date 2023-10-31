027c493ab4e36b442e355e26cc02ba3efd5d92e6
yeonkyoung <keep.ykk@gmail.com>
2023-11-01 02:04:19 +0900

    feat: 숫자 값이 4 이상인 경우 true반환, 미만인 경우 false 반환

--- src/utils/move.js
+++ src/utils/move.js
@@ -3,3 +3,8 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 const getNumber = () => {
   return MissionUtils.Random.pickNumberInRange(0, 9);
 };
+
+const checkNumber = (number) => {
+  return number >= 4;
+};
+
