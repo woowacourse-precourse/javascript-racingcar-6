a8f801da1ed22e4700e424c9813040afb21e358a
Ryong-E <think8867@gmail.com>
2023-10-31 20:29:12 +0900

    test: Controller 테스트 코드 작성
    우승자와, 공동 우승자 출력 테스트

---
+++ __tests__/ControllerTest.js
@@ -0,0 +1,57 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import Controller from '../src/controllers/Controller.js';
+
+const mockQuestions = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  });
+};
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe('controller 전체 테스트', () => {
+  test('공동 우승자 테스트', async () => {
+    const MOVING = 4;
+    const STOP = 3;
+    const controller = new Controller();
+    const movingNumber = [MOVING, MOVING, STOP];
+    const inputs = ['가나다,마바사,아자차', '1'];
+    const output = ['가나다 : -', '마바사 : -', '아자차 : ', '최종 우승자 : 가나다, 마바사'];
+    mockQuestions(inputs);
+    mockRandoms([...movingNumber]);
+    const logSpy = getLogSpy();
+    await controller.play();
+    output.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+  test('우승자 테스트', async () => {
+    const MOVING = 4;
+    const STOP = 3;
+    const controller = new Controller();
+    const movingNumber = [MOVING, STOP, STOP];
+    const inputs = ['가나다,마바사,아자차', '1'];
+    const output = ['가나다 : -', '마바사 : ', '아자차 : ', '최종 우승자 : 가나다'];
+    mockQuestions(inputs);
+    mockRandoms([...movingNumber]);
+    const logSpy = getLogSpy();
+    await controller.play();
+    output.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+});
