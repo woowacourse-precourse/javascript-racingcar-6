bc7b9f758e1123a9b95dad2a0feb6d4725793ba8
Namkyung Cho <srirachacho@gmail.com>
2023-10-31 23:02:24 +0900

    :recycle: Refactor: 프리티어 미적용된 부분 적용, 공백 제거

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,14 +1,14 @@
-const ERROR_HEADER = '[ERROR]'
+const ERROR_HEADER = "[ERROR]";
 
 export const ERROR_TYPE = {
-    EMPTY_NAME: `${ERROR_HEADER} 이름을 입력해주세요.`,
-    NAME_LENGTH: `${ERROR_HEADER} 자동차의 이름은 5자 이하만 가능합니다.`,
-    EMPTY_ATTEMPTS: `${ERROR_HEADER} 시도 횟수를 입력해주세요.`,
-    INTEGER_ATTEMPTS: `${ERROR_HEADER} 시도 횟수는 정수만 가능합니다.`,
-}
+  EMPTY_NAME: `${ERROR_HEADER} 이름을 입력해주세요.`,
+  NAME_LENGTH: `${ERROR_HEADER} 자동차의 이름은 5자 이하만 가능합니다.`,
+  EMPTY_ATTEMPTS: `${ERROR_HEADER} 시도 횟수를 입력해주세요.`,
+  INTEGER_ATTEMPTS: `${ERROR_HEADER} 시도 횟수는 정수만 가능합니다.`,
+};
 
 export const GAME_MESSAGE = {
-    CAR_NAMES: '경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)',
-    ATTEMPTS: '시도할 횟수는 몇 회인가요?',
-    WINNER: '최종 우승자: ',
-}
+  CAR_NAMES: "경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)",
+  ATTEMPTS: "시도할 횟수는 몇 회인가요?",
+  WINNER: "최종 우승자: ",
+};
