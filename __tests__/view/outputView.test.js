bf3d75df74013fe7dc8e63839150afe26594d564
이태훈 <rhymint@gmail.com>
2023-10-31 14:06:52 +0900

    test : outputView 테스트 코드 추가

---
+++ __tests__/view/outputView.test.js
@@ -0,0 +1,29 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import OutputView from "../../src/view/OutputView";
+import { CONSOLE_MESSAGE } from "../../src/constants/consoleMessages";
+import RacingCar from "../../src/model/RacingCar";
+
+const outputView = new OutputView();
+const spy = jest.spyOn(MissionUtils.Console, 'print');
+
+describe('outputView test', () => {
+  test('func printQuery', () => {
+    outputView.printQuery('test');
+
+    expect(spy).toHaveBeenCalledWith(expect.stringContaining('test'));
+  });
+
+  test('func printRacingCarState', () => {
+    const mockCar = new RacingCar('test')
+    outputView.printRacingCarState(mockCar);
+
+    expect(spy).toHaveBeenCalledWith(expect.stringContaining('test : '));
+  });
+
+  test('func outputFinalResult', () => {
+    const input = 'kim, park';
+    outputView.outputFinalResult(input);
+
+    expect(spy).toHaveBeenCalledWith(expect.stringContaining(`${CONSOLE_MESSAGE.outputFinalResult} : ${input}`));
+  });
+})
