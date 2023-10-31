1fd1742585c4e42a411b4f48e7268a3b2ab98d35
yeeun426 <thsudkcla7@naver.com>
2023-10-31 15:30:00 +0900

    feat: 이동이 끝나면 점수를 계산해 우승자 출력

--- src/RacingScore.js
+++ src/RacingScore.js
@@ -1,12 +1,29 @@
 import RacingOutput from "./RacingOutput";
 
 class RacingScore {
-  randomScore(attemptNum, racingArr, scoreArr) {
-    const racingOutput = new RacingOutput();
+  constructor() {
+    this.racingOutput = new RacingOutput();
+  }
 
+  randomScore(attemptNum, racingArr, scoreArr) {
     for (let i = 0; i < attemptNum; i++) {
-      racingOutput.printScore(racingArr, scoreArr);
+      this.racingOutput.printScore(racingArr, scoreArr);
     }
+
+    this.matchScoreCars(racingArr, scoreArr);
+  }
+
+  matchScoreCars(racingArr, scoreArr) {
+    const maxScore = Math.max(...scoreArr);
+    let maxIndex = scoreArr.indexOf(maxScore);
+    const answer = [];
+
+    while (maxIndex != -1) {
+      answer.push(racingArr[maxIndex]);
+      maxIndex = scoreArr.indexOf(maxScore, maxIndex + 1);
+    }
+
+    this.racingOutput.printAnswer(answer);
   }
 }
 
