bcbd8cf5647965a31c1fcfc24ad930094137cbe9
OAO <ashgrayblue0@gmail.com>
2023-10-31 22:28:37 +0900

    feat(Car.js): implement name attribute's getter and setter
    - set the 'name' attribute as private
    - make getter and setter
    - make error throw

--- src/module/Car.js
+++ src/module/Car.js
@@ -1,5 +1,25 @@
-class Car{
+class Car {
+  #name;
+  #goCount;
 
+  constructor() {
+    this.#name = "";
+    this.#goCount = 0;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  set name(input) {
+    const nameLength = input.length;
+
+    if (nameLength >= 1 && nameLength <= 5) {
+      this.#name = input;
+    } else {
+      throw new Error('[ERROR] 1자 이상 5자 이하의 문자가 입력되지 않음');
+    }
+  }
 }
 
 export default Car;
