d18fc13830839be3259293f134f378480b617414
lottt105 <hyobin0601@naver.com>
2023-10-31 15:02:15 +0900

    Feat: 자동차 이름 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 class App {
-  async play() {}
+  async play() {
+    const CARS = await this.getNameInput();
+  }
+
+  async getNameInput() {
+    let names = await Console.readLineAsync(MESSAGES.writeNames);
+    names = names.split(',');
+    this.checkNameInput(names);
+    return names;
+  }
+
+  checkNameInput(names) {
+    names.forEach((name) => {
+      if(name.length > 5) throw new Error(MESSAGES.nameLengthError);
+    });
+  }
 }
 
 export default App;
---
+++ src/const/messages.js
@@ -0,0 +1,10 @@
+export const MESSAGES = {
+  writeNames: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+  writeCount: '시도할 횟수는 몇 회인가요?\n',
+
+  result: '실행 결과',
+  finalWinner: '최종 우승자 : ',
+
+  nameLengthError: '[ERROR] 자동차 이름은 5자 이하만 가능합니다.',
+  numberError: '[ERROR] 숫자가 잘못된 형식입니다.',
+}
