177ae54e04969218b8c136c7901509936f947d27
bini59 <myuni0234@gmail.com>
2023-10-31 19:04:46 +0900

    feat(input) : 자동차 이름 입력 기능 구현
    - 자동차 이름을 입력받는 getCarName 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from '@woowacourse/mission-utils';
+import { getCarName } from './View.js';
+
 class App {
-  async play() {}
+  async play() {
+    await getCarName();
+
+    return this;
+  }
 }
 
 export default App;
---
+++ src/View.js
@@ -0,0 +1,9 @@
+import { Console } from '@woowacourse/mission-utils';
+import carValidate from './validation/Car.js';
+import carMessages from './messages/Car.js';
+
+export const getCarName = async () => {
+  const carName = await Console.readLineAsync(carMessages.INPUT);
+  const carNames = carName.split(',');
+  return carNames;
+};
