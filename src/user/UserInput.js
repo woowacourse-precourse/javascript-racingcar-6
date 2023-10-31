9362fd1d564fe195bd57820e00a39490e22713bc
future4ing <mirae4120@gmail.com>
2023-10-31 23:19:28 +0900

    feat : 자동차 이름 분리 및 사용자에게 시도할 횟수 입력 받는 기능 구현
    - 자동차 이름을 쉼표 기준으로 분리함
    - 사용자에게 시도할 횟수를 입력받음

--- src/user/UserInput.js
+++ src/user/UserInput.js
@@ -1,12 +1,17 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { Console } from "@woowacourse/mission-utils";
 
 class UserInput {
-
-    // 자동차 이름 입력 메소드
     async getCarNames() {
-        
-        const carNames = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        const carNames = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+
         return carNames.split(",");
+
+    }
+
+    async getMoveCount() {
+        const moveCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+
+        return moveCount;
     }
 }
 
