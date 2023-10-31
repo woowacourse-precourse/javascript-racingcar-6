a4240ee533e38dda857cc9b588eb9abd8ce1febf
okxooxoo <okxooxoo@naver.com>
2023-11-01 08:29:06 +0900

    ✅ test: 기능 ➌ 자동차 전진에 대한 테스트 코드 작성

---
+++ __tests__/MoveTest.js
@@ -0,0 +1,41 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import OutputView from "../src/view/OutputView.js";
+import Cars from "../src/app/Cars.js";
+
+const mockRandoms = (numbers) => {
+    MissionUtils.Random.pickNumberInRange = jest.fn();
+    numbers.reduce((acc, number) => {
+        return acc.mockReturnValueOnce(number);
+    }, MissionUtils.Random.pickNumberInRange);
+};
+
+const getLogSpy = () => {
+    const logSpy = jest.spyOn(MissionUtils.Console, "print");
+    logSpy.mockClear();
+    return logSpy;
+};
+
+describe("자동차 전진", () => {
+    test("전진-정지", async () => {
+        const carNames = ["poby", "seoro"];
+        const tryNumber = 2;
+        const randoms = [3, 4, 5, 6];
+        const outputs = ["poby : ", "seoro : -", "poby : -", "seoro : --"];
+        const logSpy = getLogSpy();
+
+        mockRandoms([...randoms]); // 랜덤으로 숫자가 생성된 것처럼 테스트
+
+        // when
+        const cars = new Cars(carNames);
+
+        for (var i = 0; i < tryNumber; i++) {
+            cars.moveCars()
+            OutputView.printCars(cars.names, cars.distances);
+        }
+
+        // then
+        outputs.forEach((output) => {
+            expect(logSpy).toHaveBeenCalledWith(output);
+        });
+    })
+})
--- src/app/App.js
+++ src/app/App.js
@@ -5,17 +5,21 @@ import OutputView from '../view/OutputView.js';
 
 class App {
 	async play() {
+		// 기능 ➊ 사용자에게 경주할 자동차 이름을 입력받는다.
 		const carNames = await InputView.getCarNames();
+		// 기능 ➋ 사용자에게 시도할 횟수를 입력받는다.
 		const tryNumber = await InputView.getTryNumber();
 
+		// 기능 ➌ 시도 횟수만큼 자동차를 전진시킨다.
 		const cars = new Cars(carNames);
-
+		
 		Console.print("\n실행 결과");
 		for (var i = 0; i < tryNumber; i++) {
 			cars.moveCars()
 			OutputView.printCars(cars.names, cars.distances);
 		}
 
+		// 기능 ➍ 최종 우승자를 출력한다.
 		const winners = cars.getWinners()
 		OutputView.printWinners(winners);
 	}
