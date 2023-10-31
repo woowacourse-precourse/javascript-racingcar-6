dc6b637f127de1f8a3637a72e317a8ddd65c2d26
pjc0179 <pjc0179@gmail.com>
2023-10-31 22:00:35 +0900

    refactor(utils): checkString.js생성
    - 기존 RaceController에서 관리하던 유저 자동차 이름 입력값에 대한 체크를 하던 static 메서드들을 재사용성을 위해 네이밍과 인자받는 방식을 변경하여 이곳에 저장

---
+++ src/utils/checkString.js
@@ -0,0 +1,19 @@
+const checkString = {
+  checkListValueHasSpace(list) {
+    return list.filter((value) => value.match(/\s/g)).length > 0;
+  },
+
+  checkListSameValue(list) {
+    return Array.from(new Set(list.map((car) => car))).length !== list.length;
+  },
+
+  checkListItemLongerThan(list, maxLength) {
+    return list.filter((value) => value.length > maxLength).length > 0;
+  },
+
+  checkListHasVoid(list) {
+    return list.filter((value) => value === '').length > 0;
+  },
+};
+
+export default checkString;
