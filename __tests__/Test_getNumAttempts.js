22c116e18ce32d85d0ad1e28fd0a11d5c42383c9
hj7321 <oboki2001@naver.com>
2023-11-01 08:46:31 +0900

    test: Test_getNumAttempts 테스트 코드 필요없는 부분 삭제

--- __tests__/Test_getNumAttempts.js
+++ __tests__/Test_getNumAttempts.js
@@ -1,14 +1,6 @@
 import App from "../src/App.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
 
-const mockQuestions = (inputs) => {
-  MissionUtils.Console.readLineAsync = jest.fn();
-  MissionUtils.Console.readLineAsync.mockImplementation(() => {
-    const input = inputs.shift();
-    return Promise.resolve(input);
-  });
-};
-
 describe("getNumAttempts 메서드 테스트", () => {
   beforeAll(() => {
     MissionUtils.Console.readLineAsync = jest.fn();
