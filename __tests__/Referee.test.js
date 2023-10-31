f1198afbce38aa56487a80d7e8030aaeac1fb818
uraflower <82873315+uraflower@users.noreply.github.com>
2023-10-31 19:26:54 +0900

    test: describe로 경주 우승자 판정 테스트 묶음

--- __tests__/Referee.test.js
+++ __tests__/Referee.test.js
@@ -8,18 +8,19 @@ Car.mockImplementation((name, movement) => ({
   move: jest.fn(),
 }));
 
-test('경주가 끝난 후, 가장 많이 전진한 자동차(들)을 반환한다.', () => {
-  const pobi = Car('pobi', '-');
-  const woni = Car('woni', '---');
-  const jun = Car('jun', '---');
-  const inputs = [pobi, woni, jun];
-  const expected = [woni, jun];
+describe('경주 우승자 판정', () => {
+  test('경주가 끝난 후, 가장 많이 전진한 자동차(들)을 반환한다.', () => {
+    const pobi = Car('pobi', '-');
+    const woni = Car('woni', '---');
+    const jun = Car('jun', '---');
+    const inputs = [pobi, woni, jun];
+    const expected = [woni, jun];
 
-  const outputs = Referee.decideWinners(inputs);
+    const outputs = Referee.decideWinners(inputs);
 
-  expect(outputs).toStrictEqual(expected);
+    expect(outputs).toStrictEqual(expected);
+  });
 });
-
 describe('경기 무효 판정', () => {
   test('전진한 자동차가 없으면, false를 반환한다.', () => {
     const input = 0;
