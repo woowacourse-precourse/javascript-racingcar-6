7e2e914a3624a1047cc761e70be0229304940217
hojkim77 <rlaghwnd4534@gmail.com>
2023-10-31 15:02:53 +0900

    [refactor] Jest사용을 위한 private 삭제

--- src/App.js
+++ src/App.js
@@ -15,17 +15,17 @@ class App {
 
   async play() {
     //* 자동차 이름들을 입력받습니다.
-    const carNames = await this.#inputCarNames();
+    const carNames = await this.inputCarNames();
     this.cars = carNames.map((carName) => new Car(carName));
 
     //* 시도할 횟수를 입력받습니다.
-    this.tryNum = await this.#inputTryNum();
+    this.tryNum = await this.inputTryNum();
 
     //* 경주를 실행하고 이에 대한 결과를 출력합니다.
-    this.#playRacing();
+    this.playRacing();
 
     //* 우승자를 생성합니다.
-    const winners = this.#winnerGenerator().map((winner) => winner.name);
+    const winners = this.winnerGenerator().map((winner) => winner.name);
 
     //* 우승자를 출력합니다.
     Output.printWinners(winners);
@@ -35,7 +35,7 @@ class App {
    * 입력받은 자동차 이름의 string 배열 타입
    * @returns {[string]}
    */
-  async #inputCarNames() {
+  async inputCarNames() {
     const carNamesInput = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
     );
@@ -48,7 +48,7 @@ class App {
    * 시도 횟수의 number 타입
    * @returns {number}
    */
-  async #inputTryNum() {
+  async inputTryNum() {
     const tryNumInput = await Console.readLineAsync(
       '시도할 횟수는 몇 회인가요?\n'
     );
@@ -56,7 +56,7 @@ class App {
     return validatedTryNum;
   }
 
-  #playRacing() {
+  playRacing() {
     Console.print('\n실행 결과');
 
     while (this.tryNum > 0) {
@@ -75,7 +75,7 @@ class App {
    * 우승한 자동차의 Car 배열 타입
    * @returns {[Car]}
    */
-  #winnerGenerator() {
+  winnerGenerator() {
     const maxDistance = Math.max(...this.cars.map((car) => car.distance));
     const winners = this.cars.filter((car) => car.distance === maxDistance);
 
