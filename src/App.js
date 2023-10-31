e9eb80fca52b16fe8a52e39799ae562b5c8e313f
yura <yuraa0302@gmail.com>
2023-10-31 16:37:54 +0900

    feat : 경주 할 자동차 이름 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import Validation from "./Validation";
+import CarRacingGame from "./CarRacingGame";
+import Car from "./Car";
+
 class App {
-  async play() {}
+  /**@constructor */
+  constructor() {
+    /**@type {CarRacingGame} */
+    this.carRacingGame = new CarRacingGame();
+  }
+  async play() {
+    await this.readCarNames();
+  }
+
+  async readCarNames() {
+    const answer = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    const cars = answer.split(",");
+    cars.forEach((car) => {
+      Validation.validationCarName(car);
+    });
+    Validation.validationDuplicateCarName(cars);
+    this.carRacingGame.setCars(cars.map((car) => new Car(car)));
+  }
 }
 
 export default App;
