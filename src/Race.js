2287859ae6d7f47228f0c94c05e7c79d693a96e3
sayyyho <323psh@naver.com>
2023-10-31 17:23:58 +0900

    feat: 시도 횟수 입력 시 예외처리

--- src/Race.js
+++ src/Race.js
@@ -12,9 +12,18 @@ class Race {
   }
 
   async setTrials() {
-    const trials = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    const trials = parseInt(
+      await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n'),
+      10,
+    );
+    if (
+      Number.isNaN(trials) ||
+      trials < 1 ||
+      trials > Number.MAX_SAFE_INTEGER
+    ) {
+      throw new Error('[ERROR] 유효하지 않은 횟수 입력입니다.');
+    }
     this.#trials = trials;
-    Console.print(this.#trials);
   }
 }
 
