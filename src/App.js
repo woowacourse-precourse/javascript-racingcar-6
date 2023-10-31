ed5f96b7b289f9482e0e78e2d2880b5ddab02268
Hamin <ohm55545@naver.com>
2023-10-31 23:27:40 +0900

    feat(getInput) : get input event
    get input data and throw error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import getInput from './components/getInput.js'
+
 class App {
-  async play() {}
+  async play() {
+    const inputNames = await getInput();
+    MissionUtils.Console.print(inputNames)
+  }
 }
 
 export default App;
