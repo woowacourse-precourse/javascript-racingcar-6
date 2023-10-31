1f2e6845e7a28d78fcea77c45a68bd28d865439d
Trophy198 <spsp8426@naver.com>
2023-10-31 17:06:20 +0900

    refactor: RacingGameController에서 Race 클래스 초기화와 실행 부분 분리

--- src/controller/RacingGameController.js
+++ src/controller/RacingGameController.js
@@ -5,12 +5,13 @@ import Car from '../model/Car';
 import Race from '../model/Race';
 import InputValidator from '../utils/InputValidator';
 import processCarNames from '../utils/CarNameProcessor';
+import { GameSettings } from '../constants/GameSettings';
 
 export default class RacingGameController {
   async start() {
     const carNames = await this.#createCarNames();
     const rounds = await this.#createRounds();
-    const race = this.#initializeRace(carNames, rounds);
+    const race = this.#initializeRace(carNames);
 
     this.#runRace(race, rounds);
     this.#displayWinners(race);
@@ -32,18 +33,29 @@ export default class RacingGameController {
     return rounds;
   }
 
-  #initializeRace(carNames, rounds) {
+  #initializeRace(carNames) {
     const cars = carNames.map((name) => new Car(name, randomMovementStrategy));
-    return new Race(cars, rounds);
+    return new Race(cars);
   }
 
   #runRace(race, rounds) {
     OutputView.printRaceHeader();
+    this.#executeRounds(race, rounds);
+  }
+
+  #executeRounds(race, rounds) {
     Array.from({ length: rounds }).forEach(() => {
       race.playRound();
-      const roundResults = race.getRoundResults();
+      const roundResults = this.#createRoundResults(race);
       OutputView.printRoundResult(roundResults);
     });
+  }
+
+  #createRoundResults(race) {
+    return race.getRoundResults().map((result) => ({
+      name: result.name,
+      representation: GameSettings.FORWARD_MOVE_REPRESENTATION.repeat(result.position),
+    }));
   }
 
   #displayWinners(race) {
