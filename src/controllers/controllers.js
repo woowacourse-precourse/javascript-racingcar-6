20d8ad5134503260b5e9e984981d330eedf73649
pongpongie <arevolvera@gmail.com>
2023-11-01 03:28:17 +0900

    feat(controllers): add newRaceCar function to Controller.js
    added newRaceCar function to make object of Racecar to the controller

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,6 +1,15 @@
 import { Console } from "@woowacourse/mission-utils";
+// models
 import RaceCar from "../models/RaceCar";
 
-function compareNumbers() {
-  const racecar = new RaceCar();
+// views
+import InputCarNameView from "../views/InputCarNameView";
+import RoundResultView from "../views/RoundResultView";
+import InputTrialNumberView from "../views/InputTrialNumberView";
+
+export class Controller {
+  newRaceCar() {
+    const racecar = new RaceCar();
+    racecar.name = InputCarNameView;
+  }
 }
