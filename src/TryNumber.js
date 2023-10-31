1663a82ffcf265b1070c32a54b7c7f150351a14e
byunjiin <jibyun5808@naver.com>
2023-10-31 19:08:39 +0900

    feat: 기능2. 시도할 횟수 수정

--- src/TryNumber.js
+++ src/TryNumber.js
@@ -1,7 +1,6 @@
 //기능2. 시도할 횟수
 export function trynumber(input) {
   if (parseInt(input) > 0) {
-    // 0보다 큰 정수형
     return parseInt(input);
   } else {
     throw new Error('[ERROR] 올바른 숫자를 입력해주세요');
