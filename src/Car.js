25389697cfd09fb6ecebfe7c28bdfb688d9461d3
Sunghyun627 <kksscc4@gmail.com>
2023-11-01 00:27:42 +0900

    feat: 0에서 9사이의 무작위값을 생성하는 기능 구현

--- src/Car.js
+++ src/Car.js
@@ -1,7 +1,13 @@
+import { Random } from '@woowacourse/mission-utils';
 class Car {
   constructor(name) {
     this.name = name;
     this.moveCount = 0;
+    this.randomNumber = -1;
+  }
+
+  async generateRandomNumber() {
+    return await Random.pickNumberInRange(0, 9);
   }
 }
 
