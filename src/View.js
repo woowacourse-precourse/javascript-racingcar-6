e8cc065b33495c6e591c2519a995703c0de18bca
bl0ckp1n9 <blockp1n9@gmail.com>
2023-11-01 00:03:05 +0900

    feat: MESSAGE에 개행 추가

--- src/View.js
+++ src/View.js
@@ -1,11 +1,12 @@
 import { Console } from '@woowacourse/mission-utils';
 
-const MESSAGE = {
+export const MESSAGE = {
   INPUT_TRY_COUNT: '시도할 횟수는 몇 회인가요?\n',
   INPUT_CAR_NAMES:
     '경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)\n',
-  OUTPUT_WINNER: winners => `${winners.join(', ')}가 최종 우승했습니다.`,
   OUTPUT_ERROR: '[ERROR] 자동차 이름은 5자 이하만 가능합니다.',
+  OUTPUT_EXECUTION_RESULT: '\n실행 결과',
+  OUTPUT_WINNER: winners => `${winners.join(', ')}가 최종 우승했습니다.\n`,
   OUTPUT_RACING_STATUS: racer => `${racer.carName} : ${'-'.repeat(racer.move)}`,
 };
 const inputCarNames = async () => {
