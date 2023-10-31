b518b614398c1584af3a3ea2e030aa9985aca134
baegyeong <beth0929@naver.com>
2023-11-01 06:36:38 +0900

    feat(Forward): 전진과 관련된 생성자, 메서드 분리
    - 랜덤 값에 따른 전진값 설정(forwardOrStop)
    - 자동차들의 전진 값들을 저장할 배열 설정(createRacingArray)
    - 시도횟수마다 전진 값들을 업데이트 하는 메서드 구현(racingUpdate)
    - 자동차에 따른 전진 현황을 출력하는 메서드 구현(showCarAndRacing)
    - 가장 기본적인 사이클을 수행하는 메서드 구현(oneCycleRacing)

---
+++ src/Forward.js
@@ -0,0 +1,47 @@
+import { Console } from "@woowacourse/mission-utils";
+import { GAME_HELP } from "../constants/GAME_HELP.js";
+import Car from "./Car.js";
+import TryCount from "./TryCount.js";
+import RandomNumber from "./RandomNumber.js";
+
+class Forward {
+  constructor(car, tryCount, randomNumber) {
+    this.car = car;
+    this.tryCount = tryCount;
+    this.randomNumber = randomNumber;
+    this.racingArray = [];
+  }
+
+  forwardOrStop() {
+    return this.randomNumber.getRandomNumber() > 3
+      ? GAME_HELP.FORWARD
+      : GAME_HELP.STOP;
+  }
+
+  createRacingArray() {
+    const carCount = this.car.carNameList.length;
+    this.racingArray = Array.from({ length: carCount }, () => "");
+    return this.racingArray;
+  }
+
+  racingUpdate(carIndex) {
+    return (this.racingArray[carIndex] += this.forwardOrStop());
+  }
+
+  showCarAndRacing(carIndex) {
+    Console.print(
+      `${this.car.carNameList[carIndex]} : ${this.racingArray[carIndex]}`
+    );
+  }
+
+  async oneCycleRacing() {
+    const carCount = this.car.carNameList.length;
+    for (let i = 0; i < carCount; i++) {
+      await this.racingUpdate(i);
+      await this.showCarAndRacing(i);
+    }
+    Console.print(" ");
+  }
+}
+
+export default Forward;
