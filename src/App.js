9a96dc4aa9adca644c09f42729ff28783e2de6e1
Baek <woonn97@gmail.com>
2023-10-31 18:06:06 +0900

    feat: 게임결과 화면에 출력
    * printResult() 메서드 추가하여 화면에 게임 실행 결과 출력

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ class App {
     this.cars = [];
   }
   async play() {
-    const nameInput = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    const nameInput = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
     this.cars = nameInput.split(",").map((name) => ({ name, position: 0 }));
-    const tryCount = await Console.readLineAsync("시도할 회수는 몇회인가요?");
+    const tryCount = await Console.readLineAsync("시도할 회수는 몇회인가요?\n");
     for (let i = 0; i < tryCount; i++) {
       this.randomMove();
-      Console.print(this.cars);
+      this.printResult();
     }
+  }
+  printResult() {
+    this.cars.forEach((car) => {
+      Console.print(`${car.name}: ${"-".repeat(car.position)}`);
+    });
   }
   randomMove() {
     this.cars.forEach((car) => {
