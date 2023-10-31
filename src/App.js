e9f39f836ce85db4edbfbdc5d1c74ee32e4a07cc
nari <naringst@gmail.com>
2023-10-31 12:17:43 +0900

    feat : 자동차 이름 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    //자동차 이름 입력받기
+    const carNameUserInput = await MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+  }
 }
 
 export default App;
