47372a289955f589155a7c140b72fa074c1d0e11
김다인 <dbslzhs1218@naver.com>
2023-11-01 04:50:34 +0900

    test: 이름 입력 예외 처리 테스트 추가
    빈 값, 5자 이상, 숫자, 특수문자, 공백이 포함된 문자를 입력했을 때
    예외가 발생되는지 테스트

--- __tests__/CreateCarNameTest.js
+++ __tests__/CreateCarNameTest.js
@@ -16,22 +16,32 @@ describe('CreateCarName 클래스 테스트', () => {
 
   describe('이름 입력 테스트', () => {
     test('인자로 넣은 입력 값 출력 테스트', () => {
-      // given
       const input = 'kim,lee,park';
       const output = 'kim,lee,park';
       const logSpy = getLogSpy();
 
-      // when
       namingCar.carName(input);
 
-      // then
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
     });
   });
-});
 
-describe('이름 입력 예외 처리 테스트', () => {
-  test('빈 값을 입력했을 때', () => {});
-  test('5자 이상의 이름을 입력했을 때', () => {});
-  test('숫자,특수 문자, 공백을 입력했을 때', () => {});
+  describe('이름 입력 예외 처리 테스트', () => {
+    test('빈 값, 5자 이상, 숫자, 특수문자, 공백을 입력했을 때', () => {
+      const inputs = [
+        '',
+        'redcar,blue',
+        1,
+        'red-car',
+        'red, blue',
+        '$$',
+        'red car',
+      ];
+      const input = inputs[Math.floor(Math.random() * inputs.length)];
+
+      expect(() => {
+        namingCar.wrongName(input);
+      }).toThrow('[ERROR]');
+    });
+  });
 });
--- src/CreateCarName.js
+++ src/CreateCarName.js
@@ -25,16 +25,16 @@ class CreateCarName {
   wrongName(inputNameElement) {
     if (inputNameElement === '') {
       throw new Error('[ERROR] 값이 입력되지 않았습니다.');
-    }
+    };
 
     if (inputNameElement.length > 5) {
       throw new Error('[ERROR] 이름은 5자 이하로 입력해주세요.');
-    }
+    };
 
     if (/[^a-z]/i.test(inputNameElement)) {
       throw new Error('[ERROR] 이름을 제대로 입력해주세요.');
-    }
-
+    };
+    
     return this.carNameArr.push(inputNameElement);
   }
 }
