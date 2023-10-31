174cb450ca153d4f700cbcf3253939fb02ed8d74
한지은 <hjn510450@gmail.com>
2023-10-31 19:27:33 +0900

    우승자 출력 함수 수정(게임 메시지 상수 처리, 최대 이동 횟수 구하는 알고리즘 변경)

--- src/utils/PrintWinner.js
+++ src/utils/PrintWinner.js
@@ -1,21 +1,18 @@
 import { Console } from '@woowacourse/mission-utils';
+import { GAME_MESSAGE } from '../constants/messages';
 
 /**
  * @description 가장 높은 이동 횟수를 가진 자동차를 우승자로 출력하는 함수
  * @param {Array} carNameArr 자동차 이름 배열
  * @param {Array} moveCount 자동차 별 이동 횟수
  */
-export default async function PrintWinner(carNameArr, moveCount) {
-    const maxMoveCount = Math.max(...moveCount);
-    const maxMoveCountIdxArr = [];
+export default async function printWinner(carNameArr, moveCounts) {
+  const maxMoveCount = Math.max(...moveCounts);
 
-    while(true) {
-        const maxMoveCountIdx = moveCount.indexOf(maxMoveCount);
-        if(maxMoveCountIdx === -1) break;
-        maxMoveCountIdxArr.push(maxMoveCountIdx);
-        moveCount[maxMoveCountIdx] = -1;
-    }
+  const maxMoveCountIdx = moveCounts
+    .map((moveCount, index) => moveCount === maxMoveCount ? index : -1)
+    .filter((index) => index !== -1);
 
-    const winnerName = maxMoveCountIdxArr.map((maxMoveCountIdx) => carNameArr[maxMoveCountIdx]).join(', ');
-    Console.print(`최종 우승자 : ${winnerName}`)
+  const winnerName = maxMoveCountIdx.map((idx) => carNameArr[idx]).join(', ');
+  Console.print(`${GAME_MESSAGE.gameWinner} ${winnerName}`)
 }
