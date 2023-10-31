52bb94a40d70f76deb77b7ce247816c0c0cad6eb
sihyonn <sihyeon982@naver.com>
2023-11-01 00:17:14 +0900

    test: 우승자 선정에 관한 테스트 추가

---
+++ __tests__/AdditionalTest.js
@@ -0,0 +1,30 @@
+import selectWinner from '../src/game/Winner.js';
+import { printWinner } from '../src/utils/Output.js';
+
+jest.mock('../src/utils/Output.js', () => ({
+  printWinner: jest.fn(),
+}));
+
+describe('우승자 선정', () => {
+  let carsPosition;
+
+  // 테스트 전 초기화
+  beforeEach(() => {
+    carsPosition = {
+      car1: 5,
+      car2: 3,
+      car3: 5,
+      car4: 2,
+    };
+  });
+
+  // 테스트 이후 모든 호출된 모의 함수 초기화
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('가장 많이간 차량 = 승자', () => {
+    selectWinner(carsPosition);
+    expect(printWinner).toHaveBeenCalledWith(['car1', 'car3']);
+  });
+});
