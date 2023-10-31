5d8ba8ccc603f84509f6336166f393c55c42818e
rabbit-22 <chlwlal20@gmail.com>
2023-11-01 02:32:40 +0900

    feat: 게임 진행 코드 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import RacingGame from "./RacingGame.js";
+import UserInterface from "./UserInterface.js";
+
 class App {
-  async play() {}
+  async play() {
+    try {
+      const carNames = await UserInterface.getCarNames();
+      const raceRounds = await UserInterface.getRaceRounds();
+      const game = new RacingGame(carNames, raceRounds);
+      game.start();
+      const winners = game.getWinners();
+      UserInterface.printWinners(winners);
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
 }
 
 export default App;
