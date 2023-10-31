7326f28bfc02581f2ef3b2c4b31174891e85bc05
yura <yuraa0302@gmail.com>
2023-10-31 16:18:25 +0900

    feat : 경주할 자동차의 이름 중복 여부 검증

--- src/Validation.js
+++ src/Validation.js
@@ -1,5 +1,13 @@
 const Validation = {
-  /** @param {string} */
+  /**@param {string[]}names */
+  validationDuplicateCarName: (names) => {
+    const set = new Set(names);
+    if (set.size !== names.length) {
+      throw new Error("[ERROR] 중복된 자동차 이름이 존재합니다.");
+    }
+  },
+
+  /** @param {string} name*/
   validationCarName: (name) => {
     if (name.length < 1 || name.length > 5) {
       throw new Error("[ERROR] 1~5 자의 자동차 이름을 입력해 주세요.");
