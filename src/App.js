64d689d23e1cf20ac35c50bf8a75eb45ad576853
unknown <pih9134@naver.com>
2023-11-01 03:11:45 +0900

    refactor: RunRace()분리

--- src/App.js
+++ src/App.js
@@ -10,42 +10,22 @@
 
 import { Console, Random } from '@woowacourse/mission-utils';
 import Inputs from './input/Inputs';
+import RunRace from './runRace/RunRace';
 
 class App {
   constructor() {
-    this.cars = [];
-    this.tryCount = 0;
+    // this.cars = [];
+    // this.tryCount = 0;
     this.inputs = new Inputs();
   }
 
   async play() {
-    this.cars = await this.inputs.returnCarNames();
-    this.tryCount = await this.inputs.getTryCount();
+    const cars = await this.inputs.returnCarNames();
+    const tryCount = await this.inputs.getTryCount();
     Console.print('\n실행 결과');
-    this.runRace(cars, tryCount);
-  }
-
-  isInvalidName(name) {
-    return name.length > 5 || !name;
-  }
-
-  async returnCarNames() {
-    const carNames = await Console.readLineAsync(
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
-    );
-
-    const names = carNames.split(',').map((name) => name.trim());
-
-    if (names.some(this.isInvalidName)) {
-      throw new Error('[ERROR] 잘못된 입력입니다.');
-    }
-
-    return names;
-  }
-
-  async getTryCount() {
-    const tryCount = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
-    return parseInt(tryCount, 10);
+    const progressList = new RunRace().runRace(cars, tryCount);
+    this.printWinners(cars, progressList);
+    // this.runRace(cars, tryCount);
   }
 
   runRace(cars, tryCount) {
@@ -62,9 +42,6 @@ class App {
     this.printWinners(cars, progressList);
   }
 
-  printWinners = (cars, progressList) =>
-    Console.print(`최종 우승자 : ${this.findWinners(cars, progressList).join(', ')}`);
-
   getRandomMove = () => Random.pickNumberInRange(0, 9);
 
   generateRandomProgress = () => (this.getRandomMove() >= 4 ? '-' : '');
@@ -73,6 +50,9 @@ class App {
     cars.filter(
       (car, carIndex) => progressList[carIndex].length === this.maxProgress(progressList),
     );
+
+  printWinners = (cars, progressList) =>
+    Console.print(`최종 우승자 : ${this.findWinners(cars, progressList).join(', ')}`);
 
   maxProgress = (progressList) => Math.max(...progressList.map((progress) => progress.length));
 }
