a71a173b988503d79eea0be1128a325b638c87aa
ryan-dia <cjfdnjs158@naver.com>
2023-10-31 23:34:02 +0900

    test(validator): 잘못 표기한 input 수정

--- __tests__/Validators.test.js
+++ __tests__/Validators.test.js
@@ -2,16 +2,14 @@ import Validators from '../src/utils/validator/index.js';
 
 describe('Validators test', () => {
   describe('checkRacingVehicleName', () => {
-    test.each([
-      ['aa,bb,cc,dd'],
-      ['도레,미파,솔라,시도'],
-      ['도a,레b,미c'],
-      ['airbnb', 'where', 'tomato'],
-    ])(' %s을 입력하였을 때 에러가 발생하지 않는다.', (input) => {
-      expect(() => {
-        Validators.checkRacingVehicleName(input);
-      }).not.toThrow();
-    });
+    test.each([['aa,bb,cc,dd'], ['도레,미파,솔라,시도'], ['도a,레b,미c'], ['abcde,where,tomma']])(
+      ' %s을 입력하였을 때 에러가 발생하지 않는다.',
+      (input) => {
+        expect(() => {
+          Validators.checkRacingVehicleName(input);
+        }).not.toThrow();
+      },
+    );
 
     test.each([
       ['23,45,23'],
