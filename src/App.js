34a6a369c2fbb708d3850b7168de9394452b44f3
Han <mayo3610@naver.com>
2023-10-31 13:52:46 +0900

    feat #2 : 사용자에게 레이싱 횟수 입력받기

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import { Console } from "@woowacourse/mission-utils";
 class App {
   constructor() {
     this.name = "";
+    this.turn = 0;
   }
   async readUserRacingCars() {
     await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     ).then((input) => (this.name = input));
   }
+  async readUserRacingTry() {
+    await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n").then(
+      (input) => (this.turn = input)
+    );
+  }
+
   async play() {
     await this.readUserRacingCars();
-    console.log(this.name);
+    await this.readUserRacingTry();
   }
 }
 const app = new App();
