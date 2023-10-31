14c5e542963293840165415452ac20c77d9ce217
yeonkyoung <keep.ykk@gmail.com>
2023-11-01 02:00:30 +0900

    feat: 문자열 상수화

---
+++ src/constants/messages.js
@@ -0,0 +1,14 @@
+export const START_GAME_MESSAGE =
+  "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+export const TRIAL_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+export const RESULT_MESSAGE = "실행 결과";
+export const FINAL_WINNER_MESSAGE = "최종 우승자";
+export const COLON = ":";
+export const CARS_NAME_LENGTH_USER_INPUT_ERROR =
+  "[ERROR] 1자 이상 5자 이하의 이름을 입력해주세요.";
+export const NOT_INCLUDED_COMMA_USER_INPUT_ERROR =
+  "[ERROR] 이름 쉼표(,)로 구분해서 입력해주세요.";
+// export const INPUT_ERROR = '[ERROR] '
+export const SAME_NAME_USER_INPUT_ERROR =
+  "[ERROR] 서로 다른 이름으로 입력해주세요.";
+export const NOT_NUMBER_TYPE_USER_INPUT_ERROR = "[ERROR] 숫자를 입력해주세요.";
