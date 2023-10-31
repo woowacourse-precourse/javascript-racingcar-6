2187cd3a36a58792a4a1d5843797856cf11d6220
csk6314 <37896060+csk6314@users.noreply.github.com>
2023-10-31 14:53:09 +0900

    feat: 라운드 진행 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+import { Console } from "@woowacourse/mission-utils";
+import Input from "./Input.js";
+import Car from "./Car.js";
+
 class App {
-  async play() {}
+  async initGame() {
+    const input = new Input();
+    // CarName 입력 -> Car 배열 생성
+    const carNames = await input.inputCarName();
+    const cars = [];
+    carNames.forEach((carName) => cars.push(new Car(carName)));
+    // 시도 횟수 입력
+    const tryNum = await input.inputTryNum();
+
+    this.roundStart(cars, tryNum);
+  }
+
+  roundStart(cars, tryNum) {
+    let round = 1;
+    Console.print("실행 결과");
+    while (round++ <= tryNum) {
+      this.gameRound(cars);
+    }
+  }
+
+  gameRound(cars) {
+    cars.forEach((car) => {
+      car.move();
+      Console.print(car + "");
+    });
+    Console.print("\n");
+  }
+
+  async gameFinished() {}
+
+  async play() {
+    await this.initGame();
+  }
 }
 
 export default App;
+
+const a = new App();
+a.play();
