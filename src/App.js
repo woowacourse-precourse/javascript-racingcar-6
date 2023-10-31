5c865a516035103864443800772a9563943ada63
ymj307 <dymj307@gmail.com>
2023-10-31 22:22:24 +0900

    자동차 이름 입력받기 및 입력값 유효성 검사 구현

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable class-methods-use-this */
+/* eslint-disable import/extensions */
 
-import { MissionUtils } from '@woowacourse/mission-utils';
+// import { MissionUtils } from '@woowacourse/mission-utils';
+// import { MissionUtils } from '@woowacourse/mission-utils';
+import Cars from './Cars.js';
 
 class App {
   async play() {
-    MissionUtils.Console.print('this is test right now. app.play method');
+    const cars = new Cars();
+    cars.getCarsNames();
   }
 }
 
 export default App;
-
-// MissionUtils.Console.print('this is test right now');
