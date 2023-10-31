7f32dd82611607bc299128de31c29e27b0a5296b
deeyo <130573068+jeehyojeon@users.noreply.github.com>
2023-10-31 18:24:38 +0900

    feat: validateCarNames

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Random, Console } from "@woowacourse/mission-utils";
 
+const CARNAME_LENGTH = 5;
+
 class App {
   constructor() {
     this.roundCount = null;
@@ -10,5 +12,15 @@ class App {
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
     const carNames = input.split(",");
-  }
+    this.validateCarNames(carNames);
+    }
+  
+  validateCarNames(carNames) {
+    for (let carName of carNames) {
+      if (carName.length > CARNAME_LENGTH) {
+        throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+      }
+    }
+  };
 }
+export default App;
