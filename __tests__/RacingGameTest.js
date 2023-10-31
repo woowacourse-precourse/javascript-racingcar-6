85df2f0d83125ae720f42dcfbc220f48cadf0693
Jeongeun Lee <leeju1009@gmail.com>
2023-10-31 15:36:43 +0900

    test: 게임 시도 횟수를 입력받는 함수의 return 타입 test 추가
    사용자에게 입력받은 게임 시도 횟수를 Number로 반환하는지 검사하는 test 추가

--- __tests__/RacingGameTest.js
+++ __tests__/RacingGameTest.js
@@ -1,5 +1,7 @@
 import { Console } from '@woowacourse/mission-utils';
 import RacingGame from '../src/RacingGame.js';
+import Validation from '../src/Validation.js';
+import ErrorHandler from '../src/ErrorHandler.js';
 
 const mockRacingCountInput = (racingCount) => {
   Console.readLineAsync = jest.fn();
@@ -8,3 +10,16 @@ const mockRacingCountInput = (racingCount) => {
     return Promise.resolve(racingCount);
   });
 };
+
+describe('레이싱 게임 플레이와 관련된 함수 테스트', () => {
+  const racingGame = new RacingGame();
+  const validation = new Validation();
+
+  test('게임 시도 횟수를 숫자로 변환하여 반환하는지 테스트', async () => {
+    const racingCountInput = '5';
+    mockRacingCountInput(racingCountInput);
+    const racingCount = await racingGame.getRacingCount();
+
+    expect(typeof racingCount).toBe('number');
+  });
+});
