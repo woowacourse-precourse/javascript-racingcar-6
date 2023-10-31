02d52ad9a346195e77dd42b9b1965ea0dce82f69
bmj0053 <bmj05378@gmail.com>
2023-10-31 21:47:54 +0900

    remove: 주석 제거

--- src/App.js
+++ src/App.js
@@ -18,13 +18,6 @@ class App {
       CheckLength(car)
     });
     
-    // const carList = cars.replace(/ /g, '').split(',').map(String); // 공백 제거 및 분리
-    // carList.map(car => {
-    //   if (car.length > 5) {
-    //     throw new Error('[ERROR] 자동차 이름이 5이상입니다.')
-    //   }
-    // });
-    
     let count = 0;
     let forwardCounts = [];
     carList.map(car => forwardCounts.push(0));
@@ -34,18 +27,10 @@ class App {
 
     while (count !== counts) {
       count++;
-    
       carList.map((car, idx) => {
         forwardCounts = CheckRandomNum(forwardCounts, idx)
         Console.print(car + ' : ' + '-'.repeat(forwardCounts[idx]));
       })
-      // carList.map((car, idx) => {
-      //   const randomNum = MissionUtils.Random.pickNumberInRange(0, 9);
-      //   if (randomNum >= 4) {
-      //     forwardCounts[idx]++
-      //   };
-      //   Console.print(car + ' : ' + '-'.repeat(forwardCounts[idx]));
-      // })
       Console.print('');
     }
     
@@ -53,19 +38,8 @@ class App {
     for (let i = 0; i < carList.length; i++) {
       maxForward = FindMax(maxForward, forwardCounts, i);
     };
-    // for (let i = 0; i < carList.length; i++) {
-    //   if (maxForward < forwardCounts[i]) {
-    //     maxForward = forwardCounts[i];
-    //   };
-    // };
 
     const winners = FindWinners(forwardCounts, maxForward, carList);
-    // for (let i = 0; i < forwardCounts.length; i++) {
-    //   if (forwardCounts[i] === maxForward) {
-    //     winners.push(carList[i]);
-    //   };
-    // };
-    
     Console.print('최종 우승자 : ' + winners);
   }
 };
