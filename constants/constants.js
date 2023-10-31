74ab01b938effd3b0a33999b70c136af1431a788
yeong-hwan <jghff700@naver.com>
2023-11-01 02:56:58 +0900

    refactor: 상수를 enum으로 관리하도록 변경

---
+++ constants/constants.js
@@ -0,0 +1,18 @@
+const SYMBOLS = {
+  movingDistanceSymbol: '-',
+  carNameSeperator: ',',
+  winnerNameSeperator: ','
+}
+
+const SETTING_NUMBERS = {
+  minRandomNum: 0,
+  maxRandomNum: 9,
+  moveThreshold: 4,
+  minCarNameLength: 1,
+  maxCarNameLength: 5
+}
+
+export {
+  SYMBOLS,
+  SETTING_NUMBERS
+}
