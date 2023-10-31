1e3301a9417e1aa5c1b27f47bc2a299e050040b6
sihyonn <sihyeon982@naver.com>
2023-10-31 18:05:12 +0900

    feat: 최종 우승자 출력 기능 추가

--- src/game/Winner.js
+++ src/game/Winner.js
@@ -1,9 +1,11 @@
 // 우승자 선정하기
 const selectWinner = (carsPosition) => {
   let maxPosition = Math.max(...Object.values(carsPosition));
-  let maxPositionCarName = Object.keys(carsPosition).filter(
+  let winner = Object.keys(carsPosition).filter(
     (car) => carsPosition[car] === maxPosition
   );
+
+  printWinner(winner);
 };
 
 export default selectWinner;
--- src/utils/Output.js
+++ src/utils/Output.js
@@ -5,4 +5,15 @@ const printRace = (name, position) => {
   Console.print(`${name} : ${'-'.repeat(position)}`);
 };
 
-export { printRace };
+// 최종 우승자 출력
+const printWinner = (winner) => {
+  if (winner.length <= 1) {
+    // 단독 우승
+    Console.print(`최종 우승자 : ${winner[0]}`);
+  } else {
+    // 공동 우승
+    Console.print(`최종 우승자 : ${winner.join(', ')}`);
+  }
+};
+
+export { printRace, printWinner };
