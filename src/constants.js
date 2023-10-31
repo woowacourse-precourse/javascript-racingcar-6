d2287e28767486a8935765bbb9523ce1217fd94f
joony <tjd1wns15@gmail.com>
2023-11-01 03:10:42 +0900

    Feat: 상수 추가

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const printMessages = {
+  GET_CAR_NAMES_MESSAGE:
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
+  GET_TRY_NUMBERS_MESSAGE: "시도할 횟수는 몇 회인가요?",
+  PRINT_RESULT: "실행 결과",
+  PRINT_WINNER: "최종 우승자 : ",
+};
+
+export const printErrorMessages = {
+  CAR_NAME_LENGTH_ERROR: "[ERROR] 자동차 이름은 5자 이하만 가능합니다.",
+  TRY_NUMBERS_ERROR: "[ERROR] 숫자가 잘못된 형식입니다.",
+};
