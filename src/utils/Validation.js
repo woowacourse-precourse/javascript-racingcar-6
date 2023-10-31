b49cfef5b8f246488f5193448399cd79714c35f4
sihyonn <sihyeon982@naver.com>
2023-10-31 15:20:23 +0900

    feat: 자동차 이름 입력 차량 수 유효성 검사 조건 추가

--- src/utils/Validation.js
+++ src/utils/Validation.js
@@ -4,8 +4,10 @@ import { getCarNames } from './Input.js';
 const validateCarNames = (names) => {
   names.forEach((name) => {
     if (name.length > 5)
-      throw new Error('[ERROR] 자동차 이름은 5자 이하로 입력해야합니다!');
+      throw new Error('[ERROR] 자동차 이름은 5자 이하로 입력해주세요!');
   });
+  if (names.length < 2)
+    throw new Error('[ERROR] 자동차는 두 대 이상 입력해주세요!');
 };
 
 export { validateCarNames };
