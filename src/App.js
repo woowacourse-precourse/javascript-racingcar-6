20ea3b48a05c1d6603cda85050b909fb5e312d06
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-10-31 21:07:35 +0900

    feat : 자동차 이름 입력에 대한 예외처리 추가
    validtate.carNames()
    예외처리에 걸리는 상황
    1. (,)로 자동차 이름을 구분하지 않았을 경우
    2. 자동차 이름에 공백이 들어간 경우
    3. 1명의 이름만 입력후 (,)를 찍은 경우 ex) pobi,
    4. 유저의 이름을 입력하지않은경우 ex) pobi,,woni
    5. 각 이름의 길이에서 하나라도 6이상의 이름을 입력한 경우
    6. 숫자,한글,영문 이외의 문자를 입력했을경우
    7. 10명이상의 인원을 입력했을 경우
    8. 중복된 이름이 있을경우
    9. 대소문자는 구분했지만 같은 문자인 경우 ex)Pobi,pobi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import { Console } from '@woowacourse/mission-utils';
 import Car from './Car.js';
 import RacingGame from './RacingGame.js';
 import InputReader from './View/InputReader.js';
 import OutputView from './View/OutputView.js';
-import { Console } from '@woowacourse/mission-utils';
 import { paramType } from './utils/paramType.js';
+import { validate } from './utils/validate.js';
 
 export default class App {
   #inputReader;
@@ -18,6 +19,7 @@ export default class App {
 
   async play() {
     const carNames = await this.#inputReader.carNames();
+    validate.carNames(carNames);
     const tryRound = Number(await this.#inputReader.tryRount());
 
     await this.ready(carNames, tryRound);
@@ -29,9 +31,6 @@ export default class App {
     _0 = paramType(carNames, 'string'),
     _1 = paramType(tryRound, 'number')
   ) {
-    if (carNames.split(',').some((carName) => carName.length > 5)) {
-      throw new Error('[ERROR] 이름은 5자 이하로 적어주세요 !');
-    }
     const carArray = carNames.split(',').map((name) => new Car(name));
 
     this.#racingGame = new RacingGame(carArray, tryRound);
@@ -40,6 +39,7 @@ export default class App {
   }
 
   racingStart() {
+    Console.print('실행 결과');
     while (!this.#racingGame.isFinish()) {
       this.#racingGame.roundStart();
       const roundResult = this.#racingGame.getRoundResult();
---
+++ src/utils/validate.js
@@ -0,0 +1,49 @@
+import { CarNamesError } from '../errors/UserInputErrors.js';
+import { paramType } from './paramType.js';
+
+export const validate = {
+  carNames(carNames, _ = paramType(carNames, 'string')) {
+    const COMMA = ',';
+    const carNamesArray = carNames.split(',');
+    const spaceRegExp = /\s/g;
+    const validCarNameTypeRegExp = /[a-zA-Z가-힣0-9,]/g;
+    const upperCaseFlattenArray = [...carNamesArray].map((carName) =>
+      carName.toUpperCase()
+    );
+    if (!carNames.includes(`${COMMA}`)) {
+      throw new CarNamesError('자동차 이름은 (,)로 구분해 입력해 주세요 !');
+    }
+    if (spaceRegExp.test(carNames)) {
+      throw new CarNamesError('자동차 이름에 공백을 넣지 말아주세요 !');
+    }
+    if (carNamesArray.length === 2 && carNames[carNames.length - 1] === ',') {
+      throw new CarNamesError('게임에 필요한 인원은 최소 2명 이상입니다 !');
+    }
+    if (!carNamesArray.every((carName) => carName.length > 0)) {
+      throw new CarNamesError(
+        '각 유저의 이름은 최소한 1글자 이상으로 입력해 주세요 !'
+      );
+    }
+    if (!carNamesArray.every((carName) => carName.length < 6)) {
+      throw new CarNamesError(
+        '각 이름의 길이가 5 이상인 이름이 있어요 ! 각각의 자동차 이름의 길이는 5이하로 입력해 주세요 !'
+      );
+    }
+    if (!validCarNameTypeRegExp.test(carNames)) {
+      throw new CarNamesError(
+        '자동차이름은 숫자, 영문, 한글만으로 만들어 주세요 !'
+      );
+    }
+    if (carNamesArray.length > 11) {
+      throw new CarNamesError('한 게임에 입장 가능 인원은 10명입니다 !');
+    }
+    if (new Set(carNamesArray).size !== carNamesArray.length) {
+      throw new CarNamesError('중복된 자동차 이름은 허용하지 않습니다 !');
+    }
+    if (new Set(upperCaseFlattenArray).size !== carNamesArray.length) {
+      throw new CarNamesError(
+        '대소문자를 입력할 수는 있지만 같은 문자일 경우 동일한 이름으로 간주합니다 ! 중복을 피해주세요'
+      );
+    }
+  },
+};
