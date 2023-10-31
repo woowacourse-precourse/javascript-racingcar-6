b12401d2d9710354f27b1f3ae2ce4e469413a63f
dante0214 <rn_chic@naver.com>
2023-10-31 15:00:43 +0900

    feat(App): 입력 구현
    입력기능 구현
    'getUserInput'으로 자동차 이름 받음
    'getPlayRound'로 총 라운드 받음

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async getUserInput() {
+    const userInput = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    const carNames = userInput.split(",");
+    return carNames;
+  }
+  async getPlayRound() {
+    const input = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+    const gameRound = Number(input);
+    return gameRound;
+  }
+  async play() {
+    const carNames = await this.getUserInput();
+    const gameRound = await this.getPlayRound();
+  }
 }
 
 export default App;
