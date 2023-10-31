db1dcd62d0076b5c06e77b5cca55a2da6784ceb7
Jeongeun Lee <leeju1009@gmail.com>
2023-11-01 00:22:16 +0900

    test: RacingGame 클래스 인스턴스에 필요한 인자 mocking

--- __tests__/RacingGameTest.js
+++ __tests__/RacingGameTest.js
@@ -1,6 +1,8 @@
 import { Console } from '@woowacourse/mission-utils';
 import RacingGame from '../src/RacingGame.js';
 import Validation from '../src/Validation.js';
+import Car from '../src/Car.js';
+import SettingCars from '../src/SettingCars.js';
 
 const mockRacingCountInput = (racingCount) => {
   Console.readLineAsync = jest.fn();
@@ -10,8 +12,19 @@ const mockRacingCountInput = (racingCount) => {
   });
 };
 
+const mockSettingCars = (carsNamesInput) => {
+  SettingCars.registerCars = jest.fn();
+
+  SettingCars.registerCars.mockImplementation(() => {
+    const carsNamesArray = carsNamesInput.split(',');
+    return carsNamesArray.forEach((name) => new Car(name));
+  });
+};
+
 describe('레이싱 게임 플레이와 관련된 함수 테스트', () => {
-  const racingGame = new RacingGame();
+  const racingGame = new RacingGame(
+    mockSettingCars('테스트1, 테스트2, 테스트3'),
+  );
 
   test('게임 시도 횟수를 숫자로 변환하여 반환하는지 테스트', async () => {
     const racingCountInput = '5';
