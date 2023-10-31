2dc214c81de2a3b2706d3cc19d92141f282f43e1
OAO <ashgrayblue0@gmail.com>
2023-11-01 00:07:24 +0900

    test(ComputerTest.js): test to 'pushCarList' method

--- __tests__/ComputerTest.js
+++ __tests__/ComputerTest.js
@@ -1,4 +1,11 @@
 import Computer from '../src/module/Computer.js';
+import Car from '../src/module/Car.js';
+
+function generateObject(input) {
+  const car = new Car();
+  car.name = input;
+  return car;
+}
 
 describe('Computer 클래스 테스트', () => {
   test('round의 getter, setter 테스트', () => {
@@ -10,4 +17,16 @@ describe('Computer 클래스 테스트', () => {
       computer.round = -1;
     }).toThrow('[ERROR]');
   });
+
+  test('pushCarList 테스트', () => {
+    const computer = new Computer();
+    const testArray = [];
+
+    for (let i = 1; i < 6; i++){
+      testArray.push(generateObject(i.toString()));
+    }
+
+    computer.pushCarList('1,2,3,4,5');
+    expect(computer.carList).toEqual(testArray);
+  })
 });
