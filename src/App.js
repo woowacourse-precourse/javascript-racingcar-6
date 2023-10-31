38f62dc1e5bcce0527b95de37b742547ecd4138f
조민기 <58856846+Nail0606@users.noreply.github.com>
2023-10-31 21:25:27 +0900

    Fix : 함수 클래스 메서드화

--- src/App.js
+++ src/App.js
@@ -9,85 +9,85 @@ const ERROR_MESSAGES = {
 
 class App {
   async play() {
-    const CAR_ARRAY = await inputCarNames();
-    const ATTEMPT_COUNT = +(await inputAttempt());
+    const CAR_ARRAY = await this.inputCarNames();
+    const ATTEMPT_COUNT = +(await this.inputAttempt());
 
-    checkCarName(CAR_ARRAY);
-    checkAttemptCount(ATTEMPT_COUNT);
+    this.checkCarName(CAR_ARRAY);
+    this.checkAttemptCount(ATTEMPT_COUNT);
 
-    const CAR_OBJECT_ARRAY = makeCarObject(CAR_ARRAY);
-    showRaceResults(CAR_OBJECT_ARRAY, ATTEMPT_COUNT);
-    showWinner(CAR_OBJECT_ARRAY);
+    const CAR_OBJECT_ARRAY = this.makeCarObject(CAR_ARRAY);
+    this.showRaceResults(CAR_OBJECT_ARRAY, ATTEMPT_COUNT);
+    this.showWinner(CAR_OBJECT_ARRAY);
   }
-}
 
-function showRaceResults(car_object_array, attempt_count) {
-  for (let i = 0; i < attempt_count; i++) {
-    car_object_array.forEach((car) => {
-      const RANDOMNUMBER = MissionUtils.Random.pickNumberInRange(0, 9);
-      if (RANDOMNUMBER >= 4) {
-        car.value++;
+  showRaceResults(car_object_array, attempt_count) {
+    for (let i = 0; i < attempt_count; i++) {
+      car_object_array.forEach((car) => {
+        const RANDOMNUMBER = MissionUtils.Random.pickNumberInRange(0, 9);
+        if (RANDOMNUMBER >= 4) {
+          car.value++;
+        }
+        let value = "-".repeat(car.value);
+        MissionUtils.Console.print(`${car.name} : ${value}`);
+      });
+      MissionUtils.Console.print("");
+    }
+  }
+
+  showWinner(car_object_array) {
+    const MAX_VALUE = car_object_array.reduce(
+      (max, current) => Math.max(max, current.value),
+      -Infinity
+    );
+    const WINNERS = car_object_array.filter((car) => car.value === MAX_VALUE);
+    MissionUtils.Console.print(
+      `최종 우승자 : ${WINNERS.map((car) => car.name).join(", ")}`
+    );
+  }
+
+  makeCarObject(CAR_ARRAY) {
+    //각 변수를 object화 해서 값을 1씩 증가시키는 방법이 있음.
+    const CAR_OBJECT_ARRAY = CAR_ARRAY.map((car) => ({ name: car, value: 0 }));
+    return CAR_OBJECT_ARRAY;
+  }
+
+  async inputCarNames() {
+    const CARS = await MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    const CAR_ARRAY = CARS.split(",");
+    return CAR_ARRAY;
+  }
+
+  inputAttempt() {
+    const ATTEMPT_COUNT =
+      MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+    return ATTEMPT_COUNT;
+  }
+
+  checkCarName(CAR_ARRAY) {
+    CAR_ARRAY.forEach((car) => {
+      if (car.length > 5) {
+        throw new Error(ERROR_MESSAGES.CAR_NAME_OVER);
       }
-      let value = "-".repeat(car.value);
-      MissionUtils.Console.print(`${car.name} : ${value}`);
+      if (car.length === 0) {
+        throw new Error(ERROR_MESSAGES.CAR_NAME_ZERO);
+      }
     });
-    MissionUtils.Console.print("");
+    return CAR_ARRAY;
   }
-}
 
-function showWinner(car_object_array) {
-  const MAX_VALUE = car_object_array.reduce(
-    (max, current) => Math.max(max, current.value),
-    -Infinity
-  );
-  const WINNERS = car_object_array.filter((car) => car.value === MAX_VALUE);
-  MissionUtils.Console.print(
-    `최종 우승자 : ${WINNERS.map((car) => car.name).join(", ")}`
-  );
-}
-
-function makeCarObject(CAR_ARRAY) {
-  //각 변수를 object화 해서 값을 1씩 증가시키는 방법이 있음.
-  const CAR_OBJECT_ARRAY = CAR_ARRAY.map((car) => ({ name: car, value: 0 }));
-  return CAR_OBJECT_ARRAY;
-}
-
-async function inputCarNames() {
-  const CARS = await MissionUtils.Console.readLineAsync(
-    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
-  );
-  const CAR_ARRAY = CARS.split(",");
-  return CAR_ARRAY;
-}
-
-function inputAttempt() {
-  const ATTEMPT_COUNT =
-    MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
-  return ATTEMPT_COUNT;
-}
-
-function checkCarName(CAR_ARRAY) {
-  CAR_ARRAY.forEach((car) => {
-    if (car.length > 5) {
-      throw new Error(ERROR_MESSAGES.CAR_NAME_OVER);
+  checkAttemptCount(attempt) {
+    if (
+      isNaN(attempt) ||
+      !Number.isInteger(attempt) ||
+      Math.sign(attempt) !== 1
+    ) {
+      throw new Error(ERROR_MESSAGES.INPUT_ATTEMPT_NOT_INTEGER);
     }
-    if (car.length === 0) {
-      throw new Error(ERROR_MESSAGES.CAR_NAME_ZERO);
+    if (attempt === 0) {
+      throw new Error(ERROR_MESSAGES.INPUT_ATTEMPT_ZERO);
     }
-  });
-  return CAR_ARRAY;
-}
-
-function checkAttemptCount(attempt) {
-  if (
-    isNaN(attempt) ||
-    !Number.isInteger(attempt) ||
-    Math.sign(attempt) !== 1
-  ) {
-    throw new Error(ERROR_MESSAGES.INPUT_ATTEMPT_NOT_INTEGER);
-  }
-  if (attempt === 0) {
-    throw new Error(ERROR_MESSAGES.INPUT_ATTEMPT_ZERO);
   }
 }
 
