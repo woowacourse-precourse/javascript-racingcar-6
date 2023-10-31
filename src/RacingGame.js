361da3582836a63ba2925f8ead8e396ecca39725
rlarlduf20 <kky8274@naver.com>
2023-10-31 16:48:55 +0900

    Fix: 막대바 증가에 repeat함수 사용-for문 줄이기 위해

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -12,9 +12,8 @@ const RacingGame = (carArr, tryNum) => {
     calculateMoveCntFromRandomNumber(carArr);
     for (let i = 0; i < carArr.length; i++) {
       let progressStat = '';
-      for (let j = 0; j < carArr[i].moveCnt; j++) {
-        progressStat += PROGRESS_BAR;
-      }
+      progressStat += PROGRESS_BAR.repeat(carArr[i].moveCnt);
+
       MissionUtils.Console.print(`${carArr[i].name} : ${progressStat}`);
     }
     MissionUtils.Console.print('\n');
