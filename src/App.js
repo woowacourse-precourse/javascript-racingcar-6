70f72699dc3cafa5d922b469acad6b3bdd616d70
BaeDongWoo <ehddn5410@naver.com>
2023-10-31 17:28:36 +0900

    feat:사용자에게 경주할 자동차를 입력받음

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Random, Console } from '@woowacourse/mission-utils';
 class App {
-  async play() {}
+  async play() {
+    const input = await this.inputPlayer();
+    console.log(input);
+  }
+  inputPlayer() {
+    const input = Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
+    );
+    return input;
+  }
 }
-
+const app = new App();
+app.play();
 export default App;
