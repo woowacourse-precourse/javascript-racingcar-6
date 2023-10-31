45de41bc880209ee43da9ee99707c7adfea2f3e7
고세종 <fulfilled7418@gmail.com>
2023-10-31 23:43:32 +0900

    feat: 구현 자동차 이름에 중복이 있는지, 이름에 공백이 있는지 유효성 검사 함수

--- modules/inputValidation.js
+++ modules/inputValidation.js
@@ -7,13 +7,32 @@ const checkCarNameIsOverFive = (carNames) => {
   return inputHaveOverFive ? true : false;
 };
 
+const checkCarNameWithBlank = (carNames) => {
+  const inputWithBlank = carNames.filter((car) => car.includes(' '));
+  return inputWithBlank.length !== 0 ? true : false;
+};
+
+const checkCarNamesAreDuplicated = (carNames) => {
+  const carNumber = carNames.length;
+  const nonDuplicatedCarNames = [...new Set(carNames)];
+
+  return carNumber !== nonDuplicatedCarNames.length ? true : false;
+};
+
 const checkCarNamesAreValid = (userInput) => {
   const input = userInput.split(',');
 
   const inputHaveOverName = checkCarNameIsOverFive(input);
   const inputHaveBlank = checkCarNamesHaveBlank(input);
+  const inputWithBlank = checkCarNameWithBlank(input);
+  const inputsAreDuplicated = checkCarNamesAreDuplicated(input);
 
-  if (inputHaveOverName || inputHaveBlank) {
+  if (
+    inputHaveOverName ||
+    inputHaveBlank ||
+    inputWithBlank ||
+    inputsAreDuplicated
+  ) {
     throw new Error('[ERROR] 입력이 올바른 형식이 아닙니다.');
   }
   return input;
