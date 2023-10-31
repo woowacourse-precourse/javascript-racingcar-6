ed9404144100543a38130eac55b54b6da02e8cc6
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-10-31 19:11:46 +0900

    refactor: 숫자 상수 파일과 문자열 상수 파일을 하나의 constants로 통합

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,3 +1,13 @@
+export const GAME_SETTING = Object.freeze ({
+  MOVE_FORWARD_REQUIREMENT: 4,
+  MIN_LENGTH_CAR_NAME: 1,
+  MAX_LENGTH_CAR_NAME: 5,
+  MIN_RANDOM_NUMBER: 0,
+  MAX_RANDOM_NUMBER: 9,
+  MOVE_FORWARD_POSITION: '-',
+  BLANK_SPACE: '',
+});
+
 export const LOG_MESSAGE = Object.freeze ({
   INPUT_CAR_NAME: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
   INPUT_TRY_NUMBER: '시도할 횟수는 몇 회인가요?\n',
