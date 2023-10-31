5ccc57dcefce8ffe23e40e4131dd46a80d23e9a4
0jenn0 <jenn0.6n@gmail.com>
2023-10-31 12:50:45 +0900

    Refactor: RacingInitailizer, RacingResult 캡슐화
    외부에서 사용할 메서드를 제외한 나머지 메서드를 private 메서드로 수정하여 캡슐화함

--- src/Controllers/RacingInitializer.js
+++ src/Controllers/RacingInitializer.js
@@ -3,18 +3,18 @@ import { InputView } from "../Views/index.js";
 
 export default class RacingInitializer {
   static async initializeGame() {
-    const cars = await this.initializeCars();
-    const raceRound = await this.initializeRaceRound();
+    const cars = await this.#initializeCars();
+    const raceRound = await this.#initializeRaceRound();
     return [cars, raceRound];
   }
 
-  static async initializeCars() {
+  static async #initializeCars() {
     const carNamesInput = await InputView.promptCarNames();
     const cars = Array.from(carNamesInput, (carName) => new Car(carName));
     return cars;
   }
 
-  static async initializeRaceRound() {
+  static async #initializeRaceRound() {
     const raceround = await InputView.promptRaceRound();
     return raceround;
   }
