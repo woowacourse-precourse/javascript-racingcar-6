1dbc1483ed0df4cf4d32e504b1f3a3e0856ef4c5
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-10-31 22:49:22 +0900

    ♻️ refactor: 유효성 검사의 조건을 추가한다.

--- src/util/check.js
+++ src/util/check.js
@@ -2,18 +2,24 @@ import RACING from "../constants/message"
 
 /**
  * 
- * @param {*} nameArray 5글자 이하인 이름들의 배열을 받아옵니다. 
+ * @param {*} nameArray 
+ * 1. 5글자 이하인 이름들의 배열을 받아옵니다.
+ * 2. 중복된 이름이 없는지 검사합니다.
  */
 export const checkName = async (nameArray) => {
     nameArray.forEach(name => {
         if (name.length > 5 || name.trim() === 0) throw new Error(RACING.NAMING_ERROR)
+        if (nameArray.filter(value => name === value).length > 2) throw new Error(RACING.NAMING_ERROR)
     })
 }
 
 /**
  * 
- * @param {*} string 전달받은 값이 숫자인지 확인합니다.
+ * @param {*} string 
+ * 1. 전달 받은 값이 숫자인지 확인합니다.
+ * 2. 전달 받은 값이 0 또는 음수인지 확인합니다.
  */
 export const checkNumber = async (number) => {
     if (isNaN(number)) throw new Error(RACING.NUMBER_ERROR)
+    if (number <= 0) throw new Error(RACING.NUMBER_ERROR)
 }
