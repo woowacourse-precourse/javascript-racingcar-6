e0b0c754af5473a2d8daade94842ca6b75a0bcb1
ryan-dia <cjfdnjs158@naver.com>
2023-11-01 00:20:05 +0900

    feat: index.js에서 export default 적용

--- src/utils/validator/utils/index.js
+++ src/utils/validator/utils/index.js
@@ -3,3 +3,4 @@ export { default as isValidLanguage } from './is-valid-Language/index.js';
 export { default as isValidDelimiter } from './is-valid-delimiter/index.js';
 export { default as isValidNameLength } from './is-valid-name-length/index.js';
 export { default as isValidQuantity } from './is-valid-quantity/index.js';
+export { default as isValidCount } from './is-valid-count/index.js';
