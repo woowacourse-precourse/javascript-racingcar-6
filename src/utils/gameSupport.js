dbb522ec652d5b49e636f338774f82e4c939e050
yeonkyoung <keep.ykk@gmail.com>
2023-11-01 02:31:45 +0900

    feat: 각 차수에 대한 실행 및 결과를 출력하는 함수

--- src/utils/gameSupport.js
+++ src/utils/gameSupport.js
@@ -4,4 +4,5 @@ export const getCarsClassArray = (carsStringNameArray) => {
   return carsStringNameArray.map((carName) => new Car(carName));
 };
 
-
+export const getMatchResult = (cars, trialCountNumber) => {
+  }
