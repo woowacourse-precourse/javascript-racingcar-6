b9c196ee52399ee6a5a1622fa9f8e77bd26fff57
Hyelim Choi <HyelimChoi01@gmail.com>
2023-10-31 19:13:33 +0900

    refactor: change as constants & refactor

--- src/App.js
+++ src/App.js
@@ -1,52 +1,44 @@
 import { Console, Random } from "@woowacourse/mission-utils";
+import { GAME_MESSAGE, ERROR_MESSAGE } from "./constants.js";
+
 class App {
   async play() {
-    // TODO: 경주할 자동차 이름 입력 받기
-    const InputCarNames = await Console.readLineAsync(
-      "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)\n"
-    );
-    // TODO: , 로 구분해서 저장
+    const InputCarNames = await Console.readLineAsync(GAME_MESSAGE.INPUT_CAR);
+
     const carNames = InputCarNames.split(",");
     const cars = carNames.map((name) => ({ name, position: 0 }));
 
-    // TODO: 유효처리 - 이름이 5자 이하인지
     this.checkCarNameLength(carNames);
 
-    // TODO: 시도할 횟수 입력 받기
     const InputTryCount = await Console.readLineAsync(
-      "시도할 횟수는 몇회인가요?\n"
+      GAME_MESSAGE.INPUT_TRYCOUNT
     );
     const tryCount = this.checkInputNumber(InputTryCount);
 
-    // TODO: 입력받은 횟수만큼 모든 차에 전진 or 멈춤 게임 진행
-    Console.print("\n실행 결과");
-    for (let i = 0; i < tryCount; i++) {
+    Console.print(GAME_MESSAGE.RESULT);
+    for (let i = 0; i < tryCount; i += 1) {
       this.GoOrStop(cars);
-      // TODO: 한번 진행할 때마다 현재 상태 출력
       this.CurrentStatus(cars);
     }
 
-    // TODO: 게임 종료 후 우승자 출력
-    const winner = this.getWinner(cars);
+    this.getWinner(cars);
   }
 
   checkCarNameLength(carNames) {
     carNames.forEach((carName) => {
       if (carName.length > 5) {
-        throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+        throw new Error(ERROR_MESSAGE.CAR_NAME_LENGTH);
       }
     });
-    return;
   }
 
   checkInputNumber(InputTryCount) {
     if (isNaN(InputTryCount)) {
-      throw new Error("[ERROR] 숫자만 입력 가능합니다.");
+      throw new Error(ERROR_MESSAGE.INPUT_NUMBER);
     }
     return Number(InputTryCount);
   }
 
-  // TODO: 전진할 경우 - 랜덤 숫자 생성 후 4이상일 경우 전진
   GoOrStop(cars) {
     cars.forEach((car) => {
       const randomNum = Random.pickNumberInRange(0, 9);
@@ -64,18 +56,12 @@ class App {
   }
 
   getWinner(cars) {
-    let max = 0;
-    let winner = [];
-    cars.forEach((car) => {
-      if (car.position > max) {
-        max = car.position;
-        winner = [car.name];
-      } else if (car.position === max) {
-        winner.push(car.name);
-      }
-    });
+    const maxPosition = Math.max(...cars.map((car) => car.position));
+    const winners = cars
+      .filter((car) => car.position === maxPosition)
+      .map((car) => car.name);
 
-    Console.print(`최종 우승자 : ${winner.join(", ")}`);
+    Console.print(`${GAME_MESSAGE.WINNER}${winners.join(", ")}`);
   }
 }
 
