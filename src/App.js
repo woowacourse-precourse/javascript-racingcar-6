f5875af0e21af7c2282394060f954c6773cae291
unknown <pih9134@naver.com>
2023-11-01 04:39:23 +0900

    Delete: Car.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { Console } from '@woowacourse/mission-utils';
-import Car from './Car.js';
+import { Console, Random } from '@woowacourse/mission-utils';
 import Inputs from './input/Inputs.js';
+
 // class App {
 //   constructor() {
 //     // this.cars = [];
@@ -18,88 +18,47 @@ import Inputs from './input/Inputs.js';
 
 // export default App;
 
-// class App {
-//   constructor() {
-//     // this.cars = [];
-//     // this.tryCount = 0;
-//     this.inputs = new Inputs();
-//   }
-
-//   async play() {
-//     const cars = await this.inputs.returnCarNames();
-//     const tryCount = await this.inputs.getTryCount();
-//     Console.print('\n실행 결과');
-//     this.runRace(cars, tryCount);
-//   }
-
-//   runRace(cars, tryCount) {
-//     const progressList = new Array(cars.length).fill('');
-
-//     for (let i = 0; i < tryCount; i += 1) {
-//       cars.forEach((car, carIndex) => {
-//         progressList[carIndex] += this.generateRandomProgress();
-//         Console.print(`${car} : ${progressList[carIndex]}`);
-//       });
-//       Console.print('');
-//     }
-
-//     this.printWinners(cars, progressList);
-//   }
-
-//   printWinners = (cars, progressList) =>
-//     Console.print(`최종 우승자 : ${this.findWinners(cars, progressList).join(', ')}`);
-
-//   getRandomMove = () => Random.pickNumberInRange(0, 9);
-
-//   generateRandomProgress = () => (this.getRandomMove() >= 4 ? '-' : '');
-
-//   findWinners = (cars, progressList) =>
-//     cars.filter(
-//       (car, carIndex) => progressList[carIndex].length === this.maxProgress(progressList),
-//     );
-
-//   maxProgress = (progressList) => Math.max(...progressList.map((progress) => progress.length));
-// }
-
-// export default App;
-
 class App {
   constructor() {
-    this.cars = [];
+    // this.cars = [];
+    // this.tryCount = 0;
     this.inputs = new Inputs();
   }
 
   async play() {
-    const carNames = await this.inputs.returnCarNames();
-    this.cars = carNames.map((name) => new Car(name));
-
+    const cars = await this.inputs.returnCarNames();
     const tryCount = await this.inputs.getTryCount();
-
     Console.print('\n실행 결과');
+    this.runRace(cars, tryCount);
+  }
+
+  runRace(cars, tryCount) {
+    const progressList = new Array(cars.length).fill('');
 
     for (let i = 0; i < tryCount; i += 1) {
-      this.runRaceRound();
+      cars.forEach((car, carIndex) => {
+        progressList[carIndex] += this.generateRandomProgress();
+        Console.print(`${car} : ${progressList[carIndex]}`);
+      });
+      Console.print('');
     }
 
-    this.printWinners();
+    this.printWinners(cars, progressList);
   }
 
-  runRaceRound() {
-    this.cars.forEach((car) => {
-      car.move();
-      Console.print(`${car.getName()} : ${'-'.repeat(car.getPosition())}`);
-    });
-    Console.print('');
-  }
+  printWinners = (cars, progressList) =>
+    Console.print(`최종 우승자 : ${this.findWinners(cars, progressList).join(', ')}`);
 
-  printWinners() {
-    const maxPosition = Math.max(...this.cars.map((car) => car.getPosition()));
-    const winners = this.cars
-      .filter((car) => car.getPosition() === maxPosition)
-      .map((car) => car.getName());
+  getRandomMove = () => Random.pickNumberInRange(0, 9);
 
-    Console.print(`최종 우승자 : ${winners.join(', ')}`);
-  }
+  generateRandomProgress = () => (this.getRandomMove() >= 4 ? '-' : '');
+
+  findWinners = (cars, progressList) =>
+    cars.filter(
+      (car, carIndex) => progressList[carIndex].length === this.maxProgress(progressList),
+    );
+
+  maxProgress = (progressList) => Math.max(...progressList.map((progress) => progress.length));
 }
 
 export default App;
