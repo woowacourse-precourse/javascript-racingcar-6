64b6e3d2fb3c193e621e77fa30ee56cfa9bc54aa
iamreum <102291571+iamreum@users.noreply.github.com>
2023-10-31 17:40:21 +0900

    예외 처리 추가, 메서드 분리
    - 자동차 이름과 관련된 예외 처리 추가
    
    - 사용자에게 값을 입력받을 때 Console.print된 내용 다음줄에 입력받도록 수정
    
    - 랜덤 숫자 생성 및 자동차 경주를 위한 메서드들을 클래스로 분리함

--- src/App.js
+++ src/App.js
@@ -1,48 +1,26 @@
 import {MissionUtils} from '@woowacourse/mission-utils';
+import AboutCar from "./AboutCar.js";
+import RacingCar from "./RacingCar.js";
 
 class App {
-  constructor() {
-    this.randomNumber = this.generateRandomNumbers();
-  }
-
-  generateRandomNumbers() {
-    return MissionUtils.Random.pickNumberInRange(0, 9);
-  }
-
-  moveForward() {
-
-  }
-
-  stayInPlace() {
-
-  }
-
-  decideMotion() {
-    if (this.randomNumber >= 4) {
-      this.moveForward();
-      console.log(this.randomNumber+'++');
-    }
-    else {
-      this.stayInPlace();
-      console.log(this.randomNumber+'--');
-    }
-  }
-
   async play() {
-    const CARNAME_RESPONSE = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-    const CARNAMES = CARNAME_RESPONSE.split(',').map(name => name.trim());
+    const CARNAME_RESPONSE = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+    const CARNAMES = CARNAME_RESPONSE.split(',').map(name => {
+      if (name.length >= 5) {
+        throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+      }
+      return name.trim()
+    });
     console.log(CARNAMES);
 
-    const PLAYTIME_RESPONSE = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+    const PLAYTIME_RESPONSE = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
     const PLAYTIME = parseInt(PLAYTIME_RESPONSE);
 
     if ( isNaN(PLAYTIME) || PLAYTIME < 0 ) {
       throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
     }
 
-    MissionUtils.Console.print("최종 우승자");
-    MissionUtils.Console.print(this.randomNumber);
-    MissionUtils.Console.print(this.decideMotion());
+    MissionUtils.Console.print(`최종 우승자 : ${CARNAMES}`);
   }
 }
 
