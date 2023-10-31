5c126da219200b9c0b286b40a0e8a5a34192862a
cobocho <rlaalsrb1111@naver.com>
2023-10-31 18:59:48 +0900

    fix: `jsdoc` 위치 오류 수정

--- src/service/RacingService.js
+++ src/service/RacingService.js
@@ -1,20 +1,17 @@
 import { Track, User } from '../domain/index.js';
 
-/**
- * 주어진 레이싱 조건으로 트랙을 생성 후 결과를 계산하고 반환합니다.
- *
- * @param {string[]} names Track에 참가할 유저의 이름 목록
- * @param {number} lap Track의 총 랩 수
- * @returns {
- * {
- *  records: {
- *    [key: string]: string
- *  },
- *  winners: User[]
- * }
- * } 랩별 결과와 우승자 정보를 포함한 객체
- */
 const RacingService = {
+  /**
+   * 주어진 레이싱 조건으로 트랙을 생성 후 결과를 계산하고 반환합니다.
+   * @param {string[]} names Track에 참가할 유저의 이름 목록
+   * @param {number} lap Track의 총 랩 수
+   * @returns {{
+   *  records: {
+   *    [key: string]: string
+   *  },
+   *  winners: User[]
+   * }} 랩별 결과와 우승자 정보를 포함한 객체
+   */
   getResult(names, lap) {
     /**
      * @type {{
