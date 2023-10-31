5591bf2ecfacdaa5431b963f6635ac6b0bf360ca
ryan-dia <cjfdnjs158@naver.com>
2023-11-01 01:34:50 +0900

    feat(model): 숫자 랜덤 생성기에 유효성 확인 기능 추가

--- src/model/RandomNumberGenerator.js
+++ src/model/RandomNumberGenerator.js
@@ -1,9 +1,12 @@
 import { Random } from '@woowacourse/mission-utils';
 import { SYSTEM } from '../constants/System.js';
+import Validators from '../utils/validator/index.js';
 
 const RandomNumberGenerator = {
   run() {
-    return Random.pickNumberInRange(SYSTEM.randomNumberMin, SYSTEM.randomNumberMax);
+    const randomNumber = Random.pickNumberInRange(SYSTEM.randomNumberMin, SYSTEM.randomNumberMax);
+    Validators.checkRandomNumber(randomNumber, SYSTEM.randomNumberMin, SYSTEM.randomNumberMax);
+    return randomNumber;
   },
 };
 
