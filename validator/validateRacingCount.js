e15e778d00434062b45e898e90efcac672d1bff4
yeong-hwan <jghff700@naver.com>
2023-11-01 05:49:26 +0900

    refactor: validateRacingCount 함수의 세부 검사 함수 분리
    숫자인지 검사하는 checkIsNumber 함수와 자연수인지 검사하는
    checkNaturalNumber로 분리

--- validator/validateRacingCount.js
+++ validator/validateRacingCount.js
@@ -1,17 +1,26 @@
 import ErrorMessage from '../errors/ErrorMessage.js';
 
-/**
- * 입력 받은 경주 횟수가 자연수인지 구분하여, 문제가 있을 경우 에러를 발생시킨다.
- * 
- * @param {Promise<string>} racingCountInput
- * @throws 사용자가 잘못된 값을 입력했을 경우 throw Error
- */
-export default function validateRacingCount(racingCountInput) {
-  const racingCount = Number(racingCountInput);
-  
+function checkIsNumber(userInput) {
+  if (isNaN(userInput)) {
+    throw new ErrorMessage('경주 횟수는 숫자로 입력해야 합니다.')
+  }
+}
+
+function checkNaturalNumber(racingCount) {
   if (racingCount <= 0 || !(Number.isInteger(racingCount))) {
     throw new ErrorMessage('경주 횟수는 자연수만 입력해야 합니다.');
   }
+}
+
+/**
+ * 입력 받은 경주 횟수가 자연수인지 구분하여, 문제가 있을 경우 에러를 발생시킨다.
+ * 
+ * @param {string} racingCountInput
+ * @throws 사용자가 잘못된 값을 입력했을 경우 throw Error
+ */
+export default function validateRacingCount(racingCountInput) {
+  checkIsNumber(racingCountInput);
+  checkNaturalNumber(Number(racingCountInput));
 }
 
 // console.log(validateRacingCount("0"));
