3410699ca3a688ff0fdbfb3d022ff597794ef63c
ryan-dia <cjfdnjs158@naver.com>
2023-10-31 18:29:11 +0900

    fix : return 키워드 추가

--- src/constants/Messages.js
+++ src/constants/Messages.js
@@ -1,5 +1,5 @@
 export const INPUT_MESSAGE_FUNCTION = Object.freeze({
   name(vehicle) {
-    `경주할 ${vehicle} 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분`;
+    return `경주할 ${vehicle} 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분`;
   },
 });
