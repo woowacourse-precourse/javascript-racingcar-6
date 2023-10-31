9dea2f34b80b7e5a862a14050f8a30dae0936928
ryan-dia <cjfdnjs158@naver.com>
2023-10-31 16:58:32 +0900

    refactor(validator): 불필요한 의존성  제거
    Converter을 밖에서 사용해서 isValidNameLength 함수로 주입하도록 변경

--- src/utils/validator/utils/is-valid-name-length/index.js
+++ src/utils/validator/utils/is-valid-name-length/index.js
@@ -1,15 +1,11 @@
-import SYSTEM from '../../../../constants/System.js';
-import Converter from '../../../StringConvertor.js';
+import { SYSTEM } from '../../../../constants/System.js';
 
 /**
- *
- * @param {string} input
+ * @param {string[]} input
  * @returns {boolean}
  */
 export default function isValidNameLength(input) {
-  const names = Converter.splitStringToArrayByDelimiter(input, SYSTEM.delimiter);
-
-  return names.every((name) => {
+  return input.every((name) => {
     return name.length >= SYSTEM.nameLengthMin && name.length <= SYSTEM.nameLengthMax;
   });
 }
