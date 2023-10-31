9815ffac4d6dc0b15d5a077a8f91fbf6f022c710
HyunJaeyeon <bomul0109@gmail.com>
2023-11-01 03:00:00 +0900

    refactor: Winners 클래스 리팩토링
    - 각 기능들 함수로 나타냄
    - 우승 자동차 이름만 추출

--- src/Controller/Winners.js
+++ src/Controller/Winners.js
@@ -1,9 +1,20 @@
 export class Winners {
   selectPrintWinner(carArray) {
+    const maxDistance = this.getMaxDistance(carArray);
+    const winnerCarArray = this.getWinnerCarArray(carArray, maxDistance);
+    const winnerNameArray = this.getOnlyNameArray(winnerCarArray);
+  }
+
+  getMaxDistance(carArray) {
     const distanceArray = carArray.map((carObject) => carObject.distance);
-    const maxDistance = Math.max(...distanceArray);
-    carArray = carArray.filter(
-      (carObject) => carObject.distance === maxDistance
-    );
+    return Math.max(...distanceArray);
+  }
+
+  getWinnerCarArray(carArray, maxDistance) {
+    return carArray.filter((carObject) => carObject.distance === maxDistance);
+  }
+
+  getOnlyNameArray(array) {
+    return array.map((object) => object.name);
   }
 }
