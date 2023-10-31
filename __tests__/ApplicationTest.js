864c57573fd7e0b3240a545f12e5888a8dce7959
박지윤 <tkrhkfld123@naver.com>
2023-10-31 23:26:43 +0900

    test: 자동차 이름입력, 예외 테스트 통과

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -60,3 +60,44 @@ describe("자동차 경주 게임", () => {
     await expect(app.play()).rejects.toThrow("[ERROR]");
   });
 });
+
+// 밑에서부터 본인이 작성한 테스트 코드
+
+describe("inputCarNamesAsync", () => {
+  test("자동차 이름 설정하기", async () => {
+    const validInput = "pobi,woni";
+    const app = new App();
+    const mockReadLineAsync = jest.fn(() => Promise.resolve(validInput));
+    MissionUtils.Console.readLineAsync = mockReadLineAsync;
+
+    await app.inputCarNamesAsync();
+
+    expect(mockReadLineAsync).toHaveBeenCalledWith("경주할 자동차 이름을 입력하세요 (이름은 쉼표(,)로 구분):");
+    expect(app.cars).toEqual([{ name: "pobi", position: 0 }, { name: "woni", position: 0 }]);
+  });
+
+  test("5자 이상인 이름은 오류 발생 시키기", async () => {
+    // 주어진
+    const invalidInput = "pobi,verylongname";
+    const app = new App();
+    const mockReadLineAsync = jest.fn(() => Promise.resolve(invalidInput));
+    MissionUtils.Console.readLineAsync = mockReadLineAsync;
+
+    // 실행 및 확인
+    await expect(app.inputCarNamesAsync()).rejects.toThrow("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+  });
+
+  test("이름 내의 공백을 처리하기", async () => {
+    // 주어진
+    const inputWithWhitespace = "  pobi , woni  ";
+    const app = new App();
+    const mockReadLineAsync = jest.fn(() => Promise.resolve(inputWithWhitespace));
+    MissionUtils.Console.readLineAsync = mockReadLineAsync;
+
+    // 실행
+    await app.inputCarNamesAsync();
+
+    // 당연히
+    expect(app.cars).toEqual([{ name: "pobi", position: 0 }, { name: "woni", position: 0 }]);
+  });
+});
