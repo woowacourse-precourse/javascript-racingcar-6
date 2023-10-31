128ff411199930e50df251983f7ca6c337fe0500
Kim Da Eun <midekuna@gmail.com>
2023-10-31 23:49:03 +0900

    📝 docs: utils JSDoc 작성

--- src/utils/StringUtils.js
+++ src/utils/StringUtils.js
@@ -1,12 +1,27 @@
+/**
+ * 공백 문자 유무 검사
+ * @param {string} str 검사 문자열
+ * @returns {boolean}
+ */
 export const hasWhiteSpace = (str) => {
   return str.includes(' ');
 };
 
+/**
+ * 문자열이 정수인지 검사
+ * @param {string} str 검사 문자열
+ * @returns {boolean}
+ */
 export const isInteger = (str) => {
   const num = Number(str);
   return Number.isInteger(num);
 };
 
+/**
+ * 문자열이 숫자로만 이루어져 있는지 검사
+ * @param {string} str 검사 문자열
+ * @returns {boolean}
+ */
 export const isNumber = (str) => {
   return !isNaN(Number(str));
 };
--- src/utils/throwError.js
+++ src/utils/throwError.js
@@ -1,5 +1,9 @@
 import { ERRORS } from '../constants/errors.js';
 
+/**
+ * 에러 문구를 포맷팅하여 던지는 함수
+ * @param {string} message 에러 메시지
+ */
 export function throwError(message) {
   throw new Error(`${ERRORS.prefix} ${message}`);
 }
