809e2b686cd7fb0b7ede968e9d69b8f25b94dbc7
dongwkan <dwehdqkfwk@naver.com>
2023-10-31 20:45:08 +0900

    feat: 애플리케이션 코드

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import inputCarName from "./inputCarName.js";
+import inputTryNumber from "./inputTryNumber.js";
+import allTryResult from "./allTryResult.js";
+import getFinalScore from "./getFinalScore.js";
+import getFinalWinner from "./getFinalWinner.js";
+
 class App {
-  async play() {}
+  async play() {
+    const carName = await inputCarName();
+    const tryNumber = await inputTryNumber();
+    let allScore = [];
+    for (let i = 0; i < carName.length; i++) {
+      allScore.push("");
+    }
+    allScore = allTryResult(carName, tryNumber, allScore);
+    const winnerScoreLength = getFinalScore(allScore);
+    getFinalWinner(allScore, winnerScoreLength, carName)
+  }
 }
 
 export default App;
