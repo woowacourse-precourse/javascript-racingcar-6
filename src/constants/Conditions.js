2a070ba0974fc5919d15350ce54f8fbd3454dbd2
1eecan <busyx2modernsociety@gmail.com>
2023-11-01 04:18:47 +0900

    fix: Conditions.js, validateRacingCar.js 수정
    * 자동차 이름의 최소 조건에 더 자연스럽게 어울리도록 상수 수정 및 조건 수정
    * 이름에 공백이 들어가는 경우에 대한 유효성 검증 추가

--- src/constants/Conditions.js
+++ src/constants/Conditions.js
@@ -2,7 +2,7 @@ const CONDITIONS = Object.freeze({
   TRY_MINIMUM: 1,
   MOVE_FORWARD: 4,
   MAXIMUM_CAR_NAME: 5,
-  MINIMUM_CAR_NAME: 0,
+  MINIMUM_CAR_NAME: 1,
 });
 
 export default CONDITIONS;
--- src/input/racingCar/validateRacingCar.js
+++ src/input/racingCar/validateRacingCar.js
@@ -4,7 +4,8 @@ const validateRacingCar = (array) => {
   const input_array = [...array];
   for (let i = 0; i < input_array.length; i++) {
     if (
-      input_array[i].length === CONDITIONS.MINIMUM_CAR_NAME ||
+      input_array[i].includes(" ") ||
+      input_array[i].length < CONDITIONS.MINIMUM_CAR_NAME ||
       input_array[i].length > CONDITIONS.MAXIMUM_CAR_NAME
     ) {
       return false;
