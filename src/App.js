c514984fc95b3b1898419eb2db73bcbe4ea6b591
nagyeongyun <skruddbs99@naver.com>
2023-10-31 14:48:59 +0900

    fix: 전진 실행 결과 기능 함수 분리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { Console } from '@woowacourse/mission-utils';
 import { GAME_MESSAGE } from './Constants.js';
-import User from './User.js';
 import Car from './Car.js';
 
 class App {
   async play() {
     const car = new Car();
-    await car.forwardCar();
+    await car.forwardCarResult();
   }
 }
 
--- src/Car.js
+++ src/Car.js
@@ -3,33 +3,35 @@ import { GAME_MESSAGE } from './Constants.js';
 import User from './User.js';
 
 class Car {
-  generateRandomNumber() {
-    const randomNumber = Random.pickNumberInRange(0, 9);
-
-    return randomNumber;
-  }
-
-  async forwardCar() {
+  async forwardCarResult() {
     const user = new User();
     const cars = await user.inputCarName();
     const tryCount = await user.inputTryCount();
 
     Console.print('\n' + GAME_MESSAGE.result);
 
-    const resultArray = new Array(cars.length).fill('');
+    this.printForwardResult(cars, tryCount);
+  }
+
+  generateRandomNumber() {
+    return Random.pickNumberInRange(0, 9);
+  }
+
+  tryForward(allForwardResult, index, cars) {
+    const randomNumber = this.generateRandomNumber();
+    if (randomNumber >= 4) {
+      allForwardResult[index] += '-';
+    }
+    Console.print(`${cars[index]} : ${allForwardResult[index]}`);
+  }
+
+  printForwardResult(cars, tryCount) {
+    const allForwardResult = new Array(cars.length).fill('');
 
     for (let i = 0; i < tryCount; i++) {
       cars.forEach((car, index) => {
-        const randomNumber = this.generateRandomNumber();
-        if (randomNumber >= 4) {
-          resultArray[index] += '-';
-        }
+        this.tryForward(allForwardResult, index, cars);
       });
-
-      cars.forEach((car, index) => {
-        Console.print(`${car} : ${resultArray[index]}`);
-      });
-
       Console.print('');
     }
   }
