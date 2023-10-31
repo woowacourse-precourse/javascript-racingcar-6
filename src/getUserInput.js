221a7b696996642af54e1ccf297e80e5a7ae17ba
SungChul Hong <hsc6263@gmail.com>
2023-10-31 22:57:58 +0900

    chore: 사용자 입력값 처리 및 검증 함수 수정
    수정 내용:
    
    기존:
    - 자동차 이름과 진행 횟수를 모두 입력받은 후 검증 실행
    
    변경:
    - 자동차 이름과 진행 횟수를 각각 입력받는 즉시 검증

--- src/getUserInput.js
+++ src/getUserInput.js
@@ -1,5 +1,5 @@
 import { Console } from '@woowacourse/mission-utils';
-import { printErrorMessage } from './utils/messages.js';
+import { printErrorMessage, printMessage } from './utils/messages.js';
 
 const validateNameInput = input => {
   if (input.some(item => item.length > 5)) {
@@ -19,12 +19,13 @@ const getUserInput = async () => {
   const nameInput = await Console.readLineAsync(
     '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) \n',
   ).then(res => res.split(',').map(item => item.trim()));
-
+  const carNames = validateNameInput(nameInput);
   const countInput =
     await Console.readLineAsync('시도할 횟수는 몇 회인가요? \n');
+  const count = validateCountInput(countInput);
   return {
-    carNames: validateNameInput(nameInput),
-    count: validateCountInput(countInput),
+    carNames,
+    count,
   };
 };
 
