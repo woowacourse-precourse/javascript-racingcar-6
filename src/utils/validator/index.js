494715f571c40851d1210f8a25fe387d134932f2
ryan-dia <cjfdnjs158@naver.com>
2023-10-31 16:31:38 +0900

    feat(validator): 자동차 대수 유효성 확인 기능 추가

--- src/utils/validator/index.js
+++ src/utils/validator/index.js
@@ -3,6 +3,7 @@ import Converter from '../StringConvertor.js';
 import isValidLanguage from './utils/is-valid-Language/index.js';
 import isValidDelimiter from './utils/is-valid-delimiter/index.js';
 import isValidNameLength from './utils/is-valid-name-length/index.js';
+import isValidQuantity from './utils/is-valid-quantity/index.js';
 
 const Validators = {
   checkRacingVehicleName(input) {
@@ -11,6 +12,7 @@ const Validators = {
     isValidDelimiter(input, SYSTEM.delimiter);
     isValidNameLength(namesArray);
     isValidLanguage(namesArray, LANGUAGE_OPTION);
+    isValidQuantity(namesArray);
   },
 };
 
