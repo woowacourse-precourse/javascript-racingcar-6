cab8b77a15f96dad7288de81861dd3a828a345d0
nkEllaaa <dhrtns515@gmail.com>
2023-10-31 23:16:13 +0900

    feat: getCarNames, getRaceCount 입력 받은 값 넘겨주는 부분 추가

--- src/getCarNamesAndCount.js
+++ src/getCarNamesAndCount.js
@@ -1,19 +1,23 @@
-import { Console } from '@woowacourse/mission-utils'
+import { Console } from '@woowacourse/mission-utils';
+import validation from './validation/validationCarName';
+import validateRaceCount from './validation/validationRaceCount';
 
-const getRaceCount = () => {
-  const raceCount = Console.readLineAsync('시도할 횟수는 몇 회인가요?');
+const getRaceCount = async (carNamesToArray) => {
+  const raceCount = await Console.readLineAsync('시도할 횟수는 몇 회인가요?');
   const raceCountsToNumber = parseInt(raceCount);
-  //콘솔로그 지우기
-  console.log(raceCountsToNumber,'카운트')
-  return raceCountsToNumber;
-}
+  
+  validateRaceCount(carNamesToArray, raceCountsToNumber);
+};
 
-const getCarNames = () => {
-  const carNames = Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
-  const carNamesToArray = carNames.split('');
-  //콘솔로그 지우기
-  console.log(carNamesToArray,'이름')
-  return carNamesToArray;
-}
+const getCarNames = async () => {
+  const carNames = await Console.readLineAsync(
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
+  );
 
-module.exports = { getCarNames }
+  const carNamesToArray = carNames.split(',');
+  
+  validation(carNamesToArray);
+  await getRaceCount(carNamesToArray);
+};
+
+module.exports = { getCarNames, getRaceCount };
