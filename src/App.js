8a695863410da3f461c68eb6ff09fe8fae01351a
swJaNG12 <cbdjej38483@gmail.com>
2023-10-31 22:35:16 +0900

    feat: add car status object generation feature
    adds a feature that generates a car status object.
    Each key of the object is a car's name,
    and the value is the number of times that car has moved forward.
    
    These forward counts are calculated
    by the generateRandomNumbersByAttempts method, which generates
    a specific number of random numbers
    and counts how many are 4 or greater.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Console } from '@woowacourse/mission-utils';
+import { Console, Random } from '@woowacourse/mission-utils';
 import { CarNameValidator } from './validator.js';
 
 class App {
@@ -10,8 +10,18 @@ class App {
         throw new Error(validationErrorMessage);
       }
 
+      const carNameArray = carNamesString.split(',');
       const attemptForwardCount = await this.getUsetInputForwardCount();
       Console.print(attemptForwardCount);
+      Console.print(carNameArray);
+
+      const carStatus = {};
+      carNameArray.forEach((car, idx) => {
+        const forwardCount =
+          this.generateRandomNumbersByAttempts(attemptForwardCount);
+        carStatus[carNameArray[idx]] = forwardCount;
+      });
+      Console.print(carStatus);
     } catch (error) {
       console.error(error);
     }
@@ -24,6 +34,14 @@ class App {
   }
   async getUsetInputForwardCount() {
     return await Console.readLineAsync('시도할 횟수는 몇 회인가요?');
+  }
+  generateRandomNumbersByAttempts(number) {
+    const randomNumberArray = Array(number)
+      .fill(0)
+      .map((value) => value + Random.pickNumberInRange(0, 9));
+
+    const forwardCount = randomNumberArray.filter((value) => value >= 4).length;
+    return forwardCount;
   }
 }
 
