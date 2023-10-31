0ffc0af6f320389fbc49474e26c369db6e4d832b
Lee sang Yeop <mma7710@naver.com>
2023-10-31 13:01:14 +0900

    Feat: 자동차 이름을 입력받아 각각 객체로 만들어 프로퍼티에 저장하는 기능 구현

--- src/controller/App.js
+++ src/controller/App.js
@@ -1,5 +1,23 @@
+import SYSTEM_MESSAGES from '../constants/SystemMessages.js';
+import Car from '../model/Car.js';
+import PrintConsole from '../view/PrintConsole.js';
+import UserInput from '../view/UserInput.js';
+
 class App {
-  async play() {}
+	#userInput;
+	#printConsole;
+	#racingCars;
+
+	constructor() {
+		this.#userInput = new UserInput();
+		this.#printConsole = new PrintConsole();
+		this.#racingCars = [];
+	}
+
+	async play() {
+		const carNames = await this.#userInput.inputRacingCarName();
+		this.#racingCars = carNames.map((carName) => new Car(carName));
+	}
 }
 
 export default App;
