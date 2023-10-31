ddc414dc2014a14df9c50449728054be6dc3f28a
ymj307 <dymj307@gmail.com>
2023-10-31 17:35:05 +0900

    docs/README.md 기능 목록 작성, eslint 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable class-methods-use-this */
+
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    MissionUtils.Console.print('this is test right now. app.play method');
+  }
 }
 
 export default App;
+
+// MissionUtils.Console.print('this is test right now');
