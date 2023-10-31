e45b4599b3e74071071b97e0c6ea15cbdbe859b2
kwonja <kownja87@gmail.com>
2023-10-31 22:37:36 +0900

    [feat] : 경주할 자동차 이름을 입력한다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+
+  async startGameMessage(){
+     const carNameInput = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+  }
+  async play() {
+    this.startGameMessage();
+  }
 }
 
 export default App;
