dabc299b052d51e62831002b0beec6493b04cbde
yunminwon <148843628+yunminwon@users.noreply.github.com>
2023-10-31 17:51:45 +0900

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { getCarsName, getTimes } from './input.js';
+
+export default function RacingCarGame() {
+  const namesSubmitButton = document.getElementById('car-names-submit');
+  const timesSubmitButton = document.getElementById('racing-count-submit');
+
+  if (namesSubmitButton) {
+    namesSubmitButton.addEventListener('click', getCarsName);
+  }
+
+  if (timesSubmitButton) {
+    timesSubmitButton.addEventListener('click', getTimes);
+  }
+}
+
+new RacingCarGame();
