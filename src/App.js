18aecae72f78d59f85138ca2731beb721a0f9404
yeonkyoung <keep.ykk@gmail.com>
2023-11-01 02:44:49 +0900

    chore: import 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import {
+  RESULT_MESSAGE,
+  FINAL_WINNER_MESSAGE,
+  COLON,
+} from "./constants/messages.js";
 import { userInputCarsName, userInputTrialCount } from "./userInterface.js";
 import {
   getCarsClassArray,
+  getMatchResult,
+  getWinnerArray,
 } from "./utils/gameSupport.js";
+
 class App {
   async play() {
     const carsStringNameArray = await userInputCarsName();
