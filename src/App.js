0503f97dc29b66776c9cc99424c5a083e7e77579
DESKTOP-55LJG2P\교육생47 <onmer99@gmail.com>
2023-10-31 22:13:49 +0900

    feat: 입력 관련 기능 사용자로부터 자동차의 이름과 이동 횟수를 입력 받는 기능을 담당합니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    try {
+      const carNamesInput = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+      const carNames = carNamesInput.split(",");
+
+      if (!this.areValidNames(carNames)) {
+        throw new Error("[ERROR] 이름은 5자 이하만 가능하고, 쉼표(,)로 구분해야 합니다.");
+      }
+
+      const moveTimesInput = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+      const moveTimes = parseInt(moveTimesInput, 10);
+
+      if (isNaN(moveTimes)) {
+        throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+      }
+
+      const carStatus = this.initializeCars(carNames);
+      this.raceCars(carStatus, moveTimes);
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
+      throw error;
+    }
+  }
+
+  areValidNames(carNames) {
+    return carNames.every((name) => name.length <= 5);
+  }
+
 }
 
 export default App;
