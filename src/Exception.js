704b349e2a60afb2452659ca423bdc2da1323af0
soominnnn <marisol03@naver.com>
2023-11-01 02:00:34 +0900

    🔨Reactor: 예외 처리 함수 분리 및 적용

--- src/Exception.js
+++ src/Exception.js
@@ -1,7 +1,31 @@
-export const EXEPTION = {
-    CAR_NAME_DUPLICATE_ERROR : "[ERROR] 자동차의 이름은 한 번씩만 사용할 수 있습니다.",
-    CAR_NAME_COUNT_ERROR : "[ERROR] 자동차의 이름을 2개 이상 입력해주세요.",
-    CAR_NAME_LENGTH_ERROR : "[ERROR] 자동차의 이름을 4자 이하로 정해주세요.",
-    INVALID_TYPE_TRY_ERROR : "[ERROR] 시도할 횟수를 숫자로 입력해주세요.",
-    TRY_COUNT_ERROR : "[ERROR] 시도할 횟수를 1회 이상 입력해주세요."
+class Exception {
+  CAR_NAME_DUPLICATE_ERROR = "[ERROR] 자동차의 이름은 한 번씩만 사용할 수 있습니다.";
+  CAR_NAME_COUNT_ERROR = "[ERROR] 자동차의 이름을 2개 이상 입력해주세요.";
+  CAR_NAME_LENGTH_ERROR = "[ERROR] 자동차의 이름을 4자 이하로 정해주세요.";
+  INVALID_TYPE_TRY_ERROR = "[ERROR] 시도할 횟수를 숫자로 입력해주세요.";
+  TRY_COUNT_ERROR = "[ERROR] 시도할 횟수를 1회 이상 입력해주세요.";
+
+  carNameInputException(userInput) {
+    if(userInput == '' || userInput[userInput.length-1] == '') {
+      throw new Error(this.CAR_NAME_DUPLICATE_ERROR);
+    }
+    else if(userInput.some(name => name.length >= 5)) {
+      throw new Error(this.CAR_NAME_LENGTH_ERROR);
+    }
+    else if(userInput.length != [...new Set(userInput)].length) {
+      throw new Error(this.CAR_NAME_COUNT_ERROR);
+    }
+    else {
+      return userInput;
+    }
+  }
+  numberInputException(userInput) {
+    if(!Number(userInput)) {
+      throw new Error(this.INVALID_TYPE_TRY_ERROR);
+    }
+    else if(userInput == 0) {
+      throw new Error(this.TRY_COUNT_ERROR);
+    }
+  }
 }
+export default Exception;
--- src/GetInput.js
+++ src/GetInput.js
@@ -1,7 +1,8 @@
 import { Console } from "@woowacourse/mission-utils";
-import { EXEPTION } from "./Exception";
+import Exception from "./Exception";
 import { MESSAGE } from "./Message";
 
+const exception = new Exception;
 class GetInput {
   carName = [];
 
@@ -9,18 +10,7 @@ class GetInput {
     try {
       const USER_INPUT = await Console.readLineAsync(MESSAGE.GET_CAR_NAME_MESSAGE);
       const USER_INPUT_SPLIT = USER_INPUT.split(',');
-      if(USER_INPUT_SPLIT == '' || USER_INPUT_SPLIT[USER_INPUT_SPLIT.length-1] == '') {
-          throw new Error(EXEPTION.CAR_NAME_COUNT_ERROR);
-      }
-      else if(USER_INPUT_SPLIT.some(name => name.length >= 5)) {
-          throw new Error(EXEPTION.CAR_NAME_LENGTH_ERROR);
-      }
-      else if(USER_INPUT_SPLIT.length != [...new Set(USER_INPUT_SPLIT)].length) {
-          throw new Error(EXEPTION.CAR_NAME_LENGTH_ERROR);
-      }
-      else {
-          this.carName = USER_INPUT_SPLIT;
-      }
+      this.carName = exception.carNameInputException(USER_INPUT_SPLIT);
       return this.carName;
     }catch(error) {
       throw new Error(error.message);
@@ -31,12 +21,7 @@ class GetInput {
     try {
       const USER_NUMBER_INPUT = await Console.readLineAsync(MESSAGE.GET_NUMBER_NAME_MESSAGE);
       Console.print("");
-      if(!Number(USER_NUMBER_INPUT)) {
-        throw new Error(EXEPTION.INVALID_TYPE_TRY_ERROR);
-      }
-      else if(USER_NUMBER_INPUT == 0) {
-        throw new Error(EXEPTION.TRY_COUNT_ERROR);
-      }
+      exception.numberInputException(USER_NUMBER_INPUT);
       return USER_NUMBER_INPUT;
     }catch(error) {
       throw new Error(error.message);
