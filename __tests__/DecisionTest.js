205d08c7bb0442984d0feec4e51eeab1d2a60a8f
8leamnk <silkviewrk@gmail.com>
2023-10-31 11:14:51 +0900

    test: 전진 혹은 정지 기능에 대한 테스트 코드를 수정함
    수정 내용:
    - 하드코딩 되어 있는 부분을 상수로 관리하도록 수정함

--- __tests__/DecisionTest.js
+++ __tests__/DecisionTest.js
@@ -11,8 +11,9 @@ const mockRandoms = (numbers) => {
 
 test('전진할지 정지할지 결정', () => {
   // given
+  const MOVING_FORWARD = 4;
   const randoms = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const outputs = randoms.map((random) => random >= 4);
+  const outputs = randoms.map((random) => random >= MOVING_FORWARD);
 
   mockRandoms([...randoms]);
 
