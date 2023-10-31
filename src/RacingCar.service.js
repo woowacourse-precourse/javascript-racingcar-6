9ee1e01b1802d737285499964adc667097649b1b
wonkyum-kim <dodo12120@naver.com>
2023-10-31 23:31:07 +0900

    feat: App 서비스 수정
    - validation 기능 분리
    - io 기능 분리

--- src/RacingCar.service.js
+++ src/RacingCar.service.js
@@ -1,89 +1,40 @@
-import { Random, Console } from '@woowacourse/mission-utils';
-import {
-  QUERY_MESSAGE,
-  ERROR_MESSAGE,
-  RESULT_MESSAGE,
-  CAR_NAME_LENGTH,
-  RANDOM_RANGE,
-  THRESHOLD,
-  MOVE,
-  MINIMUM_TRIES,
-} from './constants.js';
+import { Random } from '@woowacourse/mission-utils';
+import { RANDOM_RANGE, THRESHOLD, MOVE } from './constants.js';
+import { IOController } from './io/IO.controller.js';
 
 export default class RacingCarService {
+  #IOController;
+
+  constructor() {
+    this.#IOController = new IOController();
+  }
+
   async racingCarQuery() {
-    const RACING_CAR_NAME_QUERY_INPUT = await Console.readLineAsync(
-      QUERY_MESSAGE.carName
-    );
-    this.validateRacingCarNameQueryInput(
-      RACING_CAR_NAME_QUERY_INPUT.split(',')
-    );
-    const RACING_CAR_TRY_QUERY_INPUT = await Console.readLineAsync(
-      QUERY_MESSAGE.try
-    );
-    this.validateRacingCarTryQueryInput(RACING_CAR_TRY_QUERY_INPUT);
-    this.play(
-      RACING_CAR_NAME_QUERY_INPUT.split(','),
-      RACING_CAR_TRY_QUERY_INPUT
-    );
-  }
-
-  validateRacingCarNameQueryInput(cars) {
-    cars.forEach((car) => {
-      if (
-        car.length > CAR_NAME_LENGTH.max ||
-        car.length < CAR_NAME_LENGTH.min
-      ) {
-        throw new Error(ERROR_MESSAGE.length);
-      }
-    });
-    cars.forEach((car, index) => {
-      if (cars.indexOf(car) !== index) {
-        throw new Error(ERROR_MESSAGE.unique);
-      }
-    });
-  }
-
-  validateRacingCarTryQueryInput(tries) {
-    if (Number.isNaN(+tries)) {
-      throw new Error(ERROR_MESSAGE.nan);
-    }
-    if (+tries < MINIMUM_TRIES) {
-      throw new Error(ERROR_MESSAGE.range);
-    }
-  }
-
-  play(cars, tries) {
-    Console.print(RESULT_MESSAGE.intro);
-    const result = cars.reduce((acc, car) => {
+    const RACING_CAR_NAMES = await this.#IOController.readCarName(); // string[]
+    const RACING_CAR_TRIES = await this.#IOController.readCarTries(); // number
+    const progress = RACING_CAR_NAMES.reduce((acc, car) => {
       acc[car] = '';
       return acc;
     }, {});
-    for (let i = 0; i < tries; ++i) {
-      cars.forEach((car) => {
-        result[car] +=
-          Random.pickNumberInRange(RANDOM_RANGE.min, RANDOM_RANGE.max) >=
-          THRESHOLD
-            ? MOVE
-            : '';
-        Console.print(`${car} : ${result[car]}`);
-      });
-      Console.print('');
-    }
-    this.printResult(result);
+    this.play(progress, RACING_CAR_NAMES, RACING_CAR_TRIES);
   }
 
-  printResult(result) {
-    let max = 0;
-    for (const [key, value] of Object.entries(result)) {
-      max = Math.max(value.length, max);
+  pickRandomNumber() {
+    return Random.pickNumberInRange(RANDOM_RANGE.min, RANDOM_RANGE.max);
+  }
+
+  play(progress, cars, tries) {
+    this.#IOController.printResultIntro();
+    for (let i = 0; i < tries; ++i) {
+      cars.forEach((car) => {
+        progress[car] += this.pickRandomNumber() >= THRESHOLD ? MOVE : '';
+      });
+
+      cars.forEach((car, index) => {
+        const IS_LAST = index === cars.length - 1;
+        this.#IOController.printCurrentResult(car, progress[car], IS_LAST);
+      });
     }
-    const winners = [];
-    for (const [key, value] of Object.entries(result)) {
-      if (value.length === max) {
-        winners.push(key);
-      }
-    }
-    Console.print(RESULT_MESSAGE.winners + `${winners.join(', ')}`);
+    this.#IOController.printFinalResult(progress);
   }
 }
