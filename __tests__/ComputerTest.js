5e5e192e6b07ad3d1f85963b5dcacdf31d370da1
OAO <ashgrayblue0@gmail.com>
2023-11-01 01:28:15 +0900

    test(ComputerTest.js): test to 'start' method

--- __tests__/ComputerTest.js
+++ __tests__/ComputerTest.js
@@ -1,9 +1,22 @@
 import Computer from '../src/module/Computer.js';
 import Car from '../src/module/Car.js';
+import {MissionUtils} from "@woowacourse/mission-utils";
+
+const mockInput = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+
+    return Promise.resolve(input);
+  });
+};
 
 function generateObject(input) {
   const car = new Car();
+
   car.name = input;
+
   return car;
 }
 
@@ -12,21 +25,41 @@ describe('Computer 클래스 테스트', () => {
     const computer = new Computer();
 
     computer.round = 3;
+
     expect(computer.round).toEqual(3);
     expect(() => {
       computer.round = -1;
     }).toThrow('[ERROR]');
   });
 
-  test('pushCarList 테스트', () => {
+  test('pushCarList 메서드 테스트', () => {
     const computer = new Computer();
     const testArray = [];
 
-    for (let i = 1; i < 6; i++){
+    for (let i = 1; i < 6; i++) {
       testArray.push(generateObject(i.toString()));
     }
 
     computer.pushCarList('1,2,3,4,5');
+
     expect(computer.carList).toEqual(testArray);
-  })
+  });
+
+  test('start 메서드 테스트', async () => {
+    const testCase = ['pobi,woni,jun', 5];
+    const testText = ['pobi','woni','jun'];
+    const computer = new Computer();
+    const testArray = [];
+    const testValue = 5;
+
+    mockInput(testCase);
+    await computer.start();
+
+    for (let i = 0; i < 3; i++) {
+      testArray.push(generateObject(testText[i]));
+    }
+
+    expect(computer.carList).toStrictEqual(testArray);
+    expect(computer.round).toEqual(testValue);
+  });
 });
