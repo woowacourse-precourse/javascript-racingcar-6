a2fa9716f9289f79de043f32d6e49b7857e45e94
gyucheolshin <nsfsgc2295@gmail.com>
2023-11-01 02:32:51 +0900

    refactor : 입출력 및 메세지 분리 리펙토링

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { MESSAGE } from "./constants/message.js";
+import { print, readLineAsync } from "./utils/console.js";
+import { rangeNumber } from "./utils/random.js";
+
 class App {
   validateCar = (cars) => {
     for (let car of cars) {
-      if (car.length > 5) {
-        throw new Error("[ERROR] 자동차 이름은 5글자 이하입니다.");
-      }
+      this.validateCarName(car);
     }
   };
 
-  validationCarName = (cars) => {
-    try {
-      this.validateCar(cars);
-    } catch (error) {
-      MissionUtils.Console.print("에러");
-      return false;
+  validateCarName = (car) => {
+    if (car.length > 5) {
+      throw new Error(MESSAGE.ERROR_MAX_NAME_LENGTH);
     }
-    return true;
+    if (car.includes(" ")) {
+      throw new Error(MESSAGE.ERROR_NAME_GAP);
+    }
   };
 
   carCreate = (cars) => {
@@ -26,36 +26,12 @@ class App {
     return car;
   };
 
-  createCarName = async () => {
-    const input = await MissionUtils.Console.readLineAsync(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
-    );
-    const cars = input.split(",");
-    this.validateCar(cars);
-    let inputAttempts = await MissionUtils.Console.readLineAsync(
-      "시도할 횟수는 몇 회인가요?\n"
-    );
-    let car = this.carCreate(cars);
-    this.attempts(cars, car, inputAttempts);
-  };
-
-  async attempts(cars, car, inputAttempts) {
-    MissionUtils.Console.print("\n실행 결과");
-    while (inputAttempts > 0) {
-      this.racingGame(cars, car);
-      this.racingResult(cars, car);
-      inputAttempts--;
-    }
-    this.winner(cars, car);
-  }
-
   winner = (cars, car) => {
     const winner = this.getWinner(cars, car);
     this.printWinner(winner);
   };
 
   getWinner = (cars, car) => {
-    console.log(cars, car);
     let winnerScore = this.winnerScore(car);
     let winner = [];
     let index = 0;
@@ -85,12 +61,12 @@ class App {
       result += finalWinner + ", ";
     }
     result = result.substring(0, result.length - 2);
-    MissionUtils.Console.print("최종 우승자 : " + result);
+    print(MESSAGE.FINNAL_WINNER + result);
   };
 
   racingGame = (cars, car) => {
     for (let carName of cars) {
-      const moveNumber = MissionUtils.Random.pickNumberInRange(0, 9);
+      const moveNumber = rangeNumber(0, 9);
       let move = 0;
       if (moveNumber >= 4) {
         move = 1;
@@ -106,13 +82,24 @@ class App {
       for (let i = 0; i < score; i++) {
         result += "-";
       }
-      MissionUtils.Console.print(carName + " : " + result);
+      print(carName + " : " + result);
     }
-    MissionUtils.Console.print("");
+    print("");
   };
 
   async play() {
-    this.createCarName();
+    const input = await readLineAsync(MESSAGE.CAR_NAME);
+    const cars = input.split(MESSAGE.CAR_DIVISION);
+    this.validateCar(cars);
+    let inputAttempts = await readLineAsync(MESSAGE.NUMBER_OF_ATTEMPTS);
+    let car = this.carCreate(cars);
+    print(MESSAGE.GAME_RESULT);
+    while (inputAttempts > 0) {
+      this.racingGame(cars, car);
+      this.racingResult(cars, car);
+      inputAttempts--;
+    }
+    this.winner(cars, car);
   }
 }
 
---
+++ src/constants/message.js
@@ -0,0 +1,11 @@
+export const MESSAGE = Object.freeze({
+  CAR_NAME: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
+  CAR_DIVISION: ",",
+  NUMBER_OF_ATTEMPTS: "시도할 횟수는 몇 회인가요?\n",
+  GAME_RESULT: "실행 결과",
+  FINNAL_WINNER: "최종 우승자 : ",
+  ERROR_NOT_NUMBER: "[ERROR] 숫자가 잘못된 형식입니다.",
+  ERROR_MIN_NAME_LENGTH: "[ERROR] 자동차 이름은 1글자 이상입니다.",
+  ERROR_MAX_NAME_LENGTH: "[ERROR] 자동차 이름은 5글자 이하입니다.",
+  ERROR_NAME_GAP: "[ERROR] 자동차 이름에 공백이 존재합니다.",
+});
