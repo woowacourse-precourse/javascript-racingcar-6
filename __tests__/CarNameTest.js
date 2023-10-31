7beb8437b2d0edcef4da4c244d139d980cd3e4eb
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2023-11-01 01:01:06 +0900

    style: 상수 모듈 import 후 적용

--- __tests__/CarNameTest.js
+++ __tests__/CarNameTest.js
@@ -1,11 +1,12 @@
+import { VALUE } from '../src/constants/constants.js';
 import Car from '../src/model/Car.js';
 import { checkLessThanFive, checkHasSpace, checkDuplicated } from '../src/utils/Validation.js';
 
 describe('올바른 car 객체 판단 테스트', () => {
   const carList = ['아빠차', '엄마차', '내차'];
 
-  test('자동차 이름 테스트', () => {
-    const testList = carList.map((car) => new Car(car, 0));
+  test('자동차 객체 생성 테스트', () => {
+    const testList = carList.map((car) => new Car(car, VALUE.INITIAL_DISTANCE));
     testList.forEach((car, index) =>
       expect(car.getName()).toEqual(carList.at(index)),
     );
