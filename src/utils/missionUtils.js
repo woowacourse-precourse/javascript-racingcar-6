c8b71e11ea4659cb3ef920bd3bb6a28b18e81977
kimmainsain <kmsid003@gmail.com>
2023-11-01 20:07:24 +0900

    :memo: Docs : woowa/mission/utils 사용할만한거 분리

---
+++ src/utils/missionUtils.js
@@ -0,0 +1,23 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const readLineAsync = async (message) => {
+  MissionUtils.Console.readLineAsync(message);
+};
+
+const print = (message) => {
+  MissionUtils.Console.print(message);
+};
+
+const pickNumberInRange = (startInclusive, endInclusive) => {
+  MissionUtils.Random.pickNumberInRange(startInclusive, endInclusive);
+};
+
+const pickUniqueNumbersInRange = (startInclusive, endInclusive, count) => {
+  MissionUtils.Random.pickUniqueNumbersInRange(
+    startInclusive,
+    endInclusive,
+    count
+  );
+};
+
+export { readLineAsync, print, pickNumberInRange, pickUniqueNumbersInRange };
