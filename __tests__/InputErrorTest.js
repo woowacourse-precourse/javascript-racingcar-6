b6277cb09529b496df8454b568c8173008419222
hzzzi <hji2004@naver.com>
2023-11-01 17:33:14 +0900

    사용자 입력 확인하는 테스트 구현

---
+++ __tests__/InputErrorTest.js
@@ -0,0 +1,31 @@
+import { isCarNameValid, isRoundCountValid } from '../src/App.js';
+
+describe('자동차 이름 입력: 예외 처리 테스트', () => {
+  test('이름이 5자 이상이면 오류 발생', () => {
+    expect(() => isCarNameValid('porsche')).toThrowError();
+  });
+
+  test('이름 미입력 시 오류 발생', () => {
+    expect(() => isCarNameValid('')).toThrowError();
+  });
+
+  test('자동차가 2대 미만이면 오류 발생', () => {
+    expect(() => isCarNameValid('pobi')).toThrowError();
+  });
+
+  test('자동차 이름이 중복되면 오류 발생', () => {
+    expect(() => isCarNameValid('pobi,pobi')).toThrowError();
+  });
+});
+
+describe('시도 횟수 입력: 예외 처리 테스트', () => {
+  test('숫자가 아닌 것 입력하면 오류 발생', () => {
+    expect(() => isRoundCountValid('abc')).toThrowError();
+  });
+  test('음수 입력하면 오류 발생', () => {
+    expect(() => isRoundCountValid('-3')).toThrowError();
+  });
+  test('정수가 아닌 수 입력하면 오류 발생', () => {
+    expect(() => isRoundCountValid('1.23')).toThrowError();
+  });
+});
