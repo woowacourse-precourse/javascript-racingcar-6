6f814444e529abed4f376ff5f472fdd267b56dba
mingi1123 <min_gi1123@naver.com>
2023-11-01 17:09:07 +0900

    feat: 자동차 이름 입력 함수

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Random, Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+    const car_names = await this.inputCarName();
+    Console.print(car_names.split(','));
+  }
+
+  async inputCarName() {
+    const car_names = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+    );
+    return car_names;
+  }
 }
 
 export default App;
