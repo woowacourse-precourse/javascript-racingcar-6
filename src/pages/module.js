78b19e89bff795aaaefb8b12390b6593b4a98121
HyeonChae1104 <211063@jnu.ac.kr>
2023-11-01 20:34:44 +0900

    Updated : "모듈화 파일 업데이트"

--- src/pages/module.js
+++ src/pages/module.js
@@ -2,6 +2,6 @@ import inputCarName from '../components/inputCarName.js';
 import inputMoveCount from '../components/inputMoveCount.js';
 import moveCar from '../components/carMoveControl.js';
 import printWinner from '../components/printWinner.js';
-import progressRacingCar from '../components/progressRacingCar.js';
 
-export { inputCarName, inputMoveCount, moveCar, printWinner, progressRacingCar };
+
+export { inputCarName, inputMoveCount, moveCar, printWinner };
