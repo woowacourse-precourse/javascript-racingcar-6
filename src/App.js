33bd3b2230bc081ebddaf1cadfd9b1e89f6848a3
이명욱 <myeong-uk@imyeong-ug-ui-MacBookPro.local>
2023-11-01 20:20:09 +0900

    우승자 출력

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ class RacingCar {
     this.name = name;
     this.position = 0;
   }
+
+  move() {
+    // 무작위로 0에서 9까지의 숫자를 선택하여 4 이상이면 전진
+    const randomNum = MissionUtils.Random.pickNumberInRange(0, 9);
+    if (randomNum >= 4) {
+      this.position++;
+    }
+  }
 }
 
 class RacingGame {
@@ -41,6 +49,37 @@ class RacingGame {
     this.cars = carNames.split(",").map((name) => new RacingCar(name));
     this.numberOfTries = numberOfTries;
   }
+
+  moveCars() {
+    // 자동차들을 움직임
+    for (const car of this.cars) {
+      car.move();
+    }
+  }
+
+  async printRaceResult() {
+    // 경주 결과 출력
+    await MissionUtils.Console.print("실행 결과\n");
+    for (const car of this.cars) {
+      const positionText = "-".repeat(car.position);
+      MissionUtils.Console.print(`${car.name} : ${positionText}\n`);
+    }
+    await MissionUtils.Console.print("\n");
+  }
+
+  async playGame() {
+    // 게임 실행: 시도 횟수만큼 자동차를 움직이고 결과 출력
+    for (let i = 0; i < this.numberOfTries; i++) {
+      this.moveCars();
+      await this.printRaceResult();
+    }
+  }
+  getWinners() {
+    // 가장 멀리 간 자동차를 우승자로 반환
+    const maxPosition = Math.max(...this.cars.map((car) => car.position));
+    const winners = this.cars.filter((car) => car.position === maxPosition);
+    return winners.map((winner) => winner.name).join(", ");
+  }
 }
 
 class App {
@@ -50,6 +89,10 @@ class App {
     const numberOfTries = await game.getNumberOfTriesFromUser();
     
     game.initialize(carNames, numberOfTries);
+    await game.playGame();
+    
+    // 최종 우승자 출력
+    await MissionUtils.Console.print("최종 우승자: " + game.getWinners());
   }
 }
 
