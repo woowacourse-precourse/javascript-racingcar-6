9d3ed984e9b2a1dc3a1d26dbc1e6c04842c4725d
Moon Dahyun <dahyun723@ajou.ac.kr>
2023-11-01 19:30:09 +0900

    Refactor:함수->메소드

--- src/App.js
+++ src/App.js
@@ -5,50 +5,49 @@ class App {
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     );
     const cars_arr = cars.split(",");
-    for (let i = 0; i < cars_arr.length; i++) {
-      if (cars_arr[i].trim().length > 5) {
-        throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
-      }
-    }
+    await this.getError(cars_arr);
 
-    let cars_win_count_num = new Array(cars_arr.length).fill(0);
-    let cars_win_count_line = new Array(cars_arr.length).fill("");
     const trial_count = await MissionUtils.Console.readLineAsync(
       "시도할 횟수는 몇 회인가요?"
     );
     MissionUtils.Console.print("실행결과");
-    startRace(trial_count);
-
-    // console.log(cars, "주어진");
-    // console.log(cars_arr, "자동차");
-    // console.log(cars_win_count_num, "숫자승리");
-    // console.log(cars_win_count_line, "작대기승리");
-
-    function startRace(count) {
-      for (let k = 0; k < count.length; k++) {
-        getWinLine(cars_arr);
-      }
-    }
-
-    function getWinLine(arr) {
-      for (let i = 0; i < arr.length; i++) {
-        const random = MissionUtils.Random.pickNumberInRange(0, 9);
-        let win_line = "";
-        if (random >= 4) {
-          cars_win_count_num[i]++;
-          for (let j = 0; j < cars_win_count_num[i]; j++) {
-            win_line += "-";
-          }
-        }
-
-        for (let i = 0; i < arr.length; i++) {
-          MissionUtils.Console.print(`${arr[i]} : ${cars_win_count_line[i]}`);
-        }
-        cars_win_count_line[i] = win_line;
-      }
-    }
-    //Todo: isWinner함수 작성하기
+    this.startRace(trial_count, cars_arr);
   }
+
+  async getError(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].trim().length > 5) {
+        throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+      }
+    }
+  }
+
+  async startRace(count, arr) {
+    for (let k = 0; k < count.length; k++) {
+      this.getWinLine(arr);
+    }
+  }
+
+  async getWinLine(arr) {
+    let cars_win_count_num = new Array(arr.length).fill(0);
+    let cars_win_count_line = new Array(arr.length).fill("");
+
+    for (let i = 0; i < arr.length; i++) {
+      const random = MissionUtils.Random.pickNumberInRange(0, 9);
+      let win_line = "";
+
+      if (random >= 4) {
+        cars_win_count_num[i]++;
+        win_line = "-".repeat(cars_win_count_num[i]);
+      }
+
+      for (let i = 0; i < arr.length; i++) {
+        MissionUtils.Console.print(`${arr[i]} : ${cars_win_count_line[i]}`);
+      }
+      cars_win_count_line[i] = win_line;
+    }
+  }
+  //Todo: isWinner함수 작성하기
 }
 
 export default App;
