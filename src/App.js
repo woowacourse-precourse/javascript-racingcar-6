a5b5a42457e8ae70ad9fe26b712c28778e81d17d
Hamin <ohm55545@naver.com>
2023-11-01 12:40:25 +0900

    feat(randomPlay) : 랜덤 숫자로 자동차가 전진할 지 말지 결정
    자동차 마다 현재 위치를 부여함
    랜덤으로 숫자를 부여(0 - 9) 후 4 이상이라면 전진
    이름과 현재 위치만큼 '-' 출력

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import getNameInput from './components/getNameInput.js';
 import getCountInput from "./components/getCountInput.js";
+import randomPlay from "./components/randomPlay.js"
 
 class App {
   async play() {
-    const inputNames = await getNameInput();
+    const inputCars = await getNameInput();
     const inputCount = await getCountInput();
-    MissionUtils.Console.print(inputNames);
-    MissionUtils.Console.print(inputCount);
+    MissionUtils.Console.print('');
+    MissionUtils.Console.print('실행 결과');
+    for (let index = 0; index < inputCount; index++) {
+      // foreach를 사용하면 포문 안에서 비동기를 동기적으로 사용 불가
+      // for of를 활용하여 해결
+      for (const car of inputCars) {
+        await randomPlay(car);
+      }
+      MissionUtils.Console.print('');
+    }
   }
 }
 
--- src/components/getNameInput.js
+++ src/components/getNameInput.js
@@ -4,6 +4,7 @@ async function getNameInput() {
   MissionUtils.Console.print('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
   const inputNames = await MissionUtils.Console.readLineAsync('');
   const namesArray = inputNames.split(',');
+  const carArray = []
   if (namesArray.length === 1) {
     throw new Error ('[ERROR] 자동차 이름을 두 개 이상 입력하세요');
   };
@@ -11,8 +12,9 @@ async function getNameInput() {
     if (e.length > 5) {
       throw new Error('[ERROR] 자동차 이름이 너무 깁니다.')
     };
+    carArray.push([e, 0])
   });
-  return namesArray;
+  return carArray;
 };
 
 export default getNameInput;
