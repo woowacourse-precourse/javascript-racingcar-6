98882f54b0fd323ced41eb8ede8ac99628aee973
Jinhyung Lee <luciancah@gmail.com>
2023-11-01 17:04:45 +0900

    feat: App Class에 초기화 및 시작 로직 추가
    - CarRacingController 인스턴스 가진 App 클래스 정의
    - play 메소드에 게임 시작 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import CarRacingController from './controllers/CarRacingController';
+
 class App {
-  async play() {}
+  constructor() {
+    this.controller = new CarRacingController();
+  }
+
+  async play() {
+    const raceCount = await this.controller.getAndValidateInput();
+    this.controller.playRacingGame(raceCount);
+  }
 }
 
 export default App;
