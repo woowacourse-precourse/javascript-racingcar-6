682794a23b711b8d1624b4cc5da48a5a66609643
ppacman <smwfkim2@naver.com>
2023-11-01 21:39:17 +0900

    feat  : carName - 자동차 이름 5글자 초과 유효성 검사 추가

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ class App {
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
     this.cars = carInput.split(",");
+    if (this.cars.some((car) => car.length > 5)) {
+      //cars배열안에 5글자가 넘는 이름이 있는지 검사
+      throw new Error("[ERROR] 자동차의 이름은 5자 이하여야 합니다.");
+    }
   }
 
   async play() {}
