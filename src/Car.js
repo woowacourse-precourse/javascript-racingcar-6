fa552064c7e12970d991f97a618b975df92c9679
thgus5335 <thgus5335@naver.com>
2023-11-01 20:15:12 +0900

    feat: Car 클래스 구현

--- src/Car.js
+++ src/Car.js
@@ -1,5 +1,25 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { MIN_RANGE, MAX_RANGE, MARK_MOVE } from './constants.js';
+
 class Car {
+    constructor(carName) {
+        this.carName = carName;
+        this.carMove = '';
+      }
     
+    moveCar() {
+    const randomNum = MissionUtils.Random.pickNumberInRange(MIN_RANGE, MAX_RANGE);
+    if (randomNum >= 4)
+        this.carMove += MARK_MOVE;
+    }
+
+    getCarName() {
+        return this.carName;
+    }
+
+    getCarMove() {
+        return this.carMove;
+    }
 }
 
 export default Car;
