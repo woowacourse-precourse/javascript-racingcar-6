8b4f0c5fc996fd2ff95f9c424434ce303061769b
swarvy <swarvy0826@naver.com>
2023-11-01 14:17:38 +0900

    feat : 소수와 음수 시행횟수를 검사하는 로직 작성

--- src/validator/LapNumberValidator.js
+++ src/validator/LapNumberValidator.js
@@ -13,11 +13,33 @@ const checkInvalidCharacter = (input) => {
 };
 
 /**
+ * @throws {ValidationError}
+ * @param {string} input
+ */
+const checkIntegerNumber = (input) => {
+  if (!Number.isInteger(Number(input))) {
+    throw new ValidationError(GRANDPRIX_ERROR_NOTIFICATION.invalidNumber);
+  }
+};
+
+/**
+ * @throws {ValidationError}
+ * @param {string} input
+ */
+const checkNegativeIntegerNumber = (input) => {
+  if (Number(input) < 0) {
+    throw new ValidationError(GRANDPRIX_ERROR_NOTIFICATION.invalidInteger);
+  }
+};
+
+/**
  * @param {string} input - 검사할 문자열
  */
 const validateLapNumber = (input) => {
   validateCommon(input);
   checkInvalidCharacter(input);
+  checkIntegerNumber(input);
+  checkNegativeIntegerNumber(input);
 };
 
 export default validateLapNumber;
