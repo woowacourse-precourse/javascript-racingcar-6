841fd770c78738c10fed1a4e49bd096971e06f1c
2NNS-V <msy3824@naver.com>
2023-11-01 21:22:31 +0900

    feat: 자동차 이름 입력 및 오류 검사

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  constructor() {
+    this.car = [];
+    this.data = [];
+    this.winner = [];
+    this.attempt = 0;
+  }
+
+  async getCarName() {
+    const carName = (
+      await Console.readLineAsync(
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
+      )
+    ).split(",");
+    App.CarNameValidator(carName);
+  }
+  
+  static CarNameValidator(CarName) {
+    if (input.length > 5) {
+      throw new Error("[ERROR] 이름은 5자 이하가 되어야 합니다.");
+    }
+  }
+  
+  async play() {
+  }
 }
 
 export default App;
