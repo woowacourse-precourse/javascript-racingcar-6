222fe8a4bbcf2ca042584a12b112789988bb2e9d
MacBook Air <macbookair@MacBookui-MacBookAir.local>
2023-11-01 15:03:36 +0900

    Fix: ApplicationTest ApplicationTest.js 테스트코드에서 발생하는 오류 수정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import { Console } from "@woowacourse/mission-utils";
 import * as MissionUtils from "@woowacourse/mission-utils";
 
-function findError(){
-  try{
-    throw new Error("[ERROR] 자동차 이름은 5자를 초과할 수 없습니다.");
-  }catch(e){
-    console.log(e.message);
-  }
-}
-
 function getRandom(carNum,moveArray){
   for(let i = 0; i<carNum;i++){
     const number = MissionUtils.Random.pickNumberInRange(0,9);
@@ -20,11 +12,12 @@ function getRandom(carNum,moveArray){
 
 function printResult(nameArray, moveArray){
   for(let i =0; i<nameArray.length; i++){
-    process.stdout.write(nameArray[i] + " : ");
+    var hypen='';
+    hypen = hypen + nameArray[i] + ' : ';
     for(let j =0;j<moveArray[i];j++){
-      process.stdout.write("-");
+      hypen +='-';
     }
-    process.stdout.write("\n");
+    MissionUtils.Console.print(hypen);
   }
 }
 
@@ -43,28 +36,26 @@ function findMax(nameArray, moveArray, maxIdx){
 class App {
   async play() {
     const userInput = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
-    const carName = userInput.split(",");
+    const carNames = userInput.split(",");
     
-    for(let i =0;i<carName.length;i++){
-      if(carName[i].length>5){
-        findError();
-        return;
+    for(const carName of carNames){
+      if(carName.length>5){
+        throw new Error("[ERROR]");
       }
     }
 
     const userNum = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
     var move = new Array();
+    MissionUtils.Console.print("\n실행 결과");
 
-    while(move.length < carName.length){
-      move.push(0);
-    } // 배열 초기화
+    for(let i =0;i<carNames.length;i++){
+      move[i]=0;
+    }
 
-    process.stdout.write("\n");
-    console.log("실행 결과");
     for(let i=0; i<userNum;i++){
-      getRandom(carName.length, move);
-      printResult(carName,move);
-      process.stdout.write("\n");
+      getRandom(carNames.length, move);
+      printResult(carNames,move);
+      MissionUtils.Console.print(" ");
     }
 
     let max = move[0];
@@ -76,7 +67,7 @@ class App {
       }
     }
 
-    const winner = findMax(carName, move, maxIndex);
+    const winner = findMax(carNames, move, maxIndex);
 
     process.stdout.write("최종 우승자 : ");
     for(let i =0;i<winner.length;i++){
