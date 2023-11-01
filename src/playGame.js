f0fadc5380864247513a9cdb0d63ce79eed15aa6
JunSeok <ppu1234@naver.com>
2023-11-01 23:31:28 +0900

    style: 불필요한 코드 삭제

--- src/playGame.js
+++ src/playGame.js
@@ -2,7 +2,7 @@ import { Console } from '@woowacourse/mission-utils';
 import { INFO_MESSAGE } from './Util/message.js';
 import printLog from './View/printLog.js';
 
-const playGame = async (playNumber, carName) => {
+const playGame = (playNumber, carName) => {
   Console.print(INFO_MESSAGE.result);
   while (playNumber--) printLog(carName);
 };
