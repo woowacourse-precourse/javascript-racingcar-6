08db53112c6827f7005906a68ae3269fe58d79ea
pjc0179 <pjc0179@gmail.com>
2023-11-01 20:22:46 +0900

    feat: index.js try catch 추가

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-import App from "./App.js";
+import { Console } from '@woowacourse/mission-utils';
+import App from './App.js';
 
 const app = new App();
-app.play();
+try {
+  await app.play();
+} catch (error) {
+  Console.print(`${error}`);
+}
