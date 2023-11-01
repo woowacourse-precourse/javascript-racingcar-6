a99dce1173450c99f82bfefae4ecb3c77bbdbdb9
JunSeok <ppu1234@naver.com>
2023-11-01 23:31:43 +0900

    refactor: forEach 함수 사용

--- src/View/printLog.js
+++ src/View/printLog.js
@@ -2,10 +2,10 @@ import { Console } from '@woowacourse/mission-utils';
 import checkRandomNumber from '../Util/randomNumber.js';
 
 const printLog = (carName) => {
-  for (const [name, moveLog] of carName) {
+  carName.forEach((moveLog, name) => {
     if (checkRandomNumber()) carName.set(name, moveLog + '-');
     Console.print(`${name} : ${carName.get(name)}`);
-  }
+  });
   Console.print(' ');
 };
 
