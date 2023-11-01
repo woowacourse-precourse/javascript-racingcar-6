dbe2094bc06d29a733c822a4cf7ec63c1f25cdad
swarvy <swarvy0826@naver.com>
2023-11-01 14:17:11 +0900

    feat : 에러 메시지 추가

--- src/constants/GrandPrixError.js
+++ src/constants/GrandPrixError.js
@@ -5,4 +5,6 @@ export const GRANDPRIX_ERROR_NOTIFICATION = Object.freeze({
   invalidCharactor: '시도 횟수는 숫자만 입력할 수 있습니다.',
   invalidNameLength: '5자 초과의 자동차 이름 또는 공백은 입력할 수 없습니다.',
   duplicatedName: '중복된 자동차 이름은 입력할 수 없습니다.',
+  invalidNumber: '시행 횟수로 소수는 입력할 수 없습니다.',
+  invalidInteger: '시행 횟수로 음수는 입력할 수 없습니다.',
 });
