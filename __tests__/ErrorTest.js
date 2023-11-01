77d12a2593da0528951d018af017506a93d996ac
hyj5076 <hyj5076@hanmail.net>
2023-11-01 16:35:16 +0900

    test(App): 예외 발생 시 게임 종료 테스트

--- __tests__/ErrorTest.js
+++ __tests__/ErrorTest.js
@@ -1,17 +1,40 @@
 import UserInputCarName from "../src/utils/UserInputCarName";
+import App from "../src/App";
+import { Console } from "@woowacourse/mission-utils";
 
-describe("자동차 이름 입력", () => {
+jest.mock("@woowacourse/mission-utils");
+
+describe("예외 처리", () => {
   let userInputCarName;
 
   beforeEach(() => {
     userInputCarName = new UserInputCarName();
   });
 
-  test("사용자가 자동차 이름을 잘못 입력했을 경우 예외 발생", () => {
+  test("사용자가 자동차 이름을 잘못 입력했을 경우 에러 던짐", () => {
     const invalidCarNames = ["mini", "bentley"];
 
     expect(() => {
       userInputCarName.setCarNames(invalidCarNames);
     }).toThrowError("[ERROR]");
+  });
+});
+
+describe("게임 종료", () => {
+  let app;
+
+  beforeEach(() => {
+    app = new App();
+    Console.readLineAsync.mockResolvedValue("mini,bentley");
+  });
+
+  test("사용자가 자동차 이름을 잘못 입력했을 경우 게임 종료", async () => {
+    const endGameSpy = jest.spyOn(app, "endGame");
+
+    try {
+      await app.play();
+    } catch (error) {}
+
+    expect(endGameSpy).toHaveBeenCalled();
   });
 });
