834fceedbb0d44a846d4a9cfcbb539b317af1551
novice0840 <magnolia0840@gmail.com>
2023-11-01 12:26:22 +0900

    feat: 자동차 이름 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Random, Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  constructor() {
+    this.carsStatus = {};
+    this.tryNumber = 0;
+  }
+
+  async inputCarsStatus() {
+    this.carsStatus = await Console.readLineAsync();
+  }
+
+  async inputTryNumber() {
+    this.tryNumber = await Console.readLineAsync();
+  }
+
+  async play() {
+    await this.inputCarsStatus();
+    await this.inputTryNumber();
+  }
 }
 
 export default App;
