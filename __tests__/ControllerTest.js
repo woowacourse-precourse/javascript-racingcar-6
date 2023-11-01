36dc93146cb4163ce5d9ad99a8ee45695db2e3a7
rlaltj <we00425@gmail.com>
2023-11-01 12:04:07 +0900

    Test: 추가 - 전진 실행 및 결과 테스트

--- __tests__/ControllerTest.js
+++ __tests__/ControllerTest.js
@@ -10,6 +10,19 @@ const mockQuestions = (inputs) => {
   });
 };
 
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
 describe("경주 조건 입력: 자동차 이름", () => {
   test("이름 입력 받기", async () => {
     const inputs = ["kim,hee,seo"];
@@ -48,4 +61,170 @@ describe("경주 조건 입력: 자동차 이름", () => {
       await expect(controller.getCarNames()).rejects.toThrow("[ERROR]");
     }
   );
+});
+
+describe("경주 조건 입력: 시도 횟수", () => {
+  test("시도 횟수 입력 받기", async () => {
+    const inputs = [3];
+    mockQuestions(inputs);
+
+    const controller = new RaceController();
+    await controller.getNumOfTry();
+
+    expect(controller.numOfTry).toEqual(3);
+  });
+
+  test.each([0, -1, "%"])(
+    "시도횟수에 대한 예외 처리 - 양의 정수 아님",
+    async (inputs) => {
+      // given
+      mockQuestions(inputs);
+
+      // when
+      const controller = new RaceController();
+
+      // then
+      await expect(controller.getNumOfTry()).rejects.toThrow("[ERROR]");
+    }
+  );
+});
+
+describe("전진 실행 및 결과 출력", () => {
+  test("전진 실행 - 한 단계", async () => {
+    const randoms = [9, 8, 1];
+    mockRandoms([...randoms]);
+
+    const controller = new RaceController();
+    controller.cars = [
+      {
+        name: "kim",
+        point: 0,
+      },
+      {
+        name: "hee",
+        point: 0,
+      },
+      {
+        name: "seo",
+        point: 0,
+      },
+    ];
+    controller.getPoints();
+
+    expect(controller.cars).toEqual([
+      {
+        name: "kim",
+        point: 1,
+      },
+      {
+        name: "hee",
+        point: 1,
+      },
+      {
+        name: "seo",
+        point: 0,
+      },
+    ]);
+  });
+
+  test("전진 실행", async () => {
+    const randoms = [4, 5, 6, 3, 4, 5, 1, 2, 9];
+    mockRandoms([...randoms]);
+
+    const controller = new RaceController();
+    controller.cars = [
+      {
+        name: "kim",
+        point: 0,
+      },
+      {
+        name: "hee",
+        point: 0,
+      },
+      {
+        name: "seo",
+        point: 0,
+      },
+    ];
+    controller.numOfTry = 3;
+    controller.moveForward();
+
+    expect(controller.cars).toEqual([
+      {
+        name: "kim",
+        point: 1,
+      },
+      {
+        name: "hee",
+        point: 2,
+      },
+      {
+        name: "seo",
+        point: 3,
+      },
+    ]);
+  });
+
+  test("전진 결과 출력 - 한 단계", async () => {
+    const randoms = [9, 8, 1];
+    const outputs = ["kim : -\nhee : -\nseo : "];
+    const logSpy = getLogSpy();
+
+    mockRandoms([...randoms]);
+
+    const controller = new RaceController();
+    controller.cars = [
+      {
+        name: "kim",
+        point: 0,
+      },
+      {
+        name: "hee",
+        point: 0,
+      },
+      {
+        name: "seo",
+        point: 0,
+      },
+    ];
+    controller.getPoints();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("전진 결과 출력", async () => {
+    const randoms = [4, 5, 6, 3, 4, 5, 1, 2, 9];
+    const outputs = [
+      "kim : -\nhee : -\nseo : -",
+      "kim : -\nhee : --\nseo : --",
+      "kim : -\nhee : --\nseo : ---",
+    ];
+    const logSpy = getLogSpy();
+
+    mockRandoms([...randoms]);
+
+    const controller = new RaceController();
+    controller.cars = [
+      {
+        name: "kim",
+        point: 0,
+      },
+      {
+        name: "hee",
+        point: 0,
+      },
+      {
+        name: "seo",
+        point: 0,
+      },
+    ];
+    controller.numOfTry = 3;
+    controller.moveForward();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
 });
