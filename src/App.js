c4a26dd0b9051b287b05fcb0835867cb4fc0d6c1
ppacman <smwfkim2@naver.com>
2023-11-01 21:41:17 +0900

    feat : getTrial - 시도할 횟수 입력 및 초기화

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Random, Console } from "@woowacourse/mission-utils";
 class App {
   cars;
   carResults = [];
+  numberOfTrials;
   async getCarName() {
     const carInput = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
@@ -18,6 +19,12 @@ class App {
     }
 
     this.carResults = new Array(this.cars.length).fill("");
+  }
+  async getTrial() {
+    const trialNumber = await Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요?\n"
+    );
+    this.trialNumber = trialNumber;
   }
 
   async play() {}
