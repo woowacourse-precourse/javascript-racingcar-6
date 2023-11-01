2d643fd0f2e8ab609e851c5c8df819cf2ddc21a5
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-11-01 19:21:43 +0900

    feat: 자동차 이름 입력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from '@woowacourse/mission-utils';
+import { MESSAGES } from './utils/constants.js';
+
 class App {
-  async play() {}
+  async play() {
+    await this.setupGame();
+  }
+
+  async setupGame() {
+    const racingCarNames = await Console.readLineAsync(MESSAGES.INPUT_NAMES);
+  }
 }
 
 export default App;
---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+export const MESSAGES = {
+  INPUT_NAMES:
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+};
