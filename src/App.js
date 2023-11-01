2760e754db0ef0c8f32a435d3de31b2f462f2598
greatSweetMango <jhkim0097@gmail.com>
2023-11-01 18:41:27 +0900

    feat: 자동차이름 입력 파싱

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from '@woowacourse/mission-utils'
+
 class App {
-  async play() {}
+  #Cars = [];
+
+  parseCarNames() {
+    const input = Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    return input.split(',');
+  }
 }
 
 export default App;
