d323f2fd4f67aed26bc5c44735c95eea19d1413c
이명욱 <myeong-uk@imyeong-ug-ui-MacBookPro.local>
2023-11-01 12:19:27 +0900

    1. n대의 자동차 이름을 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+class RacingCar {
+  constructor(name) {
+    if (name.length > 5) {
+      throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+    }
+    this.name = name;
+  }
+}
+
 class App {
-  async play() {}
+  async play() {
+    const carNames = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분): ");
+  }
 }
 
 export default App;
