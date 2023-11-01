8478dbae2f0f5fd6b1b80e6750e6aa29535700b5
yeokyoung song <spolice0324@gmail.com>
2023-11-01 15:50:48 +0900

    test : 함수 명 변경에 따라 수정
    - import 하는 함수 명 수정
    - 함수 기능 테스트에 대한 설명 수정
    - 불필요한 describe 구성 제거

--- __tests__/UtilsTest.js
+++ __tests__/UtilsTest.js
@@ -3,34 +3,29 @@ import formatOutput from '../src/utils/formatOutput';
 import getRandomNumber from '../src/utils/getRandomNumber';
 
 describe('유틸 함수 테스트', () => {
-  describe('getData 테스트', () => {
-    describe('getRoundData 테스트', () => {
-      test('getRoundData 테스트', () => {
-        const cars = [new Car('a'), new Car('b'), new Car('c')];
-        cars[0].forward();
-        cars[0].forward();
-        cars[1].forward();
-        const result = formatOutput.convertRoundResultToString(cars);
-        expect(result).toBe('a : --\nb : -\nc : \n');
-      });
-    });
-    describe('getMaxForwardData 테스트', () => {
-      test('getMaxForwardData 테스트', () => {
-        const cars = [new Car('a'), new Car('b'), new Car('c')];
-        cars[0].forward();
-        cars[0].forward();
-        cars[1].forward();
-        const result = formatOutput.findWinningCars(cars);
-        expect(result).toEqual(['a']);
-      });
+  describe('formatOutPut 테스트', () => {
+    test('converRoundResultToString 테스트 - 경주 라운드 결과를 문자열 형태로 반환', () => {
+      const cars = [new Car('a'), new Car('b'), new Car('c')];
+      cars[0].forward();
+      cars[0].forward();
+      cars[1].forward();
+      const result = formatOutput.convertRoundResultToString(cars);
+      expect(result).toBe('a : --\nb : -\nc : \n');
     });
 
-    describe('getWinnerData 테스트', () => {
-      test('getWinnerData 테스트', () => {
-        const winners = ['a', 'b', 'c'];
-        const result = formatOutput.makeWinnerMessage(winners);
-        expect(result).toBe('최종 우승자 : a, b, c');
-      });
+    test('findWinningCars 테스트 - 가장 많이 전진한 자동차 반환', () => {
+      const cars = [new Car('a'), new Car('b'), new Car('c')];
+      cars[0].forward();
+      cars[0].forward();
+      cars[1].forward();
+      const result = formatOutput.findWinningCars(cars);
+      expect(result).toEqual(['a']);
+    });
+
+    test('makeWinnerMessage 테스트 - 최종 우승자 이름 및 출력 메세지를 조합해 결과 출력 문자열 반환', () => {
+      const winners = ['a', 'b', 'c'];
+      const result = formatOutput.makeWinnerMessage(winners);
+      expect(result).toBe('최종 우승자 : a, b, c');
     });
   });
 
