c8e95103280625a3489defe3425982e2e6379270
hyunyeee <5004ohy@naver.com>
2023-11-01 14:49:02 +0900

    feat: 사용자가 잘못된 값을 입력한 경우 예외를 발생시킨후 종료한다.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ const CAR_NAME_MESSAGE =
   "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
 const ATTEMP_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?\n";
 const WINNER_MESSAGE = "최종 우승자 : ";
+const ERROR_MESSAGE_NAME_LENGTH = "[ERROR] 이름은 5자 이하만 가능합니다.";
+const ERROR_MESSAGE_EMPTY = "[ERROR] 이름은 공백만으로 구성될 수 없습니다.";
+const ERROR_MESSAGE_ATTEMPT_TYPE = "[ERROR] 시도 횟수는 숫자여야 합니다.";
 
 class Car {
   constructor(name) {
@@ -29,21 +32,42 @@ class App {
   }
 
   async #controller() {
-    const carNames = await this.#readCarNames();
-    const cars = carNames.map((car) => new Car(car.trim()));
-    const attempCount = await this.#readAttempCount();
-    await this.#race(cars, attempCount);
+    try {
+      const carNames = await this.#readCarNames();
+      this.#validateName(carNames);
+      const cars = carNames.map((car) => new Car(car.trim()));
+      const attempCount = await this.#readAttempCount();
+      await this.#race(cars, attempCount);
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
+      throw error;
+    }
   }
 
   async #readCarNames() {
     const cars = await MissionUtils.Console.readLineAsync(CAR_NAME_MESSAGE);
-    return cars.trim().split(",");
+    return cars.split(",").map((name) => name.trim());
+  }
+
+  #validateName(carNames) {
+    carNames.forEach((name) => {
+      if (name.length === 0) {
+        throw new Error(ERROR_MESSAGE_EMPTY);
+      }
+      if (name.length > 5) {
+        throw new Error(ERROR_MESSAGE_NAME_LENGTH);
+      }
+    });
   }
 
   async #readAttempCount() {
-    const result =
+    const input =
       await MissionUtils.Console.readLineAsync(ATTEMP_COUNT_MESSAGE);
-    return result.trim();
+    const attemptCount = parseInt(input.trim(), 10);
+    if (isNaN(attemptCount) || attemptCount <= 0) {
+      throw new Error(ERROR_MESSAGE_ATTEMPT_TYPE);
+    }
+    return attemptCount;
   }
 
   async #race(cars, attemptCount) {
