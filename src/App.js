908d80b4be8f081d081b334f373e84f19fd7efcc
mingyeong:D <63152086+Kim-Mingyeong@users.noreply.github.com>
2023-11-01 20:15:37 +0900

    feat: 자동차 이름과 함께 전진한 결과 출력 기능, 우승자 발표 기능

--- src/App.js
+++ src/App.js
@@ -2,33 +2,38 @@ import { Random, Console } from "@woowacourse/mission-utils";
 import { GAME, ERROR } from "./message";
 
 class App {
-  validateCarName = (carNamesArray) => {
-    carNamesArray.forEach((carName) => {
+  constructor() {
+    this.carsName = this.getCarsName();
+    this.position = 0;
+  }
+  validateCarName = (carsNameArray) => {
+    carsNameArray.forEach((carName) => {
       if (carName.length > 5) {
         throw new Error(ERROR.CAR_NAME);
       }
     });
   };
 
-  validateNumber = (attemptCount) => {
-    if (isNaN(attemptCount)) {
+  validateNumber = (attemptNumber) => {
+    if (isNaN(attemptNumber) || attemptNumber <= 0) {
       throw new Error(ERROR.ATTEMPT_COUNT);
     }
   };
 
-  getCarName = async () => {
-    const carNames = await Console.readLineAsync(GAME.GET_CAR_NAME);
-    const carNamesArray = carNames.split(",");
-    this.validateCarName(carNamesArray);
+  getCarsName = async () => {
+    const carsName = await Console.readLineAsync(GAME.GET_CAR_NAME);
+    const carsNameArray = carsName.split(",");
+    this.validateCarName(carsNameArray);
 
-    return carNamesArray;
+    return carsNameArray;
   };
 
   getAttemptCount = async () => {
     const attemptCount = await Console.readLineAsync(GAME.GET_ATTEMPT_COUNT);
-    this.validateNumber(attemptCount);
+    const attemptNumber = Number(attemptCount);
+    this.validateNumber(attemptNumber);
 
-    return attemptCount;
+    return attemptNumber;
   };
 
   generateRandomNumber = () => {
@@ -38,14 +43,45 @@ class App {
 
   moveForwardCondition = () => {
     const randomNumber = this.generateRandomNumber();
-    let position = 0;
     if (randomNumber >= 4) {
       position += 1;
     }
     return position;
   };
 
-  async play() {}
+  moveForward = () => {
+    this.carsName.forEach((car) => car.carMove);
+    Console.print(`${car.carsName} : ${"-".repeat(car.position)}`);
+  };
+
+  selectWinnerByRace = () => {
+    const maxCarPosition = Math.max(...this.cars.map((car) => car.position));
+    const winnerCars = this.carsName.filter(
+      (car) => car.position === maxCarPosition
+    );
+    return winnerCars.map((car) => car.carName);
+  };
+
+  racingGame = () => {
+    const attemptCount = this.getAttemptCount();
+    while (attemptCount > 0) {
+      this.moveForward();
+      attemptCount -= 1;
+    }
+    const winners = this.selectWinnerByRace();
+    if (winners.length > 1) {
+      Console.print(`${GAME.WINNER}${winners.join(", ")}`);
+    } else {
+      Console.print(`${GAME.WINNER}${winners}`);
+    }
+  };
+
+  async play() {
+    await this.getCarsName();
+    await this.getAttemptCount();
+    Console.print(GAME.PRINT_EXECUTION_RESULT);
+    this.racingGame();
+  }
 }
 
 export default App;
