62cc32d829df6f05399a8227d1e733e5e6d55cfe
소인섭 <sis9745@gachon.ac.kr>
2023-11-01 16:23:38 +0900

    feat: 자동차 경주의 경주 기능 추가
    - race 함수로 경주 기능 구현
      - 함수가 호출되면, 먼저 '실행 결과' 출력
      - attempts 만큼 반복하여 진행
      - racingCars에 저장된 RacingCar 인스턴스에서 각각 move, printPosition 함수 호출

--- src/CarRace.js
+++ src/CarRace.js
@@ -1,9 +1,21 @@
 import RacingCar from './RacingCar';
+import { MissionUtils } from '@woowacourse/mission-utils';
 
 class CarRace {
   constructor(racingCarsInput, attemptsInput) {
     this.racingCars = this.#initRacingCars(racingCarsInput);
     this.attempts = Number(attemptsInput);
+  }
+
+  race() {
+    MissionUtils.Console.print('실행 결과');
+    for (let i = 0; i < this.attempts; i++) {
+      this.racingCars.forEach((racingCar) => {
+        racingCar.move();
+        racingCar.printPosition();
+        MissionUtils.Console.print();
+      });
+    }
   }
 
   #initRacingCars(racingCarsInput) {
