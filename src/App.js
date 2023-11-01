9e7021cefbace55195fa9536d28046edf815a85d
KangYunJi <dbswl701@ajou.ac.kr>
2023-11-01 14:39:48 +0900

    refactor: 입력 받을 시 출력 문구 변경

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Car from './Car.js';
 class App {
   async play() {
     // 자동차 이름 입력
-    const carStr = await MissionUtils.Console.readLineAsync('닉네임을 입력해주세요.');
+    const carStr = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
     const carArr = carStr.split(',');
     
     if (carArr.length === 1) throw new Error("[ERROR] 자동차는 2개 이상이어야 합니다.");
@@ -15,7 +15,7 @@ class App {
     })
 
     // 시도 횟수 입력
-    const tryCount = parseInt(await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?'));
+    const tryCount = parseInt(await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n'));
     if (!/^\d+$/.test(tryCount)) throw new Error("[ERROR] 시도 횟수는 숫자만 입력해야 합니다.");
     if (tryCount === 0) throw new Error("[ERROR] 시도 횟수는 0 이상이어야 합니다.");
 
