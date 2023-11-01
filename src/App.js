d72c07a14b14c62e4e541884b5fa7dfee1448015
mingi1123 <min_gi1123@naver.com>
2023-11-01 20:25:07 +0900

    refactor: 적절한 변수명으로 변경

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ class App {
   }
 
   async inputCarName() {
-    const car_names = await Console.readLineAsync(
+    const carNames = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
     );
-    return car_names;
+    return carNames;
   }
 
   async inputMoveCount() {
@@ -49,8 +49,8 @@ class App {
   }
 
   printWinner(winners) {
-    const winnersStr = winners.join(', ');
-    Console.print(`최종 우승자 : ${winnersStr}\n`);
+    const winnersString = winners.join(', ');
+    Console.print(`최종 우승자 : ${winnersString}\n`);
   }
 }
 
--- src/Car.js
+++ src/Car.js
@@ -3,16 +3,16 @@ import { Random, Console } from '@woowacourse/mission-utils';
 class Car {
   constructor(carName) {
     this.carName = carName;
-    this.count = 0;
+    this.score = 0;
     this.state = [];
   }
 
   move() {
     const number = Random.pickNumberInRange(0, 9);
     if (number >= 4) {
-      this.count++;
+      this.score++;
     }
-    this.state.push('-'.repeat(this.count));
+    this.state.push('-'.repeat(this.score));
   }
 
   printState(index) {
