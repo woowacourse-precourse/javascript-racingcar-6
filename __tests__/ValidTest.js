44fd9209e181ab23fa382cbc94ffd249771ff394
Youjiiin <yujinjang24@gmail.com>
2023-11-01 14:38:11 +0900

    style: 테스트코드에서의 상수 대문자로 수정

--- __tests__/ValidTest.js
+++ __tests__/ValidTest.js
@@ -14,15 +14,15 @@ const mockQuestions = (inputs) => {
 describe('사용자의 입력값에 대한 유효성 테스트', () => {
   //자동차 이름 유효성 검사
   test('유효한 자동차 이름을 입력하면 유효성 검사를 통과한다', async () => {
-    const carInput = 'pobi,crong,woong';
-    const result = carInput.split(',');
+    const CAR_INPUT = 'pobi,crong,woong';
+    const RESULT = CAR_INPUT.split(',');
 
     //임의 입력
-    mockQuestions([carInput]);
+    mockQuestions([CAR_INPUT]);
 
     const name = await getCarNames();
     const validatedName = checkValidInput(name);
-    expect(validatedName).toEqual(result);
+    expect(validatedName).toEqual(RESULT);
   });
 
   test.each([
@@ -44,14 +44,14 @@ describe('사용자의 입력값에 대한 유효성 테스트', () => {
 
   //시도횟수 유효성 검사
   test('유효한 시도 횟수를 입력하면 테스트를 통과한다.', async () => {
-    const tryInput = 2;
-    const result = 2;
+    const COUNT_INPUT = 2;
+    const RESULT = 2;
 
-    mockQuestions([tryInput]);
+    mockQuestions([COUNT_INPUT]);
 
     const count = await getAttemptCount();
     const validatedCount = checkValidCount(count);
-    expect(validatedCount).toEqual(result);
+    expect(validatedCount).toEqual(RESULT);
   });
   test.each(['', 'a'])(
     '유효하지 않은 시도 횟수를 입력하면 그에 해당하는 에러메시지를 출력한다.',
--- __tests__/WinnerTest.js
+++ __tests__/WinnerTest.js
@@ -9,29 +9,29 @@ const getLogSpy = () => {
 
 describe('게임 우승자 선정', () => {
   test('게임 진행이후 주어진 객체에서 value 값이 가장 길이가 긴, 가장 많이 전진한 자동차를 우승자로 선정, 출력한다.', () => {
-    const input = {
+    const CARS = {
       car1: '---',
       car2: '-',
       car3: '',
     };
-    const result = '최종 우승자 : car1';
+    const RESULT = '최종 우승자 : car1';
     const logSpy = getLogSpy();
-    findWinner(input);
+    findWinner(CARS);
 
-    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(result));
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(RESULT));
   });
 
   test('공동우승 시 최종 우승자 두명이상 출력한다.', () => {
-    const input = {
+    const CARS = {
         car1: '---',
         car2: '-',
         car3: '---',
     };
 
-    const result = '최종 우승자 : car1, car3';
+    const RESULT = '최종 우승자 : car1, car3';
     const logSpy = getLogSpy();
-    findWinner(input);
+    findWinner(CARS);
 
-    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(result));
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(RESULT));
   });
 });
