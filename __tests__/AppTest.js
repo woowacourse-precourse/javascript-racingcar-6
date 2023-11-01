f175b03395e859fcc851981413271ad5fe49c5d1
Seoan Yoon <earthdyoon@gmail.com>
2023-11-01 21:42:06 +0900

    feat: 경주 진행 상황 출력 테스트 코드 작성

---
+++ __tests__/AppTest.js
@@ -0,0 +1,50 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import App from '../src/App';
+
+const mockQuestions = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  });
+};
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+
+  numbers.reduce(
+    (acc, number) => acc.mockReturnValueOnce(number),
+    MissionUtils.Random.pickNumberInRange,
+  );
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe('경주 테스트', () => {
+  test('경주 진행 상황 출력', async () => {
+    // given
+    const MOVING_FORWARD = 4;
+    const STOP = 3;
+    const inputs = ['foo,bar,oh', '1'];
+    const outputs = ['foo : ', 'bar : ', 'oh : -'];
+    const randoms = [STOP, STOP, MOVING_FORWARD];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([...randoms]);
+
+    // when
+    const app = new App();
+    await app.play();
+
+    // then
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+});
