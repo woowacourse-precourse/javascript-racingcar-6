096012b8098ae6d5aed87b9730305ba8f1dbf599
ppacman <smwfkim2@naver.com>
2023-11-01 21:38:30 +0900

    feat : carName - 자동차 이름 입력 및 변수 선언

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import { Random, Console } from "@woowacourse/mission-utils";
 
 class App {
+  cars;
+  async getCarName() {
+    const carInput = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    this.cars = carInput.split(",");
+  }
+
   async play() {}
 }
 
