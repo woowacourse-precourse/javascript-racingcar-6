cafe2f24d206555cd561dec019ff463e8e09e3ee
이명욱 <myeong-uk@imyeong-ug-ui-MacBookPro.local>
2023-11-01 20:01:52 +0900

    n대의 자동차 이름 입력 받기

--- src/App.js
+++ src/App.js
@@ -2,68 +2,34 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class RacingCar {
   constructor(name) {
-    if (name.length > 5) {
-      throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
-    }
     this.name = name;
     this.position = 0;
-  }
-
-  move(){
-    const randomNum = MissionUtils.Random.pickNumberInRange(0, 9);
-    if (randomNum >= 4) {
-      this.position++;
-    }
   }
 }
 
 class RacingGame {
-  constructor(carNames, numberOfTries) {
-    this.cars = carNames.split(",").map((name) => new RacingCar(name));
-    this.numberOfTries = numberOfTries;
+  constructor() {
+    this.cars = [];
   }
 
-  async playGame() {
-    for (let i = 0; i < this.numberOfTries; i++) {
-      this.moveCars();
-      await this.printRaceResult();
-    }
-  }
+  async getCarNamesFromUser() {
+    const carNames = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분): ");
+    const names = carNames.split(",");
 
-  moveCars() {
-    for (const car of this.cars) {
-      car.move();
+    for (const name of names) {
+      if (name.length > 5) {
+        throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+      }
     }
-  }
-  async printRaceResult() {
-    await MissionUtils.Console.print("실행 결과\n");
-    for (const car of this.cars) {
-      const positionText = "-".repeat(car.position);
-      MissionUtils.Console.print(`${car.name} : ${positionText}\n`);
-    }
-    await MissionUtils.Console.print("\n");
-  }
 
-  getWinners() {
-    const maxPosition = Math.max(...this.cars.map((car) => car.position));
-    const winners = this.cars.filter((car) => car.position === maxPosition);
-    return winners.map((winner) => winner.name).join(", ");
+    return names.join(",");
   }
 }
 
 class App {
   async play() {
-    const carNames = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분): ");
-    const numberOfTries = await MissionUtils.Console.readLineAsync(
-      "시도할 횟수는 몇 회인가요? "
-    );
-    if (!numberOfTries || isNaN(numberOfTries)) {
-      throw new Error("[ERROR] 올바른 횟수를 입력하세요.");
-    }
-
-    const game = new RacingGame(carNames, parseInt(numberOfTries, 10));
-    await game.playGame();
-    await MissionUtils.Console.print("최종 우승자: " + game.getWinners());
+    const game = new RacingGame();
+    const carNames = await game.getCarNamesFromUser();
   }
 }
 
