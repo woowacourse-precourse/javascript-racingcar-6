f13cd959cff66021834f04e6d61941e48c1d8a81
kwaksj329 <kwaksj329@gmail.com>
2023-11-01 22:37:20 +0900

    test: 테스트 코드 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -59,4 +59,122 @@ describe("자동차 경주 게임", () => {
     // then
     await expect(app.play()).rejects.toThrow("[ERROR]");
   });
+
+  test("전진-정지, 참가 3, 우승 1", async () => {
+    // given
+    const MOVING_FORWARD = 4;
+    const STOP = 1;
+    const inputs = [
+      "pobi,woni,java", "4"
+    ];
+    const outputs = [
+      "pobi : -","woni : -","java : -",
+      "pobi : --","woni : -","java : --",
+      "pobi : --","woni : -","java : ---",
+      "pobi : --","woni : -","java : ----",
+      "java"
+    ];
+    const randoms = [
+      MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,
+      MOVING_FORWARD, STOP, MOVING_FORWARD, 
+      STOP, STOP, MOVING_FORWARD,
+      STOP, STOP, MOVING_FORWARD,
+    ];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([...randoms]);
+
+    // when
+    const app = new App();
+    await app.play();
+
+    // then
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("전진-정지, 참가 1, 우승 1", async () => {
+    // given
+    const MOVING_FORWARD = 4;
+    const STOP = 1;
+    const inputs = [
+      "pobi", "2"
+    ];
+    const outputs = [
+      "pobi : -","pobi : -",
+      "pobi"
+    ];
+    const randoms = [
+      MOVING_FORWARD, MOVING_FORWARD,
+    ];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([...randoms]);
+
+    // when
+    const app = new App();
+    await app.play();
+
+    // then
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("전진-정지, 참가 4, 우승 3", async () => {
+    // given
+    const MOVING_FORWARD = 6;
+    const STOP = 3;
+    const inputs = [
+      "pobi,woni,java,susu", "1"
+    ];
+    const outputs = [
+      "pobi : -","woni : ","java : -","susu : -",
+      "pobi, java, susu"
+    ];
+    const randoms = [
+      MOVING_FORWARD, STOP, MOVING_FORWARD, MOVING_FORWARD,
+    ];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([...randoms]);
+
+    // when
+    const app = new App();
+    await app.play();
+
+    // then
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  // ERROR 테스트케이스
+  test.each([
+    [['']],
+    [['','','']],
+    [[' ',' ',' ']],
+    [['  ','  ','  ']],
+    [["pobibi"]],
+    [["pobi,wonini"]],
+    [["pobi,woni,javava"]],
+    [["pobi,woni,java,"]],
+    [["pobi,woni,java,  "]],
+    [["pobi,woni,java","number"]],
+    [["po bi,woni,ja va","number"]],
+  ])("입력 예외 처리", async (inputs) => {
+    // given
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.play()).rejects.toThrow("[ERROR]");
+  });
+
 });
