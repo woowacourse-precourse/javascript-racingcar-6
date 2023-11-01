9ee368f6c0d1f4cbfdf48d35bd21a48713aa1652
Sunghyun627 <kksscc4@gmail.com>
2023-11-01 14:15:49 +0900

    fix: 입력값을 검증하는 validation으로 넘겨줄 인자값 수정

--- src/InputManager.js
+++ src/InputManager.js
@@ -4,14 +4,16 @@ import { isValidCarNames, isValidTryNum } from './Validation.js';
 
 class InputManager {
   async enterCarNameList() {
-    const input = (await Console.readLineAsync(GUIDE_MESSAGES.ENTER_CARNAMES)).split(',');
-    if (input.at(-1) === '') input.pop();
-    return isValidCarNames(input) && input;
+    const input = await Console.readLineAsync(GUIDE_MESSAGES.ENTER_CARNAMES);
+    const carNames = input.split(',');
+    if (carNames.at(-1) === '') carNames.pop();
+    return isValidCarNames(carNames) && carNames;
   }
 
   async enterTryNum() {
     const input = await Console.readLineAsync(GUIDE_MESSAGES.ENTER_TRYNUM);
-    return isValidTryNum(input) && Number(input);
+    const tryNum = parseInt(input, 10);
+    return isValidTryNum(input, tryNum) && tryNum;
   }
 }
 
