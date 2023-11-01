55c3904f6fa420e8b7f776e61ba8023dceeecef1
이명욱 <myeong-uk@imyeong-ug-ui-MacBookPro.local>
2023-11-01 12:46:00 +0900

    2. 몇 번의 이동을 할지 입력하기

--- src/App.js
+++ src/App.js
@@ -12,6 +12,14 @@ class RacingCar {
 class App {
   async play() {
     const carNames = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분): ");
+    const numberOfTries = await MissionUtils.Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요? "
+    );
+    if (!numberOfTries || isNaN(numberOfTries)) {
+      throw new Error("[ERROR] 올바른 횟수를 입력하세요.");
+    }
+
+
   }
 }
 
