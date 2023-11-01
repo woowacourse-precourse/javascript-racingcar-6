b7bcdfadaa8db17c0b2f656e53eb738cb4441a0a
FordangIT <fordang0819@gmail.com>
2023-11-01 23:03:32 +0900

    feat: 자동차 이름 검증

--- src/gameStart.js
+++ src/gameStart.js
@@ -18,3 +18,26 @@ const getCarName = async () => {
   });
   return players;
 };
+
+// 1-3. 자동차 이름 검증하기
+const nameValidation = (name) => {
+  // 쉼표로 구분하지 않은 경우
+  if (!name.includes(",")) {
+    throw new Error("[ERROR] 자동차 이름은 쉼표(,)를 기준으로 구분해 주세요.");
+  }
+  // 쉼표로 구분한 경우
+  const names = name.split(",").map((name) => name.trim());
+  names.map((name) => {
+    // 이름이 1자 이하인 경우
+    if (name.length < 1) {
+      throw new Error("[ERROR] 자동차 이름은 1자 이상으로 입력해 주세요.");
+    }
+    // 이름이 5자 이상인 경우
+    if (name.length > 5) {
+      throw new Error("[ERROR] 자동차 이름은 5자 이하로 입력해 주세요.");
+    }
+  });
+  return names;
+};
+
+// 2. 시도할 횟수
