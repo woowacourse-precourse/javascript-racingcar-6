e8d7db8cc7568b1a91e050d8d3488a21feacdfcd
Kysiam <how2logout@gmail.com>
2023-11-01 13:54:50 +0900

    Test: 자동차 이름, 전진 잘하는지 test

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -2,7 +2,7 @@ import Car from '../src/models/Car.js';
 import Track from '../src/models/Track.js';
 
 describe('Track method test', () => {
-  test('현재 등록되어 있는 차량', () => {
+  test('Track 객체와 Car 객체가 서로 연결되어 자동차 이름을 잘 반환하는지 test', () => {
     // given
     const cars = ['ma', 'ria'].map((carName) => new Car(carName));
     const race = new Track(cars);
@@ -12,5 +12,17 @@ describe('Track method test', () => {
 
     // then
     expect(getCars.map((car) => car.getCarName())).toEqual(['ma', 'ria']);
+  });
+
+  test('자동차 이름과 전진을 잘 하는지 test', () => {
+    // given
+    const car = new Car('Maria');
+
+    // when
+    car.moveForward();
+
+    // then
+    expect(car.getCarName()).toBe('Maria');
+    expect(car.getCarDistance()).toBe(1);
   });
 });
