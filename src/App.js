725d8a2defd462096e5abe963a14748f255dabb9
Baek, Sehee <tpgmlqor@gmail.com>
2023-11-01 13:12:07 +0900

    feat(자동차 경주):자동차 경주 게임 전진 시도 횟수

--- src/App.js
+++ src/App.js
@@ -2,29 +2,36 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    await start();
+    await racingStart();
   }
 }
 
-function start() {
+function racingStart() {
   return new Promise(async (resolve, reject) => {
-    beforeStart(resolve, reject);
+    racingProcess(resolve, reject);
   });
 }
 
-function startMent() {
+function startSentence() {
   MissionUtils.Console.print(
     "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
   );
 }
 
-async function beforeStart(resolve, reject) {
+function attemptCountSentence() {
+  MissionUtils.Console.print("시도할 횟수는 몇 회인가요?");
+}
+
+async function racingProcess(resolve, reject) {
   try {
-    startMent();
+    startSentence();
     const CARSE_NAME = await MissionUtils.Console.readLineAsync("");
     const SPLITECARS_NAME = splitCars(CARSE_NAME);
     carsForEach(SPLITECARS_NAME);
     carsNumberError(SPLITECARS_NAME);
+    attemptCountSentence();
+    const ATTEMPT_COUNT = await MissionUtils.Console.readLineAsync("");
+    attemptCount(ATTEMPT_COUNT);
     resolve();
   } catch (error) {
     console.error(error + ` 레이싱 자동차 등록에 실패했습니다.`);
@@ -56,6 +63,12 @@ function carsForEach(cars) {
   cars.forEach((car) => {
     carsNameError(car);
   });
+}
+
+function attemptCount(count) {
+  if (count < 2 || typeof count !== number) {
+    throw new Error("[ERROR]");
+  }
 }
 
 const app = new App();
