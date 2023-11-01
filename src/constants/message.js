a3007bcafff3ea9a54f690e1c6944129c00b5c89
aaminha <dks4857@gmail.com>
2023-11-01 18:51:25 +0900

    fix(message): 우승자 출력 상수 수정

--- src/constants/message.js
+++ src/constants/message.js
@@ -2,7 +2,7 @@ export const MESSAGE = {
   getCarName: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
   getPlayNum: '시도할 횟수는 몇 회인가요?',
   printResult: '실행 결과',
-  printWinner: '최종 우승자',
+  printWinner: '최종 우승자 : ',
 };
 
 export const ERROR_MESSAGE = {
