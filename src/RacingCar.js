4a3f3306e655192145726d3a47e2ababe65fd44d
Boeun <boeun0904@naver.com>
2023-11-01 20:46:44 +0900

    refactor: change to constant values

--- src/RacingCar.js
+++ src/RacingCar.js
@@ -5,18 +5,18 @@ export default class RacingCar {
 
   static RAND_END = 9;
 
+  static NUM_CENTER = 4;
+
+  static MOVE_DISTANCE = 1;
+
   constructor(name) {
     this.name = name;
     this.distance = 0;
   }
 
   moveForward() {
-    const randomNumber = Random.pickNumberInRange(
-      RacingCar.RAND_START,
-      RacingCar.RAND_END,
-    );
-
-    if (randomNumber >= 4) this.distance += 1;
+    const randomNumber = Random.pickNumberInRange(RacingCar.RAND_START, RacingCar.RAND_END);
+    if (randomNumber >= RacingCar.NUM_CENTER) this.distance += RacingCar.MOVE_DISTANCE;
   }
 
   printDistance() {
--- src/User.js
+++ src/User.js
@@ -2,14 +2,14 @@ import { Console } from '@woowacourse/mission-utils';
 import MESSAGE from './Constant.js';
 
 export default class User {
+  static MAX_NAME_LEGNTH = 5;
+
   static async getCarNameArray() {
     const input = await Console.readLineAsync(MESSAGE.COMMAND_INPUT_CAR_NAME);
-
     const carNameArr = input.split(',').map((name) => name.trim());
 
-    if (carNameArr.some((carName) => carName.length > 5 || carName === ''))
+    if (carNameArr.some((carName) => carName.length > User.MAX_NAME_LEGNTH || carName === ''))
       throw new Error(MESSAGE.ERROR_WRONG_INPUT);
-
     if (carNameArr.length !== new Set([...carNameArr]).size)
       throw new Error(MESSAGE.ERROR_WRONG_INPUT);
 
@@ -17,9 +17,7 @@ export default class User {
   }
 
   static async getTrialNumber() {
-    const input = await Console.readLineAsync(
-      MESSAGE.COMMAND_INPUT_TRIAL_NUMBER,
-    );
+    const input = await Console.readLineAsync(MESSAGE.COMMAND_INPUT_TRIAL_NUMBER);
 
     if (Number.isNaN(Number(input))) throw new Error(MESSAGE.ERROR_WRONG_INPUT);
     if (Number(input) <= 0) throw new Error(MESSAGE.ERROR_WRONG_INPUT);
