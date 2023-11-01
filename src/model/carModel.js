7fb46dc0d177f8151a857866a2669b9aeef315e0
idohyeon <9598dohyun@naver.com>
2023-11-01 22:25:59 +0900

    ref: 자동차 전진오류

--- src/model/carModel.js
+++ src/model/carModel.js
@@ -1,6 +1,3 @@
-import {MissionUtils} from "@woowacourse/mission-utils";
-import Play from "../controller/carRaceController.js";
-
 class Car {
     // 생성자: 자동차 이름과 초기 위치 설정
     constructor(name) {
@@ -10,7 +7,7 @@ class Car {
 
     // move 메서드: 자동차를 한 칸 전진시킴
     move() {
-        this.moves ++;
+        this.moves++;
     }
 }
 export default Car;
