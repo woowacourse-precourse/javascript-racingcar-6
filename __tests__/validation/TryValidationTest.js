56ba452c63d4a927becbb8c76096fa73c040b6ef
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-11-01 19:28:11 +0900

    feat:시도 횟수 유효성 검증 테스트 구체적인 케이스 와 에러 메세지 추가

--- __tests__/validation/TryValidationTest.js
+++ __tests__/validation/TryValidationTest.js
@@ -1,9 +1,23 @@
-import { ERROR_PREFIX } from "../../src/constants.js";
+import {  MESSAGE } from "../../src/constants.js";
 import { validate } from "../../src/input/validations.js";
 
-describe("시도 횟수 입력", ()=> {
-    test.each(['a', '-2', ' '])(`시도 횟수 %s 입력 결과`,(param)=>{
-        const result = ()=> validate.try(param);
-        expect(result).toThrow(ERROR_PREFIX);
+const string = 'a';
+const minus = '-2';
+const empty =' ';
+
+describe("시도 횟수 입력 유효성 검증", ()=> {
+    test("숫자 가 입력되었는 지 검증", ()=> {
+        const result = ()=> validate.try(string);
+        expect(result).toThrow(MESSAGE.TRY.ERROR.IS_NOT_NUMBER);
+    })
+
+    test("0 이상의 숫자가 입력되었는 지 검증", ()=> {
+        const result = ()=> validate.try(minus);
+        expect(result).toThrow(MESSAGE.TRY.ERROR.IS_MINUS);
+    })
+
+    test("입력 값이 있는 지 검증", ()=> {
+        const result = ()=> validate.try(empty);
+        expect(result).toThrow(MESSAGE.TRY.ERROR.IS_NOT_INPUT);
     })
 })
