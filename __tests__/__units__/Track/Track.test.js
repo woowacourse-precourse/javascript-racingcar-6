26b4dbeb3c120d90d80f6bb9d247526780c66766
cobocho <rlaalsrb1111@naver.com>
2023-11-01 11:30:17 +0900

    test: `Track` 테스트 케이스 추가 및 유틸 분리

--- __tests__/__units__/Track/Track.test.js
+++ __tests__/__units__/Track/Track.test.js
@@ -10,6 +10,17 @@ const mockRandoms = (numbers) => {
   );
 };
 
+/**
+ * 입력한 `loop`만큼 `track`의 `processLap`을 호출합니다.
+ * @param {Track} track
+ * @param {number} loop
+ */
+const processLapLoop = (track, loop) => {
+  for (let i = 0; i < loop; i += 1) {
+    track.processLap();
+  }
+};
+
 describe('Track 테스트', () => {
   afterEach(() => {
     jest.clearAllMocks();
@@ -84,47 +95,42 @@ describe('Track 테스트', () => {
     const track = Track.of(users, lap);
 
     // when
-    for (let i = 0; i < lap; i += 1) {
-      track.processLap();
-    }
+    processLapLoop(track, lap);
 
     // then
     expect(track.isEnd()).toBeTruthy();
   });
 
-  it('`getCurrentLapResult()`는 현재 랩의 결과를 반환합니다.', () => {
-    // given
-    const lap = 3;
-    const users = [User.of('레이서'), User.of('레이서2')];
-    const track = Track.of(users, lap);
-    mockRandoms([5, 2, 5, 3, 1, 2]);
-
-    // when
-    for (let i = 0; i < lap; i += 1) {
-      track.processLap();
-    }
-
-    // then
-    expect(track.getCurrentLapResult()).toEqual({
-      레이서: '--',
-      레이서2: '',
-    });
-  });
-
   it.each([
-    { randoms: [5, 2, 5, 3], winners: ['레이서1'] },
-    { randoms: [5, 2, 2, 4], winners: ['레이서1', '레이서2'] },
-  ])('`getCurrentWinners()`는 현재 랩의 선두주자들을 반환합니다.', ({ randoms, winners }) => {
+    { randoms: [5, 2, 5, 3], result: { 레이서: '--', 레이서2: '' } },
+    { randoms: [2, 2, 1, 3], result: { 레이서: '', 레이서2: '' } },
+    { randoms: [5, 7, 5, 9], result: { 레이서: '--', 레이서2: '--' } },
+  ])('`getCurrentLapResult()`는 현재 랩의 결과를 반환합니다.', ({ randoms, result }) => {
     // given
     const lap = 2;
-    const users = [User.of('레이서1'), User.of('레이서2')];
+    const users = [User.of('레이서'), User.of('레이서2')];
     const track = Track.of(users, lap);
     mockRandoms(randoms);
 
     // when
-    for (let i = 0; i < lap; i += 1) {
-      track.processLap();
-    }
+    processLapLoop(track, lap);
+
+    // then
+    expect(track.getCurrentLapResult()).toEqual(result);
+  });
+
+  it.each([
+    { randoms: [5, 2, 5, 3], winners: ['레이서'] },
+    { randoms: [5, 2, 2, 4], winners: ['레이서', '레이서2'] },
+  ])('`getCurrentWinners()`는 현재 랩의 선두주자들을 반환합니다.', ({ randoms, winners }) => {
+    // given
+    const lap = 2;
+    const users = [User.of('레이서'), User.of('레이서2')];
+    const track = Track.of(users, lap);
+    mockRandoms(randoms);
+
+    // when
+    processLapLoop(track, lap);
 
     // then
     expect(track.getCurrentWinners()).toEqual(winners);
