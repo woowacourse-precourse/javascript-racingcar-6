dd8e102fb69590e54a4f80d9fcacafdaa22f170e
JoSooha <dlrhdcjs@naver.com>
2023-11-01 21:49:57 +0900

    test: 이름, 시도 횟수에 대한 예외 케이스 테스트 작성

--- __tests__/MyTest.js
+++ __tests__/MyTest.js
@@ -23,22 +23,46 @@ const getLogSpy = () => {
   return logSpy;
 };
 
-test("전진-정지2", async() => {
-    //given
-    const inputs = ["a,b,c,d", "3"];
-    const outputs = ["a : ---", "b : --", "c : -", "d : -"];
-    const randoms = [8,8,8,8,8,8,1,1,8,1,1,1];
-    const logSpy = getLogSpy();
-
-    mockQuestions(inputs);
-    mockRandoms([...randoms]);
-
-    //when
-    const app = new App();
-    await app.play();
-
-    //then
-    outputs.forEach(output => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output))
+describe("나의 자동차 게임", () => {
+    test("전진-정지2", async() => {
+        //given
+        const inputs = ["a,b,c,d", "3"];
+        const outputs = ["a : ---", "b : --", "c : -", "d : -"];
+        const randoms = [8,8,8,8,8,8,1,1,8,1,1,1];
+        const logSpy = getLogSpy();
+    
+        mockQuestions(inputs);
+        mockRandoms([...randoms]);
+    
+        //when
+        const app = new App();
+        await app.play();
+    
+        //then
+        outputs.forEach(output => {
+          expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output))
+        })
     })
-  })
+
+    test.each([
+        [["pobi, nananna"]],
+        [["nanananan, jhon"]]
+    ])("이름에 대한 예외 처리", async (inputs) => {
+        mockQuestions(inputs);
+        const app = new App();
+
+        await expect(app.play()).rejects.toThrow("[ERROR]")
+    })
+
+    test.each([
+        [["pobi, kabi", "noNumber"]],
+        [["pobi, jhon", ""]]
+    ])("시도횟수에 대한 예외 처리", async (inputs) => {
+        mockQuestions(inputs);
+        const app = new App();
+
+        await expect(app.play()).rejects.toThrow("[ERROR]")
+    })
+
+})
+
