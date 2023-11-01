426865e0dc8a5a669ead00cb9c63c020939898ba
nkEllaaa <dhrtns515@gmail.com>
2023-11-01 19:10:49 +0900

    refactor: Car class에서 기능을 수행하지 않도록 수정

--- src/Car.js
+++ src/Car.js
@@ -1,30 +1,26 @@
 import constants from './constants';
-import { Random, Console } from '@woowacourse/mission-utils';
+import { Random } from '@woowacourse/mission-utils';
 
-export default class Car {
+class Car {
   constructor(name) {
     this.name = name;
-    this.movingCount = 0;
+    this.moveCount = 0;
   }
 
-  doForward(count) {
-    let judgeGoSTOP = 0;
-  
-    for (let i = 0; i < count; i++) {
-      judgeGoSTOP = Random.pickNumberInRange(
-        constants.NUMBER_RANGE_MIN,
-        constants.NUMBER_RANGE_MAX
-      );
-      if (judgeGoSTOP >= constants.JUDGE_NUMBER) {
-        this.movingCount++;
-      }
-      Console.print(this.printResult(this.movingCount));
+  move = () => {
+    const randomNumber = Random.pickNumberInRange(
+      constants.NUMBER_RANGE_MIN,
+      constants.NUMBER_RANGE_MAX
+    );
+
+    if (randomNumber >= constants.JUDGE_NUMBER) {
+      this.moveCount++;
     }
   }
 
-  printResult(movingCount) {
-    return `${this.name} : ${'-'.repeat(movingCount)}`;
+  printScore = () => {
+    return `${this.name} : ${'-'.repeat(this.moveCount)}`;
   }
 }
 
-export { Car }
+export default Car;
