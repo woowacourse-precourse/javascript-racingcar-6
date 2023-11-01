0e82da5af9bb4cf4102e6b6a1342b3e34c326af8
sryung <sryung1225@gmail.com>
2023-11-01 22:43:40 +0900

    test(ValidationTest): 경주 회차 유효성 테스트 적용
    숫자가 아닌 것을 기입한 경우(ERROR.roundInputNumber)
    양수가 아닌 숫자를 기입한 경우(ERROR.roundInputPositive)
    에 대한 유효성 검사 테스트를 적용해 확인
    
    테스트 실황
    Test Suites: 3 passed, 3 total
    Tests:       10 passed, 10 total
    Snapshots:   0 total
    Time:        1.154 s, estimated 2 s

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,5 +1,6 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import { UserInputCarNames } from "../src/Game/UserInputCarNames";
+import { UserInputRounds } from "../src/Game/UserInputRounds";
 import { ERROR } from "../src/constants";
 
 jest.mock("@woowacourse/mission-utils", () => ({
@@ -16,6 +17,20 @@ describe("유효성 검사 테스트", () => {
     await expect(
       UserInputCarNames.getCarNames()
     ).rejects.toThrow(ERROR.carNameInputLong);
+  });
+
+  test("숫자가 아닌 경주 회차 예외 처리", async () => {
+    MissionUtils.Console.readLineAsync.mockResolvedValue('five');
+    await expect(
+      UserInputRounds.getNumberOfRounds()
+    ).rejects.toThrow(ERROR.roundInputNumber);
+  });
+
+  test("양수가 아닌 경주 회차 예외 처리", async () => {
+    MissionUtils.Console.readLineAsync.mockResolvedValue('-3');
+    await expect(
+      UserInputRounds.getNumberOfRounds()
+    ).rejects.toThrow(ERROR.roundInputPositive);
   });
 });
 
