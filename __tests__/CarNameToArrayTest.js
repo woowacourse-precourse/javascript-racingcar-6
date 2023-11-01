afac55cc5a218795e484c7a06fdaab42dc8b5826
Younghoon Ahn <ahnqwerty@gmail.com>
2023-11-01 14:28:35 +0900

    fix: CarNameToArray 출력문 수정

--- __tests__/CarNameToArrayTest.js
+++ __tests__/CarNameToArrayTest.js
@@ -9,15 +9,15 @@ describe("문자열 배열로 분리하기 테스트", () => {
     expect(result).toContainEqual("pobi", "woni");
   });
 
-  test("5글자를 초과하는 이름이 입력됐을 때 예외 발생", () => {
+  test("5글자를 초과하는 이름이 입력됐을 때 예외 처리", () => {
     const input = "pobi,woni,junjun";
 
     expect(() => carNameToArray(input)).toThrow(`[ERROR] 각 자동차의 이름은 0 ~ 5자 사이로 입력해야 합니다.`);
   });
 
-  test("0글자 이름이 입력됐을 때 예외 발생", () => {
+  test("0글자 이름이 입력됐을 때 예외 처리", () => {
     const input = "pobi,,jun";
-    
+
     expect(() => carNameToArray(input)).toThrow(`[ERROR] 각 자동차의 이름은 0 ~ 5자 사이로 입력해야 합니다.`);
   });
 });
