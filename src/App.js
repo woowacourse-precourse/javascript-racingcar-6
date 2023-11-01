9c0e42a617067da65e63f9373d2ba37ebab59cb7
forest7592 <forest7592@hanmail.net>
2023-11-01 11:05:20 +0900

    test: unit tests
    Perform unit tests for each methods
    - getNames
    - getNumber
    - moveForward
    - makeResult
    - updateMaximum
    - winnerAnnouncement
    all of the above methods have been refactored to perform testing

--- src/App.js
+++ src/App.js
@@ -4,65 +4,79 @@ class App {
   constructor() {
     this.winners = [];
     this.max = -Infinity;
+    this.names = [];
+    this.number = -1;
+    this.results = [];
   }
 
-  async play() {
-    const moveForward = (result) => {
-      if (MissionUtils.Random.pickNumberInRange(0, 9) >= 4) {
-        result.push('-');
-      }
-    };
-    const makeResult = (names, results) => {
-      for (let i = 0; i < names.length; i += 1) {
-        moveForward(results[i]);
-        MissionUtils.Console.print(`${names[i]} : ${results[i].join('')}`);
-      }
-      MissionUtils.Console.print('');
-    };
-    const updateMaximum = (names, results, idx) => {
-      if (results[idx].length > this.max) {
-        this.winners = [names[idx]];
-        this.max = results[idx].length;
-      } else if (results[idx].length === this.max) {
-        this.winners.push(names[idx]);
-      }
-    };
-    const winnerAnnouncement = (names, results) => {
-      for (let i = 0; i < results.length; i += 1) {
-        updateMaximum(names, results, i);
-      }
-      MissionUtils.Console.print(`최종 우승자 : ${this.winners.join(', ')}`);
-    };
-
+  async getNames() {
     const string = await MissionUtils.Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
     );
-    let regex = /^.{1,5}(,.{1,5})*$/;
+    const regex = /^.{1,5}(,.{1,5})*$/;
     if (!regex.test(string)) {
       throw new Error('[ERROR] 문자열이 잘못된 형식입니다.');
     }
-    const names = string.split(',');
-
-    const results = [];
-    for (let i = 0; i < names.length; i += 1) {
-      results.push([]);
+    this.names = string.split?.(',') || [];
+    for (let i = 0; i < this.names.length; i += 1) {
+      this.results.push([]);
     }
+  }
 
-    const number = await MissionUtils.Console.readLineAsync(
+  async getNumber() {
+    this.number = +(await MissionUtils.Console.readLineAsync(
       '시도할 횟수는 몇 회인가요?\n',
-    );
-    regex = /^\d+$/;
-    if (!regex.test(number)) {
+    ));
+    const regex = /^\d+$/;
+    if (!regex.test(this.number)) {
       throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
     }
+  }
+
+  moveForward(idx) {
+    if (MissionUtils.Random.pickNumberInRange(0, 9) >= 4) {
+      this.results[idx].push('-');
+    }
+  }
+
+  makeResult() {
+    for (let i = 0; i < this.names.length; i += 1) {
+      this.moveForward(i);
+      MissionUtils.Console.print(
+        `${this.names[i]} : ${this.results[i].join('')}`,
+      );
+    }
+    MissionUtils.Console.print('');
+  }
+
+  updateMaximum(idx) {
+    if (this.results[idx].length > this.max) {
+      this.winners = [this.names[idx]];
+      this.max = this.results[idx].length;
+    } else if (this.results[idx].length === this.max) {
+      this.winners.push(this.names[idx]);
+    }
+  }
+
+  winnerAnnouncement() {
+    for (let i = 0; i < this.results.length; i += 1) {
+      this.updateMaximum(i);
+    }
+    MissionUtils.Console.print(`최종 우승자 : ${this.winners.join(', ')}`);
+  }
+
+  async play() {
+    await this.getNames();
+
+    await this.getNumber();
 
     MissionUtils.Console.print('\n실행 결과');
 
-    for (let i = 0; i < number; i += 1) {
-      makeResult(names, results);
+    for (let i = 0; i < this.number; i += 1) {
+      this.makeResult();
     }
 
-    winnerAnnouncement(names, results);
+    this.winnerAnnouncement();
   }
 }
 
