bd39f53e7d1b1b27a3b8e3fda05da24ee5b2604f
MinYoungWI <young1y@nate.com>
2023-11-01 22:14:11 +0900

    Style: 불필요한  주석 삭제

--- src/App.js
+++ src/App.js
@@ -9,9 +9,6 @@ class App {
   }
 
   async play() {
-    // MissionUtils.Console.print(
-    //   "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
-    // );
     this.namingYourCar = await MissionUtils.Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분"
     );
