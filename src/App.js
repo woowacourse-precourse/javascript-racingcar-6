fd7bf1b6602807346330e5e5454d2c67ac0532b0
yunminwon <148843628+yunminwon@users.noreply.github.com>
2023-11-01 13:30:58 +0900

    Update and rename print.js to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+const dom = new JSDOM(`<!DOCTYPE html><div class="result-container"></div>`);
+const { document } = dom.window;
+
 const resultContainer = document.querySelector('.result-container');
 
-export const showResult = (cars) => {
+const showResult = (cars) => {
   const racingResult = document.createElement('div');
   let resultString = '';
 
@@ -23,9 +30,21 @@ const printBar = (count) => {
   return forwardBar;
 };
 
-export const showWinner = (winners) => {
+const showWinner = (winners) => {
   const winner = document.createElement('div');
 
   winner.innerHTML = `최종 우승자 : ${winners.join(',')}`;
   resultContainer.appendChild(winner);
-}; 
+};
+
+// 사용 예시
+const cars = [
+  { name: 'Car1', forwardCount: 3 },
+  { name: 'Car2', forwardCount: 5 },
+];
+
+showResult(cars);
+
+const winners = ['Car2'];
+
+showWinner(winners);
