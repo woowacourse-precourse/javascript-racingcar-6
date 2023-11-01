116dbcd81e873e83fad3b23f9b0bd997408f88d5
Jeon Eonseok <jseokeon@gmail.com>
2023-11-01 21:34:33 +0900

    feat: 게임 시작 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import getCarName from './getCarName';
+import getTryCount from './getTryCount';
+import startGame from './startGame';
+
 class App {
-  async play() {}
+  async play() {
+    const CAR_NAMES = await getCarName();
+    const TRY_COUNT = await getTryCount();
+
+    startGame(CAR_NAMES, TRY_COUNT);
+  }
 }
 
 export default App;
