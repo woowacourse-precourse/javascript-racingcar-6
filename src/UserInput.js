eac620920e7f7d543832aacbc3e28151ac2ee937
future4ing <mirae4120@gmail.com>
2023-11-01 19:52:07 +0900

    feat : 사용자가 잘못된 값을 입력한 경우 예외 발생 기능 구현
    - 자동차 이름 입력 시 예외 처리
    - 시도할 횟수 입력 시 예외 처리
    - README.md 파일에 예외 추가

--- src/UserInput.js
+++ src/UserInput.js
@@ -1,18 +1,19 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console } from '@woowacourse/mission-utils';
+import { validateCarName, validateRoundCount } from './validate.js';
 
-class UserInput {
-    async getCarNames() {
-        const carNames = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+async function getCarNames() {
+  const inputName = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+  const carNames = inputName.split(',').map((name) => name.trim());
+  validateCarName(carNames);
 
-        return carNames.split(",");
-
-    }
-
-    async getMoveCount() {
-        const moveCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
-
-        return moveCount;
-    }
+  return carNames;
 }
 
-export default UserInput;
+async function getRoundCount() {
+  const inputCount = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+  validateRoundCount(inputCount);
+
+  return Number(inputCount);
+}
+
+export { getCarNames, getRoundCount };
