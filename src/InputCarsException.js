d049ae835e7d9fd516c5e8beecd3a072d0786995
TMInstaller <ajvldmlqjqclr@naver.com>
2023-11-01 23:36:51 +0900

    클래스명과 맞지않는 기능 이동

--- src/InputCarsException.js
+++ src/InputCarsException.js
@@ -26,26 +26,18 @@ class InputCarsException {
     }
   }
 
-  checkIsNameSpaced(cars) {
-    if (cars.includes(" "))
-      throw new Error(errorCarMessage.INVALID_CAR_NAME_DUPLICATE);
+  checkIsNameSpacedBetween(cars) {
+    for (let i = 0; i < cars.length; i++) {
+      if (cars[i].includes(" "))
+        throw new Error(errorCarMessage.INVALID_CAR_NAME_DUPLICATE);
+    }
   }
 
   check() {
-    // self-feedback: 예외가 있는지 체크하는 곳인데 여기서 문자열 가공을 하는 부분이 들어있다. 이를 다른 객체로 분리해야할 듯 하다.
     this.checkIsNameEmpty(this.cars);
-
-    this.cars = this.cars.split(",");
-
-    for (let i = 0; i < this.cars.length; i++) {
-      this.cars[i] = this.cars[i].trim();
-      this.checkIsNameSpaced(this.cars[i]);
-    }
-
+    this.checkIsNameSpacedBetween(this.cars);
     this.checkNameLength(this.cars);
     this.checkDuplicate(this.cars);
-
-    return this.cars;
   }
 }
 
