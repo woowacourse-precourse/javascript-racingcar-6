a2bfbe3d7613bfae6856199679299f4a066d4146
hong <ahi200@naver.com>
2023-11-01 17:34:20 +0900

    test: Screen클래스의 getUserInput  테스트코드 추가

--- __tests__/ScreenTest.js
+++ __tests__/ScreenTest.js
@@ -7,6 +7,15 @@ const getLogSpy = () => {
   return logSpy;
 };
 
+const mockQuestions = (inputs) => {
+  Console.readLineAsync = jest.fn();
+
+  Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  });
+};
+
 describe('Screen.makeDashString', () => {
   test('전달된 숫자만큼 -를 생성한다', () => {
     expect(Screen.makeDashString(1)).toBe('-');
@@ -32,15 +41,32 @@ describe('Screen.printMessage', () => {
   });
 });
 
-describe('Screen.printEmptyLine', () => {
-  test('빈 행을 콘솔에 출력해야 한다.', () => {
+describe('Screen.printWinner', () => {
+  test('배열로 전달된 목록을 콤마로 구분하여 우승자를 출력해야 한다', () => {
     // given
+    const winnerList = ['a', 'b', 'c'];
     const logSpy = getLogSpy();
+    const outputs = '최종 우승자 : a, b, c';
 
     // when
-    Screen.printEmptyLine();
+    Screen.printWinner(winnerList);
 
     // then
-    expect(logSpy).toHaveBeenCalledWith('');
+    expect(logSpy).toHaveBeenCalledWith(outputs);
+  });
+});
+
+describe('Screen.getUserInput', () => {
+  test('사용자가 입력한 값을 반환해야 한다.', async () => {
+    // given
+    const userInput = ['pobi,woni', '1'];
+    mockQuestions(userInput);
+
+    // when
+    // then
+    for await (const input of userInput) {
+      const result = await Screen.getUserInput();
+      expect(result).toBe(input);
+    }
   });
 });
