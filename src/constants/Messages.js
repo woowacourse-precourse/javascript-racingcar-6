49419865feb1277a98f04f32688b9ff906a8c38f
jinsil <jinsil378900@gmail.com>
2023-11-01 10:38:23 +0900

    Feat: 예외처리 메세지 추가하기

--- src/constants/Messages.js
+++ src/constants/Messages.js
@@ -5,9 +5,11 @@ const Messages = Object.freeze({
   WINNER: "최종 우승자 : ",
 
   ERROR: {
-    INVALID_NAME_NULL: "[ERROR] 자동차 이름을 입력해주세요",
+    INVALID_NAME_NULL: "[ERROR] 자동차 이름을 입력해주세요.",
     INVALID_NAME_NOT_SPACE: "[ERROR] 자동차 이름엔 띄어쓰기가 들어갈 수 없습니다.",
-    INVALID_NAME_LENGTH_LIMIT: "[ERROR] 이름은 5자 이하로만 입력해주세요",
+    INVALID_NAME_COMMA: "[ERROR] 쉼표의 위치를 다시 확인해주세요.",
+    INVALID_NAME_LENGTH_LIMIT: "[ERROR] 자동차 이름은 5자 이하로만 입력해주세요.",
+    INVALID_NAME_NO_SAME: "[ERROR] 자동차 이름은 중복될 수 없습니다.",
     INVALID_NUMBER_INTEGER: "[ERROR] 횟수는 1이상의 정수만 가능합니다.",
   },
 });
