4aa2ad964fd50b4d6d05609d588c6ea07951cd5f
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-11-01 21:29:55 +0900

    style: 출력 수정

--- __tests__/inputTest/getCarsTest.js
+++ __tests__/inputTest/getCarsTest.js
@@ -1,15 +1,8 @@
-import { Console } from "@woowacourse/mission-utils";
+import { mockQuestions } from "../../src/utils/testUtils/testUtils";
 import App from "../../src/App";
 import { ERROR_HEADER } from "../../src/App";
 
-const mockQuestions = (inputs) => {
-  Console.readLineAsync = jest.fn(() => {
-    const input = inputs.shift();
-    return Promise.resolve(input);
-  });
-};
-
-describe("getCars 함수테스트", () => {
+describe("레이싱카 입력", () => {
   test("레이싱카 입력 문자열 구분 처리", async () => {
     //given
     const input = ["pobi,woni,jun"];
@@ -47,10 +40,8 @@ describe("getCars 함수테스트", () => {
     async (inputs) => {
       // given
       mockQuestions(inputs);
-
       // when
       const app = new App();
-
       // then
       await expect(app.getCars()).rejects.toThrow(ERROR_HEADER);
     }
