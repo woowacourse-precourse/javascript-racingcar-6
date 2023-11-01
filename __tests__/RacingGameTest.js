690f7850e1f0b45c410e04a0d624d3f06c75acc4
seung <dmdgpdi@naver.com>
2023-11-01 18:18:04 +0900

    test(__test__/RacingGame): getCarNames 메서드 테스트 작성

--- __tests__/RacingGameTest.js
+++ __tests__/RacingGameTest.js
@@ -1,5 +1,14 @@
 import Car from '../src/Car';
 import RacingGame from '../src/RacingGame';
+import { Random, Console } from '@woowacourse/mission-utils';
+
+const mockQuestions = (input) => {
+  Console.readLineAsync = jest.fn();
+
+  Console.readLineAsync.mockImplementation(() => {
+    return Promise.resolve(input);
+  });
+};
 
 describe('hasDuplicateCarName 함수 테스트', () => {
   test('중복될 때 예외 처리', () => {
@@ -40,5 +49,37 @@ describe('hasDuplicateCarName 함수 테스트', () => {
 
     // then
     expect(result).toBeFalsy();
+  });
+});
+
+describe('getCarNames 함수 테스트', () => {
+  describe('문제 있을 때 예외 처리', () => {
+    const invailInputs = ['seung,seung,seug', 'seung,seungthae,kim,123456'];
+
+    invailInputs.forEach((input) => {
+      test(`테스트 케이스 ${input}`, async () => {
+        mockQuestions(input);
+        const racingGame = new RacingGame();
+
+        const result = () => racingGame.getCarNames();
+
+        await expect(result).rejects.toThrow();
+      });
+    });
+  });
+
+  describe('문제 없을 때 정상 동작 확인', () => {
+    const vaildInputs = ['kin, seung, thae'];
+
+    vaildInputs.forEach((input) => {
+      test(`테스트 케이스 ${input}`, async () => {
+        mockQuestions(input);
+        const racingGame = new RacingGame();
+
+        const result = racingGame.getCarNames();
+
+        await expect(result).resolves.not.toThrow();
+      });
+    });
   });
 });
