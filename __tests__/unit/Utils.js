bad5b9bfe7a4773fe9ceb8730005aea99e7d45fe
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-11-01 14:41:26 +0900

    fix: Utils 테스트에서 matcher를 추가해 기댓값과 비교하도록 수정

--- __tests__/unit/Utils.js
+++ __tests__/unit/Utils.js
@@ -4,23 +4,24 @@ import { printMessage, throwError } from '../../src/common/utils';
 
 describe('메시지 출력 테스트', () => {
   test('메시지 출력 가능 여부', () => {
+    const consolePrintSpy = jest.spyOn(Console, 'print');
+    
     const message = '시도 횟수는 1 이상을 입력하면 경주가 시작됩니다.';
-
     printMessage(message);
-    expect(Console.print);
+
+    expect(consolePrintSpy).toHaveBeenCalledWith(message);
+    consolePrintSpy.mockRestore();
   });
 });
 
 describe('에러 핸들링 테스트', () => {
   test('true일 때 에러를 던지는지 여부', () => {
     const errorMessage = '[ERROR] 유효한 자동차 이름 형식이 아닙니다.';
-
     expect(() => throwError(errorMessage, true)).toThrowError(errorMessage);
   });
 
   test('false일 때 에러를 던지지 않는지 여부', () => {
     const errorMessage = '[ERROR] 유효한 시도 횟수 값이 아닙니다.';
-
     expect(() => throwError(errorMessage, false)).not.toThrow();
   });
 });
