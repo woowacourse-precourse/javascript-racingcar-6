da20cb142ee084527ef22cdcce88a182fdf3858d
suneunkim <tjsdms89@gmail.com>
2023-11-01 15:53:54 +0900

    feat: 게임 시작 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  constructor() {
+    this.raceCount = "";
+  }
+
+  async play() {
+    await this.startRacing();
+  }
+
+  async startRacing() {
+    const carNamesInput = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분))"
+    );
+    const carNames = carNamesInput.split(",");
+    this.validation(carNames);
+    Console.print(carNames);
+    const raceCount = Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+    Console.print(raceCount);
+  }
+
+  validation(carNames) {
+    carNames.forEach((carName) => {
+      if (carName.length > 5) throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+    });
+  }
+
+  forwordOrStop() {}
+
+  raceRoundMessage() {
+    // 라운드 별 결과 출력
+  }
+
+  whoIsWinner() {
+    // 최종 우승자 : pobi, jun
+  }
 }
 
 export default App;
