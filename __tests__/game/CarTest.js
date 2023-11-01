bdb94cadc2b297d02a1e8e9e58371a926c54a91d
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-11-01 14:09:51 +0900

    feat:자동차 전진 테스트 수정
    * 각 메서드 별 로 세분화

---
+++ __tests__/game/CarTest.js
@@ -0,0 +1,29 @@
+import { RandomGenerator } from "../../src/RandomGenerator.js";
+import { Car } from "../../src/game/Car.js";
+
+const carName = "hj";
+const car = new Car(carName, randomGenerator);
+const randomGenerator = new RandomGenerator(0,9);
+
+describe("자동차 테스트", ()=>{
+    test("인스턴스 생성",()=>{
+        expect(car.getName()).toBe(carName)
+        expect(car.getPosition()).toBe(0);
+    })
+
+    test("movable", ()=>{
+        const movableRandomNumberGenerator=  new RandomGenerator(4,9);
+        const movableCar = new Car(carName, movableRandomNumberGenerator);
+
+        expect(movableCar.movable()).toBe(true);
+    })
+
+    test("moveOrStop", ()=>{
+        const movableRandomNumberGenerator=  new RandomGenerator(4,9);
+        const movableCar = new Car(carName, movableRandomNumberGenerator);
+
+        movableCar.moveOrStop()
+
+        expect(movableCar.getPosition()).toBeGreaterThan(0);
+    })
+})
