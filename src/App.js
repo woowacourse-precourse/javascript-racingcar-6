abacb15ce3f70af729d22b7402f7f71416a9c5ae
MinYoungWI <young1y@nate.com>
2023-11-01 16:43:52 +0900

    Feat: [기능목록2] 사용자에게 시도할 횟수를 묻고 입력한 값 출력하기

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   constructor() {
     this.namingYourCar = "";
+    this.numberOfTries = 0;
   }
 
   async play() {
@@ -13,6 +14,9 @@ class App {
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분"
     );
     this.userCarsName();
+    this.numberOfTries =
+      await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+    this.executionResult();
   }
 
   userCarsName() {
@@ -24,6 +28,18 @@ class App {
     });
     MissionUtils.Console.print(userCarNameInArr.join());
   }
+
+  executionResult() {
+    if (isNaN(this.numberOfTries) === true) {
+      throw new Error("[ERROR] 숫자만 입력 가능합니다.");
+    }
+    MissionUtils.Console.print(this.numberOfTries);
+    MissionUtils.Console.print("실행 결과");
+    for (let i = 0; i < this.numberOfTries; i++) {
+      gameRepeat();
+    }
+  }
+  gameRepeat() {}
 }
 
 export default App;
