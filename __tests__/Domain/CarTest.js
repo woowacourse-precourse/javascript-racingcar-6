335d56f9c41a67de6a2721796b91f538e1828771
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-11-01 21:50:16 +0900

    test: 무작위 값이 4 이상일 경우, 자동차를 전진한다

--- __tests__/Domain/CarTest.js
+++ __tests__/Domain/CarTest.js
@@ -31,4 +31,27 @@ describe('Car 테스트', () => {
       });
     });
   });
+
+  describe('전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다', () => {
+    const shouldMoveCases = [4, 5, 6, 7, 8, 9];
+    const shouldNotMoveCases = [0, 1, 2, 3];
+
+    shouldMoveCases.forEach((number) => {
+      test('4 이상의 숫자가 주어지면 움직인다.', () => {
+        const car = new Car('pobi');
+        car.move(number);
+
+        expect(car.getPosition()).toBe(1);
+      });
+    });
+
+    shouldNotMoveCases.forEach((number) => {
+      test('4 미만의 숫자가 주어지면 움직이지 않는다.', () => {
+        const car = new Car('pobi');
+        car.move(number);
+
+        expect(car.getPosition()).toBe(0);
+      });
+    });
+  });
 });
