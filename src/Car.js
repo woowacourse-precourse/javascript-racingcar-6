57543b7b127f67615edeb860a3286c65dbd1e64e
CSKIM <rlackstjq987@gmail.com>
2023-11-01 20:34:06 +0900

    feat: 총 이동횟수 입력 기능 추가
    2. 사용자는 경주의 총 이동횟수를 입력한다
          - 만약 사용자가 잘못된 값을 입력하는 경우 [Error] 로 시작하는 메세지로 예외를 발생시킨다.

--- src/Car.js
+++ src/Car.js
@@ -1,5 +1,7 @@
 class Car {
   constructor(name) {
     this.name = name;
+    this.Count = 0;
+  }
 
 export default Car;
--- src/GameStart.js
+++ src/GameStart.js
@@ -1,9 +1,22 @@
 import Car from "./Car";
+import checkGameCount from "./checkGameCount";
 import checkCarName from "./checkCarName";
+
 class GameStart {
   constructor() {
     this.cars = [];
+    this.rounds = 0;
   }
+  getGameCount = async () => {
+    const count = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+    const parseCount = parseInt(count);
+    if (!checkGameCount(parseCount)) {
+      throw new Error("[ERROR] 숫자가 아닌 값이 입력되었습니다.");
+    }
+
+    this.rounds = parseCount;
+  };
+
   getCarNames = async () => {
     const carNames = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
@@ -17,6 +30,7 @@ class GameStart {
 
   async start() {
     await this.getCarNames();
+    await this.getGameCount();
   }
 }
 
