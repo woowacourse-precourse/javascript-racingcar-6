0b51cc0af499996888673606634c33e76a16655c
hyunyeee <5004ohy@naver.com>
2023-11-01 13:41:59 +0900

    feat: 우승자 안내 문구를 출력한다.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 const CAR_NAME_MESSAGE =
   "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
 const ATTEMP_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?\n";
+const WINNER_MESSAGE = "최종 우승자 : ";
 
 class Car {
   constructor(name) {
@@ -45,13 +46,22 @@ class App {
     return result.trim();
   }
 
-  #race(cars, attemptCount) {
+  async #race(cars, attemptCount) {
     MissionUtils.Console.print("\n실행 결과");
     for (let i = 0; i < attemptCount; i++) {
       cars.forEach((car) => car.move());
       cars.forEach((car) => MissionUtils.Console.print(car.displayDistance()));
       MissionUtils.Console.print(" ");
     }
+    await this.#displayWinner(cars);
+  }
+
+  #displayWinner(cars) {
+    const maxDistance = Math.max(...cars.map((car) => car.distance));
+    const winner = cars
+      .filter((car) => car.distance === maxDistance)
+      .map((car) => car.name);
+    MissionUtils.Console.print(WINNER_MESSAGE + winner.join(", "));
   }
 }
 
