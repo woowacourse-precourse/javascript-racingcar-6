419c247d9aecf899a9cc01a60a8116dd11e9cb98
baegyeong <beth0929@naver.com>
2023-11-01 15:50:56 +0900

    refactor(Car): 예외 처리 조건 메소드를 private, static으로 정의

--- src/Car.js
+++ src/Car.js
@@ -26,38 +26,38 @@ class Car {
   }
 
   static validateCarNameList(car) {
-    if (Car.isDuplicate(car)) {
+    if (Car.#isDuplicate(car)) {
       throw new Error(CAR_VALIDATION.DUPLICATE);
     }
 
-    if (Car.nameLength(car)) {
+    if (Car.#nameLength(car)) {
       throw new Error(CAR_VALIDATION.LENGTH);
     }
 
-    if (Car.blank(car)) {
+    if (Car.#blank(car)) {
       throw new Error(CAR_VALIDATION.BLANK);
     }
 
-    if (Car.special_symbol(car)) {
+    if (Car.#special_symbol(car)) {
       throw new Error(CAR_VALIDATION.SPECIAL_SYMBOL);
     }
   }
 
-  static isDuplicate(car) {
+  static #isDuplicate(car) {
     return car.some(
       (carName) => car.indexOf(carName) !== car.lastIndexOf(carName)
     );
   }
 
-  static nameLength(car) {
+  static #nameLength(car) {
     return car.some((carName) => carName.length > CAR_NAME_MAX_LENGTH + 1);
   }
 
-  static blank(car) {
+  static #blank(car) {
     return car.some((carName) => carName === "");
   }
 
-  static special_symbol(car) {
+  static #special_symbol(car) {
     return car.some((carName) => reg.test(carName));
   }
 }
