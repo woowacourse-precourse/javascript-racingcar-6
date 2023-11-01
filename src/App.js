8776bf18e6d56e84161da52939b4cc56392b80a7
Ryugi62 <xorjf1027@naver.com>
2023-11-01 18:51:13 +0900

    시도 횟수 입력 기능 제작 : 사용자로부터 시도 횟수 입력받아 횟수만큼 게임 진행

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
-// #### 1. 자동차 생성 기능
+// #### 2. 시도 횟수 입력 기능
 
-// - 사용자로부터 자동차 이름을 입력받아 쉼표(,)로 구분하여 자동차 객체를 생성
+// - 사용자로부터 시도 횟수를 입력받아 입력한 횟수만큼 자동차게임 진행
 
-// - 이름은 5자 이하로 제한
+// - 입력한 값은 0 이상의 정수 값이어야 함
 
-//   - 5자를 초과하는 경우 에러 메시지 출력 후 프로그램 종료
+//   - 0 미만의 값이 입력된 경우 에러 메시지 출력 후 프로그램 종료
 
 class App {
   constructor() {
-    this.carList = [];
+    this.status = {};
   }
 
   async play() {
     // 사용자가 입력한 차 이름 배열
-    this.carList = this.getCarNames();
+    const CAR_LIST = await this.getCarNames();
+
+    // 시도할 횟수 입력
+    const GAME_COUNT = await this.getGameCount();
+
+    this.initGame(CAR_LIST, GAME_COUNT);
+
+    while (this.status.gameCount) {
+      this.status.gameCount--;
+    }
   }
 
   async getCarNames() {
     // CPMMENT 출력 후 입력된 값을 USER_INPUT에 저장
     const COMMENT =
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n";
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n";
     const USER_INPUT = await MissionUtils.Console.readLineAsync(COMMENT);
 
     // 공백 제거 후 ","로 입력값을 나누어 저장
@@ -33,6 +42,24 @@ class App {
 
     // 사용자가 입력한 차 이름 배열 리턴
     return CARS;
+  }
+
+  async getGameCount() {
+    const COMMENT = "시도할 횟수는 몇 회인가요?\n";
+    const GAME_COUNT = await MissionUtils.Console.readLineAsync(COMMENT);
+
+    return GAME_COUNT;
+  }
+
+  initGame(CAR_LIST, GAME_COUNT) {
+    this.status = {
+      gameCount: GAME_COUNT,
+      gameResult: {},
+    };
+
+    CAR_LIST.forEach((e) => {
+      this.status.gameResult[e] = 0;
+    });
   }
 
   debug(...args) {
