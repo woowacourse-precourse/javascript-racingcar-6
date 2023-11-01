d6d9f5283dd26b9fa0cde4ce339d431ecb09f22e
윤태웅 <omygog@naver.com>
2023-11-01 23:41:13 +0900

    fix: test 원상복구

--- __tests__/CarNamesTest.js
+++ __tests__/CarNamesTest.js
@@ -1,44 +1,44 @@
-// import InputValidator from '../src/models/InputValidator.js';
-// import { ERROR_MESSAGE } from '../src/constants/index.js';
+import InputValidator from '../src/models/InputValidator.js';
+import { ERROR_MESSAGE } from '../src/constants/index.js';
 
-// describe('자동차 이름 유효성 테스트', () => {
-//   test('자동차 이름 5자 초과', () => {
-//     const carNames = ['KIA', 'Volvo', 'Ferrari'];
+describe('자동차 이름 유효성 테스트', () => {
+  test('자동차 이름 5자 초과', () => {
+    const carNames = ['KIA', 'Volvo', 'Ferrari'];
 
-//     expect(() => {
-//       InputValidator.validateCarName(carNames);
-//     }).toThrow(ERROR_MESSAGE.tooLongName);
-//   });
+    expect(() => {
+      InputValidator.validateCarName(carNames);
+    }).toThrow(ERROR_MESSAGE.tooLongName);
+  });
 
-//   test('자동차 이름을 입력하지 않음', () => {
-//     const carNames = ['Tesla', ''];
+  test('자동차 이름을 입력하지 않음', () => {
+    const carNames = ['Tesla', ''];
 
-//     expect(() => {
-//       InputValidator.validateCarName(carNames);
-//     }).toThrow(ERROR_MESSAGE.emptyName);
-//   });
+    expect(() => {
+      InputValidator.validateCarName(carNames);
+    }).toThrow(ERROR_MESSAGE.emptyName);
+  });
 
-//   test('자동차 이름을 아무것도 입력하지 않음', () => {
-//     const carNames = [''];
+  test('자동차 이름을 아무것도 입력하지 않음', () => {
+    const carNames = [''];
 
-//     expect(() => {
-//       InputValidator.validateCarName(carNames);
-//     }).toThrow(ERROR_MESSAGE.emptyName);
-//   });
+    expect(() => {
+      InputValidator.validateCarName(carNames);
+    }).toThrow(ERROR_MESSAGE.emptyName);
+  });
 
-//   test('한 개의 유효한 입력', () => {
-//     const carNames = ['링컨'];
+  test('한 개의 유효한 입력', () => {
+    const carNames = ['링컨'];
 
-//     expect(() => {
-//       InputValidator.validateCarName(carNames);
-//     }).not.toThrow();
-//   });
+    expect(() => {
+      InputValidator.validateCarName(carNames);
+    }).not.toThrow();
+  });
 
-//   test('다수의 유효한 입력', () => {
-//     const carNames = ['g70', 'g80', 'g90', '토요타'];
+  test('다수의 유효한 입력', () => {
+    const carNames = ['g70', 'g80', 'g90', '토요타'];
 
-//     expect(() => {
-//       InputValidator.validateCarName(carNames);
-//     }).not.toThrow();
-//   });
-// });
+    expect(() => {
+      InputValidator.validateCarName(carNames);
+    }).not.toThrow();
+  });
+});
