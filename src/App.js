a46a57a3e977281946c6af3c58707732217982cf
irenee-14 <ireneee145@gmail.com>
2023-11-01 20:01:27 +0900

    feature : 에러처리 수정

--- src/App.js
+++ src/App.js
@@ -10,64 +10,75 @@ class Car {
 	{
 		if (MissionUtils.Random.pickNumberInRange(0, 9) > 3)
             this.pos++;
-	}
-	printStatus()
-	{
 		MissionUtils.Console.print(this.name + ' : ' + '-'.repeat(this.pos));
 	}
-
 }
 
 class App {
 
-findWinner(cars)
-{
-	const max = Math.max(...cars.map((car) => car.pos));
-	MissionUtils.Console.print(max);
+  checkPlayerInput(playerArr)
+  {
+    const cars =[];
+    for (let player of playerArr)
+    {
+      if (player.length > 5){
+        throw new Error("[ERROR] 5글자 이상의 이름을 입력하세요.");
+      }
+      const car = new Car(player);
+      cars.push(car);
+    }
+    return cars;
+  }
 
-}
+  checkRoundInput(round) {
+      const ret = parseInt(round);
+      if (isNaN(ret)) {
+        throw new Error("[ERROR] 숫자가 아닙니다.");
+      }
+      if (ret < 0) {
+        throw new Error("[ERROR] 양수가 아닙니다.");
+      }
+      return ret;
+    }
 
-printResult(cars)
-{
-	findWinner(cars);
-}
+  findWinner(cars)
+  {
+    const max = Math.max(...cars.map((car) => car.pos));
+    const winner = cars.filter((car) => car.pos === max);
 
-async racingGame()
-{
-	let players = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
-	let round = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+    return winner;
+  }
 
-	let playerArr = players.split(",");
+  printResult(cars)
+  {
+    const winners = this.findWinner(cars);
 
-	let cars = [];
-	for (let player of playerArr)
-	{
-		if (player.length > 5)
-			throw new Error("[Error] 5글자 이상의 이름을 입력하세요.");
+    MissionUtils.Console.print("최종 우승자 : " + winners.map((winner) => winner.name).join(", "));
+  }
 
-		let car = new Car(player);
-		cars.push(car);
-	}
-	for (let i = 0 ; i < round ; i++)
-	{
-		for (let car of cars)
-		{
-			car.race();
-			car.printStatus();
-		}
-		MissionUtils.Console.print("");
-	}
-	//for (let car of cars)
-	//{
-	//	await MissionUtils.Console.print(car.name);
+  async racingGame()
+  {
 
-	//}
-	//await MissionUtils.Console.print(round);
-	this.findWinner(cars);
-}
+    let playerInput = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+    const playerArr = playerInput.split(",");
+    const cars = this.checkPlayerInput(playerArr);
+
+    let roundInput = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+
+    const round = this.checkRoundInput(roundInput);
+
+    MissionUtils.Console.print("\n실행 결과");
+    for (let i = 0 ; i < round ; i++)
+    {
+      for (let car of cars)
+        car.race();
+      MissionUtils.Console.print("");
+    }
+    this.printResult(cars);
+  }
 
   async play() {
-	this.racingGame();
+	  await this.racingGame();
   }
 }
 
