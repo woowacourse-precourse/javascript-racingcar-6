f80c26d0eaf6434b8822d44c4ccf9295fc8f660d
jonique98 <tnals9899@naver.com>
2023-11-01 20:30:48 +0900

    refactor: 상수 더 직관적으로 변경 상수를 더 이해하기 쉽게 변경

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -5,11 +5,11 @@ const MESSAGE = Object.freeze({
 });
 
 const ERROR = Object.freeze({
-  space: '[ERROR] : 이름에 빈 공간이 있습니다',
-  specialChar: '[ERROR] : 이름에 특수문자가 있습니다',
-  empty: '[ERROR] : 빈 이름이 있습니다',
-  duplicate: '[ERROR] : 중복된 이름이 있습니다',
-  moreThanFiveLetters: '[ERROR] : 이름이 다섯 글자를 초과합니다',
+  space: '[ERROR] : 이름에 공백이 있습니다.',
+  specialChar: '[ERROR] : 이름에 특수문자가 있습니다.',
+  empty: '[ERROR] : 빈 이름이 있습니다.',
+  duplicate: '[ERROR] : 중복된 이름이 있습니다.',
+  moreThanFiveLetters: '[ERROR] : 이름이 다섯 글자를 초과합니다.',
 });
 
 export { MESSAGE, ERROR };
