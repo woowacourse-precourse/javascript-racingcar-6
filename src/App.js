8a22c9f6703b3d90dc332e7b2cca443e19151203
lucia0929 <jsu02062@gmail.com>
2023-11-01 15:04:52 +0900

    feat: (1-2) 이름 입력 처리 구현

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ class App {
 
   async getCarName() {
     const inputName = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-    return inputName;
+    return this.sanitizeCarName(inputName);
+  }
+
+  sanitizeCarName(inputName) {
+    const sanitizedName = inputName.split(',').map(name => name.trim());
+    return sanitizedName;
   }
 }
 
