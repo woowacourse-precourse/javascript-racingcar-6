5d62c75bff504bd768ada5f22cd550a133deb59d
hs <tlsgustn93@naver.com>
2023-11-01 15:31:17 +0900

    fix: App 수정

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import GameMain from "./modules/gameMain.js";
 
 class App {
-  constructor() {
-    this.gameMain = new GameMain();
-  }
-
   async play() {
+    this.gameMain = new GameMain();
     return this.gameMain.userCarName();
   }
 }
-
-const app = new App();
-app.play();
 
 export default App;
