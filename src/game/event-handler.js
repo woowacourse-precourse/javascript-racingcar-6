55acf98a18d7d0f9ee877ab6d2488565b38ebc8c
yunminwon <wymm20@naver.com>
2023-11-01 14:06:53 +0900

    오류 수정 및 확인

---
+++ src/game/event-handler.js
@@ -0,0 +1,56 @@
+import RacingCarGame from './racing-car-game.js';
+import { CAR_NAME_ALERT_MESSAGE, COUNT_ALERT_MESSAGE } from './message.js';
+
+export default class EventHandler {
+    constructor(app) {
+        this.carNameInput = document.querySelector("#car-names-input");
+        this.countInput = document.querySelector("#racing-count-input");
+        this.app = app;
+    }
+
+    isCorrectCarName() {
+        const wrongNames = this.carNameInput.value
+            .split(',')
+            .filter(name => name.length > 5 || name.length < 1);
+        if(!wrongNames.length) return true;
+        else return false;
+    }
+
+    isCorrectCount() {
+        return Number.isInteger(Number(this.countInput.value)) && this.countInput.value >= 1;
+    }
+
+    onCarNameSubmit() {
+        if(!this.isCorrectCarName()) {
+            this.popAlertMessage(CAR_NAME_ALERT_MESSAGE, this.carNameInput);
+        }
+    }
+
+    onRacingCountSubmit() {
+        if(this.isCorrectCount()) {
+            if(this.isCorrectCarName()) {
+                this.resetScreenResult();
+
+                const carNameArray = this.carNameInput.value.split(',');
+                const racingCount = Number(this.countInput.value);
+                const newGame = new RacingCarGame(carNameArray, racingCount, this.app);
+                newGame.gameStart();
+            }
+            else this.popAlertMessage(CAR_NAME_ALERT_MESSAGE, this.carNameInput);
+        }
+        else this.popAlertMessage(COUNT_ALERT_MESSAGE, this.countInput);
+    }
+
+    resetScreenResult() {
+        const gameResult = document.querySelector('#screen-result');
+        while(gameResult.nextElementSibling) {
+            gameResult.nextElementSibling.remove();
+        }
+    }
+
+    popAlertMessage(message, input) {
+        alert(message);
+        input.value = '';
+        input.focus();
+    }
+};
---
+++ src/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import EventHandler from './game/event-handler.js';
+
+const app = document.querySelector("#app");
+const handler = new EventHandler(app);
+
+app.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    const handleFunctions = {
+        "car-names-submit"() { handler.onCarNameSubmit(); },
+        "racing-count-submit"() { handler.onRacingCountSubmit(); },
+    };
+    if(Object.keys(handleFunctions).includes(e.target.id)) handleFunctions[e.target.id]();
+});
