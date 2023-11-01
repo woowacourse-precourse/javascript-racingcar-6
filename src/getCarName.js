ee4c5cd9b9a02fd098528dcf7d7d8448ac53007d
Jeon Eonseok <jseokeon@gmail.com>
2023-11-01 21:25:17 +0900

    feat: 잘못된 입력값이 들어왔을 때 error 처리

--- src/getCarName.js
+++ src/getCarName.js
@@ -1,9 +1,17 @@
 import { Console } from '@woowacourse/mission-utils';
 
 const getCarName = async function getCarNameByUserInput() {
-  const CAR_NAME = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
+  const CAR_NAME_INPUT = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
 
-  return CAR_NAME.split(',');
+  const CAR_NAME_ARRAY = CAR_NAME_INPUT.split(',');
+
+  CAR_NAME_ARRAY.forEach((element) => {
+    if (element.length > 5) {
+      throw Error('[ERROR] 입력이 잘못된 형식입니다.');
+    }
+  });
+
+  return CAR_NAME_ARRAY;
 }
 
 export default getCarName;
--- src/getTryCount.js
+++ src/getTryCount.js
@@ -3,6 +3,10 @@ import { Console } from '@woowacourse/mission-utils';
 const getTryCount = async function getTryCountByUserInput() {
   const TRY_COUNT = await Console.readLineAsync('시도할 횟수는 몇 회인가요?');
 
+  if (!!isNaN(TRY_COUNT)) {
+    throw Error('[ERROR] 숫자가 잘못된 형식입니다.');
+  }
+
   return TRY_COUNT;
 }
 
