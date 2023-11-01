37c04fab7834bb81706ed9512f858245f31df532
urjimyu <92876819+urjimyu@users.noreply.github.com>
2023-11-01 17:23:42 +0900

    feature: 게임 순차적으로 실행

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import getCarName from './getCarName';
+import getRoundNumber from './getRoundNumber';
+import showRace from './showRace';
+
 class App {
-  async play() {}
+  async play() {
+    const carName = await getCarName();
+    const roundNumber = await getRoundNumber();
+    await showRace(carName, roundNumber);
+  }
 }
 
 export default App;
