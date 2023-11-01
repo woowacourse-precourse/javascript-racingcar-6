e9d1b0d04af6b61c7a54b830bc0d8038deb9f9a3
Hyurim <kyo2301@naver.com>
2023-11-01 15:05:48 +0900

    chore(RacingGame.js) : 주석 제거

--- src/controller/game/GameInProgress.js
+++ src/controller/game/GameInProgress.js
@@ -9,3 +9,4 @@ const gameInProgress = (keyValue) => {
 };
 
 export default gameInProgress;
+
--- src/model/RacingGame.js
+++ src/model/RacingGame.js
@@ -3,12 +3,11 @@ import GAME_MESSAGE from "../constants/gameMessage.js";
 import SYMBOLS from "../constants/symbols.js";
 import NameToKeyValueConverter from "../controller/game/NameToKeyValueConverter.js";
 import HyphenAdd from "../controller/game/HyphenAdd.js";
-import gameInProgress from "../controller/game/GameInProgress.js";
+import GameInProgress from "../controller/game/GameInProgress.js";
 import Winner from "../controller/game/Winner.js";
 
 class RacingGame {
   constructor() {
-    // this.nameToKeyValueConverter = new NameToKeyValueConverter();
     this.hyphenAdd = new HyphenAdd();
     this.winner = new Winner();
   }
