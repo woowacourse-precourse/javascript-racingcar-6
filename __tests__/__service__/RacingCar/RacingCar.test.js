3e38ad7de236952b6f0e96ece4eff0055d5acb69
jang_yoonsu <shagrat8546@gmail.com>
2023-11-01 14:07:05 +0900

    feat : racing car 클래스 start racing 테스트 작성

--- __tests__/__service__/RacingCar/RacingCar.test.js
+++ __tests__/__service__/RacingCar/RacingCar.test.js
@@ -25,7 +25,7 @@ const getLogSpy = () => {
 };
 
 describe("RacingCar 테스트", () => {
-  it("create game 테스트", () => {
+  test("create game 테스트", () => {
     const rule = new NormalRacingRule();
     const game = RacingCarGame.createGame(rule);
 
@@ -33,7 +33,7 @@ describe("RacingCar 테스트", () => {
   });
 
   describe("promptCarNames 테스트", () => {
-    it("all correct case 테스트", async () => {
+    test("all correct case 테스트", async () => {
       const rule = new NormalRacingRule();
       const game = RacingCarGame.createGame(rule);
 
@@ -58,7 +58,7 @@ describe("RacingCar 테스트", () => {
   });
 
   describe("promptTotalRounds 테스트", () => {
-    it("all correct case 테스트", async () => {
+    test("all correct case 테스트", async () => {
       const rule = new NormalRacingRule();
       const game = RacingCarGame.createGame(rule);
 
@@ -80,5 +80,43 @@ describe("RacingCar 테스트", () => {
         await expect(game.promptTotalRounds()).rejects.toThrow("[ERROR]");
       }
     );
+  });
+
+  test("startRacing 테스트", async () => {
+    const MOVING_FORWARD = 4;
+    const STOP = 3;
+    const inputs = ["pobi,woni,ynnss", 3];
+    const outputs = [
+      "실행 결과",
+      "pobi : -\nwoni : -\nynnss : ",
+      "pobi : -\nwoni : -\nynnss : -",
+      "pobi : --\nwoni : -\nynnss : --",
+    ];
+    const randoms = [
+      MOVING_FORWARD,
+      MOVING_FORWARD,
+      STOP, // 1라운드
+      STOP,
+      STOP,
+      MOVING_FORWARD, // 2라운드
+      MOVING_FORWARD,
+      STOP,
+      MOVING_FORWARD, // 3라운드
+    ];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms(randoms);
+
+    const rule = new NormalRacingRule();
+    const game = RacingCarGame.createGame(rule);
+
+    await game.promptCarNames();
+    await game.promptTotalRounds();
+    game.startRacing();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
   });
 });
