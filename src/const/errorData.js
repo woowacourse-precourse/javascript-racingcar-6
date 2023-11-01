ae1ce7509c8de12445040af6f4d87e97dd599674
intersoom <sallybig@naver.com>
2023-11-01 16:59:37 +0900

    fix: 시도 횟수 관련 에러 분리

--- src/const/errorData.js
+++ src/const/errorData.js
@@ -4,5 +4,6 @@ export const ERROR_MESSEAGE = Object.freeze({
     "[ERROR] 이름이 중복되었습니다. 서로 다른 이름을 입력해주세요.",
   CAR_COUNT_MIN: "[ERROR] 자동차는 최소 2대가 있어야지 경주가 가능합니다.",
   BLANK_FORM: "[ERROR] 공백 없이 입력해주세요.",
-  NUMBER_FORM: "[ERROR] 1 이상의 숫자의 형태로 입력해주세요.",
+  NUMBER_FORM: "[ERROR] 숫자의 형태로 입력해주세요.",
+  TRY_COUNT_MIN: "[ERROR] 1 이상의 숫자를 입력해주세요.",
 });
