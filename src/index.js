93139f5996d632d462b592f24097b2a706d6d7dc
0UNG2-FIRST\MS <popduddms@gmail.com>
2023-11-01 13:47:02 +0900

    refactor: 굳이 감싸주지 않아도 될 것 같아서 삭제하고 try~catch만 남겨둠

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import App from './App.js';
 
 const app = new App();
-async function main() {
-  try {
-    await app.play();
-  } catch (error) {
-    console.log(`${error.message}`);
-  }
+try {
+  await app.play();
+} catch (error) {
+  console.log(`${error.message}`);
 }
-main();
