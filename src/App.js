deb6d7872ab7ddcb2745b04b26d2f153bc90b6ab
정수아 <sua@jeongsuaui-MacBookAir.local>
2023-11-01 20:30:15 +0900

    [feat] 자동차 이름이 5글자를 초과하는 경우 에러 처리

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import {Console, Random} from '@woowacourse/mission-utils';
 class App {
   async play() {
     const userInput = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.');
-    console.log(userInput);
 
-    const CAR_NAME = userInput.split(',');
+    const CAR_NAME_ARRAY = userInput.split(',');
+    CAR_NAME.forEach(car => {
+      if(car.length > 5)  throw new Error(`[ERROR] 자동차 이름(${car})이 5글자를 초과합니다.`)
+    });
     const CAR = CAR_NAME.map(name=>({name, distance: 0}));
-    console.log(CAR);
 
   }
 }
