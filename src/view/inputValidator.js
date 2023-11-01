a470561ed8a87919248bc8d9ee88da6e02659b4e
mieum12 <qpdlqltb1215@gmail.com>
2023-11-01 17:20:57 +0900

    test: 검증 로직 횟수는 0이하 안되게 변경

--- src/view/inputValidator.js
+++ src/view/inputValidator.js
@@ -28,7 +28,7 @@ export class InputValidator {
 
   validateAttemptCountInput(input) {
     if (isNaN(Number(input))) throw new Error(ERROR.NUMBER_ONLY_ERROR);
-    // if (input.length < 1) throw new Error(ERROR.NAME_RANGE_ERROR);
+    if (Number(input) < 0) throw new Error(ERROR.NAME_RANGE_ERROR);
     // if (!Number(input)) throw new Error(ERROR.NUMBER_ONLY_ERROR);
     // if (input <= 0) throw new Error(ERROR.NAME_RANGE_ERROR);
   }
