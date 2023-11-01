90e2bcc05071928ae0b2943cb3f056d2c843879d
hyeonjun-L <beeminor60@gmail.com>
2023-11-01 18:38:51 +0900

    Test: 자동차 경주 엣지 케이스 테스트 추가

--- src/User.js
+++ src/User.js
@@ -3,28 +3,29 @@ import { MESSAGE, ERROR_MESSAGE } from './constants/message.js';
 
 export default class User {
   constructor({ initialState }) {
-    this.validationState(initialState);
+    this.validateinitialState(initialState);
     this.state = initialState;
   }
 
   setState(nextState) {
-    this.validationState(nextState);
     this.state = nextState;
   }
 
   async promptCarNames() {
     const userInput = await Console.readLineAsync(MESSAGE.CAR_NAME);
     const carList = userInput.split(',');
+    this.validateCarList(carList);
     this.setState({ ...this.state, carList });
   }
 
   async promptPlayNumber() {
     const playNumber = Number(await Console.readLineAsync(MESSAGE.PLAY_NUMBER));
+    this.validatePlayNumber(playNumber);
     this.setState({ ...this.state, playNumber });
   }
 
-  validationState(nextState) {
-    const { playNumber, carList } = nextState;
+  validateinitialState(initialState) {
+    const { carList, playNumber } = initialState;
 
     if (typeof playNumber !== 'number' || Number.isNaN(playNumber)) {
       throw new Error(ERROR_MESSAGE.NUMBER);
@@ -39,5 +40,27 @@ export default class User {
         throw new Error(ERROR_MESSAGE.NAME);
       }
     });
+  }
+
+  validateCarList(carList) {
+    if (!Array.isArray(carList)) {
+      throw new Error(ERROR_MESSAGE.ARRAY);
+    }
+
+    carList.forEach((name) => {
+      if (name.length > 5 || name === '') {
+        throw new Error(ERROR_MESSAGE.NAME);
+      }
+    });
+  }
+
+  validatePlayNumber(playNumber) {
+    if (
+      typeof playNumber !== 'number' ||
+      Number.isNaN(playNumber) ||
+      playNumber <= 0
+    ) {
+      throw new Error(ERROR_MESSAGE.NUMBER);
+    }
   }
 }
