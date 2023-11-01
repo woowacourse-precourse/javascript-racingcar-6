9bdee8bbc4569440349a12cc2ef127ee49ff247d
lbo728 <extreme93@naver.com>
2023-11-01 15:27:21 +0900

    feat: 자동차 이름 입력 추가
    - utils/CarNameGenerator 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import printStartMessage from "./constants/gameMessage";
+import { Console } from "@woowacourse/mission-utils";
 
+import OutputView from "./view/OutputView";
+import { CarNameGenerator } from "./utils/CarNameGenerator";
 
 class App {
+
+  constructor() {
+    this.inputView = new InputView();
+  }
+
   async play() {
-    printStartMessage();
+    this.outputView.printGameStartMessage();
+    CarNameGenerator();
   }
 }
+
 
 export default App;
