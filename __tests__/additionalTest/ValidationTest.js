40dc84811c7eeaeb5f74f66ab354207ad3ab306c
sihyonn <sihyeon982@naver.com>
2023-11-01 11:12:15 +0900

    refactor: ValidationTest 변수 영문으로 수정

--- __tests__/additionalTest/ValidationTest.js
+++ __tests__/additionalTest/ValidationTest.js
@@ -6,15 +6,15 @@ import { VALIDATION } from '../../src/constants/MESSAGE.js';
 
 describe('자동차이름 확인', () => {
   test('자동차이름 5자 초과 시 에러', () => {
-    const 잘못된이름 = ['차1', '차2', '차33333'];
-    expect(() => validateCarNames(잘못된이름)).toThrowError(
+    const invalidName = ['car1', 'car2', 'car3333'];
+    expect(() => validateCarNames(invalidName)).toThrowError(
       VALIDATION.NAME_LENGTH
     );
   });
 
   test('자동차 이름이 2개 미만일 시 에러', () => {
-    const 잘못된이름 = ['자동차1'];
-    expect(() => validateCarNames(잘못된이름)).toThrowError(
+    const invalidName = ['car1'];
+    expect(() => validateCarNames(invalidName)).toThrowError(
       VALIDATION.CAR_COUNT
     );
   });
@@ -22,14 +22,14 @@ describe('자동차이름 확인', () => {
 
 describe('시도횟수 확인', () => {
   test('시도 횟수가 숫자가 아닐 시 에러', () => {
-    const 잘못된횟수 = 'abc';
-    expect(() => validateTryCount(잘못된횟수)).toThrowError(
+    const inccorectCount = 'abc';
+    expect(() => validateTryCount(inccorectCount)).toThrowError(
       VALIDATION.TRY_COUNT
     );
   });
 
   test('시도 횟수가 빈 문자열이면 에러', () => {
-    const 빈횟수 = '';
-    expect(() => validateTryCount(빈횟수)).toThrowError(VALIDATION.TRY_COUNT);
+    const empty = '';
+    expect(() => validateTryCount(empty)).toThrowError(VALIDATION.TRY_COUNT);
   });
 });
