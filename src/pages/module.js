9a06523d2022b8cdcdc14f56cef00c15ee830a89
HyeonChae1104 <211063@jnu.ac.kr>
2023-11-01 19:31:06 +0900

    FEAT: "파일 모듈화(modules)"

---
+++ src/pages/module.js
@@ -0,0 +1,7 @@
+import inputCarName from '../components/inputCarName.js';
+import inputMoveCount from '../components/inputMoveCount.js';
+import moveCar from '../components/carMoveControl.js';
+import printWinner from '../components/printWinner.js';
+import progressRacingCar from '../components/progressRacingCar.js';
+
+export { inputCarName, inputMoveCount, moveCar, printWinner, progressRacingCar };
