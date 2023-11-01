fe6ece559ebbd7401dd72da1225f69aa682e2b52
usablechan <lch8910@naver.com>
2023-11-01 20:12:39 +0900

    fix : getCarName, getTryCount함수 오류 수정

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async play() {}
   async getCarName() {
-    const carName = await MissionUtils.Console.readLineSync(
+    const carName = await MissionUtils.Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)\n"
     );
     return carName.split(",");
   }
   async getTryCount() {
-    const tryCount = await MissionUtils.Console.readLineSync(
+    const tryCount = await MissionUtils.Console.readLineAsync(
       "시도할 회수는 몇회인가요?\n"
     );
     return tryCount;
