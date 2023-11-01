58efc7d75d8c50f5cd112b9d64169b04bdc0be59
강나연 <seoultownroad@naver.com>
2023-11-01 19:27:11 +0900

    feat: 랜덤 전진, 실행 결과 출력

--- src/App.js
+++ src/App.js
@@ -11,11 +11,15 @@ class App {
         '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
       );
       let cars = this.checkCarsValidation(cars);
+
+      const num = await MissionUtils.Console.readLineAsync(
+        '시도할 횟수는 몇 회인가요?'
+      );
+      this.checkNumValidation(num);
+
+      MissionUtils.Console.print('실행 결과');
+      this.printScoreBoard(cars, num);
     }
-    const num = await MissionUtils.Console.readLineAsync(
-      '시도할 횟수는 몇 회인가요?'
-    );
-    this.checkNumValidation(num);
   }
 
   checkCarsValidation(cars) {
@@ -36,6 +40,31 @@ class App {
   checkNumValidation(num) {
     if (num === null) {
       throw Error('[ERROR] 시도 횟수를 올바르게 입력해주세요');
+    }
+  }
+
+  getRandomNumber() {
+    return MissionUtils.Random.pickNumberInRange(0, 9);
+  }
+
+  countMoving() {
+    const randomNum = this.getRandomNumber();
+    return randomNum >= 4;
+  }
+
+  printDash(count) {
+    return '-'.repeat(count);
+  }
+
+  async printScoreBoard(cars, num) {
+    let results = [];
+    for (let score = 0; score < num; score++) {
+      for (let nowCar = 0; nowCar < cars.length; nowCar++) {
+        const isMove = this.countMoving();
+        const dashes = isMove ? this.printDash(1) : '';
+        results.push(`${cars[nowCar]} : ${dashes}`);
+      }
+      await MissionUtils.Console.print(results.join('\n'));
     }
   }
 }
