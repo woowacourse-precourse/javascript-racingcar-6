5ca389b5b3b3a75fd7efb062cc891b96418aa6c5
Jaehyeok <milkboy2564@naver.com>
2023-11-01 15:02:42 +0900

    fix: 안쓰는 상수 제거

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,21 +1,3 @@
-export const ANSWER_TABLE = {
-  "0b0s": "낫싱",
-  "1b0s": "1볼",
-  "2b0s": "2볼",
-  "3b0s": "3볼",
-  "0b1s": "1스트라이크",
-  "1b1s": "1볼 1스트라이크",
-  "2b1s": "2볼 1스트라이크",
-  "0b2s": "2스트라이크",
-  "1b2s": "1볼 2스트라이크",
-  "0b3s": "3스트라이크",
-};
-
-export const CONTINUE_OR_EXIT_CODE = {
-  CONTINUE: "1",
-  EXIT: "2",
-};
-
 export const MESSAGE_TABLE = {
   CAR_NAME: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
   TRY_COUNT: "시도할 횟수는 몇 회인가요?",
