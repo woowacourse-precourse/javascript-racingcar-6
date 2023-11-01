840cea3b92f7ffea2978438157736383c81d22c4
ryan-dia <cjfdnjs158@naver.com>
2023-11-01 12:04:02 +0900

    feat(controller): 레이싱 카 이름 저장하기

--- src/controller/index.js
+++ src/controller/index.js
@@ -3,9 +3,16 @@ import InputView from '../view/InputView.js';
 import OutputView from '../view/OutputView.js';
 
 class RacingController {
+  #racingModel;
+
+  constructor(racingModel) {
+    this.#racingModel = racingModel;
+  }
+
   async startGame() {
     const racingVehicleName = await InputView.readRacingVehicleName(SYSTEM.car);
     const racingCount = await InputView.readRacingCount();
+    this.#racingModel.saveNames(racingVehicleName);
     OutputView.print('\n실행 결과');
   }
 }
