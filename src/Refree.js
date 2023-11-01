53cb31a8e1a36aa57d869d8c96494ee54f257824
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-11-01 16:38:49 +0900

    refactor : 전진 가능 유무를 파악하는 메서드의 parameter 범위 예외처리
    #isOverRange() : isMovable() 메서드가 random으로 생성될 숫자의 범위보다 크거나 작은값이 들어오면 throw error

--- src/Refree.js
+++ src/Refree.js
@@ -1,3 +1,5 @@
+import { ERROR_MESSAGE } from './constants/errorMessage.js';
+import { RANDOM_NUMBER_RANGE } from './constants/numberRange.js';
 import { MOVE_FOWARD, RACING_RULE } from './constants/racingRule.js';
 import { paramType } from './utils/paramType.js';
 
@@ -9,6 +11,10 @@ export default class Refree {
   }
 
   clearRound() {
+    if (this.isGameFinish()) {
+      throw new Error(ERROR_MESSAGE.PLAY.MORE_ROUND_THAN_ALLOWED);
+    }
+
     this.#tryRound -= RACING_RULE.DECREASE_ROUND;
   }
 
@@ -16,7 +22,20 @@ export default class Refree {
     return this.#tryRound === RACING_RULE.ALL_ROUND_DONE_AMOUNT;
   }
 
-  isMovalbe(number, _ = paramType(number, 'number')) {
+  isMovable(number, _ = paramType(number, 'number')) {
+    if (!this.#isOverRange(number)) {
+      throw new Error(
+        `${RANDOM_NUMBER_RANGE.MIN}~${RANDOM_NUMBER_RANGE.MAX} 이외의 숫자가 들어왔어요`
+      );
+    }
+
     return number >= MOVE_FOWARD;
+  }
+
+  #isOverRange(number) {
+    return Array.from(
+      { length: RANDOM_NUMBER_RANGE.MAX - RANDOM_NUMBER_RANGE.MIN + 1 },
+      (_, idx) => idx + RANDOM_NUMBER_RANGE.MIN
+    ).includes(number);
   }
 }
