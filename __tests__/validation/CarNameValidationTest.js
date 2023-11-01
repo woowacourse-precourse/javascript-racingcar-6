474126f3e9ebdb768941c94013c896ad7e8f2043
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-11-01 19:29:02 +0900

    feat:자동차 이름 유효성 검증 테스트 구체적인 케이스 와 에러 메세지 추가

--- __tests__/validation/CarNameValidationTest.js
+++ __tests__/validation/CarNameValidationTest.js
@@ -1,4 +1,4 @@
-import { ERROR_PREFIX } from "../../src/constants.js";
+import { MESSAGE } from "../../src/constants.js";
 import { validate } from "../../src/input/validations.js";
 
 const limitExceededInput = ["abcdefg"];
@@ -7,11 +7,11 @@ const duplicatedInput = ["hj","hj"]
 describe("자동차 이름 입력 유효성 검증",()=>{
     test("문자열 길이 5 이하 검증", ()=> {
         const result = ()=> validate.carNames(limitExceededInput);
-        expect(result).toThrow(ERROR_PREFIX);
+        expect(result).toThrow(MESSAGE.CAR_NAME.ERROR.IS_OVER_FIVE);
     })
 
     test("중복된 자동차 이름 검증", ()=> {
         const result = ()=> validate.carNames(duplicatedInput);
-        expect(result).toThrow(ERROR_PREFIX);
+        expect(result).toThrow(MESSAGE.CAR_NAME.ERROR.IS_DUPLICATED);
     })
 })      
