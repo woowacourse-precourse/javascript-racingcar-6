00cc91e68bba7ec10f69bdcc282e4ba132b16958
visvaden7 <visvaden7@naver.com>
2023-11-01 09:45:27 +0900

    feat: 객체 구현 및 사용자 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import {MissionUtils} from "@woowacourse/mission-utils";
 
 class App {
+    async setRacingList () {
+        const { readLineAsync } = MissionUtils.Console
+        const cars = []
+        const inputCarName = await readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n")
+        const carNames = inputCarName.split(',')
+
+        carNames.forEach((name)=> {
+            this.checkingError(name)
+            cars.push({name, distanceDriven:0})
+        })
+        return cars
+    }
     async play() {
 
     }
