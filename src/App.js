3e298579e18aef0bf33bcd0f713a9e68531449d7
Mungjin01 <beckaljm@naver.com>
2023-11-01 19:19:48 +0900

    refactor: play 메소드 분리

--- src/App.js
+++ src/App.js
@@ -5,27 +5,48 @@ import { Console as MissionUtilsConsole } from '@woowacourse/mission-utils';
 
 class App {
   async play() {
-    const cars = await Car.createCarsFromInput();
-    const numberOfAttempts = await Attempt.getNumberOfAttempts();
-
+    const cars = await this.initializeCars();
+    const numberOfAttempts = await this.getNumberOfAttempts();
+  
+    this.printExecutionResult(cars, numberOfAttempts);
+  
+    const winners = await this.determineWinners(cars, numberOfAttempts);
+    this.printFinalWinners(winners);
+  }
+  
+  async initializeCars() {
+    return await Car.createCarsFromInput();
+  }
+  
+  async getNumberOfAttempts() {
+    return await Attempt.getNumberOfAttempts();
+  }
+  
+  printExecutionResult(cars, numberOfAttempts) {
     MissionUtilsConsole.print('\n실행 결과');
     for (let i = 0; i < numberOfAttempts; i++) {
         cars.forEach(car => car.move());
         cars.forEach(car => {
-            MissionUtilsConsole.print(`${car.name} : ${car.getDistanceString()}`);
+          MissionUtilsConsole.print(`${car.name} : ${car.getDistanceString()}`);
         });
         MissionUtilsConsole.print('\n');
     }
-
-    const winners = await Winner.start(cars, numberOfAttempts);
+  }
+  
+  async determineWinners(cars, numberOfAttempts) {
+    return await Winner.start(cars, numberOfAttempts);
+  }
+  
+  printFinalWinners(winners) {
     const winnerNames = winners.map(winner => winner.name);
-
+  
     if (winners.length === 1) {
-        MissionUtilsConsole.print(`최종 우승자 : ${winnerNames}`);
+      MissionUtilsConsole.print(`최종 우승자 : ${winnerNames}`);
     } else {
-        MissionUtilsConsole.print(`최종 우승자 : ${winnerNames.join(', ')}`);
+      MissionUtilsConsole.print(`최종 우승자 : ${winnerNames.join(', ')}`);
     }
   }
+  
 }
 
 export default App;
