dce1c23aa753bc8b2cfdd4cbd609ffc10a700ed5
정민승 <jeongminseung@jeongminseung-ui-MacBookAir.local>
2023-11-01 21:15:44 +0900

    변경 내용 커밋

--- src/App.js
+++ src/App.js
@@ -1,60 +1,38 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
-import * as readline from "readline";
+import { MissionUtils, Random, Console } from "@woowacourse/mission-utils";
 
 class App {
-  constructor() {
-    //사용자 입력 받기
-    this.rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
+  async play() {
+    this.getCarName();
+  }
+  // 차 이름 받기 (유저 입력 받는 것 앞에는 비동기?)
+  async getCarName() {
+    const car = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉽표(,) 기준으로 구분)"
+    );
+    const carName = car.split(",");
+
+    //자동차 이름 예외사항
+    for (let i = 0; i < carName.length; i++) {
+      if (carName[i].length > 5) {
+        throw new Error("[ERROR] 이름이 잘못된 형식입니다.");
+      }
+    }
+    this.getTryNum(carName);
   }
 
-  async play() {
-    try {
-      const carName = await this.getCarName();
-      const tryNum = await this.getTryNum();
-      this.result(carName, tryNum);
-    } finally {
-      this.rl.close(); // 게임 재실행을 위해 readline 인터페이스를 닫아줌
-    }
-  }
-  // 차 이름 받기 (유저 입력 받는 것 앞에는 비동기가 있어야 하나?)
-  async getCarName() {
-    return new Promise((resolve, reject) => {
-      this.rl.question(
-        //이름이 통과할 때도 있고 막힐 때도 있다.
-        "경주할 자동차 이름을 입력하세요.(이름은 쉽표(,) 기준으로 구분)",
-        function (userInput) {
-          const carName = userInput.split(",");
-          for (let i = 0; i < carName.length; i++) {
-            if (carName[i].length > 5) {
-              throw new Error("[ERROR] 이름이 잘못된 형식입니다.");
-            }
-          }
-          resolve(carName);
-          return carName;
-        }
-      );
-    });
-  }
   //시도 횟수 받기
-  async getTryNum() {
-    return new Promise((resolve, reject) => {
-      this.rl.question("시도할 횟수는 몇 회인가요?", function (userNumInput) {
-        const tryNum = userNumInput;
-        if (isNaN(tryNum) || tryNum < 0) {
-          throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
-        } else {
-          resolve(tryNum);
-          return tryNum;
-        }
-      });
-    });
+  async getTryNum(carName) {
+    const tryNum = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+    if (isNaN(tryNum) || tryNum < 0) {
+      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    }
+    this.result(carName, tryNum);
   }
+
   //결과 받기
   result(carName, tryNum) {
-    console.log("실행 결과");
+    Console.print("실행 결과");
+    console.log("MissionUtils.Console.print is called with message: 실행 결과");
     //전진한 횟수 저장하는 변수
     var goCount = [];
     for (let i = 0; i < carName.length; i++) {
@@ -63,12 +41,17 @@ class App {
     // 총 tryNum만큼 실행
     for (let k = 0; k < tryNum; k++) {
       for (let i = 0; i < carName.length; i++) {
-        const randomNum = MissionUtils.Random.pickNumberInRange(0, 9); //랜덤한 숫자 불러오기
+        const randomNum = Random.pickNumberInRange(0, 9); //랜덤한 숫자 불러오기
         if (randomNum >= 4) {
           goCount[i] += 1;
         }
-        console.log(carName[i], ":", "-".repeat(goCount[i]));
+        const message = `${carName[i]} : ${"-".repeat(goCount[i])}`;
+        Console.print(message);
+        console.log(
+          `MissionUtils.Console.print is called with message: ${message}`
+        );
       }
+      Console.print("");
     }
     //우승자 return 하기
     const winner = [];
@@ -79,7 +62,7 @@ class App {
       }
     }
 
-    console.log(`최종 우승자 : ${winner}`);
+    MissionUtils.Console.print(`최종 우승자 : ${winner}`);
   }
 }
 const app = new App();
--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,1 +1,2 @@
-//왜 게임 재실행 하려고 하면 안 되는 거지?
+const app = new App();
+app.play();
