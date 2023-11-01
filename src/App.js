18f3b1be1e55fcaf07ba54e1942fe6ee531a74cf
Taek Ki Min <127868094+TaekkiMin@users.noreply.github.com>
2023-11-01 16:32:43 +0900

    feat : 우승자 결정 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class Car {
-  constructor() {
+  constructor(name) {
     this.name = name;
     this.distance = 0;
   }
@@ -31,37 +31,32 @@ class App {
         );
       }
     }
-
-    if (overlapCarNames.length != carNames.length) {
-      // 중복된 이름을 뺀 배열 overlapCarNames와
-      // 입력을 통해 만들어진 carNames 배열의 길이를 비교해 다르면 중복 예외 처리
-      throw new Error("[ERROR] 중복된 이름은 허용되지 않습니다.");
-    }
   }
 
   async play() {
     const input = await MissionUtils.Console.readLineAsync(
-      "경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분"
+      "경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분\n"
     );
-    const carNames = input.split(",");
+    const carNames = input.split(","); // 배열로 변환
     this.validateCarNames(carNames);
     this.cars = carNames.map((name) => new Car(name));
 
     this.numberOfAttempts = Number(
-      await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요")
+      await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n")
     );
+
     if (isNaN(this.numberOfAttempts) || this.numberOfAttempts <= 0) {
       throw new Error("[ERROR] 올바른 횟수를 입력하세요.");
     }
-  }
 
-  getCarNames() {
-    // 자동차 이름 입력 받기
-    const input = MissionUtils.Console.readLineAsync(
-      "경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분"
-    );
-    this.validateCarNames(carNames);
-    this.carNames = input.split(",");
+    MissionUtils.Console.print("\n실행결과");
+    // 시도횟수만큼 돌려서 "-"를 통해 차 전진하는 for문
+    for (let i = 0; i < this.numberOfAttempts; i++) {
+      this.moveCars();
+      this.moveCarsResult();
+    }
+
+    this.winner();
   }
 
   moveCars() {
@@ -79,4 +74,30 @@ class App {
       MissionUtils.Console.print(`${car.name} : ${car.getDistance()}`);
     }
   }
+
+  getWinners() {
+    let maxDistance = 0;
+    for (let i = 0; i < this.cars.length; i++) {
+      if (this.cars[i].distance > maxDistance) {
+        maxDistance = this.cars[i].distance;
+      }
+    }
+
+    let winner = [];
+    for (let i = 0; i < this.cars.length; i++) {
+      if (this.cars[i].distance === maxDistance) {
+        winner.push(this.cars[i].name);
+      }
+    }
+
+    return winner;
+  }
+
+  winner() {
+    // 최종 우승자 출력 함수
+    const winner = this.getWinners();
+    MissionUtils.Console.print(`최종 우승자 : ${winner.join(",")}`);
+  }
 }
+
+export default App;
