840ee29097b1928c11be47a01cbe98104301ba94
cchaeyoung <kimchaeyoung05@naver.com>
2023-11-01 18:01:13 +0900

    feat: 횟수 입력 처리

--- src/raceGame.js
+++ src/raceGame.js
@@ -8,6 +8,11 @@ async function startRace() {
             throw new Error("[ERROR] 자동차 이름은 5글자 이하로 입력하세요.");
         }
     }
+
+    const numMoves = parseInt(await Console.readLineAsync('시도할 횟수는 몇 회인가요? '));
+        if (numMoves <= 0) {
+            throw new Error("[ERROR] 횟수는 1 이상이어야 합니다.");
+        }
 }
 
 module.exports = startRace;
