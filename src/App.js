687941a3f6d3b4940e338c30af0c4e5d40b7f142
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2023-11-01 16:41:27 +0900

    feat : 라운드 수 만큼 게임 진행

--- src/App.js
+++ src/App.js
@@ -10,6 +10,27 @@ class App {
     const round = await MissionUtils.Console.readLineAsync(
       "시도할 횟수는 몇 회인가요?\n"
     );
+
+    // 결과 담을 딕셔너리 초기화
+    const result = {};
+    for (let car of carList) {
+      result[car] = "";
+    }
+    MissionUtils.Console.print("\n실행 결과");
+
+    // round 수만큼 게임 진행
+    for (let i = 0; i < round; i++) {
+      // 라운드 게임 진행
+      for (let car of carList) {
+        const randomNum = MissionUtils.Random.pickNumberInRange(0, 9);
+        if (randomNum >= 4) {
+          result[car] += "-";
+        }
+        // 라운드별 결과 출력
+        MissionUtils.Console.print(`${car} : ${result[car]}`)
+      }
+      MissionUtils.Console.print("")
+    }
 }
 
 export default App;
