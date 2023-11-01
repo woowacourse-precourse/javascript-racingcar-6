f17509f1fe1e29acb39463eb09dafb41323141f8
jvn4dev <jvn4develop@gmail.com>
2023-11-01 15:39:20 +0900

    fix: make App class testable & replace name validation in App class

--- src/App.js
+++ src/App.js
@@ -1,41 +1,48 @@
-import Race from './Race';
+import Race from './Race.js';
 import { MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
-  static async play() {
-    try {
-      const inputCars = await MissionUtils.Console.readLineAsync(
-        '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
-      );
-      const carNames = inputCars.split(',').map((name) => name.trim());
+  async play() {
+    const inputCars = await MissionUtils.Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+    );
+    const carNames = inputCars.split(',').map((name) => name.trim());
 
-      const numberOfAttempts = parseInt(
-        await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?'),
-      );
-
-      if (isNaN(numberOfAttempts)) {
-        throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
+    carNames.map((name) => {
+      if (name.length > 5) {
+        throw new Error('[ERROR] 이름은 5자 이하만 가능합니다.');
       }
+    });
 
-      const race = new Race(carNames);
+    const numberOfAttempts = parseInt(
+      await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n'),
+    );
 
-      for (let i = 0; i < numberOfAttempts; i++) {
-        race.playTurn();
+    if (isNaN(numberOfAttempts)) {
+      throw new Error('[ERROR] 숫자가 잘못된 형식입니다.');
+    }
+
+    const race = new Race(carNames);
+
+    for (let i = 0; i < numberOfAttempts; i++) {
+      race.playTurn();
+      if (i === 0) {
         MissionUtils.Console.print('\n실행 결과');
-        race.printStatus();
       }
+      race.printStatus();
+    }
 
-      const winners = race.getWinners();
+    const winners = race.getWinners();
 
-      if (winners.length === 1) {
-        MissionUtils.Console.print(`최종 우승자 : ${winners[0]}`);
-      } else {
-        MissionUtils.Console.print(`최종 우승자 : ${winners.join(', ')}`);
-      }
-    } catch (error) {
-      console.error(error.message);
+    if (winners.length === 1) {
+      MissionUtils.Console.print(`최종 우승자 : ${winners[0]}`);
+    } else {
+      MissionUtils.Console.print(`최종 우승자 : ${winners.join(', ')}`);
     }
   }
 }
+
+const app = new App();
+app.play();
 
 export default App;
--- src/Car.js
+++ src/Car.js
@@ -2,9 +2,6 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 class Car {
   constructor(name) {
-    if (name.length > 5) {
-      throw new Error('[ERROR] 자동차의 이름은 5글자를 초과할 수 없습니다.');
-    }
     this.name = name;
     this.distance = 0;
   }
