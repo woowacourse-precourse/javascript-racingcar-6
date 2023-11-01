196f76220ab26783cda2dea0cd5ab3b2a6a6bf11
imeureka <lky062@naver.com>
2023-11-01 22:15:08 +0900

    feat : test 코드 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -46,17 +46,91 @@ describe("자동차 경주 게임", () => {
     });
   });
 
-  test.each([
-    [["pobi,javaji"]],
-    [["pobi,eastjun"]]
-  ])("이름에 대한 예외 처리", async (inputs) => {
+  test("한번의 라운드가 아닐 때", async () => {
     // given
+    const MOVING_FORWARD = 4;
+    const STOP = 3;
+    const inputs = ["pobi,woni", "3"];
+    const outputs = [
+      "pobi : -",
+      "pobi : --",
+      "pobi : ---",
+      "woni : -",
+      "woni : --",
+      "woni : ---",
+    ];
+    const randoms = [
+      MOVING_FORWARD,
+      STOP,
+      MOVING_FORWARD,
+      STOP,
+      MOVING_FORWARD,
+      STOP,
+    ];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([...randoms]);
+
+    // when
+    const app = new App();
+    await app.play();
+
+    // then
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("input값 없을 떄", async () => {
+    // given
+    const inputs = [""];
+    const logSpy = getLogSpy();
+
     mockQuestions(inputs);
 
     // when
     const app = new App();
+    await expect(app.play()).rejects.toThrow(
+      "[ERROR] 자동차 이름을 입력해주세요."
+    );
 
     // then
-    await expect(app.play()).rejects.toThrow("[ERROR]");
+    expect(logSpy).toHaveBeenCalledWith(
+      expect.stringContaining("[ERROR] 자동차 이름을 입력해주세요.")
+    );
   });
+
+  test("자동차 이름 유효성 확인", async () => {
+    // given
+    const inputs = ["this name is too long,java"];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+    await expect(app.play()).rejects.toThrow(
+      "[ERROR] 자동차 이름의 길이가 5 초과 입니다."
+    );
+
+    // then
+    expect(logSpy).toHaveBeenCalledWith(
+      expect.stringContaining("[ERROR] 자동차 이름의 길이가 5 초과 입니다.")
+    );
+  });
+
+  test.each([[["pobi,javaji"]], [["pobi,eastjun"]]])(
+    "이름에 대한 예외 처리",
+    async (inputs) => {
+      // given
+      mockQuestions(inputs);
+
+      // when
+      const app = new App();
+
+      // then
+      await expect(app.play()).rejects.toThrow("[ERROR]");
+    }
+  );
 });
