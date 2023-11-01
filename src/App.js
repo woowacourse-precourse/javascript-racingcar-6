228d22e6dc3973ee76a31080b24127b2a27d756d
anr4876 <dustjdanr@naver.com>
2023-11-01 13:42:13 +0900

    refactor : 모듈화 해제 빌드 실패 오류로 인한 모듈화 해제

--- src/App.js
+++ src/App.js
@@ -1,8 +1,59 @@
-import { Console } from "@woowacourse/mission-utils";
-import { MESSAGES } from "./messages.js";
-import { getUserData } from "./userInput.js";
-import { carToMap, currentMatchResults } from "./carUtils.js";
-import { winners } from "./resultUtils.js";
+import { Random, Console } from "@woowacourse/mission-utils";
+
+const MESSAGES = {
+  CAR: '경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)로 구분)',
+  NUM: '시도할 횟수는 몇 회인가요?',
+  RESULT: '실행 결과',
+};
+
+// 자동차가 전진하는지 멈추는지 결정
+function goAndStop() {
+    return Random.pickNumberInRange(0, 9) >= 4;
+}
+
+// 자동차 이름 map 자료형 변환
+function carToMap(carNames, carMap) {
+    carNames.split(',').forEach(name => {
+    handling(name.length);
+    carMap.set(name, "");
+    });
+}
+
+// 전진하는 자동차 기록
+function currentMatchResults(carMap) {
+    let highestScore = 0;
+    carMap.forEach((value, key) => {
+        if (goAndStop()) carMap.set(key, value + "-");
+        if (highestScore < carMap.get(key).length) highestScore = carMap.get(key).length;
+        Console.print(`${key} : ${carMap.get(key)}`);
+    });
+    Console.print('');
+    return highestScore;
+}
+
+// 예외 처리
+function handling(length) {
+    if (length > 5) {
+        throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    }
+}
+
+// 승자 출력
+export function winners(carMap, highestScore) {
+  const carWin = [];
+  carMap.forEach((value, key) => {
+      if (value.length === highestScore) {
+          carWin.push(key);
+      }
+  });
+  Console.print(`최종 우승자 : ${carWin.join(",")}`);
+}
+
+// 유저 입력 받기
+async function getUserData(message) {
+  const userData = await Console.readLineAsync(message);
+  return userData;
+}
 
 class App {
   async play() {
--- src/userInput.js
+++
@@ -1,6 +0,0 @@
-import { Console } from "@woowacourse/mission-utils";
-
-export async function getUserData(message) {
-  const userData = await Console.readLineAsync(message);
-  return userData;
-}
