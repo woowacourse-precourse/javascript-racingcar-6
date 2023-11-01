dd7c15f3de10d640db635a04b3c38aef7ffbe8cd
donginLee <55528388+donginLee@users.noreply.github.com>
2023-11-01 12:25:15 +0900

    feat: 입력으로 받은 자동차 배열 문자열과 시도 횟수 문자열을 바탕으로 Cars 클래스 RaceNumber 클래스 생성하는 코드 작성

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import RaceNumber from "./RaceNumber.js";
 class App {
   async play() {
     const carNamesString = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+    const cars= new Cars(carNamesString);
 
     const numberString = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+    const number = new RaceNumber(numberString)
 
     Console.print('\n');
+
+
+
   }
-  
 }
 
 export default App;
