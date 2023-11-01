019ca23a7c22d6257bd66528329bc68b84da5f93
Younghoon Ahn <ahnqwerty@gmail.com>
2023-11-01 14:25:08 +0900

    feat: carNameToArray함수 테스트 생성

--- __tests__/CarNameToArrayTest.js
+++ __tests__/CarNameToArrayTest.js
@@ -1,4 +1,4 @@
-import carNameToArray from "./functions/carNameToArray.js";
+import carNameToArray from "../src/functions/carNameToArray.js";
 
 describe("문자열 배열로 분리하기 테스트", () => {
   test("split 메서드로 주어진 값을 구분", () => {
@@ -11,15 +11,13 @@ describe("문자열 배열로 분리하기 테스트", () => {
 
   test("5글자를 초과하는 이름이 입력됐을 때 예외 발생", () => {
     const input = "pobi,woni,junjun";
-    const result = carNameToArray(input);
 
-    expect(result).toThrow(`[ERROR] 각 자동차의 이름은 0 ~ 5자 사이로 입력해야 합니다.`);
+    expect(() => carNameToArray(input)).toThrow(`[ERROR] 각 자동차의 이름은 0 ~ 5자 사이로 입력해야 합니다.`);
   });
 
   test("0글자 이름이 입력됐을 때 예외 발생", () => {
     const input = "pobi,,jun";
-    const result = carNameToArray(input);
-
-    expect(result).toThrow(`[ERROR] 각 자동차의 이름은 0 ~ 5자 사이로 입력해야 합니다.`);
+    
+    expect(() => carNameToArray(input)).toThrow(`[ERROR] 각 자동차의 이름은 0 ~ 5자 사이로 입력해야 합니다.`);
   });
 });
