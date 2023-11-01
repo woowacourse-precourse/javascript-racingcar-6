0096f8eae0ae061a7fae0a3ba00b5718ef2b5707
thewronghand <penfreak77@gmail.com>
2023-11-01 18:25:46 +0900

    refactor: 필드명 validator => inputValidator 변경

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -2,27 +2,27 @@ import { Console } from '@woowacourse/mission-utils';
 import { GAME_MESSAGES, SYMBOLS } from '../utils/constants.js';
 
 class InputView {
-  #validator;
+  #inputValidator;
 
-  constructor(validator) {
-    this.#validator = validator;
+  constructor(inputValidator) {
+    this.#inputValidator = inputValidator;
   }
 
   async readCarNamesInput() {
     const input = await Console.readLineAsync(
-      GAME_MESSAGES.carNameQuery + '\n',
+      GAME_MESSAGES.carNameQuery + '\n'
     );
     const names = input.split(SYMBOLS.carNameSeparator);
-    this.#validator.validateCarNames(names);
+    this.#inputValidator.validateCarNames(names);
     return names;
   }
 
   async readRoundsNumberInput() {
     const input = await Console.readLineAsync(
-      GAME_MESSAGES.roundsNumberQuery + '\n',
+      GAME_MESSAGES.roundsNumberQuery + '\n'
     );
     const roundsNumber = parseInt(input, 10);
-    this.#validator.validateRoundsNumber(roundsNumber);
+    this.#inputValidator.validateRoundsNumber(roundsNumber);
     return roundsNumber;
   }
 }
