21981ab6c33cd8f8d8115a344aaca1d069e6ec26
cyj <cyd5538@naver.com>
2023-11-01 15:21:52 +0900

    Fix: error message fix
    Error로 돼있떤 메세지들 ERROR로 수정

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App {
 
   validateCarNames(names) {
     if(names === "") {   
-      throw new Error("[Error] 자동차 이름을 입력해주세요")
+      throw new Error("[ERROR] 자동차 이름을 입력해주세요")
     }
     names = names.split(",");
 
@@ -27,16 +27,16 @@ class App {
 
     for (const name of names) { 
       if (name.trim() !== name ) {
-        throw new Error("[Error] 이름에 중복을 제거해주세요")
+        throw new Error("[ERROR] 이름에 중복을 제거해주세요")
       }
       if (name.length > 5) { 
-        throw new Error("[Error] 자동차의 이름은 5글자 이하로 적어주세요")
+        throw new Error("[ERROR] 자동차의 이름은 5글자 이하로 적어주세요")
       }
       if (DUPLICATE_NAME.has(name)) { 
-        throw new Error("[Error] 중복된 자동차 이름이 있습니다.");
+        throw new Error("[ERROR] 중복된 자동차 이름이 있습니다.");
       }
       if (INVALID_REGEX.test(name)) { 
-        throw new Error("[Error] 이름에 특수문자를 제거해주세요.");
+        throw new Error("[ERROR] 이름에 특수문자를 제거해주세요.");
     }
       DUPLICATE_NAME.add(name);
     }
@@ -54,7 +54,7 @@ class App {
   validateCountInput(input) {
     const NUMBER = Number(input);
     if (isNaN(NUMBER) || typeof NUMBER !== 'number') {
-        throw new Error("[Error] 시도할 횟수에는 숫자를 입력해주세요.");
+        throw new Error("[ERROR] 시도할 횟수에는 숫자를 입력해주세요.");
     }
   }
 
