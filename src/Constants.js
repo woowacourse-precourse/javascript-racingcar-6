5352fd83b10dd3684d07a42232dabd45f3737a7e
Seoan Yoon <earthdyoon@gmail.com>
2023-11-01 22:18:25 +0900

    refactor: 상수 값 별도 파일에 분리 및 컨벤션에 맞게 표기법 수정

---
+++ src/Constants.js
@@ -0,0 +1,15 @@
+export const MESSAGES = {
+  raceNameInputMessage:
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표 (,) 기준으로 구분)',
+  raceCountInputMessage: '시도할 횟수는 몇 회인가요?',
+  invalidNameErrorMessage:
+    '[ERROR] 자동차 이름은 1자 이상 5자 이하만 가능합니다.',
+  invalidCountErrorMessage: '[ERROR] 숫자가 잘못된 형식입니다.',
+  duplicatedNameErrorMessage: '[ERROR] 중복된 이름을 입력할 수 없습니다.',
+};
+
+export const STRINGS = {
+  raceResult: '실행 결과',
+  raceWinner: '최종 우승자',
+  carMovingToken: '-',
+};
--- src/Screen.js
+++ src/Screen.js
@@ -1,60 +1,43 @@
 import { Console } from '@woowacourse/mission-utils';
+import { MESSAGES, STRINGS } from './Constants';
 
 class Screen {
-  static MESSAGES = {
-    RACE_NAME_INPUT_MESSAGE:
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표 (,) 기준으로 구분)',
-    RACE_COUNT_INPUT_MESSAGE: '시도할 횟수는 몇 회인가요?',
-    INVALID_NAME_ERROR_MESSAGE:
-      '[ERROR] 자동차 이름은 1자 이상 5자 이하만 가능합니다.',
-    INVALID_COUNT_ERROR_MESSAGE: '[ERROR] 숫자가 잘못된 형식입니다.',
-    DUPLICATED_NAME_ERROR_MESSAGE: '[ERROR] 중복된 이름을 입력할 수 없습니다.',
-  };
-
-  static STRINGS = {
-    RACE_RESULT: '실행 결과',
-    RACE_WINNER: '최종 우승자',
-    CAR_MOVING_TOKEN: '-',
-  };
-
   static async inputNames() {
-    Console.print(this.MESSAGES.RACE_NAME_INPUT_MESSAGE);
+    Console.print(MESSAGES.raceNameInputMessage);
     const nameInput = await Console.readLineAsync();
     const names = nameInput.split(',').map((name) => name.trim());
 
     names.forEach((name) => {
       if (name.length === 0 || name.length > 5) {
-        throw new Error(this.MESSAGES.INVALID_NAME_ERROR_MESSAGE);
+        throw new Error(MESSAGES.invalidNameErrorMessage);
       }
     });
 
     if (names.length !== new Set(names).size) {
-      throw new Error(this.MESSAGES.DUPLICATED_NAME_ERROR_MESSAGE);
+      throw new Error(MESSAGES.duplicatedNameErrorMessage);
     }
 
     return names;
   }
 
   static async inputCount() {
-    Console.print(this.MESSAGES.RACE_COUNT_INPUT_MESSAGE);
+    Console.print(MESSAGES.raceCountInputMessage);
     const count = Number(await Console.readLineAsync());
 
     if (!Number.isInteger(count) || count <= 0) {
-      throw new Error(this.MESSAGES.INVALID_COUNT_ERROR_MESSAGE);
+      throw new Error(MESSAGES.invalidCountErrorMessage);
     }
 
     return count;
   }
 
   static printResultMessage() {
-    Console.print(`\n${this.STRINGS.RACE_RESULT}`);
+    Console.print(`\n${STRINGS.raceResult}`);
   }
 
   static printResult(car) {
     const { name, moveCount } = car;
-    Console.print(
-      `${name} : ${this.STRINGS.CAR_MOVING_TOKEN.repeat(moveCount)}`,
-    );
+    Console.print(`${name} : ${STRINGS.carMovingToken.repeat(moveCount)}`);
   }
 
   static printNewline() {
@@ -62,7 +45,7 @@ class Screen {
   }
 
   static printWinnersName(winners) {
-    Console.print(`${this.STRINGS.RACE_WINNER} : ${winners.join(', ')}`);
+    Console.print(`${STRINGS.raceWinner} : ${winners.join(', ')}`);
   }
 }
 
