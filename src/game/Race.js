45abc107c3f74394f30d37358c1573c95be399e1
devGEP <ju0032564@gmail.com>
2023-11-01 16:56:40 +0900

    refact: Race.js에서의 로직 분리

--- src/game/Race.js
+++ src/game/Race.js
@@ -1,37 +1,21 @@
-import { Random } from "@woowacourse/mission-utils";
 import Car from "../model/Car.js";
 import GameResult from "./GameResult.js";
-import IOManager from "../utils/IOManager.js";
 
 class Race {
   constructor() {
     this.car = new Car();
-    this.ioManager = new IOManager();
     this.gameResult = new GameResult();
-  }
-
-  createRandomNum() {
-    return Random.pickNumberInRange(0, 9);
   }
 
   executeRace(carsName, raceCount) {
     let carsPosition = new Array(carsName.length).fill(0);
     
     for (let i=0 ; i<raceCount; i++) {
-      this.moveAllCars(carsName, carsPosition);
+      this.car.moveAllCars(carsName, carsPosition);
       console.log();
     }
 
     return this.gameResult.calculateWinner(carsName, carsPosition);
-  }
-
-  moveAllCars(carsName, carsPosition) {
-    carsName.forEach((carName, index) => {
-      const randomNum = this.createRandomNum();
-
-      carsPosition[index] += this.car.moveOrStop(randomNum);
-      this.ioManager.printCurrentPosition(carName, carsPosition[index]);
-    })
   }
 }
 
--- src/model/Car.js
+++ src/model/Car.js
@@ -1,6 +1,16 @@
 import { RANDOM_NUMBER_THRESHOLD } from "../utils/constants.js";
+import { Random } from "@woowacourse/mission-utils";
+import IOManager from "../utils/IOManager.js";
 
 class Car {
+  constructor() {
+    this.ioManager = new IOManager();
+  }
+
+  createRandomNum() {
+    return Random.pickNumberInRange(0, 9);
+  }
+
   moveOrStop(randomNum) {
     if (randomNum >= RANDOM_NUMBER_THRESHOLD) {
       return 1;
@@ -8,6 +18,15 @@ class Car {
     if (randomNum < RANDOM_NUMBER_THRESHOLD) {
       return 0;
     }
+  }
+
+  moveAllCars(carsName, carsPosition) {
+    carsName.forEach((carName, index) => {
+      const randomNum = this.createRandomNum();
+
+      carsPosition[index] += this.moveOrStop(randomNum);
+      this.ioManager.printCurrentPosition(carName, carsPosition[index]);
+    });
   }
 }
 
