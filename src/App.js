a524793ed15a71626b7fbef0072f53980242ed99
nyh98 <nsi3358@naver.com>
2023-11-01 10:31:22 +0900

    마지막수정

--- src/App.js
+++ src/App.js
@@ -7,12 +7,10 @@ class App {
     //게임 시작
     const CAR_NAMES = await Input.uswerInputCarName();
     const CYCLE_SIZE = await Input.uswerInputCycle();
-    const CAR_MOVE_DECIDE = GameStart.carMoveDecide;
-    const CAR_MOVE_TRY_RESULT = GameStart.carMoveTryResult;
 
     for (let i = 0; i < CYCLE_SIZE; i++) {
-      CAR_MOVE_TRY_RESULT(CAR_NAMES);
-      CAR_MOVE_DECIDE(CAR_NAMES);
+      GameStart.carMoveTryResult(CAR_NAMES);
+      GameStart.carMoveDecide(CAR_NAMES);
     }
   }
 }
