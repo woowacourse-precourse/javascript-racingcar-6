31ef55854c7e109a35ecc6b37e6af18cc1f69aff
hyj5076 <hyj5076@hanmail.net>
2023-11-01 17:00:04 +0900

    test(UserInputRound): 라운드 수 유효성 검증 테스트

--- __tests__/ErrorTest.js
+++ __tests__/ErrorTest.js
@@ -1,10 +1,16 @@
 import UserInputCarName from "../src/utils/UserInputCarName";
+import UserInputRound from "../src/utils/UserInputRound";
 import App from "../src/App";
 import { Console } from "@woowacourse/mission-utils";
 
-jest.mock("@woowacourse/mission-utils");
+jest.mock("@woowacourse/mission-utils", () => ({
+  Console: {
+    print: jest.fn(),
+    readLineAsync: jest.fn(),
+  },
+}));
 
-describe("예외 처리", () => {
+describe("자동차 이름 예외 처리", () => {
   let userInputCarName;
 
   beforeEach(() => {
@@ -36,5 +42,36 @@ describe("게임 종료", () => {
     } catch (error) {}
 
     expect(endGameSpy).toHaveBeenCalled();
+  });
+});
+
+describe("라운드 수 예외 처리", () => {
+  let userInputRound;
+
+  beforeEach(() => {
+    userInputRound = new UserInputRound();
+  });
+
+  test("올바른 라운드 수 입력 시 라운드 값 설정", () => {
+    const validRounds = 5;
+    userInputRound.setRounds(validRounds);
+
+    expect(userInputRound.getRounds()).toBe(validRounds);
+  });
+
+  test("올바르지 않은 라운드 수 입력 시 에러 던짐", () => {
+    const invalidRounds = -1;
+
+    expect(() => {
+      userInputRound.setRounds(invalidRounds);
+    }).toThrowError("[ERROR]");
+  });
+
+  test("문자열 입력 시 에러 던짐", () => {
+    const invalidRounds = "five";
+
+    expect(() => {
+      userInputRound.setRounds(invalidRounds);
+    }).toThrowError("[ERROR]");
   });
 });
