794304d039037ece17268a9a2bf9d72f93f3a7a7
ReturnReason <reasonz@daum.net>
2023-11-01 22:07:04 +0900

    test(UtilTest): generateRandomNumber 반환 값 테스트

--- __tests__/UtilTest.js
+++ __tests__/UtilTest.js
@@ -7,4 +7,20 @@ describe('✨ 유틸함수 테스트', () => {
     expect(randomNumber).toBeGreaterThanOrEqual(0);
     expect(randomNumber).toBeLessThanOrEqual(9);
   });
+
+  test('[generateRandomNumber] 0부터 9사이 랜덤 값을 반환한다.', () => {
+    const mockedGenerateRandomNumber = jest.fn();
+    mockedGenerateRandomNumber.mockReturnValue(0);
+
+    expect(mockedGenerateRandomNumber()).toBe(0);
+
+    mockedGenerateRandomNumber.mockReturnValue(2);
+    expect(mockedGenerateRandomNumber()).toBe(2);
+
+    mockedGenerateRandomNumber.mockReturnValue(3);
+    expect(mockedGenerateRandomNumber()).toBe(3);
+
+    mockedGenerateRandomNumber.mockReturnValue(9);
+    expect(mockedGenerateRandomNumber()).toBe(9);
+  });
 });
