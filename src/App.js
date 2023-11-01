40a14c555b808b8a4ea8c84110b4361649862462
Hyuna Cho <128371729+hyunaday@users.noreply.github.com>
2023-11-01 23:57:15 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,71 +1,80 @@
 const { Console, Random } = require('@woowacourse/mission-utils');
 
-class App {
+class Car {
+  constructor(name) {
+    this.name = name;
+    this.position = 0;
+  }
+
+  move() {
+    const DISTANCE = Random.pickNumberInRange(0, 9);
+    if (DISTANCE >= 4) {
+      this.position += 1;
+    }
+  }
+}
+
+class RaceGame {
+  constructor() {
+    this.cars = [];
+  }
+
   async play() {
     try {
-      const CAR_NAMES = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) ');
-      if (!/^[\w,]{1,25}$/.test(CAR_NAMES)) {
-        throw new Error('[ERROR] 자동차 이름이 잘못된 형식입니다.');
-      }
-
-      const NUM_MOVES = await Console.readLineAsync('Enter the number of moves: ');
-      if (!/^\d+$/.test(NUM_MOVES)) {
-        throw new Error('[ERROR] 이동횟수가 잘못된 형식입니다.');
-      }
-
-      this.startRace(CAR_NAMES, parseInt(NUM_MOVES));
+      this.initializeGame();
+      const NUM_MOVES = await this.getNumMovesFromUser();
+      await this.runRace(NUM_MOVES);
+      await this.printWinners();
     } catch (error) {
       await Console.printAsync(error.message + '\n');
     }
   }
 
-  async startRace(carNames, numMoves) {
-    const CARS = carNames.split(',').map(name => ({ name, position: 0 }));
+  initializeGame() {
+    const CAR_NAMES = Console.readLineAsync('경주할 자동차 이름을 경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
+    const NAMES = CAR_NAMES.split(',');
+    this.cars = NAMES.map(name => new Car(name));
+  }
 
-    async function race() {
-      for (let MOVE = 0; MOVE < numMoves; MOVE++) {
-        await Console.printAsync('\nRace ' + (MOVE + 1) + ':\n');
-
-        for (const CAR of CARS) {
-          const distance = Random.pickNumberInRange(0, 9);
-          if (distance >= 4) {
-            CAR.position += 1;
-          }
-
-          let track = '';
-          for (let i = 0; i < CAR.position; i++) {
-            track += '-';
-          }
-          await Console.printAsync(`${CAR.name}: ${track}\n`);
-        }
-        
-        await sleep(1000);
-      }
+  async getNumMovesFromUser() {
+    const NUM_MOVES = await Console.readLineAsync('시도할 횟수는 몇 회인가요?');
+    if (!/^\d+$/.test(NUM_MOVES)) {
+      throw new Error('[ERROR] 이동횟수가 잘못된 형식입니다.');
     }
+    return parseInt(NUM_MOVES);
+  }
 
-    race();
-
-    setTimeout(async () => {
-      const MAX_POSITION = Math.max(...CARS.map(CAR => CAR.position));
-      const WINNERS = CARS.filter(CAR => CAR.position === MAX_POSITION);
-
-      if (WINNERS.length === 1) {
-        await Console.printAsync('최종 우승자 : ' + WINNERS[0].name + '\n');
-      } else {
-        await Console.printAsync('최종 우승자 : ' + WINNERS.map(WINNER => WINNER.name).join(', ') + '\n');
+  async runRace(NUM_MOVES) {
+    for (let MOVE = 0; MOVE < NUM_MOVES; MOVE++) {
+      await Console.printAsync(`\nRace ${MOVE + 1}:\n`);
+      for (const CAR of this.cars) {
+        CAR.move();
+        this.printCarStatus(CAR);
       }
-      // No process.exit() here
-    }, numMoves * 1000);
+      await this.sleep(1000);
+    }
+  }
+
+  printCarStatus(CAR) {
+    const TRACK = '-'.repeat(CAR.position);
+    Console.printAsync(`${CAR.name}: ${TRACK}\n`);
+  }
+
+  async printWinners() {
+    const MAX_POSITION = Math.max(...this.cars.map(CAR => CAR.position));
+    const WINNERS = this.cars.filter(CAR => CAR.position === MAX_POSITION);
+
+    if (WINNERS.length === 1) {
+      await Console.printAsync(`최종 우승자: ${WINNERS[0].name}\n`);
+    } else {
+      const WINNER_NAMES = WINNERS.map(WINNER => WINNER.name).join(', ');
+      await Console.printAsync(`최종 우승자: ${WINNER_NAMES}\n`);
+    }
+  }
+
+  sleep(MS) {
+    return new Promise(resolve => setTimeout(resolve, MS));
   }
 }
 
-module.exports = App;
-
-// Define the sleep function
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-// Initialize the application
-const app = new App();
-app.play();
+module.exports = RaceGame;
