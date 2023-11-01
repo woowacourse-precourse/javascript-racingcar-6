487266c9df3b48b7d279a1b071ab57a3dd89ce0e
오지원 <64193469+jiwon-o@users.noreply.github.com>
2023-11-01 17:41:52 +0900

    feat: 자동차 이름이 5자 이상일 때의 예외처리 추가

--- src/App.js
+++ src/App.js
@@ -14,8 +14,16 @@ class App {
       "경주할 자동차 이름을 입력하세요.(이름은 쉽표(,) 기준으로 구분) \n"
     );
     const carNames = carsInput.split(",").map((name) => name.trim());
+    if (!this.validateCarNames(carNames)) {
+      throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+    }
     return carNames;
   }
+
+  validateCarNames(names) {
+    return names.every((name) => name.length <= 5);
+  }
+
 }
 
 export default App;
