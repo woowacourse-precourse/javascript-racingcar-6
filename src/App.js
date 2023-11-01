71b11033567dd983288f3428d67ba997cf4ef033
yunna <homer_j@hanmail.net>
2023-11-01 19:34:19 +0900

    feat(App) : 자동차 경주 게임 1차 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,60 @@
+import { Console } from '@woowacourse/mission-utils';
+import Car from './models/Car.js';
+import { getUserInput } from './utils/input.js';
+import { validateCarNames, validateRounds } from './utils/validation.js';
+
 class App {
-  async play() {}
+  constructor() {
+    this.cars = [];
+    this.rounds = 0;
+  }
+
+  async getInput() {
+    const { carNames, rounds } = await getUserInput();
+    const validCarNames = validateCarNames(carNames);
+    this.cars = validCarNames.map((name) => new Car(name));
+    this.rounds = validateRounds(rounds);
+  }
+
+  async runRaces() {
+    for (let i = 0; i < this.rounds; i++) {
+      this.playRound();
+      this.printCarPositions();
+    }
+  }
+
+  async play() {
+    try {
+      await this.getInput();
+      Console.print('실행 결과');
+      await this.runRaces();
+      this.printResult();
+    } catch (error) {
+      Console.print(error.message);
+      throw error;
+    }
+  }
+
+  playRound() {
+    this.cars.forEach((car) => car.move());
+  }
+
+  printCarPositions() {
+    this.cars.forEach((car) => car.printPosition());
+    Console.print('');
+  }
+
+  printResult() {
+    const winners = this.getWinners();
+    Console.print(`우승자: ${winners.join(', ')}`);
+  }
+
+  getWinners() {
+    const maxPosition = Math.max(...this.cars.map((car) => car.position));
+    return this.cars
+      .filter((car) => car.position === maxPosition)
+      .map((car) => car.name);
+  }
 }
 
 export default App;
