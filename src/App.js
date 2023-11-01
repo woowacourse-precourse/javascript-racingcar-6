c61777eec94cb9239eef065059efa7302b89411c
sryung <sryung1225@gmail.com>
2023-11-01 17:32:39 +0900

    refactor(getNumberOfRounds): 경주 회차 입력 로직을 모듈로 분리
    경주 회차 수를 입력받는 메소드 getNumberOfRounds를 App이 아닌 다른 클래스의 메소드로 분리 : Game/UserInputRounds.js
    
    테스트 실황
    Test Suites: 2 passed, 2 total
    Tests:       7 passed, 7 total
    Snapshots:   0 total
    Time:        1.049 s

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import { MissionUtils } from '@woowacourse/mission-utils';
-import { MESSAGE, ERROR } from './constants/index.js';
-import { Race } from './Game/Race.js';
 import { UserInputCarNames } from './Game/UserInputCarNames.js';
+import { UserInputRounds } from './Game/UserInputRounds.js';
+import { Race } from './Game/Race.js';
 
 class App {
   async play() {
     const CARS = await UserInputCarNames.getCarNames();
-    const ROUNDS = await this.getNumberOfRounds();
+    const ROUNDS = await UserInputRounds.getNumberOfRounds();
     const RACE = new Race(CARS, ROUNDS);
     RACE.runRace();
-  }
-
-  async getNumberOfRounds() {
-    const COUNTS = await MissionUtils.Console.readLineAsync(
-      MESSAGE.raceRoundForStart,
-    );
-    const ROUNDS = parseInt(COUNTS, 10);
-    if (isNaN(ROUNDS)) {
-      throw new Error(ERROR.roundInputNumber);
-    }
-    return ROUNDS;
   }
 }
 
---
+++ src/Game/UserInputRounds.js
@@ -0,0 +1,15 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { MESSAGE, ERROR } from '../constants/index.js';
+
+export class UserInputRounds {
+  static async getNumberOfRounds() {
+    const COUNTS = await MissionUtils.Console.readLineAsync(
+      MESSAGE.raceRoundForStart,
+    );
+    const ROUNDS = parseInt(COUNTS, 10);
+    if (isNaN(ROUNDS)) {
+      throw new Error(ERROR.roundInputNumber);
+    }
+    return ROUNDS;
+  }
+}
