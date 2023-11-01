4786036a5a41b2ac1e745a319761ff61e0476528
gaeng <78129949+hyeonkyeong31@users.noreply.github.com>
2023-11-01 18:22:35 +0900

    feat
    feat : 자동차 이름 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
-	async play() {}
+  async play() {
+    try {
+      const CAR_NAMES = await this.getCarNames();
+    } catch (error) {
+      MissionUtils.Console.print(error.message);
+    }
+  }
+  async getCarNames() {
+    const CAR_NAMES_INPUT = await MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): "
+    );
+    const CAR_NAMES = CAR_NAMES_INPUT.split(",");
+    return CAR_NAMES;
+  }
 }
 
 export default App;
