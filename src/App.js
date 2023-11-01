51b77b92c8e737b3fb1edd34811c90e1651380d9
CHHyuk <gksgur1038@naver.com>
2023-11-01 20:03:50 +0900

    함수를 class 안에 넣어서 this 불러올 수 있게 만들기(this는 메서드 내에서만 통용)

--- src/App.js
+++ src/App.js
@@ -1,55 +1,68 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
-// 자동차 객체를 배열에 추가
-async function inputCars() {
-  const input_name = await MissionUtils.Console.readLineAsync(
-    "경주할 자동차 이름을 입력해주세요.(쉼표(,) 기준으로 구분)"
-  );
-  const carArray = input_name.split(',');
-  carArray.forEach(element => {
-    if(element.length > 5) {
-      throw new Error("[ERROR] 이름의 형식이 잘못되었습니다.")
+class App {
+  carArray = [];
+  tryNumber = 0;
+
+  async play() {
+    this.carArray = await this.inputCars();
+    this.tryNumber = await this.inputTryNum();
+    for (let i = 0; i < this.tryNumber; i++) {
+      this.moveCar();
+      this.showDistance();
+    }
+    this.showWinner();
+  }
+
+  async inputCars() {
+    const input_name = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력해주세요.(쉼표(,) 기준으로 구분)"
+    );
+    const carNames = input_name.split(',');
+    const carArray = carNames.map(name => ({ name, distance: 0 }));
+    carArray.forEach((car) => {
+      if (car.name.length > 5) {
+        throw new Error("[ERROR] 이름의 형식이 잘못되었습니다.");
+      }
+    });
+  
+    return carArray;
+  }
+
+  async inputTryNum() {
+    const input_num = await Console.readLineAsync(
+      "시도할 횟수를 입력해주세요."
+    );
+    this.tryNumber = parseInt(input_num, 10);
+
+    if (isNaN(this.tryNumber)) {
+      throw new Error("[ERROR] 입력 형식이 잘못되었습니다.");
     };
-  });
 
-  return carArray;
-};
-
-// 이동할 횟수 입력
-async function inputTryNum() {
-  const input_num = await MissionUtils.Console.readLineAsync(
-    "시도할 횟수를 입력해주세요."
-  );
-  this.num = parseInt(input_num,10);
-
-  if (isNaN(this.num)) {
-    throw Error("[ERROR] 입력 형식이 잘못되었습니다.")
+    return this.tryNumber;
   };
 
-  return input_num;
-};
+  moveCar() {
+    this.carArray.forEach((car) => {
+      if (Random.pickNumberInRange(0, 9) >= 4) {
+        car.distance += 1;
+      }
+    });
+  }
 
-// 전진 조건 설정
-function moveCar() {
-  this.carArray.forEach((car) => {
-    if (MissionUtils.Random.pickNumberInRange(0,9) >= 4) {
-      car.distance += 1;
-    }
-  });
-}
+  showDistance() {
+    this.carArray.forEach((car) => {
+      const distanceText = '-'.repeat(car.distance);
+      Console.print(`${car.name} : ${distanceText}`);
+    });
+  }
 
-// 전진 표현
-function distance() {
-  this.carArray.forEach((car) => {
-    const distanceText = '-'.repeat(car.distance);
-    MissionUtils.Console.print(`${car.name} : ${distanceText}`);
-  });
-}
-
-
-class App {
-
-  async play() {}
+  showWinner() {
+    const max = Math.max(...this.carArray.map((car) => car.distance));
+    const winner = this.carArray.filter((car) => car.distance === max).map((car) => car.name);
+    const winnerText = winner.join(", ");
+    Console.print(`최종 우승자 : ${winnerText}`);
+  }
 }
 
 export default App;
