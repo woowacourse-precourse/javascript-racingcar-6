4aeb57ccdae7d0113efa25c1dce2f0b3f3e2e72b
ChoiYongWon <yongwon0824@naver.com>
2023-11-01 19:59:50 +0900

    feat: 자동차 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,54 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  constructor() {
+    this.car = [];
+    this.data = [];
+    this.winner = [];
+    this.attepmt = 0;
+  }
+
+  getData() {
+    return this.data;
+  }
+
+  getCar() {
+    return this.car;
+  }
+
+  getAttempt() {
+    return this.attepmt;
+  }
+
+  getWinner() {
+    return this.winner;
+  }
+
+  setCar(car) {
+    this.car = car;
+  }
+
+  setAttempt(attempt) {
+    this.attepmt = Number(attempt);
+  }
+
+  setData(length) {
+    this.data = new Array(length).fill("");
+  }
+
+  async initialize() {
+    let car_input = (
+      await Console.readLineAsync(
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+      )
+    ).split(",");
+
+    this.setCar(car_input);
+  }
+
+  async play() {
+    await this.initialize();
+  }
 }
 
 export default App;
