62cfd61564e6aab3a30d8419eb1282c7cf8c9517
yheseul_kang <kangyheseul@naver.com>
2023-11-01 20:43:43 +0900

    Test: 테크트 코드 수정 // TryNumber.js 파일에 대한 테스트 코드 .tobe() => .toEqual()로 변경

--- __tests__/TryNumberTest.js
+++ __tests__/TryNumberTest.js
@@ -1,9 +1,9 @@
 const number = require('../src/TryNumber');
 
 test('pobi,woni,jun은 try_number만큼 자동차 경주를 진행한다.', ()=>{
-  expect(number.number(['pobi','woni','jun'])).toBe(['-','-','-']);
+  expect(number.number(['pobi','woni','jun'])).toEqual(['-','-','-']);
 });
 
 test('pobi,woni,jun은 try_number만큼 자동차 경주를 진행한다.', ()=>{
-  expect(number.number(['pobi','woni','jun'])).toBe(undefined);
+  expect(number.number(['pobi','woni','jun'])).toEqual(undefined);
 });
