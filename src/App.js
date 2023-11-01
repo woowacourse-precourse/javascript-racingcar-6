3c1f798524de216a44d93378357e89e59e9c153c
hyeonjun-L <beeminor60@gmail.com>
2023-11-01 16:28:42 +0900

    feat: 입력받은 이동 횟수만큼 경주 진행 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import User from './User.js';
+import Cars from './Cars.js';
+
 class App {
-  async play() {}
+  constructor() {
+    this.initComponent();
+  }
+
+  initComponent() {
+    this.user = new User({
+      initialState: { playNumber: 0, carList: [] },
+    });
+  }
+
+  async play() {
+    const { user } = this;
+
+    await user.promptCarNames();
+    await user.promptPlayNumber();
+
+    this.cars = new Cars({ initialState: user.state.carList });
+
+    for (let i = 0; i < user.state.playNumber; i++) {
+      this.cars.race();
+      console.log(this.cars.state);
+    }
+  }
 }
 
 export default App;
