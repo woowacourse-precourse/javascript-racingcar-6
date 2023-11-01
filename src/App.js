4dcab2ef7e7d075f5e9b2dbb8927e9008105bd19
yongjae95 <64951@hanmail.net>
2023-11-01 20:30:18 +0900

    feat:예외처리 함수 생성 및 게임 실행

--- src/App.js
+++ src/App.js
@@ -17,6 +17,29 @@ class App {
         });
       }
     };
+
+    const validateError = function validateCheck(nameArr, nameList, runCount) {
+      let nullCheck = nameList ?? false;
+      nullCheck = runCount ?? false;
+      for (let i = 0; i < nameArr.length; i++) {
+        if (nameArr[i].length >= 5) {
+          throw new Error("[ERROR] 이름은 5글자 이내로 입력해주세요.");
+        }
+      }
+      if (nullCheck == false) {
+        throw new Error("[ERROR] 형식이 올바르지 않습니다.");
+      }
+    };
+
+    const nameList = await Console.readLineAsync(
+      "자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    const runCount = await Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요?\n"
+    );
+    const nameArr = nameList.split(",");
+    validateError(nameArr, nameList, runCount);
+    playGame(nameArr, runCount);
   }
 }
 
