05e6195fdd8d6adfa25ae353a04362fbdc8c0ed6
cchaeyoung <kimchaeyoung05@naver.com>
2023-11-01 18:09:41 +0900

    feat: 에러 처리

--- src/raceGame.js
+++ src/raceGame.js
@@ -2,17 +2,18 @@ const { Random, Console } = require('@woowacourse/mission-utils');
 const Car = require('./Car');
 
 async function startRace() {
-    const carNames = (await Console.readLineAsync('경주할 자동차 이름을 입력하세요 (이름은 쉼표(,)로 구분): ')).split(',');
-    for (const name of carNames) {
-        if (name.length > 5) {
-            throw new Error("[ERROR] 자동차 이름은 5글자 이하로 입력하세요.");
+    try {
+        const carNames = (await Console.readLineAsync('경주할 자동차 이름을 입력하세요 (이름은 쉼표(,)로 구분): ')).split(',');
+        for (const name of carNames) {
+            if (name.length > 5) {
+                throw new Error("[ERROR] 자동차 이름은 5글자 이하로 입력하세요.");
+            }
         }
-    }
 
-    const numMoves = parseInt(await Console.readLineAsync('시도할 횟수는 몇 회인가요? '));
-        if (numMoves <= 0) {
-            throw new Error("[ERROR] 횟수는 1 이상이어야 합니다.");
-        }
+        const numMoves = parseInt(await Console.readLineAsync('시도할 횟수는 몇 회인가요? '));
+            if (numMoves <= 0) {
+                throw new Error("[ERROR] 횟수는 1 이상이어야 합니다.");
+            }
 
         const cars = carNames.map(name => new Car(name.trim()));
 
@@ -34,6 +35,10 @@ async function startRace() {
         } else {
             Console.print(`\n최종 우승자: ${winners.join(', ')}`);
         }
+
+    } catch (error) {
+        Console.print(error.message);
+    }   
 }
 
 module.exports = startRace;
