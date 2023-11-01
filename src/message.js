1940e74e0968321f46cc0bace18beb3848fca267
TMInstaller <ajvldmlqjqclr@naver.com>
2023-11-01 21:04:19 +0900

    메세지 가독성 증가 작업

--- src/message.js
+++ src/message.js
@@ -1,8 +1,8 @@
 import { ERROR_CONVENTION } from "./convention.js";
 
 export const systemMessage = {
-  START: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
-  TRY_COUNT: "시도할 회수는 몇회인가요?",
+  GAME_START: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
+  INPUT_TRY_COUNT: "시도할 회수는 몇회인가요?",
   RESULT: "실행 결과",
   WINNER: "최종 우승자: ",
 };
