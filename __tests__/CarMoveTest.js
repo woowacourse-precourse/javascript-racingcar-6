f039216e27bfb6afce04412c0daaf8bc9c486cf3
whai2 <whai2125@gmail.com>
2023-11-01 22:09:56 +0900

    style: 상수명 코드 컨벤션 반영

--- __tests__/CarMoveTest.js
+++ __tests__/CarMoveTest.js
@@ -11,12 +11,12 @@ describe("자동차 이동, 정지", () => {
   test("자동차 이동", async () => {
     // given
     const inputs = ["pobi"];
-    const determinatednumber = 4;
+    const DETERMIDATED_NUMBER = 4;
     const outputs = ["pobi : -"];
     const logSpy = getLogSpy();
 
     // when
-    carMoveOrStop(inputs,determinatednumber)
+    carMoveOrStop(inputs,DETERMIDATED_NUMBER)
 
     // then
     outputs.forEach((output) => {
@@ -27,12 +27,12 @@ describe("자동차 이동, 정지", () => {
   test("자동차 정지", async () => {
     // given
     const inputs = ["pobi"];
-    const determinatednumber = 3;
+    const DETERMIDATED_NUMBER = 3;
     const outputs = ["pobi :"];
     const logSpy = getLogSpy();
 
     // when
-    carMoveOrStop(inputs,determinatednumber)
+    carMoveOrStop(inputs,DETERMIDATED_NUMBER)
 
     // then
     outputs.forEach((output) => {
