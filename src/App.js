14188777a712a9bf56aa2a6d0e7681f94398e575
lucia0929 <jsu02062@gmail.com>
2023-11-01 11:33:00 +0900

    feat: (1-1) 이름 입력 메세지 출력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+
+  async play() {
+    await this.getCarName();
+  }
+
+  async getCarName() {
+    const inputName = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    return inputName;
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
