a92269dcbb46bad89a2cc48028b25d833c2a685c
Jaewon <wellbird@naver.com>
2023-11-01 12:13:48 +0900

    style : 클래스, 메소드, 조건문 주석 추가

--- src/App.js
+++ src/App.js
@@ -13,6 +13,12 @@ import {
   roundNumberRangeError,
 } from "./Messages/Error";
 
+/**
+ * App클래스
+ * @this {Array} cars 자동차 객체를 저장할 배열
+ * @this {Array} carNames 자동자 이름 중복을 파악하기 위해 이름을 저장할 배열
+ * @this {roundNumber} roundNumber 라운드 횟수를 입력받아 저장할 변수
+ */
 class App {
   constructor() {
     this.cars = [];
@@ -20,15 +26,24 @@ class App {
     this.roundNumber = 0;
   }
 
+  /**
+   * 자동차 이름 유효성 검사
+   * @param {String} name 사용자에게 입력받은 자동차 이름
+   */
   checkValidName(name) {
+    // 이름의 길이가 없거나 5보다 클 때
     if (name.length === 0 || name.length > 5) {
       carNameLengthError();
     }
+    // 중복된 이름이 존재할 때
     if (this.carNames.includes(name)) {
       carNameDuplicateError();
     }
   }
 
+  /**
+   * 자동차 이름을 사용자에게 입력받는 메소드
+   */
   async getCar() {
     getCarNameMessage();
     const inputCarName = await Console.readLineAsync().then((res) =>
@@ -42,15 +57,24 @@ class App {
     });
   }
 
+  /**
+   * 라운드 수 유효성 검사
+   * @param {String} inputNumber 사용자에게 입력받은 라운드 수
+   */
   checkValidType(inputNumber) {
+    // 입력받은 변수의 타입이 숫자가 아닐 때
     if (Number.isNaN(+inputNumber)) {
       roundNumberTypeError();
     }
+    // 입력받은 숫자가 0보다 작을 때
     if (+inputNumber < 0) {
       roundNumberRangeError();
     }
   }
 
+  /**
+   * 라운드 수를 사용자에게 입력받는 메소드
+   */
   async getRoundNumber() {
     getRoundNumberMessage();
     const inputNumber = await Console.readLineAsync();
@@ -58,12 +82,18 @@ class App {
     this.roundNumber = inputNumber;
   }
 
+  /**
+   * 한 라운드의 진행 메소드
+   */
   processRound() {
     this.cars.forEach((car) => {
       car.move();
     });
   }
 
+  /**
+   * 우승자를 찾는 메소드
+   */
   printWinner() {
     const maxCount = Math.max(...this.cars.map((car) => car.countMove));
     const winnerArray = this.cars
@@ -72,6 +102,9 @@ class App {
     winnerMessage(winnerArray.join(", "));
   }
 
+  /**
+   * 게임 시작 main 메소드
+   */
   async play() {
     await this.getCar();
     await this.getRoundNumber();
--- src/Car.js
+++ src/Car.js
@@ -1,6 +1,13 @@
 import { Random } from "@woowacourse/mission-utils";
 import { afterMoveMessage } from "./Messages/Message";
 
+/**
+ * 자동차 객체 생성 클래스
+ * @param {String} name 생성할 자동차의 이름
+ * @this {String} name 생성된 자동차의 이름
+ * @this {Number} countMove 자동차 이동 횟수
+ * @this {String} showMove 자동차가 이동한 결과를 보여줄 때 사용할 변수
+ */
 class Car {
   constructor(name) {
     this.name = name;
@@ -8,8 +15,12 @@ class Car {
     this.showMove = "";
   }
 
+  /**
+   * 자동차의 이동 여부를 확인하고 이동할때 사용하는 메소드
+   */
   move() {
     const RANDOM_NUMBER = Random.pickNumberInRange(0, 9);
+    // 랜덤 숫자가 4 이상이면 이동
     if (RANDOM_NUMBER >= 4) {
       this.countMove += 1;
       this.showMove += "-";
