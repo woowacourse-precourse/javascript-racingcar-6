607763872cad38e07bba584a4e13ed7f756f9269
gilgor2 <misby0327@gmail.com>
2023-11-01 10:19:18 +0900

    test(GenerateCarDataListFromStrTest): check data form
    문자열을 분해해 원하는 데이터로 만들어낼 수 있는지 확인합니다.
    이름 중복, 특수기호나 숫자를 포함한 이름에도 대응할 수 있는지 확인합니다.

---
+++ __tests__/GenerateCarDataListFromStrTest.js
@@ -0,0 +1,57 @@
+import generateCarDataListFromStr from '../src/feature/GenerateCarDataListFromStr';
+
+describe('GenerateCarDataListFromStr', () => {
+  test('Data Check', () => {
+    const carNameStrList = ['a,b,b', 'a,,b', '032,12,---'];
+    const correctDataList = [
+      [
+        {
+          name: 'a',
+          distance: 0,
+        },
+        {
+          name: 'b',
+          distance: 0,
+        },
+        {
+          name: 'b',
+          distance: 0,
+        },
+      ],
+      [
+        {
+          name: 'a',
+          distance: 0,
+        },
+        {
+          name: '',
+          distance: 0,
+        },
+        {
+          name: 'b',
+          distance: 0,
+        },
+      ],
+      [
+        {
+          name: '032',
+          distance: 0,
+        },
+        {
+          name: '12',
+          distance: 0,
+        },
+        {
+          name: '---',
+          distance: 0,
+        },
+      ],
+    ];
+
+    carNameStrList.forEach((carNameStr, i) => {
+      expect(generateCarDataListFromStr(carNameStr)).toEqual(
+        correctDataList[i]
+      );
+    });
+  });
+});
