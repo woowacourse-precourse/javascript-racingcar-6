83fb56f8bcdf01d6136dcd6bbfca05017f824184
Ag-crane <enaklee401@dongguk.edu>
2023-11-01 22:56:03 +0900

    refactor: 각 기능을 함수로 분리

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
-  async play() {
-    // 게임 준비
-
-    const cars = await MissionUtils.Console.readLineAsync(
+  async prepareGame() {
+    const carsInput = await MissionUtils.Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)기준으로 구분\n"
     );
-    if (!cars.includes(",")) {
+    if (!carsInput.includes(",")) {
       throw new Error("[ERROR]: 2개 이상의 자동차가 필요합니다");
     }
-    const carList = cars.split(",").map((item) => item.trim());
+    const carList = carsInput.split(",").map((item) => item.trim());
     for (const car of carList) {
       if (car.length > 5) {
         throw new Error("[ERROR]: 이름은 5자 이하만 가능합니다");
@@ -24,33 +22,28 @@ class App {
       throw new Error("[ERROR]: 숫자가 잘못된 형식입니다");
     }
 
-    // 결과 담을 딕셔너리 초기화
-    let scoreboard = {};
+    let scoreboard = carList.reduce((acc, car) => {
+      acc[car] = "";
+      return acc;
+    }, {});
+
+    return { carList, round, scoreboard };
+  }
+
+  roundRace(carList, scoreboard) {
     for (let car of carList) {
-      scoreboard[car] = "";
-    }
-    MissionUtils.Console.print("\n실행 결과");
-
-    // round별 게임 진행하는 함수
-    function roundRace(carList, scoreboard) {
-      for (let car of carList) {
-        const randomNum = MissionUtils.Random.pickNumberInRange(0, 9);
-        if (randomNum >= 4) {
-          scoreboard[car] += "-";
-        }
-        MissionUtils.Console.print(`${car} : ${scoreboard[car]}`);
+      const randomNum = MissionUtils.Random.pickNumberInRange(0, 9);
+      if (randomNum >= 4) {
+        scoreboard[car] += "-";
       }
-      MissionUtils.Console.print("");
-
-      return scoreboard;
+      MissionUtils.Console.print(`${car} : ${scoreboard[car]}`);
     }
+    MissionUtils.Console.print("");
 
-    // round 수만큼 게임 진행
-    for (let i = 0; i < round; i++) {
-      scoreboard = roundRace(carList, scoreboard);
-    }
+    return scoreboard;
+  }
 
-    // 우승자 정하기
+  findWinner(scoreboard) {
     let winner = "";
     let max = 0;
     for (const [key, value] of Object.entries(scoreboard)) {
@@ -61,6 +54,21 @@ class App {
         winner += ", " + key;
       }
     }
+    return winner;
+  }
+
+  async play() {
+    const gameData = await this.prepareGame();
+    const { carList, round } = gameData;
+    let { scoreboard } = gameData;
+
+    MissionUtils.Console.print("\n실행 결과");
+
+    for (let i = 0; i < round; i++) {
+      scoreboard = this.roundRace(carList, scoreboard);
+    }
+
+    const winner = this.findWinner(scoreboard);
     MissionUtils.Console.print(`최종 우승자 : ${winner}`);
   }
 }
