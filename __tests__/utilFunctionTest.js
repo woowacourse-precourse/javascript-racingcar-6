ad6b12afb5152019a30fb90aec8485192a401eb2
kimyonghui <kyhui1115@gmail.com>
2023-11-01 15:59:30 +0900

    test: calculateMaxScoreCarList 함수 테스트 코드 구현

--- __tests__/utilFunctionTest.js
+++ __tests__/utilFunctionTest.js
@@ -30,3 +30,38 @@ describe('유틸함수 테스트', () => {
 
     expect(APP.addPointRace(INPUT)).toEqual(OUTPUT);
   });
+
+  test('경기 결과 우승자 리턴', () => {
+    const INPUT_CAR_LIST = ['pobi', 'lulu', 'json'];
+    const INPUT_RACE = [
+      { name: 'pobi : ----', score: 4 },
+      { name: 'lulu : --', score: 2 },
+      { name: 'json : ---', score: 3 },
+    ];
+
+    const OUTPUT = ['pobi'];
+
+    const APP = new App();
+
+    expect(APP.calculateMaxScoreCarList(INPUT_CAR_LIST, INPUT_RACE)).toEqual(
+      OUTPUT
+    );
+  });
+
+  test('경기 결과 우승자 복수 리턴', () => {
+    const INPUT_CAR_LIST = ['pobi', 'lulu', 'json'];
+    const INPUT_RACE = [
+      { name: 'pobi : ----', score: 4 },
+      { name: 'lulu : ----', score: 4 },
+      { name: 'json : --', score: 2 },
+    ];
+
+    const OUTPUT = ['pobi', 'lulu'];
+
+    const APP = new App();
+
+    expect(APP.calculateMaxScoreCarList(INPUT_CAR_LIST, INPUT_RACE)).toEqual(
+      OUTPUT
+    );
+  });
+});
