6061d8977573f5f8cff159b510276d36d82030ad
novice0840 <magnolia0840@gmail.com>
2023-11-01 12:51:57 +0900

    feat: 실행 결과 계산 및 출력

--- src/App.js
+++ src/App.js
@@ -8,6 +8,24 @@ class App {
     this.tryNumber = 0;
   }
 
+  updateCars() {
+    this.carsStatus = this.carsStatus.map((carStatus) => ({
+      ...carStatus,
+      move: Random.pickNumberInRange(0, 9) >= 4 ? carStatus.move + 1 : carStatus.move,
+    }));
+  }
+
+  printCars() {
+    this.carsStatus.forEach((carStatus) => {
+      Console.print(`${carStatus.name} : ${'-'.repeat(carStatus.move)}`);
+    });
+    Console.print('');
+  }
+
+  calculateMaxMove() {
+    return Math.max(...this.carsStatus.map((carStatus) => carStatus.move));
+  }
+
   async inputCarsStatus() {
     const carsStatus = await Console.readLineAsync(GAME_MESSAGE.INPUT_CAR);
     this.carsStatus = Validate.carsValidate(carsStatus)
@@ -23,6 +41,11 @@ class App {
   async play() {
     await this.inputCarsStatus();
     await this.inputTryNumber();
+    Console.print('\n실행 결과');
+    for (let i = 0; i < this.tryNumber; i += 1) {
+      this.updateCars();
+      this.printCars();
+    }
   }
 }
 
