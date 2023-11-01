383632be620a9503efe9d26c8f0be793eee162f1
FirstBright <mipo8890@gmail.com>
2023-11-01 10:00:09 +0900

    :recycle: style: 함수사이 공백, if return사이 공백추가, 실행 결과 출력 전 공백 추가

--- src/App.js
+++ src/App.js
@@ -17,9 +17,11 @@ class App {
     if (isNaN(intRound)) {
       throw new Error("[ERROR] 숫자를 입력해주세요.");
     }
+
     if (intRound < 0) {
       throw new Error("[ERROR] 양수를 입력해주세요.");
     }
+
     return intRound;
   }
 
@@ -30,24 +32,20 @@ class App {
       if (player.length > 5) {
         throw new Error("[ERROR] 5자리 이하의 이름만 가능합니다.");
       }
+
       const car = new Car(player);
       cars.push(car);
     }
+    
     return cars;
   }
 
   async executePlay() {
-    let players = await MissionUtils.Console.readLineAsync(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
-    );
+    let players = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
     const cars = this.checkPlayersInput(players);
-    let round =
-      await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
-
+    let round = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
     round = this.checkRoundInput(round);
-
-    await MissionUtils.Console.print("실행 결과");
-
+    await MissionUtils.Console.print("\n실행 결과");
     for (let i = 0; i < round; i++) {
       for (let car of cars) {
         car.run();
@@ -55,10 +53,9 @@ class App {
       }
       await MissionUtils.Console.print("");
     }
+
     const members = this.findCarsWithMaxForward(cars);
-    await MissionUtils.Console.print(
-      "최종 우승자 : " + members.map((member) => member.name).join(", "),
-    );
+    await MissionUtils.Console.print("최종 우승자 : " + members.map((member) => member.name).join(", "));
   }
 
   async play() {
