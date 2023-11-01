d06adf7f5a405f52ec337605d6feeff10050f6b3
urjimyu <92876819+urjimyu@users.noreply.github.com>
2023-11-01 17:22:50 +0900

    feature: 랜덤으로 숫자 추출해서 전진 여부 결정

--- src/getRandomNumber.js
+++ src/getRandomNumber.js
@@ -1,6 +1,6 @@
 import { Random } from '@woowacourse/mission-utils';
 
-export default async function getRandomNumber() {
+export default async function getRandomNumber(score) {
   const randomNumber = await Random.pickNumberInRange(0, 9);
-  return !(randomNumber < 4);
+  return randomNumber < 4 ? score : score + 1;
 }
