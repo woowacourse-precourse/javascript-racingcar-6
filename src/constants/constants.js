b64bd255b32c6f9f02b87d4b04ece4b72c138715
mieum12 <qpdlqltb1215@gmail.com>
2023-11-01 16:21:11 +0900

    fix: typo 수정

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 export const MESSAGE = {
   START_CAR_NAME:
-    "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)\n",
-  START_NUMBER_OF_ATTEMPT: "시도할 회수는 몇회인가요?\n",
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
+  START_NUMBER_OF_ATTEMPT: "시도할 횟수는 몇 회인가요?\n",
   GAME_PROGRESS: "\n실행 결과\n",
   GAME_RESULT: "\n최종 우승자 : ",
 };
