1b08eff0f843409a3b6b123914bb4218ec92630e
Collection50 <fkaus65@naver.com>
2023-11-01 20:51:17 +0900

    test(ValidatorTest): 자동차 이름이 중복된 경우의 테스트 케이스 구현

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -1,13 +1,13 @@
 import Validator from '../src/RacingCarGame/Validator/index.js';
 
 describe('Validator 클래스 테스트', () => {
-  test('자동차 이름이 5글자 이하로 입력되었을 때 validateCarNames, isValidCarNameLength 함수를 테스트한다.', () => {
+  test('자동차 이름이 5글자 이하로 중복되지 않게 입력되었을 때 validateCarNames, isValidCarNameLength 함수를 테스트한다.', () => {
     const names = 'alpha,beta,omega';
 
     expect(() => Validator.validateCarNames(names)).not.toThrow();
   });
 
-  test('자동차 이름이 5글자 초과로 입력되었을 때 validateCarNames, isValidCarNameLength 함수를 테스트한다.', () => {
+  test('자동차 이름이 5글자 초과로 입력되거나 빈 문자열만 입력되었을 때 validateCarNames, isValidCarNameLength 함수를 테스트한다.', () => {
     const nameArr = ['alphaasdf,betaff,omegagg', ', , , '];
 
     nameArr.forEach((names) => {
@@ -19,6 +19,12 @@ describe('Validator 클래스 테스트', () => {
     const names = ', , abc';
 
     expect(() => Validator.validateCarNames(names)).not.toThrow();
+  });
+
+  test('중복된 자동차 이름이 입력되었을 때 validateCarNames, validateCarNameDuplication 함수를 테스트한다.', () => {
+    const names = 'abc,abc';
+
+    expect(() => Validator.validateCarNames(names)).toThrow();
   });
 
   test('시도횟수에 음수가 입력되었을 때 validateLapCount, isSafeInteger 함수를 테스트한다.', () => {
