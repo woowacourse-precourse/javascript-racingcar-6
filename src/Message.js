d54cb2bfbf8677cd6f052fd58aa60380c9045835
HarrySeop <parkpjs3083@naver.com>
2023-11-01 21:32:58 +0900

    feat: 에러 메세지 객체 추가

---
+++ src/Message.js
@@ -0,0 +1,15 @@
+const GAME_MESSAGES = Object.freeze({
+  START: "경주할 자동차 이름을 입력하세요.(이름은 윔표(,) 기준으로 구분) \n",
+  ATTEMPT: "시도할 횟수는 몇 회인가요? \n",
+  RESULT: "실행 결과",
+  WINNER: "최종 우승자 : "
+});
+
+const ERROR_MESSAGES = Object.freeze({
+  OVER_FIVE: "[ERROR] 자동차 이름은 5자 이하만 가능합니다.",
+  SOLO_NAME: "[ERROR] 두개 이상의 자동차가 필요합니다.",
+  EMPTY_NAME: "[ERROR] 올바른 이름을 입력해야합니다.",
+  NOT_NUMBER: "[ERROR] 1 이상의 숫자만 입력이 가능합니다."
+});
+
+export { GAME_MESSAGES, ERROR_MESSAGES };
