9f489fc13db6df5d41701134c3da5bb8930d624c
usablechan <lch8910@naver.com>
2023-11-01 19:45:37 +0900

    feat : getCarName, 자동차 이름 입력

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
   async play() {}
   async getCarName() {
-    MissionUtils.Console.print(
-      "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)"
-    );
     const carName = await MissionUtils.Console.readLineSync(
-      "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)"
+      "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)\n"
     );
     return carName.split(",");
   }
