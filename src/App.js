b3d95d56e7ee210e4b449c5e897048701e5adc60
intersoom <sallybig@naver.com>
2023-11-01 20:33:44 +0900

    feat: 경주할 자동차 이름 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import Car from "./Car.js";
+import Input from "./Input.js";
+import Validation from "./Validation.js";
+
 class App {
-  async play() {}
+  cars;
+
+  async play() {
+    const carNamesInput = await Input.carNames();
+    Validation.carNames(carNamesInput);
+
+    this.cars = carNamesInput.split(",").map((carName) => new Car(carName));
+  }
 }
 
 export default App;
