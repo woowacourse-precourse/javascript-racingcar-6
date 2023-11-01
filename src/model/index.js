6f13bb203032df5bc4d2164d5ae568fc0429aedd
ryan-dia <cjfdnjs158@naver.com>
2023-11-01 11:22:07 +0900

    feat(model): vehicle private 필드 추가

--- src/model/index.js
+++ src/model/index.js
@@ -2,6 +2,12 @@ import { SYSTEM } from '../constants/System.js';
 import RandomNumberGenerator from './RandomNumberGenerator.js';
 
 class RacingModel {
+  #vehicle;
+
+  constructor(vehicle) {
+    this.#vehicle = vehicle;
+  }
+
   // 오직 내부에서만 사용하고 private 메서드이기 때문에 static을 사용하지 않음
   // eslint-disable-next-line class-methods-use-this
   #canMove() {
