c0285d896e8a81672c9b43f4e782b0ec2a58d884
intersoom <sallybig@naver.com>
2023-11-01 16:22:47 +0900

    refactor: 변수명 수정 CAR_NAME_INPUT -> CAR_NAMES_INPUT

--- src/const/messageData.js
+++ src/const/messageData.js
@@ -1,5 +1,5 @@
 export const INFORMATION_MESSEAGE = Object.freeze({
-  CAR_NAME_INPUT:
+  CAR_NAMES_INPUT:
     "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
   COUNT_INPUT: "시도할 횟수는 몇 회인가요?",
   RESULT_OUTPUT: "실행 결과",
