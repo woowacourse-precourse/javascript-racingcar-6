703bddc42970377cd99cf6fe2c9b75cec6af073b
intersoom <sallybig@naver.com>
2023-11-01 15:56:43 +0900

    feat: 에러 문구 상수 추가

---
+++ src/const/errorData.js
@@ -0,0 +1,8 @@
+export const ERROR_MESSEAGE = Object.freeze({
+  CAR_NAME_LENGTH: "[ERROR] 이름은 최소 1자에서 최대 5자까지 가능합니다.",
+  CAR_NAME_DUPLICATION:
+    "[ERROR] 이름이 중복되었습니다. 서로 다른 이름을 입력해주세요.",
+  CAR_COUNT_MIN: "[ERROR] 자동차는 최소 2대가 있어야지 경주가 가능합니다.",
+  BLANK_FORM: "[ERROR] 공백 없이 입력해주세요.",
+  NUMBER_FORM: "[ERROR] 1 이상의 숫자의 형태로 입력해주세요.",
+});
