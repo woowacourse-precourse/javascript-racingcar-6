4af29d202712a42c263061666c9f5cefa4b2a122
lucia0929 <jsu02062@gmail.com>
2023-11-01 15:26:45 +0900

    feat: (T-1-2) 이름 입력 처리 테스트 구현

--- __tests__/BeforeRefactor.test.js
+++ __tests__/BeforeRefactor.test.js
@@ -18,4 +18,41 @@ describe("App class", () => {
       expect(result).toEqual(["pobi", "woni", "jun"]);  // 수정된 부분
     });
   });
+
+  describe("sanitizeCarName method", () => {
+    let app;
+    beforeEach(() => {
+      app = new App();
+    });
+
+    test("이름 사이에 공백이 없을 때", () => {
+      const input = "pobi,woni,jun";
+      const result = app.sanitizeCarName(input);
+      expect(result).toEqual(["pobi", "woni", "jun"]);
+    });
+
+    test("이름 앞뒤에 공백이 있을 때", () => {
+      const input = " pobi ,woni , jun ";
+      const result = app.sanitizeCarName(input);
+      expect(result).toEqual(["pobi", "woni", "jun"]);
+    });
+
+    test("공백만 있는 이름이 있을 때", () => {
+      const input = "pobi, , jun";
+      const result = app.sanitizeCarName(input);
+      expect(result).toEqual(["pobi", "", "jun"]);
+    });
+
+    test("콤마가 없을 때", () => {
+      const input = "pobi woni jun";
+      const result = app.sanitizeCarName(input);
+      expect(result).toEqual(["pobi woni jun"]);
+    });
+
+    test("연속적으로 여러 개의 콤마가 나올 때", () => {
+      const input = "pobi,,,woni,,jun";
+      const result = app.sanitizeCarName(input);
+      expect(result).toEqual(["pobi", "", "", "woni", "", "jun"]);
+    });
+  });
 });
