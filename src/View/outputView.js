6518dcddc818a979d57b844ccbe76eb212ade495
Nevacat <nevacattery217@naver.com>
2023-11-01 11:15:12 +0900

    Comment: 주석 추가 및 불필요 공백 제거

--- src/View/outputView.js
+++ src/View/outputView.js
@@ -2,14 +2,15 @@ import { Console } from '@woowacourse/mission-utils';
 import { GAME } from '../Constant/constants';
 
 const OutputView = {
+  // 결과 출력
   async outputResult() {
     console.log(GAME.RESULT);
   },
-
+  // 게임 진행 출력
   async outputBoard(name, count) {
     Console.print(`${name} : ${'-'.repeat(count)}`);
   },
-
+  // 우승자 출력
   async outputWinner(winner) {
     Console.print(GAME.WINNER + String(winner.join(', ')));
   },
