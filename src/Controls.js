d297b54a4853907e3166f6752cd00334dcd5e355
yooshnn <yooshnn@kakao.com>
2023-11-01 13:17:58 +0900

    feat(Controls): implement display functions

--- src/Controls.js
+++ src/Controls.js
@@ -1,10 +1,11 @@
-import { read, write } from './IO';
+import { read, write } from './IO.js';
 
 const TEXT = {
   ENTER_CAR: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
   ENTER_MOVES: '시도할 횟수는 몇 회인가요?\n',
   ERROR_CAR: '[ERROR] 자동차 이름은 공백이 아닌 5자 이하여야 합니다.',
   ERROR_MOVES: '[ERROR] 시도 횟수는 양의 정수여야 합니다.',
+  CHAMPIONS: '최종 우승자',
 };
 
 class Controls {
@@ -19,7 +20,7 @@ class Controls {
   }
 
   static async getMoves() {
-    const userInput = await read(TEXT.ENTER_TRY);
+    const userInput = await read(TEXT.ENTER_MOVES);
     if (!this.validateMoves(userInput)) {
       throw new Error(TEXT.ERROR_MOVES);
     }
@@ -37,4 +38,22 @@ class Controls {
     if (!/^\d+$/.test(moves)) return false;
     return Number(moves) > 0;
   }
+
+  // display functions
+  static printCars(cars) {
+    write('');
+    for (const [name, position] of Object.entries(cars)) {
+      const bar = new Array(position + 1).join('-');
+      const car = `${name} : ${bar}`;
+      write(car);
+    }
+  }
+
+  static printChampions(champions) {
+    const championNames = champions.join(', ');
+    write('');
+    write(`${TEXT.CHAMPIONS} : ${championNames}`);
+  }
 }
+
+export default Controls;
