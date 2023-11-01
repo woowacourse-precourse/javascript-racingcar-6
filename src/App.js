94db9b620887cd56871ed56eda730047e201f210
FordangIT <fordang0819@gmail.com>
2023-11-01 22:16:04 +0900

    set: 세팅중

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+import { MESSAGE_TABLE } from "./constants";
 class App {
   async play() {}
+  async carNameInput() {
+    const userInput = await Console.readLineAsync(MESSAGE_TABLE.START_GAME);
+    return userInput.split(",");
+  }
+  async driveNumberInput() {
+    const userInput = await Console.readLineAsync(MESSAGE_TABLE.TRY_NUMBER);
+    return userInput;
+  }
+  pickRandomNumber() {
+    const number = Random.pickRandomNumber(0, 9);
+    return number;
+  }
+  moveForward(num) {
+    if (num >= 4) {
+      return true;
+    }
+    return false;
+  }
+  winnerOutput(obj) {
+    let max = 0;
+    for (let key in obj) {
+      let temp = obj[key].length;
+      max = Math.max(max, temp);
+    }
+  }
 }
 
 export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+export const MESSAGE_TABLE = {
+  START_GAME: "경주할 자동차 이름을 입력하세요.(입력은 쉼표(,) 기준으로 구분)",
+  TRY_NUMBER: "시도할 횟수는 몇 회인가요?",
+  WINNER_CAR: "최종우승자",
+};
