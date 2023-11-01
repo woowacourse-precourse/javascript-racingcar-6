8417c37cc410308d3d89e880b9d76f2865a644c3
Ag-crane <enaklee401@dongguk.edu>
2023-11-01 20:38:54 +0900

    style: 코드 컨벤션 적용

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ class App {
       throw new Error("[ERROR]: 2개 이상의 자동차가 필요합니다");
     }
     const carList = cars.split(",").map((item) => item.trim());
-    for (const car of carList){
-      if (car.length>5){
+    for (const car of carList) {
+      if (car.length > 5) {
         throw new Error("[ERROR]: 이름은 5자 이하만 가능합니다");
       }
     }
@@ -35,24 +35,23 @@ class App {
           result[car] += "-";
         }
         // 라운드별 결과 출력
-        MissionUtils.Console.print(`${car} : ${result[car]}`)
+        MissionUtils.Console.print(`${car} : ${result[car]}`);
       }
-      MissionUtils.Console.print("")
+      MissionUtils.Console.print("");
     }
 
     // 우승자 정하기
-    let winner = ''
+    let winner = "";
     let max = 0;
-    for (const [key,value] of Object.entries(result)){
-      if (value.length > max){
-        max = value.length
-        winner = key
-      }else if (value.length === max){
-        winner += ', '+ key
+    for (const [key, value] of Object.entries(result)) {
+      if (value.length > max) {
+        max = value.length;
+        winner = key;
+      } else if (value.length === max) {
+        winner += ", " + key;
       }
     }
-    MissionUtils.Console.print(`최종 우승자 : ${winner}`)
-
+    MissionUtils.Console.print(`최종 우승자 : ${winner}`);
   }
 }
 
