21c7729cdd04b0cebfefb32916781c88acea3e55
choekko <choekko@gmail.com>
2023-11-01 22:34:58 +0900

    feat: 양의 정수 값을 받아서 해당 값에 해당하는 횟수만큼 전진 여부 판단 후, 그 결과를 배열로 반환하는 함수 구현

--- __tests__/GameUtilTest.js
+++ __tests__/GameUtilTest.js
@@ -1,5 +1,5 @@
 import { Random } from '@woowacourse/mission-utils';
-import {hasMovedForward} from '../src/game.js';
+import {getMovingResult, hasMovedForward} from '../src/game.js';
 
 describe('게임 유틸', () => {
   describe('hasMovedForward', () => {
@@ -11,6 +11,23 @@ describe('게임 유틸', () => {
     test('Random.pickNumberInRange 유틸이 4이상의 값을 반환한다면, true를 반환해야 한다', () => {
       jest.spyOn(Random, 'pickNumberInRange').mockReturnValue(5);
       expect(hasMovedForward()).toBe(true);
+    })
+  })
+
+  describe('getMovingResult', () => {
+    test('자연수를 입력하지 않으면, 에러를 반환해야 한다.', () => {
+      expect(() => getMovingResult(-1)).toThrowError();
+    })
+    test('반환값은 모두 boolean 이어야 한다.', () => {
+      const movingResult = getMovingResult(3);
+      movingResult.forEach((result) => {
+        expect(result).toEqual(expect.any(Boolean));
+      })
+    })
+    test('입력한 자연수만큼의 길이를 갖는 배열을 반환해야한다.', () => {
+      const count = 5;
+      const movingResult = getMovingResult(count);
+      expect(movingResult.length).toBe(count);
     })
   })
 })
--- src/game.js
+++ src/game.js
@@ -1,4 +1,5 @@
 import { Random } from '@woowacourse/mission-utils';
+import {isNaturalNumber} from './validation.js';
 
 /**
  * 전진 여부 판단 함수
@@ -8,4 +9,17 @@ export const hasMovedForward = () => {
   const randomNumber = Random.pickNumberInRange(0, 9)
   if (randomNumber >= 4) return true;
   return false;
+}
+
+
+/**
+ * 횟수만큼 전진 여부 판단 후, 그 결과를 배열로 반환하는 함수
+ * @param count
+ * @returns {boolean[]}
+ */
+export const getMovingResult = (count) => {
+  if (!isNaturalNumber(count)) {
+    throw new Error('[ERROR] 인자로 자연수를 입력해주세요. (getMovingResult) ')
+  }
+  return Array.from({ length: count }).map(() => hasMovedForward());
 }
