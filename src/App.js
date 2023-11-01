c63baa1dced2d3ad0bbe7c52bf20092a2ee7c467
usablechan <lch8910@naver.com>
2023-11-01 19:44:27 +0900

    docs : 기능정의 문서 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
   async play() {}
+  async getCarName() {
+    MissionUtils.Console.print(
+      "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)"
+    );
+    const carName = await MissionUtils.Console.readLineSync(
+      "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)"
+    );
+    return carName.split(",");
+  }
 }
 
 export default App;
