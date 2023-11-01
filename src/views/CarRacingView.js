a907f926973ea703d574752862ab59547518cef2
Jinhyung Lee <luciancah@gmail.com>
2023-11-01 16:59:48 +0900

    feat: View에 경기 결과 메소드 추가
    - 각 라운드 후 각 자동차의 위치를 표시하는 showRoundResult 생성
    - 경주의 최종 우승자를 표시하는 showWinners 생성

--- src/views/CarRacingView.js
+++ src/views/CarRacingView.js
@@ -1,4 +1,6 @@
 import { Console } from '@woowacourse/mission-utils';
+import { printMessage } from '../utils/messages';
+
 class CarRacingView {
   static async promptCarNames() {
     return Console.readLineAsync(
@@ -8,6 +10,17 @@ class CarRacingView {
 
   static async promptRaceCount() {
     return Console.readLineAsync('시도할 횟수는 몇 회인가요? \n');
+  }
+
+  static showRoundResult(cars) {
+    const result = cars
+      .map(car => `${car.name} : ${'-'.repeat(car.position)}`)
+      .join('\n');
+    printMessage(`${result}\n`);
+  }
+
+  static showWinners(winners) {
+    printMessage(`최종 우승자: ${winners.join(', ')}\n`);
   }
 }
 
