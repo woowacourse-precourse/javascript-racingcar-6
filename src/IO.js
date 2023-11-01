7e2b78f3e823804001d019cc4f8dd24f3d83ed35
bl0ckp1n9 <blockp1n9@gmail.com>
2023-11-01 22:50:15 +0900

    chore: MESSAGE 오탈자 수정

--- src/IO.js
+++ src/IO.js
@@ -4,7 +4,7 @@ const MESSAGE = {
   INPUT: {
     TRY_COUNT: '시도할 횟수는 몇 회인가요?\n',
     CAR_NAMES:
-      '경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)\n',
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
   },
   OUTPUT: {
     EXECUTION_RESULT: '\n실행 결과',
