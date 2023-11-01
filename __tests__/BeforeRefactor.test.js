8af9f1292d6887af055546ef847c435c1a2c7014
lucia0929 <jsu02062@gmail.com>
2023-11-01 15:11:22 +0900

    refactor: (T-1-1) 테스트 코드 기대값 배열로 수정

--- __tests__/BeforeRefactor.test.js
+++ __tests__/BeforeRefactor.test.js
@@ -5,15 +5,17 @@ const mockInput = (input) => {
   Console.readLineAsync = jest.fn(() => Promise.resolve(input));
 };
 
-describe("App class Test", () => {
-  test("getCarName 사용자 입력", async () => {
-    const testInput = "pobi,woni,jun";
-    mockInput(testInput);
+describe("App class", () => {
+  describe("getCarName method", () => {
+    test("사용자 입력", async () => {
+      const testInput = "pobi,woni,jun";
+      mockInput(testInput);
 
-    const app = new App();
-    const result = await app.getCarName();
+      const app = new App();
+      const result = await app.getCarName();
 
-    expect(Console.readLineAsync).toHaveBeenCalledTimes(1);
-    expect(result).toBe(testInput);
+      expect(Console.readLineAsync).toHaveBeenCalledTimes(1);
+      expect(result).toEqual(["pobi", "woni", "jun"]);  // 수정된 부분
+    });
   });
 });
