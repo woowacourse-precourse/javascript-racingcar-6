54cf70589361c333c44454b57c3c79b6c9076888
Yuli Kim <119276010+yyuli@users.noreply.github.com>
2023-11-01 19:51:56 +0900

    feat: 무작위 값이 4 이상이면 한 칸 전진하는 함수 생성

--- src/model/CarModel.js
+++ src/model/CarModel.js
@@ -1,8 +1,19 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class CarModel {
   #carList;
 
   constructor(cars) {
     this.#carList = cars.map((car) => ({ name: car, move: '' }));
+  }
+
+  updateRaceResults() {
+    this.#carList = this.#carList.map((car) => {
+      const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
+      const updatedMove = car.move + (randomNumber >= 4 ? '-' : '');
+      return { ...car, move: updatedMove };
+    });
+    return this.#carList;
   }
 }
 
