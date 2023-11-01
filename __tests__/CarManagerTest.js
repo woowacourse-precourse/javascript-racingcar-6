a64c84cce034b455ccc9bb70d242c4d186238d7d
apfp77 <apfp77@naver.com>
2023-11-01 19:34:52 +0900

    test: CarManagerTest 추가
    - 매개변수 타입 체크
    - 이름으로 구성된 배열과 시도할 횟수를 숫자 타입으로 전달하여 출력 결과물 비교

---
+++ __tests__/CarManagerTest.js
@@ -0,0 +1,61 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import CarManager from "../src/CarManager";
+import ERROR_MESSAGES from "../src/constant/errorMessages";
+
+const getLogSpy = () => {
+	const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+	logSpy.mockClear();
+	return logSpy;
+}
+
+const mockRandom = (numbers) => {
+	MissionUtils.Random.pickNumberInRange = jest.fn();
+	numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+}
+
+describe('CarManager 클래스 테스트', () => {
+	test('첫번째 매개변수가 배열인지 확인한다', () => {
+		const names = 'test';
+		const carCount = names.length;
+
+		expect(() => new CarManager(names, carCount)).toThrow(ERROR_MESSAGES.arrayType);
+	});
+	test('두번째 매개변수가 숫자인지 확인한다', () => {
+		const names = ['pobbi', 'woni'];
+		const carCount = 'test';
+
+		expect(() => new CarManager(names, carCount)).toThrow(ERROR_MESSAGES.numberType);
+	});
+
+	test('최종 우승자의 출력값을 확인한다', () => {
+		const names = ['pobi','woni'];
+		const ATTEMPT_COUNT = 3;
+		const MOVING = 4;
+		const STOP = 0;
+		const random = [MOVING, STOP, MOVING, STOP, MOVING, STOP]
+		const outputs = [
+			'실행 결과',
+			'pobi : -', 
+			'woni : ',
+			'', 
+			'pobi : --', 
+			'woni : ', 
+			'', 
+			'pobi : ---' , 
+			'woni : ', 
+			'',
+			'최종 우승자 : pobi',
+		];
+		// const outputs = ['실행 결과', 'pobi : -', 'woni : '];
+		const logSpy = getLogSpy();
+		const cars = new CarManager(names, ATTEMPT_COUNT);
+		
+		// mockQuestions(inputs);
+		mockRandom([...random]);
+		cars.play();
+		const consoleOutput = logSpy.mock.calls.map((node) => node[0]);
+		expect(consoleOutput).toEqual(outputs);
+	});
+});
