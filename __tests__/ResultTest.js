acfe6debb235b25193813ec3e3abce89a1aba349
00kang <kdb070970@gmail.com>
2023-11-01 20:25:03 +0900

    test: Result 클래스에 대한 테스트 코드 업데이트

--- __tests__/ResultTest.js
+++ __tests__/ResultTest.js
@@ -6,18 +6,23 @@ describe('Result 클래스 테스트', () => {
 
   beforeEach(() => {
     result = new Result();
-  });
-
-  test('determineWinner 메서드는 carDictionary를 받아서 최종 우승자를 반환', () => {
-    const carDictionary = { car1: 3, car2: 5, car3: 2 };
-    const winners = result.determineWinner(carDictionary);
-    expect(winners).toEqual(['car2']);
-  });
-
-  test('printWinner 메서드는 우승자를 출력', () => {
-    const winners = ['car1', 'car2'];
     jest.spyOn(Console, 'print').mockImplementation(() => {});
-    result.printWinner(winners);
-    expect(Console.print).toHaveBeenCalledWith('최종 우승자 : car1, car2');
+  });
+
+  describe('determineWinner 메서드', () => {
+    test('carDictionary를 받아서 최종 우승자를 반환', () => {
+      const carDictionary = { car1: 3, car2: 5, car3: 2 };
+      const winners = result.determineWinner(carDictionary);
+      expect(winners).toEqual(['car2']);
+    });
+  });
+
+  describe('printWinner 메서드', () => {
+    test('우승자를 출력', () => {
+      const winners = ['car1', 'car2'];
+      result.printWinner(winners);
+      jest.spyOn(Console, 'print').mockImplementation(() => {});
+      expect(Console.print).toHaveBeenCalledWith('최종 우승자 : car1, car2');
+    });
   });
 });
