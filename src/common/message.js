3dc31a4c14051d2f9811d344c481f906ef027302
Seoyeon Kim <kseoy0607@gmail.com>
2023-11-01 14:47:56 +0900

    :sparkles: feat(message): 출력 메세지 작성

---
+++ src/common/message.js
@@ -0,0 +1,12 @@
+const MESSAGE = Object.freeze({
+  inputCarName:
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+  inputNumber: '시도할 횟수는 몇 회인가요?\n',
+  result: '\n실행 결과',
+  forword: '-',
+  winner: '최종 우승자 : ',
+  errorNumber: '[ERROR] 숫자가 잘못된 형식입니다.',
+  errorName: '[ERROR] 이름은 5자 이하만 가능합니다.',
+});
+
+export default MESSAGE;
