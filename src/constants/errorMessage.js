e4a380b4004a90d13208bbef141321c7fafe22ff
kimmainsain <kmsid003@gmail.com>
2023-11-01 23:46:53 +0900

    :rewind: Rewind : 알파벳만 허용 예외처리로 복구

--- src/constants/errorMessage.js
+++ src/constants/errorMessage.js
@@ -7,7 +7,6 @@ const ERROR_CAR_MESSAGE = {
     "[ERROR] 자동차 이름을 입력하지 않았습니다.",
   INVALID_INPUT_CAR_NAME_ENG_ERROR:
     "[ERROR] 자동차 이름에 영어가 아닌 문자가 포함되어 있습니다.",
-  INVALID_INPUT_CAR_NAME_ERROR: "[ERROR] 자동차의 타입이 올바르지 않습니다.",
 };
 
 const ERROR_USER_INPUT_MESSAGE = {
@@ -17,8 +16,6 @@ const ERROR_USER_INPUT_MESSAGE = {
     "[ERROR] 시도할 횟수는 자연수만 가능합니다.",
   INVALID_INPUT_USER_INPUT_ZERO_ERROR:
     "[ERROR] 시도할 횟수는 1이상이어야 합니다.",
-  INVALID_INPUT_USER_INPUT_EMPTY_ERROR:
-    "[ERROR] 시도할 횟수를 입력하지 않았습니다.",
 };
 
 export { ERROR_CAR_MESSAGE, ERROR_USER_INPUT_MESSAGE };
--- src/validations/carValidation.js
+++ src/validations/carValidation.js
@@ -9,6 +9,10 @@ const carValidation = (carName) => {
     throw new Error(ERROR_CAR_MESSAGE.INVALID_INPUT_CAR_NAME_EMPTY_ERROR);
   }
 
+  if (carName.some((name) => !/^[a-zA-Z]+$/.test(name))) {
+    throw new Error(ERROR_CAR_MESSAGE.INVALID_INPUT_CAR_NAME_ENG_ERROR);
+  }
+
   const uniqueNames = new Set(carName);
   if (uniqueNames.size !== carName.length) {
     throw new Error(ERROR_CAR_MESSAGE.INVALID_INPUT_CAR_NAME_DUPLICATION_ERROR);
