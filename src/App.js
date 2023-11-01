fa9c7a8b42a72d0cce641e86ceded1389d6835d8
lucia0929 <jsu02062@gmail.com>
2023-11-01 16:15:29 +0900

    feat: (1-2) 빈 문자열 제거 기능 추가

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ class App {
   }
 
   async getCarName() {
-    const inputName = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    const inputName = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
     return this.sanitizeCarName(inputName);
   }
 
   sanitizeCarName(inputName) {
-    const sanitizedName = inputName.split(',').map(name => name.trim());
+    const sanitizedName = inputName.split(',').map(name => name.trim()).filter(name => name);
     return sanitizedName;
   }
 }
