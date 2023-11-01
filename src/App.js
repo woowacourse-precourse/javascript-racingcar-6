f98062aba12c62c011c00fc4bfa2433227b19f56
soominnnn <105346651+soominnnn@users.noreply.github.com>
2023-11-01 19:32:45 +0900

    🔨Reactor: 변수와 클래스 사이 공백 삭제

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import GetInput from "./GetInput";
 import Output from "./Output";
 
-const getInput = new GetInput;
-
+const getInput = new GetInput();
 class App {
   async play() {
     const CAR = await getInput.getCarNameInput();
     const TRY = await getInput.getNumberInput();
-    const MOVE_COUNT_ARRAY = Array.from({length: CAR.length}, () => 0);
-    const output = new Output(CAR,MOVE_COUNT_ARRAY,TRY);
+    const MOVE_COUNT_ARRAY = Array.from({ length: CAR.length }, () => 0);
+    const output = new Output(CAR, MOVE_COUNT_ARRAY, TRY);
     output.runWhileFinish();
     output.whoIsWinner();
   }
