6ccc491dcb153685608248072f59aa1ba1c257e2
Jihyun_Kim <jihyun3313@daum.net>
2023-11-01 18:58:17 +0900

    fix: 오타 수정

--- src/validValueCheck/index.js
+++ src/validValueCheck/index.js
@@ -3,7 +3,7 @@ import { MESSAGE } from "../constants/message.js";
 export const carNameCheck = async (carsArray) => {
   carsArray.forEach((e, index) => {
     if (e.length > 5 || carsArray.lastIndexOf(e)!==index) {
-      throw new Error(MESSAGE.Error)
+      throw new Error(MESSAGE.error)
     }
   })
 }
