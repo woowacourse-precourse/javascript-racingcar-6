7ca497672a76baa62d371384e15aa697bf6591f0
오지원 <64193469+jiwon-o@users.noreply.github.com>
2023-11-01 17:23:21 +0900

    feat: 자동차들의 이름을 입력하는 메서드 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { MissionUtils, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    await this.startGame();
+  }
+
+  async startGame() {
+    const carNames = await this.inputCarNames();
+  }
+
+  async inputCarNames() {
+    const carsInput = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉽표(,) 기준으로 구분) \n"
+    );
+    const carNames = carsInput.split(",").map((name) => name.trim());
+    return carNames;
+  }
 }
 
 export default App;
+
+const app = new App();
+app.play();
