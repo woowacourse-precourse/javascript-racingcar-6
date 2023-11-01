df1599b9dbb23a4e5dbb87d8dc97acb1c339d24f
mingyeong:D <63152086+Kim-Mingyeong@users.noreply.github.com>
2023-11-01 23:49:23 +0900

    test: validator  테스트 수정

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -3,27 +3,24 @@ import { ERROR } from "../src/message.js";
 
 const mockFn = jest.fn();
 
-validateCarName = (arr) => {
-  arr.forEach((carName) => {
-    if (carName.length > 5) {
-      mockFn(carName);
-    }
-  });
-};
-
-validateCarName(["abcdef"]);
-validateCarName(["abc", "defghi"]);
+mockFn(["woowacourse"]);
+mockFn(["FuBao"]);
 
 describe("조건 검증 테스트", () => {
   const app = new App();
 
-  test("자동차 이름이 5자를 초과할 경우 에러 발생", () => {
-    expect(mockFn.mock.calls[0].length).toBeLessThanOrEqual(5);
+  // test("자동차 이름이 5자를 초과할 경우 에러 발생", () => {
+  //   expect(mockFn.mock.calls[0].length).toBeGreaterThanOrEqual(6);
+  // });
+  test("자동차 이름이 5자 이하일 경우 에러 발생하지 않음", () => {
     expect(mockFn.mock.calls[1].length).toBeLessThanOrEqual(5);
     expect(mockFn.mock.calls.length).toBe(2);
   });
 
   test("시도할 횟수가 숫자가 아닐 경우 에러 발생", () => {
     expect(() => app.validateNumber(NaN)).toThrow(ERROR.ATTEMPT_COUNT);
+  });
+  test("시도할 횟수가 숫자인 경우 에러 발생하지 않음", () => {
+    expect(() => app.validateNumber(5)).not.toThrow();
   });
 });
