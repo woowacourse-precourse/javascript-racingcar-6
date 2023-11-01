642715f0b78ae7eab9ed5238dd03581678aa4ab6
lcukylucy <101085055+luckylucyj@users.noreply.github.com>
2023-11-01 18:54:37 +0900

    feat(자동차에 이름 부여할 인풋, 사용자가 입력해 이동할 인풋, 사용자가 잘못된 값 입력할 시 error 출력할 throw문 생성)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+import {Console} from '@woowacourse/mission-utils';
 class App {
-  async play() {}
+  async play() {
+    let carNameInput = document.createElement('input');
+
+    carNameInput.addEventListener('click', updateName);
+
+    function updateName(){
+      let carNaming='';
+      carNaming=carNameInput.value;
+      return carNaming;
+    }
+
+    let advanceNum=document.createElement('input');
+
+    advanceNum.addEventListener('click', advanceOrStop);
+
+    function advanceOrStop(){
+      let count=0;
+      count=advanceNum.value;
+      return count;
+    }
+
+    function checkor(countNum){
+      let count=count;
+      let countNum = 4;
+      if(countNum =< count){
+        Console.print(count)
+      }
+    }
+
+    function getError(carNaming, advanceNum){
+      if (carNaming !== string) {
+        throw new Error('Parameter is not a string!');
+      }else if(countNum !== number){
+        throw new Error('Parameter is not a number!');
+      }
+    }
+  }
 }
 
 export default App;
