7fe1d58bcbe8bdd62541ee13625839c3f06b0fc8
정수아 <sua@jeongsuaui-MacBookAir.local>
2023-11-01 20:23:13 +0900

    [feat] 자동차 이름을 받아 문자열로 저장

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import {Console, Random} from '@woowacourse/mission-utils';
 class App {
-  async play() {}
+  async play() {
+    const userInput = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.');
+    console.log(userInput);
+
+    const CAR_NAME = userInput.split(',');
+    const CAR = CAR_NAME.map(name=>({name, distance: 0}));
+    console.log(CAR);
+  }
 }
 
 export default App;
