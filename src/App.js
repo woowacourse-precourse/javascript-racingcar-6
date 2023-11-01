3287d34de6699b592bcdace0e7b2d1dee67d3bf7
doongeon <ndmb2012@gamil.com>
2023-11-01 17:20:21 +0900

    1글자 미만 이름에 대한 예외처리 테스트 추가

--- src/App.js
+++ src/App.js
@@ -1,130 +1,134 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
-const getUserInput = async(text) => {
-  let userInput = '';
-  await MissionUtils.Console.readLineAsync(text)
-    .then((input) => {userInput = input})
-    .catch((err) => {throw err});
-
-  return userInput;
-}
-
-const splitUserInputByComma = (userInput) => {
-  const inputSplitByComma = userInput.split(',');
-  return inputSplitByComma;
-}
-
-const checkNameLength = (userNames) => {
-  userNames.forEach((val) => {
-    if(val.length < 1) throw new Error('[ERROR] : 이름은 한글자 이상입니다.');
-    if(val.length > 5) throw new Error('[ERROR] : 이름은 다섯자 이하입니다.');
-  })
-}
-
-const stringToNaturalNumber = (userInputNumber) => {
-  const number = Number(userInputNumber);
-
-  if(isNaN(number)) throw new Error('[ERROR] : 입력이 숫자가 아닙니다.');
-
-  if(!Number.isInteger(number)) throw new Error('[ERROR] : 입력이 자연수가 아닙니다.' );
-
-  if(number <= 0) throw new Error('[ERROR] : 입력이 자연수가 아닙니다.');
-
-  return number;
-}
-
-const getRandomNumber = () => {
-  return MissionUtils.Random.pickNumberInRange(0,9);
-}
-
-const generateRacingCar = (NumberOfUsers) => {
-  const racingCars = [];
-  let count = 0;
-  while(count < NumberOfUsers) {
-    racingCars.push(0);
-    count++;
-  }
-
-  return racingCars;
-}
-
-const updateRacingCarLocation = (racingCars) => {
-  const updatedRacingCarLocation = racingCars.map((value) => {
-    const randomNumber = getRandomNumber();
-
-    if(randomNumber >= 4) {
-      return value + 1;
-    } else {
-      return value;
-    }
-  })
-
-  return updatedRacingCarLocation;
-}
-
-const printGameStatus = (userNames, racingCars) => {
-  userNames.forEach((userName, index) => {
-    const userStatus = `${userName} : `;
-    const locationStatus = '-'.repeat(racingCars[index]);
-    MissionUtils.Console.print(userStatus + locationStatus);
-  })
-}
-
-const gameStart = (userNames, gameRep)=> {
-  let racingCars = generateRacingCar(userNames.length);
-  let count = 0;
-
-  while(count < gameRep) {
-    racingCars = updateRacingCarLocation(racingCars);
-    
-    printGameStatus(userNames, racingCars);
-
-    count++;
-  }
-
-  return racingCars;
-}
-
-
-const getWinner = (gameResult, userNames) => {
-  let maxScore = 0;
-
-  gameResult.forEach((score) => {
-    if(score > maxScore) maxScore = score;
-  })
-
-  const winners = [];
-
-  gameResult.forEach((score, index) => {
-    if(score == maxScore) {
-      winners.push(userNames[index]);
-    }
-  })
-
-  return winners;
-}
-
-const printWinner = (winners) => {
-  const winnerString = winners.join(', ');
-  const winnerText = `최종 우승자 : ${winnerString}`;
-
-  MissionUtils.Console.print(winnerText);
-}
-
 class App {
+  getUserInput = async (text) => {
+    let userInput = '';
+  
+    await MissionUtils.Console.readLineAsync(text)
+      .then((input) => {userInput = input})
+      .catch((err) => {throw err});
+  
+    return userInput;
+  }
+  
+  splitUserInputByComma = (userInput) => {
+    const inputSplitByComma = userInput.split(',');
+  
+    return inputSplitByComma;
+  }
+  
+  checkNameLength = (userNames) => {
+    userNames.forEach((name) => {
+      if(name.length < 1) throw new Error('[ERROR] : 이름은 한글자 이상입니다.');
+      if(name.length > 5) throw new Error('[ERROR] : 이름은 다섯자 이하입니다.');
+    })
+  }
+  
+  stringToNaturalNumber = (userInputNumber) => {
+    const number = Number(userInputNumber);
+  
+    if(isNaN(number)) throw new Error('[ERROR] : 입력이 숫자가 아닙니다.');
+    if(!Number.isInteger(number)) throw new Error('[ERROR] : 입력이 자연수가 아닙니다.' );
+    if(number <= 0) throw new Error('[ERROR] : 입력이 자연수가 아닙니다.');
+  
+    return number;
+  }
+  
+  getRandomNumber = () => {
+    return MissionUtils.Random.pickNumberInRange(0,9);
+  }
+  
+  generateRacingCar = (numberOfUsers) => {
+    const racingCars = [];
+    let count = 0;
+    while(count < numberOfUsers.length) {
+      racingCars.push(0);
+      count++;
+    }
+  
+    return racingCars;
+  }
+  
+  updateRacingCarLocation = (racingCars) => {
+    const updatedRacingCarLocation = racingCars.map((score) => {
+      const randomNumber = this.getRandomNumber();
+  
+      if(randomNumber >= 4) {
+        return score + 1;
+      } else {
+        return score;
+      }
+    })
+  
+    return updatedRacingCarLocation;
+  }
+  
+  printGameStatus = (userNames, racingCars) => {
+    userNames.forEach((userName, index) => {
+      const userStatus = `${userName} : `;
+      const scoreStatus = '-'.repeat(racingCars[index]);
+      MissionUtils.Console.print(userStatus + scoreStatus);
+    })
+  }
+  
+  gameStarter = (userNames, gameRep)=> {
+    let racingCars = this.generateRacingCar(userNames);
+    let count = 0;
+  
+    while(count < gameRep) {
+      racingCars = this.updateRacingCarLocation(racingCars);
+      
+      this.printGameStatus(userNames, racingCars);
+  
+      count++;
+    }
+  
+    return racingCars;
+  }
+  
+  getWinner = (userNames, gameResult) => {
+    let maxScore = 0;
+  
+    gameResult.forEach((score) => {
+      if(score > maxScore) maxScore = score;
+    })
+  
+    const winners = [];
+  
+    gameResult.forEach((score, index) => {
+      if(score == maxScore) {
+        winners.push(userNames[index]);
+      }
+    })
+  
+    return winners;
+  }
+  
+  printWinner = (winners) => {
+    const winnerString = winners.join(', ');
+    const winnerText = `최종 우승자 : ${winnerString}`;
+  
+    MissionUtils.Console.print(winnerText);
+  }
+
   async play() {
     try {
-      const userInputNames = await getUserInput('Enter your Input : ');
-      const userNames = await splitUserInputByComma(userInputNames);
+      MissionUtils.Console.print('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
 
-      checkNameLength(userNames);
+      const userInputNames = await this.getUserInput('');
+      const userNames = this.splitUserInputByComma(userInputNames);
 
-      const userInputGameRep = await getUserInput('시도할 횟수는 몇 회인가요?');
-      const gameRep = stringToNaturalNumber(userInputGameRep);
-      const gameResult = gameStart(userNames, gameRep);
-      const winners = getWinner(gameResult, userNames);
+      this.checkNameLength(userNames);
 
-      printWinner(winners);
+      MissionUtils.Console.print('시도할 횟수는 몇 회인가요?');
+
+      const userInputGameRep = await this.getUserInput('');
+      const gameRep = this.stringToNaturalNumber(userInputGameRep);
+
+      const gameResult = this.gameStarter(userNames, gameRep);
+      const winners = this.getWinner(userNames, gameResult);
+
+      this.printWinner(winners);
     } catch(err) {
       return Promise.reject(err);
     }
