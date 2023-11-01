630ab4282e0e550a6535d4bb056fd30159151d57
bada308 <203190@jnu.ac.kr>
2023-11-01 20:42:09 +0900

    feat: App Class 기능 구현 - 사용자 입력 받기 - RacingGame 실행 - 우승자 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+import View from "./View.js";
+import RacingGame from "./racingGame.js";
+
 class App {
-  async play() {}
+  #racingGame;
+
+  get racingGame() {
+    return this.#racingGame;
+  }
+  set racingGame(racingGame) {
+    this.#racingGame = racingGame;
+  }
+
+  async readUserInput() {
+    const carNameList = await View.Input.inputCarNames();
+    const count = await View.Input.inputCount();
+
+    return { carNameList, count };
+  }
+
+  async play() {
+    const { carNameList, count } = await this.readUserInput();
+    this.racingGame = new RacingGame(carNameList, count);
+    this.playRacingGame();
+    this.showWinner();
+  }
+
+  playRacingGame() {
+    View.Output.printResultMessage();
+
+    while (!this.racingGame.isGameEnd) {
+      this.racingGame.moveAllCars();
+
+      const result = this.racingGame.gameStatus;
+      View.Output.printResult(result);
+    }
+  }
+
+  showWinner() {
+    View.Output.printWinner(this.racingGame.winner);
+  }
 }
 
 export default App;
