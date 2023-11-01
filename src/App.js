dd25f18e9aeda4e001c2ebf82b797c95f9f87390
donginLee <55528388+donginLee@users.noreply.github.com>
2023-11-01 12:17:06 +0900

    feat: 한 줄 공백 출력 기능 개발

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import RaceNumber from "./RaceNumber.js";
 class App {
   async play() {
     const carNamesString = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
-    
+
     const numberString = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
-    
+
+    Console.print('\n');
+;
   }
   
 }
