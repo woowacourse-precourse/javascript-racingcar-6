91c7c9d6179cacf9bc591622041b74ef8ec08353
future4ing <mirae4120@gmail.com>
2023-11-01 20:04:41 +0900

    feat : App에서 입력받은 값으로 자동차 경주 게임을 실행

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import Race from './Race.js';
+import Car from './Car.js';
+import { getCarNames, getRoundCount } from './UserInput.js';
+
 class App {
-  async play() {}
+    async play() {
+        const carNames = await getCarNames();
+        const roundCount = await getRoundCount();
+    
+        const cars = carNames.map((name) => new Car(name));
+        const race = new Race(cars, roundCount);
+        race.start();
+      }   
 }
 
 export default App;
