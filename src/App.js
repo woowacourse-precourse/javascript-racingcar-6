9a7678acb4603198f786ef724e2373b27c0c7171
hyeona <90515062+BaeHyeonA@users.noreply.github.com>
2023-11-01 22:37:28 +0900

    feat: 유저 자동차 이름 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
   async play() {}
+
+  async getCarNames(){
+    const carNamesList = [];
+    const carNames = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+    carNames.split(',').forEach((carname) => {
+      if(carname.length > 5) throw new Error("[ERROR] 5자 이하 이름이 아닙니다.");
+      carNamesList.push(carname);
+    });
+    return carNamesList;
+  }
 }
 
 export default App;
