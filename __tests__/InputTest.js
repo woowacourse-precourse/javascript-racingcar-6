3a0b5e9f8ae05e5773d229a99431ec57a8918416
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2023-11-01 16:11:41 +0900

    [test] : Update 사용자 입력 테스트 3가지

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -1,35 +1,36 @@
-import App from "../src/App";
-import { MissionUtils } from "@woowacourse/mission-utils";
-
-const mockQuestions = (inputs) => {
-  // jest.fn() : 가짜 함수를 생성하는 메서드.
-  MissionUtils.Console.readLineAsync = jest.fn();
-
-  // jeset.fn().mockImplementation() : 가짜로 구현된 코드.
-  MissionUtils.Console.readLineAsync.mockImplementation(() => {
-    const input = inputs.shift(); // 배열의 첫 번째 값 input에 저장
-    return Promise.resolve(input); // Promise 형태로 첫 번째 값 반환
+const mockCheckCarName = jest.fn((strCarName) => {
+  const arrCarName = strCarName.split(",");
+  arrCarName.map((name) => {
+    if (name.trim().length > 5 || name.trim().length < 1) {
+      throw new Error("[ERROR]");
+    }
   });
-};
+});
+
+const mockCheckAttemptNumber = jest.fn((attempts) => {
+  if (attempts < 0 || isNaN(attempts) == true) {
+    throw new Error("[ERROR]");
+  }
+});
 
 describe("사용자 입력 테스트", () => {
   test("자동차 이름이 다섯 글자를 초과한 예외에 대한 처리 확인", async () => {
-    const inputs = ["abc,abcdef", "2"];
+    const inputs = "abc,abcdef";
 
-    mockQuestions(inputs);
+    await expect(() => mockCheckCarName(inputs)).toThrow("[ERROR]");
+  });
 
-    const app = new App();
+  test("자동차 이름이 공백인 예외에 대한 처리 확인", async () => {
+    const inputs = "  ";
 
-    await expect(app.play()).rejects.toThrow("[ERROR]");
+    await expect(() => mockCheckCarName(inputs)).toThrow("[ERROR]");
   });
 
   test("사용자가 이동할 횟수를 잘못 입력한 예외에 대한 처리 확인", async () => {
-    const inputs = ["abc,abcd", "a"];
+    const inputString = "a";
+    const inputNegativeNumber = -4;
 
-    mockQuestions(inputs);
-
-    const app = new App();
-
-    await expect(app.play()).rejects.toThrow("[ERROR]");
+    await expect(() => mockCheckAttemptNumber(inputString)).toThrow("[ERROR]");
+    await expect(() => mockCheckAttemptNumber(inputNegativeNumber)).toThrow("[ERROR]");
   });
 });
