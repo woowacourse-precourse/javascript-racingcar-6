023df5b6737715f8b96f94e991350a317e7cf215
Youjiiin <yujinjang24@gmail.com>
2023-11-01 15:33:37 +0900

    feat: 입력값 유효성 조건 추가 - 자동차 이름의 공백이나 특수문자, 0이하의 시도횟수 입력시 에러

--- src/constants/ErrorMsg.js
+++ src/constants/ErrorMsg.js
@@ -2,7 +2,10 @@ const NAME_ERROR = {
     ONE_NAME_ERROR : "[ERROR] 이름은 두개 이상, 쉼표(,)로 구분해서 작성해야 합니다.",
     LENGTH_ERROR : "[ERROR] 자동차의 이름은 5글자 이하로 작성해야 합니다.",
     SAME_NAME_ERROR : "[ERROR] 중복된 이름을 사용할 수 없습니다.",
-    COUNT_ERROR : "[ERROR] 잘못된 시도횟수를 입력했습니다.",
+    ONLY_CHAR_ERROR : "[ERROR] 자동차 이름에는 특수문자나 공백이 포함될 수 없습니다.",
+
+    COUNT_NUMBER_ERROR : "[ERROR] 시도횟수는 정수로 입력해 주세요.",
+    COUNT_MINUS_ERROR : "[ERROR] 시도횟수는 1이상의 정수로 입력해 주세요",
     COMMA_ERROR : "[ERROR] 쉼표의 입력이 잘못되었습니다.",
     NULL_ERROR : "[ERROR] 아무런 입력이 들어오지 않았습니다.",
 };
