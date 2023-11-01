50b06fed4b067460a5a0a1da7c82ff1e76ad34df
rheefine <rheefine@gmail.com>
2023-11-01 20:07:59 +0900

    feat: Car Model Test 추가

---
+++ __tests__/ModelTest/CarsTest.js
@@ -0,0 +1,29 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import Car from '../../src/Model/Car.js';
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
+describe('Car Model Test', () => {
+  test('Car 정상 이동 테스트', async () => {
+    // given
+    const carName = 'a,b,c';
+    const randomNum = [1, 4, 3];
+    mockRandoms([...randomNum]);
+
+    // when
+    const car = new Car();
+    car.setCarsPosition(carName);
+    car.setCarsRelocation();
+    // then
+    await expect(car.getCarsPosition()).toEqual([
+      { name: 'a', position: '' },
+      { name: 'b', position: '-' },
+      { name: 'c', position: '' },
+    ]);
+  });
+});
