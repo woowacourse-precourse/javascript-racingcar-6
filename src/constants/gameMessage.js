636e76f94038e0928ca618914d03db4d88fc8d83
kimmainsain <kmsid003@gmail.com>
2023-11-01 19:50:59 +0900

    :memo: Docs(gameMessage) : 게임 메시지 작성

---
+++ src/constants/gameMessage.js
@@ -0,0 +1,7 @@
+const GAME_MESSAGE = {
+  GAME_START: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
+  GAME_INPUT_COUNT: "시도할 횟수는 몇회인가요?",
+  GAME_RESULT: "실행 결과",
+  GAME_WINNER: "최종 우승자 : ",
+  GAME_CAR_DISTANCE: "-",
+};
