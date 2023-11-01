e780ca2ed63ce0f8da9476f4cc794738b48b241a
Jeon Eonseok <jseokeon@gmail.com>
2023-11-01 16:15:20 +0900

    fix: getCarName 함수 return 값 추가

--- src/getCarName.js
+++ src/getCarName.js
@@ -1,7 +1,9 @@
 import { Console } from '@woowacourse/mission-utils';
 
 const getCarName = async function getCarNameByUserInput() {
-  await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
+  const CAR_NAME = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)');
+
+  return CAR_NAME.split(',');
 }
 
 export default getCarName;
