c6feb68c3258ee37bdcc2a6d186b5d328371ad04
Dobbymin <gmin0701@knu.ac.kr>
2023-11-01 23:39:58 +0900

    [STYLE](progressRacingCar):  에러코드 상수 이름 변경

--- src/components/progressRacingCar.js
+++ src/components/progressRacingCar.js
@@ -4,7 +4,7 @@ import { CAR } from '../pages/texts.js';
 export default function progressRacingCar(CARS) {
     CARS.forEach((car) => {
         let info = car.name + ' : ';
-        for (let i = 0; i < car.go; i++) info += CAR.PROCESS_BAR;
+        for (let i = 0; i < car.go; i++) info += CAR.process_bar;
         MissionUtils.Console.print(info);
     });
     MissionUtils.Console.print('\n');
