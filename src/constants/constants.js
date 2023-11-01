cfc711a8b1a25dfb699ba47222a27335bc0d904d
mieum12 <qpdlqltb1215@gmail.com>
2023-11-01 17:25:14 +0900

    test: 시도횟수 검증로직 테스트

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -10,7 +10,7 @@ export const ERROR = {
   NAME_RANGE_ERROR: "[ERROR] 자동차 이름은 1~5 글자로 입력해주세요.",
   NAME_DUPLICATION_ERROR: "[ERROR] 자동차 이름 중 중복된 이름이 있습니다.",
   BLANK_ERROR: "[ERROR] 공백 없이 입력해야합니다.",
-  COMMA_START_ERROR: "[ERROR] 첫번째 자동차를 입력해주세요.",
+  COMMA_ERROR: "[ERROR] 자동차의 이름은 쉼표로 시작되거나 끝나면 안됩니다.",
   NUMBER_RANGE_ERROR: "[ERROR] 시도 횟수는 1 이상의 자연수로 입력해주세요.",
   NUMBER_ONLY_ERROR: "[ERROR] 시도 횟수는 숫자만 입력 가능합니다.",
 };
