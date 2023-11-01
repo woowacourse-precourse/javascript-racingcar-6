9409f924ae907e7b29f5f0d5617973eaf7e59fd4
nkEllaaa <dhrtns515@gmail.com>
2023-11-01 21:05:51 +0900

    fix: 바로 이전의 커밋에서 누락된 파일 추가

--- src/validation/validationCarName.js
+++ src/validation/validationCarName.js
@@ -1,19 +1,15 @@
-const validation = (carNames) => {
-  // 배열로 들어왔는지 확인
+const validateCarName = (carNames) => {
   if (!Array.isArray(carNames)) return false;
 
-  // 자동차가 있는지 확인
   if (carNames.length === 0) return false;
 
-  //각 자동차의 이름이 5초과인지 확인
   for (const carName of carNames) {
     if (carName.length > 5) return false;
   }
 
-  //자동차 이름에 중복이 있는지 확인
   if (carNames.length !== new Set(carNames).size) return false;
 
   return true;
 };
 
-export default validation;
+export default validateCarName;
--- src/validation/validationRaceCount.js
+++ src/validation/validationRaceCount.js
@@ -1,5 +1,6 @@
 const validateRaceCount = (raceCounts) => {
   const raceCountToNumber = parseInt(raceCounts);
+
   if (Number.isNaN(raceCountToNumber) || raceCountToNumber <= 0) return false;
 
   return true;
