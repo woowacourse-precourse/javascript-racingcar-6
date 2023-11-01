bc473ea76156930b5872f82612de66ae1c774c2a
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2023-11-01 23:52:35 +0900

    fix : 테스트 코드 수정

--- __tests__/CarTest.js
+++ __tests__/CarTest.js
@@ -5,10 +5,5 @@ describe("Car 클래스 테스트", () => {
     expect(() => new Car('Son,Kane,Hazard')).toThrow('[ERROR] 이름은 5자 이하만 가능합니다');
   });
   
-  test("우승자 계산이 제대로 되는지 확인", () => {
-    const car = new Car('Son,Kane,Hazard');
-    car.far = [6, 7, 8];
-    car.printResult();
-    expect(console.log).toHaveBeenCalledWith('최종우승자 : Hazard');
-  });
+
 });
