eef93a731e53d9d319b8379adf878e24e2b8215a
yeong-hwan <jghff700@naver.com>
2023-11-01 21:58:55 +0900

    test: validateRacingCount 함수 테스트 작성

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -3,6 +3,7 @@ import validateCarNames from "../src/validator/validateCarNames.js";
 import validateRacingCount from "../src/validator/validateRacingCount.js";
 
 describe('Validator 테스트', () => {
+  // validateCarNames test
   test('자동차 이름에 공백 입력', () => {
     expect(() => { 
       validateCarNames(['']);
@@ -18,6 +19,31 @@ describe('Validator 테스트', () => {
   test('자동차 이름을 중복 입력', () => {
     expect(() => { 
       validateCarNames(['same', 'same']);
+    }).toThrow(ErrorMessage);
+  });
+
+  // validateRacingCount test
+  test('경주 횟수에 숫자가 아닌 문자를 입력', () => {
+    expect(() => { 
+      validateRacingCount('One');
+    }).toThrow(ErrorMessage);
+  });
+
+  test('경주 횟수에 자연수가 아닌 수 입력 : 음수', () => {
+    expect(() => { 
+      validateRacingCount('-1');
+    }).toThrow(ErrorMessage);
+  });
+
+  test('경주 횟수에 자연수가 아닌 수 입력 : 0', () => {
+    expect(() => { 
+      validateRacingCount('0');
+    }).toThrow(ErrorMessage);
+  });
+
+  test('경주 횟수에 자연수가 아닌 수 입력 : 소수점', () => {
+    expect(() => { 
+      validateRacingCount('0.5');
     }).toThrow(ErrorMessage);
   });
 });
