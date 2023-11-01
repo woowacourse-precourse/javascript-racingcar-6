990948992a93f4e6cc1c93d57f62cade8b1bc4c6
parkblo <badmuxi@gmail.com>
2023-11-01 22:32:47 +0900

    Style: airbnb 코드 컨벤션 적용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Random, Console } from "@woowacourse/mission-utils";
+import { Random, Console } from '@woowacourse/mission-utils';
 
 class App {
   constructor() {
@@ -14,8 +14,8 @@ class App {
 
     this.initCarMoveCounts();
 
-    Console.print("실행 결과");
-    for (let i=0; i<this.playCount; i++) {
+    Console.print('실행 결과');
+    for (let i = 0; i < this.playCount; i += 1) {
       this.moveCars();
       this.printProgress();
     }
@@ -24,18 +24,18 @@ class App {
   }
 
   async userInput() {
-    this.carNames = (await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")).split(",");
+    this.carNames = (await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)')).split(',');
     this.carNumber = this.carNames.length;
 
-    for (let i=0; i<this.carNumber; i++) {
+    for (let i = 0; i < this.carNumber; i += 1) {
       if (this.carNames[i].length > 5 || this.carNames[i].length <=0) {
-        throw new Error("[ERROR] 잘못된 자동차 이름 형식입니다.")
+        throw new Error('[ERROR] 잘못된 자동차 이름 형식입니다.')
       }
     }
     
-    this.playCount = (await Console.readLineAsync("시도할 횟수는 몇 회인가요?"));
+    this.playCount = (await Console.readLineAsync('시도할 횟수는 몇 회인가요?'));
     if (isNaN(this.playCount)) {
-      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.")
+      throw new Error('[ERROR] 숫자가 잘못된 형식입니다.')
     }
     
     this.playCount = Number(this.playCount);
@@ -54,7 +54,7 @@ class App {
   }
 
   moveCars() {
-    for (let i=0; i<this.carNumber; i++) {
+    for (let i = 0; i < this.carNumber; i += 1) {
       if (this.isCarMovable()) {
         this.carMoveCounts[i] += 1;
         
@@ -66,29 +66,30 @@ class App {
   }
 
   printProgress() {
-    for (let i=0; i<this.carNumber; i++) {
-      let progressString = this.carNames[i] + " : ";
+    for (let i = 0; i < this.carNumber; i += 1) {
+      let progressString = this.carNames[i] + ' : ';
 
-      for (let j=0; j<this.carMoveCounts[i]; j++) {
-        progressString += "-";
+      for (let j = 0; j < this.carMoveCounts[i]; j += 1) {
+        progressString += '-';
       }
 
       Console.print(progressString);
     }
-    Console.print("");
+    // 한줄 추가로 띄어쓰기
+    Console.print('');
   }
 
   printWinner() {
-    let winnerString = "최종 우승자 : ";
+    let winnerString = '최종 우승자 : ';
     let winnerArray = [];
 
-    for (let i=0; i<this.carNumber; i++) {
+    for (let i = 0; i < this.carNumber; i += 1) {
       if (this.carMoveCounts[i] == this.maxMoveCounts) {
         winnerArray.push(this.carNames[i]);
       }
     }
 
-    winnerString += winnerArray.join(",");
+    winnerString += winnerArray.join(',');
     Console.print(winnerString);
   }
 
