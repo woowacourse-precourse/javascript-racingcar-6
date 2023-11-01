330566989c547a7ec748d1b5c64b2a77e536ae3e
Jihyun_Kim <jihyun3313@daum.net>
2023-11-01 18:11:48 +0900

    fix: game 생성자 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import RacingGame from "./RacingGame/index.js";
+import RacingGame from "./racingGame/index.js";
 
 class App {
+  game;
+  constructor() {
+    this.game = new RacingGame();
+  }
   async play() {
 
-    const game = new RacingGame();
-    await game.gameStart();
+    // const game = new RacingGame();
+    await this.game.gameStart();
   }
 }
 
