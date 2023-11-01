585c3bb92b14e8b9a41381fb46c0da8d21f121a8
Yeojin Kim <yeojin58@naver.com>
2023-11-01 15:42:16 +0900

    자동차 전진 조건 테스트 중복 제거

--- __tests__/CargameTest.js
+++ __tests__/CargameTest.js
@@ -19,17 +19,19 @@ describe ('App class', () => {
         jest.resetAllMocks();
     });
 
+    const moveCarsMocked = (mockedValue, expectedPosition) => {
+        MissionUtils.Random.pickNumberInRange.mockReturnValue(mockedValue);
+        app.moveCars();
+        expect(app.cars[0].position).toBe(expectedPosition);
+    }
+
     test ('자동차는 4 이상의 숫자에서만 전진한다', () => {
 
         // 랜덤숫자가 4를 리턴할 때
-        MissionUtils.Random.pickNumberInRange.mockReturnValue(4);
-        app.moveCars();
-        expect(app.cars[0].position).toBe(1);
+        moveCarsMocked(4, 1);
 
         // 랜덤숫자가 3을 리턴할 때
         app.cars[0].position = 0; // 자동차 위치 초기화
-        MissionUtils.Random.pickNumberInRange.mockReturnValue(3);
-        app.moveCars();
-        expect(app.cars[0].position).toBe(0);
+        moveCarsMocked(3, 0);
     });
 });
