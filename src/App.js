186a038fc62fd4b51cbad24710968b39d800a343
kim <kajinori@gachon.ac.kr>
2023-11-01 13:04:08 +0900

    feat: 입력받은 이름들을 배열에 저장

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import InputView from './InputView.js';
+import Car from './Car.js';
 
 class App {
   tryCount = 0;
+  carList = [];
+
   async play() {
     await this.requestCarNames();
     await this.requestTryCount();
   }
 
+  makeCarList = (carName) => {
+    this.carList.push(new Car(carName));
+  }
+
   async requestCarNames() {
     const carNames = await InputView.requestCarNames();
+    carNames.forEach((carName) => this.makeCarList(carName));
     return carNames;
   }
 
