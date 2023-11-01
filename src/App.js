418ca7bc06e58bd4760c1dfd65d01781c6eec717
yongjae95 <64951@hanmail.net>
2023-11-01 20:28:42 +0900

    feat:자동차 생성 및 게임시작 함수 생

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const playGame = function racingGame(nameArr, count) {
+      let nameObj = {};
+      for (let i = 0; i < nameArr.length; i++) {
+        nameObj[nameArr[i]] = 0;
+      }
+      for (let i = 0; i < count; i++) {
+        nameArr.forEach((num) => {
+          nameObj[num] += 1;
+        });
+        nameArr.forEach((j) => {
+          const dashes = "-".repeat(nameObj[j]);
+          Console.print(`${j} : ${dashes}`);
+        });
+      }
+    };
+  }
 }
 
 export default App;
