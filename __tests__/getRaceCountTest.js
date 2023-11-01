cecd536dc701ce55b7720f88ff491439d567b160
nkEllaaa <dhrtns515@gmail.com>
2023-11-01 19:08:25 +0900

    refactor: 진행 횟수 테스트 코드에서 Error -> false 던지는 것으로 수정

--- __tests__/getRaceCountTest.js
+++ __tests__/getRaceCountTest.js
@@ -1,19 +1,27 @@
-import valitdateRaceCount from '../src/validation/validationRaceCount';
+import validateRaceCount from '../src/validationRaceCount';
 
 describe('checkRaceCount', () => {
   test('숫자가 맞는지 확인, 아닐 경우 예외 처리', () => {
     //given
-    const raceCount = '1';
+    const raceCount = 'a';
 
     //when & then
-    expect(() => valitdateRaceCount(raceCount)).toThrowError('[ERROR]');
+    expect(validateRaceCount(raceCount)).toBeFalsy();
   });
 
-  test('숫자가 0이 아닌지 확인, 아닐 경우 예외 처리', () => {
+  test('숫자가 0인지 확인, 0이면 에러처리', () => {
     //given
     const raceCount = 0;
 
     //when & then
-    expect(() => valitdateRaceCount(raceCount)).toThrowError('[ERROR]');
+    expect(validateRaceCount(raceCount)).toBeFalsy();
+  });
+
+  test('1 이상의 숫자를 입력했을 경우 정상 작동', () => {
+    // given
+    const raceCount = 2;
+
+    // when & then
+    validateRaceCount(raceCount);
   });
 });
