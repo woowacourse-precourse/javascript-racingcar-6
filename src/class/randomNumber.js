a6b89127b46fdc6482238f4446b886a8685ef550
KYHH <kyhh93@naver.com>
2023-11-01 11:07:43 +0900

    refactor : 불필요한 코드 및 라이브러리 제거

--- src/class/randomNumber.js
+++ src/class/randomNumber.js
@@ -1,14 +1,12 @@
-import { Random, Console } from '@woowacourse/mission-utils';
+import { Random } from '@woowacourse/mission-utils';
 
 class CreateCarMoveCount {
   constructor() {
-    this.number = [];
   }
 
   createRandomNumber() {
     const getNumber = Random.pickNumberInRange(0, 9);
-    this.number.push(getNumber);
-    return this.number;
+    return getNumber;
   }
 
 }
