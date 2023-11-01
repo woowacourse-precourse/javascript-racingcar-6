2f38dbb97b5f08f37b1fed2eecc5e3d4855398e1
intersoom <sallybig@naver.com>
2023-11-01 15:56:34 +0900

    feat: 안내 문구 상수 추가

---
+++ src/const/messageData.js
@@ -0,0 +1,8 @@
+export const INFORMATION_MESSEAGE = Object.freeze({
+  CAR_NAME_INPUT:
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
+  COUNT_INPUT: "시도할 횟수는 몇 회인가요?",
+  RESULT_OUTPUT: "실행 결과",
+  MOVING_PROGRESS_MARK: "-",
+  WINNER: "최종 우승자 : ",
+});
