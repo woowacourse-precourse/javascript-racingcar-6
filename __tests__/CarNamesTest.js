1b3fe30b52e24df689a872e553b795b7e8a06294
yunji <dbswl330@gmail.com>
2023-11-01 19:52:44 +0900

    test: NumberOfAttemptTest test 추가

---
+++ __tests__/CarNamesTest.js
@@ -0,0 +1,38 @@
+import CarNames from "../src/model/car/carNames";
+
+const carNames = new CarNames();
+
+describe("createValidNameList 테스트", () => {
+  test("정상적인 자동차 이름이 입력된 경우", () => {
+    const input = "isy,yunji,y";
+    expect(() => carNames.createValidNameList(input)).not.toThrow();
+  });
+
+  test("자동차 이름이 없을경우", () => {
+    const input = "";
+    expect(() => carNames.createValidNameList(input)).toThrow(
+      "[ERROR] 이름을 입력해 주세요."
+    );
+  });
+
+  test("자동차 이름이 5글자를 초과하는 경우", () => {
+    const input = "sdf,sdfsdf";
+    expect(() => carNames.createValidNameList(input)).toThrow(
+      "[ERROR] 5글자 이하의 이름을 입력해주세요."
+    );
+  });
+
+  test("중복된 자동차 이름이 있는 경우", () => {
+    const input = "sdf,sdf,sd";
+    expect(() => carNames.createValidNameList(input)).toThrow(
+      "[ERROR] 중복된 이름이 있습니다."
+    );
+  });
+
+  test("공백인 자동차 이름이 있는 경우", () => {
+    const input = " ,sdf";
+    expect(() => carNames.createValidNameList(input)).toThrow(
+      "[ERROR] 이름은 공백이 될 수 없습니다."
+    );
+  });
+});
---
+++ __tests__/NumberOfAttemptTest.js
@@ -0,0 +1,39 @@
+import NumberOfAttempt from "../src/model/game/numberOfAttempt";
+
+describe("시도 횟수 Validator 테스트", () => {
+  test("숫자가 정수가 아닌 경우", () => {
+    expect(() => {
+      const numberOfAttempt1 = new NumberOfAttempt("1.5");
+    }).toThrow("[ERROR] 정수를 입력해 주세요.");
+  });
+
+  test("자연수가 아닌 경우", () => {
+    expect(() => {
+      const numberOfAttempt2 = new NumberOfAttempt("0");
+    }).toThrow("[ERROR] 0이상의 정수를 입력해 주세요.");
+
+    expect(() => {
+      const numberOfAttempt3 = new NumberOfAttempt("-1");
+    }).toThrow("[ERROR] 0이상의 정수를 입력해 주세요.");
+  });
+
+  test("isFinished() 테스트 - 시도 횟수가 0이 된 경우", () => {
+    const numberOfAttempt = new NumberOfAttempt("1");
+    numberOfAttempt.decrease();
+    expect(numberOfAttempt.isFinished()).toBe(true);
+  });
+
+  test("isFinished() 테스트 - 시도 횟수가 0이 아닌 경우", () => {
+    const numberOfAttempt = new NumberOfAttempt(2);
+    numberOfAttempt.decrease();
+
+    expect(numberOfAttempt.isFinished()).toBe(false);
+  });
+
+  test("decrease() 테스트", () => {
+    const numberOfAttempt = new NumberOfAttempt(3);
+
+    numberOfAttempt.decrease();
+    expect(numberOfAttempt.NumberOfAttempt).toBe(2);
+  });
+});
