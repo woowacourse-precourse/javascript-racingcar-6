150f93078113c93cca16d12ffa1982f22d985afc
intersoom <sallybig@naver.com>
2023-11-01 20:29:34 +0900

    Revert "feat: Car 클래스 생성"
    This reverts commit b019bcfacca9231ed5bb057cb696d47f045339e6.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
-import Car from "./Car.js";
-import Input from "./Input.js";
-import Validation from "./Validation.js";
-
 class App {
-  cars;
-
-  async play() {
-    const carNamesInput = await Input.carNames();
-    Validation.carNames(carNamesInput);
-
-    this.cars = carNamesInput.split(",").map((carName) => new Car(carName));
-  }
+  async play() {}
 }
 
 export default App;
--- src/const/messageData.js
+++ src/const/messageData.js
@@ -1,7 +1,7 @@
 export const INFORMATION_MESSEAGE = Object.freeze({
   CAR_NAMES_INPUT:
     "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
-  TRY_COUNT_INPUT: "시도할 횟수는 몇 회인가요?",
+  COUNT_INPUT: "시도할 횟수는 몇 회인가요?",
   RESULT_OUTPUT: "실행 결과",
   MOVING_PROGRESS_MARK: "-",
   WINNER: "최종 우승자 : ",
