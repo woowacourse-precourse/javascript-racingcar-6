d15b56d4fc9b5948143855ac2a0fa1f9a9290877
Rilee-0320 <go13209@naver.com>
2023-11-01 18:41:25 +0900

    refactor: 인덴트 조건 충족을 위한 함수 분리

--- src/App.js
+++ src/App.js
@@ -2,100 +2,92 @@ import { Random, Console } from "@woowacourse/mission-utils";
 
 class App {
   async play() {
-    try {
-      let carsInfo = {};
+    let carsInfo = {};
 
-      const carNameList = await this.inputCarNames();
-      this.checkNames(carNameList);
+    const carNameList = (await inputCarNames()).split(",");
+    checkNames(carNameList);
 
-      const roundNumber = await this.inputRoundNumber();
-      this.checkNumber(roundNumber);
+    const roundNumber = await inputRoundNumber();
+    checkNumber(roundNumber);
 
-      this.createObject(carNameList, carsInfo);
+    createObject(carNameList, carsInfo);
 
-      Console.print("\n실행 결과");
+    Console.print("\n실행 결과");
 
-      for (let n = 0; n < roundNumber; n++) {
-        this.oneRoundGame(carsInfo);
-        Console.print("\n");
-      }
+    for (let n = 0; n < roundNumber; n++) {
+      oneRoundGame(carsInfo);
+      Console.print("\n");
+    }
 
-      this.printResult(carsInfo);
-    } catch (error) {
-      throw error;
+    printResult(carsInfo);
+  }
+}
+
+async function inputCarNames() {
+  const carNames = await Console.readLineAsync(
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+  );
+  return carNames;
+}
+
+async function inputRoundNumber() {
+  return await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+}
+
+function checkNames(carNameList) {
+  for (let i = 0; i < carNameList.length; i++) {
+    if (carNameList[i].length > 5) {
+      throw new Error("[ERROR] 사용할 수 없는 이름입니다");
+    }
+  }
+}
+
+function checkNumber(roundNumber) {
+  if (isNaN(roundNumber)) {
+    throw new Error("[ERROR] 숫자를 입력해주세요");
+  }
+}
+
+function randomNumber() {
+  const number = Random.pickNumberInRange(0, 9);
+  return number;
+}
+
+function createObject(carNameList, carsInfo) {
+  for (let j = 0; j < carNameList.length; j++) {
+    carsInfo[carNameList[j]] = { score: 0 };
+  }
+}
+
+function oneRoundGame(carsInfo) {
+  const carNames = Object.keys(carsInfo);
+  for (let k = 0; k < carNames.length; k++) {
+    const number = randomNumber();
+    if (number >= 4) {
+      carsInfo[carNames[k]].score += 1;
     }
   }
 
-  async inputCarNames() {
-    const carNames = await Console.readLineAsync(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
-    );
-    return carNames.split(",");
-  }
+  carNames.forEach((carName) => {
+    const score = carsInfo[carName].score;
+    Console.print(`${carName} : ${"-".repeat(score)}`);
+  });
+}
 
-  async inputRoundNumber() {
-    return Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
-  }
-
-  checkNames(carNameList) {
-    for (let i = 0; i < carNameList.length; i++) {
-      if (carNameList[i].length > 5) {
-        throw new Error("[ERROR] 사용할 수 없는 이름입니다");
-      }
+function printResult(carsInfo) {
+  let winners = [];
+  let maxScore = 0;
+  const carNames = Object.keys(carsInfo);
+  carNames.forEach((carName) => {
+    const score = carsInfo[carName].score;
+    if (score > maxScore) {
+      maxScore = score;
+      winners = [carName];
+    } else if (score === maxScore) {
+      winners.push(carName);
     }
-  }
-
-  checkNumber(roundNumber) {
-    if (isNaN(roundNumber)) {
-      throw new Error("[ERROR] 숫자를 입력해주세요");
-    }
-  }
-
-  randomNumber() {
-    const number = Random.pickNumberInRange(0, 9);
-    return number;
-  }
-
-  createObject(carNameList, carsInfo) {
-    for (let j = 0; j < carNameList.length; j++) {
-      carsInfo[carNameList[j]] = { score: 0 };
-    }
-  }
-
-  oneRoundGame(carsInfo) {
-    const carNames = Object.keys(carsInfo);
-    for (let k = 0; k < carNames.length; k++) {
-      const number = this.randomNumber();
-      if (number >= 4) {
-        carsInfo[carNames[k]].score += 1;
-      }
-    }
-
-    carNames.forEach((carName) => {
-      const score = carsInfo[carName].score;
-      Console.print(`${carName} : ${"-".repeat(score)}`);
-    });
-  }
-
-  printResult(carsInfo) {
-    let winners = [];
-    let maxScore = 0;
-
-    const carNames = Object.keys(carsInfo);
-
-    carNames.forEach((carName) => {
-      const score = carsInfo[carName].score;
-
-      if (score > maxScore) {
-        maxScore = score;
-        winners = [carName];
-      } else if (score === maxScore) {
-        winners.push(carName);
-      }
-    });
-
-    Console.print("최종 우승자 : " + winners.join(", "));
-  }
+  });
+  Console.print("최종 우승자 : " + winners.join(", "));
 }
 
 export default App;
