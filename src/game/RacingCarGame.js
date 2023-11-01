ba7da6522bc1ff01e6c7d2744a8b26644b187de6
ImxYJL <allensain14@gmail.com>
2023-11-01 15:42:07 +0900

    feat: RacingCarGame 사용자 입력(이름, 게임 횟수)을 받는 함수들 작성

--- src/game/RacingCarGame.js
+++ src/game/RacingCarGame.js
@@ -1,4 +1,6 @@
 import Garage from './Garage.js';
+import { print, readLineAsync } from '../utility/console.js';
+import { MESSAGE, ERROR_MESSAGE } from '../constant/message.js';
 
 class RacingCarGame {
   #gameCount = 0;
@@ -6,9 +8,19 @@ class RacingCarGame {
   #nameList = [];
   
   #nameListString = '';
-  
+
+  async #getNameString() {
+    this.#nameListString = await readLineAsync(MESSAGE.nameInputGuide);
+  }
+
+  async #getGameCount() {
+    this.#gameCount = await readLineAsync(MESSAGE.countInputGuide);
+  }
+
   async gameStart() {
-    
+    await this.#getNameString();
+
+    await this.#getGameCount();
   }
 }
 
