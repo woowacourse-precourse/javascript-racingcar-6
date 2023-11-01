997fd5e8ba9c3573b4deedaa9c79000fe2a14f74
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2023-11-01 16:22:18 +0900

    feat : 경주할 자동차와 라운드 수 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    // 게임 준비
+    const cars = await MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)기준으로 구분\n"
+    );
+    const carList = cars.split(",");
+    const round = await MissionUtils.Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요?\n"
+    );
 }
 
 export default App;
