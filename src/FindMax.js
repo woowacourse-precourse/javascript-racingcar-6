9b902e15af5ecf886516fd2ceb004cae34858fb8
bmj0053 <bmj05378@gmail.com>
2023-11-01 20:11:01 +0900

    refactor: FindMax 리팩토링

--- src/FindMax.js
+++ src/FindMax.js
@@ -1,9 +1,14 @@
+function CheckMax(maxForward, forwardCounts, i) {
+  if (maxForward < forwardCounts[i]) {
+    maxForward = forwardCounts[i];
+  };
+  return maxForward
+};
+
 export function FindMax(carList, forwardCounts) {
   let maxForward = 0;
   for (let i = 0; i < carList.length; i++) {
-    if (maxForward < forwardCounts[i]) {
-      maxForward = forwardCounts[i];
-    };
+    maxForward = CheckMax(maxForward, forwardCounts, i)
   };
   return maxForward
-}
+};
