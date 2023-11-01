2e5c440a2afdc306d548383e0f0c8debddda3b9c
HarrySeop <parkpjs3083@naver.com>
2023-11-01 23:44:13 +0900

    chore: Message.js 오타 수정

--- src/Message.js
+++ src/Message.js
@@ -1,5 +1,5 @@
 const GAME_MESSAGES = Object.freeze({
-  START: "경주할 자동차 이름을 입력하세요.(이름은 윔표(,) 기준으로 구분) \n",
+  START: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) \n",
   ATTEMPT: "시도할 횟수는 몇 회인가요? \n",
   RESULT: "실행 결과",
   WINNER: "최종 우승자 : "
