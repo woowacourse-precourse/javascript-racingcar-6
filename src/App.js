6a372c383660ffe3bf5d07cf2673a70def9f5cd5
dev-yenya <dev.yenya@gmail.com>
2023-11-01 14:54:44 +0900

    ✨ feat: Add receiveCarNames method
    - Implemented the receiveCarNames method to receive and process user input for car names
    - Utilized Console.readLineAsync from @woowacourse/mission-utils for user input handling
    - Split the user input string by comma to create an array of car names
    - Fixed the play method to orchestrate the game flow

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async receiveCarNames() {
+    const carNamesInput = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    const carNames = carNamesInput.split(',');
+    return carNames;
+  }
+
+  async play() {
+    this.receiveCarNames();
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
