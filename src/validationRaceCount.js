30e128f2ce629dd758fd0f5362060e530e6675d6
nkEllaaa <dhrtns515@gmail.com>
2023-11-01 19:15:00 +0900

    chore: validation 관련 파일 경로 변경

---
+++ src/validationRaceCount.js
@@ -0,0 +1,8 @@
+const validateRaceCount = (raceCounts) => {
+  const raceCountToNumber = parseInt(raceCounts);
+  if (Number.isNaN(raceCountToNumber) || raceCountToNumber <= 0) return false;
+
+  return true;
+};
+
+export default validateRaceCount;
