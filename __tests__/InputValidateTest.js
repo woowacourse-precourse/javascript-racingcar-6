f9c4ee4b37c36c7dff80a8643cc1b7a94b5fe354
SungChul Hong <hsc6263@gmail.com>
2023-11-01 19:31:46 +0900

    feat: 테스트 코드 작성
    테스트 목록:
    
    - 입력값 유효성 테스트
    - 로직 유효성 테스트
    
    각 test case 는 test 안에 문자열로 표시함.

---
+++ __tests__/InputValidateTest.js
@@ -0,0 +1,124 @@
+import App from '../src/App.js';
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+const mockQuestions = inputs => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  });
+};
+
+const mockRandoms = numbers => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe('자동차 경주 게임 입력값 유효성 검사', () => {
+  test('입력받은 자동차 갯수가 1개인 경우', async () => {
+    // given
+    const inputs = ['car1', '1'];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.play()).rejects.toThrow('[ERROR]');
+  });
+
+  test('자동차 이름 입력값이 5자를 초과하는 경우', async () => {
+    // given
+    const inputs = ['car1, incorrect ,car3', '1'];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.play()).rejects.toThrow('[ERROR]');
+  });
+
+  test('자동차 이름 입력값이 중복되는 경우', async () => {
+    // given
+    const inputs = ['car1,car1,car3', '1'];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.play()).rejects.toThrow('[ERROR]');
+  });
+
+  test('자동차 이름 입력값이 없는 경우', async () => {
+    // given
+    const inputs = [' , car1', '1'];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.play()).rejects.toThrow('[ERROR]');
+  });
+
+  test('중간에 공백이 포함된 자동차 이름 입력값이 있는 경우', async () => {
+    // given
+    const inputs = ['car1, a car, car3', '1'];
+    const logSpy = getLogSpy();
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+    await app.play();
+
+    // then
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('a car'));
+  });
+
+  test('시도 횟수가 숫자가 아닌 경우', async () => {
+    // given
+    const inputs = ['car1,car2,car3', 'invalidType'];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.play()).rejects.toThrow('[ERROR]');
+  });
+
+  test('시도 횟수가 0인 경우', async () => {
+    // given
+    const inputs = ['car1,car2,car3', '0'];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.play()).rejects.toThrow('[ERROR]');
+  });
+
+  test('시도 횟수가 음수인 경우', async () => {
+    // given
+    const inputs = ['car1,car2,car3', '-1'];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.play()).rejects.toThrow('[ERROR]');
+  });
+});
---
+++ __tests__/LogicValidateTest.js
@@ -0,0 +1,154 @@
+import App from '../src/App.js';
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+const mockQuestions = inputs => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  });
+};
+
+const mockRandoms = numbers => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe('자동차 경주 게임 로직 유효성 검사', () => {
+  test('전진 테스트', async () => {
+    // given
+    const inputs = ['pobi,woni', '2'];
+    const randoms = [4, 0, 7, 0];
+    const outputs = ['woni : ', 'pobi : -', 'pobi : --'];
+    const logSpy = getLogSpy();
+    mockRandoms([...randoms]);
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+    await app.play();
+
+    // then
+    outputs.forEach(output => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test('우승자 테스트1', async () => {
+    // given
+    const inputs = ['pobi, woni, jin', '4'];
+    const randoms = [4, 0, 7, 0, 4, 0, 7, 0, 4, 0, 0, 0];
+    const outputs = ['최종 우승자: pobi, jin'];
+    const logSpy = getLogSpy();
+    mockRandoms([...randoms]);
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+    await app.play();
+
+    // then
+    outputs.forEach(output => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test('우승자 테스트2', async () => {
+    // given
+    const inputs = ['pobi, woni, jin', '4'];
+    const randoms = [4, 0, 7, 0, 4, 0, 7, 0, 4, 0, 7, 0];
+    const outputs = ['최종 우승자: pobi, woni, jin'];
+    const logSpy = getLogSpy();
+    mockRandoms([...randoms]);
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+    await app.play();
+
+    // then
+    outputs.forEach(output => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test('우승자 테스트3', async () => {
+    // given
+    const inputs = ['pobi, woni, jin', '4'];
+    const randoms = [4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    const outputs = ['최종 우승자: pobi'];
+    const logSpy = getLogSpy();
+    mockRandoms([...randoms]);
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+    await app.play();
+
+    // then
+    outputs.forEach(output => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test('통합 테스트1', async () => {
+    // given
+    const inputCars = ['car1, car2, car3'];
+    const inputCount = '3';
+    const randoms = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const result = [
+      '실행 결과',
+      'car1 : ',
+      'car2 : ',
+      'car3 : ',
+      'car1 : -',
+      'car2 : -',
+      'car3 : -',
+      'car1 : --',
+      'car2 : --',
+      'car3 : --',
+      '최종 우승자: car1, car2, car3',
+    ];
+    const logSpy = getLogSpy();
+    mockRandoms([...randoms]);
+    mockQuestions([...inputCars, inputCount]);
+
+    // when
+    const app = new App();
+    await app.play();
+
+    // then
+    result.forEach(output => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test('통합 테스트2', async () => {
+    // given
+    const inputCars = ['car1, car2'];
+    const inputCount = '1';
+    const randoms = [5, 3];
+    const result = ['실행 결과', 'car1 : -', 'car2 : ', '최종 우승자: car1'];
+    const logSpy = getLogSpy();
+    mockRandoms([...randoms]);
+    mockQuestions([...inputCars, inputCount]);
+
+    // when
+    const app = new App();
+    await app.play();
+
+    // then
+    result.forEach(output => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+});
