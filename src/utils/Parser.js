2d30ea8dd9d0a67f94eda9fc8736a5784667d7e5
이호연 <ho991217@kakao.com>
2023-11-01 21:00:19 +0900

    refactor: splitByComma 메서드 삭제
    stringToArray와 splitByComma는 동일한 역할을 하는 메서드이기 때문에 삭제

--- src/utils/Parser.js
+++ src/utils/Parser.js
@@ -12,7 +12,8 @@ class Parser {
    * @returns {string[]}
    */
   static stringToArray(value) {
-    const splitted = this.splitByComma(value);
+    if (UtilString.isEmptyString(value)) throw new InputError(ERROR_MESSAGE.noInput);
+    const splitted = value.split(',').map((str) => str.trim());
 
     if (UtilArray.hasEmpty(splitted)) {
       throw new InputError(ERROR_MESSAGE.noInput);
@@ -29,20 +30,6 @@ class Parser {
     if (UtilString.isEmptyString(value)) throw new InputError(ERROR_MESSAGE.noInput);
 
     return Number(value);
-  }
-
-  /**
-   * 콤마로 문자열을 분리하는 메소드.
-   *
-   * 공백은 제거됩니다.
-   *
-   * 빈 문자열이 들어올 경우 예외를 발생시킵니다.
-   * @param {string} value 콤마로 구분된 문자열
-   * @returns {string[]}
-   */
-  static splitByComma(value) {
-    if (UtilString.isEmptyString(value)) throw new InputError(ERROR_MESSAGE.noInput);
-    return value.split(',').map((str) => str.trim());
   }
 }
 
