d47bf015f62e9653863be8a75ae89691cff4ed0c
bmj0053 <bmj05378@gmail.com>
2023-11-01 20:17:43 +0900

    refactor: FindWinners 함수 리팩토링

--- src/FindWinners.js
+++ src/FindWinners.js
@@ -1,10 +1,14 @@
-export function FindWinners(forwardCounts, maxForward, carList) {
-  const winners = [];
+function CheckWinners(forwardCounts, maxForward, i, winners, carList) {
+  if (forwardCounts[i] === maxForward) {
+    winners.push(carList[i]);
+  };
+  return winners
+};
 
+export function FindWinners(forwardCounts, maxForward, carList) {
+  let winners = [];
   for (let i = 0; i < forwardCounts.length; i++) {
-    if (forwardCounts[i] === maxForward) {
-      winners.push(carList[i]);
-    };
+    winners = CheckWinners(forwardCounts, maxForward, i, winners, carList)
   };
   return winners;
-}
+};
