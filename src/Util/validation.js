bfea479cc47ebf5c923bd1502a672a1f7ef3e360
JunSeok <ppu1234@naver.com>
2023-11-01 23:25:56 +0900

    refactor: 배열 내장 메서드 사용

--- src/Util/validation.js
+++ src/Util/validation.js
@@ -2,11 +2,12 @@ import { ERROR_MESSAGE } from './message.js';
 
 const checkErrorInputName = (inputArr) => {
   if (inputArr[0] === '') throw new Error(ERROR_MESSAGE.empty);
+
   const uniqueArr = [...new Set(inputArr)];
   if (uniqueArr.length !== inputArr.length) throw new Error(ERROR_MESSAGE.duplicate);
-  for (const name of uniqueArr) {
-    if (name.length > 5) throw new Error(ERROR_MESSAGE.length);
-  }
+
+  const lengthFlag = uniqueArr.some((name) => name.length > 5);
+  if (lengthFlag) throw new Error(ERROR_MESSAGE.length);
 };
 
 const checkErrorPlayNumber = (inputNum) => {
