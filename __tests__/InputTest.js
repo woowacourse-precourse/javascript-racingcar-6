11244313b8fe984d70fe2c0dd11f4635abbe3b34
Baek <woonn97@gmail.com>
2023-11-01 18:12:27 +0900

    fix: 테스트 코드 수정

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -24,7 +24,7 @@ test("5자 이상 이름에 대한 예외처리 테스트", async () => {
   await expect(app.play()).rejects.toThrow(ERROR_MESSAGE.OVER_LENGTH_NAME);
 });
 
-test("중복 이름에 대한 예외처리 테스트", async () => {
+test("빈 값에 대한 예외처리 테스트", async () => {
   Mockfn([""]);
   const app = new App();
   await expect(app.play()).rejects.toThrow(ERROR_MESSAGE.EMPTY_NAME);
@@ -37,19 +37,19 @@ test("중복 이름에 대한 예외처리 테스트", async () => {
 });
 
 // 시도 횟수 테스트
-test("시도 횟수에 대한 예외처리 테스트", async () => {
+test("시도 횟수가 1보다 작은 값에 대한 예외처리 테스트", async () => {
   Mockfn(["baek,son", "0"]);
   const app = new App();
   await expect(app.play()).rejects.toThrow(ERROR_MESSAGE.INVALID_TRYCOUNT);
 });
 
-test("시도 횟수에 대한 예외처리 테스트", async () => {
+test("빈 값에 대한 예외처리 테스트", async () => {
   Mockfn(["baek,son", ""]);
   const app = new App();
   await expect(app.play()).rejects.toThrow(ERROR_MESSAGE.EMPTY_TRYCOUNT);
 });
 
-test("시도 횟수에 대한 예외처리 테스트", async () => {
+test("숫자가 아닌 시도 횟수에 대한 예외처리 테스트", async () => {
   Mockfn(["baek,son", "hi"]);
   const app = new App();
   await expect(app.play()).rejects.toThrow(ERROR_MESSAGE.NOT_NUMBER_TRYCOUNT);
