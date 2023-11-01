b7c18382f68a0745a292b2f1d79af9906141fb24
donggyun <donggyunkim12@gmail.com>
2023-11-01 18:58:53 +0900

    test: 1-2. 입력이 제대로 되었을때의 테스트 케이스구현

--- __tests__/UserScenarioTest.spec.js
+++ __tests__/UserScenarioTest.spec.js
@@ -1,5 +1,16 @@
+import Game from '../src/module/Game';
+import Car from '../src/module/Car';
+import { Console } from '@woowacourse/mission-utils';
 import { validateCarName } from '../src/utils/validateFn';
 import { ERROR_MESSAGE } from '../src/constant/message';
+
+const mockQuestions = (inputs) => {
+  Console.readLineAsync = jest.fn();
+
+  Console.readLineAsync.mockImplementation(() => {
+    return Promise.resolve(inputs);
+  });
+};
 
 describe('1. 자동차 이름을 입력 했을때', () => {
   test('1-1. 입력이 잘못되었을때', async () => {
@@ -23,5 +34,19 @@ describe('1. 자동차 이름을 입력 했을때', () => {
         errorMessage[i],
       );
     }
+  });
+  test('1-2. 입력이 제대로 되었을때', async () => {
+    const validateInput = 'dong,gyun,kim';
+    const expectCarList = [new Car('dong'), new Car('gyun'), new Car('kim')];
+
+    // 1-2-1. CarList 확인
+    mockQuestions(validateInput);
+
+    const game = new Game();
+    await game.init();
+    await game.createCars();
+    const { carList } = game.getCarList();
+
+    expect(carList.cars).toEqual(expectCarList);
   });
 });
