980926efa3ffc6228adce562afc6444e82991cb7
euncherry <csmo2642@naver.com>
2023-11-01 10:31:48 +0900

    feat: App.play()메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import UserInput from './UserInput.js';
+import RacingGame from './RacingGame.js';
 class App {
-  async play() {}
+  async play() {
+    const raceCarNames = await UserInput.getRaceCarNames();
+    const raceRoundCount = await UserInput.getRaceRoundCount();
+    const racingGame = new RacingGame(raceCarNames, raceRoundCount);
+
+    racingGame.start();
+
+    racingGame.calculateWinner();
+    racingGame.printWinner();
+  }
 }
 
 export default App;
