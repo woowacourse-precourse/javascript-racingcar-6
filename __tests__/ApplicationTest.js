53298a1a95a41d141858c1d7555e6f8564463322
DESKTOP-97R11IF\HWANG2 <tejaia@naver.com>
2023-11-01 21:58:45 +0900

    test: 구현한 기능 목록 테스트 추가( gamePlay()제외 )

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,5 +1,11 @@
 import App from "../src/App.js";
 import { MissionUtils } from "@woowacourse/mission-utils";
+import ErrorCheck from "../src/errorCheck.js";
+import { 
+  FORWARD_CONDITIONS,
+  INPUT_NUMBER_ERR_MSG,
+  PLAYER_NUMBER_ERR_MSG 
+} from "../src/constant.js";
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -40,11 +46,110 @@ describe("자동차 경주 게임", () => {
     const app = new App();
     await app.play();
 
-    // then
+    //then
     outputs.forEach((output) => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
     });
   });
+
+  //playerInfoInput()
+  test("입력받은 값 분리", async () => {
+    //given
+    const inputs = ["kim,lee,park"];
+    mockQuestions(inputs);
+    const app =new App();
+
+    //when
+    const result = await app.playerInfoInput();
+
+    //then
+    expect(result).toEqual(["kim", "lee", "park"]);
+  });
+
+  //playerSetting()
+  test("사용자 객체 생성 및 접근 키 생성", () => {
+    //given
+    const playerList = ["kim", "lee", "park"];
+    const app =new App();
+
+    //when
+    const { playerObject, objectKeyList } = app.playerSetting(playerList);
+
+  //then
+    expect(playerObject).toEqual({ kim: 0, lee: 0, park: 0 });
+    expect(objectKeyList).toEqual(["kim", "lee", "park"]);
+  });
+
+  //playOneRound()-success
+  test("한 라운드 실행-전진", () => {
+    // given
+    const playerObject = { kim: 0 };
+    const player = "kim";
+    const forwardNumbers = [FORWARD_CONDITIONS, FORWARD_CONDITIONS + 1];
+    const logSpy = getLogSpy();
+    mockRandoms(forwardNumbers);
+    const app = new App();
+  
+    // when
+    app.playOneRound(player, playerObject);
+  
+    // then
+    expect(playerObject[player]).toBe(1);
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining("kim : -"));
+  });
+
+  //playOneRound()-fail
+  test("한 라운드 실행-실패", () => {
+    // given
+    const notGoForward =2;
+    const playerObject = { kim: 2 };
+    const player = "kim";
+    const randoms = [notGoForward, notGoForward-1];
+    const logSpy = getLogSpy();
+    mockRandoms(randoms);
+    const app = new App();
+  
+    // when
+    app.playOneRound(player, playerObject);
+  
+    // then
+    expect(playerObject[player]).toBe(2);
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining("kim : --"));
+  });
+
+  //findWinner()
+  test("우승자 판단 및 출력", () => {
+    // given
+    const playerObject = { kim: 3, lee: 2, park: 3 };
+    const objectKeyList = ["kim", "lee", "park"];
+    const logSpy = getLogSpy();
+    const app = new App();
+  
+    // when
+    app.findWinner(playerObject, objectKeyList);
+  
+    // then
+    expect(logSpy).toHaveBeenCalledWith("최종 우승자 : kim, park");
+  });
+
+  //numberCheck()
+  test("입력이 number 타입이 아닐 때", () => {
+    //when
+    expect(() => ErrorCheck.numberCheck(Number("hello"))).toThrow(INPUT_NUMBER_ERR_MSG);
+    expect(() => ErrorCheck.numberCheck(5)).not.toThrow(INPUT_NUMBER_ERR_MSG);
+  });
+
+  //playerNumberCheck()
+  test("플레이어의 수가 2명 이하일 때", () => {
+    //given
+    const NO_ERROR_ARRAY = ["kim","lee","park"];
+    const ERROR_ARRAY = ["kim"];
+    
+    //when
+    expect(() => ErrorCheck.playerNumberCheck(ERROR_ARRAY)).toThrow(PLAYER_NUMBER_ERR_MSG);
+    expect(() => ErrorCheck.playerNumberCheck(NO_ERROR_ARRAY)).not.toThrow(PLAYER_NUMBER_ERR_MSG);
+  });
+
 
   test.each([
     [["pobi,javaji"]],
