c342e4e5219e8d7bd4c6532b3331e582e3338365
rbgksqkr <rbgks1937@naver.com>
2023-11-01 22:19:24 +0900

    test: 사용하지 않는 코드 제거 및 입력값 상수처리

--- __tests__/CarTest.js
+++ __tests__/CarTest.js
@@ -1,13 +1,5 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import CarList from "../src/CarList";
-import App from "../src/App.js";
-
-const mockQuestions = (inputs) => {
-  MissionUtils.Console.readLineAsync = jest.fn(() => {
-    const input = inputs.shift();
-    return Promise.resolve(input);
-  });
-};
 
 const mockRandoms = (numbers) => {
   MissionUtils.Random.pickNumberInRange = jest.fn();
@@ -24,19 +16,21 @@ const getLogSpy = () => {
 
 describe("CarList 메소드 테스트", () => {
   it("자동차 이름이 여러개 입력했을 때 split 되는 지 체크", () => {
+    const INPUT_DATA = "park,gyu,han";
     const carList = new CarList();
-    carList.setCarList("park,gyu,han");
+    carList.setCarList(INPUT_DATA);
     expect(carList.getCarList()[0].name).toBe("park");
   });
 
   it("1번 시도했을 때 CarList 점수를 출력", async () => {
+    const INPUT_DATA = "park,gyu";
     const random = [4, 3];
     const output = ["park : -", "gyu : "];
     const logSpy = getLogSpy();
     mockRandoms([...random]);
 
     const carList = new CarList();
-    carList.setCarList("park,gyu");
+    carList.setCarList(INPUT_DATA);
     carList.printCarListScore(1);
 
     output.forEach((element) => {
@@ -45,13 +39,14 @@ describe("CarList 메소드 테스트", () => {
   });
 
   it("1번 실행 후 최종 우승자 출력", async () => {
+    const INPUT_DATA = "park,gyu";
     const random = [3, 4];
     const output = ["최종 우승자 : gyu"];
     const logSpy = getLogSpy();
     mockRandoms([...random]);
 
     const carList = new CarList();
-    carList.setCarList("park,gyu");
+    carList.setCarList(INPUT_DATA);
     carList.printCarListScore(1);
     carList.printFinalResult();
 
