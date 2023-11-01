3b9242b77c5d6c58b97f21ad6aab9e12a16dca9d
ChoiYongWon <yongwon0824@naver.com>
2023-11-01 20:13:51 +0900

    test: 시도 횟수 input 예외처리

--- __tests__/InputValidatorTest.js
+++ __tests__/InputValidatorTest.js
@@ -8,4 +8,12 @@ describe("입력값 유효성 검사 테스트", () => {
       App.carInputValidator(input);
     }).toThrow("[ERROR] 자동차 이름은 5자 이하여야합니다.");
   });
+
+  test("시도 횟수가 숫자가 아닌 경우", () => {
+    const input = "a";
+
+    expect(() => {
+      App.attemptInputValidator(input);
+    }).toThrow("[ERROR] 숫자 형식이 아닙니다.");
+  });
 });
