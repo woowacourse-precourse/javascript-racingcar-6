1d56f91c00c8f81ff6331b8c5e8519d42a51417a
jinsil <jinsil378900@gmail.com>
2023-11-01 10:37:57 +0900

    Feat: 예외처리 추가하기

--- src/components/ValidateInput.js
+++ src/components/ValidateInput.js
@@ -9,9 +9,15 @@ export class ValidateInput {
       if (inputNameArray[i].includes(" ")) {
         throw new Error(Messages.ERROR.INVALID_NAME_NOT_SPACE);
       }
+      if (inputNameArray[i].length == 0) {
+        throw new Error(Messages.ERROR.INVALID_NAME_COMMA);
+      }
       if (inputNameArray[i].length > 5) {
         throw new Error(Messages.ERROR.INVALID_NAME_LENGTH_LIMIT);
       }
+    }
+    if (new Set(inputNameArray).size !== inputNameArray.length) {
+      throw new Error(Messages.ERROR.INVALID_NAME_NO_SAME);
     }
   }
   number(inputNumber) {
