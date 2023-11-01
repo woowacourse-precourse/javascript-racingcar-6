b40a10abc9392cd3609441f9237278604abf6d64
bl0ckp1n9 <blockp1n9@gmail.com>
2023-11-01 18:15:25 +0900

    style: formatting 수정

--- src/App.js
+++ src/App.js
@@ -7,16 +7,19 @@ class App {
     const tryCount = await input.tryCount();
     const racers = this.runRace(carNames, tryCount);
     const winnerNames = this.getWinnerNames(racers);
+
     output.winners(winnerNames);
   }
 
   runRace(carNames, tryCount) {
     output.executionResult();
+
     const racers = carNames.map(carName => new Racer(carName));
     for (let i = 0; i < tryCount; i += 1) {
       racers.forEach(racer => racer.moveForward());
       output.statesEveryIteration(racers);
     }
+
     return racers;
   }
 
@@ -24,6 +27,7 @@ class App {
     const maxMove = Math.max(...racers.map(racer => racer.move));
     const winningRacers = racers.filter(racer => racer.move === maxMove);
     const winnerNames = winningRacers.map(racer => racer.carName);
+
     return winnerNames;
   }
 }
