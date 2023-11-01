413e4bf14c944f386e2a1bf2a7ca0ff98004b3b1
swarvy <swarvy0826@naver.com>
2023-11-01 14:17:54 +0900

    test : LapNumberValidator 기능 추가에 따른 테스트 작성

--- __tests__/validator/LapNumberValidatorTest.js
+++ __tests__/validator/LapNumberValidatorTest.js
@@ -4,6 +4,8 @@ import { validateLapNumber } from '../../src/validator/index.js';
 describe('시행 횟수 유효성 검증 테스트', () => {
   const expectErrorMessage = Object.freeze({
     invalidCharactor: GRANDPRIX_ERROR_NOTIFICATION.invalidCharactor,
+    invalidNumber: GRANDPRIX_ERROR_NOTIFICATION.invalidNumber,
+    invalidInteger: GRANDPRIX_ERROR_NOTIFICATION.invalidInteger,
   });
 
   test.each([
@@ -11,6 +13,10 @@ describe('시행 횟수 유효성 검증 테스트', () => {
     { input: 'abc def', expectError: expectErrorMessage.invalidCharactor },
     { input: '123abc', expectError: expectErrorMessage.invalidCharactor },
     { input: 'abc123', expectError: expectErrorMessage.invalidCharactor },
+    { input: '1.02', expectError: expectErrorMessage.invalidNumber },
+    { input: '-19.1', expectError: expectErrorMessage.invalidNumber },
+    { input: '-19', expectError: expectErrorMessage.invalidInteger },
+    { input: '-21', expectError: expectErrorMessage.invalidInteger },
   ])('문자 입력 테스트', ({ input, expectError }) => {
     expect(() => validateLapNumber(input)).toThrow(expectError);
   });
