fcf250aa5b32bad15f7028c645d02efa15c1db75
pakxe <pigkill40@naver.com>
2023-11-01 19:42:37 +0900

    fix(RacingJudge): 동점자 발생시 제대로 처리되지 못했던 로직을 수정

--- src/model/RacingJudge.js
+++ src/model/RacingJudge.js
@@ -1,21 +1,24 @@
+import Car from './Car.js';
+
 class RacingJudge {
 	// 한번 순회할때 결정하는게 좋겠다.
 	// 오름차순으로 정렬해 주는게 좋겠다. 결과로 사용할 수 있으니
+	#maxScore = -Infinity;
+
 	getWinnerNames(drivers) {
 		const winnerNames = new Set(); // clear로 쉽게 비우기 위해 set사용함
-		let maxScore = -Infinity; //
 
-		drivers.forEach((driver) => this.#updateWinnerNames(driver, winnerNames, maxScore));
+		drivers.forEach((driver) => this.#updateWinnerNames(driver, winnerNames));
 
 		return [...winnerNames].sort();
 	}
 
-	#updateWinnerNames(driver, winnerNames, maxScore) {
-		if (driver.location > maxScore) {
+	#updateWinnerNames(driver, winnerNames) {
+		if (driver.location > this.#maxScore) {
 			winnerNames.clear();
 			winnerNames.add(driver.name);
-			maxScore = driver.location;
-		} else if (driver.location === maxScore) winnerNames.add(driver.name);
+			this.#maxScore = driver.location;
+		} else if (driver.location === this.#maxScore) winnerNames.add(driver.name);
 	}
 }
 
