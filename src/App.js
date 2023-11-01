c677281a78808bcf87dfa674e8f97a4d3d16b667
intersoom <sallybig@naver.com>
2023-11-01 22:15:18 +0900

    feat: 시도할 횟수 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import Car from "./Car.js";
+import Car from "./class/Car.js";
 import Input from "./Input.js";
+import Output from "./Output.js";
+import Progress from "./Progress.js";
 import Validation from "./Validation.js";
 
 class App {
@@ -8,8 +10,10 @@ class App {
   async play() {
     const carNamesInput = await Input.carNames();
     Validation.carNames(carNamesInput);
-
     this.cars = carNamesInput.split(",").map((carName) => new Car(carName));
+
+    const tryCountInput = await Input.tryCount();
+    Validation.tryCount(tryCountInput);
   }
 }
 
