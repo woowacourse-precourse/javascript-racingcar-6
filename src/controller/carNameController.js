ee9605ca29c4f47c313b5fe6dd904862d429d3a6
idohyeon <9598dohyun@naver.com>
2023-11-01 22:08:09 +0900

    ref: 비동기함수 오류수정

--- src/controller/carNameController.js
+++ src/controller/carNameController.js
@@ -25,7 +25,6 @@ class CarName {
         });
         new Play().inputRaceTimes(CAR_LIST);
     }
-
     // 게임 시작 메서드
     async start() {
         MissionUtils.Console.print('자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분합니다.');
