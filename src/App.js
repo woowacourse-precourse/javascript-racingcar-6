e87faa59af336d6d4a80b7a58667d151add529f7
cyj <cyd5538@naver.com>
2023-11-01 20:31:58 +0900

    Refactor: print console  edit
    경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분 이 나오고 줄바꿈해서 입력하게 수정

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ class App {
   }
   
   async getCarNames() {
-    const CAR_NAMES = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): ")
+    await Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    const CAR_NAMES = await Console.readLineAsync("")
     this.validateCarNames(CAR_NAMES)
   }
 
