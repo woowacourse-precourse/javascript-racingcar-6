04973e81467a4acc0d19dab2c9fb8a2b8178d268
이황제 <happy_kaiser@hotmail.com>
2023-11-01 18:07:49 +0900

    feat:차 이름 중복 에러

--- src/domain/inputView.js
+++ src/domain/inputView.js
@@ -11,4 +11,7 @@ export const getCarName = async () => {
   let carNameArray = [];
   const CAR_NAME = await MissionUtils.Console.readLineAsync(TEXT.GET_CAR_NAMES);
   userInputCarNameFormEror(CAR_NAME);
+  CAR_NAME_ARRAY = stringToArray(CAR_NAME);
+  userInputDuplicatedError(CAR_NAME_ARRAY);
+  await getTryNumber(CAR_NAME_ARRAY);
 };
