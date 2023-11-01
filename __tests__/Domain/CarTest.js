5307c380f946c78ddb9f9ce88a8309299eb8c970
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-11-01 22:16:42 +0900

    refactor: CarTest 중복로직 제거 및 네이밍 개선

--- __tests__/Domain/CarTest.js
+++ __tests__/Domain/CarTest.js
@@ -1,56 +1,64 @@
 import Car from '../../src/Domain/Car';
+import CAR from '../../src/constants/car';
 
 describe('Car 테스트', () => {
   const validName = ['pobi', 'crong', 'honux'];
   const invalidName = ['pengooseDev', 'thisIsTooLong'];
+  let car;
+
+  beforeEach(() => {
+    // 각 테스트 전에 새로운 Car 인스턴스 생성
+    car = new Car('pobi');
+  });
 
   describe('자신의 이름(name)과 현재 위치(position)를 생성자에서 초기화한다.', () => {
     validName.forEach((name) => {
       test(`${name} : 오빠 차 뽑았다`, () => {
         // when
-        const car = new Car(name);
+        const namedCar = new Car(name);
 
         // then
-        expect(car.getName()).toBe(name);
+        expect(namedCar.getName()).toBe(name);
       });
 
       test('기본 위치는 0이다.', () => {
-        const car = new Car('pobi');
+        const initialPosition = car.getPosition();
 
-        expect(car.getPosition()).toBe(0);
+        expect(initialPosition).toBe(0);
       });
     });
 
-    invalidName.forEach((name) => {
-      test(`자동차 이름이 5자를 초과하는 경우 예외 처리를 진행한다. (${name})`, () => {
+    invalidName.forEach((tooLongName) => {
+      test(`자동차 이름이 5자를 초과하는 경우 예외 처리를 진행한다. (${tooLongName})`, () => {
         // when
-        const createCar = () => new Car(name);
+        const createInvalidCar = () => new Car(tooLongName);
 
         // then
-        expect(createCar).toThrow();
+        expect(createInvalidCar).toThrow();
       });
     });
   });
 
   describe('전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다', () => {
+    // given
     const shouldMoveCases = [4, 5, 6, 7, 8, 9];
     const shouldNotMoveCases = [0, 1, 2, 3];
 
     shouldMoveCases.forEach((number) => {
-      test('4 이상의 숫자가 주어지면 움직인다.', () => {
-        const car = new Car('pobi');
+      test(`4 이상의 숫자가 주어지면 전진한다(${number})`, () => {
+        const initialPosition = car.getPosition();
         car.move(number);
 
-        expect(car.getPosition()).toBe(1);
+        expect(car.getPosition()).toBe(initialPosition + CAR.movement.unit);
       });
     });
 
     shouldNotMoveCases.forEach((number) => {
-      test('4 미만의 숫자가 주어지면 움직이지 않는다.', () => {
-        const car = new Car('pobi');
+      test(`4 미만의 숫자가 주어지면 정지한다(${number})`, () => {
+        const initialPosition = car.getPosition();
         car.move(number);
 
-        expect(car.getPosition()).toBe(0);
+        expect(car.getPosition()).toBe(initialPosition);
       });
     });
   });
