8bf6e2aaf2551a263f429179e9902341e9b5ba62
future4ing <mirae4120@gmail.com>
2023-11-01 18:42:22 +0900

    feat : 우승자 선정 및 출력 기능 구현
    - 자동차 중 가장 멀리 이동한 자동차를 우승자로 선정
    - 우승자가 여러 명일 경우 쉼표로 구분
    - 우승자 출력

--- src/Race.js
+++ src/Race.js
@@ -1,3 +1,6 @@
+import { Console } from '@woowacourse/mission-utils';
+
+
 class Race {
   constructor(cars, round) {
     this.round = round;
@@ -5,17 +8,37 @@ class Race {
   }
 
   start() {
-    
-    for (let i = 0; i < this.round; i+= 1) {
+    Console.print('\n실행 결과');
+    for (let i = 0; i < this.round; i += 1) {
       this.playRound();
+      this.printRoundResult();
     }
-    
+    Console.print(`최종 우승자 : ${this.checkWinners()}`);
   }
 
   playRound() {
     this.cars.forEach((car) => {
       car.move();
     });
+  }
+
+  printRoundResult() {
+    this.cars.forEach((car) => {
+        const moveCountStr = '-'.repeat(car.getMoveCount());
+        Console.print(`${car.getName()} : ${moveCountStr}`);
+    });
+    Console.print('');
+  }
+
+  checkWinners() {
+    const moveCountArr = this.cars.map((car) => car.getMoveCount());
+    const maxMoveCount = Math.max(...moveCountArr);
+
+    const winners = this.cars
+      .filter((car) => car.getMoveCount() === maxMoveCount)
+      .map((car) => car.getName());
+
+    return winners.join(', ');
   }
 }
 
