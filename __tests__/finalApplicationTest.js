402a2088533134237fcde8fa5b26d8751ce64a75
heonq <gkagjsrb@naver.com>
2023-11-01 19:39:39 +0900

    test(*): 최종 애플리케이션 테스트 코드 수정 - 가독성을 위해 자동차 갯수와 시도 횟수 축소 - 단독 우승에 대한 테스트 추가

--- __tests__/finalApplicationTest.js
+++ __tests__/finalApplicationTest.js
@@ -1,5 +1,6 @@
 import App from '../src/App.js';
 import {MissionUtils} from '@woowacourse/mission-utils';
+import CONSTANTS from '../utils/Constants.js';
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -23,18 +24,34 @@ const getLogSpy = () => {
   return logSpy;
 };
 
+const inputs = [
+  ['a,b', '2'],
+  ['a,b', '2'],
+];
+const randoms = [
+  [
+    CONSTANTS.progressValue - 1,
+    CONSTANTS.progressValue,
+    CONSTANTS.progressValue,
+    CONSTANTS.progressValue - 1,
+  ],
+  [
+    CONSTANTS.progressValue - 1,
+    CONSTANTS.progressValue,
+    CONSTANTS.progressValue,
+    CONSTANTS.progressValue,
+  ],
+];
+const outputs = [
+  ['a : ', 'b : -', 'a : -', 'b : -', '최종 우승자 : a, b'],
+  ['a : ', 'b : -', 'a : -', 'b : --', '최종 우승자 : b'],
+];
+
 describe('자동차 경주 게임', () => {
-  test('공동 우승자가 나오는 경우에 대한 테스트', async () => {
-    const inputs = ['a,b,c,d', '3'];
-    const firstTrial = [4, 4, 4, 4];
-    const secondTrial = [3, 4, 4, 4];
-    const thirdTrial = [3, 3, 3, 3];
-    const randoms = [...firstTrial, ...secondTrial, ...thirdTrial];
-    const firstResult = '\n실행 결과\na : -\nb : -\nc : -\nd : -\n\n';
-    const secondResult = 'a : -\nb : --\nc : --\nd : --\n\n';
-    const thirdResult = 'a : -\nb : --\nc : --\nd : --\n\n';
-    const winner = '최종 우승자 : b, c, d';
-    const outputs = [firstResult, secondResult, thirdResult, winner];
+  test.each([
+    [inputs[0], randoms[0], outputs[0]],
+    [inputs[1], randoms[1], outputs[1]],
+  ])('공동 우승과 단독 우승 테스트', async (inputs, randoms, outputs) => {
     const logSpy = getLogSpy();
 
     mockQuestions(inputs);
