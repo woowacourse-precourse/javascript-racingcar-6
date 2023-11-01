aaf1f44249ebb2a44b522e6bb229c1bc6de47d7a
mywhzy <cmj2393a@naver.com>
2023-11-01 17:33:32 +0900

    feat: car 클래스 형태 변경 - 생성자 추가

--- src/Car.js
+++ src/Car.js
@@ -1,16 +1,15 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
 export default class Car {
-  setCarInfo(carName) {
-    const carInfo = { name: carName, forwardCount: 0 };
-    return carInfo;
+  constructor(carName) {
+    this.name = carName;
+    this.forwardCount = 0;
   }
 
-  moveCar(carName, carInfoList) {
+  moveCar() {
     const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
-    const currentCar = carInfoList.find((car) => car.name === carName);
     if (randomNumber > 3) {
-      currentCar.forwardCount += 1;
+      this.forwardCount += 1;
     }
   }
 }
