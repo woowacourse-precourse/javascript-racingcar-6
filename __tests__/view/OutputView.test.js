8f9f8671e92916f69d104cfb15983465c4208d7e
이태훈 <rhymint@gmail.com>
2023-11-01 16:15:39 +0900

    Revert "test : 테스트코드에 mockClear() 추가"
    This reverts commit 7b25af7d65dd99afe672e0d7d62c1e2dfedce14f.

--- __tests__/view/OutputView.test.js
+++ __tests__/view/OutputView.test.js
@@ -4,34 +4,26 @@ import { CONSOLE_MESSAGE } from "../../src/constants/ConsoleMessages.js";
 import RacingCar from "../../src/model/RacingCar.js";
 
 const outputView = new OutputView();
-const getSpy = () => {
-  const spy = jest.spyOn(MissionUtils.Console, 'print');
-  spy.mockClear();
-
-  return spy;
-}
+const spy = jest.spyOn(MissionUtils.Console, 'print');
 
 describe('outputView test', () => {
   test('func printQuery', () => {
-    const spy = getSpy();
-
     outputView.printQuery('test');
+
     expect(spy).toHaveBeenCalledWith(expect.stringContaining('test'));
   });
 
   test('func printRacingCarState', () => {
     const mockCar = new RacingCar('test');
-    const spy = getSpy();
-    
     outputView.printRacingCarState(mockCar);
+
     expect(spy).toHaveBeenCalledWith(expect.stringContaining('test : '));
   });
 
   test('func outputFinalResult', () => {
     const input = 'kim, park';
-    const spy = getSpy();
-    
     outputView.outputFinalResult(input);
+
     expect(spy).toHaveBeenCalledWith(expect.stringContaining(`${CONSOLE_MESSAGE.outputFinalResult} : ${input}`));
   });
 });
