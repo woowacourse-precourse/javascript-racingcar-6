fd6c2d543882d49e5782652ecddc57afa241f5de
mingi1123 <min_gi1123@naver.com>
2023-11-01 17:27:05 +0900

    feat: 이동 횟수 입력 함수

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App {
     ErrorHandler.checkCarNames(carNames);
     const carNameArray = carNames.split(',');
     const cars = carNameArray.map((carName) => new Car(carName));
+    const moveCount = await this.inputMoveCount();
 
     Console.print(cars);
   }
@@ -17,6 +18,11 @@ class App {
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
     );
     return car_names;
+  }
+
+  async inputMoveCount() {
+    const moveCount = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    return moveCount;
   }
 }
 
