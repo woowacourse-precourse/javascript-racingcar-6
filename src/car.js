e32ec7c2f6623132193f23d4ca3034c33571e160
yunminwon <wymm20@naver.com>
2023-11-01 14:51:36 +0900

    기능 구현

---
+++ src/car.js
@@ -0,0 +1,22 @@
+export default class Car {
+    constructor(name) {
+      this.name = name;
+      this.move = 0;
+    }
+  
+    isMove() {
+      const randomNum = MissionUtils.Random.pickNumberInRange(0, 9);
+      if (randomNum >= 4) {
+        this.move += 1;
+      }
+    }
+  
+    printCurrentMovement() {
+      let result = `${this.name}: `;
+      let i;
+      for (i = 0; i < this.move; i += 1) {
+        result += '-';
+      }
+      return result;
+    }
+  }
---
+++ src/index.js
@@ -0,0 +1,86 @@
+import Car from './Car.js';
+import checkCarName from './checkCarName.js';
+import Layout from './Layout.js';
+
+export default class RacingCarGame {
+  constructor() {
+    this.layout = new Layout();
+    document.getElementById('car-names-submit').onclick = (
+      this.onCarNameInputSubmit.bind(this)
+    );
+    document.getElementById('racing-count-submit').onclick = (
+      this.onCountInputSubmit.bind(this)
+    );
+  }
+
+  onCarNameInputSubmit(e) {
+    e.preventDefault();
+    const inputCarNames = Layout.inputCarName();
+    this.carName = inputCarNames.split(',');
+    if (!checkCarName(this.carName)) {
+      alert('자동차 이름 입력이 올바르지 않습니다. 다시 입력해주세요.');
+      this.layout.makeHiddenCountAndResultSection();
+    } else {
+      this.layout.makeVisibleCountSection();
+    }
+  }
+
+  onCountInputSubmit(e) {
+    e.preventDefault();
+    const inputCountString = Layout.inputCountString();
+    this.inputCount = Number(inputCountString);
+    if (inputCountString.length === 0 || this.inputCount < 1) {
+      alert(
+        '시도할 횟수 입력이 올바르지 않습니다. 1 이상의 숫자로 입력해주세요.'
+      );
+      this.layout.makeVisibleCountSection();
+    } else {
+      this.startRacingCarGame();
+    }
+  }
+
+  startRacingCarGame() {
+    const car = this.carName.map((name) => new Car(name));
+    let i;
+    let moveResult = '';
+    for (i = 0; i < this.inputCount; i += 1) {
+      moveResult += this.constructor.moveCar(car);
+    }
+    this.layout.printMoveResult(moveResult);
+    const winner = this.constructor.findWinner(car);
+    this.printResult(winner);
+  }
+
+  static moveCar(car) {
+    let moveResult = '';
+    car.forEach((eachCar) => {
+      eachCar.isMove();
+      moveResult += `${eachCar.printCurrentMovement()}<br />`;
+    });
+    moveResult += '<br />';
+    return moveResult;
+  }
+
+  static findWinner(car) {
+    let winner = '';
+    let winnerMove = 0;
+    car.forEach((eachCar) => {
+      const eachCarName = eachCar.name;
+      const eachCarMove = eachCar.move;
+      if (winner.length === 0 || winnerMove < eachCarMove) {
+        winner = `${eachCarName}`;
+        winnerMove = eachCarMove;
+      } else if (winnerMove === eachCarMove) {
+        winner += `, ${eachCarName}`;
+      }
+    });
+    return winner;
+  }
+
+  printResult(winner) {
+    this.layout.makeVisibleResultSection();
+    this.layout.printWinnerResult(winner);
+  }
+}
+
+const player = new RacingCarGame();
