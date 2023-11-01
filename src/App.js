70643be15800c2c626dd36043d97d2a38fae2c81
Hys-Lee <dltkdgus3275@gmail.com>
2023-11-01 20:29:58 +0900

    Feat(processTrialInput): make a handler for trial number from input
    add, also, exceptions for valid trial number.
    add, also, function for get input from user.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { seperateCarNames, processTrialInput } from '../utils/inputHandling.js';
+
 class App {
-  async play() {}
+  carNamesArray;
+  #trialNum;
+  async init() {
+    this.carNamesArray = seperateCarNames(
+      await MissionUtils.Console.readLineAsync(
+        '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+      ),
+    );
+    this.#trialNum = processTrialInput(
+      await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n'),
+    );
+  }
+  constructor() {
+    this.carNamesArray = [];
+    this.#trialNum = 0;
+  }
+  async play() {
+    await this.init();
+  }
 }
 
 export default App;
