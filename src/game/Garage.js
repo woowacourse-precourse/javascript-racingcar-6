1e2bfe4ad29934831658842e7547f3e587aff0ee
ImxYJL <allensain14@gmail.com>
2023-11-01 16:39:10 +0900

    feat: Garage 클래스 필드, 생성자 작성

--- src/game/Garage.js
+++ src/game/Garage.js
@@ -1,6 +1,16 @@
 import Car from './Car.js';
 
 class Garage {
+  #carList = [];
 
+  constructor(nameList) {
+    nameList.forEach((name) => {
+      this.#registerCar(name);
+    });
+  }
+
+  #registerCar(name) {
+
+  }
 }
 export default Garage;
