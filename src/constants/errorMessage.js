4029a8e7bfde0ad2c4e7091881e30761bfac8cd8
yu-heejin <jkgt0521@naver.com>
2023-11-01 19:47:17 +0900

    feat(User.js): 입력 값에 대한 예외처리 추가

---
+++ src/constants/errorMessage.js
@@ -0,0 +1,4 @@
+export const ERROR_MESSAGE = {
+    carNameLengthOutOf5Exception: '[ERROR] 이름은 5자 이내로 제한합니다.',
+    numberFormatException: '[ERROR] 숫자만 입력해주세요.',
+};
--- src/modules/User.js
+++ src/modules/User.js
@@ -1,3 +1,4 @@
+import { ERROR_MESSAGE } from '../constants/errorMessage.js';
 import { GAME_MESSAGE } from '../constants/gameMessage.js';
 import { MissionUtils } from '@woowacourse/mission-utils';
 
@@ -5,14 +6,34 @@ export default class User {
 
     async inputCarNames() {
         const carNames = await MissionUtils.Console.readLineAsync(GAME_MESSAGE.inputCarNames);
+        
         return this.splitCarNames(carNames);
     }
 
-    async inputGameCount() {
-        return await MissionUtils.Console.readLineAsync(GAME_MESSAGE.inputGameCount);
+    splitCarNames(carNames) {
+        this.checkInputCarNamesError(carNames.split(','));
+
+        return carNames.split(',');
     }
 
-    splitCarNames(carNames) {
-        return carNames.split(',');
+    checkInputCarNamesError(carNames) {
+        carNames.forEach(name => {
+            if (name.length > 5) {
+                throw new Error(ERROR_MESSAGE.carNameLengthOutOf5Exception);
+            }
+        });
+    }
+
+    async inputGameCount() {
+        const count = await MissionUtils.Console.readLineAsync(GAME_MESSAGE.inputGameCount);
+        this.checkInputGameCountError(count);
+
+        return count;
+    }
+
+    checkInputGameCountError(count) {
+        if (Number.isNaN(count)) {
+            throw new Error(ERROR_MESSAGE.numberFormatException);
+        }
     }
 }
