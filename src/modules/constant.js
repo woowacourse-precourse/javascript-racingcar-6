1f827c9543dcd082dd2091b6c82ee8a4870e76c7
hs <tlsgustn93@naver.com>
2023-11-01 15:17:20 +0900

    fix: constant 수정

--- src/modules/constant.js
+++ src/modules/constant.js
@@ -1,5 +1,6 @@
 const GAME_MESSAGE = {
-  GAME_START: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)",
+  GAME_START:
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
   TRY_INPUT: "시도할 횟수는 몇 회인가요?\n",
   GAME_RESULT: "\n실행 결과",
   DASH: "-",
