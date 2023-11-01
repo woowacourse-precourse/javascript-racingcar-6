0c4e9348c2cccd52c319fbd32445820d589d0382
jueun-park98 <jooeun06161@gmail.com>
2023-11-01 16:36:56 +0900

    refactor: Validator.js 객체 안의 문자열 변수를 소문자로 변경

--- src/Controller/Validator.js
+++ src/Controller/Validator.js
@@ -6,18 +6,18 @@ const LEAST_ROUND_NUMBER = 1;
 const ALPHABETIC_REGULAR_EXPRESSION = /^[a-zA-Z]+$/;
 
 const NAME_ERROR_MESSAGE = {
-  EMPTY_NAME: '[ERROR] 입력값에 이름을 적지 않았거나 공백을 적었습니다.',
-  CONTAINS_NUMBER: '[ERROR] 입력값에 숫자가 포함되어 있습니다.',
-  INVALID_CHARACTER: '[ERROR] 입력값에 쉼표와 알파벳을 제외한 문자가 들어있습니다.',
-  LENGTH_ERROR: '[ERROR] 입력한 이름의 길이가 5자를 넘어갑니다.',
-  LESS_THAN_TWO_NAMES: '[ERROR] 두 개 이상의 이름이 입력되지 않았습니다.',
+  empty_name: '[ERROR] 입력값에 이름을 적지 않았거나 공백을 적었습니다.',
+  contains_number: '[ERROR] 입력값에 숫자가 포함되어 있습니다.',
+  invalid_character: '[ERROR] 입력값에 쉼표와 알파벳을 제외한 문자가 들어있습니다.',
+  length_error: '[ERROR] 입력한 이름의 길이가 5자를 넘어갑니다.',
+  less_than_two_names: '[ERROR] 두 개 이상의 이름이 입력되지 않았습니다.',
 }
 
 const ROUND_ERROR_MESSAGE = {
-  EMPTY_VALUE: '[ERROR] 입력값에 숫자를 적지 않았거나 공백을 적었습니다.',
-  NOT_NUMERIC: '[ERROR] 입력값에 숫자를 제외한 문자가 포함되어 있습니다.',
-  NOT_INTEGER: '[ERROR] 입력값이 정수가 아닙니다.',
-  LESS_THAN_ONE: '[ERROR] 입력값이 0이거나 음수입니다.',
+  empty_value: '[ERROR] 입력값에 숫자를 적지 않았거나 공백을 적었습니다.',
+  not_numeric: '[ERROR] 입력값에 숫자를 제외한 문자가 포함되어 있습니다.',
+  not_integer: '[ERROR] 입력값이 정수가 아닙니다.',
+  less_than_one: '[ERROR] 입력값이 0이거나 음수입니다.',
 }
 
 const Validator = {
@@ -38,7 +38,7 @@ const Validator = {
 
 const validateNameCounts = (names) => {
   if (names.length < LEAST_NAME_COUNTS) {
-    throw NAME_ERROR_MESSAGE.LESS_THAN_TWO_NAMES;
+    throw NAME_ERROR_MESSAGE.less_than_two_names;
   }
 }
 
@@ -51,49 +51,49 @@ const validateName = (name) => {
 
 const validateEmptyName = (string) => {
   if (isEmpty(string)) {
-    throw NAME_ERROR_MESSAGE.EMPTY_NAME;
+    throw NAME_ERROR_MESSAGE.empty_name;
   }
 }
 
 const validateEmptyRound = (string) => {
   if (isEmpty(string)) {
-    throw ROUND_ERROR_MESSAGE.EMPTY_VALUE;
+    throw ROUND_ERROR_MESSAGE.empty_value;
   }
 }
 
 const validateConatinsNumber = (name) => {
   if (isNumeric(name)) {
-    throw NAME_ERROR_MESSAGE.CONTAINS_NUMBER;
+    throw NAME_ERROR_MESSAGE.contains_number;
   }
 }
 
 const validateInvalidCharacter = (name) => {
   if (hasSpecialCharacter(name)) {
-    throw NAME_ERROR_MESSAGE.INVALID_CHARACTER;
+    throw NAME_ERROR_MESSAGE.invalid_character;
   }
 }
 
 const validateLength = (name) => {
   if (!hasCorrectLength(name)) {
-    throw NAME_ERROR_MESSAGE.LENGTH_ERROR;
+    throw NAME_ERROR_MESSAGE.length_error;
   }
 }
 
 const validateNumeric = (number) => {
   if (!isNumeric(number)) {
-    throw ROUND_ERROR_MESSAGE.NOT_NUMERIC;
+    throw ROUND_ERROR_MESSAGE.not_numeric;
   }
 }
 
 const validateInteger = (number) => {
   if (!isInteger(number)) {
-    throw ROUND_ERROR_MESSAGE.NOT_INTEGER;
+    throw ROUND_ERROR_MESSAGE.not_integer;
   }
 }
 
 const validateCorrectNumber = (number) => {
   if (isLessThanOne(number)) {
-    throw ROUND_ERROR_MESSAGE.LESS_THAN_ONE;
+    throw ROUND_ERROR_MESSAGE.less_than_one;
   }
 }
 
