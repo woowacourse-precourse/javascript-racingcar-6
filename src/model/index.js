83b0d1b39e1ed4b04cebfe702b01201516eb644e
ryan-dia <cjfdnjs158@naver.com>
2023-11-01 11:16:43 +0900

    refactor(model): canMove 메서드 static 제거 및 private 키워드 추가
    canMove를 내부에서만 사용하기 때문에 static 제거후 private 추가
    eslint 해당 라인만 무시하도록 주석 및 설명 추가

--- src/model/index.js
+++ src/model/index.js
@@ -2,7 +2,9 @@ import { SYSTEM } from '../constants/System.js';
 import RandomNumberGenerator from './RandomNumberGenerator.js';
 
 class RacingModel {
-  static canMove() {
+  // 오직 내부에서만 사용하고 private 메서드이기 때문에 static을 사용하지 않음
+  // eslint-disable-next-line class-methods-use-this
+  #canMove() {
     const randomNumber = RandomNumberGenerator.run();
     return randomNumber >= SYSTEM.canMoveNumber;
   }
