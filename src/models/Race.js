dd6e2b93704fc738c71c50f95f3723c21f686183
Jinhyo Park <vkstk0131@yonsei.ac.kr>
2023-11-01 17:47:01 +0900

    style(models): 모든 줄의 indent size를 2로 수정
    수정 파일:
    - Race.js: indent size 수정
    - Car.js: indent size 수정

--- src/models/Race.js
+++ src/models/Race.js
@@ -4,45 +4,45 @@
  * @property {number} totalRound - 총 시도 횟수
  */
 class Race {
-    /** @type {object} */
-    #racingCars;
+  /** @type {object} */
+  #racingCars;
 
-    /** @type {number} */
-    #totalRound;
+  /** @type {number} */
+  #totalRound;
 
-    /**
-     * 생성자
-     * @param {object} racingCars - 참가 자동차 목록
-     * @param {number} totalRound - 총 시도 횟수
-     */
-    constructor(racingCars = [], totalRound = 0) {
-        this.#racingCars = racingCars;
-        this.#totalRound = totalRound;
-    }
+  /**
+   * 생성자
+   * @param {object} racingCars - 참가 자동차 목록
+   * @param {number} totalRound - 총 시도 횟수
+   */
+  constructor(racingCars = [], totalRound = 0) {
+    this.#racingCars = racingCars;
+    this.#totalRound = totalRound;
+  }
 
-    /**
-     * 참가 자동차 목록 반환
-     * @returns {object} 참가 자동차 목록
-     */
-    getracingCars() {
-        return this.#racingCars;
-    }
+  /**
+   * 참가 자동차 목록 반환
+   * @returns {object} 참가 자동차 목록
+   */
+  getracingCars() {
+    return this.#racingCars;
+  }
 
-    /**
-     * 총 시도 횟수 반환
-     * @returns {number} 총 시도 횟수
-     */
-    getTotalRound() {
-        return this.#totalRound;
-    }
+  /**
+   * 총 시도 횟수 반환
+   * @returns {number} 총 시도 횟수
+   */
+  getTotalRound() {
+    return this.#totalRound;
+  }
 
-    /**
-     * 참가 자동차 목록에 자동차 추가
-     * @param {object} car - 참가 자동차
-     */
-    addRacingCar(car) {
-        this.#racingCars.push(car);
-    }
+  /**
+   * 참가 자동차 목록에 자동차 추가
+   * @param {object} car - 참가 자동차
+   */
+  addRacingCar(car) {
+    this.#racingCars.push(car);
+  }
 }
 
 export default Race;
