24ae0df0bf5e91cc40839aaf8d6ac1de72c3fbfb
youjeong Jeong <wjddbwjd1205@naver.com>
2023-11-01 19:39:31 +0900

    refactor: @woowacourse/mission-utils에서 Random으로 바로 사용가능하도록 리펙터링

--- src/Car.js
+++ src/Car.js
@@ -1,4 +1,4 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { Random } from "@woowacourse/mission-utils";
 
 class Car {
     constructor(carName) {
@@ -6,7 +6,7 @@ class Car {
     }
 
     decideCarAction() {
-        let condition = MissionUtils.Random.pickNumberInRange(0, 9);
+        let condition = Random.pickNumberInRange(0, 9);
         if(condition >= 4) return true; // 전진
         else return false; // 멈춤
     }
