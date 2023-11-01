55e6cf3e87ae1ca8b74f152145aa762b5b1ad469
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-11-01 23:14:42 +0900

    refactor: Validator 테스트 is 메서드 중복로직 제거

--- __tests__/utils/ValidatorTest.js
+++ __tests__/utils/ValidatorTest.js
@@ -3,7 +3,7 @@ import RACING_GAME from '../../src/constants/racingGame';
 import CustomError from '../../src/errors/error';
 
 describe('Validator 테스트', () => {
-  describe('is는 boolean을 반환한다.', () => {
+  describe('is로 시작하는 메서드는 boolean을 반환한다.', () => {
     describe('isEmptyString()', () => {
       // given
       const cases = [
@@ -29,42 +29,47 @@ describe('Validator 테스트', () => {
     });
 
     describe('isValidCarNameLength()', () => {
-      test('자동차 이름의 길이가 유효하면 true를 반환해야 한다.', () => {
-        // given
-        const validCarName = 'pobi';
+      // 자동차의 이름은 1자 이상 5자 이하만 가능하다.
 
-        // when
-        const result = Validator.isValidCarNameLength(validCarName);
+      // given
+      const cases = [
+        {
+          input: 'pobi',
+          expected: true,
+          description: '정상적으로 생성되는 경우',
+        },
+        {
+          input: 'crong',
+          expected: true,
+          description: '정상적으로 생성되는 경우',
+        },
+        {
+          input: '',
+          expected: false,
+          description: '길이가 0인 경우',
+        },
+        {
+          input: 'pengoose',
+          expected: false,
+          description: '길이가 5를 초과하는 경우',
+        },
+      ];
 
-        // then
-        expect(result).toBe(true);
-      });
+      const isValidLength = (name) => Validator.isValidCarNameLength(name);
 
-      test('자동차 이름의 길이가 0자인 경우, false를 반환한다.', () => {
-        // given
-        const invalidCarName = '';
+      cases.forEach(({ input, expected, description }) => {
+        test(`${description}, ${expected}를 반환한다.`, () => {
+          // when
+          const result = isValidLength(input);
 
-        // when
-        const result = Validator.isValidCarNameLength(invalidCarName);
-
-        // then
-        expect(result).toBe(false);
-      });
-
-      test('자동차 이름의 길이가 5자를 넘는 경우, false를 반환한다.', () => {
-        // given
-        const invalidCarName = 'pengooseDevIsTooLong';
-
-        // when
-        const result = Validator.isValidCarNameLength(invalidCarName);
-
-        // then
-        expect(result).toBe(false);
+          // then
+          expect(result).toBe(expected);
+        });
       });
     });
   });
 
-  describe('throw는 예외를 던진다.', () => {
+  describe('validate로 시작하는 메서드는 CustomError를 던진다.', () => {
     describe('validateDuplicatedCarName()', () => {
       test('중복된 자동차 이름이 있으면 예외를 던져야 한다.', () => {
         // given
@@ -89,7 +94,7 @@ describe('Validator 테스트', () => {
         };
 
         // then
-        expect(() => validateDuplicatedCarName).not.toThrow();
+        expect(validateDuplicatedCarName).not.toThrow();
       });
     });
 
