12fb25280915500f83d2be29fdf58fa42ba7de15
HyeseonLee <hyesun1999@gmail.com>
2023-11-01 16:43:33 +0900

    feat: 에러 메세지, 게임 진행시 사용되는 상수, 프롬프트 메세지를 상수로 정리

---
+++ src/constatns/gameConstant.js
@@ -0,0 +1,16 @@
+export const CONSTANTS = {
+    MIN_VALUE : 0,
+    MAX_VALUE : 9,
+    GO_FORWAD : 4,
+}
+
+export const PLAYER_INPUT_MESSAGE = {
+    'CAR_NAME_PROMPT' : '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+    'ROUND_PLAY_NUMBER_PROMPT' : '시도할 회수는 몇 회인가요?',
+}
+
+export const GAME_MESSAGE = {
+    'PROGRESS_ICON' : '-',
+    'ROUND_START' : '실행 결과',
+    'WINNER_MESSAGE' : '최종 우승자 : ',
+}
