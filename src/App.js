5429afe5477dac61e33718f668c47730d3c9f993
gaeng <78129949+hyeonkyeong31@users.noreply.github.com>
2023-11-01 18:25:19 +0900

    feat : 경주를 시도할 횟수 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -4,16 +4,26 @@ class App {
   async play() {
     try {
       const CAR_NAMES = await this.getCarNames();
+      const TRY_COUNT = await this.getTryCount();
     } catch (error) {
       MissionUtils.Console.print(error.message);
     }
   }
+
   async getCarNames() {
     const CAR_NAMES_INPUT = await MissionUtils.Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): "
     );
     const CAR_NAMES = CAR_NAMES_INPUT.split(",");
     return CAR_NAMES;
+  }
+
+  async getTryCount() {
+    const TRY_COUNT_INPUT = await MissionUtils.Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요? "
+    );
+    const TRY_COUNT = parseInt(TRY_COUNT_INPUT);
+    return TRY_COUNT;
   }
 }
 
