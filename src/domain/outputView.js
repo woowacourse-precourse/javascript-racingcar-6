6e0902e06331c35bf22a2d43688b1f0a6ec7b317
이황제 <happy_kaiser@hotmail.com>
2023-11-01 16:21:04 +0900

    feat: 실행 결과 출력

---
+++ src/domain/outputView.js
@@ -0,0 +1,20 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import {
+  finalWinnerResultSort,
+  makeEmptyArray,
+  makeRandomNumberTest,
+  resultJudge,
+} from "./controller.js";
+import {
+  racingProgressPrint,
+  winnerIsAllPrint,
+  oneWinnerPrint,
+  winnerIsManyPrint,
+} from "./print.js";
+import { TEXT } from "../contants/contants.js";
+
+export const racingProgress = async (carNameArray, tryNumber) => {
+  const RESULT = [];
+  let countArr = [];
+  makeEmptyArray(carNameArray, countArr);
+};
