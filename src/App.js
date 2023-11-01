6da757eed8b4ed933def58ffafd3450cae904d59
이동주 <leednj0113@naver.com>
2023-11-01 12:23:30 +0900

    feat: 경주할 자동차 이름 입력

--- src/App.js
+++ src/App.js
@@ -7,6 +7,30 @@ class App {
     this.tryCount = 0;
   }
 
+  async getCarNames() {
+    const input = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+
+    const cars = input.split(",");
+    this.isInputVaild(cars);
+    this.setCarMove(cars);
+  }
+
+  isInputVaild(cars) {
+    for (const car of cars) {
+      if (car.length > 5)
+        throw new Error("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+      if (this.cars.hasOwnProperty(car))
+        throw new Error("[ERROR] 자동차 이름은 중복 불가능합니다.");
+    }
+  }
+
+  setCarMove(cars) {
+    cars.forEach((car) => (this.cars[car] = 0));
+  }
+  async play() {
+    await this.getCarNames();
 }
 
 export default App;
