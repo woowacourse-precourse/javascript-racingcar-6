d19a82fd1cbb94dc1681abdaa8ec79421b927821
ZXLeeChris <lovejh429@naver.com>
2023-11-01 22:49:39 +0900

    Feat: 자동차 이름, 횟수 입력받기 함수, 이름, 횟수 유효한지 함수 Docs: 이름, 횟수 입력/유효성 기능 추가

--- src/App.js
+++ src/App.js
@@ -30,51 +30,22 @@ class App {
   }
 
   async play() { // 게임 시작 메서드
-    /*자동차 이름, 시도할 횟수 입력받기, 이름 유효한지 체크하기
-    실행결과 출력
-    게임 시작
-    우승자 찾기, 우승자 출력 */
-    
-    const carNames = await Console.readLineAsync(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
-    ); // 콘솔에서 자동차 이름 입력 받기
-    if (carNames.length > 5) {
-      throw new Error("[ERROR] 이름은 5자 이하만 가능합니다."); // 이름이 5자 초과면 에러 발생
-    }
-
-    const roundInput = await Console.readLineAsync(
-      "시도할 횟수는 몇 회인가요?"
-    ); // 콘솔에서 시도할 횟수 입력 받기
-    const rounds = Number(roundInput); // 숫자로 변환
-    if (isNaN(rounds) || rounds < 1) {
-      throw new Error("[ERROR] 횟수는 1 이상의 자연수만 가능합니다."); // 횟수가 자연수가 아니면 에러 발생
-    }
+    await this.inputCars(); // 자동차 이름 입력 받기
+    await this.inputRounds(); // 시도할 횟수 입력 받기
 
     Console.print("\n실행 결과"); // 실행 결과 출력 시작
     this.runGame(); // 게임 실행
     this.findWinners(); // 우승자 찾기
     this.showWinners(); // 우승자 출력
-
-  } //이제보니 이거도 인덴트가 3이었네...! 
-  
-  runGame() { // 게임 실행 메서드
-    for (let i = 0; i < this.rounds; i++) {
-      this.moveCars(); // 자동차들 전진
-      this.showCars(); // 자동차들 출력
-      Console.print(""); 
-    }
   }
 
-  moveCars() { // 자동차들 전진 메서드
-    for (const car of this.cars) {
-      car.moveForward(); // 각 자동차 전진
-    }
-  }
-
-  showCars() { // 자동차들 출력 메서드
-    for (const car of this.cars) {
-      Console.print(car.toString()); // 각 자동차 출력 형식으로 출력
-    }
+  async inputRounds() { // 시도할 횟수 입력 받기
+    const roundInput = await Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요?"
+    ); // 콘솔에서 시도할 횟수 입력 받기
+    const rounds = Number(roundInput); // 숫자로 변환
+    this.validateRound(rounds); // 횟수 유효성 검사
+    this.rounds = rounds; // rounds에 저장
   }
 
   findWinners() { // 우승자 찾기 메서드
@@ -88,6 +59,46 @@ class App {
   }
 
 } // App class 닫음
+
+async function inputCars() { // 자동차 이름 입력 받기 메서드
+  const carNames = await Console.readLineAsync(
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+  ); // 콘솔에서 자동차 이름 입력 받기
+  const names = carNames.split(","); // 쉼표로 구분하여 배열로 변환
+  for (const name of names) {
+    this.validateName(name); // 이름 유효성 검사
+    const car = new Car(name); // Car 객체 생성
+    this.cars.push(car); // cars 배열에 추가
+  }
+}
+
+function validateName(name) { // 이름 유효성 검사
+  if (name.length > 5) {
+    throw new Error("[ERROR] 이름은 5자 이하만 가능합니다."); // 이름이 5자 초과면 에러 발생
+  }
+}
+function validateRound(rounds) { // 횟수 유효성 검사
+  if (isNaN(rounds) || rounds < 1) {
+    throw new Error("[ERROR] 횟수는 1 이상의 자연수만 가능합니다."); // 횟수가 자연수가 아니면 에러 발생
+  }
+}
+function runGame() { // 게임 실행 메서드
+  for (let i = 0; i < this.rounds; i++) {
+    this.moveCars(); // 자동차들 전진
+    this.showCars(); // 자동차들 출력
+    Console.print(""); 
+  }
+}
+function moveCars() { // 자동차들 전진 함수
+  for (const car of this.cars) {
+    car.moveForward(); // 각 자동차 전진
+  }
+}
+function showCars() { // 자동차들 출력 함수
+  for (const car of this.cars) {
+    Console.print(car.toString()); // 각 자동차 출력 형식으로 출력
+  }
+}
 
 export default App;
 
