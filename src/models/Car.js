dedd205ee11110fad0751405de9d437114916b1f
unboxing96 <unboxing96@gmail.com>
2023-11-01 19:20:22 +0900

    feat(Car): 자동차 클래스 작성: 이동, 현재 위치 찾기, 이름 얻기

---
+++ src/models/Car.js
@@ -0,0 +1,26 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+import { MAGIC_NUM } from "../utils/constants.js";
+
+export default class Car {
+  constructor(name) {
+    this.name = name;
+    this.position = 0;
+  }
+
+  move() {
+    if (
+      MissionUtils.Random.pickNumberInRange(...MAGIC_NUM.RANDOM_RANGE) >=
+      MAGIC_NUM.MOVE_THRESHOLD
+    ) {
+      this.position += 1;
+    }
+  }
+
+  getPosition() {
+    return this.position;
+  }
+
+  getName() {
+    return this.name;
+  }
+}
