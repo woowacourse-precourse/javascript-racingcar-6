9c51fe54997530d13024b6d1d77a9d373f05b8ba
ryan-dia <cjfdnjs158@naver.com>
2023-11-01 19:03:23 +0900

    fix: 테스트 문구 쉽게 변경 및 통일

--- __tests__/model/RandomNumberGenerator.test.js
+++ __tests__/model/RandomNumberGenerator.test.js
@@ -2,7 +2,7 @@ import { SYSTEM } from '../../src/constants/System.js';
 import RandomNumberGenerator from '../../src/model/RandomNumberGenerator.js';
 
 describe('RandomNumberGenerator', () => {
-  test('should return a random number between 0 and 9', () => {
+  test('랜덤한 숫자는 0~9사이의 값이 나와야 한다.', () => {
     const result = RandomNumberGenerator.run();
     expect(result).toBeGreaterThanOrEqual(SYSTEM.randomNumberMin);
     expect(result).toBeLessThanOrEqual(SYSTEM.randomNumberMax);
