99648d348123ac7de050036a43374cd8daf8d257
chichoc <kny6152@naver.com>
2023-11-01 18:10:36 +0900

    feat: 입력값별 에러 여부 확인 기능 추가
    애러 여부 확인 함수 생성
    - 입력값별 각각 생성
    - 입력값 저장하기 전 에러여부 확인 함수 호출

--- src/App.js
+++ src/App.js
@@ -11,15 +11,33 @@ class App {
   }
 
   async getCarName() {
-    const input = await Console.readLineAsync(`경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n`);
-    return input.split(',');
+    const input = (
+      await Console.readLineAsync(`경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n`)
+    ).split(',');
+
+    if (!this.validateCarName(input)) {
+      throw new Error('[ERROR] 이름이 잘못된 형식입니다.');
+    }
+
+    return input;
   }
 
   async getAttempts() {
-    const input = await Console.readLineAsync(`시도할 횟수는 몇 회인가요?\n`);
-    return +input;
+    const input = +(await Console.readLineAsync(`시도할 횟수는 몇 회인가요?\n`));
+
+    if (!this.validateAttempts(input)) {
+      throw new Error('[ERROR] 횟수는 0보다 큰 숫자만 입력해주세요.');
+    }
+    return input;
   }
 
+  validateCarName(arr) {
+    return arr.every((name) => 0 < name.length && name.length <= 5);
+  }
+
+  validateAttempts(num) {
+    return !isNaN(num) && 0 < num;
+  }
 }
 
 export default App;
