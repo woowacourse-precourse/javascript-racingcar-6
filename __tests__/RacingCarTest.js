67152d4b05fa372509f90ad589cb72230ab255e0
heonq <gkagjsrb@naver.com>
2023-11-01 20:10:43 +0900

    test(RacingCar): RacingCar 객체에 대한 테스트코드 리팩터링 - 자동차 이동을 결정하는 랜덤 값을 상수로 대체 - 출력이 예상되는 결과를 배열로 저장하도록 수정 - racingCar.tryProgress에 입력하는 시도횟수를 변수로 선언한 다음 입력 - 가독성을 위해 생성하는 자동차 갯수 축소

--- __tests__/RacingCarTest.js
+++ __tests__/RacingCarTest.js
@@ -1,6 +1,7 @@
 import RacingCar from '../src/RacingCar';
 import {MissionUtils} from '@woowacourse/mission-utils';
 import OutputView from '../src/Views/OutputView';
+import CONSTANTS from '../utils/Constants';
 
 const mockRandoms = (numbers) => {
   MissionUtils.Random.pickNumberInRange = jest.fn();
@@ -15,7 +16,7 @@ const getLogSpy = () => {
   return logSpy;
 };
 
-const carNamesArray = ['car1', 'car2', 'car3', 'car4'];
+const carNamesArray = ['car1', 'car2'];
 const racingCar = new RacingCar(carNamesArray);
 
 describe('RacingCar클래스 테스트', () => {
@@ -23,15 +24,21 @@ describe('RacingCar클래스 테스트', () => {
     expect(racingCar.cars.length).toEqual(carNamesArray.length);
   });
   test('각 Car 인스턴스의 이동 이력 출력 테스트', () => {
-    const randoms = [4, 4, 4, 4, 4, 3, 3, 3];
-    const result1 = '\n실행 결과\ncar1 : -\ncar2 : -\ncar3 : -\ncar4 : -\n\n';
-    const result2 = 'car1 : --\ncar2 : -\ncar3 : -\ncar4 : -\n';
-    const expectedResult = [result1, result2];
+    const randoms = [
+      CONSTANTS.progressValue,
+      CONSTANTS.progressValue,
+      CONSTANTS.progressValue,
+      CONSTANTS.progressValue - 1,
+    ];
+    const result1 = ['\n실행 결과\n', 'car1 : -\n', 'car2 : -\n\n'];
+    const result2 = ['car1 : --\n', 'car2 : -\n\n'];
+    const expectedResult = [...result1, ...result2];
+    const trialCount = 2;
     const logSpy = getLogSpy();
 
     mockRandoms([...randoms]);
 
-    racingCar.tryProgress(2);
+    racingCar.tryProgress(trialCount);
     OutputView.printProgressStatus(racingCar);
 
     expectedResult.forEach((result) => {
@@ -42,6 +49,7 @@ describe('RacingCar클래스 테스트', () => {
   test('우승한 자동차를 출력하는 기능 테스트', () => {
     const expectedWinner = '최종 우승자 : car1';
     const logSpy = getLogSpy();
+
     OutputView.printWinner(racingCar);
 
     expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(expectedWinner));
