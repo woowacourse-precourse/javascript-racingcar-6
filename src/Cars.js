218f64ad0d743423f07f1e3691028f8a149b1d13
hyeonjun-L <beeminor60@gmail.com>
2023-11-01 17:22:37 +0900

    feat: Cars 컴포넌트 validation 작성

--- src/Cars.js
+++ src/Cars.js
@@ -1,6 +1,6 @@
 import { Random, Console } from '@woowacourse/mission-utils';
 import Car from './Car.js';
-import { MESSAGE } from './constants/message.js';
+import { MESSAGE, ERROR_MESSAGE } from './constants/message.js';
 
 export default class Cars {
   constructor({ initialState }) {
@@ -15,7 +15,7 @@ export default class Cars {
 
   race(playNumber) {
     Console.print(MESSAGE.RACE_START);
-    for (let i = 0; i < playNumber; i++) {
+    for (let i = 0; i < playNumber; i += 1) {
       this.forward();
       this.viewCurrentState();
       Console.print('');
@@ -49,5 +49,17 @@ export default class Cars {
       .map(({ state }) => state.name);
 
     Console.print(`최종 우승자: ${winners.join(', ')}`);
+  }
+
+  validationState(nextState) {
+    if (!Array.isArray(nextState)) {
+      throw new Error(ERROR_MESSAGE.ARRAY);
+    }
+
+    nextState.forEach((name) => {
+      if (typeof name !== 'string' || name.length > 5 || name === '') {
+        throw new Error(ERROR_MESSAGE.NAME);
+      }
+    });
   }
 }
