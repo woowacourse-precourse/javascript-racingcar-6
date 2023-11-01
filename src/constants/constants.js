73aa1dc101ba2010389a4fadd526588dc149483e
song_eun <dmswo546@naver.com>
2023-11-01 23:50:49 +0900

    style: 공백  변경

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -6,8 +6,8 @@ const GAME_INT = {
 };
 
 const INPUT_MESSAGE = {
-  CAR_NAME: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
-  ATTEMPT_NUMBER: '시도할 횟수는 몇 회인가요?\n',
+  CAR_NAME: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+  ATTEMPT_NUMBER: '시도할 횟수는 몇 회인가요?',
 };
 
 const OUTPUT_MESSAGE = {
