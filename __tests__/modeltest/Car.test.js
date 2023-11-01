e179de5bc24990e368963806f9d5e98b86a4097d
최나기 <choenagi@choenagiui-MacBookAir.local>
2023-11-01 21:20:21 +0900

    feat(Car): onestepForward 함수 구현
    Car클래스 내부함수, 랜덤으로 나온 값에 따라 누적거리를 증가시킨다.

--- __tests__/modeltest/Car.test.js
+++ __tests__/modeltest/Car.test.js
@@ -23,3 +23,33 @@ describe("Car getName", () => {
 		});
 	});
 });
+
+describe("Car getDistance", () => {
+    const car = new Car("test");
+    test("getDistance은 Function type이다 ", () => {
+		expect(typeof car.getDistance).toBe("function");
+	})
+	test('car.getDistance은 값을 정상적으로 리턴한다. ', () => {
+        const car = new Car("test");
+		expect(car.getDistance()).toStrictEqual(0);
+	});
+})
+
+describe("Car oneStepForward", () => {
+    const car = new Car("test");
+    test("oneStepForward은 Function type이다 ", () => {
+		expect(typeof car.oneStepForward).toBe("function");
+	})
+    test("Random.pickNumberInRange가 호출된다 ", () => {
+        Random.pickNumberInRange = jest.fn();
+        Random.pickNumberInRange.mockReturnValue(1);
+        car.oneStepForward();
+        expect(Random.pickNumberInRange).toHaveBeenCalled();
+    })
+    test("oneStepForward은 distance에 값을 추가한다 ", () => {
+        Random.pickNumberInRange = jest.fn();
+        Random.pickNumberInRange.mockReturnValue(5);
+        car.oneStepForward();
+		expect(car.getDistance()).toBe(1);
+	})
+})
--- src/model/Car.js
+++ src/model/Car.js
@@ -8,8 +8,10 @@ class Car {
     this.#name = carName;
     this.#distance = 0;
   }
+  oneStepForward() { this.#distance += Car.#oneOrZero(); }
   getName() { return (this.#name); }
   getDistance() { return (this.#distance); }
+  static #oneOrZero() { return Random.pickNumberInRange(0, 9) > 3 ? 1 : 0; }
 }
 
 export default Car;
