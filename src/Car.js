f9d20491f62d703a1a9d31a87d8314d99f7e71f7
Seongbin Kim <seongbin9786@gmail.com>
2023-11-01 16:27:05 +0900

    lint: 코드 포매팅

--- src/Car.js
+++ src/Car.js
@@ -1,21 +1,20 @@
 export class Car {
-    #name;
-    #moves;
-  
-    constructor(name) {
-      this.#name = name;
-      this.#moves = 0;
-    }
-  
-    moveForward() {
-      this.#moves++;
-    }
-  
-    score() {
-      return { 
-        name: this.#name,
-        moves: this.#moves,
-      }
+  #name;
+  #moves;
+
+  constructor(name) {
+    this.#name = name;
+    this.#moves = 0;
+  }
+
+  moveForward() {
+    this.#moves++;
+  }
+
+  score() {
+    return { 
+      name: this.#name,
+      moves: this.#moves,
     }
   }
-  
+}
