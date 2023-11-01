82c344a01d44a272d48db90523b0d27836ef7db9
ryan-dia <cjfdnjs158@naver.com>
2023-11-01 11:56:26 +0900

    feat: 레이싱 모델을 컨트롤러에 주입하도록 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import racingController from './controller/index.js';
+import RacingController from './controller/index.js';
+import Car from './model/Car.js';
+import RacingModel from './model/index.js';
 
 class App {
   #controller;
 
   constructor() {
-    this.#controller = new racingController();
+    this.#controller = new RacingController(new RacingModel(new Car()));
   }
 
   async play() {
