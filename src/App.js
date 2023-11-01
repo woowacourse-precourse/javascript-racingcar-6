07e51433c6866677307e9f7e2c82a432eecf7c37
gelim <b4winter@qraod.com>
2023-11-01 19:28:10 +0900

    1. 게임에 관한 에러메시지 정리 - 에러메시지 -게임 운영 메시지
    2-1. 자동차 이름 입력 및 유효성검사
    2-2. 시도 횟수 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,54 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+import { errorMessages, promptMessages } from './GameMessage';
+
 class App {
-  async play() {}
+
+  // 자동차 이름 입력
+  async setCarNames() {
+    const inputCarNames = await Console.readLineAsync(promptMessages.carName);
+    const carNames = inputCarNames.replace(/\s/g, '').split(',');
+    this.validateCars(carNames);
+    return carNames.reduce((acc, value) => {
+      acc[value] = 0;
+      return acc;
+    }, {});
+  }
+
+  // 자동차 유효성 검사 
+  validateCars(carNames) {
+    const raceCarNames = new Set();
+
+    if (carNames.some(name => name === '' || name.length > 5 || raceCarNames.has(name)) ||carNames.length < 2 ) {
+      throw new Error(
+        carNames.length < 2 ? errorMessages.insufficientCarCount :
+        carNames.some(name => name === '') ? errorMessages.invalidCarNameFormat :
+        carNames.some(name => name.length > 5) ? errorMessages.carNameMaxLengthExceeded :
+        errorMessages.duplicateCarName
+      );
+    }
+
+    carNames.forEach(name => raceCarNames.add(name));
 }
+
+ // 시도 횟수 입력
+  async inputTryCount() {
+    let raceTryCount;
+    while (true) {
+      const inputTryCount = await Console.readLineAsync(promptMessages.tryCount);
+      raceTryCount = Number(inputTryCount);
+
+      if (!inputTryCount.trim() || isNaN(raceTryCount) || raceTryCount <= 0) {
+        Console.print(errorMessages.invalidTryCount);
+      } else {
+        break;
+      }
+    }
+
+    return raceTryCount;
+  }
+
+}
+
+
 
 export default App;
---
+++ src/GameMessage.js
@@ -0,0 +1,14 @@
+export const errorMessages = {
+    invalidCarNameFormat: '[ERROR] 입력값이 잘못된 형식입니다.',
+    carNameMaxLengthExceeded: '[ERROR] 자동차 이름은 5자 이하만 가능합니다.',
+    duplicateCarName: '[ERROR] 중복된 자동차 이름이 있습니다.',
+    invalidTryCount: '[ERROR] 올바른 숫자를 입력하세요.',
+    insufficientCarCount: '[ERROR] 경주할 자동차는 2대 이상이어야 합니다.',
+  };
+
+  export const promptMessages = {
+    carName: '경주할 자동차 이름을 입력하세요.',
+    tryCount: '시도 할 횟수를 입력하세요.(0과 공란 불가)',
+    gameResult: '게임 결과',
+    finalWinner: '축하합니다! "최종레이싱 최종우승" :',
+  };
