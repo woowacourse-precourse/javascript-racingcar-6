fca3efc44ecbc0666795f7052a094b148d89836d
yu-heejin <jkgt0521@naver.com>
2023-11-01 19:33:12 +0900

    refactor(Game.js): depth가 3을 넘지 않도록 수정

--- src/modules/Game.js
+++ src/modules/Game.js
@@ -13,8 +13,8 @@ export default class Game {
         
         for (let i = 0; i < count; i++) {
             const randomNumbers = await this.createRandomNumsersForCar(carNames.length);
-            this.calculateGameResult(carNames, this.gameResults, randomNumbers);
-            this.printGameResult(carNames, this.gameResults)
+            this.getGameResult(randomNumbers);
+            this.printGameResult(carNames);
         }
 
         this.printGameWinner(winStandard, carNames);
@@ -40,37 +40,45 @@ export default class Game {
         return randomNumbers;
     }
 
-    calculateGameResult(carNames, gameResults, randomNumbers) {
-        for (let i = 0; i < carNames.length; i++) {
-            const randomNumber = randomNumbers[i];
-
-            if (randomNumber > this.GO_STANDARD) {
-                const result = `${gameResults[i] ? gameResults[i] : ''}-`;
-                gameResults[i] = result;
-            }
+    getGameResult(randomNumbers) {
+        for (let i = 0; i < randomNumbers.length; i++) {
+            this.calculateGameResult(randomNumbers[i], i);
         }
     }
 
-    printGameResult(carNames, gameResults) {
+    calculateGameResult(randomNumber, index) {
+        if (randomNumber > this.GO_STANDARD) {
+            this.gameResults[index] = `${this.gameResults[index] ? this.gameResults[index] : ''}-`;
+        }
+    }
+
+    printGameResult(carNames) {
         MissionUtils.Console.print(GAME_MESSAGE.gameResult);
 
         for (let i = 0; i < carNames.length; i++) {
-            MissionUtils.Console.print(`${carNames[i]} : ${gameResults[i] ? gameResults[i] : ''}`);
+            MissionUtils.Console.print(`${carNames[i]} : ${this.gameResults[i] ? this.gameResults[i] : ''}`);
         }
         MissionUtils.Console.print('');
     }
 
     printGameWinner(winStandard, carNames) {
         MissionUtils.Console.print(GAME_MESSAGE.gameResult);
+        MissionUtils.Console.print(`최종 우승자 : ${this.getGameWinner(winStandard, carNames.length)}`);
+    }
 
+    getGameWinner(winStandard, count) {
         const winner = [];
 
-        for (let i = 0; i < carNames.length; i++) {
-          if (this.gameResults[i] === winStandard) {
-            winner.push(carNames[i]);
-          }
+        for (let i = 0; i < count; i++) {
+            this.getWinnerList(winner, winStandard, i);
         }
-    
-        MissionUtils.Console.print(`최종 우승자 : ${winner}`);
+
+        return winner;
+    }
+
+    getWinnerList(winner, winStandard, index) {
+        if (this.gameResults[index] === winStandard) {
+            winner.push(carNames[index]);
+        }
     }
 }
