782e3df87265da2353217de790f3b67e7e7776b2
psst54 <psst5491@naver.com>
2023-11-01 11:52:38 +0900

    ✨ 결과 출력하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { getCarName, getMoveNumber, getCarsMovedCount } from "./getUserInput";
-import { moveCars } from "./move";
+import { moveCars, printResult } from "./move";
 
 class App {
   async play() {
@@ -10,6 +10,7 @@ class App {
 
     while (moveNumber--) {
       moveCars(names, carsMovedCount);
+      printResult(names, carsMovedCount);
     }
   }
 }
--- src/move.js
+++ src/move.js
@@ -1,7 +1,19 @@
+import { Console } from "@woowacourse/mission-utils";
+
 function moveCar(name, carsMovedCount) {
   carsMovedCount[name]++;
 }
 
 export function moveCars(names, carsMovedCount) {
   names.forEach((name) => moveCar(name, carsMovedCount));
+}
+
+function getResultString(count) {
+  return "-".repeat(count);
+}
+
+export function printResult(names, carsMovedCount) {
+  names.forEach((name) =>
+    Console.print("pobi : " + getResultString(carsMovedCount[name]))
+  );
 }
