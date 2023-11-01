7bab52bd13c623e5dc90a704e8e85e09b2a6ac7e
urjimyu <92876819+urjimyu@users.noreply.github.com>
2023-11-01 11:18:41 +0900

    feature: 사용자로부터 자동차 이름 입력 받기

---
+++ src/getCarName.js
@@ -0,0 +1,13 @@
+import { Console } from '@woowacourse/mission-utils';
+import STRINGS from './constants/strings';
+
+export default async function getCarName() {
+  const inputCarNames = await Console.readLineAsync(STRINGS.INPUT_CAR);
+  const parsedCarNames = inputCarNames.split(',');
+  parsedCarNames.forEach(name => {
+    if (name.length > 5) {
+      throw Error(STRINGS.ERROR_LENGTH);
+    }
+  });
+  return parsedCarNames;
+}
