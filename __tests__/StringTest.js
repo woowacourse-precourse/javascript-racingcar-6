d3bad7def2cb20da50797002c3da30cc8dd0b495
thwlstn99 <136067544+thwlstn@users.noreply.github.com>
2023-11-01 22:57:28 +0900

    [자동차 경주] 소진수 미션 제출합니다.

--- __tests__/StringTest.js
+++ __tests__/StringTest.js
@@ -1,30 +1,79 @@
-describe("문자열 테스트", () => {
-  test("split 메서드로 주어진 값을 구분", () => {
-    const input = "1,2";
-    const result = input.split(",");
+import RacingGame from "./RacingGame";
+import App from "./App";
+import * as MissionUtils from "@woowacourse/mission-utils";
 
-    expect(result).toContain("2", "1");
-    expect(result).toContainEqual("1", "2");
+describe("RacingGame", () => {
+  // 테스트를 위해 MissionUtils.Console.print를 모의화합니다.
+  MissionUtils.Console.print = jest.fn();
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
-  test("split 메서드로 구분자가 포함되지 않은 경우 값을 그대로 반환", () => {
-    const input = "1";
-    const result = input.split(",");
+  test("play - 경주 상황과 우승자를 출력해야 합니다", async () => {
+    // 자동차 이름과 시도 횟수를 모의화합니다.
+    const 모의화된자동차이름 = "자동차1, 자동차2, 자동차3";
+    const 모의화된시도횟수 = 5;
 
-    expect(result).toContain("1");
+    // 비동기적인 사용자 입력을 모의화합니다.
+    MissionUtils.Console.readLineAsync = jest
+      .fn()
+      .mockResolvedValueOnce(모의화된자동차이름)
+      .mockResolvedValueOnce(String(모의화된시도횟수));
+
+    // App 인스턴스를 생성하고 play 메서드를 호출합니다.
+    const 앱 = new App();
+    await 앱.play();
+
+    // MissionUtils.Console.print가 예상한 메시지로 호출되었는지 확인합니다.
+    expect(MissionUtils.Console.print).toHaveBeenCalledWith("자동차1 : -");
+    expect(MissionUtils.Console.print).toHaveBeenCalledWith("자동차2 : -");
+    expect(MissionUtils.Console.print).toHaveBeenCalledWith("자동차3 : -");
+    expect(MissionUtils.Console.print).toHaveBeenCalledWith("자동차1 : --");
+    expect(MissionUtils.Console.print).toHaveBeenCalledWith("자동차2 : --");
+    expect(MissionUtils.Console.print).toHaveBeenCalledWith("자동차3 : --");
+    expect(MissionUtils.Console.print).toHaveBeenCalledWith("자동차1 : ---");
+    expect(MissionUtils.Console.print).toHaveBeenCalledWith("자동차2 : ---");
+    expect(MissionUtils.Console.print).toHaveBeenCalledWith("자동차3 : ---");
+    expect(MissionUtils.Console.print).toHaveBeenCalledWith("자동차1 : ----");
+    expect(MissionUtils.Console.print).toHaveBeenCalledWith("자동차2 : ----");
+    expect(MissionUtils.Console.print).toHaveBeenCalledWith("자동차3 : ----");
+    expect(MissionUtils.Console.print).toHaveBeenCalledWith("자동차1 : -----");
+    expect(MissionUtils.Console.print).toHaveBeenCalledWith("자동차2 : -----");
+    expect(MissionUtils.Console.print).toHaveBeenCalledWith("자동차3 : -----");
+    expect(MissionUtils.Console.print).toHaveBeenCalledWith(
+      "최종 우승자 : 자동차1, 자동차2, 자동차3"
+    );
   });
 
-  test("substring 메서드로 특정 구간 값을 반환", () => {
-    const input = "(1,2)";
-    const result = input.substring(1, 4);
+  test("play - 입력이 잘못된 경우 에러를 처리해야 합니다", async () => {
+    // 유효하지 않은 사용자 입력 (빈 자동차 이름)을 모의화합니다.
+    MissionUtils.Console.readLineAsync = jest
+      .fn()
+      .mockResolvedValueOnce("")
+      .mockResolvedValueOnce("5");
 
-    expect(result).toEqual("1,2");
-  });
+    // App 인스턴스를 생성하고 play 메서드를 호출합니다.
+    const 앱 = new App();
+    await 앱.play();
 
-  test("at 메서드로 특정 위치의 문자 찾기", () => {
-    const input = "abc";
-    const result = input.at(0)
+    // 에러 메시지가 출력되었는지 확인합니다.
+    expect(MissionUtils.Console.print).toHaveBeenCalledWith(
+      "[ERROR] 자동차 이름을 입력하세요."
+    );
 
-    expect(result).toEqual("a");
+    // 유효하지 않은 사용자 입력 (숫자가 아닌 시도 횟수)을 모의화합니다.
+    MissionUtils.Console.readLineAsync = jest
+      .fn()
+      .mockResolvedValueOnce("자동차1, 자동차2, 자동차3")
+      .mockResolvedValueOnce("잘못된값");
+
+    // App 인스턴스를 생성하고 play 메서드를 호출합니다.
+    await 앱.play();
+
+    // 에러 메시지가 출력되었는지 확인합니다.
+    expect(MissionUtils.Console.print).toHaveBeenCalledWith(
+      "[ERROR] 숫자가 잘못된 형식입니다."
+    );
   });
 });
