b349b8e51b1ec57e1f49615d3eaefc0bc9750ad4
KangYunJi <dbswl701@ajou.ac.kr>
2023-11-01 16:58:53 +0900

    test: 테스트코드 작성

---
+++ __tests__/CarTest.js
@@ -0,0 +1,83 @@
+import App from "../src/App.js";
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const mockQuestions = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+    return Promise.resolve(input);
+  });
+};
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe("자동차 경주 게임", () => {
+  test("랜덤한 수에 따라 전진 출력", async () => {
+    // given
+    const MOVING_FORWARD = 4;
+    const STOP = 3;
+    const inputs = ["pobi,woni", "2"];
+    const outputs = ["pobi : --", "woni : -"];
+    const randoms = [MOVING_FORWARD, STOP, MOVING_FORWARD, MOVING_FORWARD];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([...randoms]);
+
+    // when
+    const app = new App();
+    await app.play();
+
+    // then
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test.each([
+    [["pobi,javaji", "5"]],
+    [["pobi,,woni", "5"]],
+    [["pobi,woni#", "5"]],
+    [["pobi, ,woni", "5"]],
+    [["pobi, woni,jun", "5"]],
+    [["pobi,123,jun", "5"]],
+    [["pobi,jun,pobi", "5"]],
+    [["pobi", "5"]]
+  ])("자동차 이름 입력에 대한 예외 처리", async (inputs) => {
+    // given
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.play()).rejects.toThrow("[ERROR]");
+  });
+
+  test.each([
+    [["pobi,woni", "0"]],
+    [["pobi,woni", "-1"]],
+  ])("시도 횟수 입력에 대한 예외 처리", async (inputs) => {
+    // given
+    mockQuestions(inputs);
+    const logSpy = getLogSpy();
+
+    // when
+    const app = new App();
+    console.log(logSpy)
+    // then
+    await expect(app.play()).rejects.toThrow("[ERROR]");
+  });
+});
