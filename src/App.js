6cb44258cf2c0132477b71c4dd81500feff759be
hzzzi <hji2004@naver.com>
2023-11-01 21:51:09 +0900

    파일 2차 분리

--- src/App.js
+++ src/App.js
@@ -1,59 +1,25 @@
 import { Console } from '@woowacourse/mission-utils';
-import Car from './Car.js';
 import InputValidation from './InputValidation.js';
+import Race from './RacingProcess.js';
 
-function getWinner(carArr) {
-  let winners = [];
-  let winnerPosition = 0;
-  carArr.forEach((car) => {
-    if (winnerPosition < car.position) {
-      winnerPosition = car.position;
-      winners = [car.name];
-    } else if (winnerPosition === car.position) {
-      winners.push(car.name);
-    }
-  });
-  return winners;
-}
+const inputValidation = new InputValidation();
+const race = new Race();
 
 class App {
   async play() {
-    const inputValidation = new InputValidation();
-
     let carNameArr = await this.getCarName();
     inputValidation.isCarNameValid(carNameArr);
 
     let roundCount = await this.getRoundCount();
     inputValidation.isRoundCountValid(roundCount);
 
-    let carArr = this.createCarArray(carNameArr);
+    let carArr = race.createCarArray(carNameArr);
 
     Console.print('\n실행 결과');
-    this.runRace(carArr, roundCount);
-    let winners = getWinner(carArr);
+    race.runRace(carArr, roundCount);
+
+    let winners = race.getWinner(carArr);
     Console.print(`최종 우승자 : ${winners}`);
-  }
-
-  createCarArray(carNameArr) {
-    let carArr = [];
-    carNameArr.forEach((element) => {
-      carArr.push(new Car(element));
-    });
-    return carArr;
-  }
-
-  oneRound(carArr) {
-    carArr.forEach((car) => {
-      car.goForward();
-      car.showPosition();
-    });
-  }
-
-  runRace(carArr, roundCount) {
-    for (let i = 0; i < roundCount; i++) {
-      this.oneRound(carArr);
-      Console.print('\n');
-    }
   }
 
   async getCarName() {
