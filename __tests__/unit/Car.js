e605f95322eca89b9ba4357b478d19d6c8fb993a
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-11-01 14:09:01 +0900

    refactor: Car.js 테스트 중 중복되는 코드 삭제

--- __tests__/unit/Car.js
+++ __tests__/unit/Car.js
@@ -4,26 +4,17 @@ import Car from '../../src/model/Car.js';
 describe('자동차 이름 및 위치 테스트', () => {
   test('자동차 이름 생성 가능 여부', () => {
     const car = new Car('Tesla');
-
     expect(car.getName()).toBe('Tesla');
   });
 
   test('시작 위치가 0으로 설정 여부', () => {
     const car = new Car('Tesla');
-
     expect(car.getPosition()).toBe(0);
   });
 
-  test('전진 메서드 호출 시 위치 +1 증가 여부', () => {
+  test('전진 메서드 호출 시 위치 +1 가능 여부', () => {
     const car = new Car('Tesla');
-
     car.moveForward();
     expect(car.getPosition()).toBe(1);
-  });
-
-  test('전진 메서드 미호출 시 위치 0으로 멈춤 여부', () => {
-    const car = new Car('Tesla');
-    
-    expect(car.getPosition()).toBe(0);
   });
 });
