7a305f0f517a57a483e41c8e12e759d781d6cc78
soi-ha <soy2302ten@gmail.com>
2023-11-01 09:08:02 +0900

    chore: InputCarName, 입력 1개일때 예외처리 추가

--- src/InputCarName.js
+++ src/InputCarName.js
@@ -6,6 +6,9 @@ export default async function InputCarName() {
 		const nameList = answer.split(',');
 		const REGEX = /^[ㄱ-ㅎ|가-힣|a-z|A-Z|]+$/;
 		const filterName = nameList.filter((name) => name.length < 5 && REGEX.test(name));
+		if (nameList.length === 1) {
+			throw new Error('[ERROR] 이름은 2개 이상 작성해야 합니다.');
+		}
 		if (filterName.length !== nameList.length) {
 			throw new Error('[ERROR] 공백없이 5글자 이하의 영어 및 한글만 입력해주세요.');
 		}
