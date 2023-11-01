c2a68472cbbb8368bd548797575d8bd06b46bf38
정은우 <wjddmsdn_8976@naver.com>
2023-11-01 22:37:07 +0900

    App.js : isCarName 차 이름 조건 부합 확인

--- src/App.js
+++ src/App.js
@@ -1,40 +1,50 @@
 import { Console, Random } from "@woowacourse/mission-utils";
-
 class App {
   constructor(name) {
     this.name = name;
   }
+  
   async play() {
-    await this.start();
+    await this.inputCarName();
     this.run();
   }
 
-  async start() {
+  async inputCarName() {
     try {
       const carName = await Console.readLineAsync(
         "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
       );
-      const carNameList = carName.split(',')
-      console.log(carNameList[0])
-    } catch (error) {
-      // reject 되는 경우
-      for(let i=0;i<length(carNameList);i++){
-        if(!isNaN(carNameList[i])){
-          throw "[ERROR] 값이 비어있습니다."
-        }
-        if(length(carNameList[i])>5){
-          throw "[ERROR] 5자리 이하의 값만 입력해주세요"
-        }
+      const cars = carName.split(",");
+
+      for (let i = 0; i < cars.length; i++) {
+        this.isCarName(cars, i);
+        carNameList.push(cars[i]); 
       }
+      console.log(cars[0]);
+    } catch (error) {
+      console.error(error.message); // 에러 메시지 출력
     }
   }
+
+  isCarName(checkList, i) {
+    if (isNaN(parseFloat(checkList[i])) || checkList[i].trim() === "") {
+      throw new Error("[ERROR] 값이 비어있습니다.");
+    }
+    if (checkList[i].length > 5) {
+      throw new Error("[ERROR] 5자리 이하의 값만 입력해주세요.");
+    }
+  }
+
+  checkCarRandomNumber() {}
+
   run() {
     // this.randomNumber();
     // this.inputNumber();
     // this.checkNumber();
   }
 }
-const app = new App();
-app.play();
 
-export default App;
+const carNameList = [];
+
+
+new App().inputCarName().catch(console.error);
