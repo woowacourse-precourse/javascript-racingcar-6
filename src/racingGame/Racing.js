0850f66955f45336b84e2b9d1ce5898f9e088854
Jihyun_Kim <jihyun3313@daum.net>
2023-11-01 19:49:58 +0900

    test: Racing 클래스 테스트코드

--- src/racingGame/Racing.js
+++ src/racingGame/Racing.js
@@ -7,28 +7,29 @@ class Racing {
   async racingStart(carNameArray, racingNumber) {
     await MissionUtils.Console.print(MESSAGE.result);
 
-    const scoreStorage = this.#makeScoreStorage(carNameArray); // 이름별 누적거리 저장을 위한 2차원배열 생성
+    const scoreStorage = this.makeScoreStorage(carNameArray); // 이름별 누적거리 저장을 위한 2차원배열 생성
 
     for (let i = 1; i <= racingNumber; i++) {
-      this.#eachRacing(scoreStorage);  // 각 차수별 레이싱 진행
-      this.#printScore(scoreStorage) // 누적거리 출력
+      this.eachRacing(scoreStorage);  // 각 차수별 레이싱 진행
+      this.printScore(scoreStorage) // 누적거리 출력
     }
 
     return scoreStorage;
   }
 
-  #makeScoreStorage(carNameArray) {
+  makeScoreStorage(carNameArray) {
     const scoreStorage = [];
     scoreStorage.push(carNameArray);  // 첫 번째 행에 이름 저장 scoreStorage[NAME]
     scoreStorage.push(Array(carNameArray.length).fill(0)) // 두 번째 행에 누적거리저장 scoreStorage[DISTANCE]
     return scoreStorage;
   }
 
-  #eachRacing(scoreStorage) {
+  eachRacing(scoreStorage) {
     scoreStorage[DISTANCE].forEach((e, index) => {
       let isMove = this.#isMove() // 전진여부확인메소드
       scoreStorage[DISTANCE][index] += isMove;
     })
+    return scoreStorage;
   }
 
   #isMove() {
@@ -37,7 +38,7 @@ class Racing {
     else return false;
   }
 
-  #printScore(scoreStorage) {
+  printScore(scoreStorage) {
     scoreStorage[NAME].forEach((name, index) => {
       let distance = this.#showDistanceWithHyphen(scoreStorage[DISTANCE][index]);
       MissionUtils.Console.print(`${name} : ${distance} `);
