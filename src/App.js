039bd1fe1a0afc7cf9ced2e43fb635693206d759
KangYunJi <dbswl701@ajou.ac.kr>
2023-11-01 14:23:15 +0900

    feat: 자동차 이름 입력 예외사항 처리

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ class App {
     // 자동차 이름 입력
     const carStr = await MissionUtils.Console.readLineAsync('닉네임을 입력해주세요.');
     const carArr = carStr.split(',');
+    
+    if (carArr.length === 1) throw new Error("[ERROR] 자동차는 2개 이상이어야 합니다.");
+    carArr.map((v, idx) => {
+      if (v.length > 5) throw new Error("[ERROR] 자동차 이름은 5자 이하여야 합니다.");
+      if (!/[\w]+/.test(v)) throw new Error("[ERROR] 자동차 이름은 문자여야 합니다.");
+      if (carArr.indexOf(v) !== idx) throw new Error("[ERROR] 자동차 이름은 중복될 수 없습니다.");
+    })
 
   }
 }
