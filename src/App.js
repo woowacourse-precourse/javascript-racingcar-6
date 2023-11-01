41a4e6f887afe09ebadb66fb69bdd736134d0510
Romuru <pastelblue0721@gmail.com>
2023-11-01 23:10:40 +0900

    feat(1). 유저가 경주할 자동차의 이름과 시도할 횟수를 입력

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
-class App {}
+
+const { Random, Console } = MissionUtils;
+
+class App {
+  async userInputHandler() {
+    const playerBeforeDivide = await Console.readLineAsync(
+      "자동차 이름을 입력하세요.(이름은 쉼표(,)으로 구분, 공백없는 5글자 이하)\n"
+    );
+    const gameRound = await Console.readLineAsync(
+      "게임을 시도할 횟수를 입력해 주세요.\n"
+    );
+  }
+
+  async play() {}
+}
 
 export default App;
