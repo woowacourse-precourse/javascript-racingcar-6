a0dfe511bc9946ef323814e1f8be8a9a178312cf
irenee-14 <ireneee145@gmail.com>
2023-11-01 20:13:05 +0900

    refactoring: 불필요한 공백 제거

--- src/App.js
+++ src/App.js
@@ -56,7 +56,6 @@ class App {
 
   async racingGame()
   {
-
     let playerInput = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
     const cars = this.checkPlayerInput(playerInput.split(","));
 
