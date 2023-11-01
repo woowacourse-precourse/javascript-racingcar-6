549bfb24a46a1fe03bd3a4e99651b119d5046b7b
sayyyho <323psh@naver.com>
2023-11-01 17:41:00 +0900

    feat: 우승자 판별 후 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,75 @@
-import Game from './Game.js';
+import { Console, Random } from '@woowacourse/mission-utils';
+import Input from './Input';
+
+// 배열로서 carName, carValue 관리
 
 class App {
+  constructor() {
+    this.trial = 0;
+    this.carList = [];
+    this.totalResult = [];
+  }
+
   async play() {
-    const game = new Game();
-    game.play();
+    await this.init();
+    for (let trial = 0; trial < this.trial; trial += 1) {
+      this.setValue();
+      this.printResult();
+    }
+    this.printWinner();
+  }
+
+  async init() {
+    const input = new Input();
+    const { carNames } = await input.startInput();
+    this.carList = carNames.split(',');
+    this.trial = await input.setTrial();
+    Console.print('\n실행 결과');
+    this.carList.forEach(() => {
+      this.totalResult.push(0);
+    });
+  }
+
+  setValue() {
+    const randomValue = [];
+    this.carList.forEach(() => {
+      randomValue.push(Random.pickNumberInRange(0, 9));
+    });
+    this.checkResult(randomValue);
+  }
+
+  checkResult(randomValue) {
+    const totalResult = [];
+    randomValue.forEach((item, index) => {
+      if (item > 3) {
+        totalResult.push(this.totalResult[index] + 1);
+      }
+      if (item < 4) {
+        totalResult.push(this.totalResult[index]);
+      }
+    });
+    this.totalResult = [...totalResult];
+  }
+
+  printResult() {
+    this.carList.forEach((car, index) => {
+      Console.print(`${car} : ${'-'.repeat(this.totalResult[index])}`);
+    });
+    Console.print('\n');
+  }
+
+  printWinner() {
+    const winner = [];
+    const maxNumber = Math.max(...this.totalResult);
+    this.totalResult.forEach((number, index) => {
+      if (maxNumber === number) {
+        winner.push(this.carList[index]);
+      }
+    });
+    Console.print(`최종 우승자 : ${winner.join(', ')}`);
   }
 }
 
 export default App;
+
+// 굳이 랜덤만 받아올거면 Car 객체는 필요없을듯
--- src/Input.js
+++ src/Input.js
@@ -3,6 +3,7 @@ import { Console } from '@woowacourse/mission-utils';
 class Input {
   constructor() {
     this.carSet = {};
+    this.trials = 0;
   }
 
   async startInput() {
@@ -34,6 +35,22 @@ class Input {
       carNames,
       carNumber,
     };
+  }
+
+  async setTrial() {
+    const trials = parseInt(
+      await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n'),
+      10,
+    );
+    if (
+      Number.isNaN(trials) ||
+      trials < 1 ||
+      trials > Number.MAX_SAFE_INTEGER
+    ) {
+      throw new Error('[ERROR] 유효하지 않은 횟수 입력입니다.');
+    }
+    this.trials = trials;
+    return trials;
   }
 }
 
