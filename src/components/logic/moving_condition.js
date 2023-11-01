3bf112f4538387fe6f1e21ce41c2caa5c0507f6f
leelsj2007 <leelsj2007@gmail.com>
2023-11-01 15:57:33 +0900

    feat : (전진 기능) 시도별 랜덤 값 0-9 중 4 이상일 경우 전진 기능 구현

--- src/components/logic/moving_condition.js
+++ src/components/logic/moving_condition.js
@@ -1,7 +1,11 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 const movingCondition = (carPosition) => {
-    return 0;
+    let moveDetermain = MissionUtils.Random.pickNumberInRange(0, 9);
+    if (moveDetermain >= 4){
+        carPosition += 1;
+    }
+    return carPosition;
 };
 
 export default movingCondition;
