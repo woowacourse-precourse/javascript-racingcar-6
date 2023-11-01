f05238419b046716e9162cd9ccafa941eab1d6be
ryan-dia <cjfdnjs158@naver.com>
2023-11-01 15:12:33 +0900

    feat(view): JSDocs을 이용하여 타입 작성

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -3,12 +3,19 @@ import { INPUT_MESSAGE_FUNCTION, OUTPUT_MESSAGE } from '../constants/Messages.js
 import Validators from '../utils/validator/index.js';
 
 const InputView = {
+  /**
+   * @param {string} vehicle
+   * @returns {string}
+   */
   async readRacingVehicleName(vehicle) {
     const racingVehicleName = await Console.readLineAsync(INPUT_MESSAGE_FUNCTION.name(vehicle));
     Validators.checkRacingVehicleName(racingVehicleName);
     return racingVehicleName;
   },
 
+  /**
+   * @returns {number}
+   */
   async readRacingCount() {
     const racingCount = await Console.readLineAsync(OUTPUT_MESSAGE.count);
     Validators.checkRacingCount(racingCount);
--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -2,14 +2,23 @@ import { Console } from '@woowacourse/mission-utils';
 import { OUTPUT_MESSAGE_FUNCTION } from '../constants/Messages.js';
 
 const OutputView = {
+  /**
+   * @param {string} message
+   */
   print(message) {
     Console.print(message);
   },
 
+  /**
+   * @param {Map} racingResult
+   */
   printRacingResult(racingResult) {
     racingResult.forEach((progress, vehicleName) => Console.print(`${vehicleName} : ${progress}`));
   },
 
+  /**
+   * @param {string[]} finalWinner
+   */
   printFinalWinner(finalWinner) {
     this.print(OUTPUT_MESSAGE_FUNCTION.finalWinner(finalWinner));
   },
