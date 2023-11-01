8e996e054aa40a21b855a3ed537b0ee82208beda
An-jihyun <anjh322@gmail.com>
2023-11-01 22:14:34 +0900

    ✨ feat: 자동차 이름 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  getCarName = async () => {
+    const input = await Console.readLineAsync(
+      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n'
+    );
+    const cars = input.split(',').map((v) => v.trim());
+    return cars;
+  };
+
+  async play() {
+    const car = await this.getCarName();
+  }
 }
 
 export default App;
