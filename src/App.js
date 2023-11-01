0a42459ccc480a9197c2815e4fe91861bd90f667
이채림 <leecr1215@naver.com>
2023-11-01 20:03:56 +0900

    refactor : async await 활용

--- src/App.js
+++ src/App.js
@@ -4,44 +4,43 @@ import { validateCarNamesInput, validateNumberOfAttemptsInput } from './validati
 
 class App {
   async play() {
-    await Console.readLineAsync(MESSAGES.CAR_NAME).then(async (cars) => {
-      const carArr = cars.split(',');
+    const input = await Console.readLineAsync(MESSAGES.CAR_NAME);
+    const cars = input.split(',');
 
-      validateCarNamesInput(carArr);
+    validateCarNamesInput(cars);
 
-      const map = new Map();
-      carArr.forEach((car) => map.set(car, 0));
+    const carMap = new Map();
+    cars.forEach((car) => carMap.set(car, 0));
 
-      await Console.readLineAsync(MESSAGES.NUMBER_OF_ATTEMPTS).then((attemptsNumber) => {
-        validateNumberOfAttemptsInput(attemptsNumber);
+    const numberOfAttempts = await Console.readLineAsync(MESSAGES.NUMBER_OF_ATTEMPTS);
 
-        Console.print(MESSAGES.RESULT);
+    validateNumberOfAttemptsInput(numberOfAttempts);
+    Console.print(MESSAGES.RESULT);
 
-        [...Array(parseInt(attemptsNumber))].forEach(() => {
-          carArr.forEach((car) => {
-            const randomNum = Random.pickNumberInRange(0, 9);
-            if (randomNum >= 4) {
-              map.set(car, map.get(car) + 1);
-            }
+    [...Array(parseInt(numberOfAttempts))].forEach(() => {
+      cars.forEach((car) => {
+        const randomNum = Random.pickNumberInRange(0, 9);
 
-            Console.print(`${car} : ${'-'.repeat(map.get(car))}`);
-          });
-          Console.print('\n');
-        });
+        if (randomNum >= 4) {
+          carMap.set(car, carMap.get(car) + 1);
+        }
 
-        const winners = [];
-        const sortCars = [...map].sort((a, b) => b[1] - a[1]);
-        const max = sortCars[0][1];
-
-        sortCars.forEach(([car, carNum]) => {
-          if (carNum === max) {
-            winners.push(car);
-          }
-        });
-
-        Console.print(MESSAGES.WINNER.concat(winners.join(', ')));
+        Console.print(`${car} : ${'-'.repeat(carMap.get(car))}`);
       });
+      Console.print('\n');
     });
+
+    const winners = [];
+    const sortCars = [...carMap].sort((a, b) => b[1] - a[1]);
+    const max = sortCars[0][1];
+
+    sortCars.forEach(([car, cnt]) => {
+      if (cnt === max) {
+        winners.push(car);
+      }
+    });
+
+    Console.print(MESSAGES.WINNER.concat(winners.join(', ')));
   }
 }
 
