3038ba7a621d256b5608f160df1179456f462cc9
hypnotes <hyp0notes@gmail.com>
2023-11-01 22:25:48 +0900

    feat(constructors) : create necessary variables for class

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
-}
+  constructor() {
+    this.cars = new Map();
+    this.tryOut = 0;
+    this.randomNumber = 0;
+    this.dashes = '';
+  }
 
 export default App;
