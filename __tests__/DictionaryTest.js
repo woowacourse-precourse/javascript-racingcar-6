1df7fa3256d61572391df8b5ca45abcf7dabf87b
junghanlee <dlwjdgks126@hansung.ac.kr>
2023-11-01 15:40:49 +0900

    딕셔너리 null 확인 수정

--- __tests__/DictionaryTest.js
+++ __tests__/DictionaryTest.js
@@ -8,6 +8,7 @@ describe("딕셔너리 확인", () => {
     const CAR_DICT = await app.setCarDict(CAR_INPUT);
 
     expect(CAR_DICT).toBeTruthy();
+    expect(Object.keys(CAR_DICT).length).toBeGreaterThan(0);
   });
   test("딕셔너리 타입 확인", async () => {
     const CAR_INPUT = ["pobi,woni"];
@@ -17,4 +18,5 @@ describe("딕셔너리 확인", () => {
 
     expect(typeof CAR_DICT).toEqual('object');
   })
+
 });
