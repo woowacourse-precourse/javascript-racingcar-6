a7924be3f70de9f640813debb954e965caeeb7a5
이동주 <leednj0113@naver.com>
2023-11-01 12:15:12 +0900

    Revert "docs: 기능별 실행할 순서 변경"
    This reverts commit 15c24d67a2fecd7babfbc0ee11b9b156c2a97877.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
-import { Random, Console } from "@woowacourse/mission-utils";
 class App {
   async play() {}
-  constructor() {
-    this.cars = {};
-    this.tryCount = 0;
-  }
 }
 
 export default App;
