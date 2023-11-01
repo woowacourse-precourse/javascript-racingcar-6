2f2cd94d18b1f5da76e2061789c723118b19f5b4
yunji <dbswl330@gmail.com>
2023-11-01 17:31:49 +0900

    refactor: create carNames class
    move functions for carNames from racingCars class to caeNames class

---
+++ src/model/car/carNames.js
@@ -0,0 +1,51 @@
+class Names {
+  #names = [];
+
+  getNames() {
+    return this.#names;
+  }
+
+  createValidNameList(inputNamesString) {
+    this.checkInputNull(inputNamesString);
+    this.parseCarNames(inputNamesString);
+    this.checkNamesDuplication();
+    this.checkNameBlank();
+    this.checkNameLength();
+  }
+
+  checkInputNull(inputNamesString) {
+    if (inputNamesString === "") {
+      throw new Error("[ERROR] 이름을 입력해 주세요.");
+    }
+  }
+
+  parseCarNames(inputNamesString) {
+    this.#names = inputNamesString.split(",").map((name) => name.trim());
+  }
+
+  checkNamesDuplication() {
+    const uniqueNames = new Set(this.#names);
+
+    if (uniqueNames.size !== this.#names.length) {
+      throw new Error("[ERROR] 중복된 이름이 있습니다.");
+    }
+  }
+
+  checkNameBlank() {
+    for (let i = 0; i < this.#names.length; i += 1) {
+      if (this.#names[i] === "") {
+        throw new Error("[ERROR] 이름은 공백이 될 수 없습니다.");
+      }
+    }
+  }
+
+  checkNameLength() {
+    for (let i = 0; i < this.#names.length; i += 1) {
+      if (this.#names[i].length > 5) {
+        throw new Error("[ERROR] 5글자 이하의 이름을 입력해주세요.");
+      }
+    }
+  }
+}
+
+export default Names;
--- src/model/car/racingCars.js
+++ src/model/car/racingCars.js
@@ -1,59 +1,20 @@
 import RacingCar from "./racingCar.js";
+import Names from "./carNames.js";
 
 class RacingCars {
   #cars = [];
+
+  constructor() {
+    this.carNames = new Names();
+  }
 
   getCars() {
     return this.#cars;
   }
 
-  checkInputNull(inputNamesString) {
-    if (!inputNamesString) {
-      throw new Error("[ERROR] 이름을 입력해 주세요.");
-    }
-  }
-
-  parseCarNames(inputNamesString) {
-    const carNamesList = inputNamesString.split(",").map((name) => name.trim());
-    return carNamesList;
-  }
-
-  checkDuplicateNames(carNames) {
-    const uniqueNames = new Set(carNames);
-
-    if (uniqueNames.size !== carNames.length) {
-      throw new Error("[ERROR] 중복된 이름이 있습니다.");
-    }
-  }
-
-  checkNameBlank(carNames) {
-    carNames.forEach((carName) => {
-      if (carName === "") {
-        throw new Error("[ERROR] 이름은 공백이 될 수 없습니다.");
-      }
-    });
-  }
-
-  checkNameLength(carNames) {
-    carNames.forEach((carName) => {
-      if (carName.length > 5) {
-        throw new Error("[ERROR] 5글자 이하의 이름을 입력해주세요.");
-      }
-    });
-  }
-
-  carNamesValidation(inputNamesString) {
-    this.checkInputNull(inputNamesString);
-    const carNames = this.parseCarNames(inputNamesString);
-    this.checkNameBlank(carNames);
-    this.checkDuplicateNames(carNames);
-    this.checkNameLength(carNames);
-    return carNames;
-  }
-
   createCars(inputNamesString) {
-    const carNames = this.carNamesValidation(inputNamesString);
-    carNames.forEach((car) => {
+    this.carNames.createValidNameList(inputNamesString);
+    this.carNames.getNames().forEach((car) => {
       this.createCar(car);
     });
   }
