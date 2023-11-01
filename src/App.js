a5f35fd0e90c651a0baee8e97503caeb76b9a7d9
Kykim0818 <kykim8180@gmail.com>
2023-11-01 18:15:32 +0900

    feat: 자동차 이름 입력 받기 (문구 출력 및 이름 변수 초기화)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const carNames = await this.enterCarnames();
+  }
+
+  async enterCarnames() {
+    const inputCarNames = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    return inputCarNames.split(",");
+  }
 }
 
 export default App;
