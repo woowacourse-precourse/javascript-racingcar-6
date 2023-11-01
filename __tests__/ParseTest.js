b896c24799cf2d7347a3a499bf76194fec69648c
fecapark <fecapark@gmail.com>
2023-11-01 17:28:38 +0900

    test: parse.js/makeZeroValuedObjectFromKeys 함수 테스트 작성

--- __tests__/ParseTest.js
+++ __tests__/ParseTest.js
@@ -1,5 +1,5 @@
 import ScoreBoard from "../src/core/scoreboard.js";
-import { makeScoreboardByNames } from "../src/utils/parse.js";
+import { makeZeroValuedObjectFromKeys } from "../src/utils/parse.js";
 
 describe("형 변환 테스트", () => {
   test("이름들로 빈 스코어보드 만들기 테스트", () => {
@@ -9,6 +9,21 @@ describe("형 변환 테스트", () => {
     inputs.forEach((input, index) => {
       const scoreboard = new ScoreBoard(input);
       expect(scoreboard.board).toEqual(outputs[index]);
+    });
+  });
+
+  test("key들에 대해 값이 전부 0인 객체 만들기 테스트", () => {
+    const inputs = [["a", "b", "c"], [], [""], ["a", "", "c", "c"]];
+    const outputs = [
+      { a: 0, b: 0, c: 0 },
+      {},
+      { "": 0 },
+      { a: 0, "": 0, c: 0 },
+    ];
+
+    inputs.forEach((input, index) => {
+      const zeroValuedObject = makeZeroValuedObjectFromKeys(input);
+      expect(zeroValuedObject).toEqual(outputs[index]);
     });
   });
 });
