7214a3924e82b86c8a762435fd90e4fafddd8fe9
kimmoonju-102 <kimmun3549@naver.com>
2023-11-01 23:46:48 +0900

    feat : 사용자 출력받기

--- src/Racingcar/views/outputView.js
+++ src/Racingcar/views/outputView.js
@@ -2,6 +2,7 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 class Output {
   static printCurrentPositions(carData) {
+    MissionUtils.Console.print("실행결과");
     carData.forEach((car) => {
       const positionIndicator = "-".repeat(car.position);
       MissionUtils.Console.print(`${car.name} : ${positionIndicator}`);
