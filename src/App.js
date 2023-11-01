2a774b58b1c9109981e0c9008b155b104e806448
이동주 <leednj0113@naver.com>
2023-11-01 12:22:27 +0900

    feat: 생성자 및 임포트 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+
 class App {
   async play() {}
+  constructor() {
+    this.cars = {};
+    this.tryCount = 0;
+  }
+
 }
 
 export default App;
