b38f720c97e581ab35be5193c7df898725cfd64f
parkblo <badmuxi@gmail.com>
2023-11-01 19:32:55 +0900

    Feat: 경주할 자동차 이름, 시도할 횟수 입력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Random, Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  constructor() {
+    this.carNames = [];
+    this.playCount;
+  }
+  
+  async play() {
+    await this.userInput();
+    
+  }
+
+  async userInput() {
+    this.carNames = (await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")).split(",");
+    this.playCount = (await Console.readLineAsync("시도할 횟수는 몇 회인가요?"));
+    this.playCount = Number(this.playCount);
+  }
+
+
 }
 
 export default App;
