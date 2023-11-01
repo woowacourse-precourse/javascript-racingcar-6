e1a456cbcd741d0a43005cda0c64125a32bcfa73
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-11-01 23:46:23 +0900

    feat:에러문 띄어쓰기 추가

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 import { DIVIDER, ERROR_PREFIX } from './constants.js';
 
 export const errorMessage = (issue) => {
-  return `${ERROR_PREFIX}${issue}`;
+  return `${ERROR_PREFIX} ${issue}`;
 };
 
 export const stringToArray = (input) => {
