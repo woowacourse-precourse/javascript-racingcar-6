d088835653e3c2e5144a4293b6b62af9e0d27972
j1hwan <jihwan@pm.me>
2023-11-01 19:54:02 +0900

    feat: play 활성화

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import Race from './PlayRacing.js';
+import Car from './model/Car.js';
+import { getCarNames, getGameCount } from './Input.js';
+
 class App {
-  async play() {}
+  async play() {
+    const carNames = await getCarNames();
+    const gameCount = await getGameCount();
+
+    const cars = carNames.map((name) => new Car(name));
+    const race = new Race(cars, gameCount);
+    race.start();
+  }
 }
 
 export default App;
