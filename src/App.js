1b974f523081b3306f0fcea58bed842b7423f782
jaeml00 <jaeml0630@gmail.com>
2023-11-01 19:57:03 +0900

    style: delete white line

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import CarRacingGame from './CarRacingGame';
 class App {
   async play() {
     const CarRacing = new CarRacingGame();
-
     await CarRacing.gameStart().catch((error) => {
       throw error;
     });
