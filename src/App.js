e800094c87d2164219c6d33324ffaebf5332763d
mincheol.shin <shinmch22@naver.com>
2023-11-01 22:51:11 +0900

    Feat: message 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import Game from './game.js';
+import { ANNOUNCEMENT } from './message.js';
 
 class App {
   async play() {
     const game = new Game();
     await game.start();
     for (let i = 0; i < game.numberOfAttempt; i++) {
-      if (i === 0) MissionUtils.Console.print(`\n실행 결과`);
+      if (i === 0) MissionUtils.Console.print(ANNOUNCEMENT.EXECUTION);
       game.execution();
     }
     game.end();
