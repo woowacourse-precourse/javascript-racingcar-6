2677af66a337c0becf22d55c95e200de5e367753
hzkkong <khj0227798@gmail.com>
2023-11-01 15:44:30 +0900

    🔅출력문자 수정

--- src/Logs.js
+++ src/Logs.js
@@ -1,7 +1,7 @@
 export const PLAYER_INPUT = {
     CARS_NAME_PROMPT:
         '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
-    TRY_NUMBER_PROMPT: '시도할 횟수는 몇 회 인가요?',
+    TRY_NUMBER_PROMPT: '시도할 횟수는 몇 회인가요?',
 };
 
 export const RACE = {
