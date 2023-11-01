d7787935b309aec3bc834a3c3dbcb0edfe0dba67
ccconac <qwerpoiu91@naver.com>
2023-11-01 15:17:59 +0900

    :white_check_mark: test: 우승자 안내 문구 검증 테스트 코드 추가

---
+++ __tests__/WinnerTest.js
@@ -0,0 +1,40 @@
+import Winner from '../src/Winner';
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe('게임 우승자 출력 검증 테스트 ', () => {
+  test('단독 우승자일 경우 안내 문구 출력', () => {
+    const forward = new Map([
+      ['pobi', '----'],
+      ['woni', '--'],
+      ['jun', '---'],
+    ]);
+
+    const winner = new Winner(forward);
+    const logSpy = getLogSpy();
+
+    winner.getRaceWinner();
+
+    expect(logSpy).toHaveBeenCalledWith('\n최종 우승자 : pobi');
+  });
+
+  test('공동 우승자가 존재할 경우 쉼표(,)를 이용하여 구분 후 안내 문구 출력', () => {
+    const forward = new Map([
+      ['pobi', '----'],
+      ['woni', '--'],
+      ['jun', '----'],
+    ]);
+
+    const winner = new Winner(forward);
+    const logSpy = getLogSpy();
+
+    winner.getRaceWinner();
+
+    expect(logSpy).toHaveBeenCalledWith('\n최종 우승자 : pobi, jun');
+  });
+});
