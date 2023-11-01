f5228c57e6fa801881e3fd885b0f2b2f42a6cfea
Sunghyun627 <kksscc4@gmail.com>
2023-11-01 14:40:12 +0900

    feat: 입력에 공백이 포함된 경우 에러처리

--- src/Constants.js
+++ src/Constants.js
@@ -5,8 +5,10 @@ export const GUIDE_MESSAGES = Object.freeze({
 
 export const ERROR_MESSAGES = Object.freeze({
   EMPTY_INPUT: '[ERROR] 아무것도 입력하지 않았습니다.',
-  NOT_VALID_CARNAME_LENGTH: '[ERROR] 올바른 차 이름의 길이가 아닙니다.',
-  INCLUDE_NON_NUMERIC_VALUES: '[ERROR] 입력값에 숫자 이외의 값이 포함되어 있습니다.',
+  INVALID_CARNAME_LENGTH: '[ERROR] 올바른 차 이름의 길이가 아닙니다.',
+  INCLUDE_BLANK: '[ERROR] 입력값에 공백이 포함되어 있습니다.',
+  INCLUDE_NON_NUMERIC_VALUES: '[ERROR] 시도 횟수에 숫자 이외의 값이 포함되어 있습니다.',
+  SMALLER_THAN_ONE: '[ERROR] 시도 횟수의 입력값으로 1보다 작은 값이 들어왔습니다.',
 });
 
 export const MOVE_FORWARD_CRITERIA = 4;
--- src/Validation.js
+++ src/Validation.js
@@ -1,30 +1,31 @@
 import { ERROR_MESSAGES } from './Constants.js';
 
 export const isValidCarNames = (carNames) => {
-  if (!isNotEmptyCarNames(carNames)) throw new Error(ERROR_MESSAGES.EMPTY_INPUT);
-  if (!carNames.every(isValidCarNameLength))
-    throw new Error(ERROR_MESSAGES.NOT_VALID_CARNAME_LENGTH);
+  if (!isEmptyCarNames(carNames)) throw new Error(ERROR_MESSAGES.EMPTY_INPUT);
+  if (!carNames.every(isValidCarNameLength)) throw new Error(ERROR_MESSAGES.INVALID_CARNAME_LENGTH);
   return true;
 };
 
-export const isValidTryNum = (input) => {
-  if (!isNotEmptyTryNum(input)) throw new Error(ERROR_MESSAGES.EMPTY_INPUT);
+export const isValidTryNum = (input, tryNum) => {
+  if (!isEmptyTryNum(input)) throw new Error(ERROR_MESSAGES.EMPTY_INPUT);
+  if (isIncludeBlank(input)) throw new Error();
   if (!isNumber(input)) throw new Error(ERROR_MESSAGES.INCLUDE_NON_NUMERIC_VALUES);
+  if (!isGreaterThanZero(input)) throw new Error(ERROR_MESSAGES.SMALLER_THAN_ONE);
   return true;
 };
 
-const isNotEmptyCarNames = (input) => {
-  return input.length !== 0;
+const isEmptyCarNames = (carNames) => {
+  return carNames.length !== 0;
 };
 
 const isValidCarNameLength = (carName) => {
   return carName.length !== 0 && carName.length <= 5;
 };
 
-const isNotEmptyTryNum = (input) => {
+const isEmptyTryNum = (input) => {
   return input !== undefined;
 };
 
-const isNumber = (input) => {
-  return !input.includes(' ') && !Number.isNaN(Number(input));
+const isIncludeBlank = (input) => {
+  return input.includes(' ');
 };
