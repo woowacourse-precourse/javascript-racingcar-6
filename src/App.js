e19cb87cc0a57a448e4feb94032b460bc7a56d85
Jaehyeok <milkboy2564@naver.com>
2023-11-01 14:30:17 +0900

    feat: 자동차 전진 및 우승자 판단 기능 완성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import { Console } from "@woowacourse/mission-utils";
+import { makeRandomNumber } from "./utils/func.js";
 
 class App {
   #carDistanceTable;
   #tryCount;
 
+  initializeDistance(carNameList) {
+    this.#carDistanceTable = carNameList.reduce((acc, cur) => {
+      acc[cur] = 0;
+      return acc;
+    }, {});
+  }
+
   async play() {
     await this.receiveCarNameInput();
     await this.receiveTryCountInput();
+
+    for (let i = 0; i < this.#tryCount; i++) {
+      this.accumulateDistanceEachCar();
+      this.printDistanceResult();
+    }
+
+    const winners = this.chooseWinner();
+    this.printWinnerResult(winners);
   }
 
   async receiveCarNameInput() {
@@ -21,36 +37,64 @@ class App {
         "[ERROR] 자동차의 이름은 쉼표로 구분된 5자 이하 문자만 가능합니다. 프로그램을 종료합니다."
       );
     }
-  }
 
-  initializeDistance() {
-    this.#carDistanceTable = carNameList.reduce((acc, cur) => {
-      acc[cur] = 0;
-      return acc;
-    }, {});
+    this.initializeDistance(carNameList);
   }
 
   async receiveTryCountInput() {
-    this.#tryCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
-  }
+    const tryCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
 
-  validateUserInput() {
-    const isInvalidCount = isNaN(parseInt(this.#tryCount));
-
-    if (isInvalidCount) {
+    if (this.validateTryCountInput(tryCount)) {
       throw new Error(
         "[ERROR] 입력값이 유효하지 않습니다. 프로그램을 종료합니다."
       );
     }
+
+    this.#tryCount = parseInt(tryCount);
   }
 
-  accumulateDistanceEachCar() {}
+  validateTryCountInput(tryCount) {
+    return isNaN(parseInt(tryCount));
+  }
 
-  printDistanceResult() {}
+  accumulateDistanceEachCar() {
+    for (let i = 0; i < Object.entries(this.#carDistanceTable).length; i++) {
+      const randomNumber = makeRandomNumber();
+      const key = Object.keys(this.#carDistanceTable)[i];
 
-  calculateDistance() {}
+      if (randomNumber < 4) continue;
 
-  printWinnerResult() {}
+      this.#carDistanceTable[key] = this.#carDistanceTable[key] + 1;
+    }
+  }
+
+  printDistanceResult() {
+    for (let i = 0; i < Object.entries(this.#carDistanceTable).length; i++) {
+      const key = Object.keys(this.#carDistanceTable)[i];
+      Console.print(`${key} : ${"-".repeat(this.#carDistanceTable[key])}`);
+    }
+
+    Console.print("\n");
+  }
+
+  chooseWinner() {
+    if (Object.keys(this.#carDistanceTable).length === 0) {
+      return [];
+    }
+
+    const values = Object.values(this.#carDistanceTable);
+    const maxValue = Math.max(...values);
+    const keysWithMaxValue = Object.keys(this.#carDistanceTable).filter(
+      (key) => this.#carDistanceTable[key] === maxValue
+    );
+
+    return keysWithMaxValue;
+  }
+
+  printWinnerResult(winners) {
+    const finalWinner = winners.join(", ");
+    Console.print(`최종 우승자 : ${finalWinner}`);
+  }
 }
 
 export default App;
