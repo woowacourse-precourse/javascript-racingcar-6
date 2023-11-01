ba2831b9409eef404ee165172ec871d74c6a7edf
8ark <sorlti6952@gmail.com>
2023-11-01 17:19:02 +0900

    Feat : "자동차 할당 메소드 작성"
    Feat : "자동차 할당 메소드 작성"
    
    MissionUtils를 이용하여 입력받은 자동차들을 쉼표 기준으로 분리함
    입력값에 대한 예외처리 수행

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
+  async car_assign() {
+    let users = MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+
+    if (users === "") {
+      throw new Error("[ERROR] 공백은 입력할 수 없습니다");
+    }
+
+    users = users.split(",");
+
+    users.forEach((user) => {
+      if (user.trim() === "") {
+        throw new Error("[ERROR] 공백은 입력할 수 없습니다");
+      }
+      if (user.length > 5) {
+        throw new Error("[ERROR] 5자 이하의 자동차 이름만 가능합니다.");
+      }
+    });
+    return users;
+  }
+
+  async get_random_number() {}
+
+  async play_racing(users, cnt) {}
+
+  async get_winner() {}
+
   async play() {
-    console.log("Hello, world!");
+    const USERS = this.car_assign();
   }
 }
 
