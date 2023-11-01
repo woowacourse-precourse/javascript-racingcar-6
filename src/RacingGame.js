00be7e3dea7c17deda7ce376fc0acddb95f52f2f
seung <dmdgpdi@naver.com>
2023-11-01 18:25:41 +0900

    feat(RacingGame): getCarNames 메서드 구현

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -1,4 +1,6 @@
-import { ERROR } from './constants/constants';
+import { Random, Console } from '@woowacourse/mission-utils';
+import Car from './Car';
+import { ERROR, REGEX, CONSOLE_MESSAGE } from './constants/constants';
 
 class RacingGame {
   #cars;
@@ -9,6 +11,32 @@ class RacingGame {
     this.#cars = cars;
     this.#moveCount = moveCount;
     this.#winners = winners;
+  }
+
+  async getCarNames() {
+    try {
+      const input = await Console.readLineAsync(
+        CONSOLE_MESSAGE.ENTER_CAR_NAMES
+      );
+      const separatedInput = input.match(REGEX.COMMA_SEPARATED) || [];
+
+      if (separatedInput.length === 0) {
+        throw new Error(ERROR.printError(ERROR.EMPTY_INPUT));
+      }
+
+      const carNames = [...separatedInput];
+
+      for (const carName of carNames) {
+        if (!this.hasDuplicateCarName(carName)) {
+          const newCar = new Car(carName);
+          this.#cars.push(newCar);
+        }
+      }
+
+      return;
+    } catch (e) {
+      throw e;
+    }
   }
 
   hasDuplicateCarName(carName) {
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,7 @@
 export const ERROR = Object.freeze({
   INVALID_CAR_NAME: '형식에 맞지 않는 차 이름입니다.',
   DUPLICATE_CAR_NAME: '이미 존재하는 차 이름입니다.',
+  EMPTY_INPUT: '입력은 비어있거나 쉼표만 포함해서는 안됩니다.',
   printError(message) {
     console.error(`[ERROR] : ${message}`);
   },
@@ -8,9 +9,15 @@ export const ERROR = Object.freeze({
 
 export const REGEX = Object.freeze({
   VALID_CAR_NAME: /^[^\0,]{1,5}$/,
+  COMMA_SEPARATED: /[^,]+/g,
 });
 
 export const CONSTANT = Object.freeze({
   MOVEMENT_THRESHOLD: 4,
   PROGRESS_BAR: '-',
+});
+
+export const CONSOLE_MESSAGE = Object.freeze({
+  ENTER_CAR_NAMES:
+    '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
 });
