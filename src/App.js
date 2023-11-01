5ece26e57c59f6780b8e0232f03a13f50fcddea3
2NNS-V <msy3824@naver.com>
2023-11-01 21:43:16 +0900

    feat: 시도 횟수 입력

--- src/App.js
+++ src/App.js
@@ -7,6 +7,19 @@ class App {
     this.winner = [];
     this.attempt = 0;
   }
+  
+  setCar(car) {
+    this.car = car;
+  }
+
+  setAttempt(attempt) {
+    return this.attempt;
+  }
+
+  setData(length) {
+    this.data = new Array(length).fill("");
+  }
+
 
   async getCarName() {
     const carName = (
@@ -14,7 +27,7 @@ class App {
         "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
       )
     ).split(",");
-    App.CarNameValidator(carName);
+    return carName;
   }
   
   static CarNameValidator(CarName) {
@@ -22,8 +35,46 @@ class App {
       throw new Error("[ERROR] 이름은 5자 이하가 되어야 합니다.");
     }
   }
+
+  async getAttempt() {
+    const attempt = (
+      await Console.readLineAsync(
+        "시도할 횟수는 몇 회인가요?\n"
+      )
+    );
+    return attempt;
+  }
+
+  static AttemptValidator(attempt) {
+    try {
+      if (attempt < 1) {
+        throw new Error("[ERROR] 시도 횟수는 1회 이상이어야 합니다.\n");
+      }
+      Number(attempt)
+    }
+    catch(e) {
+      throw new Error("[ERROR] 숫자가 잘못된 형식입니다.\n");
+    }
+  }
+
+  async init() { 
+    const carName = await this.getCarName();
+    App.CarNameValidator(carName);
+
+    this.setCar(carName);
+
+    const attempt = await this.getAttempt();
+    App.AttemptValidator(attempt);
+
+    this.setAttempt(attempt);
+    this.setData(this.car.length);
+  }
+  
   
   async play() {
+    await this.init();
+    Console.print("실행 결과\n");
+
   }
 }
 
