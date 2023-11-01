793424ed8e841e04694e61917de4be56133f74af
Myomyoring <narcissuscat@naver.com>
2023-11-01 20:09:17 +0900

    [test] 자동차 입력 테스트

--- __tests__/MyTest.js
+++
@@ -1,9 +0,0 @@
-describe('입력 값 테스트', () => {
- test('split 메소드로 값 구분', () => {
-  const input = 'a,b';
-  const result = input.split(',');
-
-  expect(result).toContain('a', 'b');
-  expect(result).toContainEqual('a', 'b');
- });
-});
