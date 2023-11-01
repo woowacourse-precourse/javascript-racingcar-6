80b4771a05280d7ff39e6a47c84b2c472360e58c
hojkim77 <rlaghwnd4534@gmail.com>
2023-11-01 10:54:08 +0900

    [test] 게임 전체 실행 입출력 test 수정

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,5 +1,5 @@
-import App from "../src/App.js";
-import { MissionUtils } from "@woowacourse/mission-utils";
+import App from '../src/App.js';
+import { MissionUtils } from '@woowacourse/mission-utils';
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -18,19 +18,33 @@ const mockRandoms = (numbers) => {
 };
 
 const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
   logSpy.mockClear();
   return logSpy;
 };
 
-describe("자동차 경주 게임", () => {
-  test("전진-정지", async () => {
+describe('자동차 경주 게임', () => {
+  test('게임 전체 실행', async () => {
     // given
-    const MOVING_FORWARD = 4;
+    const GO = 4;
     const STOP = 3;
-    const inputs = ["pobi,woni", "1"];
-    const outputs = ["pobi : -"];
-    const randoms = [MOVING_FORWARD, STOP];
+    const inputs = ['hoj77,ghwnd,KHJ', '4'];
+    const outputs = [
+      'hoj77 : -',
+      'ghwnd :',
+      'KHJ : -',
+      'hoj77 : --',
+      'ghwnd : -',
+      'KHJ : -',
+      'hoj77 : --',
+      'ghwnd : --',
+      'KHJ : --',
+      'hoj77 : ---',
+      'ghwnd : --',
+      'KHJ : ---',
+      '최종 우승자 : hoj77, KHJ',
+    ];
+    const randoms = [GO, STOP, GO, GO, GO, STOP, STOP, GO, GO, GO, STOP, GO];
     const logSpy = getLogSpy();
 
     mockQuestions(inputs);
@@ -44,19 +58,5 @@ describe("자동차 경주 게임", () => {
     outputs.forEach((output) => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
     });
-  });
-
-  test.each([
-    [["pobi,javaji"]],
-    [["pobi,eastjun"]]
-  ])("이름에 대한 예외 처리", async (inputs) => {
-    // given
-    mockQuestions(inputs);
-
-    // when
-    const app = new App();
-
-    // then
-    await expect(app.play()).rejects.toThrow("[ERROR]");
   });
 });
