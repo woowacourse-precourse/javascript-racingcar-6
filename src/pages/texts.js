5b931dde0337e0c33e0407d5bf669282c3f065e3
Dobbymin <gmin0701@knu.ac.kr>
2023-11-01 23:34:52 +0900

    [STYLE]: 전체적인 스타일 변경

--- src/pages/texts.js
+++ src/pages/texts.js
@@ -1,15 +1,27 @@
+/** 에러 출력  */
 export const ERROR = {
-    COUNT: '[ERROR] 숫자만 입력해주세요.',
-    NAME: '[ERROR] 이름은 문자만 입력해주세요.',
-    LENGTH: '[ERROR] 이름은 5자 이하로 입력해주세요.',
-    DOUBLE: '[ERROR] 서로 다른 이름을 입력해주세요.',
-    INCLUDE_0: '[ERROR] 빈칸은 입력할 수 없습니다.',
+    /** 숫자만 입력 */
+    count: '[ERROR] 숫자만 입력해주세요.',
+    name: '[ERROR] 이름은 문자만 입력해주세요.',
+    length: '[ERROR] 이름은 5자 이하로 입력해주세요.',
+    double: '[ERROR] 서로 다른 이름을 입력해주세요.',
+    blank: '[ERROR] 빈칸은 입력할 수 없습니다.',
 };
 
+/** 자동차 경주 문구 출력 */
 export const CAR = {
-    INPUT: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
-    COUNT: '시도할 횟수는 몇 회인가요?\n',
-    RESULT: '\n실행 결과',
-    WINNER: '최종 우승자 : ',
-    PROCESS_BAR: '-',
+    /** 자동차 이름 */
+    input: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+    /** 시도 횟수 */
+    count: '시도할 횟수는 몇 회인가요?\n',
+    /** 실행 결과 */
+    result: '\n실행 결과',
+    /** 최종 우승자 */
+    winner: '최종 우승자 : ',
+    /** 게임 진행상황 */
+    process_bar: '-',
+};
+
+export const LETTER = {
+    comma: ',',
 };
