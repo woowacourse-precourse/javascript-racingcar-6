ac93600eec00a33e0607cb2cb08c05d2570156f5
mieum12 <qpdlqltb1215@gmail.com>
2023-11-01 18:06:17 +0900

    test 정규표현식

--- src/view/inputValidator.js
+++ src/view/inputValidator.js
@@ -10,13 +10,16 @@ export class InputValidator {
    * @returns {void}
    */
   validateCarNamesInput(input) {
-    // null값이거나 빈칸이 들어간 경우
-    if (input == null || input.includes(" ")) {
-      throw new Error(ERROR.BLANK_ERROR);
-    }
-    //쉼표로 시작하거나 끝난 경우
-    if (input.charAt(0) === "," || input.charAt(input.length - 1) === ",") {
-      throw new Error(ERROR.COMMA_ERROR);
+    // // null값이거나 빈칸이 들어간 경우
+    // if (input == null || input.includes(" ")) {
+    //   throw new Error(ERROR.BLANK_ERROR);
+    // }
+    // //쉼표로 시작하거나 끝난 경우
+    // if (input.charAt(0) === "," || input.charAt(input.length - 1) === ",") {
+    //   throw new Error(ERROR.COMMA_ERROR);
+    // }
+    if (!/^(\w+,)+\w+$/.test(input)) {
+      throw new Error("[ERROR] 에러에러"); // 유효하지 않은 차이름 오류 타입 생성
     }
   }
 
@@ -29,9 +32,13 @@ export class InputValidator {
    */
 
   validateAttemptCountInput(input) {
-    //숫자가 아닌 경우
-    if (isNaN(Number(input))) throw new Error(ERROR.NUMBER_ONLY_ERROR);
-    // 0보다 작은 경우 (0회 가능하게 일단 만듦)
-    if (Number(input) < 0) throw new Error(ERROR.NAME_RANGE_ERROR);
+    // //숫자가 아닌 경우
+    // if (isNaN(Number(input))) throw new Error(ERROR.NUMBER_ONLY_ERROR);
+    // // 0보다 작은 경우 (0회 가능하게 일단 만듦)
+    // if (Number(input) < 0) throw new Error(ERROR.NAME_RANGE_ERROR);
+    if (!/^\d+$/.test(input)) {
+      throw new Error(ERROR.NUMBER_ONLY_ERROR);
+    }
+    if (input < 0) throw new Error(ERROR.NAME_RANGE_ERROR);
   }
 }
