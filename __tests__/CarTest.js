48fc20fa82bd2e7f3402e5a8594f55b930a151b8
rbgksqkr <rbgks1937@naver.com>
2023-11-01 21:54:36 +0900

    test: 최종 우승자 출력하는 테스트 코드 작성

--- __tests__/CarTest.js
+++ __tests__/CarTest.js
@@ -23,16 +23,32 @@ const getLogSpy = () => {
 };
 
 describe("CarList 메소드 테스트", () => {
-  test("자동차 이름이 여러개 입력했을 때 split 되는 지 체크", () => {
+  it("자동차 이름이 여러개 입력했을 때 split 되는 지 체크", () => {
     const carList = new CarList();
     carList.setCarList("park,gyu,han");
     expect(carList.getCarList()[0].name).toBe("park");
   });
 
-  test("1번 시도했을 때 CarList 점수를 출력하는 메소드", async () => {
+  it("1번 시도했을 때 CarList 점수를 출력", async () => {
     const inputs = ["park,gyu", "1"];
     const random = [4, 3];
     const output = ["park : -", "gyu : "];
+    const logSpy = getLogSpy();
+    mockQuestions(inputs);
+    mockRandoms([...random]);
+
+    const app = new App();
+    await app.play();
+
+    output.forEach((element) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(element));
+    });
+  });
+
+  it("1번 실행 후 최종 우승자 출력", async () => {
+    const inputs = ["park,gyu", "1"];
+    const random = [3, 4];
+    const output = ["최종 우승자 : gyu"];
     const logSpy = getLogSpy();
     mockQuestions(inputs);
     mockRandoms([...random]);
