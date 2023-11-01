c14d0aefd5bf54b59d4dd61de52a2951f7f39589
JunSeok <ppu1234@naver.com>
2023-11-01 15:53:02 +0900

    style: 코드 스타일 수정

--- src/Util/inputData.js
+++ src/Util/inputData.js
@@ -1,20 +1,20 @@
 import { checkErrorInputName, checkErrorPlayNumber } from './validation.js';
 
-import { Console } from '@woowacourse/mission-utils';
+import { MissionUtils } from '@woowacourse/mission-utils';
 import { INFO_MESSAGE } from './message.js';
 
-const getPlayNumber = async () => {
-  const inputNumber = +(await Console.readLineAsync(INFO_MESSAGE.playNumber));
-  checkErrorPlayNumber(inputNumber);
-  return inputNumber;
-};
-
 const getCarName = async () => {
-  const inputNameArr = (await Console.readLineAsync(INFO_MESSAGE.carName)).split(',').map((name) => name.trim());
+  const inputNameArr = (await MissionUtils.Console.readLineAsync(INFO_MESSAGE.carName)).split(',').map((name) => name.trim());
   checkErrorInputName(inputNameArr);
   const carName = new Map();
   inputNameArr.forEach((name) => carName.set(name, ''));
   return carName;
 };
 
-export { getPlayNumber, getCarName };
+const getPlayNumber = async () => {
+  const inputNumber = +(await MissionUtils.Console.readLineAsync(INFO_MESSAGE.playNumber));
+  checkErrorPlayNumber(inputNumber);
+  return inputNumber;
+};
+
+export { getCarName, getPlayNumber };
