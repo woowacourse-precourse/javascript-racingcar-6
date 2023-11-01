bd59d68de2debf6ba0b53befb10e8eab4025c28b
kwaksj329 <kwaksj329@gmail.com>
2023-11-01 22:49:42 +0900

    fix: random 값 범위 수정

--- src/model/Racing.js
+++ src/model/Racing.js
@@ -3,7 +3,7 @@ import { Random } from "@woowacourse/mission-utils";
 class Racing {
 
   moveRacingCar(distanceList, index) {
-    if (Random.pickNumberInRange(1, 9) >= 4) {
+    if (Random.pickNumberInRange(0, 9) >= 4) {
       return ++distanceList[index];
     }
     return distanceList[index];
