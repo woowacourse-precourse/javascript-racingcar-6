86cc3481027340c377efbba2b3646a08fca9c487
정수아 <sua@jeongsuaui-MacBookAir.local>
2023-11-01 20:44:29 +0900

    [feat] 게임 횟수를 입력받고 숫자의 형식이 틀린 경우 에러 처리

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import {Console, Random} from '@woowacourse/mission-utils';
 class App {
   async play() {
-    const userInput = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.');
+    let userInput = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)
+    );
 
     const CAR_NAME_ARRAY = userInput.split(',');
     CAR_NAME.forEach(car => {
@@ -9,7 +10,22 @@ class App {
     });
     const CAR = CAR_NAME.map(name=>({name, distance: 0}));
 
+    userInput = await Console.readLineAsync('시도할 횟수는 몇 회인가요?');
+
+    const COUNT = Number(userInput);
+    if(isNaN(COUNT) || COUNT > 9 || COUNT < 0)  throw new Error('[ERROR] 잘못된 횟수를 입력했습니다. 0부터 9까지의 정수를 입력해 주세요.')
+
+    Console.print('실행 결과');
+
+    carRace(CAR, COUNT);
+
+
   }
 }
 
 export default App;
+
+export const carRace = (CAR, COUNT) => {
+  console.log(CAR);
+  console.log(COUNT);
+}
