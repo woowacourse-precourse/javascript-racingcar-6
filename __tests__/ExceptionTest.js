f56b004e2a4e69c83c1757102ea9332e14ce53ee
Dev-steadyBoy <85480086+Dev-steadyBoy@users.noreply.github.com>
2023-11-01 10:51:06 +0900

    refactor: 하드 코드를 자바스크립트 API로 수정, 코드 포맷 변경

--- __tests__/ExceptionTest.js
+++ __tests__/ExceptionTest.js
@@ -1,129 +1,105 @@
-import BaseExceptionHandler from "../src/exception/Errorcase.js";
+import BaseExceptionHandler from '../src/exception/Errorcase';
 
-import CarNaming from "../src/exception/CarNaming.js";
-import Frequency from "../src/exception/Frequency.js";
-import RaceStatus from "../src/exception/RaceStatus.js";
+import CarNaming from '../src/exception/CarNaming';
+import Frequency from '../src/exception/Frequency';
 
-describe("CarNaming클래스 - 자동차 이름 예외처리 로직", () => {
+describe('CarNaming클래스 - 자동차 이름 예외처리 로직', () => {
   let exception;
   beforeEach(() => {
     exception = new BaseExceptionHandler();
   });
 
-  test("입력값 [sasasas]", () => {
-    const input = ["sasasas"];
+  test('입력값 [sasasas]', () => {
+    const input = ['sasasas'];
     const result = () => exception.checkAllException(new CarNaming(input));
 
     expect(result).toThrow();
   });
 
-  test("입력값 [aaaaaa, bbbbbb]", () => {
-    const input = ["aaaaaa", "bbbbbb"];
+  test('입력값 [aaaaaa, bbbbbb]', () => {
+    const input = ['aaaaaa', 'bbbbbb'];
     const result = () => exception.checkAllException(new CarNaming(input));
 
     expect(result).toThrow();
   });
 
-  test("입력값을 주지 않았을 때 ", () => {
-    const input = "";
+  test('입력값을 주지 않았을 때 ', () => {
+    const input = '';
     const result = () => exception.checkAllException(new CarNaming(input));
 
     expect(result).toThrow();
   });
 
-  test("입력값(중복) [aaaaa, aaaaa]", () => {
-    const input = ["aaaaa", "aaaaa"];
+  test('입력값(중복) [aaaaa, aaaaa]', () => {
+    const input = ['aaaaa', 'aaaaa'];
     const result = () => exception.checkAllException(new CarNaming(input));
 
     expect(result).toThrow();
   });
 
-  test("입력값의 크기가 넘어갈 때", () => {
-    const input = [
-      "a",
-      "b",
-      "c",
-      "d",
-      "e",
-      "f",
-      "g",
-      "h",
-      "i",
-      "j",
-      "k",
-      "l",
-      "m",
-      "n",
-      "o",
-      "p",
-      "q",
-      "r",
-      "s",
-      "t",
-      "u",
-      "v",
-    ];
+  test('입력값의 크기가 넘어갈 때', () => {
+    const input = new Array(22).map((_, idx) => String.fromCodePoint(idx + 97));
     const result = () => exception.checkAllException(new CarNaming(input));
 
     expect(result).toThrow();
   });
 
-  test("입력값 [a   a, b   b]", () => {
-    const input = ["a   a", " b   b"];
+  test('입력값 [a   a, b   b]', () => {
+    const input = ['a   a', ' b   b'];
     const result = () => exception.checkAllException(new CarNaming(input));
 
     expect(result).toThrow();
   });
 
-  test("입력값 [우테코, 사랑해]", () => {
-    const input = ["우테코", "사랑해"];
+  test('입력값 [우테코, 사랑해]', () => {
+    const input = ['우테코', '사랑해'];
     const result = () => exception.checkAllException(new CarNaming(input));
 
     expect(result).toThrow();
   });
-  test("입력값 [aaa, , ccc]", () => {
-    const input = ["aaa", "", "ccc"];
+  test('입력값 [aaa, , ccc]', () => {
+    const input = ['aaa', '', 'ccc'];
     const result = () => exception.checkAllException(new CarNaming(input));
 
     expect(result).toThrow();
   });
-  test("입력값 [#^&@#, **#&]", () => {
-    const input = ["#^&@#", "**#&"];
+  test('입력값 [#^&@#, **#&]', () => {
+    const input = ['#^&@#', '**#&'];
     const result = () => exception.checkAllException(new CarNaming(input));
 
     expect(result).toThrow();
   });
 });
 
-describe("Frequency클래스 - 게임 횟수 예외처리 로직", () => {
+describe('Frequency클래스 - 게임 횟수 예외처리 로직', () => {
   let exception;
   beforeEach(() => {
     exception = new BaseExceptionHandler();
   });
 
-  test("입력값 1.2", () => {
+  test('입력값 1.2', () => {
     const input = 1.2;
     const result = () => exception.checkAllException(new Frequency(input));
 
     expect(result).toThrow();
   });
 
-  test("입력값 -1", () => {
+  test('입력값 -1', () => {
     const input = -1;
     const result = () => exception.checkAllException(new Frequency(input));
 
     expect(result).toThrow();
   });
 
-  test("입력값 999", () => {
+  test('입력값 999', () => {
     const input = 999;
     const result = () => exception.checkAllException(new Frequency(input));
 
     expect(result).toThrow();
   });
 
-  test("입력값 ㅇㅌㅋㅅㄹㅎ", () => {
-    const input = "ㅇㅌㅋㅅㄹㅎ";
+  test('입력값 ㅇㅌㅋㅅㄹㅎ', () => {
+    const input = 'ㅇㅌㅋㅅㄹㅎ';
     const result = () => exception.checkAllException(new Frequency(input));
 
     expect(result).toThrow();
