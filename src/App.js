7522f077978e3ff299f6a5960a03d225196e6717
hypnotes <hyp0notes@gmail.com>
2023-11-01 22:26:42 +0900

    feat(init cars) : verify and initialize cars from input

--- src/App.js
+++ src/App.js
@@ -8,4 +8,17 @@ class App {
     this.dashes = '';
   }
 
+  verifyAndInitCars(userInputCarNames) {
+    const carsArray = userInputCarNames.split(',');
+    carsArray.forEach(carName => {
+      if (carName.length > 5) {
+        throw new Error('[ERROR] 자동차 이름은 5자 이하만 가능합니다.');
+      } else if (carName.length === 0) {
+        throw new Error('[ERROR] 자동차 이름은 1자 이상이어야 합니다.');
+      } else if (this.cars.has(carName)) {
+        throw new Error('[ERROR] 자동차 이름은 중복될 수 없습니다.');
+      }
+      this.cars.set(carName, 0);
+    });
+  }
 export default App;
