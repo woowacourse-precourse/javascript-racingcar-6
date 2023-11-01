6bd2397c13a13da032928816d11b95a30848928d
LEEHYEYEONG <elto789@naver.com>
2023-11-01 10:33:41 +0900

    예외처리 - 5자리 이상의 길이를 가진 자동차 이름을 입력하였을 때 - 시도할 횟수에 숫자를 입력하지 않은 경우

--- src/App.js
+++ src/App.js
@@ -11,8 +11,10 @@ async function gameStart() {
   Console.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
   const inputCarName = await Console.readLineAsync("");
   const carName = inputCarName.split(',');
+  carNameValidation(carName);
   Console.print("시도할 횟수는 몇 회인가요?");
   const inputNumber = await Console.readLineAsync("");
+  tryValidation(inputNumber);
   const carDict = {};
   for (let i = 0; i < carName.length; i++) {
     const item = carName[i];
@@ -62,6 +64,20 @@ function gameResult(carDict) {
     }
     let strTemp = winCarName.join(', ')
     Console.print("최종 우승자 : " + strTemp)
+  }
+}
+
+function carNameValidation(carName) {
+  for (let i = 0; i < carName.length; i++) {
+    if (carName[i].length > 5) {
+      throw new Error("[ERROR] 자동차의 이름은 5자리 이하여야 합니다.");
+    }
+  }
+}
+
+function tryValidation(number) {
+  if (isNaN(number)) {
+    throw new Error("[ERROR] 숫자가 잘못된 형식입니다.");
   }
 }
 
