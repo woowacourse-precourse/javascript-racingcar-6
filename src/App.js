04c373827c16cea2ef4c8a3a17940b66e92b46eb
이원주 <turtle4265@naver.com>
2023-11-01 22:59:26 +0900

    [refactor] 상수 분리

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Console, Random } from "@woowacourse/mission-utils";
+import { NUMBER, MESSAGE, ERRORS } from "./constants.js";
 class App {
   constructor() {
     this.cars = [];
@@ -12,33 +13,31 @@ class App {
 
 async function getInput() {
   try {
-    const carNames = await Console.readLineAsync(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
-    );
+    const carNames = await Console.readLineAsync(MESSAGE.INPUT_NAMES);
 
     const carNameList = carNames.split(",");
     const isUnderFiveCharacter = carNameList.every(
-      (name) => name.trim().length <= 5
+      (name) => name.trim().length <= NUMBER.NAME_LENGTH
     );
     if (/[^a-zA-Z0-9,]/.test(carNames) || !isUnderFiveCharacter) {
-      throw new Error("[ERROR] 입력 형식이 잘못되었습니다.");
+      throw new Error(ERRORS.WRONG_INPUT);
     }
 
-    const chance = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+    const chance = await Console.readLineAsync(MESSAGE.CHANCE);
 
     if (!/^[1-9]\d*$/.test(chance)) {
-      throw new Error("[ERROR] 입력 형식이 잘못되었습니다.");
+      throw new Error(WRONG_INPUT);
     }
 
     return { chance, carNameList };
   } catch (error) {
-    throw new Error("[ERROR] 입력 형식이 잘못되었습니다.");
+    throw new Error(WRONG_INPUT);
   }
 }
 
 function playGame(chance, carNameList) {
   let runningCars = new Array(carNameList.length).fill(0);
-  Console.print("\n실행 결과");
+  Console.print(MESSAGE.RESULT);
   for (let i = 0; i < chance; i++) {
     updateCars(runningCars);
     printCars(runningCars, carNameList);
@@ -49,7 +48,7 @@ function playGame(chance, carNameList) {
 function updateCars(runningCars) {
   for (let i = 0; i < runningCars.length; i++) {
     let number = Random.pickNumberInRange(1, 9);
-    if (number >= 4) {
+    if (number >= NUMBER.CAN_MOVE) {
       runningCars[i] += 1;
     }
   }
@@ -64,17 +63,16 @@ function printCars(runningCars, carNameList) {
     Console.print(`${carNameList[i]} : ${running}`);
   }
   Console.print(" ");
-
 }
 
 function printWinner(runningCars, carNameList) {
-  const cars = runningCars.map((run, idx) => ({ name: carNameList[idx], run}));
+  const cars = runningCars.map((run, idx) => ({ name: carNameList[idx], run }));
 
   const maxRunner = Math.max(...cars.map((car) => car.run));
   const winners = cars.filter((car) => car.run === maxRunner);
   const result = winners.map((car) => car.name).join(", ");
-  
-  Console.print(`최종 우승자 : ${result}`);
+
+  Console.print(`${MESSAGE.WINNER}${result}`);
 }
 
 export default App;
