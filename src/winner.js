6df71cae78d86e47a9b60c87fb8b64679ea82f6b
psst54 <psst5491@naver.com>
2023-11-01 12:13:18 +0900

    🐛 우승자 계산 방법 수정

--- src/winner.js
+++ src/winner.js
@@ -2,14 +2,14 @@ export function getWinner(names, carsMovedCount) {
   let maxCount = -1;
   let maxCountNames = [];
 
-  for (const name in names) {
+  names.forEach((name) => {
     if (carsMovedCount[name] > maxCount) {
       maxCount = carsMovedCount[name];
       maxCountNames = [name];
     } else if (carsMovedCount[name] === maxCount) {
       maxCountNames.push(name);
     }
-  }
+  });
 
   return maxCountNames.join(", ");
 }
