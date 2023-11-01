9e871a69e343d113f2759300259ec2579d639b06
coddingyun <igy2840@gmail.com>
2023-11-01 10:08:08 +0900

    fix: 파일명 대문자로 적용된 것 git에도 적용

---
+++ src/model/TrialNum.js
@@ -0,0 +1,41 @@
+import ERROR_MESSAGE from '../constant/errorMessage';
+import messagePrinter from '../utils/messagePrinter';
+
+class TrialNum {
+  #trials;
+
+  constructor(trialNum) {
+    this.#trials = trialNum;
+    this.validate();
+  }
+
+  validate() {
+    this.validateIsNumber();
+    this.validateNumberRange();
+    this.validateNoSpace();
+  }
+
+  validateIsNumber() {
+    if (Number.isNaN(this.#trials)) {
+      messagePrinter.errorPrint(ERROR_MESSAGE.not_number);
+    }
+  }
+
+  validateNumberRange() {
+    if (this.#trials < 1) {
+      messagePrinter.errorPrint(ERROR_MESSAGE.less_than_one_trial);
+    }
+  }
+
+  validateNoSpace() {
+    if (typeof this.#trials === 'string' && this.#trials.includes(' ')) {
+      messagePrinter.errorPrint(ERROR_MESSAGE.has_space);
+    }
+  }
+
+  getTrials() {
+    return this.#trials;
+  }
+}
+
+export default TrialNum;
