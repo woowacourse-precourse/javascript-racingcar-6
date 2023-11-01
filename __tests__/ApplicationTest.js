e9c41016549063de51dd11016e892dc829af9060
kim-young-dong <opa9904@gmail.com>
2023-11-01 23:37:23 +0900

    test: 자동차 이름에 공백 입력시의 예외 처리 테스트 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -47,7 +47,7 @@ describe('자동차 경주 게임', () => {
     });
   });
 
-  test.each([[['pobi,javaji']], [['pobi,eastjun']]])(
+  test.each([[['pobi,javaji']], [['pobi,eastjun']], [['pobi, ,']]])(
     '이름에 대한 예외 처리',
     async inputs => {
       // given
