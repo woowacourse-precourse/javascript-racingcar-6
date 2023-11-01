d5a84d4805af85df5dbb4f88e5fd296f2fd74a41
조경은 <kavvy99@naver.com>
2023-11-01 22:49:24 +0900

    fix: h3태그 내용  대체되는 오류 발견 및 해결

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
-// class App {
-//   async play() {
-//     console.log("애플리케이션 시작");
-
-    
-//     console.log(MissionUtils.Random.pickNumberInRange(0, 9))
-
-//   }
-// }
-
-// export default App;
-
-// 자동차 이름 예외 처리
 const carNameInput = document.querySelector('#carName');
 const carNames = [];
 
 function carNameCheckEvent() {
   const carNames = carNameInput.value.split(",");
-  console.log(carNames);
   const carsObject = {};
   for (let i = 0; i < carNames.length; i++) {
     carsObject[carNames[i]] = 0; 
   }
-  console.log(carsObject);
-
 
   let hasError = false;
-  // 유저가 입력한 자동차 이름이 다섯 자 이하가 아니면
+
   for (i=0; i<carNames.length; i++) {
     if (carNames[i].length > 5) {
       hasError = true;
@@ -33,7 +17,6 @@ function carNameCheckEvent() {
       console.log("[ERROR] 자동자의 이름이 너무 깁니다.");
       break;
     }
-    // 유저가 입력한 값이 없으면
     else if (carNames[i].length === 0) {
       alert("[ERROR] 경주에 참여한 자동차가 없습니다.");
       console.log("[ERROR] 경주에 참여한 자동차가 없습니다.");
@@ -41,7 +24,6 @@ function carNameCheckEvent() {
     }
   } 
 
-  // 자동차 이름 관련한 에러가 없으면
   if (!hasError) {
     const body = document.querySelector('body');
     const tryNumQuestion = document.createElement('h3');
@@ -57,64 +39,53 @@ function carNameCheckEvent() {
     body.appendChild(tryNumberInput);
     body.appendChild(startRaceBtn);
 
-    // 경주 게임 시작 함수
-    // 시도 횟수 관련 에러(숫자 아님) 예외 처리 먼저
     function startRaceEvent() {
       const tryNumber = parseInt(tryNumberInput.value);
-      // console.log(parseInt(tryNumber));
-      // console.log(isNaN(tryNumber.value));
+
       if (isNaN(tryNumber) === true) {
         hasError = true;
         alert("[ERROR] 숫자가 잘못된 형식입니다.");
         console.log("[ERROR] 숫자가 잘못된 형식입니다.");
       }
       // 에러 없으면
-      else {
-        const gameStart = document.querySelector('h3');
-        gameStart.innerHTML = "실행 결과";
+        else {
+          const gameStart = document.createElement('h3');
+          gameStart.innerHTML = "실행 결과";
+          body.appendChild(gameStart);
 
-        for (const carValue in carsObject) {
-          const randomValue = MissionUtils.Random.pickNumberInRange(0, tryNumber); 
-          carsObject[carValue] = randomValue; // 랜덤값을 각 자동차의 값으로 설정
-        }
-        console.log(carsObject);
-
-        let maxCarValue = -1;
-        const winningCars = [];
-
-        // 객체를 웹 상에 출력
-        for (const car in carsObject) {
-          const carStatus = document.createElement('p');
-          const dashes = '-'.repeat(carsObject[car]);
-          carStatus.innerHTML = `${car} : ${dashes}`;
-          body.appendChild(carStatus);
-
-          if (carsObject[car] > maxCarValue) {
-            maxCarValue = carsObject[car];
-            winningCars.length = 0; // 우승자 배열 초기화
-            winningCars.push(car); // 새로운 최고 점수 우승자 추가
-          } else if (carsObject[car] === maxCarValue) {
-            winningCars.push(car); // 동점자 추가
+          for (const carValue in carsObject) {
+            const randomValue = MissionUtils.Random.pickNumberInRange(0, tryNumber); 
+            carsObject[carValue] = randomValue; // 랜덤값을 각 자동차의 값으로 설정
           }
+
+          let maxCarValue = -1;
+          const winningCars = [];
+
+          // 객체를 웹 상에 출력
+          for (const car in carsObject) {
+            const carStatus = document.createElement('p');
+            const dashes = '-'.repeat(carsObject[car]);
+            carStatus.innerHTML = `${car} : ${dashes}`;
+            body.appendChild(carStatus);
+
+            if (carsObject[car] > maxCarValue) {
+              maxCarValue = carsObject[car];
+              winningCars.length = 0; // 우승자 배열 초기화
+              winningCars.push(car); // 새로운 최고 점수 우승자 추가
+            } else if (carsObject[car] === maxCarValue) {
+              winningCars.push(car); // 동점자 추가
+            }
+          }
+
+          // 최종 우승자 출력
+          const gameResult = document.createElement('h3');
+          if (winningCars.length === 1) {
+            gameResult.innerHTML = `최종 우승자: ${winningCars[0]}`;
+          } else {
+            gameResult.innerHTML = `최종 우승자: ${winningCars.join(', ')}`;
+          }
+          body.appendChild(gameResult);
         }
-
-        // 최종 우승자 출력
-        const gameResult = document.querySelector('h3');
-        if (winningCars.length === 1) {
-          gameResult.innerHTML = `최종 우승자: ${winningCars[0]}`;
-        } else {
-          gameResult.innerHTML = `최종 우승자: ${winningCars.join(', ')}`;
-        }
-
-        body.appendChild(gameResult);
       }
-      body.appendChild(gameStart);
-      body.appendChild(gameResult);
-      
-      }
-
     }
   }
-
-
-
