6dcc5f6f14b8be68aae3bf751d023695626684b1
소인섭 <sis9745@gachon.ac.kr>
2023-11-01 17:40:30 +0900

    feat: App.js 파일에 play 기능 구현
    - 필요한 값(racingCarsInput, attemptsInput)을 입력
    - 입력 값을 토대로 CarRace 인스턴스를 생성 후 race와 printWinner 함수를 순차적으로 실행

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { inputRacingCars, inputAttempts } from './inputs';
+import CarRace from './CarRace';
+
 class App {
-  async play() {}
+  async play() {
+    const racingCarsInput = await inputRacingCars();
+    const attemptsInput = await inputAttempts();
+
+    const carRace = new CarRace(racingCarsInput, attemptsInput);
+    carRace.race();
+    carRace.printWinners();
+  }
 }
 
 export default App;
