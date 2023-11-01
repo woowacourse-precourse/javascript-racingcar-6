6bcdffe066481193552644a1dc114b8fef6abc63
aaminha <dks4857@gmail.com>
2023-11-01 18:09:14 +0900

    test: 자동차 1회 경주 테스트 추가

---
+++ __tests__/PlayRoundsTest.js
@@ -0,0 +1,62 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { playRounds } from '../src/functions/gameProgress';
+
+const mockRandoms = numbers => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe('자동차 1회 경주 테스트', () => {
+  test('랜덤 값이 3이하 시 정지', () => {
+    // given
+    const carDistanceList = [
+      { carName: 'pobi', distance: '' },
+      { carName: 'woni', distance: '' },
+      { carName: 'jun', distance: '' },
+    ];
+    const playNum = 1;
+    const outputs = ['pobi : ', 'woni : ', 'jun : '];
+    const randoms = [0, 1, 3];
+    const logSpy = getLogSpy();
+
+    mockRandoms(randoms);
+
+    // when
+    expect(playRounds(carDistanceList, playNum));
+
+    // then
+    outputs.forEach(output => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test('랜덤 값이 4이상 시 전진', () => {
+    // given
+    const carDistanceList = [
+      { carName: 'pobi', distance: '' },
+      { carName: 'woni', distance: '' },
+    ];
+    const playNum = 1;
+    const outputs = ['pobi : -', 'woni : -'];
+    const randoms = [4, 9];
+    const logSpy = getLogSpy();
+
+    mockRandoms(randoms);
+
+    // when
+    expect(playRounds(carDistanceList, playNum));
+
+    // then
+    outputs.forEach(output => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+});
