72389451bb175395c548557433a7fd63e0105d86
youngeun <jessicak0828@gmail.com>
2023-11-02 16:30:01 +0900

    refactor: 상수 key 값 좀 더 가독성 좋게 변경

--- src/constants.js
+++ src/constants.js
@@ -1,19 +1,23 @@
 export const INPUT_REQUEST = Object.freeze({
-	NAME: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
-	FREQUENCY: '시도할 횟수는 몇 회인가요?'
+	carName: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+	trialFrequency: '시도할 횟수는 몇 회인가요?'
 });
 
 export const RESULT = Object.freeze({
-	GAME_PROGRESS: '실행 결과',
-	FINAL_WINNER: '최종 우승자 : '
+	gameResult: '실행 결과',
+	finalWinner: '최종 우승자 : '
 });
 
 export const ERROR = Object.freeze({
-	NAME: '[ERROR]',
-	NO_INPUT: '값을 입력해주세요.',
-	NO_ALONE: '두 명 이상을 입력해주세요.',
-	ONLY_UNDER_FIVE: '5자리 이하만 가능합니다.',
-	ONLY_NUMBER: '숫자만 입력가능합니다.',
-	NO_ZERO: '1 이상을 입력해주세요.'
+	name: "[ERROR]",
+	//car name 
+	noInput: '값을 입력해주세요.',
+	moreThanTwo: '두 명 이상을 입력해주세요.',
+	onlyUnderFive: '5자리 이하만 가능합니다.',
+	// trial frequency
+	noBlank: '공백은 빼고 입력해주세요.',
+	onlyNumber: '숫자만 입력 가능합니다.',
+	noUnderZero: '1 이상을 입력해주세요.',
+	noDuplicate: '중복된 이름이 있습니다.'
 });
 
