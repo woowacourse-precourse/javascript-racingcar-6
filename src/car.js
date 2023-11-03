9ee96a95a20da63cba438ece80401d9762d200f8
swJaNG12 <cbdjej38483@gmail.com>
2023-11-04 00:06:48 +0900

    refactor : separate Car class from App class

---
+++ src/car.js
@@ -0,0 +1,23 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
+class Car {
+  constructor(name) {
+    this.name = name;
+    this.status = '';
+  }
+
+  move() {
+    const randomNumber = Random.pickNumberInRange(0, 9);
+    if (randomNumber >= 4) {
+      this.status += '-';
+    }
+  }
+
+  getCarStatus() {
+    return this.status;
+  }
+
+  getCarName() {
+    return this.name;
+  }
+}
