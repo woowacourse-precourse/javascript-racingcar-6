eb6c3732008e6d3b77d9711f4d96af624051e180
badahertz52 <badahertz52@gmail.com>
2023-11-08 22:35:25 +0900

    fix<Input> :  자동차 이름, 게임 횟수 유효성 테스트에서 놓친 부분 추가
    - 자동차 이름을 ","로 구분한 것인지에 대한 유효성 테스트 추가
    - 입력한 게임 횟수가 undefined,null,' '  인 경우 0으로 인식되는 오류 해결

--- src/Input.js
+++ src/Input.js
@@ -11,6 +11,7 @@ class Input {
       const text = await MissionUtils.Console.readLineAsync(MESSAGE.inputName);
       if (!isRound) {
         // 자동차 이름에 대한 input
+        this.isDelimiter(text);
         this.nameArray = text.split(RULE.delimiter);
         this.validateName();
       } else {
@@ -21,18 +22,26 @@ class Input {
       throw new Error(error);
     }
   }
-
+  isDelimiter(text) {
+    if (!text.includes(RULE.delimiter)) throw new Error(MESSAGE.delimiterError);
+  }
   validateName() {
     this.nameArray.forEach((name) => {
+      const isUndefined = !!name.replaceAll(' ', '');
       const isCorrectLength =
-        RULE.name.min < name.length && name.length <= RULE.name.max;
-      const pass = isCorrectLength && name.split('').every((i) => /.*/.test(i));
+        RULE.name.min <= name.length && name.length <= RULE.name.max;
+      const pass =
+        isCorrectLength &&
+        !isUndefined &&
+        name.split('').every((i) => /.*/.test(i));
       if (!pass) throw new Error(MESSAGE.nameError);
     });
   }
 
   validateRound(text) {
-    const pass = !Number.isNaN(Number(text)) && Number(text) >= 0;
+    const isUndefined = text !== '0' && !!!text;
+    const round = Number(text);
+    const pass = !isUndefined && !Number.isNaN(round) && round >= 0;
     if (!pass) {
       throw new Error(MESSAGE.roundError);
     } else {
