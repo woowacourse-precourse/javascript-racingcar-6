8fcfda64fd1abf2d2f0407ed5ab1af783d46cf0d
badahertz52 <badahertz52@gmail.com>
2023-11-08 21:50:21 +0900

    docs: Rule.js 생성

---
+++ src/constant/Rule.js
@@ -0,0 +1,17 @@
+const RULE = Object.freeze({
+  name: {
+    min: 0,
+    max: 5,
+  },
+  delimiter: ',',
+  round: {
+    min: 0,
+  },
+  randomNumber: {
+    min: 0,
+    max: 9,
+  },
+  pointOfMovement: 4,
+});
+
+export { RULE };
