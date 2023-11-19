3dfc9a95a08a3ffb5249f7bdc0c95799e297f27e
badahertz52 <badahertz52@gmail.com>
2023-11-19 17:59:49 +0900

    docs : 입력 쿼리 앞에 개행 추가

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -2,7 +2,7 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 const InputView = {
   async readText(query) {
-    const text = await MissionUtils.Console.readLineAsync(`${query}\n`);
+    const text = await MissionUtils.Console.readLineAsync(`\n${query}\n`);
     //뛰어쓰기,공백도 글자로 포함
     return text;
   },
