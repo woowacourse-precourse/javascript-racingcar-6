9d072a37d9b224b8ff8418d46b9fde6daca1d4cc
gouz7514 <gouz7514@gmail.com>
2023-12-05 15:09:45 +0900

    Update : App에서 gameController를 private field로 저장해서 사용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import GameController from "./Controller/GameController.js";
 
 class App {
+  #gameController;
+
   constructor() {
-    this.gameController = new GameController();
+    this.#gameController = new GameController();
   }
 
   async play() {
-    await this.gameController.init();
+    await this.#gameController.init();
   }
 }
 
