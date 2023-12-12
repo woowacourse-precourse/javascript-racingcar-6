76eb73406ec57aa853e382ba21b9d90e418801b1
최원서 <dnjstj88@naver.com>
2023-12-12 15:25:50 +0900

    docs: 리드미 수정, constant 수정

--- src/Constants/Message.js
+++ src/Constants/Message.js
@@ -1,6 +1,6 @@
 export const INPUT_MESSAGE = {
   CAR: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
-  TRY: '시도할 횟수는 몇 회인가요?',
+  TRY: '시도할 횟수는 몇 회인가요?\n',
 };
 export const OUTPUT_MESSAGE = {
   RESULT: '실행 결과',
@@ -8,5 +8,6 @@ export const OUTPUT_MESSAGE = {
 };
 export const ERROR_MESSAGE = {
   CAR_LENGH: '[ERROR] 차 이름의 길이는 5이하 이어야 합니다.',
+  CAR_DUPLICATE: '[ERROR] 차 이름은 중복될 수 없습니다.',
   TRYNUM_NUMBER: '[ERROR] 시도 횟수는 숫자를 입력해야 합니다',
 };
