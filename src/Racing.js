0bd68e8dacc80d33fee613b8c601719047bddb49
최원서 <dnjstj88@naver.com>
2023-12-12 16:36:25 +0900

    feat: 우승자 출력 구현

--- src/Racing.js
+++ src/Racing.js
@@ -6,15 +6,16 @@ class Racing {
   #cars;
   #tryNum;
   #status;
+  #statusNum;
   constructor() {}
 
   async getCars() {
     this.#cars = await InputView.readCarName();
-    console.log(this.#cars);
+    this.#status = Array.from({ length: this.#cars.length }, () => '');
+    this.#statusNum = Array.from({ length: this.#cars.length }, () => 0);
   }
   async getTryNum() {
     this.#tryNum = await InputView.readTryNum();
-    console.log(this.#tryNum);
   }
   isGo() {
     let goNum = 0;
@@ -22,6 +23,7 @@ class Racing {
       goNum = getRandomNum();
       if (goNum > 4) {
         this.#status[j] += '-';
+        this.#statusNum[j] += 1;
       }
     }
   }
@@ -31,12 +33,24 @@ class Racing {
     }
     OutputView.LineBreaking();
   }
+  getWinner() {
+    let max = 0;
+    let winner = [];
+    this.#statusNum.map((e) => {
+      if (e > max) max = e;
+    });
+    this.#statusNum.map((e, i) => {
+      if (e == max) winner.push(this.#cars[i]);
+    });
+    Console.print(`최종 우승자 : ${winner.join(', ')}`);
+  }
+
   startRacing() {
-    this.#status = Array.from({ length: this.#cars.length }, () => '');
     for (let i = 0; i < this.#tryNum; i++) {
       this.isGo();
       this.printStatus();
     }
+    this.getWinner();
   }
 }
 
