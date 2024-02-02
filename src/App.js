f0436e44cec51be1479cea84bbd4d45b7cca0d61
pigpgw <ceh20002@naver.com>
2024-02-03 00:10:33 +0900

    add: apply validator in getUserNames

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import {Console} from "@woowacourse/mission-utils";
 import Car from "./Car.js";
+import Validator from "./Validator.js";
 
 class App {
   #carList = [];
 
+  constructor() {
+    this.validator = new Validator();
+  }
+
   async play() {
-    
+    await this.mainLogic();
+  }
+
+  async mainLogic(){
+    await this.getUserCarNames();
+  }
+
+  async getUserCarNames(){
+    const userInput = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    const carNameList = userInput.split(",")
+    // console.log('userinput',userInput, typeof userInput)
+
+    carNameList.forEach(item => {
+      if (this.validator.isValidCarName(item)) this.#carList.push(item)
+    });
   }
 }
 
--- src/Validator.js
+++ src/Validator.js
@@ -1,14 +1,18 @@
 class Validator{
-    isValidCarName(){
 
+    isValidCarName(input){
+        if (!this.isValidNameLength(input)) throw new Error("[ERROR] 올바르지 않은 차 이름 길이입니다.");
+        if (!this.isNumber(input)) throw new Error("[ERROR] 올바르지 않은 차 이름입니다.");
+        return true
     }
 
-    isValidNameLength(){
-
+    isValidNameLength(input){
+        if (input.length <= 5) return true 
     }
 
-    isNumber(){
-
+    isNumber(input){
+        if (isNaN(input)) return true
     }
 }
+
 export default Validator
