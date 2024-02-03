a9cf93c922c04738f28f87fb4c3b33b0c24f037b
pigpgw <ceh20002@naver.com>
2024-02-04 03:30:31 +0900

    refactor: separate inputOutput fnc  in App Class

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import {Console} from "@woowacourse/mission-utils";
 import Car from "./Car.js";
 import Validator from "./Validator.js";
+import InputOutput from "./InputOutput.js";
 import { startComment, winnerCommnet, LINE_BREAK } from "./Constant.js";
 
 class App {
@@ -9,6 +10,7 @@ class App {
 
   constructor() {
     this.validator = new Validator();
+    this.inputOutput = new InputOutput();
   }
 
   async play() {
@@ -22,17 +24,9 @@ class App {
   }
 
   async settingGarage(){
-    const carNameList = await this.getCarNames();
+    const carNameList = await this.inputOutput.getCarNames();
     this.makingCar(carNameList);
   }
-
-  // eslint-disable-next-line class-methods-use-this
-  async getCarNames(){
-    const userInput = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-    const carNameList = userInput.split(",");
-    Console.print(`${carNameList}`);
-    return carNameList;
-  }  
 
   makingCar(carNameList){
     carNameList.forEach(item => {
@@ -44,15 +38,10 @@ class App {
   }
 
   async startRace(){
-    startComment;
-    const userInputCount = await this.getAttemptCount();
+    // Console.print(startComment);
+    this.inputOutput.print(startComment)
+    const userInputCount = await this.inputOutput.getAttemptCount();
     this.simulateRace(userInputCount);
-  }
-
-  async getAttemptCount(){
-    const userInput = await Console.readLineAsync("시도할 횟수는 몇 회인가요?");
-    if (this.validator.isNumber(userInput)) throw new Error("[ERROR] 올바르지 않은 형식의 입력입니다.");
-    return userInput;
   }
 
   simulateRace(count){
@@ -61,7 +50,7 @@ class App {
         car.tryAdvance();
         Console.print(`${car.getName()} : ${car.getMovedDistance()}`)
       });
-      LINE_BREAK;
+      this.inputOutput.print(LINE_BREAK)
     }
   }
 
@@ -81,7 +70,8 @@ class App {
   announceWinners(scoreList){
     const winnerScore = Math.max(...scoreList);
     const winner = this.#garage.filter(car => car.getMovedDistance().length === winnerScore).map(car => car.getName());
-    Console.print(`${winnerCommnet} : ${winner}`)
+    // Console.print(`${winnerCommnet} : ${winner}`)
+    this.inputOutput.print(`${winnerCommnet} : ${winner}`);
   }
 }
 
